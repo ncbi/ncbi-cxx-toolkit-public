1a0b1a5a5e32482df5ff830202273ff857e9ecdb
Denis Vakatov <vakatov@ncbi.nlm.nih.gov>
1998-10-05 19:43:38 +0000

    Initial revision
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@177 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ include/corelib/ncbimisc.hpp
@@ -0,0 +1,58 @@
+#ifndef NCBISTD__HPP
+#define NCBISTD__HPP
+
+/*  $RCSfile$  $Revision$  $Date$
+* ===========================================================================
+*
+*                            PUBLIC DOMAIN NOTICE
+*               National Center for Biotechnology Information
+*
+*  This software/database is a "United States Government Work" under the
+*  terms of the United States Copyright Act.  It was written as part of
+*  the author's official duties as a United States Government employee and
+*  thus cannot be copyrighted.  This software/database is freely available
+*  to the public for use. The National Library of Medicine and the U.S.
+*  Government have not placed any restriction on its use or reproduction.
+*
+*  Although all reasonable efforts have been taken to ensure the accuracy
+*  and reliability of the software and data, the NLM and the U.S.
+*  Government do not and cannot warrant the performance or results that
+*  may be obtained by using this software or data. The NLM and the U.S.
+*  Government disclaim all warranties, express or implied, including
+*  warranties of performance, merchantability or fitness for any particular
+*  purpose.
+*
+*  Please cite the author in any work or product based on this material.
+*
+* ===========================================================================
+*
+* Author:  Denis Vakatov
+*
+* File Description:
+*   The NCBI C++/STL use hints
+*
+* --------------------------------------------------------------------------
+* $Log$
+* Revision 1.1  1998/10/05 19:43:38  vakatov
+* Initial revision
+*
+* ==========================================================================
+*/
+
+
+#if ( __SGI_STL >= 0x300 )  &&  ( __SGI_STL_PORT >= 0x3010 )
+
+#include <stl_config.h>
+
+// use standard STL namespace
+__STL_USING_NAMESPACE;
+
+// replace "namespace" by "class" if namespaces are not supported
+#if !defined(__STL_NAMESPACES)  ||  defined(__STL_NO_NAMESPACES)
+#define namespace class
+#endif
+
+#endif /* __SGI_STL && __SGI_STL_PORT */
+
+
+#endif /* NCBISTD__HPP */
