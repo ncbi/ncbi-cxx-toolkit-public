70441a363d5c2e3093d9314b88d5757592cbe0e8
sandomir <sandomir@ncbi.nlm.nih.gov>
1998-12-09 17:45:37 +0000

    *** empty log message ***
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@377 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/corelib/ncbiapp.cpp
+++ src/corelib/ncbiapp.cpp
@@ -32,6 +32,9 @@
 *
 * ---------------------------------------------------------------------------
 * $Log$
+* Revision 1.8  1998/12/09 17:45:37  sandomir
+* *** empty log message ***
+*
 * Revision 1.7  1998/12/09 16:49:56  sandomir
 * CCgiApplication added
 *
@@ -58,6 +61,7 @@
 
 #include <ncbistd.hpp>
 #include <ncbiapp.hpp>
+#include <cgiapp.hpp>
 
 BEGIN_NCBI_SCOPE
 
