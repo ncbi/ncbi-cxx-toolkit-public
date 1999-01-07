8801ffddb1bf788eeb678169982f1bdf4e2c82a5
Denis Vakatov <vakatov@ncbi.nlm.nih.gov>
1999-01-07 22:03:42 +0000

    s_URL_Decode():  typo fixed
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@483 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/cgi/ncbicgi.cpp
+++ src/cgi/ncbicgi.cpp
@@ -30,6 +30,9 @@
 *
 * --------------------------------------------------------------------------
 * $Log$
+* Revision 1.20  1999/01/07 22:03:42  vakatov
+* s_URL_Decode():  typo fixed
+*
 * Revision 1.19  1999/01/07 21:15:22  vakatov
 * Changed prototypes for URL_DecodeString() and URL_EncodeString()
 *
@@ -385,6 +388,7 @@ static int s_HexChar(char ch) THROWS_NONE
 
 
 // URL-decode string "str" into itself
+// Return 0 on success;  otherwise, return 1-based error position
 static SIZE_TYPE s_URL_Decode(string& str)
 {
     SIZE_TYPE len = str.length();
@@ -400,7 +404,7 @@ static SIZE_TYPE s_URL_Decode(string& str)
             int i1 = s_HexChar(str[pos+1]);
             if (i1 < 0  ||  15 < i1)
                 return (pos + 2);
-            int i2 = s_HexChar(str[pos+1]);
+            int i2 = s_HexChar(str[pos+2]);
             if (i2 < 0  ||  15 < i2)
                 return (pos + 3);
             str[p] = s_HexChar(str[pos+1]) * 16 + s_HexChar(str[pos+2]);
@@ -454,7 +458,7 @@ static SIZE_TYPE s_ParseIsIndex(const string& str,
 
         string name = str.substr(beg, end - beg);
         if ((err_pos = s_URL_Decode(name)) != 0)
-            return beg + err_pos + 1;  // error
+            return beg + err_pos;  // error
 
         // store
         if ( indexes ) {
@@ -630,7 +634,7 @@ SIZE_TYPE CCgiRequest::ParseEntries(const string& str, TCgiEntries& entries)
 
         string name = str.substr(beg, mid - beg);
         if ((err_pos = s_URL_Decode(name)) != 0)
-            return beg + err_pos + 1;  // error
+            return beg + err_pos;  // error
 
         // parse and URL-decode value(if any)
         string value;
@@ -644,7 +648,7 @@ SIZE_TYPE CCgiRequest::ParseEntries(const string& str, TCgiEntries& entries)
 
             value = str.substr(mid, end - mid);
             if ((err_pos = s_URL_Decode(value)) != 0)
-                return mid + err_pos + 1;  // error
+                return mid + err_pos;  // error
 
             beg = end + 1;
         } else {  // has no value
@@ -681,49 +685,34 @@ extern string URL_EncodeString(const string& str)
     static const char s_Encode[256][4] = {
         "%00", "%01", "%02", "%03", "%04", "%05", "%06", "%07",
         "%08", "%09", "%0A", "%0B", "%0C", "%0D", "%0E", "%0F",
-
         "%10", "%11", "%12", "%13", "%14", "%15", "%16", "%17",
         "%18", "%19", "%1A", "%1B", "%1C", "%1D", "%1E", "%1F",
-
         "+",   "!",   "%22", "%23", "$",   "%25", "%26", "'",
         "(",   ")",   "*",   "%2B", ",",   "-",   ".",   "%2F",
-
         "0",   "1",   "2",   "3",   "4",   "5",   "6",   "7",
         "8",   "9",   "%3A", "%3B", "%3C", "%3D", "%3E", "%3F",
-
         "%40", "A",   "B",   "C",   "D",   "E",   "F",   "G",
         "H",   "I",   "J",   "K",   "L",   "M",   "N",   "O",
-
         "P",   "Q",   "R",   "S",   "T",   "U",   "V",   "W",
         "X",   "Y",   "Z",   "%5B", "%5C", "%5D", "%5E", "_",
-
         "%60", "a",   "b",   "c",   "d",   "e",   "f",   "g",
         "h",   "i",   "j",   "k",   "l",   "m",   "n",   "o",
-
         "p",   "q",   "r",   "s",   "t",   "u",   "v",   "w",
         "x",   "y",   "z",   "%7B", "%7C", "%7D", "%7E", "%7F",
-
         "%80", "%81", "%82", "%83", "%84", "%85", "%86", "%87",
         "%88", "%89", "%8A", "%8B", "%8C", "%8D", "%8E", "%8F",
-
         "%90", "%91", "%92", "%93", "%94", "%95", "%96", "%97",
         "%98", "%99", "%9A", "%9B", "%9C", "%9D", "%9E", "%9F",
-
         "%A0", "%A1", "%A2", "%A3", "%A4", "%A5", "%A6", "%A7",
         "%A8", "%A9", "%AA", "%AB", "%AC", "%AD", "%AE", "%AF",
-
         "%B0", "%B1", "%B2", "%B3", "%B4", "%B5", "%B6", "%B7",
         "%B8", "%B9", "%BA", "%BB", "%BC", "%BD", "%BE", "%BF",
-
         "%C0", "%C1", "%C2", "%C3", "%C4", "%C5", "%C6", "%C7",
         "%C8", "%C9", "%CA", "%CB", "%CC", "%CD", "%CE", "%CF",
-
         "%D0", "%D1", "%D2", "%D3", "%D4", "%D5", "%D6", "%D7",
         "%D8", "%D9", "%DA", "%DB", "%DC", "%DD", "%DE", "%DF",
-
         "%E0", "%E1", "%E2", "%E3", "%E4", "%E5", "%E6", "%E7",
         "%E8", "%E9", "%EA", "%EB", "%EC", "%ED", "%EE", "%EF",
-
         "%F0", "%F1", "%F2", "%F3", "%F4", "%F5", "%F6", "%F7",
         "%F8", "%F9", "%FA", "%FB", "%FC", "%FD", "%FE", "%FF"
     };
