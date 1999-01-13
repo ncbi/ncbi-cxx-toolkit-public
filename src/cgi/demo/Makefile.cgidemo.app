f182625603f748eca6c4a59937e7c8d5e8d72081
lewisg <lewisg@ncbi.nlm.nih.gov>
1999-01-13 14:46:45 +0000

    simple cgi demo
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@495 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ src/cgi/demo/Makefile.cgidemo.app
@@ -0,0 +1,11 @@
+#################################
+# $Id$
+# Author:  Denis Vakatov (vakatov@ncbi.nlm.nih.gov)
+#################################
+
+# Build test CoreLib application "cgidemo"
+#################################
+
+APP = cgidemo
+OBJ = cgidemo
+LIB = xncbi
---
+++ src/cgi/demo/cgidemo.cpp
@@ -0,0 +1,72 @@
+/*  $Id$
+* ===========================================================================
+*
+*                            PUBLIC DOMAIN NOTICE
+*               National Center for Biotechnology Information
+*
+*  This software/database is a "United States Government Work" under the
+*  terms of the United States Copyright Act.  It was written as part of
+*  the author's official duties as a United States Government employee and
+*  thus cannot be copyrighted.  This software/database is freely available
+*  to the public for use. The National Library of Medicine and the U.S.
+*  Government have not placed any restriction on its use or reproduction.
+*
+*  Although all reasonable efforts have been taken to ensure the accuracy
+*  and reliability of the software and data, the NLM and the U.S.
+*  Government do not and cannot warrant the performance or results that
+*  may be obtained by using this software or data. The NLM and the U.S.
+*  Government disclaim all warranties, express or implied, including
+*  warranties of performance, merchantability or fitness for any particular
+*  purpose.
+*
+*  Please cite the author in any work or product based on this material.
+*
+* ===========================================================================
+*
+* Author:  Lewis Geer
+*
+* File Description:
+*   sample cgi program
+*
+* ---------------------------------------------------------------------------
+* $Log$
+* Revision 1.1  1999/01/13 14:46:45  lewisg
+* simple cgi demo
+*
+* ===========================================================================
+*/
+
+// toolkit public headers
+#include <corelib/ncbicgi.hpp>
+#include <corelib/ncbicgir.hpp>
+// project private headers
+
+USING_NCBI_SCOPE;  // this turns on the ncbi namespace
+
+
+// the extern "C" main(...) is necessary to make main() visible
+// outside of the ncbi namespace
+
+extern "C" int main(int argc, char *argv[]) 
+{
+    CCgiRequest Request(argc, argv);
+    CCgiResponse Response;
+
+    // write out the Content-type header
+    Response.WriteHeader();
+
+    // Get the multimap.  see http://www.sgi.com/Technology/STL/Multimap.html
+    // on how to manipulate multimaps
+    TCgiEntries Entries = Request.GetEntries();
+
+    // this program expects queries of the form cgidemo?name=Fred
+    // the following line extracts the "Fred"
+    string Name = (Entries.find("name"))->second;
+
+    // print out the results
+    Response.out() << "<html><body>Hello, " << Name;
+    Response.out() << "</body></html>" << endl;
+    Response.Flush();
+
+    return 0;  
+}
