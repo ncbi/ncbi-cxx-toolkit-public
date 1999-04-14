a52ad9fd48500908496795db0c387bdf9b6b5d7a
Denis Vakatov <vakatov@ncbi.nlm.nih.gov>
1999-04-14 19:20:16 +0000

    + NCBI_OBSOLETE_STR_COMPARE + HAVE_NO_AUTO_PTR + HAVE_NO_SNPRINTF + HAVE_LIMITS
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@563 78c7ea69-d796-4a43-9a09-de51944f1b03

--- acconfig.h
+++ acconfig.h
@@ -16,3 +16,11 @@
 /* Can use exception specifications("throw(...)" after func. proto) */
 #undef NCBI_USE_THROW_SPEC
 
+/* Pre-standard basic_string::compare() -- most probably, <bastring> */
+#undef NCBI_OBSOLETE_STR_COMPARE
+
+/* "auto_ptr" template class is not implemented in <memory> */
+#undef HAVE_NO_AUTO_PTR
+
+/* "snprintf()" is not defined in <stdio.h> */
+#undef HAVE_NO_SNPRINTF
