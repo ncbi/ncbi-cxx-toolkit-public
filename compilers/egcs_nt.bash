50e53cfe2f95d03075f67569697356420fc7409b
Denis Vakatov <vakatov@ncbi.nlm.nih.gov>
1999-04-14 22:00:32 +0000

    Initial revision
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@578 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ compilers/egcs_nt.bash
@@ -0,0 +1,33 @@
+
+# This "bash" script makes some "configure" tests to work
+# in the CYGWIN32_NT "bash" environment with EGC C/C++
+# preprocessor/compiler/linker.
+#
+# NOTE:  "configure" script was produced by "autoconf" ver. 2.13,
+#
+# (By Denis Vakatov,  vakatov@ncbi.nlm.nih.gov)  ($Revision$)
+
+#############################################################################
+
+
+USR_BUILD_DIR="$1"
+DEF_BUILD_DIR="egc"
+
+set -a
+
+NCBI_C_INCLUDE=""
+NCBI_C_LIB=""
+
+CONFIG_SHELL="bash"
+$CONFIG_SHELL ./configure --exec_prefix="${USR_BUILD_DIR:=$DEF_BUILD_DIR}"
+
+
+cat << EOD
+Usage:
+  'bash $0 [dir]' -- configure and install to "dir"
+
+Notes:
+  "dir" is the optional destination directory name ("$DEF_BUILD_DIR" by deflt).
+  You can change the path to compiler components in the very
+  beginning of this("$0") script.
+EOD
