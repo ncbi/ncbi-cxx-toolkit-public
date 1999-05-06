db283cb1afdf061d3cfc8d9d3f9214eb18d68998
Denis Vakatov <vakatov@ncbi.nlm.nih.gov>
1999-05-06 22:59:41 +0000

    Made all args(but the first one) to pass directly to "configure"; e.g. to build a release(non-debug) version:  sunpro50.sh "."  --without-debug Separate between debug and release C libs:  NCBI_C_*_LIB Added THREAD_LIBS, NETWORK_LIBS and FASTCGI_LIBS
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@641 78c7ea69-d796-4a43-9a09-de51944f1b03

--- compilers/sunpro50.sh
+++ compilers/sunpro50.sh
@@ -9,25 +9,33 @@
 USR_BUILD_DIR="$1"
 DEF_BUILD_DIR="."
 
-USR_OPT_FLAGS="$2"
-DEF_OPT_FLAGS="-g -D_DEBUG"
+MT_FLAGS="-mt"
 
 set -a
 
 CPATH="/netopt/SUNWspro6/bin"
+PM_PATH="/net/neptune/pubmed1/Cvs5"
+NCBI_C_PATH="$NCBI/ver0.0/ncbi"
+
 
 CC="$CPATH/cc"
-CFLAGS="-Xc ${USR_OPT_FLAGS:=$DEF_OPT_FLAGS}"
+CFLAGS="-Xc ${MT_FLAGS}"
 CXX="$CPATH/CC"
-CXXFLAGS="+w +w2 ${USR_OPT_FLAGS:=$DEF_OPT_FLAGS} -DNCBI_USE_NEW_IOSTREAM -DNCBI_USE_NEW_HEADERS -DFAST_CGI"
+CXXFLAGS="+w +w2 -DNCBI_USE_NEW_HEADERS ${MT_FLAGS}"
 AR="$CXX -xar -o"
 RANLIB=":"
-LDFLAGS="-xildoff ${USR_OPT_FLAGS:=$DEF_OPT_FLAGS}"
+LDFLAGS="-xildoff"
+LIBS="-Bstatic -L$CPATH/../SC5.0/lib -lm -Bdynamic -ldl"
 
-PM_PATH="/net/neptune/pubmed1/Cvs5"
-NCBI_C_INCLUDE="$NCBI/ver0.0/ncbi/include"
-NCBI_C_LIB="$NCBI/ver0.0/ncbi/altlib"
+NCBI_C_INCLUDE="$NCBI_C_PATH/include"
+NCBI_C_DEBUG_LIB="$NCBI_C_PATH/altlib"
+NCBI_C_RELEASE_LIB="$NCBI_C_PATH/lib"
+
+THREAD_LIBS="-lthread -lpthread"
+NETWORK_LIBS="-lsocket -lnsl"
+FASTCGI_LIBS="-lfcgi5.0"
 
 KeepStateTarget=".KEEP_STATE:"
 
-sh configure --exec_prefix=${USR_BUILD_DIR:=$DEF_BUILD_DIR}
+shift
+sh configure --exec_prefix=${USR_BUILD_DIR:=$DEF_BUILD_DIR} $*
