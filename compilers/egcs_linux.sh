56ff26b10f9aed2ec62ae138cc8cdd292797ac97
Denis Vakatov <vakatov@ncbi.nlm.nih.gov>
1999-05-10 15:22:06 +0000

    Made all paths be configurable(by the outside env. variables having the same name};  The default values are for the internal "coremake" build.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@660 78c7ea69-d796-4a43-9a09-de51944f1b03

--- compilers/egcs_linux.sh
+++ compilers/egcs_linux.sh
@@ -11,10 +11,10 @@ DEF_BUILD_DIR="egcs_linux"
 
 set -a
 
-PM_PATH="/net/neptune/pubmed1/Cvs5"
-NCBI_C_INCLUDE="/home/vakatov/ncbi/include"
-NCBI_C_RELEASE_LIB="/home/vakatov/ncbi/lib"
-NCBI_C_DEBUG_LIB="/home/vakatov/ncbi/lib"
+PM_PATH=${PM_PATH:="/net/neptune/pubmed1/Cvs5"}
+NCBI_C_INCLUDE=${NCBI_C_INCLUDE:="/export/home/coremake/ncbi/include"}
+NCBI_C_RELEASE_LIB=${NCBI_C_RELEASE_LIB:="/export/home/coremake/ncbi/lib"}
+NCBI_C_DEBUG_LIB=${NCBI_C_DEBUG_LIB:="/export/home/coremake/ncbi/altlib"}
 
 if test $# -gt 0; then shift ; fi
 sh configure --exec_prefix=${USR_BUILD_DIR:=$DEF_BUILD_DIR} $*
