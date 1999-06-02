7805e5b283c041c73fe3f8d2ccc9768bea5b4bec
Dmitriy (Dima) Beloslyudtsev <beloslyu@ncbi.nlm.nih.gov>
1999-06-02 20:48:15 +0000

    first working version
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@709 78c7ea69-d796-4a43-9a09-de51944f1b03

--- compilers/egcs_sun.sh
+++ compilers/egcs_sun.sh
@@ -11,8 +11,14 @@ DEF_BUILD_DIR="egcs_sun"
 
 set -a
 
+CC="gcc"
+CXX="g++"
+CXXFLAGS="-fsquangle"
+#AR="/netopt/SUNWspro6/bin/CC -xar -o"
 PM_PATH="/net/neptune/pubmed1/Cvs5"
-NCBI_C_INCLUDE="$NCBI/ver0.0/ncbi/include"
-NCBI_C_LIB="$NCBI/ver0.0/ncbi/altlib"
+NCBI_C_INCLUDE=${NCBI_C_INCLUDE:="$NCBI/ver0.0/ncbi/include"}
+NCBI_C_LIB=${NCBI_C_RELEASE_LIB:="$NCBI/ver0.0/ncbi/altlib"}
+NCBI_C_DEBUG_LIB=${NCBI_C_DEBUG_LIB:="$NCBI/ver0.0/ncbi/altlib"}
 
+if test $# -gt 0; then shift ; fi
 sh configure --exec_prefix=${USR_BUILD_DIR:=$DEF_BUILD_DIR}
