1fc63b74d747f7ce67334a671d3a5f6e55b0f249
Dmitriy (Dima) Beloslyudtsev <beloslyu@ncbi.nlm.nih.gov>
1999-06-08 13:18:29 +0000

    fixed some inportability
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@737 78c7ea69-d796-4a43-9a09-de51944f1b03

--- scripts/if_diff.sh
+++ scripts/if_diff.sh
@@ -46,7 +46,7 @@ test $# -gt 1  ||  Usage "too few cmd.-line parameters"
 args=
 i=0
 for f in $* ; do
-  i=$[i+1]
+  i=`expr $i + 1`
   if test $i -eq $# ; then
     dest="$f"
   else
@@ -54,22 +54,22 @@ for f in $* ; do
   fi
 done
 
-if test ! -d $dest ; then
-  test -f $1  ||
+if test ! -d "$dest" ; then
+  test -f "$1"  ||
     Usage "the destination is not a directory, and the source is not a file"
   test $# -lt 3  ||
     Usage "the destination is not a directory, and too many arguments"
-  ExecAction "$1" $dest
+  ExecAction "$1" "$dest"
   exit 0
 fi
 
 
-test -d $dest  ||  Usage "destination is neither file nor directory"
+test -d "$dest"  ||  Usage "destination is neither file nor directory"
 
 i=2
 for f in $args ; do
-  i=$[i+1]
-  test -f $f  ||  Usage "\"$f\"(field #$i) is not a file"
+  i=`expr $i + 1`
+  test -f "$f"  ||  Usage "\"$f\"(field #$i) is not a file"
 done
 
 for f in $args ; do
