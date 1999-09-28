a2ffa78a9b66a5f91fbe060e8eae40ed4538b017
Denis Vakatov <vakatov@ncbi.nlm.nih.gov>
1999-09-28 22:25:35 +0000

    Works on my PC-NT-MSVC6.0
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@914 78c7ea69-d796-4a43-9a09-de51944f1b03

--- compilers/msvc_nt.bash
+++ compilers/msvc_nt.bash
@@ -1,5 +1,7 @@
 #############################################################################
 # Setup the local working environment for the "configure" script
+# ATTENTION:  Use BASH(only!) to launch:  bash compilers/msvc_nt.bash
+#
 #   Compiler:   MSVC++ (6.0 SP3)
 #   OS:         Win-NT (4.0)
 #   Processor:  Intel  (i586)
@@ -11,19 +13,25 @@
 # Path to the MSVC++ package
 MSVC_DRIVE="D"
 MSVC_PATH="Program Files/Microsoft Visual Studio"
+PATH_DOS="$MSVC_DRIVE:\\\\`echo $MSVC_PATH | sed 's%/%\\\\\\\\%g'`"
+PATH_BSH="//$MSVC_DRIVE/$MSVC_PATH"
+
+# Export the following interpreter variables
+set -a
 
 # Shell for the "configure" script and makefiles
 CONFIG_SHELL="bash"
 
-# Working environment
-PATH_BSH="//$MSVC_DRIVE/$MSVC_PATH"
+# MSVC working environment
 LIB="$PATH_DOS\\VC98\\lib"
 INCLUDE="$PATH_DOS\\VC98\\include"
 
 # Path
-PATH_DOS="$MSVC_DRIVE:\\\\`echo $MSVC_PATH | sed 's%/%\\\\\\\\%g'`"
 PATH="$PATH_BSH/Common/MSDev98/Bin:$PATH_BSH/VC98/Bin:$PATH"
 
 # Compiler
 CC="cl.exe /TC"
 CXX="cl.exe /TP"
+
+## Configure
+${CONFIG_SHELL} `dirname $0`/../configure "$@"
