95ef6ce3f729c0e2136b99efa2e077c3cb33eb2c
Denis Vakatov <vakatov@ncbi.nlm.nih.gov>
1999-10-12 16:33:50 +0000

    Moved all TEST suite code from "ncbi_buffer.c" to "test/test_ncbi_buffer.c"
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@946 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/connect/ncbi_buffer.c
+++ src/connect/ncbi_buffer.c
@@ -30,20 +30,22 @@
  *
  * ---------------------------------------------------------------------------
  * $Log$
+ * Revision 6.2  1999/10/12 16:32:14  vakatov
+ * Moved all TEST suite code from "ncbi_buffer.c" to "test/test_ncbi_buffer.c"
+ *
  * Revision 6.1  1999/08/17 19:45:22  vakatov
  * Moved all real code from NCBIBUF to NCBI_BUFFER;  the code has been cleaned
  * from the NCBI C toolkit specific types and API calls.
  * NCBIBUF module still exists for the backward compatibility -- it
  * provides old NCBI-wise interface.
- *
  * ===========================================================================
  */
+
+#include <ncbi_buffer.h>
 
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
-
-#include <ncbi_buffer.h>
 
 
 /* Buffer chunk
@@ -258,151 +260,4 @@ extern BUF BUF_Destroy(BUF buf)
   return 0;
 }
 
-
-
-/******************************************************************************
- *  TEST
- */
-
-#ifdef TEST_MODULE__NCBI_BUFFER
-
-#include <stdio.h>
-
-
-#  if !defined(NDEBUG) || defined(_DEBUG)
-#    define verify(expr)  assert(expr)
-#  else
-#    define verify(expr)  (void)(expr)
-#  endif /* !NDEBUG | _DEBUG */
-
-
-static unsigned s_Rand(void)
-{ /* a uniform random number generator */
-  static unsigned s_Random = 1;
-  s_Random = s_Random * 1103515245 + 12345;
-  return (s_Random / 65536) % 32768;
-}
-
-
-int main(void)
-{ /* test application */
-#  define X_MAX_N_IO  (unsigned) 4
-#  define X_MAX_READ  (size_t)   (3 * BUF_DEF_CHUNK_SIZE)
-#  define X_TIMES     (unsigned) (s_Rand() % X_MAX_N_IO)
-#  define X_BYTES     (size_t)   (s_Rand() % X_MAX_READ)
-
-  BUF buf = 0;
-  int/*bool*/ do_loop = 1 /* true */;
-
-  /* a simple test */
-  {{
-    char charbuf[128];
-    verify(BUF_PushBack(&buf, (const char*)"0", 1));
-    verify(BUF_Write(&buf, (const char*)"1", 1));
-    verify(BUF_Peek(buf, charbuf, sizeof(charbuf)) == 2);
-    verify(BUF_PushBack(&buf, (const char*)"BB", 2));
-    verify(BUF_PushBack(&buf, (const char*)"aa", 2));
-    verify(BUF_Write(&buf, (const char*)"23", 3));
-    verify(BUF_Read(buf, charbuf, sizeof(charbuf)) == 9);
-    assert(strcmp(charbuf, (const char*)"aaBB0123") == 0);
-    buf = BUF_Destroy(buf);
-  }}
-
-  /* read up to the very end of input stream */
-  while ( do_loop ) {
-    char charbuf[X_MAX_READ];
-    unsigned i, n_times;
-
-    /* read from the input stream, write to the NCBI IO-buf */
-    n_times = X_TIMES;
-    for (i = 0;  i < n_times;  i++) {
-      size_t n_bytes = X_BYTES;
-      if ( !n_bytes )
-        continue;
-      n_bytes = fread(charbuf, 1, n_bytes, stdin);
-      fprintf(stderr, "\
-STDIN     %5lu\n", (unsigned long)n_bytes);
-      if ( !n_bytes ) {
-        do_loop = 0 /* false */; /* end of the input stream */
-        break;
-      }
-      verify(BUF_Write(&buf, charbuf, n_bytes));
-      fprintf(stderr, "\
-BUF_Write %5lu\n", (unsigned long)n_bytes);
-    }
-
-    /* peek & read from the NCBI IO-buf, write to the output stream */
-    n_times = X_TIMES;
-    for (i = 0;  i < n_times  &&  BUF_Size(buf);  i++) {
-      int/*bool*/ do_peek = (s_Rand() % 2 == 0);
-      size_t n_peek;
-      size_t n_bytes = X_BYTES;
-      if ( !n_bytes )
-        continue;
-
-      /* peek from the NCBI IO-buf */
-      if ( do_peek ) {
-        unsigned j, n_peek_times = s_Rand() % 3 + 1;
-        for (j = 0;  j < n_peek_times;  j++) {
-          n_peek = BUF_Peek(buf, charbuf, n_bytes);
-          fprintf(stderr, "\
-\tBUF_Peek %5lu\n", (unsigned long)n_peek);
-        }
-      }
-
-      /* read(or just discard) the data */
-      if (do_peek  &&  s_Rand() % 2 == 0)
-        n_bytes = BUF_Read(buf, 0, n_bytes);
-      else
-        n_bytes = BUF_Read(buf, charbuf, n_bytes);
-
-      fprintf(stderr, "\
-\t\tBUF_Read %5lu\n", (unsigned long)n_bytes);
-      assert(!do_peek  ||  n_bytes == n_peek);
-
-      /* push back & re-read */
-      if (s_Rand() % 3 == 0) {
-        size_t n_pushback = s_Rand() % n_bytes;
-        verify(BUF_PushBack(&buf, charbuf + n_bytes - n_pushback, n_pushback));
-        verify(BUF_Read(buf, charbuf + n_bytes - n_pushback, n_pushback));
-      }
-      
-      /* write the read data to the output stream */
-      verify(n_bytes == fwrite(charbuf, 1, n_bytes, stdout));
-      fprintf(stderr, "\
-\t\tSTDOUT   %5lu\n", (unsigned long)n_bytes);
-    }
-  }
-
-  /* flush the IO-buf to the output stream */
-  while ( BUF_Size(buf) ) {
-    char charbuf[256];
-    size_t n_bytes = BUF_Read(buf, charbuf, sizeof(charbuf));
-    {{
-      char   tmp[sizeof(charbuf)];
-      size_t n_pushback = s_Rand() % 64;
-      if (n_pushback > n_bytes)
-        n_pushback = n_bytes;
-
-      verify(BUF_PushBack(&buf, charbuf + n_bytes - n_pushback, n_pushback));
-      verify(BUF_Read(buf, tmp, n_pushback)
-             == n_pushback);
-      memcpy(charbuf + n_bytes - n_pushback, tmp, n_pushback);
-    }}
-    fprintf(stderr, "\
-\t\tBUF_Read/flush %5lu\n", (unsigned long)n_bytes);
-    assert(n_bytes);
-    verify(n_bytes == fwrite(charbuf, 1, n_bytes, stdout));
-    fprintf(stderr, "\
-\t\tSTDOUT  /flush %5lu\n", (unsigned long)n_bytes);
-  }
-
-  /* cleanup & exit */
-  BUF_Destroy(buf);
-  return 0;
-}
-
-#endif /* TEST_MODULE__NCBI_BUFFER */
-
-/* EOF */
 
