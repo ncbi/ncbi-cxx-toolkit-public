c645bd69c085cdc4e1e5697acc343f9896b8c667
Eugene Vasilchenko <vasilche@ncbi.nlm.nih.gov>
1999-11-15 19:36:22 +0000

    Fixed warnings on GCC
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@1023 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/serial/datatool/code.cpp
+++ src/serial/datatool/code.cpp
@@ -1,3 +1,41 @@
+/*  $Id$
+* ===========================================================================
+*
+*                            PUBLIC DOMAIN NOTICE
+*               National Center for Biotechnology Information
+*
+*  This software/database is a "United States Government Work" under the
+*  terms of the United States Copyright Act.  It was written as part of
+*  the author's official duties as a United States Government employee and
+*  thus cannot be copyrighted.  This software/database is freely available
+*  to the public for use. The National Library of Medicine and the U.S.
+*  Government have not placed any restriction on its use or reproduction.
+*
+*  Although all reasonable efforts have been taken to ensure the accuracy
+*  and reliability of the software and data, the NLM and the U.S.
+*  Government do not and cannot warrant the performance or results that
+*  may be obtained by using this software or data. The NLM and the U.S.
+*  Government disclaim all warranties, express or implied, including
+*  warranties of performance, merchantability or fitness for any particular
+*  purpose.
+*
+*  Please cite the author in any work or product based on this material.
+*
+* ===========================================================================
+*
+* Author: Eugene Vasilchenko
+*
+* File Description:
+*   Class code generator
+*
+* ---------------------------------------------------------------------------
+* $Log$
+* Revision 1.21  1999/11/15 19:36:13  vasilche
+* Fixed warnings on GCC
+*
+* ===========================================================================
+*/
+
 #include "code.hpp"
 #include "filecode.hpp"
 #include "type.hpp"
@@ -5,12 +43,13 @@
 
 static inline
 void
-Write(CNcbiOstream& out, const CNcbiOstrstream& source)
+Write(CNcbiOstream& out, const CNcbiOstrstream& src)
 {
+    CNcbiOstrstream& source = const_cast<CNcbiOstrstream&>(src);
     size_t size = source.pcount();
     if ( size != 0 ) {
-        out.write(const_cast<CNcbiOstrstream&>(source).str(), size);
-        const_cast<CNcbiOstrstream&>(source).freeze(false);
+        out.write(source.str(), size);
+        source.freeze(false);
     }
 }
 
