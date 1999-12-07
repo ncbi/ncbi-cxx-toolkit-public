e9830ae88c3f24ec8fd58b7b0e100ed6c78cc971
Denis Vakatov <vakatov@ncbi.nlm.nih.gov>
1999-12-07 22:02:29 +0000

    "grep":  escaped dots "sed":   + underscore
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@1098 78c7ea69-d796-4a43-9a09-de51944f1b03

--- compilers/cxx_filter.WorkShop5.sh
+++ compilers/cxx_filter.WorkShop5.sh
@@ -14,27 +14,27 @@ egrep -v -e '
 ^"/netopt/SUNWspro6/SC5\.0/include/CC/\./strstream", line 147:
 ^"/netopt/SUNWspro6/SC5\.0/include/CC/\./strstream", line 182:
 ^"/netopt/SUNWspro6/SC5\.0/include/CC/\./strstream", line 222:
-^"/netopt/SUNWspro6/SC5\.0/include/CC/std/errno.h", line 20:
-^"/netopt/SUNWspro6/SC5.0/include/CC/errno_iso_SUNWCC.h", line 16:
-^"/netopt/SUNWspro6/SC5.0/include/CC/\./fstream", line 275:
-^"/netopt/SUNWspro6/SC5.0/include/CC/\./fstream", line 319:
-^"/netopt/SUNWspro6/SC5.0/include/CC/./fstream", line 362: 
+^"/netopt/SUNWspro6/SC5\.0/include/CC/std/errno\.h", line 20:
+^"/netopt/SUNWspro6/SC5\.0/include/CC/errno_iso_SUNWCC\.h", line 16:
+^"/netopt/SUNWspro6/SC5\.0/include/CC/\./fstream", line 275:
+^"/netopt/SUNWspro6/SC5\.0/include/CC/\./fstream", line 319:
+^"/netopt/SUNWspro6/SC5\.0/include/CC/\./fstream", line 362: 
 ^"/netopt/ncbi_tools/include/..*\.h", line .*: Warning \(Anachronism\): Attempt to redefine .* without using #undef\.$
-^"/netopt/Sybase/clients/current/include/sybdb.h", line [0-9]*: Warning: There are two consecutive underbars in "db__
+^"/netopt/Sybase/clients/current/include/sybdb\.h", line [0-9]*: Warning: There are two consecutive underbars in "db__
 : Warning: Could not find source for std::is[a-z][a-z]*(int)
 : Warning: Could not find source for std::toupper\(int\)\.
 : Warning: Could not find source for std::tolower\(int\)\.
 : Warning: String literal converted to char\* in
 :     Where: While specializing "std::basic_.*fstream<char, std::char_traits<char>>".
-:     Where: Specialized in non-template code.
-^[0-9][0-9]* Warning\(s\) detected.
+:     Where: Specialized in non-template code\.
+^[0-9][0-9]* Warning\(s\) detected\.
 ^: ' |
 
 sed '
 s/std::basic_string<char, std::char_traits<char>, std::allocator<char>>/std::string/g
-s/std::\([a-z]*\)<\([^,<>]*\), std::allocator<\2>>/std::\1<\2>/g
-s/std::\([a-z]*\)<\([^,<>]*\), std::less<\2>, std::allocator<\2>>/std::\1<\2>/g
-s/std::\([a-z]*\)<\([^,<>]*\), \([^,<>]*\), std::less<\2>, std::allocator<std::pair<const \2, \3>>>/std::\1<\2, \3>/g
-s/std::\([a-z]*\)<\([^,<>]*<[^<>]*>\), std::allocator<\2>>/std::\1<\2>/g
-s/std::\([a-z]*\)<\([^,<>]*\), \([^,<>]*<[^<>]*>\), std::less<\2>, std::allocator<std::pair<const \2, \3>>>/std::\1<\2, \3>/g
+s/std::\([a-z_]*\)<\([^,<>]*\), std::allocator<\2>>/std::\1<\2>/g
+s/std::\([a-z_]*\)<\([^,<>]*\), std::less<\2>, std::allocator<\2>>/std::\1<\2>/g
+s/std::\([a-z_]*\)<\([^,<>]*\), \([^,<>]*\), std::less<\2>, std::allocator<std::pair<const \2, \3>>>/std::\1<\2, \3>/g
+s/std::\([a-z_]*\)<\([^,<>]*<[^<>]*>\), std::allocator<\2>>/std::\1<\2>/g
+s/std::\([a-z_]*\)<\([^,<>]*\), \([^,<>]*<[^<>]*>\), std::less<\2>, std::allocator<std::pair<const \2, \3>>>/std::\1<\2, \3>/g
 '
