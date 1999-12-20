10471e8ebb224187e60eae393b7d7673376781ff
Denis Vakatov <vakatov@ncbi.nlm.nih.gov>
1999-12-20 19:42:51 +0000

    CreateMakefile_App():  show the full list of basic LIBS
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@1137 78c7ea69-d796-4a43-9a09-de51944f1b03

--- scripts/new_project.sh
+++ scripts/new_project.sh
@@ -105,6 +105,7 @@ OBJ = $proj_name
 
 # PRE_LIBS = $(NCBI_C_LIBPATH) .....
 LIB        = xncbi
+# LIB      = xserial xhtml xcgi xncbi
 # LIBS     = $(NCBI_C_LIBPATH) -lncbi $(NETWORK_LIBS) $(ORIG_LIBS)
 
 # CPPFLAGS = $(ORIG_CPPFLAGS) $(NCBI_C_INCLUDE)
