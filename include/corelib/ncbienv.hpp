f71a4413787b52f28ee7bbccc0f08cbaa71c3200
Denis Vakatov <vakatov@ncbi.nlm.nih.gov>
2000-01-20 16:36:04 +0000

    Added class CNcbiArguments::   application command-line arguments & name Added CNcbiEnvironment::Reset(), and comments to CNcbiEnvironment:: Dont #include <ncbienv.inl>
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@1223 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/corelib/ncbienv.hpp
+++ include/corelib/ncbienv.hpp
@@ -26,42 +26,97 @@
 *
 * ===========================================================================
 *
-* Author: Eugene Vasilchenko
+* Authors:  Eugene Vasilchenko, Denis Vakatov
 *
 * File Description:
-*   Unified interface to application environment
+*   Unified interface to application:
+*      environment     -- CNcbiEnvironment
+*      cmd.-line args  -- CNcbiArguments
 *
 * ---------------------------------------------------------------------------
 * $Log$
+* Revision 1.2  2000/01/20 16:36:02  vakatov
+* Added class CNcbiArguments::   application command-line arguments & name
+* Added CNcbiEnvironment::Reset(), and comments to CNcbiEnvironment::
+* Dont #include <ncbienv.inl>
+*
 * Revision 1.1  1999/05/04 16:14:06  vasilche
 * Fixed problems with program environment.
 * Added class CNcbiEnvironment for cached access to C environment.
-*
 * ===========================================================================
 */
 
 #include <corelib/ncbistd.hpp>
 #include <map>
+#include <vector>
 
 BEGIN_NCBI_SCOPE
+
+
+///////////////////////////////////////////////////////
+//  CNcbiEnvironment::   application environment
 
 class CNcbiEnvironment
 {
 public:
+    // 'ctors
     CNcbiEnvironment(void);
-    CNcbiEnvironment(const char* const* env);
+    CNcbiEnvironment(const char* const* envp);
     virtual ~CNcbiEnvironment(void);
 
+    // Delete all cached entries, load new ones from "envp" (if not NULL)
+    void Reset(const char* const* envp = 0);
+
+    // Get env.value by name
+    // If it is not cached then call "Load(name)" to load the env.value
+    // (the loaded name/value pair will then be cached, too)
     const string& Get(const string& name) const;
 
 protected:
+    // Call "::getenv()"
     virtual string Load(const string& name) const;
 
 private:
+    // Cached environment <name,value>
     mutable map<string, string> m_Cache;
 };
 
-#include <corelib/ncbienv.inl>
+
+
+///////////////////////////////////////////////////////
+//  CNcbiArguments::   application command-line arguments & name
+
+class CNcbiArguments
+{
+public:
+    // 'tors & assignment operator
+    CNcbiArguments(int argc, const char* const* argv,
+                   const string& program_name = NcbiEmptyString);
+    virtual ~CNcbiArguments(void);
+
+    CNcbiArguments(const CNcbiArguments& args);
+    CNcbiArguments& operator= (const CNcbiArguments& args);
+
+    // Delete all cached args & prog.name
+    // Load new ones from "argc", "argv", "program_name"
+    void Reset(int argc, const char* const* argv,
+               const string& program_name = NcbiEmptyString);
+
+    // Accessing and adding args
+    SIZE_TYPE     Size       (void) const;           // # of arguments
+    const string& operator[] (SIZE_TYPE pos) const;  // read-only access
+    void          Add        (const string& arg);    // add new arg
+
+    // Program name
+    const string& GetProgramName(void) const;
+    string GetProgramBasename(void) const;
+    string GetProgramDirname (void) const;  // (including last '/')
+    void SetProgramName(const string& program_name);
+
+private:
+    string         m_ProgramName;  // if different from the default m_Args[0]
+    vector<string> m_Args;
+};
 
 END_NCBI_SCOPE
 
--- src/corelib/ncbienv.cpp
+++ src/corelib/ncbienv.cpp
@@ -23,34 +23,65 @@
 *
 * ===========================================================================
 *
-* Author: Eugene Vasilchenko
+* Authors:  Eugene Vasilchenko, Denis Vakatov
 *
 * File Description:
-*   Unified interface to application environment
+*   Unified interface to application:
+*      environment     -- CNcbiEnvironment
+*      cmd.-line args  -- CNcbiArguments
 *
 * ---------------------------------------------------------------------------
 * $Log$
+* Revision 1.2  2000/01/20 16:36:04  vakatov
+* Added class CNcbiArguments::   application command-line arguments & name
+* Added CNcbiEnvironment::Reset(), and comments to CNcbiEnvironment::
+* Dont #include <ncbienv.inl>
+*
 * Revision 1.1  1999/05/04 16:14:46  vasilche
 * Fixed problems with program environment.
 * Added class CNcbiEnvironment for cached access to C environment.
-*
 * ===========================================================================
 */
 
-#include <corelib/ncbistd.hpp>
 #include <corelib/ncbienv.hpp>
+#include <algorithm>
+
 
 BEGIN_NCBI_SCOPE
 
-CNcbiEnvironment::CNcbiEnvironment(void)
-{
+
+///////////////////////////////////////////////////////
+//  CNcbiEnvironment::
+
+
+CNcbiEnvironment::CNcbiEnvironment(void) {
+    return;
 }
 
-CNcbiEnvironment::CNcbiEnvironment(const char* const* env)
+
+CNcbiEnvironment::CNcbiEnvironment(const char* const* envp)
 {
-    // load ready environment values
-    for ( ; *env; ++env ) {
-        const char* s = *env;
+    Reset(envp);
+}
+
+
+CNcbiEnvironment::~CNcbiEnvironment(void) {
+    return;
+}
+
+
+void CNcbiEnvironment::Reset(const char* const* envp)
+{
+    // delete old environment values
+    m_Cache.clear();
+
+
+    // load new environment values from "envp"
+    if ( !envp )
+        return;
+
+    for ( ;  *envp;  envp++) {
+        const char* s = *envp;
         const char* eq = strchr(s, '=');
         if ( !eq ) {
             ERR_POST("CNcbiEnvironment: bad string '" << s << "'");
@@ -60,9 +91,6 @@ CNcbiEnvironment::CNcbiEnvironment(const char* const* env)
     }
 }
 
-CNcbiEnvironment::~CNcbiEnvironment(void)
-{
-}
 
 const string& CNcbiEnvironment::Get(const string& name) const
 {
@@ -72,6 +100,7 @@ const string& CNcbiEnvironment::Get(const string& name) const
     return m_Cache[name] = Load(name);
 }
 
+
 string CNcbiEnvironment::Load(const string& name) const
 {
     const char* s = getenv(name.c_str());
@@ -80,5 +109,132 @@ string CNcbiEnvironment::Load(const string& name) const
     else
         return s;
 }
+
+
+
+///////////////////////////////////////////////////////
+//  CNcbiArguments::
+
+
+CNcbiArguments::CNcbiArguments(int argc, const char* const* argv,
+                               const string& program_name)
+{
+    Reset(argc, argv, program_name);
+}
+
+
+CNcbiArguments::~CNcbiArguments(void) {
+    return;
+}
+
+
+CNcbiArguments::CNcbiArguments(const CNcbiArguments& args)
+    : m_ProgramName(args.m_ProgramName),
+      m_Args(args.m_Args) {
+    return;
+}
+
+
+CNcbiArguments& CNcbiArguments::operator= (const CNcbiArguments& args)
+{
+    if (&args == this)
+        return *this;
+
+    m_ProgramName = args.m_ProgramName;
+    m_Args.clear();
+    copy(args.m_Args.begin(), args.m_Args.end(), back_inserter(m_Args));
+    return *this;
+}
+
+
+void CNcbiArguments::Reset(int argc, const char* const* argv,
+                           const string& program_name)
+{
+    // check args
+    if (argc < 0) {
+        THROW1_TRACE(runtime_error,
+                     "CNcbiArguments(): negative # of cmd.-line arguments");
+    }
+
+    if ((argc == 0) != (argv == 0)) {
+        if (argv == 0) {
+            THROW1_TRACE(runtime_error,
+                         "CNcbiArguments(): non-zero \"argc\", zero \"argv\"");
+        }
+        ERR_POST(Info <<
+                 "CNcbiArguments(): zero \"argc\", non-zero \"argv\"");
+    }
+
+    // clear old args, store new ones
+    m_Args.clear();
+    for (int i = 0;  i < argc;  i++) {
+        if ( !argv[i] ) {
+            ERR_POST(Warning <<
+                     "CNcbiArguments() -- NULL cmd.-line arg #" << i);
+            continue;
+        }
+        m_Args.push_back(argv[i]);
+    }
+
+    // set application name
+    SetProgramName(program_name);
+}
+
+
+SIZE_TYPE CNcbiArguments::Size(void) const
+{
+    return m_Args.size();
+}
+
+
+const string& CNcbiArguments::operator[] (SIZE_TYPE pos) const
+{
+    return m_Args[pos];
+}
+
+
+const string& CNcbiArguments::GetProgramName(void) const
+{
+    if ( !m_ProgramName.empty() )
+        return m_ProgramName;
+    if ( m_Args.size() )
+        return m_Args[0];
+
+    static const string kDefProgramName("ncbi");
+    return kDefProgramName;
+}
+
+
+string CNcbiArguments::GetProgramBasename(void) const
+{
+    const string& name = GetProgramName();
+    SIZE_TYPE base_pos = name.find_last_of("/\\:");
+    if (base_pos == NPOS)
+      return name;
+    return name.substr(base_pos + 1);
+}
+
+
+string CNcbiArguments::GetProgramDirname(void) const
+{
+    const string& name = GetProgramName();
+    SIZE_TYPE base_pos = name.find_last_of("/\\:");
+    if (base_pos == NPOS)
+      return NcbiEmptyString;
+    return name.substr(0, base_pos + 1);
+}
+
+
+void CNcbiArguments::SetProgramName(const string& program_name)
+{
+    m_ProgramName = program_name;
+}
+
+
+void CNcbiArguments::Add(const string& arg)
+{
+    m_Args.push_back(arg);
+}
+
 
 END_NCBI_SCOPE
