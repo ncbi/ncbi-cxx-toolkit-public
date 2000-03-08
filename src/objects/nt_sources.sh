b522dc0d1b342573d880063ba26af29b56cdbf82
Eugene Vasilchenko <vasilche@ncbi.nlm.nih.gov>
2000-03-08 19:57:19 +0000

    Script to update objects' sources on Windows-NT
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@1299 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ src/objects/nt_sources.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# $Id$
+#
+# Script to update ASN.1 objects' sources on Windows-NT
+#	(using bash and datatool)
+#
+
+cd $(dirname $(echo $0 | sed 's%\\%/%g'))
+ROOT="$(echo $PWD | sed 's%//\([a-zA-Z]\)/%\1:\\%' | sed 's%/src/objects%%')"
+TOOL="$ROOT/compilers/msvc_prj/datatool/Release/datatool"
+
+OBJECTS="$ROOT/src/objects"
+
+MODULES='general biblio medline medlars pub pubmed seqloc seqalign seqblock seqfeat seqres seqset seq submit proj mmdb1 mmdb2 mmdb3 cdd ncbimime access featdef objprt seqcode'
+
+for m in $MODULES; do \
+    echo Updating $m
+    (
+	cd $m
+	M="$(grep ^MODULE_IMPORT $m.module | sed 's/^.*=//' | sed 's/$/ /' | sed 's/\(objects[/a-z]*\) /-M \1.asn /g')"
+	if ! "$TOOL" -m "$m.asn" $M -oA -of "$m.files" -or "objects/$m" -oR "$ROOT" -od "$m.def"; then
+	    echo ERROR!
+	    exit 2
+	fi
+    ) || exit 2
+done
