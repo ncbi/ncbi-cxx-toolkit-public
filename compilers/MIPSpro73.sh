3b38eace2de05607fbb28a751afa14284f7a5b0d
Denis Vakatov <vakatov@ncbi.nlm.nih.gov>
2000-06-14 21:47:20 +0000

    [MIPSpro73]  Allow to build 64-bit binaries; more accurate $(MATH_LIBS), $(LIBS), and $(NETWORK_LIBS)
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@1632 78c7ea69-d796-4a43-9a09-de51944f1b03

--- compilers/MIPSpro73.sh
+++ compilers/MIPSpro73.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 #############################################################################
 # Setup the local working environment for the "configure" script
-#   Compiler:   MIPSpro 7.3 (-n32)
+#   Compiler:   MIPSpro 7.3
 #   OS:         IRIX64 (6.5)
 #   Processor:  mips
 #
@@ -12,11 +12,28 @@
 set -a
 NCBI_COMPILER="MIPSpro73"
 
+#  32/64-bit architecture
+case "$1" in
+ 32 )  ARCH_FLAG="-n32" ;;
+ 64 )  ARCH_FLAG="-64" ;;
+ *  )  echo "USAGE: `basename $0` {32|64} [build_dir] [--configure-flags]"
+     exit 1 ;;
+esac
+shift
+
+#  Build directory (optional)
+if test -n "$1" ; then
+  case "$1" in
+   -*) EXEC_PREFIX= ;;
+   *)  EXEC_PREFIX="--exec-prefix=$1" ; shift ;;
+  esac
+fi
+
+# Tools
 CC="cc"
 CXX="CC"
 CXXCPP="$CXX -E -LANG:std"
-LIBS="-lm"
 
 
 ## Configure
-${CONFIG_SHELL-/bin/sh} `dirname $0`/../configure "$@"
+${CONFIG_SHELL-/bin/sh} `dirname $0`/../configure $EXEC_PREFIX "$@"
