877f00574624fc21b4e64dabfd2c65c9654dfd6e
Anton Lavrentiev <lavr@ncbi.nlm.nih.gov>
2000-10-20 17:36:05 +0000

    Partially working dispd dispatcher client (service mapping works) Checkin for backup purposes; working code '#if 0'-ed out
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@2069 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/connect/ncbi_dispd.c
+++ src/connect/ncbi_dispd.c
@@ -31,6 +31,10 @@
  *
  * --------------------------------------------------------------------------
  * $Log$
+ * Revision 6.5  2000/10/20 17:36:05  lavr
+ * Partially working dispd dispatcher client (service mapping works)
+ * Checkin for backup purposes; working code '#if 0'-ed out
+ *
  * Revision 6.4  2000/10/05 22:43:30  lavr
  * Another dummy revision: still in development
  *
@@ -48,99 +52,261 @@
  */
 
 #include "ncbi_servicep_dispd.h"
+#include <stdlib.h>
+#include <string.h>
 
-const SSERV_VTable *SERV_DISPD_Open(SERV_ITER iter, SConnNetInfo *net_info)
+#if 1
+
+const SSERV_VTable *SERV_DISPD_Open(SERV_ITER iter,
+                                    const SConnNetInfo *net_info)
 {
     return 0;
 }
 
-#if 0
+#else
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
     static SSERV_Info* s_GetNextInfo(SERV_ITER iter);
+    static int/*bool*/ s_Update(SERV_ITER iter, const char *text);
     static void s_Close(SERV_ITER iter);
 
-    static const SSERV_VTable s_op = { s_GetNextInfo, s_Close };
+    static const SSERV_VTable s_op = { s_GetNextInfo, s_Update, s_Close };
 
 #ifdef __cplusplus
 } /* extern "C" */
 #endif
 
-struct SDISPD_DataTag {
-    struct SConnNetInfo* net_info;
-    struct SSERV_Info*   s_info;
-    unsigned             n_info;
-    CONN                 conn;
-};
+
+typedef struct SDISPD_DataTag {
+    SConnNetInfo* net_info;
+    SSERV_Info**  s_info;
+    size_t        n_info;
+    size_t        n_max_info;  
+} SDISPD_Data;
 
 
-const SSERV_VTable *SERV_DISPD_Open(SERV_ITER iter, SConnNetInfo *net_info)
+static void s_FreeData(SDISPD_Data *data)
 {
-    CONNECTOR c;
-    CONN conn;
+    if (!data)
+        return;
 
-    if (!net_info && !(net_info = ConnNetInfo_Create(iter->service)))
-        return 0;
+    if (data->net_info)
+        ConnNetInfo_Destroy(&(data->net_info));
 
-    if (!(c = SOCK_CreateConnector(info->host, info->port, info->max_try)) ||
-        CONN_Create(c, &conn) != eIO_Success) {
-        ConnNetInfo_Destroy(&net_info);
-        return 0;
+    if (data->s_info) {
+        size_t i;
+
+        for (i = 0; i < data->n_info; i++)
+            free(data->s_info[i]);
+        free(data->s_info);
     }
 
-    if (!(iter->data = calloc(sizeof(struct SDISPD_DataTag)))) {
-        ConnNetInfo_Destroy(&net_info);
-        CONN_Close(conn);
-        return 0;
-    }
-
-    data = (struct SDISPD_DataTag)(iter->data);
-    data->net_info = net_info;
-    data->conn = conn;
-
-    CONN_SetTimeout(conn, eIO_Open, &net_info->timeout);
-    CONN_SetTimeout(conn, eIO_ReadWrite, &net_info->timeout);
-
-    return s_op;
+    free(data);
 }
+
+
+static int/*bool*/ s_AddServerInfo(SDISPD_Data *data, SSERV_Info *info)
+{
+    if (data->n_info == data->n_max_info) {
+        size_t n = data->n_max_info + 10;
+        SSERV_Info** temp;
+
+        if (data->s_info)
+            temp = (SSERV_Info**) realloc(data->s_info, sizeof(*temp) * n);
+        else
+            temp = (SSERV_Info**) malloc(sizeof(*temp) * n);
+        if (!temp)
+            return 0;
+
+        data->s_info = temp;
+        data->n_max_info = n;
+    }
+
+    data->s_info[data->n_info++] = info;
+    return 1;
+}
+
+
+static int/*bool*/ s_Resolve(SERV_ITER iter)
+{
+    const static char client_mode[] = "Client-Mode: DISPATCH_ONLY\r\n";
+    const static char accepted_types[] = "Accepted-Server-Types:";
+    const static char service[] = "service=";
+    SConnNetInfo *net_info = ((SDISPD_Data *)iter->data)->net_info;
+    TSERV_Type type = (iter->type & ~fSERV_StatelessOnly), t;
+    char service_arg[128];
+    char buffer[128];
+    size_t buflen;
+    SOCK s;
+    int/*bool*/ server_error = 0/*false*/;
+    EIO_Status  status;
+    BUF         buf = 0;
+    
+    /* Form service name argument (as CGI argument) */
+    if (strlen(iter->service) + sizeof(service) > sizeof(service_arg))
+        return 0/*failed*/;
+    strcpy(service_arg, service);
+    strcat(service_arg, iter->service);
+    /* Form accepted server types (as customized header) */
+    strcpy(buffer, accepted_types);
+    buflen = sizeof(accepted_types) - 1;
+    for (t = 1; t; t <<= 1) {
+        if (type & t) {
+            const char *name = SERV_TypeStr(t);
+            size_t namelen = strlen(name);
+            
+            if (namelen) {
+                if (buflen + namelen < sizeof(buffer) - 1) {
+                    buffer[buflen++] = ' ';
+                    strcpy(&buffer[buflen], name);
+                    buflen += namelen;
+                }
+            } else
+                break;
+        }
+    }
+    assert(buflen < sizeof(buffer));
+    if (buffer[buflen - 1] != ':' && buflen < sizeof(buffer) - 2)
+        strcpy(&buffer[buflen - 1], "\r\n");
+    else
+        buffer[0] = '\0';
+    /* Append customized header with client mode information */
+    buflen = strlen(buffer);
+    assert(buflen < sizeof(buffer));
+    if (buflen + sizeof(client_mode)-1 < sizeof(buffer))
+        strcpy(&buffer[buflen], client_mode);
+
+    /* Now connect to the mapper with everything above */
+    if (!(s = URL_Connect(net_info->host, net_info->port,
+                          net_info->path, service_arg,
+                          eReqMethodGet, 0/* request_length */,
+                          &net_info->timeout, &net_info->timeout,
+                          *buffer ? buffer : 0, 0/*no arg encoding*/))) {
+        return 0/* failed */;
+    }
+
+    /* set timeout */
+    SOCK_SetTimeout(s, eIO_Read, &net_info->timeout);
+
+    /* check status (assume the reply status is in the first line) */
+    if ((status = SOCK_StripToPattern(s, "\r\n", 2, &buf, 0)) == eIO_Success) {
+        int http_v1, http_v2, http_status = 0;
+        buflen = BUF_Peek(buf, buffer, sizeof(buffer)-1);
+        assert(2 <= buflen   &&  buflen < sizeof(buffer));
+        buffer[buflen] = '\0';
+        if (sscanf(buffer, " HTTP/%d.%d %d ",
+                   &http_v1, &http_v2, &http_status) != 3  ||
+            http_status < 200  ||  299 < http_status)
+            server_error = 1/*true*/;
+    }
+
+    if (!server_error) {
+        /* Grab the entire HTTP header */
+        status = SOCK_StripToPattern(s, "\r\n\r\n", 4, &buf, 0);
+        if (status == eIO_Success) {
+            size_t header_size = BUF_Size(buf);
+            char *header = malloc(header_size + 1);
+
+            if ((header_size = BUF_Read(buf, header, header_size)) != 0) {
+                header[header_size] = '\0';
+                if (!s_Update(iter, header))
+                    status = eIO_Unknown;
+            }
+            free(header);
+        }
+    }
+
+    BUF_Destroy(buf);
+
+    if (server_error || status != eIO_Success)
+        return 0/* fail */;
+
+    return 1/* success */;
+}
+
+
+const SSERV_VTable *SERV_DISPD_Open(SERV_ITER iter,
+                                    const SConnNetInfo *net_info)
+{
+    SDISPD_Data *data;
+
+    if (!(data = calloc(1, sizeof(*data))))
+        return 0;
+    data->net_info = ConnNetInfo_Clone(net_info);
+    iter->data = data;
+
+    if (!s_Resolve(iter)) {
+        iter->data = 0;
+        s_FreeData(data);
+        return 0;
+    }
+    
+    return &s_op;
+}
+
 
 static void s_Close(SERV_ITER iter)
 {
-    if (iter->data) {
-        struct SDISPD_DataTag *data = (struct SDISPD_DataTag *)iter->data;
+    s_FreeData(iter->data);
+    iter->data = 0;
+}
 
-        if (data->net_info)
-            ConnNetInfo_Destroy(&data->net_info);
-        if (data->info)
-            free(data->info);
-        if (data->conn)
-            CONN_Close(conn);
-        free(iter->data);
-        iter->data = 0;
+
+static int/*bool*/ s_Update(SERV_ITER iter, const char *text)
+{
+    const char server_info[] = "Server-Info-";
+    char *buf = malloc(strlen(text) + 1);
+    char *b = buf, *c;
+    SSERV_Info *info;
+    int n = 0;
+
+    strcpy(buf, text);
+    for (b = buf; (c = strchr(b, '\n')) != 0; b = c + 1) {
+        unsigned d1;
+        size_t d2;
+        char *p;
+
+        *c = '\0';
+        if (strncasecmp(b, server_info, sizeof(server_info) - 1) != 0)
+            continue;
+        b += sizeof(server_info) - 1;
+        if ((p = strchr(b, '\r')) != 0)
+            *p = '\0';
+        if (sscanf(b, "%u: %n", &d1, &d2) < 1)
+            break;
+        if (!(info = SERV_ReadInfo(b + d2, 0)))
+            break;
+        if (!s_AddServerInfo((SDISPD_Data *)iter->data, info))
+            break;
+        n++;
     }
+    free(buf);
+
+    return n != 0;
 }
 
-static int/*bool*/ s_Connect(struct SDISPD_DataTag *data)
-{
-    
-}
 
-static SSERV_Info* s_GetNext(SERV_ITER iter)
+static SSERV_Info* s_GetNextInfo(SERV_ITER iter)
 {
-    struct SDISPD_DataTag data = (struct SDISPD_DataTag *)iter->data;
-    unsigned n;
+    SDISPD_Data *data = (SDISPD_Data *)iter->data;
+    SSERV_Info *info;
 
     if (!data)
         return 0;
 
-    if (!data->n_info && !s_Connect(data))
+    if (!data->n_info && !s_Resolve(iter))
         return 0;
 
-    
-    memmove(&data->info[0], &data->info[1], sizeof(SSERV_Info));
+    assert(data->n_info > 0);
 
+    info = data->s_info[0];
+    memmove(&data->s_info[0], &data->s_info[1],
+            (--data->n_info)*sizeof(*data->s_info));
+
+    return info;
 }
+
 #endif
