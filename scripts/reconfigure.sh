58a25d7b717582bcea2b28f4a6ab6626f8bb48da
Denis Vakatov <vakatov@ncbi.nlm.nih.gov>
2001-02-16 17:14:00 +0000

    Initial revision
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@2487 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ scripts/reconfigure.sh
@@ -0,0 +1,117 @@
+#! /bin/sh
+
+# $Id$
+# Author:  Denis Vakatov, NCBI 
+# 
+#  Refresh makefiles and other configurables in the build tree.
+
+
+###  Paths
+
+script_name=`basename $0`
+script_dir=`dirname $0`
+
+top_srcdir=`(cd "${script_dir}/.." ; pwd)`
+builddir=`pwd`
+status_dir=`(cd "${builddir}/../status" ; pwd)`
+
+###  What to do (cmd-line arg)
+
+method="$1"
+
+
+###  Checks
+
+if test ! -x "$top_srcdir/scripts/hello.sh"  -o  ! -f "$builddir/../inc/ncbiconf.h" ; then
+  cat <<EOF
+
+[$script_name]  ERROR:
+  This script must be run only using its counterpart (which is also
+  called "$script_name") located in the appropriate build directory,
+  like this:
+    ( cd c++/GCC-Debug/build  &&  ./$script_name $method )
+EOF
+   exit 1
+fi
+
+
+###  Usage
+
+if test $# -ne 1 ; then
+  cat<<EOF
+USAGE:  ./$script_name {recheck | reconf | update}
+
+ "recheck"
+    Exact equivalent of running the 'configure' script all over again,
+    using just the same cmd.-line arguments as in the original 'configure'
+    run, and without using cached check results from that run.
+
+ "reconf"
+    Same as target "recheck" above, but do use cached check results
+    obtained from the original 'configure' run.
+
+ "update"
+    Do just the same substitutions in just the same set of configurables
+    ("*.in" files, mostly "Makefile.in") as the last run configuration did.
+    Do not re-check the working environment (such as availability
+    of 3rd-party packages), and do not process configurables which
+    were added after the last full-scale run of 'configure'
+    (or "recheck", or "reconfig").
+
+EXAMPLE:
+  It must be run from the appropriate build directory, like:
+    ( cd c++/GCC-Debug/build  &&  ./$script_name reconf )
+EOF
+    exit 0
+fi
+
+
+### Startup banner
+
+cat <<EOF
+Reconfiguring the NCBI C++ Toolkit in:
+  - build  tree:  $builddir
+  - source tree:  $top_srcdir
+  - method:       $method
+
+EOF
+
+
+### Action
+
+case "$method" in
+  update )
+    ${status_dir}/config.status
+    ;;
+
+  reconf )
+    cd ${top_srcdir}  && \
+    rm -f config.status config.cache config.log  && \
+    cp -p ${status_dir}/config.status ${status_dir}/config.cache .  && \
+    ./config.status --recheck  && \
+    mv config.status config.cache config.log ${status_dir}  && \
+    ${status_dir}/config.status
+
+    cd ${top_srcdir}  &&  rm -f config.status config.cache config.log
+    ;;
+
+  recheck )
+    cd ${top_srcdir}  && \
+    rm -f config.status config.cache config.log  && \
+    cp -p ${status_dir}/config.status .  && \
+    ./config.status --recheck  && \
+    mv config.status config.cache config.log ${status_dir}  && \
+    ${status_dir}/config.status
+
+    cd ${top_srcdir}  &&  rm -f config.status config.cache config.log
+    ;;
+
+  * )
+    cat <<EOF
+[$script_name]  ERROR:  Invalid method name.
+  Help:  Method name must be one of:  "update", "reconf", "recheck".
+  Hint:  Run this script without arguments to get full usage info.
+EOF
+    exit 1
+    ;;
+esac
