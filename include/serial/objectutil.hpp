5c1389c2d69253b0ef863d7ebbc5969e502e2253
Mikhail (Michael) Kholodov <kholodov@ncbi.nlm.nih.gov>
2001-04-04 20:04:31 +0000

    Added: Equals() function template for comparing two objects
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@2672 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ include/serial/objectutil.hpp
@@ -0,0 +1,64 @@
+#ifndef OBJECTUTIL__HPP
+#define OBJECTUTIL__HPP
+
+/*  $Id$
+* ===========================================================================
+*
+*                            PUBLIC DOMAIN NOTICE
+*               National Center for Biotechnology Information
+*
+*  This software/database is a "United States Government Work" under the
+*  terms of the United States Copyright Act.  It was written as part of
+*  the author's official duties as a United States Government employee and
+*  thus cannot be copyrighted.  This software/database is freely available
+*  to the public for use. The National Library of Medicine and the U.S.
+*  Government have not placed any restriction on its use or reproduction.
+*
+*  Although all reasonable efforts have been taken to ensure the accuracy
+*  and reliability of the software and data, the NLM and the U.S.
+*  Government do not and cannot warrant the performance or results that
+*  may be obtained by using this software or data. The NLM and the U.S.
+*  Government disclaim all warranties, express or implied, including
+*  warranties of performance, merchantability or fitness for any particular
+*  purpose.
+*
+*  Please cite the author in any work or product based on this material.
+*
+* ===========================================================================
+*
+* Author: Michael Kholodov
+*
+* File Description:
+*   !!! PUT YOUR DESCRIPTION HERE !!!
+*
+* ---------------------------------------------------------------------------
+* $Log$
+* Revision 1.1  2001/04/04 20:04:31  kholodov
+* Added: Equals() function template for comparing two objects
+*
+*
+* ===========================================================================
+*/
+
+#include <serial/serialdef.hpp>
+#include <serial/objecttype.hpp>
+
+BEGIN_NCBI_SCOPE
+
+// Compares two objects by converting them to binary streams and performing
+// byte comparison
+
+extern bool SerialCompare(TConstObjectPtr a, 
+		   TConstObjectPtr b, 
+		   TTypeInfo type);
+
+template <class T>
+bool Equals(const T& a, const T& b) {
+
+  TTypeInfo type = Type<T>::GetTypeInfo();
+  return SerialCompare(&a, &b, type);
+}
+
+END_NCBI_SCOPE
+
+#endif  /* OBJECTUTIL__HPP */
