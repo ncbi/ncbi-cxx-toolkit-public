ebf6ca77108b4a1919e0bd14bd53e9638154bbfe
Anton Lavrentiev <lavr@ncbi.nlm.nih.gov>
2001-04-24 21:24:59 +0000

    Make log flush in DEBUG mode
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@2756 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/connect/ncbi_util.c
+++ src/connect/ncbi_util.c
@@ -30,6 +30,9 @@
  *
  * ---------------------------------------------------------------------------
  * $Log$
+ * Revision 6.9  2001/04/24 21:24:59  lavr
+ * Make log flush in DEBUG mode
+ *
  * Revision 6.8  2001/01/23 23:20:14  lavr
  * Comments added to some "boolean" 1s and 0s
  *
@@ -280,6 +283,9 @@ static void s_LOG_FileHandler(void* user_data, SLOG_Handler* call_data)
         char* str = LOG_ComposeMessage(call_data, fLOG_Default);
         if ( str ) {
             fprintf(fp, "%s\n", str);
+#ifndef NDEBUG
+            fflush(fp);
+#endif
             free(str);
         }
     }
