f9e94cb706584cdb8b76e5992473a663c1c41977
Denis Vakatov <vakatov@ncbi.nlm.nih.gov>
2001-07-20 20:53:31 +0000

    @bindir@  --->  @exec_prefix@/bin
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@3150 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/serial/test/test_serial.sh
+++ src/serial/test/test_serial.sh
@@ -4,7 +4,7 @@ for f in webenv.ent webenv.bin test.asn test.asb test.bin; do
     cp @srcdir@/$f ./
 done
 
-@bindir@/serialtest > test.out 2> test.err
+@exec_prefix@/bin/serialtest > test.out 2> test.err
 
 for f in webenv.ent webenv.bin test.asn test.asb test.bin; do
     cmp $f ${f}o || echo Error in file ${f}o
