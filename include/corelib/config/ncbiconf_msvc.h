492387fa0f03002b383b19be08256b9bf7a2e78b
Denis Vakatov <vakatov@ncbi.nlm.nih.gov>
2001-07-24 23:30:41 +0000

    #define NCBI_CXX_TOOLKIT Get rid of undefined stuff. Beautified.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@3165 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/corelib/config/ncbiconf_msvc.h
+++ include/corelib/config/ncbiconf_msvc.h
@@ -1,134 +1,44 @@
-/* /cxx/MSVC-Debug/inc/ncbiconf.h.  Generated automatically by configure.  */
-/* config.h.in.  Generated automatically from configure.in by autoheader.  */
+/* $Id$
+ * By Denis Vakatov, NCBI (vakatov@ncbi.nlm.nih.gov)
+ *
+ * Win32, MSVC++ 6.0
+ *
+ * NOTE:  Unlike its UNIX counterpart, this configuration header
+ *        is manually maintained in order to keep it in-sync with the
+ *        "configure"-generated configuration headers.
+ */
 
-/* Define if type char is unsigned and you are not using gcc.  */
-#ifndef __CHAR_UNSIGNED__
-/* #undef __CHAR_UNSIGNED__ */
-#endif
+#define NCBI_CXX_TOOLKIT  1
 
-/* Define to empty if the keyword does not work.  */
-/* #undef const */
+#define NCBI_OS        "MSWIN"
+#define NCBI_OS_MSWIN  1
 
-/* Define to `unsigned' if <sys/types.h> doesn't define.  */
-/* #undef size_t */
+#define HOST         "i386-pc-win32"
+#define HOST_CPU     "i386"
+#define HOST_VENDOR  "pc"
+#define HOST_OS      "win32"
 
-/* Define if you have the ANSI C header files.  */
-#define STDC_HEADERS 1
+#define HAVE_STRDUP              1
+#define NCBI_USE_THROW_SPEC      1
+#define HAVE_NO_AUTO_PTR         1
+#define HAVE_NO_MINMAX_TEMPLATE  1
 
-/* Define if your processor stores words with the most significant
-   byte first (like Motorola and SPARC, unlike Intel and VAX).  */
-/* #undef WORDS_BIGENDIAN */
+#define SIZEOF___INT64      8
+#define SIZEOF_CHAR         1
+#define SIZEOF_DOUBLE       8
+#define SIZEOF_INT          4
+#define SIZEOF_LONG         4
+#define SIZEOF_LONG_DOUBLE  8
+#define SIZEOF_LONG_LONG    0
+#define SIZEOF_SHORT        2
+#define SIZEOF_VOIDP        4
 
-/* Host info */
-#define HOST "i386-pc-cygwin32"
-#define HOST_CPU "i386"
-#define HOST_VENDOR "pc"
-#define HOST_OS "cygwin32"
+#define STDC_HEADERS  1
 
-/* Platform info */
-#define NCBI_OS "MSWIN"
-/* #undef NCBI_OS_UNIX */
-#define NCBI_OS_MSWIN 1
-/* #undef NCBI_OS_MAC */
-
-/* There is gethostbyname_r() */
-/* #undef HAVE_GETHOSTBYNAME_R */
-
-/* There is strdup() */
-#define HAVE_STRDUP 1
-
-/* Does not give enough support to the in-class template functions */
-/* #undef NO_INCLASS_TMPL */
-
-/* Can use exception specifications("throw(...)" after func. proto) */
-#define NCBI_USE_THROW_SPEC 1
-
-/* Non-standard basic_string::compare() -- most probably, from <bastring> */
-/* #undef NCBI_OBSOLETE_STR_COMPARE */
-
-/* "auto_ptr" template class is not implemented in <memory> */
-#define HAVE_NO_AUTO_PTR 1
-
-/* "min"/"max" templates are not implemented in <algorithm> */
-#define HAVE_NO_MINMAX_TEMPLATE 1
-
-/* SYBASE libraries are available */
-/* #undef HAVE_LIBSYBASE */
-
-/* NCBI C Toolkit libs are available */
-/* #undef HAVE_NCBI_C */
-
-/* Fast-CGI library is available */
-/* #undef HAVE_LIBFASTCGI */
-
-/* NCBI SSS DB library is available */
-/* #undef HAVE_LIBSSSDB */
-
-/* NCBI PubMed libraries are available */
-/* #undef HAVE_PUBMED */
-
-/* New C++ streams dont have ios_base:: */
-/* #undef HAVE_NO_IOS_BASE */
-
-/* This is good for the GNU C/C++ compiler on Solaris */ 
-/* #undef __EXTENSIONS__ */
-
-/* This is good for the EGCS C/C++ compiler on Linux(e.g. proto for putenv) */
-/* #undef _SVID_SOURCE */
-
-/* There is no "std::char_traits" type defined */
-/* #undef HAVE_NO_CHAR_TRAITS */
-
-/* The number of bytes in a __int64.  */
-#define SIZEOF___INT64 8
-
-/* The number of bytes in a char.  */
-#define SIZEOF_CHAR 1
-
-/* The number of bytes in a double.  */
-#define SIZEOF_DOUBLE 8
-
-/* The number of bytes in a int.  */
-#define SIZEOF_INT 4
-
-/* The number of bytes in a long.  */
-#define SIZEOF_LONG 4
-
-/* The number of bytes in a long double.  */
-#define SIZEOF_LONG_DOUBLE 8
-
-/* The number of bytes in a long long.  */
-#define SIZEOF_LONG_LONG 0
-
-/* The number of bytes in a short.  */
-#define SIZEOF_SHORT 2
-
-/* The number of bytes in a void*.  */
-#define SIZEOF_VOIDP 4
-
-/* Define if you have the <fstream> header file.  */
-#define HAVE_FSTREAM 1
-
-/* Define if you have the <fstream.h> header file.  */
-#define HAVE_FSTREAM_H 1
-
-/* Define if you have the <iostream> header file.  */
-#define HAVE_IOSTREAM 1
-
-/* Define if you have the <iostream.h> header file.  */
-#define HAVE_IOSTREAM_H 1
-
-/* Define if you have the <strstrea.h> header file.  */
-#define HAVE_STRSTREA_H 1
-
-/* Define if you have the <strstream> header file.  */
-#define HAVE_STRSTREAM 1
-
-/* Define if you have the <strstream.h> header file.  */
-/* #undef HAVE_STRSTREAM_H */
-
-/* Define if you have the <unistd.h> header file.  */
-/* #undef HAVE_UNISTD_H */
-
-/* Define if you have the <windows.h> header file.  */
-#define HAVE_WINDOWS_H 1
+#define HAVE_FSTREAM     1
+#define HAVE_FSTREAM_H   1
+#define HAVE_IOSTREAM    1
+#define HAVE_IOSTREAM_H  1
+#define HAVE_STRSTREA_H  1
+#define HAVE_STRSTREAM   1
+#define HAVE_WINDOWS_H   1
