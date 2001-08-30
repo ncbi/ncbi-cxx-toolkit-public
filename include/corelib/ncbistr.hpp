2cd65eab60bc65a303bb7064a2fd742f9a2d4095
Denis Vakatov <vakatov@ncbi.nlm.nih.gov>
2001-08-30 00:39:29 +0000

    + NStr::StringToNumeric() Moved all of "ncbistr.inl" code to here. NStr::BoolToString() to return "const string&" rather than "string". Also, well-groomed the code.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@3334 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/corelib/ncbistr.hpp
+++ include/corelib/ncbistr.hpp
@@ -1,5 +1,5 @@
-#ifndef NCBISTR__HPP
-#define NCBISTR__HPP
+#ifndef CORELIB___NCBISTR__HPP
+#define CORELIB___NCBISTR__HPP
 
 /*  $Id$
 * ===========================================================================
@@ -26,13 +26,19 @@
 *
 * ===========================================================================
 *
-* Author: Eugene Vasilchenko
+* Authors:  Eugene Vasilchenko, Denis Vakatov
 *
 * File Description:
 *   The NCBI C++ standard methods for dealing with std::string
 *
 * ---------------------------------------------------------------------------
 * $Log$
+* Revision 1.7  2001/08/30 00:39:29  vakatov
+* + NStr::StringToNumeric()
+* Moved all of "ncbistr.inl" code to here.
+* NStr::BoolToString() to return "const string&" rather than "string".
+* Also, well-groomed the code.
+*
 * Revision 1.6  2001/05/21 21:46:17  vakatov
 * SIZE_TYPE, NPOS -- moved from <ncbistl.hpp> to <ncbistr.hpp> and
 * made non-macros (to avoid possible name clashes)
@@ -67,12 +73,13 @@
 
 BEGIN_NCBI_SCOPE
 
-// Empty "C" string (points to a '\0')
+
+/// Empty "C" string (points to a '\0')
 extern const char *const kEmptyCStr;
 #define NcbiEmptyCStr NCBI_NS_NCBI::kEmptyCStr
 
 
-// Empty "C++" string
+/// Empty "C++" string
 class CNcbiEmptyString
 {
 public:
@@ -91,43 +98,56 @@ typedef NCBI_NS_STD::string::size_type SIZE_TYPE;
 static const SIZE_TYPE NPOS = NCBI_NS_STD::string::npos;
 
 
-// String-processing utilities
+
+/////////////////////////////////////////////////////////////////////////////
+//  NStr::
+//    String-processing utilities
+//
 
 class NStr
 {
 public:
-    // conversion functions (throw an exception on the conversion error)
-    static int StringToInt(const string& str, int base = 10);
-    static unsigned int StringToUInt(const string& str, int base = 10);
-    static long StringToLong(const string& str, int base = 10);
-    static unsigned long StringToULong(const string& str, int base = 10);
-    static double StringToDouble(const string& str);
+    /// convert "str" to a (non-negative) "int" value.
+    /// return "-1" if "str" contains any symbols other than [0-9], or
+    /// if it represents a number that does not fit into "int".
+    static int NStr::StringToNumeric(const string& str);
+
+    /// String-to-X conversion functions (throw exception if conversion error)
+    static int           StringToInt    (const string& str, int base = 10);
+    static unsigned int  StringToUInt   (const string& str, int base = 10);
+    static long          StringToLong   (const string& str, int base = 10);
+    static unsigned long StringToULong  (const string& str, int base = 10);
+    static double        StringToDouble (const string& str);
+
+    /// X-to-String conversion functions
     static string IntToString(long value, bool sign=false);
     static string UIntToString(unsigned long value);
     static string DoubleToString(double value);
     static string PtrToString(const void* ptr);
 
-    // 'true, 'false'
-    static string BoolToString(bool value);
-    // 'true, 't', 'false', 'f'  (case-insensitive)
-    static bool   StringToBool(const string& str);
+    /// Return one of: 'true, 'false'
+    static const string& BoolToString(bool value);
+    /// Can recognize (case-insensitive) one of:  'true, 't', 'false', 'f'
+    static bool          StringToBool(const string& str);
 
-    /*  str[pos:pos+n) == pattern  --> return 0
-     *  str[pos:pos+n) <  pattern  --> return negative value
-     *  str[pos:pos+n) >  pattern  --> return positive value
-     */
+
+    /// String comparison
     enum ECase {
         eCase,
-        eNocase  /* ignore character case */
+        eNocase  /// ignore character case
     };
+
+    ///  str[pos:pos+n) == pattern  --> return 0
+    ///  str[pos:pos+n) <  pattern  --> return negative value
+    ///  str[pos:pos+n) >  pattern  --> return positive value
     static int CompareCase(const string& str, SIZE_TYPE pos, SIZE_TYPE n,
                            const char* pattern);
     static int CompareCase(const string& str, SIZE_TYPE pos, SIZE_TYPE n,
                            const string& pattern);
     static int CompareCase(const char* s1, const char* s2);
-    
+
     static int CompareCase(const string& s1, const string& s2);
-    
+
     static int CompareNocase(const string& str, SIZE_TYPE pos, SIZE_TYPE n,
                              const char* pattern);
     static int CompareNocase(const string& str, SIZE_TYPE pos, SIZE_TYPE n,
@@ -148,8 +168,8 @@ public:
                        ECase use_case = eCase);
     static int Compare(const string& s1, const string& s2,
                        ECase use_case = eCase);
-    
-    // The following 4 methods change the passed string, then return it
+
+    /// The following 4 methods change the passed string, then return it
     static string& ToLower(string& str);
     static char*   ToLower(char*   str);
     static string& ToUpper(string& str);
@@ -166,88 +186,251 @@ public:
                          ECase use_case = eCase);
 
     enum ETrunc {
-        eTrunc_Begin,  // truncate leading  spaces only
-        eTrunc_End,    // truncate trailing spaces only
-        eTrunc_Both    // truncate spaces at both begin and end of string
+        eTrunc_Begin,  /// truncate leading  spaces only
+        eTrunc_End,    /// truncate trailing spaces only
+        eTrunc_Both    /// truncate spaces at both begin and end of string
     };
     static string TruncateSpaces(const string& str, ETrunc where=eTrunc_Both);
 
-    // Starting from position "start_pos", replace no more than "max_replace"
-    // occurrences of substring "search" by string "replace".
-    // If "max_replace" is zero -- then replace all occurrences.
-    // The result will be put to string "dst", and it will be returned as well.
+    /// Starting from position "start_pos", replace no more than "max_replace"
+    /// occurrences of substring "search" by string "replace".
+    /// If "max_replace" is zero -- then replace all occurrences.
+    /// Result will be put to string "dst", and it will be returned as well.
     static string& Replace(const string& src,
                            const string& search,
                            const string& replace,
                            string& dst,
                            SIZE_TYPE start_pos = 0, size_t max_replace = 0);
 
-    // The same as the above Replace(), but return new string
+    /// The same as the above Replace(), but return new string
     static string Replace(const string& src,
                           const string& search,
                           const string& replace,
                           SIZE_TYPE start_pos = 0, size_t max_replace = 0);
 
-    // Split string "str" using symbols from "delim" as delimiters
-    // Add the resultant tokens to the list "arr". Return "arr".
+    /// Split string "str" using symbols from "delim" as delimiters
+    /// Add the resultant tokens to the list "arr". Return "arr".
     static list<string>& Split(const string& str,
                                const string& delim,
                                list<string>& arr);
 
-    // Make a printable version of "str". The non-printable characters will
-    // be represented as "\r", "\n", "\v", "\t", "\0", "\\", or
-    // "\xDD" where DD is the character's code in hexadecimal.
+    /// Make a printable version of "str". The non-printable characters will
+    /// be represented as "\r", "\n", "\v", "\t", "\0", "\\", or
+    /// "\xDD" where DD is the character's code in hexadecimal.
     static string PrintableString(const string& str);
 }; // struct NStr
 
 
-// predicates
 
-// case-sensitive string comparison
+/////////////////////////////////////////////////////////////////////////////
+//  Predicates
+//
+
+
+/// Case-sensitive string comparison
 struct PCase
 {
-    // return difference
+    /// Return difference between "s1" and "s2"
     int Compare(const string& s1, const string& s2) const;
 
-    // returns true if s1 < s2
+    /// Return TRUE if s1 < s2
     bool Less(const string& s1, const string& s2) const;
 
-    // returns true if s1 == s2
+    /// Return TRUE if s1 == s2
     bool Equals(const string& s1, const string& s2) const;
 
-    // returns true if s1 < s2
+    /// Return TRUE if s1 < s2
     bool operator()(const string& s1, const string& s2) const;
 };
 
-// case-INsensitive string comparison
+
+/// Case-INsensitive string comparison
 struct PNocase
 {
-    // return difference
+    /// Return difference between "s1" and "s2"
     int Compare(const string& s1, const string& s2) const;
 
-    // returns true if s1 < s2
+    /// Return TRUE if s1 < s2
     bool Less(const string& s1, const string& s2) const;
 
-    // returns true if s1 == s2
+    /// Return TRUE if s1 == s2
     bool Equals(const string& s1, const string& s2) const;
 
-    // returns true if s1 < s2 ignoring case
+    /// Return TRUE if s1 < s2 ignoring case
     bool operator()(const string& s1, const string& s2) const;
 };
 
-// algorithms
 
-// Check equivalence of arguments using predicate
+
+/////////////////////////////////////////////////////////////////////////////
+//  Algorithms
+//
+
+
+/// Check equivalence of arguments using predicate
 template<class Arg1, class Arg2, class Pred>
 inline
 bool AStrEquiv(const Arg1& x, const Arg2& y, Pred pr)
-{  
+{
     return pr.Equals(x, y);
 }
 
 
-#include <corelib/ncbistr.inl>
+
+
+
+/////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////
+//  IMPLEMENTATION of INLINE functions
+/////////////////////////////////////////////////////////////////////////////
+
+
+
+/////////////////////////////////////////////////////////////////////////////
+//  CNcbiEmptyString::
+//
+
+inline
+const string& CNcbiEmptyString::Get(void)
+{
+    const string* str = m_Str;
+    return str? *str: FirstGet();
+}
+
+
+
+/////////////////////////////////////////////////////////////////////////////
+//  NStr::
+//
+
+inline
+int NStr::Compare(const string& str, SIZE_TYPE pos, SIZE_TYPE n,
+                  const char* pattern, ECase use_case)
+{
+    return use_case == eCase?
+        CompareCase(str, pos, n, pattern): CompareNocase(str, pos, n, pattern);
+}
+
+inline
+int NStr::Compare(const string& str, SIZE_TYPE pos, SIZE_TYPE n,
+                  const string& pattern, ECase use_case)
+{
+    return use_case == eCase?
+        CompareCase(str, pos, n, pattern): CompareNocase(str, pos, n, pattern);
+}
+
+inline
+int NStr::Compare(const char* s1, const char* s2, ECase use_case)
+{
+    return use_case == eCase? CompareCase(s1, s2): CompareNocase(s1, s2);
+}
+
+inline
+int NStr::Compare(const string& s1, const char* s2, ECase use_case)
+{
+    return Compare(s1, 0, NPOS, s2, use_case);
+}
+
+inline
+int NStr::Compare(const char* s1, const string& s2, ECase use_case)
+{
+    return Compare(s2, 0, NPOS, s1, use_case);
+}
+
+inline
+int NStr::Compare(const string& s1, const string& s2, ECase use_case)
+{
+    return Compare(s1, 0, NPOS, s2, use_case);
+}
+
+inline
+int NStr::CompareCase(const string& s1, const string& s2)
+{
+    return CompareCase(s1.c_str(), s2.c_str());
+}
+
+inline
+int NStr::CompareNocase(const string& s1, const string& s2)
+{
+    return CompareNocase(s1.c_str(), s2.c_str());
+}
+
+inline
+bool NStr::StartsWith(const string& str, const string& start, ECase use_case)
+{
+    return str.size() >= start.size()  &&
+        Compare(str, 0, start.size(), start, use_case) == 0;
+}
+
+inline
+bool NStr::EndsWith(const string& str, const string& end, ECase use_case)
+{
+    return str.size() >= end.size()  &&
+        Compare(str, str.size() - end.size(), end.size(), end, use_case) == 0;
+}
+
+
+
+/////////////////////////////////////////////////////////////////////////////
+//  PCase::
+//
+
+inline
+int PCase::Compare(const string& s1, const string& s2) const
+{
+    return NStr::Compare(s1, s2, NStr::eCase);
+}
+
+inline
+bool PCase::Less(const string& s1, const string& s2) const
+{
+    return Compare(s1, s2) < 0;
+}
+
+inline
+bool PCase::Equals(const string& s1, const string& s2) const
+{
+    return Compare(s1, s2) == 0;
+}
+
+inline
+bool PCase::operator()(const string& s1, const string& s2) const
+{
+    return Less(s1, s2);
+}
+
+
+
+/////////////////////////////////////////////////////////////////////////////
+//  PNocase::
+//
+
+inline
+int PNocase::Compare(const string& s1, const string& s2) const
+{
+    return NStr::Compare(s1, s2, NStr::eNocase);
+}
+
+inline
+bool PNocase::Less(const string& s1, const string& s2) const
+{
+    return Compare(s1, s2) < 0;
+}
+
+inline
+bool PNocase::Equals(const string& s1, const string& s2) const
+{
+    return Compare(s1, s2) == 0;
+}
+
+inline
+bool PNocase::operator()(const string& s1, const string& s2) const
+{
+    return Less(s1, s2);
+}
+
 
 END_NCBI_SCOPE
 
-#endif  /* NCBISTR__HPP */
+#endif  /* CORELIB___NCBISTR__HPP */
