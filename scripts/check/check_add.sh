10e2a65bf8d431bf7de926c63bdd7146c33bf079
Vladimir Ivanov <ivanov@ncbi.nlm.nih.gov>
2001-10-05 21:39:16 +0000

    Initial revision
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@3583 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ scripts/check/check_add.sh
@@ -0,0 +1,114 @@
+#! /bin/sh
+
+# $Id$
+# Author:  Vladimir Ivanov, NCBI 
+#
+###########################################################################
+#
+# Compile a script and copy necessary files to run test in the build tree.
+#
+# Usage: (Run only from Makefile.meta)
+#    check_add.sh <project_srcdir> <project_name>
+#
+# Example:
+#    check_add.sh ~/c++/src/html/test test_jsmenu
+#
+# Note:
+#    1) Environment variable CHECK_RUN_FILE must be set;
+#    2) We assume that current work dir is <project_name> in build dir.
+#
+###########################################################################
+
+
+# Parameters
+x_out=$CHECK_RUN_FILE
+x_srcdir=$1
+x_test=$2
+
+
+# Check to test application
+if test ! -f ${x_srcdir}/Makefile.${x_test}.app ;  then
+   echo "Warning: File \"${x_srcdir}/Makefile.${x_test}.app\" not found."
+   exit 0
+fi
+
+x_tpath=`echo "${x_srcdir}/${x_test}" | sed 's%^.*/src/%%'`
+if grep -c CHECK_CMD $x_srcdir/Makefile.$x_test.app > /dev/null ; then 
+   echo "TEST -- ${x_tpath}"
+else 
+   echo "SKIP -- ${x_tpath}"
+   exit 0; 
+fi
+
+
+# Copy specified files to the build (current) directory
+x_files=`grep '^ *CHECK_COPY' ${x_srcdir}/Makefile.${x_test}.app | sed -e 's/^.*=//'`
+
+if test ! -z "${x_files}" ; then
+   for i in ${x_files} ; do
+      x_copy=${x_srcdir}/${i}
+      if test -f ${x_copy}  -o  -d ${x_copy} ; then
+         cp -prf "${x_copy}" .
+      else
+         echo "Warning:  \"${x_copy}\" must be file or directory!"
+         exit 0
+      fi
+   done
+fi
+
+# Get cmd-lines to run test
+x_run=`grep '^ *CHECK_CMD' ${x_srcdir}/Makefile.${x_test}.app | sed -e 's/^.*=//'`
+
+if test -z "${x_run}"; then
+    # If command line not defined, then just run the test without parameters
+    x_run=${x_test}
+fi
+x_run=`echo "${x_run}" | sed -e 's/ /%gj_s4%/g'`
+
+
+# Write test commands for current test into a shell script file
+x_pwd=`pwd`
+x_cnt=1
+test_out="$x_pwd/$x_test.test_out"
+
+for x_cmd in ${x_run} ; do
+   x_cmd=`echo "${x_cmd}" | sed -e 's/%gj_s4%/ /g' | sed -e 's/^ *//'`
+
+cat >> $x_out <<EOF
+#######################################################
+
+   echo "$test_out" >> \${res_journal}
+   (
+     echo "======================================================================"
+     echo "$x_cmd"
+     echo "======================================================================"
+     echo 
+   ) > $test_out 2>&1
+   rm -f $x_pwd/core
+   ( cd $x_pwd  &&  $x_cmd ) >> $test_out 2>&1
+   result=\$?
+
+   echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" >> $test_out
+   echo "@@@ EXIT CODE: \${result}" >> $test_out
+   if test -f $x_pwd/core ; then
+      echo "@@@ CORE DUMPED" >> $test_out
+      rm -f $x_pwd/core
+   fi
+
+   x_cmd=\`echo "$x_pwd/$x_cmd" | sed 's%^.*/build/%%'\`
+   if test \${result} -eq 0 ; then
+      echo "OK  --  \$x_cmd"
+      echo "OK  --  \$x_cmd" >> \${res_log}
+      count_ok=\`expr \${count_ok} + 1\`
+   else
+      echo "ERR --  \$x_cmd"
+      echo "ERR [\${result}] --  \$x_cmd" >> \${res_log}
+      count_err=\`expr \${count_err} + 1\`
+   fi
+
+EOF
+   x_cnt=`expr ${x_cnt} + 1`
+   test_out="$x_pwd/$x_test.test_out$x_cnt"
+done
+
+exit 0
---
+++ scripts/check/check_run.sh
@@ -0,0 +1,167 @@
+#! /bin/sh
+
+# $Id$
+# Author:  Vladimir Ivanov, NCBI 
+#
+###########################################################################
+#
+# Build script file for run tests in the build tree. Using "make".
+# Scripts exit code is equival to count of tests, executed with errors.
+#
+# Usage: (Run only from Makefile.meta)
+#    check_run.sh <make_command_line>
+#
+# Example:
+#    check_run.sh make check_add_r
+#
+###########################################################################
+
+
+cmd=$*
+
+# Define name for the check script file
+script_name="check.sh"
+CHECK_RUN_FILE="`pwd`/$script_name"
+export CHECK_RUN_FILE
+
+
+# Prepare file for test script
+if test -f "${CHECK_RUN_FILE}" ; then
+   rm -f ${CHECK_RUN_FILE}
+fi
+
+cat >> ${CHECK_RUN_FILE} <<EOF
+
+res_journal="${CHECK_RUN_FILE}.journal"
+res_log="${CHECK_RUN_FILE}.log"
+res_concat="${CHECK_RUN_FILE}.out"
+
+
+##  Printout USAGE info and exit
+
+Usage() {
+  cat <<EOF_usage
+
+USAGE:  ./$script_name {run | clean | concat}
+
+ run     Run the tests. Create output file ("*.test_out") for each tests, 
+         plus journal and log files. 
+ clean   Remove all files created during the last "run" and this script itself.
+ concat  Concatenate all files created during the last "run" into one big 
+         file "\$res_log".
+
+ERROR:  \$1
+EOF_usage
+
+    exit 1
+}
+
+
+
+if test \$# -ne 1 ; then
+  Usage "Invalid number of arguments."
+fi
+
+
+###  What to do (cmd-line arg)
+
+method="\$1"
+
+
+### Action
+
+case "\$method" in
+  run )
+    ;;
+  clean )
+    x_files=\`cat \$res_journal | sed -e 's/ /%gj_s4%/g'\`
+    for x_file in \${x_files} ; do
+      x_file=\`echo "\${x_file}" | sed -e 's/%gj_s4%/ /g'\`
+      rm -f echo \$x_file
+    done
+    rm -f \$res_journal \$res_log \$res_concat
+    rm -f ${CHECK_RUN_FILE} 
+    exit 0
+    ;;
+  concat )
+    rm -f "\$res_concat"
+    ( 
+    cat \$res_log
+    x_files=\`cat \$res_journal | sed -e 's/ /%gj_s4%/g'\`
+    for x_file in \${x_files} ; do
+      x_file=\`echo "\${x_file}" | sed -e 's/%gj_s4%/ /g'\`
+      echo 
+      echo 
+      cat \$x_file
+    done
+    ) >> \$res_concat
+    exit 0
+    ;;
+  * )
+    Usage "Invalid method name."
+    ;;
+esac
+
+
+# Run
+
+count_ok=0
+count_err=0
+rm -f "\$res_journal"
+rm -f "\$res_log"
+
+EOF
+
+
+# Run make
+echo "======================================================================"
+${cmd}
+result=$?
+
+
+# Write make result 
+echo "----------------------------------------------------------------------"
+
+if test ${result} -eq 0 ; then
+    cat >> ${CHECK_RUN_FILE} <<EOF
+echo
+echo "Succeded: \${count_ok}"
+echo "Failed:   \${count_err}"
+echo
+
+if test \${count_err} -eq 0 ; then
+   echo
+   echo "******** ALL TESTS COMPLETED SUCCESSFULLY ********"
+   echo
+fi
+
+exit \${count_err}
+EOF
+
+else
+   echo "Error in compiling tests"
+   exit ${result}
+fi
+
+chmod a+x ${CHECK_RUN_FILE}
+
+
+cat <<EOF
+The test script was succesfully built.
+
+Do you want to run the tests right now? [y/n]
+EOF
+
+read answer
+echo
+case "$answer" in
+ n | N )  echo "Run \"${CHECK_RUN_FILE} run\" to launch the tests." ; exit 0 ;;
+esac
+
+
+# Launch the tests
+${CHECK_RUN_FILE} run
+
+
+# Exit
+exit $?
