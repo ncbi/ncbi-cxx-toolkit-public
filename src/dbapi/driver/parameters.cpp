e2cffeb0b20cc261faa96865ca3908895b829a09
Anton Lavrentiev <lavr@ncbi.nlm.nih.gov>
2001-10-22 15:21:45 +0000

    +g_SubstituteParam
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@3685 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/dbapi/driver/parameters.cpp
+++ src/dbapi/driver/parameters.cpp
@@ -1,34 +1,34 @@
 /* $Id$
-* ===========================================================================
-*
-*                            PUBLIC DOMAIN NOTICE                          
-*               National Center for Biotechnology Information
-*                                                                          
-*  This software/database is a "United States Government Work" under the   
-*  terms of the United States Copyright Act.  It was written as part of    
-*  the author's official duties as a United States Government employee and 
-*  thus cannot be copyrighted.  This software/database is freely available 
-*  to the public for use. The National Library of Medicine and the U.S.    
-*  Government have not placed any restriction on its use or reproduction.  
-*                                                                          
-*  Although all reasonable efforts have been taken to ensure the accuracy  
-*  and reliability of the software and data, the NLM and the U.S.          
-*  Government do not and cannot warrant the performance or results that    
-*  may be obtained by using this software or data. The NLM and the U.S.    
-*  Government disclaim all warranties, express or implied, including       
-*  warranties of performance, merchantability or fitness for any particular
-*  purpose.                                                                
-*                                                                          
-*  Please cite the author in any work or product based on this material.   
-*
-* ===========================================================================
-*
-* Author:  Vladimir Soussov
-*   
-* File Description:  Params container implementation
-*
-*
-*/
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE                          
+ *               National Center for Biotechnology Information
+ *                                                                          
+ *  This software/database is a "United States Government Work" under the   
+ *  terms of the United States Copyright Act.  It was written as part of    
+ *  the author's official duties as a United States Government employee and 
+ *  thus cannot be copyrighted.  This software/database is freely available 
+ *  to the public for use. The National Library of Medicine and the U.S.    
+ *  Government have not placed any restriction on its use or reproduction.  
+ *                                                                          
+ *  Although all reasonable efforts have been taken to ensure the accuracy  
+ *  and reliability of the software and data, the NLM and the U.S.          
+ *  Government do not and cannot warrant the performance or results that    
+ *  may be obtained by using this software or data. The NLM and the U.S.    
+ *  Government disclaim all warranties, express or implied, including       
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.                                                                
+ *                                                                          
+ *  Please cite the author in any work or product based on this material.   
+ *
+ * ===========================================================================
+ *
+ * Author:  Vladimir Soussov
+ *   
+ * File Description:  Params container implementation
+ *
+ *
+ */
 
 #include <dbapi/driver/util/parameters.hpp>
 
@@ -157,14 +157,28 @@ CDB_Params::~CDB_Params()
 }
 
 
-END_NCBI_SCOPE
+void g_SubstituteParam(string& query, const string& name, const string& val)
+{
+    size_t name_len = name.length();
+    size_t val_len = val.length();
 
+    for (size_t pos = 0; (pos = query.find(name, pos))
+             != string::npos; pos += val_len) {
+        query.replace(pos, name_len, val);
+    }
+}
+
+
+END_NCBI_SCOPE
 
 
 
 /*
  * ===========================================================================
  * $Log$
+ * Revision 1.3  2001/10/22 15:21:45  lavr
+ * +g_SubstituteParam
+ *
  * Revision 1.2  2001/10/04 18:41:24  soussov
  * fixes bug in CDB_Params::SetParam
  *
