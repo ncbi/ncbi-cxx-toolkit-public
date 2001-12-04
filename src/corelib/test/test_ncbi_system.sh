1176ed1c596ee1855cd73f6bae9e6e3247896638
Denis Vakatov <vakatov@ncbi.nlm.nih.gov>
2001-12-04 17:40:39 +0000

    Fixed for the case of $PATH not including curr.dir
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@3898 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/corelib/test/test_ncbi_system.sh
+++ src/corelib/test/test_ncbi_system.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
-test_ncbi_system cpu; if test $? -ne 255; then exit 1; fi
-test_ncbi_system mem; if test $? -ne 255; then exit 2; fi
+./test_ncbi_system cpu; if test $? -ne 255; then exit 1; fi
+./test_ncbi_system mem; if test $? -ne 255; then exit 2; fi
 exit 0
