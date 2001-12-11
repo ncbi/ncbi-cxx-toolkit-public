0f1b2183803460fc3a40ea6fa24fcfa614c38389
Aaron Ucko <ucko@ncbi.nlm.nih.gov>
2001-12-11 19:54:45 +0000

    Introduce thread pools.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@3941 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ include/util/thread_pool.hpp
@@ -0,0 +1,331 @@
+#ifndef THREAD_POOL__HPP
+#define THREAD_POOL__HPP
+
+/*  $Id$
+* ===========================================================================
+*
+*                            PUBLIC DOMAIN NOTICE
+*               National Center for Biotechnology Information
+*
+*  This software/database is a "United States Government Work" under the
+*  terms of the United States Copyright Act.  It was written as part of
+*  the author's official duties as a United States Government employee and
+*  thus cannot be copyrighted.  This software/database is freely available
+*  to the public for use. The National Library of Medicine and the U.S.
+*  Government have not placed any restriction on its use or reproduction.
+*
+*  Although all reasonable efforts have been taken to ensure the accuracy
+*  and reliability of the software and data, the NLM and the U.S.
+*  Government do not and cannot warrant the performance or results that
+*  may be obtained by using this software or data. The NLM and the U.S.
+*  Government disclaim all warranties, express or implied, including
+*  warranties of performance, merchantability or fitness for any particular
+*  purpose.
+*
+*  Please cite the author in any work or product based on this material.
+*
+* ===========================================================================
+*
+* Author:  Aaron Ucko
+*
+* File Description:
+*   Pools of generic request-handling threads.
+*
+*   TEMPLATES:
+*      CBlockingQueue<>  -- queue of requests, with efficiently blocking Get()
+*      CThreadInPool<>   -- abstract request-handling thread
+*      CPoolOfThreads<>  -- abstract pool of threads sharing a request queue
+*
+*   SPECIALIZATIONS:
+*      CStdRequest       -- abstract request type
+*      CStdThreadInPool  -- thread handling CStdRequest
+*      CStdPoolOfThreads -- pool of threads handling CStdRequest
+*
+* ---------------------------------------------------------------------------
+* $Log$
+* Revision 1.1  2001/12/11 19:54:44  ucko
+* Introduce thread pools.
+*
+* ===========================================================================
+*/
+
+#include <corelib/ncbistd.hpp>
+#include <corelib/ncbithr.hpp>
+#include <corelib/ncbi_limits.hpp>
+
+#include <queue>
+
+BEGIN_NCBI_SCOPE
+
+
+/////////////////////////////////////////////////////////////////////////////
+//
+//  TEMPLATES:
+//
+//     CBlockingQueue<>  -- queue of requests, with efficiently blocking Get()
+//     CThreadInPool<>   -- abstract request-handling thread
+//     CPoolOfThreads<>  -- abstract pool of threads sharing a request queue
+//
+
+template <typename TRequest>
+class CBlockingQueue
+{
+public:
+    CBlockingQueue(unsigned int max_size = kMax_UInt)
+        : m_MaxSize(max_size), m_Sem(0,1) {}
+
+    void     Put(const TRequest& data); // Throws exception if full
+    TRequest Get(void);                 // Blocks politely if queue is empty
+    bool     IsEmpty(void);
+
+private:
+    volatile queue<TRequest> m_Queue;
+    unsigned int             m_MaxSize;
+    CSemaphore               m_Sem;   // Raised iff the queue contains data
+    CMutex                   m_Mutex; // Guards access to everything else
+};
+
+
+// Forward declaration
+template <typename TRequest> class CPoolOfThreads;
+
+
+template <typename TRequest>
+/* abstract */ class CThreadInPool : public CThread
+{
+public:
+    typedef CPoolOfThreads<TRequest> TPool;
+
+    CThreadInPool(TPool* pool) : m_Pool(pool) {}
+
+protected:
+    virtual ~CThreadInPool(void) {}
+
+    virtual void Init(void) {} // called at beginning of Main()
+
+    // Called from Main() for each request this thread handles
+    virtual void ProcessRequest(const TRequest& req) = 0;
+
+    virtual void x_OnExit(void) {} // called by OnExit()
+
+private:
+    // to prevent overriding; inherited from CThread
+    virtual void* Main(void);
+    virtual void OnExit(void);
+
+    TPool* m_Pool;
+};
+
+
+template <typename TRequest>
+/* abstract */ class CPoolOfThreads
+{
+public:
+    friend class CThreadInPool<TRequest>;
+    typedef CThreadInPool<TRequest> TThread;
+
+    CPoolOfThreads(unsigned int max_threads, unsigned int queue_size,
+                   int spawn_threshold = 1)
+        : m_MaxThreads(max_threads), m_Threshold(spawn_threshold),
+          m_Queue(queue_size)
+        {}
+    virtual ~CPoolOfThreads(void) {}
+
+    void Spawn(unsigned int num_threads);
+    void AcceptRequest(const TRequest& req);
+
+protected:
+    virtual TThread* NewThread(void) = 0;
+
+    volatile unsigned int    m_ThreadCount;
+    unsigned int             m_MaxThreads;
+    volatile int             m_Delta;     // # unfinished requests - # threads
+    int                      m_Threshold; // for delta
+    CMutex                   m_Mutex;     // for volatile counters
+    CBlockingQueue<TRequest> m_Queue;
+};
+
+/////////////////////////////////////////////////////////////////////////////
+//
+//  SPECIALIZATIONS:
+//
+//     CStdRequest       -- abstract request type
+//     CStdThreadInPool  -- thread handling CStdRequest
+//     CStdPoolOfThreads -- pool of threads handling CStdRequest
+//
+
+/* abstract */ class CStdRequest : public CObject
+{
+public:
+    virtual ~CStdRequest(void) {}
+
+    // Called by whichever thread handles this request.
+    virtual void Process(void) = 0;
+};
+
+
+class CStdThreadInPool : public CThreadInPool< CRef< CStdRequest > >
+{
+public:
+    typedef CThreadInPool< CRef< CStdRequest > > TParent;
+
+    CStdThreadInPool(TPool* pool) : TParent(pool) {}
+
+protected:
+    virtual void ProcessRequest(const CRef<CStdRequest>& req)
+        { req->Process(); }
+
+    // virtual void Init(void); // called before processing any requests
+    // virtual void x_OnExit(void); // called just before exiting
+};
+
+
+class CStdPoolOfThreads : public CPoolOfThreads< CRef< CStdRequest > >
+{
+public:
+    typedef CPoolOfThreads< CRef< CStdRequest > > TParent;
+    CStdPoolOfThreads(unsigned int max_threads, unsigned int queue_size,
+                      int spawn_threshold = 1)
+        : TParent(max_threads, queue_size, spawn_threshold) {}
+
+    // void Spawn(unsigned int num_threads);
+    // void AcceptRequest(const TRequest& req);
+
+    // Causes all threads in the pool to exit cleanly after finishing
+    // all pending requests, optionally waiting for them to die.
+    virtual void KillAllThreads(bool wait);
+
+protected:
+    virtual TThread* NewThread(void)
+        { return new CStdThreadInPool(this); }
+};
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////////////////////////
+//  IMPLEMENTATION of INLINE functions
+/////////////////////////////////////////////////////////////////////////////
+
+
+/////////////////////////////////////////////////////////////////////////////
+//   CBlockingQueue<>::
+//
+
+template <typename TRequest>
+void CBlockingQueue<TRequest>::Put(const TRequest& data)
+{
+    CMutexGuard guard(m_Mutex);
+    // Having the mutex, we can safely drop "volatile"
+    queue<TRequest>& q = const_cast<queue<TRequest>&>(m_Queue);
+    if (q.size() == m_MaxSize) {
+        throw runtime_error("Attempt to insert into a full queue");
+    } else if (q.empty()) {
+        m_Sem.Post();
+    }
+    q.push(data);
+}
+
+
+template <typename TRequest>
+TRequest CBlockingQueue<TRequest>::Get(void)
+{
+    m_Sem.Wait();
+
+    CMutexGuard guard(m_Mutex);
+    // Having the mutex, we can safely drop "volatile"
+    queue<TRequest>& q = const_cast<queue<TRequest>&>(m_Queue);
+    TRequest result = q.front();
+    q.pop();
+    if ( ! q.empty() ) {
+        m_Sem.Post();
+    }
+    return result;
+}
+
+
+template <typename TRequest>
+bool CBlockingQueue<TRequest>::IsEmpty(void)
+{
+    CMutexGuard guard(m_Mutex);
+    return const_cast<queue<TRequest>&>(m_Queue).empty();
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+//   CThreadInPool<>::
+//
+
+template <typename TRequest>
+void* CThreadInPool<TRequest>::Main(void)
+{
+    Detach();
+    Init();
+
+    for (;;) {
+        {{
+            CMutexGuard guard(m_Pool->m_Mutex);
+            m_Pool->m_Delta--;
+        }}
+        ProcessRequest(m_Pool->m_Queue.Get());
+    }
+
+    return 0; // Unreachable, but necessary for WorkShop build
+}
+
+
+template <typename TRequest>
+void CThreadInPool<TRequest>::OnExit(void)
+{
+    try {
+        x_OnExit();
+    } catch (...) {
+        // Ignore exceptions; there's nothing useful we can do anyway
+    }
+    CMutexGuard guard(m_Pool->m_Mutex);
+    m_Pool->m_ThreadCount--;
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+//   CPoolOfThreads<>::
+//
+
+template <typename TRequest>
+void CPoolOfThreads<TRequest>::Spawn(unsigned int num_threads)
+{
+    for (unsigned int i = 0; i < num_threads; i++)
+    {
+        {{
+            CMutexGuard guard(m_Mutex);
+            m_ThreadCount++;
+        }}
+        NewThread()->Run();
+    }
+}
+
+
+template <typename TRequest>
+void CPoolOfThreads<TRequest>::AcceptRequest(const TRequest& req)
+{
+    bool new_thread = false;
+    {{
+        CMutexGuard guard(m_Mutex);
+        m_Queue.Put(req);
+        if (++m_Delta >= m_Threshold  &&  m_ThreadCount < m_MaxThreads) {
+            // Add another thread to the pool because they're all busy.
+            m_ThreadCount++;
+            new_thread = true;
+        }
+    }}
+
+    if (new_thread) {
+        NewThread()->Run();
+    }
+}
+
+END_NCBI_SCOPE
+
+#endif  /* THREAD_POOL__HPP */
---
+++ src/util/thread_pool.cpp
@@ -0,0 +1,93 @@
+/*  $Id$
+* ===========================================================================
+*
+*                            PUBLIC DOMAIN NOTICE
+*               National Center for Biotechnology Information
+*
+*  This software/database is a "United States Government Work" under the
+*  terms of the United States Copyright Act.  It was written as part of
+*  the author's official duties as a United States Government employee and
+*  thus cannot be copyrighted.  This software/database is freely available
+*  to the public for use. The National Library of Medicine and the U.S.
+*  Government have not placed any restriction on its use or reproduction.
+*
+*  Although all reasonable efforts have been taken to ensure the accuracy
+*  and reliability of the software and data, the NLM and the U.S.
+*  Government do not and cannot warrant the performance or results that
+*  may be obtained by using this software or data. The NLM and the U.S.
+*  Government disclaim all warranties, express or implied, including
+*  warranties of performance, merchantability or fitness for any particular
+*  purpose.
+*
+*  Please cite the author in any work or product based on this material.
+*
+* ===========================================================================
+*
+* Author:  Aaron Ucko
+*
+* File Description:
+*   Pools of generic request-handling threads.
+*
+* ---------------------------------------------------------------------------
+* $Log$
+* Revision 1.1  2001/12/11 19:54:45  ucko
+* Introduce thread pools.
+*
+* ===========================================================================
+*/
+
+#include <corelib/ncbistd.hpp>
+#include <util/thread_pool.hpp>
+
+BEGIN_NCBI_SCOPE
+
+class CFatalRequest : public CStdRequest
+{
+public:
+    CFatalRequest(CSemaphore* sem) : m_Sem(sem) {}
+
+protected:
+    // Kill the current thread
+    virtual void Process(void);
+
+private:
+    CSemaphore* m_Sem;
+};
+
+
+void CFatalRequest::Process(void)
+{ 
+    if (m_Sem) {
+        m_Sem->Post();
+    }
+    CThread::Exit(0);
+}
+
+
+void CStdPoolOfThreads::KillAllThreads(bool wait)
+{
+    unsigned int   n;
+    {{
+        CMutexGuard guard(m_Mutex);
+        m_MaxThreads = 0;  // Forbid spawning new threads
+        n = m_ThreadCount; // Capture for use without mutex
+    }}
+
+    auto_ptr<CSemaphore> sem;
+    if (wait) {
+        sem.reset(new CSemaphore(0, n));
+    }
+    CFatalRequest* poison = new CFatalRequest(sem.get());
+
+    for (unsigned int i = 0;  i < n;  i++) {
+        AcceptRequest(poison);
+    }
+    if (wait) {
+        // Wait for all threads to post to the semaphore
+        for (unsigned int i = 0;  i < n;  i++) {
+            sem->Wait();
+        }
+    }
+}
+
+END_NCBI_SCOPE
