85af5c1f867e1e33e7702545f2e23f3163d9e198
Denis Vakatov <vakatov@ncbi.nlm.nih.gov>
2001-12-17 17:51:57 +0000

    Fallback cleanup (using TRAP);  also, detect if KILL did not work out well
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@3974 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/connect/test/test_ncbi_socket.sh
+++ src/connect/test/test_ncbi_socket.sh
@@ -1,9 +1,14 @@
 #! /bin/sh
 # $Id$
+
+exit_code=0
+
 ./test_ncbi_socket 5555 &
-pid=$!
+server_pid=$!
+trap 'kill $server_pid' 1 2 15
+
 sleep 1
-./test_ncbi_socket localhost 5555
-exit=$?
-kill $pid
-exit $exit
+./test_ncbi_socket localhost 5555  ||  exit_code=1
+
+kill $server_pid  ||  exit_code=2
+exit $exit_code
