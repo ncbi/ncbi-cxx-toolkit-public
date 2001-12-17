c6cb826e9f79d96cac83dd2f1f40f66c689e9928
Aaron Ucko <ucko@ncbi.nlm.nih.gov>
2001-12-17 17:49:25 +0000

    Sleep before launching client so that server has more time to initialize.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@3973 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/connect/test/test_threaded_client_server.sh
+++ src/connect/test/test_threaded_client_server.sh
@@ -5,6 +5,7 @@ status=0
 ./test_threaded_server -port 12345 -queue 50 &
 server_pid=$!
 trap 'kill $server_pid' 1 2 15
+sleep 1
 ./test_threaded_client -port 12345 || status=1
 kill $server_pid || status=2
 exit $status
