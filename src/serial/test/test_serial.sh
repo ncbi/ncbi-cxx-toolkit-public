230f387d5292a2bba5ee9466a45d659fea3653ed
Denis Vakatov <vakatov@ncbi.nlm.nih.gov>
2002-01-16 23:20:42 +0000

    Safer file content comparing;  plus some beautification
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@4097 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/serial/test/test_serial.sh
+++ src/serial/test/test_serial.sh
@@ -1,25 +1,34 @@
-#! /bin/sh
+@script_shell@
 
-if test -n "@NCBI_C_ncbi@"; then
+#
+# $Id$
+# Author:  Eugene Vasilchenko
+# 
+
+
+if test -n "@NCBI_C_ncbi@" ; then
     testprefix=c
 else
     testprefix=cpp
 fi
 
-for f in webenv.ent webenv.bin ${testprefix}test.asn ${testprefix}test.asb; do
+for f in webenv.ent webenv.bin ${testprefix}test.asn ${testprefix}test.asb ; do
     cp @srcdir@/$f ./
 done
+
 mv ${testprefix}test.asn test.asn
 mv ${testprefix}test.asb test.asb
 
 @build_root@/bin/serialtest > test.out 2> test.err
 
-for f in webenv.ent webenv.bin test.asn test.asb; do
-    i=`tr -d "\n " < $f`
-    o=`tr -d "\n " < ${f}o`
-    if test $i != $o; then
-        echo Error in file ${f}o;
+for f in webenv.ent webenv.bin test.asn test.asb ; do
+    tr -d "\n " < ${f}  > ${f}.$$.tmp
+    tr -d "\n " < ${f}o > ${f}o.$$.tmp
+    cmp -s ${f}.$$.tmp ${f}o.$$.tmp
+    if test $? -ne 0 ; then
+        echo "Error in file ${f}o"
     fi
+    rm ${f}.$$.tmp ${f}o.$$.tmp
 done
 
-echo Test finished.
+echo "Test finished."
