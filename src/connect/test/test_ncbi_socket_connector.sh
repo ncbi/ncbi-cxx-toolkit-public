f15bc9f494019fd9f129404ee8cbf6fffbb5b2e6
Anton Lavrentiev <lavr@ncbi.nlm.nih.gov>
2002-01-28 19:31:04 +0000

    Test port number has been changed
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@4182 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/connect/test/test_ncbi_socket_connector.sh
+++ src/connect/test/test_ncbi_socket_connector.sh
@@ -3,12 +3,12 @@
 
 exit_code=0
 
-./socket_io_bouncer 5555 &
+./socket_io_bouncer 5151 &
 server_pid=$!
 trap 'kill $server_pid' 1 2 15
 
 sleep 1
-./test_ncbi_socket_connector localhost 5555  ||  exit_code=1
+./test_ncbi_socket_connector localhost 5151  ||  exit_code=1
 
 kill $server_pid  ||  exit_code=2
 exit $exit_code
