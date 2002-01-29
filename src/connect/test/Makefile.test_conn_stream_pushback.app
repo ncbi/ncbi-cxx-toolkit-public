97905a6c437332faafd6886d9006e78b0737f854
Anton Lavrentiev <lavr@ncbi.nlm.nih.gov>
2002-01-29 16:03:23 +0000

    Redesigned to use xpbacktest
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@4209 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/connect/test/Makefile.test_conn_stream_pushback.app
+++ src/connect/test/Makefile.test_conn_stream_pushback.app
@@ -2,7 +2,7 @@
 
 APP = test_conn_stream_pushback
 SRC = test_conn_stream_pushback
-LIB = xconnect xutil xncbi
+LIB = xconnect xpbacktest xutil xncbi
 
 LIBS = $(NETWORK_LIBS) $(ORIG_LIBS)
 #LINK = purify $(ORIG_LINK)
--- src/connect/test/test_conn_stream_pushback.cpp
+++ src/connect/test/test_conn_stream_pushback.cpp
@@ -30,6 +30,9 @@
  *
  * --------------------------------------------------------------------------
  * $Log$
+ * Revision 1.5  2002/01/29 16:03:17  lavr
+ * Redesigned to use xpbacktest
+ *
  * Revision 1.4  2002/01/28 20:28:08  lavr
  * Redesigned; pushbacks and standard putbacks and ungets are used together
  *
@@ -45,39 +48,16 @@
  * ==========================================================================
  */
 
-#include "test_assert.h"
+#include "../../util/test/pbacktest.hpp"
 
-#include <connect/ncbi_util.h>
 #include <connect/ncbi_conn_stream.hpp>
 #include <connect/ncbi_core_cxx.hpp>
-#include <util/stream_pushback.hpp>
-#include <stdlib.h>
-#include <time.h>
-
-
-BEGIN_NCBI_SCOPE
-
-
-static const size_t kBufferSize = 1024*1024;
-
-
-static size_t s_Read(iostream& ios, char* buffer, size_t size)
-{
-    ios.read(buffer, size);
-    size_t count = ios.gcount();
-    if (count  &&  ios.eof())
-        ios.clear();
-    return count;
-}
-
-
-END_NCBI_SCOPE
+#include <connect/ncbi_util.h>
 
 
 int main(int argc, char* argv[])
 {
     USING_NCBI_SCOPE;
-    size_t i, j, k;
 
     CORE_SetLOG(LOG_cxx2c());
     SetDiagTrace(eDT_Enable);
@@ -92,105 +72,10 @@ int main(int argc, char* argv[])
     LOG_POST("Creating HTTP connection to http://" + host + path + args);
     CConn_HttpStream ios(host, path, args, uhdr);
 
-    LOG_POST("Generating array of random data");
-    unsigned int seed = argc > 1
-        ? (unsigned int) atoi(argv[1]) : (unsigned int) time(0);
-    LOG_POST("Seed = " << seed);
-    srand(seed);
-    char *buf1 = new char[kBufferSize + 1];
-    char *buf2 = new char[kBufferSize + 2];
-    for (j = 0; j < kBufferSize/1024; j++) {
-        for (i = 0; i < 1024 - 1; i++)
-            buf1[j*1024 + i] = "0123456789"[rand() % 10];
-        buf1[j*1024 + 1024 - 1] = '\n';
-    }
-    buf1[kBufferSize] = '\0';
+    int n = TEST_StreamPushback(ios,
+                                argc > 1 ? (unsigned int) atoi(argv[1]) : 0,
+                                false/*no rewind*/);
 
-    LOG_POST("Sending data down to server");
-    if (!(ios << buf1)) {
-        ERR_POST("Error sending data");
-        return 1;
-    }
-
-    LOG_POST("Doing random reads and pushbacks of the reply");
-    char c = 0;
-    size_t buflen = 0;
-    for (k = 0; buflen < kBufferSize; k++) {
-        size_t m = kBufferSize + 1 - buflen;
-        if (m > 10)
-            m /= 10;
-        i = rand() % m + 1;
-
-        if (buflen + i > kBufferSize + 1)
-            i = kBufferSize + 1 - buflen;
-        LOG_POST(Info << "Reading " << i << " byte(s)");
-        j = s_Read(ios, &buf2[buflen], i);
-        if (!ios.good() && !ios.eof()) {
-            ERR_POST("Error receiving data");
-            return 2;
-        }
-        if (j != i)
-            LOG_POST("Bytes requested: " << i << ", received: " << j);
-        if (c && buf2[buflen] != c) {
-            LOG_POST(Error <<
-                     "Mismatch, putback: " << c << ", read: " << buf2[buflen]);
-            return 2;
-        } else
-            c = 0;
-
-        if (ios.good() && rand() % 5 == 0 && j > 1) {
-            c = buf2[buflen + j - 1];
-            if (rand() & 1) {
-                LOG_POST(Info << "Putback ('" << c << "')");
-                ios.putback(c);
-            } else {
-                LOG_POST(Info << "Unget ('" << c << "')");
-                ios.unget();
-            }
-            if (!ios.good()) {
-                ERR_POST("Error putting a byte back");
-                return 2;
-            }
-            j--;
-        }
-        _ASSERT(j > 0);
-
-        buflen += j;
-        i = rand() % j + 1;
-        if (i != j || --i) {
-            buflen -= i;
-            LOG_POST(Info << "Pushing back " << i << " byte(s)");
-            UTIL_StreamPushback(ios, &buf2[buflen], i);
-            c = buf2[buflen];
-        }
-        //LOG_POST(Info << "Obtained " << buflen << " of " << kBufferSize);
-    }
-
-    LOG_POST(Info << buflen << " bytes obtained in " << k << " iteration(s)" <<
-             (ios.eof() ? " (EOF)" : ""));
-    buf2[buflen] = '\0';
-
-    for (i = 0; i < kBufferSize; i++) {
-        if (!buf2[i])
-            break;
-        if (buf2[i] != buf1[i])
-            break;
-    }
-
-    if (i < kBufferSize) {
-        ERR_POST("Not entirely bounced, mismatch position: " << i + 1);
-        return 1;
-    } else if (buflen > kBufferSize) {
-        ERR_POST("Sent: " << kBufferSize << ", bounced: " << buflen);
-        return 1;
-    } else
-        LOG_POST(Info << "Test passed");
-    
-    CORE_SetREG(0);
     CORE_SetLOG(0);
-
-    delete[] buf1;
-    delete[] buf2;
-
-    return 0/*okay*/;
+    return n;
 }
