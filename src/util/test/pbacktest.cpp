d334188d437daea48b95c4a0a1746451f0ffd2b4
Anton Lavrentiev <lavr@ncbi.nlm.nih.gov>
2002-02-04 20:23:40 +0000

    Long comment and workaround for MSVC putback()/unget() bug/feature Additional test for stream positioning during read back
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@4259 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/util/test/pbacktest.cpp
+++ src/util/test/pbacktest.cpp
@@ -30,6 +30,10 @@
  *
  * --------------------------------------------------------------------------
  * $Log$
+ * Revision 1.3  2002/02/04 20:23:40  lavr
+ * Long comment and workaround for MSVC putback()/unget() bug/feature
+ * Additional test for stream positioning during read back
+ *
  * Revision 1.2  2002/01/30 20:07:13  lavr
  * Verbose message about the difference, not just a position number
  *
@@ -56,13 +60,37 @@ static const size_t kBufferSize = 1024*1024;
 
 static size_t s_Read(iostream& ios, char* buffer, size_t size)
 {
+    if (ios.eof())
+        ios.clear();
     ios.read(buffer, size);
     size_t count = ios.gcount();
-    if (count  &&  ios.eof())
-        ios.clear();
     return count;
 }
 
+
+/* NOTE about MSVC compiler and its C++ std. library:
+ *
+ * The C++ standard is very confusing on the stream's ability to do
+ * pushback() and unget(); it says nothing about the case whether
+ * they are guaranteed, and if so, under what circumstances.
+ * The only clear message is seen in "The C++ Programming Language",
+ * 3rd ed. by B.Stroustrup, p.644, which reads "what is guaranteed
+ * is that you can back up one character after a successful read."
+ * Most of the implementations obey this; but MSVC's one does not.
+ * If you do only unformatted reads from the file, your program gets data
+ * via unbuffered stream (!) because basic_filebuf::uflow() gets control
+ * and never allocates internal filebuf buffer; whereas in formatted
+ * input filebuf::underflow() is used, and it does proper buffer allocation,
+ * which in turn allows the backup after a read.
+ * A bug or not a bug, we had to put putback and unget tests to only
+ * follow a first pushback operation, which (internally) changes the
+ * stream buffer and always keeps "usual backup condition", described in
+ * the standard, thus allowing at least one-char backup after a read.
+ *
+ * So, if you plan to do a portable backup, which can possibly follow
+ * an unformatted read, do so by using UTIL_StreamPushback() instead of
+ * standard means, as the latter may be broken.
+ */
 
 extern int TEST_StreamPushback(iostream&    ios,
                                unsigned int seed_in,
@@ -94,6 +122,9 @@ extern int TEST_StreamPushback(iostream&    ios,
     LOG_POST("Doing random reads and pushbacks of the reply");
     char c = 0;
     size_t buflen = 0;
+#ifdef NCBI_COMPILER_MSVC
+    bool first_pushback_done = false;
+#endif
     for (k = 0; buflen < kBufferSize; k++) {
         size_t m = kBufferSize + 1 - buflen;
         if (m > 10)
@@ -110,37 +141,53 @@ extern int TEST_StreamPushback(iostream&    ios,
         }
         if (j != i)
             LOG_POST("Bytes requested: " << i << ", received: " << j);
+        _ASSERT(j > 0);
         if (c && buf2[buflen] != c) {
             LOG_POST(Error <<
                      "Mismatch, putback: " << c << ", read: " << buf2[buflen]);
             return 2;
         } else
             c = 0;
+        buflen += j;
 
-        if (ios.good() && rand() % 5 == 0 && j > 1) {
-            c = buf2[buflen + j - 1];
-            if (rand() & 1) {
-                LOG_POST(Info << "Putback ('" << c << "')");
-                ios.putback(c);
-            } else {
-                LOG_POST(Info << "Unget ('" << c << "')");
-                ios.unget();
-            }
+        if (rewind && rand() % 7 == 0 && buflen < kBufferSize) {
+            LOG_POST(Info << "Testing seekg(" << buflen << ", ios::beg)");
+            ios.seekg(buflen, ios::beg);
             if (!ios.good()) {
-                ERR_POST("Error putting a byte back");
+                ERR_POST("Error in stream re-positioning");
                 return 2;
             }
-            j--;
+        } else if (ios.good() && rand() % 5 == 0 && j > 1) {
+#ifdef NCBI_COMPILER_MSVC
+            if (!rewind || first_pushback_done) {
+#endif
+                c = buf2[--buflen];
+                if (rand() & 1) {
+                    LOG_POST(Info << "Putback ('" << c << "')");
+                    ios.putback(c);
+                } else {
+                    LOG_POST(Info << "Unget ('" << c << "')");
+                    ios.unget();
+                }
+                if (!ios.good()) {
+                    ERR_POST("Error putting a byte back");
+                    return 2;
+                }
+                j--;
+#ifdef NCBI_COMPILER_MSVC
+            }
+#endif
         }
-        _ASSERT(j > 0);
 
-        buflen += j;
         i = rand() % j + 1;
         if (i != j || --i) {
             buflen -= i;
             LOG_POST(Info << "Pushing back " << i << " byte(s)");
             UTIL_StreamPushback(ios, &buf2[buflen], i);
             c = buf2[buflen];
+#ifdef NCBI_COMPILER_MSVC
+            first_pushback_done = true;
+#endif
         }
         //LOG_POST(Info << "Obtained " << buflen << " of " << kBufferSize);
     }
