7933962f943f3e1c7f472242018072a61831acab
Vladimir Ivanov <ivanov@ncbi.nlm.nih.gov>
2002-02-13 20:19:44 +0000

    Added support of dynamic popup menus
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@4330 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/html/test/test_jsmenu.cpp
+++ src/html/test/test_jsmenu.cpp
@@ -1,47 +1,33 @@
 /*  $Id$
-* ===========================================================================
-*
-*                            PUBLIC DOMAIN NOTICE
-*               National Center for Biotechnology Information
-*
-*  This software/database is a "United States Government Work" under the
-*  terms of the United States Copyright Act.  It was written as part of
-*  the author's official duties as a United States Government employee and
-*  thus cannot be copyrighted.  This software/database is freely available
-*  to the public for use. The National Library of Medicine and the U.S.
-*  Government have not placed any restriction on its use or reproduction.
-*
-*  Although all reasonable efforts have been taken to ensure the accuracy
-*  and reliability of the software and data, the NLM and the U.S.
-*  Government do not and cannot warrant the performance or results that
-*  may be obtained by using this software or data. The NLM and the U.S.
-*  Government disclaim all warranties, express or implied, including
-*  warranties of performance, merchantability or fitness for any particular
-*  purpose.
-*
-*  Please cite the author in any work or product based on this material.
-*
-* ===========================================================================
-*
-* Author:  Vladimir Ivanov, Denis Vakatov
-*
-* File Description:
-*   Test Javascript popup menus
-*
-* ---------------------------------------------------------------------------
-* $Log$
-* Revision 1.3  2001/08/15 19:42:39  ivanov
-* Added test for use method AddMenuItem( node, ...)
-*
-* Revision 1.2  2001/08/14 17:02:00  ivanov
-* Changed tests for popup menu in connection with change means init menu.
-* Added second test for use menu in HTML templates.
-*
-* Revision 1.1  2001/07/23 21:50:33  vakatov
-* Initial revision
-*
-* ===========================================================================
-*/
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Author:  Vladimir Ivanov, Denis Vakatov
+ *
+ * File Description:   Test Javascript popup menus
+ * 
+ */
 
 #include <corelib/ncbiapp.hpp>
 #include <corelib/ncbienv.hpp>
@@ -121,7 +107,7 @@ static void Test_Html(void)
     body->AppendChild(anchor3);
 
     // Enable using popup menus (we can skip call this function)
-    // html->EnablePopupMenu();
+    //html->EnablePopupMenu();
     
     // Print in HTML format
     html->Print(cout);
@@ -175,6 +161,9 @@ static void Test_Template(void)
     view->AppendChild(anchor2);
 
     page.AddTagMap("VIEW", view);
+
+    // Enable using popup menus (we can skip call this function)
+    //page.EnablePopupMenu();
 
     // Print test result
     page.Print(cout, CNCBINode::eHTML);
@@ -251,3 +240,24 @@ int main(int argc, const char* argv[])
 {
     return theTestApplication.AppMain(argc, argv, 0, eDS_Default, 0);
 }
+
+
+/*
+ * ===========================================================================
+ *
+ * $Log$
+ * Revision 1.4  2002/02/13 20:19:44  ivanov
+ * Added support of dynamic popup menus
+ *
+ * Revision 1.3  2001/08/15 19:42:39  ivanov
+ * Added test for use method AddMenuItem( node, ...)
+ *
+ * Revision 1.2  2001/08/14 17:02:00  ivanov
+ * Changed tests for popup menu in connection with change means init menu.
+ * Added second test for use menu in HTML templates.
+ *
+ * Revision 1.1  2001/07/23 21:50:33  vakatov
+ * Initial revision
+ *
+ * ===========================================================================
+ */
