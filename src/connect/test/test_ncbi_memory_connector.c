7e8e2772d961e6f6b11fcde890aa88c56ce11914
Anton Lavrentiev <lavr@ncbi.nlm.nih.gov>
2002-02-20 20:53:48 +0000

    Use xconntest to perform standard tests
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@4376 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/connect/test/test_ncbi_memory_connector.c
+++ src/connect/test/test_ncbi_memory_connector.c
@@ -30,6 +30,9 @@
  *
  * --------------------------------------------------------------------------
  * $Log$
+ * Revision 6.2  2002/02/20 20:53:48  lavr
+ * Use xconntest to perform standard tests
+ *
  * Revision 6.1  2002/02/20 19:14:40  lavr
  * Initial revision
  *
@@ -38,95 +41,33 @@
 
 #include "test_assert.h"
 
-#include <connect/ncbi_connection.h>
+#include "ncbi_conntest.h"
 #include <connect/ncbi_memory_connector.h>
 #include <connect/ncbi_util.h>
-#include <stdio.h>
-#include <stdlib.h>
 
 
-static const char* s_ProgramName;
-
-static void Usage(const char* message)
+int main(void)
 {
-    fprintf(stderr,
-            "\nUsage: %s <input_file>\n"
-            "\nERROR:  %s!\n",
-            s_ProgramName, message);
-    abort();
-}
+    static STimeout timeout/* = 0 */;
+    CONNECTOR       connector;
+    FILE*           data_file;
 
-
-int main(int argc, const char* argv[])
-{
-    CONNECTOR  connector;
-    EIO_Status status;
-    CONN       conn;
-    FILE*      fp;
-
-    /* cmd.-line args */
-    s_ProgramName = argv[0];
-    if (argc != 2) {
-        Usage("Input file name required");
-    }
-
-    if (!(fp = fopen(argv[1], "r"))) {
-        Usage("Input file must exist and be readable");
-    }
-
-    /* log and data log streams */
+    /* Log and data-log streams */
     CORE_SetLOGFILE(stderr, 0/*false*/);
-    fprintf(stderr, "Starting the MEMORY CONNECTOR test...\n");
+    data_file = fopen("test_ncbi_memory_connector.log", "wb");
+    assert(data_file);
 
-    /* create connector, and bind it to the connection */
+     /* Run the tests */
     connector = MEMORY_CreateConnector(0);
-    if ( !connector ) {
-        Usage("Failed to create MEMORY connector");
-    }
+    CONN_TestConnector(connector, &timeout, data_file,
+                       fTC_SingleBounceCheck);
 
-    verify(CONN_Create(connector, &conn) == eIO_Success);
- 
-    /* pump the data from file thru connection and compare */
-    while (!feof(fp)) {
-        char buf[1000];
-        char fub[sizeof(buf)];
-        size_t n_read, n_written;
+    connector = MEMORY_CreateConnector(0);
+    CONN_TestConnector(connector, &timeout, data_file,
+                       fTC_Everything);
 
-        /* read from the source */
-        if (!(n_read = fread(buf, 1, sizeof(buf), fp))) {
-            if (!feof(fp))
-                fprintf(stderr, "File read error\n");
-            break;
-        }
-
-        /* write to the connection */
-        status = CONN_Write(conn, buf, n_read, &n_written);
-        if (status != eIO_Success || n_written != n_read) {
-            fprintf(stderr, "CONN_Write() failed (status: %s)\n",
-                    IO_StatusStr(status));
-            return 1;
-        }
-
-        /* read back from the connection */
-        status = CONN_Read(conn, fub, sizeof(fub), &n_read, eIO_Plain);
-        if (status != eIO_Success || n_read != n_written) {
-            fprintf(stderr, "CONN_Write() failed (status: %s)\n",
-                    IO_StatusStr(status));
-            return 1;
-        }
-
-        /* compare */
-        if (memcmp(buf, fub, n_read) != 0) {
-            fprintf(stderr, "MISMATCH");
-            return 1;
-        }
-
-        fprintf(stderr, "PROCESSED: %ld  bytes\n", (long) n_read);
-    }
-    
-    /* cleanup, exit */
-    fclose(fp);
-    verify(CONN_Close(conn) == eIO_Success);
+    /* Cleanup and Exit */
+    fclose(data_file);
     CORE_SetLOG(0);
     return 0;
 }
