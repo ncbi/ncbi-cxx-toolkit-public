ce6f1b9c9bb51d75fb03f970fd428f55c9437236
Aaron Ucko <ucko@ncbi.nlm.nih.gov>
2002-03-13 15:06:12 +0000

    Scaled down to save time.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@4495 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/connect/test/test_threaded_client_server.sh
+++ src/connect/test/test_threaded_client_server.sh
@@ -4,10 +4,10 @@
 status=0
 port="555`expr $$ % 100`"
 
-./test_threaded_server -port $port -queue 50 &
+./test_threaded_server -port $port &
 server_pid=$!
 trap 'kill $server_pid' 1 2 15
 sleep 1
-./test_threaded_client -port $port || status=1
+./test_threaded_client -port $port -requests 34 || status=1
 kill $server_pid || status=2
 exit $status
