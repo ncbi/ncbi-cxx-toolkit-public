a5eda76e13b521c31f1970bb292c88b08a3512ee
Denis Vakatov <vakatov@ncbi.nlm.nih.gov>
2002-03-14 23:19:22 +0000

    Adjusted for the apparent LINUX specifics
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@4506 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/corelib/test/test_ncbi_system.sh
+++ src/corelib/test/test_ncbi_system.sh
@@ -1,5 +1,14 @@
 #! /bin/sh
+# $Id$
 
-./test_ncbi_system cpu; if test $? -ne 255; then exit 1; fi
-./test_ncbi_system mem; if test $? -ne 255; then exit 2; fi
+
+./test_ncbi_system cpu;  test_cpu=$?;  echo "exit_code(CPU-test) = $test_cpu"
+./test_ncbi_system mem;  test_mem=$?;  echo "exit_code(MEM-test) = $test_mem"
+
+if test "`uname -s`" = "Linux" ; then
+   test $test_cpu -eq 137  -a  $test_mem -eq   0  ||  exit 1
+else
+   test $test_cpu -eq 255  -a  $test_mem -eq 255  ||  exit 1
+fi
+
 exit 0
