f45f8f975d3fc8c5b5a7514b718567f50747374b
Vladimir Ivanov <ivanov@ncbi.nlm.nih.gov>
2002-04-05 14:28:15 +0000

    Checking exit code for MEM-test under Linux changed to 255
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@4691 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/corelib/test/test_ncbi_system.sh
+++ src/corelib/test/test_ncbi_system.sh
@@ -6,7 +6,7 @@
 ./test_ncbi_system mem;  test_mem=$?;  echo "exit_code(MEM-test) = $test_mem"
 
 if test "`uname -s`" = "Linux" ; then
-   test $test_cpu -eq 137  -a  $test_mem -eq   0  ||  exit 1
+   test $test_cpu -eq 137  -a  $test_mem -eq 255  ||  exit 1
 else
    test $test_cpu -eq 255  -a  $test_mem -eq 255  ||  exit 1
 fi
