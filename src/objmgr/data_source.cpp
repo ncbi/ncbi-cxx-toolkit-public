6eccb878ec000ddbbeb3b1a6080aece4deb3ad0c
Aleksey Grichenko <grichenk@ncbi.nlm.nih.gov>
2002-04-05 21:23:08 +0000

    More duplicate id warnings fixed
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@4699 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objmgr/data_source.cpp
+++ src/objmgr/data_source.cpp
@@ -30,6 +30,9 @@
 *
 * ---------------------------------------------------------------------------
 * $Log$
+* Revision 1.32  2002/04/05 21:23:08  grichenk
+* More duplicate id warnings fixed
+*
 * Revision 1.31  2002/04/05 20:27:52  grichenk
 * Fixed duplicate identifier warning
 *
@@ -1436,22 +1439,22 @@ void CDataSource::x_DropAlign(const CSeq_align& align,
         // Find the TSE containing the align
         TTSESet::iterator tse_info = tse_set->second.begin();
         for ( ; tse_info != tse_set->second.end(); ++tse_info) {
-            TAnnotMap::iterator annot = (*tse_info)->m_AnnotMap.find(
+            TAnnotMap::iterator annot_it = (*tse_info)->m_AnnotMap.find(
                 mapit->first);
-            if (annot == (*tse_info)->m_AnnotMap.end())
+            if (annot_it == (*tse_info)->m_AnnotMap.end())
                 continue;
-            TRangeMap::iterator rg = annot->second.begin(
+            TRangeMap::iterator rg = annot_it->second.begin(
                 mapit->second.GetOverlappingRange());
-            for ( ; rg != annot->second.end(); ++rg) {
+            for ( ; rg != annot_it->second.end(); ++rg) {
                 if (rg->second->IsAlign()  &&
                     &(rg->second->GetAlign()) == &align)
                     break;
             }
-            if (rg == annot->second.end())
+            if (rg == annot_it->second.end())
                 continue;
             // Delete the align from all indexes
-            annot->second.erase(rg);
-            if (annot->second.size() == 0) {
+            annot_it->second.erase(rg);
+            if (annot_it->second.size() == 0) {
                 tse_set->second.erase(tse_info);
             }
             if (tse_set->second.size() == 0) {
@@ -1477,22 +1480,22 @@ void CDataSource::x_DropGraph(const CSeq_graph& graph,
         // Find the TSE containing the graph
         TTSESet::iterator tse_info = tse_set->second.begin();
         for ( ; tse_info != tse_set->second.end(); ++tse_info) {
-            TAnnotMap::iterator annot = (*tse_info)->m_AnnotMap.find(
+            TAnnotMap::iterator annot_it = (*tse_info)->m_AnnotMap.find(
                 mapit->first);
-            if (annot == (*tse_info)->m_AnnotMap.end())
+            if (annot_it == (*tse_info)->m_AnnotMap.end())
                 continue;
-            TRangeMap::iterator rg = annot->second.begin(
+            TRangeMap::iterator rg = annot_it->second.begin(
                 mapit->second.GetOverlappingRange());
-            for ( ; rg != annot->second.end(); ++rg) {
+            for ( ; rg != annot_it->second.end(); ++rg) {
                 if (rg->second->IsGraph()  &&
                     &(rg->second->GetGraph()) == &graph)
                     break;
             }
-            if (rg == annot->second.end())
+            if (rg == annot_it->second.end())
                 continue;
             // Delete the graph from all indexes
-            annot->second.erase(rg);
-            if (annot->second.size() == 0) {
+            annot_it->second.erase(rg);
+            if (annot_it->second.size() == 0) {
                 tse_set->second.erase(tse_info);
             }
             if (tse_set->second.size() == 0) {
