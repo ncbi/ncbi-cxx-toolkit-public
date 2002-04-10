f3b70e93ac3655191cb1720f671b82c822304318
Vladimir Ivanov <ivanov@ncbi.nlm.nih.gov>
2002-04-10 14:47:04 +0000

    Changed assert() to condition with call Abort()
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@4744 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/corelib/ncbidbg_p.hpp
+++ src/corelib/ncbidbg_p.hpp
@@ -2,50 +2,40 @@
 #define NCBIDBG_P__HPP
 
 /*  $Id$
-* ===========================================================================
-*
-*                            PUBLIC DOMAIN NOTICE
-*               National Center for Biotechnology Information
-*
-*  This software/database is a "United States Government Work" under the
-*  terms of the United States Copyright Act.  It was written as part of
-*  the author's official duties as a United States Government employee and
-*  thus cannot be copyrighted.  This software/database is freely available
-*  to the public for use. The National Library of Medicine and the U.S.
-*  Government have not placed any restriction on its use or reproduction.
-*
-*  Although all reasonable efforts have been taken to ensure the accuracy
-*  and reliability of the software and data, the NLM and the U.S.
-*  Government do not and cannot warrant the performance or results that
-*  may be obtained by using this software or data. The NLM and the U.S.
-*  Government disclaim all warranties, express or implied, including
-*  warranties of performance, merchantability or fitness for any particular
-*  purpose.
-*
-*  Please cite the author in any work or product based on this material.
-*
-* ===========================================================================
-*
-* Author:  Andrei Gourianov, gouriano@ncbi.nlm.nih.gov
-*
-* File Description:
-*   declaration of debugging function(s)
-*
-* ---------------------------------------------------------------------------
-* $Log$
-* Revision 1.2  2001/12/14 17:58:53  gouriano
-* changed GetValidateAction so it never returns Default
-*
-* Revision 1.1  2001/12/13 19:46:59  gouriano
-* added xxValidateAction functions
-*
-* ===========================================================================
-*/
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Author:  Andrei Gourianov, gouriano@ncbi.nlm.nih.gov
+ *
+ * File Description:
+ *   declaration of debugging function(s)
+ * 
+ */
+
 
 #include <corelib/ncbi_safe_static.hpp>
 #include <corelib/ncbithr.hpp>
-
-#include <assert.h>
 
 
 BEGIN_NCBI_SCOPE
@@ -54,7 +44,10 @@ BEGIN_NCBI_SCOPE
 #if defined(_DEBUG)
 
 // 'simple' verify
-#define xncbi_Verify(expression) assert(expression)
+#define xncbi_Verify(expression) \
+if ( !(expression) ) { \
+    Abort(); \
+}
 
 // Abort execution (by default), or throw exception (if explicitly specified
 // by calling xncbi_SetValidateAction(eValidate_Throw)) if
@@ -68,7 +61,9 @@ do { \
         } \
     } \
     else { \
-        assert( expression); \
+        if ( !(expression) ) { \
+            Abort(); \
+        } \
     } \
 } while (0)
 
@@ -89,5 +84,20 @@ do { \
 
 
 END_NCBI_SCOPE
+
+/*
+ * ===========================================================================
+ * $Log$
+ * Revision 1.3  2002/04/10 14:47:04  ivanov
+ * Changed assert() to condition with call Abort()
+ *
+ * Revision 1.2  2001/12/14 17:58:53  gouriano
+ * changed GetValidateAction so it never returns Default
+ *
+ * Revision 1.1  2001/12/13 19:46:59  gouriano
+ * added xxValidateAction functions
+ *
+ * ===========================================================================
+ */
 
 #endif // NCBIDBG_P__HPP
