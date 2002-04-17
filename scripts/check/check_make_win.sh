f5b57386e1c75438b83699e417747a5c28884c2c
Vladimir Ivanov <ivanov@ncbi.nlm.nih.gov>
2002-04-17 16:45:24 +0000

    Fixed command line to run test -- modification PATH directly in cmd.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@4853 78c7ea69-d796-4a43-9a09-de51944f1b03

--- scripts/check/check_make_win.sh
+++ scripts/check/check_make_win.sh
@@ -252,8 +252,7 @@ RunTest() {
    cd "\$x_path"
 
    # NOTE: Run SH need to correct run tests with parameters in single quotes
-   x_run="PATH='.:\$PATH'; \$x_run" 
-   (sh -c "\$x_run") >> \$x_test_out 2>&1
+   (sh -c "PATH=.:\$PATH; \$x_run") >> \$x_test_out 2>&1
    result=\$?
 
    # Write result of the test into the his output file
