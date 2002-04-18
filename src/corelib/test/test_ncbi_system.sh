c0fdee1084e2f24625ad1db60ba33265b32e9ad0
Vladimir Ivanov <ivanov@ncbi.nlm.nih.gov>
2002-04-18 18:46:37 +0000

    Adjusted for the apparent CYGWIN specifics
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@4868 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/corelib/test/test_ncbi_system.sh
+++ src/corelib/test/test_ncbi_system.sh
@@ -1,13 +1,16 @@
 #! /bin/sh
 # $Id$
 
-
 ./test_ncbi_system cpu;  test_cpu=$?;  echo "exit_code(CPU-test) = $test_cpu"
 ./test_ncbi_system mem;  test_mem=$?;  echo "exit_code(MEM-test) = $test_mem"
 
-if test "`uname -s`" = "Linux" ; then
+os=`uname -s`
+
+if test "$os" = "Linux" ; then
    test $test_cpu -eq 137  ||  exit 1
-   test $test_mem -eq 0 -o $test_mem -eq 255  ||  exit 1
+   test $test_mem -eq 0  -o  $test_mem -eq 255  ||  exit 1
+elif test -n "`echo $os | grep CYGWIN`" ; then
+   test $test_cpu -eq 3  -a  $test_mem -eq 3  ||  exit 1
 else
    test $test_cpu -eq 255  -a  $test_mem -eq 255  ||  exit 1
 fi
