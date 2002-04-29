140fa5c5335d045ab2693662a6d005c69d2a2b07
Vladimir Ivanov <ivanov@ncbi.nlm.nih.gov>
2002-04-29 19:25:29 +0000

    Another attempt to fix error with quotes in test command line. Changed means for time-guard run, now don't need accessory script.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@5008 78c7ea69-d796-4a43-9a09-de51944f1b03

--- scripts/check/check_exec.sh
+++ scripts/check/check_exec.sh
@@ -13,14 +13,12 @@
 #    timeout - maximal time of execution "cmd-line" in seconds
 #
 # Note:
-#    For protect infinity execution <cmd-line>, it run together with 
-#    time-quard script, which terminate check process if it still executing
-#    after "timeout" seconds. 
+#    For protect infinity execution <cmd-line>, this script terminate 
+#    check process if it still executing after "timeout" seconds.
 #    Script return <cmd-line> exit code or value above 0 in case error
 #    to parent shell.
 #
 ###########################################################################
-
 
 
 # Get parameters
@@ -30,17 +28,20 @@ script_dir=`dirname $0`
 script_dir=`(cd "$script_dir"; pwd)`
 
 # Run command
-sh -c "sleep 1; $*" &
+sleep 1  &&  "$@"  &
 pid=$!
-echo $pid
 
 # Execution time-guard
-check_exec_guard="$script_dir/check_exec_guard.sh"
-$check_exec_guard $timeout $pid &
-trap 'kill $! $pid' 1 2 15
+( sleep $timeout  &&  kill $pid ) >/dev/null 2>&1 &
+guard_pid=$!
+trap 'kill $guard_pid $pid' 1 2 15
 
 # Wait ending of execution
 wait $pid
+exit=$?
 
-# Return exit code
-exit $?
+# Terminate the time-guard, test terminated himself
+kill $guard_pid >/dev/null 2>&1
+
+# Return test exit code
+exit $exit
