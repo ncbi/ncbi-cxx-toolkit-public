0d358fb41ace30361e152573cf5263fbd8eb8f83
Christopher Lanczycki <lanczyck@ncbi.nlm.nih.gov>
2002-05-08 21:06:17 +0000

    top-level documentation revamp: added files
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@5109 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ doc/doc.js
@@ -0,0 +1,23 @@
+function print(t) { document.write(t); }
+function printbr(t) { document.write(t, "<br>"); }
+function print_author(username, realname, datestr, hostname)
+{
+    var host='ncbi.nlm.nih.gov';
+	if (hostname)
+	    host = hostname;
+	print('<tr><td>\n<address>\n');
+	print_mailto(username, realname, hostname);
+	print('</address>\n</td>');
+	if (datestr)
+	    print('<td ALIGN=RIGHT><i>' + datestr + '</i></td>\n');
+	//if (datestr && Date.parse(document.lastModified) !=0)
+	//    print('<td ALIGN=RIGHT><i>' + document.lastModified + '</i></td>\n');
+	print('</tr>\n');
+}
+function print_mailto(username, realname, hostname)
+{
+    var host='ncbi.nlm.nih.gov';
+    if (hostname)
+	    host = hostname;
+    print('<a href="mailto:' + username + '@' + host + '">' + realname + '</a>\n');
+}
