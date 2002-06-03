425054d1e11b27ed41337c022dfbfcd6e4bcf0d2
Andrei Gourianov <gouriano@ncbi.nlm.nih.gov>
2002-06-03 20:26:10 +0000

    initial revision
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@5305 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ include/util/ddump_viewer.hpp
@@ -0,0 +1,80 @@
+#ifndef DEBUGDUMP_VIEWER__H
+#define DEBUGDUMP_VIEWER__H
+
+/*  $Id$
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Author:  Andrei Gourianov
+ *
+ * File Description:
+ *      Console Debug Dump Viewer
+ *
+ */
+
+#include <corelib/ncbistd.hpp>
+#include <corelib/ddumpable.hpp>
+
+BEGIN_NCBI_SCOPE
+
+//---------------------------------------------------------------------------
+//
+
+#define DEBUGDUMP_BPT(name,obj)  \
+    do {CDebugDumpViewer().Bpt(name,&(obj),__FILE__,__LINE__);} while (0)
+
+
+//---------------------------------------------------------------------------
+//  CDebugDumpViewer interface
+
+class CDebugDumpViewer
+{
+public:
+    CDebugDumpViewer() {}
+    virtual ~CDebugDumpViewer() {}
+
+    void Bpt(const string& name, const CDebugDumpable* curr_object,
+             const char* file, int line);
+private:
+    void        Info(const string& name, const CDebugDumpable* curr_object,
+                     const string& location);
+    bool        GetInput(string& input);
+    const void* StrToPtr(const string& str);
+    bool        CheckAddr( const void* addr, bool report);
+};
+
+
+END_NCBI_SCOPE
+
+/*
+ * ===========================================================================
+ * $Log$
+ * Revision 1.1  2002/06/03 20:26:10  gouriano
+ * initial revision
+ *
+ *
+ * ===========================================================================
+ */
+
+#endif // DEBUGDUMP_VIEWER__H
