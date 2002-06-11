02afc06c03ca2c286e2c0d58133b9e7d18e630b5
Vladimir Ivanov <ivanov@ncbi.nlm.nih.gov>
2002-06-11 19:25:35 +0000

    Added class CPipeIOStream, CPipeStreambuf
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@5422 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/connect/ncbi_pipe.cpp
+++ src/connect/ncbi_pipe.cpp
@@ -29,8 +29,10 @@
 
 #include <corelib/ncbipipe.hpp>
 #include <corelib/ncbiexec.hpp>
+#include <memory>
 
 #if defined(NCBI_OS_MSWIN)
+#  include <io.h>
 #  include <fcntl.h>
 #  if !defined(popen)
 #    define popen _popen
@@ -45,6 +47,7 @@
 #    define dup2 _dup2
 #  endif
 #elif defined(NCBI_OS_UNIX)
+#  include <unistd.h>
 #  include <errno.h>
 #elif defined(NCBI_OS_MAC)
 #  error "Class CPipe defined only for MS Windows and UNIX platforms"
@@ -57,12 +60,8 @@ BEGIN_NCBI_SCOPE
 // Throw exception with diagnostic message
 static void s_ThrowException(const string& what)
 {
-    const char* errmsg = strerror(errno);
-    if ( !errmsg ) {
-        errmsg = "unknown reason";
-    }
     // Throw exception
-    throw CPipe::CException("CPipe: " + what + ": " + errmsg);
+    throw CPipe::CException("CPipe: " + what);
 }
 
 
@@ -200,6 +199,8 @@ void CPipe::Open(const char *cmdname, const vector<string> args,
 
     size_t cnt = args.size(), i = 0;
     const char** x_args = new const char*[cnt+2];
+    typedef ArrayDeleter<const char*> TArgsDeleter;
+    AutoPtr<const char*, TArgsDeleter> p_args = x_args;
     
     iterate(vector<string>, arg, args) {
          x_args[i+1] = arg->c_str();
@@ -207,7 +208,6 @@ void CPipe::Open(const char *cmdname, const vector<string> args,
     }
     x_args[cnt+1] = 0;
     m_Pid = CExec::SpawnVP(CExec::eNoWait, cmdname, x_args);
-    delete[] x_args;
     if ( m_Pid == -1 ) {
         s_ThrowException("Error execution a child process");
     }
@@ -239,7 +239,7 @@ int CPipe::Close()
 }
 
 
-size_t CPipe::Read(void *buffer, const size_t count, const EChildIOHandle from_handle)
+size_t CPipe::Read(void *buffer, const size_t count, const EChildIOHandle from_handle) const
 {
     int fd = (from_handle == eStdOut) ? m_StdOut : m_StdErr;
     if ( m_Pid == -1  ||  fd == -1 ) {
@@ -249,12 +249,162 @@ size_t CPipe::Read(void *buffer, const size_t count, const EChildIOHandle from_h
 }
 
 
-size_t CPipe::Write(const void *buffer, const size_t count)
+size_t CPipe::Write(const void *buffer, const size_t count) const
 {
     if ( m_Pid == -1  ||  m_StdIn == -1 ) {
         s_ThrowException("Writing to unopened pipe");
     }
     return write(m_StdIn, buffer, count);
+}
+
+
+//////////////////////////////////////////////////////////////////////////////
+//
+// CPipeStrembuf
+//
+
+class CPipeStreambuf : public streambuf
+{
+public:
+    CPipeStreambuf(const CPipe& pipe, streamsize buf_size);
+    virtual ~CPipeStreambuf();
+
+    // Set handle of the child process for reading by default (eStdOut/eStdErr)
+    void SetReadHandle(const CPipe::EChildIOHandle handle) const;
+
+protected:
+    virtual CT_INT_TYPE overflow(CT_INT_TYPE c);
+    virtual CT_INT_TYPE underflow(void);
+    virtual int         sync(void);
+
+private:
+    const CPipe*    m_Pipe;      // underlying connection pipe
+    CT_CHAR_TYPE*   m_Buf;       // of size  2 * m_BufSize
+    CT_CHAR_TYPE*   m_WriteBuf;  // m_Buf
+    CT_CHAR_TYPE*   m_ReadBuf;   // m_Buf + m_BufSize
+    streamsize      m_BufSize;   // of m_WriteBuf, m_ReadBuf
+    mutable CPipe::EChildIOHandle
+                    m_ReadHandle;// handle for reading by default
+};
+
+
+CPipeStreambuf::CPipeStreambuf(const CPipe& pipe, const streamsize buf_size)
+    : m_Pipe(&pipe), m_Buf(0), m_BufSize(buf_size ? buf_size : 1), m_ReadHandle(CPipe::eStdOut)
+{
+    auto_ptr<CT_CHAR_TYPE> bp(new CT_CHAR_TYPE[2 * m_BufSize]);
+
+    m_WriteBuf = bp.get();
+    setp(m_WriteBuf, m_WriteBuf + m_BufSize);
+
+    m_ReadBuf = bp.get() + m_BufSize;
+    setg(0, 0, 0); // we wish to have underflow() called at the first read
+    m_Buf = bp.release();
+}
+
+
+CPipeStreambuf::~CPipeStreambuf(void)
+{
+    sync();
+    delete[] m_Buf;
+}
+
+
+void CPipeStreambuf::SetReadHandle(const CPipe::EChildIOHandle handle) const
+{
+    m_ReadHandle = handle;
+}
+
+
+CT_INT_TYPE CPipeStreambuf::overflow(CT_INT_TYPE c)
+{
+    size_t n_written;
+
+    if ( pbase() ) {
+        // Send buffer
+        size_t n_write = pptr() - pbase();
+        if ( !n_write ) {
+            return CT_NOT_EOF(CT_EOF);
+        }
+        n_written = m_Pipe->Write(m_WriteBuf, n_write * sizeof(CT_CHAR_TYPE));
+        _ASSERT(n_written);
+
+        // Update buffer content (get rid of the sent data)
+        if ((n_written /= sizeof(CT_CHAR_TYPE)) != n_write) {
+            memmove(m_WriteBuf, pbase() + n_written,
+                    (n_write - n_written)*sizeof(CT_CHAR_TYPE));
+        }
+        setp(m_WriteBuf + n_write - n_written, m_WriteBuf + m_BufSize);
+
+        // Store char
+        return CT_EQ_INT_TYPE(c, CT_EOF)
+            ? CT_NOT_EOF(CT_EOF) : sputc(CT_TO_CHAR_TYPE(c));
+    }
+
+    if ( !CT_EQ_INT_TYPE(c, CT_EOF) ) {
+        CT_CHAR_TYPE b = CT_TO_CHAR_TYPE(c);
+        // Send char
+        n_written = m_Pipe->Write(&b, sizeof(b));
+        _ASSERT(n_written == sizeof(b));
+        return c;
+    }
+    return CT_NOT_EOF(CT_EOF);
+}
+
+
+CT_INT_TYPE CPipeStreambuf::underflow(void)
+{
+    _ASSERT(!gptr() || gptr() >= egptr());
+    // Read from the pipe
+    size_t n_read = m_Pipe->Read(m_ReadBuf, m_BufSize * sizeof(CT_CHAR_TYPE), m_ReadHandle);
+    if (n_read == 0) {
+        return CT_EOF;
+    }
+    // Update input buffer with the data we just read
+    setg(m_ReadBuf, m_ReadBuf, m_ReadBuf + n_read/sizeof(CT_CHAR_TYPE));
+
+    return CT_TO_INT_TYPE(*m_ReadBuf);
+}
+
+
+int CPipeStreambuf::sync(void)
+{
+    do {
+        if (CT_EQ_INT_TYPE(overflow(CT_EOF), CT_EOF))
+            return -1;
+    } while (pbase()  &&  pptr() > pbase());
+    return 0;
+}
+
+
+
+//////////////////////////////////////////////////////////////////////////////
+//
+// CPipeIOStream
+//
+
+
+CPipeIOStream::CPipeIOStream(const CPipe& pipe, streamsize buf_size)
+    : iostream(0), m_StreamBuf(0)
+{
+    auto_ptr<CPipeStreambuf> sb(new CPipeStreambuf(pipe, buf_size));
+    init(sb.get());
+    m_StreamBuf = sb.release();
+}
+
+
+CPipeIOStream::~CPipeIOStream(void)
+{
+    streambuf* sb = rdbuf();
+    delete sb;
+    if (sb != m_StreamBuf) {
+        delete m_StreamBuf;
+    }
+}
+
+void CPipeIOStream::SetReadHandle(const CPipe::EChildIOHandle handle) const
+{
+    if (m_StreamBuf)
+        m_StreamBuf->SetReadHandle(handle);
 }
 
 
@@ -264,6 +414,9 @@ END_NCBI_SCOPE
 /*
  * ===========================================================================
  * $Log$
+ * Revision 1.3  2002/06/11 19:25:35  ivanov
+ * Added class CPipeIOStream, CPipeStreambuf
+ *
  * Revision 1.2  2002/06/10 18:35:27  ivanov
  * Changed argument's type of a running child program from char*[]
  * to vector<string>
