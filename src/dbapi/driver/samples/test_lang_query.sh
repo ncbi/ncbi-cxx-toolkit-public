f5d869a7a66f29c307fee3b44b35d6295e1a6f15
Denis Vakatov <vakatov@ncbi.nlm.nih.gov>
2002-06-14 23:07:12 +0000

    Total rewrite...
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@5465 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/dbapi/driver/samples/test_lang_query.sh
+++ src/dbapi/driver/samples/test_lang_query.sh
@@ -1,47 +1,83 @@
 #! /bin/sh
+# $Id$
 
 driver_list="ctlib dblib ftds"
-test_app="lang_query"
+server_list="MSSQL3 STRAUSS MOZART"
+
+res_file="/tmp/$0.$$"
+trap 'rm -f $res_file' 1 2 15
+
+n_ok=0
+n_err=0
+sum_list=""
 
 
 # Run one test (RunTest sql_command reg_expression) 
-RunTest() {
-  sql=$1
-  reg_exp=$2
-  tmp="/tmp/$0.$$"
-  $cmd "$sql" > $tmp 2>&1
-  grep "Can not load a driver" $tmp  &&  return
-  egrep "$reg_exp" $tmp
-  result=$?
-  rm $tmp
-  test $result -eq 0  ||  exit 1
+RunTest()
+{
+  sql="$1"
+  reg_exp="$2"
+
+  echo
+  $cmd "$sql" > $res_file 2>&1
+
+  if test $? -eq 0 ; then
+    if grep "$reg_exp" $res_file > /dev/null 2>&1 ; then
+      echo "OK:"
+      grep "$reg_exp" $res_file | tail -1
+      n_ok=`expr $n_ok + 1`
+      sum_list="$sum_list XXX_SEPARATOR +  $cmd '$sql'"
+      return
+    fi
+  fi
+
+  # error occured
+  n_err=`expr $n_err + 1`
+  sum_list="$sum_list XXX_SEPARATOR -  $cmd '$sql'"
+
+  cat $res_file
 }
 
 
+# Loop through all combinations of {driver, server, test}
 for driver in $driver_list ; do
-  echo
-  echo "*************************************************************"
-  echo "                          " $driver
-  echo "*************************************************************"
-  echo 
- 
-  cmd="./lang_query -d $driver -S MSSQL3 -Q"
+  cat <<EOF
 
-  echo "**************************** 1 ******************************"
-  echo 
-  RunTest 'select qq = 57.55 + 0.0033' '^*.>57\.5533<'
 
-  echo "**************************** 2 ******************************"
-  echo 
-  RunTest 'select qq = 57 + 33' '^*.>90<'
 
-  echo "**************************** 3 ******************************"
-  echo 
-  RunTest 'select qq = GETDATE()' '<ROW><qq>../../.... ..:..:..<'
+******************* DRIVER:  $driver  ************************
 
-  echo "**************************** 4 ******************************"
-  echo 
-  RunTest 'select name, type from sysobjects' '<ROW><name>*.'
+EOF
+  for server in $server_list ; do
+    cat <<EOF
+
+
+~~~~~~ SERVER:  $server  ~~~~~~~~~~~~~~~~~~~~~~~~
+EOF
+
+    cmd="./lang_query -d $driver -S $server -Q"
+    RunTest 'select qq = 57.55 + 0.0033' '<ROW><qq>57\.5533<'
+    RunTest 'select qq = 57 + 33' '<ROW><qq>90<'
+    RunTest 'select qq = GETDATE()' '<ROW><qq>../../.... ..:..:..<'
+    RunTest 'select name, type from sysobjects' '<ROW><name>'
+  done
 done
 
-exit 0
+rm -f $res_file
+
+
+# Print summary
+cat <<EOF
+
+
+*******************************************************
+
+SUCCEEDED:  $n_ok
+FAILED:     $n_err
+EOF
+echo "$sum_list" | sed 's/XXX_SEPARATOR/\
+/g'
+
+
+# Exit
+exit $n_err
