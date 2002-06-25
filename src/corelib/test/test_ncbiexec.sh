73a6e73fc0c5cd045d901849c8857d98924edee0
Vladimir Ivanov <ivanov@ncbi.nlm.nih.gov>
2002-06-25 20:18:24 +0000

    Workaround for CYGWIN added
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@5542 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/corelib/test/test_ncbiexec.sh
+++ src/corelib/test/test_ncbiexec.sh
@@ -1,7 +1,14 @@
 #! /bin/sh
 # $Id$
 
-test_ncbiexec
-test $? -eq 99  ||  exit 1
+os=`uname -s`
+
+test_ncbiexec;  exit_code=$?;  echo "exit code = $exit_code"
+
+if test -n "`echo $os | grep CYGWIN`" ; then
+   test $exit_code -eq 0  -o  $exit_code -eq 99  ||  exit 1
+else
+   test $exit_code -eq 99  ||  exit 1
+fi
 
 exit 0
