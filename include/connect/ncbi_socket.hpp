854928b42bada6102717ff234f435e10b7bbbc8d
Anton Lavrentiev <lavr@ncbi.nlm.nih.gov>
2002-08-13 19:28:29 +0000

    Move most methods out-of-line; fix inline methods to have "inline"
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@5867 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/connect/ncbi_socket.hpp
+++ include/connect/ncbi_socket.hpp
@@ -234,7 +234,6 @@ public:
     static string gethostname  (void);               // empty string on error
     static string ntoa         (unsigned int host);
     static string gethostbyaddr(unsigned int host);  // empty string on error
-
     static unsigned int gethostbyname(const string& hostname);  // 0 on error
 
     static unsigned int htonl(unsigned int   value);  // host-to-net, long
@@ -256,179 +255,60 @@ public:
 //  CSocket::
 //
 
-CSocket::CSocket(void) :
-    m_Socket(0), m_IsOwned(eTakeOwnership)
-{
-}
-
-
-CSocket::CSocket(const string&   host,
-                 unsigned short  port,
-                 const STimeout* timeout) :
-    m_IsOwned(eTakeOwnership)
-{
-    if (SOCK_Create(host.c_str(), port, timeout, &m_Socket) != eIO_Success)
-        m_Socket = 0;
-}
-
-
-CSocket::~CSocket(void)
-{
-    if (m_Socket && m_IsOwned != eNoOwnership)
-        SOCK_Close(m_Socket);
-}
-
-
-void CSocket::Reset(SOCK sock, EOwnership if_to_own)
-{
-    if ( m_Socket )
-        Close();
-    m_Socket = sock;
-    m_IsOwned = if_to_own;
-}
-
-
-EIO_Status CSocket::GetStatus(EIO_Event direction) const
+inline EIO_Status CSocket::GetStatus(EIO_Event direction) const
 {
     return direction == eIO_Open ? (m_Socket ? eIO_Success : eIO_Closed)
         : (m_Socket ? SOCK_Status(m_Socket, direction) : eIO_Closed);
 }
 
 
-EIO_Status CSocket::Connect(const string&   host,
-                            unsigned short  port,
-                            const STimeout* timeout)
-{
-    if ( m_Socket )
-        return eIO_Unknown;
-    EIO_Status status = SOCK_Create(host.c_str(), port, timeout, &m_Socket);
-    if (status != eIO_Success)
-        m_Socket = 0;
-    return status;
-}
-
-
-EIO_Status CSocket::Reconnect(const STimeout* timeout)
-{
-    return m_Socket ? SOCK_Reconnect(m_Socket, 0, 0, timeout) : eIO_Closed;
-}
-
-
-EIO_Status CSocket::Shutdown(EIO_Event how)
-{
-    return m_Socket ? SOCK_Shutdown(m_Socket, how) : eIO_Closed;
-}
-
-
-EIO_Status CSocket::Close(void)
-{
-    if ( !m_Socket )
-        return eIO_Closed;
-    EIO_Status status = m_IsOwned != eNoOwnership
-        ? SOCK_Close(m_Socket) : eIO_Success;
-    m_Socket = 0;
-    return status;
-}
-
-
-EIO_Status CSocket::Wait(EIO_Event event, const STimeout* timeout)
-{
-    return m_Socket ? SOCK_Wait(m_Socket, event, timeout) : eIO_Closed; 
-}
-
-
-EIO_Status CSocket::SetTimeout(EIO_Event event, const STimeout* timeout)
+inline EIO_Status CSocket::SetTimeout(EIO_Event event, const STimeout* timeout)
 {
     return m_Socket ? SOCK_SetTimeout(m_Socket, event, timeout) : eIO_Closed;
 }
 
 
-const STimeout* CSocket::GetTimeout(EIO_Event event) const
+inline const STimeout* CSocket::GetTimeout(EIO_Event event) const
 {
     return m_Socket ? SOCK_GetTimeout(m_Socket, event) : 0;
 }
 
 
-EIO_Status CSocket::Read(void*          buf,
-                         size_t         size,
-                         size_t*        n_read,
-                         EIO_ReadMethod how)
-{
-    if ( m_Socket )
-        return SOCK_Read(m_Socket, buf, size, n_read, how);
-    if ( n_read )
-        *n_read = 0;
-    return eIO_Closed;
-}
-
-
-EIO_Status CSocket::PushBack(const void* buf, size_t size)
-{
-    return m_Socket ? SOCK_PushBack(m_Socket, buf, size) : eIO_Closed;
-}
-
-
-EIO_Status CSocket::Write(const void*     buf,
-                          size_t          size,
-                          size_t*         n_written,
-                          EIO_WriteMethod how)
-{
-    if ( m_Socket )
-        return SOCK_Write(m_Socket, buf, size, n_written, how);
-    if ( n_written )
-        *n_written = 0;
-    return eIO_Closed;
-}
-
-
-void CSocket::GetPeerAddress(unsigned int* host, unsigned short* port,
-                             ENH_ByteOrder byte_order) const
-{
-    if ( !m_Socket ) {
-        if ( host )
-            *host = 0;
-        if ( port )
-            *port = 0;
-    } else
-        SOCK_GetPeerAddress(m_Socket, host, port, byte_order);
-}
-
-
-SOCK CSocket::GetSOCK(void) const
+inline SOCK CSocket::GetSOCK(void) const
 {
     return m_Socket;
 }
 
- 
+
 EIO_Status CSocket::GetOSHandle(void* handle_buf, size_t handle_size) const
 {
     return m_Socket
         ? SOCK_GetOSHandle(m_Socket, handle_buf, handle_size) : eIO_Closed;
 }
 
-
-void CSocket::SetReadOnWrite(ESwitch read_on_write)
+ 
+inline void CSocket::SetReadOnWrite(ESwitch read_on_write)
 {
     if ( m_Socket )
         SOCK_SetReadOnWrite(m_Socket, read_on_write);
 }
 
 
-void CSocket::SetDataLogging(ESwitch log_data)
+inline void CSocket::SetDataLogging(ESwitch log_data)
 {
     if ( m_Socket )
         SOCK_SetDataLogging(m_Socket, log_data);
 }
 
 
-void CSocket::SetInterruptOnSignal(ESwitch interrupt)
+inline void CSocket::SetInterruptOnSignal(ESwitch interrupt)
 {
     if ( m_Socket )
         SOCK_SetInterruptOnSignal(m_Socket, interrupt);
 }
 
 
-bool CSocket::IsServerSide(void) const
+inline bool CSocket::IsServerSide(void) const
 {
     return m_Socket && SOCK_IsServerSide(m_Socket) ? true : false;
 }
@@ -439,86 +319,13 @@ bool CSocket::IsServerSide(void) const
 //  CListeningSocket::
 //
 
-CListeningSocket::CListeningSocket(void) :
-    m_Socket(0)
-{
-}
-
-
-CListeningSocket::CListeningSocket(unsigned short port, unsigned short backlog)
-{
-    if (LSOCK_Create(port, backlog, &m_Socket) != eIO_Success)
-        m_Socket = 0;
-}
-
-
-CListeningSocket::~CListeningSocket(void)
-{
-    if ( m_Socket )
-        LSOCK_Close(m_Socket);
-}
-
-
-EIO_Status CListeningSocket::GetStatus(void) const
+inline EIO_Status CListeningSocket::GetStatus(void) const
 {
     return m_Socket ? eIO_Success : eIO_Closed;
 }
 
 
-EIO_Status CListeningSocket::Listen(unsigned short port,
-                                    unsigned short backlog)
-{
-    if ( m_Socket )
-        return eIO_Unknown;
-    EIO_Status status = LSOCK_Create(port, backlog, &m_Socket);
-    if (status != eIO_Success)
-        m_Socket = 0;
-    return status;
-}
-
-
-EIO_Status CListeningSocket::Accept(CSocket*&       sock,
-                                    const STimeout* timeout) const
-{
-    if ( !m_Socket )
-        return eIO_Unknown;
-    SOCK x_sock;
-    EIO_Status status = LSOCK_Accept(m_Socket, timeout, &x_sock);
-    if (status != eIO_Success)
-        sock = 0;
-    else if (!(sock = new CSocket)) {
-        SOCK_Close(x_sock);
-        status = eIO_Unknown;
-    } else
-        sock->Reset(x_sock, eTakeOwnership);
-    return status;
-}
-
-
-EIO_Status CListeningSocket::Accept(CSocket&        sock,
-                                    const STimeout* timeout) const
-{
-    if ( !m_Socket )
-        return eIO_Unknown;
-    SOCK x_sock;
-    EIO_Status status = LSOCK_Accept(m_Socket, timeout, &x_sock);
-    if (status == eIO_Success)
-        sock.Reset(x_sock, eTakeOwnership);
-    return status;
-}
-
-
-EIO_Status CListeningSocket::Close(void)
-{
-    if ( !m_Socket )
-        return eIO_Closed;
-    EIO_Status status = LSOCK_Close(m_Socket);
-    m_Socket = 0;
-    return status;
-}
-
-
-LSOCK CListeningSocket::GetLSOCK(void) const
+inline LSOCK CListeningSocket::GetLSOCK(void) const
 {
     return m_Socket;
 }
@@ -537,100 +344,67 @@ EIO_Status CListeningSocket::GetOSHandle(void*  handle_buf,
 //  CSocketAPI::
 //
 
-void CSocketAPI::AllowSigPipe(void)
+inline void CSocketAPI::AllowSigPipe(void)
 {
     SOCK_AllowSigPipeAPI();
 }
 
 
-EIO_Status CSocketAPI::Initialize(void)
+inline EIO_Status CSocketAPI::Initialize(void)
 {
     return SOCK_InitializeAPI();
 }
 
 
-EIO_Status CSocketAPI::Shutdown(void)
+inline EIO_Status CSocketAPI::Shutdown(void)
 {
     return SOCK_ShutdownAPI();
 }
 
 
-void CSocketAPI::SetReadOnWrite(ESwitch read_on_write)
+inline void CSocketAPI::SetReadOnWrite(ESwitch read_on_write)
 {
     SOCK_SetReadOnWriteAPI(read_on_write);
 }
 
 
-void CSocketAPI::SetDataLogging(ESwitch log_data)
+inline void CSocketAPI::SetDataLogging(ESwitch log_data)
 {
     SOCK_SetDataLoggingAPI(log_data);
 }
 
 
-void CSocketAPI::SetInterruptOnSignal(ESwitch interrupt)
+inline void CSocketAPI::SetInterruptOnSignal(ESwitch interrupt)
 {
     SOCK_SetInterruptOnSignalAPI(interrupt);
 }
 
 
-string CSocketAPI::gethostname(void)
-{
-    char hostname[128];
-    if (SOCK_gethostname(hostname, sizeof(hostname)) == 0)
-        return string(hostname);
-    return kEmptyStr;
-}
-
-
-string CSocketAPI::ntoa(unsigned int host)
-{
-    char ipaddr[64];
-    if (SOCK_ntoa(host, ipaddr, sizeof(ipaddr)) == 0)
-        return string(ipaddr);
-    return kEmptyStr;
-}
-
-
-string CSocketAPI::gethostbyaddr(unsigned int host)
-{
-    char hostname[128];
-
-    if (SOCK_gethostbyaddr(host, hostname, sizeof(hostname)) != 0)
-        return string(hostname);
-    return kEmptyStr;
-}    
-
-
-unsigned int CSocketAPI::gethostbyname(const string& hostname)
-{
-    const char* host = hostname.c_str();
-    return SOCK_gethostbyname(*host ? host : 0);
-}
-
-
-unsigned int CSocketAPI::htonl(unsigned int value)
+inline unsigned int CSocketAPI::htonl(unsigned int value)
 {
     return SOCK_htonl(value);
 }
 
 
-unsigned int CSocketAPI::ntohl(unsigned int value)
+inline unsigned int CSocketAPI::ntohl(unsigned int value)
 {
     return SOCK_ntohl(value);
 }
 
 
-unsigned int CSocketAPI::htons(unsigned short value)
+inline unsigned int CSocketAPI::htons(unsigned short value)
 {
     return SOCK_htons(value);
 }
 
 
-unsigned int CSocketAPI::ntohs(unsigned short value)
+inline unsigned int CSocketAPI::ntohs(unsigned short value)
 {
     return SOCK_ntohs(value);
 }
 
+
+/////////////////////////////////////////////////////////////////////////////
 
 
 END_NCBI_SCOPE
@@ -639,6 +413,9 @@ END_NCBI_SCOPE
 /*
  * ---------------------------------------------------------------------------
  * $Log$
+ * Revision 6.4  2002/08/13 19:28:29  lavr
+ * Move most methods out-of-line; fix inline methods to have "inline"
+ *
  * Revision 6.3  2002/08/12 20:58:09  lavr
  * More comments on parameters usage of certain methods
  *
