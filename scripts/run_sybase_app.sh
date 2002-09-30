fda5c6e1cee3632b7f73895eb2e3ef994d073da3
Vladimir Ivanov <ivanov@ncbi.nlm.nih.gov>
2002-09-30 16:16:04 +0000

    Initial revision -- empty script
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@6278 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ scripts/run_sybase_app.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+# $Id$
+# Author:  Vladimir Ivanov, NCBI 
+#
+###########################################################################
+# 
+#  Run SYBASE application under MS Windows.
+#  To run it under UNIX use configurable script "run_sybase_app.sh"
+#  in build dir.
+#
+###########################################################################
+
+
+#@script_shell@
+
+#SYBASE="@SYBASE_PATH@"
+#export SYBASE
+#exec "$@"
