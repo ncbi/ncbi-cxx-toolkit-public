da97914a9166c42864c521179dd2b98d7b4b1801
Andrei Gourianov <gouriano@ncbi.nlm.nih.gov>
2002-11-14 21:06:08 +0000

    auxiliary classes to use by DTD parser
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@6712 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ include/serial/datatool/dtdaux.hpp
@@ -0,0 +1,232 @@
+#ifndef DTDAUX_HPP
+#define DTDAUX_HPP
+
+/*  $Id$
+* ===========================================================================
+*
+*                            PUBLIC DOMAIN NOTICE
+*               National Center for Biotechnology Information
+*
+*  This software/database is a "United States Government Work" under the
+*  terms of the United States Copyright Act.  It was written as part of
+*  the author's official duties as a United States Government employee and
+*  thus cannot be copyrighted.  This software/database is freely available
+*  to the public for use. The National Library of Medicine and the U.S.
+*  Government have not placed any restriction on its use or reproduction.
+*
+*  Although all reasonable efforts have been taken to ensure the accuracy
+*  and reliability of the software and data, the NLM and the U.S.
+*  Government do not and cannot warrant the performance or results that
+*  may be obtained by using this software or data. The NLM and the U.S.
+*  Government disclaim all warranties, express or implied, including
+*  warranties of performance, merchantability or fitness for any particular
+*  purpose.
+*
+*  Please cite the author in any work or product based on this material.
+*
+* ===========================================================================
+*
+* Author: Andrei Gourianov
+*
+* File Description:
+*   DTD parser's auxiliary stuff:
+*       DTDEntity
+*       DTDAttribute
+*       DTDElement
+*       DTDEntityLexer
+*
+* ===========================================================================
+*/
+
+#include <corelib/ncbistd.hpp>
+#include <corelib/ncbistre.hpp>
+#include <serial/datatool/dtdlexer.hpp>
+#include <list>
+#include <map>
+
+BEGIN_NCBI_SCOPE
+
+class CFileModules;
+class CDataModule;
+class CDataType;
+class CDataMemberContainerType;
+class CDataValue;
+class CDataMember;
+class CEnumDataType;
+class CEnumDataTypeValue;
+
+
+/////////////////////////////////////////////////////////////////////////////
+// DTDEntity
+
+class DTDEntity
+{
+public:
+    DTDEntity(void);
+    DTDEntity(const DTDEntity& other);
+    virtual ~DTDEntity(void);
+
+    void SetName(const string& name);
+    const string& GetName(void) const;
+
+    void SetData(const string& data);
+    const string& GetData(void) const;
+
+    void SetExternal(void);
+    bool IsExternal(void) const;
+
+private:
+    string m_Name;
+    string m_Data;
+    bool   m_External;
+};
+
+/////////////////////////////////////////////////////////////////////////////
+// DTDAttribute
+
+class DTDAttribute
+{
+public:
+    DTDAttribute(void);
+    DTDAttribute(const DTDAttribute& other);
+    virtual ~DTDAttribute(void);
+
+    enum EType {
+        eUnknown,
+        eString,
+        eEnum,
+        eId,
+        eIdRef,
+        eIdRefs,
+        eNmtoken,
+        eNmtokens,
+        eEntity,
+        eEntities,
+        eNotation
+    };
+    enum EValueType {
+        eDefault,
+        eRequired,
+        eImplied,
+        eFixed
+    };
+
+    void SetName(const string& name);
+    const string& GetName(void) const;
+
+    void SetType(EType type);
+    EType GetType(void) const;
+
+    void SetValueType(EValueType valueType);
+    EValueType GetValueType(void) const;
+
+    void SetValue(const string& value);
+    const string& GetValue(void) const;
+
+    void AddEnumValue(const string& value);
+    const list<string>& GetEnumValues(void) const;
+
+private:
+    string m_Name;
+    EType m_Type;
+    EValueType m_ValueType;
+    string m_Value;
+    list<string> m_ListEnum;
+};
+
+/////////////////////////////////////////////////////////////////////////////
+// DTDElement
+
+class DTDElement
+{
+public:
+    DTDElement(void);
+    DTDElement(const DTDElement& other);
+    virtual ~DTDElement(void);
+
+    enum EType {
+        eUnknown,
+        eString,   // #PCDATA
+        eAny,      // ANY
+        eEmpty,    // EMPTY
+        eSequence, // (a,b,c)
+        eChoice    // (a|b|c)
+                   // mixed content is not implemented
+    };
+    enum EOccurrence {
+        eOne,
+        eOneOrMore,
+        eZeroOrMore,
+        eZeroOrOne
+    };
+
+    void SetName(const string& name);
+    const string& GetName(void) const;
+
+    void SetType( EType type);
+    void SetTypeIfUnknown( EType type);
+    EType GetType(void) const;
+
+    void SetOccurrence( const string& ref_name, EOccurrence occ);
+    EOccurrence GetOccurrence(const string& ref_name) const;
+
+    void SetOccurrence( EOccurrence occ);
+    EOccurrence GetOccurrence(void) const;
+
+    // i.e. element contains other elements
+    void AddContent( const string& ref_name);
+    const list<string>& GetContent(void) const;
+
+    // element is contained somewhere
+    void SetReferenced(void);
+    bool IsReferenced(void) const;
+
+    // element does not have any specific name
+    void SetEmbedded(void);
+    bool IsEmbedded(void) const;
+    string CreateEmbeddedName(int depth) const;
+
+    void AddAttribute(DTDAttribute& attrib);
+    bool HasAttributes(void) const;
+    const list<DTDAttribute>& GetAttributes(void) const;
+
+private:
+    string m_Name;
+    EType m_Type;
+    EOccurrence m_Occ;
+    list<string> m_Refs;
+    list<DTDAttribute> m_Attrib;
+    map<string,EOccurrence> m_RefOcc;
+    bool m_Refd;
+    bool m_Embd;
+};
+
+/////////////////////////////////////////////////////////////////////////////
+// DTDEntityLexer
+
+class DTDEntityLexer : public DTDLexer
+{
+public:
+    DTDEntityLexer(CNcbiIstream& in, bool autoDelete=true);
+    virtual ~DTDEntityLexer(void);
+protected:
+    bool m_AutoDelete;
+    CNcbiIstream* m_Str;
+};
+
+
+END_NCBI_SCOPE
+
+#endif // DTDAUX_HPP
+
+
+/*
+ * ==========================================================================
+ * $Log$
+ * Revision 1.1  2002/11/14 21:06:08  gouriano
+ * auxiliary classes to use by DTD parser
+ *
+ *
+ *
+ * ==========================================================================
+ */
---
+++ src/serial/datatool/dtdaux.cpp
@@ -0,0 +1,328 @@
+
+/*  $Id$
+* ===========================================================================
+*
+*                            PUBLIC DOMAIN NOTICE
+*               National Center for Biotechnology Information
+*
+*  This software/database is a "United States Government Work" under the
+*  terms of the United States Copyright Act.  It was written as part of
+*  the author's official duties as a United States Government employee and
+*  thus cannot be copyrighted.  This software/database is freely available
+*  to the public for use. The National Library of Medicine and the U.S.
+*  Government have not placed any restriction on its use or reproduction.
+*
+*  Although all reasonable efforts have been taken to ensure the accuracy
+*  and reliability of the software and data, the NLM and the U.S.
+*  Government do not and cannot warrant the performance or results that
+*  may be obtained by using this software or data. The NLM and the U.S.
+*  Government disclaim all warranties, express or implied, including
+*  warranties of performance, merchantability or fitness for any particular
+*  purpose.
+*
+*  Please cite the author in any work or product based on this material.
+*
+* ===========================================================================
+*
+* Author: Andrei Gourianov
+*
+* File Description:
+*   DTD parser's auxiliary stuff:
+*       DTDEntity
+*       DTDAttribute
+*       DTDElement
+*       DTDEntityLexer
+*
+* ===========================================================================
+*/
+
+#include <serial/datatool/dtdaux.hpp>
+
+BEGIN_NCBI_SCOPE
+
+/////////////////////////////////////////////////////////////////////////////
+// DTDEntity
+
+DTDEntity::DTDEntity(void)
+{
+    m_External = false;
+}
+DTDEntity::DTDEntity(const DTDEntity& other)
+{
+    m_Name = other.m_Name;
+    m_Data = other.m_Data;
+    m_External = other.m_External;
+}
+DTDEntity::~DTDEntity(void)
+{
+}
+
+void DTDEntity::SetName(const string& name)
+{
+    m_Name = name;
+}
+const string& DTDEntity::GetName(void) const
+{
+    return m_Name;
+}
+
+void DTDEntity::SetData(const string& data)
+{
+    m_Data = data;
+}
+const string& DTDEntity::GetData(void) const
+{
+    return m_Data;
+}
+
+void DTDEntity::SetExternal(void)
+{
+    m_External = true;
+}
+bool DTDEntity::IsExternal(void) const
+{
+    return m_External;
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// DTDAttribute
+
+DTDAttribute::DTDAttribute(void)
+{
+    m_Type = eUnknown;
+    m_ValueType = eDefault;
+}
+DTDAttribute::DTDAttribute(const DTDAttribute& other)
+{
+    m_Name = other.m_Name;
+    m_Type = other.m_Type;
+    m_ValueType = other.m_ValueType;
+    m_Value = other.m_Value;
+    for (list<string>::const_iterator i = other.m_ListEnum.begin();
+        i != other.m_ListEnum.end(); ++i) {
+        m_ListEnum.push_back( *i);
+    }
+}
+DTDAttribute::~DTDAttribute(void)
+{
+}
+
+void DTDAttribute::SetName(const string& name)
+{
+    m_Name = name;
+}
+const string& DTDAttribute::GetName(void) const
+{
+    return m_Name;
+}
+
+void DTDAttribute::SetType(EType type)
+{
+    m_Type = type;
+}
+DTDAttribute::EType DTDAttribute::GetType(void) const
+{
+    return m_Type;
+}
+
+void DTDAttribute::SetValueType(EValueType valueType)
+{
+    m_ValueType = valueType;
+}
+DTDAttribute::EValueType DTDAttribute::GetValueType(void) const
+{
+    return m_ValueType;
+}
+
+void DTDAttribute::SetValue(const string& value)
+{
+    m_Value = value;
+}
+const string& DTDAttribute::GetValue(void) const
+{
+    return m_Value;
+}
+
+void DTDAttribute::AddEnumValue(const string& value)
+{
+    m_ListEnum.push_back(value);
+}
+const list<string>& DTDAttribute::GetEnumValues(void) const
+{
+    return m_ListEnum;
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// DTDElement
+
+DTDElement::DTDElement(void)
+{
+    m_Type = eUnknown;
+    m_Occ  = eOne;
+    m_Refd = false;
+    m_Embd = false;
+}
+
+DTDElement::DTDElement(const DTDElement& other)
+{
+    m_Name = other.m_Name;
+    m_Type = other.eUnknown;
+    m_Occ  = other.m_Occ;
+    m_Refd = other.m_Refd;
+    m_Embd = other.m_Embd;
+    for (list<string>::const_iterator i = other.m_Refs.begin();
+        i != other.m_Refs.end(); ++i) {
+        m_Refs.push_back( *i);
+    }
+    for (map<string,EOccurrence>::const_iterator i = other.m_RefOcc.begin();
+        i != other.m_RefOcc.end(); ++i) {
+        m_RefOcc[i->first] = i->second;
+    }
+     ;
+    for (list<DTDAttribute>::const_iterator i = other.m_Attrib.begin();
+        i != other.m_Attrib.end(); ++i) {
+        m_Attrib.push_back(*i);
+    }
+}
+
+DTDElement::~DTDElement(void)
+{
+}
+
+
+void DTDElement::SetName(const string& name)
+{
+    m_Name = name;
+}
+const string& DTDElement::GetName(void) const
+{
+    return m_Name;
+}
+
+
+void DTDElement::SetType( EType type)
+{
+    _ASSERT(m_Type == eUnknown || m_Type == type);
+    m_Type = type;
+}
+
+void DTDElement::SetTypeIfUnknown( EType type)
+{
+    if (m_Type == eUnknown) {
+        m_Type = type;
+    }
+}
+
+DTDElement::EType DTDElement::GetType(void) const
+{
+    return (EType)m_Type;
+}
+
+
+void DTDElement::SetOccurrence( const string& ref_name, EOccurrence occ)
+{
+    m_RefOcc[ref_name] = occ;
+}
+DTDElement::EOccurrence DTDElement::GetOccurrence(
+    const string& ref_name) const
+{
+    map<string,EOccurrence>::const_iterator i = m_RefOcc.find(ref_name);
+    return (i != m_RefOcc.end()) ? i->second : eOne;
+}
+
+
+void DTDElement::SetOccurrence( EOccurrence occ)
+{
+    m_Occ = occ;
+}
+DTDElement::EOccurrence DTDElement::GetOccurrence(void) const
+{
+    return m_Occ;
+}
+
+
+void DTDElement::AddContent( const string& ref_name)
+{
+    m_Refs.push_back( ref_name);
+}
+
+const list<string>& DTDElement::GetContent(void) const
+{
+    return m_Refs;
+}
+
+
+void DTDElement::SetReferenced(void)
+{
+    m_Refd = true;
+}
+bool DTDElement::IsReferenced(void) const
+{
+    return m_Refd;
+}
+
+
+void DTDElement::SetEmbedded(void)
+{
+    m_Embd = true;
+}
+bool DTDElement::IsEmbedded(void) const
+{
+    return m_Embd;
+}
+string DTDElement::CreateEmbeddedName(int depth) const
+{
+    string name, tmp;
+    list<string>::const_iterator i;
+    for ( i = m_Refs.begin(); i != m_Refs.end(); ++i) {
+        tmp = i->substr(0,depth);
+        tmp[0] = toupper(tmp[0]);
+        name += tmp;
+    }
+    return name;
+}
+
+void DTDElement::AddAttribute(DTDAttribute& attrib)
+{
+    m_Attrib.push_back(attrib);
+}
+bool DTDElement::HasAttributes(void) const
+{
+    return !m_Attrib.empty();
+}
+const list<DTDAttribute>& DTDElement::GetAttributes(void) const
+{
+    return m_Attrib;
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+// DTDEntityLexer
+
+DTDEntityLexer::DTDEntityLexer(CNcbiIstream& in, bool autoDelete)
+    : DTDLexer(in)
+{
+    m_Str = &in;
+    m_AutoDelete = autoDelete;
+}
+DTDEntityLexer::~DTDEntityLexer(void)
+{
+    if (m_AutoDelete) {
+        delete m_Str;
+    }
+}
+
+
+END_NCBI_SCOPE
+
+
+/*
+ * ==========================================================================
+ * $Log$
+ * Revision 1.1  2002/11/14 21:02:15  gouriano
+ * auxiliary classes to use by DTD parser
+ *
+ *
+ *
+ * ==========================================================================
+ */
