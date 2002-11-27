f282846c95d3728e029ef270a50a7450c36f5f31
Anton Lavrentiev <lavr@ncbi.nlm.nih.gov>
2002-11-27 21:09:43 +0000

    Take advantage of CStreamUtils::Readsome() in CIStream::Read() CIStream::Eof() modified to use get() instead of operator>>()
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@6806 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/data_loaders/genbank/reader.cpp
+++ src/objtools/data_loaders/genbank/reader.cpp
@@ -1,28 +1,29 @@
 /*  $Id$
-* ===========================================================================
-*                            PUBLIC DOMAIN NOTICE
-*               National Center for Biotechnology Information
-*
-*  This software/database is a "United States Government Work" under the
-*  terms of the United States Copyright Act.  It was written as part of
-*  the author's official duties as a United States Government employee and
-*  thus cannot be copyrighted.  This software/database is freely available
-*  to the public for use. The National Library of Medicine and the U.S.
-*  Government have not placed any restriction on its use or reproduction.
-*
-*  Although all reasonable efforts have been taken to ensure the accuracy
-*  and reliability of the software and data, the NLM and the U.S.
-*  Government do not and cannot warrant the performance or results that
-*  may be obtained by using this software or data. The NLM and the U.S.
-*  Government disclaim all warranties, express or implied, including
-*  warranties of performance, merchantability or fitness for any particular
-*  purpose.
-*
-*  Please cite the author in any work or product based on this material.
-* ===========================================================================
-*/
+ * ===========================================================================
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ * ===========================================================================
+ */
 
 #include <objects/objmgr/reader.hpp>
+#include <util/stream_utils.hpp>
 
 BEGIN_NCBI_SCOPE
 BEGIN_SCOPE(objects)
@@ -79,47 +80,17 @@ void CBlob::Restore(istream &is)
 
 size_t CIStream::Read(istream &is, char* buffer, size_t bufferLength)
 {
-#ifdef NCBI_COMPILER_GCC
-#  if NCBI_COMPILER_VERSION < 300
-#    define NCBI_NO_READSOME
-#  endif
-#endif
-
-#ifdef NCBI_NO_READSOME
-#undef NCBI_NO_READSOME
-    // Special case: GCC had no readsome() prior to ver 3.0;
-    // read() will set "eof" flag if gcount() < bufferLength
-    is.read(buffer, bufferLength);
-    size_t count = is.gcount();
-    // Reset "eof" flag if some data have been read
-    if (count  &&  is.eof())
-        is.clear();
-    return count;
-#else
-    // Try to read data
-    size_t n = is.readsome(buffer, bufferLength);
-    if (n != 0  ||  is.eof())
-        return n; // success
-    // No data found in the buffer, try to read from the real source
-    is.read(buffer, 1);
-    if ( !is.good() )
-        return 0;
-    if (bufferLength == 1)
-        return 1; // Do not need more data
-    // Read more data (up to the bufferLength)
-    return is.readsome(buffer+1, bufferLength-1) + 1;
-#endif
+  return CStreamUtils::Readsome(is, buffer, bufferLength);
 }
 
 bool CIStream::Eof()
 {
-  char c;
-  (*this) >> c;
+  CT_INT_TYPE c = get();
 
-  if(this->eof())
+  if (eof())
     return true;
 
-  this->putback(c);
+  putback(c);
   return false;
 }
 
@@ -133,47 +104,51 @@ END_SCOPE(objects)
 END_NCBI_SCOPE
 
 /*
-* $Log$
-* Revision 1.6  2002/05/06 03:28:47  vakatov
-* OM/OM1 renaming
-*
-* Revision 1.5  2002/03/27 20:23:50  butanaev
-* Added connection pool.
-*
-* Revision 1.4  2002/03/27 18:06:08  kimelman
-* stream.read/write instead of << >>
-*
-* Revision 1.3  2002/03/21 19:14:54  kimelman
-* GB related bugfixes
-*
-* Revision 1.2  2002/03/20 04:50:13  kimelman
-* GB loader added
-*
-* Revision 1.1  2002/01/11 19:06:21  gouriano
-* restructured objmgr
-*
-* Revision 1.6  2001/12/13 00:19:25  kimelman
-* bugfixes:
-*
-* Revision 1.5  2001/12/12 21:46:40  kimelman
-* Compare interface fix
-*
-* Revision 1.4  2001/12/10 20:08:01  butanaev
-* Code cleanup.
-*
-* Revision 1.3  2001/12/07 21:24:59  butanaev
-* Interface development, code beautyfication.
-*
-* Revision 1.2  2001/12/07 16:43:58  butanaev
-* Fixed includes.
-*
-* Revision 1.1  2001/12/07 16:10:22  butanaev
-* Switching to new reader interfaces.
-*
-* Revision 1.2  2001/12/06 18:06:22  butanaev
-* Ported to linux.
-*
-* Revision 1.1  2001/12/06 14:35:22  butanaev
-* New streamable interfaces designed, ID1 reimplemented.
-*
-*/
+ * $Log$
+ * Revision 1.7  2002/11/27 21:09:43  lavr
+ * Take advantage of CStreamUtils::Readsome() in CIStream::Read()
+ * CIStream::Eof() modified to use get() instead of operator>>()
+ *
+ * Revision 1.6  2002/05/06 03:28:47  vakatov
+ * OM/OM1 renaming
+ *
+ * Revision 1.5  2002/03/27 20:23:50  butanaev
+ * Added connection pool.
+ *
+ * Revision 1.4  2002/03/27 18:06:08  kimelman
+ * stream.read/write instead of << >>
+ *
+ * Revision 1.3  2002/03/21 19:14:54  kimelman
+ * GB related bugfixes
+ *
+ * Revision 1.2  2002/03/20 04:50:13  kimelman
+ * GB loader added
+ *
+ * Revision 1.1  2002/01/11 19:06:21  gouriano
+ * restructured objmgr
+ *
+ * Revision 1.6  2001/12/13 00:19:25  kimelman
+ * bugfixes:
+ *
+ * Revision 1.5  2001/12/12 21:46:40  kimelman
+ * Compare interface fix
+ *
+ * Revision 1.4  2001/12/10 20:08:01  butanaev
+ * Code cleanup.
+ *
+ * Revision 1.3  2001/12/07 21:24:59  butanaev
+ * Interface development, code beautyfication.
+ *
+ * Revision 1.2  2001/12/07 16:43:58  butanaev
+ * Fixed includes.
+ *
+ * Revision 1.1  2001/12/07 16:10:22  butanaev
+ * Switching to new reader interfaces.
+ *
+ * Revision 1.2  2001/12/06 18:06:22  butanaev
+ * Ported to linux.
+ *
+ * Revision 1.1  2001/12/06 14:35:22  butanaev
+ * New streamable interfaces designed, ID1 reimplemented.
+ *
+ */
