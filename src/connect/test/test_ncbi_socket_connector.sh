170b5cc900282a66823f5debc344514859aa1178
Anton Lavrentiev <lavr@ncbi.nlm.nih.gov>
2002-12-04 16:56:33 +0000

    Add more logging to the test run
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@6829 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/connect/test/test_ncbi_socket_connector.sh
+++ src/connect/test/test_ncbi_socket_connector.sh
@@ -3,13 +3,27 @@
 
 exit_code=0
 port="555`expr $$ % 100`"
+rm -f socket_io_bouncer.log test_ncbi_socket_connector.log
 
-socket_io_bouncer $port &
+CONN_DEBUG_PRINTOUT=ALL; export CONN_DEBUG_PRINTOUT
+
+socket_io_bouncer $port >>socket_io_bouncer.log 2>&1 &
 server_pid=$!
 trap 'kill $server_pid' 1 2 15
 
 sleep 1
-./test_ncbi_socket_connector localhost $port  ||  exit_code=1
+./test_ncbi_socket_connector localhost $port >>test_ncbi_socket_connector.log 2>&1  ||  exit_code=1
 
 kill $server_pid  ||  exit_code=2
+if [ $exit_code != 0 ]; then
+  if [ -s socket_io_bouncer.log ]; then
+    echo '=== socket_io_bouncer.log ==='
+    cat socket_io_bouncer.log
+  fi
+  if [ -s test_ncbi_socket_connector.log ]; then
+    echo '=== test_ncbi_socket_connector.log ==='
+    cat test_ncbi_socket_connector.log
+  fi
+fi
+
 exit $exit_code
