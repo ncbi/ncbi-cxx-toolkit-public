c820942d7986f544e72cb425f91328f8bf8b3584
Anton Lavrentiev <lavr@ncbi.nlm.nih.gov>
2002-12-05 21:43:31 +0000

    Fix in assignment and compare in URL_Connect()
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@6852 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/connect/ncbi_connutil.c
+++ src/connect/ncbi_connutil.c
@@ -798,7 +798,7 @@ extern SOCK URL_Connect
     }
 
     /* connect to HTTPD */
-    if ((st= SOCK_CreateEx(host, port, c_timeout, &sock, log) != eIO_Success)){
+    if ((st= SOCK_CreateEx(host, port, c_timeout, &sock, log)) != eIO_Success){
         CORE_LOGF(eLOG_Error,
                   ("[URL_Connect]  Socket connect to %s:%hu failed: %s", host,
                    port, st==eIO_Unknown? strerror(errno) : IO_StatusStr(st)));
@@ -1458,6 +1458,9 @@ extern size_t HostPortToString(unsigned int   host,
 /*
  * --------------------------------------------------------------------------
  * $Log$
+ * Revision 6.46  2002/12/05 21:43:31  lavr
+ * Fix in assignment and compare in URL_Connect()
+ *
  * Revision 6.45  2002/12/04 16:50:47  lavr
  * Use SOCK_CreateEx() in URL_Connect()
  *
