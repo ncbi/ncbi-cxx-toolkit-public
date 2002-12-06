763e32f20d761e75c940024391b05354e47d8e4d
Aaron Ucko <ucko@ncbi.nlm.nih.gov>
2002-12-06 22:37:45 +0000

    Add WorkShop 5.4 (Forte 7) support scripts.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@6874 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ compilers/cxx_filter.WorkShop54.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+# $Id$
+# Authors:  Denis Vakatov, NCBI; Aaron Ucko, NCBI
+#
+#  Filter out redundant warnings issued by WorkShop 7 C++ 5.4 compiler.
+#  Simplify the output.
+
+sed '
+s/std::basic_string<char, std::char_traits<char>, std::allocator<char>>/std::string/g
+s/std::basic_\([a-z]*stream\)<char, std::char_traits<char>>/std::\1/g
+s/std::\([a-z_]*\)<\([^,<>]*\), std::allocator<\2>>/std::\1<\2>/g
+s/std::\([a-z_]*\)<\([^,<>]*\), std::less<\2>, std::allocator<\2>>/std::\1<\2>/g
+s/std::\([a-z_]*\)<\([^,<>]*\), \([^,<>]*\), std::less<\2>, std::allocator<std::pair<const \2, \3>>>/std::\1<\2, \3>/g
+s/std::\([a-z_]*\)<\([^,<>]*<[^<>]*>\), std::allocator<\2>>/std::\1<\2>/g
+s/std::\([a-z_]*\)<\([^,<>]*\), \([^,<>]*<[^<>]*>\), std::less<\2>, std::allocator<std::pair<const \2, \3>>>/std::\1<\2, \3>/g
+' | nawk -f `dirname $0`/cxx_filter.WorkShop54.awk
