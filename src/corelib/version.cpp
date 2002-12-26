7540db5cd77f6ac10159c1f66e2806f9009f6110
Vladimir Ivanov <ivanov@ncbi.nlm.nih.gov>
2002-12-26 17:11:10 +0000

    Initial revision
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@7069 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ src/corelib/version.cpp
@@ -0,0 +1,82 @@
+/*  $Id$
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Authors:  Denis Vakatov, Vladimir Ivanov
+ *
+ * File Description:
+ *   CVersionInfo -- a version info storage class
+ *
+ */
+
+#include <corelib/version.hpp>
+
+
+BEGIN_NCBI_SCOPE
+
+
+CVersionInfo::CVersionInfo(unsigned int ver_major, unsigned int  ver_minor,
+                           unsigned int  patch_level, const string& name) 
+    : m_Major(ver_major),
+      m_Minor(ver_minor),
+      m_PatchLevel(patch_level),
+      m_Name(name)
+
+{
+}
+
+
+CVersionInfo::CVersionInfo(const CVersionInfo& version)
+    : m_Major(version.m_Major),
+      m_Minor(version.m_Minor),
+      m_PatchLevel(version.m_PatchLevel),
+      m_Name(version.m_Name)
+{
+}
+
+
+string CVersionInfo::Print(void) const
+{
+    CNcbiOstrstream os;
+    os << m_Major << "." << m_Minor << "." << m_PatchLevel;
+    if ( !m_Name.empty() ) {
+        os << " (" << m_Name << ")";
+    }
+    os << '\0';
+    return os.str();
+}
+
+
+
+END_NCBI_SCOPE
+
+
+/*
+ * ===========================================================================
+ * $Log$
+ * Revision 1.1  2002/12/26 17:10:47  ivanov
+ * Initial revision
+ *
+ * ===========================================================================
+ */
