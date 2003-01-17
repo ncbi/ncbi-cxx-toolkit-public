1a3b8211f03fc6cc480d51908021428f3864caee
Anton Lavrentiev <lavr@ncbi.nlm.nih.gov>
2003-01-17 01:26:22 +0000

    Initial revision
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@7322 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ src/connect/test/test_ncbi_dsock.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+# $Id$
+
+exit_code=0
+port="555`expr $$ % 100`"
+
+./test_ncbi_dsock server $port >/dev/null 2>&1 &
+server_pid=$!
+trap 'kill $server_pid' 1 2 15
+
+sleep 1
+./test_ncbi_dsock client $port  ||  exit_code=1
+
+kill $server_pid  ||  exit_code=2
+exit $exit_code
