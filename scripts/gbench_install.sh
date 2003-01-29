02ccce21cfc1fd5029a838e4ad592ab109c6eb3f
kapustin <kapustin@ncbi.nlm.nih.gov>
2003-01-29 19:12:58 +0000

    *** empty log message ***
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@7475 78c7ea69-d796-4a43-9a09-de51944f1b03

--- scripts/gbench_install.sh
+++ scripts/gbench_install.sh
@@ -12,7 +12,7 @@ script_dir=`(cd "${script_dir}" ; pwd)`
 . ${script_dir}/common.sh
 
 
-PLUGINS='doc_basic doc_table algo_basic algo_stdio view_text view_graphic view_align view_sequence view_table'
+PLUGINS='doc_basic doc_table algo_basic algo_stdio algo_align view_text view_graphic view_align view_sequence view_table'
 BINS='gbench-bin gbench_plugin_scan'
 LIBS='gui_doc gui_view gui_algo'
 
