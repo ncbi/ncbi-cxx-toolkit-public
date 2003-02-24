edf4806598a2641db6082a21619a80304423ed95
Vladimir Ivanov <ivanov@ncbi.nlm.nih.gov>
2003-02-24 18:03:31 +0000

    Fixed check script build for imported projects (via import_project.sh).
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@7751 78c7ea69-d796-4a43-9a09-de51944f1b03

--- scripts/check/check_make_unix.sh
+++ scripts/check/check_make_unix.sh
@@ -9,15 +9,17 @@
 # UNIX build tree.
 #
 # Usage:
-#    check_make_unix.sh <in_test_list> <out_check_script> <build_dir>
+#    check_make_unix.sh <test_list> <build_dir> <target_dir> <check_script>
 #
-#    in_test_list     - list of tests (it build with "make check_r")
-#                       (default: "<build_dir>/check.sh.list")
-#    out_check_script - name of the output test script
-#                       (default: "<build_dir>/check.sh")
-#    build_dir        - path to UNIX build tree like".../build/..."
-#                       (default: will try determine path from current work
-#                       directory -- root of build tree ) 
+#    test_list       - a list of tests (it build with "make check_r")
+#                      (default: "<build_dir>/check.sh.list")
+#    build_dir       - path to UNIX build tree like".../build/..."
+#                      (default: will try determine path from current work
+#                      directory -- root of build tree ) 
+#    target_dir      - path where the check script and logs will be created
+#                      (default: current dir) 
+#    check_script    - name of the check script (without path).
+#                      (default: "check.sh" / "<target_dir>/check.sh")
 #
 #    If any parameter is skipped that will be used default value for it.
 #
@@ -31,45 +33,81 @@
 res_out="check.sh"
 res_list="$res_out.list"
 
-# Field delimiters in the list (this symbols used directly in the "sed" command)
+# Fields delimiters in the list (this symbols used directly in the "sed" command)
 x_delim=" ____ "
 x_delim_internal="~"
 x_tmp="/var/tmp"
 
 x_list=$1
-x_out=$2
-x_build_dir=$3
+x_build_dir=$2
+x_target_dir=$3
+x_out=$4
 
-if test ! -z "$x_build_dir"; then
-   if test ! -d "$x_build_dir"; then
+# Check for build dir
+if [ ! -z "$x_build_dir" ]; then
+   if [ ! -d "$x_build_dir" ]; then
       echo "Build directory \"$x_build_dir\" don't exist."
       exit 1 
    fi
-   # Expand path and remove trailing slash
    x_build_dir=`(cd "$x_build_dir"; pwd | sed -e 's/\/$//g')`
 else
-   # Get build dir name from current path
-   x_build_dir=`pwd | sed -e 's%/build.*$%%'`/build
+   # Get build dir name from the current path
+   x_build_dir=`pwd | sed -e 's%/build.*$%%'`
+   if [ -d "$x_build_dir/build" ]; then
+      x_build_dir="$x_build_dir/build"
+   fi
+fi
+
+# Check for target dir
+if [ ! -z "$x_target_dir" ]; then
+   if [ ! -d "$x_target_dir" ]; then
+      echo "Target directory \"$x_target_dir\" don't exist."
+      exit 1 
+   fi
+    x_target_dir=`(cd "$x_target_dir"; pwd | sed -e 's/\/$//g')`
+else
+   x_target_dir=`pwd`
 fi
 
 x_conf_dir=`dirname "$x_build_dir"`
 x_root_dir=`dirname "$x_conf_dir"`
 
-if test -z "$x_list"; then
-   x_list="$x_build_dir/$res_list"
+# Check for a imported project or intree project
+res=`pwd | grep "/internal/c++/src/"`
+if [ ! -z "$res" ] ; then
+   x_import_prj="yes"
+   x_compile_dir="`pwd | sed -e 's%/internal/c++/src.*$%%g'`/internal/c++/src"
+else
+   x_import_prj="no"
+   x_compile_dir="$x_build_dir"
 fi
 
-if test -z "$x_out"; then
-   x_out="$x_build_dir/$res_out"
+if [ -z "$x_list" ]; then
+   x_list="$x_target_dir/$res_list"
+fi
+
+if [ -z "$x_out" ]; then
+   x_out="$x_target_dir/$res_out"
 fi
 
 x_script_name=`echo "$x_out" | sed -e 's%^.*/%%'`
 
-# Check list
-if test ! -f "$x_list"; then
+# Check for a list file
+if [ ! -f "$x_list" ]; then
    echo "Check list file \"$x_list\" not found."
    exit 1 
 fi
+
+
+#echo ----------------------------------------------------------------------
+#echo "Imported project  :" $x_import_prj
+#echo "C++ root dir      :" $x_root_dir
+#echo "Configuration dir :" $x_conf_dir
+#echo "Build dir         :" $x_build_dir
+#echo "Compile dir       :" $x_compile_dir
+#echo "Target dir        :" $x_target_dir
+#echo "Check script      :" $x_out
+#echo ----------------------------------------------------------------------
 
 #//////////////////////////////////////////////////////////////////////////
 
@@ -104,8 +142,7 @@ EOF_usage
     exit 1
 }
 
-
-if test \$# -ne 1 ; then
+if [ \$# -ne 1 ]; then
    Usage "Invalid number of arguments."
 fi
 
@@ -158,7 +195,7 @@ case "\$method" in
          x_code=\`cat \$x_file | grep -c '@@@ EXIT CODE:'\`
          test \$x_code -ne 0 || continue
          x_good=\`cat \$x_file | grep -c '@@@ EXIT CODE: 0'\`
-         if test \$x_good -ne 1 ; then
+         if [ \$x_good -ne 1 ]; then
             echo 
             echo 
             cat \$x_file
@@ -173,16 +210,17 @@ case "\$method" in
       ;;
 esac
 
+
 # Add current, build and scripts directories to PATH
 PATH=".:$x_build_dir:$x_root_dir/scripts:\$PATH"
 export PATH
 
 EOF
 
-if test -n "$x_conf_dir"  -a  -d "$x_conf_dir/lib";  then
+if [ -n "$x_conf_dir"  -a  -d "$x_conf_dir/lib" ];  then
    cat >> $x_out <<EOF
 # Adjust PATH and LD_LIBRARY_PATH for running tests
-if test -n "\$LD_LIBRARY_PATH"; then
+if [ -n "\$LD_LIBRARY_PATH" ]; then
    LD_LIBRARY_PATH="$x_conf_dir/lib:\$LD_LIBRARY_PATH"
 else
    LD_LIBRARY_PATH="$x_conf_dir/lib"
@@ -207,7 +245,8 @@ rm -f "\$res_log"
 
 RunTest() {
    # Parameters
-   x_work_dir="\$1"
+   x_work_dir_tail="\$1"
+   x_work_dir="$x_compile_dir/\$x_work_dir_tail"
    x_test="\$2"
    x_app="\$3"
    x_run="\$4"
@@ -215,8 +254,7 @@ RunTest() {
    x_timeout="\$6"
 
    # Check existence of the test's application directory
-   if test -d "\$x_work_dir"; then
-
+   if [ -d "\$x_work_dir" ]; then
       # Write header to output file 
       echo "\$x_test_out" >> \$res_journal
       (
@@ -231,11 +269,10 @@ RunTest() {
 
       # Goto the test's directory 
       cd "\$x_work_dir"
-      x_work_dir_tail=\`echo "\$x_work_dir" | sed 's%^.*/build/%%'\` 
       x_cmd="[\$x_work_dir_tail] \$x_run"
 
       # And run test if it exist
-      if test -f "\$x_app"; then
+      if [ -f "\$x_app" ]; then
          # Fix empty parameters (replace "" to \"\", '' to \'\')
          x_run_fix=\`echo "\$x_run" | sed -e 's/""/\\\\\\\\\\"\\\\\\\\\\"/g' -e "s/''/\\\\\\\\\\'\\\\\\\\\\'/g"\`
          # Run check
@@ -251,13 +288,13 @@ RunTest() {
          # Write result of the test into the his output file
          echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" >> \$x_test_out
          echo "@@@ EXIT CODE: \$result" >> \$x_test_out
-         if test -f "\$x_work_dir/core"; then
+         if [ -f "\$x_work_dir/core" ]; then
             echo "@@@ CORE DUMPED" >> \$x_test_out
             rm -f "\$x_work_dir/core"
          fi
 
          # And write result also on the screen and into the log
-         if test \$result -eq 0; then
+         if [ \$result -eq 0 ]; then
             echo "OK  --  \$x_cmd"
             echo "OK  --  \$x_cmd" >> \$res_log
             count_ok=\`expr \$count_ok + 1\`
@@ -304,27 +341,30 @@ for x_row in $x_tests; do
 
    # Check application requirements
    for x_req in $x_requires; do
-     test -f "$x_conf_dir/status/$x_req.enabled"  ||  continue 2
+      test -f "$x_conf_dir/status/$x_req.enabled"  ||  continue 2
    done
 
    # Application base build directory
-   x_work_dir="$x_build_dir/`echo \"$x_row\" | sed -e 's/~.*$//'`"
+   x_work_dir_tail="`echo \"$x_row\" | sed -e 's/~.*$//'`"
+   x_work_dir="$x_compile_dir/$x_work_dir_tail"
 
    # Copy specified files to the build directory
-   if test ! -z "$x_files" ; then
-      for i in $x_files ; do
-         x_copy="$x_src_dir/$i"
-         if test -f "$x_copy"  -o  -d "$x_copy" ; then
-            cp -prf "$x_copy" "$x_work_dir"
-         else
-            echo "Warning:  \"$x_copy\" must be file or directory!"
-            exit 0
-         fi
-      done
+   if [ "$x_import_prj" = "no" ]; then
+      if [ ! -z "$x_files" ]; then
+         for i in $x_files ; do
+            x_copy="$x_src_dir/$i"
+            if [ -f "$x_copy"  -o  -d "$x_copy" ]; then
+               cp -prf "$x_copy" "$x_work_dir"
+            else
+               echo "Warning:  \"$x_copy\" must be file or directory!"
+               exit 0
+            fi
+         done
+      fi
    fi
 
    # Generate extension for tests output file
-   if test "$x_test" != "$x_test_prev" ; then 
+   if [ "$x_test" != "$x_test_prev" ]; then 
       x_cnt=1
       x_test_out="test_out"
    else
@@ -338,7 +378,7 @@ for x_row in $x_tests; do
    # Write test commands for current test into a shell script file
    cat >> $x_out <<EOF
 ######################################################################
-RunTest "$x_work_dir" \\
+RunTest "$x_work_dir_tail" \\
         "$x_test" \\
         "$x_app" \\
         "$x_cmd" \\
@@ -362,7 +402,7 @@ echo "Failed    : \$count_err"
 echo "Absent    : \$count_absent"
 echo
 
-if test \$count_err -eq 0; then
+if [ \$count_err -eq 0 ]; then
    echo
    echo "******** ALL TESTS COMPLETED SUCCESSFULLY ********"
    echo
