e02a5b2340710b2fd06ac6de98c0902aaa82868b
Aaron Ucko <ucko@ncbi.nlm.nih.gov>
2003-03-26 17:37:04 +0000

    Add FLTK's library directory to the runpath (should fix in-house IRIX builds)
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@8126 78c7ea69-d796-4a43-9a09-de51944f1b03

--- scripts/gbench_install.sh
+++ scripts/gbench_install.sh
@@ -259,5 +259,9 @@ chmod 755 ${target_dir}/bin/move-gbench.sh
 COMMON_ExecRB ${target_dir}/bin/move-gbench.sh ${target_dir}
 
 echo "Configuring plugin cache"
-COMMON_AddRunpath ${target_dir}/lib
+# COMMON_AddRunpath may interfere with compiled-in runpaths, so we
+# explicitly search FLTK's library directory.
+fltk_config=`sed -ne 's/^FLTK_CONFIG *= *//p' ${src_dir}/build/Makefile.mk`
+fltk_libdir=`$fltk_config --exec-prefix`/lib
+COMMON_AddRunpath ${src_dir}/lib:${fltk_libdir}
 COMMON_ExecRB ${target_dir}/bin/gbench_plugin_scan ${target_dir}/plugins
