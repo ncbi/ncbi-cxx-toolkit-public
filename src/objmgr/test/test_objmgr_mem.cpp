4dd864d287794891f181efad8496de9a3d1000d5
Eugene Vasilchenko <vasilche@ncbi.nlm.nih.gov>
2003-03-28 15:15:45 +0000

    Added file header comments.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@8166 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objmgr/test/test_objmgr_mem.cpp
+++ src/objmgr/test/test_objmgr_mem.cpp
@@ -144,7 +144,6 @@ int CMemTestApp::Run(void)
                 id.SetGi(gi);
                 CBioseq_Handle bh = scope.GetBioseqHandle(id);
                 const CSeq_entry& entry = bh.GetTopLevelSeqEntry();
-                const CBioseq& seq = entry.GetSeq();
                 {
                     const CObject* obj = &entry;
                     int c = reinterpret_cast<const int*>(obj)[1];
@@ -208,3 +207,41 @@ int main(int argc, const char* argv[])
 {
     return CMemTestApp().AppMain(argc, argv, 0, eDS_Default, 0);
 }
+
+/*  $Id$
+* ===========================================================================
+*
+*                            PUBLIC DOMAIN NOTICE
+*               National Center for Biotechnology Information
+*
+*  This software/database is a "United States Government Work" under the
+*  terms of the United States Copyright Act.  It was written as part of
+*  the author's official duties as a United States Government employee and
+*  thus cannot be copyrighted.  This software/database is freely available
+*  to the public for use. The National Library of Medicine and the U.S.
+*  Government have not placed any restriction on its use or reproduction.
+*
+*  Although all reasonable efforts have been taken to ensure the accuracy
+*  and reliability of the software and data, the NLM and the U.S.
+*  Government do not and cannot warrant the performance or results that
+*  may be obtained by using this software or data. The NLM and the U.S.
+*  Government disclaim all warranties, express or implied, including
+*  warranties of performance, merchantability or fitness for any particular
+*  purpose.
+*
+*  Please cite the author in any work or product based on this material.
+*
+* ===========================================================================
+*
+* Authors:  Eugene Vasilchenko
+*
+* File Description:
+*   Test memory leaks in C++ object manager
+*
+* ---------------------------------------------------------------------------
+* $Log$
+* Revision 1.5  2003/03/28 15:15:45  vasilche
+* Added file header comments.
+*
+* ===========================================================================
+*/
