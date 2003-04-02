05a9915b6c20076dd6e4e82448f05b5d27bfdb23
siyan <siyan@ncbi.nlm.nih.gov>
2003-04-02 18:19:49 +0000

    meta file for doxygen modules
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@8260 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ doc/tools/doxygen/metamodules_doxygen.h
@@ -0,0 +1,175 @@
+/*  $Id$
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Author:  Karanjit Siyan
+ *
+ * File Description:
+ *   Comment blocks for doxygen metamodules
+ *
+ */
+
+/** @defgroup CORELIB
+ *  Core Library members
+ */
+
+/** @defgroup AppFramework Application Framework
+ *  @ingroup CORELIB
+ */
+
+/** @defgroup Args Argument Processing
+ *  @ingroup CORELIB
+ */
+
+/** @defgroup Counters Atomic Counters
+ *  @ingroup CORELIB
+ */
+
+/** @defgroup Debug
+ *  @ingroup CORELIB
+ */
+
+/** @defgroup Diagnostics
+ *  @ingroup CORELIB
+ */
+
+/** @defgroup Environment Environment Interface
+ *  @ingroup CORELIB
+ */
+
+/** @defgroup Files Files and Directory
+ *  @ingroup CORELIB
+ */
+
+/** @defgroup MTWrappers MT Test Wrappers
+ *  @ingroup CORELIB
+ */
+
+/** @defgroup Object Object and Ref classes
+ *  @ingroup CORELIB
+ */
+
+/** @defgroup Portability Portability Definitions
+ *  @ingroup CORELIB
+ */
+
+/** @defgroup Dll Portable DLL
+ *  @ingroup CORELIB
+ */
+
+/** @defgroup Exception Portable Exception Handling
+ *  @ingroup CORELIB
+ */
+
+/** @defgroup Pipes Portable Process Pipes
+ *  @ingroup CORELIB
+ */
+
+/** @defgroup Registry
+ *  @ingroup CORELIB
+ */
+
+/** @defgroup STL STL Use Hints
+ *  @ingroup CORELIB
+ */
+
+/** @defgroup Stream Stream Wrappers
+ *  @ingroup CORELIB
+ */
+
+/** @defgroup String String Manipulations
+ *  @ingroup CORELIB
+ */
+
+/** @defgroup Utility Template Utilities
+ *  @ingroup CORELIB
+ */
+
+/** @defgroup Threads
+ *  @ingroup CORELIB
+ */
+
+/** @defgroup Time
+ *  @ingroup CORELIB
+ */
+
+/** @defgroup Version
+ *  @ingroup CORELIB
+ */
+
+
+
+/**
+ *  @defgroup CGI 
+ *  CGI Members
+ */
+
+/** @defgroup OBJECTMANAGER OBJECT MANAGER
+ *  Object Manager members
+ */
+
+/** @defgroup ALGO ALGORITHMS
+ *  Algorithm members
+ */
+
+
+/** @defgroup APP APPLICATION
+ *  Application members
+ */
+
+
+/** @defgroup CTOOLS 
+ *  Ctool members
+ */
+
+
+/** @defgroup DBAPI 
+ *  Dbapi members
+ */
+
+
+/** @defgroup GUI 
+ *  GUI members
+ */
+
+
+/** @defgroup INTERNAL 
+ *  Internal members
+ */
+
+
+/** @defgroup OBJECTS
+ *  Objects members
+ */
+
+
+/** @defgroup SERIAL 
+ *  Serial members
+ */
+
+
+/** @defgroup UTIL 
+ *  Util members
+ */
+
