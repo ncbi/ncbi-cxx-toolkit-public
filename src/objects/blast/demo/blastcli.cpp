212049cdb9bd70b60bd1819a93139aaa09b0af95
boemker <boemker@ncbi.nlm.nih.gov>
2003-04-23 20:04:43 +0000

    Expanded tabs to spaces.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@8587 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objects/blast/demo/blastcli.cpp
+++ src/objects/blast/demo/blastcli.cpp
@@ -52,334 +52,334 @@
 using namespace ncbi;
 using namespace objects;
 
-//	==========================================================================
+//  ==========================================================================
 //
-//	write prints requests and replies.
+//  write prints requests and replies.
 //
-//	==========================================================================
+//  ==========================================================================
 
 template<class T>
 void write(ostream& os, CRef<T> t)
 {
-	auto_ptr<CObjectOStream> x(
-		CObjectOStream::Open(eSerial_AsnText, os));
+    auto_ptr<CObjectOStream> x(
+        CObjectOStream::Open(eSerial_AsnText, os));
 
-	*x << *t;
+    *x << *t;
 }
 
 template<class T>
 void write(ostream& os, const list<CRef<T> >& t)
 {
-	auto_ptr<CObjectOStream> x(
-		CObjectOStream::Open(eSerial_AsnText, os));
+    auto_ptr<CObjectOStream> x(
+        CObjectOStream::Open(eSerial_AsnText, os));
 
-	for(list<CRef<T> >::const_iterator i = t.begin(); i != t.end(); ++i)
-		*x << **i;
+    for(list<CRef<T> >::const_iterator i = t.begin(); i != t.end(); ++i)
+        *x << **i;
 }
 
-//	==========================================================================
+//  ==========================================================================
 //
-//	submit a request & return the reply
+//  submit a request & return the reply
 //
-//	==========================================================================
+//  ==========================================================================
 
 static CRef<CBlast4_reply>
 submit(CRef<CBlast4_request_body> body, bool echo = true)
 {
-	// create a request and, if echoing is enabled, print it to stdout
-	// to show what a request object looks like.
+    // create a request and, if echoing is enabled, print it to stdout
+    // to show what a request object looks like.
 
-	CRef<CBlast4_request> request(new CBlast4_request);
-	request->SetBody(*body);
-	if(echo)
-		write(cout, request);
+    CRef<CBlast4_request> request(new CBlast4_request);
+    request->SetBody(*body);
+    if(echo)
+        write(cout, request);
 
-	// submit the request to the server & get the reply.  if echoing is
-	// enabled, print the reply to stdout to show what a reply object
-	// looks like.
+    // submit the request to the server & get the reply.  if echoing is
+    // enabled, print the reply to stdout to show what a reply object
+    // looks like.
 
-	CRef<CBlast4_reply> reply(new CBlast4_reply);
-	CBlast4Client().Ask(*request, *reply);
-	if(echo)
-		write(cout, reply);
+    CRef<CBlast4_reply> reply(new CBlast4_reply);
+    CBlast4Client().Ask(*request, *reply);
+    if(echo)
+        write(cout, reply);
 
-	return reply;
+    return reply;
 }
 
-//	==========================================================================
+//  ==========================================================================
 //
-//	sample "get-databases" command
+//  sample "get-databases" command
 //
-//	==========================================================================
+//  ==========================================================================
 
 static void
 get_databases()
 {
-	CRef<CBlast4_request_body> body(new CBlast4_request_body);
+    CRef<CBlast4_request_body> body(new CBlast4_request_body);
     body->SetGet_databases();
 
-	submit(body);
+    submit(body);
 }
 
-//	==========================================================================
+//  ==========================================================================
 //
-//	sample "get-matrices" command
+//  sample "get-matrices" command
 //
-//	==========================================================================
+//  ==========================================================================
 
 static void
 get_matrices()
 {
-	CRef<CBlast4_request_body> body(new CBlast4_request_body);
+    CRef<CBlast4_request_body> body(new CBlast4_request_body);
     body->SetGet_matrices();
 
-	submit(body);
+    submit(body);
 }
 
-//	==========================================================================
+//  ==========================================================================
 //
-//	sample "get-parameters" command
+//  sample "get-parameters" command
 //
-//	==========================================================================
+//  ==========================================================================
 
 static void
 get_parameters()
 {
-	CRef<CBlast4_request_body> body(new CBlast4_request_body);
+    CRef<CBlast4_request_body> body(new CBlast4_request_body);
     body->SetGet_parameters();
 
-	submit(body);
+    submit(body);
 }
 
-//	==========================================================================
+//  ==========================================================================
 //
-//	sample "get-programs" command
+//  sample "get-programs" command
 //
-//	==========================================================================
+//  ==========================================================================
 
 static void
 get_programs()
 {
-	CRef<CBlast4_request_body> body(new CBlast4_request_body);
+    CRef<CBlast4_request_body> body(new CBlast4_request_body);
     body->SetGet_programs();
 
-	submit(body);
+    submit(body);
 }
 
-//	==========================================================================
+//  ==========================================================================
 //
-//	sample "get-search-results" command
+//  sample "get-search-results" command
 //
-//	==========================================================================
+//  ==========================================================================
 
 static CRef<CBlast4_reply>
 get_search_results(string id, bool echo = true)
 {
-	CRef<CBlast4_get_search_results_request> gsr(
-		new CBlast4_get_search_results_request);
-	gsr->SetRequest_id(id);
+    CRef<CBlast4_get_search_results_request> gsr(
+        new CBlast4_get_search_results_request);
+    gsr->SetRequest_id(id);
 
-	CRef<CBlast4_request_body> body(new CBlast4_request_body);
+    CRef<CBlast4_request_body> body(new CBlast4_request_body);
     body->SetGet_search_results(*gsr);
 
-	return submit(body, echo);
+    return submit(body, echo);
 }
 
-//	==========================================================================
+//  ==========================================================================
 //
-//	sample "get-sequences" command
+//  sample "get-sequences" command
 //
-//	==========================================================================
+//  ==========================================================================
 
 static void
 get_sequences()
 {
-	CRef<CSeq_id> id(new CSeq_id);
-	id->SetGi(3091);
+    CRef<CSeq_id> id(new CSeq_id);
+    id->SetGi(3091);
 
-	CRef<CBlast4_database> db(new CBlast4_database);
-	db->SetName("nr");
-	db->SetType(eBlast4_residue_type_protein);
+    CRef<CBlast4_database> db(new CBlast4_database);
+    db->SetName("nr");
+    db->SetType(eBlast4_residue_type_protein);
 
-	CRef<CBlast4_get_sequences_request> gsr(new CBlast4_get_sequences_request);
-	gsr->SetDatabase(*db);
-	gsr->SetSeq_ids().push_back(id);
+    CRef<CBlast4_get_sequences_request> gsr(new CBlast4_get_sequences_request);
+    gsr->SetDatabase(*db);
+    gsr->SetSeq_ids().push_back(id);
 
-	CRef<CBlast4_request_body> body(new CBlast4_request_body);
+    CRef<CBlast4_request_body> body(new CBlast4_request_body);
     body->SetGet_sequences(*gsr);
 
-	submit(CRef<CBlast4_request_body>(body));
+    submit(CRef<CBlast4_request_body>(body));
 }
 
-//	==========================================================================
+//  ==========================================================================
 //
-//	sample "finish-params" and "queue-search" commands
+//  sample "finish-params" and "queue-search" commands
 //
-//	==========================================================================
+//  ==========================================================================
 
 static void
 setp(list<CRef<CBlast4_parameter> >& l, string n, int x)
 {
-	CRef<CBlast4_value> v(new CBlast4_value);
-	v->SetInteger(x);
+    CRef<CBlast4_value> v(new CBlast4_value);
+    v->SetInteger(x);
 
-	CRef<CBlast4_parameter> p(new CBlast4_parameter);
-	p->SetName(n);
-	p->SetValue(*v);
+    CRef<CBlast4_parameter> p(new CBlast4_parameter);
+    p->SetName(n);
+    p->SetValue(*v);
 
-	l.push_back(p);
+    l.push_back(p);
 }
 
 static void
 setp(list<CRef<CBlast4_parameter> >& l, string n, string x)
 {
-	CRef<CBlast4_value> v(new CBlast4_value);
-	v->SetString(x);
+    CRef<CBlast4_value> v(new CBlast4_value);
+    v->SetString(x);
 
-	CRef<CBlast4_parameter> p(new CBlast4_parameter);
-	p->SetName(n);
-	p->SetValue(*v);
+    CRef<CBlast4_parameter> p(new CBlast4_parameter);
+    p->SetName(n);
+    p->SetValue(*v);
 
-	l.push_back(p);
+    l.push_back(p);
 }
 
 static void
 setp(list<CRef<CBlast4_parameter> >& l, string n, const char* x)
 {
-	setp(l, n, string(x));
+    setp(l, n, string(x));
 }
 
 static void
 setp(list<CRef<CBlast4_parameter> >& l, string n,
-	CRef<CBlast4_cutoff> x)
+    CRef<CBlast4_cutoff> x)
 {
-	CRef<CBlast4_value> v(new CBlast4_value);
-	v->SetCutoff(*x);
+    CRef<CBlast4_value> v(new CBlast4_value);
+    v->SetCutoff(*x);
 
-	CRef<CBlast4_parameter> p(new CBlast4_parameter);
-	p->SetName(n);
-	p->SetValue(*v);
+    CRef<CBlast4_parameter> p(new CBlast4_parameter);
+    p->SetName(n);
+    p->SetValue(*v);
 
-	l.push_back(p);
+    l.push_back(p);
 }
 
 static void
 finish_params()
 {
-	CRef<CBlast4_finish_params_request> q(new CBlast4_finish_params_request);
-	q->SetProgram("blastn");
-	q->SetService("plain");
+    CRef<CBlast4_finish_params_request> q(new CBlast4_finish_params_request);
+    q->SetProgram("blastn");
+    q->SetService("plain");
 
     CRef<CBlast4_cutoff> cutoff(new CBlast4_cutoff);
     cutoff->SetE_value(10);
 
-	CRef<CBlast4_request_body> body(new CBlast4_request_body);
+    CRef<CBlast4_request_body> body(new CBlast4_request_body);
     body->SetFinish_params(*q);
 
-	submit(body);
+    submit(body);
 }
 
 static CRef<CBioseq>
 get_seq(int gi)
 {
-	CObjectManager objmgr;
-	CScope scope(objmgr);
-	objmgr.RegisterDataLoader(*new CGBDataLoader, CObjectManager::eDefault);
-	scope.AddDefaults();
+    CObjectManager objmgr;
+    CScope scope(objmgr);
+    objmgr.RegisterDataLoader(*new CGBDataLoader, CObjectManager::eDefault);
+    scope.AddDefaults();
 
-	CRef<CSeq_id> id(new CSeq_id);
-	id->SetGi(gi);
+    CRef<CSeq_id> id(new CSeq_id);
+    id->SetGi(gi);
 
-	CBioseq* s =
-		const_cast<CBioseq*>(&scope.GetBioseqHandle(*id).GetBioseq());
+    CBioseq* s =
+        const_cast<CBioseq*>(&scope.GetBioseqHandle(*id).GetBioseq());
 
-	return CRef<CBioseq>(s);
+    return CRef<CBioseq>(s);
 }
 
 static string
 queue_search()
 {
-	CRef<CBioseq> seq(get_seq(3091));
+    CRef<CBioseq> seq(get_seq(3091));
 
-	CRef<CSeq_entry> seqentry(new CSeq_entry);
-	seqentry->SetSeq(*seq);
+    CRef<CSeq_entry> seqentry(new CSeq_entry);
+    seqentry->SetSeq(*seq);
 
-	CRef<CBioseq_set> seqset(new CBioseq_set);
-	seqset->SetSeq_set().push_back(seqentry);
+    CRef<CBioseq_set> seqset(new CBioseq_set);
+    seqset->SetSeq_set().push_back(seqentry);
 
-	CRef<CBlast4_subject> subject(new CBlast4_subject);
-	subject->SetDatabase("nr");
+    CRef<CBlast4_subject> subject(new CBlast4_subject);
+    subject->SetDatabase("nr");
 
-	CRef<CBlast4_queue_search_request> q(new CBlast4_queue_search_request);
-	q->SetProgram("blastp");
-	q->SetService("plain");
+    CRef<CBlast4_queue_search_request> q(new CBlast4_queue_search_request);
+    q->SetProgram("blastp");
+    q->SetService("plain");
     q->SetQueries(*seqset);
-	q->SetSubject(*subject);
+    q->SetSubject(*subject);
 
     CRef<CBlast4_cutoff> cutoff(new CBlast4_cutoff);
     cutoff->SetE_value(2e4);
 
-	list<CRef<CBlast4_parameter> >& l = q->SetParams();
+    list<CRef<CBlast4_parameter> >& l = q->SetParams();
     setp(l, "cutoff", cutoff);
     setp(l, "filter", "L;");
     setp(l, "gap-open", 9);
     setp(l, "gap-extend", 1);
     setp(l, "word-size", 2);
-	setp(l, "matrix", "PAM30");
+    setp(l, "matrix", "PAM30");
 
-	CRef<CBlast4_request_body> body(new CBlast4_request_body);
+    CRef<CBlast4_request_body> body(new CBlast4_request_body);
     body->SetQueue_search(*q);
 
-	CRef<CBlast4_reply> reply = submit(body);
-	return reply->GetBody().GetQueue_search().GetRequest_id();
+    CRef<CBlast4_reply> reply = submit(body);
+    return reply->GetBody().GetQueue_search().GetRequest_id();
 }
 
 static bool
 search_pending(CRef<CBlast4_reply> reply)
 {
-	const list<CRef<CBlast4_error> >& errors = reply->GetErrors();
-	for(list<CRef<CBlast4_error> >::const_iterator i = errors.begin();
-			i != errors.end(); ++i)
-		if((*i)->GetCode() == eBlast4_error_code_search_pending)
-			return true;
-	return false;
+    const list<CRef<CBlast4_error> >& errors = reply->GetErrors();
+    for(list<CRef<CBlast4_error> >::const_iterator i = errors.begin();
+            i != errors.end(); ++i)
+        if((*i)->GetCode() == eBlast4_error_code_search_pending)
+            return true;
+    return false;
 }
 
 
 static void
 queue_search_and_poll_for_results()
 {
-	string request_id = queue_search();
+    string request_id = queue_search();
 
-	// the request is virtually certain not to be ready yet; we check
-	// now because we want to show the reply that indicates that the
-	// search is still pending.
+    // the request is virtually certain not to be ready yet; we check
+    // now because we want to show the reply that indicates that the
+    // search is still pending.
 
-	get_search_results(request_id, true);
+    get_search_results(request_id, true);
 
-	// NCBI requests that clients wait 30 seconds before the first check
-	// and between subsequent checks.
+    // NCBI requests that clients wait 30 seconds before the first check
+    // and between subsequent checks.
 
-	bool ready = false;
-	while(!ready) {
-		SleepSec(30);
-		ready = !search_pending(get_search_results(request_id, false));
-	}
+    bool ready = false;
+    while(!ready) {
+        SleepSec(30);
+        ready = !search_pending(get_search_results(request_id, false));
+    }
 
-	// since the initial get_search_results is virtually certain not
-	// to have succeeded, and since the subsequent get_search_results
-	// calls had echoing suppressed, we can be pretty sure that we
-	// have not echoed the search results.  make the call once more
-	// just for the echo.  (a real client would not do this.)
+    // since the initial get_search_results is virtually certain not
+    // to have succeeded, and since the subsequent get_search_results
+    // calls had echoing suppressed, we can be pretty sure that we
+    // have not echoed the search results.  make the call once more
+    // just for the echo.  (a real client would not do this.)
 
-	get_search_results(request_id, true);
+    get_search_results(request_id, true);
 }
 
-//	==========================================================================
+//  ==========================================================================
 //
-//	CBlastcliApplication
+//  CBlastcliApplication
 //
-//	==========================================================================
+//  ==========================================================================
 
 
 class CBlastcliApplication : public CNcbiApplication {
@@ -387,7 +387,7 @@ private:
     virtual void Init(void);
     virtual int  Run(void);
     virtual void Exit(void);
-	string usage;
+    string usage;
 };
 
 
@@ -397,15 +397,15 @@ CBlastcliApplication::Init(void)
     auto_ptr<CArgDescriptions> arg_desc(new CArgDescriptions);
 
     arg_desc->SetUsageContext(
-		GetArguments().GetProgramBasename(),
-		"ASN.1 interface to BLAST 1.0 demonstration client");
+        GetArguments().GetProgramBasename(),
+        "ASN.1 interface to BLAST 1.0 demonstration client");
 
     arg_desc->AddExtra(1, 1,
-		"get-databases | get-matrices | get-sequences |\n"
-		"queue-search | get-search-results RID",
-		CArgDescriptions::eString);
+        "get-databases | get-matrices | get-sequences |\n"
+        "queue-search | get-search-results RID",
+        CArgDescriptions::eString);
 
-	arg_desc->PrintUsage(usage);
+    arg_desc->PrintUsage(usage);
 
     SetupArgDescriptions(arg_desc.release());
 }
@@ -414,28 +414,28 @@ int
 CBlastcliApplication::Run(void)
 {
     CArgs args = GetArgs();
-	string a = args[1].AsString();
-	if(a == "finish-params")
-		finish_params();
-	else if(a == "get-databases")
-		get_databases();
-	else if(a == "get-matrices")
-		get_matrices();
-	else if(a == "get-parameters")
-		get_parameters();
-	else if(a == "get-programs")
-		get_programs();
-	else if(a == "get-sequences")
-		get_sequences();
-	else if(a == "queue-search")
-		queue_search_and_poll_for_results();
-	else if(a == "get-search-results")
-		get_search_results(args[2].AsString());
-	else {
-		cerr << usage << endl;
-		exit(-1);
-	}
-	return 0;
+    string a = args[1].AsString();
+    if(a == "finish-params")
+        finish_params();
+    else if(a == "get-databases")
+        get_databases();
+    else if(a == "get-matrices")
+        get_matrices();
+    else if(a == "get-parameters")
+        get_parameters();
+    else if(a == "get-programs")
+        get_programs();
+    else if(a == "get-sequences")
+        get_sequences();
+    else if(a == "queue-search")
+        queue_search_and_poll_for_results();
+    else if(a == "get-search-results")
+        get_search_results(args[2].AsString());
+    else {
+        cerr << usage << endl;
+        exit(-1);
+    }
+    return 0;
 }
 
 void
