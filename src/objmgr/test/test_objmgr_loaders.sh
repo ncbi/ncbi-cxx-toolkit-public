5a2322caa9cf3e46eeeb3c179427b2b1c22d8293
Eugene Vasilchenko <vasilche@ncbi.nlm.nih.gov>
2003-05-28 16:20:47 +0000

    Added check for enabled Sybase before running PUBSEQOS check.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@9109 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objmgr/test/test_objmgr_loaders.sh
+++ src/objmgr/test/test_objmgr_loaders.sh
@@ -1,6 +1,26 @@
 #! /bin/sh
 #$Id$
 
-GENBANK_LOADER_METHOD="PUBSEQOS" $*  &&  GENBANK_LOADER_METHOD="ID1" $*
+status_dir="../../../../status"
 
-exit $?
+if test -f "$status_dir/Sybase.enabled"; then
+    methods="PUBSEQOS ID1"
+else
+    echo Sybase is disabled: skipping PIBSEQOS loader test
+    methods="ID1"
+fi
+
+exitcode=0
+for method in $methods; do
+    echo "Checking GenBank loader $method:"
+    GENBANK_LOADER_METHOD="$method"
+    export GENBANK_LOADER_METHOD
+    $*
+    error=$?
+    if test $error -ne 0; then
+        echo "Test of GenBank loader $method failed: $error"
+        exitcode=$error
+    fi
+done
+
+exit $exitcode
