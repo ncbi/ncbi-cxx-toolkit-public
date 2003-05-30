fab0974a432219719d0127839ab2f6075bf9bc29
Anatoliy Kuznetsov <kuznets@ncbi.nlm.nih.gov>
2003-05-30 20:29:39 +0000

    Implemented annotations loading
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@9156 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/objects/util/lds/lds_object.hpp
+++ include/objects/util/lds/lds_object.hpp
@@ -37,24 +37,20 @@
 #include <objects/util/lds/lds_db.hpp>
 #include <objects/util/lds/lds_set.hpp>
 #include <objects/util/lds/lds_expt.hpp>
-
+#include <objects/util/lds/lds_coreobjreader.hpp>
 
 BEGIN_NCBI_SCOPE
 BEGIN_SCOPE(objects)
 
 //////////////////////////////////////////////////////////////////
 //
-// SLDS_ObjectDB related methods.
+// SLDS_ObjectDB and SLDS_AnnotDB related methods.
 //
 
 class CLDS_Object
 {
 public:
-    CLDS_Object(SLDS_TablesCollection& db, const map<string, int>& obj_map)    
-    : m_db(db),
-      m_ObjTypeMap(obj_map),
-      m_MaxRecId(0)
-    {}
+    CLDS_Object(SLDS_TablesCollection& db, const map<string, int>& obj_map);    
 
     // Delete all objects living in the specified set of files.
     // All deleted ids are added to deleted set.
@@ -68,8 +64,33 @@ public:
                            const string& file_name,
                            CFormatGuess::EFormat format);
 
-    // Return max record id. 0 if no record found.
-    int FindMaxRecId();
+    // Return max record id in "object" table. 0 if no record found.
+    int FindMaxObjRecId();
+    // Return max record id in "annotation" table. 0 if no record found.
+    int FindMaxAnnRecId();
+
+protected:
+
+    // Checks if parsed object is a "bio object" (returns TRUE) 
+    // or an annotation. if applicable object_str_id parameter receives
+    // objject or annotation id. (fasta format).
+    bool IsObject(const CLDS_CoreObjectsReader::SObjectDetails& parse_info,
+                  string* object_str_id);
+
+    // Save object to the database, return record id.
+    // NOTE: This function recursively finds all objects' parents and saves
+    // the whole genealogy tree (not only the immediate argument).
+    int SaveObject(int file_id, 
+                   CLDS_CoreObjectsReader* sniffer,
+                   CLDS_CoreObjectsReader::SObjectDetails* obj_info);
+
+    // Save object information, return record id. This function is specific 
+    // for fasta format.
+    int SaveObject(int file_id,
+                   const string& seq_id,
+                   const string& description,
+                   size_t offset,
+                   int type_id);
 
 private:
     CLDS_Object(const CLDS_Object&);
@@ -79,7 +100,8 @@ private:
     SLDS_TablesCollection&  m_db;
     const map<string, int>& m_ObjTypeMap;
 
-    int                     m_MaxRecId;
+    int                     m_MaxObjRecId;  // Max. id in "object" table
+    int                     m_MaxAnnRecId;  // Max. id in "annotation" table
 };
 
 
@@ -90,6 +112,9 @@ END_NCBI_SCOPE
 /*
  * ===========================================================================
  * $Log$
+ * Revision 1.3  2003/05/30 20:29:39  kuznets
+ * Implemented annotations loading
+ *
  * Revision 1.2  2003/05/23 20:33:33  kuznets
  * Bulk changes in lds library, code reorganizations, implemented top level
  * objects read, metainformation persistance implemented for top level objects...
