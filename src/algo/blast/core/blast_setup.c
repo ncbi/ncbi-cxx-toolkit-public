cd988450e33d4b6f8642984ecee07e6ae2e01e33
Ilya Dondoshansky <dondosha@ncbi.nlm.nih.gov>
2003-06-06 20:37:53 +0000

    Removed unused functions and arguments; removed cvs logs
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@9322 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/algo/blast/core/blast_setup.c
+++ src/algo/blast/core/blast_setup.c
@@ -1,3 +1,4 @@
+static char const rcsid[] = "$Id$";
 /* ===========================================================================
 *
 *                            PUBLIC DOMAIN NOTICE
@@ -33,249 +34,6 @@ Contents: Utilities initialize/setup BLAST.
 $Revision$
 
 ******************************************************************************/
-
-/*
-* $Log$
-* Revision 1.25  2003/06/06 17:52:17  dondosha
-* Reordered arguments in BLAST_MainSetUp
-*
-* Revision 1.24  2003/06/06 17:12:09  dondosha
-* Use BLAST_GetQueryLength function to calculate length of one query within a concatenated set
-*
-* Revision 1.23  2003/06/05 18:33:16  dondosha
-* Compiler warning fixes
-*
-* Revision 1.22  2003/06/05 17:15:23  dondosha
-* SeqLoc is no longer used for query masking/filtering
-*
-* Revision 1.21  2003/05/30 15:52:11  coulouri
-* various lint-induced cleanups
-*
-* Revision 1.20  2003/05/21 22:29:04  dondosha
-* Fix for two sequences tblastn traceback
-*
-* Revision 1.19  2003/05/20 15:47:16  dondosha
-* Fix in BLAST_SetUpSubject for two sequences case
-*
-* Revision 1.18  2003/05/18 21:57:37  camacho
-* Use Uint1 for program name whenever possible
-*
-* Revision 1.17  2003/05/15 22:01:22  coulouri
-* add rcsid string to sources
-*
-* Revision 1.16  2003/05/08 18:20:47  dondosha
-* Corrected order of query frames
-*
-* Revision 1.15  2003/05/06 21:27:31  dondosha
-* Moved auxiliary functions from blast_driver.c to relevant modules
-*
-* Revision 1.14  2003/05/01 17:08:45  dondosha
-* BLAST_SetUpAuxStructures moved from blast_setup to blast_engine module
-*
-* Revision 1.13  2003/05/01 16:56:30  dondosha
-* Fixed strict compiler warnings
-*
-* Revision 1.12  2003/05/01 15:33:39  dondosha
-* Reorganized the setup of BLAST search
-*
-* Revision 1.11  2003/04/21 18:19:52  dondosha
-* UMR fixes
-*
-* Revision 1.10  2003/04/21 16:18:22  dondosha
-* Minor correction in BlastSetUp_GetSequence
-*
-* Revision 1.9  2003/04/18 22:26:45  dondosha
-* Removed structures generated from ASN.1 from the main engine of BLAST
-*
-* Revision 1.8  2003/04/17 21:14:41  dondosha
-* Added cutoff score hit parameters that is calculated from e-value
-*
-* Revision 1.7  2003/04/11 16:41:16  dondosha
-* Added an extern declaration to eliminate compiler warning
-*
-* Revision 1.6  2003/04/09 18:44:14  dondosha
-* Advance buffer pointer for masking residues for all programs
-*
-* Revision 1.5  2003/04/09 18:01:52  dondosha
-* Fixed creation of double integer location list for reverse strands for mask-at-hash case.
-*
-* Revision 1.4  2003/04/03 14:17:45  coulouri
-* fix warnings, remove unused parameter
-*
-* Revision 1.3  2003/04/02 17:20:41  dondosha
-* Added calculation of ungapped cutoff score in correct place
-*
-* Revision 1.2  2003/04/01 17:45:54  dondosha
-* Corrections for filtering with translated queries
-*
-* Revision 1.1  2003/03/31 18:22:30  camacho
-* Moved from parent directory
-*
-* Revision 1.54  2003/03/28 23:16:02  dondosha
-* Correction for multiple query case
-*
-* Revision 1.53  2003/03/28 18:12:14  coulouri
-* replumb to remove matrix from lookup structure, pass in directly to wordfinders
-*
-* Revision 1.52  2003/03/28 15:55:45  dondosha
-* Doxygen warnings fix
-*
-* Revision 1.51  2003/03/27 20:58:30  dondosha
-* Implementing query multiplexing for all types of BLAST programs
-*
-* Revision 1.50  2003/03/25 16:30:25  dondosha
-* Strict compiler warning fixes
-*
-* Revision 1.49  2003/03/25 16:22:44  camacho
-* Correction to setting ambiguous character in score block
-*
-* Revision 1.48  2003/03/24 17:27:42  dondosha
-* Improvements and additions for doxygen
-*
-* Revision 1.47  2003/03/21 21:12:09  madden
-* Minor fix for doxygen
-*
-* Revision 1.46  2003/03/14 16:55:09  dondosha
-* Minor corrections to eliminate strict compiler warnings
-*
-* Revision 1.45  2003/03/12 17:04:35  dondosha
-* Fix for multiple freeing of filter SeqLoc
-*
-* Revision 1.44  2003/03/10 19:32:16  dondosha
-* Fixed wrong order of structure initializations
-*
-* Revision 1.43  2003/03/07 20:42:23  dondosha
-* Added ewp argument to be initialized in BlastSetup_Main
-*
-* Revision 1.42  2003/03/07 15:30:47  coulouri
-* correct include
-*
-* Revision 1.41  2003/03/06 17:05:14  dondosha
-* Comments fixes for doxygen
-*
-* Revision 1.40  2003/03/05 21:38:21  dondosha
-* Cast argument to CharPtr in call to BlastScoreBlkFill, to get rid of compiler warning
-*
-* Revision 1.39  2003/03/05 21:18:28  coulouri
-* refactor and comment lookup table creation
-*
-* Revision 1.38  2003/03/05 20:58:50  dondosha
-* Corrections for handling effective search space for multiple queries
-*
-* Revision 1.37  2003/02/28 22:56:49  dondosha
-* BlastSetUp_SeqBlkNew moved to blast_util.c; compiler warning for types mismatch fixed
-*
-* Revision 1.36  2003/02/26 15:42:19  madden
-* const CharPtr becomes const Char *
-*
-* Revision 1.35  2003/02/25 20:03:02  madden
-* Remove BlastSetUp_Concatenate and BlastSetUp_Standard
-*
-* Revision 1.34  2003/02/14 17:37:24  madden
-* Doxygen compliant comments
-*
-* Revision 1.33  2003/02/13 21:40:40  madden
-* Validate options, send back message if problem
-*
-* Revision 1.32  2003/02/13 20:46:36  dondosha
-* Set the kbp and kbp_gap to kbp_std and kbp_gap_std respectively at the end of set up
-*
-* Revision 1.31  2003/02/07 23:44:35  dondosha
-* Corrections for multiple query sequences, in particular for lower case masking
-*
-* Revision 1.30  2003/01/28 15:14:21  madden
-* BlastSetUp_Main gets additional args for parameters
-*
-* Revision 1.29  2003/01/22 20:49:19  dondosha
-* Set the ambiguous residue before the ScoreBlk is filled
-*
-* Revision 1.28  2003/01/10 21:25:42  dondosha
-* Corrected buffer length for sequence block in case of multiple queries/strands
-*
-* Revision 1.27  2003/01/10 18:50:10  madden
-* Version of BlastSetUp_Main that does not require num_seqs or dblength
-*
-* Revision 1.26  2003/01/06 17:44:40  madden
-* First revision of BlastSetUp_Main
-*
-* Revision 1.25  2003/01/02 17:11:19  dondosha
-* Call different functions to fill protein/nucleotide lookup table
-*
-* Revision 1.24  2002/12/27 18:03:52  madden
-* Blastx fixes, added function BlastSetUp_ConvertProteinSeqLoc
-*
-* Revision 1.23  2002/12/24 16:21:40  madden
-* BlastSetUp_Mega renamed to BlastSetUp_Concatenate, unused arg frame removed
-*
-* Revision 1.22  2002/12/24 16:09:53  madden
-* Cleanup from strict compiler options
-*
-* Revision 1.21  2002/12/24 14:48:23  madden
-* Create lookup table for proteins
-*
-* Revision 1.20  2002/12/23 19:57:32  madden
-* Remove reverse buffer from BlastSetUp_GetSequence and other parts of code
-*
-* Revision 1.19  2002/12/20 20:54:35  dondosha
-* 1. Use value 14 (corresponding to N) for masked bases in blastn
-* 2. Corrected buffer offsets for masking filtered locations;
-* 3. Corrected how reverse strand buffer is masked.
-* 4. BlastScoreBlkGappedFill made external (probably temporarily).
-*
-* Revision 1.18  2002/12/20 15:15:24  madden
-* remove unused variables etc.
-*
-* Revision 1.17  2002/12/20 15:07:02  madden
-* Fixes to get both strands for (megablast) lookup table creation
-*
-* Revision 1.16  2002/12/19 21:23:09  madden
-* Add LookupTableWrapPtr to setup functions
-*
-* Revision 1.15  2002/12/19 19:58:02  madden
-* Add function BlastSetUp_Mega for megablast setup, set DoubleIntPtrs for lookup table
-*
-* Revision 1.14  2002/12/13 14:38:04  madden
-* Add function BlastSetUp_FilterSeqLoc2DoubleInt to convert SeqLocs to array of offsets for looup creation
-*
-* Revision 1.13  2002/12/12 21:21:12  dondosha
-* Changed blast_lookup.h to mb_lookup.h
-*
-* Revision 1.12  2002/12/12 16:05:21  madden
-* Remove include/need for blastdef.h
-*
-* Revision 1.11  2002/10/24 14:07:29  madden
-* Handle lower-case masking for blastx/tblastx
-*
-* Revision 1.9  2002/10/24 12:24:50  madden
-* Fixes for filtering, combine filtered SeqLocs
-*
-* Revision 1.8  2002/10/23 22:42:34  madden
-* Save context and frame information
-*
-* Revision 1.7  2002/10/22 15:50:39  madden
-* fix translating searches
-*
-* Revision 1.6  2002/10/17 15:45:33  madden
-* fixes for effective lengths
-*
-* Revision 1.5  2002/10/08 21:00:01  madden
-* Merge filtered locs and lower-case loc
-*
-* Revision 1.4  2002/10/07 21:04:15  madden
-* Fixes for filtering
-*
-* Revision 1.3  2002/10/02 20:46:04  madden
-* Added comments for parsing dust options
-*
-* Revision 1.2  2002/10/02 20:05:56  madden
-* Add filtering functions
-*
-* Revision 1.1  2002/09/20 20:36:33  madden
-* First version of setup functions
-*
-*/
-
-static char const rcsid[] = "$Id$";
 
 #include <blast_setup.h>
 #include <seg.h>
@@ -338,116 +96,8 @@ BlastScoreBlkGappedFill(BLAST_ScoreBlkPtr sbp, const BlastScoringOptionsPtr scor
 	return 0;
 }
 
-
-/** Converts a protein (translated) SeqLocPtr from the protein
- *	coordinates to the nucl. coordinates.  Only works on a 
- *	SeqLocPtr of type SeqIntPtr right now.  
- *
- * @param slp SeqLoc in protein coordinates [in]
- * @param frame Frame of translated DNA [in] 
- * @param full_length full length of DNA [in]
- * @param slp_out back-translated SeqLoc (DNA coordinates). [out]
-*/
-static Int2
-BlastSetUp_ConvertProteinSeqLoc(SeqLocPtr slp, Int2 frame, Int4 full_length, SeqLocPtr *slp_out)
-
-{
-	Int4 from, to;
-	SeqIntPtr seq_int;
-    	SeqLocPtr seqloc_new=NULL;
-
-	if (slp == NULL)
-		return 0;
-
-	if (slp->choice == SEQLOC_PACKED_INT)
-		slp = slp->data.ptrvalue;
-
-	while (slp)
-	{
-		if (slp->choice != SEQLOC_INT)
-			return -1;
-
-		seq_int = slp->data.ptrvalue;
-
-		if (frame < 0)
-		{
-			to = full_length - CODON_LENGTH*seq_int->from + frame;
-			from = full_length - CODON_LENGTH*seq_int->to + frame + 1;
-			ValNodeLink(&seqloc_new, 
-				SeqLocIntNew(from, to, Seq_strand_minus, SeqIdDup(seq_int->id)));
-		}
-		else
-		{
-			from = CODON_LENGTH*seq_int->from + frame - 1;
-			to = CODON_LENGTH*seq_int->to + frame - 1;
-			ValNodeLink(&seqloc_new, 
-				SeqLocIntNew(from, to, Seq_strand_plus, SeqIdDup(seq_int->id)));
-		}
-		slp = slp->next;
-	}
-
-	if (seqloc_new)
-		ValNodeAddPointer(slp_out, SEQLOC_PACKED_INT, seqloc_new);
-    
-	return 0;
-}
-
-/** Converts a DNA SeqLocPtr from the nucl. coordinates to 
- * the protein (translated) coordinates.  Only works on a SeqLocPtr of type 
- * SEQLOC_INT or SEQLOC_PACKED_INT right now.
- * @param slp back-translated SeqLoc (DNA coordinates). [in]
- * @param frame frame to use for translation. [in]
- * @param full_length length of nucleotide sequence. [in]
- * @param seqid SeqId to be used in SeqLoc. [in]
- * @param seqloc_translated SeqLoc in protein coordinates. [out]
-*/
-static Int2
-BlastSetUp_TranslateDNASeqLoc(SeqLocPtr slp, Int2 frame, Int4 full_length,
-	const SeqIdPtr seqid, SeqLocPtr *seqloc_translated) 
-{
-    SeqIntPtr seq_int;
-    Int4 from, to;
-    SeqLocPtr seqloc_new=NULL;
-    
-    if (slp == NULL)
-        return 0;
-    
-    if (slp->choice == SEQLOC_PACKED_INT)
-        slp = slp->data.ptrvalue;
-    
-    while (slp) 
-    {
-        if (slp->choice != SEQLOC_INT)
-            return 1;
-        
-        seq_int = slp->data.ptrvalue;
-        
-        if (frame < 0) 
-	{
-            	from = (full_length + frame - seq_int->to)/CODON_LENGTH;
-            	to = (full_length + frame - seq_int->from)/CODON_LENGTH;
-		ValNodeLink(&seqloc_new, 
-                   SeqLocIntNew(from, to, Seq_strand_minus, SeqIdDup(seqid)));
-        } 
-	else 
-	{
-            	from = (seq_int->from - frame + 1)/CODON_LENGTH;
-            	to = (seq_int->to - frame + 1)/CODON_LENGTH;
-		ValNodeLink(&seqloc_new, 
-                   SeqLocIntNew(from, to, Seq_strand_plus, SeqIdDup(seqid)));
-        }
-        slp = slp->next;
-    }
-
-    if (seqloc_new)
-	ValNodeAddPointer(seqloc_translated, SEQLOC_PACKED_INT, seqloc_new);
-    
-    return 0;
-}
-
 Int2 BLAST_GetTranslatedSeqLoc(SeqLocPtr query_slp, 
-        SeqLocPtr PNTR protein_slp_head, Int4Ptr full_dna_length, 
-        Int4Ptr PNTR frame_info_ptr)
+        SeqLocPtr PNTR protein_slp_head)
 {
    BioseqPtr protein_bsp;
    SeqLocPtr protein_slp = NULL, last_slp = NULL;
@@ -460,17 +110,11 @@ Int2 BLAST_GetTranslatedSeqLoc(SeqLocPtr query_slp,
    Uint1Ptr buffer, buffer_var, sequence;
    Int2 status;
    ValNodePtr vnp;
-   Int4 frame_array[6] = { 1, 2, 3, -1, -2, -3 };
-   Int4Ptr frame_info;
-   SeqIdPtr seqid;
    
    if (!query_slp)
       return -1;
 
-   *full_dna_length = SeqLocTotalLen("blastn", query_slp);
    *protein_slp_head = NULL;
-   *frame_info_ptr = frame_info = 
-      (Int4Ptr) Malloc(6*ValNodeLen(query_slp)*sizeof(Int4));
 
    gcp = GeneticCodeFind(1, NULL);
    for (vnp = (ValNodePtr)gcp->data.ptrvalue; vnp != NULL; 
@@ -494,15 +138,11 @@ Int2 BLAST_GetTranslatedSeqLoc(SeqLocPtr query_slp,
             buffer_var += query_length + 1;
          }
          sequence = BLAST_GetTranslation(buffer_var, NULL, query_length, 
-                       ABS(frame_array[i]), &protein_length, genetic_code);
+                       i%3+1, &protein_length, genetic_code);
          protein_bsp = BLAST_MakeTempProteinBioseq(sequence+1, 
                           protein_length, Seq_code_ncbistdaa);
          protein_slp = SeqLocIntNew(0, protein_length-1, Seq_strand_plus, 
                                     protein_bsp->id);
-         /* Attach the underlying query id to the unique id of this 
-            SeqLoc */
-         seqid = SeqLocId(protein_slp);
-         seqid->next = SeqLocId(query_slp);
 
          if (*protein_slp_head == NULL)
             *protein_slp_head = protein_slp;
@@ -510,7 +150,6 @@ Int2 BLAST_GetTranslatedSeqLoc(SeqLocPtr query_slp,
             ValNodeLink(&last_slp, protein_slp);
          last_slp = protein_slp;
          
-         frame_info[index] = frame_array[i];
          index++;
       }
 
