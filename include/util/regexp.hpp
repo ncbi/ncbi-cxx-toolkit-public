ecc469c0583dc79e504da33d2f2f0b07d9f68ae9
clausen <clausen@ncbi.nlm.nih.gov>
2003-06-20 18:32:42 +0000

    Changed to native interface for regexp
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@9528 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/util/regexp.hpp
+++ include/util/regexp.hpp
@@ -35,7 +35,7 @@
 */
 
 #include <corelib/ncbistd.hpp>
-#include <util/regexp/pcreposix.h>
+#include <util/regexp/pcre.h>
 
 BEGIN_NCBI_SCOPE
 
@@ -44,12 +44,12 @@ const size_t kRegexpMaxSubPatterns = 100;
 class CRegexp
 {
 public:
-    // flags for constructor and Set
+    // Flags for constructor and Set
     enum ECompile {
         eCompile_default     = 0,
-        eCompile_ignore_case = REG_ICASE,
-        eCompile_newline     = REG_NEWLINE,
-        eCompile_both        = REG_NEWLINE | REG_ICASE
+        eCompile_ignore_case = PCRE_CASELESS,
+        eCompile_newline     = PCRE_MULTILINE,
+        eCompile_both        = PCRE_MULTILINE | PCRE_CASELESS
     };
     
     // ctors
@@ -59,63 +59,51 @@ public:
     
     // Set regular expression pattern
     void Set(const string &pat, // Perl regular expression
-             int flags = 0);    // Flags
-    
-    // struct to return Match results
-    struct SMatches {
-        SMatches() : m_Begin(-1), m_End(-1) {}
-        int m_Begin;  // 0 based index of beginning of match
-        int m_End;    // 0 based index 1 greater than the last char of match
-    };
-    
-    // Flags for Match and GetMatch
+             int flags = 0);    // Options
+       
+    // GetMatch flags
     enum EMatch {
         eMatch_default   = 0,
-        eMatch_not_begin = REG_NOTBOL,            // ^ won't match string begin
-        eMatch_not_end   = REG_NOTEOL,            // $ won't match string end
-        eMatch_not_both  = REG_NOTBOL | REG_NOTEOL
+        eMatch_not_begin = PCRE_NOTBOL,           // ^ won't match string begin
+        eMatch_not_end   = PCRE_NOTEOL,           // $ won't match string end
+        eMatch_not_both  = PCRE_NOTBOL | PCRE_NOTEOL
     };
     
-    // Used to match pre-compiled perl compatible regular expression
-    // compiled either by constructor or subsequent Set. Use occurrence
-    // to find occurrences after the first (0 is 1st, 1 is 2nc, etc).
-    // Use EMatch for flags. Retuns array of SMatches struct. The 0-th index
-    // in array holds the first and one beyond last (0 based) indexes of
-    // the occurrence-th match with pattern. Indicies > 0 hold matches to 
-    // sub-patterns (e.g., pattern = "(d.*s) *are" has sub-pattern "(d.*s)").
-    // If you are trying to find multiple matches to a pattern in cstr,
-    // it is more efficient to use occurrence = 0 and manipulate where cstr
-    // points before each call than to call Match multiple times, 
-    // incrementing occurrence..
-    const SMatches* Match(const char *cstr,       // String to search
-                          size_t occurrence = 0,  // Occurrence to find
-                          int flags = 0);         // Flags
-
-    // Similar to Match, but returns a string corresponding to the match
+    // Returns a string corresponding to the match
     // to pattern or sub-pattern. Use idx = 0 for complete pattern. Use idx
-    // > 0 for sub-patterns.
-    string GetMatch (const char *cstr,            // String to search
-                     size_t idx = 0,              // What to return
-                     size_t occurrence = 0,       // Occurrence to find
-                     int flags = 0);              // Flags
+    // > 0 for sub-patterns. Returns empty string when no match
+    string GetMatch (const char *str,             // String to search
+                     size_t offset = 0,           // Starting offset in str
+                     size_t idx = 0,              // (sub) match to return
+                     int flags = 0,               // Options
+                     bool noreturn = false);      // Returns kEmptyStr if true
     
-    // Should only be called after Match has been called. This function just
-    // applies SMatches array calculated by Match against cstr to
-    // return a string holding the pattern match. You can do the same thing
-    // that this function does, this function is just for convenience. Function
-    // GetMatch above is equivalent to a call to Match followed by a call to
-    // GetSub.
-    string GetSub (const char *cstr,              // String to search
-                   size_t idx = 0) const;         // What to return
-
+    // Should only be called after GetMatch has been called with the
+    // same cstr. GetMatch internally stores locations on cstr where
+    // pattern and sub patterns were found. This function will return
+    // the substring at location of pattern match (idx 0) or sub pattern
+    // match (idx > 0). Returns empty string when no match
+    string GetSub (const char *str,               // String to search
+                   size_t idx = 0) const;         // (sub) match to return
     
+    // Returns the number of patterns + sub patterns found as a result
+    // of the most recent GetMatch call
+    int NumFound() const;
+    
+    // Returns array where index 0 is location of first character in
+    // pattern/sub pattern and index 1 is 1 beyond last character in
+    // pattern/sub pattern. Throws if called with idx >= NumFound(). 
+    // Use idx == 0 for pattern, idx > 0 for sub patterns
+    const int* GetResults(size_t idx) const;
+       
 private:
     // Disable copy constructor and assignment operator
     CRegexp(const CRegexp &);
     void operator= (const CRegexp &);
     
-    regex_t  m_PReg;                             // Compiled pattern
-    SMatches m_Results[kRegexpMaxSubPatterns];   // Results of Match
+    pcre  *m_PReg;                                  // Compiled pattern
+    int m_Results[(kRegexpMaxSubPatterns +1) * 3];  // Results of Match
+    int m_NumFound;                                 // #(sub) patterns
 };
 
 
@@ -124,6 +112,9 @@ END_NCBI_SCOPE
 /*
 * ---------------------------------------------------------------------------
 * $Log$
+* Revision 1.2  2003/06/20 18:32:42  clausen
+* Changed to native interface for regexp
+*
 * Revision 1.1  2003/06/03 14:47:46  clausen
 * Initial version
 *
