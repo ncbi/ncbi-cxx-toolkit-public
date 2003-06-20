c77171ab3d5fbbc624b73847075153819975a57d
clausen <clausen@ncbi.nlm.nih.gov>
2003-06-20 18:26:37 +0000

    Switched to native regexp interface
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@9525 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/util/regexp.cpp
+++ src/util/regexp.cpp
@@ -36,96 +36,88 @@
 BEGIN_NCBI_SCOPE
 
 
-CRegexp::CRegexp(const string &pat, int flags) 
+CRegexp::CRegexp(const string &pat, int flags) : m_NumFound(0)
 {
-    if (regcomp(&m_PReg, pat.c_str(), flags)) {
-        throw runtime_error("Could not compile pattern");
+    const char *error;
+    int erroffset;
+    m_PReg = pcre_compile(pat.c_str(), flags, &error, &erroffset, NULL);
+    if (m_PReg == NULL) {
+        throw runtime_error(error);
     }
 }
 
 
 CRegexp::~CRegexp()
 {
-    regfree(&m_PReg);    
+    (*pcre_free)(m_PReg);    
 }
 
 
 void CRegexp::Set(const string &pat, int flags)
 {
-    regfree(&m_PReg);
+    if (m_PReg != NULL) {
+        (*pcre_free)(m_PReg);
+    }
             
-    if (regcomp(&m_PReg, pat.c_str(), flags)) {
-        throw runtime_error("Could not compile pattern");
+    const char *error;
+    int erroffset;
+    m_PReg = pcre_compile(pat.c_str(), flags, &error, &erroffset, NULL);
+    if (m_PReg == NULL) {
+        throw runtime_error(error);
     }
 }
 
 
-const CRegexp::SMatches* CRegexp::Match
-(const char *cstr,
- size_t occurrence, 
- int flags)
+string CRegexp::GetSub(const char *str, size_t idx) const
 {
-    const char *cptr = cstr;
-    regmatch_t rslts[kRegexpMaxSubPatterns];
-    size_t count = 0, new_offset = 0, old_offset = 0;
-    int rtn = 0;
-    do {
-        rtn = regexec(&m_PReg, cptr, kRegexpMaxSubPatterns, rslts, flags);
-        if (rtn != 0 && rtn != REG_NOMATCH) {
-            throw runtime_error("Could not execute match");
-        }
-        if (rtn != REG_NOMATCH) {
-            old_offset = new_offset;
-            new_offset += rslts[0].rm_eo;
-            cptr += rslts[0].rm_eo;
-        }
-        count++;
-    } while(count <= occurrence && rtn != REG_NOMATCH);
-    if (rtn == REG_NOMATCH) {
-        m_Results[0].m_Begin = -1;
-        m_Results[0].m_End = -1;
-        return m_Results;
-    }
-    for (size_t i = 0; i < kRegexpMaxSubPatterns; i++) {
-        if (m_Results[i].m_Begin == -1 && rslts[i].rm_so == -1) {
-            break;
-        }
-        m_Results[i].m_Begin = rslts[i].rm_so + old_offset;
-        m_Results[i].m_End = rslts[i].rm_eo + old_offset;
-    }       
-    return m_Results;
-}
-
-
-string CRegexp::GetSub(const char *cstr, size_t idx) const
-{
-    if (idx >= kRegexpMaxSubPatterns || m_Results[idx].m_Begin < 0 || 
-        m_Results[idx].m_End < 0)
+    if ((int)idx >= m_NumFound)
     {
         return kEmptyStr;
     }
-    string str(cstr);
-    size_t len = m_Results[idx].m_End - m_Results[idx].m_Begin;
-    return str.substr(m_Results[idx].m_Begin, len);
+    size_t len = m_Results[2 * idx + 1] - m_Results[2 * idx];
+    return string(str + m_Results[2 * idx], len);
 }
 
 
 string CRegexp::GetMatch
-(const char *cstr,
+(const char *str,
+ size_t offset,
  size_t idx,
- size_t occurrence,
- int flags)
+ int flags,
+ bool noreturn)
 {
-    Match(cstr, occurrence, flags);
-    return GetSub(cstr, idx);
+    m_NumFound = pcre_exec(m_PReg, NULL, str, strlen(str), offset,
+        flags, m_Results, (kRegexpMaxSubPatterns +1) * 3);
+    if (noreturn) {
+        return kEmptyStr;
+    } else {   
+        return GetSub(str, idx);
+    }
 }
 
+
+int CRegexp::NumFound() const
+{
+    return m_NumFound;
+}
+
+
+const int* CRegexp::GetResults(size_t idx) const
+{
+    if ((int)idx >= m_NumFound) {
+        throw runtime_error("idx >= NumFound()");
+    }
+    return m_Results + 2 * idx;
+}
 
 END_NCBI_SCOPE
  
 /*
 * ---------------------------------------------------------------------------
 * $Log$
+* Revision 1.2  2003/06/20 18:26:37  clausen
+* Switched to native regexp interface
+*
 * Revision 1.1  2003/06/03 14:46:23  clausen
 * Initial version
 *
