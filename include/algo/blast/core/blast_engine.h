e6b939b868c2229860fca65ee28ed2803e3ca271
Ilya Dondoshansky <dondosha@ncbi.nlm.nih.gov>
2003-06-26 21:28:09 +0000

    Separated database and two sequences search engines
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@9610 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/algo/blast/core/blast_engine.h
+++ include/algo/blast/core/blast_engine.h
@@ -49,77 +49,76 @@ extern "C" {
 
 #define OFFSET_ARRAY_SIZE 4096
 
-/** The high level function performing the BLAST search after all the setup
- * has been done.
+/** The high level function performing the BLAST search against a BLAST 
+ * database after all the setup has been done.
+ * @param program_number Type of BLAST program [in]
  * @param query The query sequence [in]
- * @param lookup The lookup table [in]
- * @param query_info Additional query information [in]
- * @param db Structure containing BLAST database [in]
- * @param subject_blk Subject sequence in two sequences case [in]
- * @param ewp Structure for tracking the initial hits [in]
- * @param gap_align Structure for gapped alignment [in]
- * @param score_options Hit scoring options [in]
- * @param word_params Options for processing initial word hits [in]
- * @param ext_params Options and parameters for the gapped extension [in]
- * @param hit_params Options for saving the HSPs [in]
- * @param psi_options Options specific to PSI-BLAST [in]
- * @param db_options Options for handling BLAST database [in]
- * @param results_ptr Structure holding all saved results [out]
- */
-Int4 
-BLAST_SearchEngineCore(BLAST_SequenceBlkPtr query, 
-        LookupTableWrapPtr lookup, BlastQueryInfoPtr query_info,
-        ReadDBFILEPtr db, BLAST_SequenceBlkPtr subject_blk, 
-        BLAST_ExtendWordPtr ewp, BlastGapAlignStructPtr gap_align, 
-        const BlastScoringOptionsPtr score_options, 
-        const BlastInitialWordParametersPtr word_params, 
-        BlastExtensionParametersPtr ext_params, 
-        BlastHitSavingParametersPtr hit_params, 
-        PSIBlastOptionsPtr psi_options, 
-        BlastDatabaseOptionsPtr db_options,
-        BlastResultsPtr PNTR results_ptr,
-        BlastReturnStatPtr return_stats);
-
-
-/** The high level function performing the BLAST search after all the setup
- * has been done.
- * @param blast_program Type of BLAST program (blastn, blastp, etc.) [in]
- * @param query The query sequence block [in]
  * @param query_info Additional query information [in]
  * @param rdfp Structure containing BLAST database [in]
- * @param subject_blk Subject sequence in two sequences case [in]
- * @param sbp Scoring statistical information [in]
+ * @param sbp Scoring and statistical parameters [in]
  * @param score_options Hit scoring options [in]
- * @param lookup_options Lookup table options [in]
- * @param lookup_segments Query segments for which lookup table should be 
- *                        created [in]
+ * @param lookup_options Options for creation of lookup table [in]
+ * @param lookup_segments Query segments for which lookup table has to be 
+ *                        built [in]
  * @param word_options Options for processing initial word hits [in]
- * @param ext_options Options for the gapped extension [in]
- * @param eff_len_options Options for calculation of the effective search 
- *                        space [in]
+ * @param ext_options Options and parameters for the gapped extension [in]
  * @param hit_options Options for saving the HSPs [in]
- * @param word_params_ptr Parameters for processing initial word hits [out]
- * @param ext_params_ptr Parameters for the gapped extension [out]
+ * @param eff_len_options Options for setting effective lengths [in]
  * @param psi_options Options specific to PSI-BLAST [in]
  * @param db_options Options for handling BLAST database [in]
- * @param results Structure holding all saved results [out]
- * @param return_stats Return statistics numbers [out]
+ * @param results Structure holding all saved results [in] [out]
+ * @param return_stats Return statistics containing numbers of hits on 
+ *                     different stages of the search [out]
  */
-Int2 BLAST_SearchEngine(const Uint1 blast_program, BLAST_SequenceBlkPtr query, 
-        BlastQueryInfoPtr query_info,
-        ReadDBFILEPtr rdfp, BLAST_SequenceBlkPtr subject_blk, 
-        BLAST_ScoreBlkPtr sbp, 
-        const BlastScoringOptionsPtr score_options, 
-        const LookupTableOptionsPtr lookup_options, ValNodePtr lookup_segments,
-        const BlastInitialWordOptionsPtr word_options, 
-        const BlastExtensionOptionsPtr ext_options, 
-        const BlastEffectiveLengthsOptionsPtr eff_len_options,
-        const BlastHitSavingOptionsPtr hit_options, 
-        BlastInitialWordParametersPtr PNTR word_params_ptr, 
-        BlastExtensionParametersPtr PNTR ext_params_ptr, 
-        PSIBlastOptionsPtr psi_options, 
-        BlastDatabaseOptionsPtr db_options,
-        BlastResultsPtr PNTR results, BlastReturnStatPtr return_stats);
+Int4 
+BLAST_DatabaseSearchEngine(Uint1 program_number, 
+   BLAST_SequenceBlkPtr query, BlastQueryInfoPtr query_info,
+   ReadDBFILEPtr rdfp, BLAST_ScoreBlkPtr sbp, 
+   const BlastScoringOptionsPtr score_options, 
+   const LookupTableOptionsPtr lookup_options,
+   ValNodePtr lookup_segments, 
+   const BlastInitialWordOptionsPtr word_options, 
+   const BlastExtensionOptionsPtr ext_options, 
+   const BlastHitSavingOptionsPtr hit_options,
+   const BlastEffectiveLengthsOptionsPtr eff_len_options,
+   const PSIBlastOptionsPtr psi_options, 
+   const BlastDatabaseOptionsPtr db_options,
+   BlastResultsPtr results, BlastReturnStatPtr return_stats);
+
+/** The high level function performing BLAST comparison of two sequences,
+ * after all the setup has been done.
+ * @param program_number Type of BLAST program [in]
+ * @param query Query sequence [in]
+ * @param query_info Query information structure [in]
+ * @param subject Subject sequence [in]
+ * @param sbp Scoring and statistical parameters [in]
+ * @param score_options Hit scoring options [in]
+ * @param lookup_options Options for creation of lookup table [in]
+ * @param lookup_segments Query segments for which lookup table has to be 
+ *                        built [in]
+ * @param word_options Options for processing initial word hits [in]
+ * @param ext_options Options and parameters for the gapped extension [in]
+ * @param hit_options Options for saving the HSPs [in]
+ * @param eff_len_options Options for setting effective lengths [in]
+ * @param psi_options Options specific to PSI-BLAST [in]
+ * @param db_options Options for handling BLAST database [in]
+ * @param results Structure holding all saved results [in] [out]
+ * @param return_stats Return statistics containing numbers of hits on 
+ *                     different stages of the search [out]
+ */
+Int4 
+BLAST_TwoSequencesEngine(Uint1 program_number, BLAST_SequenceBlkPtr query, 
+   BlastQueryInfoPtr query_info, BLAST_SequenceBlkPtr subject, 
+   BLAST_ScoreBlkPtr sbp, const BlastScoringOptionsPtr score_options, 
+   const LookupTableOptionsPtr lookup_options,
+   ValNodePtr lookup_segments, 
+   const BlastInitialWordOptionsPtr word_options, 
+   const BlastExtensionOptionsPtr ext_options, 
+   const BlastHitSavingOptionsPtr hit_options, 
+   const BlastEffectiveLengthsOptionsPtr eff_len_options,
+   const PSIBlastOptionsPtr psi_options, 
+   const BlastDatabaseOptionsPtr db_options,
+   BlastResultsPtr results, BlastReturnStatPtr return_stats);
 
 #ifdef __cplusplus
 }
