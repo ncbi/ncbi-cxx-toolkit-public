6f05f0c4e62e75b2da5a50c871be3c8e7c417c16
Denis Vakatov <vakatov@ncbi.nlm.nih.gov>
2003-07-14 20:26:39 +0000

    CCgiResponse::SetHeaderValue() -- date format to conform with RFC1123
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@9797 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/cgi/ncbicgir.cpp
+++ src/cgi/ncbicgir.cpp
@@ -28,76 +28,8 @@
 * File Description:
 *   CCgiResponse  -- CGI response generator class
 *
-* ---------------------------------------------------------------------------
-* $Log$
-* Revision 1.19  2003/03/12 16:10:23  kuznets
-* iterate -> ITERATE
-*
-* Revision 1.18  2003/02/24 20:01:55  gouriano
-* use template-based exceptions instead of errno and parse exceptions
-*
-* Revision 1.17  2002/07/18 20:18:36  lebedev
-* NCBI_OS_MAC: STDOUT_FILENO define added
-*
-* Revision 1.16  2002/07/11 14:22:59  gouriano
-* exceptions replaced by CNcbiException-type ones
-*
-* Revision 1.15  2002/04/15 01:18:42  lavr
-* Check if status is less than 100
-*
-* Revision 1.14  2002/03/19 00:34:56  vakatov
-* Added convenience method CCgiResponse::SetStatus().
-* Treat the status right in WriteHeader() for both plain and "raw CGI" cases.
-* Made all header values to be case-insensitive.
-*
-* Revision 1.13  2001/10/04 18:17:53  ucko
-* Accept additional query parameters for more flexible diagnostics.
-* Support checking the readiness of CGI input and output streams.
-*
-* Revision 1.12  2001/07/17 22:39:54  vakatov
-* CCgiResponse:: Made GetOutput() fully consistent with out().
-* Prohibit copy constructor and assignment operator.
-* Retired "ncbicgir.inl", moved inline functions to the header itself, made
-* some non-inline. Improved comments, got rid of some unused STL headers.
-* Well-groomed the code.
-*
-* Revision 1.11  2000/11/01 20:36:31  vasilche
-* Added HTTP_EOL string macro.
-*
-* Revision 1.10  1999/11/02 20:35:43  vakatov
-* Redesigned of CCgiCookie and CCgiCookies to make them closer to the
-* cookie standard, smarter, and easier in use
-*
-* Revision 1.9  1999/05/11 03:11:52  vakatov
-* Moved the CGI API(along with the relevant tests) from "corelib/" to "cgi/"
-*
-* Revision 1.8  1999/04/28 16:54:43  vasilche
-* Implemented stream input processing for FastCGI applications.
-* Fixed POST request parsing
-*
-* Revision 1.7  1999/02/22 22:45:39  vasilche
-* Fixed map::insert(value_type) usage.
-*
-* Revision 1.6  1998/12/28 17:56:36  vakatov
-* New CVS and development tree structure for the NCBI C++ projects
-*
-* Revision 1.5  1998/12/23 13:57:55  vasilche
-* Default output stream will be NcbiCout.
-*
-* Revision 1.4  1998/12/11 22:00:34  vasilche
-* Added raw CGI response
-*
-* Revision 1.3  1998/12/11 18:00:54  vasilche
-* Added cookies and output stream
-*
-* Revision 1.2  1998/12/10 19:58:22  vasilche
-* Header option made more generic
-*
-* Revision 1.1  1998/12/09 20:18:18  vasilche
-* Initial implementation of CGI response generator
-*
-* ===========================================================================
 */
+
 
 #include <cgi/ncbicgir.hpp>
 #include <cgi/cgi_exception.hpp>
@@ -183,9 +115,10 @@ void CCgiResponse::SetHeaderValue(const string& name, const tm& date)
         return;
     }
 
-    char buff[64];
-    if ( !::strftime(buff, sizeof(buff), "%a %b %d %H:%M:%S %Y", &date) ) {
-        NCBI_THROW(CCgiErrnoException,eErrno,
+    char buff[30];
+    if ( !::strftime(buff, sizeof(buff),
+                     "%a, %d %b %Y %H:%M:%S GMT", &date) ) {
+        NCBI_THROW(CCgiErrnoException, eErrno,
                    "CCgiResponse::SetHeaderValue() -- strftime() failed");
     }
     SetHeaderValue(name, buff);
@@ -263,3 +196,79 @@ void CCgiResponse::Flush(void) const
 
 
 END_NCBI_SCOPE
+
+
+/*
+* ===========================================================================
+* $Log$
+* Revision 1.20  2003/07/14 20:26:39  vakatov
+* CCgiResponse::SetHeaderValue() -- date format to conform with RFC1123
+*
+* Revision 1.19  2003/03/12 16:10:23  kuznets
+* iterate -> ITERATE
+*
+* Revision 1.18  2003/02/24 20:01:55  gouriano
+* use template-based exceptions instead of errno and parse exceptions
+*
+* Revision 1.17  2002/07/18 20:18:36  lebedev
+* NCBI_OS_MAC: STDOUT_FILENO define added
+*
+* Revision 1.16  2002/07/11 14:22:59  gouriano
+* exceptions replaced by CNcbiException-type ones
+*
+* Revision 1.15  2002/04/15 01:18:42  lavr
+* Check if status is less than 100
+*
+* Revision 1.14  2002/03/19 00:34:56  vakatov
+* Added convenience method CCgiResponse::SetStatus().
+* Treat the status right in WriteHeader() for both plain and "raw CGI" cases.
+* Made all header values to be case-insensitive.
+*
+* Revision 1.13  2001/10/04 18:17:53  ucko
+* Accept additional query parameters for more flexible diagnostics.
+* Support checking the readiness of CGI input and output streams.
+*
+* Revision 1.12  2001/07/17 22:39:54  vakatov
+* CCgiResponse:: Made GetOutput() fully consistent with out().
+* Prohibit copy constructor and assignment operator.
+* Retired "ncbicgir.inl", moved inline functions to the header itself, made
+* some non-inline. Improved comments, got rid of some unused STL headers.
+* Well-groomed the code.
+*
+* Revision 1.11  2000/11/01 20:36:31  vasilche
+* Added HTTP_EOL string macro.
+*
+* Revision 1.10  1999/11/02 20:35:43  vakatov
+* Redesigned of CCgiCookie and CCgiCookies to make them closer to the
+* cookie standard, smarter, and easier in use
+*
+* Revision 1.9  1999/05/11 03:11:52  vakatov
+* Moved the CGI API(along with the relevant tests) from "corelib/" to "cgi/"
+*
+* Revision 1.8  1999/04/28 16:54:43  vasilche
+* Implemented stream input processing for FastCGI applications.
+* Fixed POST request parsing
+*
+* Revision 1.7  1999/02/22 22:45:39  vasilche
+* Fixed map::insert(value_type) usage.
+*
+* Revision 1.6  1998/12/28 17:56:36  vakatov
+* New CVS and development tree structure for the NCBI C++ projects
+*
+* Revision 1.5  1998/12/23 13:57:55  vasilche
+* Default output stream will be NcbiCout.
+*
+* Revision 1.4  1998/12/11 22:00:34  vasilche
+* Added raw CGI response
+*
+* Revision 1.3  1998/12/11 18:00:54  vasilche
+* Added cookies and output stream
+*
+* Revision 1.2  1998/12/10 19:58:22  vasilche
+* Header option made more generic
+*
+* Revision 1.1  1998/12/09 20:18:18  vasilche
+* Initial implementation of CGI response generator
+*
+* ===========================================================================
+*/
