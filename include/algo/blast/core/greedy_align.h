ab53b6c4e4aceb35669e5c53a9774bc411ff43e8
Ilya Dondoshansky <dondosha@ncbi.nlm.nih.gov>
2003-07-23 18:08:16 +0000

    Functions and structures for greedy gapped alignment
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@10000 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ include/algo/blast/core/greedy_align.h
@@ -0,0 +1,121 @@
+/* $Id$
+* ===========================================================================
+*
+*                            PUBLIC DOMAIN NOTICE
+*               National Center for Biotechnology Information
+*
+*  This software/database is a "United States Government Work" under the
+*  terms of the United States Copyright Act.  It was written as part of
+*  the author's offical duties as a United States Government employee and
+*  thus cannot be copyrighted.  This software/database is freely available
+*  to the public for use. The National Library of Medicine and the U.S.
+*  Government have not placed any restriction on its use or reproduction.
+*
+*  Although all reasonable efforts have been taken to ensure the accuracy
+*  and reliability of the software and data, the NLM and the U.S.
+*  Government do not and cannot warrant the performance or results that
+*  may be obtained by using this software or data. The NLM and the U.S.
+*  Government disclaim all warranties, express or implied, including
+*  warranties of performance, merchantability or fitness for any particular
+*  purpose.
+*
+*  Please cite the author in any work or product based on this material.
+*
+* ===========================================================================*/
+
+/*****************************************************************************
+
+File name: greedy_align.h
+
+Author: Ilya Dondoshansky
+
+Contents: Copy of mbalign.h from ncbitools library
+
+******************************************************************************
+ * $Revision$
+ * */
+#ifndef _GREEDY_H_
+#define _GREEDY_H_
+
+#include <ncbi.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef Uint4 edit_op_t; /* 32 bits */
+typedef struct {
+    edit_op_t *op;                  /* array of edit operations */
+    Uint4 size, num;         /* size of allocation, number in use */
+    edit_op_t last;                 /* most recent operation added */
+} edit_script_t;
+
+edit_script_t *edit_script_free(edit_script_t *es);
+edit_script_t *edit_script_new(void);
+edit_script_t *edit_script_append(edit_script_t *es, edit_script_t *et);
+
+enum {
+    EDIT_OP_MASK = 0x3,
+    EDIT_OP_ERR  = 0x0,
+    EDIT_OP_INS  = 0x1,
+    EDIT_OP_DEL  = 0x2,
+    EDIT_OP_REP  = 0x3
+};
+
+enum {         /* half of the (fixed) match score */
+    ERROR_FRACTION=2,  /* 1/this */
+    MAX_SPACE=1000000,
+    sC = 0, sI = 1, sD = 2, LARGE=100000000
+};
+
+#define ICEIL(x,y) ((((x)-1)/(y))+1)
+
+/* ----- pool allocator ----- */
+typedef struct _three_val_ {
+    Int4 I, C, D;
+} ThreeVal, PNTR ThreeValPtr;
+
+typedef struct mb_space_struct {
+    ThreeValPtr space_array;
+    Int4 used, size;
+    struct mb_space_struct *next;
+} MBSpace, *MBSpacePtr;
+
+#define EDIT_VAL(op) (op >> 2)
+
+#define EDIT_OPC(op) (op & EDIT_OP_MASK)
+
+MBSpacePtr new_mb_space(void);
+void refresh_mb_space(MBSpacePtr sp);
+void free_mb_space(MBSpacePtr sp);
+ThreeValPtr get_mb_space(MBSpacePtr S, Int4 amount);
+
+typedef struct greedy_align_mem {
+   Int4Ptr PNTR flast_d;
+   Int4Ptr max_row_free;
+   ThreeValPtr PNTR flast_d_affine;
+   Int4Ptr uplow_free;
+   MBSpacePtr space;
+} GreedyAlignMem, PNTR GreedyAlignMemPtr;
+
+Int4 
+BLAST_GreedyAlign PROTO((const UcharPtr s1, Int4 len1,
+			     const UcharPtr s2, Int4 len2,
+			     Boolean reverse, Int4 xdrop_threshold, 
+			     Int4 match_cost, Int4 mismatch_cost,
+			     Int4Ptr e1, Int4Ptr e2, GreedyAlignMemPtr abmp, 
+			     edit_script_t *S, Uint1 rem));
+Int4 
+BLAST_AffineGreedyAlign PROTO((const UcharPtr s1, Int4 len1,
+				  const UcharPtr s2, Int4 len2,
+				  Boolean reverse, Int4 xdrop_threshold, 
+				  Int4 match_cost, Int4 mismatch_cost,
+				  Int4 gap_open, Int4 gap_extend,
+				  Int4Ptr e1, Int4Ptr e2, 
+				  GreedyAlignMemPtr abmp, 
+				  edit_script_t *S, Uint1 rem));
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* _GREEDY_H_ */
