90520c47b651a71043eb716a4cbe53756609891e
Aaron Ucko <ucko@ncbi.nlm.nih.gov>
2003-07-25 19:38:28 +0000

    Kill the executable upon receiving a signal, in an attempt to avoid having it stick around and eat CPU.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@10101 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objmgr/test/test_objmgr_loaders.sh
+++ src/objmgr/test/test_objmgr_loaders.sh
@@ -6,7 +6,7 @@ status_dir="../../../status"
 if test -f "$status_dir/Sybase.enabled"; then
     methods="PUBSEQOS ID1"
 else
-    echo Sybase is disabled: skipping PIBSEQOS loader test
+    echo Sybase is disabled: skipping PUBSEQOS loader test
     methods="ID1"
 fi
 
@@ -15,7 +15,10 @@ for method in $methods; do
     echo "Checking GenBank loader $method:"
     GENBANK_LOADER_METHOD="$method"
     export GENBANK_LOADER_METHOD
-    $*
+    "$@" &
+    pid=$!
+    trap 'kill $pid' 1 2 15
+    wait $pid > /dev/null 2>&1
     error=$?
     if test $error -ne 0; then
         echo "Test of GenBank loader $method failed: $error"
