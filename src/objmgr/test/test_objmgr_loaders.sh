6c9db11127235a62458a36f0ed5e620d7551ea69
Aaron Ucko <ucko@ncbi.nlm.nih.gov>
2003-07-26 16:02:19 +0000

    Exit immediately if the child seems to have been killed, even if it was the first test.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@10108 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objmgr/test/test_objmgr_loaders.sh
+++ src/objmgr/test/test_objmgr_loaders.sh
@@ -23,6 +23,10 @@ for method in $methods; do
     if test $error -ne 0; then
         echo "Test of GenBank loader $method failed: $error"
         exitcode=$error
+        case $error in
+            # signal 1 (HUP), 2 (INTR), 9 (KILL), or 15 (TERM).
+            129|130|137|143) echo "Apparently killed"; break ;;
+        esac
     fi
 done
 
