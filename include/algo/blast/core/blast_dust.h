f8c973318b279d627920999e3468790916e1f099
Ilya Dondoshansky <dondosha@ncbi.nlm.nih.gov>
2003-08-01 17:22:12 +0000

    Minor change in comment
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@10235 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/algo/blast/core/blast_dust.h
+++ include/algo/blast/core/blast_dust.h
@@ -44,7 +44,7 @@ extern "C" {
 #endif
 
 #include <blast_def.h>
-#include <blastkar.h>  /* Needed for ncbi4na_to_blastna definition only */
+#include <blastkar.h>  /* Needed for NCBI4NA_TO_BLASTNA definition only */
 
 Int2 SeqBufferDust (Uint1* sequence, Int4 length, Int4 offset,
                     Int2 level, Int2 window, Int2 minwin, Int2 linker,
