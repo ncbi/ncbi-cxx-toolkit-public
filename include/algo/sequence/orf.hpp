90ee6495b8a6ca90dcf36a12cd7b688d93848ae4
Michael DiCuccio <dicuccio@ncbi.nlm.nih.gov>
2003-08-15 12:17:38 +0000

    COmpilation fixes for MSVC
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@10450 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/algo/sequence/orf.hpp
+++ include/algo/sequence/orf.hpp
@@ -57,15 +57,106 @@ public:
     /// seq must be in ncbi8na
     template<class Seq>
     static void FindOrfs(const Seq& seq, vector<COrf>& results,
-                         unsigned int min_length_bp = 3);
+                         unsigned int min_length_bp = 3)
+    {
+        vector<TSeqPos> begins, ends;
+
+        // find ORFs on the forward sequence and report them as-is
+        FindForwardOrfs(seq, begins, ends, min_length_bp);
+        for (unsigned int i = 0;  i < begins.size();  i++) {
+            COrf orf(begins[i], ends[i], eNa_strand_plus);
+            results.push_back(orf);
+        }
+
+        // find ORFs on the complement and munge the numbers
+        begins.clear();
+        ends.clear();
+        Seq comp(seq);
+        CSeqMatch::CompNcbi8na(comp);
+        FindForwardOrfs(comp, begins, ends, min_length_bp);
+        for (unsigned int i = 0;  i < begins.size();  i++) {
+            COrf orf(comp.length() - ends[i] - 1,
+                     comp.length() - begins[i] - 1,
+                     eNa_strand_minus);
+            results.push_back(orf);
+        }
+    }
 
     /// Find all ORFs in forward orientation with
     /// length in *base pairs* >= min_length_bp.
     /// seq must be in ncbi8na.
     template<class Seq>
-    static void FindForwardOrfs(const Seq& seq, vector<TSeqPos>& from,
-                                vector<TSeqPos>& to,
-                                unsigned int min_length_bp = 3);
+    static void FindForwardOrfs(const Seq& seq, vector<TSeqPos>& begins,
+                                vector<TSeqPos>& ends,
+                                unsigned int min_length_bp = 3)
+    {
+        string scodons1, scodons2;
+        CSeqMatch::IupacToNcbi8na("TAR", scodons1);  // uaa and uag
+        CSeqMatch::IupacToNcbi8na("TRA", scodons2);  // uaa and uga
+    
+        // find only those positions that definitely match a stop
+        vector<TSeqPos> stops1, stops2, dummy, all_stops;
+        CSeqMatch::FindMatchesNcbi8na(seq, scodons1, stops1, dummy);
+        CSeqMatch::FindMatchesNcbi8na(seq, scodons2, stops2, dummy);
+        set_union(stops1.begin(), stops1.end(),
+                  stops2.begin(), stops2.end(),
+                  back_inserter(all_stops));
+
+        // now sort them out by reading frame
+        vector<vector<TSeqPos> > stops;
+        stops.resize(3);
+        for (unsigned int i = 0;  i < all_stops.size();  i++) {
+            stops[all_stops[i] % 3].push_back(all_stops[i]);
+        }
+
+        TSeqPos from, to;
+        // for each reading frame, calculate the orfs
+        for (int frame = 0;  frame < 3;  frame++) {
+
+            if (stops[frame].empty()) {
+                // no stops in this frame; the whole sequence,
+                // minus some scraps at the ends, is one ORF
+                from = frame;
+                // 'to' should be the largest index within the
+                // sequence length that gives an ORF length
+                // divisible by 3
+                to = ((seq.size() - from) / 3) * 3 + from - 1;
+                if (to - from + 1 >= min_length_bp) {
+                    begins.push_back(from);
+                    ends.push_back(to);
+                }
+                continue;  // we're done for this reading frame
+            }
+        
+            // deal specially with first ORF
+            from = frame;
+            to = stops[frame].front() - 1;
+            if (to - from + 1 >= min_length_bp) {
+                begins.push_back(from);
+                ends.push_back(to);
+            }
+
+            for (unsigned int i = 0;  i < stops[frame].size() - 1;  i++) {
+                from = stops[frame][i] + 3;
+                to = stops[frame][i + 1] - 1;
+                if (to - from + 1 >= min_length_bp) {
+                    begins.push_back(from);
+                    ends.push_back(to);
+                }
+            }
+        
+            // deal specially with last ORF
+            from = stops[frame].back() + 3;
+            // 'to' should be the largest index within the
+            // sequence length that gives an orf length
+            // divisible by 3
+            to = ((seq.size() - from) / 3) * 3 + from - 1;
+            if (to - from + 1 >= min_length_bp) {
+                begins.push_back(from);
+                ends.push_back(to);
+            }
+        }
+    }
 private:
     TSeqPos m_from;
     TSeqPos m_to;
@@ -73,107 +164,6 @@ private:
 };
 
 
-template<class Seq>
-void COrf::FindOrfs(const Seq& seq, vector<COrf>& results,
-                    unsigned int min_length_bp)
-{
-    vector<TSeqPos> begins, ends;
-
-    // find ORFs on the forward sequence and report them as-is
-    FindForwardOrfs(seq, begins, ends, min_length_bp);
-    for (unsigned int i = 0;  i < begins.size();  i++) {
-        COrf orf(begins[i], ends[i], eNa_strand_plus);
-        results.push_back(orf);
-    }
-
-    // find ORFs on the complement and munge the numbers
-    begins.clear();
-    ends.clear();
-    Seq comp(seq);
-    CSeqMatch::CompNcbi8na(comp);
-    FindForwardOrfs(comp, begins, ends, min_length_bp);
-    for (unsigned int i = 0;  i < begins.size();  i++) {
-        COrf orf(comp.length() - ends[i] - 1,
-                 comp.length() - begins[i] - 1,
-                 eNa_strand_minus);
-        results.push_back(orf);
-    }
-}
-
-
-template<class Seq>
-inline
-void COrf::FindForwardOrfs(const Seq& seq, vector<TSeqPos>& begins,
-                                vector<TSeqPos>& ends,
-                                unsigned int min_length_bp)
-{
-    string scodons1, scodons2;
-    CSeqMatch::IupacToNcbi8na("TAR", scodons1);  // uaa and uag
-    CSeqMatch::IupacToNcbi8na("TRA", scodons2);  // uaa and uga
-    
-    // find only those positions that definitely match a stop
-    vector<TSeqPos> stops1, stops2, dummy, all_stops;
-    CSeqMatch::FindMatchesNcbi8na(seq, scodons1, stops1, dummy);
-    CSeqMatch::FindMatchesNcbi8na(seq, scodons2, stops2, dummy);
-    set_union(stops1.begin(), stops1.end(),
-              stops2.begin(), stops2.end(),
-              back_inserter(all_stops));
-
-    // now sort them out by reading frame
-    vector<vector<TSeqPos> > stops;
-    stops.resize(3);
-    for (unsigned int i = 0;  i < all_stops.size();  i++) {
-        stops[all_stops[i] % 3].push_back(all_stops[i]);
-    }
-
-    TSeqPos from, to;
-    // for each reading frame, calculate the orfs
-    for (int frame = 0;  frame < 3;  frame++) {
-
-        if (stops[frame].empty()) {
-            // no stops in this frame; the whole sequence,
-            // minus some scraps at the ends, is one ORF
-            from = frame;
-            // 'to' should be the largest index within the
-            // sequence length that gives an ORF length
-            // divisible by 3
-            to = ((seq.size() - from) / 3) * 3 + from - 1;
-            if (to - from + 1 >= min_length_bp) {
-                begins.push_back(from);
-                ends.push_back(to);
-            }
-            continue;  // we're done for this reading frame
-        }
-        
-        // deal specially with first ORF
-        from = frame;
-        to = stops[frame].front() - 1;
-        if (to - from + 1 >= min_length_bp) {
-            begins.push_back(from);
-            ends.push_back(to);
-        }
-
-        for (unsigned int i = 0;  i < stops[frame].size() - 1;  i++) {
-            from = stops[frame][i] + 3;
-            to = stops[frame][i + 1] - 1;
-            if (to - from + 1 >= min_length_bp) {
-                begins.push_back(from);
-                ends.push_back(to);
-            }
-        }
-        
-        // deal specially with last ORF
-        from = stops[frame].back() + 3;
-        // 'to' should be the largest index within the
-        // sequence length that gives an orf length
-        // divisible by 3
-        to = ((seq.size() - from) / 3) * 3 + from - 1;
-        if (to - from + 1 >= min_length_bp) {
-            begins.push_back(from);
-            ends.push_back(to);
-        }
-    }
-}
 
 
 END_NCBI_SCOPE
@@ -181,6 +171,9 @@ END_NCBI_SCOPE
 /*
  * ===========================================================================
  * $Log$
+ * Revision 1.3  2003/08/15 12:17:38  dicuccio
+ * COmpilation fixes for MSVC
+ *
  * Revision 1.2  2003/08/14 21:18:20  ucko
  * Don't repeat default argument specifications in definitions.
  *
