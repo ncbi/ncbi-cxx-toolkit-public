05333f4d8a79abd70ba9622c68f99d86e1e019b9
Vladimir Ivanov <ivanov@ncbi.nlm.nih.gov>
2003-09-02 20:39:40 +0000

    Initial revision
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@10769 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ src/connect/test/Makefile.test_ncbi_pipe_connector.app
@@ -0,0 +1,9 @@
+# $Id$
+
+APP = test_ncbi_pipe_connector
+SRC = test_ncbi_pipe_connector
+LIB = xncbi xconnect
+
+#LIBS = $(NETWORK_LIBS) $(ORIG_LIBS) 
+
+CHECK_CMD =
---
+++ src/connect/test/test_ncbi_pipe_connector.cpp
@@ -0,0 +1,247 @@
+/*  $Id$
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Author:  Denis Vakatov, Anton Lavrentiev, Vladimir Ivanov
+ *
+ * File Description:
+ *   Standard test for the PIPE CONNECTOR
+ *
+ */
+
+#include <corelib/ncbiapp.hpp>
+#include <corelib/ncbienv.hpp>
+#include <corelib/ncbiargs.hpp>
+#include <corelib/ncbi_system.hpp>
+
+#include <connect/ncbi_pipe.hpp>
+#include <connect/ncbi_pipe_connector.hpp>
+#include "../ncbi_priv.h"
+#include <connect/ncbi_connection.h>
+
+#if defined(NCBI_OS_MSWIN)
+#  include <io.h>
+#elif defined(NCBI_OS_UNIX)
+#  include <unistd.h>
+#else
+#   error "Pipe tests configured for Windows and Unix only."
+#endif
+
+#include "test_assert.h"  // This header must go last
+
+
+USING_NCBI_SCOPE;
+
+
+const size_t   kBufferSize  = 1024;    // I/O buffer size
+const STimeout kTimeout     = {2, 0};  // I/O timeout
+
+
+////////////////////////////////
+// Auxiliary functions
+//
+
+// Delay a some time. Needs only for more proper output.
+static void Delay() 
+{
+    SleepMilliSec(300);
+}
+
+
+// Reading from file stream
+static string s_ReadFile(FILE* fs) 
+{
+    char buf[kBufferSize];
+    size_t cnt = read(fileno(fs), buf, kBufferSize-1);
+    buf[cnt] = 0;
+    string str = buf;
+    Delay();
+    cerr << "Read from file stream " << cnt << " bytes:" << endl;
+    cerr.write(buf, cnt);
+    cerr << endl;
+    return str;
+}
+
+
+// Writing to file stream
+static void s_WriteFile(FILE* fs, string message) 
+{
+    write(fileno(fs), message.c_str(), message.length());
+    cerr << "Wrote to file stream " << message.length() << " bytes:" << endl;
+    cerr << message << endl;
+}
+
+
+////////////////////////////////
+// Test application
+//
+
+class CTest : public CNcbiApplication
+{
+public:
+    virtual void Init(void);
+    virtual int  Run(void);
+};
+
+
+void CTest::Init(void)
+{
+    // Set error posting and tracing on maximum
+    SetDiagTrace(eDT_Enable);
+    SetDiagPostFlag(eDPF_All);
+    SetDiagPostLevel(eDiag_Info);
+
+    // Log and data log streams
+    CORE_SetLOGFILE(stderr, 0/*false*/);
+}
+
+
+int CTest::Run(void)
+{
+    CONN        conn;
+    CONNECTOR   connector;
+    char        buf[kBufferSize];
+    size_t      n_read    = 0;
+    size_t      n_written = 0;
+    string      message;
+    EIO_Status  status;
+
+    // Run the test
+    LOG_POST("Starting the PIPE CONNECTOR test...\n");
+
+    // Initialization of variables and structures
+    const string app = GetArguments().GetProgramName();
+    string str;
+    vector<string> args;
+
+    // Pipe connector for reading
+#if defined(NCBI_OS_UNIX)
+    args.push_back("-l");
+    connector = PIPE_CreateConnector("ls", args, CPipe::fStdIn_Close);
+    Delay();
+#elif defined (NCBI_OS_MSWIN)
+    string cmd = GetEnvironment().Get("COMSPEC");
+    args.push_back("/c");
+    args.push_back("dir *.*");
+    connector = PIPE_CreateConnector(cmd, args, CPipe::fStdIn_Close);
+#endif
+    assert(connector);
+    assert(CONN_Create(connector, &conn) == eIO_Success);
+    CONN_SetTimeout(conn, eIO_Read,  &kTimeout);
+    CONN_SetTimeout(conn, eIO_Close, &kTimeout);
+    Delay();
+    status = CONN_Read(conn, buf, kBufferSize, &n_read, eIO_ReadPersist);
+    NcbiCout.write(buf, n_read);
+    NcbiCout << endl;
+    NcbiCout.flush();
+    assert(status == eIO_Success  ||  status == eIO_Closed);
+    assert(n_read > 0);
+    assert(CONN_Close(conn) == eIO_Success);
+
+    // Pipe connector for writing
+    args.clear();
+    args.push_back("one");
+    connector = PIPE_CreateConnector(app, args, CPipe::fStdOut_Close);
+    assert(connector);
+    assert(CONN_Create(connector, &conn) == eIO_Success);
+    CONN_SetTimeout(conn, eIO_Write, &kTimeout);
+    CONN_SetTimeout(conn, eIO_Close, &kTimeout);
+    Delay();
+    message = "Child, are you ready?";
+    status = CONN_Write(conn, message.c_str(), message.length(), &n_written);
+    assert(status == eIO_Success);
+    assert(n_written == message.length());
+    assert(CONN_Close(conn) == eIO_Success);
+   
+    // Bidirectional pipe
+    args.clear();
+    args.push_back("one");
+    args.push_back("two");
+    connector = PIPE_CreateConnector(app, args);
+    assert(connector);
+    assert(CONN_Create(connector, &conn) == eIO_Success);
+    CONN_SetTimeout(conn, eIO_ReadWrite, &kTimeout);
+    CONN_SetTimeout(conn, eIO_Close,     &kTimeout);
+    Delay();
+    message = "Child, are you ready again?";
+    status = CONN_Write(conn, message.c_str(), message.length(), &n_written);
+    assert(status == eIO_Success);
+    assert(n_written == message.length());
+    message = "Ok. Test 2 running.";
+    status = CONN_Read(conn, buf, kBufferSize, &n_read, eIO_ReadPlain);
+    assert(status == eIO_Success);
+    assert(n_read == message.length());
+    assert(memcmp(buf, message.c_str(), n_read) == 0);
+    assert(CONN_Close(conn) == eIO_Success);
+
+    LOG_POST("\nTEST execution completed successfully!\n");
+    CORE_SetLOG(0);
+
+    return 0;
+}
+
+
+///////////////////////////////////
+// APPLICATION OBJECT  and  MAIN
+//
+
+int main(int argc, const char* argv[])
+{
+    // Invalid arguments
+    if (argc > 3) {
+        exit(1);
+    }
+
+    // Spawned process for unidirectional test
+    if (argc == 2) {
+        cerr << endl << "--- CPipe unidirectional test ---" << endl;
+        string command = s_ReadFile(stdin);
+        _TRACE("read back >>" << command << "<<");
+        assert(command == "Child, are you ready?");
+        NcbiCout << "Ok. Test 1 running." << endl;
+        exit(0);
+    }
+
+    // Spawned process for bidirectional test (direct from pipe)
+    if (argc == 3) {
+        cerr << endl << "--- CPipe bidirectional test (pipe) ---" << endl;
+        string command = s_ReadFile(stdin);
+        assert(command == "Child, are you ready again?");
+        s_WriteFile(stdout, "Ok. Test 2 running.");
+        exit(0);
+    }
+
+    // Execute main application function
+    return CTest().AppMain(argc, argv, 0, eDS_Default, 0);
+}
+
+
+/*
+ * ===========================================================================
+ * $Log$
+ * Revision 1.1  2003/09/02 20:39:40  ivanov
+ * Initial revision
+ *
+ * ===========================================================================
+ */
