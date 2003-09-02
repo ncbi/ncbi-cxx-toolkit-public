6e8e153dda1e511f9ed8920f65b46fbd56d1e030
Vladimir Ivanov <ivanov@ncbi.nlm.nih.gov>
2003-09-02 20:25:29 +0000

    Initial revision
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@10764 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ include/connect/ncbi_pipe_connector.hpp
@@ -0,0 +1,81 @@
+#ifndef CONNECT___NCBI_PIPE_CONNECTOR__HPP
+#define CONNECT___NCBI_PIPE_CONNECTOR__HPP
+
+/*  $Id$
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Author:  Vladimir Ivanov
+ *
+ *
+ */
+
+/// @file ncbi_pipe_connector.hpp
+/// Implement CONNECTOR for a pipe interprocess communication
+/// (based on the NCBI CPipe).
+///
+/// See in "connectr.h" for the detailed specification of the underlying
+/// connector("CONNECTOR", "SConnectorTag") methods and structures.
+
+
+#include <connect/ncbi_connector.h>
+#include <connect/ncbi_pipe.hpp>
+
+
+/** @addtogroup Connectors
+ *
+ * @{
+ */
+
+
+BEGIN_NCBI_SCOPE
+
+
+/// Create CPipe-based CONNECTOR.
+///
+/// Create new CONNECTOR structure to handle a data transfer between two
+/// process over interprocess pipe. Return NULL on error.
+extern NCBI_XCONNECT_EXPORT CONNECTOR PIPE_CreateConnector
+(const string&         cmd,
+ const vector<string>& args,
+ CPipe::TCreateFlags   create_flags = 0
+);
+
+
+END_NCBI_SCOPE
+
+
+/* @} */
+
+
+/*
+ * ===========================================================================
+ * $Log$
+ * Revision 1.1  2003/09/02 20:25:29  ivanov
+ * Initial revision
+ *
+ * ==========================================================================
+ */
+
+#endif /* CONNECT___NCBI_PIPE_CONNECTOR__HPP */
