1c5a099e74e81667fe9b110604f3ed47867c2065
Joshua (Josh) Cherry <jcherry@ncbi.nlm.nih.gov>
2003-09-12 15:05:08 +0000

    Removed inappropriate CAntigenic:: in class body
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@11004 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/algo/sequence/antigenic.hpp
+++ include/algo/sequence/antigenic.hpp
@@ -63,7 +63,7 @@ public:
                              unsigned int min_len = 6);
 
 private:
-    static const double CAntigenic::sm_Pa_table[26];
+    static const double sm_Pa_table[26];
 
     template<class Seq>
     friend void x_PredictAGSites(const Seq& seq, TLocVec& results,
@@ -79,6 +79,9 @@ END_NCBI_SCOPE
 /*
  * ===========================================================================
  * $Log$
+ * Revision 1.4  2003/09/12 15:05:08  jcherry
+ * Removed inappropriate CAntigenic:: in class body
+ *
  * Revision 1.3  2003/09/10 16:20:35  ucko
  * Tweak to avoid undefined symbols with WorkShop.
  *
