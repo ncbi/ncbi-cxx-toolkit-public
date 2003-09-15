5f83a035074bd7edc17fcf7477090caa713f91da
kapustin <kapustin@ncbi.nlm.nih.gov>
2003-09-15 21:32:03 +0000

    Minor code cleanup
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@11034 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/algo/align/nw_formatter.cpp
+++ src/algo/align/nw_formatter.cpp
@@ -54,21 +54,19 @@ void CNWFormatter::AsSeqAlign(CSeq_align* seqalign) const
     if(seqalign == 0) {
         NCBI_THROW(CAlgoAlignException,
                    eBadParameter,
-                   "Zero pointer passed to CNWFormatter::AsSeqAlign()");
+                   "Invalid address specified");
     }
     
     const vector<CNWAligner::ETranscriptSymbol>& transcript = 
         *(m_aligner->GetTranscript());
 
-    if(!transcript.size()) {
+    if(transcript.size() == 0) {
         NCBI_THROW(CAlgoAlignException,
                    eNoData,
                    "Zero size transcript (forgot to run the aligner?)");
     }
 
     seqalign->Reset();
-
-    if(transcript.size() == 0) return;
 
     // the alignment is pairwise
     seqalign->SetDim(2);
@@ -178,12 +176,11 @@ void CNWFormatter::AsText(string* output, ETextFormatType type,
     const vector<CNWAligner::ETranscriptSymbol>& transcript =
         *(m_aligner->GetTranscript());
 
-    if(!transcript.size()) {
+    if(transcript.size() == 0) {
         NCBI_THROW(CAlgoAlignException,
                    eNoData,
                    "Zero size transcript (forgot to run the aligner?)");
     }
-
 
     switch (type) {
 
@@ -472,6 +469,9 @@ END_NCBI_SCOPE
 /*
  * ===========================================================================
  * $Log$
+ * Revision 1.4  2003/09/15 21:32:03  kapustin
+ * Minor code cleanup
+ *
  * Revision 1.3  2003/09/12 19:43:04  kapustin
  * Add checking for empty transcript
  *
