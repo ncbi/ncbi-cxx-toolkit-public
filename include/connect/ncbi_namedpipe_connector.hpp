f57eb66cc8480a554b48cd5bd5248bcea8a82655
Anton Lavrentiev <lavr@ncbi.nlm.nih.gov>
2003-09-23 21:02:21 +0000

    Rename BufferSize into PipeSize; accept string instead of char*
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@11137 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/connect/ncbi_namedpipe_connector.hpp
+++ include/connect/ncbi_namedpipe_connector.hpp
@@ -60,10 +60,10 @@ BEGIN_NCBI_SCOPE
 /// Create CNamedPipe-based CONNECTOR.
 ///
 /// Create new CONNECTOR structure to handle a data transfer between two
-/// process over nemed pipe. Return NULL on error.
+/// process over nemed pipe.  Return NULL on error.
 extern NCBI_XCONNECT_EXPORT CONNECTOR NAMEDPIPE_CreateConnector
-(const char*  pipename,
- size_t       bufsize = CNamedPipe::kDefaultBufferSize
+(const string& pipename,
+ size_t        pipesize = CNamedPipe::kDefaultPipeSize
  );
 
 
@@ -76,6 +76,9 @@ END_NCBI_SCOPE
 /*
  * ===========================================================================
  * $Log$
+ * Revision 1.4  2003/09/23 21:02:21  lavr
+ * Rename BufferSize into PipeSize; accept string instead of char*
+ *
  * Revision 1.3  2003/09/03 13:56:11  ivanov
  * Renamed to .hpp
  *
