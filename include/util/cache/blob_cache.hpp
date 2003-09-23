843ebaec9f9a95386ed2cc20a0e9c3affbce8afc
Anatoliy Kuznetsov <kuznets@ncbi.nlm.nih.gov>
2003-09-23 16:20:31 +0000

    "Doxygenized" header comments.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@11123 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/util/cache/blob_cache.hpp
+++ include/util/cache/blob_cache.hpp
@@ -32,18 +32,28 @@
  *
  */
 
+/// @file blob_cache.hpp
+/// BLOB cache interface specs. 
+///
+/// File describes interfaces used to create local cache of remote
+/// binary large objects (BLOBS).
+
+
 #include <util/reader_writer.hpp>
+#include <string>
 
 
 BEGIN_NCBI_SCOPE
 
 
 
-//////////////////////////////////////////////////////////////////
-//
-// BLOB cache read/write/maintenance interface.
-//
-
+/// BLOB cache read/write/maintanance interface.
+///
+/// IBLOB_Cache describes caching service. Any large binary object
+/// can be stored in cache and later retrived. Such cache is a 
+/// temporary storage and some objects can be purged from cache based
+/// on an immediate request, version or access time 
+/// based replacement (or another implementation specific depreciation rule).
 class IBLOB_Cache
 {
 public:
@@ -69,67 +79,112 @@ public:
         eDropAll    ///< Drop all versions last accessed before "access time"
     };
 
-    // Add or replace BLOB
-    // flag parameter specifies our approach to old versions of the same BLOB
-    // (keep or drop)
+    /// Add or replace BLOB
+    ///
+    /// @param key BLOB identification key
+    /// @param version BLOB version
+    /// @param data pointer on data buffer
+    /// @param size data buffer size
+    /// @param flag indicator to keep old BLOBs or drop it from the cache
     virtual void Store(const string& key,
                        int           version,
                        const void*   data,
                        size_t        size,
                        EKeepVersions keep_versions = eDropOlder) = 0;
 
-    // Check if BLOB exists, return non zero size.
-    // 0 value indicates that BLOB does not exist in the cache
+    /// Check if BLOB exists, return BLOB size.
+    ///
+    /// @param key - BLOB identification key
+    /// @param version - BLOB version
+    /// @return BLOB size or 0 if it doesn't exist
     virtual size_t GetSize(const string& key,
                            int           version) = 0;
 
-    // Fetch the BLOB
-    // Function returns FALSE if BLOB does not exists.
-    // Throws an exception if provided memory is insufficient to read the BLOB
-    virtual bool Read(const string& key,
-                      int           version,
-                      void*         buf,
+    /// Fetch the BLOB
+    ///
+    /// @param key BLOB identification key
+    /// @param version BLOB version
+    /// @param buf pointer on destination buffer
+    /// @param size buffer size
+    /// @return FALSE if BLOB doesn't exist
+    ///
+    /// @note Throws an exception if provided memory buffer is insufficient 
+    /// to read the BLOB
+    virtual bool Read(const string& key, 
+                      int           version, 
+                      void*         buf, 
                       size_t        buf_size) = 0;
 
-    // Return sequential stream interface to read BLOB data.
-    // Function returns NULL if BLOB does not exist
-    virtual IReader* GetReadStream(const string& key,
-                                   int           version) = 0;
+    /// Return sequential stream interface to read BLOB data.
+    /// 
+    /// @param key BLOB identification key
+    /// @param version BLOB version
+    /// @return Interface pointer or NULL if BLOB does not exist
+    virtual IReader* GetReadStream(const string& key, 
+                                   int   version) = 0;
 
-    // Return sequential stream interface to write BLOB data.
-    virtual IWriter* GetWriteStream(const string& key,
-                                    int           version,
-                                    EKeepVersions keep_versions = eDropOlder)
-        = 0;
+    /// Return sequential stream interface to write BLOB data.
+    ///
+    /// @param key BLOB identification key
+    /// @param version BLOB version
+    /// @param flag indicator to keep old BLOBs or drop it from the cache
+    /// @return Interface pointer or NULL if BLOB does not exist
+    virtual IWriter* GetWriteStream(const string&    key, 
+                                    int              version,
+                                    EKeepVersions    keep_versions = eDropOlder) = 0;
 
-    // Remove all versions of the specified BLOB
+    /// Remove all versions of the specified BLOB
+    ///
+    /// @param key BLOB identification key
     virtual void Remove(const string& key) = 0;
 
-    // Return access time for the specified BLOB
+    /// Return last access time for the specified BLOB
+    ///
+    /// Class implementation may want to implement access time based
+    /// aging scheme for cache managed objects. In this case it needs to
+    /// track time of every request to BLOB data.
+    ///
+    /// @param key BLOB identification key
+    /// @param version BLOB version
+    /// @return last access time
     virtual time_t GetAccessTime(const string& key,
                                  int           version) = 0;
 
-    // Delete all BLOBs with access time older than specified
+    /// Delete all BLOBs with access time older than specified
+    ///
+    /// @param access_time time point, indicates objects 
+    /// newer than that which to keep in cache
+    /// @param keep_last_version type of cleaning action
     virtual void Purge(time_t           access_time,
                        EKeepLastVersion keep_last_version = eDropAll) = 0;
-
-    // Delete all versions of the given BLOB with the access time earlier
-    // than specified
+    /// Delete BLOBs with access time older than specified
+    /// 
+    /// Function finds all BLOB versions with the specified key
+    /// and removes the old instances.
+    /// @param key - BLOB key
+    /// @param access_time time point, indicates objects 
+    /// newer than that which to keep in cache
+    /// @param keep_last_version type of cleaning action
     virtual void Purge(const string&    key,
                        time_t           access_time,
                        EKeepLastVersion keep_last_version = eDropAll) = 0;
+
 
     virtual ~IBLOB_Cache() {}
 };
 
 
-END_NCBI_SCOPE
+    virtual ~IBLOB_Cache(){}
 
+END_NCBI_SCOPE
 
 
 /*
  * ===========================================================================
  * $Log$
+ * Revision 1.5  2003/09/23 16:20:31  kuznets
+ * "Doxygenized" header comments.
+ *
  * Revision 1.4  2003/09/22 22:42:14  vakatov
  * Extended EKeepVersions.
  * + EKeepLastVersion -- for Purge().
