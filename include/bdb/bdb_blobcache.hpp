c86a6bd8f1cf0107aef918a5640182ae70898710
Anatoliy Kuznetsov <kuznets@ncbi.nlm.nih.gov>
2003-09-24 14:30:17 +0000

    Initial revision
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@11156 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ include/bdb/bdb_blobcache.hpp
@@ -0,0 +1,153 @@
+#ifndef BDB__BLOBCACHE__HPP
+#define BDB__BLOBCACHE__HPP
+
+/*  $Id$
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Authors:  Anatoliy Kuznetsov
+ *
+ * File Description: BerkeleyDB based local BLOB cache. 
+ *
+ */
+
+/// @file bdb_blobcache.hpp
+/// IBLOB_Cache interface implemented on top of Berkeley DB
+
+
+#include <util/cache/blob_cache.hpp>
+#include <bdb/bdb_file.hpp>
+#include <bdb/bdb_blob.hpp>
+#include <bdb/bdb_env.hpp>
+
+BEGIN_NCBI_SCOPE
+
+
+/// BLOB storage file structure
+
+struct NCBI_BDB_EXPORT SBLOB_CacheDB : public CBDB_BLobFile
+{
+    CBDB_FieldString       key;
+    CBDB_FieldInt4         version;
+
+    SBLOB_CacheDB()
+    {
+        BindKey("key",     &key);
+        BindKey("version", &version);
+    }
+};
+
+
+/// BLOB access time file structure
+
+struct NCBI_BDB_EXPORT SBLOB_Cache_AccessTimeDB : public CBDB_File
+{
+    CBDB_FieldString       key;
+    CBDB_FieldInt4         version;
+    CBDB_FieldInt4         time_stamp;
+
+    SBLOB_Cache_AccessTimeDB()
+    {
+        BindKey("key",     &key);
+        BindKey("version", &version);
+
+        BindData("time_stamp", &time_stamp);
+    }
+};
+
+
+
+
+/// BDB cache implementation.
+///
+/// Class implements IBLOB_Cache interface using local Berkeley DB
+/// database.
+class NCBI_BDB_EXPORT BDB_BLOB_Cache : public IBLOB_Cache
+{
+public:
+    BDB_BLOB_Cache();
+    virtual ~BDB_BLOB_Cache();
+
+    void Open(const char* cache_path);
+
+
+    // IBLOB_Cache interface
+
+    virtual void Store(const string& key,
+                       int           version,
+                       const void*   data,
+                       size_t        size,
+                       EKeepVersions keep_versions = eDropOlder);
+
+    virtual size_t GetSize(const string& key,
+                           int           version);
+
+    virtual bool Read(const string& key, 
+                      int           version, 
+                      void*         buf, 
+                      size_t        buf_size) = 0;
+
+    virtual IReader* GetReadStream(const string& key, 
+                                   int   version) = 0;
+
+    virtual IWriter* GetWriteStream(const string&    key, 
+                                    int              version,
+                                    EKeepVersions    keep_versions = eDropOlder);
+
+    virtual void Remove(const string& key);
+
+    virtual time_t GetAccessTime(const string& key,
+                                 int           version);
+
+    virtual void Purge(time_t           access_time,
+                       EKeepVersions keep_last_version = eDropAll);
+
+    virtual void Purge(const string&    key,
+                       time_t           access_time,
+                       EKeepVersions keep_last_version = eDropAll);
+private:
+    void x_UpdateAccessTime(const string&    key,
+                            int              version);
+
+    void x_DropBLOB(const char*    key,
+                    int            version);
+private:
+    CBDB_Env                   m_Env;
+    SBLOB_CacheDB              m_BlobDB;
+    SBLOB_Cache_AccessTimeDB   m_TimeDB;
+};
+
+END_NCBI_SCOPE
+
+/*
+ * ===========================================================================
+ * $Log$
+ * Revision 1.1  2003/09/24 14:29:42  kuznets
+ * Initial revision
+ *
+ *
+ * ===========================================================================
+ */
+
+#endif
---
+++ src/bdb/bdb_blobcache.cpp
@@ -0,0 +1,366 @@
+/*  $Id$
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Author: Anatoliy Kuznetsov
+ *
+ * File Description:  BDB libarary BLOB cache implementation.
+ *
+ */
+
+#include <corelib/ncbitime.hpp>
+#include <bdb/bdb_blobcache.hpp>
+#include <bdb/bdb_cursor.hpp>
+#include <corelib/ncbimtx.hpp>
+
+
+BEGIN_NCBI_SCOPE
+
+// Mutex to sync cache requests coming from different threads
+// All requests are protected with one mutex
+static CFastMutex x_BDB_BLOB_CacheMutex;
+
+
+class CBDB_BLOB_CacheIReader : public IReader
+{
+public:
+
+    CBDB_BLOB_CacheIReader(CBDB_BLobStream* blob_stream)
+    : m_BlobStream(blob_stream)
+    {}
+
+    virtual ~CBDB_BLOB_CacheIReader()
+    {
+        delete m_BlobStream;
+    }
+
+    virtual ssize_t Read(void* buf, size_t buf_size)
+    {
+        if (buf_size == 0)
+            return 0;
+
+        size_t bytes_read;
+
+        {{
+        CFastMutexGuard guard(x_BDB_BLOB_CacheMutex);
+        m_BlobStream->Read(buf, buf_size, &bytes_read);
+        }}
+
+        return bytes_read;
+    }
+private:
+    CBDB_BLOB_CacheIReader(const CBDB_BLOB_CacheIReader&);
+    CBDB_BLOB_CacheIReader& operator=(const CBDB_BLOB_CacheIReader&);
+
+private:
+    CBDB_BLobStream* m_BlobStream;
+};
+
+
+class CBDB_BLOB_CacheIWriter : public IWriter
+{
+public:
+
+    CBDB_BLOB_CacheIWriter(CBDB_BLobStream* blob_stream)
+    : m_BlobStream(blob_stream)
+    {}
+
+    virtual ~CBDB_BLOB_CacheIWriter()
+    {
+        delete m_BlobStream;
+    }
+
+    virtual ssize_t Write(const void* buf, size_t buf_size)
+    {
+        if (buf_size == 0)
+            return 0;
+
+        {{
+        CFastMutexGuard guard(x_BDB_BLOB_CacheMutex);
+        m_BlobStream->Write(buf, buf_size);
+        }}
+        return (ssize_t)buf_size;
+    }
+
+private:
+    CBDB_BLOB_CacheIWriter(const CBDB_BLOB_CacheIWriter&);
+    CBDB_BLOB_CacheIWriter& operator=(const CBDB_BLOB_CacheIWriter&);
+
+private:
+    CBDB_BLobStream* m_BlobStream;
+};
+
+
+
+
+
+BDB_BLOB_Cache::BDB_BLOB_Cache()
+{
+}
+
+BDB_BLOB_Cache::~BDB_BLOB_Cache()
+{
+}
+
+void BDB_BLOB_Cache::Open(const char* path)
+{
+    CFastMutexGuard guard(x_BDB_BLOB_CacheMutex);
+
+    m_Env.OpenWithLocks(path);
+    m_BlobDB.SetEnv(m_Env);
+    m_TimeDB.SetEnv(m_Env);
+
+    m_BlobDB.Open("lc_blob.db", CBDB_RawFile::eReadWriteCreate);
+    m_TimeDB.Open("lc_time.db", CBDB_RawFile::eReadWriteCreate);
+}
+
+void BDB_BLOB_Cache::Store(const string& key,
+                           int           version,
+                           const void*   data,
+                           size_t        size,
+                           EKeepVersions keep_versions)
+{
+    {{
+
+    CFastMutexGuard guard(x_BDB_BLOB_CacheMutex);
+
+    m_BlobDB.key = key;
+    m_BlobDB.version = version;
+
+    if (m_BlobDB.Fetch() == eBDB_Ok) {   // BLOB exists, nothing to do
+        return;
+    }
+
+    }}
+
+    if (keep_versions == eDropAll || keep_versions == eDropOlder) {
+        Purge(key, 0, keep_versions);
+    }
+
+
+    CFastMutexGuard guard(x_BDB_BLOB_CacheMutex);
+
+    m_BlobDB.key = key;
+    m_BlobDB.version = version;
+
+    m_BlobDB.Insert(data, size);
+
+    x_UpdateAccessTime(key, version);
+
+    m_TimeDB.key = key;
+    m_TimeDB.version = version;
+
+    CTime time_stamp(CTime::eCurrent);
+    m_TimeDB.time_stamp = time_stamp.GetTimeT();
+
+    m_TimeDB.UpdateInsert();
+}
+
+
+size_t BDB_BLOB_Cache::GetSize(const string& key,
+                               int           version)
+{
+    CFastMutexGuard guard(x_BDB_BLOB_CacheMutex);
+
+    m_BlobDB.key = key;
+    m_BlobDB.version = version;
+    EBDB_ErrCode ret = m_BlobDB.Fetch();
+
+    if (ret != eBDB_Ok) {
+        return 0;
+    }
+    return m_BlobDB.LobSize();
+}
+
+
+bool BDB_BLOB_Cache::Read(const string& key, 
+                          int           version, 
+                          void*         buf, 
+                          size_t        buf_size)
+{
+    CFastMutexGuard guard(x_BDB_BLOB_CacheMutex);
+
+    m_BlobDB.key = key;
+    m_BlobDB.version = version;
+    EBDB_ErrCode ret = m_BlobDB.Fetch();
+    if (ret != eBDB_Ok) {
+        return false;
+    }
+    ret = m_BlobDB.GetData(buf, buf_size);
+    if (ret != eBDB_Ok) {
+        return false;
+    }
+
+    x_UpdateAccessTime(key, version);
+
+    return true;
+}
+
+
+void BDB_BLOB_Cache::Remove(const string& key)
+{
+    Purge(key, 0, eDropAll);
+}
+
+
+void BDB_BLOB_Cache::x_UpdateAccessTime(const string&  key,
+                                        int            version)
+{
+    m_TimeDB.key = key;
+    m_TimeDB.version = version;
+
+    CTime time_stamp(CTime::eCurrent);
+    m_TimeDB.time_stamp = time_stamp.GetTimeT();
+
+    m_TimeDB.UpdateInsert();
+}
+
+
+time_t BDB_BLOB_Cache::GetAccessTime(const string& key,
+                                     int           version)
+{
+    CFastMutexGuard guard(x_BDB_BLOB_CacheMutex);
+
+    m_TimeDB.key = key;
+    m_TimeDB.version = version;
+
+    EBDB_ErrCode ret = m_TimeDB.Fetch();
+    if (ret == eBDB_Ok) {
+        time_t t = m_TimeDB.time_stamp;
+        return t;
+    }
+
+    return 0;
+}
+
+void BDB_BLOB_Cache::Purge(time_t           access_time,
+                           EKeepVersions    keep_last_version)
+{
+    CFastMutexGuard guard(x_BDB_BLOB_CacheMutex);
+
+    if (keep_last_version == eDropAll || access_time == 0) {
+        m_BlobDB.Truncate();
+        m_TimeDB.Truncate();
+        return;
+    }
+
+    // Search the records
+
+    CBDB_FileCursor cur(m_TimeDB);
+    cur.SetCondition(CBDB_FileCursor::eFirst);
+
+    while (cur.Fetch() == eBDB_Ok) {
+        time_t t = m_TimeDB.time_stamp;
+        int version = m_TimeDB.version;
+        const char* key = m_TimeDB.key;
+
+        if (t < access_time) {
+            x_DropBLOB(key, version);
+        }
+    }
+}
+
+void BDB_BLOB_Cache::Purge(const string&    key,
+                           time_t           access_time,
+                           EKeepVersions    keep_last_version)
+{
+    CFastMutexGuard guard(x_BDB_BLOB_CacheMutex);
+
+    // Search the records
+
+    CBDB_FileCursor cur(m_TimeDB);
+    cur.SetCondition(CBDB_FileCursor::eEQ);
+
+    cur.From << key;
+
+    while (cur.Fetch() == eBDB_Ok) {
+        time_t t = m_TimeDB.time_stamp;
+
+        const char* key_str = m_TimeDB.key;
+        int version = m_TimeDB.version;
+
+        if (access_time == 0 || 
+            keep_last_version == eDropAll ||
+            t < access_time) {
+            x_DropBLOB(key_str, version);
+        }
+    }    
+}
+
+
+IReader* BDB_BLOB_Cache::GetReadStream(const string& key, 
+                                       int   version)
+{
+    CFastMutexGuard guard(x_BDB_BLOB_CacheMutex);
+
+    m_BlobDB.key = key;
+    m_BlobDB.version = version;
+    
+    EBDB_ErrCode ret = m_BlobDB.Fetch();
+    if (ret != eBDB_Ok) {
+        return 0;
+    }
+    CBDB_BLobStream* bstream = m_BlobDB.CreateStream();
+    return new CBDB_BLOB_CacheIReader(bstream);
+
+}
+
+IWriter* BDB_BLOB_Cache::GetWriteStream(const string&    key, 
+                                        int              version,
+                                        EKeepVersions    keep_versions)
+{
+    if (keep_versions == eDropAll || keep_versions == eDropOlder) {
+        Purge(key, 0, keep_versions);
+    }
+
+    CFastMutexGuard guard(x_BDB_BLOB_CacheMutex);
+
+    m_BlobDB.key = key;
+    m_BlobDB.version = version;
+
+    CBDB_BLobStream* bstream = m_BlobDB.CreateStream();
+    return new CBDB_BLOB_CacheIWriter(bstream);
+}
+
+void BDB_BLOB_Cache::x_DropBLOB(const char*    key,
+                                int            version)
+{
+    m_BlobDB.key = key;
+    m_BlobDB.version = version;
+
+    m_BlobDB.Delete();
+}
+
+
+END_NCBI_SCOPE
+
+/*
+ * ===========================================================================
+ * $Log$
+ * Revision 1.1  2003/09/24 14:30:17  kuznets
+ * Initial revision
+ *
+ *
+ * ===========================================================================
+ */
