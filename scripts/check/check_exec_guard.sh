99bdefcb6abde52aa8bcce955ac2ae0921f2e3c0
Vladimir Ivanov <ivanov@ncbi.nlm.nih.gov>
2003-09-29 17:58:32 +0000

    Allow to wait a timeout less than sleep_time
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@11272 78c7ea69-d796-4a43-9a09-de51944f1b03

--- scripts/check/check_exec_guard.sh
+++ scripts/check/check_exec_guard.sh
@@ -23,16 +23,15 @@
 # Parameters
 timeout=$1
 pid=$2
-
-# Recalculate timeout 
 sleep_time=5
-timeout="`expr $timeout / $sleep_time`"
 
-# Wait ending of process with "pid" or the specified time maximum
-time=0
-while [ $time -lt $timeout ]; do
+# Wait 
+while [ $timeout -gt 0 ]; do
   kill -0 $pid > /dev/null 2>&1  ||  exit 0
-  time="`expr $time + 1`"
+  if [ $timeout -lt  $sleep_time ]; then
+    sleep_time=$timeout
+  fi
+  timeout="`expr $timeout - $sleep_time`"
   sleep $sleep_time
 done
 
