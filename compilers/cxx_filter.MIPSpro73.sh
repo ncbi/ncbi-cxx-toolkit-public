8459db5929502c653ecfb53b57554c745c02cc69
Aaron Ucko <ucko@ncbi.nlm.nih.gov>
2003-10-01 00:44:16 +0000

    Trivial filter to screen out remarks and warnings from system headers.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@11330 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ compilers/cxx_filter.MIPSpro73.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+awk 'BEGIN { keep=1 }
+/^[^ ]/ { keep=1 }
+/^cc-[0-9]+ CC: (REMARK|WARNING) File = \/usr\/include/ { keep=0 }
+keep
+
+BEGIN { status=0 }
+/^cc-[0-9]+ CC: ERROR/ { status=1 }
+END { exit status }'
