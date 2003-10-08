04865658777221e985e8fd059ecd187e8a81168c
shomrat <shomrat@ncbi.nlm.nih.gov>
2003-10-08 13:36:41 +0000

    Initial version
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@11430 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ src/util/sequtil/sequtil_convert.cpp
@@ -0,0 +1,220 @@
+/*  $Id$
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Author:  Mati Shomrat
+ *
+ * File Description:
+ *      Sequence conversion utility.
+ */   
+#include <corelib/ncbistd.hpp>
+#include <corelib/ncbistr.hpp>
+#include <vector>
+
+#include <util/sequtil/sequtil_convert.hpp>
+#include "sequtil_convert_imp.hpp"
+
+
+BEGIN_NCBI_SCOPE
+
+
+//  -- Conversion methods
+
+// string to string
+SIZE_TYPE CSeqConvert::Convert
+(const string& src,
+ TCoding src_coding,
+ TSeqPos pos,
+ TSeqPos length,
+ string& dst, 
+ TCoding dst_coding)
+{
+    return CSeqConvert_imp::Convert(src, src_coding,
+                                    pos, length,
+                                    dst, dst_coding);
+}
+
+
+// string to vector
+SIZE_TYPE CSeqConvert::Convert
+(const string& src,
+ TCoding src_coding,
+ TSeqPos pos,
+ TSeqPos length,
+ vector< char >& dst,
+ TCoding dst_coding)
+{
+    return CSeqConvert_imp::Convert(src, src_coding,
+                                    pos, length,
+                                    dst, dst_coding);
+}
+
+
+// vector to string
+SIZE_TYPE CSeqConvert::Convert
+(const vector< char >& src,
+ TCoding src_coding,
+ TSeqPos pos,
+ TSeqPos length,
+ string& dst,
+ TCoding dst_coding)
+{
+    return CSeqConvert_imp::Convert(src, src_coding,
+                                    pos, length,
+                                    dst, dst_coding);
+}
+
+
+// vector to vector
+SIZE_TYPE CSeqConvert::Convert
+(const vector< char >& src,
+ TCoding src_coding,
+ TSeqPos pos,
+ TSeqPos length,
+ vector< char >& dst,
+ TCoding dst_coding)
+{
+    return CSeqConvert_imp::Convert(src, src_coding,
+                                    pos, length,
+                                    dst, dst_coding);
+}
+
+// char* to char*
+SIZE_TYPE CSeqConvert::Convert
+(const char src[],
+ TCoding src_coding,
+ TSeqPos pos,
+ TSeqPos length,
+ char dst[],
+ TCoding dst_coding)
+{
+    return CSeqConvert_imp::Convert(src, src_coding,
+                                    pos, length,
+                                    dst, dst_coding);
+}
+
+
+//  -- Get part of a sequence (same coding conversion)
+
+SIZE_TYPE CSeqConvert::Subseq
+(const string& src,
+ TCoding coding,
+ TSeqPos pos,
+ TSeqPos length,
+ string& dst)
+{
+    return CSeqConvert_imp::Subseq(src, coding, pos, length, dst);
+}
+
+
+SIZE_TYPE CSeqConvert::Subseq
+(const string& src,
+ TCoding coding,
+ TSeqPos pos,
+ TSeqPos length,
+ vector<char>& dst)
+{
+    return CSeqConvert_imp::Subseq(src, coding, pos, length, dst);
+}
+
+
+SIZE_TYPE CSeqConvert::Subseq
+(const vector<char>& src,
+ TCoding coding,
+ TSeqPos pos,
+ TSeqPos length,
+ string& dst)
+{
+    return CSeqConvert_imp::Subseq(src, coding, pos, length, dst);
+}
+
+
+SIZE_TYPE CSeqConvert::Subseq
+(const vector<char>& src,
+ TCoding coding,
+ TSeqPos pos,
+ TSeqPos length,
+ vector<char>& dst)
+{
+    return CSeqConvert_imp::Subseq(src, coding, pos, length, dst);
+}
+
+
+SIZE_TYPE CSeqConvert::Subseq
+(const char* src,
+ TCoding coding,
+ TSeqPos pos,
+ TSeqPos length,
+ char* dst)
+{
+    return CSeqConvert_imp::Subseq(src, coding, pos, length, dst);
+}
+
+
+// -- Packing
+
+SIZE_TYPE CSeqConvert::Pack
+(const string& src,
+ TCoding src_coding,
+ vector<char>& dst,
+ TCoding& dst_coding,
+ TSeqPos length)
+{
+    return CSeqConvert_imp::Pack(src, src_coding, dst, dst_coding, length);
+}
+
+
+SIZE_TYPE CSeqConvert::Pack
+(const vector<char>& src,
+ TCoding src_coding,
+ vector<char>& dst,
+ TCoding& dst_coding,
+ TSeqPos length)
+{
+    return CSeqConvert_imp::Pack(src, src_coding, dst, dst_coding, length);
+}
+
+SIZE_TYPE CSeqConvert::Pack
+(const char* src,
+ TSeqPos length,
+ TCoding src_coding,
+ char* dst,
+ TCoding& dst_coding)
+{
+    return CSeqConvert_imp::Pack(src, length, src_coding, dst, dst_coding);
+}
+
+
+END_NCBI_SCOPE
+
+ /*
+* ===========================================================================
+*
+* $Log$
+* Revision 1.1  2003/10/08 13:34:37  shomrat
+* Initial version
+*
+*
+* ===========================================================================
+*/
---
+++ src/util/sequtil/sequtil_convert_imp.hpp
@@ -0,0 +1,241 @@
+#ifndef UTIL_SEQUTIL___SEQUTIL_CONVERT_IMP__HPP
+#define UTIL_SEQUTIL___SEQUTIL_CONVERT_IMP__HPP
+
+/*  $Id$
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Author:  Mati Shomrat
+ *
+ * File Description:
+ *   
+ */   
+#include <corelib/ncbistd.hpp>
+#include <corelib/ncbiexpt.hpp>
+
+#include <util/sequtil/sequtil.hpp>
+#include "sequtil_shared.hpp"
+
+BEGIN_NCBI_SCOPE
+
+
+class CSeqConvert_imp
+{
+public:
+    typedef CSeqUtil::TCoding TCoding;
+
+    // Conversion
+
+    template <typename SrcCont, typename DstCont>
+    static SIZE_TYPE Convert
+    (const SrcCont& src,
+     TCoding src_coding,
+     TSeqPos pos,
+     TSeqPos length,
+     DstCont& dst, 
+     TCoding dst_coding)
+    {
+        _ASSERT(!OutOfRange(pos, src, src_coding));
+        
+        if ( src.empty()  ||  (length == 0) ) {
+            return 0;
+        }
+        
+        AdjustLength(src, src_coding, pos, length);
+        ResizeDst(dst, dst_coding, length);
+        
+        return Convert(&(src[0]), src_coding, pos, length,
+            &(dst[0]), dst_coding);
+    }
+
+
+    static SIZE_TYPE Convert(const char* src, TCoding src_coding,
+                             TSeqPos pos, TSeqPos length,
+                             char* dst, TCoding dst_coding);
+
+    // Subseq
+
+    template <typename SrcCont, typename DstCont>
+    static SIZE_TYPE Subseq
+    (const SrcCont& src,
+     TCoding coding,
+     TSeqPos pos,
+     TSeqPos length,
+     DstCont& dst)
+    {
+        _ASSERT(!OutOfRange(pos, src, coding));
+
+        if ( src.empty()  ||  (length == 0) ) {
+            return 0;
+        }
+        
+        AdjustLength(src, coding, pos, length);
+        ResizeDst(dst, coding, length);
+        
+        return Subseq(&(src[0]), coding, pos, length, &(dst[0]));
+    }
+
+    static SIZE_TYPE Subseq(const char* src, TCoding coding,
+                            TSeqPos pos, TSeqPos length,
+                            char* dst);
+
+    // Pack
+
+    template <typename SrcCont, typename DstCont>
+    static SIZE_TYPE Pack
+    (const SrcCont& src,
+     TCoding src_coding,
+     DstCont& dst,
+     TCoding& dst_coding,
+     TSeqPos length)
+    {
+        if ( src.empty()  ||  (length == 0) ) {
+            return 0;
+        }
+        
+        AdjustLength(src, src_coding, 0, length);
+        // we allocate enough memory for ncbi4na coding
+        // if the result will be ncbi2na coding we'll resize (see below)
+        ResizeDst(dst, CSeqUtil::e_Ncbi4na, length);
+        
+        SIZE_TYPE res = Pack(&(src[0]), length, src_coding, 
+                             &(dst[0]), dst_coding);
+        if ( dst_coding == CSeqUtil::e_Ncbi2na ) {
+            size_t new_size = res / 4;
+            if ( (res % 4) != 0 ) {
+                ++new_size;
+            }
+            dst.resize(new_size);
+        }
+        return res;
+    }
+
+    static SIZE_TYPE Pack(const char* src, TSeqPos length, TCoding src_coding,
+                          char* dst, TCoding& dst_coding);
+
+private:
+
+    // Conversion methods:
+
+    // --- NA conversions:
+
+    // iupacna -> ...
+    static SIZE_TYPE x_ConvertIupacnaToIupacna(const char* src, TSeqPos pos,
+        TSeqPos length, char* dst);
+    static SIZE_TYPE x_ConvertIupacnaTo2na(const char* src, TSeqPos pos, 
+        TSeqPos length, char* dst);
+    static SIZE_TYPE x_ConvertIupacnaTo2naExpand(const char* src, TSeqPos pos,
+        TSeqPos length, char* dst);
+    static SIZE_TYPE x_ConvertIupacnaTo4na(const char* src, TSeqPos pos, 
+        TSeqPos length, char* dst);
+    static SIZE_TYPE x_ConvertIupacnaTo8na(const char* src, TSeqPos pos, 
+        TSeqPos length, char* dst);
+
+    // ncbi2na -> ...
+    static SIZE_TYPE x_Convert2naToIupacna(const char* src, TSeqPos pos, 
+        TSeqPos length, char* dst);
+    static SIZE_TYPE x_Convert2naTo2naExpand(const char* src, TSeqPos pos,
+        TSeqPos length, char* dst);
+    static SIZE_TYPE x_Convert2naTo4na(const char* src, TSeqPos pos, 
+        TSeqPos length, char* dst);
+    static SIZE_TYPE x_Convert2naTo8na(const char* src, TSeqPos pos, 
+        TSeqPos length, char* dst);
+
+    // ncbi2na_expand -> ...
+    static SIZE_TYPE x_Convert2naExpandToIupacna(const char* src, TSeqPos pos,
+        TSeqPos length, char* dst);
+    static SIZE_TYPE x_Convert2naExpandTo2na(const char* src, TSeqPos pos,
+        TSeqPos length, char* dst);
+    static SIZE_TYPE x_Convert2naExpandTo4na(const char* src, TSeqPos pos, 
+        TSeqPos length, char* dst);
+    static SIZE_TYPE x_Convert2naExpandTo8na(const char* src, TSeqPos pos, 
+        TSeqPos length, char* dst);
+
+    // ncbi4na -> ...
+    static SIZE_TYPE x_Convert4naToIupacna(const char* src, TSeqPos pos, 
+        TSeqPos length, char* dst);
+    static SIZE_TYPE x_Convert4naTo2na(const char* src, TSeqPos pos, 
+        TSeqPos length, char* dst);
+    static SIZE_TYPE x_Convert4naTo2naExpand(const char* src, TSeqPos pos,
+        TSeqPos length, char* dst);
+    static SIZE_TYPE x_Convert4naTo8na(const char* src, TSeqPos pos, 
+        TSeqPos length, char* dst);
+
+    // ncbi8na (ncbi4na_expand) -> ...
+    static SIZE_TYPE x_Convert8naToIupacna(const char* src, TSeqPos pos, 
+        TSeqPos length, char* dst);
+    static SIZE_TYPE x_Convert8naTo2na(const char* src, TSeqPos pos, 
+        TSeqPos length, char* dst);
+    static SIZE_TYPE x_Convert8naTo2naExpand(const char* src, TSeqPos pos,
+        TSeqPos length, char* dst);
+    static SIZE_TYPE x_Convert8naTo4na(const char* src, TSeqPos pos, 
+        TSeqPos length, char* dst);
+
+    // --- AA conversions:
+
+    // iupacaa -> ...
+    static SIZE_TYPE x_ConvertIupacaaToEaa(const char* src, TSeqPos pos, 
+        TSeqPos length, char* dst);
+    static SIZE_TYPE x_ConvertIupacaaToStdaa(const char* src, TSeqPos pos, 
+        TSeqPos length, char* dst);
+
+    // ncbieaa -> ...
+    static SIZE_TYPE x_ConvertEaaToIupacaa(const char* src, TSeqPos pos, 
+        TSeqPos length, char* dst);
+    static SIZE_TYPE x_ConvertEaaToStdaa(const char* src, TSeqPos pos, 
+        TSeqPos length, char* dst);
+
+    // ncbistdaa (ncbi8aa) -> ...
+    static SIZE_TYPE x_ConvertStdaaToIupacaa(const char* src, TSeqPos pos, 
+        TSeqPos length, char* dst);
+    static SIZE_TYPE x_ConvertStdaaToEaa(const char* src, TSeqPos pos, 
+        TSeqPos length, char* dst);
+
+
+    
+
+    // Test for amibiguous bases (not A,C,G or T) starting at position 0.
+    static bool x_HasAmbig(const char* src, TCoding src_coding, size_t length);
+    static bool x_HasAmbigNcbi8na(const char* src, size_t length);
+    static bool x_HasAmbigNcbi4na(const char* src, size_t length);
+    static bool x_HasAmbigIupacna(const char* src, size_t length);
+};
+
+
+
+END_NCBI_SCOPE
+
+
+#endif  /* UTIL_SEQUTIL___SEQUTIL_CONVERT_IMP__HPP */
+
+/*
+* ===========================================================================
+*
+* $Log$
+* Revision 1.1  2003/10/08 13:35:10  shomrat
+* Initial version
+*
+*
+* ===========================================================================
+*/
