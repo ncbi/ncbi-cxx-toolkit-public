ad1318b21ee52f41f5241cfb35f5c678f7518341
Michael DiCuccio <dicuccio@ncbi.nlm.nih.gov>
2003-10-20 17:48:05 +0000

    Initial revision
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@11618 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ include/objtools/data_loaders/cdd/cdd.hpp
@@ -0,0 +1,82 @@
+#ifndef OBJTOOLS_DATA_LOADERS_CDD___CDD__HPP
+#define OBJTOOLS_DATA_LOADERS_CDD___CDD__HPP
+
+/*  $Id$
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Author: Philip Johnson, Mike DiCuccio
+ *
+ * File Description: CDD consensus sequence data loader
+ *
+ * ===========================================================================
+ */
+
+#include <corelib/ncbistd.hpp>
+#include <corelib/ncbimtx.hpp>
+#include <objmgr/data_loader.hpp>
+
+#include <map>
+
+BEGIN_NCBI_SCOPE
+BEGIN_SCOPE(objects)
+
+
+//
+// class CCddDataLoader is used to retrieve consensus sequences from
+// the CDD server
+//
+
+class NCBI_XLOADER_CDD_EXPORT CCddDataLoader : public CDataLoader
+{
+public:
+    CCddDataLoader(void);
+
+    void GetRecords(const CSeq_id_Handle& idh, const EChoice choice);
+
+private:
+
+    // cached map of resolved consensus sequences
+    typedef map<int, CRef<CSeq_entry> > TCddEntries;
+    TCddEntries m_Entries;
+
+    // mutex guarding input into the map
+    CMutex m_Mutex;
+};
+
+
+END_SCOPE(objects)
+END_NCBI_SCOPE
+
+
+/*
+ * ===========================================================================
+ * $Log$
+ * Revision 1.1  2003/10/20 17:48:05  dicuccio
+ * Initial revision
+ *
+ * ===========================================================================
+ */
+
+#endif  // OBJTOOLS_DATA_LOADERS_CDD___CDD__HPP
---
+++ src/objtools/data_loaders/cdd/cdd.cpp
@@ -0,0 +1,116 @@
+/*  $Id$
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Author: Philip Johnson, Mike DiCuccio
+ *
+ * File Description: CDD consensus sequence data loader
+ *
+ * ===========================================================================
+ */
+
+#include <corelib/ncbistd.hpp>
+#include <connect/ncbi_conn_stream.hpp>
+#include <objects/seqloc/Seq_id.hpp>
+#include <objects/general/Dbtag.hpp>
+#include <objects/general/Object_id.hpp>
+#include <objects/seqset/Seq_entry.hpp>
+#include <objmgr/impl/data_source.hpp>
+
+#include <objtools/data_loaders/cdd/cdd.hpp>
+
+#include <serial/serial.hpp>
+#include <serial/objistr.hpp>
+
+
+BEGIN_NCBI_SCOPE
+BEGIN_SCOPE(objects)
+
+
+CCddDataLoader::CCddDataLoader()
+    : CDataLoader("CDD_DataLoader")
+{
+}
+
+
+/*---------------------------------------------------------------------------*/
+// PRE : ??
+// POST: ??
+void CCddDataLoader::GetRecords(const CSeq_id_Handle& idh,
+                                const EChoice choice)
+{
+    CConstRef<CSeq_id> id = idh.GetSeqId();
+    if ( !id  ||  !id->IsGeneral()  ||  id->GetGeneral().GetDb() != "CDD") {
+        return;
+    }
+
+    CMutexGuard LOCK(m_Mutex);
+
+    int cdd_id = id->GetGeneral().GetTag().GetId();
+    TCddEntries::iterator iter = m_Entries.find(cdd_id);
+    if (iter != m_Entries.end()) {
+        // found, already added
+        return;
+    }
+
+    // retrieve the seq-entry, if possible
+    _TRACE("CCddDataLoader(): loading id = " << id->AsFastaString());
+
+    // create HTTP connection to CDD server
+    string url("http://web.ncbi.nlm.nih.gov/Structure/cdd/cddsrv.cgi");
+    string params("uid=");
+    params += NStr::IntToString(cdd_id);
+    params += "&getcseq";
+    CConn_HttpStream inHttp(url);
+    inHttp << params;
+
+
+    CRef<CSeq_entry> entry(new CSeq_entry());
+    try {
+        auto_ptr<CObjectIStream> is
+            (CObjectIStream::Open(eSerial_AsnText, inHttp));
+        *is >> *entry;
+    }
+    catch (...) {
+        _TRACE("CCddDataLoader(): failed to parse data");
+    }
+
+    // save our entry in all relevant places
+    m_Entries[cdd_id] = entry;
+    GetDataSource()->AddTSE(*entry);
+}
+
+
+END_SCOPE(objects)
+END_NCBI_SCOPE
+
+
+/*
+ * ===========================================================================
+ * $Log$
+ * Revision 1.1  2003/10/20 17:47:49  dicuccio
+ * Initial revision
+ *
+ * ===========================================================================
+ */
