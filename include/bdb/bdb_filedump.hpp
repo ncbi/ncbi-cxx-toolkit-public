bccb38419c060d2751911e4250893195e44ae995
Anatoliy Kuznetsov <kuznets@ncbi.nlm.nih.gov>
2003-10-27 14:18:22 +0000

    Initial revision
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@11750 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ include/bdb/bdb_filedump.hpp
@@ -0,0 +1,115 @@
+#ifndef BDB_FILEDUMP__HPP
+#define BDB_FILEDUMP__HPP
+
+/* $Id$
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Author:  Anatoliy Kuznetsov
+ *   
+ * File Description: Dumper for BDB files into text format.
+ *
+ */
+
+/// @file bdb_filedump.hpp
+/// BDB File covertion into text.
+
+
+BEGIN_NCBI_SCOPE
+
+/** @addtogroup BDB_Files
+ *
+ * @{
+ */
+
+class CBDB_File;
+
+/// Utility class to convert DBD files into text files. 
+///
+
+class NCBI_BDB_EXPORT CBDB_FileDumper
+{
+public:
+    /// Constructor.
+    /// @param col_separator
+    ///    Column separator
+    CBDB_FileDumper(const string& col_separator = "\t");
+    CBDB_FileDumper(const CBDB_FileDumper& fdump);
+    CBDB_FileDumper& operator=(const CBDB_FileDumper& fdump);
+
+
+    void SetColumnSeparator(const string& col_separator);
+
+    /// Convert BDB file into text file
+    void Dump(const string& dump_file_name, CBDB_File& db);
+
+    /// Convert BDB file into text and write it into the specified stream
+    void Dump(CNcbiOstream& out, CBDB_File& db);
+
+protected:
+    string     m_ColumnSeparator;
+}; 
+
+
+inline
+CBDB_FileDumper::CBDB_FileDumper(const string& col_separator)
+: m_ColumnSeparator(col_separator)
+{
+}
+
+inline
+CBDB_FileDumper::CBDB_FileDumper(const CBDB_FileDumper& fdump)
+: m_ColumnSeparator(fdump.m_ColumnSeparator)
+{
+}
+
+inline
+CBDB_FileDumper& CBDB_FileDumper::operator=(const CBDB_FileDumper& fdump)
+{
+    m_ColumnSeparator = fdump.m_ColumnSeparator;
+}
+
+inline
+void CBDB_FileDumper::SetColumnSeparator(const string& col_separator)
+{
+    m_ColumnSeparator = col_separator;
+}
+
+/* @} */
+
+END_NCBI_SCOPE
+
+
+
+/*
+ * ===========================================================================
+ * $Log$
+ * Revision 1.1  2003/10/27 14:17:57  kuznets
+ * Initial revision
+ *
+ *
+ * ===========================================================================
+ */
+
+#endif
---
+++ src/bdb/bdb_filedump.cpp
@@ -0,0 +1,93 @@
+/*  $Id$
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Author: Anatoliy Kuznetsov
+ *
+ * File Description:  BDB File covertion into text.
+ *
+ */
+
+#include <corelib/ncbistre.hpp>
+
+#include <bdb/bdb_file.hpp>
+#include <bdb/bdb_cursor.hpp>
+#include <bdb/bdb_filedump.hpp>
+
+BEGIN_NCBI_SCOPE
+
+void CBDB_FileDumper::Dump(const string& dump_file_name, CBDB_File& db)
+{
+    CNcbiOfstream out(dump_file_name.c_str());
+    if (!out) {
+        string err = "Cannot open text file:";
+        err.append(dump_file_name);
+        BDB_THROW(eInvalidValue, err);
+    }
+
+    Dump(out, db);
+}
+
+void CBDB_FileDumper::Dump(CNcbiOstream& out, CBDB_File& db)
+{
+    const CBDB_BufferManager* key  = db.GetKeyBuffer();
+    const CBDB_BufferManager* data = db.GetDataBuffer();
+
+    CBDB_FileCursor cur(db);
+    cur.SetCondition(CBDB_FileCursor::eFirst);
+
+    while (cur.Fetch() == eBDB_Ok) {
+
+        if (key) {
+            for (unsigned i = 0; i < key->FieldCount(); ++i) {
+                const CBDB_Field& fld = key->GetField(i);
+                if (i != 0)
+                    out << m_ColumnSeparator;
+                out << fld.GetString();
+            }
+        }
+
+        if (data) {
+            for (unsigned i = 0; i < data->FieldCount(); ++i) {
+                const CBDB_Field& fld = data->GetField(i);
+                out << m_ColumnSeparator << fld.GetString();
+            }
+        }
+        out << endl;
+
+    }
+}
+
+END_NCBI_SCOPE
+
+
+/*
+ * ===========================================================================
+ * $Log$
+ * Revision 1.1  2003/10/27 14:18:22  kuznets
+ * Initial revision
+ *
+ *
+ * ===========================================================================
+ */
