b9a222b20f0b86e4e73d7074e8a7dc95b80b8d0b
Anatoliy Kuznetsov <kuznets@ncbi.nlm.nih.gov>
2003-10-30 16:38:08 +0000

    CVersionInfo changed:  - added accessors for major, minor and patch level  - Match function to compare versions Added FindVersion algorithm to scan containers for the best version
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@11832 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/corelib/version.hpp
+++ include/corelib/version.hpp
@@ -26,7 +26,7 @@
  *
  * ===========================================================================
  *
- * Authors:  Denis Vakatov, Vladimir Ivanov
+ * Authors:  Denis Vakatov, Vladimir Ivanov, Anatoliy Kuznetsov
  *
  *
  */
@@ -79,24 +79,90 @@ public:
     /// - <ver_major>.<ver_minor>.<patch_level> (<name>)
     virtual string Print(void) const;
 
+    /// Major version
+    unsigned int GetMajor(void) const { return m_Major; }
+    /// Minor version
+    unsigned int GetMinor(void) const { return m_Minor; }
+    /// Patch level
+    unsigned int GetPatchLevel(void) const { return m_PatchLevel; }
+
+    const string& GetName(void) const { return m_Name; }
+
+    /// Patch level comparison mode
+    /// @sa Match
+    enum EPatchLevelCompare 
+    {
+        eAnyLevel,
+        eNewerLevel,
+        eExactLevel
+    };
+
+    /// Check if version matches another version.
+    /// @param version_info
+    ///   Version Info to compare with
+    /// @param pl_mode
+    ///   Patch level comparison mode
+    bool Match(const CVersionInfo& version_info, 
+               EPatchLevelCompare  pl_mode) const;
+
 protected:
-    unsigned     int m_Major;       ///< Major number
-    unsigned     int m_Minor;       ///< Minor number
-    unsigned     int m_PatchLevel;  ///< Patch level
-    const string m_Name;            ///< Name
+    unsigned  int    m_Major;       ///< Major number
+    unsigned  int    m_Minor;       ///< Minor number
+    unsigned  int    m_PatchLevel;  ///< Patch level
+    const     string m_Name;        ///< Name
 };
 
+
+/// Algorithm function to find version in the container
+///
+/// Scans the provided iterator for version with the same major and
+/// minor version and the newest patch level.
+///
+/// @param first
+///    first iterator to start search 
+/// @param last
+///    ending iterator (typically returned by end() fnction of an STL
+///    container)
+/// @return 
+///    iterator on the best version or last
+template<class It>
+It FindVersion(It first, It last, const CVersionInfo& info)
+{
+    It best_version = last;  // not found by default
+    unsigned int max_patch_level = 0;
+
+    for (;first != last; ++first) {
+        const CVersionInfo& vinfo = *first;
+        if (info.Match(vinfo, CVersionInfo::eNewerLevel)) {
+            // Candidate?
+            if (vinfo.GetPatchLevel() > max_patch_level) {
+                best_version = first;
+                max_patch_level = vinfo.GetPatchLevel();
+            }
+        }
+    
+    } // for
+    
+    return best_version; 
+}
+
+/* @} */
 
 
 END_NCBI_SCOPE
 
 
-/* @} */
 
 
 /*
  * ===========================================================================
  * $Log$
+ * Revision 1.5  2003/10/30 16:38:08  kuznets
+ * CVersionInfo changed:
+ *  - added accessors for major, minor and patch level
+ *  - Match function to compare versions
+ * Added FindVersion algorithm to scan containers for the best version
+ *
  * Revision 1.4  2003/09/08 12:17:42  siyan
  * Documentation changes.
  *
