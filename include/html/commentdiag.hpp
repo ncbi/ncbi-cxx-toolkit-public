a71e4a911893944f85951d29842932f118adfb4c
Vladimir Ivanov <ivanov@ncbi.nlm.nih.gov>
2003-11-03 14:41:15 +0000

    Log moved to end
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@11875 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/html/commentdiag.hpp
+++ include/html/commentdiag.hpp
@@ -1,50 +1,40 @@
-#ifndef COMMENTDIAG__HPP
-#define COMMENTDIAG__HPP
+#ifndef HTML___COMMENTDIAG__HPP
+#define HTML___COMMENTDIAG__HPP
 
 /*  $Id$
-* ===========================================================================
-*
-*                            PUBLIC DOMAIN NOTICE
-*               National Center for Biotechnology Information
-*
-*  This software/database is a "United States Government Work" under the
-*  terms of the United States Copyright Act.  It was written as part of
-*  the author's official duties as a United States Government employee and
-*  thus cannot be copyrighted.  This software/database is freely available
-*  to the public for use. The National Library of Medicine and the U.S.
-*  Government have not placed any restriction on its use or reproduction.
-*
-*  Although all reasonable efforts have been taken to ensure the accuracy
-*  and reliability of the software and data, the NLM and the U.S.
-*  Government do not and cannot warrant the performance or results that
-*  may be obtained by using this software or data. The NLM and the U.S.
-*  Government disclaim all warranties, express or implied, including
-*  warranties of performance, merchantability or fitness for any particular
-*  purpose.
-*
-*  Please cite the author in any work or product based on this material.
-*
-* ===========================================================================
-*
-* Author:  Aaron Ucko
-*
-* File Description:
-*   Diagnostic handler for embedding diagnostics in comments.
-*
-* ---------------------------------------------------------------------------
-* $Log$
-* Revision 1.2  2003/04/25 13:45:21  siyan
-* Added doxygen groupings
-*
-* Revision 1.1  2001/11/19 15:20:22  ucko
-* Switch CGI stuff to new diagnostics interface.
-*
-* ===========================================================================
-*/
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Author:  Aaron Ucko
+ *
+ * File Description:
+ *   Diagnostic handler for embedding diagnostics in comments.
+ *
+ */
 
 #include <corelib/ncbistd.hpp>
 #include <html/html.hpp>
-
 
 /** @addtogroup DiagHandler
  *
@@ -52,7 +42,6 @@
  */
 
 
-// (BEGIN_NCBI_SCOPE must be followed by END_NCBI_SCOPE later in this file)
 BEGIN_NCBI_SCOPE
 
 
@@ -92,11 +81,25 @@ CCommentDiagHandler* SetDiagNode(CNCBINode* node, CDiagHandler* handler = NULL)
 }
 
 
-// (END_NCBI_SCOPE must be preceded by BEGIN_NCBI_SCOPE)
 END_NCBI_SCOPE
 
 
 /* @} */
 
+#endif  /* HTML___COMMENTDIAG__HPP */
 
-#endif  /* COMMENTDIAG__HPP */
+
+/*
+ * ===========================================================================
+ * $Log$
+ * Revision 1.3  2003/11/03 14:41:15  ivanov
+ * Log moved to end
+ *
+ * Revision 1.2  2003/04/25 13:45:21  siyan
+ * Added doxygen groupings
+ *
+ * Revision 1.1  2001/11/19 15:20:22  ucko
+ * Switch CGI stuff to new diagnostics interface.
+ *
+ * ===========================================================================
+ */
