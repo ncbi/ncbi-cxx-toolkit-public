578836668c39549b1bdd2794d0734d78a73e2cd3
Vladimir Ivanov <ivanov@ncbi.nlm.nih.gov>
2003-11-03 14:42:16 +0000

    Moved log to end
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@11876 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/html/components.hpp
+++ include/html/components.hpp
@@ -1,118 +1,37 @@
-#ifndef COMPONENTS__HPP
-#define COMPONENTS__HPP
+#ifndef HTML___COMPONENTS__HPP
+#define HTML___COMPONENTS__HPP
 
 /*  $Id$
-* ===========================================================================
-*
-*                            PUBLIC DOMAIN NOTICE
-*               National Center for Biotechnology Information
-*
-*  This software/database is a "United States Government Work" under the
-*  terms of the United States Copyright Act.  It was written as part of
-*  the author's official duties as a United States Government employee and
-*  thus cannot be copyrighted.  This software/database is freely available
-*  to the public for use. The National Library of Medicine and the U.S.
-*  Government have not placed any restriction on its use or reproduction.
-*
-*  Although all reasonable efforts have been taken to ensure the accuracy
-*  and reliability of the software and data, the NLM and the U.S.
-*  Government do not and cannot warrant the performance or results that
-*  may be obtained by using this software or data. The NLM and the U.S.
-*  Government disclaim all warranties, express or implied, including
-*  warranties of performance, merchantability or fitness for any particular
-*  purpose.
-*
-*  Please cite the author in any work or product based on this material.
-*
-* ===========================================================================
-*
-* Author:  Lewis Geer
-*
-* File Description:
-*   The individual html components used on a page
-*
-* ---------------------------------------------------------------------------
-* $Log$
-* Revision 1.22  2003/04/25 13:45:23  siyan
-* Added doxygen groupings
-*
-* Revision 1.21  1999/10/28 13:40:29  vasilche
-* Added reference counters to CNCBINode.
-*
-* Revision 1.20  1999/04/15 22:03:44  vakatov
-* CQueryBox:: use "enum { kNo..., };" rather than "static const int kNo...;"
-*
-* Revision 1.19  1999/01/28 21:58:04  vasilche
-* QueryBox now inherits from CHTML_table (not CHTML_form as before).
-* Use 'new CHTML_form("url", queryBox)' as replacement of old QueryBox.
-*
-* Revision 1.18  1999/01/21 21:12:53  vasilche
-* Added/used descriptions for HTML submit/select/text.
-* Fixed some bugs in paging.
-*
-* Revision 1.17  1999/01/20 18:12:42  vasilche
-* Added possibility to change label of buttons.
-*
-* Revision 1.16  1999/01/19 21:17:37  vasilche
-* Added CPager class
-*
-* Revision 1.15  1999/01/15 20:22:32  volodya
-* syntax fix
-*
-* Revision 1.14  1999/01/14 21:25:16  vasilche
-* Changed CPageList to work via form image input elements.
-*
-* Revision 1.13  1999/01/07 17:06:32  vasilche
-* Added default query text in CQueryBox.
-* Added query text width in CQueryBox.
-*
-* Revision 1.12  1999/01/07 16:41:52  vasilche
-* CHTMLHelper moved to separate file.
-* TagNames of CHTML classes ara available via s_GetTagName() static
-* method.
-* Input tag types ara available via s_GetInputType() static method.
-* Initial selected database added to CQueryBox.
-* Background colors added to CPagerBax & CSmallPagerBox.
-*
-* Revision 1.11  1999/01/05 21:47:09  vasilche
-* Added 'current page' to CPageList.
-* CPageList doesn't display forward/backward if empty.
-*
-* Revision 1.10  1998/12/28 23:29:02  vakatov
-* New CVS and development tree structure for the NCBI C++ projects
-*
-* Revision 1.9  1998/12/28 16:48:04  vasilche
-* Removed creation of QueryBox in CHTMLPage::CreateView()
-* CQueryBox extends from CHTML_form
-* CButtonList, CPageList, CPagerBox, CSmallPagerBox extend from CNCBINode.
-*
-* Revision 1.8  1998/12/23 21:20:56  vasilche
-* Added more HTML tags (almost all).
-* Importent ones: all lists (OL, UL, DIR, MENU), fonts (FONT, BASEFONT).
-*
-* Revision 1.7  1998/12/21 22:24:55  vasilche
-* A lot of cleaning.
-*
-* Revision 1.6  1998/12/11 22:53:39  lewisg
-* added docsum page
-*
-* Revision 1.5  1998/12/11 18:13:50  lewisg
-* frontpage added
-*
-* Revision 1.4  1998/12/09 23:02:55  lewisg
-* update to new cgiapp class
-*
-* Revision 1.3  1998/12/08 00:34:54  lewisg
-* cleanup
-*
-* Revision 1.2  1998/11/23 23:47:48  lewisg
-* *** empty log message ***
-*
-* Revision 1.1  1998/10/29 16:15:51  lewisg
-* version 2
-*
-* ===========================================================================
-*/
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Author:  Lewis Geer
+ *
+ * File Description:
+ *   The individual html components used on a page
+ *
+ */
 
 #include <html/html.hpp>
 
@@ -124,6 +43,7 @@
 
 
 BEGIN_NCBI_SCOPE
+
 
 class CSubmitDescription
 {
@@ -138,6 +58,7 @@ public:
     CNCBINode* CreateComponent(void) const;
 };
 
+
 class COptionDescription
 {
 public:
@@ -150,6 +71,7 @@ public:
 
     CNCBINode* CreateComponent(const string& def) const;
 };
+
 
 class CSelectDescription
 {
@@ -171,6 +93,7 @@ public:
     CNCBINode* CreateComponent(void) const;
 };
 
+
 class CTextInputDescription
 {
 public:
@@ -184,6 +107,7 @@ public:
     CNCBINode* CreateComponent(void) const;
 };
 
+
 class CQueryBox: public CHTML_table
 {
     // parent class
@@ -191,38 +115,37 @@ class CQueryBox: public CHTML_table
 
 public:
 
-    //////// 'tors
+    // 'tors
 
     CQueryBox(void);
 
-    //////// flags
+    // flags
 
     enum flags {
         kNoLIST     = 0x1,
         kNoCOMMENTS = 0x2
     };
 
-    ////////  members
+    // members
 
     int m_Width; // in pixels
     string m_BgColor;
 
-    //    map<string, string> m_HiddenValues;
+    // map<string, string> m_HiddenValues;
 
     CSubmitDescription m_Submit;
     CSelectDescription m_Database;
     CTextInputDescription m_Term;
     CSelectDescription m_DispMax;
 
-    //////// subpages
+    // subpages
 
     virtual void CreateSubNodes(void);
     virtual CNCBINode* CreateComments(void);
 };
 
 
-// makes a button followed by a drop down
-
+// Makes a button followed by a drop down.
 class CButtonList: public CNCBINode
 {
     // parent class
@@ -237,7 +160,8 @@ public:
     virtual void CreateSubNodes(void);
 };
 
-// makes a set of pagination links
+
+// Makes a set of pagination links
 
 class CPageList: public CHTML_table
 {
@@ -247,10 +171,10 @@ class CPageList: public CHTML_table
 public:
     CPageList(void);
     
-    map<int, string> m_Pages; // number, href
-    string m_Forward; // forward url
-    string m_Backward; // backward url
-    int m_Current; // current page number
+    map<int, string> m_Pages;   // number, href
+    string m_Forward;           // forward url
+    string m_Backward;          // backward url
+    int m_Current;              // current page number
     
     virtual void CreateSubNodes(void);
     
@@ -261,6 +185,7 @@ private:
                           const string& imageStart, const string& imageEnd);
 };
 
+
 class CPagerBox: public CNCBINode
 {
     // parent class
@@ -269,17 +194,18 @@ class CPagerBox: public CNCBINode
 public:
     CPagerBox(void);
 
-    int m_Width; // in pixels
-    CButtonList* m_TopButton;  // display button
-    CButtonList* m_LeftButton; // save button
-    CButtonList* m_RightButton; // order button
-    CPageList* m_PageList;  // the pager
-    int m_NumResults;  // the number of results to display
+    int           m_Width;       // in pixels
+    CButtonList*  m_TopButton;   // display button
+    CButtonList*  m_LeftButton;  // save button
+    CButtonList*  m_RightButton; // order button
+    CPageList*    m_PageList;    // the pager
+    int           m_NumResults;  // the number of results to display
 
     string m_BgColor;
     
     virtual void CreateSubNodes(void);
 };
+
 
 class CSmallPagerBox: public CNCBINode
 {
@@ -289,9 +215,9 @@ class CSmallPagerBox: public CNCBINode
 public:
     CSmallPagerBox(void);
 
-    int m_Width; // in pixels
-    CPageList * m_PageList;  // the pager
-    int m_NumResults;  // the number of results to display
+    int        m_Width;      // in pixels
+    CPageList* m_PageList;   // the pager
+    int        m_NumResults; // the number of results to display
 
     string m_BgColor;
     
@@ -299,11 +225,101 @@ public:
 };
 
 
-/* @} */
-
-
 #include <html/components.inl>
+
 
 END_NCBI_SCOPE
 
-#endif
+
+/* @} */
+
+
+#endif /* HTML___COMPONENTS__HPP */
+
+
+/*
+ * ===========================================================================
+ * $Log$
+ * Revision 1.23  2003/11/03 14:42:16  ivanov
+ * Moved log to end
+ *
+ * Revision 1.22  2003/04/25 13:45:23  siyan
+ * Added doxygen groupings
+ *
+ * Revision 1.21  1999/10/28 13:40:29  vasilche
+ * Added reference counters to CNCBINode.
+ *
+ * Revision 1.20  1999/04/15 22:03:44  vakatov
+ * CQueryBox:: use "enum { kNo..., };" rather than "static const int kNo...;"
+ *
+ * Revision 1.19  1999/01/28 21:58:04  vasilche
+ * QueryBox now inherits from CHTML_table (not CHTML_form as before).
+ * Use 'new CHTML_form("url", queryBox)' as replacement of old QueryBox.
+ *
+ * Revision 1.18  1999/01/21 21:12:53  vasilche
+ * Added/used descriptions for HTML submit/select/text.
+ * Fixed some bugs in paging.
+ *
+ * Revision 1.17  1999/01/20 18:12:42  vasilche
+ * Added possibility to change label of buttons.
+ *
+ * Revision 1.16  1999/01/19 21:17:37  vasilche
+ * Added CPager class
+ *
+ * Revision 1.15  1999/01/15 20:22:32  volodya
+ * syntax fix
+ *
+ * Revision 1.14  1999/01/14 21:25:16  vasilche
+ * Changed CPageList to work via form image input elements.
+ *
+ * Revision 1.13  1999/01/07 17:06:32  vasilche
+ * Added default query text in CQueryBox.
+ * Added query text width in CQueryBox.
+ *
+ * Revision 1.12  1999/01/07 16:41:52  vasilche
+ * CHTMLHelper moved to separate file.
+ * TagNames of CHTML classes ara available via s_GetTagName() static
+ * method.
+ * Input tag types ara available via s_GetInputType() static method.
+ * Initial selected database added to CQueryBox.
+ * Background colors added to CPagerBax & CSmallPagerBox.
+ *
+ * Revision 1.11  1999/01/05 21:47:09  vasilche
+ * Added 'current page' to CPageList.
+ * CPageList doesn't display forward/backward if empty.
+ *
+ * Revision 1.10  1998/12/28 23:29:02  vakatov
+ * New CVS and development tree structure for the NCBI C++ projects
+ *
+ * Revision 1.9  1998/12/28 16:48:04  vasilche
+ * Removed creation of QueryBox in CHTMLPage::CreateView()
+ * CQueryBox extends from CHTML_form
+ * CButtonList, CPageList, CPagerBox, CSmallPagerBox extend from CNCBINode.
+ *
+ * Revision 1.8  1998/12/23 21:20:56  vasilche
+ * Added more HTML tags (almost all).
+ * Importent ones: all lists (OL, UL, DIR, MENU), fonts (FONT, BASEFONT).
+ *
+ * Revision 1.7  1998/12/21 22:24:55  vasilche
+ * A lot of cleaning.
+ *
+ * Revision 1.6  1998/12/11 22:53:39  lewisg
+ * added docsum page
+ *
+ * Revision 1.5  1998/12/11 18:13:50  lewisg
+ * frontpage added
+ *
+ * Revision 1.4  1998/12/09 23:02:55  lewisg
+ * update to new cgiapp class
+ *
+ * Revision 1.3  1998/12/08 00:34:54  lewisg
+ * cleanup
+ *
+ * Revision 1.2  1998/11/23 23:47:48  lewisg
+ * *** empty log message ***
+ *
+ * Revision 1.1  1998/10/29 16:15:51  lewisg
+ * version 2
+ *
+ * ===========================================================================
+ */
