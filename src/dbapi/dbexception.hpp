99874d308abb80fb76aa88b168d28cd02c397695
Denis Vakatov <vakatov@ncbi.nlm.nih.gov>
2003-11-04 22:27:14 +0000

    CDbapiException to inherit from CDB_ClientEx. Minor style fixes.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@11946 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/dbapi/dbexception.hpp
+++ src/dbapi/dbexception.hpp
@@ -1,60 +1,68 @@
-#ifndef _DBEXCEPTION_HPP_
-#define _DBEXCEPTION_HPP_
+#ifndef DBAPI___DBEXCEPTION__HPP
+#define DBAPI___DBEXCEPTION__HPP
 
 /* $Id$
-* ===========================================================================
-*
-*                            PUBLIC DOMAIN NOTICE                          
-*               National Center for Biotechnology Information
-*                                                                          
-*  This software/database is a "United States Government Work" under the   
-*  terms of the United States Copyright Act.  It was written as part of    
-*  the author's official duties as a United States Government employee and 
-*  thus cannot be copyrighted.  This software/database is freely available 
-*  to the public for use. The National Library of Medicine and the U.S.    
-*  Government have not placed any restriction on its use or reproduction.  
-*                                                                          
-*  Although all reasonable efforts have been taken to ensure the accuracy  
-*  and reliability of the software and data, the NLM and the U.S.          
-*  Government do not and cannot warrant the performance or results that    
-*  may be obtained by using this software or data. The NLM and the U.S.    
-*  Government disclaim all warranties, express or implied, including       
-*  warranties of performance, merchantability or fitness for any particular
-*  purpose.                                                                
-*                                                                          
-*  Please cite the author in any work or product based on this material.   
-*
-* ===========================================================================
-*
-* File Name:  $Id$
-*
-* Author:  Michael Kholodov
-*   
-* File Description:  DBAPI exception class
-*
-*
-* $Log$
-* Revision 1.1  2002/01/30 14:51:23  kholodov
-* User DBAPI implementation, first commit
-*
-*
-*
-*/
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Author:  Michael Kholodov, Denis Vakatov
+ *   
+ * File Description:  DBAPI exception class
+ *
+ */
 
-#include <corelib/ncbistd.hpp>
 #include <dbapi/driver/exception.hpp>
-#include <string>
 
-USING_NCBI_SCOPE;
 
-class CDbapiException : public CDB_Exception {
+BEGIN_NCBI_SCOPE
 
+
+class CDbapiException : public CDB_ClientEx
+{
 public:
-  CDbapiException(const string& errMsg) 
-    : CDB_Exception(eClient, eDB_Error, -1, "DBAPI interface", errMsg) {}
-
-
+    CDbapiException(const string& msg)
+        : CDB_ClientEx(eDB_Error, 1000, "DBAPI interface", msg)
+    {
+        return;
+    }
 };
 
 
-#endif // _GENERAL_EXCEPTION_HPP_
+END_NCBI_SCOPE
+
+
+/*
+ * ===========================================================================
+ * $Log$
+ * Revision 1.2  2003/11/04 22:27:14  vakatov
+ * CDbapiException to inherit from CDB_ClientEx.
+ * Minor style fixes.
+ *
+ * Revision 1.1  2002/01/30 14:51:23  kholodov
+ * User DBAPI implementation, first commit
+ * ===========================================================================
+ */
+
+
+#endif  /* DBAPI___DBEXCEPTION__HPP */
