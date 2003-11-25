16a88da0265441f273a20a9a9acf4197b25da267
Paul Thiessen <thiessen@ncbi.nlm.nih.gov>
2003-11-25 14:36:16 +0000

    restructure macros
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@12243 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/asniotest/asniotest.cpp
+++ src/objtools/asniotest/asniotest.cpp
@@ -59,8 +59,6 @@ USING_SCOPE(objects);
 #define WARNINGMSG(stream) ERR_POST(Warning << stream)
 #define ERRORMSG(stream) ERR_POST(Error << stream << '!')
 
-static string err;  // just so I don't have to keep declaring this all over... ;)
-
 // directory that contains test files (default overridden by env. var. ASNIOTEST_FILES)
 static string pathToFiles(".");
 
@@ -96,6 +94,7 @@ bool ReadASNFromFile(const char *filename, ASNClass *ASNobject, bool isBinary, s
     bool okay = true;
     try {
         *inObject >> *ASNobject;
+        INFOMSG("read file " << filename);
     } catch (exception& e) {
         *err = e.what();
         okay = false;
@@ -104,13 +103,19 @@ bool ReadASNFromFile(const char *filename, ASNClass *ASNobject, bool isBinary, s
     return okay;
 }
 
-// for writing ASN data; all files written this way will be removed at program end
+// for writing out ASN data (to temporary files); all files written this way will be removed at program end
 template < class ASNClass >
-bool WriteASNToFile(const char *filename, const ASNClass& ASNobject, bool isBinary,
+bool WriteASNToFile(const ASNClass& ASNobject, bool isBinary,
     string *err, EFixNonPrint fixNonPrint = eFNP_Default)
 {
     err->erase();
-    string fullPath = pathToFiles + CDirEntry::GetPathSeparator() + filename;
+
+    // construct a temp file name
+    CNcbiOstrstream oss;
+    oss << "test_" << (filesCreated.size() + 1) << (isBinary ? ".bin" : ".txt") << '\0';
+    auto_ptr<char> filename;
+    filename.reset(oss.str());
+    string fullPath = pathToFiles + CDirEntry::GetPathSeparator() + filename.get();
     if (CFile(fullPath).Exists()) {
         *err = "Can't overwrite file ";
         *err += fullPath;
@@ -142,6 +147,7 @@ bool WriteASNToFile(const char *filename, const ASNClass& ASNobject, bool isBina
     try {
         *outObject << ASNobject;
         outStream->flush();
+        INFOMSG("wrote file " << filename.get());
     } catch (exception& e) {
         *err = e.what();
         okay = false;
@@ -150,68 +156,85 @@ bool WriteASNToFile(const char *filename, const ASNClass& ASNobject, bool isBina
     return okay;
 }
 
-// test function declaration
-#define TEST_FUNC(func) \
-    int func (void)
+// test function macros
+#define BEGIN_TEST_FUNC(func) \
+    int func (void) { \
+        int nErrors = 0; \
+        string err; \
+        INFOMSG("Running " #func " test");
+
+#define END_TEST_FUNC \
+        return nErrors; \
+    }
+
+#define ADD_ERR(msg) \
+    do { \
+        ERRORMSG(msg); \
+        nErrors++; \
+    } while (0)
+
+#define ADD_ERR_RETURN(msg) \
+    do { \
+        ERRORMSG(msg); \
+        nErrors++; \
+        return nErrors; \
+    } while (0)
+
+BEGIN_TEST_FUNC(BasicFileIO)
+
+    // read in a trivial object
+    CSeq_id seqId;
+    if (!ReadASNFromFile("pdbSeqId.txt", &seqId, false, &err))
+        ADD_ERR_RETURN("failed to load pdbSeqId.txt: " << err);
+
+    // test text and binary output
+    if (!WriteASNToFile(seqId, false, &err) ||
+        !WriteASNToFile(seqId, true, &err))
+        ADD_ERR("failed to write CSeq_id: " << err);
+
+    // now try a bigger object (in binary this time)
+    CCdd cdd;
+    if (!ReadASNFromFile("ADF.bin", &cdd, true, &err))
+        ADD_ERR_RETURN("failed to load ADF.bin: " << err);
+    if (!WriteASNToFile(cdd, false, &err) ||
+        !WriteASNToFile(cdd, true, &err))
+        ADD_ERR("failed to write CCdd: " << err);
+
+END_TEST_FUNC
+
+
+BEGIN_TEST_FUNC(AssignAndOutput)
+
+    // try to output a copied simple object
+    CSeq_id s1;
+    if (!ReadASNFromFile("pdbSeqId.txt", &s1, false, &err))
+        ADD_ERR_RETURN("failed to load pdbSeqId.txt: " << err);
+    CSeq_id s2;
+    s2.Assign(s1);
+    if (!WriteASNToFile(s2, false, &err) ||
+        !WriteASNToFile(s2, true, &err))
+        ADD_ERR("failed to write Assign()'ed Seq-id: " << err);
+
+    // try to output a copied complex object
+    CCdd c1;
+    if (!ReadASNFromFile("ADF.bin", &c1, true, &err))
+        ADD_ERR_RETURN("failed to load ADF.bin: " << err);
+    CCdd c2;
+    c2.Assign(c1);
+    if (!WriteASNToFile(c2, false, &err) ||
+        !WriteASNToFile(c2, true, &err))
+        ADD_ERR("failed to write Assign()'ed Cdd: " << err);
+
+END_TEST_FUNC
 
 // to call test functions, counting errors
 #define RUN_TEST(func) \
     do { \
-        INFOMSG("Running " #func " test"); \
         int errors = func(); \
         nErrors += errors; \
         if (errors) \
             ERRORMSG(#func " test failed"); \
     } while (0)
-
-#define ERR_RETURN_N(nErrs, msg) \
-    do { \
-        ERRORMSG(msg); \
-        return nErrs; \
-    } while (0)
-
-#define ERR_RETURN(msg) ERR_RETURN_N(1, msg)
-
-#define OKAY 0
-
-
-TEST_FUNC(BasicFileIO)
-{
-    // read in a trivial object
-    CSeq_id seqId;
-    if (!ReadASNFromFile("pdbSeqId.txt", &seqId, false, &err))
-        ERR_RETURN("failed to load pdbSeqId.txt: " << err);
-
-    // test text and binary output
-    if (!WriteASNToFile("t1.txt", seqId, false, &err) ||
-        !WriteASNToFile("t1.bin", seqId, true, &err))
-        ERR_RETURN("failed to write CSeq_id: " << err);
-
-    // now try a bigger object (in binary this time)
-    CCdd cdd;
-    if (!ReadASNFromFile("ADF.bin", &cdd, true, &err))
-        ERR_RETURN("failed to load ADF.bin: " << err);
-    if (!WriteASNToFile("t2.txt", cdd, false, &err) ||
-        !WriteASNToFile("t2.bin", cdd, true, &err))
-        ERR_RETURN("failed to write CCdd: " << err);
-
-    return OKAY;
-}
-
-TEST_FUNC(AssignAndOutput)
-{
-    CSeq_id s1;
-    if (!ReadASNFromFile("pdbSeqId.txt", &s1, false, &err))
-        ERR_RETURN("failed to load pdbSeqId.txt: " << err);
-
-    CSeq_id s2;
-    s2.Assign(s1);
-    if (!WriteASNToFile("t3.txt", s2, false, &err) ||
-        !WriteASNToFile("t3.bin", s2, true, &err))
-        ERR_RETURN("failed to write Assign()'ed Seq-id: " << err);
-
-    return OKAY;
-}
 
 int ASNIOTestApp::Run(void)
 {
@@ -242,7 +265,7 @@ int ASNIOTestApp::Run(void)
     }
 
     if (nErrors == 0)
-        INFOMSG("No errors encountered, all test succeeded!");
+        INFOMSG("No errors encountered, all tests succeeded!");
     else
         ERRORMSG(nErrors << " error" << ((nErrors == 1) ? "" : "s") << " encountered");
     return nErrors;
@@ -270,6 +293,9 @@ int main(int argc, const char* argv[])
 /*
 * ---------------------------------------------------------------------------
 * $Log$
+* Revision 1.3  2003/11/25 14:36:16  thiessen
+* restructure macros
+*
 * Revision 1.2  2003/11/21 15:26:12  thiessen
 * add Assign test
 *
