11ecece0a7e6ead250becefe290c8cef9cf84e22
Anatoliy Kuznetsov <kuznets@ncbi.nlm.nih.gov>
2003-12-10 19:10:53 +0000

    Initial revision
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@12501 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ include/bdb/bdb_trans.hpp
@@ -0,0 +1,123 @@
+#ifndef BDB_TRANS__HPP
+#define BDB_TRANS__HPP
+/* $Id$
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Author:  Anatoliy Kuznetsov
+ *   
+ * File Description: Transaction support
+ *
+ */
+
+/// @file bdb_trans.hpp
+/// Wrapper around Berkeley DB transaction structure
+
+#include <bdb/bdb_env.hpp>
+
+#include <vector>
+
+BEGIN_NCBI_SCOPE
+
+/** @addtogroup BDB
+ *
+ * @{
+ */
+
+class   CBDB_RawFile;
+
+/// BDB transaction object.
+///
+
+class NCBI_BDB_EXPORT CBDB_Transaction
+{
+public:
+    CBDB_Transaction(CBDB_Env& env);
+
+    ~CBDB_Transaction();
+        
+    /// Commit transaction
+    void Commit();
+    
+    /// Abort transaction
+    void Abort();
+    
+    /// Rollback transaction (same as Abort)
+    void Rollback() { Abort(); }
+
+protected:
+
+    /// Add file to the list of connected files
+    void AddFile(CBDB_RawFile* dbfile);
+    
+    /// Remove file from the list of connected files.
+    /// Has no effect if file has not been added before by AddFile
+    void RemoveFile(CBDB_RawFile* dbfile);
+    
+    /// Return TRUE if transaction handle has been requested by some
+    /// client (File)
+    bool IsInProgress() const { return m_Txn != 0; }
+
+    /// Get transaction handle.
+    ///
+    /// Function uses lazy initialization paradigm, and actual
+    /// transaction is created "on demand". First call to GetTxn()
+    /// creates the handle which lives until commit or rollback point.
+    ///
+    /// @return Transaction handle
+    ///
+    DB_TXN*  GetTxn();
+
+private:
+    /// Abort transaction with error checking or without
+    void x_Abort(bool ignore_errors);
+
+private:
+    CBDB_Transaction(const CBDB_Transaction& trans);
+    CBDB_Transaction& operator=(const CBDB_Transaction& trans);
+protected:
+    CBDB_Env&               m_Env;        ///< Associated environment
+    DB_TXN*                 m_Txn;        ///< Transaction handle
+    vector<CBDB_RawFile*>   m_TransFiles; ///< Files connected to transaction
+    
+private:
+    friend class CBDB_RawFile;
+};
+
+/* @} */
+
+END_NCBI_SCOPE
+
+/*
+ * ===========================================================================
+ * $Log$
+ * Revision 1.1  2003/12/10 19:10:53  kuznets
+ * Initial revision
+ *
+ *
+ * ===========================================================================
+ */
+
+
+#endif  /* BDB_ENV__HPP */
---
+++ src/bdb/bdb_trans.cpp
@@ -0,0 +1,112 @@
+/*  $Id$
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Author: Anatoliy Kuznetsov
+ *
+ * File Description:  BDB libarary transaction class implementation.
+ *
+ */
+
+#include <bdb/bdb_trans.hpp>
+#include <bdb/bdb_file.hpp>
+#include <db.h>
+
+BEGIN_NCBI_SCOPE
+
+CBDB_Transaction::CBDB_Transaction(CBDB_Env& env)
+ : m_Env(env),
+   m_Txn(0)
+{}
+
+CBDB_Transaction::~CBDB_Transaction()
+{
+    if (m_Txn) {       // Active transaction is in progress
+        x_Abort(true); // Abort - ignore errors (no except. from constructor)
+        // TODO: Inform all file subscribers we are history now
+    }
+}
+
+DB_TXN* CBDB_Transaction::GetTxn()
+{
+    if (m_Txn == 0) {
+        m_Txn = m_Env.CreateTxn(0, 0);
+    }
+    return m_Txn;
+}
+
+void CBDB_Transaction::Commit()
+{
+    if (m_Txn) {
+        int ret = m_Txn->commit(m_Txn, DB_TXN_SYNC);
+        m_Txn = 0;
+        BDB_CHECK(ret, "DB_TXN::commit");
+    }
+}
+
+void CBDB_Transaction::Abort()
+{
+    x_Abort(false); // abort with full error processing
+}
+
+void CBDB_Transaction::x_Abort(bool ignore_errors)
+{
+    if (m_Txn) {
+        int ret = m_Txn->abort(m_Txn);
+        m_Txn = 0;
+        if (!ignore_errors) {
+            BDB_CHECK(ret, "DB_TXN::abort");
+        }
+    }
+}
+
+void CBDB_Transaction::AddFile(CBDB_RawFile* dbfile) 
+{
+    m_TransFiles.push_back(dbfile);
+}
+
+
+void CBDB_Transaction::RemoveFile(CBDB_RawFile* dbfile)
+{
+    NON_CONST_ITERATE(vector<CBDB_RawFile*>, it, m_TransFiles) {
+        if (dbfile == *it) {
+            m_TransFiles.erase(it);
+            break;
+        }
+    }
+}
+
+
+END_NCBI_SCOPE
+
+
+/*
+ * ===========================================================================
+ * $Log$
+ * Revision 1.1  2003/12/10 19:10:09  kuznets
+ * Initial revision
+ *
+ *
+ * ===========================================================================
+ */
