b7d35e28f6531b2961a75b92210822b4e89ea387
Aaron Ucko <ucko@ncbi.nlm.nih.gov>
2003-12-24 13:29:07 +0000

    Sigh... head -n N may be ubiquitous, but tail -n N is not, so just take the whole log file.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@12733 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/corelib/test/test_ncbi_os_unix.sh
+++ src/corelib/test/test_ncbi_os_unix.sh
@@ -5,7 +5,7 @@ test_ncbi_os_unix
 test $? = 0  ||  exit 1
 sleep 1
 test -f ./test_ncbi_os_unix.log  ||  exit 1
-line=`tail -n 1 ./test_ncbi_os_unix.log 2>/dev/null`
+line=`cat ./test_ncbi_os_unix.log 2>/dev/null`
 echo "$line"
 test `echo "$line" | grep -s -c "COMPLETED SUCCESSFULLY"` != 1  &&  exit 1
 exit 0
