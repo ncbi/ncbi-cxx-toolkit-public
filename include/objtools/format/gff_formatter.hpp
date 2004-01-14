63d470b82d5072d11b4ae6ce8eb3ccb824f0f741
shomrat <shomrat@ncbi.nlm.nih.gov>
2004-01-14 15:59:50 +0000

    Initial Revision
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@12956 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ include/objtools/format/gff_formatter.hpp
@@ -0,0 +1,123 @@
+#ifndef OBJTOOLS_FORMAT___GFF_FORMATTER__HPP
+#define OBJTOOLS_FORMAT___GFF_FORMATTER__HPP
+
+/*  $Id$
+* ===========================================================================
+*
+*                            PUBLIC DOMAIN NOTICE
+*               National Center for Biotechnology Information
+*
+*  This software/database is a "United States Government Work" under the
+*  terms of the United States Copyright Act.  It was written as part of
+*  the author's official duties as a United States Government employee and
+*  thus cannot be copyrighted.  This software/database is freely available
+*  to the public for use. The National Library of Medicine and the U.S.
+*  Government have not placed any restriction on its use or reproduction.
+*
+*  Although all reasonable efforts have been taken to ensure the accuracy
+*  and reliability of the software and data, the NLM and the U.S.
+*  Government do not and cannot warrant the performance or results that
+*  may be obtained by using this software or data. The NLM and the U.S.
+*  Government disclaim all warranties, express or implied, including
+*  warranties of performance, merchantability or fitness for any particular
+*  purpose.
+*
+*  Please cite the author in any work or product based on this material.
+*
+* ===========================================================================
+*
+* Author:  Aaron Ucko, NCBI
+*          Mati Shomrat
+*
+* File Description:
+*
+*/
+
+/// @file flat_gff_formatter.hpp
+/// Flat formatter for Generic Feature Format (incl. Gene Transfer Format)
+///
+/// These formats are somewhat loosely defined (for the record, at
+/// http://www.sanger.ac.uk/Software/formats/GFF/GFF_Spec.shtml and
+/// http://genes.cs.wustl.edu/GTF2.html respectively) so we default to
+/// GenBank/DDBJ/EMBL keys and qualifiers except as needed for GTF
+/// compatibility.
+
+#include <corelib/ncbistd.hpp>
+#include <objects/seqfeat/Seq_feat.hpp>
+#include <objtools/format/item_formatter.hpp>
+
+/** @addtogroup Miscellaneous
+ *
+ * @{
+ */
+
+
+BEGIN_NCBI_SCOPE
+BEGIN_SCOPE(objects)
+
+
+class CFlatFeature;
+
+
+class CGFFFormatter : public CFlatItemFormatter
+{
+public:
+    enum EGFFFlags {
+        fGTFCompat = 0x1, ///< Represent CDSs (and exons) per GTF.
+        fGTFOnly   = 0x3, ///< Omit all other features.
+        fShowSeq   = 0x4, ///< Show the actual sequence in a "##" comment.
+    };
+    typedef int TGFFFlags; ///< Binary OR of EGFFFlags
+
+    CGFFFormatter(void);
+
+    virtual void Start       (IFlatTextOStream& text_os);
+    virtual void StartSection(IFlatTextOStream& text_os);
+    virtual void EndSection  (IFlatTextOStream& text_os);
+
+    virtual void FormatLocus(const CLocusItem& locus, IFlatTextOStream& text_os);
+    virtual void FormatDate(const CDateItem& date, IFlatTextOStream& text_os);
+    virtual void FormatFeature(const CFeatureItemBase& feat, IFlatTextOStream& text_os);
+    virtual void FormatBasecount(const CBaseCountItem& bc, IFlatTextOStream& text_os);
+    virtual void FormatSequence(const CSequenceItem& seq, IFlatTextOStream& text_os);
+
+private:
+    string x_GetGeneID(const CFlatFeature& feat, const string& gene_name, CFFContext& ctx) const;
+    string x_GetSourceName(CFFContext& ctx) const;
+    void   x_AddFeature(list<string>& l, const CSeq_loc& loc,
+                        const string& source, const string& key,
+                        const string& score, int frame, const string& attrs,
+                        bool gtf, CFFContext& ctx) const;
+
+    mutable TGFFFlags           m_GFFFlags;
+    //CRef<IFlatTextOStream> m_Stream;
+    mutable string              m_SeqType;
+    mutable string              m_EndSequence;
+    /// Taken from head
+    mutable string              m_Date;
+    mutable CSeq_inst::TStrand  m_Strandedness;
+
+    typedef vector<CConstRef<CSeq_feat> > TFeatVec;
+    mutable map<string, TFeatVec>  m_Genes;
+};
+
+
+END_SCOPE(objects)
+END_NCBI_SCOPE
+
+
+/* @} */
+
+
+/*
+* ===========================================================================
+*
+* $Log$
+* Revision 1.1  2004/01/14 15:59:50  shomrat
+* Initial Revision
+*
+*
+* ===========================================================================
+*/
+
+#endif  /* OBJTOOLS_FORMAT___GFF_FORMATTER__HPP */
---
+++ include/objtools/format/items/ctrl_items.hpp
@@ -0,0 +1,144 @@
+#ifndef OBJTOOLS_FORMAT_ITEMS___CTRL_ITEM__HPP
+#define OBJTOOLS_FORMAT_ITEMS___CTRL_ITEM__HPP
+
+/*  $Id$
+* ===========================================================================
+*
+*                            PUBLIC DOMAIN NOTICE
+*               National Center for Biotechnology Information
+*
+*  This software/database is a "United States Government Work" under the
+*  terms of the United States Copyright Act.  It was written as part of
+*  the author's official duties as a United States Government employee and
+*  thus cannot be copyrighted.  This software/database is freely available
+*  to the public for use. The National Library of Medicine and the U.S.
+*  Government have not placed any restriction on its use or reproduction.
+*
+*  Although all reasonable efforts have been taken to ensure the accuracy
+*  and reliability of the software and data, the NLM and the U.S.
+*  Government do not and cannot warrant the performance or results that
+*  may be obtained by using this software or data. The NLM and the U.S.
+*  Government disclaim all warranties, express or implied, including
+*  warranties of performance, merchantability or fitness for any particular
+*  purpose.
+*
+*  Please cite the author in any work or product based on this material.
+*
+* ===========================================================================
+*
+* Author: Mati Shomrat
+*
+* File Description:
+*   item representing end of section line (slash line)
+*   
+*/
+#include <corelib/ncbistd.hpp>
+
+#include <objtools/format/items/item_base.hpp>
+#include <objtools/format/formatter.hpp>
+
+
+BEGIN_NCBI_SCOPE
+BEGIN_SCOPE(objects)
+
+
+class CFFContext;
+
+
+class CCtrlItem : public CFlatItem
+{
+public:
+    CCtrlItem(CFFContext& ctx) : CFlatItem(ctx) {}
+    
+protected:
+    void x_GatherInfo(CFFContext& ctx) {}
+};
+
+
+///////////////////////////////////////////////////////////////////////////
+//
+// START
+//
+// Signals the start of the data
+
+class CStartItem : public CCtrlItem
+{
+public:
+    CStartItem(CFFContext& ctx) : CCtrlItem(ctx) {}
+    void Format(IFormatter& f, IFlatTextOStream& text_os) const {
+        f.Start(text_os);
+    }
+    
+private:
+    void x_GatherInfo(CFFContext& ctx) {}
+};
+
+
+///////////////////////////////////////////////////////////////////////////
+//
+// START SECTION
+// 
+// Signals the begining of a new section
+
+class CStartSectionItem : public CCtrlItem
+{
+public:
+    CStartSectionItem(CFFContext& ctx) : CCtrlItem(ctx) {}
+    void Format(IFormatter& f, IFlatTextOStream& text_os) const {
+        f.StartSection(text_os);
+    }
+};
+
+
+///////////////////////////////////////////////////////////////////////////
+//
+// END SECTION
+//
+// Signals the end of a section
+
+class CEndSectionItem : public CCtrlItem
+{
+public:
+    CEndSectionItem(CFFContext& ctx) : CCtrlItem(ctx) {}
+    void Format(IFormatter& f, IFlatTextOStream& text_os) const {
+        f.EndSection(text_os);
+    }
+};
+
+
+///////////////////////////////////////////////////////////////////////////
+//
+// END
+//
+// Signals the termination of data
+
+class CEndItem : public CCtrlItem
+{
+public:
+    CEndItem(CFFContext& ctx) : CCtrlItem(ctx) {}
+    void Format(IFormatter& f, IFlatTextOStream& text_os) const {
+        f.End(text_os);
+    }
+};
+
+
+END_SCOPE(objects)
+END_NCBI_SCOPE
+
+
+/*
+* ===========================================================================
+*
+* $Log$
+* Revision 1.1  2004/01/14 15:57:49  shomrat
+* Initial Revision
+*
+* Revision 1.1  2003/12/17 19:46:33  shomrat
+* Initial revision (adapted from flat lib)
+*
+*
+* ===========================================================================
+*/
+
+
+#endif  /* OBJTOOLS_FORMAT_ITEMS___CTRL_ITEM__HPP */
