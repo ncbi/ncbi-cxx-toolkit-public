92fdcd5d7c0ad67cb8b75d188f65ad67323cd81d
gorelenk <gorelenk@ncbi.nlm.nih.gov>
2004-02-04 23:36:45 +0000

    Initial revision.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@13348 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ src/app/project_tree_builder/proj_src_resolver.cpp
@@ -0,0 +1,129 @@
+/* $Id$
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Author:  Viatcheslav Gorelenkov
+ *
+ */
+
+#include <app/project_tree_builder/proj_src_resolver.hpp>
+#include <app/project_tree_builder/proj_builder_app.hpp>
+
+BEGIN_NCBI_SCOPE
+
+// collect include statements and resolve to abs pathes 
+static void s_GetMakefileIncludes(const string& applib_mfilepath,
+                                  const string& source_base_dir,
+                                  list<string>* includes)
+{
+    includes->clear();
+
+    CNcbiIfstream ifs(applib_mfilepath.c_str(), 
+                      IOS_BASE::in | IOS_BASE::binary);
+    if ( !ifs )
+        NCBI_THROW(CProjBulderAppException, eFileOpen, applib_mfilepath);
+
+    string strline;
+    while ( getline(ifs, strline) ) {
+        const string include_token("include ");
+        strline = NStr::TruncateSpaces(strline);
+        if ( NStr::StartsWith(strline, include_token) ) {
+            string include = strline.substr(include_token.length());
+            
+            const string srcdir_token("$(srcdir)");
+            if (include.find(srcdir_token) == 0) {
+                include = include.substr(srcdir_token.length());
+                include = CDirEntry::ConcatPath(source_base_dir, include);
+                include = CDirEntry::NormalizePath(include);
+            }
+            includes->push_back(include);
+        }
+    }
+}
+
+
+//-----------------------------------------------------------------------------
+CProjSRCResolver::CProjSRCResolver(const string&       applib_mfilepath,
+                             const string&       source_base_dir,
+                             const list<string>& sources_src)
+ :m_MakefilePath    (applib_mfilepath),
+  m_m_SourcesBaseDir(source_base_dir),
+  m_SourcesSrc      (sources_src)
+{
+}
+    
+
+CProjSRCResolver::~CProjSRCResolver(void)
+{
+}
+
+
+void CProjSRCResolver::ResolveTo(list<string>* sources_dst)
+{
+    sources_dst->clear();
+
+    ITERATE(list<string>, p, m_SourcesSrc) {
+        const string& src = *p;
+        if ( !CSymResolver::IsDefine(src) ) {
+            sources_dst->push_back(src);
+        } else {
+            PrepereResolver();
+            string src_define = FilterDefine(src);
+            list<string> resolved_src_defines;
+            m_Resolver.Resolve(src_define, &resolved_src_defines);
+
+            copy(resolved_src_defines.begin(),
+                 resolved_src_defines.end(),
+                 back_inserter(*sources_dst));
+        }
+    }
+}
+
+
+void CProjSRCResolver::PrepereResolver(void)
+{
+    if ( !m_Resolver.IsEmpty() )
+        return;
+
+    list<string> include_makefiles;
+    s_GetMakefileIncludes(m_MakefilePath, 
+                          m_m_SourcesBaseDir, &include_makefiles);
+
+    CSymResolver::LoadFrom(m_MakefilePath, &m_Resolver);
+    ITERATE(list<string>, p, include_makefiles) {
+        const string& makefile_path = *p;
+        m_Resolver += CSymResolver(makefile_path);
+    }
+}
+
+END_NCBI_SCOPE
+
+/*
+ * ===========================================================================
+ * $Log$
+ * Revision 1.1  2004/02/04 23:36:45  gorelenk
+ * Initial revision.
+ *
+ * ===========================================================================
+ */
