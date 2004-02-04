7014d86331d3bf0e7e910a89a5f69781027011c3
Anatoliy Kuznetsov <kuznets@ncbi.nlm.nih.gov>
2004-02-04 19:16:50 +0000

    Fixed compilation bug (Workshop)
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@13333 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/bdb/test/test_bdb.cpp
+++ src/bdb/test/test_bdb.cpp
@@ -115,7 +115,8 @@ void ValidateRecord(const T& dbf1, unsigned int id)
 
     int idata1 = dbf1.idata;
     unsigned int id_key = dbf1.IdKey;
-    string s = dbf1.str;
+    string s;
+    s = dbf1.str;
     int i21 = dbf1.i2;
 
     assert(s == string(buf));
@@ -1565,6 +1566,9 @@ int main(int argc, const char* argv[])
 /*
  * ===========================================================================
  * $Log$
+ * Revision 1.36  2004/02/04 19:16:50  kuznets
+ * Fixed compilation bug (Workshop)
+ *
  * Revision 1.35  2004/02/04 17:05:47  kuznets
  * Minor fix (compilation).
  *
