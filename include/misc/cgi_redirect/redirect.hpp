1cf257fee5e93c39c9fd66b73c5338d31e7230f8
Vladimir Ivanov <ivanov@ncbi.nlm.nih.gov>
2004-02-09 17:58:13 +0000

    Added and changed comments (by Denis Vakatov)
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@13410 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/misc/cgi_redirect/redirect.hpp
+++ include/misc/cgi_redirect/redirect.hpp
@@ -52,32 +52,32 @@ BEGIN_NCBI_SCOPE
 ///
 /// Defines class for CGI redirection.
 ///
-/// CCgiRedirectApplication nherits its basic functionality from
-/// CCgiApplication and defines additional method for remapping cgi entries.
+/// CCgiRedirectApplication inherits its basic functionality from
+/// CCgiApplication and defines additional method for remapping CGI entries.
 
 class NCBI_XCGI_EXPORT CCgiRedirectApplication: public CCgiApplication
 {
     typedef CCgiApplication CParent;
 public:
-    // 
     virtual void Init(void);
     virtual int  ProcessRequest(CCgiContext& ctx);
 
 public:
-    /// Remap CGI entries.
+    /// Remap CGI entries for the redirection.
     ///
-    /// Default implementation use registry file to obtain rules for
-    /// entries remapping (see begin of the file for registry section
-    /// and parameters description). New entries will be placed into
-    /// "new_entries" accordingly received rules. It rules is not defined,
-    /// that each old entrie just copies into new one.
+    /// This default implementation uses registry file to obtain rules for
+    /// the entries' remapping (see details in this class's description)
+    /// New entries will be placed into "new_entries" according to these rules.
+    /// If there is no rule defined for an entry, then the entry will be copied
+    /// to "new_entries" as is, unchanged.
     ///
     /// @param ctx
-    ///   Current CGI context. Can be used to get original entries and
-    ///   server context. 
+    ///   Current CGI context.
+    ///   Can be used to get original entries and server context. 
     /// @param new_entries
-    ///   Storage for new CGI entries. These entries will be used insteed of
-    ///   original entries to generate redirected request.
+    ///   Storage for the new, remapped CGI entries. Initially, it is empty,
+    ///   and this method should fill it up. These entries will then be used
+    ///   instead of original entries to generate request(URL) for the redirection.
     /// @return
     ///   Reference to "new_entries" parameter.
     /// @sa
@@ -86,10 +86,10 @@ public:
 
 protected:
     const CHTMLPage& GetPage(void) const;
-    CHTMLPage& GetPage(void);
+    CHTMLPage&       GetPage(void);
 
 private:
-    CHTMLPage m_Page;  ///< HTML page used to print out redirect information.
+    CHTMLPage m_Page;  ///< HTML page used to send back the redirect information.
 };
 
 
@@ -102,6 +102,9 @@ END_NCBI_SCOPE
 /*
  * ===========================================================================
  * $Log$
+ * Revision 1.2  2004/02/09 17:58:13  ivanov
+ * Added and changed comments (by Denis Vakatov)
+ *
  * Revision 1.1  2004/02/09 17:10:19  ivanov
  * Initial revision
  *
