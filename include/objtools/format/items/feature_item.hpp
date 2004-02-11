63cfdb37dc638202d6517f2bba54e3cd3cffbce4
shomrat <shomrat@ncbi.nlm.nih.gov>
2004-02-11 16:38:51 +0000

    added methods for gathering and formatting of source features
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@13493 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/objtools/format/items/feature_item.hpp
+++ include/objtools/format/items/feature_item.hpp
@@ -94,24 +94,26 @@ public:
     }
     bool operator<(const CFeatureItemBase& f2) const 
         { return m_Feat->Compare(*f2.m_Feat, *m_Loc, *f2.m_Loc) < 0; }
-    string GetKey(void) const
+
+    const CSeq_feat& GetFeat(void)  const { return *m_Feat; }
+    const CSeq_loc&  GetLoc(void)   const { return *m_Loc; }
+    virtual string GetKey(void) const
         { return m_Feat->GetData().GetKey(CSeqFeatData::eVocabulary_genbank); }
-    const CSeq_loc&  GetLoc(void)  const { return *m_Loc; }
-    const CSeq_feat& GetFeat(void) const { return *m_Feat; }
 
 protected:
+
+    // constructor
     CFeatureItemBase(const CSeq_feat& feat, CFFContext& ctx,
                      const CSeq_loc* loc = 0)
-        : CFlatItem(ctx), m_Feat(&feat), m_Context(&ctx),
-          m_Loc(loc ? loc : &feat.GetLocation())
-        { }
-    void x_GatherInfo(CFFContext& ctx) {}
-    virtual void x_AddQuals   (void) const = 0;
+        : CFlatItem(ctx), m_Feat(&feat), m_Loc(loc ? loc : &feat.GetLocation())
+    {}
+
+    virtual void x_AddQuals   (CFFContext& ctx) const = 0;
     virtual void x_FormatQuals(void) const = 0;
 
-    CConstRef<CSeq_feat>        m_Feat;
-    mutable CRef<CFFContext>  m_Context;
-    mutable CConstRef<CSeq_loc> m_Loc;
+    CConstRef<CSeq_feat>    m_Feat;
+    CConstRef<CSeq_loc>     m_Loc;
+
     mutable CRef<CFlatFeature>  m_FF; ///< populated on demand then cached here
 };
 
@@ -119,19 +121,26 @@ protected:
 class CFeatureItem : public CFeatureItemBase
 {
 public:
+    // constructors
     CFeatureItem(const CSeq_feat& feat, CFFContext& ctx,
                      const CSeq_loc* loc = 0, bool is_product = false)
         : CFeatureItemBase(feat, ctx, loc), m_IsProduct(is_product)
-        { }
+    {
+        x_GatherInfo(ctx);
+    }
     CFeatureItem(const CMappedFeat& feat, CFFContext& ctx,
                      const CSeq_loc* loc = 0, bool is_product = false)
         : CFeatureItemBase(feat.GetOriginalFeature(), ctx,
                            loc ? loc : &feat.GetLocation()),
           m_IsProduct(is_product)
-        { }
+    {
+        x_GatherInfo(ctx);
+    }
 
 private:
-    void x_AddQuals(void) const;
+    void x_GatherInfo(CFFContext& ctx);
+
+    void x_AddQuals(CFFContext& ctx)       const;
     void x_AddQuals(const CGene_ref& gene) const;
     void x_AddQuals(const CCdregion& cds)  const;
     void x_AddQuals(const CProt_ref& prot) const;
@@ -143,10 +152,10 @@ private:
     void x_ImportQuals(const CSeq_feat::TQual& quals) const;
 
     void x_FormatQuals   (void) const;
-    void x_FormatQual    (EFeatureQualifier slot, const string& name,
-                          IFlatQVal::TFlags flags = 0) const;
-    void x_FormatNoteQual(EFeatureQualifier slot, const string& name,
-                          IFlatQVal::TFlags flags = 0) const
+    void x_FormatQual(EFeatureQualifier slot, const string& name,
+        IFlatQVal::TFlags flags = 0) const;
+    void x_FormatNoteQual(EFeatureQualifier slot, const string& name, 
+                          IFlatQVal::TFlags flags = 0) const 
         { x_FormatQual(slot, name, flags | IFlatQVal::fIsNote); }
 
     typedef multimap<EFeatureQualifier, CConstRef<IFlatQVal> > TQuals;
@@ -159,34 +168,52 @@ private:
 class CSourceFeatureItem : public CFeatureItemBase
 {
 public:
+    typedef CRange<TSeqPos> TRange;
+
+    CSourceFeatureItem(const CBioSource& src, TRange range, CFFContext& ctx);
     CSourceFeatureItem(const CSeq_feat& feat, CFFContext& ctx,
                            const CSeq_loc* loc = 0)
         : CFeatureItemBase(feat, ctx, loc), m_WasDesc(false)
-        { }
+    {
+        x_GatherInfo(ctx);
+    }
     CSourceFeatureItem(const CMappedFeat& feat, CFFContext& ctx,
                            const CSeq_loc* loc = 0)
         : CFeatureItemBase(feat.GetOriginalFeature(), ctx,
                            loc ? loc : &feat.GetLocation()),
           m_WasDesc(false)
-        { }
-    CSourceFeatureItem(const CBioSource& src, CFFContext& ctx);
-    CSourceFeatureItem(const COrg_ref& org,   CFFContext& ctx);
+    {
+        x_GatherInfo(ctx);
+    }
+
+    bool WasDesc(void) const { return m_WasDesc; }
+    const CBioSource& GetSource(void) const {
+        return m_Feat->GetData().GetBiosrc();
+    }
+    string GetKey(void) const { return "source"; }
 
 private:
-    void x_AddQuals(void)                  const;
-    void x_AddQuals(const CBioSource& src) const;
-    void x_AddQuals(const COrg_ref&   org) const;
+    void x_GatherInfo(CFFContext& ctx) {
+        x_AddQuals(ctx);
+    }
+
+    void x_AddQuals(CFFContext& ctx)       const;
+    void x_AddQuals(const CBioSource& src, CFFContext& ctx) const;
+    void x_AddQuals(const COrg_ref& org, CFFContext& ctx) const;
 
     // XXX - massage slot as necessary and perhaps sanity-check value's type
     void x_AddQual (ESourceQualifier slot, const IFlatQVal* value) const
         { m_Quals.insert(TQuals::value_type(slot,CConstRef<IFlatQVal>(value))); }
 
     void x_FormatQuals   (void) const;
-    void x_FormatQual    (ESourceQualifier slot, const string& name,
-                          IFlatQVal::TFlags flags = 0) const;
+    void x_FormatGBNoteQuals(void) const;
+    void x_FormatNoteQuals(void) const;
+    void x_FormatQual(ESourceQualifier slot, const string& name,
+        CFlatFeature::TQuals& qvec, IFlatQVal::TFlags flags = 0) const;
     void x_FormatNoteQual(ESourceQualifier slot, const string& name,
-                          IFlatQVal::TFlags flags = 0) const
-        { x_FormatQual(slot, name, flags | IFlatQVal::fIsNote); }
+            CFlatFeature::TQuals& qvec, IFlatQVal::TFlags flags = 0) const {
+        x_FormatQual(slot, name, qvec, flags | IFlatQVal::fIsNote); 
+    }
 
     typedef multimap<ESourceQualifier, CConstRef<IFlatQVal> > TQuals;
 
@@ -202,6 +229,9 @@ END_NCBI_SCOPE
 * ===========================================================================
 *
 * $Log$
+* Revision 1.3  2004/02/11 16:38:51  shomrat
+* added methods for gathering and formatting of source features
+*
 * Revision 1.2  2004/01/14 15:57:23  shomrat
 * removed commented code
 *
