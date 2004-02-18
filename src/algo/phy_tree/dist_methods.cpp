ff5e0259d91d2dd707848b875dc8ce2b7e2f9cd2
Joshua (Josh) Cherry <jcherry@ncbi.nlm.nih.gov>
2004-02-18 22:57:33 +0000

    Reflect change to util/creaders/alignment_file.[ch]pp
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@13679 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/algo/phy_tree/dist_methods.cpp
+++ src/algo/phy_tree/dist_methods.cpp
@@ -321,7 +321,7 @@ void CDistMethods::Divergence(const CAlnVec& avec_in, TMatrix& result)
 }
 
 
-void CDistMethods::Divergence(const CAlignment& aln, TMatrix& result)
+void CDistMethods::Divergence(const CAlignmentFile& aln, TMatrix& result)
 {
     int nseqs = aln.GetSeqs().size();
     result.Resize(nseqs, nseqs);
@@ -341,6 +341,9 @@ END_NCBI_SCOPE
 /*
  * ===========================================================================
  * $Log$
+ * Revision 1.4  2004/02/18 22:57:33  jcherry
+ * Reflect change to util/creaders/alignment_file.[ch]pp
+ *
  * Revision 1.3  2004/02/10 23:07:46  ucko
  * +<math.h> for log()
  *
