6d8d53d962f9370a08feceff3c680f0e65768fdd
shomrat <shomrat@ncbi.nlm.nih.gov>
2004-02-19 18:04:01 +0000

    Implemented Contig item
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@13736 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/format/contig_item.cpp
+++ src/objtools/format/contig_item.cpp
@@ -31,9 +31,14 @@
 */
 #include <corelib/ncbistd.hpp>
 
+#include <objects/seq/Delta_ext.hpp>
+#include <objects/seq/Delta_seq.hpp>
+#include <objects/seq/Seq_literal.hpp>
+
 #include <objtools/format/formatter.hpp>
 #include <objtools/format/text_ostream.hpp>
 #include <objtools/format/items/contig_item.hpp>
+#include <objtools/format/items/flat_seqloc.hpp>
 #include <objtools/format/context.hpp>
 
 
@@ -42,15 +47,15 @@ BEGIN_SCOPE(objects)
 
 
 CContigItem::CContigItem(CFFContext& ctx) :
-    CFlatItem(ctx)
+    CFlatItem(ctx), m_Loc(new CSeq_loc)
 {
+    x_GatherInfo(ctx);
 }
 
 
 void CContigItem::Format
 (IFormatter& formatter,
  IFlatTextOStream& text_os) const
-
 {
     formatter.FormatContig(*this, text_os);
 }
@@ -58,7 +63,42 @@ void CContigItem::Format
 
 void CContigItem::x_GatherInfo(CFFContext& ctx)
 {
-    // !!!
+    typedef CRef<CSeq_loc>  TLoc;
+
+    CSeq_loc_mix::Tdata& data = m_Loc->SetMix().Set();
+    const CSeq_inst& inst = ctx.GetActiveBioseq().GetInst();
+    if ( !inst.CanGetExt() ) {
+        return;
+    }
+    const CSeq_ext& ext = inst.GetExt();
+
+    if ( ctx.IsSegmented()  &&  ctx.HasParts() ) {
+        ITERATE (CSeg_ext::Tdata, it, ext.GetSeg().Get()) {
+            data.push_back(*it);
+        }
+    } else if ( ctx.IsDelta() ) {
+        ITERATE (CDelta_ext::Tdata, it, ext.GetDelta().Get()) {
+            if ( (*it)->IsLoc() ) {
+                CSeq_loc& l = const_cast<CSeq_loc&>((*it)->GetLoc());
+                CRef<CSeq_loc> lr(&l);
+                data.push_back(lr);
+            } else {  // literal
+                const CSeq_literal& lit = (*it)->GetLiteral();
+                TSeqPos len = lit.CanGetLength() ? lit.GetLength() : 0;
+                if ( lit.CanGetSeq_data() ) {
+                    // data with 0 length => gap
+                    if ( len == 0 ) {
+                        data.push_back(TLoc(new CFlatGapLoc(0)));
+                    } else {
+                        // !!! don't know what to do here
+                    }
+                } else {
+                    // no data => gap
+                    data.push_back(TLoc(new CFlatGapLoc(len)));
+                }
+            }
+        }
+    }
 }
 
 
@@ -70,6 +110,9 @@ END_NCBI_SCOPE
 * ===========================================================================
 *
 * $Log$
+* Revision 1.3  2004/02/19 18:04:01  shomrat
+* Implemented Contig item
+*
 * Revision 1.2  2003/12/18 17:43:32  shomrat
 * context.hpp moved
 *
