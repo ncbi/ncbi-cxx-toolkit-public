bfc9cb84cd60af04756c3cefe713dd7c06556236
Ilya Dondoshansky <dondosha@ncbi.nlm.nih.gov>
2004-02-24 20:31:39 +0000

    Typo fix; removed irrelevant CVS log comments
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@13845 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/algo/blast/api/db_blast.cpp
+++ src/algo/blast/api/db_blast.cpp
@@ -55,7 +55,7 @@ BEGIN_SCOPE(blast)
 
 CDbBlast::CDbBlast(const TSeqLocVector& queries, BlastSeqSrc* bssp,
                    EProgram p)
-    : , mi_bQuerySetUpDone(false), m_pSeqSrc(bssp) 
+    : mi_bQuerySetUpDone(false), m_pSeqSrc(bssp) 
 {
     m_tQueries = queries;
     mi_pScoreBlock = NULL;
@@ -314,6 +314,9 @@ END_NCBI_SCOPE
  * ===========================================================================
  *
  * $Log$
+ * Revision 1.15  2004/02/24 20:31:39  dondosha
+ * Typo fix; removed irrelevant CVS log comments
+ *
  * Revision 1.14  2004/02/24 18:19:35  dondosha
  * Removed lookup options argument from call to BLAST_MainSetUp
  *
@@ -355,110 +358,5 @@ END_NCBI_SCOPE
  *
  * Revision 1.1  2003/10/29 22:37:36  dondosha
  * Database BLAST search class methods
- *
- * Revision 1.33  2003/09/11 17:45:03  camacho
- * Changed CBlastOption -> CBlastOptions
- *
- * Revision 1.32  2003/09/10 20:01:30  dondosha
- * Use lookup_wrap.h
- *
- * Revision 1.31  2003/09/09 20:31:13  camacho
- * Add const type qualifier
- *
- * Revision 1.30  2003/09/09 15:18:02  camacho
- * Fix includes
- *
- * Revision 1.29  2003/09/09 12:55:09  camacho
- * Moved setup member functions to blast_setup_cxx.cpp
- *
- * Revision 1.28  2003/09/03 19:36:58  camacho
- * Fix include path for blast_setup.hpp, code clean up
- *
- * Revision 1.27  2003/08/28 22:42:54  camacho
- * Change BLASTGetSequence signature
- *
- * Revision 1.26  2003/08/28 17:37:06  camacho
- * Fix memory leaks, properly reset structure wrappers
- *
- * Revision 1.25  2003/08/28 15:48:23  madden
- * Make buf one longer for sentinel byte
- *
- * Revision 1.24  2003/08/25 17:15:49  camacho
- * Removed redundant typedef
- *
- * Revision 1.23  2003/08/19 22:12:47  dondosha
- * Cosmetic changes
- *
- * Revision 1.22  2003/08/19 20:27:06  dondosha
- * EProgram enum type is no longer part of CBlastOptions class
- *
- * Revision 1.21  2003/08/19 13:46:13  dicuccio
- * Added 'USING_SCOPE(objects)' to .cpp files for ease of reading implementation.
- *
- * Revision 1.20  2003/08/18 22:17:36  camacho
- * Renaming of SSeqLoc members
- *
- * Revision 1.19  2003/08/18 20:58:57  camacho
- * Added blast namespace, removed *__.hpp includes
- *
- * Revision 1.18  2003/08/18 19:58:50  dicuccio
- * Fixed compilation errors after change from pair<> to SSeqLoc
- *
- * Revision 1.17  2003/08/18 17:07:41  camacho
- * Introduce new SSeqLoc structure (replaces pair<CSeq_loc, CScope>).
- * Change in function to read seqlocs from files.
- *
- * Revision 1.16  2003/08/15 15:59:13  dondosha
- * Changed call to BLAST_Results2CSeqAlign according to new prototype
- *
- * Revision 1.15  2003/08/12 19:21:08  dondosha
- * Subject id argument to BLAST_Results2CppSeqAlign is now a simple pointer, allowing it to be NULL
- *
- * Revision 1.14  2003/08/11 19:55:55  camacho
- * Early commit to support query concatenation and the use of multiple scopes.
- * Compiles, but still needs work.
- *
- * Revision 1.13  2003/08/11 15:23:59  dondosha
- * Renamed conversion functions between BlastMaskLoc and CSeqLoc; added algo/blast/core to headers from core BLAST library
- *
- * Revision 1.12  2003/08/11 14:00:41  dicuccio
- * Indenting changes.  Fixed use of C++ namespaces (USING_SCOPE(objects) inside of
- * BEGIN_NCBI_SCOPE block)
- *
- * Revision 1.11  2003/08/08 19:43:07  dicuccio
- * Compilation fixes: #include file rearrangement; fixed use of 'list' and
- * 'vector' as variable names; fixed missing ostrea<< for __int64
- *
- * Revision 1.10  2003/08/01 17:40:56  dondosha
- * Use renamed functions and structures from local blastkar.h
- *
- * Revision 1.9  2003/07/31 19:45:33  camacho
- * Eliminate Ptr notation
- *
- * Revision 1.8  2003/07/30 19:58:02  coulouri
- * use ListNode
- *
- * Revision 1.7  2003/07/30 15:00:01  camacho
- * Do not use Malloc/MemNew/MemFree
- *
- * Revision 1.6  2003/07/29 14:15:12  camacho
- * Do not use MemFree/Malloc
- *
- * Revision 1.5  2003/07/28 22:20:17  camacho
- * Removed unused argument
- *
- * Revision 1.4  2003/07/23 21:30:40  camacho
- * Calls to options member functions
- *
- * Revision 1.3  2003/07/15 19:21:36  camacho
- * Use correct strands and sequence buffer length
- *
- * Revision 1.2  2003/07/14 22:16:37  camacho
- * Added interface to retrieve masked regions
- *
- * Revision 1.1  2003/07/10 18:34:19  camacho
- * Initial revision
- *
- *
  * ===========================================================================
  */
