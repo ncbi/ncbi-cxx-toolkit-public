3f489fc11fdf0c4919411256f7a23fc88c64beb6
gorelenk <gorelenk@ncbi.nlm.nih.gov>
2004-02-26 18:35:17 +0000

    Added (_MSC_VER <= 1200) for definition of NCBI_PROMOTE - typename presents causes a compilation error on MSVC 6.0.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@13894 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/util/math/promote.hpp
+++ include/util/math/promote.hpp
@@ -80,10 +80,10 @@ public: \
 // this macro makes the syntax a little easier to understand when declaring a
 // promoted type
 //
-#ifdef NCBI_COMPILER_MSVC
-#define NCBI_PROMOTE(a,b) SPromoteTraits<a,b>::TPromote
+#if defined(NCBI_COMPILER_MSVC) && (_MSC_VER <= 1200)
+#  define NCBI_PROMOTE(a,b) SPromoteTraits<a,b>::TPromote
 #else
-#define NCBI_PROMOTE(a,b) typename SPromoteTraits<a,b>::TPromote
+# define NCBI_PROMOTE(a,b) typename SPromoteTraits<a,b>::TPromote
 #endif
 
 //
@@ -199,6 +199,10 @@ END_NCBI_SCOPE
 /*
  * ===========================================================================
  * $Log$
+ * Revision 1.4  2004/02/26 18:35:17  gorelenk
+ * Added (_MSC_VER <= 1200) for definition of NCBI_PROMOTE - typename presents
+ * causes a compilation error on MSVC 6.0.
+ *
  * Revision 1.3  2004/02/26 17:44:29  dicuccio
  * Restore preprocessor guard for 'typename' in SPromote<>
  *
