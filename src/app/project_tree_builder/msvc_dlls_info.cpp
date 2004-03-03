4f5d3220f53d8c1b5b468a80211ca45b85b7d280
gorelenk <gorelenk@ncbi.nlm.nih.gov>
2004-03-03 22:16:45 +0000

    Added implementation of class CMsvcDllsInfo.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@13982 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/app/project_tree_builder/msvc_dlls_info.cpp
+++ src/app/project_tree_builder/msvc_dlls_info.cpp
@@ -29,16 +29,74 @@
  */
 
 #include <app/project_tree_builder/msvc_dlls_info.hpp>
+#include <app/project_tree_builder/proj_builder_app.hpp>
+#include <app/project_tree_builder/msvc_prj_defines.hpp>
+#include <corelib/ncbistre.hpp>
 
 BEGIN_NCBI_SCOPE
 
-// TODO
+
+CMsvcDllsInfo::CMsvcDllsInfo(const string& file_path)
+{
+    CNcbiIfstream ifs(file_path.c_str(), IOS_BASE::in | IOS_BASE::binary);
+    if ( !ifs )
+        NCBI_THROW(CProjBulderAppException, eFileOpen, file_path);
+
+    m_Registry.Read(ifs);
+}
+
+
+void CMsvcDllsInfo::GetDllsList(list<string>* dlls_ids) const
+{
+    dlls_ids->clear();
+
+    string dlls_ids_str = 
+        m_Registry.GetString("DllBuild", "DLLs", "");
+    
+    NStr::Split(dlls_ids_str, LIST_SEPARATOR, *dlls_ids);
+}
+
+
+bool CMsvcDllsInfo::SDllInfo::IsEmpty(void) const
+{
+    return  m_Hosting.empty()        &&
+            m_ExportDefines.empty()  &&
+            m_Depends.empty();
+}
+ 
+       
+void CMsvcDllsInfo::SDllInfo::Clear(void)
+{
+    m_Hosting.clear();
+    m_ExportDefines.clear();
+    m_Depends.clear();
+}
+
+
+void CMsvcDllsInfo::GelDllInfo(const string& dll_id, SDllInfo* dll_info) const
+{
+    dll_info->Clear();
+
+    string hosting_str = m_Registry.GetString(dll_id, "Hosting", "");
+    NStr::Split(hosting_str, LIST_SEPARATOR, dll_info->m_Hosting);
+
+    string export_defines_str = 
+        m_Registry.GetString(dll_id, "ExportDefines", "");
+    NStr::Split(export_defines_str, LIST_SEPARATOR, dll_info->m_ExportDefines);
+
+    string depends_str = m_Registry.GetString(dll_id, "Dependencies", "");
+    NStr::Split(depends_str, LIST_SEPARATOR, dll_info->m_Depends);
+}
+
 
 END_NCBI_SCOPE
 
 /*
  * ===========================================================================
  * $Log$
+ * Revision 1.2  2004/03/03 22:16:45  gorelenk
+ * Added implementation of class CMsvcDllsInfo.
+ *
  * Revision 1.1  2004/03/03 17:07:14  gorelenk
  * Initial revision.
  *
