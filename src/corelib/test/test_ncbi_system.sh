33fcf7b6a7ea14782d502c23bc33bc7d9c973452
Vladimir Ivanov <ivanov@ncbi.nlm.nih.gov>
2004-03-12 14:45:14 +0000

    Added case branch for SunOS.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@14239 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/corelib/test/test_ncbi_system.sh
+++ src/corelib/test/test_ncbi_system.sh
@@ -16,20 +16,24 @@ echo "exit_code(MEM-test) = $test_mem"
 test $test_general -eq 0  ||  exit 1
 
 case "`uname -s`" in
+ SunOS )
+   # exit code 158 -- signal 30 (CPU time exceeded)
+   test $test_cpu -eq 255  -o  $test_cpu -eq 158  ||  exit 1
+   test $test_mem -eq 0    -o  $test_mem -eq 255  ||  exit 1
+   ;;
  Linux | FreeBSD )
    test $test_cpu -eq 137  ||  exit 1
-   test $test_mem -eq 0  -o  $test_mem -eq 255  ||  exit 1
+   test $test_mem -eq 0    -o  $test_mem -eq 255  ||  exit 1
    ;;
  Darwin )
    test $test_cpu -eq 255  ||  exit 1
    test $test_mem -eq 0    ||  exit 1
    ;;
  *CYGWIN* )
-   test $test_cpu -eq 3  -a  $test_mem -eq 3  ||  exit 1
+   test $test_cpu -eq 3    -o  $test_mem -eq 3    ||  exit 1
    ;;
  * )
    test $test_mem -eq 255  ||  exit 1
-   # exit code 158 -- signal 30 (CPU time exceeded)
    test $test_cpu -eq 255  -o  $test_cpu -eq 158  ||  exit 1
    ;;
 esac
