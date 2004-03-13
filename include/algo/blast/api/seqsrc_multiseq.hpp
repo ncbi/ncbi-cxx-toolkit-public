389ef0d4d145cb6ff40005422824f7b8181f220a
Ilya Dondoshansky <dondosha@ncbi.nlm.nih.gov>
2004-03-13 00:24:45 +0000

    C++ implementation of BlastSeqSrc for a set of sequences
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@14275 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ include/algo/blast/api/seqsrc_multiseq.hpp
@@ -0,0 +1,144 @@
+/*  $Id$
+* ===========================================================================
+*
+*                            PUBLIC DOMAIN NOTICE
+*               National Center for Biotechnology Information
+*
+*  This software/database is a "United States Government Work" under the
+*  terms of the United States Copyright Act.  It was written as part of
+*  the author's official duties as a United States Government employee and
+*  thus cannot be copyrighted.  This software/database is freely available
+*  to the public for use. The National Library of Medicine and the U.S.
+*  Government have not placed any restriction on its use or reproduction.
+*
+*  Although all reasonable efforts have been taken to ensure the accuracy
+*  and reliability of the software and data, the NLM and the U.S.
+*  Government do not and cannot warrant the performance or results that
+*  may be obtained by using this software or data. The NLM and the U.S.
+*  Government disclaim all warranties, express or implied, including
+*  warranties of performance, merchantability or fitness for any particular
+*  purpose.
+*
+*  Please cite the author in any work or product based on this material.
+*
+* ===========================================================================
+*
+* Author:  Ilya Dondoshansky
+*
+* File Description:
+*   Implementation of the BlastSeqSrc interface for a vector of sequence 
+*   locations.
+*
+*/
+
+#ifndef MULTISEQ_SRC_H
+#define MULTISEQ_SRC_H
+
+#include <algo/blast/core/blast_seqsrc.h>
+#include <algo/blast/core/blast_def.h>
+#include <algo/blast/core/blast_message.h>
+#include <algo/blast/api/blast_types.hpp>
+#include <algo/blast/api/blast_exception.hpp>
+
+BEGIN_NCBI_SCOPE
+USING_SCOPE(objects);
+//USING_SCOPE(blast);
+BEGIN_SCOPE(blast)
+
+/** Encapsulates the arguments needed to initialize multi-sequence source. */
+typedef struct MultiSeqSrcNewArgs {
+    TSeqLocVector seq_vector;
+    EProgram program;
+} MultiSeqSrcNewArgs;
+
+/** Contains information about all sequences in a set */
+class MultiSeqInfo : public CObject 
+{
+public: 
+    MultiSeqInfo(const TSeqLocVector& seq_vector, EProgram program);
+    ~MultiSeqInfo();
+    Uint4 GetMaxLength();
+    void SetMaxLength(Uint4 val);
+    Uint4 GetAvgLength();
+    void SetAvgLength(Uint4 val);
+    bool GetIsProtein();
+    Uint4 GetNumSeqs();
+    BLAST_SequenceBlk* GetSeqBlk(int index);
+    void* GetSeqId(int index);
+    void* GetSeqLoc(int index);
+    Blast_Message* GetErrorMessage();
+private:
+    // Passed from outside - not owned:
+    TSeqLocVector m_vSeqVector; ///< Vector of sequence locations
+    // Internal fields
+    bool m_ibIsProt; ///< Are these sequences protein or nucleotide? 
+    vector<BLAST_SequenceBlk*> m_ivSeqBlkVec; ///< Vector of sequence blocks
+    unsigned int m_iMaxLength; ///< Length of the longest sequence in this set
+    unsigned int m_iAvgLength; ///< Average length of sequences in this set
+    Blast_Message* m_icErrMsg; ///< Error message in case of set up failure
+};
+
+inline Uint4 MultiSeqInfo::GetMaxLength()
+{
+    return m_iMaxLength;
+}
+
+inline void MultiSeqInfo::SetMaxLength(Uint4 length)
+{
+    m_iMaxLength = length;
+}
+
+inline Uint4 MultiSeqInfo::GetAvgLength()
+{
+    return m_iAvgLength;
+}
+
+inline void MultiSeqInfo::SetAvgLength(Uint4 length)
+{
+    m_iAvgLength = length;
+}
+
+inline bool MultiSeqInfo::GetIsProtein()
+{
+    return m_ibIsProt;
+}
+
+inline Uint4 MultiSeqInfo::GetNumSeqs()
+{
+    return m_ivSeqBlkVec.size();
+}
+
+inline BLAST_SequenceBlk* MultiSeqInfo::GetSeqBlk(int index)
+{
+    return m_ivSeqBlkVec[index];
+}
+
+inline Blast_Message* MultiSeqInfo::GetErrorMessage()
+{
+    return m_icErrMsg;
+}
+
+/** Multi-sequence source constructor 
+ * @param seq_src BlastSeqSrc structure (already allocated) to populate [in]
+ * @param args Pointer to MultiSeqSrcNewArgs structure above [in]
+ * @return Updated bssp structure (with all function pointers initialized
+ */
+BlastSeqSrc* MultiSeqSrcNew(BlastSeqSrc* seq_src, void* args);
+
+/** Initialize the sequence source structure.
+ * @param seq_vector Vector of sequence locations [in]
+ * @param program Type of BLAST to be performed [in]
+ */
+BlastSeqSrc* 
+MultiSeqSrcInit(const TSeqLocVector& seq_vector, EProgram program);
+
+/** Multi sequence source destructor: frees its internal data structure and the
+ * BlastSeqSrc structure itself.
+ * @param seq_src BlastSeqSrc structure to free [in]
+ * @return NULL
+ */
+BlastSeqSrc* MultiSeqSrcFree(BlastSeqSrc* seq_src);
+
+END_SCOPE(blast)
+END_NCBI_SCOPE
+#endif /* MULTISEQ_SRC_H */
---
+++ src/algo/blast/api/seqsrc_multiseq.cpp
@@ -0,0 +1,437 @@
+/*  $Id$
+* ===========================================================================
+*
+*                            PUBLIC DOMAIN NOTICE
+*               National Center for Biotechnology Information
+*
+*  This software/database is a "United States Government Work" under the
+*  terms of the United States Copyright Act.  It was written as part of
+*  the author's official duties as a United States Government employee and
+*  thus cannot be copyrighted.  This software/database is freely available
+*  to the public for use. The National Library of Medicine and the U.S.
+*  Government have not placed any restriction on its use or reproduction.
+*
+*  Although all reasonable efforts have been taken to ensure the accuracy
+*  and reliability of the software and data, the NLM and the U.S.
+*  Government do not and cannot warrant the performance or results that
+*  may be obtained by using this software or data. The NLM and the U.S.
+*  Government disclaim all warranties, express or implied, including
+*  warranties of performance, merchantability or fitness for any particular
+*  purpose.
+*
+*  Please cite the author in any work or product based on this material.
+*
+* ===========================================================================
+*
+* Author:  Ilya Dondoshansky
+*
+* File Description:
+*   Implementation of the BlastSeqSrc interface for a vector of sequence 
+*   locations.
+*
+*/
+
+static char const rcsid[] = "$Id$";
+
+#include <objects/seqloc/Seq_id.hpp>
+#include <objmgr/util/sequence.hpp>
+#include <algo/blast/api/multiseq_src.hpp>
+#include <algo/blast/core/blast_def.h>
+#include "blast_setup.hpp"
+#include <algo/blast/api/blast_exception.hpp>
+
+
+BEGIN_NCBI_SCOPE
+USING_SCOPE(objects);
+BEGIN_SCOPE(blast)
+
+MultiSeqInfo::MultiSeqInfo(const TSeqLocVector& seq_vector, EProgram program)
+    : m_vSeqVector(seq_vector)
+{
+    m_ibIsProt = (program == eBlastp || program == eBlastx);
+    
+    try {
+        SetupSubjects(seq_vector, program, &m_ivSeqBlkVec, &m_iMaxLength);
+    } catch (const CBlastException& exptn) {
+        Blast_MessageWrite(&m_icErrMsg, BLAST_SEV_ERROR, exptn.GetErrCode(),
+                           1, exptn.GetErrCodeString());
+    }
+
+    // Do not set right away
+    m_iAvgLength = 0;
+}
+
+MultiSeqInfo::~MultiSeqInfo()
+{
+    NON_CONST_ITERATE(vector<BLAST_SequenceBlk*>, itr, m_ivSeqBlkVec) {
+        *itr = BlastSequenceBlkFree(*itr);
+    }
+    m_ivSeqBlkVec.clear();
+}
+
+void* MultiSeqInfo::GetSeqId(int index)
+{
+    CSeq_id* seqid = 
+        const_cast<CSeq_id*>(&sequence::GetId(*m_vSeqVector[index].seqloc,
+                                              m_vSeqVector[index].scope));
+
+    return (void*) seqid;
+}
+
+void* MultiSeqInfo::GetSeqLoc(int index)
+{
+    return (void*) &m_vSeqVector[index];
+}
+
+/** Retrieves the length of the longest sequence in the BlastSeqSrc.
+ * @param multiseq_handle Pointer to the structure containing sequences [in]
+ * @param ignoreme Unused by this implementation [in]
+ */
+static Int4 MultiSeqGetMaxLength(void* multiseq_handle, void* ignoreme)
+{
+    Int4 retval = 0;
+    Uint4 index;
+    MultiSeqInfo* seq_info = (MultiSeqInfo*) multiseq_handle;
+
+    ASSERT(seq_info);
+    
+    if ((retval = seq_info->GetMaxLength()) > 0)
+        return retval;
+
+    for (index=0; index<seq_info->GetNumSeqs(); ++index)
+        retval = MAX(retval, seq_info->GetSeqBlk(index)->length);
+    seq_info->SetMaxLength(retval);
+
+    return retval;
+}
+
+/** Retrieves the length of the longest sequence in the BlastSeqSrc.
+ * @param multiseq_handle Pointer to the structure containing sequences [in]
+ * @param ignoreme Unused by this implementation [in]
+ */
+static Int4 MultiSeqGetAvgLength(void* multiseq_handle, void* ignoreme)
+{
+    Int8 total_length = 0;
+    Uint4 num_seqs = 0;
+    Uint4 avg_length;
+    Uint4 index;
+    MultiSeqInfo* seq_info = (MultiSeqInfo*) multiseq_handle;
+
+    ASSERT(seq_info);
+
+    if ((avg_length = seq_info->GetAvgLength()) > 0)
+        return avg_length;
+
+    if ((num_seqs = seq_info->GetNumSeqs()) == 0)
+        return 0;
+    for (index = 0; index < num_seqs; ++index) 
+        total_length += (Int8) seq_info->GetSeqBlk(index)->length;
+    avg_length = (Uint4) (total_length / num_seqs);
+    seq_info->SetAvgLength(avg_length);
+
+    return avg_length;
+}
+
+/** Retrieves the number of sequences in the BlastSeqSrc.
+ * @param multiseq_handle Pointer to the structure containing sequences [in]
+ * @param ignoreme Unused by this implementation [in]
+ */
+static Int4 MultiSeqGetNumSeqs(void* multiseq_handle, void* ignoreme)
+{
+    MultiSeqInfo* seq_info = (MultiSeqInfo*) multiseq_handle;
+
+    ASSERT(seq_info);
+    return seq_info->GetNumSeqs();
+}
+
+/** Returns 0 as total length, indicating that this is NOT a database!
+ * @param multiseq_handle Pointer to the structure containing sequences [in]
+ * @param ignoreme Unused by this implementation [in]
+ */
+static Int8 MultiSeqGetTotLen(void* multiseq_handle, void* ignoreme)
+{
+    return 0;
+}
+
+/** Needed for completeness only.
+ * @param multiseq_handle Pointer to the structure containing sequences [in]
+ * @param ignoreme Unused by this implementation [in]
+ */
+static char* MultiSeqGetName(void* multiseq_handle, void* ignoreme)
+{
+    return NULL;
+}
+
+/** Needed for completeness only
+ * @param multiseq_handle Pointer to the structure containing sequences [in]
+ * @param ignoreme Unused by this implementation [in]
+ */
+static char* MultiSeqGetDefinition(void* multiseq_handle, void* ignoreme)
+{
+    return NULL;
+}
+
+/** Needed for completeness only.
+ * @param multiseq_handle Pointer to the structure containing sequences [in]
+ * @param ignoreme Unused by this implementation [in]
+ */
+static char* MultiSeqGetDate(void* multiseq_handle, void* ignoreme)
+{
+    return NULL;
+}
+
+/** Retrieves the date of the BLAST database.
+ * @param multiseq_handle Pointer to the structure containing sequences [in]
+ * @param ignoreme Unused by this implementation [in]
+ */
+static Boolean MultiSeqGetIsProt(void* multiseq_handle, void* ignoreme)
+{
+    MultiSeqInfo* seq_info = (MultiSeqInfo*) multiseq_handle;
+
+    ASSERT(seq_info);
+
+    return (Boolean) seq_info->GetIsProtein();
+}
+
+/** Retrieves the sequence meeting the criteria defined by its second argument.
+ * @param multiseq_handle Pointer to the structure containing sequences [in]
+ * @param args Pointer to GetSeqArg structure [in]
+ * @return return codes defined in blast_seqsrc.h
+ */
+static Int2 MultiSeqGetSequence(void* multiseq_handle, void* args)
+{
+    MultiSeqInfo* seq_info = (MultiSeqInfo*) multiseq_handle;
+    GetSeqArg* seq_args = (GetSeqArg*) args;
+    Int4 index;
+
+    ASSERT(seq_info);
+    ASSERT(args);
+
+    if (seq_info->GetNumSeqs() == 0 || !seq_args)
+        return BLAST_SEQSRC_ERROR;
+
+    index = seq_args->oid;
+
+    if (index >= (Int4) seq_info->GetNumSeqs())
+        return BLAST_SEQSRC_EOF;
+
+    BlastSequenceBlkCopy(&seq_args->seq, seq_info->GetSeqBlk(index));
+    /* If this is a nucleotide sequence, and it is the traceback stage, 
+       we need the uncompressed buffer, stored in the 'sequence_start' 
+       pointer. */
+    if (seq_args->encoding != BLASTP_ENCODING &&
+        seq_args->encoding != NCBI2NA_ENCODING) {
+        seq_args->seq->sequence = seq_args->seq->sequence_start + 1;
+    }
+    seq_args->seq->oid = index;
+    return BLAST_SEQSRC_SUCCESS;
+}
+
+/** Retrieves the sequence identifier given its index into the sequence vector.
+ * Client code is responsible for deallocating the return value. 
+ * @param multiseq_handle Pointer to the structure containing sequences [in]
+ * @param args Pointer to integer indicating ordinal id [in]
+ * @return Sequence id structure generated from ASN.1 spec, 
+ *         cast to a void pointer.
+ */
+static ListNode* MultiSeqGetSeqId(void* multiseq_handle, void* args)
+{
+    MultiSeqInfo* seq_info = (MultiSeqInfo*) multiseq_handle;
+    Int4 index;
+    ListNode* seqid_wrap;
+
+    ASSERT(seq_info);
+    ASSERT(args);
+
+    index = *((Int4*) args);
+
+    seqid_wrap = ListNodeAddPointer(NULL, BLAST_SEQSRC_CPP_SEQID, 
+                                    seq_info->GetSeqId(index));
+    return seqid_wrap;
+}
+
+/** Retrieves the sequence identifier in character string form.
+ * Client code is responsible for deallocating the return value. 
+ * @param multiseq_handle Pointer to the structure containing sequences [in]
+ * @param args Pointer to integer indicating index into the vector [in]
+ */
+static char* MultiSeqGetSeqIdStr(void* multiseq_handle, void* args)
+{
+    CSeq_id* seqid;
+    char *seqid_str = NULL;
+    ListNode* seqid_wrap;
+
+    ASSERT(args);
+    seqid_wrap = MultiSeqGetSeqId(multiseq_handle, args);
+    if (seqid_wrap->choice != BLAST_SEQSRC_CPP_SEQID)
+        return NULL;
+    seqid = (CSeq_id*) seqid_wrap->ptr;
+
+    if (seqid)
+        seqid_str = strdup(seqid->GetSeqIdString().c_str());
+
+    return seqid_str;
+}
+
+/** Retrieves the sequence identifier given its index into the sequence vector.
+ * Client code is responsible for deallocating the return value. 
+ * @param multiseq_handle Pointer to the structure containing sequences [in]
+ * @param args Pointer to integer indicating ordinal id [in]
+ * @return Sequence id structure generated from ASN.1 spec, 
+ *         cast to a void pointer.
+ */
+static ListNode* MultiSeqGetSeqLoc(void* multiseq_handle, void* args)
+{
+    MultiSeqInfo* seq_info = (MultiSeqInfo*) multiseq_handle;
+    Int4 index;
+    ListNode* seqloc_wrap;
+
+    ASSERT(seq_info);
+    ASSERT(args);
+
+    index = *((Int4*) args);
+
+    seqloc_wrap = ListNodeAddPointer(NULL, BLAST_SEQSRC_CPP_SEQLOC, 
+                                     seq_info->GetSeqLoc(index));
+    return seqloc_wrap;
+}
+
+/** Retrieve length of a given sequence.
+ * @param multiseq_handle Pointer to the structure containing sequences [in]
+ * @param args Pointer to integer indicating index into the sequences 
+ *             vector [in]
+ * @return Length of the sequence or BLAST_SEQSRC_ERROR.
+ */
+static Int4 MultiSeqGetSeqLen(void* multiseq_handle, void* args)
+{
+    MultiSeqInfo* seq_info = (MultiSeqInfo*) multiseq_handle;
+    Int4 index;
+
+    ASSERT(seq_info);
+    ASSERT(args);
+
+    index = *((Int4*) args);
+    return seq_info->GetSeqBlk(index)->length;
+}
+
+/** Gets the next sequence index, given a BlastSeqSrc pointer. */
+static Int4 MultiSeqIteratorNext(void* seqsrc, BlastSeqSrcIterator* itr)
+{
+    BlastSeqSrc* seq_src = (BlastSeqSrc*) seqsrc;
+    Int4 retval = BLAST_SEQSRC_EOF;
+    Int2 status = 0;
+
+    ASSERT(seq_src);
+    ASSERT(itr);
+
+    if ((status = BLASTSeqSrcGetNextChunk(seq_src, itr))
+        == BLAST_SEQSRC_EOF) {
+        return status;
+    }
+    retval = itr->current_pos++;
+
+    return retval;
+}
+
+/** Gets the next sequence index, given a MultiSeqInfo pointer. */
+static Int2 MultiSeqGetNextChunk(void* multiseq_handle, 
+                                 BlastSeqSrcIterator* itr)
+{
+    MultiSeqInfo* seq_info = (MultiSeqInfo*) multiseq_handle;
+
+    ASSERT(itr);
+
+    if (itr->current_pos == UINT4_MAX) {
+        itr->current_pos = 0;
+    }
+
+    if (itr->current_pos >= seq_info->GetNumSeqs())
+        return BLAST_SEQSRC_EOF;
+
+    return BLAST_SEQSRC_SUCCESS;
+}
+
+ListNode* MultiSeqGetErrorMessage(void* multiseq_handle, void* args)
+{
+    MultiSeqInfo* seq_info = (MultiSeqInfo*) multiseq_handle;
+    Blast_Message* error_msg = seq_info->GetErrorMessage();
+    ListNode* retval = NULL;
+    if (error_msg) {
+        retval = 
+            ListNodeAddPointer(NULL, BLAST_SEQSRC_MESSAGE, (void*) error_msg);
+    }
+    return retval;
+}
+
+BlastSeqSrc* MultiSeqSrcNew(BlastSeqSrc* retval, void* args)
+{
+    MultiSeqSrcNewArgs* seqsrc_args = (MultiSeqSrcNewArgs*) args;
+
+    if (!retval)
+        return NULL;
+    
+    MultiSeqInfo* seq_info = 
+        new MultiSeqInfo(seqsrc_args->seq_vector, seqsrc_args->program);
+    
+    /* Initialize the BlastSeqSrc structure fields with user-defined function
+     * pointers and seq_info */
+    SetDeleteFnPtr(retval, &MultiSeqSrcFree);
+    SetDataStructure(retval, (void*) seq_info);
+    SetGetNumSeqs(retval, &MultiSeqGetNumSeqs);
+    SetGetMaxSeqLen(retval, &MultiSeqGetMaxLength);
+    SetGetAvgSeqLen(retval, &MultiSeqGetAvgLength);
+    SetGetTotLen(retval, &MultiSeqGetTotLen);
+    SetGetName(retval, &MultiSeqGetName);
+    SetGetDefinition(retval, &MultiSeqGetDefinition);
+    SetGetDate(retval, &MultiSeqGetDate);
+    SetGetIsProt(retval, &MultiSeqGetIsProt);
+    SetGetSequence(retval, &MultiSeqGetSequence);
+    SetGetSeqIdStr(retval, &MultiSeqGetSeqIdStr);
+    SetGetSeqId(retval, &MultiSeqGetSeqId);
+    SetGetSeqLoc(retval, &MultiSeqGetSeqLoc);
+    SetGetSeqLen(retval, &MultiSeqGetSeqLen);
+    SetGetNextChunk(retval, &MultiSeqGetNextChunk);
+    SetIterNext(retval, &MultiSeqIteratorNext);
+    SetGetError(retval, &MultiSeqGetErrorMessage);
+
+    return retval;
+}
+
+BlastSeqSrc* MultiSeqSrcFree(BlastSeqSrc* seq_src)
+{
+    if (!seq_src) 
+        return NULL;
+    MultiSeqInfo* seq_info = (MultiSeqInfo*)GetDataStructure(seq_src);
+
+    delete seq_info;
+    sfree(seq_src);
+
+    return NULL;
+}
+
+BlastSeqSrc*
+MultiSeqSrcInit(const TSeqLocVector& seq_vector, EProgram program)
+{
+    BlastSeqSrc* seq_src = NULL;
+    BlastSeqSrcNewInfo bssn_info;
+    MultiSeqSrcNewArgs* args =
+        (MultiSeqSrcNewArgs*) calloc(1, sizeof(MultiSeqSrcNewArgs));;
+    args->seq_vector = (TSeqLocVector) seq_vector;
+    args->program = program;
+    bssn_info.constructor = &MultiSeqSrcNew;
+    bssn_info.ctor_argument = (void*) args;
+
+    seq_src = BlastSeqSrcNew(&bssn_info);
+    ListNode* error_wrap = BLASTSeqSrcGetError(seq_src);
+    Blast_Message* error_msg = NULL;
+    if (error_wrap && error_wrap->choice == BLAST_SEQSRC_MESSAGE)
+        error_msg = (Blast_Message*) error_wrap->ptr;
+    if (error_msg) {
+        ThrowBlastException((CBlastException::EErrCode) error_msg->code,
+                            error_msg->message);
+    }
+    return seq_src;
+}
+
+END_SCOPE(blast)
+END_NCBI_SCOPE
