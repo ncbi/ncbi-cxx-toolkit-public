833e2ff254ed5373b0b5f7eab66ab63e83169870
Vladimir Ivanov <ivanov@ncbi.nlm.nih.gov>
2004-03-15 20:13:17 +0000

    Disabled this test for  GCC-2.95 compiler on Linux
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@14303 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/test/objmgr/test_objmgr_loaders_pubseqos.sh
+++ src/objtools/test/objmgr/test_objmgr_loaders_pubseqos.sh
@@ -3,9 +3,13 @@
 
 method="PUBSEQOS"
 
-status_dir="../../../../status"
-if test ! -f "$status_dir/Sybase.enabled"; then
+if test ! -f "../../../../status/Sybase.enabled"; then
     echo Sybase is disabled: skipping PUBSEQOS loader test
+    exit 0
+fi
+
+if grep "^signature = GCC_295-.*-linux-gnu" ../../../Makefile.mk > /dev/null 2>&1; then
+    echo "This test is disabled for GCC-2.95 compiler on Linux"
     exit 0
 fi
 
