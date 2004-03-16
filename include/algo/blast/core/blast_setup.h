061da301a169d7762b5a563c9c7997bab5878b1c
Christiam Camacho <camacho@ncbi.nlm.nih.gov>
2004-03-16 18:55:36 +0000

    Moved log to end of file
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@14347 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/algo/blast/core/blast_setup.h
+++ include/algo/blast/core/blast_setup.h
@@ -34,9 +34,150 @@ $Revision$
 
 ******************************************************************************/
 
+#ifndef __BLAST_SETUP__
+#define __BLAST_SETUP__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <algo/blast/core/blast_def.h>
+#include <algo/blast/core/blast_options.h>
+#include <algo/blast/core/blast_stat.h>
+#include <algo/blast/core/blast_extend.h>
+#include <algo/blast/core/blast_gapalign.h>
+
+/** "Main" setup routine for BLAST. Calculates all information for BLAST search
+ * that is dependent on the ASN.1 structures.
+ * @param program_number Type of BLAST program (0=blastn, ...). [in]
+ * @param qsup_options options for query setup. [in]
+ * @param scoring_options options for scoring. [in]
+ * @param hit_options options for saving hits. [in]
+ * @param query_blk BLAST_SequenceBlk* for the query. [in]
+ * @param query_info The query information block [in]
+ * @param lookup_segments Start/stop locations for non-masked query 
+ *                        segments [out]
+ * @param filter_slp_out Filtering/masking locations. [out]
+ * @param sbpp Contains scoring information. [out]
+ * @param blast_message error or warning [out] 
+ */
+Int2 BLAST_MainSetUp(Uint1 program_number,
+        const QuerySetUpOptions* qsup_options,
+        const BlastScoringOptions* scoring_options,
+        const BlastHitSavingOptions* hit_options,
+        BLAST_SequenceBlk* query_blk,
+        BlastQueryInfo* query_info, BlastSeqLoc* *lookup_segments,
+        BlastMaskLoc* *filter_slp_out,
+        BlastScoreBlk* *sbpp, Blast_Message* *blast_message);
+
+/** BlastScoreBlkGappedFill, fills the ScoreBlkPtr for a gapped search.  
+ *      Should be moved to blast_stat.c in the future.
+ * @param sbp Contains fields to be set, should not be NULL. [out]
+ * @param scoring_options Scoring_options [in]
+ * @param program Used to set fields on sbp [in]
+ * @param query_info Query information containing context information [in]
+ *
+*/
+Int2
+BlastScoreBlkGappedFill(BlastScoreBlk * sbp,
+                        const BlastScoringOptions * scoring_options,
+                        Uint1 program, BlastQueryInfo * query_info);
+
+/** Function to calculate effective query length and db length as well as
+ * effective search space. 
+ * @param program_number blastn, blastp, blastx, etc. [in]
+ * @param scoring_options options for scoring. [in]
+ * @param eff_len_params Used to calculate effective lengths [in]
+ * @param sbp Karlin-Altschul parameters [out]
+ * @param query_info The query information block, which stores the effective
+ *                   search spaces for all queries [in] [out]
+*/
+Int2 BLAST_CalcEffLengths (Uint1 program_number, 
+   const BlastScoringOptions* scoring_options,
+   const BlastEffectiveLengthsParameters* eff_len_params, 
+   const BlastScoreBlk* sbp, BlastQueryInfo* query_info);
+
+/** Set up the auxiliary structures for gapped alignment / traceback only 
+ * @param program_number blastn, blastp, blastx, etc. [in]
+ * @param seq_src Sequence source information, with callbacks to get 
+ *                sequences, their lengths, etc. [in]
+ * @param scoring_options options for scoring. [in]
+ * @param eff_len_options Options overriding real database sizes for
+ *                        calculating effective lengths [in]
+ * @param ext_options options for gapped extension. [in]
+ * @param hit_options options for saving hits. [in]
+ * @param query_info The query information block [in]
+ * @param sbp Contains scoring information. [in]
+ * @param ext_params Parameters for gapped extension [out]
+ * @param hit_params Parameters for saving hits [out]
+ * @param eff_len_params Parameters for search space calculations [out]
+ * @param gap_align Gapped alignment information and allocated memory [out]
+ */
+Int2 
+BLAST_GapAlignSetUp(Uint1 program_number,
+   const BlastSeqSrc* seq_src,
+   const BlastScoringOptions* scoring_options,
+   const BlastEffectiveLengthsOptions* eff_len_options,
+   const BlastExtensionOptions* ext_options,
+   const BlastHitSavingOptions* hit_options,
+   BlastQueryInfo* query_info, 
+   BlastScoreBlk* sbp, 
+   BlastExtensionParameters** ext_params,
+   BlastHitSavingParameters** hit_params,
+   BlastEffectiveLengthsParameters** eff_len_params,
+   BlastGapAlignStruct** gap_align);
+
+/** Recalculates the parameters that depend on an individual sequence, if
+ * this is not a database search.
+ * @param program_number BLAST program [in]
+ * @param subject_length Length of the current subject sequence [in]
+ * @param scoring_options Scoring options [in]
+ * @param query_info The query information structure. Effective lengths
+ *                   are recalculated here. [in] [out]
+ * @param sbp Scoring statistical parameters [in]
+ * @param ext_params Parameters for gapped extensions. [in]
+ * @param hit_params Parameters for saving hits. Score cutoffs are recalculated
+ *                   here [in] [out]
+ * @param word_params Parameters for ungapped extension. Score cutoffs are
+ *                    recalculated here [in] [out]
+ * @param eff_len_params Parameters for effective lengths calculation. Reset
+ *                       with the current sequence data [in] [out]
+ */
+Int2 BLAST_OneSubjectUpdateParameters(Uint1 program_number,
+                    Uint4 subject_length,
+                    const BlastScoringOptions* scoring_options,
+                    BlastQueryInfo* query_info, 
+                    BlastScoreBlk* sbp, 
+                    const BlastExtensionParameters* ext_params,
+                    BlastHitSavingParameters* hit_params,
+                    BlastInitialWordParameters* word_params,
+                    BlastEffectiveLengthsParameters* eff_len_params);
+
+/** BlastScoreBlkMatrixInit, fills score matrix parameters in the ScoreBlkPtr
+ *      Should be moved to blast_stat.c in the future.
+ * @param program_number Used to set fields on sbp [in]
+ * @param scoring_options Scoring_options [in]
+ * @param sbp Contains fields to be set, should not be NULL. [out]
+ *
+*/
+
+Int2
+BlastScoreBlkMatrixInit(Uint1 program_number, 
+   const BlastScoringOptions* scoring_options,
+   BlastScoreBlk* sbp);
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* !__BLAST_SETUP__ */
+
 /*
  *
 * $Log$
+* Revision 1.35  2004/03/16 18:55:36  camacho
+* Moved log to end of file
+*
 * Revision 1.34  2004/03/15 19:54:51  dondosha
 * Removed unneeded arguments in BLAST_GapAlignSetUp
 *
@@ -208,140 +349,3 @@ $Revision$
 
 */
 
-#ifndef __BLAST_SETUP__
-#define __BLAST_SETUP__
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-#include <algo/blast/core/blast_def.h>
-#include <algo/blast/core/blast_options.h>
-#include <algo/blast/core/blast_stat.h>
-#include <algo/blast/core/blast_extend.h>
-#include <algo/blast/core/blast_gapalign.h>
-
-/** "Main" setup routine for BLAST. Calculates all information for BLAST search
- * that is dependent on the ASN.1 structures.
- * @param program_number Type of BLAST program (0=blastn, ...). [in]
- * @param qsup_options options for query setup. [in]
- * @param scoring_options options for scoring. [in]
- * @param hit_options options for saving hits. [in]
- * @param query_blk BLAST_SequenceBlk* for the query. [in]
- * @param query_info The query information block [in]
- * @param lookup_segments Start/stop locations for non-masked query 
- *                        segments [out]
- * @param filter_slp_out Filtering/masking locations. [out]
- * @param sbpp Contains scoring information. [out]
- * @param blast_message error or warning [out] 
- */
-Int2 BLAST_MainSetUp(Uint1 program_number,
-        const QuerySetUpOptions* qsup_options,
-        const BlastScoringOptions* scoring_options,
-        const BlastHitSavingOptions* hit_options,
-        BLAST_SequenceBlk* query_blk,
-        BlastQueryInfo* query_info, BlastSeqLoc* *lookup_segments,
-        BlastMaskLoc* *filter_slp_out,
-        BlastScoreBlk* *sbpp, Blast_Message* *blast_message);
-
-/** BlastScoreBlkGappedFill, fills the ScoreBlkPtr for a gapped search.  
- *      Should be moved to blast_stat.c in the future.
- * @param sbp Contains fields to be set, should not be NULL. [out]
- * @param scoring_options Scoring_options [in]
- * @param program Used to set fields on sbp [in]
- * @param query_info Query information containing context information [in]
- *
-*/
-Int2
-BlastScoreBlkGappedFill(BlastScoreBlk * sbp,
-                        const BlastScoringOptions * scoring_options,
-                        Uint1 program, BlastQueryInfo * query_info);
-
-/** Function to calculate effective query length and db length as well as
- * effective search space. 
- * @param program_number blastn, blastp, blastx, etc. [in]
- * @param scoring_options options for scoring. [in]
- * @param eff_len_params Used to calculate effective lengths [in]
- * @param sbp Karlin-Altschul parameters [out]
- * @param query_info The query information block, which stores the effective
- *                   search spaces for all queries [in] [out]
-*/
-Int2 BLAST_CalcEffLengths (Uint1 program_number, 
-   const BlastScoringOptions* scoring_options,
-   const BlastEffectiveLengthsParameters* eff_len_params, 
-   const BlastScoreBlk* sbp, BlastQueryInfo* query_info);
-
-/** Set up the auxiliary structures for gapped alignment / traceback only 
- * @param program_number blastn, blastp, blastx, etc. [in]
- * @param seq_src Sequence source information, with callbacks to get 
- *                sequences, their lengths, etc. [in]
- * @param scoring_options options for scoring. [in]
- * @param eff_len_options Options overriding real database sizes for
- *                        calculating effective lengths [in]
- * @param ext_options options for gapped extension. [in]
- * @param hit_options options for saving hits. [in]
- * @param query_info The query information block [in]
- * @param sbp Contains scoring information. [in]
- * @param ext_params Parameters for gapped extension [out]
- * @param hit_params Parameters for saving hits [out]
- * @param eff_len_params Parameters for search space calculations [out]
- * @param gap_align Gapped alignment information and allocated memory [out]
- */
-Int2 
-BLAST_GapAlignSetUp(Uint1 program_number,
-   const BlastSeqSrc* seq_src,
-   const BlastScoringOptions* scoring_options,
-   const BlastEffectiveLengthsOptions* eff_len_options,
-   const BlastExtensionOptions* ext_options,
-   const BlastHitSavingOptions* hit_options,
-   BlastQueryInfo* query_info, 
-   BlastScoreBlk* sbp, 
-   BlastExtensionParameters** ext_params,
-   BlastHitSavingParameters** hit_params,
-   BlastEffectiveLengthsParameters** eff_len_params,
-   BlastGapAlignStruct** gap_align);
-
-/** Recalculates the parameters that depend on an individual sequence, if
- * this is not a database search.
- * @param program_number BLAST program [in]
- * @param subject_length Length of the current subject sequence [in]
- * @param scoring_options Scoring options [in]
- * @param query_info The query information structure. Effective lengths
- *                   are recalculated here. [in] [out]
- * @param sbp Scoring statistical parameters [in]
- * @param ext_params Parameters for gapped extensions. [in]
- * @param hit_params Parameters for saving hits. Score cutoffs are recalculated
- *                   here [in] [out]
- * @param word_params Parameters for ungapped extension. Score cutoffs are
- *                    recalculated here [in] [out]
- * @param eff_len_params Parameters for effective lengths calculation. Reset
- *                       with the current sequence data [in] [out]
- */
-Int2 BLAST_OneSubjectUpdateParameters(Uint1 program_number,
-                    Uint4 subject_length,
-                    const BlastScoringOptions* scoring_options,
-                    BlastQueryInfo* query_info, 
-                    BlastScoreBlk* sbp, 
-                    const BlastExtensionParameters* ext_params,
-                    BlastHitSavingParameters* hit_params,
-                    BlastInitialWordParameters* word_params,
-                    BlastEffectiveLengthsParameters* eff_len_params);
-
-/** BlastScoreBlkMatrixInit, fills score matrix parameters in the ScoreBlkPtr
- *      Should be moved to blast_stat.c in the future.
- * @param program_number Used to set fields on sbp [in]
- * @param scoring_options Scoring_options [in]
- * @param sbp Contains fields to be set, should not be NULL. [out]
- *
-*/
-
-Int2
-BlastScoreBlkMatrixInit(Uint1 program_number, 
-   const BlastScoringOptions* scoring_options,
-   BlastScoreBlk* sbp);
-
-
-#ifdef __cplusplus
-}
-#endif
-#endif /* !__BLAST_SETUP__ */
