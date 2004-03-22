e5a400589bd05141cd5bd676e206e395c23ee38b
Joshua (Josh) Cherry <jcherry@ncbi.nlm.nih.gov>
2004-03-22 17:33:24 +0000

    gbench_remote_* scripts
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@14484 78c7ea69-d796-4a43-9a09-de51944f1b03

--- scripts/gbench_install.sh
+++ scripts/gbench_install.sh
@@ -286,6 +286,8 @@ echo "Preparing scripts"
 COMMON_ExecRB cp ${source_dir}/gbench_install/run-gbench.sh ${target_dir}/bin/run-gbench.sh
 chmod 755 ${target_dir}/bin/run-gbench.sh
 
+COMMON_ExecRB cp ${source_dir}/gbench_install/gbench_remote_template.sh ${target_dir}/bin/gbench_remote_template.sh
+
 COMMON_ExecRB cp -p ${source_dir}/gbench.ini ${target_dir}/etc/
 
 COMMON_ExecRB cp -p ${source_dir}/news.ini ${target_dir}/etc/
@@ -329,3 +331,11 @@ done
 # Do this last, to be sure the symlink doesn't end up dangling.
 rm -f ${src_dir}/bin/gbench
 ln -s ${target_dir}/bin/run-gbench.sh ${src_dir}/bin/gbench
+
+for ftype in auto asntext asnbin xml fasta newick textalign; do
+	 rm -f ${src_dir}/bin/gbench_remote_$ftype
+	 ln -s ${target_dir}/bin/gbench_remote_$ftype ${src_dir}/bin
+done
+
+rm -f ${src_dir}/bin/gbench_remote
+ln -s ${src_dir}/bin/gbench_remote_auto ${src_dir}/bin/gbench_remote
