91fa7e62c8d5634a823644d03467c24613a4031a
Vladimir Ivanov <ivanov@ncbi.nlm.nih.gov>
2004-04-05 18:48:18 +0000

    Fixed paths to binaries and libaries
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@14766 78c7ea69-d796-4a43-9a09-de51944f1b03

--- compilers/msvc710_prj/install.sh
+++ compilers/msvc710_prj/install.sh
@@ -15,8 +15,8 @@ compiler="${3:-msvc7}"
 compiler="${compiler}_prj"
 
 
-if test -n "$4" ; then
-  echo "USAGE:  `basename $script` [build_dir] [install_dir] [compiler]"
+if test -n "$3" ; then
+  echo "USAGE:  `basename $script` [build_dir] [install_dir]"
 fi
 
 
@@ -100,9 +100,9 @@ done
 # Libraries
 for i in 'Debug' 'Release' ; do
   for j in '' 'DLL' ; do
-    if test -d "$builddir"/compilers/$compiler/lib/$i$j ; then
+    if test -d "$builddir"/compilers/$compiler/static/lib/$i$j ; then
       makedir "$target/$i$j" -p
-      cd "$builddir"/compilers/$compiler/lib/$i$j
+      cd "$builddir"/compilers/$compiler/static/lib/$i$j
       cp -p *.lib "$target/$i$j"
     fi
     if test -d "$builddir"/compilers/$compiler/dll/bin/$i$j ; then
@@ -116,27 +116,27 @@ done
 # Executables
 makedir "$bindir" -p
 for i in 'DLL' '' ; do
-  if test -d "$builddir"/compilers/$compiler/bin/Release$i ; then
-    if ls "$builddir"/compilers/$compiler/bin/Release$i/*.exe >/dev/null 2>&1 ; then
-      cd "$builddir"/compilers/$compiler/bin/Release$i
+  if test -d "$builddir"/compilers/$compiler/static/bin/Release$i ; then
+    if ls "$builddir"/compilers/$compiler/static/bin/Release$i/*.exe >/dev/null 2>&1 ; then
+      cd "$builddir"/compilers/$compiler/static/bin/Release$i
       cp -p *.exe *.dll *.exp "$bindir"
       break
     fi
   fi
 done
 
+
 cd "$builddir"/compilers/$compiler
 
 # Gbench public installation
-for i in ReleaseDLL DebugDLL; do
-  if test -d ./gbench/gbench_install/"$i" ; then
-    makedir "$bindir"/gbench/"$i" -p
-    cp -pr ./gbench/gbench_install/"$i"/* "$bindir"/gbench/"$i"
-  fi
-done
+#for i in ReleaseDLL DebugDLL; do
+#  if test -d ./gbench/gbench_install/"$i" ; then
+#    makedir "$bindir"/gbench/"$i" -p
+#    cp -pr ./gbench/gbench_install/"$i"/* "$bindir"/gbench/"$i"
+#  fi
+#done
 
 # CVS checkout info file
 cp -p "$builddir"/cvs_info "$target"
 
 exit 0
-
