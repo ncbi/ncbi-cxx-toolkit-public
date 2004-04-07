0bbb935b0b149c0f63d2290da99d916386382eb6
Christiam Camacho <camacho@ncbi.nlm.nih.gov>
2004-04-07 19:11:17 +0000

    Initial revision
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@14828 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ src/algo/blast/core/blast_psi_priv.c
@@ -0,0 +1,76 @@
+static char const rcsid[] =
+    "$Id$";
+/* ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Author:  Christiam Camacho
+ *
+ */
+
+#include "blast_psi_priv.h"
+
+void**
+_PSIAllocateMatrix(unsigned int ncols, unsigned int nrows, 
+                 unsigned int data_type_sz)
+{
+    void** retval = NULL;
+    unsigned int i = 0;
+
+    if ( !(retval = (void**) malloc(sizeof(void*) * ncols)))
+        return NULL;
+
+    for (i = 0; i < ncols; i++) {
+        if ( !(retval[i] = (void*) calloc(nrows, data_type_sz))) {
+            retval = _PSIDeallocateMatrix(retval, i);
+            break;
+        }
+    }
+    return retval;
+}
+
+void**
+_PSIDeallocateMatrix(void** matrix, unsigned int ncols)
+{
+    unsigned int i = 0;
+
+    if (!matrix)
+        return NULL;
+
+    for (i = 0; i < ncols; i++) {
+        sfree(matrix[i]);
+    }
+    sfree(matrix);
+    return NULL;
+}
+
+/*
+ * ===========================================================================
+ *
+ * $Log$
+ * Revision 1.1  2004/04/07 19:11:17  camacho
+ * Initial revision
+ *
+ *
+ * ===========================================================================
+ */
---
+++ src/algo/blast/core/blast_psi_priv.h
@@ -0,0 +1,83 @@
+#ifndef ALGO_BLAST_CORE___BLAST_PSI_PRIV__H
+#define ALGO_BLAST_CORE___BLAST_PSI_PRIV__H
+
+/*  $Id$
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Author:  Christiam Camacho
+ *
+ */
+
+/** @file blast_psi_priv.h
+ *  Private interface for Position Iterated BLAST API.
+ */
+
+#include <algo/blast/core/blast_psi.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define MASTER_INDEX 0
+
+/** Generic 2 dimensional matrix allocator.
+ * Allocates a ncols by nrows matrix with cells of size data_type_sz. Must be
+ * freed using x_DeallocateMatrix
+ * @param ncols number of columns in matrix
+ * @param nrows number of rows in matrix
+ * @param data_type_sz size of the data type (in bytes) to allocate for each
+ * element in the matrix
+ * @return pointer to allocated memory or NULL in case of failure
+ */
+void**
+_PSIAllocateMatrix(unsigned int ncols, unsigned int nrows, 
+                   unsigned int data_type_sz);
+
+/** Generic 2 dimensional matrix deallocator.
+ * Deallocates the memory allocated by x_AllocateMatrix
+ * @param matrix matrix to deallocate
+ * @param ncols number of columns in the matrix
+ * @return NULL
+ */
+void**
+_PSIDeallocateMatrix(void** matrix, unsigned int ncols);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+/*
+ * ===========================================================================
+ *
+ * $Log$
+ * Revision 1.1  2004/04/07 19:11:17  camacho
+ * Initial revision
+ *
+ *
+ * ===========================================================================
+ */
+
+#endif /* !ALGO_BLAST_CORE__BLAST_PSI_PRIV__H */
