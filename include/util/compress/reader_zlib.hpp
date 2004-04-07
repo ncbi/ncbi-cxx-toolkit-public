4cd3d8afd5954985c6638945d98c269d3612f6ed
Vladimir Ivanov <ivanov@ncbi.nlm.nih.gov>
2004-04-07 16:16:17 +0000

    Cosmeic changes. Fixed header and footer.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@14820 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/util/compress/reader_zlib.hpp
+++ include/util/compress/reader_zlib.hpp
@@ -2,39 +2,43 @@
 #define READER_ZLIB__HPP_INCLUDED
 
 /*  $Id$
-* ===========================================================================
-*                            PUBLIC DOMAIN NOTICE
-*               National Center for Biotechnology Information
-*
-*  This software/database is a "United States Government Work" under the
-*  terms of the United States Copyright Act.  It was written as part of
-*  the author's official duties as a United States Government employee and
-*  thus cannot be copyrighted.  This software/database is freely available
-*  to the public for use. The National Library of Medicine and the U.S.
-*  Government have not placed any restriction on its use or reproduction.
-*
-*  Although all reasonable efforts have been taken to ensure the accuracy
-*  and reliability of the software and data, the NLM and the U.S.
-*  Government do not and cannot warrant the performance or results that
-*  may be obtained by using this software or data. The NLM and the U.S.
-*  Government disclaim all warranties, express or implied, including
-*  warranties of performance, merchantability or fitness for any particular
-*  purpose.
-*
-*  Please cite the author in any work or product based on this material.
-* ===========================================================================
-*
-*  Author:  Eugene Vasilchenko
-*
-*  File Description: byte reader with gzip decompression
-*
-*/
+ * ===========================================================================
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ * ===========================================================================
+ *
+ *  Author:  Eugene Vasilchenko
+ *
+ *  File Description: byte reader with gzip decompression
+ *
+ */
 
 #include <corelib/ncbiobj.hpp>
-
 #include <util/bytesrc.hpp>
-
 #include <memory>
+
+
+/** @addtogroup Compression
+ *
+ * @{
+ */
 
 
 BEGIN_NCBI_SCOPE
@@ -81,31 +85,38 @@ END_SCOPE(objects)
 END_NCBI_SCOPE
 
 
+/* @} */
+
+
 /*
-* $Log$
-* Revision 1.1  2003/12/23 17:01:32  kuznets
-* Moved from objmgr to util/compress
-*
-* Revision 1.5  2003/10/21 14:27:35  vasilche
-* Added caching of gi -> sat,satkey,version resolution.
-* SNP blobs are stored in cache in preprocessed format (platform dependent).
-* Limit number of connections to GenBank servers.
-* Added collection of ID1 loader statistics.
-*
-* Revision 1.4  2003/10/15 13:43:57  vasilche
-* Removed obsolete class CResultZBtSrc.
-* Some code formatting.
-*
-* Revision 1.3  2003/10/14 22:35:09  ucko
-* Add missing declaration of CResultZBtSrcRdr::GetCompressedSize.
-*
-* Revision 1.2  2003/07/24 20:41:43  vasilche
-* Added private constructor to make MSVC-DLL happy.
-*
-* Revision 1.1  2003/07/24 19:28:08  vasilche
-* Implemented SNP split for ID1 loader.
-*
-*
-*/
+ * ===========================================================================
+ * $Log$
+ * Revision 1.2  2004/04/07 16:12:00  ivanov
+ * Cosmeic changes. Fixed header and footer.
+ *
+ * Revision 1.1  2003/12/23 17:01:32  kuznets
+ *  Moved from objmgr to util/compress
+ *
+ * Revision 1.5  2003/10/21 14:27:35  vasilche
+ * Added caching of gi -> sat,satkey,version resolution.
+ * SNP blobs are stored in cache in preprocessed format (platform dependent).
+ * Limit number of connections to GenBank servers.
+ * Added collection of ID1 loader statistics.
+ *
+ * Revision 1.4  2003/10/15 13:43:57  vasilche
+ * Removed obsolete class CResultZBtSrc.
+ * Some code formatting.
+ *
+ * Revision 1.3  2003/10/14 22:35:09  ucko
+ * Add missing declaration of CResultZBtSrcRdr::GetCompressedSize.
+ *
+ * Revision 1.2  2003/07/24 20:41:43  vasilche
+ * Added private constructor to make MSVC-DLL happy.
+ *
+ * Revision 1.1  2003/07/24 19:28:08  vasilche
+ * Implemented SNP split for ID1 loader.
+ *
+ * ===========================================================================
+ */ 
 
 #endif // READER_ZLIB__HPP_INCLUDED
--- src/util/compress/reader_zlib.cpp
+++ src/util/compress/reader_zlib.cpp
@@ -1,37 +1,38 @@
 /*  $Id$
-* ===========================================================================
-*                            PUBLIC DOMAIN NOTICE
-*               National Center for Biotechnology Information
-*
-*  This software/database is a "United States Government Work" under the
-*  terms of the United States Copyright Act.  It was written as part of
-*  the author's official duties as a United States Government employee and
-*  thus cannot be copyrighted.  This software/database is freely available
-*  to the public for use. The National Library of Medicine and the U.S.
-*  Government have not placed any restriction on its use or reproduction.
-*
-*  Although all reasonable efforts have been taken to ensure the accuracy
-*  and reliability of the software and data, the NLM and the U.S.
-*  Government do not and cannot warrant the performance or results that
-*  may be obtained by using this software or data. The NLM and the U.S.
-*  Government disclaim all warranties, express or implied, including
-*  warranties of performance, merchantability or fitness for any particular
-*  purpose.
-*
-*  Please cite the author in any work or product based on this material.
-* ===========================================================================
-*
-*  Author:  Eugene Vasilchenko
-*
-*  File Description: byte reader with gzip decompression
-*
-*/
+ * ===========================================================================
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ * ===========================================================================
+ *
+ *  Author:  Eugene Vasilchenko
+ *
+ *  File Description: byte reader with gzip decompression
+ *
+ */
 
 #include <util/compress/reader_zlib.hpp>
 #include <util/compress/compress.hpp>
 
 #include <corelib/ncbitime.hpp>
 #include <util/compress/zlib.hpp>
+
 
 BEGIN_NCBI_SCOPE
 BEGIN_SCOPE(objects)
@@ -253,46 +254,52 @@ size_t CResultZBtSrcX::Read(char* buffer, size_t buffer_length)
 END_SCOPE(objects)
 END_NCBI_SCOPE
 
+
 /*
-* $Log$
-* Revision 1.1  2003/12/23 17:02:09  kuznets
-* Moved from objmgr to util/compress
-*
-* Revision 1.10  2003/11/26 17:55:59  vasilche
-* Implemented ID2 split in ID1 cache.
-* Fixed loading of splitted annotations.
-*
-* Revision 1.9  2003/10/21 14:27:35  vasilche
-* Added caching of gi -> sat,satkey,version resolution.
-* SNP blobs are stored in cache in preprocessed format (platform dependent).
-* Limit number of connections to GenBank servers.
-* Added collection of ID1 loader statistics.
-*
-* Revision 1.8  2003/10/14 22:36:08  ucko
-* Fix typo in last log message
-*
-* Revision 1.7  2003/10/14 22:35:51  ucko
-* Remove initialization of CNlmZipBtRdr::m_CompressedSize (neither
-* declared nor used anywhere)
-*
-* Revision 1.6  2003/10/14 21:06:25  vasilche
-* Fixed compression statistics.
-* Disabled caching of SNP blobs.
-*
-* Revision 1.5  2003/10/14 18:59:55  vasilche
-* Temporarily remove collection of compression statistics.
-*
-* Revision 1.4  2003/10/14 18:31:55  vasilche
-* Added caching support for SNP blobs.
-* Added statistics collection of ID1 connection.
-*
-* Revision 1.3  2003/09/05 17:29:40  grichenk
-* Structurized Object Manager exceptions
-*
-* Revision 1.2  2003/07/24 20:35:42  vasilche
-* Added private constructor to make MSVC-DLL happy.
-*
-* Revision 1.1  2003/07/24 19:28:09  vasilche
-* Implemented SNP split for ID1 loader.
-*
-*/
+ * ===========================================================================
+ * $Log$
+ * Revision 1.2  2004/04/07 16:16:17  ivanov
+ * Cosmeic changes. Fixed header and footer.
+ *
+ * Revision 1.1  2003/12/23 17:02:09  kuznets
+ * Moved from objmgr to util/compress
+ *
+ * Revision 1.10  2003/11/26 17:55:59  vasilche
+ * Implemented ID2 split in ID1 cache.
+ * Fixed loading of splitted annotations.
+ *
+ * Revision 1.9  2003/10/21 14:27:35  vasilche
+ * Added caching of gi -> sat,satkey,version resolution.
+ * SNP blobs are stored in cache in preprocessed format (platform dependent).
+ * Limit number of connections to GenBank servers.
+ * Added collection of ID1 loader statistics.
+ *
+ * Revision 1.8  2003/10/14 22:36:08  ucko
+ * Fix typo in last log message
+ *
+ * Revision 1.7  2003/10/14 22:35:51  ucko
+ * Remove initialization of CNlmZipBtRdr::m_CompressedSize (neither
+ * declared nor used anywhere)
+ *
+ * Revision 1.6  2003/10/14 21:06:25  vasilche
+ * Fixed compression statistics.
+ * Disabled caching of SNP blobs.
+ *
+ * Revision 1.5  2003/10/14 18:59:55  vasilche
+ * Temporarily remove collection of compression statistics.
+ *
+ * Revision 1.4  2003/10/14 18:31:55  vasilche
+ * Added caching support for SNP blobs.
+ * Added statistics collection of ID1 connection.
+ *
+ * Revision 1.3  2003/09/05 17:29:40  grichenk
+ * Structurized Object Manager exceptions
+ *
+ * Revision 1.2  2003/07/24 20:35:42  vasilche
+ * Added private constructor to make MSVC-DLL happy.
+ *
+ * Revision 1.1  2003/07/24 19:28:09  vasilche
+ * Implemented SNP split for ID1 loader.
+ *
+ * ===========================================================================
+ */
