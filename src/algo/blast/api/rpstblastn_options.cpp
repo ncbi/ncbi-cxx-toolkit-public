a8984b1bf7f8772dd266b9ce8b9705226ecf39dd
papadopo <papadopo@ncbi.nlm.nih.gov>
2004-04-16 14:08:30 +0000

    options handle for translated RPS blast
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@14992 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ src/algo/blast/api/rpstblastn_options.cpp
@@ -0,0 +1,96 @@
+/*  $Id$
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Authors:  Jason Papadopoulos
+ *
+ */
+
+/// @file rpstblastn_options.cpp
+/// Implements the CRPSTBlastnOptionsHandle class.
+
+#include <algo/blast/api/rpstblastn_options.hpp>
+#include "blast_setup.hpp"
+
+/** @addtogroup AlgoBlast
+ *
+ * @{
+ */
+
+
+BEGIN_NCBI_SCOPE
+BEGIN_SCOPE(blast)
+
+CRPSTBlastnOptionsHandle::CRPSTBlastnOptionsHandle(EAPILocality locality)
+    : CBlastProteinOptionsHandle(locality)
+{
+    if (m_Opts->GetLocality() == CBlastOptions::eRemote) {
+        return;
+    }
+    SetDefaults();
+    m_Opts->SetProgram(eRPSTblastn);
+}
+
+void 
+CRPSTBlastnOptionsHandle::SetLookupTableDefaults()
+{
+    CBlastProteinOptionsHandle::SetLookupTableDefaults();
+    SetWordThreshold(BLAST_WORD_THRESHOLD_TBLASTN);
+    m_Opts->SetLookupTableType(RPS_LOOKUP_TABLE);
+}
+
+void
+CRPSTBlastnOptionsHandle::SetQueryOptionDefaults()
+{
+    CBlastProteinOptionsHandle::SetQueryOptionDefaults();
+    SetFilterString("F");
+}
+
+void
+CRPSTBlastnOptionsHandle::SetHitSavingOptionsDefaults()
+{
+    CBlastProteinOptionsHandle::SetHitSavingOptionsDefaults();
+    m_Opts->SetSumStatisticsMode(false);
+}
+
+void
+CRPSTBlastnOptionsHandle::SetSubjectSequenceOptionsDefaults()
+{
+    SetDbGeneticCode(BLAST_GENETIC_CODE);
+}
+
+END_SCOPE(blast)
+END_NCBI_SCOPE
+
+
+/* @} */
+
+/*
+ * =======================================================================
+ * $Log$
+ * Revision 1.1  2004/04/16 14:07:32  papadopo
+ * options handle for translated RPS blast
+ *
+ * =======================================================================
+ */
