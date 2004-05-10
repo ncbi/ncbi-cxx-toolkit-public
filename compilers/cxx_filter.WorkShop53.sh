4226619bd058d808e81618102c6df4d712aa0bbd
Aaron Ucko <ucko@ncbi.nlm.nih.gov>
2004-05-10 19:17:44 +0000

    Simplify templates more extensively in an attempt to avoid hitting nawk's line-length limit.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@15500 78c7ea69-d796-4a43-9a09-de51944f1b03

--- compilers/cxx_filter.WorkShop53.sh
+++ compilers/cxx_filter.WorkShop53.sh
@@ -6,12 +6,28 @@
 #  Filter out redundant warnings issued by WorkShop 6u2 C++ 5.3 compiler.
 #  Simplify the output.
 
-sed '
-s/std::basic_string<char, std::char_traits<char>, std::allocator<char>>/std::string/g
-s/std::basic_\([a-z]*stream\)<char, std::char_traits<char>>/std::\1/g
-s/std::\([a-z_]*\)<\([^,<>]*\), std::allocator<\2>>/std::\1<\2>/g
-s/std::\([a-z_]*\)<\([^,<>]*\), std::less<\2>, std::allocator<\2>>/std::\1<\2>/g
-s/std::\([a-z_]*\)<\([^,<>]*\), \([^,<>]*\), std::less<\2>, std::allocator<std::pair<const \2, \3>>>/std::\1<\2, \3>/g
-s/std::\([a-z_]*\)<\([^,<>]*<[^<>]*>\), std::allocator<\2>>/std::\1<\2>/g
-s/std::\([a-z_]*\)<\([^,<>]*\), \([^,<>]*<[^<>]*>\), std::less<\2>, std::allocator<std::pair<const \2, \3>>>/std::\1<\2, \3>/g
-' | nawk -f `dirname $0`/cxx_filter.WorkShop53.awk
+head='s/std::basic_string<char, std::char_traits<char>, std::allocator<char>>/std::string/g
+s/std::basic_\([a-z]*stream\)<char, std::char_traits<char>>/std::\1/g'
+body='s/\(std::[a-z_]*\)<\([^,<>]*\), std::allocator«\2»>/\1«\2»/g
+s/\(std::[a-z_]*\)<\([^,<>]*\), std::less«\2», std::allocator«\2»>/\1«\2»/g
+s/\(std::[a-z_]*\)<\([^,<>]*\), \([^,<>]*\), std::less«\2», std::allocator«std::pair«const \2¸ \3»»>/\1«\2¸ \3»/g
+s/\(std::[a-z_]*\)<\([^,<>]*\), \([^,<>]*«const \2¸ \([^<>]*\)»\), __rwstd::__select1st«\3¸ \2», std::less«\2», std::allocator«\3»>/\1«\2¸ [\4]»/g
+s/<\([^,<>]*\), \([^<>]*\)>/<\1¸ \2>/g
+s/<\([^,<>]*\), \([^<>]*\)>/<\1¸ \2>/g
+s/<\([^,<>]*\), \([^<>]*\)>/<\1¸ \2>/g
+s/<\([^,<>]*\), \([^<>]*\)>/<\1¸ \2>/g
+s/<\([^,<>]*\), \([^<>]*\)>/<\1¸ \2>/g
+s/<\([^,<>]*\), \([^<>]*\)>/<\1¸ \2>/g
+s/<\([^<>]*\)>/«\1»/g'
+tail='y/«¸»/<,>/'
+
+sed "$head
+$body
+$body
+$body
+$body
+$body
+$body
+$body
+$body
+$tail" | nawk -f `dirname $0`/cxx_filter.WorkShop53.awk
