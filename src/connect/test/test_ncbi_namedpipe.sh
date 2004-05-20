da18f18b6ee63872f457c12418a3807d5c05f4ef
Vladimir Ivanov <ivanov@ncbi.nlm.nih.gov>
2004-05-20 16:13:49 +0000

    Increased sleep time between server and client runs from 2 to 4 seconds
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@15732 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/connect/test/test_ncbi_namedpipe.sh
+++ src/connect/test/test_ncbi_namedpipe.sh
@@ -7,7 +7,7 @@ test_ncbi_namedpipe server $$ &
 server_pid=$!
 trap 'kill -9 $server_pid' 1 2 15
 
-sleep 2
+sleep 4
 $CHECK_EXEC test_ncbi_namedpipe client $$ ||  exit_code=1
 
 kill $server_pid  ||  exit_code=2
