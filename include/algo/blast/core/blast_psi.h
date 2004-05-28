5f27f58aa64585ae7b6bfe9bc723dd4fc132d478
Christiam Camacho <camacho@ncbi.nlm.nih.gov>
2004-05-28 15:56:02 +0000

    Initial revision
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@15883 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ include/algo/blast/core/blast_psi.h
@@ -0,0 +1,173 @@
+#ifndef ALGO_BLAST_CORE___BLAST_PSI__H
+#define ALGO_BLAST_CORE___BLAST_PSI__H
+
+/* $Id$
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's offical duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Author:  Christiam Camacho
+ *
+ */
+
+/** @file blast_psi.h
+ * High level definitions and declarations for the PSI-BLAST API.
+ */
+
+#include <algo/blast/core/ncbi_std.h>
+#include <algo/blast/core/blast_stat.h>
+#include <algo/blast/core/blast_options.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef REAL_PROT_ALPHABET
+#   define REAL_PROT_ALPHABET   20
+#endif
+
+/* Defaults for PSI-BLAST options - these are application level options */
+#define PSI_MAX_NUM_PASSES      1
+#define PSI_SCALING_FACTOR      32
+
+/** Percent identity threshold for discarding near-identical matches */
+#define PSI_NEAR_IDENTICAL      0.94
+#define PSI_IDENTICAL           1.0
+
+/** Structure to describe the characteristics of a position in the model */
+typedef struct PsiDesc {
+    Uint1 letter;               /**< Preferred letter at this position */
+    Boolean used;               /**< Is this letter being used? */
+    double e_value;             /**< E-value of the highest HSP including this
+                                     position */
+    SSeqRange extents;          /**< Extent of this aligned position */
+} PsiDesc;
+
+typedef struct PsiInfo {
+    Uint4 query_sz;  /**< Size of the query */
+    Uint4 num_seqs;  /**< Number of sequences in involved w/o including
+                                 the query */
+} PsiInfo;
+
+/** This structure is to be populated at the API level from the Seq-aligns */
+typedef struct PsiAlignmentData {
+    Uint4** res_counts;/**< matrix which keeps track of the number of
+                               residues aligned with the query at each query 
+                               position (columns of multiple alignment). Its
+                               dimensions are query_sz by alphabet_size */
+    Uint4* match_seqs;/**< Count of how many sequences match the query
+                               at each query position, default value is 1 to 
+                               include the query itself. This dynamically 
+                               allocated array has a length of query_sz */
+    PsiDesc** desc_matrix;   /**< Matrix of PsiDesc structures, each cell
+                                  represents an aligned character with the 
+                                  query. Its dimensions are query_sz by
+                                  num_seqs + 1. */
+    Boolean* use_sequences;  /**< Determines if sequences must be used or not.
+                                  This dynamically allocated array has a length
+                                  of num_seqs + 1. */
+
+    PsiInfo* dimensions;     /**< Dimensions of the multiple sequence alignment
+                               (query size by number of sequences aligned + 1
+                               (to include the query) */
+    Uint1* query;   /**< Query sequence (aka master, consensus) */
+} PsiAlignmentData;
+
+/** The functions to create internal structure to store intermediate data 
+ * while creating a PSSM */
+PsiAlignmentData*
+PSIAlignmentDataNew(const Uint1* query, const PsiInfo* info);
+
+PsiAlignmentData*
+PSIAlignmentDataFree(PsiAlignmentData* align_data);
+
+/** This is the return value from all the processing performed in this library.
+ * At the API level this information should be copied into an Score-mat ASN.1
+ * object */
+typedef struct PsiMatrix {
+    int** pssm;             /**< The PSSM, its dimensions are query_sz+1 by 
+                                 PSI_ALPHABET_SIZE */
+    int** scaled_pssm;      /**< not to be used by the public ? Dimensions are
+                                 the same as above Needed in the last 2 stages
+                                 of PSI-BLAST */
+    double** res_freqs;     /**< The residue frequencies. Dimensions are the
+                                 same as above (FIXME?) */
+    Uint4 ncols;     /**< Number of columns in the matrices above
+                                 (query size+1) */
+} PsiMatrix;
+
+/** Allocates a new PsiMatrix structure */
+PsiMatrix*
+PSIMatrixNew(Uint4 query_sz, Uint4 alphabet_size);
+
+/** Deallocates the PsiMatrix structure passed in.
+ * @param matrix structure to deallocate [in]
+ * @return NULL
+ */
+PsiMatrix*
+PSIMatrixFree(PsiMatrix* matrix);
+
+
+/** This structure returns detailed information collected during the process of
+ * creating a PSSM.
+ */
+typedef struct PsiDiagnostics {
+    double** info_content;
+    Uint4 ncols;     /**< Number of columns in the matrix above
+                                 (query size+1) */
+    /* FIXME: add sequence weights */
+} PsiDiagnostics;
+
+/** Allocates a new PSI-BLAST diagnostics structure
+ */
+PsiDiagnostics*
+PSIDiagnosticsNew(Uint4 query_sz, Uint4 alphabet_size);
+
+/** Deallocates the PsiDiagnostics structure passed in.
+ * @param diags structure to deallocate [in]
+ * @return NULL
+ */
+PsiDiagnostics*
+PSIDiagnosticsFree(PsiDiagnostics* diags);
+
+/****************************************************************************/
+
+/* TOP LEVEL FUNCTION FIXME: alignment should contain data from multiple
+ * sequence alignment 
+ * @param diagnostics If non-NULL this structure will be populated and it is
+ *        the caller's responsibility to deallocate this structure.
+ * @retval The PSSM along with residue frequencies and statistical information
+ * (the latter is returned in the sbp)
+ */
+PsiMatrix*
+PSICreatePSSM(PsiAlignmentData* alignment,      /* [in] but modified */
+              const PSIBlastOptions* options,   /* [in] */
+              BlastScoreBlk* sbp,               /* [in] */
+              PsiDiagnostics* diagnostics);     /* [out] */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* !ALGO_BLAST_CORE__BLAST_PSI__H */
+
---
+++ src/algo/blast/core/blast_psi.c
@@ -0,0 +1,322 @@
+static char const rcsid[] =
+    "$Id$";
+/* ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's offical duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Author:  Christiam Camacho
+ *
+ */
+
+/** @file blast_psi.c
+ * Implementation of the high level PSI-BLAST API
+ */
+    
+#include <algo/blast/core/blast_psi.h>
+#include <algo/blast/core/blast_stat.h>
+#include <algo/blast/core/blast_encoding.h>
+#include "blast_psi_priv.h"
+
+/* FIXME: document all local variables */
+
+/****************************************************************************/
+/* Use the following #define's to enable/disable functionality */
+
+/* Taking gaps into account when constructing a PSSM was introduced in the 
+ * 2001 paper "Improving the accuracy of PSI-BLAST protein database searches
+ * with composition based-statistics and other refinements". This feature 
+ * can be disabled by defining the PSI_IGNORE_GAPS_IN_COLUMNS symbol below */
+/* #define PSI_IGNORE_GAPS_IN_COLUMNS */
+/****************************************************************************/
+
+PsiMatrix*
+PSICreatePSSM(PsiAlignmentData* alignment,      /* [in] */
+              const PSIBlastOptions* options,   /* [in] */
+              BlastScoreBlk* sbp,               /* [in] */
+              PsiDiagnostics* diagnostics)      /* [out] */
+{
+    PsiMatrix* retval = NULL;
+
+    PsiAlignedBlock* aligned_block = NULL;
+    PsiSequenceWeights* seq_weights = NULL; 
+
+    aligned_block = _PSIAlignedBlockNew(alignment->dimensions->query_sz);
+    seq_weights = _PSISequenceWeightsNew(alignment->dimensions, sbp);
+    retval = PSIMatrixNew(alignment->dimensions->query_sz, sbp->alphabet_size);
+
+    PSIPurgeBiasedSegments(alignment);
+    PSIComputeAlignmentBlocks(alignment, aligned_block);
+    PSIComputeSequenceWeights(alignment, aligned_block, seq_weights);
+    PSIComputeResidueFrequencies(alignment, seq_weights, sbp, aligned_block,
+                                 options, retval);
+    PSIConvertResidueFreqsToPSSM(retval, alignment->query, sbp, 
+                                 seq_weights->std_prob);
+    PSIScaleMatrix(alignment->query, alignment->dimensions->query_sz, 
+                   seq_weights->std_prob, NULL, retval, sbp);
+
+    if (diagnostics) {
+        diagnostics = _PSISaveDiagnostics(alignment, aligned_block,
+                                          seq_weights);
+    } else {
+
+        /* FIXME: Deallocate structures selectively as some of these will be
+         * copied into the diagnostics structure */
+        _PSIAlignedBlockFree(aligned_block);
+        _PSISequenceWeightsFree(seq_weights);
+    }
+
+    return retval;
+}
+
+/****************************************************************************/
+
+/** Initializes the alignment data structure with the query sequence
+ * information. 
+ */
+static void
+PSIExtractQuerySequenceInfo(PsiAlignmentData* alignment);
+
+PsiAlignmentData*
+PSIAlignmentDataNew(const Uint1* query, const PsiInfo* info)
+{
+    PsiAlignmentData* retval = NULL;        /* the return value */
+    Uint4 s = 0;                            /* index in sequences */
+    Uint4 p = 0;                            /* index on positions */
+
+    if ( !query || !info ) {
+        return NULL;
+    }
+
+    if ( !(retval = (PsiAlignmentData*) calloc(1, sizeof(PsiAlignmentData)))) {
+         return NULL;
+    }
+
+    /* This doesn't need to be query_sz + 1 (posC) */
+    retval->res_counts = (Uint4**) _PSIAllocateMatrix(info->query_sz,
+                                                      PSI_ALPHABET_SIZE,
+                                                      sizeof(Uint4));
+    if ( !(retval->res_counts) ) {
+        return PSIAlignmentDataFree(retval);
+    }
+
+    retval->match_seqs = (Uint4*) calloc(info->query_sz, sizeof(int));
+    if ( !(retval->match_seqs)) {
+        return PSIAlignmentDataFree(retval);
+    }
+
+    retval->desc_matrix = (PsiDesc**) _PSIAllocateMatrix(info->num_seqs + 1,
+                                                         info->query_sz,
+                                                         sizeof(PsiDesc));
+    if (!retval->desc_matrix) {
+        return PSIAlignmentDataFree(retval);
+    }
+    for (s = 0; s < info->num_seqs + 1; s++) {
+        for (p = 0; p < info->query_sz; p++) {
+            retval->desc_matrix[s][p].letter = -1;
+            retval->desc_matrix[s][p].used = FALSE;
+            retval->desc_matrix[s][p].e_value = kDefaultEvalueForPosition;
+            retval->desc_matrix[s][p].extents.left = -1;
+            retval->desc_matrix[s][p].extents.right = info->query_sz;
+        }
+    }
+
+    retval->use_sequences = (Boolean*) calloc(info->num_seqs + 1, 
+                                              sizeof(Boolean));
+    if (!retval->use_sequences) {
+        return PSIAlignmentDataFree(retval);
+    }
+    /* All sequences are valid candidates for taking part in 
+       PSSM construction */
+    for (s = 0; s < info->num_seqs + 1; s++) {
+        retval->use_sequences[s] = TRUE;
+    }
+
+    if ( !(retval->dimensions = (PsiInfo*) calloc(1, sizeof(PsiInfo)))) {
+        return PSIAlignmentDataFree(retval);
+    }
+    memcpy((void*) retval->dimensions, (void*) info, sizeof(*info));
+
+    retval->query = (Uint1*) malloc(info->query_sz * sizeof(Uint1));
+    if ( !retval->query ) {
+        return PSIAlignmentDataFree(retval);
+    }
+    memcpy((void*) retval->query, (void*) query, info->query_sz);
+
+    PSIExtractQuerySequenceInfo(retval);
+
+    return retval;
+}
+
+PsiAlignmentData*
+PSIAlignmentDataFree(PsiAlignmentData* alignment)
+{
+    if ( !alignment ) {
+        return NULL;
+    }
+
+    if (alignment->res_counts) {
+        _PSIDeallocateMatrix((void**) alignment->res_counts,
+                             alignment->dimensions->query_sz);
+        alignment->res_counts = NULL;
+    }
+
+    if (alignment->match_seqs) {
+        sfree(alignment->match_seqs);
+    }
+
+    if (alignment->desc_matrix) {
+        _PSIDeallocateMatrix((void**) alignment->desc_matrix,
+                             alignment->dimensions->num_seqs + 1);
+        alignment->desc_matrix = NULL;
+    }
+
+    if (alignment->use_sequences) {
+        sfree(alignment->use_sequences);
+    }
+
+    if (alignment->dimensions) {
+        sfree(alignment->dimensions);
+    }
+
+    if (alignment->query) {
+        sfree(alignment->query);
+    }
+
+    sfree(alignment);
+    return NULL;
+}
+
+PsiMatrix*
+PSIMatrixNew(Uint4 query_sz, Uint4 alphabet_size)
+{
+    PsiMatrix* retval = NULL;
+
+    if ( !(retval = (PsiMatrix*) calloc(1, sizeof(PsiMatrix)))) {
+        return NULL;
+    }
+    retval->ncols = query_sz + 1;
+
+    retval->pssm = (int**) _PSIAllocateMatrix(query_sz + 1, alphabet_size,
+                                              sizeof(int));
+    if ( !(retval->pssm) ) {
+        return PSIMatrixFree(retval);
+    }
+
+    retval->scaled_pssm = (int**) _PSIAllocateMatrix(query_sz + 1, 
+                                                     alphabet_size,
+                                                     sizeof(int));
+    if ( !(retval->scaled_pssm) ) {
+        return PSIMatrixFree(retval);
+    }
+
+    retval->res_freqs = (double**) _PSIAllocateMatrix(query_sz + 1, 
+                                                      alphabet_size, 
+                                                      sizeof(double));
+    if ( !(retval->res_freqs) ) {
+        return PSIMatrixFree(retval);
+    }
+
+    return retval;
+}
+
+PsiMatrix*
+PSIMatrixFree(PsiMatrix* matrix)
+{
+    if ( !matrix ) {
+        return NULL;
+    }
+
+    if (matrix->pssm) {
+        _PSIDeallocateMatrix((void**) matrix->pssm, matrix->ncols);
+    }
+
+    if (matrix->scaled_pssm) {
+        _PSIDeallocateMatrix((void**) matrix->scaled_pssm, matrix->ncols);
+    }
+
+    if (matrix->res_freqs) {
+        _PSIDeallocateMatrix((void**) matrix->res_freqs, matrix->ncols);
+    }
+
+    sfree(matrix);
+
+    return NULL;
+}
+
+PsiDiagnostics*
+PSIDiagnosticsNew(Uint4 query_sz, Uint4 alphabet_size)
+{
+    PsiDiagnostics* retval = NULL;
+
+    if ( !(retval = (PsiDiagnostics*) calloc(1, sizeof(PsiDiagnostics)))) {
+        return NULL;
+    }
+
+    retval->info_content = (double**) _PSIAllocateMatrix(query_sz,
+                                                         alphabet_size,
+                                                         sizeof(double));
+    if ( !(retval->info_content) ) {
+        return PSIDiagnosticsFree(retval);
+    }
+    retval->ncols = query_sz;
+
+    return retval;
+}
+
+PsiDiagnostics*
+PSIDiagnosticsFree(PsiDiagnostics* diags)
+{
+    if ( !diags )
+        return NULL;
+
+    if (diags->info_content) {
+        _PSIDeallocateMatrix((void**) diags->info_content, diags->ncols);
+    }
+
+    sfree(diags);
+
+    return NULL;
+}
+
+/****************************************************************************/
+/* Auxiliary functions to populate PsiAlignmentData structure */
+static void
+PSIExtractQuerySequenceInfo(PsiAlignmentData* alignment)
+{
+    Uint4 i = 0;
+
+    ASSERT(alignment);
+
+    for (i = 0; i < alignment->dimensions->query_sz; i++) {
+        alignment->desc_matrix[kQueryIndex][i].letter = alignment->query[i];
+        alignment->desc_matrix[kQueryIndex][i].used = TRUE;
+        alignment->desc_matrix[kQueryIndex][i].e_value = 
+            PSI_INCLUSION_ETHRESH / 2;
+        alignment->desc_matrix[kQueryIndex][i].extents.left = 0;
+        alignment->desc_matrix[kQueryIndex][i].extents.right =
+            alignment->dimensions->query_sz;
+
+        alignment->res_counts[i][alignment->query[i]]++;
+        alignment->match_seqs[i]++;
+    }
+}
