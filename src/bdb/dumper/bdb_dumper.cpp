f36c73fbeaa68ab8fa97976048ac16196cf58f46
Anatoliy Kuznetsov <kuznets@ncbi.nlm.nih.gov>
2004-06-21 15:11:38 +0000

    Work in progress
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@16266 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/bdb/dumper/bdb_dumper.cpp
+++ src/bdb/dumper/bdb_dumper.cpp
@@ -39,6 +39,7 @@
 #include <bdb/bdb_expt.hpp>
 #include <bdb/bdb_types.hpp>
 #include <bdb/bdb_file.hpp>
+#include <bdb/bdb_cursor.hpp>
 #include <bdb/bdb_filedump.hpp>
 
 
@@ -48,30 +49,30 @@ USING_NCBI_SCOPE;
 
 struct SBDB_FieldDescription
 {
-	string        field_name;
-	string        type;
-	unsigned int  length;
-	bool          is_null;
-	bool          is_primary_key;
+    string        field_name;
+    string        type;
+    unsigned int  length;
+    bool          is_null;
+    bool          is_primary_key;
 };
 
 class CBDB_ConfigStructureParser
 {
 public:
-	typedef vector<SBDB_FieldDescription> TFileStructure;
+    typedef vector<SBDB_FieldDescription> TFileStructure;
 public:
-	CBDB_ConfigStructureParser() {};
+    CBDB_ConfigStructureParser() {};
 
-	void ParseConfigFile(const string& fname);
+    void ParseConfigFile(const string& fname);
 
-	bool ParseStructureLine(const string&           line, 
-	                        SBDB_FieldDescription*  descr);
-	
-	const TFileStructure& GetStructure() const { return m_FileStructure; }
-	TFileStructure& SetStructure() { return m_FileStructure; }
-	
+    bool ParseStructureLine(const string&           line, 
+                            SBDB_FieldDescription*  descr);
+    
+    const TFileStructure& GetStructure() const { return m_FileStructure; }
+    TFileStructure& SetStructure() { return m_FileStructure; }
+    
 private:
-	TFileStructure   m_FileStructure;
+    TFileStructure   m_FileStructure;
 };
 
 
@@ -80,107 +81,114 @@ private:
 /// Returns TRUE if parsing is successfull
 bool 
 CBDB_ConfigStructureParser::ParseStructureLine(const string&           line, 
-		                                       SBDB_FieldDescription*  descr)
+                                               SBDB_FieldDescription*  descr)
 {
-	list<string> tokens;
-	
-	NStr::Split(line, string(" \t"), tokens, NStr::eMergeDelims);
-	
-	descr->field_name = descr->type = "";
-	descr->length = 0;
-	descr->is_null = descr->is_primary_key = false;
-	
-	unsigned cnt = 0;
-	ITERATE(list<string>, it, tokens) {
-		switch (cnt) {
-		case 0:     // field name
-			descr->field_name = *it;
-			break;
-		case 1:     // field type
-			{
-			string s = *it;
-			
-			string::size_type offs1 = s.find_first_of("(");			
-			if (offs1 != string::npos) {
-				string::size_type offs2 = s.find_first_of(")");
-				if (offs2 == string::npos || offs2 < offs1) { 
-					// TODO: parsing error here
-					return false;
-				}
-				const char *ls = s.c_str() + offs1;
-				descr->length = ::atoi(ls);
-				s.resize(offs1);
-			}
-			
-			descr->type = s;
-			}
-			break;
-		default: // PK or NULL or NOT NULL
-			{
-			int cmp = NStr::CompareNocase(*it, string("PK"));
-			if (cmp == 0) {
-				descr->is_primary_key = true;
-			}
-			else {
-				cmp = NStr::CompareNocase(*it, string("key"));
-				if (cmp == 0) {
-					descr->is_primary_key = true;
-				}
-				else {
-					cmp = NStr::CompareNocase(*it, string("NULL"));
-					if (cmp == 0) {
-						descr->is_null = true;
-					}
-				}
-			}
-			}
-			break;
-		} // switch
-		++cnt;
-	} // ITERATE
-	
-	return true;
+    list<string> tokens;
+    
+    NStr::Split(line, string(" \t"), tokens, NStr::eMergeDelims);
+    
+    descr->field_name = descr->type = "";
+    descr->length = 0;
+    descr->is_null = descr->is_primary_key = false;
+    
+    unsigned cnt = 0;
+    ITERATE(list<string>, it, tokens) {
+        switch (cnt) {
+        case 0:     // field name
+            descr->field_name = *it;
+            break;
+        case 1:     // field type
+            {
+            string s = *it;
+            
+            string::size_type offs1 = s.find_first_of("(");            
+            if (offs1 != string::npos) {
+                string::size_type offs2 = s.find_first_of(")");
+                if (offs2 == string::npos || offs2 < offs1) { 
+                    // TODO: parsing error here
+                    return false;
+                }
+                const char *ls = s.c_str() + offs1;
+                descr->length = ::atoi(ls);
+                s.resize(offs1);
+            }
+            
+            descr->type = s;
+            }
+            break;
+        default: // PK or NULL or NOT NULL
+            {
+            int cmp = NStr::CompareNocase(*it, string("PK"));
+            if (cmp == 0) {
+                descr->is_primary_key = true;
+            }
+            else {
+                cmp = NStr::CompareNocase(*it, string("key"));
+                if (cmp == 0) {
+                    descr->is_primary_key = true;
+                }
+                else {
+                    cmp = NStr::CompareNocase(*it, string("NULL"));
+                    if (cmp == 0) {
+                        descr->is_null = true;
+                    }
+                }
+            }
+            }
+            break;
+        } // switch
+        ++cnt;
+    } // ITERATE
+    
+    return true;
 }
 
 void CBDB_ConfigStructureParser::ParseConfigFile(const string& fname)
 {
-	string line;	
-	unsigned line_idx = 0;
-	
-	m_FileStructure.resize(0);
-	
-	CBDB_FieldFactory ffact;
-	
-	CNcbiIfstream fi(fname.c_str());
-	for ( ;fi.good(); ++line_idx) {
-		getline(fi, line);
+    string line;    
+    unsigned line_idx = 0;
+    
+    m_FileStructure.resize(0);
+    
+    CBDB_FieldFactory ffact;
+    
+    CNcbiIfstream fi(fname.c_str());
+    for ( ;fi.good(); ++line_idx) {
+        getline(fi, line);
 
-		// check if it is a comment
-		const char* s = line.c_str();
-		for( ;isspace(*s); ++s) {}
-		
-		if (*s == 0 || *s == '#') // empty line or comment
-			continue;
-				
-		SBDB_FieldDescription fdescr;
-		bool parsed = ParseStructureLine(line, &fdescr);
-		
-		CBDB_FieldFactory::EType ft = ffact.GetType(fdescr.type);
-		if (ft == CBDB_FieldFactory::eUnknown) {
-			BDB_THROW(eInvalidType, fdescr.type);			
-		}
-				
-		if (parsed) {
-			m_FileStructure.push_back(fdescr);
-		} else {
-			NcbiCerr << "Error in line: " << line_idx << endl;
-			return;
-		}
-		
-	} // for
+        // check if it is a comment
+        const char* s = line.c_str();
+        for( ;isspace(*s); ++s) {}
+        
+        if (*s == 0 || *s == '#') // empty line or comment
+            continue;
+                
+        SBDB_FieldDescription fdescr;
+        bool parsed = ParseStructureLine(line, &fdescr);
+        
+        CBDB_FieldFactory::EType ft = ffact.GetType(fdescr.type);
+        if (ft == CBDB_FieldFactory::eUnknown) {
+            BDB_THROW(eInvalidType, fdescr.type);            
+        }
+                
+        if (parsed) {
+            m_FileStructure.push_back(fdescr);
+        } else {
+            NcbiCerr << "Error in line: " << line_idx << endl;
+            return;
+        }
+        
+    } // for
 }
 
 
+///////////////////////////////////////////////////////////////////////
+
+
+/// BDB file dumper application
+///
+/// @internal
+///
 class CBDB_FileDumperApp : public CNcbiApplication
 {
 public:
@@ -191,71 +199,105 @@ public:
 void CBDB_FileDumperApp::Init(void)
 {
 
-	// Setup command line arguments and parameters
-		
+    // Setup command line arguments and parameters
+        
     // Create command-line argument descriptions class
     auto_ptr<CArgDescriptions> arg_desc(new CArgDescriptions);
 
     // Specify USAGE context
     arg_desc->SetUsageContext(GetArguments().GetProgramBasename(),
                               "BDB dumper");
-	
-	arg_desc->AddPositional("dbname", 
-			                "BDB database name", CArgDescriptions::eString);
+    
+    arg_desc->AddPositional("dbname", 
+                            "BDB database name", CArgDescriptions::eString);
 
-	arg_desc->AddPositional("confname",
-			                "BDB database structure configuration file", 
-			                 CArgDescriptions::eString);
+    arg_desc->AddPositional("confname",
+                            "BDB database structure configuration file", 
+                             CArgDescriptions::eString);
+    
+    arg_desc->AddOptionalKey("k",
+                             "dbkey",
+                             "File key value.(Dumps record for only this key)",
+                             CArgDescriptions::eString);
+
+    arg_desc->AddOptionalKey("cs",
+                             "column_separator",
+                             "Column separator string (default:TAB)",
+                             CArgDescriptions::eString);
+
+    arg_desc->AddFlag("nl", "Do NOT print field labels(names)");
 
     // Setup arg.descriptions for this application
     SetupArgDescriptions(arg_desc.release());
-	
+    
 }
 
 int CBDB_FileDumperApp::Run(void)
 {
     try
     {
-	    CArgs args = GetArgs();
-		const string& db_name = args["dbname"].AsString();
-		const string& conf_name = args["confname"].AsString();
-		CBDB_ConfigStructureParser parser;
-		parser.ParseConfigFile(conf_name);
-		
-		CBDB_ConfigStructureParser::TFileStructure& fs = 
-				parser.SetStructure();
-		
-		
-		CBDB_FieldFactory ffact;	
-		CBDB_File db_file;
-		db_file.SetFieldOwnership(true);
-		
-		NON_CONST_ITERATE(CBDB_ConfigStructureParser::TFileStructure, it, fs) {
-			CBDB_FieldFactory::EType ft = ffact.GetType(it->type);
-			if (ft == CBDB_FieldFactory::eString ||
-				ft == CBDB_FieldFactory::eLString) {
-				if (it->length == 0) {
-					it->length = 4096;
-				}
-			}
-			CBDB_Field*	field = ffact.Create(ft);
-			if (it->is_primary_key) {
-				db_file.BindKey(it->field_name.c_str(), 
-   					            field, 
-						        it->length);
-			} else {
-				db_file.BindData(it->field_name.c_str(), 
-   					            field, 
-						        it->length,
-				  it->is_null ? eNullable : eNotNullable);
-			}
-		}
-		
-		db_file.Open(db_name.c_str(), CBDB_File::eReadOnly);
-		
-		CBDB_FileDumper fdump;
-		fdump.Dump(NcbiCout, db_file);
-		
+        CArgs args = GetArgs();
+        const string& db_name = args["dbname"].AsString();
+        const string& conf_name = args["confname"].AsString();
+        CBDB_ConfigStructureParser parser;
+        parser.ParseConfigFile(conf_name);
+        
+        CBDB_ConfigStructureParser::TFileStructure& fs = 
+                parser.SetStructure();
+        
+        
+        CBDB_FieldFactory ffact;    
+        CBDB_File db_file;
+        db_file.SetFieldOwnership(true);
+        
+        NON_CONST_ITERATE(CBDB_ConfigStructureParser::TFileStructure, it, fs) {
+            CBDB_FieldFactory::EType ft = ffact.GetType(it->type);
+            if (ft == CBDB_FieldFactory::eString ||
+                ft == CBDB_FieldFactory::eLString) {
+                if (it->length == 0) {
+                    it->length = 4096;
+                }
+            }
+            CBDB_Field*    field = ffact.Create(ft);
+            if (it->is_primary_key) {
+                db_file.BindKey(it->field_name.c_str(), 
+                                   field, 
+                                it->length);
+            } else {
+                db_file.BindData(it->field_name.c_str(), 
+                                   field, 
+                                it->length,
+                  it->is_null ? eNullable : eNotNullable);
+            }
+        }
+        
+        db_file.Open(db_name.c_str(), CBDB_File::eReadOnly);
+
+        CBDB_FileDumper fdump;
+        if (args["nl"]) {
+            fdump.SetColumnNames(CBDB_FileDumper::eDropNames);
+        }
+        fdump.SetValueFormatting(CBDB_FileDumper::eQuoteStrings);
+        
+        if (args["cs"]) {
+            fdump.SetColumnSeparator(args["cs"].AsString());
+        }
+
+        if (args["k"]) {
+            const string& key_str = args["k"].AsString();
+
+            CBDB_FileCursor cur(db_file);
+            cur.SetCondition(CBDB_FileCursor::eEQ);
+            cur.From << key_str;
+
+            fdump.Dump(NcbiCout, cur);
+
+            return 0;
+        }
+        
+        
+        fdump.Dump(NcbiCout, db_file);
+        
     }
     catch (CBDB_ErrnoException& ex)
     {
@@ -280,6 +322,9 @@ int main(int argc, const char* argv[])
 /*
  * ===========================================================================
  * $Log$
+ * Revision 1.2  2004/06/21 15:11:38  kuznets
+ * Work in progress
+ *
  * Revision 1.1  2004/06/17 16:28:24  kuznets
  * Initial revision
  *
