d2682e1526b532407260984e0f33c9b1977eedae
Anton Lavrentiev <lavr@ncbi.nlm.nih.gov>
2004-06-22 16:50:51 +0000

    Note default content type (if explicit one is not provided)
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@16314 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/cgi/ncbicgir.hpp
+++ include/cgi/ncbicgir.hpp
@@ -2,36 +2,36 @@
 #define CGI___NCBICGIR__HPP
 
 /*  $Id$
-* ===========================================================================
-*
-*                            PUBLIC DOMAIN NOTICE
-*               National Center for Biotechnology Information
-*
-*  This software/database is a "United States Government Work" under the
-*  terms of the United States Copyright Act.  It was written as part of
-*  the author's official duties as a United States Government employee and
-*  thus cannot be copyrighted.  This software/database is freely available
-*  to the public for use. The National Library of Medicine and the U.S.
-*  Government have not placed any restriction on its use or reproduction.
-*
-*  Although all reasonable efforts have been taken to ensure the accuracy
-*  and reliability of the software and data, the NLM and the U.S.
-*  Government do not and cannot warrant the performance or results that
-*  may be obtained by using this software or data. The NLM and the U.S.
-*  Government disclaim all warranties, express or implied, including
-*  warranties of performance, merchantability or fitness for any particular
-*  purpose.
-*
-*  Please cite the author in any work or product based on this material.
-*
-* ===========================================================================
-*
-* Authors:  Eugene Vasilchenko, Denis Vakatov
-*
-* File Description:
-*   CCgiResponse  -- CGI response generator class
-*
-*/
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Authors:  Eugene Vasilchenko, Denis Vakatov
+ *
+ * File Description:
+ *   CCgiResponse  -- CGI response generator class
+ *
+ */
 
 #include <cgi/ncbicgi.hpp>
 #include <map>
@@ -72,7 +72,7 @@ public:
     string GetHeaderValue (const string& name) const;
     bool   HaveHeaderValue(const string& name) const;
 
-    // Set/get content type
+    // Set/get content type [text/html by default if not provided]
     void   SetContentType(const string& type);
     string GetContentType(void) const;
 
@@ -187,6 +187,9 @@ END_NCBI_SCOPE
  * ===========================================================================
  *
  * $Log$
+ * Revision 1.16  2004/06/22 16:50:51  lavr
+ * Note default content type (if explicit one is not provided)
+ *
  * Revision 1.15  2003/11/05 18:40:55  dicuccio
  * Added export specifiers
  *
