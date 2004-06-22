432d6ed0ab0608731ebe124d55f3a99529d13474
Ilya Dondoshansky <dondosha@ncbi.nlm.nih.gov>
2004-06-22 17:51:00 +0000

    1. Added locking mechanism for writing results from multiple threads; 2. Added more doxygen comments;
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@16319 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/algo/blast/core/hspstream_collector.c
+++ src/algo/blast/core/hspstream_collector.c
@@ -41,6 +41,10 @@ static char const rcsid[] =
 
 /** Default hit saving stream methods */
 
+/** Free the BlastHSPStream with its HSP list collector data structure.
+ * @param hsp_stream The HSP stream to free [in]
+ * @return NULL.
+ */
 static BlastHSPStream* 
 BlastHSPListCollectorFree(BlastHSPStream* hsp_stream) 
 {
@@ -52,6 +56,10 @@ BlastHSPListCollectorFree(BlastHSPStream* hsp_stream)
    return NULL;
 }
 
+/** Prohibit any future writing to the HSP stream when all results are written.
+ * Also perform sorting of results here to prepare them for reading.
+ * @param hsp_stream The HSP stream to close [in] [out]
+ */ 
 static void 
 BlastHSPListCollectorClose(BlastHSPStream* hsp_stream)
 {
@@ -65,11 +73,19 @@ BlastHSPListCollectorClose(BlastHSPStream* hsp_stream)
       Blast_HSPResultsReverseSort(stream_data->results);
    }
    stream_data->results_sorted = TRUE;
+   stream_data->x_lock = MT_LOCK_Delete(stream_data->x_lock);
 }
 
+/** Read one HSP list from the results saved in an HSP list collector. Once an
+ * HSP list is read from the stream, it relinquishes ownership and removes it
+ * from the internal results data structure.
+ * @param hsp_stream The HSP stream to read from [in]
+ * @param hsp_list_out The read HSP list. [out]
+ * @return Success, error, or end of reading, when nothing left to read.
+ */
 static int 
 BlastHSPListCollectorRead(BlastHSPStream* hsp_stream, 
-                              BlastHSPList** hsp_list_out) 
+                          BlastHSPList** hsp_list_out) 
 {
    BlastHSPListCollectorData* stream_data = 
       (BlastHSPListCollectorData*) GetData(hsp_stream);
@@ -120,14 +136,21 @@ BlastHSPListCollectorRead(BlastHSPStream* hsp_stream,
    return kBlastHSPStream_Success;
 }
 
+/** Write an HSP list to the collector HSP stream. The HSP stream assumes 
+ * ownership of the HSP list and sets the dereferenced pointer to NULL.
+ * @param hsp_stream Stream to write to. [in] [out]
+ * @param hsp_list Pointer to the HSP list to save in the collector. [in]
+ * @return Success or error, if stream is already closed for writing.
+ */
 static int 
 BlastHSPListCollectorWrite(BlastHSPStream* hsp_stream, 
-                              BlastHSPList** hsp_list)
+                           BlastHSPList** hsp_list)
 {
    BlastHSPListCollectorData* stream_data = 
       (BlastHSPListCollectorData*) GetData(hsp_stream);
 
-   /** @todo Lock the mutex here */
+   /** Lock the mutex, if necessary */
+   MT_LOCK_Do(stream_data->x_lock, eMT_Lock);
 
    /** Prohibit writing after reading has already started. This prohibition
     *  can be lifted later. There is no inherent problem in using read and
@@ -156,11 +179,17 @@ BlastHSPListCollectorWrite(BlastHSPStream* hsp_stream,
    /* Free the caller from this pointer's ownership. */
    *hsp_list = NULL;
 
-   /** @todo Unlock the mutex here */
+   /** Unlock the mutex */
+   MT_LOCK_Do(stream_data->x_lock, eMT_Unlock);
 
    return kBlastHSPStream_Success;
 }
 
+/** Initialize function pointers and data structure in a collector HSP stream.
+ * @param hsp_stream The stream to initialize [in] [out]
+ * @param args Pointer to the collector data structure. [in]
+ * @return Filled HSP stream.
+ */
 static BlastHSPStream* 
 BlastHSPListCollectorNew(BlastHSPStream* hsp_stream, void* args) 
 {
@@ -180,8 +209,9 @@ BlastHSPListCollectorNew(BlastHSPStream* hsp_stream, void* args)
 }
 
 BlastHSPStream* 
-Blast_HSPListCollectorInit(Uint1 program, BlastHitSavingOptions* hit_options,
-                           Int4 num_queries, Boolean sort_on_read)
+Blast_HSPListCollectorInitMT(Uint1 program, BlastHitSavingOptions* hit_options,
+                           Int4 num_queries, Boolean sort_on_read,
+                           MT_LOCK lock)
 {
     BlastHSPListCollectorData* stream_data = 
        (BlastHSPListCollectorData*) malloc(sizeof(BlastHSPListCollectorData));
@@ -201,9 +231,19 @@ Blast_HSPListCollectorInit(Uint1 program, BlastHitSavingOptions* hit_options,
     stream_data->results_sorted = FALSE;
     stream_data->sort_on_read = sort_on_read;
     stream_data->first_query_index = 0;
+    stream_data->x_lock = lock;
 
     info.constructor = &BlastHSPListCollectorNew;
     info.ctor_argument = (void*)stream_data;
 
     return BlastHSPStreamNew(&info);
 }
+
+BlastHSPStream* 
+Blast_HSPListCollectorInit(Uint1 program, BlastHitSavingOptions* hit_options,
+                           Int4 num_queries, Boolean sort_on_read)
+{
+   return Blast_HSPListCollectorInitMT(program, hit_options, num_queries, 
+                                       sort_on_read, NULL);
+}
+
