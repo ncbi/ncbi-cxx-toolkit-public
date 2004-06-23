0a205eac33e053ee5f1730c6784f4503d137ecbc
kapustin <kapustin@ncbi.nlm.nih.gov>
2004-06-23 19:31:10 +0000

    Minor cleanup
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@16369 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/algo/align/demo/splign/seq_loader.cpp
+++ src/algo/align/demo/splign/seq_loader.cpp
@@ -57,212 +57,213 @@ USING_SCOPE(objects);
 
 void CSeqLoader::Open(const string& filename_index)
 {
-  CNcbiIfstream idxstream (filename_index.c_str());
-  if(idxstream) {
+    CNcbiIfstream idxstream (filename_index.c_str());
 
-    // first read file names
-    char buf [1024];
-    bool found_start = false;
-    while(idxstream.good()) {
-      buf[0] = 0;
-      idxstream.getline(buf, sizeof buf, '\n');
-      if(buf[0] == '#') continue;
-      if(idxstream.fail()) {
-	goto throw_file_corrupt;
-      }
-      if(strcmp(buf,"$$$FI") == 0) {
-	found_start = true;
-	break;
-      }
+    if(idxstream) {
+        
+        // first read file names
+        char buf [1024];
+        bool found_start = false;
+        while(idxstream.good()) {
+
+            buf[0] = 0;
+            idxstream.getline(buf, sizeof buf, '\n');
+            if(buf[0] == '#') continue;
+            if(idxstream.fail()) {
+                goto throw_file_corrupt;
+            }
+            if(strcmp(buf,"$$$FI") == 0) {
+                found_start = true;
+                break;
+            }
+        }
+
+        if(!found_start) {
+            goto throw_file_corrupt;
+        }
+
+        vector<string> filenames;
+        vector<size_t> indices;
+        found_start = false;
+        m_min_idx = kMax_UInt;
+        while(idxstream.good()) {
+            buf[0] = 0;
+            idxstream.getline(buf, sizeof buf, '\n');
+
+            if(strcmp("$$$SI", buf) == 0) {
+                found_start = true;
+                break;
+            }
+            if(buf[0] == '#' || buf[0] == 0) {
+                continue;
+            }
+            CNcbiIstrstream iss (buf);
+            size_t idx = kMax_UInt;
+            string name;
+            iss >> name >> idx;
+            if(idx == kMax_UInt) {
+                goto throw_file_corrupt;
+            }
+            filenames.push_back(name);
+            indices.push_back(idx);
+            if(idx < m_min_idx) {
+                m_min_idx = idx;
+            }
+        }
+        if(!found_start) {
+            goto throw_file_corrupt;
+        }
+
+        const size_t fndim = filenames.size();
+        m_filenames.resize(fndim);
+        for(size_t i = 0; i < fndim; ++i) {
+            m_filenames[indices[i] - m_min_idx] = filenames[i];
+        }
+        
+        m_idx.clear();
+        while(idxstream.good()) {
+            
+            buf[0] = 0;
+            idxstream.getline(buf, sizeof buf, '\n');
+            
+            if(buf[0] == '#') continue; // skip comments
+            if(idxstream.eof()) break;
+            
+            if(idxstream.fail()) {
+                goto throw_file_corrupt;
+            }
+            
+            CNcbiIstrstream iss (buf);
+            string id;
+            SIdxTarget s;
+            iss >> id >> s.m_filename_idx >> s.m_offset;
+            if(s.m_offset == kMax_UInt) {
+                goto throw_file_corrupt;
+            }
+            m_idx[id] = s;
+        }
+        
+        return;
     }
-    if(!found_start) {
-	goto throw_file_corrupt;
+    else {      
+        
+        NCBI_THROW( CSplignAppException,
+                    eCannotOpenFile,
+                    filename_index.c_str());
     }
-
-    vector<string> filenames;
-    vector<size_t> indices;
-    found_start = false;
-    m_min_idx = kMax_UInt;
-    while(idxstream.good()) {
-      buf[0] = 0;
-      idxstream.getline(buf, sizeof buf, '\n');
-
-      if(strcmp("$$$SI", buf) == 0) {
-	found_start = true;
-	break;
-      }
-      if(buf[0] == '#' || buf[0] == 0) {
-	continue;
-      }
-      CNcbiIstrstream iss (buf);
-      size_t idx = kMax_UInt;
-      string name;
-      iss >> name >> idx;
-      if(idx == kMax_UInt) {
-	goto throw_file_corrupt;
-      }
-      filenames.push_back(name);
-      indices.push_back(idx);
-      if(idx < m_min_idx) {
-	m_min_idx = idx;
-      }
-    }
-    if(!found_start) goto throw_file_corrupt;
-
-    const size_t fndim = filenames.size();
-    m_filenames.resize(fndim);
-    for(size_t i = 0; i < fndim; ++i) {
-      m_filenames[indices[i] - m_min_idx] = filenames[i];
-    }
-
-    m_idx.clear();
-    while(idxstream.good()) {
-
-      buf[0] = 0;
-      idxstream.getline(buf, sizeof buf, '\n');
-
-      if(buf[0] == '#') continue; // skip comments
-      if(idxstream.eof()) break;
-
-      if(idxstream.fail()) {
-	goto throw_file_corrupt;
-      }
-
-      CNcbiIstrstream iss (buf);
-      string id;
-      SIdxTarget s;
-      iss >> id >> s.m_filename_idx >> s.m_offset;
-      if(s.m_offset == kMax_UInt) {
-	goto throw_file_corrupt;
-      }
-      m_idx[id] = s;
-    }
-
-    return;
-  }
-  else {      
-    NCBI_THROW(
-	       CSplignAppException,
-	       eCannotOpenFile,
-	       filename_index.c_str());
-  }
 
  throw_file_corrupt: {
-
-    NCBI_THROW(
-	       CSplignAppException,
-	       eErrorReadingIndexFile,
-	       "File is corrupt");
-  }
+        
+        NCBI_THROW( CSplignAppException,
+                    eErrorReadingIndexFile,
+                    "File is corrupt");
+    }
 }
 
 
 void CSeqLoader::Load(const string& id, vector<char>* seq,
 		      size_t from, size_t to)
 {
-  if(seq == 0) {
-    NCBI_THROW(
-               CSplignAppException,
-               eInternal,
-               "Null pointer passed to CSeqLoader::Load()");
+    if(seq == 0) {
+        NCBI_THROW( CSplignAppException,
+                    eInternal,
+                    "Null pointer passed to CSeqLoader::Load()");
+        
+    }
     
-  }
-  
-  auto_ptr<istream> input (0);
-
-  map<string, SIdxTarget>::const_iterator im = m_idx.find(id);
-  if(im == m_idx.end()) {
-    string msg ("Unable to locate ");
-    msg += id;
-    NCBI_THROW(
-	       CSplignAppException,
-	       eInternal,
-	       msg.c_str());
-  }
-  else {
-    const string& filename = m_filenames[im->second.m_filename_idx-m_min_idx];
-    input.reset(new ifstream (filename.c_str()));
-    input->seekg(im->second.m_offset);
-  }
-  
-  char buf [1024];
-  input->getline(buf, sizeof buf, '\n'); // info line
-  if(input->fail()) {
-    NCBI_THROW(
-	       CSplignAppException,
-	       eCannotReadFile,
-	       "Unable to read sequence data");
-  }
-
-  seq->clear();
-
-  if(from == 0 && to == kMax_UInt) {
-    // read entire sequence until the next one or eof
-    while(*input) {
-      CT_POS_TYPE i0 = input->tellg();
-      input->getline(buf, sizeof buf, '\n');
-      if(!*input) {
-	break;
-      }
-      CT_POS_TYPE i1 = input->tellg();
-      if(i1 - i0 > 1) {
-	CT_OFF_TYPE line_size = i1 - i0;
-	line_size = line_size - 1;
-	if(buf[0] == '>') break;
-	size_t size_old = seq->size();
-	seq->resize(size_old + line_size);
-	transform(buf, buf + line_size, seq->begin() + size_old,
-                  (int(*)(int))toupper);
-      }
-      else if (i0 == i1) {
-	break; // GCC hack
-      }
+    auto_ptr<istream> input (0);
+    
+    map<string, SIdxTarget>::const_iterator im = m_idx.find(id);
+    if(im == m_idx.end()) {
+        string msg ("Unable to locate ");
+        msg += id;
+        NCBI_THROW( CSplignAppException,
+                    eInternal,
+                    msg.c_str());
     }
-  }
-  else {
-    // read only a portion of a sequence
-    const size_t dst_seq_len = to - from + 1;
-    seq->resize(dst_seq_len + sizeof buf);
-    CT_POS_TYPE i0 = input->tellg(), i1;
-    CT_OFF_TYPE dst_read = 0, src_read = 0;
-    while(*input) {
-      input->getline(buf, sizeof buf, '\n');
-      if(buf[0] == '>' || !*input) {
-	seq->resize(dst_read);
-	return;
-      }
-      i1 = input->tellg();
-
-      CT_OFF_TYPE off = i1 - i0;
-      if (off > 1) {
-	src_read += off - 1;
-      }
-      else if (off == 1) {
-	continue;
-      }
-      else { 
-	break; // GCC hack
-      }
-
-      if(src_read > CT_OFF_TYPE(from)) {
-	CT_OFF_TYPE line_size = i1 - i0;
-	line_size = line_size - 1;
-	size_t start  = dst_read? 0: (line_size - (src_read - from));
-	size_t finish = (src_read > CT_OFF_TYPE(to))?
-	                (line_size - (src_read - to) + 1):
-	                (size_t)line_size;
-	transform(buf + start, buf + finish, seq->begin() + dst_read,
-                  (int(*)(int))toupper);
-	dst_read += finish - start;
-	if(dst_read >= CT_OFF_TYPE(dst_seq_len)) {
-	  seq->resize(dst_seq_len);
-	  return;
-	}
-      }
-      i0 = i1;
+    else {
+        const string& filename = m_filenames[im->second.m_filename_idx-m_min_idx];
+        input.reset(new ifstream (filename.c_str()));
+        input->seekg(im->second.m_offset);
     }
-    seq->resize(dst_read);
-  }
+  
+    char buf [1024];
+    input->getline(buf, sizeof buf, '\n'); // info line
+    if(input->fail()) {
+        NCBI_THROW( CSplignAppException,
+                    eCannotReadFile,
+                    "Unable to read sequence data");
+    }
+    
+    seq->clear();
+    
+    if(from == 0 && to == kMax_UInt) {
+        // read entire sequence until the next one or eof
+        while(*input) {
+            CT_POS_TYPE i0 = input->tellg();
+            input->getline(buf, sizeof buf, '\n');
+            if(!*input) {
+                break;
+            }
+            CT_POS_TYPE i1 = input->tellg();
+            if(i1 - i0 > 1) {
+                CT_OFF_TYPE line_size = i1 - i0;
+                line_size = line_size - 1;
+                if(buf[0] == '>') break;
+                size_t size_old = seq->size();
+                seq->resize(size_old + line_size);
+                transform(buf, buf + line_size, seq->begin() + size_old,
+                          (int(*)(int))toupper);
+            }
+            else if (i0 == i1) {
+                break; // GCC hack
+            }
+        }
+    }
+    else {
+        // read only a portion of a sequence
+        const size_t dst_seq_len = to - from + 1;
+        seq->resize(dst_seq_len + sizeof buf);
+        CT_POS_TYPE i0 = input->tellg(), i1;
+        CT_OFF_TYPE dst_read = 0, src_read = 0;
+        while(*input) {
+            input->getline(buf, sizeof buf, '\n');
+            if(buf[0] == '>' || !*input) {
+                seq->resize(dst_read);
+                return;
+            }
+            i1 = input->tellg();
+            
+            CT_OFF_TYPE off = i1 - i0;
+            if (off > 1) {
+                src_read += off - 1;
+            }
+            else if (off == 1) {
+                continue;
+            }
+            else { 
+                break; // GCC hack
+            }
+            
+            if(src_read > CT_OFF_TYPE(from)) {
+                CT_OFF_TYPE line_size = i1 - i0;
+                line_size = line_size - 1;
+                size_t start  = dst_read? 0: (line_size - (src_read - from));
+                size_t finish = (src_read > CT_OFF_TYPE(to))?
+                    (line_size - (src_read - to) + 1):
+                    (size_t)line_size;
+                transform(buf + start, buf + finish, seq->begin() + dst_read,
+                          (int(*)(int))toupper);
+                dst_read += finish - start;
+                if(dst_read >= CT_OFF_TYPE(dst_seq_len)) {
+                    seq->resize(dst_seq_len);
+                    return;
+                }
+            }
+            i0 = i1;
+        }
+        seq->resize(dst_read);
+    }
 }
 
 
@@ -367,6 +368,9 @@ END_NCBI_SCOPE
 /*
  * ===========================================================================
  * $Log$
+ * Revision 1.18  2004/06/23 19:31:10  kapustin
+ * Minor cleanup
+ *
  * Revision 1.17  2004/05/21 21:41:02  gorelenk
  * Added PCH ncbi_pch.hpp
  *
