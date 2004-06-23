0b4feb85bea49904c7528412458eb6070b613e1e
lebedev <lebedev@ncbi.nlm.nih.gov>
2004-06-23 17:03:44 +0000

    Initial revision
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@16356 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ include/corelib/config/ncbiconf_xcode.h
@@ -0,0 +1,342 @@
+/* $Id$
+ * By Vlad Lebedev, NCBI (lebedev@ncbi.nlm.nih.gov)
+ *
+ * Mac OS X - xCode Build (GCC 3.3)
+ *
+ * NOTE:  Unlike its UNIX counterpart, this configuration header
+ *        is manually maintained in order to keep it in-sync with the
+ *        "configure"-generated configuration headers.
+ */
+
+
+/* Define to 1 if you have the <arpa/inet.h> header file. */
+#define HAVE_ARPA_INET_H 1
+
+/* Define to 1 if you have the `atoll' function. */
+#define HAVE_ATOLL 1
+
+/* Define to 1 if Berkeley DB libraries are available. */
+#define HAVE_BERKELEY_DB 1
+
+/* Define to 1 if the preprocessor supports GNU-style variadic macros. */
+#define HAVE_CPP_GNU_VARARGS 1
+
+/* Define to 1 if the preprocessor supports C99-style variadic macros. */
+#define HAVE_CPP_STD_VARARGS 1
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1 if FLTK is available. */
+#define HAVE_FLTK 1
+
+/* Define to 1 if you have the <fstream> header file. */
+#define HAVE_FSTREAM 1
+
+/* Define to 1 if you have the <fstream.h> header file. */
+#define HAVE_FSTREAM_H 1
+
+/* Define to 1 if you have the `getaddrinfo' function. */
+#define HAVE_GETADDRINFO 1
+
+/* Define to 1 if you have the `getnameinfo' function. */
+#define HAVE_GETNAMEINFO 1
+
+/* Define to 1 if you have the `getpagesize' function. */
+#define HAVE_GETPAGESIZE 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the <iostream> header file. */
+#define HAVE_IOSTREAM 1
+
+/* Define to 1 if you have the <iostream.h> header file. */
+#define HAVE_IOSTREAM_H 1
+
+/* Define to 1 if you have `ios(_base)::register_callback'. */
+#define HAVE_IOS_REGISTER_CALLBACK 1
+
+/* Define to 1 if libbz2 is available. */
+#define HAVE_LIBBZ2 1
+
+/* Define to 1 if CRYPT is available, either in its own library or as part of
+   the standard libraries. */
+#define HAVE_LIBCRYPT 1
+
+/* Define to 1 if DL is available, either in its own library or as part of the
+   standard libraries. */
+#define HAVE_LIBDL 1
+
+/* Define to 1 if libexpat is available. */
+/* #undef HAVE_LIBEXPAT */
+
+/* Define to 1 if FastCGI libraries are available. */
+/* #undef HAVE_LIBFASTCGI */
+
+/* Define to 1 if FreeTDS libraries are available. */
+/* #undef HAVE_LIBFTDS */
+
+/* Define to 1 if NCBI GEO DB library is available. */
+/* #undef HAVE_LIBGEODB */
+
+/* Define to 1 if you have libglut. */
+/* #undef HAVE_LIBGLUT */
+
+/* Define to 1 if ICONV is available, either in its own library or as part of
+   the standard libraries. */
+/* #undef HAVE_LIBICONV */
+
+/* Define to 1 if KSTAT is available, either in its own library or as part of
+   the standard libraries. */
+/* #undef HAVE_LIBKSTAT */
+
+/* Define to 1 if you have libOSMesa. */
+/* #undef HAVE_LIBOSMESA */
+
+/* Define to 1 if libpcre is available. */
+#define HAVE_LIBPCRE 1
+
+/* Define to 1 if NCBI PubMed libraries are available. */
+/* #undef HAVE_LIBPUBMED */
+
+/* Define to 1 if RPCSVC is available, either in its own library or as part of
+   the standard libraries. */
+/* #undef HAVE_LIBRPCSVC */
+
+/* Define to 1 if RT is available, either in its own library or as part of the
+   standard libraries. */
+/* #undef HAVE_LIBRT */
+
+/* Define to 1 if libsablot is available. */
+/* #undef HAVE_LIBSABLOT */
+
+/* Define to 1 if the SP SGML library is available. */
+/* #undef HAVE_LIBSP */
+
+/* Define to 1 if libsqlite is available. */
+#define HAVE_LIBSQLITE 1
+
+/* Define to 1 if the NCBI SSS DB library is available. */
+/* #undef HAVE_LIBSSSDB */
+
+/* Define to 1 if the NCBI SSS UTILS library is available. */
+/* #undef HAVE_LIBSSSUTILS */
+
+/* Define to 1 if SYBASE libraries are available. */
+/* #undef HAVE_LIBSYBASE */
+
+/* Define to 1 if SYBASE DBLib is available. */
+/* #undef HAVE_LIBSYBDB */
+
+/* Define to 1 if libungif is available. */
+/* #undef HAVE_LIBUNGIF */
+
+/* Define to 1 if libxml2 is available. */
+/* #undef HAVE_LIBXML */
+
+/* Define to 1 if libXpm is available. */
+/* #undef HAVE_LIBXPM */
+
+/* Define to 1 if libz is available. */
+#define HAVE_LIBZ 1
+
+/* Define to 1 if you have the <limits> header file. */
+#define HAVE_LIMITS 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if MySQL is available. */
+/* #undef HAVE_MYSQL */
+
+/* Define to 1 if the NCBI C toolkit is available. */
+/* #undef HAVE_NCBI_C */
+
+/* Define to 1 if you have the <netinet/in.h> header file. */
+#define HAVE_NETINET_IN_H 1
+
+/* Define to 1 if you have OpenGL (-lGL). */
+#define HAVE_OPENGL 1
+
+/* Define to 1 if you have the <poll.h> header file. */
+#define HAVE_POLL_H 1
+
+/* If you have the `readdir_r' function, define to the number of arguments it
+   takes (normally 2 or 3). */
+#define HAVE_READDIR_R 3
+
+/* Define to 1 if you have `union semun'. */
+#define HAVE_SEMUN 1
+
+/* Define to 1 if `sin_len' is a member of `struct sockaddr_in'. */
+#define HAVE_SIN_LEN 1
+
+/* Define to 1 if the system has the type `socklen_t'. */
+#define HAVE_SOCKLEN_T 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the `strcasecmp' function. */
+#define HAVE_STRCASECMP 1
+
+/* Define to 1 if you have the `strdup' function. */
+#define HAVE_STRDUP 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <strstream> header file. */
+#define HAVE_STRSTREAM 1
+
+/* Define to 1 if you have SysV semaphores. */
+#define HAVE_SYSV_SEMAPHORES 1
+
+/* Define to 1 if you have the <sys/socket.h> header file. */
+#define HAVE_SYS_SOCKET_H 1
+
+/* Define to 1 if you have the <sys/sockio.h> header file. */
+#define HAVE_SYS_SOCKIO_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/sysinfo.h> header file. */
+/* #undef HAVE_SYS_SYSINFO_H */
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the `timegm' function. */
+#define HAVE_TIMEGM 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to 1 if you have the `vasprintf' function. */
+#define HAVE_VASPRINTF 1
+
+/* Define to 1 if you have the `vprintf' function. */
+#define HAVE_VPRINTF 1
+
+/* Define to 1 if you have the `vsnprintf' function. */
+#define HAVE_VSNPRINTF 1
+
+/* Define to 1 if the system has the type `wstring'. */
+#define HAVE_WSTRING 1
+
+/* Full GNU-style system type */
+#define HOST "powerpc-apple-darwin7.2.0"
+
+/* CPU type only */
+#define HOST_CPU "powerpc"
+
+/* System OS only */
+#define HOST_OS "darwin7.2.0"
+
+/* System vendor only */
+#define HOST_VENDOR "apple"
+
+/* Compiler name */
+#define NCBI_COMPILER_GCC 1
+
+/* Compiler version as three-digit integer */
+#define NCBI_COMPILER_VERSION 330
+
+/* This is the NCBI C++ Toolkit. */
+#define NCBI_CXX_TOOLKIT 1
+
+/* Define to 1 if `string::compare()' is non-standard. */
+#define NCBI_OBSOLETE_STR_COMPARE 1
+
+/* Operating system name */
+#define NCBI_OS "UNIX"
+
+/* Define to 1 on Mac OS X. */
+#define NCBI_OS_DARWIN 1
+
+/* Define to 1 on Unix. */
+#define NCBI_OS_UNIX 1
+
+/* Define to the architecture size. */
+#define NCBI_PLATFORM_BITS 32
+
+/* Define to 1 if prototypes can use exception specifications. */
+#define NCBI_USE_THROW_SPEC 1
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "cpp-core@ncbi.nlm.nih.gov"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "ncbi-tools++"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "ncbi-tools++ 0.0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "ncbi-tools--"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.0"
+
+/* The size of a `char', as computed by sizeof. */
+#define SIZEOF_CHAR 1
+
+/* The size of a `double', as computed by sizeof. */
+#define SIZEOF_DOUBLE 8
+
+/* The size of a `float', as computed by sizeof. */
+#define SIZEOF_FLOAT 4
+
+/* The size of a `int', as computed by sizeof. */
+#define SIZEOF_INT 4
+
+/* The size of a `long', as computed by sizeof. */
+#define SIZEOF_LONG 4
+
+/* The size of a `long double', as computed by sizeof. */
+#define SIZEOF_LONG_DOUBLE 8
+
+/* The size of a `long long', as computed by sizeof. */
+#define SIZEOF_LONG_LONG 8
+
+/* The size of a `short', as computed by sizeof. */
+#define SIZEOF_SHORT 2
+
+/* The size of a `size_t', as computed by sizeof. */
+#define SIZEOF_SIZE_T 4
+
+/* The size of a `void*', as computed by sizeof. */
+#define SIZEOF_VOIDP 4
+
+/* The size of a `__int64', as computed by sizeof. */
+#define SIZEOF___INT64 0
+
+/* Define to 1 if the stack grows down. */
+#define STACK_GROWS_DOWN 1
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Define to 1 if using a local copy of bzlib. */
+/* #undef USE_LOCAL_BZLIB */
+
+/* Define to 1 if using a local copy of PCRE. */
+/* #undef USE_LOCAL_PCRE */
+
+/* Define to 1 if your processor stores words with the most significant byte
+   first (like Motorola and SPARC, unlike Intel and VAX). */
+#define WORDS_BIGENDIAN 1
+
+ 
+#define HAVE_LIBJPEG 1
+#define HAVE_LIBTIFF 1
+#define HAVE_LIBPNG  1
+#define HAVE_LIBGIF  1
