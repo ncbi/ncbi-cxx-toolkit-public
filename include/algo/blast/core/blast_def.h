5f0b1b34ddeaee3dd982070c3652c3f0fe6f6103
madden <madden@ncbi.nlm.nih.gov>
2004-06-28 13:37:45 +0000

    Add typedef for BlastMaskInformation
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@16420 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/algo/blast/core/blast_def.h
+++ include/algo/blast/core/blast_def.h
@@ -88,6 +88,14 @@ typedef struct BlastMaskLoc {
    struct BlastMaskLoc* next; /**< Pointer to the next query mask */
 } BlastMaskLoc;
 
+
+/** Encapsulates masking/filtering information. */
+typedef struct BlastMaskInformation {
+   BlastMaskLoc* filter_slp; /**< masking locations. */
+   Boolean mask_at_hash; /**< if TRUE masking used only for building lookup table. */
+} BlastMaskInformation; 
+
+
 /** Structure to hold a sequence. */
 typedef struct BLAST_SequenceBlk {
    Uint1* sequence; /**< Sequence used for search (could be translation). */
