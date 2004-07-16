b354315affe5eaa563f30b6abb60e6b9b78b31b3
Andrei Gourianov <gouriano@ncbi.nlm.nih.gov>
2004-07-16 16:35:39 +0000

    MAGIC add batch file used in pre-build rule for projects with ASN dependencies
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@16680 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ compilers/msvc710_prj/asn_prebuild.bat
@@ -0,0 +1,19 @@
+@echo off
+
+set OUTDIR=%1%
+shift
+set CONFIGURATIONNAME=%1%
+shift
+set SOLUTIONPATH=%1
+
+:label_begin
+shift
+set PROJECT=%1%
+if "%PROJECT%"=="" goto label_done
+if exist %OUTDIR%\%PROJECT% goto label_begin
+devenv /build %CONFIGURATIONNAME% /project %PROJECT% %SOLUTIONPATH%
+goto label_begin
+
+:label_done
+
+
