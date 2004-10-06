35cc4076b6edd4083816a1c5a381eeba7119ea66
Ilya Dondoshansky <dondosha@ncbi.nlm.nih.gov>
2004-10-06 14:53:00 +0000

    Implementation of IBlastSeqInfoSrc with TSeqLocVector
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@18096 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ include/algo/blast/api/seqinfosrc_seqvec.hpp
@@ -0,0 +1,81 @@
+#ifndef ALGO_BLAST_API__SEQINFOSRC_SEQVEC__HPP
+#define ALGO_BLAST_API__SEQINFOSRC_SEQVEC__HPP
+
+/*  $Id$
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Author:  Ilya Dondoshansky
+ *
+ */
+
+/** @file seqinfosrc_seqvec.hpp
+ * Defines a concrete strategy for the IBlastSeqInfoSrc interface for
+ * sequence identifiers retrieval from a CSeqDB class.
+ */
+
+#include <ncbi_pch.hpp>
+#include <algo/blast/api/blast_seqinfosrc.hpp>
+#include <algo/blast/api/blast_types.hpp>
+
+/** @addtogroup AlgoBlast
+ *
+ * @{
+ */
+
+BEGIN_NCBI_SCOPE
+BEGIN_SCOPE(blast)
+
+/// Implementation of the IBlastSeqInfoSrc interface to encapsulate retrieval
+/// of sequence identifiers and lengths from a vector of Seq-locs. 
+class CSeqVecSeqInfoSrc : public IBlastSeqInfoSrc
+{
+public:
+    /// Constructor from a vector of sequence locations.
+    CSeqVecSeqInfoSrc(const TSeqLocVector seqv);
+    /// Destructor
+    virtual ~CSeqVecSeqInfoSrc();
+    /// Retrieve a sequence identifier given its index in the vector.
+    list< CRef<CSeq_id> > GetId(Uint4 index) const;
+    /// Retrieve sequence length given its index in the vector.
+    Uint4 GetLength(Uint4 index) const;
+private:
+    TSeqLocVector m_SeqVec;
+
+};
+
+END_SCOPE(blast)
+END_NCBI_SCOPE
+
+/*
+ * ===========================================================================
+ *
+ * $Log$
+ * Revision 1.1  2004/10/06 14:51:37  dondosha
+ * Implementation of IBlastSeqInfoSrc with TSeqLocVector
+ *
+ *
+ * ===========================================================================
+ */
+#endif  /* ALGO_BLAST_API__SEQINFOSRC_SEQVEC_HPP */
---
+++ src/algo/blast/api/seqinfosrc_seqvec.cpp
@@ -0,0 +1,106 @@
+static char const rcsid[] =
+    "$Id$";
+/* ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Author:  Ilya Dondoshansky
+ *
+ */
+
+/** @file seqinfosrc_seqvec.cpp
+ * Implementation of the concrete strategy for an IBlastSeqInfoSrc interface to
+ * retrieve sequence identifiers and lengths from a vector of Seq-locs.
+ */
+
+#include <ncbi_pch.hpp>
+#include <objmgr/util/sequence.hpp>
+#include <algo/blast/api/seqinfosrc_seqvec.hpp>
+#include <algo/blast/api/blast_exception.hpp>
+
+/** @addtogroup AlgoBlast
+ *
+ * @{
+ */
+
+BEGIN_NCBI_SCOPE
+USING_SCOPE(objects);
+BEGIN_SCOPE(blast)
+
+CSeqVecSeqInfoSrc::CSeqVecSeqInfoSrc(const TSeqLocVector seqv)
+    : m_SeqVec(seqv)
+{
+    if (seqv.size() == 0) {
+        NCBI_THROW(CBlastException, eBadParameter, 
+                   "Empty sequence vector for id and length retrieval");
+    }
+}
+
+CSeqVecSeqInfoSrc::~CSeqVecSeqInfoSrc()
+{
+}
+
+list< CRef<CSeq_id> > CSeqVecSeqInfoSrc::GetId(Uint4 index) const
+{
+    if (index >= m_SeqVec.size()) {
+        NCBI_THROW(CBlastException, eBadParameter, 
+                   "Index out of range for id retrieval");
+    }
+
+    list < CRef<CSeq_id> > seqid_list;
+    CRef<CSeq_id> seqid_ref;
+
+    seqid_ref.Reset(const_cast<CSeq_id*>(
+                        &sequence::GetId(*m_SeqVec[index].seqloc, 
+                                         m_SeqVec[index].scope)));
+
+    seqid_list.push_back(seqid_ref);
+    return seqid_list;
+}
+
+Uint4 CSeqVecSeqInfoSrc::GetLength(Uint4 index) const
+{
+    if (index >= m_SeqVec.size()) {
+        NCBI_THROW(CBlastException, eBadParameter, 
+                   "Index out of range for length retrieval");
+    }
+
+    return sequence::GetLength(*m_SeqVec[index].seqloc, 
+                               m_SeqVec[index].scope);
+}
+
+END_SCOPE(blast)
+END_NCBI_SCOPE
+
+/* @} */
+
+/*
+ * ===========================================================================
+ *
+ * $Log$
+ * Revision 1.1  2004/10/06 14:53:00  dondosha
+ * Implementation of IBlastSeqInfoSrc with TSeqLocVector
+ *
+ *
+ * ===========================================================================
+ */
