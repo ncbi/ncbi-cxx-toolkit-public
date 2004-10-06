e8d66a84ceb3b72ee897085390353a5596d8f10f
Ilya Dondoshansky <dondosha@ncbi.nlm.nih.gov>
2004-10-06 14:52:45 +0000

    Implementation of IBlastSeqInfoSrc with CSeqDB
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@18095 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ include/algo/blast/api/seqinfosrc_seqdb.hpp
@@ -0,0 +1,84 @@
+#ifndef ALGO_BLAST_API__SEQINFOSRC_SEQDB__HPP
+#define ALGO_BLAST_API__SEQINFOSRC_SEQDB__HPP
+
+/*  $Id$
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Author:  Ilya Dondoshansky
+ *
+ */
+
+/** @file seqinfosrc_seqdb.hpp
+ * Defines a concrete strategy for the IBlastSeqInfoSrc interface for
+ * sequence identifiers retrieval from a CSeqDB class.
+ */
+
+#include <ncbi_pch.hpp>
+#include <algo/blast/api/blast_seqinfosrc.hpp>
+#include <objtools/readers/seqdb/seqdb.hpp>
+
+/** @addtogroup AlgoBlast
+ *
+ * @{
+ */
+
+BEGIN_NCBI_SCOPE
+USING_SCOPE(objects);
+BEGIN_SCOPE(blast)
+
+/// Implementation of the IBlastSeqInfoSrc interface to encapsulate retrieval of
+/// sequence identifiers and lengths from a BLAST database. 
+class CSeqDbSeqInfoSrc : public IBlastSeqInfoSrc
+{
+public:
+    /// Constructor: includes initializing the CSeqDB object.
+    /// @param dbname Name of the BLAST database [in]
+    /// @param is_protein Is this a protein or nucleotide database? [in]
+    CSeqDbSeqInfoSrc(const string& dbname, bool is_protein);
+    virtual ~CSeqDbSeqInfoSrc();
+    /// Retrieve a sequence identifier given its ordinal number.
+    list< CRef<CSeq_id> > GetId(Uint4 oid) const;
+    /// Retrieve sequence length given its ordinal number.
+    Uint4 GetLength(Uint4 oid) const;
+
+private:
+    CRef<CSeqDB> m_iSeqDb;
+
+};
+
+END_SCOPE(blast)
+END_NCBI_SCOPE
+
+/*
+ * ===========================================================================
+ *
+ * $Log$
+ * Revision 1.1  2004/10/06 14:51:22  dondosha
+ * Implementation of IBlastSeqInfoSrc with CSeqDB
+ *
+ *
+ * ===========================================================================
+ */
+#endif  /* ALGO_BLAST_API__SEQINFOSRC_SEQDB_HPP */
---
+++ src/algo/blast/api/seqinfosrc_seqdb.cpp
@@ -0,0 +1,81 @@
+static char const rcsid[] =
+    "$Id$";
+/* ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Author:  Ilya Dondoshansky
+ *
+ */
+
+/** @file seqinfosrc_seqdb.cpp
+ * Implementation of the concrete strategy for an IBlastSeqInfoSrc interface to
+ * retrieve sequence identifiers and lengths from a BLAST database.
+ */
+
+#include <ncbi_pch.hpp>
+#include <algo/blast/api/seqinfosrc_seqdb.hpp>
+
+/** @addtogroup AlgoBlast
+ *
+ * @{
+ */
+
+BEGIN_NCBI_SCOPE
+USING_SCOPE(objects);
+BEGIN_SCOPE(blast)
+
+CSeqDbSeqInfoSrc::CSeqDbSeqInfoSrc(const string& dbname, bool is_protein)
+{
+    m_iSeqDb.Reset(new CSeqDB(dbname, (is_protein ? 'p' : 'n')));
+}
+
+CSeqDbSeqInfoSrc::~CSeqDbSeqInfoSrc()
+{
+}
+
+list< CRef<CSeq_id> > CSeqDbSeqInfoSrc::GetId(Uint4 index) const
+{
+    return m_iSeqDb->GetSeqIDs(index);
+}
+
+Uint4 CSeqDbSeqInfoSrc::GetLength(Uint4 index) const
+{
+    return m_iSeqDb->GetSeqLength(index);
+}
+
+END_SCOPE(blast)
+END_NCBI_SCOPE
+
+/* @} */
+
+/*
+ * ===========================================================================
+ *
+ * $Log$
+ * Revision 1.1  2004/10/06 14:52:45  dondosha
+ * Implementation of IBlastSeqInfoSrc with CSeqDB
+ *
+ *
+ * ===========================================================================
+ */
