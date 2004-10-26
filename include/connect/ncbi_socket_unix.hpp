10f8f2e156d3350f6b8c62dab4e5d79a7055c7bf
Anton Lavrentiev <lavr@ncbi.nlm.nih.gov>
2004-10-26 14:44:44 +0000

    Initial revision
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@18515 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ include/connect/ncbi_socket_unix.hpp
@@ -0,0 +1,106 @@
+#ifndef CONNECT___NCBI_SOCKET_UNIX__HPP
+#define CONNECT___NCBI_SOCKET_UNIX__HPP
+
+/*  $Id$
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Author:  Anton Lavrentiev
+ *
+ * File Description:
+ *   TCP/IP socket API extension for UNIX
+ *
+ */
+
+#include <connect/ncbi_socket.hpp>
+
+
+/** @addtogroup Sockets
+ *
+ * @{
+ */
+
+
+BEGIN_NCBI_SCOPE
+
+
+class NCBI_XCONNECT_EXPORT CUNIXSocket : public CSocket
+{
+public:
+    // Create unconnected socket
+    CUNIXSocket(void) { };
+
+    CUNIXSocket(const string&   filename,
+                const STimeout* timeout = kInfiniteTimeout,
+                ESwitch         log     = eDefault);
+
+    // May be called on a socket, which is not connected yet
+    EIO_Status Connect(const string&   filename,
+                       const STimeout* timeout = kDefaultTimeout,
+                       ESwitch         log     = eDefault);
+
+private:
+    // disable copy constructor and assignment
+    CUNIXSocket(const CUNIXSocket&);
+    CUNIXSocket& operator= (const CUNIXSocket&);
+};
+
+
+class NCBI_XCONNECT_EXPORT CUNIXListeningSocket : public CListeningSocket
+{
+public:
+    // Create unbound socket
+    CUNIXListeningSocket(void) { };
+
+    CUNIXListeningSocket(const string&  filename,
+                         unsigned short backlog = 5,
+                         ESwitch        log     = eDefault);
+
+    EIO_Status Listen(const string&  filename,
+                      unsigned short backlog = 5,
+                      ESwitch        log     = eDefault);
+
+private:
+    // disable copy constructor and assignment
+    CUNIXListeningSocket(const CUNIXListeningSocket&);
+    CUNIXListeningSocket& operator= (const CUNIXListeningSocket&);
+};
+
+
+/* @} */
+
+
+END_NCBI_SCOPE
+
+
+/*
+ * ---------------------------------------------------------------------------
+ * $Log$
+ * Revision 1.1  2004/10/26 14:44:44  lavr
+ * Initial revision
+ *
+ * ===========================================================================
+ */
+
+#endif /* CONNECT___NCBI_SOCKET_UNIX__H */
