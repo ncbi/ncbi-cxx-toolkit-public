4a2caa11d4c1c873175a82961e6e3dea8efe6914
papadopo <papadopo@ncbi.nlm.nih.gov>
2004-11-01 16:37:57 +0000

    Add doxygen tags, remove unused constants
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@18644 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/algo/blast/core/ncbi_math.h
+++ include/algo/blast/core/ncbi_math.h
@@ -32,7 +32,6 @@
 
 /** @file ncbi_math.h
  * Prototypes for portable math library (ported from C Toolkit)
- * @todo FIXME doxygen comments
  */
 
 #include <algo/blast/core/ncbi_std.h> 
@@ -44,60 +43,94 @@
 extern "C" {
 #endif
 
-/* log(x+1) for all x > -1 */
-extern double BLAST_Log1p (double);
+/** Natural logarithm with shifted input
+ *  @param x input operand (x > -1)
+ *  @return log(x+1)
+ */
+extern double BLAST_Log1p (double x);
 
-/* exp(x)-1 for all x */
-extern double BLAST_Expm1 (double);
+/** Exponentional with base e 
+ *  @param x input operand
+ *  @return exp(x) - 1
+ */
+extern double BLAST_Expm1 (double x);
 
-/* Factorial function */
+/** Factorial function
+ *  @param n input operand
+ *  @return (double)(1 * 2 * 3 * ... * n)
+ */
 extern double BLAST_Factorial(Int4 n);
 
-/* Logarithm of the factorial Fn */
+/** Logarithm of the factorial 
+ *  @param x input operand
+ *  @return log(1 * 2 * 3 * ... * x)
+ */
 extern double BLAST_LnFactorial (double x);
 
-/* log(gamma(n)), integral n */
-extern double BLAST_LnGammaInt (Int4);
+/** log(gamma(n)), integral n 
+ *  @param n input operand
+ *  @return log(1 * 2 * 3 * ... (n-1))
+ */
+extern double BLAST_LnGammaInt (Int4 n);
 
-/* Romberg numerical integrator */
-extern double BLAST_RombergIntegrate (double (*f) (double, void*), void* fargs, double p, double q, double eps, Int4 epsit, Int4 itmin);
+/** Romberg numerical integrator 
+ *  @param f Pointer to the function to integrate; the first argument
+ *               is the variable to integrate over, the second is a pointer
+ *               to a list of additional arguments that f may need
+ *  @param fargs Pointer to an array of extra arguments or parameters
+ *               needed to compute the function to be integrated. None
+ *               of the items in this list may vary over the region
+ *               of integration
+ *  @param p Left-hand endpoint of the integration interval
+ *  @param q Right-hand endpoint of the integration interval
+ *           (q is assumed >= p)
+ *  @param eps The relative error tolerance that indicates convergence
+ *  @param epsit The number of consecutive diagonal entries in the 
+ *               Romberg array whose relative difference must be less than
+ *               eps before convergence is assumed. This is presently 
+ *               limited to 1, 2, or 3
+ *  @param itmin The minimum number of diagnonal Romberg entries that
+ *               will be computed
+ *  @return The computed integral of f() between p and q
+ */
+extern double BLAST_RombergIntegrate (double (*f) (double, void*), 
+                                      void* fargs, double p, double q, 
+                                      double eps, Int4 epsit, Int4 itmin);
 
-/* Greatest common divisor */
-extern long BLAST_Gcd (long, long);
+/* Greatest common divisor 
+ * @param a First operand (any integer)
+ * @param b Second operand (any integer)
+ * @return The largest integer that evenly divides a and b
+ */
+extern long BLAST_Gcd (long a, long b);
 
-/* Nearest integer */
-extern long BLAST_Nint (double);
+/** Nearest integer 
+ *  @param x Input to round (rounded value must be representable
+ *           as a 32-bit signed integer)
+ *  @return floor(x + 0.5);
+ */
+extern long BLAST_Nint (double x);
 
-/* Integral power of x */
+/* Integral power of x 
+ * @param x floating-point base of the exponential
+ * @param n (integer) exponent
+ * @return x multiplied by itself n times
+ */
 extern double BLAST_Powi (double x, Int4 n);
 
-/* Error codes for the CTX_NCBIMATH context */
-#define ERR_NCBIMATH_INVAL	1 /* invalid parameter */
-#define ERR_NCBIMATH_DOMAIN	2 /* domain error */
-#define ERR_NCBIMATH_RANGE	3 /* range error */
-#define ERR_NCBIMATH_ITER	4 /* iteration limit exceeded */
-
-#define LOGDERIV_ORDER_MAX	4
+/** Number of derivatives of log(x) to carry in gamma-related 
+    computations */
+#define LOGDERIV_ORDER_MAX	4  
+/** Number of derivatives of polygamma(x) to carry in gamma-related 
+    computations for non-integral values of x */
 #define POLYGAMMA_ORDER_MAX	LOGDERIV_ORDER_MAX
 
+/** value of pi is only used in gamma-related computations */
 #define NCBIMATH_PI	3.1415926535897932384626433832795
-#define NCBIMATH_E	2.7182818284590452353602874713527
-/* Euler's constant */
-#define NCBIMATH_EULER 0.5772156649015328606065120900824
-/* Catalan's constant */
-#define NCBIMATH_CATALAN	0.9159655941772190150546035149324
 
-/* sqrt(2) */
-#define NCBIMATH_SQRT2	1.4142135623730950488016887242097
-/* sqrt(3) */
-#define NCBIMATH_SQRT3	1.7320508075688772935274463415059
-/* sqrt(PI) */
-#define NCBIMATH_SQRTPI 1.7724538509055160272981674833411
-/* Natural log(2) */
+/** Natural log(2) */
 #define NCBIMATH_LN2	0.69314718055994530941723212145818
-/* Natural log(10) */
-#define NCBIMATH_LN10	2.3025850929940456840179914546844
-/* Natural log(PI) */
+/** Natural log(PI) */
 #define NCBIMATH_LNPI	1.1447298858494001741434273513531
 
 #ifdef __cplusplus
@@ -108,6 +141,9 @@ extern double BLAST_Powi (double x, Int4 n);
  * ===========================================================================
  *
  * $Log$
+ * Revision 1.8  2004/11/01 16:37:57  papadopo
+ * Add doxygen tags, remove unused constants
+ *
  * Revision 1.7  2004/05/19 14:52:01  camacho
  * 1. Added doxygen tags to enable doxygen processing of algo/blast/core
  * 2. Standardized copyright, CVS $Id string, $Log and rcsid formatting and i
