bc8ee5130010ca1908717ccfaa13f87aa5727972
Aaron Ucko <ucko@ncbi.nlm.nih.gov>
2004-12-13 15:37:24 +0000

    Accept a requested version (which must start with a digit) as an optional first argument and DTRT.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@19279 78c7ea69-d796-4a43-9a09-de51944f1b03

--- compilers/GCC.sh
+++ compilers/GCC.sh
@@ -11,7 +11,33 @@
 CXX="g++"
 CC="gcc"
 
-$CXX --version > /dev/null 2>&1
+platform="`uname -s``uname -r`-`uname -p`"
+platform="`echo $platform | sed -e 's/SunOS5\./solaris/; s/\(sol.*\)-i386/\1-intel/'`"
+
+case "$1" in
+  [1-9]*)
+     # Look for the specified version in various reasonable places
+     # (tuned for NCBI's installations).
+     if /usr/local/gcc-$1/bin/$CXX -dumpversion >/dev/null 2>&1; then
+       CXX=/usr/local/gcc-$1/bin/$CXX
+       CC=/usr/local/gcc-$1/bin/$CC
+     elif /netopt/gcc/$1/$platform/bin/$CXX -dumpversion >/dev/null 2>&1; then
+       CXX=/netopt/gcc/$1/$platform/bin/$CXX
+       CC=/netopt/gcc/$1/$platform/bin/$CC
+     elif $CXX -V$1 -dumpversion >/dev/null 2>&1; then
+       CXX="$CXX -V$1"
+       CC="$CC -V$1"
+     elif test "`$CXX -dumpversion 2>/dev/null`" \!= "$1"; then
+       cat <<EOF
+ERROR:  cannot find GCC version $1; you may need to adjust PATH explicitly.
+EOF
+       exit 1
+     fi
+  ;;
+esac
+
+
+$CXX -dumpversion > /dev/null 2>&1
 if test "$?" -ne 0 ; then
    cat <<EOF
 ERROR:  cannot find GCC compiler ($CXX)
@@ -25,7 +51,7 @@ fi
 if test -n "$1" ; then
   case "$1" in
    -h )
-    echo "USAGE: `basename $0` [build_dir [--configure-flags] | -h]"
+    echo "USAGE: `basename $0` [version] [build_dir [--configure-flags] | -h]"
     exit 0
     ;;
    --* )  ;;
