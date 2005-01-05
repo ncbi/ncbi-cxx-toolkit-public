7c4b08a0ac35c28bbed798da624bd1f5ded28d35
Christiam Camacho <camacho@ncbi.nlm.nih.gov>
2005-01-05 17:01:51 +0000

    Update documentation
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@19664 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/algo/blast/core/blast_message.h
+++ include/algo/blast/core/blast_message.h
@@ -50,9 +50,9 @@ typedef enum {
 
 /** Structure to hold the a message from the BLAST code. */
 typedef struct Blast_Message {
-	BlastSeverity severity; /**< severity code (0, 1, 2, 3) */
-	Int4 code;		/**< major code for error. */
-	Int4 subcode;	/**< minor code for this error. */
+	BlastSeverity severity; /**< severity code */
+	Int4 code;		/**< major code for error (currently unused) */
+	Int4 subcode;	/**< minor code for this error (currently unused). */
 	char* message;	/**< User message to be saved. */
 } Blast_Message;
 
@@ -65,7 +65,7 @@ Blast_Message* Blast_MessageFree(Blast_Message* blast_msg);
 
 /** Writes a message to a structure.  The Blast_Message* is allocated.
  * @param blast_msg structure to be filled in [in] 
- * @param severity severity code (0, 1, 2, 3) [in] 
+ * @param severity severity code [in] 
  * @param code major code for this error [in]
  * @param subcode minor code for this error [in] 
  * @param message User message to be saved [in]
