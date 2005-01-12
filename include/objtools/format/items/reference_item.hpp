e639b8878fe320e8ec2e7c27ae8aedb0789f1444
shomrat <shomrat@ncbi.nlm.nih.gov>
2005-01-12 16:42:07 +0000

    Code refactoring
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@19778 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/objtools/format/items/reference_item.hpp
+++ include/objtools/format/items/reference_item.hpp
@@ -34,10 +34,8 @@
 *
 */
 #include <corelib/ncbistd.hpp>
-
-#include <objects/general/Date_std.hpp>
+#include <objects/biblio/Cit_book.hpp>
 #include <objects/seq/Pubdesc.hpp>
-#include <objects/biblio/Imprint.hpp>
 
 #include <objtools/format/items/item_base.hpp>
 
@@ -47,23 +45,19 @@ BEGIN_SCOPE(objects)
 
 
 class CSeq_loc;
-class CSeqdesc;
 class CSeq_feat;
 class CAuth_list;
 class CCit_art;
-class CCit_book;
 class CCit_gen;
 class CCit_jour;
 class CCit_let;
 class CCit_pat;
 class CCit_proc;
 class CCit_sub;
-class CImprint;
 class CMedline_entry;
 class CPub;
-class CPub_equiv;
 class CPub_set;
-class CTitle;
+class CDate;
 class CAffil;
 class CBioseqContext;
 class IFormatter;
@@ -79,50 +73,88 @@ public:
         eSubmission   // direct submission
     };
 
-    typedef ECategory                       TCategory;
-    typedef vector<CRef<CReferenceItem> >   TReferences;
-    typedef CImprint::TPrepub               TPrepub;
-    typedef CImprint::TPubstatus            TPubstatus;
+    enum EPubType
+    {
+        ePub_not_set,
+        ePub_sub,
+        ePub_gen,
+        ePub_jour,
+        ePub_book,
+        ePub_book_art,
+        ePub_thesis,
+        ePub_pat
+    };
 
+    typedef ECategory                       TCategory;
+    typedef EPubType                        TPubType;
+    typedef vector< CRef<CReferenceItem> >  TReferences;
+    typedef list<string>                    TStrList;
+    typedef int                             TSeqid;
+    typedef CPubdesc::TReftype              TReftype;
+
+    /// constructors
     CReferenceItem(const CSeqdesc&  desc, CBioseqContext& ctx);
-    CReferenceItem(const CSeq_feat& feat, CBioseqContext& ctx);
-        
+    CReferenceItem(const CSeq_feat& feat, CBioseqContext& ctx,
+        const CSeq_loc* loc = NULL);
+    
+    /// format
     void Format(IFormatter& formatter, IFlatTextOStream& text_os) const;
+    
+    TPubType          GetPubType   (void) const { return m_PubType;    }
+    TCategory         GetCategory  (void) const { return m_Category;   }
+    const CPubdesc&   GetPubdesc   (void) const { return *m_Pubdesc;   }
+    TReftype          GetReftype   (void) const;
+    const CSeq_loc&   GetLoc       (void) const { return *m_Loc;       }
+    int               GetSerial    (void) const { return m_Serial;     }
+
+    // Date
+    bool              IsSetDate    (void) const { return m_Date;       }
+    const CDate&      GetDate      (void) const { return *m_Date;       }
+    //Authors
+    bool              IsSetAuthors (void) const { return m_Authors;    }
+    const CAuth_list& GetAuthors   (void) const { return *m_Authors;   }
+
+    bool              IsSetBook    (void) const { return m_Book;       }
+    const CCit_book&  GetBook      (void) const { return *m_Book;      }
+
+    bool              IsSetPatent  (void) const { return m_Patent;     }
+    const CCit_pat&   GetPatent    (void) const { return *m_Patent;    }
+    TSeqid            GetPatSeqid  (void) const { return m_PatentId;   }
+
+    bool              IsSetGen     (void) const { return m_Gen;        }
+    const CCit_gen&   GetGen       (void) const { return *m_Gen;       }
+
+    bool              IsSetSub     (void) const { return m_Sub;        }
+    const CCit_sub&   GetSub       (void) const { return *m_Sub;       }
+
+    bool              IsSetJournal (void) const { return m_Journal;    }
+    const CCit_jour&  GetJournal   (void) const { return *m_Journal;   }
+
+    int               GetPMID      (void) const { return m_PMID;       }
+    int               GetMUID      (void) const { return m_MUID;       }
+
+    const string&     GetTitle     (void) const { return m_Title;      }
+    
+    const string&     GetUniqueStr (void) const;
+
+    bool              IsJustUids   (void) const { return m_JustUids;   }
+    bool              IsElectronic (void) const { return m_Elect;      }
+    const string&     GetConsortium(void) const { return m_Consortium; }
+    const string&     GetRemark    (void) const { return m_Remark;     }
+
+    void SetLoc(const CConstRef<CSeq_loc>& loc);
+
+    // test if matches a publication in the set
+    bool Matches(const CPub_set& ps) const;
 
     // sort, merge duplicates and cleans up remaining items
     static void Rearrange(TReferences& refs, CBioseqContext& ctx);
-
-    bool Matches(const CPub_set& ps) const;
-               
-    const CPubdesc&     GetPubdesc   (void) const { return *m_Pubdesc;        }
-    CPubdesc::TReftype  GetReftype   (void) const { return GetPubdesc().GetReftype(); }
-    TCategory           GetCategory  (void) const { return m_Category;        }
-    const CDate*        GetDate      (void) const { return m_Date;            }
-    TPrepub             GetPrepub    (void) const { return m_Prepub;          }
-    TPubstatus          GetPubstatus (void) const { return m_Pubstatus;       }
-    int                 GetSerial    (void) const { return m_Serial;          }
-    const CSeq_loc*     GetLoc       (void) const { return m_Loc;             }
-    const CAuth_list*   GetAuthors   (void) const { return m_Authors;         }
-    const string&       GetConsortium(void) const { return m_Consortium;      }
-    const string&       GetTitle     (void) const { return m_Title;           }
-    const string&       GetJournal   (void) const { return m_Journal;         }
-    const int           GetPMID      (void) const { return m_PMID;            }
-    const int           GetMUID      (void) const { return m_MUID;            }
-    const string&       GetRemark    (void) const { return m_Remark;          }
-    const string&       GetVolume    (void) const { return m_Volume;          }
-    const string&       GetPages     (void) const { return m_Pages;           }
-    const string&       GetIssue     (void) const { return m_Issue;           }
-    const CCit_book*    GetBook      (void) const { return m_Book;            }
-    bool                JustUids     (void) const { return m_JustUids;        }
-    const string&       GetUniqueStr (void) const { return m_UniqueStr;       }
-    bool                IsEPublish   (void) const { return m_IsElectronicPub; }
-    
-    static string GetAuthString(const CAuth_list* alp);
-    static void GetAuthNames(list<string>& authors, const CAuth_list* alp);
+    // get names from authors list
+    static void GetAuthNames(const CAuth_list& alp, TStrList& authors);
+    // format author list
+    static void FormatAuthors(const CAuth_list& alp, string& auth);
+    // format affiliation
     static void FormatAffil(const CAffil& affil, string& result, bool gen_sub = false);
-
-    void SetLoc(const CConstRef<CSeq_loc>& loc);
-    bool UseDate(void) const { return m_UseDate; }
 
 private:
     
@@ -133,51 +165,64 @@ private:
     void x_Init(const CMedline_entry& mle,  CBioseqContext& ctx);
     void x_Init(const CCit_art&       art,  CBioseqContext& ctx);
     void x_Init(const CCit_jour&      jour, CBioseqContext& ctx);
-    void x_Init(const CCit_book&      book, CBioseqContext& ctx, 
-        bool for_art = false);
+    void x_Init(const CCit_book&      book, CBioseqContext& ctx);
     void x_Init(const CCit_pat&       pat,  CBioseqContext& ctx);
     void x_Init(const CCit_let&       man,  CBioseqContext& ctx);
+    void x_Init(const CCit_proc&      proc, CBioseqContext& ctx);
+    void x_InitProc(const CCit_book&      book, CBioseqContext& ctx);
 
     void x_AddAuthors(const CAuth_list& auth);
-    void x_SetJournal(const CTitle& title, CBioseqContext& ctx);
-    void x_SetJournal(const CCit_gen& gen, CBioseqContext& ctx);
     void x_AddImprint(const CImprint& imp, CBioseqContext& ctx);
     
     // Genbank format specific
     void x_GatherRemark(CBioseqContext& ctx);
 
+    void x_CreateUniqueStr(void) const;
     void x_CleanData(void);
     bool x_Matches(const CPub& pub) const;
 
+    // data
+    TPubType              m_PubType;
+    TCategory             m_Category;
     CConstRef<CPubdesc>   m_Pubdesc;
-    CConstRef<CPub>       m_Pub;         // main entry
-    CConstRef<CSeq_loc>   m_Loc;         // null if from a descriptor
+    CConstRef<CAuth_list> m_Authors;
+    CConstRef<CCit_book>  m_Book;
+    CConstRef<CCit_pat>   m_Patent;
+    TSeqid                m_PatentId;
+    CConstRef<CCit_gen>   m_Gen;
+    CConstRef<CCit_sub>   m_Sub;
+    CConstRef<CCit_jour>  m_Journal;
+    CConstRef<CSeq_loc>   m_Loc;
+    CConstRef<CDate>      m_Date;
     int                   m_PMID;
     int                   m_MUID;
-    TCategory             m_Category;    // (un)published / submitted
     int                   m_Serial;
-    CConstRef<CAuth_list> m_Authors;
-    string                m_Consortium;
-    string                m_Title;
-    string                m_Journal;     // or contact info for submissions
-    string                m_Volume;      // normally numeric
-    string                m_Issue;
-    string                m_Pages;
-    CConstRef<CDate>      m_Date;
-    string                m_Remark;      // genbank specific
+    mutable string        m_UniqueStr;
     bool                  m_JustUids;
-    CConstRef<CCit_book>  m_Book;
-    TPrepub               m_Prepub;
-    TPubstatus            m_Pubstatus;
-    string                m_UniqueStr;
-    bool                  m_UseDate;
-    bool                  m_IsElectronicPub;
+    string                m_Title;
+    bool                  m_Elect;
+    string                m_Consortium;
+    string                m_Remark;      // genbank specific
 };
 
 
 ///////////////////////////////////////////////////////////////////////////
 //
 // INLINE METHODS
+
+inline
+const string& CReferenceItem::GetUniqueStr(void) const
+{
+    x_CreateUniqueStr();
+    return m_UniqueStr;
+}
+
+
+inline
+CReferenceItem::TReftype CReferenceItem::GetReftype(void) const
+{
+    return m_Pubdesc->GetReftype();
+}
 
 /*
 inline string CReferenceItem::x_GetURL(int id)
@@ -194,6 +239,9 @@ END_NCBI_SCOPE
 * ===========================================================================
 *
 * $Log$
+* Revision 1.14  2005/01/12 16:42:07  shomrat
+* Code refactoring
+*
 * Revision 1.13  2004/11/15 20:04:10  shomrat
 * Added Pubstatus
 *
