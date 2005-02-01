42cad117d4c071a6f9b16786f2fff0f5bf283860
papadopo <papadopo@ncbi.nlm.nih.gov>
2005-02-01 18:38:25 +0000

    make structure names and function arguments more meaningful
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@20039 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/algo/blast/core/greedy_align.h
+++ include/algo/blast/core/greedy_align.h
@@ -28,8 +28,7 @@
  */
 
 /** @file greedy_align.h
- * Copy of mbalign.h from ncbitools library 
- * @todo FIXME need better file description
+ * Prototypes and structures for greedy gapped alignment
  */
 
 #ifndef _GREEDY_H_
@@ -55,10 +54,13 @@ typedef struct MBEditOp {
     Uint4 num_ops : 30;    /**< number of operations of this type to perform */
 } MBEditOp;
 
-/** Macro for a formula estimating maximal gap length in the greedy algorithm */
+/** Macro for a formula estimating maximal gap length in the greedy algorithm
+    (ICEIL = 'Integer Ceiling'?) */
 #define ICEIL(x,y) ((((x)-1)/(y))+1)
 
-/** Fraction of the sequence length in calculation of maximal cost (= 2) */
+/** sequence_length / (this number) is a measure of how hard the 
+    alignment code will work to find the optimal alignment; in fact
+    this gives a worst case bound on the number of loop iterations */
 #define GREEDY_MAX_COST_FRACTION 2
 
 /** Edit script structure for saving traceback information for greedy gapped 
@@ -97,17 +99,19 @@ MBGapEditScriptAppend(MBGapEditScript *dest_script,
 
 /* ----- pool allocator ----- */
 
-/** Bookkeeping structure that maintains the smallest distances
-    at an intermediate point in the greedy alignment */
-typedef struct SThreeVal {
-    Int4 I;     /**< Best distance for a path ending in an insertion */
-    Int4 C;     /**< Best distance for a path ending in a match */
-    Int4 D;     /**< Best distance for a path ending in a deletion */
-} SThreeVal;
+/** Bookkeeping structure for greedy alignment. When aligning
+    two sequences, the members of this structure store the
+    largest offset into the second sequence that leads to a
+    high-scoring alignment for a given start point */
+typedef struct SGreedyOffset {
+    Int4 insert_off;    /**< Best offset for a path ending in an insertion */
+    Int4 match_off;     /**< Best offset for a path ending in a match */
+    Int4 delete_off;    /**< Best offset for a path ending in a deletion */
+} SGreedyOffset;
 
 /** Space structure for greedy alignment algorithm */
 typedef struct SMBSpace {
-    SThreeVal* space_array;     /**< array of bookkeeping structures */
+    SGreedyOffset* space_array; /**< array of bookkeeping structures */
     Int4 space_allocated;       /**< number of structures allocated */
     Int4 space_used;            /**< number of structures actually in use */
     struct SMBSpace *next;      /**< pointer to next structure in list */
@@ -125,11 +129,13 @@ void MBSpaceFree(SMBSpace* sp);
 
 /** All auxiliary memory needed for the greedy extension algorithm. */
 typedef struct SGreedyAlignMem {
-   Int4** flast_d;              /**< 2-D array of distances */
-   Int4* max_row_free;          /**< array of maximum distances */
-   SThreeVal** flast_d_affine;  /**< Like flast_d but for affine searches */
-   Int4* uplow_free;            /**< ? */
-   SMBSpace* space;             /**< local memory pool for SThreeVal structs */
+   Int4** last_seq2_off;              /**< 2-D array of distances */
+   Int4* max_score;                   /**< array of maximum scores */
+   SGreedyOffset** last_seq2_off_affine;  /**< Like last_seq2_off but for 
+                                               affine searches */
+   Int4* diag_bounds;                 /**< bounds on ranges of diagonals */
+   SMBSpace* space;                   /**< local memory pool for 
+                                           SGreedyOffset structs */
 } SGreedyAlignMem;
 
 /** Perform the greedy extension algorithm with non-affine gap penalties.
@@ -141,8 +147,8 @@ typedef struct SGreedyAlignMem {
  * @param xdrop_threshold X-dropoff value to use in extension [in]
  * @param match_cost Match score to use in extension [in]
  * @param mismatch_cost Mismatch score to use in extension [in]
- * @param extent1 Length of extension on sequence 1 [out]
- * @param extent2 Length of extension on sequence 2 [out]
+ * @param seq1_align_len Length of extension on sequence 1 [out]
+ * @param seq2_align_len Length of extension on sequence 2 [out]
  * @param aux_data Structure containing all preallocated memory [in]
  * @param script Edit script structure for saving traceback. 
  *          Traceback is not saved if NULL is passed. [in] [out]
@@ -156,7 +162,7 @@ BLAST_GreedyAlign (const Uint1* seq1, Int4 len1,
                    const Uint1* seq2, Int4 len2,
                    Boolean reverse, Int4 xdrop_threshold, 
                    Int4 match_cost, Int4 mismatch_cost,
-                   Int4* extent1, Int4* extent2, 
+                   Int4* seq1_align_len, Int4* seq2_align_len, 
                    SGreedyAlignMem* aux_data, 
                    MBGapEditScript *script, Uint1 rem);
 
@@ -169,10 +175,10 @@ BLAST_GreedyAlign (const Uint1* seq1, Int4 len1,
  * @param xdrop_threshold X-dropoff value to use in extension [in]
  * @param match_cost Match score to use in extension [in]
  * @param mismatch_cost Mismatch score to use in extension [in]
- * @param gap_open Gap opening penalty [in]
- * @param gap_extend Gap extension penalty [in]
- * @param extent1 Length of extension on sequence 1 [out]
- * @param extent2 Length of extension on sequence 2 [out]
+ * @param in_gap_open Gap opening penalty [in]
+ * @param in_gap_extend Gap extension penalty [in]
+ * @param seq1_align_len Length of extension on sequence 1 [out]
+ * @param seq2_align_len Length of extension on sequence 2 [out]
  * @param aux_data Structure containing all preallocated memory [in]
  * @param script Edit script structure for saving traceback. 
  *          Traceback is not saved if NULL is passed. [in] [out]
@@ -185,8 +191,8 @@ BLAST_AffineGreedyAlign (const Uint1* seq1, Int4 len1,
                          const Uint1* seq2, Int4 len2,
                          Boolean reverse, Int4 xdrop_threshold, 
                          Int4 match_cost, Int4 mismatch_cost,
-                         Int4 gap_open, Int4 gap_extend,
-                         Int4* extent1, Int4* extent2, 
+                         Int4 in_gap_open, Int4 in_gap_extend,
+                         Int4* seq1_align_len, Int4* seq2_align_len, 
                          SGreedyAlignMem* aux_data, 
                          MBGapEditScript *script, Uint1 rem);
 
