be312d77405d0ba8111a07ee2ce8f1fecd710fd7
ssikorsk <ssikorsk@ncbi.nlm.nih.gov>
2005-02-15 17:48:47 +0000

    Added odbc and msdblib drivers to the test script
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@20327 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/dbapi/test/dbapi_unit_test.sh
+++ src/dbapi/test/dbapi_unit_test.sh
@@ -5,7 +5,7 @@
 ulimit -n 1024 > /dev/null 2>&1
 
 # Declare drivers and servers
-driver_list="ctlib dblib ftds"
+driver_list="ctlib dblib ftds odbc msdblib" # mysql
 server_list="MS_DEV1 STRAUSS MOZART"
 server_mssql="MS_DEV1"
 
