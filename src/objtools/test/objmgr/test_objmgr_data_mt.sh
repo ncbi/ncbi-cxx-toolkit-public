38586b62b33c5ba5ebfe367861e6ad030ae045a8
Eugene Vasilchenko <vasilche@ncbi.nlm.nih.gov>
2005-03-15 19:18:36 +0000

    Abort on null pointer access in test.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@20941 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/test/objmgr/test_objmgr_data_mt.sh
+++ src/objtools/test/objmgr/test_objmgr_data_mt.sh
@@ -1,6 +1,9 @@
 #! /bin/sh
 #$Id$
 
+NCBI_ABORT_ON_NULL=1
+export NCBI_ABORT_ON_NULL
+
 for args in "" "-fromgi 30240900 -togi 30241000"; do
     echo "Testing: test_objmgr_data_mt $args"
     if $CHECK_EXEC time test_objmgr_data_mt $args; then
