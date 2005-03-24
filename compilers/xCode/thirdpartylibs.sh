23df12f11193d508145c0a94806ce656f1517214
lebedev <lebedev@ncbi.nlm.nih.gov>
2005-03-24 15:34:04 +0000

    Initial revision
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@21151 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ compilers/xCode/thirdpartylibs.sh
@@ -0,0 +1,99 @@
+#!/bin/sh
+
+if [ -z "$1" -o -z "$2" -o -z "$3" ]; then
+  echo "Usage:"
+  echo "   thirdpartylib <NCBI C++ Toolkit directory> <Temporary working directory> <Installation directory>"
+  echo ""
+  echo "   example:"
+  echo "     thirdpartylib /Users/vlad/c++ /Users/vlad/tmp /Users/vlad/sw"
+  exit -1
+else
+  NCBICPP=$1
+  TEMP=$2
+  DEST=$3
+fi
+
+if [ -f $TEMP ]; then
+  echo "Temporary directory does not exist"
+  exit -1
+fi
+
+
+LIBS="
+ giflib-4.1.3*http://cogent.dl.sourceforge.net/sourceforge/libungif
+ libpng-1.2.8-config*ftp://swrinde.nde.swri.edu/pub/png/src
+ sqlite-2.8.16*http://www.sqlite.org
+ db-4.3.27.NC*http://downloads.sleepycat.com
+ fltk-1.1.6-source*http://cogent.dl.sourceforge.net/sourceforge/fltk"
+
+
+cd $TEMP
+if [ "$4" = "download" ]; then
+	for lib in $LIBS; do
+		name=`echo $lib | awk -F* '{ print $1 }'` 
+		url=`echo $lib | awk -F* '{ print $2 }'` 
+
+		filename=$name".tar.gz"
+		fullurl=$url"/"$filename
+		echo Downloading: $filename from $url
+		`curl -o $TEMP/$filename $fullurl`
+		if [ "$?" != "0" ]; then  
+			echo Could not download $filename
+			exit -1 
+		fi
+		echo $filename downloaded successfully
+	done
+	echo All libraries downloaded!
+fi 
+
+
+cd $TEMP
+for lib in $LIBS; do
+	name=`echo $lib | awk -F* '{ print $1 }'` 
+	filename=$name".tar.gz"
+	echo Processing $filename
+	
+	conf="--prefix="$DEST
+	echo Unpacking $filename
+	tar -xzvf $filename
+	
+	# Apply NCBI patches to FLTK
+	if [ $name = "fltk-1.1.6-source" ]; then
+		name="fltk-1.1.6"
+		cd $name
+		echo Appling NCBI FLTK patches
+		for patch_file in `ls $NCBICPP/src/gui/patches/fltk/*1.1.6.patch`; do
+		   echo "Applying patch: $patch_file"
+		   patch -p1 <$patch_file
+		done
+		
+		conf="--prefix="$DEST" --enable-shared --enable-threads --disable-localpng --disable-localjpeg --disable-localzlib"
+		CPPFLAGS="-I$DEST/include"
+		LDFLAGS="-L$DEST/lib"
+		CFLAGS=$CPPFLAGS
+		CXXFLAGS="$CPPFLAGS $LDFLAGS"
+		export CPPFLAGS LDFLAGS CFLAGS CXXFLAGS
+		cd $TEMP
+	fi
+	
+	echo Configuring in $name
+	if [ $name = "db-4.3.27.NC" ]; then
+		cd $name/build_unix
+		../dist/configure $conf
+	else
+		cd $name
+		./configure $conf
+	fi
+	
+	echo Make and Install in $name
+	make
+	make install
+	if [ "$?" != "0" ]; then  
+		echo Error installing $filename. Check the log output.
+		exit -1 
+	fi
+	cd $TEMP
+done
+
+echo "All libraries were successfully installed"
+exit 0
