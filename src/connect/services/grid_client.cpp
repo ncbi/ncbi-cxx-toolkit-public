6dd72d57fd2828e820242093f9c1e6f3b76164d8
didenko <didenko@ncbi.nlm.nih.gov>
2005-03-25 16:25:41 +0000

    Initial version
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@21180 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ src/connect/services/grid_client.cpp
@@ -0,0 +1,137 @@
+/*  $Id$
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *   Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Authors:  Maxim Didenko
+ *
+ * File Description:
+ *
+ */
+
+#include <ncbi_pch.hpp>
+#include <connect/services/grid_client.hpp>
+
+BEGIN_NCBI_SCOPE
+
+//////////////////////////////////////////////////////////////////////////////
+//
+CGridClient::CGridClient(CNetScheduleClient& ns_client, 
+                         INetScheduleStorage& storage)
+: m_NSClient(ns_client), m_NSStorage(storage)
+{
+    m_JobSubmiter.reset(new CGridJobSubmiter(*this));
+    m_JobStatus.reset(new CGridJobStatus(*this));
+}
+ 
+CGridClient::~CGridClient()
+{
+}
+
+CGridJobSubmiter& CGridClient::GetJobSubmiter()
+{
+    return *m_JobSubmiter;
+}
+CGridJobStatus& CGridClient::GetJobStatus(const string& job_key)
+{
+    m_JobStatus->x_SetJobKey(job_key);
+    return *m_JobStatus;
+}
+
+void CGridClient::CancelJob(const string& job_key)
+{
+    m_NSClient.CancelJob(job_key);
+}
+
+//////////////////////////////////////////////////////////////////////////////
+//
+CGridJobSubmiter::CGridJobSubmiter(CGridClient& grid_client)
+: m_GridClient(grid_client)
+{
+}
+
+CGridJobSubmiter::~CGridJobSubmiter()
+{
+}
+void CGridJobSubmiter::SetJobInput(const string& input)
+{
+    m_Input = input;
+}
+CNcbiOstream& CGridJobSubmiter::GetOStream()
+{
+    return m_GridClient.GetStorage().CreateOStream(m_Input);
+}
+
+string CGridJobSubmiter::Submit()
+{
+    string job_key = m_GridClient.GetNSClient().SubmitJob(m_Input);
+    m_GridClient.GetStorage().Reset();
+    m_Input.clear();
+    return job_key;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+//
+
+CGridJobStatus::CGridJobStatus(CGridClient& grid_client)
+: m_GridClient(grid_client), m_RetCode(0), m_BlobSize(0)
+
+{
+}
+CGridJobStatus::~CGridJobStatus()
+{
+}
+
+CNetScheduleClient::EJobStatus CGridJobStatus::GetStatus()
+{
+    return m_GridClient.GetNSClient().
+        GetStatus(m_JobKey, &m_RetCode, &m_Output, &m_ErrMsg);
+}
+
+ CNcbiIstream& CGridJobStatus::GetIStream()
+ {
+     return m_GridClient.GetStorage().GetIStream(m_Output,&m_BlobSize);
+ }
+
+ void CGridJobStatus::x_SetJobKey(const string& job_key)
+ {
+     m_JobKey = job_key;
+     m_Output.clear();
+     m_ErrMsg.clear();
+     m_RetCode = 0;
+     m_BlobSize = 0;
+     m_GridClient.GetStorage().Reset();
+ }
+
+
+END_NCBI_SCOPE
+
+/*
+ * ===========================================================================
+ * $Log$
+ * Revision 1.1  2005/03/25 16:25:41  didenko
+ * Initial version
+ *
+ * ===========================================================================
+ */
+ 
---
+++ src/connect/services/grid_client_app.cpp
@@ -0,0 +1,75 @@
+/*  $Id$
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *   Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Authors:  Maxim Didenko
+ *
+ * File Description:
+ *
+ */
+
+#include <ncbi_pch.hpp>
+#include <connect/services/grid_client.hpp>
+#include <connect/services/grid_client_app.hpp>
+#include <connect/services/grid_default_factories.hpp>
+
+
+BEGIN_NCBI_SCOPE
+
+
+CGridClientApp::CGridClientApp(CNetScheduleClient* ns_client, 
+                               INetScheduleStorage* storage)
+: m_NSClient(ns_client), m_NSStorage(storage)
+{
+}
+
+CGridClientApp::~CGridClientApp()
+{
+}
+
+void CGridClientApp::Init(void)
+{
+    CNcbiApplication::Init();
+    if (!m_NSClient.get()) {
+        CNetScheduleClientFactory cf(GetConfig());
+        m_NSClient.reset(cf.CreateInstance());
+    }
+    if( !m_NSStorage.get()) {
+        CNetScheduleStorageFactory_NetCache cf(GetConfig());
+        m_NSStorage.reset(cf.CreateInstance());
+    }
+    m_GridClient.reset(new CGridClient(*m_NSClient, *m_NSStorage));
+}
+
+END_NCBI_SCOPE
+
+/*
+ * ===========================================================================
+ * $Log$
+ * Revision 1.1  2005/03/25 16:25:41  didenko
+ * Initial version
+ *
+ * ===========================================================================
+ */
+ 
