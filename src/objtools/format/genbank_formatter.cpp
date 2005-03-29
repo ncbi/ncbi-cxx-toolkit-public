0074941c0ccd4581c4d8227b780cdf70e733f988
shomrat <shomrat@ncbi.nlm.nih.gov>
2005-03-29 18:18:09 +0000

    Format barcode comment
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@21289 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/format/genbank_formatter.cpp
+++ src/objtools/format/genbank_formatter.cpp
@@ -75,6 +75,7 @@ CGenbankFormatter::CGenbankFormatter(void)
 {
     SetIndent(string(12, ' '));
     SetFeatIndent(string(21, ' '));
+    SetBarcodeIndent(string(35, ' '));
 }
 
 
@@ -482,16 +483,67 @@ void CGenbankFormatter::x_Remark
 // COMMENT
 
 
+void CGenbankFormatter::x_AddOneBarCodeElement
+(list<string>& l,
+ const string& label,
+ const string& value) const
+{
+    Wrap(l, label, value, eBarcode);
+}
+
+
+void CGenbankFormatter::x_FormatBarcodeComment
+(const CBarcodeComment& barcode,
+ IFlatTextOStream& text_os) const
+{
+    list<string> l;
+    static const string kBarcodeHeader =
+        "Barcode Consortium: Standard Data Elements";
+
+    if (barcode.IsFirst()) {
+        Wrap(l, "COMMENT", kBarcodeHeader);
+    } else {
+        Wrap(l, kEmptyStr, kBarcodeHeader, eSubp);
+    }
+
+    l.push_back("            ");
+    x_AddOneBarCodeElement(l, "Organism:", barcode.GetTaxname());
+    x_AddOneBarCodeElement(l, "Collected By:", barcode.GetSubsource(CSubSource::eSubtype_collected_by));
+    x_AddOneBarCodeElement(l, "Collection Date:", barcode.GetSubsource(CSubSource::eSubtype_collection_date));
+    x_AddOneBarCodeElement(l, "Country:", barcode.GetSubsource(CSubSource::eSubtype_country));
+    x_AddOneBarCodeElement(l, "Identified By:", barcode.GetSubsource(CSubSource::eSubtype_identified_by));
+    x_AddOneBarCodeElement(l, "Isolate:", barcode.GetOrgmod(COrgMod::eSubtype_isolate));
+    x_AddOneBarCodeElement(l, "Lat-Lon:", barcode.GetSubsource(CSubSource::eSubtype_lat_lon));
+    x_AddOneBarCodeElement(l, "Specimen Voucher:", barcode.GetOrgmod(COrgMod::eSubtype_specimen_voucher));
+    x_AddOneBarCodeElement(l, "Forward Primer:", barcode.GetSubsource(CSubSource::eSubtype_fwd_primer_seq));
+    x_AddOneBarCodeElement(l, "Reverse Primer:", barcode.GetSubsource(CSubSource::eSubtype_rev_primer_seq));
+    x_AddOneBarCodeElement(l, "Fwd Primer Name:", barcode.GetSubsource(CSubSource::eSubtype_fwd_primer_name));
+    x_AddOneBarCodeElement(l, "Rev Primer Name:", barcode.GetSubsource(CSubSource::eSubtype_rev_primer_name));
+    l.push_back("            ");
+    NON_CONST_ITERATE(list<string>, it, l) {
+        TrimSpaces(*it, 16);
+    }
+
+    text_os.AddParagraph(l);
+}
+
+
 void CGenbankFormatter::FormatComment
 (const CCommentItem& comment,
  IFlatTextOStream& text_os)
 {
     list<string> l;
-    
-    if ( !comment.IsFirst() ) {
-        Wrap(l, kEmptyStr, comment.GetComment(), eSubp);
+
+    // Barcode comments are a special case
+    const CBarcodeComment* barcode = dynamic_cast<const CBarcodeComment*>(&comment);
+    if (barcode != NULL) {
+        x_FormatBarcodeComment(*barcode, text_os);
     } else {
-        Wrap(l, "COMMENT", comment.GetComment());
+        if (!comment.IsFirst()) {
+            Wrap(l, kEmptyStr, comment.GetComment(), eSubp);
+        } else {
+            Wrap(l, "COMMENT", comment.GetComment());
+        }
     }
 
     text_os.AddParagraph(l);
@@ -780,6 +832,9 @@ END_NCBI_SCOPE
 * ===========================================================================
 *
 * $Log$
+* Revision 1.30  2005/03/29 18:18:09  shomrat
+* Format barcode comment
+*
 * Revision 1.29  2005/03/28 21:27:23  ucko
 * If we must use sprintf(), #include <stdio.h> for it.
 *
