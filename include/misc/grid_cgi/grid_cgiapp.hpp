c1a2996fef4ad2c21b14b7c003edae0e2c995d6e
didenko <didenko@ncbi.nlm.nih.gov>
2005-03-31 14:54:55 +0000

    + comments
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@21327 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/misc/grid_cgi/grid_cgiapp.hpp
+++ include/misc/grid_cgi/grid_cgiapp.hpp
@@ -29,6 +29,7 @@
  * Author:  Maxim Didenko
  *
  * File Description:
+ *    Base class for Grid CGI Application
  *
  */
 
@@ -41,47 +42,117 @@
 #include <connect/services/netschedule_client.hpp>
 #include <connect/services/netschedule_storage.hpp>
 
+/// @file grid_cgiapp.hpp
+/// NetSchedule Framework specs. 
+///
+
 
 BEGIN_NCBI_SCOPE
 
+/** @addtogroup NetScheduleClient
+ *
+ * @{
+ */
+
 
 /////////////////////////////////////////////////////////////////////////////
-//  CGridCgiApplication::
-//
-
+///  Grid Cgi Application
+///
 class CGridCgiApplication : public CCgiApplication
 {
 public:
 
+    /// This method is called on the CGI application initialization -- before
+    /// starting to process a HTTP request or even receiving one.
+    /// If you decide to override it, remember to call 
+    /// CGridCgiApplication::Init().
+    ///
     virtual void Init(void);
+
+    /// Do not override this method yourself! -- it includes all the GRIDCGI
+    /// specific machinery. If you override it, do call 
+    /// CGridCgiApplication::Run() from inside your overriding method.
+    ///
     virtual int  ProcessRequest(CCgiContext& ctx);
 
 protected:
 
+    /// Show a page with input data
+    ///
     virtual void ShowParamsPage(CHTMLPage& page) const  = 0;
+
+    /// Collect parameters from HTML form
+    /// If this method returns false that means that input parameters were
+    /// not specified (or were incorrect). In this case method ShowParamsPage
+    /// will be called.
+    ///
     virtual bool CollectParams(void) = 0;
 
+    /// This method is called when a job is ready to be send to a worker node.
+    /// Override this method to prepare an input data for the worker node.
+    /// 
     virtual void OnJobSubmit(CNcbiOstream& os,CHTMLPage& page) = 0;
 
+    /// This method is call when a worker node finished its job and 
+    /// its result is ready to be retrieved.
+    /// Override this method to get a result from a worker node 
+    /// and render a result HTML page
+    ///
     virtual void OnJobDone(CNcbiIstream& is, CHTMLPage& page) = 0;
+
+    /// This method is call when a worker node repored its failure.
+    /// Override this method to get a error message and render 
+    /// a error HTML page
+    ///
     virtual void OnJobFailed(const string& msg, CHTMLPage& page) {}
 
+    /// This method is call if a job was canceled during its execution
+    /// Override this message to show a job cancelation message.
+    ///
     virtual void OnJobCanceled(CHTMLPage& page) {}
+
+    /// When a job is running a HTML page periodically (using java script)
+    /// calls this CGI to check a job status. If the job is not ready yet
+    /// this method is called.
+    /// Override this method to render a information HTML page or to request
+    /// the job cancelation.
+    ///
     virtual void OnStatusCheck(CHTMLPage& page) {}
 
-    virtual string GetPageTitle() const = 0;
-    virtual string GetPageTemplate() const = 0;
-    virtual string GetRefreshJScript() const;
+    /// Return a name page name. It is used when an inctance of CHTMLPage
+    /// class is created.
+    ///
+    virtual string GetPageTitle(void) const = 0;
 
-    CGridClient& GetGridClient() { return *m_GridClient; }
+    /// Return a name of a file this HTML page template. It is used when
+    /// an inctance of CHTMLPage class is created.
+    ///
+    virtual string GetPageTemplate(void) const = 0;
+
+    /// Get a java script code which is used to reload an HTML page 
+    /// when a job is still runnig so the CGI can automatically check
+    /// the job status. To make reloading mechanism work the HTML page 
+    /// template file has to have <@JSCRIPT@> meta tag somewhere before 
+    /// the <body> tag. See grid_cgi_sample.html file in the sample
+    /// dirctory.
+    ///
+    virtual string GetRefreshJScript(void) const;
+
+    /// When a job is still runnig this method is called to check if
+    /// a job cancelation has been requested. If so the job will be 
+    /// canceled.
+    ///
+    virtual bool JobStopRequested(void) const { return false; }
+
+    /// Get a Grid Client.
+    ///
+    CGridClient& GetGridClient(void) { return *m_GridClient; }
 
 private:
 
     auto_ptr<CNetScheduleClient> m_NSClient;
     auto_ptr<INetScheduleStorage> m_NSStorage;
     auto_ptr<CGridClient> m_GridClient;
-
-    bool m_StopJob;
 
 };
 
@@ -93,6 +164,9 @@ END_NCBI_SCOPE
 /*
  * ===========================================================================
  * $Log$
+ * Revision 1.2  2005/03/31 14:54:55  didenko
+ * + comments
+ *
  * Revision 1.1  2005/03/30 21:16:39  didenko
  * Initial version
  *
