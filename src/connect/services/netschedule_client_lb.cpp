c5478b49c2c6e44e4d45308cc87e41458915ae4e
Anatoliy Kuznetsov <kuznets@ncbi.nlm.nih.gov>
2005-04-06 12:38:13 +0000

    LB class factory moved from ns_client.cpp
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@21432 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/connect/services/netschedule_client_lb.cpp
+++ src/connect/services/netschedule_client_lb.cpp
@@ -33,6 +33,7 @@
 #include <ncbi_pch.hpp>
 #include <corelib/ncbistd.hpp>
 #include <corelib/ncbitime.hpp>
+#include <corelib/plugin_manager_impl.hpp>
 #include <connect/ncbi_socket.hpp>
 #include <connect/ncbi_conn_exception.hpp>
 #include <connect/services/netschedule_client.hpp>
@@ -383,11 +384,136 @@ bool CNetScheduleClient_LB::x_GetJobWaitNotify(SServiceAddress& sa,
 
 
 
+
+///////////////////////////////////////////////////////////////////////////////
+
+const char* kNetScheduleDriverName = "netscheduleclient";
+
+/// @internal
+class CNetScheduleClientCF : public IClassFactory<CNetScheduleClient>
+{
+public:
+
+    typedef CNetScheduleClient                 TDriver;
+    typedef CNetScheduleClient                 IFace;
+    typedef IFace                              TInterface;
+    typedef IClassFactory<CNetScheduleClient>  TParent;
+    typedef TParent::SDriverInfo               TDriverInfo;
+    typedef TParent::TDriverList               TDriverList;
+
+    /// Construction
+    ///
+    /// @param driver_name
+    ///   Driver name string
+    /// @param patch_level
+    ///   Patch level implemented by the driver.
+    ///   By default corresponds to interface patch level.
+    CNetScheduleClientCF(const string& driver_name = kNetScheduleDriverName,
+                         int patch_level = -1)
+        : m_DriverVersionInfo
+        (ncbi::CInterfaceVersion<IFace>::eMajor,
+         ncbi::CInterfaceVersion<IFace>::eMinor,
+         patch_level >= 0 ?
+            patch_level : ncbi::CInterfaceVersion<IFace>::ePatchLevel),
+          m_DriverName(driver_name)
+    {
+        _ASSERT(!m_DriverName.empty());
+    }
+
+    /// Create instance of TDriver
+    virtual TInterface*
+    CreateInstance(const string& driver  = kEmptyStr,
+                   CVersionInfo version = NCBI_INTERFACE_VERSION(IFace),
+                   const TPluginManagerParamTree* params = 0) const
+    {
+        TDriver* drv = 0;
+        if (params && (driver.empty() || driver == m_DriverName)) {
+            if (version.Match(NCBI_INTERFACE_VERSION(IFace))
+                                != CVersionInfo::eNonCompatible) {
+
+            CConfig conf(params);
+            const string& client_name = 
+                conf.GetString(m_DriverName, 
+                               "client_name", CConfig::eErr_Throw, "noname");
+            const string& queue_name = 
+                conf.GetString(m_DriverName, 
+                               "queue_name", CConfig::eErr_Throw, "noname");
+            const string& service = 
+                conf.GetString(m_DriverName, 
+                               "service", CConfig::eErr_NoThrow, "");
+            if (!service.empty()) {
+                unsigned int rebalance_time = conf.GetInt(m_DriverName, 
+                                                "rebalance_time",
+                                                CConfig::eErr_NoThrow, 10);
+                unsigned int rebalance_requests = conf.GetInt(m_DriverName,
+                                                "rebalance_requests",
+                                                CConfig::eErr_NoThrow, 100);
+                drv = new CNetScheduleClient_LB(client_name, 
+                                                service, queue_name,
+                                                rebalance_time, 
+                                                rebalance_requests);
+
+                const string& services_list = conf.GetString(m_DriverName,
+                                                "sevices_lis",
+                                                 CConfig::eErr_NoThrow, "");
+                vector<string> services;
+                NStr::Tokenize(services_list, ",", services);
+                for(vector<string>::const_iterator it = services.begin();
+                                                   it != services.end(); ++it) {
+                    string host, sport;
+                    if (NStr::SplitInTwo(*it,":",host,sport)) {
+                        try {
+                            unsigned int port = NStr::StringToUInt(sport);
+                            static_cast<CNetScheduleClient_LB*>(drv)->
+                                                  AddServiceAddress(host,port);
+                        } catch(CStringException&) {}
+                    }
+                }
+            } else { // non lb client
+                const string& host = 
+                    conf.GetString(m_DriverName, 
+                                  "host", CConfig::eErr_Throw, "");
+                unsigned int port = conf.GetInt(m_DriverName,
+                                               "port",
+                                               CConfig::eErr_Throw, 9100);
+
+                drv = new CNetScheduleClient(host, port, 
+                                             client_name, queue_name);
+            }
+            }                               
+        }
+        return drv;
+    }
+
+    void GetDriverVersions(TDriverList& info_list) const
+    {
+        info_list.push_back(TDriverInfo(m_DriverName, m_DriverVersionInfo));
+    }
+protected:
+    CVersionInfo  m_DriverVersionInfo;
+    string        m_DriverName;
+
+};
+
+void NCBI_XCONNECT_EXPORT NCBI_EntryPoint_xnetschedule(
+     CPluginManager<CNetScheduleClient>::TDriverInfoList&   info_list,
+     CPluginManager<CNetScheduleClient>::EEntryPointRequest method)
+{
+       CHostEntryPointImpl<CNetScheduleClientCF>::
+       NCBI_EntryPointImpl(info_list, method);
+
+}
+
+
+
 END_NCBI_SCOPE
 
 /*
  * ===========================================================================
  * $Log$
+ * Revision 1.6  2005/04/06 12:38:13  kuznets
+ * LB class factory moved from ns_client.cpp
+ *
  * Revision 1.5  2005/04/01 15:16:52  kuznets
  * Added penalty to unavailable service
  *
