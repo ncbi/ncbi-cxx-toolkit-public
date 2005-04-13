a0dd36c1a3af891c019d961d9d773756b1f17b53
Andrei Gourianov <gouriano@ncbi.nlm.nih.gov>
2005-04-13 16:04:01 +0000

    MAGIC Handle paths with spaces
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@21597 78c7ea69-d796-4a43-9a09-de51944f1b03

--- compilers/msvc710_prj/asn_prebuild.bat
+++ compilers/msvc710_prj/asn_prebuild.bat
@@ -1,17 +1,17 @@
 @echo off
 
-set OUTDIR=%1%
+set OUTDIR=%~1%
 shift
-set CONFIGURATIONNAME=%1%
+set CONFIGURATIONNAME=%~1%
 shift
-set SOLUTIONPATH=%1
+set SOLUTIONPATH=%~1%
 
 :label_begin
 shift
-set PROJECT=%1%
+set PROJECT=%~1%
 if "%PROJECT%"=="" goto label_done
-if exist %OUTDIR%\%PROJECT% goto label_begin
-devenv /build %CONFIGURATIONNAME% /project %PROJECT% %SOLUTIONPATH%
+if exist "%OUTDIR%\%PROJECT%" goto label_begin
+devenv /build "%CONFIGURATIONNAME%" /project "%PROJECT%" "%SOLUTIONPATH%"
 goto label_begin
 
 :label_done
