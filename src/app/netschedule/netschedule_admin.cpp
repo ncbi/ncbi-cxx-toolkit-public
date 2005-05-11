edcdbc299ff1c01e7a3ff953f271d69a4ffab387
Anatoliy Kuznetsov <kuznets@ncbi.nlm.nih.gov>
2005-05-11 13:01:52 +0000

    Implemented variety of logging commands
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@22231 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/app/netschedule/netschedule_admin.cpp
+++ src/app/netschedule/netschedule_admin.cpp
@@ -45,6 +45,24 @@
 USING_NCBI_SCOPE;
 
 
+class CNetScheduleClient_LB_Admin : public CNetScheduleClient_LB
+{
+public:
+    CNetScheduleClient_LB_Admin(const string& client_name,
+                                const string& lb_service_name,
+                                const string& queue_name)
+    : CNetScheduleClient_LB(client_name, 
+                            lb_service_name, 
+                            queue_name)
+    {}
+
+    using CNetScheduleClient_LB::ObtainServerList;
+    using CNetScheduleClient_LB::GetServiceList;
+};
+
+
+
+
 /// NetSchedule admin application
 ///
 /// @internal
@@ -54,6 +72,7 @@ class CNetScheduleAdmin : public CNcbiApplication
 public:
     void Init(void);
     int Run(void);
+    void Run(CNetScheduleClient_Control& nc_client);
 };
 
 
@@ -71,49 +90,30 @@ void CNetScheduleAdmin::Init(void)
 
     arg_desc->AddFlag("v", "Server version");
 
+    arg_desc->AddOptionalKey("q",
+                             "queue",
+                             "Queue name",
+                             CArgDescriptions::eString);
+
+    arg_desc->AddOptionalKey("k",
+                             "key",
+                             "job key",
+                             CArgDescriptions::eString);
+
+
     arg_desc->AddOptionalKey("log",
                              "server_logging",
                              "Switch server side logging",
                              CArgDescriptions::eBoolean);
 
-    arg_desc->AddOptionalKey("drop",
-                             "drop_queue",
-                             "Drop ALL jobs in the queue (no questions asked!)",
-                             CArgDescriptions::eString);
+    arg_desc->AddFlag("drop", "Drop Queue");
 
-    arg_desc->AddOptionalKey("stat",
-                             "stat_queue",
-                             "Print queue statistics",
-                             CArgDescriptions::eString);
+    arg_desc->AddFlag("stat", "stat queue");
 
-    arg_desc->AddOptionalKey(
-        "dump",
-        "dump_queue",
-        "Print queue dump or job dump (queuename:job_key)",
-        CArgDescriptions::eString);
-
+    arg_desc->AddFlag("dump", "Queue dump");
 
     SetupArgDescriptions(arg_desc.release());
 }
-
-
-
-class CNetScheduleClient_LB_Admin : public CNetScheduleClient_LB
-{
-public:
-    CNetScheduleClient_LB_Admin(const string& client_name,
-                                const string& lb_service_name,
-                                const string& queue_name)
-    : CNetScheduleClient_LB(client_name, 
-                            lb_service_name, 
-                            queue_name)
-    {}
-
-    using CNetScheduleClient_LB::ObtainServerList;
-    using CNetScheduleClient_LB::TServiceList;
-    using CNetScheduleClient_LB::GetServiceList;
-};
-
 
 
 int CNetScheduleAdmin::Run(void)
@@ -135,89 +135,70 @@ int CNetScheduleAdmin::Run(void)
 
         const CNetScheduleClient_LB_Admin::TServiceList& lst = 
             cli_lb.GetServiceList();
-    }
 
+        CNetScheduleClient_LB_Admin::TServiceList::const_iterator it =
+            lst.begin();
 
+        ITERATE(CNetScheduleClient_LB_Admin::TServiceList, it, lst) {
+            
+            string conn_host = CSocketAPI::gethostbyaddr(it->host);
 
-/*
-    unsigned short port = args["port"].AsInteger();
-
-
-    CNetScheduleClient_Control nc_client(host, port);
-
-    if (args["retry"]) {
-        int retry = args["retry"].AsInteger();
-        if (retry < 0) {
-            ERR_POST(Error << "Invalid retry count: " << retry);
-        }
-        for (int i = 0; i < retry; ++i) {
-            try {
-                nc_client.CheckConnect(kEmptyStr);
-                break;
-            } 
-            catch (exception&) {
-                SleepMilliSec(5 * 1000);
+            string queue = "noname";
+            if (args["q"]) {  
+                queue = args["q"].AsString(); 
             }
+
+            NcbiCout << "Connecting to: " << conn_host 
+                     << ":" << it->port << " Queue=" << queue << NcbiEndl;
+
+            CNetScheduleClient_Control nc_control(conn_host, it->port, queue);
+
+            Run(nc_control);
+
+            NcbiCout << NcbiEndl;
         }
     }
+
+    return 0;
+}
+
+void CNetScheduleAdmin::Run(CNetScheduleClient_Control& nc_client)
+{
+    CArgs args = GetArgs();
 
     if (args["log"]) {  // logging control
-        
         bool on_off = args["log"].AsBoolean();
-
         nc_client.Logging(on_off);
-        NcbiCout << "Logging turned " 
-                 << (on_off ? "ON" : "OFF") << " on the server" << NcbiEndl;
+        NcbiCout << "Logging turned "
+                 << (on_off ? "ON" : "OFF") << NcbiEndl;
     }
+
     if (args["drop"]) {  
-        string queue = args["drop"].AsString(); 
-        CNetScheduleClient_Control drop_client(host, port, queue);
-        drop_client.DropQueue();
-        NcbiCout << "Queue droppped: " << queue << NcbiEndl;
+        nc_client.DropQueue();
     }
 
-    if (args["stat"]) {  
-        string queue = args["stat"].AsString(); 
-        CNetScheduleClient_Control cl(host, port, queue);
-        cl.PrintStatistics(NcbiCout);
-        NcbiCout << NcbiEndl;
+    if (args["stat"]) {
+        nc_client.PrintStatistics(NcbiCout);
     }
 
-    if (args["dump"]) {  
-        string param = args["dump"].AsString(); 
-        string queue, job_key;
+    if (args["dump"]) {
+
+        string job_key;
+        if (args["k"]) {
+            job_key = args["k"].AsString();
+        }
         
-        NStr::SplitInTwo(param, ":", queue, job_key);
-
-        CNetScheduleClient_Control cl(host, port, queue);
-        cl.DumpQueue(NcbiCout, job_key);
-        NcbiCout << NcbiEndl;
+        nc_client.DumpQueue(NcbiCout, job_key);
     }
 
-
-    if (args["monitor"]) {
-        string queue = args["monitor"].AsString(); 
-        CNetScheduleClient_Control cl(host, port, queue);
-        cl.Monitor(NcbiCout);
-    }
-
-    if (args["s"]) {  // shutdown
-        nc_client.ShutdownServer();
-        NcbiCout << "Shutdown request has been sent to server" << NcbiEndl;
-        return 0;
-    }
-
-    if (args["v"]) {  // version
+    if (args["v"]) {
         string version = nc_client.ServerVersion();
         if (version.empty()) {
             NcbiCout << "NetCache server communication error." << NcbiEndl;
-            return 1;
         }
         NcbiCout << version << NcbiEndl;
     }
-*/
 
-    return 0;
 }
 
 
@@ -230,6 +211,9 @@ int main(int argc, const char* argv[])
 /*
  * ===========================================================================
  * $Log$
+ * Revision 1.2  2005/05/11 13:01:52  kuznets
+ * Implemented variety of logging commands
+ *
  * Revision 1.1  2005/05/10 19:24:48  kuznets
  * Initial revision
  *
