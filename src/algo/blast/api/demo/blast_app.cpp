42dd3c58b60b0331ed6158f6e36696e3a52d32f1
madden <madden@ncbi.nlm.nih.gov>
2005-05-16 14:13:11 +0000

    remove reference to SetPrelimHitlistSize
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@22342 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/algo/blast/api/demo/blast_app.cpp
+++ src/algo/blast/api/demo/blast_app.cpp
@@ -406,14 +406,6 @@ CBlastApplication::ProcessCommandLineArgs(CBlastOptionsHandle* opts_handle,
 
     if (args["hitlist"].AsInteger()) {
         opt.SetHitlistSize(args["hitlist"].AsInteger());
-        /* Hitlist size for preliminary alignments is increased, unless 
-           no traceback is performed. */
-        if (args["ungapped"].AsBoolean() || args["greedy"].AsInteger() == 1) {
-            opt.SetPrelimHitlistSize(args["hitlist"].AsInteger());
-        } else {
-            opt.SetPrelimHitlistSize(MIN(2*args["hitlist"].AsInteger(),
-                                         args["hitlist"].AsInteger() + 50));
-        }
     }
 
     if (args["cull"].AsInteger()) {
