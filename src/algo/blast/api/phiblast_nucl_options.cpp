273485b7355c4117eb745ecce32e141c6824f655
Ilya Dondoshansky <dondosha@ncbi.nlm.nih.gov>
2005-05-26 14:35:04 +0000

    Implementation of PHI BLAST options handle classes
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@22562 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ src/algo/blast/api/phiblast_nucl_options.cpp
@@ -0,0 +1,71 @@
+/*  $Id$
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Authors:  Ilya Dondoshansky
+ *
+ */
+
+/// @file phiblast_nucl_options.cpp
+/// Implements the CPHIBlastNuclOptionsHandle class.
+
+#include <ncbi_pch.hpp>
+#include <algo/blast/api/phiblast_nucl_options.hpp>
+#include "blast_setup.hpp"
+
+/** @addtogroup AlgoBlast
+ *
+ * @{
+ */
+
+
+BEGIN_NCBI_SCOPE
+BEGIN_SCOPE(blast)
+
+CPHIBlastNuclOptionsHandle::CPHIBlastNuclOptionsHandle(EAPILocality locality)
+    : CBlastNucleotideOptionsHandle(locality)
+{
+    if (m_Opts->GetLocality() == CBlastOptions::eRemote) {
+        return;
+    }
+    SetDefaults();
+    m_Opts->SetProgram(ePHIBlastn);
+}
+
+END_SCOPE(blast)
+END_NCBI_SCOPE
+
+
+/* @} */
+
+
+/*
+ * ===========================================================================
+ * $Log$
+ * Revision 1.1  2005/05/26 14:35:04  dondosha
+ * Implementation of PHI BLAST options handle classes
+ *
+ *
+ * ===========================================================================
+ */
