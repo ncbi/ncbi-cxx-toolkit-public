1efac0ee4ce8ea3eed310e40dff35e2be0a2af4b
Aleksandr Morgulis <morgulis@ncbi.nlm.nih.gov>
2005-06-02 14:37:08 +0000

    Untemplatized the library as SUN CC compiler's support for templates is pretty weak.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@22649 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/algo/dustmask/symdust.hpp
+++ include/algo/dustmask/symdust.hpp
@@ -50,24 +50,52 @@
 
 BEGIN_NCBI_SCOPE
 
-struct CSymDustMasker_base
-{ 
-    struct CTrivialConverter { Uint1 operator()( Uint1 r ) const{ return r; } }; 
-};
-
-template< typename seq_t, typename convert_t >
 class CSymDustMasker
 {
+    private:
+
+        struct CIupac2Ncbi2na_converter
+        {
+            Uint1 operator()( Uint1 r ) const
+            {
+                switch( r )
+                {
+                    case 67: return 1;
+                    case 71: return 2;
+                    case 84: return 3;
+                    default: return 0;
+                }
+            }
+        };
+
+        typedef objects::CSeqVector seq_t;
+        typedef CIupac2Ncbi2na_converter convert_t;
+
+    public:
+
+        typedef seq_t sequence_type;
+        typedef sequence_type::size_type size_type;
+        typedef std::pair< size_type, size_type > TMaskedInterval;
+        typedef std::vector< TMaskedInterval > TMaskList;
+
+    private:
+
+        struct lcr
+        {
+            TMaskedInterval bounds_;
+            Uint4 score_;
+            size_type len_;
+
+            lcr( size_type start, size_type stop, Uint4 score, size_type len )
+                : bounds_( start, stop ), score_( score ), len_( len )
+            {}
+        };
+
     public:
 
         static const Uint4 DEFAULT_LEVEL  = 20;
         static const Uint4 DEFAULT_WINDOW = 64;
         static const Uint4 DEFAULT_LINKER = 1;
-
-        typedef seq_t sequence_type;
-        typedef typename sequence_type::size_type size_type;
-        typedef std::pair< size_type, size_type > TMaskedInterval;
-        typedef std::vector< TMaskedInterval > TMaskList;
 
         // This is public to work aroung the bug in SUN C++ compiler.
         typedef std::list< lcr > lcr_list_type;
@@ -87,29 +115,8 @@ class CSymDustMasker
 
     private:
 
-        struct lcr
-        {
-            TMaskedInterval bounds_;
-            Uint4 score_;
-            size_type len_;
-
-            lcr( size_type start, size_type stop, Uint4 score, size_type len )
-                : bounds_( start, stop ), score_( score ), len_( len )
-            {}
-        };
-
-        struct MaskInt2SeqLoc
-        {
-            MaskInt2SeqLoc( objects::CSeq_id & seq_id ) : seq_id_( seq_id ) {}
-
-            objects::CSeq_loc * operator()( const TMaskedInterval & i ) const
-            { return new objects::CSeq_loc( seq_id_, i->first, i->second ); }
-
-            objects::CSeq_id & seq_id_;
-        };
-
         typedef Uint1 triplet_type;
-        typedef typename sequence_type::const_iterator seq_citer_type;
+        typedef sequence_type::const_iterator seq_citer_type;
         typedef std::vector< Uint4 > thres_table_type;
 
         static const triplet_type TRIPLET_MASK = 0x3F;
@@ -127,7 +134,7 @@ class CSymDustMasker
                 size_type stop() const { return stop_; }
                 size_type size() const { return triplet_list_.size(); }
 
-                bool add( typename sequence_type::value_type n );
+                bool add( sequence_type::value_type n );
                           
             private:
                 
@@ -153,189 +160,6 @@ class CSymDustMasker
 
         convert_t converter_;
 };
-
-//------------------------------------------------------------------------------
-template< typename seq_t, typename convert_t >
-CSymDustMasker< seq_t, convert_t >::triplets::triplets( 
-    const sequence_type & seq, size_type window, 
-    lcr_list_type & lcr_list, thres_table_type & thresholds )
-    : start_( 0 ), stop_( 0 ), max_size_( window - 2 ), 
-      lcr_list_( lcr_list ), thresholds_( thresholds )
-{
-    triplet_type t = ((seq[0]&3)<<4) + ((seq[1]&3)<<2) + (seq[2]&3);
-    triplet_list_.push_back( t );
-}
-
-//------------------------------------------------------------------------------
-template< typename seq_t, typename convert_t >
-inline bool CSymDustMasker< seq_t, convert_t >::triplets::add( 
-    typename sequence_type::value_type n )
-{
-    typedef std::vector< Uint1 > counts_type;
-    typedef typename lcr_list_type::iterator lcr_iter_type;
-    static counts_type counts( 64 );
-
-    bool result = false;
-
-    triplet_list_.push_front( 
-        ((triplet_list_.front()<<2)&TRIPLET_MASK) + (n&3) );
-    ++stop_;
-
-    if( triplet_list_.size() > max_size_ )
-    {
-        triplet_list_.pop_back();
-        ++start_;
-        result = true;
-    }
-
-    std::fill_n( counts.begin(), 64, 0 );
-    Uint4 score = 0, count = 0;
-    lcr_iter_type lcr_iter = lcr_list_.begin();
-    Uint4 max_lcr_score = 0;
-    size_type max_len = 0;
-    size_type pos = stop_;
-
-    for( impl_citer_type it = triplet_list_.begin(), 
-                         iend = triplet_list_.end();
-         it != iend; ++it, ++count, --pos )
-    {
-        Uint1 cnt = counts[*it];
-
-        if( cnt > 0 )
-        {
-            score += cnt;
-
-            if( score*10 > thresholds_[count] )
-            {
-                while(    lcr_iter != lcr_list_.end() 
-                       && pos <= lcr_iter->bounds_.first )
-                {
-                    if(    max_lcr_score == 0 
-                        || max_len*lcr_iter->score_ 
-                           > max_lcr_score*lcr_iter->len_ )
-                    {
-                        max_lcr_score = lcr_iter->score_;
-                        max_len = lcr_iter->len_;
-                    }
-
-                    ++lcr_iter;
-                }
-
-                if( max_lcr_score == 0 || score*max_len >= max_lcr_score*count )
-                {
-                    max_lcr_score = score;
-                    max_len = count;
-                    lcr_iter = lcr_list_.insert( 
-                        lcr_iter, lcr( pos, stop_ + 2, max_lcr_score, count ) );
-                }
-            }
-        }
-
-        ++counts[*it];
-    }
-
-    return result;
-}
-    
-//------------------------------------------------------------------------------
-template< typename seq_t, typename convert_t >
-inline CSymDustMasker< seq_t, convert_t >::CSymDustMasker( 
-    Uint4 level, size_type window, size_type linker )
-    : level_( (level >= 2 && level <= 64) ? level : DEFAULT_LEVEL ), 
-      window_( (window >= 8 && window <= 64) ? window : DEFAULT_WINDOW ), 
-      linker_( (linker >= 1 && linker <= 32) ? linker : DEFAULT_LINKER )
-{
-    thresholds_.reserve( window_ - 2 );
-    thresholds_.push_back( 1 );
-
-    for( size_type i = 1; i < window_ - 2; ++i )
-        thresholds_.push_back( i*level_ );
-}
-
-//------------------------------------------------------------------------------
-template< typename seq_t, typename convert_t >
-std::auto_ptr< typename CSymDustMasker< seq_t, convert_t >::TMaskList >
-CSymDustMasker< seq_t, convert_t >::operator()( const sequence_type & seq )
-{
-    std::auto_ptr< TMaskList > res( new TMaskList );
-
-    if( seq.size() > 3 )
-    {
-        lcr_list_.clear();
-        triplets tris( seq, window_, lcr_list_, thresholds_ );
-        seq_citer_type it = seq.begin() + tris.stop() + 3;
-        const seq_citer_type seq_end = seq.end();
-
-        while( it != seq_end )
-        {
-            while( !lcr_list_.empty() )
-            {
-                TMaskedInterval b = lcr_list_.back().bounds_;
-
-                if( b.first >= tris.start() )
-                    break;
-
-                if( res->empty() )
-                    res->push_back( b );
-                else
-                {
-                    TMaskedInterval last = res->back();
-
-                    if( last.second < b.first )
-                        res->push_back( b );
-                    else if( last.second < b.second )
-                        res->back().second = b.second;
-                }
-
-                lcr_list_.pop_back();
-            }
-
-            tris.add( converter_( *it ) );
-            ++it;
-        }
-
-        while( !lcr_list_.empty() )
-        {
-                TMaskedInterval b = lcr_list_.back().bounds_;
-
-                if( res->empty() )
-                    res->push_back( b );
-                else
-                {
-                    TMaskedInterval last = res->back();
-
-                    if( last.second < b.first )
-                        res->push_back( b );
-                    else if( last.second < b.second )
-                        res->back().second = b.second;
-                }
-
-                lcr_list_.pop_back();
-        }
-    }
-
-    return res;
-}
-
-//------------------------------------------------------------------------------
-template< typename seq_t, typename convert_t >
-void CSymDustMasker< seq_t, convert_t >::GetMaskedLocs( 
-    objects::CSeq_id & seq_id,
-    const sequence_type & seq, 
-    std::vector< CConstRef< objects::CSeq_loc > > & locs )
-{
-    typedef std::vector< CConstRef< objects::CSeq_loc > > locs_type;
-    locs.clear();
-    std::auto_ptr< TMaskList > res = (*this)( seq );
-    std::transform( res.begin(), res.end(), 
-                    std::back_inserter< locs_type >,
-                    MaskInt2SeqLoc( seq_id ) );
-}
-
-//------------------------------------------------------------------------------
-typedef CSymDustMasker< 
-    objects::CSeqVector, CSymDustMasker_base::CTrivialConverter > 
-    TSymDustMasker;
 
 END_NCBI_SCOPE
 
--- src/algo/dustmask/symdust.cpp
+++ src/algo/dustmask/symdust.cpp
@@ -35,5 +35,180 @@
 #include <algo/dustmask/symdust.hpp>
 
 BEGIN_NCBI_SCOPE
+
+//------------------------------------------------------------------------------
+CSymDustMasker::triplets::triplets( 
+    const sequence_type & seq, size_type window, 
+    lcr_list_type & lcr_list, thres_table_type & thresholds )
+    : start_( 0 ), stop_( 0 ), max_size_( window - 2 ), 
+      lcr_list_( lcr_list ), thresholds_( thresholds )
+{
+    triplet_type t = ((seq[0]&3)<<4) + ((seq[1]&3)<<2) + (seq[2]&3);
+    triplet_list_.push_back( t );
+}
+
+//------------------------------------------------------------------------------
+inline bool CSymDustMasker::triplets::add( sequence_type::value_type n )
+{
+    typedef std::vector< Uint1 > counts_type;
+    typedef lcr_list_type::iterator lcr_iter_type;
+    static counts_type counts( 64 );
+
+    bool result = false;
+
+    triplet_list_.push_front( 
+        ((triplet_list_.front()<<2)&TRIPLET_MASK) + (n&3) );
+    ++stop_;
+
+    if( triplet_list_.size() > max_size_ )
+    {
+        triplet_list_.pop_back();
+        ++start_;
+        result = true;
+    }
+
+    std::fill_n( counts.begin(), 64, 0 );
+    Uint4 score = 0, count = 0;
+    lcr_iter_type lcr_iter = lcr_list_.begin();
+    Uint4 max_lcr_score = 0;
+    size_type max_len = 0;
+    size_type pos = stop_;
+
+    for( impl_citer_type it = triplet_list_.begin(), 
+                         iend = triplet_list_.end();
+         it != iend; ++it, ++count, --pos )
+    {
+        Uint1 cnt = counts[*it];
+
+        if( cnt > 0 )
+        {
+            score += cnt;
+
+            if( score*10 > thresholds_[count] )
+            {
+                while(    lcr_iter != lcr_list_.end() 
+                       && pos <= lcr_iter->bounds_.first )
+                {
+                    if(    max_lcr_score == 0 
+                        || max_len*lcr_iter->score_ 
+                           > max_lcr_score*lcr_iter->len_ )
+                    {
+                        max_lcr_score = lcr_iter->score_;
+                        max_len = lcr_iter->len_;
+                    }
+
+                    ++lcr_iter;
+                }
+
+                if( max_lcr_score == 0 || score*max_len >= max_lcr_score*count )
+                {
+                    max_lcr_score = score;
+                    max_len = count;
+                    lcr_iter = lcr_list_.insert( 
+                        lcr_iter, lcr( pos, stop_ + 2, max_lcr_score, count ) );
+                }
+            }
+        }
+
+        ++counts[*it];
+    }
+
+    return result;
+}
+    
+//------------------------------------------------------------------------------
+CSymDustMasker::CSymDustMasker( 
+    Uint4 level, size_type window, size_type linker )
+    : level_( (level >= 2 && level <= 64) ? level : DEFAULT_LEVEL ), 
+      window_( (window >= 8 && window <= 64) ? window : DEFAULT_WINDOW ), 
+      linker_( (linker >= 1 && linker <= 32) ? linker : DEFAULT_LINKER )
+{
+    thresholds_.reserve( window_ - 2 );
+    thresholds_.push_back( 1 );
+
+    for( size_type i = 1; i < window_ - 2; ++i )
+        thresholds_.push_back( i*level_ );
+}
+
+//------------------------------------------------------------------------------
+std::auto_ptr< CSymDustMasker::TMaskList > 
+CSymDustMasker::operator()( const sequence_type & seq )
+{
+    std::auto_ptr< TMaskList > res( new TMaskList );
+
+    if( seq.size() > 3 )
+    {
+        lcr_list_.clear();
+        triplets tris( seq, window_, lcr_list_, thresholds_ );
+        seq_citer_type it = seq.begin() + tris.stop() + 3;
+        const seq_citer_type seq_end = seq.end();
+
+        while( it != seq_end )
+        {
+            while( !lcr_list_.empty() )
+            {
+                TMaskedInterval b = lcr_list_.back().bounds_;
+
+                if( b.first >= tris.start() )
+                    break;
+
+                if( res->empty() )
+                    res->push_back( b );
+                else
+                {
+                    TMaskedInterval last = res->back();
+
+                    if( last.second < b.first )
+                        res->push_back( b );
+                    else if( last.second < b.second )
+                        res->back().second = b.second;
+                }
+
+                lcr_list_.pop_back();
+            }
+
+            tris.add( converter_( *it ) );
+            ++it;
+        }
+
+        while( !lcr_list_.empty() )
+        {
+                TMaskedInterval b = lcr_list_.back().bounds_;
+
+                if( res->empty() )
+                    res->push_back( b );
+                else
+                {
+                    TMaskedInterval last = res->back();
+
+                    if( last.second < b.first )
+                        res->push_back( b );
+                    else if( last.second < b.second )
+                        res->back().second = b.second;
+                }
+
+                lcr_list_.pop_back();
+        }
+    }
+
+    return res;
+}
+
+//------------------------------------------------------------------------------
+void CSymDustMasker::GetMaskedLocs( 
+    objects::CSeq_id & seq_id,
+    const sequence_type & seq, 
+    std::vector< CConstRef< objects::CSeq_loc > > & locs )
+{
+    typedef std::vector< CConstRef< objects::CSeq_loc > > locs_type;
+    std::auto_ptr< TMaskList > res = (*this)( seq );
+    locs.clear();
+    locs.reserve( res->size() );
+
+    for( TMaskList::const_iterator it = res->begin(); it != res->end(); ++it )
+        locs.push_back( CConstRef< objects::CSeq_loc >( 
+            new objects::CSeq_loc( seq_id, it->first, it->second ) ) );
+}
+
 END_NCBI_SCOPE
 
