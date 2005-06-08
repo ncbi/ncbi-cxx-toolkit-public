1ea9204e582f6be62ebcb04617ae06d426002118
Christiam Camacho <camacho@ncbi.nlm.nih.gov>
2005-06-08 19:30:51 +0000

    Add safe casting from result of & operator to boolean value
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@22756 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/algo/blast/core/blast_program.c
+++ src/algo/blast/core/blast_program.c
@@ -37,39 +37,41 @@ static char const rcsid[] =
     
 #include <algo/blast/core/blast_program.h>
 
+#define SAFE_CAST_INT_TO_BOOLEAN(p) (((p) != 0) ? TRUE : FALSE)
+
 /* Classify query sequence */
 Boolean Blast_QueryIsProtein(EBlastProgramType p)
-{ return p & PROTEIN_QUERY_MASK; }
+{ return SAFE_CAST_INT_TO_BOOLEAN(p & PROTEIN_QUERY_MASK); }
 
 Boolean Blast_QueryIsNucleotide(EBlastProgramType p)
-{ return p & NUCLEOTIDE_QUERY_MASK; }
+{ return SAFE_CAST_INT_TO_BOOLEAN(p & NUCLEOTIDE_QUERY_MASK); }
 
 Boolean Blast_QueryIsPssm(EBlastProgramType p)
-{ return p & PSSM_QUERY_MASK; }
+{ return SAFE_CAST_INT_TO_BOOLEAN(p & PSSM_QUERY_MASK); }
 
 /* Classify subject sequence */
 Boolean Blast_SubjectIsProtein(EBlastProgramType p)
-{ return p & PROTEIN_SUBJECT_MASK; }
+{ return SAFE_CAST_INT_TO_BOOLEAN(p & PROTEIN_SUBJECT_MASK); }
 
 Boolean Blast_SubjectIsNucleotide(EBlastProgramType p)
-{ return p & NUCLEOTIDE_SUBJECT_MASK; }
+{ return SAFE_CAST_INT_TO_BOOLEAN(p & NUCLEOTIDE_SUBJECT_MASK); }
 
 Boolean Blast_SubjectIsPssm(EBlastProgramType p)
-{ return p & PSSM_SUBJECT_MASK; }
+{ return SAFE_CAST_INT_TO_BOOLEAN(p & PSSM_SUBJECT_MASK); }
 
 /* Handle translated searches */
 Boolean Blast_QueryIsTranslated(EBlastProgramType p)
-{ return p & TRANSLATED_QUERY_MASK; }
+{ return SAFE_CAST_INT_TO_BOOLEAN(p & TRANSLATED_QUERY_MASK); }
 
 Boolean Blast_SubjectIsTranslated(EBlastProgramType p)
-{ return p & TRANSLATED_SUBJECT_MASK; }
+{ return SAFE_CAST_INT_TO_BOOLEAN(p & TRANSLATED_SUBJECT_MASK); }
 
 /* Handle special programs */
 Boolean Blast_ProgramIsPsiBlast(EBlastProgramType p)
-{ return p & PSSM_QUERY_MASK; }
+{ return SAFE_CAST_INT_TO_BOOLEAN(p & PSSM_QUERY_MASK); }
 
 Boolean Blast_ProgramIsPhiBlast(EBlastProgramType p)
-{ return (p & PATTERN_QUERY_MASK) != 0 ? TRUE : FALSE; }
+{ return SAFE_CAST_INT_TO_BOOLEAN(p & PATTERN_QUERY_MASK); }
 
 Boolean Blast_ProgramIsRpsBlast(EBlastProgramType p)
-{ return p & PSSM_SUBJECT_MASK; }
+{ return SAFE_CAST_INT_TO_BOOLEAN(p & PSSM_SUBJECT_MASK); }
