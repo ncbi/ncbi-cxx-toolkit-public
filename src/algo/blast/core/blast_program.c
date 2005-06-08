49e02989b34fbe54a6be155355ee33b7e425ca7f
madden <madden@ncbi.nlm.nih.gov>
2005-06-08 17:26:39 +0000

    Definitions for various programs supported by core BLAST (from Christiam)
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@22748 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ src/algo/blast/core/blast_program.c
@@ -0,0 +1,75 @@
+#ifndef SKIP_DOXYGEN_PROCESSING
+static char const rcsid[] =
+    "$Id$";
+#endif /* SKIP_DOXYGEN_PROCESSING */
+/* ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's offical duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Author:  Christiam Camacho / Ilya Dondoshansky
+ *
+ */
+
+/** @file blast_program.c
+ * Implementation auxiliary functions to determine traits of the various BLAST
+ * programs supported by core BLAST
+ */
+    
+#include <algo/blast/core/blast_program.h>
+
+/* Classify query sequence */
+Boolean Blast_QueryIsProtein(EBlastProgramType p)
+{ return p & PROTEIN_QUERY_MASK; }
+
+Boolean Blast_QueryIsNucleotide(EBlastProgramType p)
+{ return p & NUCLEOTIDE_QUERY_MASK; }
+
+Boolean Blast_QueryIsPssm(EBlastProgramType p)
+{ return p & PSSM_QUERY_MASK; }
+
+/* Classify subject sequence */
+Boolean Blast_SubjectIsProtein(EBlastProgramType p)
+{ return p & PROTEIN_SUBJECT_MASK; }
+
+Boolean Blast_SubjectIsNucleotide(EBlastProgramType p)
+{ return p & NUCLEOTIDE_SUBJECT_MASK; }
+
+Boolean Blast_SubjectIsPssm(EBlastProgramType p)
+{ return p & PSSM_SUBJECT_MASK; }
+
+/* Handle translated searches */
+Boolean Blast_QueryIsTranslated(EBlastProgramType p)
+{ return p & TRANSLATED_QUERY_MASK; }
+
+Boolean Blast_SubjectIsTranslated(EBlastProgramType p)
+{ return p & TRANSLATED_SUBJECT_MASK; }
+
+/* Handle special programs */
+Boolean Blast_ProgramIsPsiBlast(EBlastProgramType p)
+{ return p & PSSM_QUERY_MASK; }
+
+Boolean Blast_ProgramIsPhiBlast(EBlastProgramType p)
+{ return (p & PATTERN_QUERY_MASK) != 0 ? TRUE : FALSE; }
+
+Boolean Blast_ProgramIsRpsBlast(EBlastProgramType p)
+{ return p & PSSM_SUBJECT_MASK; }
