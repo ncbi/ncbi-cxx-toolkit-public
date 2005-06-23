a5da84665d44b200475d76e3d9e5667a978db611
Aaron Ucko <ucko@ncbi.nlm.nih.gov>
2005-06-23 15:48:04 +0000

    MAGIC resync with configure-generated ncbiconf.h
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@22957 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/corelib/config/ncbiconf_xcode.h
+++ include/corelib/config/ncbiconf_xcode.h
@@ -9,8 +9,17 @@
  */
 
 
+/* Define to 1 if C++ streams delete their buffers. */
+/* #undef AUTOMATIC_STREAMBUF_DESTRUCTION */
+
+/* Define to 1 if necessary to get FIONBIO (e.g., on Solaris) */
+/* #undef BSD_COMP */
+
 /* Define to 1 if you have the <arpa/inet.h> header file. */
 #define HAVE_ARPA_INET_H 1
+
+/* Define to 1 if you have the `asprintf' function. */
+#define HAVE_ASPRINTF 1
 
 /* Define to 1 if you have the `atoll' function. */
 #define HAVE_ATOLL 1
@@ -18,8 +27,21 @@
 /* Define to 1 if NCBI C++ API for BerkeleyDB is available. */
 #define HAVE_BDB 1
 
+/* Define to 1 if NCBI C++ API for BerkeleyDB based data cache is available.
+   */
+#define HAVE_BDB_CACHE 1
+
 /* Define to 1 if Berkeley DB libraries are available. */
 #define HAVE_BERKELEY_DB 1
+
+/* Define to 1 if the Berkeley `db_cxx' library is available. */
+/* #undef HAVE_BERKELEY_DB_CXX */
+
+/* Define to 1 if the `Boost.Test' libraries are available. */
+/* #undef HAVE_BOOST_TEST */
+
+/* Define to 1 if CPPUNIT libraries are available. */
+/* #undef HAVE_CPPUNIT */
 
 /* Define to 1 if the preprocessor supports GNU-style variadic macros. */
 #define HAVE_CPP_GNU_VARARGS 1
@@ -30,8 +52,20 @@
 /* Define to 1 if you have the <dlfcn.h> header file. */
 #define HAVE_DLFCN_H 1
 
+/* Define to 1 if you don't have `vprintf' but do have `_doprnt.' */
+/* #undef HAVE_DOPRNT */
+
+/* Define to 1 if you have the <errno.h> header file. */
+#define HAVE_ERRNO_H 1
+
+/* Define to 1 if you have the `FCGX_Accept_r' function. */
+/* #undef HAVE_FCGX_ACCEPT_R */
+
 /* Define to 1 if FLTK is available. */
 #define HAVE_FLTK 1
+
+/* Define to 1 if you have the `freehostent' function. */
+#define HAVE_FREEHOSTENT 1
 
 /* Define to 1 if you have the <fstream> header file. */
 #define HAVE_FSTREAM 1
@@ -39,14 +73,69 @@
 /* Define to 1 if you have the <fstream.h> header file. */
 #define HAVE_FSTREAM_H 1
 
+/* Define to 1 if your localtime_r returns a int. */
+#define HAVE_FUNC_LOCALTIME_R_INT 1
+
+/* Define to 1 if your localtime_r returns a struct tm*. */
+/* #undef HAVE_FUNC_LOCALTIME_R_TM */
+
 /* Define to 1 if you have the `getaddrinfo' function. */
 #define HAVE_GETADDRINFO 1
+
+/* If you have the `gethostbyaddr_r' function, define to the number of
+   arguments it takes (normally 7 or 8). */
+/* #undef HAVE_GETHOSTBYADDR_R */
+
+/* If you have the `gethostbyname_r' function, define to the number of
+   arguments it takes (normally 5 or 6). */
+/* #undef HAVE_GETHOSTBYNAME_R */
+
+/* Define to 1 if you have the `getipnodebyaddr' function. */
+#define HAVE_GETIPNODEBYADDR 1
+
+/* Define to 1 if you have the `getipnodebyname' function. */
+#define HAVE_GETIPNODEBYNAME 1
+
+/* Define to 1 if you have the `getloadavg' function. */
+#define HAVE_GETLOADAVG 1
 
 /* Define to 1 if you have the `getnameinfo' function. */
 #define HAVE_GETNAMEINFO 1
 
 /* Define to 1 if you have the `getpagesize' function. */
 #define HAVE_GETPAGESIZE 1
+
+/* Define to 1 if you have the `getpwuid' function. */
+#define HAVE_GETPWUID 1
+
+/* If you have the `getpwuid_r' function, define to the number of arguments it
+   takes (normally 4 or 5). */
+#define HAVE_GETPWUID_R 5
+
+/* If you have the `getservbyname_r' function, define to the number of
+   arguments it takes (normally 5 or 6). */
+/* #undef HAVE_GETSERVBYNAME_R */
+
+/* Define to 1 if you have the `gettimeofday' function. */
+#define HAVE_GETTIMEOFDAY 1
+
+/* Define to 1 if you have the `getuid' function. */
+#define HAVE_GETUID 1
+
+/* Define to 1 if ICU libraries are available. */
+/* #undef HAVE_ICU */
+
+/* Define to 1 if you have the <ieeefp.h> header file. */
+/* #undef HAVE_IEEEFP_H */
+
+/* Define to 1 if you have the `inet_ntoa_r' function. */
+/* #undef HAVE_INET_NTOA_R */
+
+/* Define to 1 if you have the `inet_ntop' function. */
+#define HAVE_INET_NTOP 1
+
+/* Define to 1 if the system has the type `intptr_t'. */
+#define HAVE_INTPTR_T 1
 
 /* Define to 1 if you have the <inttypes.h> header file. */
 #define HAVE_INTTYPES_H 1
@@ -60,6 +149,9 @@
 /* Define to 1 if you have `ios(_base)::register_callback'. */
 #define HAVE_IOS_REGISTER_CALLBACK 1
 
+/* Define to 1 if you have the `lchown' function. */
+/* #undef HAVE_LCHOWN */
+
 /* Define to 1 if libbz2 is available. */
 #define HAVE_LIBBZ2 1
 
@@ -70,9 +162,6 @@
 /* Define to 1 if DL is available, either in its own library or as part of the
    standard libraries. */
 #define HAVE_LIBDL 1
-
-/* Define to 1 if you have the `pthread_setconcurrency' function. */
-#define HAVE_PTHREAD_SETCONCURRENCY 1
 
 /* Define to 1 if libexpat is available. */
 /* #undef HAVE_LIBEXPAT */
@@ -86,22 +175,34 @@
 /* Define to 1 if NCBI GEO DB library is available. */
 /* #undef HAVE_LIBGEODB */
 
+/* Define to 1 if libgif is available. */
+#define HAVE_LIBGIF 1
+
 /* Define to 1 if you have libglut. */
 /* #undef HAVE_LIBGLUT */
 
 /* Define to 1 if ICONV is available, either in its own library or as part of
    the standard libraries. */
-/* #undef HAVE_LIBICONV */
+#define HAVE_LIBICONV 1
+
+/* Define to 1 if libjpeg is available. */
+#define HAVE_LIBJPEG 1
 
 /* Define to 1 if KSTAT is available, either in its own library or as part of
    the standard libraries. */
 /* #undef HAVE_LIBKSTAT */
+
+/* Define to 1 if liboechem is available. */
+/* #undef HAVE_LIBOECHEM */
 
 /* Define to 1 if you have libOSMesa. */
 /* #undef HAVE_LIBOSMESA */
 
 /* Define to 1 if libpcre is available. */
 /* #undef HAVE_LIBPCRE */
+
+/* Define to 1 if libpng is available. */
+#define HAVE_LIBPNG 1
 
 /* Define to 1 if NCBI PubMed libraries are available. */
 /* #undef HAVE_LIBPUBMED */
@@ -135,6 +236,9 @@
 /* Define to 1 if SYBASE DBLib is available. */
 /* #undef HAVE_LIBSYBDB */
 
+/* Define to 1 if libtiff is available. */
+#define HAVE_LIBTIFF 1
+
 /* Define to 1 if libungif is available. */
 /* #undef HAVE_LIBUNGIF */
 
@@ -150,6 +254,18 @@
 /* Define to 1 if you have the <limits> header file. */
 #define HAVE_LIMITS 1
 
+/* Define to 1 if you have the `localtime_r' function. */
+#define HAVE_LOCALTIME_R 1
+
+/* Define to 1 if local LBSM support is available. */
+/* #undef HAVE_LOCAL_LBSM */
+
+/* Define to 1 if you have the `lutimes' function. */
+/* #undef HAVE_LUTIMES */
+
+/* Define to 1 if you have the <malloc.h> header file. */
+/* #undef HAVE_MALLOC_H */
+
 /* Define to 1 if you have the <memory.h> header file. */
 #define HAVE_MEMORY_H 1
 
@@ -159,18 +275,60 @@
 /* Define to 1 if the NCBI C toolkit is available. */
 /* #undef HAVE_NCBI_C */
 
+/* Define to 1 if you have the <netdb.h> header file. */
+#define HAVE_NETDB_H 1
+
 /* Define to 1 if you have the <netinet/in.h> header file. */
 #define HAVE_NETINET_IN_H 1
+
+/* Define to 1 if you have the <netinet/tcp.h> header file. */
+#define HAVE_NETINET_TCP_H 1
+
+/* Define to 1 if `auto_ptr<T>' is missing or broken. */
+/* #undef HAVE_NO_AUTO_PTR */
+
+/* Define to 1 if `std::char_traits' is missing. */
+/* #undef HAVE_NO_CHAR_TRAITS */
+
+/* Define to 1 if new C++ streams lack `ios_base::'. */
+/* #undef HAVE_NO_IOS_BASE */
+
+/* Define to 1 if `min'/`max' templates are not implemented. */
+/* #undef HAVE_NO_MINMAX_TEMPLATE */
+
+/* Define to 1 if ODBC libraries are available. */
+/* #undef HAVE_ODBC */
+
+/* Define to 1 if you have the <odbcss.h> header file. */
+/* #undef HAVE_ODBCSS_H */
 
 /* Define to 1 if you have OpenGL (-lGL). */
 #define HAVE_OPENGL 1
 
+/* Define to 1 if the ORBacus CORBA package is available. */
+/* #undef HAVE_ORBACUS */
+
 /* Define to 1 if you have the <poll.h> header file. */
 #define HAVE_POLL_H 1
+
+/* Define to 1 if you have the `pthread_atfork' function. */
+/* #undef HAVE_PTHREAD_ATFORK */
+
+/* Define to 1 if you have the `pthread_setconcurrency' function. */
+#define HAVE_PTHREAD_SETCONCURRENCY 1
+
+/* Define to 1 if the PUBSEQ service is available. */
+/* #undef HAVE_PUBSEQ_OS */
+
+/* Define to 1 if Python libraries are available. */
+/* #undef HAVE_PYTHON */
 
 /* If you have the `readdir_r' function, define to the number of arguments it
    takes (normally 2 or 3). */
 #define HAVE_READDIR_R 3
+
+/* Define to 1 if you have the `sched_yield' function. */
+#define HAVE_SCHED_YIELD 1
 
 /* Define to 1 if you have `union semun'. */
 #define HAVE_SEMUN 1
@@ -190,6 +348,9 @@
 /* Define to 1 if you have the `strcasecmp' function. */
 #define HAVE_STRCASECMP 1
 
+/* Define to 1 if strcasecmp treats letters as lowercase. */
+#define HAVE_STRCASECMP_LC 1
+
 /* Define to 1 if you have the `strdup' function. */
 #define HAVE_STRDUP 1
 
@@ -202,8 +363,29 @@
 /* Define to 1 if you have the <strstream> header file. */
 #define HAVE_STRSTREAM 1
 
+/* Define to 1 if you have the <strstream.h> header file. */
+/* #undef HAVE_STRSTREAM_H */
+
+/* Define to 1 if you have the <strstrea.h> header file. */
+/* #undef HAVE_STRSTREA_H */
+
+/* Define to 1 if you have the `strtok_r' function. */
+#define HAVE_STRTOK_R 1
+
+/* Define to 1 if SYBASE has reentrant libraries. */
+/* #undef HAVE_SYBASE_REENTRANT */
+
+/* Define to 1 if Linux-like 1-arg sysinfo exists. */
+/* #undef HAVE_SYSINFO_1 */
+
+/* Define to 1 if you have the `sysmp' function. */
+/* #undef HAVE_SYSMP */
+
 /* Define to 1 if you have SysV semaphores. */
 #define HAVE_SYSV_SEMAPHORES 1
+
+/* Define to 1 if you have the <sys/select.h> header file. */
+#define HAVE_SYS_SELECT_H 1
 
 /* Define to 1 if you have the <sys/socket.h> header file. */
 #define HAVE_SYS_SOCKET_H 1
@@ -217,6 +399,9 @@
 /* Define to 1 if you have the <sys/sysinfo.h> header file. */
 /* #undef HAVE_SYS_SYSINFO_H */
 
+/* Define to 1 if you have the <sys/time.h> header file. */
+#define HAVE_SYS_TIME_H 1
+
 /* Define to 1 if you have the <sys/types.h> header file. */
 #define HAVE_SYS_TYPES_H 1
 
@@ -225,6 +410,9 @@
 
 /* Define to 1 if you have the <unistd.h> header file. */
 #define HAVE_UNISTD_H 1
+
+/* Define to 1 if you have the `utimes' function. */
+#define HAVE_UTIMES 1
 
 /* Define to 1 if you have the `vasprintf' function. */
 #define HAVE_VASPRINTF 1
@@ -235,37 +423,115 @@
 /* Define to 1 if you have the `vsnprintf' function. */
 #define HAVE_VSNPRINTF 1
 
+/* Define to 1 if you have the <wchar.h> header file. */
+#define HAVE_WCHAR_H 1
+
+/* Define to 1 if you have the <windows.h> header file. */
+/* #undef HAVE_WINDOWS_H */
+
 /* Define to 1 if the system has the type `wstring'. */
 #define HAVE_WSTRING 1
 
+/* Define to 1 if wxWindows is available. */
+/* #undef HAVE_WXWINDOWS */
+
+/* Define to 1 if Xalan-C++ is available. */
+/* #undef HAVE_XALAN */
+
+/* Define to 1 if Xerces-C++ is available. */
+/* #undef HAVE_XERCES */
+
 /* Full GNU-style system type */
-#define HOST "powerpc-apple-darwin7.6.0"
+#define HOST "powerpc-apple-darwin7.9.0"
 
 /* CPU type only */
 #define HOST_CPU "powerpc"
 
 /* System OS only */
-#define HOST_OS "darwin7.6.0"
+#define HOST_OS "darwin7.9.0"
 
 /* System vendor only */
 #define HOST_VENDOR "apple"
 
+/* Define as const if the declaration of iconv() needs const. */
+#define ICONV_CONST const
+
+/* Define to 0xffffffff if your operating system doesn't. */
+/* #undef INADDR_NONE */
+
+/* Compiler name */
+/* #undef NCBI_COMPILER_COMPAQ */
+
 /* Compiler name */
 #define NCBI_COMPILER_GCC 1
+
+/* Compiler name */
+/* #undef NCBI_COMPILER_ICC */
+
+/* Compiler name */
+/* #undef NCBI_COMPILER_KCC */
+
+/* Compiler name */
+/* #undef NCBI_COMPILER_MIPSPRO */
+
+/* Compiler name */
+/* #undef NCBI_COMPILER_MSVC */
+
+/* Compiler name */
+/* #undef NCBI_COMPILER_UNKNOWN */
 
 /* Compiler version as three-digit integer */
 #define NCBI_COMPILER_VERSION (__GNUC__ * 100 + \
                                __GNUC_MINOR__ * 10 + \
                                __GNUC_PATCHLEVEL__)
 
+/* Compiler name */
+/* #undef NCBI_COMPILER_VISUALAGE */
+
+/* Compiler name */
+/* #undef NCBI_COMPILER_WORKSHOP */
+
 /* This is the NCBI C++ Toolkit. */
 #define NCBI_CXX_TOOLKIT 1
+
+/* Rename DBLIB symbols in FTDS to avoid name clash with Sybase DBLIB. */
+#define NCBI_FTDS_RENAME_SYBDB 1
+
+/* Define to 1 if `string::compare()' is non-standard. */
+/* #undef NCBI_OBSOLETE_STR_COMPARE */
 
 /* Operating system name */
 #define NCBI_OS "Mac OS X"
 
+/* Define to 1 on AIX. */
+/* #undef NCBI_OS_AIX */
+
+/* Define to 1 on *BSD. */
+/* #undef NCBI_OS_BSD */
+
+/* Define to 1 on Cygwin. */
+/* #undef NCBI_OS_CYGWIN */
+
 /* Define to 1 on Mac OS X. */
 #define NCBI_OS_DARWIN 1
+
+/* Define to 1 on IRIX. */
+/* #undef NCBI_OS_IRIX */
+
+/* Define to 1 on Linux. */
+/* #undef NCBI_OS_LINUX */
+
+/* Define to 1 on MacOS. */
+/* #undef NCBI_OS_MAC */
+
+/* Define to 1 on Windows. */
+/* #undef NCBI_OS_MSWIN */
+
+/* Define to 1 on Tru64 Unix. */
+/* #undef NCBI_OS_OSF1 */
+
+/* Define to 1 on Solaris. */
+/* #undef NCBI_OS_SOLARIS */
 
 /* Define to 1 on Unix. */
 #define NCBI_OS_UNIX 1
@@ -275,6 +541,9 @@
 
 /* Define to 1 if prototypes can use exception specifications. */
 #define NCBI_USE_THROW_SPEC 1
+
+/* Define to 1 if the BSD-style netdb interface is reentrant. */
+/* #undef NETDB_REENTRANT */
 
 /* Define to the address where bug reports for this package should be sent. */
 #define PACKAGE_BUGREPORT "cpp-core@ncbi.nlm.nih.gov"
@@ -327,23 +596,40 @@
 /* Define to 1 if the stack grows down. */
 #define STACK_GROWS_DOWN 1
 
+/* Define to 1 if the stack grows up. */
+/* #undef STACK_GROWS_UP */
+
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
+
+/* Define to 1 if you can safely include both <sys/time.h> and <time.h>. */
+#define TIME_WITH_SYS_TIME 1
 
 /* Define to 1 if using a local copy of bzlib. */
 /* #undef USE_LOCAL_BZLIB */
 
 /* Define to 1 if using a local copy of PCRE. */
-/* #undef USE_LOCAL_PCRE */
-#define USE_LOCAL_PCRE
+#define USE_LOCAL_PCRE 1
 
 /* Define to 1 if your processor stores words with the most significant byte
    first (like Motorola and SPARC, unlike Intel and VAX). */
 #define WORDS_BIGENDIAN 1
 
- 
-#define HAVE_LIBJPEG 1
-#define HAVE_LIBTIFF 1
-#define HAVE_LIBPNG  1
-#define HAVE_LIBGIF  1
-#define HAVE_INTPTR_T 1
+/* Define to 1 if the X Window System is missing or not being used. */
+/* #undef X_DISPLAY_MISSING */
+
+/* Enable GNU extensions on systems that have them.  */
+#ifndef _GNU_SOURCE
+# define _GNU_SOURCE 1
+#endif
+
+/* Define to 1 if type `char' is unsigned and you are not using gcc.  */
+#ifndef __CHAR_UNSIGNED__
+/* # undef __CHAR_UNSIGNED__ */
+#endif
+
+/* Define to empty if `const' does not conform to ANSI C. */
+/* #undef const */
+
+/* Define to `unsigned' if <sys/types.h> does not define. */
+/* #undef size_t */
