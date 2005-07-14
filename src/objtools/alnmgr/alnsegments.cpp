2bb6c25cea657fd416921dbce6500b76b0c4e4e2
Kamen Todorov <todorov@ncbi.nlm.nih.gov>
2005-07-14 16:36:00 +0000

    x_CreateSegmentsVector -> Build (in the exceptions messages)
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@23281 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/alnmgr/alnsegments.cpp
+++ src/objtools/alnmgr/alnsegments.cpp
@@ -133,7 +133,7 @@ CAlnMixSegments::Build(bool gap_join,
                             row->m_StartIt->first <
                             start_its_i->second->first) {
                             string errstr =
-                                string("CAlnMixSegments::x_CreateSegmentsVector():")
+                                string("CAlnMixSegments::Build():")
                                 + " Internal error: Integrity broken" +
                                 " row=" + NStr::IntToString(row->m_RowIdx) +
                                 " seq=" + NStr::IntToString(row->m_SeqIdx)
@@ -159,7 +159,7 @@ CAlnMixSegments::Build(bool gap_join,
 #if _DEBUG
                         if (row == refseq) {
                             string errstr =
-                                string("CAlnMixSegments::x_CreateSegmentsVector():")
+                                string("CAlnMixSegments::Build():")
                                 + " Internal error: Infinite loop detected.";
                             NCBI_THROW(CAlnException, eMergeFailure, errstr);
                         }                            
@@ -184,7 +184,7 @@ CAlnMixSegments::Build(bool gap_join,
                             row->m_StartIt->first <
                             start_its_i->second->first) {
                             string errstr =
-                                string("CAlnMixSegments::x_CreateSegmentsVector():")
+                                string("CAlnMixSegments::Build():")
                                 + " Internal error: Integrity broken" +
                                 " row=" + NStr::IntToString(row->m_RowIdx) +
                                 " seq=" + NStr::IntToString(row->m_SeqIdx)
@@ -560,6 +560,9 @@ END_NCBI_SCOPE
 * ===========================================================================
 *
 * $Log$
+* Revision 1.5  2005/07/14 16:36:00  todorov
+* x_CreateSegmentsVector -> Build (in the exceptions messages)
+*
 * Revision 1.4  2005/07/12 18:02:27  todorov
 * Fixed the class name in the exception message.
 *
