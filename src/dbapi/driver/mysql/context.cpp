e372900dcb0b94b635fe90221b113679c2541b8b
ssikorsk <ssikorsk@ncbi.nlm.nih.gov>
2005-07-19 13:49:14 +0000

    Export DBAPI_RegisterDriver_MYSQL(I_DriverMgr& mgr)
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@23374 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/dbapi/driver/mysql/context.cpp
+++ src/dbapi/driver/mysql/context.cpp
@@ -141,6 +141,7 @@ DBAPI_RegisterDriver_MYSQL(void)
 }
 
 ///////////////////////////////////////////////////////////////////////////////
+NCBI_DBAPIDRIVER_MYSQL_EXPORT
 void DBAPI_RegisterDriver_MYSQL(I_DriverMgr& mgr)
 {
     mgr.RegisterDriver("mysql", MYSQL_CreateContext);
@@ -168,6 +169,9 @@ END_NCBI_SCOPE
 /*
  * ===========================================================================
  * $Log$
+ * Revision 1.13  2005/07/19 13:49:14  ssikorsk
+ * Export DBAPI_RegisterDriver_MYSQL(I_DriverMgr& mgr)
+ *
  * Revision 1.12  2005/06/07 16:22:51  ssikorsk
  * Included <dbapi/driver/driver_mgr.hpp> to make CDllResolver_Getter<I_DriverContext> explicitly visible.
  *
