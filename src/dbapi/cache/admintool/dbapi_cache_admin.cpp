e6f562d38648e0aa5ec873eb15237ad6f9b54864
Anatoliy Kuznetsov <kuznets@ncbi.nlm.nih.gov>
2005-07-21 18:54:59 +0000

    Code cleanup
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@23442 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/dbapi/cache/admintool/dbapi_cache_admin.cpp
+++ src/dbapi/cache/admintool/dbapi_cache_admin.cpp
@@ -41,6 +41,7 @@
 
 USING_NCBI_SCOPE;
 
+
 /// DBAPI cache administration tool
 ///
 /// @internal
@@ -50,15 +51,15 @@ class CDBAPI_CacheAdmin : public CNcbiApplication
 public:
     void Init(void);
     int Run(void);
+
 protected:
     int Connect(const CArgs& args);
-
     unsigned GetTimeout(const CArgs& args);
+
 private:
     IDataSource*            m_Ds;
     auto_ptr<IConnection>   m_Conn;
     CDBAPI_Cache            m_Cache;
-
 };
 
 
@@ -71,85 +72,87 @@ void CDBAPI_CacheAdmin::Init(void)
     arg_desc->SetUsageContext(GetArguments().GetProgramBasename(),
                               "DBAPI cache admin library");
 
-    arg_desc->AddOptionalKey("s",
+    arg_desc->AddDefaultKey("s",
                              "server",
                              "Database server name (default: MSSQL10)",
-                             CArgDescriptions::eString);
+                             CArgDescriptions::eString,
+                             "MSSQL10");
 
-    arg_desc->AddOptionalKey("d",
+    arg_desc->AddDefaultKey("d",
                              "database",
                              "Database name (default: NCBI_Cache)",
-                             CArgDescriptions::eString);
+                             CArgDescriptions::eString,
+                             "NCBI_Cache");
 
-    arg_desc->AddOptionalKey("u",
+    arg_desc->AddDefaultKey("u",
                              "username",
                              "Login name (default: cwrite)",
-                             CArgDescriptions::eString);
+                             CArgDescriptions::eString,
+                             "cwrite");
 
-    arg_desc->AddOptionalKey("p",
-                             "password",
-                             "Password",
-                             CArgDescriptions::eString);
+    arg_desc->AddDefaultKey("p",
+                            "password",
+                            "Password",
+                            CArgDescriptions::eString,
+                            "allowed");
 
     arg_desc->AddFlag("m",
                       "Run cache maintenance (Timeout based BLOB removal).");
 
 
 
-    arg_desc->AddOptionalKey("st",
+    arg_desc->AddDefaultKey("st",
                       "stimeout",
                       "BLOB expiration timeout in seconds",
-                      CArgDescriptions::eInteger);
+                      CArgDescriptions::eInteger, 
+                      "0");
 
-    arg_desc->AddOptionalKey("mt",
-                      "mtimeout",
-                      "BLOB expiration timeout in minutes",
-                      CArgDescriptions::eInteger);
+    arg_desc->AddDefaultKey("mt",
+                            "mtimeout",
+                            "BLOB expiration timeout in minutes",
+                            CArgDescriptions::eInteger,
+                            "0");
 
-    arg_desc->AddOptionalKey("ht",
-                      "htimeout",
-                      "BLOB expiration timeout in hours",
-                      CArgDescriptions::eInteger);
+    arg_desc->AddDefaultKey("ht",
+                            "htimeout",
+                            "BLOB expiration timeout in hours",
+                            CArgDescriptions::eInteger,
+                            "0");
 
-    arg_desc->AddOptionalKey("dt",
-                      "dtimeout",
-                      "BLOB expiration timeout in days",
-                      CArgDescriptions::eInteger);
+    arg_desc->AddDefaultKey("dt",
+                            "dtimeout",
+                            "BLOB expiration timeout in days",
+                            CArgDescriptions::eInteger,
+                            "0");
 
     SetupArgDescriptions(arg_desc.release());
 }
+
 
 int CDBAPI_CacheAdmin::Connect(const CArgs& args)
 {
     CDriverManager &db_drv_man = CDriverManager::GetInstance();
     string drv_name;
-
-#ifdef NCBI_OS_MSWIN
-    DBAPI_RegisterDriver_ODBC(db_drv_man);
-    drv_name = "odbc";
-#else
     DBAPI_RegisterDriver_FTDS(db_drv_man);
     drv_name = "ftds";
-#endif
+
     IDataSource* ds = db_drv_man.CreateDs(drv_name);
 
     if (ds == 0) {
-        NcbiCerr << "Cannot init driver: " << drv_name
-                    << NcbiEndl;
+        ERR_POST(Error << "Cannot init driver: " << drv_name);
         return 1;
     }
 
     m_Conn.reset(ds->CreateConnection());
     if (m_Conn.get() == 0) {
-        NcbiCerr << "Cannot create connection. Driver: " << drv_name
-                    << NcbiEndl;
+        ERR_POST("Cannot create connection. Driver: " << drv_name);
         return 1;
     }
 
-    string server = (args["s"]) ? args["s"].AsString() : "MSSQL10";
-    string database = (args["d"]) ? args["d"].AsString() : "NCBI_Cache";
-    string user = (args["u"]) ? args["u"].AsString() : "cwrite";
-    string passwd = (args["p"]) ? args["p"].AsString() : "allowed";
+    string server   = args["s"].AsString();
+    string database = args["d"].AsString();
+    string user     = args["u"].AsString();
+    string passwd   = args["p"].AsString();
 
     m_Conn->Connect(user, passwd, server, database);
 
@@ -161,10 +164,10 @@ int CDBAPI_CacheAdmin::Connect(const CArgs& args)
 unsigned CDBAPI_CacheAdmin::GetTimeout(const CArgs& args)
 {
     unsigned timeout = 24 * 60 * 60;
-    unsigned sec  = (args["st"]) ? args["st"].AsInteger() : 0;
-    unsigned min  = (args["mt"]) ? args["mt"].AsInteger() : 0;
-    unsigned hr   = (args["ht"]) ? args["ht"].AsInteger() : 0;
-    unsigned days = (args["dt"]) ? args["dt"].AsInteger() : 0;
+    unsigned sec  = args["st"].AsInteger();
+    unsigned min  = args["mt"].AsInteger();
+    unsigned hr   = args["ht"].AsInteger();
+    unsigned days = args["dt"].AsInteger();
 
     unsigned tout = sec + (min * 60) + (hr * 60 * 60) + (days * 24 * 60 * 60);
 
@@ -191,9 +194,8 @@ int CDBAPI_CacheAdmin::Run(void)
         }
 
     } catch( CDB_Exception& dbe ) {
-        NcbiCerr << dbe.what() << NcbiEndl
-                 << dbe.GetMsg()
-                 << NcbiEndl;
+        ERR_POST(dbe.what() << dbe.GetMsg());
+                 
     }
 
     return 0;
@@ -209,6 +211,9 @@ int main(int argc, const char* argv[])
 /*
  * ===========================================================================
  * $Log$
+ * Revision 1.4  2005/07/21 18:54:59  kuznets
+ * Code cleanup
+ *
  * Revision 1.3  2005/04/04 13:03:56  ssikorsk
  * Revamp of DBAPI exception class CDB_Exception
  *
