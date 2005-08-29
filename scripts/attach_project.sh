00883c3c7f20bd58041a70ebd3b8f1e8415c369d
Denis Vakatov <vakatov@ncbi.nlm.nih.gov>
2005-08-29 15:35:27 +0000

    draft #2
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@24001 78c7ea69-d796-4a43-9a09-de51944f1b03

--- scripts/attach_project.sh
+++ scripts/attach_project.sh
@@ -16,73 +16,85 @@ script_dir=`dirname $0`
 script_dir=`(cd "${script_dir}" ; pwd)`
 
 # "yes" or "no"
-do_update="no"
+do_update="yes"
+
 #-----------------------------------------------------------------------------
 
-DYLD_BIND_AT_LAUNCH=1
-export DYLD_BIND_AT_LAUNCH
 . ${script_dir}/common.sh
 
 # has one optional argument: message
 Usage()
 {
     cat <<EOF 1>&2
-USAGE: $script_name proj [-d]
+USAGE: $script_name proj [-n]
 SYNOPSIS:
  Replace project placeholder folders by real ones
 ARGUMENTS:
   proj -- mandatory name of proj.attach file in scripts/projects or
           scripts/internal/projects folder
-  -d   -- do update (otherwise just report)
+  -n   -- do not actually do anything, just report
 EOF
     test -z "$1"  ||  echo ERROR: $1 1>&2
     exit 1
 }
 
+
 #-----------------------------------------------------------------------------
 # analyze script arguments
+
 proj=
-if test $# -eq 0; then
+if test $# -eq 0 ; then
   Usage "No argument provided"
 fi
 for cmd_arg in "$@"; do
   case "$cmd_arg" in
-    -d ) do_update="yes" ;;
-    *  ) proj=$cmd_arg ;;
+    -n ) do_update="no" ;;
+    *  ) if -n "$proj"
+           Usage "Only one project file is allowed"
+         else
+           proj="$cmd_arg"
+         fi
+         ;;
   esac
 done
 
+
+
 #-----------------------------------------------------------------------------
 # check existence of files and folders
-cd $initial_dir
-proj_file="$script_dir/projects/$proj.attach"
-if test ! -f $proj_file; then
-  proj_file="$script_dir/internal/projects/$proj.attach"
+
+proj_file="$proj"
+if test ! -f "$proj_file" ; then
+  proj_file="$script_dir/projects/$proj.attach"
+  if test ! -f $proj_file ; then
+    proj_file="$script_dir/internal/projects/$proj.attach"
+  fi
 fi
-test -f $proj_file || Usage "$proj.attach file not found"
+test -f $proj_file  ||  Usage "Project file '$proj.attach' not found"
+
+
 #-----------------------------------------------------------------------------
 
-while read dir cvs_loc
+cat "$proj_file" | while read dir cvs_loc
 do
-  cd $initial_dir
-  if test ! -d $dir; then
-    echo "ERROR: $dir is not found -- skipping"
-    continue
+  cd "$initial_dir"
+  if test ! -d "$dir" ; then
+    COMMON_Error "$dir is not found"
   fi
   dir_name=`dirname $dir`
   base_name=`basename $dir`
   echo "=================================================================="
-  if test $do_update = "yes"; then
-    cd $dir_name
+  if test "$do_update" = "yes" ; then
+    COMMON_Exec cd $dir_name
     echo "removing existing $dir"
-    rm -r $base_name
+    COMMON_Exec rm -r "$base_name"
     echo "------------------------------------------------------------------"
     echo "CVS checkout $cvs_loc into $dir"
-    cvs co -d $base_name $cvs_loc > /dev/null
+    COMMON_Exec cvs co -d "$base_name" "$cvs_loc" > /dev/null
   else
     echo "$dir to be replaced by files from CVS in $cvs_loc"
   fi
-done < $proj_file
+done
 
 echo "------------------------------------------------------------------"
 echo "Done"
