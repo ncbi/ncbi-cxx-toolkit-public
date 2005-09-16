68298da0462cb3fe4971c4cf5bf1555762d2836d
chetvern <chetvern@ncbi.nlm.nih.gov>
2005-09-16 14:24:07 +0000

    Added header and trailer comments
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@24269 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/app/gnomon/gnomon_local.cpp
+++ src/app/gnomon/gnomon_local.cpp
@@ -1,3 +1,34 @@
+/*  $Id$
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Authors:  Alexandre Souvorov
+ *
+ * File Description:
+ *
+ */
+
 #include <ncbi_pch.hpp>
 #include <corelib/ncbiapp.hpp>
 #include <corelib/ncbienv.hpp>
@@ -159,3 +190,15 @@ int main(int argc, const char* argv[])
     // Execute main application function
     return CGnomon_localApplication().AppMain(argc, argv, 0, eDS_Default, 0);
 }
+
+/*
+ * ===========================================================================
+ * $Log$
+ * Revision 1.2  2005/09/16 14:24:07  chetvern
+ * Added header and trailer comments
+ *
+ * Revision 1.1  2005/09/16 10:07:25  chetvern
+ * Initial revision
+ *
+ * ===========================================================================
+ */
