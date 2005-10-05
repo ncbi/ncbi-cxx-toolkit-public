0a681f13c49f72ac1b2e013dbbce87785a0a2ef6
Eugene Vasilchenko <vasilche@ncbi.nlm.nih.gov>
2005-10-05 18:14:15 +0000

    Changed command lines to allow CHECK_EXEC to save core. Added debug log for ID2.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@24552 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/test/objmgr/test_objmgr_data_ids.sh
+++ src/objtools/test/objmgr/test_objmgr_data_ids.sh
@@ -5,11 +5,13 @@ OBJMGR_SCOPE_AUTORELEASE_SIZE=0
 export OBJMGR_SCOPE_AUTORELEASE_SIZE
 OBJMGR_BLOB_CACHE=0
 export OBJMGR_BLOB_CACHE
+GENBANK_ID2_DEBUG=5
+export GENBANK_ID2_DEBUG
 
 for mode in "" "-no_reset" "-keep_handles" "-no_reset -keep_handles"; do
     for file in test_objmgr_data.id*; do
         echo "Testing: $@ $mode -idlist $file"
-        if $CHECK_EXEC time $@ $mode -idlist "$file"; then
+        if time $CHECK_EXEC "$@" $mode -idlist "$file"; then
             echo "Done."
         else
             exit 1
