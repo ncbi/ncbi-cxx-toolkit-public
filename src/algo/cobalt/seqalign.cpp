7e88a27704040ad0f6f735a4e000df6b6bf3b3ec
papadopo <papadopo@ncbi.nlm.nih.gov>
2005-11-18 20:25:22 +0000

    Make main seqalign generator private, and add extra member to extract a subset of the complete aligned results
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@25345 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/algo/cobalt/seqalign.cpp
+++ src/algo/cobalt/seqalign.cpp
@@ -48,10 +48,11 @@ BEGIN_SCOPE(cobalt)
 USING_SCOPE(objects);
 
 CRef<CSeq_align>
-CMultiAligner::GetSeqalignResults() const
+CMultiAligner::x_GetSeqalign(vector<CSequence>& align,
+                             vector<int>& indices)
 {
-    int num_queries = m_Results.size();
-    int length = m_Results[0].GetLength();
+    int num_queries = align.size();
+    int length = align[0].GetLength();
 
     CRef<CSeq_align> retval(new CSeq_align);
     retval->SetType(CSeq_align::eType_global);
@@ -62,7 +63,7 @@ CMultiAligner::GetSeqalignResults() const
 
     for (int i = 0; i < num_queries; i++) {
         CRef<CSeq_id> id(const_cast<CSeq_id *>(
-                                      &m_tQueries[i].seqloc->GetWhole()));
+                       &m_tQueries[indices[i]].seqloc->GetWhole()));
         denseg->SetIds().push_back(id);
     }
 
@@ -72,15 +73,15 @@ CMultiAligner::GetSeqalignResults() const
 
     for (i = 1, seg_len = 1; i < length; i++, seg_len++) {
         for (j = 0; j < num_queries; j++) {
-            if ((m_Results[j].GetLetter(i) == CSequence::kGapChar &&
-                 m_Results[j].GetLetter(i-1) != CSequence::kGapChar) ||
-                (m_Results[j].GetLetter(i) != CSequence::kGapChar &&
-                 m_Results[j].GetLetter(i-1) == CSequence::kGapChar)) 
+            if ((align[j].GetLetter(i) == CSequence::kGapChar &&
+                 align[j].GetLetter(i-1) != CSequence::kGapChar) ||
+                (align[j].GetLetter(i) != CSequence::kGapChar &&
+                 align[j].GetLetter(i-1) == CSequence::kGapChar)) 
                 break;
         }
         if (j < num_queries) {
             for (j = 0; j < num_queries; j++) {
-                if (m_Results[j].GetLetter(i-1) == CSequence::kGapChar) {
+                if (align[j].GetLetter(i-1) == CSequence::kGapChar) {
                     denseg->SetStarts().push_back(-1);
                 }
                 else {
@@ -95,7 +96,7 @@ CMultiAligner::GetSeqalignResults() const
     }
 
     for (j = 0; j < num_queries; j++) {
-        if (m_Results[j].GetLetter(i-1) == CSequence::kGapChar)
+        if (align[j].GetLetter(i-1) == CSequence::kGapChar)
             denseg->SetStarts().push_back(-1);
         else
             denseg->SetStarts().push_back(seq_off[j]);
@@ -107,11 +108,44 @@ CMultiAligner::GetSeqalignResults() const
     return retval;
 }
 
+
+CRef<CSeq_align>
+CMultiAligner::GetSeqalignResults()
+{
+    int num_queries = m_Results.size();
+    vector<int> indices(num_queries);
+
+    for (int i = 0; i < num_queries; i++) {
+        indices[i] = i;
+    }
+    return x_GetSeqalign(m_Results, indices);
+}
+
+
+CRef<CSeq_align>
+CMultiAligner::GetSeqalignResults(vector<int>& indices)
+{
+    int num_selected = indices.size();
+    vector<CSequence> new_align(num_selected);
+    vector<int> new_indices(num_selected);
+
+    for (int i = 0; i < num_selected; i++) {
+        _ASSERT(indices[i] >= 0 && indices[i] < m_Results.size());
+        new_align[i] = m_Results[indices[i]];
+        new_indices[i] = i;
+    }
+    CSequence::CompressSequences(new_align, new_indices);
+    return x_GetSeqalign(new_align, indices);
+}
+
 END_SCOPE(cobalt)
 END_NCBI_SCOPE
 
 /*-----------------------------------------------------------------------
   $Log$
+  Revision 1.4  2005/11/18 20:25:22  papadopo
+  Make main seqalign generator private, and add extra member to extract a subset of the complete aligned results
+
   Revision 1.3  2005/11/08 19:49:19  papadopo
   fix solaris compile warnings
 
