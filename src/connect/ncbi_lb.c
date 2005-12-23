8f39ca60e07274ac09d9e093dea2f979d7b7c8dc
Anton Lavrentiev <lavr@ncbi.nlm.nih.gov>
2005-12-23 18:10:19 +0000

    Implement host_and_port preference and candidate latching on pref < 0.0
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@25866 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/connect/ncbi_lb.c
+++ src/connect/ncbi_lb.c
@@ -35,33 +35,67 @@
 #include <stdlib.h>
 
 
+typedef enum {
+    ePref_Mismatch  = 0,
+    ePref_HostMatch = 1,
+    ePref_FullMatch = 2
+} EPrefMatch;
+
+
+static EPrefMatch s_Match(SERV_ITER iter,
+                          unsigned int host, unsigned short port)
+{
+    EPrefMatch match;
+    if (!iter->pref  ||  !iter->host  ||  iter->host != host)
+        match =  ePref_Mismatch;
+    else if (!iter->port)
+        match = ePref_HostMatch;
+    else if (iter->port == port)
+        match = ePref_FullMatch;
+    else
+        match = ePref_Mismatch;
+    return match;
+}
+
+
 size_t LB_Select(SERV_ITER     iter,          void*  data,
                  FGetCandidate get_candidate, double bonus)
 {
     double total = 0.0, access = 0.0, point = 0.0, p = 0.0, status;
+    EPrefMatch best_match = ePref_Mismatch;
     const SSERV_Info* info;
     SLB_Candidate* cand;
     size_t i, n;
 
-    assert(iter);
+    assert(bonus > 1.0);
+    assert(iter  &&  get_candidate);
+    best_match = ePref_Mismatch;
     for (n = 0; ; n++) {
+        EPrefMatch match;
         if (!(cand = get_candidate(data, n))) {
             assert(n);
             break;
         }
-        if ((iter->type & fSERV_Promiscuous)  ||  iter->mask)
-            return 0;
+        if (iter->ismask  ||  iter->promiscuous)
+            return 0/*first entry fits*/;
         info   = cand->info;
         status = cand->status;
-        if ((iter->host == info->host  ||
-             (!iter->host  &&  info->locl  &&  info->coef < 0.0))) {
+        match  = s_Match(iter, info->host, info->port);
+        assert(status > 0.0);
+        if (( best_match  &&  best_match <= match)  ||
+            (!best_match  &&  !iter->host  &&  !iter->port
+             &&  info->locl  &&  info->coef < 0.0)) {
+            if (best_match < match) {
+                best_match = match;
+                access = 0.0;
+            }
             if (iter->pref  ||  info->coef <= 0.0) {
                 status *= bonus;
                 if (access < status  &&  (iter->pref  ||  info->coef < 0.0)) {
                     access =  status;
-                    point  =  total + status; /* Latch this local server */
-                    p      = -info->coef;
-                    assert(point > 0.0);
+                    point  =  total + status; /* latch this local server  */
+                    p      = -info->coef;     /* this may result negative */
+                    i      = n;
                 }
             } else
                 status *= info->coef;
@@ -70,7 +104,11 @@ size_t LB_Select(SERV_ITER     iter,          void*  data,
         cand->status = total;
     }
 
-    if (point <= 0.0  ||  iter->pref >= 0.0) {
+    if (best_match  &&  point > 0.0  &&  iter->pref < 0.0) {
+        /* Latched preference */
+        cand = get_candidate(data, i);
+        status = access;
+    } else {
         if (iter->pref > 0.0) {
             if (point > 0.0  &&  total != access) {
                 p = SERV_Preference(iter->pref, access/total, n);
@@ -80,13 +118,12 @@ size_t LB_Select(SERV_ITER     iter,          void*  data,
                            " -> Pref = %lf", iter->pref, access, total,
                            access/total, (int) n, p));
 #endif /*NCBI_LB_DEBUG*/
-                status = total*p;
-                p = total*(1.0 - p)/(total - access);
+                status = total * p;
+                p = total * (1.0 - p) / (total - access);
                 for (i = 0; i < n; i++) {
                     cand = get_candidate(data, n);
-                    assert(cand);
                     if (point <= cand->status)
-                        cand->status  = p*(cand->status - access) + status;
+                        cand->status  = p * (cand->status - access) + status;
                     else
                         cand->status *= p;
                 }
@@ -110,20 +147,24 @@ size_t LB_Select(SERV_ITER     iter,          void*  data,
         /* We take pre-chosen local server only if its status is not less
            than p% of the average remaining status; otherwise, we ignore
            the server, and apply the generic procedure by random seeding.*/
-        if (point <= 0.0  ||  access*(n - 1) < p*0.01*(total - access))
+        if (point <= 0.0  ||  access * (n - 1) < p * 0.01 * (total - access)) {
             point = (total * rand()) / (double) RAND_MAX;
+        }
+
+        total = 0.0;
+        for (i = 0; i < n; i++) {
+            cand = get_candidate(data, i);
+            assert(cand);
+            if (point <= cand->status) {
+                status = cand->status - total;
+                break;
+            }
+            total = cand->status;
+        }
     }
 
-    status = 0.0;
-    for (i = 0; i < n; i++) {
-        cand = get_candidate(data, i);
-        assert(cand);
-        if (point <= cand->status)
-            break;
-        status = cand->status;
-    }
     assert(cand  &&  i < n);
-    cand->status -= status;
+    cand->status = status;
 
     return i;
 }
