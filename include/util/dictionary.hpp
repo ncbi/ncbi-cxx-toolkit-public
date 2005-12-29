88f38121205d8d04bd8ad2f2d9b1a9e37b507fe7
Michael DiCuccio <dicuccio@ncbi.nlm.nih.gov>
2005-12-29 19:28:19 +0000

    Split text utility classes out of dictionary.[hc]pp and placed them in a separate compilation unit (dictionary_util.[hc]pp).  Added an implementation of Porter's stemming algorithm
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@25925 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/util/dictionary.hpp
+++ include/util/dictionary.hpp
@@ -214,76 +214,6 @@ private:
 
 
 
-///
-/// Standard dictionary utility functions
-///
-class NCBI_XUTIL_EXPORT CDictionaryUtil
-{
-public:
-    enum {
-        eMaxSoundex = 4,
-        eMaxMetaphone = 4
-    };
-
-    /// Compute the Soundex key for a given word
-    /// The Soundex key is defined as:
-    ///   - the first leter of the word, capitalized
-    ///   - convert the remaining letters based on simple substitutions and
-    ///     groupings of similar letters
-    ///   - remove duplicates
-    ///   - pad with '0' to the maximum number of characters
-    ///
-    /// The final step is non-standard; the usual pad is ' '
-    static void GetSoundex(const string& in, string* out,
-                           size_t max_chars = eMaxSoundex,
-                           char pad_char = '0');
-
-    /// Compute the Metaphone key for a given word
-    /// Metaphone is a more advanced algorithm than Soundex; instead of
-    /// matching simple letters, Metaphone matches diphthongs.  The rules are
-    /// complex, and try to match how languages are pronounced.  The
-    /// implementation here borrows some options from Double Metaphone; the
-    /// modifications from the traditional Metaphone algorithm include:
-    ///  - all leading vowels are rendered as 'a' (from Double Metaphone)
-    ///  - rules regarding substitution of dge/dgi/dgy -> j were loosened a bit
-    ///  to permit such substitutions at the end of the word
-    ///  - 'y' is treated as a vowel if surrounded by consonants
-    static void GetMetaphone(const string& in, string* out,
-                             size_t max_chars = eMaxMetaphone);
-
-    /// Return the Levenshtein edit distance between two words.  Two possible
-    /// methods of computation are supported - an exact method with quadratic
-    /// complexity and a method suitable for similar words with a near-linear
-    /// complexity.  The similar algorithm is suitable for almost all words we
-    /// would encounter; it will render inaccuracies if the number of
-    /// consecutive differences is greater than three.
-
-    enum EDistanceMethod {
-        /// This method performs an exhausive search, and has an
-        /// algorithmic complexity of O(n x m), where n = length of str1 and 
-        /// m = length of str2
-        eEditDistance_Exact,
-
-        /// This method performs a simpler search, looking for the distance
-        /// between similar words.  Words with more than two consecutively
-        /// different characters will be scored incorrectly.
-        eEditDistance_Similar
-    };
-    static size_t GetEditDistance(const string& str1, const string& str2,
-                                  EDistanceMethod method = eEditDistance_Exact);
-
-    /// Compute a nearness score for two different words or phrases
-    static int Score(const string& word1, const string& word2,
-                     size_t max_metaphone = eMaxMetaphone);
-
-    /// Compute a nearness score based on metaphone as well as raw distance
-    static int Score(const string& word1, const string& meta1,
-                     const string& word2, const string& meta2,
-                     EDistanceMethod method = eEditDistance_Similar);
-
-};
-
-
 
 END_NCBI_SCOPE
 
@@ -291,6 +221,11 @@ END_NCBI_SCOPE
 /*
  * ===========================================================================
  * $Log$
+ * Revision 1.4  2005/12/29 19:28:19  dicuccio
+ * Split text utility classes out of dictionary.[hc]pp and placed them in a
+ * separate compilation unit (dictionary_util.[hc]pp).  Added an implementation of
+ * Porter's stemming algorithm
+ *
  * Revision 1.3  2004/08/17 13:26:39  dicuccio
  * Large update.  Added more constructors for CSimpleDictionary; support
  * pre-computed metaphone keys in CSimpleDictionary; fixed several bugs in edit
