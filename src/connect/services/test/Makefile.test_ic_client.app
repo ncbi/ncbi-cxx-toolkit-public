dcd30bbec5b6f42617a35e883a992a187b397269
Anatoliy Kuznetsov <kuznets@ncbi.nlm.nih.gov>
2006-01-03 15:38:57 +0000

    + test for net ICache
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@25938 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ src/connect/services/test/Makefile.test_ic_client.app
@@ -0,0 +1,8 @@
+# $Id$
+
+APP = test_ic_client
+SRC = test_ic_client
+LIB = xconnserv xconnect xutil xncbi
+
+LIBS = $(NETWORK_LIBS) $(DL_LIBS) $(ORIG_LIBS)
+
---
+++ src/connect/services/test/test_ic_client.cpp
@@ -0,0 +1,127 @@
+/*  $Id$
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Authors:  Anatoliy Kuznetsov
+ *
+ * File Description:  Network ICache client test
+ *
+ */
+
+#include <ncbi_pch.hpp>
+#include <corelib/ncbiapp.hpp>
+#include <corelib/ncbiargs.hpp>
+#include <corelib/ncbienv.hpp>
+#include <corelib/ncbireg.hpp>
+#include <corelib/ncbi_system.hpp>
+#include <corelib/ncbimisc.hpp>
+
+#include <connect/services/neticache_client.hpp>
+#include <connect/ncbi_socket.hpp>
+#include <connect/ncbi_types.h>
+#include <connect/ncbi_core_cxx.hpp>
+#include <test/test_assert.h>  /* This header must go last */
+
+
+USING_NCBI_SCOPE;
+
+    
+///////////////////////////////////////////////////////////////////////
+
+
+/// Test application for network ICache client
+///
+/// @internal
+///
+class CTestICClient : public CNcbiApplication
+{
+public:
+    void Init(void);
+    int Run(void);
+};
+
+
+
+
+void CTestICClient::Init(void)
+{
+    // Setup command line arguments and parameters
+
+    // Create command-line argument descriptions class
+    auto_ptr<CArgDescriptions> arg_desc(new CArgDescriptions);
+
+    // Specify USAGE context
+    arg_desc->SetUsageContext(GetArguments().GetProgramBasename(),
+                              "Network ICache client test");
+    
+    arg_desc->AddPositional("hostname", 
+                            "NetCache host name.", CArgDescriptions::eString);
+
+    arg_desc->AddPositional("port",
+                            "Port number.", 
+                            CArgDescriptions::eInteger);
+    
+    // Setup arg.descriptions for this application
+    SetupArgDescriptions(arg_desc.release());
+
+//    CONNECT_Init(&GetConfig());
+
+    SetDiagPostLevel(eDiag_Info);
+    SetDiagTrace(eDT_Enable);
+}
+
+
+
+        
+
+
+int CTestICClient::Run(void)
+{
+    CArgs args = GetArgs();
+    const string& host  = args["hostname"].AsString();
+    unsigned short port = args["port"].AsInteger();
+
+    const char test_data[] = "A quick brown fox, jumps over lazy dog.";
+    const char test_data2[] = "New data.";
+    string key;
+
+    return 0;
+}
+
+
+int main(int argc, const char* argv[])
+{
+    return CTestICClient().AppMain(argc, argv, 0, eDS_Default, 0);
+}
+
+
+/*
+ * ===========================================================================
+ * $Log$
+ * Revision 6.1  2006/01/03 15:38:57  kuznets
+ * + test for net ICache
+ *
+ *
+ * ===========================================================================
+ */
