2771bda336a95a479e3293f206922e8ceabb03f8
ssikorsk <ssikorsk@ncbi.nlm.nih.gov>
2006-01-19 17:01:16 +0000

    Removed MOZART from the test-suite because it is going to be closed down
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@26246 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/dbapi/driver/samples/test_lang_query.sh
+++ src/dbapi/driver/samples/test_lang_query.sh
@@ -8,7 +8,7 @@ ulimit -n 1536 > /dev/null 2>&1
 
 driver_list="ctlib dblib ftds ftds63 odbc msdblib"
 # server_list="MS_DEV2 BARTOK BARTOK_12"
-server_list="MS_DEV1 STRAUSS MOZART"
+server_list="MS_DEV1 STRAUSS"
 # server_mssql="MS_DEV2"
 server_mssql="MS_DEV1"
 
