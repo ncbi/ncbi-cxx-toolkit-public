6025794a1727092d7b5056283e626a2400affbe7
papadopo <papadopo@ncbi.nlm.nih.gov>
2006-01-23 16:33:18 +0000

    remove extra_extensions count
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@26298 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/algo/blast/core/blast_diagnostics.h
+++ include/algo/blast/core/blast_diagnostics.h
@@ -71,8 +71,6 @@ typedef struct BlastGappedStats {
    Int4 seqs_ungapped_passed; /**< Number of sequences with top HSP 
                                  after ungapped extension passing the
                                  e-value threshold. */
-   Int4 extra_extensions; /**< Number of extra gapped extensions performed for
-                             ungapped HSPs above the e-value threshold. */
    Int4 extensions; /**< Total number of gapped extensions performed. */
    Int4 good_extensions; /**< Number of HSPs below the e-value threshold after
                             gapped extension */
