3276f5ed1a6b4b12ddab5d601e247b7547270716
Andrei Gourianov <gouriano@ncbi.nlm.nih.gov>
2006-01-26 19:40:25 +0000

    Set vars only when needed
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@26422 78c7ea69-d796-4a43-9a09-de51944f1b03

--- compilers/msvc800exp_prj/msvcvars.bat
+++ compilers/msvc800exp_prj/msvcvars.bat
@@ -1,2 +1,9 @@
-REM $Id: msvcvars.bat,v 1.1 2006/01/11 17:33:40 gouriano Exp $
-call "%VS80COMNTOOLS%vsvars32.bat"
+@echo off
+REM
+REM $Id: msvcvars.bat,v 1.2 2006/01/26 19:40:25 gouriano Exp $
+REM
+
+@if not "%VSINSTALLDIR%"=="" goto end
+@call "%VS80COMNTOOLS%vsvars32.bat"
+
+:end
