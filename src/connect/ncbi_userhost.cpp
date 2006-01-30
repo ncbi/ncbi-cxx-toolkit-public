dc344cf89e9ceba1b00c7eac80d629627d47f67a
Aleksey Grichenko <grichenk@ncbi.nlm.nih.gov>
2006-01-30 19:55:27 +0000

    Initial revision
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@26487 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ src/connect/ncbi_userhost.cpp
@@ -0,0 +1,77 @@
+/*  $Id$
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Author:  Aleksey Grichenko
+ *
+ * File Description:
+ *   User and host setup for NCBI Diag
+ *
+ */
+
+#include <ncbi_pch.hpp>
+#include <connect/ncbi_socket.h>
+#include <connect/ncbi_connutil.h>
+#include <connect/ncbi_userhost.hpp>
+
+BEGIN_NCBI_SCOPE
+
+void SetDiagUserAndHost(TDiagUserAndHost flags)
+{
+    CDiagContext& ctx = GetDiagContext();
+    if ((flags & fDiag_AddUser) != 0  &&
+        ((flags & fDiag_OverrideExisting) != 0) ||
+        ctx.GetProperty("username").empty()) {
+        const int buf_len = 256;
+        char buf[buf_len];
+        CONNUTIL_GetUsername(buf, buf_len);
+        if ( *buf ) {
+            GetDiagContext().SetUsername(buf);
+        }
+    }
+    if ((flags & fDiag_AddHost) != 0  &&
+        ((flags & fDiag_OverrideExisting) != 0)  ||
+        ctx.GetProperty("hostname").empty()) {
+        const int buf_len = 256;
+        char buf[buf_len];
+        if ( SOCK_gethostname(buf, buf_len) == 0) {
+            GetDiagContext().SetHostname(buf);
+        }
+    }
+}
+
+
+END_NCBI_SCOPE
+
+
+/*
+ * ===========================================================================
+ *
+ * $Log$
+ * Revision 1.1  2006/01/30 19:55:27  grichenk
+ * Initial revision
+ *
+ *
+ * ===========================================================================
+ */
