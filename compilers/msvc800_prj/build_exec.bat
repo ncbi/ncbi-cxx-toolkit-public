09a39120d46a20cdaba496958529fbf50eb29af7
Vladimir Ivanov <ivanov@ncbi.nlm.nih.gov>
2006-02-08 19:07:56 +0000

    Removed 'command' argument from msbuild cmd line -- it does't support all needed commands, use 'build' by default
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@26632 78c7ea69-d796-4a43-9a09-de51944f1b03

--- compilers/msvc800_prj/build_exec.bat
+++ compilers/msvc800_prj/build_exec.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
-REM $Id: build_exec.bat,v 1.5 2006/02/08 16:47:24 ivanov Exp $
+REM $Id: build_exec.bat,v 1.6 2006/02/08 19:07:56 ivanov Exp $
 REM ===========================================================================
 REM 
 REM                            PUBLIC DOMAIN NOTICE
@@ -41,5 +41,5 @@ echo Usage: "%0 <solution> <command> <cfg> <target>"
 exit 1
 
 :be_build
-msbuild %1 /t:"%4:%2" /p:Configuration=%3 /v:d
+msbuild %1 /t:"%4" /p:Configuration=%3 /v:d
 
