c4373e0afe541c853525fd07cc9041b8edc2ded2
didenko <didenko@ncbi.nlm.nih.gov>
2006-03-21 18:59:20 +0000

    Added logic to discover missing services
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@27378 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/app/netcache/netcache_check.sh
+++ src/app/netcache/netcache_check.sh
@@ -4,23 +4,47 @@
 # Check netcached services
 
 services="
-   NC_FluAnalysis  \
-   NC_Geo          \
-   NC_gMap         \
-   NC_MapView      \
-   NC_PCAssayGraph \
-   NC_PhyloTree    \
-   NC_Sage         \
-   NC_Splign       \
-   NC_test         \
+   NC_FluAnalysis    \
+   NC_gds_kmeans     \
+   NC_Geo            \
+   NC_GeoSubmission  \
+   NC_gMap           \
+   NC_gProt          \
+   NC_HGRG_UniGene   \
+   NC_MapView        \
+   NC_ncgraph        \
+   NC_Pasc           \
+   NC_PascL          \
+   NC_PCAssayGraph   \
+   NC_PCSketcher     \
+   NC_PhyloTree      \
+   NC_ProtMap        \
+   NC_Sage           \
+   NC_Splign         \
+   NC_TaxCommonTree  \
+   NC_test           \
+   NC_TraceAssmV     \
+   NC_TraceViewer    \
+   NC_Reserve"
+
+hosts="
+   service0:9001   \
    service1:9001   \
    service2:9001   \
+   service3:9001   \
    service0:9005   \
    service3:9005   \
+   service1:9002   \
+   service2:9002   \
+   service1:9003   \
+   service2:9003   \
    service1:9009   \
    netcache:9009"
 
 # For Nagios test
+
+lbsmc='/opt/machine/lbsm/bin/lbsmc'
+lbsm_feedback='/opt/machine/lbsm/sbin/lbsm_feedback'
 
 stable_client="./netcache_check"
 send_nsca="/home/ivanov/nagios/root/opt/machine/nagios/bin/send_nsca"
@@ -62,10 +86,25 @@ n_ok=0
 n_err=0
 sum_list=''
 
-for service in $services ; do
+service_machines="service0 service1 service2 service3"
+mask="NC_.*"
+
+for s in $service_machines ; do
+    list=`$lbsmc -g $s 0 2>&1 | tail +7 | cut -f1 -d' ' | sed '/-/{x;q;}' | grep -s "$mask"`
+    test -n list  ||  list=""
+    wholelist=`echo $wholelist $list`
+done
+wholelist=`for s in $wholelist; do echo $s; done | sort | uniq`
+
+for s in $wholelist; do
+    echo " $services " | grep " $s " 2>&1 >/dev/null
+    test $? -ne 0 && missing_services=`echo $missing_services $s`
+done 
+
+for service in `echo $services $hosts`; do
    echo "-------------------------------------------------------"
    printf '%-40s' "Testing service '$service'"
-   $CHECK_EXEC netcache_check $service > $res_file 2>&1
+   $CHECK_EXEC ./netcache_check $service > $res_file 2>&1
    if test $? -eq 0 ; then
       echo ":OK"
       n_ok=`expr $n_ok + 1`
@@ -87,6 +126,7 @@ cat <<EOF
 
 SUCCEEDED:  $n_ok
 FAILED:     $n_err
+MISSING SERVICES: $missing_services
 
 EOF
 #echo "$sum_list" | sed 's/_SEPARATOR_/\
