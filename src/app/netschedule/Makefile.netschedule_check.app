4f5430ea5f487030af55b7958b0074dc02538c09
didenko <didenko@ncbi.nlm.nih.gov>
2006-03-28 16:13:53 +0000

    + netschedule_check.sh
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@27493 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/app/netschedule/Makefile.netschedule_check.app
+++ src/app/netschedule/Makefile.netschedule_check.app
@@ -5,7 +5,8 @@
 APP = netschedule_check
 SRC = netschedule_check
 
-
-
 LIB = xconnserv xconnect xutil xncbi
 LIBS = $(NETWORK_LIBS) $(DL_LIBS) $(ORIG_LIBS)
+
+CHECK_CMD  = netschedule_check.sh
+CHECK_COPY = netschedule_check.sh
---
+++ src/app/netschedule/netschedule_check.sh
@@ -0,0 +1,41 @@
+# $Id$
+#!/bin/sh
+#
+# Check netschedule services
+
+services="NS_test"
+hosts="service0:9051"
+
+res_file="/tmp/$0.$$"
+trap 'rm -f $res_file' 1 2 15
+
+n_ok=0
+n_err=0
+sum_list=''
+
+for service in `echo $services $hosts`; do
+   echo "-------------------------------------------------------"
+   printf '%-40s' "Testing service '$service'"
+   $CHECK_EXEC netschedule_check $service > $res_file 2>&1
+   if test $? -eq 0 ; then
+      echo ":OK"
+      n_ok=`expr $n_ok + 1`
+   else
+      echo ":ERR"
+      cat $res_file
+      n_err=`expr $n_err + 1`
+   fi
+done
+
+rm -f $res_file
+
+cat <<EOF
+
+*******************************************************
+
+SUCCEEDED:  $n_ok
+FAILED:     $n_err
+
+EOF
+
+exit $n_err
