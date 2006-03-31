66c48b86a3e5a5e5c64bd1b25e72051eed542079
Anatoliy Kuznetsov <kuznets@ncbi.nlm.nih.gov>
2006-03-31 16:56:57 +0000

    initial revision
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@27601 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ src/algo/align/splign/splign_cmdargs.cpp
@@ -0,0 +1,173 @@
+/* $Id$
+* ===========================================================================
+*
+*                            public DOMAIN NOTICE                          
+*               National Center for Biotechnology Information
+*                                                                          
+*  This software/database is a "United States Government Work" under the   
+*  terms of the United States Copyright Act.  It was written as part of    
+*  the author's official duties as a United States Government employee and 
+*  thus cannot be copyrighted.  This software/database is freely available 
+*  to the public for use. The National Library of Medicine and the U.S.    
+*  Government have not placed any restriction on its use or reproduction.  
+*                                                                          
+*  Although all reasonable efforts have been taken to ensure the accuracy  
+*  and reliability of the software and data, the NLM and the U.S.          
+*  Government do not and cannot warrant the performance or results that    
+*  may be obtained by using this software or data. The NLM and the U.S.    
+*  Government disclaim all warranties, express or implied, including       
+*  warranties of performance, merchantability or fitness for any particular
+*  purpose.                                                                
+*                                                                          
+*  Please cite the author in any work or product based on this material.
+*
+* ===========================================================================
+*
+* Author:  Yuri Kapustin
+*          Anatoliy Kuznetsov
+*
+* File Description:
+*   Splign command line argument utilities
+*/
+
+
+#include <ncbi_pch.hpp>
+
+#include <algo/align/splign/splign_cmdargs.hpp>
+#include <algo/align/nw/nw_spliced_aligner16.hpp>
+
+
+BEGIN_NCBI_SCOPE
+
+
+const char kQuality_high[] = "high";
+const char kQuality_low[] = "low";
+
+const char kStrandPlus[] = "plus";
+const char kStrandMinus[] = "minus";
+const char kStrandBoth[] = "both";
+const char kStrandAuto[] = "auto";
+
+
+
+void CSplignArgUtil::SetupArgDescriptions(CArgDescriptions* argdescr)
+{
+    argdescr->AddFlag("cross", "Cross-species mode");
+
+    argdescr->AddDefaultKey
+        ("strand", "strand", "Spliced sequence's strand.",
+         CArgDescriptions::eString, kStrandPlus);
+    
+    argdescr->AddFlag ("noendgaps",
+                       "Skip detection of unaligning regions at the ends.",
+                       true);
+    
+    argdescr->AddFlag ("nopolya", "Assume no Poly(A) tail.",  true);
+    
+    argdescr->AddDefaultKey
+        ("compartment_penalty", "compartment_penalty",
+         "Penalty to open a new compartment "
+         "(compartment identification parameter). "
+         "Multiple compartments will only be identified if "
+         "they have at least this level of coverage.",
+         CArgDescriptions::eDouble,
+         NStr::DoubleToString(CSplign::s_GetDefaultCompartmentPenalty()));
+    
+    argdescr->AddDefaultKey
+        ("min_compartment_idty", "min_compartment_identity",
+         "Minimal compartment identity to align.",
+         CArgDescriptions::eDouble,
+         NStr::DoubleToString(CSplign::s_GetDefaultMinCompartmentIdty()));
+    
+    argdescr->AddOptionalKey
+        ("min_singleton_idty", "min_singleton_identity",
+         "Minimal singleton compartment identity to align. Singletons are "
+         "per subject and strand",
+         CArgDescriptions::eDouble);
+    
+    argdescr->AddDefaultKey
+        ("max_extent", "max_extent",
+         "Max genomic extent to look for exons beyond compartment boundaries "
+         "as determined with Blast hits.",
+         CArgDescriptions::eInteger,
+         NStr::IntToString(CSplign::s_GetDefaultMaxGenomicExtent()) );
+    
+    argdescr->AddDefaultKey
+        ("min_exon_idty", "identity",
+         "Minimal exon identity. Lower identity segments "
+         "will be marked as gaps.",
+         CArgDescriptions::eDouble,
+         NStr::DoubleToString(CSplign::s_GetDefaultMinExonIdty()));
+
+    argdescr->AddDefaultKey
+        ("Wm", "match", "match score",
+         CArgDescriptions::eInteger,
+         NStr::IntToString(CNWAligner::GetDefaultWm()).c_str());
+    
+    argdescr->AddDefaultKey
+        ("Wms", "mismatch", "mismatch score",
+         CArgDescriptions::eInteger,
+         NStr::IntToString(CNWAligner::GetDefaultWms()).c_str());
+    
+    argdescr->AddDefaultKey
+        ("Wg", "gap", "gap opening score",
+         CArgDescriptions::eInteger,
+         NStr::IntToString(CNWAligner::GetDefaultWg()).c_str());
+    
+    argdescr->AddDefaultKey
+        ("Ws", "space", "gap extension (space) score",
+         CArgDescriptions::eInteger,
+         NStr::IntToString(CNWAligner::GetDefaultWs()).c_str());
+    
+    argdescr->AddDefaultKey
+        ("Wi0", "Wi0", "Conventional intron (GT/AG) score",
+         CArgDescriptions::eInteger,
+         NStr::IntToString(CSplicedAligner16::GetDefaultWi(0)).c_str());
+    
+    argdescr->AddDefaultKey
+        ("Wi1", "Wi1", "Conventional intron (GC/AG) score",
+         CArgDescriptions::eInteger,
+         NStr::IntToString(CSplicedAligner16::GetDefaultWi(1)).c_str());
+    
+    argdescr->AddDefaultKey
+        ("Wi2", "Wi2", "Conventional intron (AT/AC) score",
+         CArgDescriptions::eInteger,
+         NStr::IntToString(CSplicedAligner16::GetDefaultWi(2)).c_str());
+    
+    argdescr->AddDefaultKey
+        ("Wi3", "Wi3", "Arbitrary intron score",
+         CArgDescriptions::eInteger,
+         NStr::IntToString(CSplicedAligner16::GetDefaultWi(3)).c_str());
+    
+    CArgAllow_Strings* constrain_strand = new CArgAllow_Strings;
+    constrain_strand->Allow(kStrandPlus)->Allow(kStrandMinus)
+        ->Allow(kStrandBoth)->Allow(kStrandAuto);
+    
+    argdescr->SetConstraint("strand", constrain_strand);
+    
+    CArgAllow* constrain01 = new CArgAllow_Doubles(0,1);
+    argdescr->SetConstraint("min_compartment_idty", constrain01);
+    argdescr->SetConstraint("min_exon_idty", constrain01);
+    argdescr->SetConstraint("compartment_penalty", constrain01);
+    
+    CArgAllow* constrain_positives = new CArgAllow_Integers(1, kMax_Int);
+    argdescr->SetConstraint("max_extent", constrain_positives);
+
+}
+
+
+
+END_NCBI_SCOPE
+
+
+/*
+ * ===========================================================================
+ *
+ * $Log$
+ * Revision 1.1  2006/03/31 16:56:57  kuznets
+ * initial revision
+ *
+ *
+ * ===========================================================================
+ */
+
