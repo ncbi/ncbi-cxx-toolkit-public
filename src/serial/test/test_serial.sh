e9f2daf5add9befd71c7c02ff1eb11b0459b52f0
Vladimir Ivanov <ivanov@ncbi.nlm.nih.gov>
2006-05-08 18:33:49 +0000

    Used diff instead or tr/cmp. Moved list of copying files to CHECK_COPY of Makefile.test_serial.app.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@28191 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/serial/test/test_serial.sh
+++ src/serial/test/test_serial.sh
@@ -7,10 +7,6 @@
 testprefix=c
 echo " $FEATURES " | grep " C-Toolkit " > /dev/null  ||  testprefix=cpp
 
-for f in webenv.ent webenv.bin ${testprefix}test_serial.asn ${testprefix}test_serial.asb ; do
-    cp $top_srcdir/src/serial/test/$f ./
-done
-
 mv ${testprefix}test_serial.asn test_serial.asn
 mv ${testprefix}test_serial.asb test_serial.asb
 
@@ -19,14 +15,11 @@ $CHECK_EXEC test_serial > test_serial.out 2> test_serial.err  ||  exit 1
 status=0
 
 for f in webenv.ent webenv.bin test_serial.asn test_serial.asb ; do
-    tr -d "\r\n " < ${f}  > ${f}.$$.tmp
-    tr -d "\r\n " < ${f}o > ${f}o.$$.tmp
-    cmp -s ${f}.$$.tmp ${f}o.$$.tmp
+    diff -w ${f} ${f}o
     if test $? -ne 0 ; then
         echo "Error in file ${f}o"
         status=1
     fi
-    rm ${f}.$$.tmp ${f}o.$$.tmp
 done
 
 test $status -eq 0  ||  exit 2
