81c0ff2992028e2aa440c031311a764ae172c629
papadopo <papadopo@ncbi.nlm.nih.gov>
2006-05-24 18:29:39 +0000

    1. Change megablast containment criteria so that HSPs must lie completely    inside higher-scoring HSPs 2. Since megablast and non-megablast containment tests are more similar    now, factor out common operations (and rearrange the tests so the cheapest    ones come first)
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@28478 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/algo/blast/core/blast_itree.c
+++ src/algo/blast/core/blast_itree.c
@@ -758,32 +758,30 @@ s_HSPIsContained(const BlastHSP *in_hsp,
     if (in_q_start != tree_q_start)
         return FALSE;
        
-    if (min_diag_separation > 0) {
-
-        if (MB_HSP_CONTAINED(in_hsp->query.offset, 
-                             tree_hsp->query.offset, tree_hsp->query.end,
-                             in_hsp->subject.offset, 
-                             tree_hsp->subject.offset, tree_hsp->subject.end,
-                             min_diag_separation) && 
-            in_hsp->score <= tree_hsp->score &&
-            SIGN(in_hsp->subject.frame) == SIGN(tree_hsp->subject.frame)) {
-            return TRUE;
-        }
-    } 
-    else if (CONTAINED_IN_HSP(tree_hsp->query.offset, tree_hsp->query.end, 
+    if (in_hsp->score <= tree_hsp->score &&
+        SIGN(in_hsp->subject.frame) == SIGN(tree_hsp->subject.frame) &&
+        CONTAINED_IN_HSP(tree_hsp->query.offset, tree_hsp->query.end, 
                               in_hsp->query.offset,
                               tree_hsp->subject.offset, tree_hsp->subject.end, 
-                              in_hsp->subject.offset)) {
-
-        if (CONTAINED_IN_HSP(tree_hsp->query.offset, tree_hsp->query.end, 
+                              in_hsp->subject.offset) &&
+        CONTAINED_IN_HSP(tree_hsp->query.offset, tree_hsp->query.end, 
                              in_hsp->query.end,
                              tree_hsp->subject.offset, tree_hsp->subject.end, 
-                             in_hsp->subject.end) &&
-            in_hsp->score <= tree_hsp->score &&
-            SIGN(in_hsp->subject.frame) == SIGN(tree_hsp->subject.frame)) {
+                             in_hsp->subject.end)) {
+
+        if (min_diag_separation == 0)
+            return TRUE;
+
+        if (MB_HSP_CLOSE(tree_hsp->query.offset, tree_hsp->subject.offset,
+                         in_hsp->query.offset, in_hsp->subject.offset,
+                         min_diag_separation) ||
+            MB_HSP_CLOSE(tree_hsp->query.end, tree_hsp->subject.end,
+                         in_hsp->query.end, in_hsp->subject.end,
+                         min_diag_separation)) {
             return TRUE;
         }
     }
+
     return FALSE;
 }
 
