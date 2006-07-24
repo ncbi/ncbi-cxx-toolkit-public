e30dfc499485e5210488f5695240ab8bb9129e54
Aaron Ucko <ucko@ncbi.nlm.nih.gov>
2006-07-24 14:00:19 +0000

    Include ncbiobj.hpp, not just ncbistd.hpp, for CRef<>.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@29309 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/algo/align/util/score_builder.hpp
+++ include/algo/align/util/score_builder.hpp
@@ -32,7 +32,7 @@
  *
  */
 
-#include <corelib/ncbistd.hpp>
+#include <corelib/ncbiobj.hpp>
 
 
 BEGIN_NCBI_SCOPE
@@ -80,6 +80,9 @@ END_NCBI_SCOPE
 /*
  * ===========================================================================
  * $Log$
+ * Revision 1.2  2006/07/24 14:00:19  ucko
+ * Include ncbiobj.hpp, not just ncbistd.hpp, for CRef<>.
+ *
  * Revision 1.1  2006/07/24 13:16:45  dicuccio
  * Added CScoreBuilder, a general class to provide support for generating scores
  * for alignments
