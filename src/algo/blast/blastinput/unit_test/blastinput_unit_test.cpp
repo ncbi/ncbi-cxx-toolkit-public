443bbb83561ab482c08e666d5bb7f1099e81d7aa
papadopo <papadopo@ncbi.nlm.nih.gov>
2006-08-30 18:03:40 +0000

    add protein fasta parsing unit tests
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@29848 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/algo/blast/blastinput/unit_test/blastinput_unit_test.cpp
+++ src/algo/blast/blastinput/unit_test/blastinput_unit_test.cpp
@@ -32,6 +32,11 @@
 
 #include <ncbi_pch.hpp>
 #include <objmgr/object_manager.hpp>
+#include <objects/seqloc/Seq_loc.hpp>
+#include <objects/seqloc/Seq_interval.hpp>
+#include <objects/seqloc/Seq_id.hpp>
+#include <objtools/readers/reader_exception.hpp>
+#include <algo/blast/api/sseqloc.hpp>
 #include <algo/blast/blastinput/blast_input.hpp>
 #include <algo/blast/blastinput/blast_fasta_input.hpp>
 
@@ -67,18 +72,92 @@ using boost::unit_test::test_suite;
 #define CHECK_EQUAL(x, y) CHECK_NO_THROW(BOOST_CHECK_EQUAL(x, y))
 #define CHECK_THROW(s, x) BOOST_CHECK_THROW(s, x)
 
-BOOST_AUTO_UNIT_TEST(s_ReadFasta)
-{
-    CRef<CObjectManager> om(CObjectManager::GetInstance());
-    CNcbiIfstream infile("data/aa.129295");
+#define DECLARE_SOURCE(file)                                    \
+    CRef<CObjectManager> om(CObjectManager::GetInstance());     \
+    CNcbiIfstream infile(file);                                 \
     CBlastFastaInputSource source(*om, infile);
 
+BOOST_AUTO_UNIT_TEST(s_ReadFastaProtein)
+{
+    DECLARE_SOURCE("data/aa.129295");
+
     CHECK(source.End() == false);
+    blast::SSeqLoc ssl = source.GetNextSSeqLoc();
+    CHECK(source.End() == true);
+
+    CHECK(ssl.seqloc->IsInt() == true);
+
+    CHECK(ssl.seqloc->GetInt().IsSetStrand() == true);
+    CHECK_EQUAL(eNa_strand_unknown, ssl.seqloc->GetInt().GetStrand());
+
+    CHECK(ssl.seqloc->GetInt().IsSetFrom() == true);
+    CHECK_EQUAL((TSeqPos)0, ssl.seqloc->GetInt().GetFrom());
+
+    CHECK(ssl.seqloc->GetInt().IsSetTo() == true);
+    CHECK_EQUAL((TSeqPos)231, ssl.seqloc->GetInt().GetTo());
+
+    CHECK(ssl.seqloc->GetInt().IsSetId() == true);
+    CHECK_EQUAL(CSeq_id::e_Local, ssl.seqloc->GetInt().GetId().Which());
+
+    CHECK(!ssl.mask);
+}
+
+BOOST_AUTO_UNIT_TEST(s_RangeBoth)
+{
+    DECLARE_SOURCE("data/aa.129295");
+
+    source.m_Config.SetFrom(50);
+    source.m_Config.SetTo(100);
+    blast::SSeqLoc ssl = source.GetNextSSeqLoc();
+
+    CHECK_EQUAL((TSeqPos)50, ssl.seqloc->GetInt().GetFrom());
+    CHECK_EQUAL((TSeqPos)100, ssl.seqloc->GetInt().GetTo());
+}
+
+BOOST_AUTO_UNIT_TEST(s_RangeStartOnly)
+{
+    DECLARE_SOURCE("data/aa.129295");
+
+    source.m_Config.SetFrom(50);
+    blast::SSeqLoc ssl = source.GetNextSSeqLoc();
+
+    CHECK_EQUAL((TSeqPos)50, ssl.seqloc->GetInt().GetFrom());
+    CHECK_EQUAL((TSeqPos)231, ssl.seqloc->GetInt().GetTo());
+}
+
+BOOST_AUTO_UNIT_TEST(s_RangeInvalid)
+{
+    DECLARE_SOURCE("data/aa.129295");
+
+    source.m_Config.SetFrom(100);
+    source.m_Config.SetTo(50);
+    CHECK_THROW(source.GetNextSSeqLoc(), CObjReaderException);
+}
+
+BOOST_AUTO_UNIT_TEST(s_ParseDefline)
+{
+    DECLARE_SOURCE("data/aa.129295");
+
+    source.m_Config.SetBelieveDeflines(true);
+    blast::SSeqLoc ssl = source.GetNextSSeqLoc();
+    CHECK_EQUAL(CSeq_id::e_Gi, ssl.seqloc->GetInt().GetId().Which());
+    CHECK_EQUAL(129295, ssl.seqloc->GetInt().GetId().GetGi());
+}
+
+BOOST_AUTO_UNIT_TEST(s_BadProtStrand)
+{
+    DECLARE_SOURCE("data/aa.129295");
+
+    source.m_Config.SetStrand(eNa_strand_both);
+    CHECK_THROW(source.GetNextSSeqLoc(), CObjReaderException);
 }
 
 /*
  * ===========================================================================
  * $Log$
+ * Revision 1.3  2006/08/30 18:03:40  papadopo
+ * add protein fasta parsing unit tests
+ *
  * Revision 1.2  2006/08/29 20:43:28  papadopo
  * add includes, add first test
  *
