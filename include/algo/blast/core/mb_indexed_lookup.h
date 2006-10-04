bffb0ca33c57c422478499d4a504d53316428af7
papadopo <papadopo@ncbi.nlm.nih.gov>
2006-10-04 19:29:28 +0000

    declarations for indexed database wordfinder
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@30449 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ include/algo/blast/core/mb_indexed_lookup.h
@@ -0,0 +1,90 @@
+/* $Id$
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Author:  Aleksandr Morgulis
+ *
+ */
+
+/** @file mb_indexed_lookup.h
+ * Declarations for functions that extract hits from indexed blast
+ * databases (specialized for megablast)
+ */
+
+#ifndef __MB_INDEXED_LOOKUP__
+#define __MB_INDEXED_LOOKUP__
+
+#include <algo/blast/core/blast_parameters.h>
+#include <algo/blast/core/blast_extend.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef _NCBISTD_       /* C++ toolkit only */
+/** Function used to collect ungapped alignments from a previously
+ * indexed nucleotide database. This is only used by the C++ API
+ * @param idb Pointer to object representing indexed database [in]
+ * @param oid Ordinal ID of the database sequence to scan [in]
+ * @param chunk Current chunk of the database sequence [in]
+ * @param init_hitlist Container for any hits found [out]
+ */
+extern void MB_IdbGetResults(void * idb, Int4 oid, Int4 chunk,
+                             BlastInitHitList * init_hitlist);
+#endif
+
+/** Finds all runs of a specified number of exact matches between 
+ * two nucleotide sequences. Assumes the subject sequence is part
+ * of a previously indexed database
+ * @param subject The subject sequence [in]
+ * @param query Ignored
+ * @param query_info Ignored
+ * @param lookup Pointer to the (wrapper) lookup table structure [in]
+ * @param matrix Ignored
+ * @param word_params Ignored
+ * @param ewp Ignored
+ * @param offset_pairs Ignored
+ * @param max_hits Ignored
+ * @param init_hitlist Structure to hold all hits information. Has to be 
+ *        allocated up front [out]
+ * @param ungapped_stats Ignored
+ */
+extern Int2 MB_IndexedWordFinder( 
+        BLAST_SequenceBlk * subject,
+        BLAST_SequenceBlk * query,
+        BlastQueryInfo * query_info,
+        LookupTableWrap * lookup_wrap,
+        Int4 ** matrix,
+        const BlastInitialWordParameters * word_params,
+        Blast_ExtendWord * ewp,
+        BlastOffsetPair * offset_pairs,
+        Int4 max_hits,
+        BlastInitHitList * init_hitlist,
+        BlastUngappedStats * ungapped_stats );
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MB_INDEXED_LOOKUP__ */
