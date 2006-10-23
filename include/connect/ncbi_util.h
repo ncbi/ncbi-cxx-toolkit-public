628b8da5916adc167457e4cf1733e3a3d2e4bda5
Anton Lavrentiev <lavr@ncbi.nlm.nih.gov>
2006-10-23 20:54:26 +0000

    Some inline doc formatting
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@30748 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/connect/ncbi_util.h
+++ include/connect/ncbi_util.h
@@ -28,8 +28,11 @@
  *
  * Author:  Denis Vakatov, Anton Lavrentiev
  *
+ * @file
  * File Description:
- *   Auxiliary (optional) core mostly to support "ncbi_core.[ch]"
+ *   Auxiliaries (mostly optional core to back and complement "ncbi_core.[ch]")
+ * @sa
+ *  ncbi_core.h
  *
  * 1. CORE support:
  *    macros:     LOG_Write(), LOG_Data(),
@@ -73,15 +76,19 @@ extern "C" {
  *  MT locking
  */
 
-/* Set the MT critical section lock/unlock handler -- to be used by the core
- * internals to protect internal static variables and other MT-sensitive
+/** Set the MT critical section lock/unlock handler -- to be used by the core
+ * internals for protection of internal static variables and other MT-sensitive
  * code from being accessed/changed by several threads simultaneously.
- * It is also to protect fully the core log handler, including its setting up
- * (see CORE_SetLOG below), and its callback and cleanup functions.
- * NOTES:
- * This function itself is so NOT MT-safe!
- * If there is an active MT-lock handler set already, and it is different from
- * the new one, then MT_LOCK_Delete() is called for the old(replaced) handler.
+ * It is also to fully protect the core log handler, including its setting up,
+ * and its callback and cleanup functions.
+ * @li <b>NOTES:</b>  This function itself is NOT MT-safe!
+ * @li <b>NOTES:</b>  If there is an active CORE MT-lock set already, which
+ *     is different from the new one, then MT_LOCK_Delete() is called for
+ *     the old lock (i.e. the one being replaced).
+ * @param lk
+ *  MT-Lock as created by MT_LOCK_Create
+ * @sa
+ *  MT_LOCK_Create, CORE_SetLOG
  */
 extern NCBI_XCONNECT_EXPORT void    CORE_SetLOCK(MT_LOCK lk);
 extern NCBI_XCONNECT_EXPORT MT_LOCK CORE_GetLOCK(void);
@@ -93,8 +100,10 @@ extern NCBI_XCONNECT_EXPORT MT_LOCK CORE_GetLOCK(void);
  */
 
 
-/* Slightly customized LOG_WriteInternal() -- to distinguish between
- * logging only a message vs. logging message with some data.
+/** Slightly customized LOG_WriteInternal() -- to distinguish between
+ * logging only a message vs logging a message with some data.
+ * @sa
+ *  LOG_WriteInternal
  */
 #define LOG_Write(lg,level,module,file,line,message) \
   (void) ((lg  ||  level == eLOG_Fatal) ? \
@@ -104,68 +113,99 @@ extern NCBI_XCONNECT_EXPORT MT_LOCK CORE_GetLOCK(void);
   (LOG_WriteInternal(lg,level,module,file,line,message,data,size), 0) : 1)
 
 
-/* Auxiliary plain macro to write message (maybe, with raw data) to the log
+/** Auxiliary plain macros to write message (maybe, with raw data) to the log.
+ * @sa
+ *   LOG_Write, LOG_Data
  */
-#define LOG_WRITE(lg, level, message) \
+#define  LOG_WRITE(lg, level, message) \
   LOG_Write(lg, level, THIS_MODULE, THIS_FILE, __LINE__, message)
 
-/* AIX's <pthread.h> defines LOG_DATA to be an integer constant; we must
-   explicitly drop such definitions to avoid trouble. */
-#ifdef LOG_DATA
+#ifdef   LOG_DATA
+/* AIX's <pthread.h> defines LOG_DATA to be an integer constant;
+   we must explicitly drop such definitions to avoid trouble */
 #  undef LOG_DATA
 #endif
-#define LOG_DATA(lg, data, size, message) \
+#define  LOG_DATA(lg, data, size, message) \
   LOG_Data(lg, eLOG_Trace, THIS_MODULE, THIS_FILE, __LINE__, \
            data, size, message)
 
 
-/* Defaults for the THIS_FILE and THIS_MODULE macros (used by LOG_WRITE)
+/** Defaults for the THIS_FILE and THIS_MODULE macros (used by LOG_WRITE).
  */
-#if !defined(THIS_FILE)
+#ifndef   THIS_FILE
 #  define THIS_FILE __FILE__
 #endif
 
-#if !defined(THIS_MODULE)
+#ifndef   THIS_MODULE
 #  define THIS_MODULE 0
 #endif
 
 
-/* Set the log handler (no logging if "lg" is passed zero) -- to be used by
+/** Set the log handle (no logging if "lg" is passed zero) -- to be used by
  * the core internals.
  * If there is an active log handler set already, and it is different from
- * the new one, then LOG_Delete() is called for the old(replaced) logger.
+ * the new one, then LOG_Delete is called for the old logger (that is,
+ * the one being replaced).
+ * @param lg
+ *  LOG handle as returned by LOG_Create, or NULL to stop logging
+ * @sa
+ *  LOG_Create, LOG_Delete, CORE_GetLOG
  */
 extern NCBI_XCONNECT_EXPORT void CORE_SetLOG(LOG lg);
+
+
+/** Get the log handle which is be used by the core internals.
+ *  @li <b>NOTES:</b>  You may not delete the handle (by means of LOG_Delete).
+ * @return
+ *  LOG handle as set by CORE_SetLOG or NULL if no logging is currently active
+ * @sa
+ *  CORE_SetLOG, LOG_Create, LOG_Delete
+ */
 extern NCBI_XCONNECT_EXPORT LOG  CORE_GetLOG(void);
 
 
-/* Standard logging to the specified file stream
+/** Standard logging to the specified file stream.
+ * @param fp
+ *  The file stream to log to
+ * @param auto_close
+ *  Do "fclose(fp)" when the LOG is reset/destroyed.
+ * @sa
+ *  CORE_SetLOG
  */
 extern NCBI_XCONNECT_EXPORT void CORE_SetLOGFILE
-(FILE*       fp,         /* the file stream to log to */
- int/*bool*/ auto_close  /* do "fclose(fp)" when the LOG is reset/destroyed */
+(FILE*       fp,
+ int/*bool*/ auto_close
  );
 
 
-/* CORE_SetLOGFILE(fopen(filename, "a"), TRUE)
- * Return zero on error, non-zero on success
+/** Same as CORE_SetLOGFILE(fopen(filename, "a"), TRUE).
+ * @param filename
+ *  Filename to write the log into
+ * @return
+ *  Return zero on error, non-zero on success
+ * @sa
+ *  CORE_SetLOG
  */
 extern NCBI_XCONNECT_EXPORT int/*bool*/ CORE_SetLOGFILE_NAME
-(const char* filename  /* log.-file name */
+(const char* filename
  );
 
 
+/** LOG formatting flags: what parts of the message to actually appear.
+ * @sa
+ *   CORE_SetLOGFormatFlags
+ */
 typedef enum {
-    fLOG_Default  = 0x0, /* "fLOG_Short" if NDEBUG, else "fLOG_Full"        */
+    fLOG_Default       = 0x0,    /**< fLOG_Short if NDEBUG, else fLOG_Full   */
 
-    fLOG_Level    = 0x1,
-    fLOG_Module   = 0x2,
-    fLOG_FileLine = 0x4, /* (must always be printed for "eLOG_Trace" level) */
-    fLOG_DateTime = 0x8,
-    fLOG_OmitNoteLevel = 0x4000,/* do not add "NOTE:" if eLOG_Note is level */
-    fLOG_None     = 0x8000 /* nothing but specified parts plus msg and data */
+    fLOG_Level         = 0x1,
+    fLOG_Module        = 0x2,
+    fLOG_FileLine      = 0x4,    /**< always here for eLOG_Trace level       */
+    fLOG_DateTime      = 0x8,
+    fLOG_OmitNoteLevel = 0x4000, /**< do not add NOTE if eLOG_Note is level  */
+    fLOG_None          = 0x8000  /**< nothing but spec'd parts, msg and data */
 } ELOG_Format;
-typedef unsigned int TLOG_FormatFlags;  /* binary OR of "ELOG_FormatFlags"  */
+typedef unsigned int TLOG_FormatFlags; /**< binary OR of "ELOG_FormatFlags"  */
 #define fLOG_Short  (fLOG_Level)
 #define fLOG_Full   (fLOG_Level | fLOG_Module | fLOG_FileLine)
 
@@ -174,44 +214,75 @@ extern NCBI_XCONNECT_EXPORT TLOG_FormatFlags CORE_SetLOGFormatFlags
 );
 
 
-/* Compose message using the "call_data" info.
- * Full format:
+/** Compose message using the "call_data" info.
+ * Full log record format:
  *     mm/dd/yy HH:MM:SS "<file>", line <line>: [<module>] <level>: <message>
  *     \n----- [BEGIN] Raw Data (<raw_size> bytes) -----\n
  *     <raw_data>
  *     \n----- [END] Raw Data -----\n
  *
- *
- * NOTE:  the returned string must be deallocated using "free()".
+ * @li <b>NOTE:</b>  the returned string must be deallocated using "free()".
+ * @param call_data
+ *  Parts of the message
+ * @param format_flags
+ *  Which fields of "call_data" to use
+ * @sa
+ *  CORE_SetLOG, CORE_SetLOGFormatFlags
  */
 extern NCBI_XCONNECT_EXPORT char* LOG_ComposeMessage
 (const SLOG_Handler* call_data,
- TLOG_FormatFlags    format_flags  /* which fields of "call_data" to use */
+ TLOG_FormatFlags    format_flags
  );
 
 
-/* LOG_Reset() specialized to log to a "FILE*" stream.
+/** LOG_Reset specialized to log to a "FILE*" stream using LOG_ComposeMessage.
+ * @param lg
+ *  Created by LOG_Create
+ * @param fp
+ *  The file stream to log to
+ * @param auto_close
+ *  Whether to do "fclose(fp)" when the LOG is reset/destroyed
+ * @sa
+ *  LOG_Create, LOG_Reset, LOG_ComposeMessage
  */
 extern NCBI_XCONNECT_EXPORT void LOG_ToFILE
-(LOG         lg,         /* created by LOG_Create() */
- FILE*       fp,         /* the file stream to log to */
- int/*bool*/ auto_close  /* do "fclose(fp)" when the LOG is reset/destroyed */
+(LOG         lg,
+ FILE*       fp,
+ int/*bool*/ auto_close
  );
 
 
-/* Add current "errno" (and maybe its description) to the message:
- *   <message> {errno=<errno>,<descr>}
- * Return "buf".
+/** Add current "errno" (and maybe its description) to the message:
+ * <message> {errno=<errno>,<descr>}
+ * @param message
+ *  [in]  message text (can be NULL)
+ * @param x_errno
+ *  [in]  error code (if it is zero, then use "descr" only)
+ * @param descr
+ *  [in]  error description (if NULL, then use "strerror(x_errno)")
+ * @param buf
+ *  [out] buffer to put the composed message to
+ * @param buf_size
+ *  [in]  maximal buffer size
+ * @return
+ *  Return "buf"
+ * @sa
+ *  LOG_ComposeMessage
  */
 extern NCBI_XCONNECT_EXPORT char* MessagePlusErrno
-(const char*  message,  /* [in]  message (can be NULL) */
- int          x_errno,  /* [in]  error code (if it's zero, use "descr" only) */
- const char*  descr,    /* [in]  if NULL, then use "strerror(x_errno)" */
- char*        buf,      /* [out] buffer to put the composed message to */
- size_t       buf_size  /* [in]  max. buffer size */
+(const char*  message,
+ int          x_errno,
+ const char*  descr,
+ char*        buf,
+ size_t       buf_size
  );
 
 
+/** Special log writing macro that makes use of a specified error number,
+ * and an optional description thereof.
+ * @sa
+ *  LOG_WRITE_ERRNO
+ */
 #define LOG_WRITE_ERRNO_EX(lg, level, message, x_errno, x_descr)  do {   \
     if (lg  ||  level == eLOG_Fatal) {                                   \
         char _buf[1024];                                                 \
@@ -221,6 +292,10 @@ extern NCBI_XCONNECT_EXPORT char* MessagePlusErrno
 } while (0)
 
 
+/** Variant of LOG_WRITE_ERRNO_EX that uses current errno.
+ * @sa
+ *   LOG_WRITE_ERRNO_EX
+ */
 #define LOG_WRITE_ERRNO(lg, level, message)                              \
      LOG_WRITE_ERRNO_EX(lg, level, message, errno, 0)
 
@@ -230,10 +305,12 @@ extern NCBI_XCONNECT_EXPORT char* MessagePlusErrno
  *  REGISTRY
  */
 
-/* Set the registry (no registry if "rg" is passed zero) -- to be used by
+/** Set the registry (no registry if "rg" is passed zero) -- to be used by
  * the core internals.
  * If there is an active registry set already, and it is different from
  * the new one, then REG_Delete() is called for the old(replaced) registry.
+ * @param rg
+ *  
  */
 extern NCBI_XCONNECT_EXPORT void CORE_SetREG(REG rg);
 extern NCBI_XCONNECT_EXPORT REG  CORE_GetREG(void);
@@ -244,21 +321,28 @@ extern NCBI_XCONNECT_EXPORT REG  CORE_GetREG(void);
  *  Auxiliary API
  */
 
-/* Return read-only textual but machine-readable platform description.
+/**
+ * @return
+ *  Return read-only textual but machine-readable platform description.
  */
 extern NCBI_XCONNECT_EXPORT const char* CORE_GetPlatform(void);
 
 
-/* Obtain and store current user's name in the buffer provided.
- * Return 0 when the user name cannot be determined.
- * Otherwise, return "buf".
+/** Obtain and store current user's name in the buffer provided.
  * Note that resultant strlen(buf) is always guaranteed to be less
  * than "bufsize", extra non-fit characters discarded.
  * Both "buf" and "bufsize" must not be zeros.
+ * @param buf
+ *  Pointer to buffer to store the user name at
+ * @param bufsize
+ *  Size of buffer in bytes
+ * @return
+ *  Return 0 when the user name cannot be determined.
+ *  Otherwise, return "buf".
  */
 extern NCBI_XCONNECT_EXPORT const char* CORE_GetUsername
-(char*        buf,       /* Pointer to buffer to store the user name at */
- size_t       bufsize    /* Size of buffer in bytes                     */
+(char*        buf,
+ size_t       bufsize
  );
 
 
@@ -273,14 +357,21 @@ extern NCBI_XCONNECT_EXPORT size_t CORE_GetVMPageSize(void);
  *  CRC32
  */
 
-/* Calculate/Update CRC32
- * Return the checksum updated according to the contents of the block
- * pointed to by "ptr" and having "count" bytes in it.
+/** Calculate/Update CRC32
+ * @param checksum
+ *  Checksum to update (start with 0)
+ * @param ptr
+ *  Block of data
+ * @param count
+ *  Size of data
+ * @return
+ *  Return the checksum updated according to the contents of the block
+ *  pointed to by "ptr" and having "count" bytes in it.
  */
 extern NCBI_XCONNECT_EXPORT unsigned int CRC32_Update
-(unsigned int checksum,  /* Checksum to update (start with 0) */
- const void*  ptr,       /* Block of data                     */
- size_t       count      /* Size of data                      */
+(unsigned int checksum,
+ const void*  ptr,
+ size_t       count
  );
 
 
@@ -288,14 +379,26 @@ extern NCBI_XCONNECT_EXPORT unsigned int CRC32_Update
 /******************************************************************************
  *  Miscellanea
  */
-
+/**
+ * @param name
+ *  TODO!
+ * @param mask
+ *  TODO!
+ * @param ignore_case
+ *  TODO!
+ */
 extern NCBI_XCONNECT_EXPORT int/*bool*/ UTIL_MatchesMaskEx
 (const char* name,
  const char* mask,
  int/*bool*/ ignore_case
 );
 
-/* Same as UTIL_MatchesMaskEx(name, mask, 1) */
+/** Same as UTIL_MatchesMaskEx(name, mask, 1)
+ * @param name
+ *  TODO!
+ * @param mask
+ *  TODO!
+*/
 extern NCBI_XCONNECT_EXPORT int/*bool*/ UTIL_MatchesMask
 (const char* name,
  const char* mask
@@ -313,6 +416,9 @@ extern NCBI_XCONNECT_EXPORT int/*bool*/ UTIL_MatchesMask
 /*
  * ---------------------------------------------------------------------------
  * $Log$
+ * Revision 6.25  2006/10/23 20:54:26  lavr
+ * Some inline doc formatting
+ *
  * Revision 6.24  2006/06/15 02:43:28  lavr
  * GetUsername, GetVMPageSize (got "CORE" prefix), CRC32 moved here
  *
