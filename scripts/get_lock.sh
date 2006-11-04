88359ad1fa51be6c2f2cf02c92266a3cb4d2a546
Aaron Ucko <ucko@ncbi.nlm.nih.gov>
2006-11-04 17:22:25 +0000

    If another process has the lock, wait a little while (up to 15 seconds) for it to become available rather than immediately aborting.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@30889 78c7ea69-d796-4a43-9a09-de51944f1b03

--- scripts/get_lock.sh
+++ scripts/get_lock.sh
@@ -11,7 +11,26 @@ you have received this message in error, delete `pwd`/$file and try again.
 EOF
 
 # echo no | mv -i "$file.$$" "$file" # Doesn't work on Solaris. :-/
-[ -f "$file" ] || mv "$file.$$" "$file" # Not atomic. :-/
+if [ -f "$file" ]; then
+    if [ x`echo -n` = x-n ]; then
+        n=''; c='\c'
+    else
+        n='-n'; c=''
+    fi
+    echo $n "Waiting for $file$c" >&2
+    for x in 1 2 3 4 5; do
+        sleep $x
+        [ -f "$file" ] || break
+        echo $n ".$c" >&2
+    done
+    echo >&2
+fi
+[ -f "$file" ] || mv "$file.$$" "$file" # Not (quite) atomic. :-/
+
+# Try to work around a weird race condition observed on Mac OS X 10.4
+if [ -f "$file.$$" ]; then
+    sleep 1
+fi
 
 if [ -f "$file.$$" ]; then
     cat "$file" >& 2
