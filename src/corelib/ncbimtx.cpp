e33f9d331ed7d6e11d09fc531c8771ad6b300f9c
Andrei Gourianov <gouriano@ncbi.nlm.nih.gov>
2006-11-20 17:39:18 +0000

    Workaround for ICC900
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@31175 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/corelib/ncbimtx.cpp
+++ src/corelib/ncbimtx.cpp
@@ -730,8 +730,17 @@ void CRWLock::Unlock(void)
         }
 #if defined(_DEBUG)
         // Check if the unlocking thread is in the owners list
+#if defined(NCBI_COMPILER_ICC) && NCBI_COMPILER_VERSION == 900
+	vector<CThreadSystemID>::iterator found = m_Readers.begin();
+	for ( ; found != m_Readers.end(); ++found) {
+	    if (*found == self_id) {
+	        break;
+	    }
+	}
+#else
         vector<CThreadSystemID>::iterator found =
             find(m_Readers.begin(), m_Readers.end(), self_id);
+#endif
         _ASSERT(found != m_Readers.end());
         m_Readers.erase(found);
         if ( m_Count == 0 ) {
@@ -1029,6 +1038,9 @@ END_NCBI_SCOPE
 /*
  * ===========================================================================
  * $Log$
+ * Revision 1.22  2006/11/20 17:39:18  gouriano
+ * Workaround for ICC900
+ *
  * Revision 1.21  2006/04/05 21:56:08  vakatov
  * Authors +=  Eugene Vasilchenko
  *
