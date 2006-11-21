610b99be802dc6a2de7cd8d0410a9364a334db08
papadopo <papadopo@ncbi.nlm.nih.gov>
2006-11-21 16:36:12 +0000

    remove all prototypes except the actual protein wordfinder
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@31215 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/algo/blast/core/aa_ungapped.h
+++ include/algo/blast/core/aa_ungapped.h
@@ -28,13 +28,16 @@
  * Protein ungapped extension code.
  */
 
-#ifndef AA_UNGAPPED__H
-#define AA_UNGAPPED__H
+#ifndef ALGO_BLAST_CORE__AA_UNGAPPED_H
+#define ALGO_BLAST_CORE__AA_UNGAPPED_H
 
-#include <algo/blast/core/blast_def.h>
-#include <algo/blast/core/blast_util.h>
+#include <algo/blast/core/ncbi_std.h>
 #include <algo/blast/core/blast_lookup.h>
+#include <algo/blast/core/blast_parameters.h>
 #include <algo/blast/core/blast_extend.h>
+#include <algo/blast/core/blast_hits.h>
+#include <algo/blast/core/lookup_wrap.h>
+#include <algo/blast/core/blast_diagnostics.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -55,306 +58,19 @@ extern "C" {
  * @param ungapped_stats Various hit counts. Not filled if NULL [out]
  */
 Int2 BlastAaWordFinder(BLAST_SequenceBlk* subject,
-		       BLAST_SequenceBlk* query,
+                       BLAST_SequenceBlk* query,
                        BlastQueryInfo* query_info,
-		       LookupTableWrap* lookup,
-		       Int4** matrix,
-		       const BlastInitialWordParameters* word_params,
-		       Blast_ExtendWord* ewp,
+                       LookupTableWrap* lookup,
+                       Int4** matrix,
+                       const BlastInitialWordParameters* word_params,
+                       Blast_ExtendWord* ewp,
                        BlastOffsetPair* NCBI_RESTRICT offset_pairs,
-		       Int4 offset_array_size,
-		       BlastInitHitList* init_hitlist, 
-             BlastUngappedStats* ungapped_stats);
-
-/** Scan a subject sequence for word hits and trigger two-hit extensions.
- *
- * @param subject the subject sequence [in]
- * @param query the query sequence [in]
- * @param lookup_wrap the lookup table [in]
- * @param ewp Structure containing the diagonal array
- * @param matrix the substitution matrix [in]
- * @param word_params structure containing per-context cutoff information [in]
- * @param query_info structure containing context ranges [in]
- * @param offset_pairs Array for storing query and subject offsets. [in]
- * @param array_size the number of elements in each offset array [in]
- * @param ungapped_hsps hsps resulting from the ungapped extension [out]
- * @param ungapped_stats Various hit counts. Not filled if NULL [out]
- */
-
-Int2 BlastAaWordFinder_TwoHit(const BLAST_SequenceBlk* subject,
-			      const BLAST_SequenceBlk* query,
-			      const LookupTableWrap* lookup_wrap,
-			      Blast_ExtendWord * ewp,
-			      Int4 ** matrix,
-			      const BlastInitialWordParameters * word_params,
-			      BlastQueryInfo * query_info,
-                              BlastOffsetPair* NCBI_RESTRICT offset_pairs,
-			      Int4 array_size,
-	                      BlastInitHitList* ungapped_hsps, 
-                              BlastUngappedStats* ungapped_stats);
-
-/** Scan a subject sequence for word hits and trigger two-hit extensions
- * (specialized for RSP blast).
- *
- * @param subject the subject sequence [in]
- * @param query the query sequence [in]
- * @param lookup_wrap the lookup table [in]
- * @param ewp Structure containing the diagonal array [in]
- * @param matrix the substitution matrix [in]
- * @param cutoff cutoff score for saving ungapped HSPs [in]
- * @param dropoff x dropoff [in]
- * @param ungapped_hsps hsps resulting from the ungapped extension [out]
- * @param ungapped_stats Various hit counts. Not filled if NULL [out]
- */
-Int2 BlastRPSWordFinder_TwoHit(const BLAST_SequenceBlk* subject,
-	 		       const BLAST_SequenceBlk* query,
-			       const LookupTableWrap* lookup_wrap,
-			       Blast_ExtendWord * ewp,
-			       Int4 ** matrix,
-			       Int4 cutoff,
-			       Int4 dropoff,
-	                       BlastInitHitList* ungapped_hsps, 
-                               BlastUngappedStats* ungapped_stats);
-
-/** Scan a subject sequence for word hits and trigger one-hit extensions.
- *
- * @param subject the subject sequence
- * @param query the query sequence
- * @param lookup_wrap the lookup table
- * @param ewp Structure containing the diagonal array [in]
- * @param matrix the substitution matrix [in]
- * @param word_params structure containing per-context cutoff information [in]
- * @param query_info structure containing context ranges [in]
- * @param offset_pairs Array for storing query and subject offsets. [in]
- * @param array_size the number of elements in each offset array
- * @param ungapped_hsps hsps resulting from the ungapped extensions [out]
- * @param ungapped_stats Various hit counts. Not filled if NULL [out]
- */
-Int2 BlastAaWordFinder_OneHit(const BLAST_SequenceBlk* subject,
-			      const BLAST_SequenceBlk* query,
-			      const LookupTableWrap* lookup_wrap,
-			      Blast_ExtendWord * ewp,
-			      Int4 ** matrix,
-			      const BlastInitialWordParameters * word_params,
-			      BlastQueryInfo * query_info,
-                              BlastOffsetPair* NCBI_RESTRICT offset_pairs,
-			      Int4 array_size,
-	            BlastInitHitList* ungapped_hsps, 
-               BlastUngappedStats* ungapped_stats);
-
-/** Scan a subject sequence for word hits and trigger one-hit extensions
- * (spcialized for RPS blast).
- *
- * @param subject the subject sequence
- * @param query the query sequence
- * @param lookup_wrap the lookup table
- * @param ewp Structure containing the diagonal array [in]
- * @param matrix the substitution matrix [in]
- * @param cutoff cutoff score for saving ungapped HSPs [in]
- * @param dropoff x dropoff [in]
- * @param ungapped_hsps hsps resulting from the ungapped extensions [out]
- * @param ungapped_stats Various hit counts. Not filled if NULL [out]
- */
-Int2 BlastRPSWordFinder_OneHit(const BLAST_SequenceBlk* subject,
-                               const BLAST_SequenceBlk* query,
-                               const LookupTableWrap* lookup_wrap,
-                               Blast_ExtendWord * ewp,
-                               Int4 ** matrix,
-                               Int4 cutoff,
-                               Int4 dropoff,
-                               BlastInitHitList* ungapped_hsps, 
-                               BlastUngappedStats* ungapped_stats);
-
-/**
- * Beginning at s_off and q_off in the subject and query, respectively,
- * extend to the right until the cumulative score becomes negative or
- * drops by at least 'dropoff', or the end of at least one sequence 
- * is reached.
- *
- * @param matrix the substitution matrix [in]
- * @param subject subject sequence [in]
- * @param query query sequence [in]
- * @param s_off subject offset [in]
- * @param q_off query offset [in]
- * @param dropoff the X dropoff parameter [in]
- * @param displacement the length of the extension [out]
- * @param maxscore the score derived from a previous left extension [in]
- * @param s_last_off the rightmost subject offset examined [out]
- * @return The score of the extension
- */
-Int4 BlastAaExtendRight(Int4 ** matrix,
-			const BLAST_SequenceBlk* subject,
-			const BLAST_SequenceBlk* query,
-			Int4 s_off,
-			Int4 q_off,
-			Int4 dropoff,
-			Int4* displacement,
-	                Int4 maxscore,
-	                Int4* s_last_off);
-
-/**
- * Identical to BlastAaExtendRight, except the score matrix
- * is position-specific
- *
- * @param matrix the substitution matrix representing query sequence [in]
- * @param subject subject sequence [in]
- * @param query_size number of rows in the scoring matrix [in]
- * @param s_off subject offset [in]
- * @param q_off query offset [in]
- * @param dropoff the X dropoff parameter [in]
- * @param displacement the length of the extension [out]
- * @param maxscore the score derived from a previous left extension [in]
- * @param s_last_off the rightmost subject offset examined [out]
- * @return The score of the extension
- */
-Int4 BlastPSSMExtendRight(Int4 ** matrix,
-			const BLAST_SequenceBlk* subject,
-			Int4 query_size,
-			Int4 s_off,
-			Int4 q_off,
-			Int4 dropoff,
-			Int4* displacement,
-	                Int4 maxscore,
-	                Int4* s_last_off);
-
-
-/**
- * Beginning at s_off and q_off in the subject and query, respectively,
- * extend to the left until the cumulative score drops by at least 
- * 'dropoff', or the end of at least one sequence is reached.
- *
- * @param matrix the substitution matrix [in]
- * @param subject subject sequence [in]
- * @param query query sequence [in]
- * @param s_off subject offset [in]
- * @param q_off query offset [in]
- * @param dropoff the X dropoff parameter [in]
- * @param displacement the length of the extension [out]
- * @param score the score so far (probably from initial word hit) [in]
- * @return The score of the extension
- */
-Int4 BlastAaExtendLeft(Int4 ** matrix,
-		       const BLAST_SequenceBlk* subject,
-		       const BLAST_SequenceBlk* query,
-		       Int4 s_off,
-		       Int4 q_off,
-		       Int4 dropoff,
-		       Int4* displacement,
-                       Int4 score);
-
-/**
- * Identical to BlastAaExtendLeft, except the query is 
- * represented by a position-specific score matrix.
- *
- * @param matrix the substitution matrix representing the query [in]
- * @param subject subject sequence [in]
- * @param s_off subject offset [in]
- * @param q_off query offset [in]
- * @param dropoff the X dropoff parameter [in]
- * @param displacement the length of the extension [out]
- * @param score the score so far (probably from initial word hit) [in]
- * @return The score of the extension
- */
-Int4 BlastPSSMExtendLeft(Int4 ** matrix,
-		       const BLAST_SequenceBlk* subject,
-		       Int4 s_off,
-		       Int4 q_off,
-		       Int4 dropoff,
-		       Int4* displacement,
-                       Int4 score);
-
-
-/** Perform a one-hit extension. Beginning at the specified hit,
- * extend to the left, then extend to the right. 
- *
- * @param matrix the substitution matrix [in]
- * @param subject subject sequence [in]
- * @param query query sequence [in]
- * @param s_off subject offset [in]
- * @param q_off query offset [in]
- * @param dropoff X dropoff parameter [in]
- * @param hsp_q the offset in the query where the HSP begins [out]
- * @param hsp_s the offset in the subject where the HSP begins [out]
- * @param hsp_len the length of the HSP [out]
- * @param word_size number of letters in the initial word hit [in]
- * @param use_pssm TRUE if the scoring matrix is position-specific [in]
- * @param s_last_off the rightmost subject offset examined [out]
- * @return the score of the hsp.
- */
-
-Int4 BlastAaExtendOneHit(Int4 ** matrix,
-	                 const BLAST_SequenceBlk* subject,
-	                 const BLAST_SequenceBlk* query,
-	                 Int4 s_off,
-	                 Int4 q_off,
-	                 Int4 dropoff,
-			 Int4* hsp_q,
-			 Int4* hsp_s,
-			 Int4* hsp_len,
-                         Int4 word_size,
-	                 Boolean use_pssm,
-	                 Int4* s_last_off);
-	                 
-/** Perform a two-hit extension. Given two hits L and R, begin
- * at R and extend to the left. If we do not reach L, abort the extension.
- * Otherwise, begin at R and extend to the right.
- *
- * @param matrix the substitution matrix [in]
- * @param subject subject sequence [in]
- * @param query query sequence [in]
- * @param s_left_off left subject offset [in]
- * @param s_right_off right subject offset [in]
- * @param q_right_off right query offset [in]
- * @param dropoff X dropoff parameter [in]
- * @param hsp_q the offset in the query where the HSP begins [out]
- * @param hsp_s the offset in the subject where the HSP begins [out]
- * @param hsp_len the length of the HSP [out]
- * @param use_pssm TRUE if the scoring matrix is position-specific [in]
- * @param word_size number of letters in one word [in]
- * @param right_extend set to TRUE if an extension to the right happened [out]
- * @param s_last_off the rightmost subject offset examined [out]
- * @return the score of the hsp.
- */
-
-Int4 BlastAaExtendTwoHit(Int4 ** matrix,
-	                 const BLAST_SequenceBlk* subject,
-	                 const BLAST_SequenceBlk* query,
-	                 Int4 s_left_off,
-	                 Int4 s_right_off,
-	                 Int4 q_right_off,
-	                 Int4 dropoff,
-			 Int4* hsp_q,
-			 Int4* hsp_s,
-			 Int4* hsp_len,
-	                 Boolean use_pssm,
-	                 Int4 word_size,
-	                 Boolean *right_extend,
-	                 Int4* s_last_off);
-
-/** Allocates memory for the BLAST_DiagTable*. This function also 
- * sets many of the parametes such as diag_array_length etc.
- * @param qlen Length of the query [in]
- * @param multiple_hits Specifies whether multiple hits method is used [in]
- * @param window_size The max. distance between two hits that are extended [in]
- * @return The allocated BLAST_DiagTable structure
-*/
-BLAST_DiagTable*
-BlastDiagTableNew (Int4 qlen, Boolean multiple_hits, Int4 window_size);
-
-/** Deallocate memory for the diagonal table structure 
- * @param diag_table the object to be freed [in]
- * @return NULL
-*/
-BLAST_DiagTable*
-BlastDiagTableFree(BLAST_DiagTable* diag_table);
-
-/** Reset the diagonal array structure. Used when offset has wrapped around.
- * @param diag pointer to the diagonal array structure [in]
- */
-Int4 BlastDiagClear(BLAST_DiagTable* diag);
+                       Int4 offset_array_size,
+                       BlastInitHitList* init_hitlist, 
+                       BlastUngappedStats* ungapped_stats);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif /* AA_UNGAPPED__H */
+#endif /* !ALGO_BLAST_CORE__AA_UNGAPPED_H */
