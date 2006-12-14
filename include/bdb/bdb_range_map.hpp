363c6da024869411581fce3dd19aaf0f15668060
Anatoliy Kuznetsov <kuznets@ncbi.nlm.nih.gov>
2006-12-14 17:22:47 +0000

    Added implementation
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@31652 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/bdb/bdb_range_map.hpp
+++ include/bdb/bdb_range_map.hpp
@@ -35,11 +35,14 @@
 /// @file bdb_range_map.hpp
 /// BDB range map storage
 
+#include <corelib/ncbistd.hpp>
+#include <corelib/ncbiexpt.hpp>
+
 #include <bdb/bdb_blob.hpp>
 #include <bdb/bdb_cursor.hpp>
 
 #include <util/bitset/ncbi_bitset.hpp>
-#include <util/bitset/bmserial.h>
+#include <util/bitset/ncbi_bitset_util.hpp>
 
 
 BEGIN_NCBI_SCOPE
@@ -52,6 +55,8 @@ BEGIN_NCBI_SCOPE
 /// The free list can be used externally to do id assignments, so ids fall into some
 /// predefined regions of the mapped list
 ///
+/// Range 0-0 is RESERVED for the free list storage.
+///
 /// <pre>
 /// Example:
 ///    10-15 --> 100-115
@@ -62,11 +67,11 @@ BEGIN_NCBI_SCOPE
 /// Typical use case: add a range of ids and take them out excluding 
 /// out of free list. When the free list is empty - add a new range.
 ///
-template<class BV>
+template<class TBV>
 class CBDB_RangeMap : public CBDB_BLobFile
 {
 public: 
-    typedef  BV  TBitVector;
+    typedef  TBV  TBitVector;
 
 public:
     CBDB_FieldUint4  FromId;  ///< From id
@@ -74,6 +79,9 @@ public:
 public:
     CBDB_RangeMap();
     ~CBDB_RangeMap();
+
+    /// Load free list (Storage should be open first)
+    void LoadFreeList();
 
     /// Save free list
     Save();
@@ -84,22 +92,218 @@ public:
                   bool add_to_free_list);
 
     /// Get list of free ids.
-    const TBitVector& GetFreeList() const;
-    TBitVector& GetFreeList();
+    const TBitVector& GetFreeList() const { return *m_FreeList; }
+    TBitVector& GetFreeList() { return *m_FreeList; }
 
     /// Remap one single id
+    ///
+    /// @return Remapped id or 0 if remapping is impossible
+    ///
     unsigned Remap(unsigned id, bool ignore_free_list=false);
     
     /// Remap a bitvector into another bitvector
-    /// Method scans for available ranges, do a remapping (where it can), clears bits in
-    /// the src. bit-vector (for successfully remapped ids)
     ///
-    void Remap(TBitVector& bv_src, TBitVector& bv_dst, bool ignore_free_list=false);
-
+    /// @param bv_src
+    ///     Source bit vector
+    /// @param bv_dst
+    ///     Destination set
+    /// @param bv_remapped
+    ///     Set of successfully remapped ids
+    ///
+    void Remap(const TBitVector& bv_src, 
+               TBitVector*       bv_dst, 
+               TBitVector*       bv_remapped,
+               bool              ignore_free_list=false);
 protected:
     TBitVector*   m_FreeList;
 };
 
+
+
+/////////////////////////////////////////////////////////////////////////////
+//  IMPLEMENTATION of INLINE functions
+/////////////////////////////////////////////////////////////////////////////
+
+template<class TBV>
+CBDB_RangeMap<TBV>::CBDB_RangeMap()
+    : m_FreeList(new TBitVector(BM_GAP)
+{
+    this->BindKey("FromId", &FromId);
+    this->BindKey("ToId",   &ToId);
+}
+
+template<class TBV>
+CBDB_RangeMap<TBV>::~CBDB_RangeMap()
+{
+    try {
+        Save();
+    } 
+    catch (exception&)
+    {
+    }
+    catch (CException&)
+    {
+    }
+
+    delete m_FreeList;
+}
+
+template<class TBV>
+void CBDB_RangeMap<TBV>::LoadFreeList()
+{
+    _ASSERT(m_FreeList);
+
+    TBuffer buf;
+    this->FromId = 0;
+    this->ToId = 0;
+    EBDB_ErrCode err = CBDB_BLobFile::ReadRealloc(buf);
+    if (err == eBDB_Ok) {
+        bm::deserialize(*m_FreeList, buf);
+    } else {
+        m_FreeList->clear(true);
+    }
+}
+
+template<class TBV>
+void CBDB_RangeMap<TBV>::Save()
+{
+    _ASSERT(m_FreeList);
+
+    m_FreeList->optimize();
+    TBuffer buf;
+    BV_Serialize(*m_FreeList, buf);
+
+    return UpdateInsert(buf);
+}
+
+template<class TBV>
+void CBDB_RangeMap<TBV>::AddRange(unsigned  from, 
+                                  unsigned  to, 
+                                  unsigned  dest, 
+                                  bool      add_to_free_list)
+{
+    if (from == 0 && to == 0) {
+        BDB_THROW(eInvalidValue, "0-0 range is reserved.")
+    }
+    if (from > to) {
+        BDB_THROW(eInvalidValue, "Incorrect range values.")
+    }
+
+    {{
+    CBDB_FileCursor cur(*this);
+
+    // check range overlaps
+    //
+    cur.SetCondition(CBDB_FileCursor::eLE);
+    cur.From << from;
+    EBDB_ErrCode ret = cur.FetchFirst();
+    if (ret == eBDB_Ok) {
+        unsigned from_db = this->FromId;
+        unsigned to_db = this->ToId;
+
+        //  from <= from1 <= to   ?
+        //
+        if (from >= from_db && from <= to_db) {
+            BDB_THROW(eInvalidValue, 
+                      "Range overlaps with the previous range.");
+        }
+    }
+
+    cur.SetCondition(CBDB_FileCursor::eGE);
+    cur.From << to;
+    EBDB_ErrCode ret = cur.FetchFirst();
+    if (ret == eBDB_Ok) {
+        unsigned from_db = this->FromId;
+        unsigned to_db = this->ToId;
+
+        //  from <= to1 <= to   ?
+        //
+        if (to >= from_db && to <= to_db) {
+            BDB_THROW(eInvalidValue, 
+                      "Range overlaps with the next range.");
+        }
+    }
+    }}
+
+    this->FromId = from;
+    this->ToId = to;
+
+    EBDB_ErrCode ret = Insert(&dest, sizeof(dest));
+
+    if (ret == eBDB_Ok) {
+        if (add_to_free_list) {
+            m_FreeList->set_range(from, to, true);
+        }
+    }
+    return ret;
+}
+
+template<class TBV>
+unsigned CBDB_RangeMap<TBV>::Remap(unsigned id, bool ignore_free_list)
+{
+    if (!ignore_free_list) {
+        if (m_FreeList->test(id)) {
+            return 0;
+        }
+    }
+    CBDB_FileCursor cur(*this);
+
+    // check range overlaps
+    //
+    cur.SetCondition(CBDB_FileCursor::eLE);
+    cur.From << id;
+    unsigned dst;
+    void* ptr = &dst;
+    EBDB_ErrCode ret = cur.FetchFirst(&ptr, sizeof(dst), eReallocForbidden);
+    if (ret == eBDB_Ok) {
+        unsigned from_id = this->FromId;
+        unsigned to_id = this->ToId;
+        if (from_id <= id && id <= to_id) {
+            return dst + (id - from_id);
+        }
+    }
+    return 0;
+}
+
+template<class TBV>
+void CBDB_RangeMap<TBV>::Remap(const TBitVector& bv_src, 
+                               TBitVector*       bv_dst, 
+                               TBitVector*       bv_remapped,
+                               bool              ignore_free_list)
+{
+    CBDB_FileCursor cur(*this);
+    unsigned from_id, to_id, dst;
+    from_id = to_id = dst = 0;
+
+    typename TBV::enumerator en(bv_src.first());
+    for ( ;en.valid(); ++en) {
+        unsigned id = *en;
+        if (!ignore_free_list) {
+            if (m_FreeList->test(id)) {
+                continue;
+            }
+        }
+        
+        if (from_id <= id && id <= to_id) {
+            bv_dst->set(dst + (id - from_id));
+            bv_remapped && bv_remapped->set(id);
+        } else {
+            cur.SetCondition(CBDB_FileCursor::eLE);
+            cur.From << id;
+            void* ptr = &dst;
+            EBDB_ErrCode ret = 
+                cur.FetchFirst(&ptr, sizeof(dst), eReallocForbidden);
+            if (ret == eBDB_Ok) {
+                from_id = this->FromId;
+                to_id = this->ToId;
+                if (from_id <= id && id <= to_id) {
+                    bv_dst->set(dst + (id - from_id));
+                    bv_remapped && bv_remapped->set(id);
+                }
+            }
+        }
+    } // for
+}
 
 END_NCBI_SCOPE
 
@@ -107,9 +311,11 @@ END_NCBI_SCOPE
 /*
  * ===========================================================================
  * $Log$
+ * Revision 1.2  2006/12/14 17:22:47  kuznets
+ * Added implementation
+ *
  * Revision 1.1  2006/12/12 21:56:14  kuznets
  * initial revision
- *
  *
  * ===========================================================================
  */
