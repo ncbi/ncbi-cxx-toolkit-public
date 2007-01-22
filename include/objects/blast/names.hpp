5d97f9cdde13d4d99e87bb1e3f03c67b0988bc79
bealer <bealer@ncbi.nlm.nih.gov>
2007-01-22 19:49:39 +0000

    - CBlast4Fields class:   . Better documentation.   . Move all implementations to cpp file.   . Add Match() function to verify parameter against field.   . Add match-checking value getters for Blast4_value types.   . KnownField() method to check whether a field name is known.
    - Add seperate fields for structured filtering options.
    
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@32189 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/objects/blast/names.hpp
+++ include/objects/blast/names.hpp
@@ -44,32 +44,75 @@
 #include <corelib/ncbistl.hpp>
 #include <objects/blast/NCBI_Blast4_module.hpp>
 #include <objects/blast/Blast4_value.hpp>
+#include <objects/blast/Blast4_parameter.hpp>
 
 BEGIN_NCBI_SCOPE
 
 BEGIN_objects_SCOPE // namespace ncbi::objects::
 
+/// Field properties for options in a Blast4 parameter list.
 class CBlast4Field {
 public:
+    /// Default constructor (for STL)
     CBlast4Field();
     
+    /// Construct field with name and type.
     CBlast4Field(std::string nm, CBlast4_value::E_Choice ch);
     
-    const string & GetName() const
-    {
-        return m_Name;
-    }
+    /// Get field name (key).
+    const string & GetName() const;
     
-    CBlast4_value::E_Choice GetType() const
-    {
-        return m_Type;
-    }
+    /// Get field type.
+    CBlast4_value::E_Choice GetType() const;
+    
+    /// Match field name and type to parameter.
+    bool Match(CBlast4_parameter & p) const;
+    
+    /// Verify parameter name and type, and get boolean value.
+    bool GetBoolean(CBlast4_parameter & p) const;
+    
+    /// Verify parameter name and type, and get big integer value.
+    Int8 GetBig_integer(CBlast4_parameter & p) const;
+    
+    /// Verify parameter name and type, and get cutoff value.
+    CRef<CBlast4_cutoff> GetCutoff(CBlast4_parameter & p) const;
+    
+    /// Verify parameter name and type, and get integer value.
+    int GetInteger(CBlast4_parameter & p) const;
+    
+    /// Verify parameter name and type, and get integer list value.
+    list<int> GetIntegerList(CBlast4_parameter & p) const;
+    
+    /// Verify parameter name and type, and get matrix (pssm) value.
+    CRef<CPssmWithParameters> GetMatrix(CBlast4_parameter & p) const;
+    
+    /// Verify parameter name and type, and get query mask value.
+    CRef<CBlast4_mask> GetQueryMask(CBlast4_parameter & p) const;
+    
+    /// Verify parameter name and type, and get real value.
+    double GetReal(CBlast4_parameter & p) const;
+    
+    /// Verify parameter name and type, and get strand_type value.
+    EBlast4_strand_type GetStrandType(CBlast4_parameter & p) const;
+    
+    /// Verify parameter name and type, and get string value.
+    string GetString(CBlast4_parameter & p) const;
+    
+    /// Check whether a field of the given name exists.
+    static bool KnownField(string name);
     
 private:
-    string                  m_Name;
+    /// Field name string as used in Blast4_parameter objects.
+    string m_Name;
+    
+    /// Field value type as used in Blast4_parameter objects.
     CBlast4_value::E_Choice m_Type;
     
-    static map<string, CBlast4Field> m_Fields;
+    /// Type for map of all blast4 field objects.
+    typedef map<string, CBlast4Field> TFieldMap;
+    
+    /// Map of all blast4 field objects.
+    static TFieldMap m_Fields;
 };
 
 extern CBlast4Field B4Param_CompositionBasedStats;
@@ -77,6 +120,7 @@ extern CBlast4Field B4Param_Culling;
 extern CBlast4Field B4Param_CutoffScore;
 extern CBlast4Field B4Param_DbGeneticCode;
 extern CBlast4Field B4Param_DbLength;
+extern CBlast4Field B4Param_DustFiltering;
 extern CBlast4Field B4Param_EffectiveSearchSpace;
 extern CBlast4Field B4Param_EntrezQuery;
 extern CBlast4Field B4Param_EvalueThreshold;
@@ -90,6 +134,7 @@ extern CBlast4Field B4Param_HitlistSize;
 extern CBlast4Field B4Param_HspRangeMax;
 extern CBlast4Field B4Param_InclusionThreshold;
 extern CBlast4Field B4Param_LCaseMask;
+extern CBlast4Field B4Param_MaskAtHash;
 extern CBlast4Field B4Param_MatchReward;
 extern CBlast4Field B4Param_MatrixName;
 extern CBlast4Field B4Param_MatrixTable;
@@ -98,12 +143,15 @@ extern CBlast4Field B4Param_PercentIdentity;
 extern CBlast4Field B4Param_PHIPattern;
 extern CBlast4Field B4Param_PseudoCountWeight;
 extern CBlast4Field B4Param_QueryGeneticCode;
+extern CBlast4Field B4Param_RepeatFiltering;
+extern CBlast4Field B4Param_RepeatFilteringDB;
 extern CBlast4Field B4Param_RequiredEnd;
 extern CBlast4Field B4Param_RequiredStart;
 extern CBlast4Field B4Param_StrandOption;
 extern CBlast4Field B4Param_MBTemplateLength;
 extern CBlast4Field B4Param_MBTemplateType;
 extern CBlast4Field B4Param_OutOfFrameMode;
+extern CBlast4Field B4Param_SegFiltering;
 extern CBlast4Field B4Param_UngappedMode;
 extern CBlast4Field B4Param_UseRealDbSize;
 extern CBlast4Field B4Param_WindowSize;
