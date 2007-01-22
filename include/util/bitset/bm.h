e6d7b4ebb252bb7f9229ae2f9ff251876ec723b5
Victor Joukov <joukovv@ncbi.nlm.nih.gov>
2007-01-22 21:35:42 +0000

    Cosmetics in License
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@32193 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/util/bitset/bm.h
+++ include/util/bitset/bm.h
@@ -1,8 +1,7 @@
 /*
 Copyright(c) 2002-2005 Anatoliy Kuznetsov(anatoliy_kuznetsov at yahoo.com)
 
-PermiFor more information please visit:  http://bmagic.sourceforge.net
-ssion is hereby granted, free of charge, to any person 
+Permission is hereby granted, free of charge, to any person 
 obtaining a copy of this software and associated documentation 
 files (the "Software"), to deal in the Software without restriction, 
 including without limitation the rights to use, copy, modify, merge, 
