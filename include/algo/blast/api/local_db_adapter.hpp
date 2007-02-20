91afc7342c4d3250e087e6df378f2de6def2cd75
Christiam Camacho <camacho@ncbi.nlm.nih.gov>
2007-02-20 21:28:02 +0000

    Initial revision
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@32615 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ include/algo/blast/api/local_db_adapter.hpp
@@ -0,0 +1,147 @@
+/* $Id$
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE                          
+ *               National Center for Biotechnology Information
+ *                                                                          
+ *  This software/database is a "United States Government Work" under the   
+ *  terms of the United States Copyright Act.  It was written as part of    
+ *  the author's official duties as a United States Government employee and 
+ *  thus cannot be copyrighted.  This software/database is freely available 
+ *  to the public for use. The National Library of Medicine and the U.S.    
+ *  Government have not placed any restriction on its use or reproduction.  
+ *                                                                          
+ *  Although all reasonable efforts have been taken to ensure the accuracy  
+ *  and reliability of the software and data, the NLM and the U.S.          
+ *  Government do not and cannot warrant the performance or results that    
+ *  may be obtained by using this software or data. The NLM and the U.S.    
+ *  Government disclaim all warranties, express or implied, including       
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.                                                                
+ *                                                                          
+ *  Please cite the author in any work or product based on this material.   
+ *
+ * ===========================================================================
+ *
+ * Author: Christiam Camacho
+ *
+ */
+
+/** @file local_db_adapter.hpp
+ * Declares class which provides internal BLAST database representations to the
+ * internal BLAST APIs
+ */
+
+#ifndef ALGO_BLAST_API___LOCAL_DB_ADAPTER_HPP
+#define ALGO_BLAST_API___LOCAL_DB_ADAPTER_HPP
+
+#include <algo/blast/core/blast_seqsrc.h>
+#include <algo/blast/api/uniform_search.hpp> // for CSearchDatabase
+#include <objtools/readers/seqdb/seqdb.hpp>  // for CSeqDB
+
+/** @addtogroup AlgoBlast
+ *
+ * @{
+ */
+
+// Forward declaration
+struct BlastSeqSrc;
+
+BEGIN_NCBI_SCOPE
+BEGIN_SCOPE(blast)
+
+// Forward declaration
+class IBlastSeqInfoSrc;
+
+/// Interface to create a BlastSeqSrc suitable for use in CORE BLAST from a
+/// a variety of BLAST database/subject representations
+class NCBI_XBLAST_EXPORT CLocalDbAdapter : public CObject
+{
+public:
+    /// Constructor
+    /// @param dbinfo 
+    ///     Description of BLAST database
+    CLocalDbAdapter(const CSearchDatabase& dbinfo);
+    
+    /// Constructor
+    /// @param seqdb 
+    ///     CSeqDB object for an already opened BLAST database
+    CLocalDbAdapter(CRef<CSeqDB> seqdb);
+    
+    /// Constructor
+    /// @param seqsrc 
+    ///     BlastSeqSrc object for an already opened BLAST database
+    //CLocalDbAdapter(BlastSeqSrc* seqsrc);
+
+    /// Constructor
+    /// @param subject_sequences
+    ///     Set of sequences which should be used as subjects
+    /// @param opts_handle
+    ///     Options to be used (needed to create the ILocalQueryData)
+    CLocalDbAdapter(CRef<IQueryFactory> subject_sequences,
+                    CConstRef<CBlastOptionsHandle> opts_handle);
+
+    /// Destructor
+    virtual ~CLocalDbAdapter();
+
+    /// This method should be called so that if the implementation has an
+    /// internal "bookmark" of the chunks of the database it has assigned to
+    /// different threads, this can be reset at the start of a PSI-BLAST
+    /// iteration (or when reusing the same object to iterate over the 
+    /// database/subjects when the query is split).
+    /// This method should be called before calling MakeSeqSrc() to
+    /// ensure proper action on retrieving the BlastSeqSrc (in some cases it
+    /// might cause a re-construction of the underlying BlastSeqSrc
+    /// implementation). Note that in some cases, this operation might not apply
+    void ResetBlastSeqSrcIteration();
+
+    /// Retrieves or constructs the BlastSeqSrc
+    /// @note ownership of the constructed object is handled by this class
+    BlastSeqSrc* MakeSeqSrc();
+
+    /// Retrieves or constructs the IBlastSeqInfoSrc
+    /// @note ownership of the constructed object is handled by this class
+    IBlastSeqInfoSrc* MakeSeqInfoSrc();
+
+private:
+    /// True if the BlastSeqSrc object below is owned by this class
+    //bool m_OwnSeqSrc;
+
+    /// Pointer to the BlastSeqSrc
+    BlastSeqSrc* m_SeqSrc;
+
+    /// True if the IBlastSeqInfoSrc object below is owned by this class
+    //bool m_OwnSeqInfoSrc;
+
+    /// Pointer to the IBlastSeqInfoSrc
+    IBlastSeqInfoSrc* m_SeqInfoSrc;
+
+    /// Object containing BLAST database description
+    CRef<CSearchDatabase> m_DbInfo;
+
+    /// BLAST database handle
+    CRef<CSeqDB> m_SeqDb;
+
+    /// IQueryFactory containing the subject sequences
+    CRef<IQueryFactory> m_QueryFactory;
+
+    /// Options to be used when instantiating the subject sequences
+    CConstRef<CBlastOptionsHandle> m_OptsHandle;
+    
+    /// Initialize the internal CSeqDB object from a CSearchDatabase object
+    void x_InitSeqDB();
+
+    /// Prohibit copy-constructor
+    CLocalDbAdapter(const CLocalDbAdapter&);
+    /// Prohibit assignment operator
+    CLocalDbAdapter & operator=(const CLocalDbAdapter&);
+};
+
+
+END_SCOPE(BLAST)
+END_NCBI_SCOPE
+
+/* @} */
+
+#endif /* ALGO_BLAST_API___LOCAL_DB_ADAPTER__HPP */
+
---
+++ src/algo/blast/api/local_db_adapter.cpp
@@ -0,0 +1,198 @@
+#ifndef SKIP_DOXYGEN_PROCESSING
+static char const rcsid[] =
+    "$Id$";
+#endif /* SKIP_DOXYGEN_PROCESSING */
+/* ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Author:  Christiam Camacho
+ *
+ */
+
+/** @file local_db_adapter.cpp
+ * Defines class which provides internal BLAST database representations to the
+ * internal BLAST APIs
+ */
+
+#include <ncbi_pch.hpp>
+#include <algo/blast/api/local_db_adapter.hpp>
+#include <algo/blast/api/seqsrc_seqdb.hpp>  // for SeqDbBlastSeqSrcInit
+#include <algo/blast/api/seqinfosrc_seqdb.hpp>  // for CSeqDbSeqInfoSrc
+#include <algo/blast/api/seqsrc_query_factory.hpp>  // for QueryFactoryBlastSeqSrcInit
+#include "psiblast_aux_priv.hpp"    // for CPsiBlastValidate
+#include "seqinfosrc_bioseq.hpp"    // for CBioseqInfoSrc
+
+/** @addtogroup AlgoBlast
+ *
+ * @{
+ */
+
+BEGIN_NCBI_SCOPE
+BEGIN_SCOPE(blast)
+
+CLocalDbAdapter::CLocalDbAdapter(const CSearchDatabase& dbinfo)
+    : m_SeqSrc(0), m_SeqInfoSrc(0)
+{
+    m_DbInfo.Reset(new CSearchDatabase(dbinfo));
+}
+
+CLocalDbAdapter::CLocalDbAdapter(CRef<CSeqDB> seqdb)
+    : m_SeqSrc(0), m_SeqInfoSrc(0), m_SeqDb(seqdb)
+{
+    if (m_SeqDb.Empty()) {
+        NCBI_THROW(CBlastException, eInvalidArgument, "NULL CSeqDB");
+    }
+}
+
+CLocalDbAdapter::CLocalDbAdapter(CRef<IQueryFactory> subject_sequences,
+                                 CConstRef<CBlastOptionsHandle> opts_handle)
+    : m_SeqSrc(0), m_SeqInfoSrc(0), m_QueryFactory(subject_sequences),
+    m_OptsHandle(opts_handle)
+{
+    if (subject_sequences.Empty()) {
+        NCBI_THROW(CBlastException, eInvalidArgument, 
+                   "Missing subject sequence data");
+    }
+    if (opts_handle.Empty()) {
+        NCBI_THROW(CBlastException, eInvalidArgument, "Missing options");
+    }
+    CPsiBlastValidate::QueryFactory(subject_sequences, *opts_handle,
+                                    CPsiBlastValidate::eQFT_Subject);
+}
+
+CLocalDbAdapter::~CLocalDbAdapter()
+{
+    if (m_SeqSrc) {
+        m_SeqSrc = BlastSeqSrcFree(m_SeqSrc);
+    }
+    if (m_SeqInfoSrc) {
+        delete m_SeqInfoSrc;
+        m_SeqInfoSrc = 0;
+    }
+}
+
+void
+CLocalDbAdapter::ResetBlastSeqSrcIteration()
+{
+    if (m_SeqSrc) {
+        BlastSeqSrcResetChunkIterator(m_SeqSrc);
+    }
+}
+
+/// Checks if the BlastSeqSrc initialization succeeded
+/// @throws CBlastException if BlastSeqSrc initialization failed
+static void
+s_CheckForBlastSeqSrcErrors(const BlastSeqSrc* seqsrc)
+{
+    if ( !seqsrc ) {
+        return;
+    }
+
+    char* error_str = BlastSeqSrcGetInitError(seqsrc);
+    if (error_str) {
+        string msg(error_str);
+        sfree(error_str);
+        NCBI_THROW(CBlastException, eSeqSrcInit, msg);
+    }
+}
+
+BlastSeqSrc*
+CLocalDbAdapter::MakeSeqSrc()
+{
+    if ( ! m_SeqSrc ) {
+        if (m_DbInfo.NotEmpty()) {
+            x_InitSeqDB();
+            m_SeqSrc = SeqDbBlastSeqSrcInit(m_SeqDb.GetNonNullPointer());
+        } else if (m_SeqDb.NotEmpty()) {
+            m_SeqSrc = SeqDbBlastSeqSrcInit(m_SeqDb.GetNonNullPointer());
+        } else if (m_QueryFactory.NotEmpty() && m_OptsHandle.NotEmpty()) {
+            m_SeqSrc = QueryFactoryBlastSeqSrcInit(m_QueryFactory,
+                           m_OptsHandle->GetOptions().GetProgramType());
+        } else {
+            abort();
+        }
+        s_CheckForBlastSeqSrcErrors(m_SeqSrc);
+        _ASSERT(m_SeqSrc);
+    }
+    return m_SeqSrc;
+}
+
+void
+CLocalDbAdapter::x_InitSeqDB()
+{
+    _ASSERT(m_DbInfo.NotEmpty());
+    if (m_SeqDb) {
+        return;
+    }
+
+    const CSeqDB::ESeqType type = m_DbInfo->IsProtein()
+        ? CSeqDB::eProtein
+        : CSeqDB::eNucleotide;
+
+    // FIXME: refactor code in SplitDB/LibEntrezCacheEx.cpp ?
+    if ( !m_DbInfo->GetEntrezQueryLimitation().empty() ) {
+        NCBI_THROW(CException, eUnknown, "Unimplemented");
+    }
+    if ( !m_DbInfo->GetGiListLimitation().empty() ) {
+        NCBI_THROW(CException, eUnknown, "Unimplemented");
+    }
+
+    /*
+    CRef<CMySeqDbGiList> g(new
+        CMySeqDbGiList(m_DbInfo->GetEntrezQueryLimitation(),
+        m_DbInfo->GetGiListLimitation()));
+
+    m_DbHandle.Reset(new CSeqDB(m_DbInfo->GetDatabaseName(), type, &*g));
+    */
+    m_SeqDb.Reset(new CSeqDB(m_DbInfo->GetDatabaseName(), type));
+}
+
+IBlastSeqInfoSrc*
+CLocalDbAdapter::MakeSeqInfoSrc()
+{
+    if ( !m_SeqInfoSrc ) {
+        if (m_DbInfo.NotEmpty()) {
+            x_InitSeqDB();
+            m_SeqInfoSrc = new CSeqDbSeqInfoSrc(m_SeqDb);
+        } else if (m_SeqDb.NotEmpty()) {
+            m_SeqInfoSrc = new CSeqDbSeqInfoSrc(m_SeqDb);
+        } else if (m_QueryFactory.NotEmpty() && m_OptsHandle.NotEmpty()) {
+            CRef<IRemoteQueryData> subj_data
+                (m_QueryFactory->MakeRemoteQueryData());
+            CRef<CBioseq_set> subject_bioseqs(subj_data->GetBioseqSet());
+            EBlastProgramType p(m_OptsHandle->GetOptions().GetProgramType());
+            bool is_prot = Blast_SubjectIsProtein(p) ? true : false;
+            m_SeqInfoSrc = new CBioseqSeqInfoSrc(*subject_bioseqs, is_prot);
+        } else {
+            abort();
+        }
+        _ASSERT(m_SeqInfoSrc);
+    }
+    return m_SeqInfoSrc;
+}
+
+END_SCOPE(Blast)
+END_NCBI_SCOPE
+
+/* @} */
