c6961c309bfddc8066ab64ed0d7ce6652e82f43d
Anton Lavrentiev <lavr@ncbi.nlm.nih.gov>
2007-04-20 16:21:57 +0000

    Check added for test_tar
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@33567 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/util/test/Makefile.test_tar.app
+++ src/util/test/Makefile.test_tar.app
@@ -4,3 +4,8 @@ APP = test_tar
 SRC = test_tar
 LIB = $(COMPRESS_LIBS) xutil xncbi
 LIBS = $(CMPRS_LIBS) $(ORIG_LIBS)
+
+CHECK_REQUIRES = unix
+
+CHECK_CMD = test_tar.sh
+CHECK_COPY = test_tar.sh
---
+++ src/util/test/test_tar.sh
@@ -0,0 +1,37 @@
+#! /bin/sh
+
+# Locate native tar;  exit if none found
+string="`whereis tar 2>/dev/null`"
+if [ -z "$string" ]; then
+  tar="`which tar 2>/dev/null`"
+else
+  tar="`echo $string | cut -f2 -d' '`"
+fi
+test -x "$tar"  ||  exit 0
+
+mkdir /tmp/test_tar.$$.1  ||  exit 1
+trap 'rm -rf /tmp/test_tar.$$.*' 0 1 2 15
+
+cp -rp . /tmp/test_tar.$$.1/ 2>/dev/null
+
+mkdir /tmp/test_tar.$$.1/testdir.$$ 2>/dev/null
+
+date >/tmp/test_tar.$$.1/testdir.$$/datefile 2>/dev/null
+
+ln -s /tmp/test_tar.$$.1/testdir.$$/datefile  /tmp/test_tar.$$.1/testdir.$$/12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890 2>/dev/null
+
+touch /tmp/test_tar.$$.1/12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890 2>/dev/null
+
+(cd /tmp/test_tar.$$.1  &&  $tar cvf /tmp/test_tar.$$.tar .)       ||  exit 1
+
+test_tar -T -f /tmp/test_tar.$$.tar                                ||  exit 1
+
+mkdir /tmp/test_tar.$$.2                                           ||  exit 1
+
+cat /tmp/test_tar.$$.tar | test_tar -C /tmp/test_tar.$$.2 -x -f -  ||  exit 1
+
+diff -r /tmp/test_tar.$$.1 /tmp/test_tar.$$.2 2>/dev/null          ||  exit 1
+
+test_tar -C /tmp/test_tar.$$.2 -c -f - . 2>/dev/null | $tar tvf -  ||  exit 1
+
+exit 0
