893dc6c0295cecfa566626419e0b5195033647a4
Aaron Ucko <ucko@ncbi.nlm.nih.gov>
2007-05-03 15:05:33 +0000

    Drop redundant __cplusplus conditionals -- this is a C++ source file anyway.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@33789 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/connect/ncbi_core_cxx.cpp
+++ src/connect/ncbi_core_cxx.cpp
@@ -219,18 +219,14 @@ extern MT_LOCK MT_LOCK_cxx2c(CRWLock* lock, bool pass_ownership)
  *                                 Fini                                *
  ***********************************************************************/
 
-#ifdef __cplusplus
 extern "C" {
-#endif /*__cplusplus*/
 static void s_Fini(void)
 {
     CORE_SetREG(0);
     CORE_SetLOG(0);
     CORE_SetLOCK(0);
 }
-#ifdef __cplusplus
 }
-#endif /*__cplusplus*/
 
 
 
