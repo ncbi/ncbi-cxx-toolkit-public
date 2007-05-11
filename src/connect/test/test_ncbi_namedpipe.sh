6489b3085e78be514b52f046c80c72c67bce7d79
Anton Lavrentiev <lavr@ncbi.nlm.nih.gov>
2007-05-11 19:23:06 +0000

    Better cleanup
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@33892 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/connect/test/test_ncbi_namedpipe.sh
+++ src/connect/test/test_ncbi_namedpipe.sh
@@ -4,15 +4,15 @@
 exit_code=0
 
 test_ncbi_namedpipe server $$ &
-server_pid=$!
-trap 'kill -9 $server_pid' 1 2 15
+spid=$!
+trap 'kill -9 $spid; rm -f ./ncbi_test_pipename_$$' 0 1 2 15
 
 sleep 10
 $CHECK_EXEC test_ncbi_namedpipe client $$ ||  exit_code=1
 
-kill $server_pid  ||  exit_code=2
-( kill -9 $server_pid ) >/dev/null 2>&1
+kill $spid  ||  exit_code=2
+( kill -9 $spid ) >/dev/null 2>&1
 
-rm ./.ncbi_test_pipename$$ >/dev/null 2>&1
+rm -f ./.ncbi_test_pipename_$$ >/dev/null 2>&1
 
 exit $exit_code
