5a3283c64cdab4189d2ee26c4181d091d4afdffb
Victor Joukov <joukovv@ncbi.nlm.nih.gov>
2007-06-06 19:46:45 +0000

    Added server monitor - factored out of NetSchedule.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@34184 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ include/connect/server_monitor.hpp
@@ -0,0 +1,64 @@
+#ifndef CONNECT___SERVER_MONITOR__HPP
+#define CONNECT___SERVER_MONITOR__HPP
+
+/*  $Id$
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Authors:  Anatoliy Kuznetsov, Victor Joukov
+ *
+ * File Description: Server monitoring
+ *
+ */
+
+#include <corelib/ncbimtx.hpp>
+#include <connect/ncbi_socket.hpp>
+
+
+BEGIN_NCBI_SCOPE
+
+/// Server monitor
+///
+class CServer_Monitor
+{
+public:
+    CServer_Monitor();
+
+    ~CServer_Monitor();
+    // Pass open socket for monitor output. The original socket is
+    // empty afterwards, ownership is handled by monitor. It activates
+    // the monitor.
+    void SetSocket(CSocket& socket);
+    bool IsMonitorActive();
+    void SendMessage(const char* msg, size_t length);
+    void SendString(const string& str);
+
+private:
+    CFastMutex m_Lock; 
+    CSocket*   m_Sock;
+};
+
+END_NCBI_SCOPE
+
+#endif /* CONNECT___SERVER_MONITOR__HPP */
