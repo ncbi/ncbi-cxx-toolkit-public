20264256e0721dda0f3737a7393df52f4d3083b8
Anton Lavrentiev <lavr@ncbi.nlm.nih.gov>
2007-06-18 17:01:40 +0000

    Comment test stages with log messages
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@34320 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/util/test/test_tar.sh
+++ src/util/test/test_tar.sh
@@ -12,6 +12,8 @@ test -x "$tar"  ||  exit 0
 mkdir /tmp/test_tar.$$.1  ||  exit 1
 trap 'rm -rf /tmp/test_tar.$$.* &' 0 1 2 15
 
+echo "Preparing file staging area"
+
 cp -rp . /tmp/test_tar.$$.1/ 2>/dev/null
 
 mkdir /tmp/test_tar.$$.1/testdir 2>/dev/null
@@ -35,7 +37,11 @@ touch /tmp/test_tar.$$.1/1234567890123456789012345678901234567890123456789012345
 rm -rf /tmp/test_tar.$$.1
 mkdir  /tmp/test_tar.$$.1                                                         ||  exit 1
 
+echo "Creating test archive using native tar utility"
+
 (cd /tmp/test_tar.$$.1  &&  $tar xf  /tmp/test_tar.$$.tar)
+
+echo "Testing the archive"
 
 test_tar -T -f /tmp/test_tar.$$.tar                                               ||  exit 1
 
@@ -43,6 +49,9 @@ sleep 1
 mkdir /tmp/test_tar.$$.1/newdir 2>/dev/null
 date 2>/dev/null | tee -a /tmp/test_tar.$$.1/testdir.$$/datefile /tmp/test_tar.$$.1/newdir/datefile >/tmp/test_tar.$$.1/datefile 2>/dev/null
 cp -fp /tmp/test_tar.$$.1/newdir/datefile /tmp/test_tar.$$.1/newdir/dummyfile 2>/dev/null
+
+echo "Testing simple update"
+
 test_tar -C /tmp/test_tar.$$.1 -u -v -f /tmp/test_tar.$$.tar ./datefile ./newdir  ||  exit 1
 
 mv -f /tmp/test_tar.$$.1/datefile /tmp/test_tar.$$.1/phonyfile 2>/dev/null
@@ -51,6 +60,8 @@ mkdir /tmp/test_tar.$$.1/datefile 2>/dev/null
 sleep 1
 date >>/tmp/test_tar.$$.1/newdir/datefile 2>/dev/null
 
+echo "Testing incremental update"
+
 test_tar -C /tmp/test_tar.$$.1 -u -U -v -E -f /tmp/test_tar.$$.tar ./newdir ./datefile ./phonyfile  ||  exit 1
 
 rmdir /tmp/test_tar.$$.1/datefile 2>/dev/null
@@ -58,15 +69,25 @@ mv -f /tmp/test_tar.$$.1/phonyfile /tmp/test_tar.$$.1/datefile 2>/dev/null
 
 mkdir /tmp/test_tar.$$.2                                                          ||  exit 1
 
+echo "Testing piping and extraction"
+
 cat /tmp/test_tar.$$.tar | test_tar -C /tmp/test_tar.$$.2 -v -x -f -              ||  exit 1
 
 diff -r /tmp/test_tar.$$.1 /tmp/test_tar.$$.2 2>/dev/null                         ||  exit 1
 
+echo "Testing archive creation"
+
 test_tar -C /tmp/test_tar.$$.2 -c -f - . 2>/dev/null | $tar tBvf -                ||  exit 1
+
+echo "Testing piping and native archive format compatibility"
 
 test_tar -C /tmp/test_tar.$$.2 -x -B -f /tmp/test_tar.$$.tar '*testdir/?*'        ||  exit 1
 
+echo  "Testing backup feature"
+
 (cd /tmp/test_tar.$$.2/testdir  &&  test "`echo * | tr ' ' '\n' | grep -v -c '[.]bak$'`" = "`echo * | tr ' ' '\n' | grep -c '[.]bak$'`")  ||  exit 1
+
+echo "Testing singe entry streaming feature"
 
 test_tar -X -v -f /tmp/test_tar.$$.tar '*test_tar' | cmp -l - ./test_tar          ||  exit 1
 
