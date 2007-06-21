1ebcab139316b70f3a02462d5a400e8e575fb98f
Anton Lavrentiev <lavr@ncbi.nlm.nih.gov>
2007-06-21 01:08:14 +0000

    More logging as the test progresses
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@34366 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/util/test/test_tar.sh
+++ src/util/test/test_tar.sh
@@ -9,86 +9,89 @@ else
 fi
 test -x "$tar"  ||  exit 0
 
-mkdir /tmp/test_tar.$$.1  ||  exit 1
-trap 'rm -rf /tmp/test_tar.$$.* &' 0 1 2 15
+test_base="/tmp/test_tar.$$"
+mkdir $test_base.1  ||  exit 1
+trap 'rm -rf $test_base* &' 0 1 2 15
 
 echo "Preparing file staging area"
 
-cp -rp . /tmp/test_tar.$$.1/ 2>/dev/null
+cp -rp . $test_base.1/ 2>/dev/null
 
-mkdir /tmp/test_tar.$$.1/testdir 2>/dev/null
+mkdir $test_base.1/testdir 2>/dev/null
 
-date >/tmp/test_tar.$$.1/testdir/datefile 2>/dev/null
+date >$test_base.1/testdir/datefile 2>/dev/null
 
-ln -s /tmp/test_tar.$$.1/testdir/datefile /tmp/test_tar.$$.1/testdir/ABS12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890 2>/dev/null
+ln -s $test_base.1/testdir/datefile $test_base.1/testdir/ABS12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890 2>/dev/null
 
-ln -s                 ../testdir/datefile /tmp/test_tar.$$.1/testdir/REL12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890 2>/dev/null
+ln -s           ../testdir/datefile $test_base.1/testdir/REL12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890 2>/dev/null
 
-ln    /tmp/test_tar.$$.1/testdir/datefile /tmp/test_tar.$$.1/testdir/linkfile 2>/dev/null
+ln    $test_base.1/testdir/datefile $test_base.1/testdir/linkfile 2>/dev/null
 
-mkdir /tmp/test_tar.$$.1/testdir/12345678901234567890123456789012345678901234567890 2>/dev/null
+mkdir $test_base.1/testdir/12345678901234567890123456789012345678901234567890 2>/dev/null
 
-touch /tmp/test_tar.$$.1/testdir/12345678901234567890123456789012345678901234567890/12345678901234567890123456789012345678901234567890 2>/dev/null
+touch $test_base.1/testdir/12345678901234567890123456789012345678901234567890/12345678901234567890123456789012345678901234567890 2>/dev/null
 
-touch /tmp/test_tar.$$.1/12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890 2>/dev/null
-
-(cd /tmp/test_tar.$$.1  &&  $tar cvf /tmp/test_tar.$$.tar .)
-
-rm -rf /tmp/test_tar.$$.1
-mkdir  /tmp/test_tar.$$.1                                                         ||  exit 1
+touch $test_base.1/12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890 2>/dev/null
 
 echo "Creating test archive using native tar utility"
 
-(cd /tmp/test_tar.$$.1  &&  $tar xf  /tmp/test_tar.$$.tar)
+(cd $test_base.1  &&  $tar cvf $test_base.tar .)
+
+rm -rf $test_base.1
+mkdir  $test_base.1                                                   ||  exit 1
+
+echo "Sanitizing test archive from unsupported features"
+
+(cd $test_base.1  &&  $tar xf  $test_base.tar)
 
 echo "Testing the archive"
 
-test_tar -T -f /tmp/test_tar.$$.tar                                               ||  exit 1
+test_tar -T -f $test_base.tar                                         ||  exit 1
 
 sleep 1
-mkdir /tmp/test_tar.$$.1/newdir 2>/dev/null
-date 2>/dev/null | tee -a /tmp/test_tar.$$.1/testdir.$$/datefile /tmp/test_tar.$$.1/newdir/datefile >/tmp/test_tar.$$.1/datefile 2>/dev/null
-cp -fp /tmp/test_tar.$$.1/newdir/datefile /tmp/test_tar.$$.1/newdir/dummyfile 2>/dev/null
+mkdir $test_base.1/newdir 2>/dev/null
+date 2>/dev/null | tee -a $test_base.1/testdir.$$/datefile $test_base.1/newdir/datefile >$test_base.1/datefile 2>/dev/null
+cp -fp $test_base.1/newdir/datefile $test_base.1/newdir/dummyfile 2>/dev/null
 
 echo "Testing simple update"
 
-test_tar -C /tmp/test_tar.$$.1 -u -v -f /tmp/test_tar.$$.tar ./datefile ./newdir  ||  exit 1
+test_tar -C $test_base.1 -u -v -f $test_base.tar ./datefile ./newdir  ||  exit 1
 
-mv -f /tmp/test_tar.$$.1/datefile /tmp/test_tar.$$.1/phonyfile 2>/dev/null
-mkdir /tmp/test_tar.$$.1/datefile 2>/dev/null
+mv -f $test_base.1/datefile $test_base.1/phonyfile 2>/dev/null
+mkdir $test_base.1/datefile 2>/dev/null
 
 sleep 1
-date >>/tmp/test_tar.$$.1/newdir/datefile 2>/dev/null
+date >>$test_base.1/newdir/datefile 2>/dev/null
 
 echo "Testing incremental update"
 
-test_tar -C /tmp/test_tar.$$.1 -u -U -v -E -f /tmp/test_tar.$$.tar ./newdir ./datefile ./phonyfile  ||  exit 1
+test_tar -C $test_base.1 -u -U -v -E -f $test_base.tar ./newdir ./datefile ./phonyfile  ||  exit 1
 
-rmdir /tmp/test_tar.$$.1/datefile 2>/dev/null
-mv -f /tmp/test_tar.$$.1/phonyfile /tmp/test_tar.$$.1/datefile 2>/dev/null
+rmdir $test_base.1/datefile 2>/dev/null
+mv -f $test_base.1/phonyfile $test_base.1/datefile 2>/dev/null
 
-mkdir /tmp/test_tar.$$.2                                                          ||  exit 1
+mkdir $test_base.2                                                    ||  exit 1
 
-echo "Testing piping and extraction"
+echo "Testing piping in and extraction"
 
-cat /tmp/test_tar.$$.tar | test_tar -C /tmp/test_tar.$$.2 -v -x -f -              ||  exit 1
+cat $test_base.tar | test_tar -C $test_base.2 -v -x -f -              ||  exit 1
 
-diff -r /tmp/test_tar.$$.1 /tmp/test_tar.$$.2 2>/dev/null                         ||  exit 1
+diff -r $test_base.1 $test_base.2 2>/dev/null                         ||  exit 1
 
-echo "Testing archive creation"
+echo "Testing piping out and compatibility with native tar utility"
 
-test_tar -C /tmp/test_tar.$$.2 -c -f - . 2>/dev/null | $tar tBvf -                ||  exit 1
+test_tar -C $test_base.2 -c -f - . 2>/dev/null | $tar tBvf -          ||  exit 1
 
-echo "Testing piping and native archive format compatibility"
+echo "Testing backup feature"
 
-test_tar -C /tmp/test_tar.$$.2 -x -B -f /tmp/test_tar.$$.tar '*testdir/?*'        ||  exit 1
-
-echo  "Testing backup feature"
-
-(cd /tmp/test_tar.$$.2/testdir  &&  test "`echo * | tr ' ' '\n' | grep -v -c '[.]bak$'`" = "`echo * | tr ' ' '\n' | grep -c '[.]bak$'`")  ||  exit 1
+test_tar -C $test_base.2 -x -B -f $test_base.tar '*testdir/?*'        ||  exit 1
+files="`echo $test_base.2/testdir/* | tr ' ' '\n' | grep -v -c '[.]bak$'`"
+bkups="`echo $test_base.2/testdir/* | tr ' ' '\n' | grep    -c '[.]bak$'`"
+echo "Files: $files --- Backups: $bkups"
+test _"$files" = _"$bkups"                                            ||  exit 1
 
 echo "Testing singe entry streaming feature"
 
-test_tar -X -v -f /tmp/test_tar.$$.tar '*test_tar' | cmp -l - ./test_tar          ||  exit 1
+test_tar -X -v -f $test_base.tar '*test_tar' | cmp -l - ./test_tar    ||  exit 1
 
 exit 0
