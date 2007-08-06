a13289114e283b2e30bc8b06143f47350dd70779
Victor Joukov <joukovv@ncbi.nlm.nih.gov>
2007-08-06 15:46:22 +0000

    Added release configuration scripts.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@34890 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ scripts/copy_saved_config.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+abs_saved_config_dir=$1
+build_name=$2
+
+abs_top_srcdir="`cd $abs_saved_config_dir/../../c++ && pwd`"
+
+mkdir $abs_top_srcdir/$build_name
+mkdir $abs_top_srcdir/$build_name/bin
+mkdir $abs_top_srcdir/$build_name/build
+mkdir $abs_top_srcdir/$build_name/inc
+mkdir $abs_top_srcdir/$build_name/lib
+mkdir $abs_top_srcdir/$build_name/status
+
+sed "
+s|@script_shell@|#!/bin/sh|
+s|@builddir@|$abs_top_srcdir/$build_name/build|
+s|@abs_top_srcdir@|$abs_top_srcdir|" <$abs_top_srcdir/src/reconfigure.sh.in >$abs_top_srcdir/$build_name/build/reconfigure.sh
+chmod +x $abs_top_srcdir/$build_name/build/reconfigure.sh
+
+touch $abs_top_srcdir/$build_name/inc/ncbiconf_unix.h
+
+sed "
+s|@build_name@|$build_name|g
+s|@saved_config_abs_top_srcdir@|$abs_top_srcdir|g" <$abs_saved_config_dir/config.status.in >$abs_top_srcdir/$build_name/status/config.status
+chmod +x $abs_top_srcdir/$build_name/status/config.status
