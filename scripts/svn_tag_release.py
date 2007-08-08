a2edc077d33fdb44febf40f4dd040db407788f3d
Victor Joukov <joukovv@ncbi.nlm.nih.gov>
2007-08-08 23:08:22 +0000

    Interim.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@34944 78c7ea69-d796-4a43-9a09-de51944f1b03

--- scripts/svn_tag_release.py
+++ scripts/svn_tag_release.py
@@ -2,46 +2,52 @@
 
 import sys, popen2
 
+# ORed flags for path part
+FLAG_COPY      = 1
+FLAG_RECURSIVE = 2
+
+def add_path(tree, path_descr):
+    cur = tree
+    for flag, path_part in path_descr:
+        new_cur = cur[1].get(path_part, (flag, {}))
+        if new_cur[0] != flag:
+            new_cur = new_cur[0] | flag, new_cur[1]
+        cur[1][path_part] = new_cur
+        cur = new_cur
+
 def get_project(root, project_name):
     url = root + "/trunk/c++/scripts/projects/" + project_name + ".lst"
-    r,w,e = popen2.popen3("svn cat "+url)
-    err = e.read()
-    if err:
-        print err
-        return {}
-    dirs = r.read().split('\n')
-    proj_tree = {}
+#    r,w,e = popen2.popen3("svn cat "+url)
+#    err = e.read()
+#    if err:
+#        print err
+#        return None
+#    dirs = r.read().split('\n')
+    dirs = file("test.lst")
+    inc_tree = 0, {}
+    src_tree = 0, {}
     for line in dirs:
         line = line.strip()
         if not line: continue
         parts = line.split(' ', 1)
         path_parts = parts[0].split('/')
-        print path_parts
-        cur = proj_tree
-        for path_part in path_parts:
-            if path_part[-1] == '$':
-                path_part = path_part[:-1]
-                node = {}
-            else:
-                node = None
-            # Following code is different from setdefault in that it replaces
-            # None with empty dict for non-term path_part
-            # It handles the case when generic terminating path mentioned before
-            # more precise one e.g.:
-            # connect$
-            # connect/services$
-            # now it handles it so that only connect/services$ needed to include
-            # connect with regular files and connect/services also.
-            new_cur = cur.get(path_part, node)
-            if new_cur is None and not node is None:
-                new_cur = node
-            cur[path_part] = new_cur
-            cur = new_cur
-    print proj_tree
-    return proj_tree
+        path_descr = zip([0] * len(path_parts), path_parts)
+        get_src = len(parts) <= 1 or parts[1] != "update-only"
+        last_part = path_parts[-1]
+        last_flag = FLAG_COPY
+        if last_part[-1] == '$':
+            last_part = last_part[:-1]
+        else:
+            last_flag |= FLAG_RECURSIVE
+        path_descr[-1] = last_flag, last_part
+        add_path(inc_tree, path_descr)
+        if get_src: add_path(src_tree, path_descr)
+    print "Include", inc_tree
+    print "Source", src_tree
+    return inc_tree, src_tree
 
 def list_url(url):
-    print "svn list "+url
+#    print "svn list "+url
     r,w,e = popen2.popen3("svn list "+url)
     res = []
     for line in r:
@@ -55,23 +61,29 @@ def list_url(url):
     return res
 
 def add_tree(dirlist, url, root, proj_tree):
+    level_flag, children = proj_tree
+    # Should we filter?
+    if level_flag & FLAG_RECURSIVE:
+        return
     eff_url = url + '/' + root
     nodes = list_url(eff_url)
     for node in nodes:
-        if not proj_tree is None:
-            dir_node = node[-1] == '/'
-            if dir_node: node = node[:-1]
-            if not dir_node: continue
-            if not node in proj_tree:
-                dirlist.append(root + '/' + node)
-            else:
-                add_tree(dirlist, url, root + '/' + node, proj_tree[node])
+        dir_node = node[-1] == '/'
+        if dir_node: node = node[:-1]
+        if not dir_node: continue
+        child = children.get(node)
+        if not child or (not child[1] and not child[0] & FLAG_COPY):
+            dirlist.append(root + '/' + node)
+            print root + '/' + node
+        else:
+            add_tree(dirlist, url, root + '/' + node, child)
 
 def get_list_to_remove(root, proj_tree):
+    inc_tree, src_tree = proj_tree
     dirlist = ["doc", "scripts/internal"]
     url = root + "/trunk/c++"
-    add_tree(dirlist, url, "src", proj_tree)
-    add_tree(dirlist, url, "include", proj_tree)
+    add_tree(dirlist, url, "include", inc_tree)
+    add_tree(dirlist, url, "src",     src_tree)
     return dirlist
 
 def main(args):
@@ -91,13 +103,13 @@ def main(args):
     if not proj_tree:
         return -1
     dirlist = get_list_to_remove(root, proj_tree)
-    target = "tags/%s/current/c++" % project
-    tags = list_url(root+'/tags')
-    if not project+'/' in tags:
-        mktagdir = "mkdir tags/%s " % project
-    else:
-        mktagdir = ''
-    print "svnmucc -U %s %smkdir tags/%s/current cp %s trunk/c++ %s %s" % (root, mktagdir, project, revision, target, " ".join(map(lambda x: "rm %s/%s" % (target, x), dirlist)))
+#    target = "tags/%s/current/c++" % project
+#    mucc_cmd = "svnmucc -U %s" % root
+#    tags = list_url(root+'/tags')
+#    if not project+'/' in tags:
+#        mucc_cmd += " mkdir tags/%s " % project
+#    mucc_cmd += " mkdir tags/%s/current cp %s trunk/c++ %s " % (project, revision, target)
+#    mucc_cmd += " ".join(map(lambda x: "rm %s/%s" % (target, x), dirlist)))
     return 0
 
 if __name__ == "__main__":
