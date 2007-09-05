3f5c026114797a4fc80c268912448aa2fd09a896
chetvern <chetvern@ncbi.nlm.nih.gov>
2007-09-05 20:31:36 +0000

    Optimized CObjHolder
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@35304 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/algo/align/prosplign/ObjHolder.hpp
+++ src/algo/align/prosplign/ObjHolder.hpp
@@ -43,50 +43,39 @@ BEGIN_SCOPE(prosplign)
 template<class T>
 class CObjHolder
 {
+    static const int m_MaxAllocSize = 1024*1024;
 public:
-    inline CObjHolder(int alloc_size = 1024) : m_AllocSize(alloc_size) {}
- //   inline int size() const { return (int)m_Holder.size(); }
-    inline void Add(T *pt) { m_PointerHolder.push_back(pt); } 
+    inline CObjHolder(int alloc_size = 1024) : m_PointerHolder(NULL), m_AllocSize(alloc_size) {}
+    ~CObjHolder()
+    {
+        for(size_t i = 0; i < m_ObjHolder.size(); ++i)
+            delete[] m_ObjHolder[i];
+    }
+    inline void Add(T*tail, T* head) { head->m_Prev = m_PointerHolder; m_PointerHolder = tail;} 
     inline T *Get() { 
-        if(m_PointerHolder.empty()) {
-            //swap
-            vector<vector<T> > vtmp;
-            vtmp.swap(m_ObjHolder);
-            m_ObjHolder.resize(vtmp.size() + 1);
-            /*  does not work because of bug in Linux compiler
-            typedef vector<vector<T> >::iterator VVIT;
-            vector<vector<T> >::iterator vit = vtmp.begin();
-            vector<vector<T> >::iterator oit = m_ObjHolder.begin();
-            for(;vit != vtmp.end(); ++vit, ++oit) vit->swap(*oit);
-            */
-            int ii;
-            for(ii=0;ii != (int)vtmp.size(); ++ii) vtmp[ii].swap(m_ObjHolder[ii]);
-            /*end of workaround*/
-            //deal with the new one
-            m_ObjHolder.back().resize(m_AllocSize);
-            /* does not work because of bug in Linux compiler, workaround below
-            for(vector<T>::iterator it = m_ObjHolder.back().begin(); it!= m_ObjHolder.back().end(); ++it) {
-                m_PointerHolder.push_back(&*it);
+        if(m_PointerHolder == NULL) {
+            T* new_batch = new T[m_AllocSize];
+            m_ObjHolder.push_back(new_batch);
+            for (int i = 0; i<m_AllocSize; ++i) {
+                new_batch->m_Prev = m_PointerHolder;
+                m_PointerHolder = new_batch++;
             }
-            */
-            for(ii=0;ii<(int)m_ObjHolder.back().size(); ++ii) m_PointerHolder.push_back(&(m_ObjHolder.back()[ii]));
-            /*end of workaround*/
             //change alloc size
-            if(m_AllocSize < m_MaxAllocSize) m_AllocSize *=2;
-            if(m_AllocSize > m_MaxAllocSize) m_AllocSize = m_MaxAllocSize;
+            if(m_AllocSize < m_MaxAllocSize) {
+                m_AllocSize *=2;
+                if(m_AllocSize > m_MaxAllocSize) m_AllocSize = m_MaxAllocSize;
+            }
         }
-        T *tmp = m_PointerHolder.back();
-        m_PointerHolder.pop_back();
+        T *tmp = m_PointerHolder;
+        m_PointerHolder = tmp->m_Prev;
         return tmp;
     }
 private:        
-    vector<T *> m_PointerHolder;
-    vector<vector<T> > m_ObjHolder;
+    T * m_PointerHolder;
+    vector<T*> m_ObjHolder;
     int m_AllocSize;
-    static int m_MaxAllocSize;
 };
 
-template<class T> int CObjHolder<T>::m_MaxAllocSize = 1024*1024;
 
 END_SCOPE(prosplign)
 END_NCBI_SCOPE
