5351469535a9671758c13abd6d066699d33f7678
ssikorsk <ssikorsk@ncbi.nlm.nih.gov>
2007-09-17 21:49:45 +0000

    Set up $SYBASE if it was not set previously. It is needed to find interfaces-file.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@35466 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/dbapi/test/dbapi_unit_test.sh
+++ src/dbapi/test/dbapi_unit_test.sh
@@ -104,6 +104,11 @@ Unknown driver initialization error.
 EOF
     else
         for server in $server_list ; do
+            if test \( -z "$SYBASE" -o "$SYBASE" == "No_Sybase" \) -a -f "/netopt/Sybase/clients/current/interfaces" ; then
+                SYBASE="/netopt/Sybase/clients/current"
+                exprort SYBASE
+            fi
+
             if test \( $driver = "ctlib" -o $driver = "dblib" \) -a \( $server = $server_mssql -o $server = $server_mssql2005 \) ; then
                 continue
             fi
