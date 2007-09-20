db66803fcf0e5f2960b332610eb08db7d306c522
ssikorsk <ssikorsk@ncbi.nlm.nih.gov>
2007-09-20 21:34:08 +0000

    Call blk_textxfer for null-values of type TEXT or IMAGE in CTL_BCPInCmd::Send(). That is required by FreeTDS ctlib.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@35519 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/dbapi/driver/ctlib/bcp.cpp
+++ src/dbapi/driver/ctlib/bcp.cpp
@@ -534,63 +534,87 @@ bool CTL_BCPInCmd::Send(void)
                 size_t invalid_len = 0;
                 CDB_Stream& par = dynamic_cast<CDB_Stream&> (param);
 
-                for (datalen = (CS_INT) par.Size();  datalen > 0;
-                     datalen -= (CS_INT) len)
-                {
-                    len = par.Read(buff + invalid_len, sizeof(buff) - invalid_len);
-
-                    valid_len = CStringUTF8::GetValidBytesCount(buff, len);
-                    invalid_len = len - valid_len;
-
-#if 0 //  #if defined(HAVE_WSTRING)
-                    if (x_IsUnicodeClientAPI()) {
-                        CWString unicode_str(buff, valid_len, eEncoding_UTF8);
-
-                        string wcharle_str = MakeUCS2LE(unicode_str.AsUnicode(eEncoding_UTF8));
-
-                        SetHasFailed((Check(
-                            blk_textxfer(
-                                x_GetSybaseCmd(),
-                                (CS_BYTE*) wcharle_str.c_str(),
-                                (CS_INT) wcharle_str.size(),
-                                0)
-                            ) == CS_FAIL));
-                    } else {
-                        SetHasFailed((Check(blk_textxfer(x_GetSybaseCmd(),
-                                                         (CS_BYTE*) buff,
-                                                         (CS_INT) valid_len,
-                                                         0)
-                                             ) == CS_FAIL));
-                    }
-#else
+                if (par.IsNULL()) {
+                    // Because of FreeTDS ...
                     SetHasFailed((Check(blk_textxfer(x_GetSybaseCmd(),
-                                                     (CS_BYTE*) buff,
-                                                     (CS_INT) valid_len,
-                                                     0)
-                                         ) == CS_FAIL));
-#endif
+                                                    (CS_BYTE*) buff,
+                                                    (CS_INT) 0,
+                                                    0)
+                                        ) == CS_FAIL));
 
                     CHECK_DRIVER_ERROR(
                         HasFailed(),
                         "blk_textxfer failed for the text/image field." + GetDbgInfo(), 123005
                         );
+                } else {
+                    for (datalen = (CS_INT) par.Size();  datalen > 0;
+                        datalen -= (CS_INT) len)
+                    {
+                        len = par.Read(buff + invalid_len, sizeof(buff) - invalid_len);
 
-                    if (valid_len < len) {
-                        memmove(buff, buff + valid_len, invalid_len);
+                        valid_len = CStringUTF8::GetValidBytesCount(buff, len);
+                        invalid_len = len - valid_len;
+
+#if 0 //  #if defined(HAVE_WSTRING)
+                        if (x_IsUnicodeClientAPI()) {
+                            CWString unicode_str(buff, valid_len, eEncoding_UTF8);
+
+                            string wcharle_str = MakeUCS2LE(unicode_str.AsUnicode(eEncoding_UTF8));
+
+                            SetHasFailed((Check(
+                                blk_textxfer(
+                                    x_GetSybaseCmd(),
+                                    (CS_BYTE*) wcharle_str.c_str(),
+                                    (CS_INT) wcharle_str.size(),
+                                    0)
+                                ) == CS_FAIL));
+                        } else {
+                            SetHasFailed((Check(blk_textxfer(x_GetSybaseCmd(),
+                                                            (CS_BYTE*) buff,
+                                                            (CS_INT) valid_len,
+                                                            0)
+                                                ) == CS_FAIL));
+                        }
+#else
+                        SetHasFailed((Check(blk_textxfer(x_GetSybaseCmd(),
+                                                        (CS_BYTE*) buff,
+                                                        (CS_INT) valid_len,
+                                                        0)
+                                            ) == CS_FAIL));
+#endif
+
+                        CHECK_DRIVER_ERROR(
+                            HasFailed(),
+                            "blk_textxfer failed for the text/image field." + GetDbgInfo(), 123005
+                            );
+
+                        if (valid_len < len) {
+                            memmove(buff, buff + valid_len, invalid_len);
+                        }
                     }
                 }
             } else if (param.GetType() == eDB_Image) {
                 CDB_Stream& par = dynamic_cast<CDB_Stream&> (param);
 
-                for (datalen = (CS_INT) par.Size();  datalen > 0; datalen -= (CS_INT) len) {
-                    len = par.Read(buff, sizeof(buff));
-
-                    SetHasFailed((Check(blk_textxfer(x_GetSybaseCmd(), (CS_BYTE*) buff, (CS_INT) len, 0)) == CS_FAIL));
+                if (par.IsNULL()) {
+                    // Because of FreeTDS ...
+                    SetHasFailed((Check(blk_textxfer(x_GetSybaseCmd(), (CS_BYTE*) buff, (CS_INT) 0, 0)) == CS_FAIL));
 
                     CHECK_DRIVER_ERROR(
                         HasFailed(),
                         "blk_textxfer failed for the text/image field." + GetDbgInfo(), 123005
                         );
+                } else {
+                    for (datalen = (CS_INT) par.Size();  datalen > 0; datalen -= (CS_INT) len) {
+                        len = par.Read(buff, sizeof(buff));
+
+                        SetHasFailed((Check(blk_textxfer(x_GetSybaseCmd(), (CS_BYTE*) buff, (CS_INT) len, 0)) == CS_FAIL));
+
+                        CHECK_DRIVER_ERROR(
+                            HasFailed(),
+                            "blk_textxfer failed for the text/image field." + GetDbgInfo(), 123005
+                            );
+                    }
                 }
             } else {
                 continue;
