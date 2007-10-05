3156db26a7e038cef967b97473aa05db111a9f1e
Aaron Ucko <ucko@ncbi.nlm.nih.gov>
2007-10-05 14:30:10 +0000

    Run Studio 12 rather than Studio Express prereleases.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@35647 78c7ea69-d796-4a43-9a09-de51944f1b03

--- compilers/WorkShop59.sh
+++ compilers/WorkShop59.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 #############################################################################
 # Setup the local working environment for the "configure" script
-#   Compiler:    Sun C++ 5.9 (Studio Express)
+#   Compiler:    Sun C++ 5.9 (Studio 12)
 #   OS:          Solaris
 #   Processors:  Sparc,  Intel
 #
@@ -10,7 +10,7 @@
 
 
 ## Compiler location and attributes
-WS_BIN="/netopt/studiox/SUNWspro/bin"
+WS_BIN="/netopt/studio12/SUNWspro/bin"
 export WS_BIN
 
 ## Configure using generic script "WorkShop.sh"
