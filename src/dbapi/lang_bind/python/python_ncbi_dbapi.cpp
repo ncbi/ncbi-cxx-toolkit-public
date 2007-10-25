43e164ebed01a5665b3983d65c5ccd4871281c74
ssikorsk <ssikorsk@ncbi.nlm.nih.gov>
2007-10-25 20:31:47 +0000

    Changed the module version from 1.5 to 1.6.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@35860 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/dbapi/lang_bind/python/python_ncbi_dbapi.cpp
+++ src/dbapi/lang_bind/python/python_ncbi_dbapi.cpp
@@ -2902,7 +2902,7 @@ void init_common(const string& module_name)
 
     // Fix plugin manager ...
     CFile file(python::RetrieveModuleFileName());
-    string module_dir = file.GetDir() + "python_ncbi_dbapi/1.5";
+    string module_dir = file.GetDir() + "python_ncbi_dbapi/1.6";
     CDriverManager::GetInstance().AddDllSearchPath(module_dir.c_str());
 
 
