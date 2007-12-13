d54c9a6dbb4942c69ba353b127a8a5da87eb35e2
Aleksey Grichenko <grichenk@ncbi.nlm.nih.gov>
2007-12-13 17:17:59 +0000

    Test both old and new log formats.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@36286 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/corelib/test/Makefile.test_ncbidiag_mt.app
+++ src/corelib/test/Makefile.test_ncbidiag_mt.app
@@ -4,4 +4,5 @@ APP = test_ncbidiag_mt
 SRC = test_ncbidiag_mt
 LIB = xncbi test_mt
 
-CHECK_CMD =
+CHECK_CMD = test_ncbidiag_mt -format old
+CHECK_CMD = test_ncbidiag_mt -format new
--- src/corelib/test/test_ncbidiag_mt.cpp
+++ src/corelib/test/test_ncbidiag_mt.cpp
@@ -49,24 +49,46 @@ public:
     virtual bool Thread_Init(int idx);
     virtual bool Thread_Run(int idx);
 protected:
+    virtual bool TestApp_Args(CArgDescriptions& args);
     virtual bool TestApp_Init(void);
     virtual bool TestApp_Exit(void);
+private:
+    typedef list<string> TStringList;
+
+    void x_TestOldFormat(TStringList& messages);
+    void x_TestNewFormat(TStringList& messages);
 };
 
 
 static char s_Str[k_NumThreadsMax*200] = "";
 static ostrstream s_Sout(s_Str, k_NumThreadsMax*200, ios::out);
 
+static int s_CreateLine = 0;
+static int s_LogLine = 0;
+static int s_ErrLine = 0;
+
+
+bool CTestDiagApp::TestApp_Args(CArgDescriptions& args)
+{
+    args.AddOptionalKey("format", "Format", "Log format",
+        CArgDescriptions::eString);
+    args.SetConstraint("format", &(*new CArgAllow_Strings, "old", "new"));
+    return true;
+}
+
 
 bool CTestDiagApp::Thread_Init(int idx)
 {
+    if (!s_CreateLine) s_CreateLine = __LINE__ + 1;
     LOG_POST("Thread " + NStr::IntToString(idx) + " created");
     return true;
 }
 
 bool CTestDiagApp::Thread_Run(int idx)
 {
+    if (!s_LogLine) s_LogLine = __LINE__ + 1;
     LOG_POST("LOG message from thread " + NStr::IntToString(idx));
+    if (!s_ErrLine) s_ErrLine = __LINE__ + 1;
     ERR_POST("ERROR message from thread " + NStr::IntToString(idx));
     for ( int i = 0; i < 1000000; ++i ) {
         CThreadSystemID::GetCurrent();
@@ -77,10 +99,16 @@ bool CTestDiagApp::Thread_Run(int idx)
 
 bool CTestDiagApp::TestApp_Init(void)
 {
+    const CArgs& args = GetArgs();
+    if ( args["format"] ) {
+        GetDiagContext().SetOldPostFormat(args["format"].AsString() == "old");
+    }
     NcbiCout << NcbiEndl
              << "Testing NCBIDIAG with "
              << NStr::IntToString(s_NumThreads)
-             << " threads..."
+             << " threads ("
+             << (GetDiagContext().IsSetOldPostFormat() ? "old" : "new")
+             << " format)..."
              << NcbiEndl;
     // Output to the string stream -- to verify the result
     SetDiagStream(&s_Sout);
@@ -92,17 +120,34 @@ bool CTestDiagApp::TestApp_Exit(void)
 {
     // Verify the result
     string test_res(s_Str);
-    typedef list<string> string_list;
-    string_list messages;
+    TStringList messages;
 
     // Get the list of messages and check the size
     NStr::Split(test_res, "\r\n", messages);
 
     assert(messages.size() == s_NumThreads*3+1);
 
+    if (GetDiagContext().IsSetOldPostFormat()) {
+        x_TestOldFormat(messages);
+    }
+    else {
+        x_TestNewFormat(messages);
+    }
+
+    // Cleaunp
+    SetDiagStream(0);
+
+    NcbiCout << "Test completed successfully!"
+             << NcbiEndl << NcbiEndl;
+    return true;
+}
+
+
+void CTestDiagApp::x_TestOldFormat(TStringList& messages)
+{
     // Verify "created" messages
     for (unsigned int i=0; i<s_NumThreads; i++) {
-        string_list::iterator it = find(
+        TStringList::iterator it = find(
             messages.begin(),
             messages.end(),
             "Thread " + NStr::IntToString(i) + " created");
@@ -113,7 +158,7 @@ bool CTestDiagApp::TestApp_Exit(void)
 
     // Verify "Error" messages
     for (unsigned int i=0; i<s_NumThreads; i++) {
-        string_list::iterator it = find(
+        TStringList::iterator it = find(
             messages.begin(),
             messages.end(),
             "Error: ERROR message from thread " + NStr::IntToString(i));
@@ -124,7 +169,7 @@ bool CTestDiagApp::TestApp_Exit(void)
 
     // Verify "Log" messages
     for (unsigned int i=0; i<s_NumThreads; i++) {
-        string_list::iterator it = find(
+        TStringList::iterator it = find(
             messages.begin(),
             messages.end(),
             "LOG message from thread " + NStr::IntToString(i));
@@ -132,15 +177,64 @@ bool CTestDiagApp::TestApp_Exit(void)
         messages.erase(it);
     }
     assert(messages.size() == 1);
-
-    // Cleaunp
-    SetDiagStream(0);
-
-    NcbiCout << "Test completed successfully!"
-             << NcbiEndl << NcbiEndl;
-    return true;
 }
 
+
+void CTestDiagApp::x_TestNewFormat(TStringList& messages)
+{
+    CDiagContext::TUID uid = GetDiagContext().GetUID();
+    CDiagContext::TPID pid = GetDiagContext().GetPID();
+    // string app_name = GetProgramDisplayName();
+    int create_msg_count = 0;
+    int err_msg_count = 0;
+    int log_msg_count = 0;
+    int other_msg_count = 0;
+
+    ITERATE(TStringList, it, messages) {
+        SDiagMessage msg(*it);
+        assert(msg.GetUID() == uid);
+        assert(msg.m_PID == pid);
+        SDiagMessage::TTID tid = 0;
+
+        string msg_text(msg.m_Buffer, msg.m_BufferLen);
+        size_t pos = msg_text.find(" created");
+        if (pos != NPOS  &&  msg_text.find("Thread ") == 0) {
+            assert(strcmp(msg.m_Function, "Thread_Init") == 0);
+            assert(msg.m_Line == s_CreateLine);
+            create_msg_count++;
+        }
+        else if (msg_text.find("LOG ") == 0) {
+            pos = msg_text.find_last_of(" ");
+            assert(pos != NPOS);
+            tid = NStr::StringToUInt8(msg_text.substr(pos + 1));
+            assert(msg.m_TID != 0);
+            assert(strcmp(msg.m_Function, "Thread_Run") == 0);
+            assert(msg.m_Line == s_LogLine);
+            log_msg_count++;
+        }
+        else if (msg_text.find("ERROR ") == 0) {
+            pos = msg_text.find_last_of(" ");
+            assert(pos != NPOS);
+            tid = NStr::StringToUInt8(msg_text.substr(pos + 1));
+            assert(msg.m_TID != 0);
+            assert(strcmp(msg.m_Function, "Thread_Run") == 0);
+            assert(msg.m_Line == s_ErrLine);
+            err_msg_count++;
+        }
+        else {
+            other_msg_count++;
+            continue;
+        }
+        assert(msg.m_Severity == eDiag_Error);
+        assert(strcmp(msg.m_File, "test_ncbidiag_mt.cpp") == 0);
+        assert(strcmp(msg.m_Module, "TEST") == 0);
+        assert(strcmp(msg.m_Class, "CTestDiagApp") == 0);
+    }
+    assert(create_msg_count == s_NumThreads);
+    assert(log_msg_count == s_NumThreads);
+    assert(err_msg_count == s_NumThreads);
+    assert(other_msg_count == 1);
+}
 
 
 /////////////////////////////////////////////////////////////////////////////
