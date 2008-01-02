f3652980445fa2bcb7948d39af1bc109fcfb5fce
kazimird <kazimird@ncbi.nlm.nih.gov>
2008-01-02 17:51:42 +0000

    In "common.sh", implemented functions for file and directory installation (with support for rollback) (CXX-177).
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@36420 78c7ea69-d796-4a43-9a09-de51944f1b03

--- scripts/common.sh
+++ scripts/common.sh
@@ -93,6 +93,43 @@ COMMON_Error()
 
 
 #
+#  COMMON_RollbackAndError(...)
+#
+#  Before aborting, executes the rollback procedure if $x_common_rb is
+#  defined, otherwise, this function is equivalent to COMMON_Error.
+#
+
+COMMON_RollbackAndError()
+{
+    test -n "$x_common_rb" && $x_common_rb
+
+    COMMON_Error "$@"
+}
+
+
+#
+#  COMMON_CheckNArgsRB(fn, minn, maxn, arg, ...)
+#
+#  Verifies that the number of arguments (arg, ...) passed
+#  to the "fn" function is >= "minn" and is <= "maxn". Either
+#  of two boundaries can be omitted by specifying an empty string.
+#
+
+COMMON_CheckNArgsRB()
+{
+    fn="$1"; shift
+    minn="$1"; shift
+    maxn="$1"; shift
+
+    test -z "$minn" || test "$#" -ge "$minn" || COMMON_RollbackAndError \
+        "$fn($@): insufficient number of arguments ($# < $minn)"
+
+    test -z "$maxn" || test "$#" -le "$maxn" || COMMON_RollbackAndError \
+        "$fn($@): too many arguments ($# > $maxn)"
+}
+
+
+#
 #  Execute a command;  on error, post error message to STDERR and abort.
 #  NOTE:  call "COMMON_SetupScriptName()" beforehand for nicer diagnostics.
 #
@@ -120,11 +157,217 @@ COMMON_ExecRB()
 
     x_status=$?
     if test $x_status -ne 0 ; then
-        if [ ! -z "$x_common_rb" ]; then
-            $x_common_rb
-        fi
-        COMMON_Error -s $x_status "$@"
+        COMMON_RollbackAndError -s $x_status "$@"
     fi
+}
+
+
+#
+#  COMMON_List_Unshift(listname, value)
+#
+#  Inserts a new element with the specified value at the beginning
+#  of the "listname" list.
+#
+
+COMMON_List_Unshift()
+{
+    COMMON_CheckNArgsRB 'COMMON_List_Unshift' 2 2 "$@"
+
+    listname="$1"
+    value="$2"
+
+    if eval "test -n \"\$$listname\""; then
+        eval "$listname=\"$value
+\$$listname\""
+    else
+        eval "$listname=\"$value\""
+    fi
+}
+
+
+#
+#  COMMON_List_Push(listname, value)
+#
+#  Appends a new element with the specified value to
+#  the "listname" list.
+#
+
+COMMON_List_Push()
+{
+    COMMON_CheckNArgsRB 'COMMON_List_Push' 2 2 "$@"
+
+    listname="$1"
+    value="$2"
+
+    if eval "test -n \"\$$listname\""; then
+        eval "$listname=\"\$$listname
+$value\""
+    else
+        eval "$listname=\"$value\""
+    fi
+}
+
+
+#
+#  COMMON_List_ForEach(listname, fn)
+#
+#  Iterates through the list specified by "listname" and executes
+#  shell command "fn" with list element values as its last arguments.
+#
+
+COMMON_List_ForEach()
+{
+    COMMON_CheckNArgsRB 'COMMON_List_ForEach' 2 2 "$@"
+
+    listname="$1"
+    fn="$2"
+
+    eval "echo \"\$$listname\"" | while read el; do
+        $fn "$el"
+    done
+}
+
+
+#
+#  COMMON_RegisterInstalled(pathname)
+#
+#  Registers the file specified by the "pathname" argument as
+#  freshly installed (that is, previously non-existent).
+#
+
+COMMON_RegisterInstalled()
+{
+    COMMON_CheckNArgsRB 'COMMON_RegisterInstalled' 1 1 "$@"
+
+    COMMON_List_Unshift 'x_common_uninstall' "$1"
+}
+
+
+#
+#  COMMON_RegisterInstalledDir(pathname)
+#
+#  Registers the directory specified by the "pathname" argument as
+#  newly created (that is, previously non-existent).
+#
+
+COMMON_RegisterInstalledDir()
+{
+    COMMON_CheckNArgsRB 'COMMON_RegisterInstalledDir' 1 1 "$@"
+
+    COMMON_List_Unshift 'x_common_uninstall_dirs' "$1"
+}
+
+
+#
+#  COMMON_InstallRB(cmd, src, dest)
+#
+#  Installs the file specified by the "src" pathname to the location
+#  specified by the "dest" file or directory name. The installation
+#  is done by executing the "cmd" command, which can be a shell
+#  command or an external script.
+#
+
+COMMON_InstallRB()
+{
+    COMMON_CheckNArgsRB 'COMMON_InstallRB' 3 3 "$@"
+
+    cmd="$1"
+    src="$2"
+    dest="$3"
+
+    test -r "$src" || COMMON_RollbackAndError "Cannot read '$src'"
+
+    if test -d "$dest"; then
+        destpathname="$dest/`basename "$src"`"
+    else
+        destpathname="$dest"
+    fi
+
+    if test -e "$destpathname"; then
+        COMMON_ExecRB mv -f "$destpathname" "$destpathname.save"
+    fi
+
+    COMMON_ExecRB $cmd "$src" "$dest"
+
+    COMMON_RegisterInstalled "$destpathname"
+}
+
+
+#
+#  COMMON_InstallDirRB(cmd, dir)
+#
+#  Creates the "dir" dirctory by means of executing the "cmd" command.
+#
+
+COMMON_InstallDirRB()
+{
+    COMMON_CheckNArgsRB 'COMMON_InstallDirRB' 2 2 "$@"
+
+    cmd="$1"
+    dir="$2"
+
+    if test ! -d "$dir"; then
+        tmpdirlist=''
+
+        COMMON_List_Push 'tmpdirlist' "$dir"
+        currentdir="$dir"
+        parentdir="`dirname "$currentdir"`"
+
+        while test "$currentdir" != "$parentdir" -a ! -d "$parentdir"; do
+            COMMON_List_Push 'tmpdirlist' "$parentdir"
+            currentdir="$parentdir"
+            parentdir="`dirname "$currentdir"`"
+        done
+
+        COMMON_ExecRB $cmd "$dir"
+
+        COMMON_RegisterInstalledDir "$tmpdirlist"
+    fi
+}
+
+
+#
+#  COMMON_CommitInstall()
+#
+#  Commits changes to the file system made by the COMMON_InstallRB and
+#  COMMON_InstallDirRB functions by removing the .save files and internal
+#  "rollback" information.
+#
+
+COMMON_CommitInstall()
+{
+    x_CommitFile()
+    {
+        rm -f "$1.save"
+    }
+
+    COMMON_List_ForEach 'x_common_uninstall' x_CommitFile 2> /dev/null
+
+    unset x_common_uninstall x_common_uninstall_dirs
+}
+
+
+#
+#  COMMON_RollbackInstall()
+#
+#  Uninstalls files and directories that have been installed so far by
+#  COMMON_InstallRB and COMMON_InstallDirRB.
+#
+
+COMMON_RollbackInstall()
+{
+    x_RollbackFile()
+    {
+        test -e "$1.save" && mv -f "$1.save" "$1" || rm -f "$1"
+    }
+
+    COMMON_List_ForEach 'x_common_uninstall' x_RollbackFile 2> /dev/null
+
+    unset x_common_uninstall
+
+    COMMON_List_ForEach 'x_common_uninstall_dirs' rmdir 2> /dev/null
+
+    unset x_common_uninstall_dirs
 }
 
 
