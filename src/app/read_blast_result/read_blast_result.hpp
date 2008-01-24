1dc39da9344cb7cf555fa0febfe81c94cb10af31
Aaron Ucko <ucko@ncbi.nlm.nih.gov>
2008-01-24 19:21:17 +0000

    -#include <malloc.h> (non-portable; <stdlib.h> should suffice).
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@36664 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/app/read_blast_result/read_blast_result.hpp
+++ src/app/read_blast_result/read_blast_result.hpp
@@ -31,7 +31,6 @@
 * ===========================================================================
 */
 #include <stdio.h>
-#include <malloc.h>
 #include <ctype.h>
 #include <string.h>
 #include <stdlib.h>
