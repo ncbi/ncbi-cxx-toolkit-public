5a8d75158001fb9b726e5fce1463c411c60e39e3
Anton Lavrentiev <lavr@ncbi.nlm.nih.gov>
2008-02-14 16:00:19 +0000

    Traces purified
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@36877 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/connect/ncbi_lb.c
+++ src/connect/ncbi_lb.c
@@ -84,10 +84,10 @@ size_t LB_Select(SERV_ITER     iter,          void*  data,
             char addr[80];
             const char* name = SERV_NameOfInfo(info);
             SOCK_HostPortToString(info->host, info->port, addr, sizeof(addr));
-            CORE_LOGF_X(1, eLOG_Note,
-                        ("%d: %s %s\tR=%lf\tS=%lf\tT=%lf\tA=%lf\tP=%lf",
-                         (int) n, name, addr, info->rate, status, total,
-                         access, point));
+            CORE_LOGF(eLOG_Note,
+                      ("%d: %s %s\tR=%lf\tS=%lf\tT=%lf\tA=%lf\tP=%lf",
+                       (int) n, name, addr, info->rate, status, total,
+                       access, point));
         }}
 #endif /*NCBI_LB_DEBUG*/
     }
@@ -102,11 +102,11 @@ size_t LB_Select(SERV_ITER     iter,          void*  data,
             if (point > 0.0  &&  access > 0.0  &&  total != access) {
                 p = SERV_Preference(iter->pref, access/total, n);
 #ifdef NCBI_LB_DEBUG
-                CORE_LOGF_X(2, eLOG_Note,
-                            ("(P=%lf,\tA=%lf,\tT=%lf,\tA/T=%lf,\tN=%d) "
-                             "-> P=%lf",
-                             iter->pref, access, total, access/total
-                             (int)n, p));
+                CORE_LOGF(eLOG_Note,
+                          ("(P=%lf,\tA=%lf,\tT=%lf,\tA/T=%lf,\tN=%d) "
+                           "-> P=%lf",
+                           iter->pref, access, total, access/total,
+                           (int) n, p));
 #endif /*NCBI_LB_DEBUG*/
                 status = total * p;
                 p = total * (1.0 - p) / (total - access);
@@ -127,10 +127,10 @@ size_t LB_Select(SERV_ITER     iter,          void*  data,
                     status = cand->status;
                     SOCK_HostPortToString(info->host, info->port,
                                           addr, sizeof(addr));
-                    CORE_LOGF_X(3, eLOG_Note,
-                                ("%d: %s %s\tS=%lf\t%.2lf%%",
-                                 (int) i, SERV_NameOfInfo(info), addr,
-                                 p, p / total * 100.0));
+                    CORE_LOGF(eLOG_Note,
+                              ("%d: %s %s\tS=%lf\t%.2lf%%",
+                               (int) i, SERV_NameOfInfo(info), addr,
+                               p, p / total * 100.0));
                 }
 #endif /*NCBI_LB_DEBUG*/
             }
@@ -143,7 +143,7 @@ size_t LB_Select(SERV_ITER     iter,          void*  data,
         if (point <= 0.0  ||  access * (n - 1) < p * 0.01 * (total - access)) {
             point = (total * rand()) / (double) RAND_MAX;
 #ifdef NCBI_LB_DEBUG
-            CORE_LOGF_X(4, eLOG_Note, ("P = %lf", point));
+            CORE_LOGF(eLOG_Note, ("P = %lf", point));
 #endif /*NCBI_LB_DEBUG*/
         }
 
