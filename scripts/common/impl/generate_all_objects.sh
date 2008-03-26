f420746f5402ef409f30d1d0b49779e7f690b89d
Aaron Ucko <ucko@ncbi.nlm.nih.gov>
2008-03-26 18:21:10 +0000

    Track latest developments: xalgognomon.asn is now simply gnomon.asn, and gui_dlg_seq_feat_edit.asn has retired altogether.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@37331 78c7ea69-d796-4a43-9a09-de51944f1b03

--- scripts/common/impl/generate_all_objects.sh
+++ scripts/common/impl/generate_all_objects.sh
@@ -15,11 +15,10 @@ for arg in "$@"; do
 done
 
 for spec in src/serial/test/we_cpp.asn src/objects/*/*.asn \
-  src/objtools/eutils/*/*.dtd \
-  src/algo/gnomon/xalgognomon.asn src/algo/ms/formats/*/*.dtd \
+  src/objtools/eutils/*/*.dtd src/gui/objects/*.asn \
+  src/algo/gnomon/gnomon.asn src/algo/ms/formats/*/*.dtd \
   src/app/project_tree_builder/msvc71_project.dtd \
   src/app/sample/asn/sample_asn.asn src/app/sample/soap/soap_dataobj.xsd \
-  src/gui/dialogs/edit/feature/*.asn src/gui/objects/*.asn \
   src/internal/objects/*/*.asn src/internal/mapview/objects/*/*.asn \
   src/internal/gbench/app/sviewer/objects/*.asn \
   src/internal/gbench/plugins/radar/lib/*.asn ; do
