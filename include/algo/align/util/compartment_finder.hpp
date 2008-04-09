0ac85e163b13f6d08880d843842bea2ebe57f65e
kapustin <kapustin@ncbi.nlm.nih.gov>
2008-04-09 18:06:24 +0000

    Stronger preference for compact models
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@37495 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/algo/align/util/compartment_finder.hpp
+++ include/algo/align/util/compartment_finder.hpp
@@ -266,7 +266,7 @@ private:
 /////////////////////////////////////////////////////////////////////////////
 
 
-const double kPenaltyPerIntronBase = -1e-7; // a small penalty to prefer
+const double kPenaltyPerIntronBase = -2e-5; // a small penalty to prefer
                                             // more compact models among equal
 
 const double kPenaltyPerIntronPos  = -1e-9; // a small penalty to favor uniform
