902e1108ee055517f210c3f583bd5f1ce448ae90
Aaron Ucko <ucko@ncbi.nlm.nih.gov>
2008-04-13 23:59:09 +0000

    Correct invocation of the actual binary to use $CHECK_EXEC and abstain from ./, which, while good practice on Unix, breaks on Windows. :-/
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@37522 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/cgi/test/test_cgi_entry_reader.sh
+++ src/cgi/test/test_cgi_entry_reader.sh
@@ -4,4 +4,4 @@ CONTENT_LENGTH=`wc -c < $1 | tr -d ' '`
 #CONTENT_LENGTH=-1
 REQUEST_METHOD=POST
 export CONTENT_TYPE CONTENT_LENGTH REQUEST_METHOD
-exec ./test_cgi_entry_reader < $1
+exec $CHECK_EXEC test_cgi_entry_reader < $1
