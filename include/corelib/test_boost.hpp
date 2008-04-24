8a47a577aca6e02f266d4671885bbcf0d3905b81
ivanovp <ivanovp@ncbi.nlm.nih.gov>
2008-04-24 18:17:30 +0000

    New library with different utility functions and classes for using BOOST.Test. Just one utility class for now - special reporter to add information about disabled tests to standard boost report. JIRA: CXX-352
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@37655 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ include/corelib/test_boost.hpp
@@ -0,0 +1,78 @@
+#ifndef CORELIB___TEST_BOOST__HPP
+#define CORELIB___TEST_BOOST__HPP
+
+/*  $Id$
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Author:  Pavel Ivanov
+ *
+ */
+
+/// @file test_boost.hpp
+/// Utility stuff for more convenient using of Boost.Test library.
+///
+/// This header must be included after all Boost.Test headers.
+
+
+#include <corelib/ncbistd.hpp>
+
+#include <boost/test/unit_test.hpp>
+
+#include <string>
+
+
+/** @addtogroup Tests
+ *
+ * @{
+ */
+
+
+BEGIN_NCBI_SCOPE
+
+/// Redefinition of the name of global function used in Boost.Test for
+/// initialization. It is called now from NCBI wrapper function called before
+/// user-defined.
+#define init_unit_test_suite  NcbiInitUnitTestSuite
+
+
+/// Define some test as disabled in current configuration
+/// This function should be called for every existing test which is not added
+/// to Boost's test suite for execution. Such tests will be included in
+/// detailed report given by Boost after execution.
+///
+/// @param test_name
+///   Name of the disabled test
+/// @param reason
+///   Reason of test disabling. It will be printed in the report.
+extern void NcbiBoostTestDisable(const string&  test_name,
+                                 const string&  reason = "");
+
+
+END_NCBI_SCOPE
+
+
+/* @} */
+
+#endif  /* CORELIB___TEST_BOOST__HPP */
---
+++ src/corelib/test_boost.cpp
@@ -0,0 +1,229 @@
+/*  $Id$
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Author:  Pavel Ivanov
+ *
+ * File Description:
+ *   Implementation of special reporter for Boost.Test framework and utility
+ *   functions for embedding it into the Boost.
+ *
+ */
+
+#include <ncbi_pch.hpp>
+
+#include <boost/test/results_reporter.hpp>
+#include <boost/test/output/plain_report_formatter.hpp>
+#include <boost/test/output/xml_report_formatter.hpp>
+#include <boost/test/detail/unit_test_parameters.hpp>
+
+#include <corelib/ncbienv.hpp>
+#include <corelib/test_boost.hpp>
+#undef init_unit_test_suite
+
+#include <list>
+
+#include <common/test_assert.h>  /* This header must go last */
+
+
+/// Prototype of global user-defined initialization function
+extern boost::unit_test::test_suite*
+NcbiInitUnitTestSuite( int argc, char* argv[] );
+
+
+BEGIN_NCBI_SCOPE
+
+// Some shortening typedefs
+typedef boost::unit_test::results_reporter::format  TBoostFormatter;
+typedef boost::unit_test::test_unit                 TBoostTestUnit;
+
+
+/// Reporter for embedding in Boost framework and adding non-standard
+/// information to detailed report given by Boost.
+class CNcbiBoostReporter : public TBoostFormatter
+{
+public:
+    /// Create reporter tuned for printing report of specific format
+    ///
+    /// @param format
+    ///   Format of the report
+    CNcbiBoostReporter(boost::unit_test::output_format format);
+    virtual ~CNcbiBoostReporter(void);
+
+    /// Add new test marked as disabled
+    ///
+    /// @param test_name
+    ///   Name of the disabled test
+    /// @param reason
+    ///   Reason of test disabling. It will be printed in the report.
+    void AddDisabledTest(const string& test_name, const string& reason);
+
+    // TBoostFormatter interface
+    virtual
+    void results_report_start(std::ostream& ostr);
+    virtual
+    void results_report_finish(std::ostream& ostr);
+    virtual
+    void test_unit_report_start(TBoostTestUnit const& tu, std::ostream& ostr);
+    virtual
+    void test_unit_report_finish(TBoostTestUnit const& tu, std::ostream& ostr);
+    virtual
+    void do_confirmation_report(TBoostTestUnit const& tu, std::ostream& ostr);
+
+private:
+    typedef list< pair<string, string> >  TDisabledList;
+
+    /// If reporter already printed info about disabled tests or not
+    bool                       m_DisabledPrinted;
+    /// Standard reporter from Boost for particular report format
+    auto_ptr<TBoostFormatter>  m_Upper;
+    /// If report is XML or not
+    bool                       m_IsXML;
+    /// List of disabled tests
+    TDisabledList              m_DisabledList;
+};
+
+
+CNcbiBoostReporter::CNcbiBoostReporter(boost::unit_test::output_format format)
+    : m_DisabledPrinted(false)
+{
+    if (format == boost::unit_test::XML) {
+        m_IsXML = true;
+        m_Upper.reset(new boost::unit_test::output::xml_report_formatter());
+    }
+    else {
+        m_IsXML = false;
+        m_Upper.reset(new boost::unit_test::output::plain_report_formatter());
+    }
+}
+
+CNcbiBoostReporter::~CNcbiBoostReporter(void)
+{
+}
+
+void
+CNcbiBoostReporter::AddDisabledTest(const string&  test_name,
+                                    const string&  reason)
+{
+    m_DisabledList.push_back(make_pair(test_name, reason));
+}
+
+void
+CNcbiBoostReporter::results_report_start(std::ostream& ostr)
+{
+    m_Upper->results_report_start(ostr);
+}
+
+void
+CNcbiBoostReporter::results_report_finish(std::ostream& ostr)
+{
+    m_Upper->results_report_finish(ostr);
+}
+
+void
+CNcbiBoostReporter::test_unit_report_start(TBoostTestUnit const&  tu,
+                                           std::ostream&          ostr)
+{
+    if (! m_DisabledPrinted) {
+        ITERATE(TDisabledList, it, m_DisabledList) {
+            if (m_IsXML) {
+                ostr << "<TestCase name=\"" << it->first << "\" result=\"disabled\"";
+                if (! it->second.empty()) {
+                    ostr << " reason=\"" << it->second << "\"";
+                }
+                ostr << "/>";
+            }
+            else {
+                ostr << "Test case \"" << it->first << "\" disabled";
+                if (! it->second.empty()) {
+                    ostr << " because " << it->second;
+                }
+                ostr << '\n';
+            }
+        }
+
+        m_DisabledPrinted = true;
+    }
+
+    m_Upper->test_unit_report_start(tu, ostr);
+}
+
+void
+CNcbiBoostReporter::test_unit_report_finish(TBoostTestUnit const&  tu,
+                                            std::ostream&          ostr)
+{
+    m_Upper->test_unit_report_finish(tu, ostr);
+}
+
+void
+CNcbiBoostReporter::do_confirmation_report(TBoostTestUnit const&  tu,
+                                           std::ostream&          ostr)
+{
+    m_Upper->do_confirmation_report(tu, ostr);
+}
+
+
+
+/// The singleton object for reporter
+CNcbiBoostReporter* s_NcbiReporter = NULL;
+
+
+void NcbiBoostTestDisable(const string&  test_name,
+                          const string&  reason)
+{
+    // reporter must be already created by init_unit_test_suite()
+    assert(s_NcbiReporter);
+
+    s_NcbiReporter->AddDisabledTest(test_name, reason);
+}
+
+
+END_NCBI_SCOPE
+
+
+// Global initialization function called from Boost framework
+boost::unit_test::test_suite* init_unit_test_suite( int argc, char* argv[] )
+{
+    // This function should not be called yet
+    assert(! NCBI_NS_NCBI::s_NcbiReporter);
+
+    boost::unit_test::output_format format =
+                            boost::unit_test::runtime_config::report_format();
+
+    NCBI_NS_NCBI::CNcbiEnvironment env;
+    string is_autobuild = env.Get("NCBI_AUTOMATED_BUILD");
+    if (! is_autobuild.empty()) {
+        format = boost::unit_test::XML;
+        boost::unit_test::results_reporter::set_level(
+                                           boost::unit_test::DETAILED_REPORT);
+    }
+
+    NCBI_NS_NCBI::s_NcbiReporter =
+                                new NCBI_NS_NCBI::CNcbiBoostReporter(format);
+
+    boost::unit_test::results_reporter::set_format(
+                                                NCBI_NS_NCBI::s_NcbiReporter);
+
+    return NcbiInitUnitTestSuite(argc, argv);
+}
