d68eccff6b4865ea9d8d90ce25d6e8558e6308e5
Aaron Ucko <ucko@ncbi.nlm.nih.gov>
2008-05-06 14:27:25 +0000

    Store the ultimate output in a temporary file to reduce the likelihood that it will end up interleaved with other files' output.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@37807 78c7ea69-d796-4a43-9a09-de51944f1b03

--- compilers/unix/cxx_filter.MIPSpro73.sh
+++ compilers/unix/cxx_filter.MIPSpro73.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+# Store the ultimate output in a temporary file to reduce the
+# likelihood that it will end up interleaved with other files' output.
+tmpout=${TMP:-/tmp}/ncbicxx.$$
+trap "rm $tmpout" 0 1 2 15
+
 awk 'BEGIN { keep=1 }
 /^[^ ]/ { keep=1 }
 /^cc-[0-9]+ CC: (REMARK|WARNING) File = \/usr\/include/ { keep=0 }
@@ -7,4 +12,8 @@ keep
 BEGIN { status=0 }
 /^cc-[0-9]+ CC: ERROR/ { status=1 }
 /^CC ERROR:/ { status=1 }
-END { exit status }'
+END { exit status }' > $tmpout
+status=$?
+
+cat $tmpout
+exit $status
