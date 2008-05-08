c21a108166bce331bfa768cd7fc4c46a0af81a00
kapustin <kapustin@ncbi.nlm.nih.gov>
2008-05-08 23:56:53 +0000

    Prefer confirmed non-consensus splices among equally scoring choices
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@37840 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/algo/align/nw/nw_spliced_aligner.cpp
+++ src/algo/align/nw/nw_spliced_aligner.cpp
@@ -33,7 +33,9 @@
 
 
 #include <ncbi_pch.hpp>
+
 #include "messages.hpp"
+#include <algo/align/nw/nw_formatter.hpp>
 #include <algo/align/nw/nw_spliced_aligner.hpp>
 #include <algo/align/nw/align_exception.hpp>
 
@@ -99,4 +101,109 @@ bool CSplicedAligner::x_CheckMemoryLimit()
 }
 
 
+size_t GetSplicePriority(const  char * dnr, const char* acc)
+{
+    // GA-AG, GT-TG
+    const size_t rv (dnr[0] == 'G' && acc[1] == 'G' && dnr[1] == acc[0] &&
+                     (dnr[1] == 'A' || dnr[1] == 'T')? 1: 0);
+    return rv;
+}
+
+
+// Prefer experimentally verified non-consensus
+// splices among equally scoring alternatives
+void CSplicedAligner::CheckPreferences(void)
+{
+    if(m_Transcript.size() == 0) {
+        NCBI_THROW(CAlgoAlignException, eNoSeqData, g_msg_NoAlignment);
+        
+    }
+    
+    const char * p1 (GetSeq1()), * p2 (GetSeq2());
+    for(int t (m_Transcript.size() - 1), csq_matches(0); t >= 0; --t) {
+        
+        switch(m_Transcript[t]) {
+            
+        case eTS_Match:
+            ++csq_matches;
+            ++p1;
+            ++p2;
+            break;
+            
+        case eTS_Replace:
+            csq_matches = 0;
+            ++p1;
+            ++p2;
+            break;
+            
+        case eTS_Insert:
+        case eTS_SlackInsert:
+            csq_matches = 0;
+            ++p2;
+            break;
+            
+        case eTS_Delete:
+        case eTS_SlackDelete:
+            csq_matches = 0;
+            ++p1;
+            break;
+            
+        case eTS_Intron:
+            int ti (t - 1);
+            for(; ti >= 0 && m_Transcript[ti] == eTS_Intron; --ti);
+            const int ilen (t - ti);
+            const char * p2e (p2 + ilen);
+            if(CNWFormatter::SSegment::s_IsConsensusSplice(p2, p2e - 2, true)) {
+                p2 += ilen;
+                t = ti + 1;
+            }
+            else {
+
+                size_t maxpr (0), jmaxpr (0);
+                // explore the left of the splice
+                int j;
+                for(j = -1; j >= -csq_matches && *(p1 + j) == *(p2e + j); --j) {
+                    const size_t pr (GetSplicePriority(p2 + j, p2e + j - 2));
+                    if(pr > maxpr) {
+                        maxpr = pr;
+                        jmaxpr = j;
+                    }
+                }
+
+                // explore the right of the splice
+                for(j = 1; j <= ti + 1 && m_Transcript[ti - j + 1] == eTS_Match
+                        && *(p1 + j - 1) == *(p2 + j - 1); ++j)
+                    {
+                        const size_t pr (GetSplicePriority(p2 + j, p2e + j - 2));
+                        if(pr > maxpr) {
+                            maxpr = pr;
+                            jmaxpr = j;
+                        }
+                    }
+
+                if(jmaxpr == 0) {
+                    p2 += ilen;
+                    t = ti + 1;
+                }
+                else {
+                    // adjust the splice site
+                    const int incr (jmaxpr < 0? -1: 1);
+                    for(int k (0); k != jmaxpr; k += incr) {
+                        swap(m_Transcript[t-k], m_Transcript[ti-k]);
+                    }
+                    t = ti - jmaxpr + 1;
+                    p1 += jmaxpr;
+                    p2 = p2 + ilen + jmaxpr;
+                }
+            }
+            csq_matches = 0;
+            break;
+            
+        default:
+            NCBI_THROW(CAlgoAlignException, eInternal, "Unexpected transcript symbol");
+        }
+    }
+}
+
+    
 END_NCBI_SCOPE
