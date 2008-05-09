a46692e61e967d98b04606659f42b6f327f34eff
Andrei Gourianov <gouriano@ncbi.nlm.nih.gov>
2008-05-09 14:53:53 +0000

    Corrected server URL
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@37845 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/app/sample/soap/client/soap_client_sample.cpp
+++ src/app/sample/soap/client/soap_client_sample.cpp
@@ -66,7 +66,8 @@ CSampleSoapClient::CSampleSoapClient(void)
     : CSoapHttpClient(
 // This Server URL is valid only within NCBI
 //        "http://graceland.ncbi.nlm.nih.gov:6224/staff/gouriano/cgi/samplesoap/samplesoap.cgi",
-        "http://intranet.ncbi.nlm.nih.gov/staff/gouriano/cgi/samplesoap/samplesoap.cgi",
+//        "http://intranet.ncbi.nlm.nih.gov/staff/gouriano/cgi/samplesoap/samplesoap.cgi",
+        "http://intrawebdev:6224/staff/gouriano/samplesoap/samplesoap.cgi",
         "http://ncbi.nlm.nih.gov/")
 {
 // Register incoming object types
