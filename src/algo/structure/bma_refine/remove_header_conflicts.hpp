608a2793e98bbbf1bd101afc9b4cfcf79d527e0c
Paul Thiessen <thiessen@ncbi.nlm.nih.gov>
2008-05-20 21:33:38 +0000

    copy files rather than "weird" #includes
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@37954 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ src/algo/structure/bma_refine/remove_header_conflicts.hpp
@@ -0,0 +1,38 @@
+/*  $Id$
+* ===========================================================================
+*
+*                            PUBLIC DOMAIN NOTICE
+*               National Center for Biotechnology Information
+*
+*  This software/database is a "United States Government Work" under the
+*  terms of the United States Copyright Act.  It was written as part of
+*  the author's official duties as a United States Government employee and
+*  thus cannot be copyrighted.  This software/database is freely available
+*  to the public for use. The National Library of Medicine and the U.S.
+*  Government have not placed any restriction on its use or reproduction.
+*
+*  Although all reasonable efforts have been taken to ensure the accuracy
+*  and reliability of the software and data, the NLM and the U.S.
+*  Government do not and cannot warrant the performance or results that
+*  may be obtained by using this software or data. The NLM and the U.S.
+*  Government disclaim all warranties, express or implied, including
+*  warranties of performance, merchantability or fitness for any particular
+*  purpose.
+*
+*  Please cite the author in any work or product based on this material.
+*
+* ===========================================================================
+*
+* Authors:  Paul Thiessen
+*
+* File Description:
+*      work around some symbol conflicts between ncbi and wx
+*
+* ===========================================================================
+*/
+
+// ncbi uses #args, wx simply 0/1
+#ifdef HAVE_READDIR_R
+#undef HAVE_READDIR_R
+#define HAVE_READDIR_R 1
+#endif
