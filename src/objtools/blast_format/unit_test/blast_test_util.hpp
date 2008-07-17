59bc58b4209c5b47bceaf1a3cb3ab12c44217a8c
Christiam Camacho <camacho@ncbi.nlm.nih.gov>
2008-07-17 20:15:07 +0000

    Fixes for irix build
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@38575 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/blast_format/unit_test/blast_test_util.hpp
+++ src/objtools/blast_format/unit_test/blast_test_util.hpp
@@ -72,7 +72,7 @@ namespace ncbi {
 namespace TestUtil {
 
 // Random integer generator for use with std::generate
-#if defined(__ICC)
+#if defined(__ICC) || defined(NCBI_OS_IRIX)
 template <int lowest_value = 0, int highest_value = INT_MAX>
 #else
 template <int lowest_value = 0, int highest_value = ncbi::CRandom::GetMax()>
