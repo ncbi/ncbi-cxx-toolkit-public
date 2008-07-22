44ee11207da1877144324b9360f866a59519a0a3
Christiam Camacho <camacho@ncbi.nlm.nih.gov>
2008-07-22 21:41:01 +0000

    In progress
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@38632 78c7ea69-d796-4a43-9a09-de51944f1b03

--- scripts/projects/blast/post_build/rpm/make_rpm.py
+++ scripts/projects/blast/post_build/rpm/make_rpm.py
@@ -4,20 +4,20 @@
 
 import sys, os, shutil;
 
+verbose = False
+
 # Current version of BLAST
-blast_version = "2.2.18"
+BLAST_VERSION = "2.2.18"
 # Name of the temporary rpmbuild directory
-rpmbuild_home = "rpmbuild"
-# NCBI SVN repository
-svn_ncbi = "https://svn.ncbi.nlm.nih.gov/repos/toolkit"
-package_name = "ncbi-blast-" + blast_version + "+"
-# Name of the source tarball to create
-tarball = package_name + ".tgz"
+RPMBUILD_HOME = "rpmbuild"
+PACKAGE_NAME = "ncbi-blast-" + blast_version + "+"
+# Name of the source TARBALL to create
+TARBALL = PACKAGE_NAME + ".tgz"
+RPM_SPEC = "ncbi-blast.spec"
 
 def setup_rpmbuild():
     """ Prepare local rpmbuild directory. """
-    global rpmbuild_home
-    shutil.rmtree(rpmbuild_home)
+    cleanup_rpm()
     for d in [ 'BUILD', 'SOURCES', 'SPECS', 'SRPMS', 'tmp', 'RPMS' ]:
         os.mkdir(d)
     cwd = os.getcwd()
@@ -30,21 +30,92 @@ def setup_rpmbuild():
     fname = os.path.join(os.path.expanduser("~"), ".rpmmacros");
     try:
         f = open(fname, "w")
-        print >> f, "%_topdir %( echo", rpmbuild_home, ")"
+        print >> f, "%_topdir %( echo", RPMBUILD_HOME, ")"
         print >> f, "%_tmppath %{_topdir}/tmp"
         print >> f
         print >> f, "%packager Christiam E. Camacho (camacho@ncbi.nlm.nih.gov)"
     finally:
         f.close()
+    if verbose: print "Created ~/.rpmmacros"
+
+def cleanup_rpm():
+    """ Delete rpm files """
+    shutil.rmtree(RPMBUILD_HOME)
+    os.remove(os.path.join(os.path.expanduser("~"), ".rpmmacros"))
+
+def safe_exec(cmd):
+    """ Executes a command and checks its return value, throwing an
+        exception if it fails.
+    """
+    from subprocess import *
+    if verbose: print cmd
+    try:
+        retcode = call(cmd, shell=True)
+        if retcode < 0:
+            raise RuntimeError("Child was terminated by signal " + -retcode)
+    except OSError, e:
+        raise RuntimeError("Execution failed: " + e)
+
+def svn_checkout():
+    # NCBI SVN repository
+    SVN_NCBI = "https://svn.ncbi.nlm.nih.gov/repos_htpasswd/toolkit"
+
+    # Check out the sources
+    cmd = "svn co --username svnread --password allowed " + SVN_NCBI
+    cmd += "/release/blast/" + BLAST_VERSION + PACKAGE_NAME
+    if os.path.exists(PACKAGE_NAME):
+        shutil.rmtree(PACKAGE_NAME)
+    safe_exec(cmd)
+
+    # Remove unneeded directories
+    shutil.rmtree(os.path.join(PACKAGE_NAME, "builds")
+    shutil.rmtree(os.path.join(PACKAGE_NAME, "scripts")
+    cmd = "find " + PACKAGE_NAME + " -type d -name .svn | xargs rm -fr "
+    safe_exec(cmd)
+
+def compress_sources():
+    import tarfile
+    tar = TarFile(TARBALL, "w:bz2")
+    tar.add(PACKAGE_NAME)
+    tar.close()
 
 def cleanup():
     """ Remove all files created. """
-    global tarball
-    os.remove(tarball)
-    shutil.rmtree(package_name)
+    os.remove(TARBALL)
+    shutil.rmtree(PACKAGE_NAME)
+
+def run_rpm():
+    shutil.rmtree(PACKAGE_NAME)
+    shutil.move(TARBALL, os.path.join(RPMBUILD_HOME, "SOURCES"))
+    dest_spec = os.path.join(RPMBUILD_HOME, "SPECS", RPM_SPEC)
+    shutil.move(RPM_SPEC, dest_spec)
+    cmd = "rpmbuild -ba " + dest_spec
+    safe_exec(cmd)
+
+def move_rpms_to_installdir(installdir)
+
+    installer_dir = os.path.join(installdir, "installer")
+    if not os.path.exists(installer_dir):
+        os.makedirs(installer_dir)
+
+    cmd = "find " + RPMBUILD_HOME + " -name \"*.rpm\" | xargs 
 
 def main():
+    """ Creates RPMs for linux. """
+    parser = OptionParser(sys.argv[0] + " <installation directory>")
+    parser.add_option("-v", "--verbose", action="store_true", default=False,
+                      help="Show verbose output")
+    options, args = parser.parse_args()
+    installdir = args
+    global verbose
+    verbose = options.verbose
     setup_rpmbuild()
+    cleanup()
+    svn_checkout()
+    compress_sources()
+    run_rpm()
+    move_rpms_to_installdir(installdir)
+    cleanup_rpm()
     
 if __name__ == "__main__":
     sys.exit(main())
