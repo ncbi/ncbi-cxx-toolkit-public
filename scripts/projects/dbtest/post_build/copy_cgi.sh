f274b61cabcfe69adb9ef3a00878f1dbe29eebfc
ivanovp <ivanovp@ncbi.nlm.nih.gov>
2008-07-28 20:34:50 +0000

    Create release infrastructure for dbtest applications. JIRA: CXX-584
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@38736 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ scripts/projects/dbtest/post_build/copy_cgi.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+BIN_DIR=$1
+SRC_DIR=$2
+INSTALL_DIR=$3
+
+CGI_NAME="test_stat_ext.cgi"
+CGI_PATH="internal/cppcore/test_stat_ext/cgi"
+CGI_XSL_DIR="xsl"
+CGI_INIFILE_NAME="test_stat_ext.cgi.ini"
+
+
+if [ ! -d "$BIN_DIR" -o ! -d "$INSTALL_DIR" ]; then
+    echo "Parameters given ('$BIN_DIR' and '$INSTALL_DIR') are not directories" >&2
+    exit 1
+fi
+
+if [ -f "$BIN_DIR/$CGI_NAME" ]; then
+    cp    "$BIN_DIR/$CGI_NAME" "$INSTALL_DIR/" || exit 2
+    cp -R "$SRC_DIR/$CGI_PATH/$CGI_XSL_DIR" "$INSTALL_DIR/" || exit 3
+    cp    "$SRC_DIR/$CGI_PATH/$CGI_INIFILE_NAME" "$INSTALL_DIR/" || exit 4
+fi
+
+exit 0
