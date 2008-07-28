f5f5f98d2bf6e22d14c6610136dc9f98a0db6daa
Jonathan Kans <kans@ncbi.nlm.nih.gov>
2008-07-28 17:16:09 +0000

    initial checkin
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@38731 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ src/app/streamtest/process_fasta.hpp
@@ -0,0 +1,98 @@
+/*
+* ===========================================================================
+*
+*                            PUBLIC DOMAIN NOTICE
+*               National Center for Biotechnology Information
+*
+*  This software/database is a "United States Government Work" under the
+*  terms of the United States Copyright Act.  It was written as part of
+*  the author's official duties as a United States Government employee and
+*  thus cannot be copyrighted.  This software/database is freely available
+*  to the public for use. The National Library of Medicine and the U.S.
+*  Government have not placed any restriction on its use or reproduction.
+*
+*  Although all reasonable efforts have been taken to ensure the accuracy
+*  and reliability of the software and data, the NLM and the U.S.
+*  Government do not and cannot warrant the performance or results that
+*  may be obtained by using this software or data. The NLM and the U.S.
+*  Government disclaim all warranties, express or implied, including
+*  warranties of performance, merchantability or fitness for any particular
+*  purpose.
+*
+*  Please cite the author in any work or product based on this material.
+*
+* ===========================================================================
+*
+* Author:
+*
+* File Description:
+*
+* ===========================================================================
+*/
+
+#ifndef __process_fasta__hpp__
+#define __process_fasta__hpp__
+
+//  ============================================================================
+class CFastaProcess
+//  ============================================================================
+    : public CSeqEntryProcess
+{
+public:
+    //  ------------------------------------------------------------------------
+    CFastaProcess()
+    //  ------------------------------------------------------------------------
+        : CSeqEntryProcess()
+        , m_out( 0 )
+    {};
+
+    //  ------------------------------------------------------------------------
+    ~CFastaProcess()
+    //  ------------------------------------------------------------------------
+    {
+    };
+
+    //  ------------------------------------------------------------------------
+    void ProcessInitialize(
+        const CArgs& args )
+    //  ------------------------------------------------------------------------
+    {
+        m_out = new CFastaOstream( args["o"].AsOutputFile() );
+    };
+
+    //  ------------------------------------------------------------------------
+    void ProcessFinalize()
+    //  ------------------------------------------------------------------------
+    {
+        delete m_out;
+    }
+
+    //  ------------------------------------------------------------------------
+    virtual void SeqEntryInitialize(
+        CRef<CSeq_entry>& se )
+    //  ------------------------------------------------------------------------
+    {
+        CSeqEntryProcess::SeqEntryInitialize( se );
+    };
+
+    //  ------------------------------------------------------------------------
+    void SeqEntryProcess()
+    //  ------------------------------------------------------------------------
+    {
+        try {
+            FOR_ALL_BIOSEQS_WITHIN_SEQENTRY (bit, *m_entry) {
+                const CBioseq& bioseq = *bit;
+                m_out->Write( bioseq, 0, true );
+                ++m_objectcount;
+            }
+        }
+        catch (CException& e) {
+            LOG_POST(Error << "error processing seqentry: " << e.what());
+        }
+    };
+
+protected:
+    CFastaOstream* m_out;
+};
+
+#endif
