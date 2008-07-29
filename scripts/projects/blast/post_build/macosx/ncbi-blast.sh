afc2d3107d54f89a75ff22a7b77a8d4904660689
Christiam Camacho <camacho@ncbi.nlm.nih.gov>
2008-07-29 19:12:55 +0000

    Fixes for macosx
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@38752 78c7ea69-d796-4a43-9a09-de51944f1b03

--- scripts/projects/blast/post_build/macosx/ncbi-blast.sh
+++ scripts/projects/blast/post_build/macosx/ncbi-blast.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 INSTALLDIR=$1
-SRCDIR=$2
+SCRIPTDIR=$2
 BLAST_VERSION=$3
 PRODUCT="ncbi-blast-$BLAST_VERSION+"
 
 if [ $# -ne 3 ] ; then
-    echo "Usage: ncbi-blast.sh [installation directory] [source_directory_for_binaries] [BLAST version]";
+    echo "Usage: ncbi-blast.sh [installation directory] [MacOSX post-build script directory] [BLAST version]";
     exit 1;
 fi
 
@@ -15,31 +15,32 @@ MASKING_BINS="windowmasker dustmasker segmasker"
 DB_BINS="blastdbcmd makeblastdb makembindex"
 ALL_BINS="$BLAST_BINS $MASKING_BINS $DB_BINS"
 
-rm -rf $PRODUCT.dmg
+rm -rf $PRODUCT.dmg $PRODUCT _stage $INSTALLDIR/installer
+mkdir -p _stage/bin _stage/doc
 
-rm -rf _stage
-mkdir _stage
-mkdir _stage/bin
-mkdir _stage/doc
+# This is needed because the binary ncbi-blast.pmproj has this string hard
+# coded
+cp -p $INSTALLDIR/LICENSE ../license.txt
+for f in large-Blue_ncbi_logo.tiff ncbi-blast.pmproj welcome.txt; do
+    echo copying $f to local directory
+    cp -p $SCRIPTDIR/$f .
+done
 
-for bin in $ALL_BINS
-do
-echo copying $bin
-cp -p $INSTALLDIR/bin/$bin _stage/bin
+for bin in $ALL_BINS; do
+    echo copying $bin
+    cp -p $INSTALLDIR/bin/$bin _stage/bin
 done
 
 echo building package
-rm -rf BLAST
-mkdir BLAST
-/Developer/Tools/packagemaker -build -proj ncbi-blast.pmproj -p BLAST/$PRODUCT.pkg
+mkdir $PRODUCT
+/Developer/Tools/packagemaker -build -proj ncbi-blast.pmproj -p $PRODUCT/$PRODUCT.pkg
 
 echo creating disk image
-hdiutil create $PRODUCT.dmg -srcfolder BLAST
+hdiutil create $PRODUCT.dmg -srcfolder $PRODUCT
 
 echo moving disk image
 mkdir $INSTALLDIR/installer
 mv $PRODUCT.dmg $INSTALLDIR/installer
 
 echo done
-rm -rf _stage
-rm -rf BLAST
+rm -rf _stage $PRODUCT
