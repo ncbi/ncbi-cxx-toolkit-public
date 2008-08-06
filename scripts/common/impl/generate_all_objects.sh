e05322b5d42d745919ace60401cf994e58bbc18d
Aaron Ucko <ucko@ncbi.nlm.nih.gov>
2008-08-06 17:39:50 +0000

    Belatedly adjust path to project_tree_builder/msvc71_project.dtd. Less belatedly, cover internal/blast/SplitDB/asn if present.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@38871 78c7ea69-d796-4a43-9a09-de51944f1b03

--- scripts/common/impl/generate_all_objects.sh
+++ scripts/common/impl/generate_all_objects.sh
@@ -17,11 +17,13 @@ done
 for spec in src/serial/test/we_cpp.asn src/objects/*/*.asn \
   src/objtools/eutils/*/*.dtd src/gui/objects/*.asn \
   src/algo/gnomon/gnomon.asn src/algo/ms/formats/*/*.dtd \
-  src/app/project_tree_builder/msvc71_project.dtd \
+  src/build-system/project_tree_builder/msvc71_project.dtd \
   src/app/sample/asn/sample_asn.asn src/app/sample/soap/soap_dataobj.xsd \
   src/internal/objects/*/*.asn src/internal/mapview/objects/*/*.asn \
   src/internal/gbench/app/sviewer/objects/*.asn \
-  src/internal/gbench/plugins/radar/lib/*.asn ; do
+  src/internal/gbench/plugins/radar/lib/*.asn \
+  src/internal/blast/SplitDB/asn*/*.asn \
+  src/internal/blast/SplitDB/BlastdbInfo/asn/*.asn ; do
     if test -f "$spec"; then
         case $spec in
             */seq_annot_ref.asn ) continue ;; # sample data, not a spec
@@ -42,5 +44,14 @@ for spec in src/serial/test/we_cpp.asn src/objects/*/*.asn \
         echo "Warning: $spec not found"
     fi
 done
+
+splitdb_dir=src/internal/blast/SplitDB/asn
+if [ -f $splitdb_dir/Makefile.asntool ]; then
+    if $force || [ ! -f $splitdb_dir/objPSSM.c ]; then
+        top_srcdir=`pwd`
+        subsubdir=$top_srcdir/src/internal
+        (cd $splitdb_dir && ${MAKE-make} -f Makefile.asntool sources top_srcdir=$top_srcdir builddir=$subsubdir) || exit $?
+    fi
+fi
 
 exit 0
