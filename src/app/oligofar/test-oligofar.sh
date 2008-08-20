2656103cc71f5775d955f3533bb502f8d330f5d8
rotmistr <rotmistr@ncbi.nlm.nih.gov>
2008-08-20 23:37:13 +0000

    Fixed to actually run internal tests
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@39031 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/app/oligofar/test-oligofar.sh
+++ src/app/oligofar/test-oligofar.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-./oligofar -VT+ || exit 1
-./oligofar -i NM_012345.reads -d NM_012345.fa -o NM_012345.reads.test -w9 -n1 -L2G || exit 11
-./oligofar -i NM_012345.pairs -d NM_012345.fa -o NM_012345.pairs.test -w9 -n1 -L2G || exit 12
+./oligofar -T+ -i NM_012345.reads -d NM_012345.fa -o NM_012345.reads.test -w9 -n1 -L2G || exit 11
+./oligofar -T+ -i NM_012345.pairs -d NM_012345.fa -o NM_012345.pairs.test -w9 -n1 -L2G || exit 12
 diff NM_012345.reads.test NM_012345.reads.out > /dev/null || exit 21
 diff NM_012345.pairs.test NM_012345.pairs.out > /dev/null || exit 22
 rm NM_012345.reads.test NM_012345.pairs.test
