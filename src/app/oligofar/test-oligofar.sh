5eecb77c62bdd8cd5faf4b0bb774ff28dc0c12ea
rotmistr <rotmistr@ncbi.nlm.nih.gov>
2008-08-20 01:44:43 +0000

    Added auto removal of output files
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@39007 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/app/oligofar/test-oligofar.sh
+++ src/app/oligofar/test-oligofar.sh
@@ -2,4 +2,6 @@
 ./oligofar -i NM_012345.reads -d NM_012345.fa -o NM_012345.reads.test -w9 -n1 -L2G || exit 1
 ./oligofar -i NM_012345.pairs -d NM_012345.fa -o NM_012345.pairs.test -w9 -n1 -L2G || exit 2
 diff NM_012345.reads.test NM_012345.reads.out > /dev/null || exit 3
-diff NM_012345.pairs.test NM_012345.pairs.out > /dev/null || exit 3
+diff NM_012345.pairs.test NM_012345.pairs.out > /dev/null || exit 4
+rm NM_012345.reads.test NM_012345.pairs.test
+exit 0
