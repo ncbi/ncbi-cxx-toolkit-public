2dc21f9443ad529e7c3a6a3c959c96f05c3901fd
George Coulouris <coulouri@ncbi.nlm.nih.gov>
2008-09-16 16:35:06 +0000

    correct relative paths, add missing file
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@39293 78c7ea69-d796-4a43-9a09-de51944f1b03

--- scripts/projects/blast/post_build/macosx/ncbi-blast.sh
+++ scripts/projects/blast/post_build/macosx/ncbi-blast.sh
@@ -18,14 +18,14 @@ ALL_BINS="$BLAST_BINS $MASKING_BINS $DB_BINS"
 rm -rf $PRODUCT.dmg $PRODUCT _stage $INSTALLDIR/installer
 mkdir -p _stage/usr/local/ncbi/blast/bin _stage/usr/local/ncbi/blast/doc _stage/etc/paths.d
 
-cp -p ./ncbi_blast _stage/etc/paths.d
+cp -p $SCRIPTDIR/ncbi_blast _stage/etc/paths.d
 
 # This is needed because the binary ncbi-blast.pmproj has this string hard
 # coded
-cp -p $INSTALLDIR/LICENSE ../license.txt
-for f in large-Blue_ncbi_logo.tiff ncbi-blast.pmproj welcome.txt; do
+cp -p $INSTALLDIR/LICENSE ./license.txt
+for f in large-Blue_ncbi_logo.tiff ncbi-blast.pmdoc welcome.txt; do
     echo copying $f to local directory
-    cp -p $SCRIPTDIR/$f .
+    cp -rp $SCRIPTDIR/$f .
 done
 
 for bin in $ALL_BINS; do
