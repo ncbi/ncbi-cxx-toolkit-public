15ede3b40a10278e62e783364e2607cdac760990
Andrei Gourianov <gouriano@ncbi.nlm.nih.gov>
2008-09-30 14:54:23 +0000

    changed PTB version. JIRA: CXX-730
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@39489 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/build-system/project_tree_builder/proj_builder_app.cpp
+++ src/build-system/project_tree_builder/proj_builder_app.cpp
@@ -365,7 +365,7 @@ struct PIsExcludedByRequires
 //-----------------------------------------------------------------------------
 CProjBulderApp::CProjBulderApp(void)
 {
-    SetVersion( CVersionInfo(1,6,2) );
+    SetVersion( CVersionInfo(1,6,3) );
     m_ScanningWholeTree = false;
     m_Dll = false;
     m_AddMissingLibs = false;
