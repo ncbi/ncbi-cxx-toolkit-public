e0a97f8988500f3d1e37a22547c204c5673c5de8
Andrei Gourianov <gouriano@ncbi.nlm.nih.gov>
2008-10-03 14:59:23 +0000

    Minor corrections. JIRA: CXX-542
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@39540 78c7ea69-d796-4a43-9a09-de51944f1b03

--- compilers/xcode30_prj/dll/UtilityProjects/configure_ncbi_cpp_dll.sh
+++ compilers/xcode30_prj/dll/UtilityProjects/configure_ncbi_cpp_dll.sh
@@ -3,4 +3,5 @@ xcodebuild -project ../static/UtilityProjects/PTB.xcodeproj -target project_tree
 if test $? -eq 0; then
 ../static/bin/ReleaseDLL/project_tree_builder -dll -logfile UtilityProjects/ncbi_cpp_dll_configuration_log.txt -conffile ../../../src/build-system/project_tree_builder.ini ../../../ scripts/projects/xcode_cpp.lst ncbi_cpp_dll
 fi
+echo done
 open UtilityProjects/ncbi_cpp_dll_configuration_log.txt
--- compilers/xcode30_prj/static/UtilityProjects/configure_ncbi_cpp.sh
+++ compilers/xcode30_prj/static/UtilityProjects/configure_ncbi_cpp.sh
@@ -3,4 +3,5 @@ xcodebuild -project UtilityProjects/PTB.xcodeproj -target project_tree_builder -
 if test $? -eq 0; then
 ./bin/ReleaseDLL/project_tree_builder -logfile UtilityProjects/ncbi_cpp_configuration_log.txt -conffile ../../../src/build-system/project_tree_builder.ini ../../../ scripts/projects/xcode_cpp.lst ncbi_cpp
 fi
+echo done
 open UtilityProjects/ncbi_cpp_configuration_log.txt
