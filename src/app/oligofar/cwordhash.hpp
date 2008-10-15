5128a9048156f1a7f9ec7f9cea71bee23c5c3e1e
rotmistr <rotmistr@ncbi.nlm.nih.gov>
2008-10-15 21:03:40 +0000

    Removed debugging output
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@39685 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/app/oligofar/cwordhash.hpp
+++ src/app/oligofar/cwordhash.hpp
@@ -27,7 +27,7 @@ public:
         m_indexBits = bits;
         m_indexMask = CBitHacks::WordFootprint<Uint4>( bits );
         Clear();
-        cerr << "Setting index bits: " << int( m_indexBits ) << " mask " << hex << m_indexMask << dec << " table size " << m_table.size() << endl;
+//        cerr << "Setting index bits: " << int( m_indexBits ) << " mask " << hex << m_indexMask << dec << " table size " << m_table.size() << endl;
     }
 
     void Clear() {
