3dea7bcaf8e89c8fa8f60053dddb0598903ace2c
Grzegorz (Greg) Boratyn <boratyng@ncbi.nlm.nih.gov>
2008-10-15 20:16:18 +0000

    Corrected default gap penalties
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@39680 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/algo/cobalt/options.cpp
+++ src/algo/cobalt/options.cpp
@@ -54,8 +54,8 @@ const CMultiAlignerOptions::TMode CMultiAlignerOptions::kQClustersModeMask
    | CMultiAlignerOptions::fMediumQueryClusters 
    | CMultiAlignerOptions::fLargeQueryClusters);
 
-const int CMultiAlignerOptions::kDefaultGapOpen = BLAST_GAP_OPEN_PROT;
-const int CMultiAlignerOptions::kDefaultGapExtend = BLAST_GAP_EXTN_PROT;
+const int CMultiAlignerOptions::kDefaultGapOpen = -BLAST_GAP_OPEN_PROT;
+const int CMultiAlignerOptions::kDefaultGapExtend = -BLAST_GAP_EXTN_PROT;
 const double CMultiAlignerOptions::kDefaultUserConstraintsScore = 1000000;
 
 CMultiAlignerOptions::CMultiAlignerOptions(void)
