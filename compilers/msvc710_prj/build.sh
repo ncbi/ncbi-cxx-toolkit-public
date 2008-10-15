772f40e796555aa1ee1cd1654eb23cb649a89793
Vladimir Ivanov <ivanov@ncbi.nlm.nih.gov>
2008-10-15 14:19:41 +0000

    Generate list of built configurations. Formatting. JIRA: CXX-536
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@39664 78c7ea69-d796-4a43-9a09-de51944f1b03

--- compilers/msvc710_prj/build.sh
+++ compilers/msvc710_prj/build.sh
@@ -12,22 +12,21 @@ cfgs="${1:-DebugDLL Debug ReleaseDLL Release}"
  
 ########### Global variables
 
-dirs='static dll'
+build_trees='static dll'
 sol_static="ncbi_cpp.sln gui\ncbi_gui.sln"
-sol_dll="ncbi_cpp_dll.sln gui\ncbi_gui_dll.sln gbench\ncbi_gbench.sln"
+sol_dll="ncbi_cpp_dll.sln gui\ncbi_gui_dll.sln"
 timer="date +'%H:%M'"
 
 ########## Functions
 
 error()
 {
-  echo "[`basename $script`] ERROR:  $1"
-  exit 1
-
+    echo "[`basename $script`] ERROR:  $1"
+    exit 1
 }
 
 generate_msvc7_error_check_file() {
-  cat <<-EOF >$1
+    cat <<-EOF >$1
 	/.*--* (Reb|B)uild( All | )started: Project:/ {
 	  expendable = ""
 	}
@@ -53,39 +52,42 @@ build_dir=`dirname $script`
 build_dir=`(cd "$build_dir"; pwd)`
 
 if [ ! -d $build_dir ] ; then
-  error "Build directory $build_dir not found"
-  exit 1
+    error "Build directory $build_dir not found"
+    exit 1
 fi
 cd $build_dir
 
 # Configuration to build configure
 cfg_configure=`echo $cfgs | sed 's| .*$||g'`
 if [ -s $cfg_configure ] ; then
-  error "Configuration to build configure is not specified"
-  exit 2
+    error "Configuration to build configure is not specified"
+    exit 2
 fi
+out=".build.$$"
 
 chmod +x $build_dir/build_exec.bat
+rm -f $build_dir/cfgs.log
 
 
 # Configure
 
-for dir in $dirs ; do
-   if [ $dir = dll ] ; then
-     test $cfg_configure != ReleaseDLL -a $cfg_configure != DebugDLL  &&  continue  
-   fi
-   sols=`eval echo "$"sol_${dir}""`
-   for sol in $sols ; do
-     alias=`echo $sol | sed -e 's|\\\\.*$||g' -e 's|_.*$||g'`
-     start=`eval $timer`
-     echo Start time: $start
-     echo "INFO: Configure \"$dir\\$alias\""
-     $build_dir/build_exec.bat "$dir\\build\\$sol" build $cfg_configure "-CONFIGURE-"
-     if [ $? -ne 0 ] ; then
-       exit 3
-     fi
-     echo "Build time: $start - `eval $timer`"
-   done
+for tree in $build_trees ; do
+    if [ $tree = dll ] ; then
+        test $cfg_configure != ReleaseDLL -a $cfg_configure != DebugDLL  &&  continue  
+    fi
+    sols=`eval echo "$"sol_${tree}""`
+    for sol in $sols ; do
+        alias=`echo $sol | sed -e 's|\\\\.*$||g' -e 's|_.*$||g'`
+        start=`eval $timer`
+        echo Start time: $start
+        echo "INFO: Configure \"$tree\\$alias\""
+        echo "Command line: " $build_dir/build_exec.bat "$tree\\build\\$sol" build $cfg_configure "-CONFIGURE-"
+        $build_dir/build_exec.bat "$tree\\build\\$sol" build $cfg_configure "-CONFIGURE-"
+        if [ $? -ne 0 ] ; then
+            exit 3
+        fi
+        echo "Build time: $start - `eval $timer`"
+    done
 done
 
 
@@ -97,34 +99,36 @@ generate_msvc7_error_check_file $check_awk
 
 # Build
 
-for cfg in $cfgs ; do
-  for dir in $dirs ; do
-     if [ $dir = dll ] ; then
-       test $cfg != ReleaseDLL -a $cfg != DebugDLL  &&  continue  
-     fi
-     sols=`eval echo "$"sol_${dir}""`
-     for sol in $sols ; do
-       alias=`echo $sol | sed -e 's|\\\\.*$||g' -e 's|_.*$||g'`
-       start=`eval $timer`
-       echo Start time: $start
-       echo "INFO: Building \"$dir\\$cfg\\$alias\""
-       $build_dir/build_exec.bat "$dir\\build\\$sol" build $cfg "-BUILD-ALL-" >/tmp/build.$$ 2>&1
-       status=$?
-       cat /tmp/build.$$
-       echo "Build time: $start - `eval $timer`"
-       if [ $status -ne 0 ] ; then
-         # Check on errors (skip expendable projects)
-         failed="1"
-         grep '^ *Build: .* succeeded, .* failed' /tmp/build.$$ >/dev/null 2>&1  && \
-           awk -f $check_awk /tmp/build.$$ >/tmp/res.$$ 2>/dev/null  &&  test ! -s /tmp/res.$$  &&  failed="0"
-         rm -f /tmp/build.$$ /tmp/res.$$ >/dev/null 2>&1
-         if [ "$failed" = "1" ]; then
-           exit 4
-         fi
-       fi
-       rm -f /tmp/build.$$ >/dev/null 2>&1
-     done
-  done
+for tree in $build_trees ; do
+    for cfg in $cfgs ; do
+        if [ $tree = dll ] ; then
+            test $cfg != ReleaseDLL -a $cfg != DebugDLL  &&  continue  
+        fi
+        sols=`eval echo "$"sol_${tree}""`
+        for sol in $sols ; do
+            alias=`echo $sol | sed -e 's|\\\\.*$||g' -e 's|_.*$||g'`
+            start=`eval $timer`
+            echo Start time: $start
+            echo "$tree,$sol,$cfg" >> $build_dir/cfgs.log
+            echo "INFO: Building \"$tree\\$cfg\\$alias\""
+            echo "Command line: " $build_dir/build_exec.bat "$tree\\build\\$sol" build $cfg "-BUILD-ALL-"
+            $build_dir/build_exec.bat "$tree\\build\\$sol" build $cfg "-BUILD-ALL-" >$out 2>&1
+            status=$?
+            cat $out
+            echo "Build time: $start - `eval $timer`"
+            if [ $status -ne 0 ] ; then
+                # Check on errors (skip expendable projects)
+                failed="1"
+                grep '^ *Build: .* succeeded, .* failed' $out >/dev/null 2>&1  && \
+                    awk -f $check_awk $out >$out.res 2>/dev/null  &&  test ! -s $out.res  &&  failed="0"
+                rm -f $out $out.res >/dev/null 2>&1
+                if [ "$failed" = "1" ]; then
+                    exit 4
+                fi
+            fi
+            rm -f $out >/dev/null 2>&1
+        done
+    done
 done
 
 
--- compilers/msvc800_prj/build.sh
+++ compilers/msvc800_prj/build.sh
@@ -14,9 +14,8 @@ arch="$2"
 
 ########### Global variables
 
-dirs='static dll'
+build_trees='static dll'
 sol_static="ncbi_cpp.sln gui\ncbi_gui.sln"
-#sol_dll="ncbi_cpp_dll.sln gui\ncbi_gui_dll.sln gbench\ncbi_gbench.sln"
 sol_dll="ncbi_cpp_dll.sln gui\ncbi_gui_dll.sln"
 timer="date +'%H:%M'"
 
@@ -25,13 +24,12 @@ timer="date +'%H:%M'"
 
 error()
 {
-  echo "[`basename $script`] ERROR:  $1"
-  exit 1
-
+    echo "[`basename $script`] ERROR:  $1"
+    exit 1
 }
 
 generate_msvc8_error_check_file() {
-  cat <<-EOF >$1
+    cat <<-EOF >$1
 	/.*--* (Reb|B)uild( All | )started: Project:/ {
 	  expendable = ""
 	}
@@ -57,8 +55,8 @@ build_dir=`dirname $script`
 build_dir=`(cd "$build_dir"; pwd)`
 
 if [ ! -d $build_dir ] ; then
-  error "Build directory $build_dir not found"
-  exit 1
+    error "Build directory $build_dir not found"
+    exit 1
 fi
 cd $build_dir
 
@@ -74,31 +72,32 @@ cfg_configure='ReleaseDLL'
 out=".build.$$"
 
 chmod +x $build_dir/build_exec.bat
+rm -f $build_dir/cfgs.log
 
 
 # Configure
 
-for dir in $dirs ; do
-   if [ $dir = dll ] ; then
-     test $cfg_configure != ReleaseDLL -a $cfg_configure != DebugDLL  &&  continue  
-   fi
-   sols=`eval echo "$"sol_${dir}""`
-   for sol in $sols ; do
-     alias=`echo $sol | sed -e 's|\\\\.*$||g' -e 's|_.*$||g'`
-     start=`eval $timer`
-     echo Start time: $start
-     echo "INFO: Configure \"$dir\\$alias\""
-     echo "Command line: " $build_dir/build_exec.bat "$dir\\build\\$sol" build "$arch" "$cfg_configure" "-CONFIGURE-" $out
-     $build_dir/build_exec.bat "$dir\\build\\$sol" build "$arch" "$cfg_configure" "-CONFIGURE-" $out
-     status=$?
-# don't need to cat here, because msdev2005 double output to console also
-#     cat $out
-     rm -f $out >/dev/null 2>&1
-     if [ $status -ne 0 ] ; then
-       exit 3
-     fi
-     echo "Build time: $start - `eval $timer`"
-   done
+for tree in $build_trees ; do
+    if [ $tree = dll ] ; then
+        test $cfg_configure != ReleaseDLL -a $cfg_configure != DebugDLL  &&  continue  
+    fi
+    sols=`eval echo "$"sol_${tree}""`
+    for sol in $sols ; do
+        alias=`echo $sol | sed -e 's|\\\\.*$||g' -e 's|_.*$||g'`
+        start=`eval $timer`
+        echo Start time: $start
+        echo "INFO: Configure \"$tree\\$alias\""
+        echo "Command line: " $build_dir/build_exec.bat "$tree\\build\\$sol" build "$arch" "$cfg_configure" "-CONFIGURE-" $out
+        $build_dir/build_exec.bat "$tree\\build\\$sol" build "$arch" "$cfg_configure" "-CONFIGURE-" $out
+        status=$?
+        # don't need to 'cat' here, because msdev2005 double output to console also
+        # cat $out
+        rm -f $out >/dev/null 2>&1
+        if [ $status -ne 0 ] ; then
+            exit 3
+        fi
+        echo "Build time: $start - `eval $timer`"
+    done
 done
 
 
@@ -110,37 +109,37 @@ generate_msvc8_error_check_file $check_awk
 
 # Build
 
-for cfg in $cfgs ; do
-  for dir in $dirs ; do
-     if [ $dir = dll ] ; then
-       test $cfg != ReleaseDLL -a $cfg != DebugDLL  &&  continue  
-     fi
-     sols=`eval echo "$"sol_${dir}""`
-     for sol in $sols ; do
-       alias=`echo $sol | sed -e 's|\\\\.*$||g' -e 's|_.*$||g'`
-       start=`eval $timer`
-       echo Start time: $start
-       echo "INFO: Building \"$dir\\$cfg\\$alias\""
-       echo "Command line: " $build_dir/build_exec.bat "$dir\\build\\$sol" build "$arch" "$cfg" "-BUILD-ALL-" $out
-       $build_dir/build_exec.bat "$dir\\build\\$sol" build "$arch" "$cfg" "-BUILD-ALL-" $out
-       status=$?
-# don't need to cat here, because msdev2005 double output to console also
-#       cat $out
-       echo "Build time: $start - `eval $timer`"
-       if [ $status -ne 0 ] ; then
-         # Check on errors (skip expendable projects)
-         failed="1"
-         grep '^==* Build: .* succeeded, .* failed' $out >/dev/null 2>&1  && \
-           awk -f $check_awk $out >$out.res 2>/dev/null  &&  test ! -s $out.res  &&  failed="0"
-         rm -f $out.res >/dev/null 2>&1
-         rm -f $out     >/dev/null 2>&1
-         if [ "$failed" = "1" ]; then
-           exit 4
-         fi
-       fi
-       rm -f $out >/dev/null 2>&1
-     done
-  done
+for tree in $build_trees ; do
+    for cfg in $cfgs ; do
+        if [ $tree = dll ] ; then
+            test $cfg != ReleaseDLL -a $cfg != DebugDLL  &&  continue  
+        fi
+        sols=`eval echo "$"sol_${tree}""`
+        for sol in $sols ; do
+            alias=`echo $sol | sed -e 's|\\\\.*$||g' -e 's|_.*$||g'`
+            start=`eval $timer`
+            echo Start time: $start
+            echo "$tree,$sol,$cfg" >> $build_dir/cfgs.log
+            echo "INFO: Building \"$tree\\$cfg\\$alias\""
+            echo "Command line: " $build_dir/build_exec.bat "$tree\\build\\$sol" build "$arch" "$cfg" "-BUILD-ALL-" $out
+            $build_dir/build_exec.bat "$tree\\build\\$sol" build "$arch" "$cfg" "-BUILD-ALL-" $out
+            status=$?
+            # don't need to 'cat' here, because msdev2005 double output to console also
+            # cat $out
+            echo "Build time: $start - `eval $timer`"
+            if [ $status -ne 0 ] ; then
+                # Check on errors (skip expendable projects)
+                failed="1"
+                grep '^==* Build: .* succeeded, .* failed' $out >/dev/null 2>&1  && \
+                    awk -f $check_awk $out >$out.res 2>/dev/null  &&  test ! -s $out.res  &&  failed="0"
+                rm -f $out $out.res >/dev/null 2>&1
+                if [ "$failed" = "1" ]; then
+                    exit 4
+                fi
+            fi
+            rm -f $out >/dev/null 2>&1
+        done
+    done
 done
 
 
