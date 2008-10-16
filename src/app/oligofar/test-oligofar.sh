fe9cebef215a05a20f7d296df7bcb3b454c1217d
rotmistr <rotmistr@ncbi.nlm.nih.gov>
2008-10-16 14:28:52 +0000

    Added two more tests
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@39706 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/app/oligofar/test-oligofar.sh
+++ src/app/oligofar/test-oligofar.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 errors=''
-ok="0,0,0,0,0"
+ok="0,0,0,0,0,0,0"
 function run () {
-	echo -- "$@"
+	echo -- "$@" >&2
 	"$@"
 	return $?
 }
@@ -15,6 +15,12 @@ errors="${errors},$?"
 run ./oligofar -T+ -i NM_012345.pairs -d NM_012345.fa -o NM_012345.pairs.out -w9 -n1 -L2G "$@"
 errors="${errors},$?"
 diff NM_012345.pairs.ref NM_012345.pairs.out > NM_012345.pairs.diff
+errors="${errors},$?"
+x=$(run ./oligofar -i test1.reads -d NM_012345.fa -Ou -n2 -e1 | tee test1.reads.out | awk '{print $14}' | sort | uniq -c)
+test "$x" = '     30 AGCCTTCCTCTGAGTCGCAGCCCCCCATGGAGGCCCAGTCT'
+errors="${errors},$?"
+x=$(run ./oligofar -i test2.reads -d NM_012345.fa -Ou -S6 -n2 -e1 | tee test2.reads.out | awk '{print $6}' | sort | uniq -c)
+test "$x" = '     22 100'
 errors="${errors},$?"
 if test "$errors" == "$ok" ; then
 	rm NM_012345.reads.{out,diff} NM_012345.pairs.{out,diff}
