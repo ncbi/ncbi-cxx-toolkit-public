2e949cda398d0c57ca7beeccb66e562ea30c0069
Anton Lavrentiev <lavr@ncbi.nlm.nih.gov>
2008-10-22 20:34:12 +0000

    Added verbosity
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@39825 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/connect/test/test_ncbi_http_get.sh
+++ src/connect/test/test_ncbi_http_get.sh
@@ -1,8 +1,12 @@
 #! /bin/sh
 #$Id$
 
+CONN_DEBUG_PRINTOUT=SOME;  export CONN_DEBUG_PRINTOUT
+
 if [ "`echo $FEATURES | grep -wic GNUTLS`" = "1" ]; then
-    CONN_USESSL=1; export CONN_USESSL
+    CONN_USESSL=1
+    CONN_GNUTLS_LOGLEVEL=7
+    export CONN_USESSL CONN_GNUTLS_LOGLEVEL
     url='https://test.gnutls.org:5556'
 else
     url='http://www.ncbi.nlm.nih.gov/entrez/viewer.cgi?view=0&maxplex=1&save=idf&val=4959943'
