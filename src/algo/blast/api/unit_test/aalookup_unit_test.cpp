d793652c899a0d261541887d57a6dc9d43b96bbd
Eugene Vasilchenko <vasilche@ncbi.nlm.nih.gov>
2008-11-10 17:13:12 +0000

    Removed reference to deprecated header.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@40045 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/algo/blast/api/unit_test/aalookup_unit_test.cpp
+++ src/algo/blast/api/unit_test/aalookup_unit_test.cpp
@@ -37,7 +37,6 @@
 #include <corelib/ncbitime.hpp>
 #include <objmgr/object_manager.hpp>
 #include <objmgr/scope.hpp>
-#include <objmgr/gbloader.hpp>
 #include <objmgr/util/sequence.hpp>
 
 #include <objects/seqloc/Seq_loc.hpp>
