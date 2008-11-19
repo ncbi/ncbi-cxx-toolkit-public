9dfded2469dcf41ea7b2ac355455d7b7c7e6aa7d
Aaron Ucko <ucko@ncbi.nlm.nih.gov>
2008-11-19 18:37:45 +0000

    Identify GH accessions as GenBank EST sequences.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@40192 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objects/seqloc/Seq_id.cpp
+++ src/objects/seqloc/Seq_id.cpp
@@ -1042,6 +1042,7 @@ static const char * kBuiltInGuide[] = {
     "2+6  GE  gb_est",
     "2+6  GF  gb_sts",
     "2+6  GG  gb_con",
+    "2+6  GH  gb_est",
     "2+6  ??  unreserved_nuc",
     "",
     "# SPECIAL CASES",
