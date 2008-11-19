3b9fc44156e6ba3cabc57a33848e8cee9ce0def1
rotmistr <rotmistr@ncbi.nlm.nih.gov>
2008-11-19 18:32:37 +0000

    Made tests more readable
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@40191 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/app/oligofar/test-oligofar.sh
+++ src/app/oligofar/test-oligofar.sh
@@ -1,43 +1,38 @@
 #!/bin/sh
 errors=''
-ok="0,0,0,0,0,0,0,0,0"
+ok=""
 function run () {
 	test="$1"
 	shift
 	echo -e "\n\e[33m$test\t\e[34m" "$@" "\e[0m" >&2
 	"$@"
+	rc=$?
+	if test "$rc" != 0 ; then
+		errors="$errors$test failed;"
+	fi
 	return $?
 }
 run "Version" ./oligofar -V
-errors="${errors}$?"
 
 run "Single Reads" ./oligofar -T+ -i NM_012345.reads -d NM_012345.fa -o NM_012345.reads.out -w9 -n1 -L2G "$@" 
-errors="${errors},$?"
-diff NM_012345.reads.ref NM_012345.reads.out > NM_012345.reads.diff
-errors="${errors},$?"
+run "Single Reads diff" diff NM_012345.reads.ref NM_012345.reads.out > NM_012345.reads.diff
 
 run "Paired Reads" ./oligofar -T+ -i NM_012345.pairs -d NM_012345.fa -o NM_012345.pairs.out -w9 -n1 -L2G -m50 "$@"
-errors="${errors},$?"
-diff NM_012345.pairs.ref NM_012345.pairs.out > NM_012345.pairs.diff
-errors="${errors},$?"
+run "Paired Reads diff" diff NM_012345.pairs.ref NM_012345.pairs.out > NM_012345.pairs.diff
 
 x=$(run "2mism+1gap" ./oligofar -i test1.reads -d NM_012345.fa -Ou -n2 -e1 | tee test1.reads.out | awk '{print $14}' | sort | uniq -c)
-test "$x" = '     30 AGCCTTCCTCTGAGTCGCAGCCCCCCATGGAGGCCCAGTCT'
-errors="${errors},$?"
+run "2mism+1gap result" test "$x" = '     30 AGCCTTCCTCTGAGTCGCAGCCCCCCATGGAGGCCCAGTCT'
 
 x=$(run "Strides" ./oligofar -i test2.reads -d NM_012345.fa -Ou -S6 -n2 -e1 | tee test2.reads.out | awk '{print $6}' | sort | uniq -c)
-test "$x" = '     22 100'
-errors="${errors},$?"
+run "Strides result" test "$x" = '     22 100'
 
 for R in solexa outside solid decr ; do 
-	echo "R=$R" ; run "Pairs $R" ./oligofar -i test3.pairs -d NM_012345.fa -D400-600 -R $R |awk '($6>100)' ; 
+	echo "R=$R"; run "Orientation $R" ./oligofar -i test3.pairs -d NM_012345.fa -D400-600 -R $R |awk '($6>100)' ; 
 done > test3.pairs.out
-diff test3.pairs.ref test3.pairs.out > test3.pairs.diff
-errors="${errors},$?"
+run "Orientation diff" diff test3.pairs.ref test3.pairs.out > test3.pairs.diff
 
 x=$(run "Pairs Length" ./oligofar -i test4.pairs -d NM_012345.fa -Ou -w10 -D80 -m 9|awk '($6==100)'|wc -l)
-test "$x" = '4'
-errors="${errors},$?"
+run "Pairs Length result" test "$x" = '4'
 
 if test "$errors" == "$ok" ; then
 	rm NM_012345.reads.{out,diff} NM_012345.pairs.{out,diff}
@@ -45,5 +40,6 @@ else
 	head NM_012345.{reads,pairs}.diff
 fi
 
-echo "ERRORS=$errors";
+echo -e "\x1b[31m$errors\x1b[0m" | tr ';' "\n"
+if test "$errors" == "$ok" ; then echo -e "\x1b[32mOK\x1b[0m" ; fi
 test "$errors" == "$ok"
