e48e3fa1fe48213e1ce89633d3410eb0775abd29
Christiam Camacho <camacho@ncbi.nlm.nih.gov>
2008-11-24 14:55:16 +0000

    Fix missing propagation of masking algorithms for subjects when initializing search objects with a CSearchDatabase object
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@40229 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/algo/blast/api/setup_factory.cpp
+++ src/algo/blast/api/setup_factory.cpp
@@ -295,7 +295,8 @@ CSetupFactory::CreateBlastSeqSrc(const CSearchDatabase& db)
 {
     bool prot = (db.GetMoleculeType() == CSearchDatabase::eBlastDbIsProtein);
 
-    BlastSeqSrc* retval = SeqDbBlastSeqSrcInit(db.GetDatabaseName(), prot);
+    BlastSeqSrc* retval = SeqDbBlastSeqSrcInit(db.GetDatabaseName(), prot, 0, 0,
+                                               db.GetFilteringAlgorithms());
     char* error_str = BlastSeqSrcGetInitError(retval);
     if (error_str) {
         string msg(error_str);
