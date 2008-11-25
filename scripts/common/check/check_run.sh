7721449fccad2b4fcc32e652059fde64c1ca17a7
ivanovp <ivanovp@ncbi.nlm.nih.gov>
2008-11-25 13:38:37 +0000

    Always load test results to database (even if some tests failed). JIRA: CXX-822
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@40246 78c7ea69-d796-4a43-9a09-de51944f1b03

--- scripts/common/check/check_run.sh
+++ scripts/common/check/check_run.sh
@@ -90,9 +90,8 @@ esac
 $CHECK_RUN_FILE run
 result=$?
 
-if test $result -eq 0 -a -n "$NCBI_AUTOMATED_BUILD"; then
+if test -n "$NCBI_AUTOMATED_BUILD"; then
    $CHECK_RUN_FILE load_to_db
-   result=$?
 fi
 
 
