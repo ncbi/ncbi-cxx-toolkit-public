2e72de122119e87df8ade55dd7ee5def6bf6084f
Andrei Gourianov <gouriano@ncbi.nlm.nih.gov>
2008-12-19 19:05:31 +0000

    Corrected project list name. JIRA: CXX-542
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@40484 78c7ea69-d796-4a43-9a09-de51944f1b03

--- compilers/xcode30_prj/dll/UtilityProjects/configure_ncbi_gui_dll.sh
+++ compilers/xcode30_prj/dll/UtilityProjects/configure_ncbi_gui_dll.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 xcodebuild -project ../static/UtilityProjects/PTB.xcodeproj -target project_tree_builder -configuration ReleaseDLL
 if test $? -eq 0; then
-../static/bin/ReleaseDLL/project_tree_builder -dll -logfile ncbi_gui_dll_configuration_log.txt -conffile ../../../src/build-system/project_tree_builder.ini ../../../ scripts/projects/ncbi_gui_dll.lst ncbi_gui_dll
+../static/bin/ReleaseDLL/project_tree_builder -dll -logfile ncbi_gui_dll_configuration_log.txt -conffile ../../../src/build-system/project_tree_builder.ini ../../../ scripts/projects/ncbi_gui.lst ncbi_gui_dll
 fi
 echo done
 open ncbi_gui_dll_configuration_log.txt
