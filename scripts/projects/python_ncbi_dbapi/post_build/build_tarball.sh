2d0a73b753771799570edd58a1fcc5e37306d04f
ivanovp <ivanovp@ncbi.nlm.nih.gov>
2009-01-09 19:56:39 +0000

    Add test to release and create installation tarball. JIRA: CXX-863
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@40675 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ scripts/projects/python_ncbi_dbapi/post_build/build_tarball.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+
+INSTALL_DIR=$1
+VERSION=$2
+
+if [[ -z "$INSTALL_DIR" || -z "$VERSION" ]]; then
+    echo "Installation directory or version are empty"
+    exit 1
+fi
+
+PYTHON_DIR="$(which python)"
+PYTHON_DIR="${PYTHON_DIR%/*}"
+PYTHON_DIR="${PYTHON_DIR%/*}"
+
+if [ -z "$PYTHON_DIR" ]; then
+    echo "Cannot find python directory"
+    exit 2
+fi
+
+
+PYTHON_SUBDIR="python2.5/site-packages/python_ncbi_dbapi"
+TARBALL_DIR="$INSTALL_DIR/build_tarball"
+
+mkdir -p "$TARBALL_DIR/$PYTHON_SUBDIR/$VERSION" || exit 3
+cp -R "$PYTHON_DIR/lib/$PYTHON_SUBDIR"/* "$TARBALL_DIR/$PYTHON_SUBDIR/" || exit 4
+
+cp "$INSTALL_DIR/lib"/* "$TARBALL_DIR/$PYTHON_SUBDIR/$VERSION" || exit 5
+ln -s "python_ncbi_dbapi/$VERSION/libpython_ncbi_dbapi.so" "$TARBALL_DIR/$PYTHON_SUBDIR/../python_ncbi_dbapi.so" || exit 6
+
+pushd "$TARBALL_DIR"
+tar -zcf "$INSTALL_DIR/installation_${VERSION}.tar.gz" python2.5 || exit 7
+popd
+
+rm -rf "$TARBALL_DIR"
