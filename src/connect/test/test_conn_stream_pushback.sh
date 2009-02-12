3fa28b0cb1a4b8b5f22403a80a28278fe7e3c7ee
Anton Lavrentiev <lavr@ncbi.nlm.nih.gov>
2009-02-12 23:45:05 +0000

    Use wc without file whose name otherwise appears next to count
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@41062 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/connect/test/test_conn_stream_pushback.sh
+++ src/connect/test/test_conn_stream_pushback.sh
@@ -16,7 +16,7 @@ while [ $i -lt $j ]; do
   $CHECK_EXEC test_conn_stream_pushback >$log 2>&1
   exit_code=$?
   if [ "$exit_code" != "0" ]; then
-    if [ "`wc -l $log`" -gt "40" ]; then
+    if [ "`cat $log | wc -l`" -gt "40" ]; then
       cat -n $log | head -20
       echo '......'
       cat -n $log | tail -20
