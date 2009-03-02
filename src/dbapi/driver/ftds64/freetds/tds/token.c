7883a2fe8ee1a405d83903ae7770be8f70089ab8
ivanovp <ivanovp@ncbi.nlm.nih.gov>
2009-03-02 21:26:47 +0000

    Missed fix to make ftds64 driver fully functional with Sybase.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@41208 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/dbapi/driver/ftds64/freetds/tds/token.c
+++ src/dbapi/driver/ftds64/freetds/tds/token.c
@@ -2986,16 +2986,16 @@ tds5_process_dyn_result2(TDSSOCKET * tds)
             break;
         }
 
-        /* Adjust column size according to client's encoding */
-        curcol->on_server.column_size = curcol->column_size;
-        adjust_character_column_size(tds, curcol);
-
         /* numeric and decimal have extra info */
         if (is_numeric_type(curcol->column_type)) {
             curcol->column_prec = tds_get_byte(tds);    /* precision */
             curcol->column_scale = tds_get_byte(tds);   /* scale */
             /* FIXME check prec/scale, don't let server crash us */
         }
+
+        /* Adjust column size according to client's encoding */
+        curcol->on_server.column_size = curcol->column_size;
+        adjust_character_column_size(tds, curcol);
 
         /* discard Locale */
         tds_get_n(tds, NULL, tds_get_byte(tds));
