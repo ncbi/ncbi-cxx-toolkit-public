154838b41c5889e79e7ad6677955ab5d04eb7b1b
Anton Lavrentiev <lavr@ncbi.nlm.nih.gov>
2009-04-01 15:48:45 +0000

    Retire unused asnwrite.inl
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@41454 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/ctools/asn/asnwrite.hpp
+++ include/ctools/asn/asnwrite.hpp
@@ -1,36 +1,36 @@
-#ifndef ASNWRITE__HPP
-#define ASNWRITE__HPP
+#ifndef CTOOLS__ASN___ASNWRITE__HPP
+#define CTOOLS__ASN___ASNWRITE__HPP
 
-/*  $Id$
-* ===========================================================================
-*
-*                            PUBLIC DOMAIN NOTICE
-*               National Center for Biotechnology Information
-*
-*  This software/database is a "United States Government Work" under the
-*  terms of the United States Copyright Act.  It was written as part of
-*  the author's official duties as a United States Government employee and
-*  thus cannot be copyrighted.  This software/database is freely available
-*  to the public for use. The National Library of Medicine and the U.S.
-*  Government have not placed any restriction on its use or reproduction.
-*
-*  Although all reasonable efforts have been taken to ensure the accuracy
-*  and reliability of the software and data, the NLM and the U.S.
-*  Government do not and cannot warrant the performance or results that
-*  may be obtained by using this software or data. The NLM and the U.S.
-*  Government disclaim all warranties, express or implied, including
-*  warranties of performance, merchantability or fitness for any particular
-*  purpose.
-*
-*  Please cite the author in any work or product based on this material.
-*
-* ===========================================================================
-*
-* Author: Eugene Vasilchenko
-*
-* File Description:
-*   !!! PUT YOUR DESCRIPTION HERE !!!
-*/
+/* $Id$
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Author: Eugene Vasilchenko
+ *
+ * File Description:
+ *   !!! PUT YOUR DESCRIPTION HERE !!!
+ */
 
 #include <corelib/ncbistd.hpp>
 #include <html/html.hpp>
@@ -60,8 +60,6 @@ private:
     int m_Mode;
 };
 
-#include <ctools/asn/asnwrite.inl>
-
 END_NCBI_SCOPE
 
-#endif  /* ASNWRITE__HPP */
+#endif  /* CTOOLS__ASN___ASNWRITE__HPP */
