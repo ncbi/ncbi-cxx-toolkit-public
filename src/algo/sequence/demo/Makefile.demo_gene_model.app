2ae7b1e038afd06e2cb656889930408ff47f3ee3
Michael DiCuccio <dicuccio@ncbi.nlm.nih.gov>
2009-08-19 16:43:03 +0000

    Added demo app + standard data for gene model creation
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@42860 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ src/algo/sequence/demo/Makefile.demo_gene_model.app
@@ -0,0 +1,12 @@
+# $Id$
+
+SRC = demo_gene_model
+APP = demo_gene_model
+
+CPPFLAGS = $(ORIG_CPPFLAGS)
+CXXFLAGS = $(FAST_CXXFLAGS)
+LDFLAGS  = $(FAST_LDFLAGS)
+
+LIB = xalgoseq xalnmgr tables xregexp $(PCRE_LIB) xobjutil taxon1 $(OBJMGR_LIBS)
+
+LIBS = $(PCRE_LIBS) $(CMPRS_LIBS) $(NETWORK_LIBS) $(DL_LIBS) $(ORIG_LIBS)
---
+++ src/algo/sequence/demo/demo_gene_model.cpp
@@ -0,0 +1,103 @@
+/*  $Id$
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Author: Mike DiCuccio
+ *
+ * File Description:
+ *
+ * ===========================================================================
+ */
+
+#include <ncbi_pch.hpp>
+
+#include <corelib/ncbiapp.hpp>
+#include <corelib/ncbiargs.hpp>
+#include <corelib/ncbienv.hpp>
+
+#include <objmgr/object_manager.hpp>
+#include <objtools/data_loaders/genbank/gbloader.hpp>
+#include <objmgr/scope.hpp>
+
+#include <algo/sequence/gene_model.hpp>
+
+USING_NCBI_SCOPE;
+USING_SCOPE(objects);
+
+class CGeneModelDemoApp : public CNcbiApplication {
+public:
+    CGeneModelDemoApp(void) {DisableArgDescriptions();};
+    virtual void Init(void);
+    virtual int Run(void);
+};
+
+/*---------------------------------------------------------------------------*/
+
+void CGeneModelDemoApp::Init(void)
+{
+    auto_ptr<CArgDescriptions> arg_desc(new CArgDescriptions);
+
+    arg_desc->AddDefaultKey("i", "InputFile",
+                            "File containing seq-align to use",
+                            CArgDescriptions::eInputFile,
+                            "-");
+
+    arg_desc->AddDefaultKey("o", "OutputFile",
+                            "File containing seq-annot for gene model",
+                            CArgDescriptions::eOutputFile,
+                            "-");
+
+    SetupArgDescriptions(arg_desc.release());
+}
+
+
+int CGeneModelDemoApp::Run(void)
+{
+    const CArgs& args = GetArgs();
+    CNcbiIstream& istr = args["i"].AsInputFile();
+    CSeq_align align;
+    istr >> MSerial_AsnText >> align;
+
+    CRef<CObjectManager> om = CObjectManager::GetInstance();
+    CGBDataLoader::RegisterInObjectManager(*om);
+    CScope scope(*om);
+    scope.AddDefaults();
+
+    CSeq_annot annot;
+    CBioseq_set seqs;
+    CGeneModel::CreateGeneModelFromAlign(align, scope,
+                                         annot, seqs);
+
+    CNcbiOstream& ostr = args["o"].AsOutputFile();
+    ostr << MSerial_AsnText << annot;
+
+    return 0;
+}
+
+//---------------------------------------------------------------------------
+int main(int argc, char** argv)
+{
+    CGeneModelDemoApp theApp;
+    return theApp.AppMain(argc, argv, NULL, eDS_Default, 0);
+}
