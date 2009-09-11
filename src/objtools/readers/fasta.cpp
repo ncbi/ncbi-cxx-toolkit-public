44bd50e157493263e2cb5af142fc0b1b6cf56a7f
Aaron Ucko <ucko@ncbi.nlm.nih.gov>
2009-09-11 14:28:40 +0000

    Allow untagged local IDs to contain colons (in addition to [A-Za-z0-9_.-]) for compatibility with unitprotkb_swissprot.  JIRA: CXX-1303.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@43102 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/readers/fasta.cpp
+++ src/objtools/readers/fasta.cpp
@@ -511,7 +511,7 @@ void CFastaReader::ParseTitle(const TStr& s)
 bool CFastaReader::IsValidLocalID(const string& s)
 {
     static const char* const kLegal =
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_.";
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_.:";
     return (!s.empty()  &&  s.find_first_not_of(kLegal) == NPOS);
 }
 
@@ -1158,7 +1158,7 @@ static SIZE_TYPE s_EndOfFastaID(const string& str, SIZE_TYPE pos)
 static bool s_IsValidLocalID(const string& s)
 {
     static const char* const kLegal =
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_.";
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_.:";
     return (!s.empty()  &&  s.find_first_not_of(kLegal) == NPOS);
 }
 
