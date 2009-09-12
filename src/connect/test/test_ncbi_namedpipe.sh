88be7d19175310649c8ee7ff421269a6987b207e
Anton Lavrentiev <lavr@ncbi.nlm.nih.gov>
2009-09-12 00:43:20 +0000

    Reduce server startup time (typo) which most likely caused tests to fail intermittently!
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@43110 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/connect/test/test_ncbi_namedpipe.sh
+++ src/connect/test/test_ncbi_namedpipe.sh
@@ -7,7 +7,7 @@ test_ncbi_namedpipe server $$ &
 spid=$!
 trap 'kill -9 $spid; rm -f ./.ncbi_test_pipename_$$' 0 1 2 15
 
-sleep 10
+sleep 1
 $CHECK_EXEC test_ncbi_namedpipe client $$  ||  exit_code=1
 
 kill $spid  ||  exit_code=2
