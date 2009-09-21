16a556a1d1f9b9143066625b788f928f2b38522b
Anton Lavrentiev <lavr@ncbi.nlm.nih.gov>
2009-09-21 12:16:07 +0000

    Separate client/server outputs
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@43201 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/connect/test/test_ncbi_namedpipe.sh
+++ src/connect/test/test_ncbi_namedpipe.sh
@@ -1,18 +1,42 @@
 #! /bin/sh
 # $Id$
 
-exit_code=0
+outlog()
+{
+  logfile="$1"
+  if [ -s "$logfile" ]; then
+    echo "=== $logfile ==="
+    if [ "`cat $logfile 2>/dev/null | wc -l`" -gt "200" ]; then
+      head -100 "$logfile"
+      echo '...'
+      tail -100 "$logfile"
+    else
+      cat "$logfile"
+    fi
+  fi
+}
 
-test_ncbi_namedpipe server $$ &
+exit_code=0
+server_log=test_ncbi_namedpipe_server.log
+client_log=test_ncbi_namedpipe_client.log
+
+rm -f $server_log $client_log
+
+test_ncbi_namedpipe server $$ >$server_log 2>&1 &
 spid=$!
 trap 'kill -9 $spid; rm -f ./.ncbi_test_pipename_$$' 0 1 2 15
 
 sleep 1
-$CHECK_EXEC test_ncbi_namedpipe client $$  ||  exit_code=1
+$CHECK_EXEC test_ncbi_namedpipe client $$ >$client_log  ||  exit_code=1
 
 kill $spid  ||  exit_code=2
 ( kill -9 $spid ) >/dev/null 2>&1
 
 rm -f ./.ncbi_test_pipename_$$ >/dev/null 2>&1
+
+if [ $exit_code != 0 ]; then
+  outlog "$server_log"
+  outlog "$client_log"
+fi
 
 exit $exit_code
