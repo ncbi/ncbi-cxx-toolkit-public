47909c40732c6197f6e5449251a1505b52e442da
Anton Lavrentiev <lavr@ncbi.nlm.nih.gov>
2009-09-21 18:50:24 +0000

    Proper redirect
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@43217 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/connect/test/test_ncbi_socket_connector.sh
+++ src/connect/test/test_ncbi_socket_connector.sh
@@ -26,12 +26,12 @@ CONN_DEBUG_PRINTOUT=SOME;  export CONN_DEBUG_PRINTOUT
 
 port="575`expr $$ % 100`"
 
-socket_io_bouncer $port >$server_log 2>&1 &
+socket_io_bouncer $port >>$server_log 2>&1 &
 spid=$!
 trap 'kill -9 $spid' 0 1 2 15
 
 sleep 1
-$CHECK_EXEC test_ncbi_socket_connector localhost $port >client_log 2>&1  ||  exit_code=1
+$CHECK_EXEC test_ncbi_socket_connector localhost $port >>client_log 2>&1  ||  exit_code=1
 
 kill $spid  ||  exit_code=2
 ( kill -9 $spid ) >/dev/null 2>&1
