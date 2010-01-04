c79c8687071919627c747a295d878f0bdbccedd0
rotmistr <rotmistr@ncbi.nlm.nih.gov>
2010-01-04 15:42:12 +0000

    Removed lcl| from output
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@44305 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/app/oligofar/aoutputformatter.cpp
+++ src/app/oligofar/aoutputformatter.cpp
@@ -7,5 +7,8 @@ USING_OLIGOFAR_SCOPES;
 
 string AOutputFormatter::GetSubjectId( int ord ) const 
 {
-    return m_seqIds.GetSeqDef( ord ).GetBestIdString();
+    //return m_seqIds.GetSeqDef( ord ).GetBestIdString();
+    string label;
+    CSeq_id( m_seqIds.GetSeqDef( ord ).GetBestIdString() ).GetLabel( &label, CSeq_id::eContent );
+    return label;
 }
