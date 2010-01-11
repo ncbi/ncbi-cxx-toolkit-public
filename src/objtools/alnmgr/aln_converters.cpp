8f1717381f16063f56ddf2fc2c1d088ea24f147f
Kamen Todorov <todorov@ncbi.nlm.nih.gov>
2010-01-11 15:43:17 +0000

    Per Mike DiCuccio's suggestion, treat Spliced-exon-chunk of type "mismatch" just like regular "matches" so that they are included when exploring the segments. JIRA: AM-29
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@44389 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/alnmgr/aln_converters.cpp
+++ src/objtools/alnmgr/aln_converters.cpp
@@ -535,49 +535,47 @@ ConvertSplicedToPairwiseAln(CPairwiseAln& pairwise_aln,      ///< output
                 default:
                     break;
                 }
-                if (!chunk.IsMismatch()) {
-                    if (row_1 == 0  &&  row_2 == 0) {
-                        if (product_len != 0) {
-                            /// insert the range
+                if (row_1 == 0  &&  row_2 == 0) {
+                    if (product_len != 0) {
+                        /// insert the range
+                        pairwise_aln.insert
+                            (CPairwiseAln::TAlnRng
+                             (product_plus ? product_pos : product_pos - product_len + 1,
+                              product_plus ? product_pos : product_pos - product_len + 1,
+                              product_len,
+                              true));
+                    }
+                } else if (row_1 == 1  &&  row_2 == 1) {
+                    if (genomic_len != 0) {
+                        /// insert the range
+                        pairwise_aln.insert
+                            (CPairwiseAln::TAlnRng
+                             (genomic_plus ? genomic_pos : genomic_pos - genomic_len + 1,
+                              genomic_plus ? genomic_pos : genomic_pos - genomic_len + 1,
+                              genomic_len,
+                              true));
+                    }
+                } else {
+                    if (product_len != 0  &&  product_len == genomic_len  &&
+                        (direction == CAlnUserOptions::eBothDirections  ||
+                         (direct ?
+                          direction == CAlnUserOptions::eDirect :
+                          direction == CAlnUserOptions::eReverse))) {
+                        /// insert the range
+                        if (row_1 == 0) {
                             pairwise_aln.insert
                                 (CPairwiseAln::TAlnRng
                                  (product_plus ? product_pos : product_pos - product_len + 1,
-                                  product_plus ? product_pos : product_pos - product_len + 1,
-                                  product_len,
-                                  true));
-                        }
-                    } else if (row_1 == 1  &&  row_2 == 1) {
-                        if (genomic_len != 0) {
-                            /// insert the range
+                                  genomic_plus ? genomic_pos : genomic_pos - genomic_len + 1,
+                                  genomic_len,
+                                  direct));
+                        } else {
                             pairwise_aln.insert
                                 (CPairwiseAln::TAlnRng
                                  (genomic_plus ? genomic_pos : genomic_pos - genomic_len + 1,
-                                  genomic_plus ? genomic_pos : genomic_pos - genomic_len + 1,
+                                  product_plus ? product_pos : product_pos - product_len + 1,
                                   genomic_len,
-                                  true));
-                        }
-                    } else {
-                        if (product_len != 0  &&  product_len == genomic_len  &&
-                            (direction == CAlnUserOptions::eBothDirections  ||
-                             (direct ?
-                              direction == CAlnUserOptions::eDirect :
-                              direction == CAlnUserOptions::eReverse))) {
-                            /// insert the range
-                            if (row_1 == 0) {
-                                pairwise_aln.insert
-                                    (CPairwiseAln::TAlnRng
-                                     (product_plus ? product_pos : product_pos - product_len + 1,
-                                      genomic_plus ? genomic_pos : genomic_pos - genomic_len + 1,
-                                      genomic_len,
-                                      direct));
-                            } else {
-                                pairwise_aln.insert
-                                    (CPairwiseAln::TAlnRng
-                                     (genomic_plus ? genomic_pos : genomic_pos - genomic_len + 1,
-                                      product_plus ? product_pos : product_pos - product_len + 1,
-                                      genomic_len,
-                                      direct));
-                            }
+                                  direct));
                         }
                     }
                 }
