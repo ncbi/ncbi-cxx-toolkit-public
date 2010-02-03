f818c993394c507ecde4a991f6ae32b4332ed3f1
kazimird <kazimird@ncbi.nlm.nih.gov>
2010-02-03 20:32:34 +0000

    Initial revision of gicache.[hc] taken from CVS::distrib/network/sybutils/ctlib. JIRA: CXX-439
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@44725 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ src/objtools/data_loaders/genbank/gicache/gicache.h
@@ -0,0 +1,44 @@
+#ifndef _GI_CACHE_HPP__
+#define _GI_CACHE_HPP__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define DEFAULT_GI_CACHE_PATH "/panfs/pan1.be-md.ncbi.nlm.nih.gov/id_dumps/gi_cache"
+#define DEFAULT_GI_CACHE_PREFIX "gi2acc"
+
+/* Populates the cache */
+int         GICache_PopulateAccessions(char *server, const char *cache_prefix,
+                                       const char *sql_gi_cond);
+
+/* Initializes the cache. If cache_prefix argument is not provided, default name
+ * is used. If local cache is not available, use default path and prefix. 
+ */
+void        GICache_ReadData(const char *cache_prefix);
+/* Remaps cache files */
+void        GICache_ReMap(int delay_in_sec);
+/* Retrieves accession.version by gi.
+ * Accession buffer must be preallocated by caller. 
+ * If buffer is too small, retrieved accession is truncated, and return code 
+ * is 0, otherwise return code is 1.
+ */
+int         GICache_GetAccession(int gi, char* acc, int buf_len);
+/* Retrieves gi length */
+int         GICache_GetLength(int gi);
+/* Returns maximal gi available in cache */
+int         GICache_GetMaxGi(void);
+
+/* Internal loading interface, non MT safe */
+/* Initialize cache for loading */
+int         GICache_LoadStart(const char *cache_prefix);
+/* Add gi's data to cache */
+int         GICache_LoadAdd(int gi, int len, const char* accession, int version);
+/* Finish load, flush modifications to disk */
+int         GICache_LoadEnd(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
