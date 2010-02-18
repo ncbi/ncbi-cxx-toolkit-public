e3a0334a9e39be71b33680cdaa23e98c9766d095
Anton Lavrentiev <lavr@ncbi.nlm.nih.gov>
2010-02-18 17:51:25 +0000

    Check localhost:5556 w/GNUTLS
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@44815 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/connect/test/test_ncbi_http_get.sh
+++ src/connect/test/test_ncbi_http_get.sh
@@ -7,7 +7,11 @@ if [ "`echo $FEATURES | grep -wic '[^-]GNUTLS'`" = "1" ]; then
     CONN_USESSL=1
     CONN_GNUTLS_LOGLEVEL=7
     export CONN_USESSL CONN_GNUTLS_LOGLEVEL
-    url='https://test.gnutls.org:5556'
+    if [ "`netstat -a -n | grep -w 5556 | grep -c ':5556'`" != "0" ]; then
+      url='https://localhost:5556'
+    else
+      url='https://test.gnutls.org:5556'
+    fi
 else
     url='http://www.ncbi.nlm.nih.gov/entrez/viewer.cgi?view=0&maxplex=1&save=idf&val=4959943'
 fi
