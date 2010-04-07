4a2c995f8f99100c7740043ac182f7aae5210c39
Anton Lavrentiev <lavr@ncbi.nlm.nih.gov>
2010-04-07 18:27:49 +0000

    Additional error codes
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@45378 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/connect/error_codes.hpp
+++ include/connect/error_codes.hpp
@@ -44,7 +44,7 @@ BEGIN_NCBI_SCOPE
 
 // Here are only error codes used in C++ sources. For error codes used in
 // C sources see src/connect/ncbi_priv.h.
-NCBI_DEFINE_ERRCODE_X(Connect_Stream,    315,  8);
+NCBI_DEFINE_ERRCODE_X(Connect_Stream,    315, 10);
 NCBI_DEFINE_ERRCODE_X(Connect_Pipe,      316, 16);
 NCBI_DEFINE_ERRCODE_X(Connect_ThrServer, 317,  8);
 NCBI_DEFINE_ERRCODE_X(Connect_Core,      318,  8);
