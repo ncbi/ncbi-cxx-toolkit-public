574026813db4271f312fd2581ba6490fe44d886e
Alexandre Souvorov <souvorov@ncbi.nlm.nih.gov>
2010-05-04 14:58:56 +0000

    Do not recover bad CDSes with protein info; set open flag if not conf Start without UTR
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@45643 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/algo/gnomon/score.cpp
+++ src/algo/gnomon/score.cpp
@@ -1415,13 +1415,6 @@ void CGnomonEngine::GetScore(CGeneModel& model) const
 
     CCDSInfo cds_info = model.GetCdsInfo();
 
-    if (best_score == BadScore() && (model.Type()&CGeneModel::eProt)!=0 ) {    // try to recover CDS if there are proteins
-        cds_info.Clear();
-        model.SetCdsInfo( cds_info );
-        FindStartsStops(model, m_data->m_ds[model.Strand()], mrna, mrnamap, starts, stops, frame);
-        best_score = SelectBestReadingFrame(model, mrna, mrnamap, starts, stops, frame, best_start, best_stop);
-    }
-
     if (best_score == BadScore()) {
         cds_info.Clear();
         model.SetCdsInfo( cds_info );
@@ -1429,14 +1422,14 @@ void CGnomonEngine::GetScore(CGeneModel& model) const
     }
 
     bool is_open = false;
-    if( best_start<0) {
-        if(starts[frame].size() > 1) {
-            int new_start = starts[frame][1];
-            int newlen = best_stop-new_start;
-            if(newlen > 75 || cds_info.ConfirmedStart()) {
-                is_open = !cds_info.ConfirmedStart();
-                best_start = new_start;
-            }
+    if(best_start == 0) {
+        is_open = !cds_info.ConfirmedStart();
+    } else if(best_start<0 && starts[frame].size() > 1) {
+        int new_start = starts[frame][1];
+        int newlen = best_stop-new_start;
+        if(newlen > 75 || cds_info.ConfirmedStart()) {
+            is_open = !cds_info.ConfirmedStart();
+            best_start = new_start;
         }
     }
 
