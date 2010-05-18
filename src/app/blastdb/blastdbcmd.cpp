20e5b2b1f284c97f671016e77efc45275d1cde17
maning <maning@ncbi.nlm.nih.gov>
2010-05-18 18:13:49 +0000

    Support entries like "1p86".   JIRA SB-581
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@45790 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/app/blastdb/blastdbcmd.cpp
+++ src/app/blastdb/blastdbcmd.cpp
@@ -134,30 +134,22 @@ CBlastDBCmdApp::x_AddSeqId(CBlastDBCmdApp::TQueries& retval,
         return;
     } 
 
-    string lcl_entry = entry;
-    CRef<CSeq_id> sid;
-    try {
-        sid.Reset(new CSeq_id(entry));
-    } catch(CException e) {
-        lcl_entry = "lcl|" + entry;
-        sid.Reset(new CSeq_id(lcl_entry));
-    }
-
     // FASTA / target_only just need one id
-    if (m_FASTA || m_TargetOnly) {
-        if (m_TargetOnly && !sid->IsGi()) {
+    if (m_TargetOnly) {
+        if (!NStr::StringToInt(entry, NStr::fConvErr_NoThrow) 
+         && !CSeq_id(entry).IsGi()) {
             NCBI_THROW(CInputException, eInvalidInput, 
                 "target_only must be used with gi entry.");
         }
-        retval.push_back(CRef<CBlastDBSeqId>(new CBlastDBSeqId(lcl_entry)));
+        retval.push_back(CRef<CBlastDBSeqId>(new CBlastDBSeqId(entry)));
         return;
     } 
 
     // Default: find oid first and add all pertinent
-    int oid;
-    m_BlastDb->SeqidToOid(*sid, oid);
-    if (oid>=0) {
-        x_AddOid(retval, oid);
+    vector<int> oids;
+    m_BlastDb->AccessionToOids(entry, oids);
+    if (!oids.empty()) {
+        x_AddOid(retval, oids[0]);
     }
 }
 
