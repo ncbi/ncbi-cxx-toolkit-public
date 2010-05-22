fa7e53cef497099f433565f8668fd308a040926e
Anton Lavrentiev <lavr@ncbi.nlm.nih.gov>
2010-05-22 22:25:21 +0000

    Revert to 167590 (PREV-2) to rollback FreeBSD bug-around
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@45834 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/connect/test/test_assert.h
+++ src/connect/test/test_assert.h
@@ -36,24 +36,6 @@
  */
 
 #include "../ncbi_config.h"
-#if 0
-#ifdef NCBI_OS_BSD
-#  include <sys/param.h>
-#  ifdef __FreeBSD_version
-#    if __FreeBSD_version / 100000 == 8
-     /* If a client orderly closes a data connection and does some reconnect
-      * attempts (connect / close in rather rapid succession), while the
-      * server side proceeds with closing the original connection, FreeBSD
-      * 8.0 sometimes returns -1 from the server's close() with errno set to
-      * "Connection reset by peer".
-      * We consider this behavior as a kernel bug / race condition as it
-      * disappears if either server or client (or both) get ktrace'd.
-      */
-#      define TEST_IGNORE_CLOSE 1
-#    endif /*__FreeBSD_version/100000==8*/
-#  endif /*__FreeBSD_version*/
-#endif /*NCBI_OS_BSD*/
-#endif
 #include "test_assert_impl.h"
 
 #endif  /* TEST_ASSERT__H */
