41d7761252d16b272c361843dc05be22b43a283f
Michael DiCuccio <dicuccio@ncbi.nlm.nih.gov>
2010-06-09 12:58:44 +0000

    White space change in comments
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@46023 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/format/comment_item.cpp
+++ src/objtools/format/comment_item.cpp
@@ -841,7 +841,7 @@ void CGenomeAnnotComment::x_GatherInfo(CBioseqContext& ctx)
 
     CNcbiOstrstream text;
 
-    text << *refseq << " INFORMATION:  ";
+    text << *refseq << " INFORMATION: ";
     if ( !m_GenomeBuildNumber.empty() ) {
          text << "Features on this sequence have been produced for build "
               << m_GenomeBuildNumber << " of the NCBI's genome annotation"
