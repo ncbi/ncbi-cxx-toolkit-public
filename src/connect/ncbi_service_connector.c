e4858c40a329a185cf60c2eb7f682787f843b4f2
Anton Lavrentiev <lavr@ncbi.nlm.nih.gov>
2010-06-10 19:12:01 +0000

    While adjusting from LB info, delete the Host: tag unconditionally
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@46080 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/connect/ncbi_service_connector.c
+++ src/connect/ncbi_service_connector.c
@@ -426,11 +426,9 @@ static int/*bool*/ s_AdjustNetInfo(SConnNetInfo* net_info,
         strcpy(net_info->host, uuu->net_info->host);
         net_info->port = uuu->net_info->port;
     }
-    if (net_info->http_proxy_adjusted) {
-        ConnNetInfo_DeleteUserHeader(net_info, "Host:");
-        net_info->http_proxy_adjusted = 0/*false*/;
-    }
 
+    ConnNetInfo_DeleteUserHeader(net_info, "Host:");
+    net_info->http_proxy_adjusted = 0/*false*/;
     return 1/*true - adjusted*/;
 }
 
