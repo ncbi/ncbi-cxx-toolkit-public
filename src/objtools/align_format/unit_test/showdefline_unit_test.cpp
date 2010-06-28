2b8a6e4c677a47da5fc764a29ca1e34eba8f6aa1
Irena Zaretskaya <zaretska@ncbi.nlm.nih.gov>
2010-06-28 17:21:11 +0000

    Fixed unit test data to adjust to JIRA WB-437
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@46302 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/align_format/unit_test/showdefline_unit_test.cpp
+++ src/objtools/align_format/unit_test/showdefline_unit_test.cpp
@@ -121,11 +121,9 @@ struct CShowBlastDeflineTest : public CShowBlastDefline {
         string evalue_string[] = {"2e-141", "3e-141"};
         string bit_string[] = {" 503", " 502"};
         int sum_n[] = {1, 1};
-                        
-        string id_url[] = {"<a href=\"http://www.ncbi.nlm.nih.gov/entrez/query.\
-fcgi?cmd=Retrieve&db=Nucleotide&list_uids=18426812&dopt=GenBank&RID=&log$=nucltop&blast_rank=1\" >",
-                           "<a href=\"http://www.ncbi.nlm.nih.gov/entrez/query.\
-fcgi?cmd=Retrieve&db=Nucleotide&list_uids=6680636&dopt=GenBank&RID=&log$=nucltop&blast_rank=1\" >"};
+                
+        string id_url[] = {"<a href=\"http://www.ncbi.nlm.nih.gov/nucleotide/18426812?report=genbank&log$=nucltop&blast_rank=1&RID=\" >",
+                           "<a href=\"http://www.ncbi.nlm.nih.gov/nucleotide/6680636?report=genbank&log$=nucltop&blast_rank=1&RID=\" >"};
         string score_url[] = {"<a href=#18426812>", "<a href=#6680636>"};
         bool is_new[] = {false, false};
         bool was_checked[] = {false, false};
