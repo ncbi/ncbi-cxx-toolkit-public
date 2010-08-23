4b7deec99099dfe3b227caa661d48296a494fbaf
Sergey Satskiy <satskyse@ncbi.nlm.nih.gov>
2010-08-23 15:17:09 +0000

    Fix: suppress a warning from the newest compilers by adding the suggested parentheses (JIRA: CXX-287)
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@46904 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/util/impl/floating_point_comparison.hpp
+++ include/util/impl/floating_point_comparison.hpp
@@ -93,8 +93,8 @@ safe_fpt_division( FPT f1, FPT f2 )
         return fpt_limits<FPT>::max_value();
 
     // Avoid underflow.
-    if( f1 == static_cast<FPT>(0) ||
-        f2 > static_cast<FPT>(1) && f1 < f2*fpt_limits<FPT>::min_value() )
+    if( (f1 == static_cast<FPT>(0)) ||
+        ((f2 > static_cast<FPT>(1)) && (f1 < f2*fpt_limits<FPT>::min_value())) )
         return static_cast<FPT>(0);
 
     return f1/f2;
