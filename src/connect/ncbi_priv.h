2e4b7c69c516952ac411f132ca1eb453ef93ba82
Anton Lavrentiev <lavr@ncbi.nlm.nih.gov>
2010-10-14 18:32:45 +0000

    New SOCK errcodes
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@47537 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/connect/ncbi_priv.h
+++ src/connect/ncbi_priv.h
@@ -252,7 +252,7 @@ NCBI_C_DEFINE_ERRCODE_X(Connect_HTTP,       307,  18);
 NCBI_C_DEFINE_ERRCODE_X(Connect_LB,         308,   0);
 NCBI_C_DEFINE_ERRCODE_X(Connect_Sendmail,   309,  31);
 NCBI_C_DEFINE_ERRCODE_X(Connect_Service,    310,   8);
-NCBI_C_DEFINE_ERRCODE_X(Connect_Socket,     311, 150);
+NCBI_C_DEFINE_ERRCODE_X(Connect_Socket,     311, 153);
 NCBI_C_DEFINE_ERRCODE_X(Connect_Crypt,      312,   6);
 NCBI_C_DEFINE_ERRCODE_X(Connect_LocalNet,   313,  11);
 NCBI_C_DEFINE_ERRCODE_X(Connect_Mghbn,      319,  16);
