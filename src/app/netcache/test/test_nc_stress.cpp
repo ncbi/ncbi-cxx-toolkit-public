5f6df0d80d87c293672dc55415eaab9bd4269d51
kazimird <kazimird@ncbi.nlm.nih.gov>
2010-11-02 15:36:11 +0000

    In test_nc_stress.cpp: minor code clean-up; removed commented pieces of code.  JIRA: CXX-1979
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@47821 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/app/netcache/test/test_nc_stress.cpp
+++ src/app/netcache/test/test_nc_stress.cpp
@@ -50,7 +50,6 @@
 
 USING_NCBI_SCOPE;
 
-    
 ///////////////////////////////////////////////////////////////////////
 
 
@@ -79,31 +78,28 @@ public:
     int Run(void);
 private:
 
-    void StressTestPutGet(size_t           blob_size, 
-                          unsigned         bcount, 
+    void StressTestPutGet(size_t           blob_size,
+                          unsigned         bcount,
                           TTransactionLog* tlog);
 
-    void StressTestPut(size_t           blob_size, 
-                       unsigned         bcount, 
+    void StressTestPut(size_t           blob_size,
+                       unsigned         bcount,
                        TTransactionLog* tlog);
 
     void DeleteRandomBlobs(TTransactionLog* tlog, unsigned bcount);
 
     /// Read BLOB according to the transaction log
     /// (all of them straight or randomly)
-    void StressTestGet(const TTransactionLog& tlog, 
+    void StressTestGet(const TTransactionLog& tlog,
                        bool                   random,
                        unsigned               count);
-
-    CNetCacheAPI GetClient() const { return m_API; }
 
     unsigned char* AllocateTestBlob(size_t blob_size) const;
 
     void CheckBlob(unsigned char* blob, size_t blob_size) const;
 
-    void CheckGetBlob(const STransactionInfo& ti, 
-                      unsigned char*          buf,
-                      CNetCacheAPI&           cl);
+    void CheckGetBlob(const STransactionInfo& ti,
+                      unsigned char*          buf);
     void AddToAppLog(const TTransactionLog& tlog);
 
 private:
@@ -115,61 +111,52 @@ private:
 };
 
 
-
-int  s_Count;
-Int8 s_MaxSize;
-
+static int  s_Count;
+static Int8 s_MaxSize;
 
 
 void CTestNetCacheStress::StressTestPutGet(size_t           blob_size,
                                            unsigned         bcount,
                                            TTransactionLog* tlog)
 {
-    /*m_Os << "Put BLOBs size=" << blob_size << " count=" << bcount 
-             << NcbiEndl;*/
-
     StressTestPut(blob_size, bcount, tlog);
-
-    /*m_Os << "Get BLOBs size=" << blob_size << " count=" << bcount 
-             << NcbiEndl << NcbiEndl;*/
 
     StressTestGet(*tlog, false /*random*/, bcount);
 }
 
 
-void CTestNetCacheStress::CheckGetBlob(const STransactionInfo& ti, 
-                                       unsigned char*          buf,
-                                       CNetCacheAPI&           cl)
+void CTestNetCacheStress::CheckGetBlob(const STransactionInfo& ti,
+                                       unsigned char*          buf)
 {
     try {
         size_t n_read, bsize;
 
-        CNetCacheAPI::EReadResult rres = 
-            cl.GetData(ti.key, buf, ti.blob_size, &n_read, &bsize);
+        CNetCacheAPI::EReadResult rres =
+            m_API.GetData(ti.key, buf, ti.blob_size, &n_read, &bsize);
 
         if (rres == CNetCacheAPI::eNotFound && ti.deleted == false) {
-            LOG_POST(Error << "BLOB not found: " 
-                            << ti.key << " " 
-                            << ti.blob_size << " " 
+            LOG_POST(Error << "BLOB not found: "
+                            << ti.key << " "
+                            << ti.blob_size << " "
                             << ti.time_stamp);
             return;
         }
 
         if (ti.deleted && rres != CNetCacheAPI::eNotFound) {
-            LOG_POST(Error << "Deleted BLOB found: " 
-                            << ti.key << " " 
-                            << bsize << " " 
+            LOG_POST(Error << "Deleted BLOB found: "
+                            << ti.key << " "
+                            << bsize << " "
                             << ti.time_stamp);
             return;
         }
         if (rres == CNetCacheAPI::eNotFound) {
             return;
         }
-        
+
         if (n_read != ti.blob_size) {
-            LOG_POST(Error << "BLOB read error: " 
-                            << ti.key << " " 
-                            << ti.blob_size << " " 
+            LOG_POST(Error << "BLOB read error: "
+                            << ti.key << " "
+                            << ti.blob_size << " "
                             << ti.time_stamp << " "
                             << "n_read=" << n_read << " "
                             << "blob_size=" << ti.blob_size << " "
@@ -177,9 +164,9 @@ void CTestNetCacheStress::CheckGetBlob(const STransactionInfo& ti,
             return;
         }
         if (bsize != ti.blob_size) {
-            LOG_POST(Error << "BLOB size error: " 
-                            << ti.key << " " 
-                            << ti.blob_size << " " 
+            LOG_POST(Error << "BLOB size error: "
+                            << ti.key << " "
+                            << ti.blob_size << " "
                             << ti.time_stamp << " "
                             << "n_read=" << n_read << " "
                             << "blob_size=" << ti.blob_size << " "
@@ -192,18 +179,18 @@ void CTestNetCacheStress::CheckGetBlob(const STransactionInfo& ti,
     catch (CNetCacheException& ex)
     {
         if (!ti.deleted) {
-            LOG_POST(Error << "BLOB check error (NetCache exception): " 
-                            << ti.key << " " 
-                            << ti.blob_size << " " 
+            LOG_POST(Error << "BLOB check error (NetCache exception): "
+                            << ti.key << " "
+                            << ti.blob_size << " "
                             << ti.time_stamp << " "
                             << ex.what());
         }
     }
     catch (exception& ex)
     {
-        LOG_POST(Error << "BLOB check error: " 
-                        << ti.key << " " 
-                        << ti.blob_size << " " 
+        LOG_POST(Error << "BLOB check error: "
+                        << ti.key << " "
+                        << ti.blob_size << " "
                         << ti.time_stamp << " "
                         << ex.what());
     }
@@ -214,69 +201,56 @@ void CTestNetCacheStress::StressTestGet(const TTransactionLog& tlog,
                                         bool                   random,
                                         unsigned               count)
 {
-    CNetCacheAPI cl = GetClient();
-
     size_t   buf_size = 0;
-    unsigned char* buf = 0;
+    unsigned char* buf = NULL;
 
     if (random) {
         for (unsigned i = 0; i < count; ++i) {
             unsigned idx = rand() % count;
             const STransactionInfo& ti = tlog[idx];
 
-            if (buf_size < ti.blob_size || buf == 0) {
+            if (buf_size < ti.blob_size || buf == NULL) {
                 delete [] buf;
                 buf = new unsigned char[buf_size = ti.blob_size];
             }
 
             try {
-                CheckGetBlob(ti, buf, cl);
+                CheckGetBlob(ti, buf);
             }
             catch (exception& ex)
             {
-                LOG_POST(Error << "BLOB check error: " 
-                                << ti.key << " " 
-                                << ti.blob_size << " " 
+                LOG_POST(Error << "BLOB check error: "
+                                << ti.key << " "
+                                << ti.blob_size << " "
                                 << ti.time_stamp << " "
                                 << ex.what());
             }
-
-            /*if (i % 50 == 1) {
-                m_Os << "." << flush;
-            }*/
         } // for
 
     } else {
         for (unsigned i = 0; i < count; ++i) {
             const STransactionInfo& ti = tlog[i];
 
-            if (buf_size < ti.blob_size || buf == 0) {
+            if (buf_size < ti.blob_size || buf == NULL) {
                 delete [] buf;
                 buf = new unsigned char[buf_size = ti.blob_size];
             }
 
             try {
-                CheckGetBlob(ti, buf, cl);
+                CheckGetBlob(ti, buf);
             }
             catch (exception& ex)
             {
-                LOG_POST(Error << "BLOB check error: " 
-                                << ti.key << " " 
-                                << ti.blob_size << " " 
+                LOG_POST(Error << "BLOB check error: "
+                                << ti.key << " "
+                                << ti.blob_size << " "
                                 << ti.time_stamp << " "
                                 << ex.what());
             }
-
-            /*if (i % 50 == 1) {
-                m_Os << "." << flush;
-            }*/
         } // for
-
     }
 
-    //m_Os << NcbiEndl;
-
-    delete buf;
+    delete[] buf;
 }
 
 
@@ -284,46 +258,42 @@ void CTestNetCacheStress::StressTestPut(size_t           blob_size,
                                         unsigned         bcount,
                                         TTransactionLog* tlog)
 {
-    CNetCacheAPI cl = GetClient();
-
     unsigned char* buf = AllocateTestBlob(blob_size);
-    CheckBlob(buf, blob_size); // just in case
 
-    // Put blobs
+    try {
+        // Put blobs
 
-    for (unsigned i = 0; i < bcount; ++i) {
-        STransactionInfo ti;
-        ti.blob_size = blob_size;
-        ti.time_stamp = GetFastLocalTime().AsString();
-        ti.deleted = false;
+        for (unsigned i = 0; i < bcount; ++i) {
+            STransactionInfo ti;
+            ti.blob_size = blob_size;
+            ti.time_stamp = GetFastLocalTime().AsString();
+            ti.deleted = false;
 
-        if (rand() % 2) {
-            ti.key = cl.PutData(buf, blob_size);
-        } else {
-            auto_ptr<IWriter> writer(cl.PutData(&ti.key));
-            auto_ptr<CWStream> os(new CWStream(writer.release(), 0,0, 
-                                               CRWStreambuf::fOwnWriter));
-            os->write((char*)buf, blob_size);
-        }
+            if (rand() % 2) {
+                ti.key = m_API.PutData(buf, blob_size);
+            } else {
+                auto_ptr<IWriter> writer(m_API.PutData(&ti.key));
+                auto_ptr<CWStream> os(new CWStream(writer.release(), 0,0,
+                                                   CRWStreambuf::fOwnWriter));
+                os->write((char*)buf, blob_size);
+            }
 
-        if ((rand() & 3) == 0 && !cl.HasBlob(ti.key))
-            LOG_POST(Error << "\nERROR: newly added BLOB disappeared: " <<
-                ti.key << " size " << ti.blob_size <<
-                " timestamp " << ti.time_stamp);
+            if ((rand() & 3) == 0 && !m_API.HasBlob(ti.key))
+                LOG_POST(Error << "\nERROR: newly added BLOB disappeared: " <<
+                    ti.key << " size " << ti.blob_size <<
+                    " timestamp " << ti.time_stamp);
 
-        if (tlog) {
-            tlog->push_back(ti);
-        }
-
-        /*if (i % 50 == 1) {
-            m_Os << "." << flush;
-        }*/
-
-    } // for
+            if (tlog) {
+                tlog->push_back(ti);
+            }
+        } // for
+    }
+    catch (...) {
+        delete[] buf;
+        throw;
+    }
 
     delete [] buf;
-
-    //m_Os << NcbiEndl;
 }
 
 
@@ -333,8 +303,6 @@ void CTestNetCacheStress::DeleteRandomBlobs(TTransactionLog* tlog,
     if (!bcount)
         return;
 
-    CNetCacheAPI cl = GetClient();
-
     for (unsigned i = 0; i < bcount; ++i) {
         unsigned idx = rand() % bcount;
         STransactionInfo& ti = (*tlog)[idx];
@@ -343,21 +311,17 @@ void CTestNetCacheStress::DeleteRandomBlobs(TTransactionLog* tlog,
             continue;
 
         try {
-            cl.Remove(ti.key);
+            m_API.Remove(ti.key);
             ti.deleted = true;
         }
         catch (exception& ex)
         {
-            LOG_POST(Error << "BLOB remove error: " 
-                            << ti.key << " " 
-                            << ti.blob_size << " " 
+            LOG_POST(Error << "BLOB remove error: "
+                            << ti.key << " "
+                            << ti.blob_size << " "
                             << ti.time_stamp << " "
                             << ex.what());
         }
-
-        /*if (i % 50 == 1) {
-            m_Os << "." << flush;
-        }*/
     } // for
 }
 
@@ -383,11 +347,11 @@ unsigned char* CTestNetCacheStress::AllocateTestBlob(size_t blob_size) const
 }
 
 
-void CTestNetCacheStress::CheckBlob(unsigned char* blob, 
+void CTestNetCacheStress::CheckBlob(unsigned char* blob,
                                     size_t blob_size) const
 {
     if (blob_size == 0) {
-        throw runtime_error("AllocateTestBlob(): Invalid blob size");
+        throw runtime_error("CheckBlob(): Invalid blob size");
     }
 
     unsigned char init = *blob;
@@ -395,11 +359,10 @@ void CTestNetCacheStress::CheckBlob(unsigned char* blob,
     for (size_t i = 1; i < blob_size; ++i) {
         unsigned char prev = blob[i-1] + init;
         if (blob[i] != prev) {
-            throw runtime_error("Incorrect blob value at position:" + 
+            throw runtime_error("Incorrect blob value at position:" +
                                 NStr::IntToString(i));
         }
     } // for
-
 }
 
 void CTestNetCacheStress::AddToAppLog(const TTransactionLog& tlog)
@@ -422,18 +385,9 @@ int CTestNetCacheStress::Run(void)
     client_ip += NStr::IntToString(m_Num);
     CDiagContext::GetRequestContext().SetClientIP(client_ip);
 
-    /*size_t  bsizes[] = {256, 512, 1024, 
-                    1024 * 4, 1024 * 10, 1024 * 100, 1024 * 256,
-                    1024 * 1024, 1024 * 1024 * 2, 1024 * 1024 * 12};*/
-
-    //size_t bsizes[] = { 10240 };
-
-    //unsigned bins = sizeof(bsizes) / sizeof(bsizes[0]);
-    //int bin_count = (s_Count / bins) * 2;
     Int8 total_count = 0;
     for (int count = 0; count < s_Count; ++count) {
         try {
-            //m_Os << "Iteration " << count << ", thread " << m_Num << NcbiEndl;
             TTransactionLog tlog;
             int blob_size = int(rand() * s_MaxSize / RAND_MAX + 1);
             total_count += blob_size;
@@ -445,7 +399,6 @@ int CTestNetCacheStress::Run(void)
                 m_Os << "Progress: " << count << " out of " << s_Count
                      << " in thread " << CThread::GetSelf() << NcbiEndl;
 
-                //m_Os << "Random delete...." << NcbiEndl;
                 DeleteRandomBlobs(&m_Log, m_Log.size()/10);
             }
         }
@@ -461,13 +414,7 @@ int CTestNetCacheStress::Run(void)
          << " = " << total_count << NcbiEndl;
 
 
-    //m_Os << "Waiting...." << NcbiEndl;
-    //SleepMilliSec(10 * 1000);
-
-    //m_Os << "Random delete...." << NcbiEndl;
     DeleteRandomBlobs(&m_Log, m_Log.size()/10);
-
-    //m_Os << "Random read test...." << NcbiEndl;
 
     StressTestGet(m_Log, true /*random*/, m_Log.size());
 
@@ -477,9 +424,11 @@ int CTestNetCacheStress::Run(void)
 class CTestNetCacheStressThread : public CThread
 {
 public:
-    CTestNetCacheStressThread(CNetCacheAPI& api, CNcbiOstream& os, int num)
-        : m_Test(new CTestNetCacheStress(api, os, num))
-    {}
+    CTestNetCacheStressThread(CNetCacheAPI::TInstance api,
+            CNcbiOstream& os, int num) :
+        m_Test(new CTestNetCacheStress(api, os, num))
+    {
+    }
 
     virtual void* Main()
     {
@@ -511,36 +460,32 @@ void CTestNetCacheStressApp::Init(void)
     // Specify USAGE context
     arg_desc->SetUsageContext(GetArguments().GetProgramBasename(),
                               "Net cache client");
-    
-    arg_desc->AddPositional("service", 
-        "NetCache service name or host:port", 
+
+    arg_desc->AddPositional("service",
+        "NetCache service name or host:port",
         CArgDescriptions::eString);
 
-    arg_desc->AddOptionalKey("count", 
+    arg_desc->AddOptionalKey("count",
                              "count",
                              "Number of blobs to submit",
                              CArgDescriptions::eInteger);
-    
-    arg_desc->AddOptionalKey("size", 
+
+    arg_desc->AddOptionalKey("size",
                              "size",
                              "Maximum size of blobs to submit",
                              CArgDescriptions::eInteger);
-    
-    arg_desc->AddOptionalKey("threads", 
+
+    arg_desc->AddOptionalKey("threads",
                              "threads",
                              "Number of threads",
                              CArgDescriptions::eInteger);
 
-    arg_desc->AddOptionalKey("timeout", 
+    arg_desc->AddOptionalKey("timeout",
                              "timeout",
                              "Communication timeout in msec",
                              CArgDescriptions::eInteger);
     // Setup arg.descriptions for this application
     SetupArgDescriptions(arg_desc.release());
-
-    //SetDiagPostLevel(eDiag_Info);
-    //SetDiagTrace(eDT_Enable);
-
 }
 
 
@@ -571,13 +516,13 @@ int CTestNetCacheStressApp::Run(void)
     }
 
 
-    auto_ptr<CNetCacheAPI> nc(new CNetCacheAPI(service_name, "stress_test"));
+    CNetCacheAPI nc(service_name, "stress_test");
 
     STimeout to = {timeout/1000, timeout*1000};
-    nc->SetCommunicationTimeout(to);
+    nc.SetCommunicationTimeout(to);
 
     if (threads < 2) {
-       auto_ptr<CTestNetCacheStress> test(new CTestNetCacheStress(*nc, cout, 0));
+       auto_ptr<CTestNetCacheStress> test(new CTestNetCacheStress(nc, cout, 0));
        return test->Run();
     }
 
@@ -585,7 +530,7 @@ int CTestNetCacheStressApp::Run(void)
     thread_list.reserve(threads);
     for (unsigned i = 0; i < threads; ++i) {
         CRef<CThread> thread(
-               new CTestNetCacheStressThread(*nc, cout, i + 1));
+               new CTestNetCacheStressThread(nc, cout, i + 1));
         thread_list.push_back(thread);
         thread->Run();
     } // for
