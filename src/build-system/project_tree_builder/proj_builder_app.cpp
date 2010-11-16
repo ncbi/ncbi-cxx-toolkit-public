de8d8c206604ab4c7fd48ea69a3b4f49e9babf78
Andrei Gourianov <gouriano@ncbi.nlm.nih.gov>
2010-11-16 16:19:11 +0000

    Changed version number. JIRA: CXX-2011
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@47941 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/build-system/project_tree_builder/proj_builder_app.cpp
+++ src/build-system/project_tree_builder/proj_builder_app.cpp
@@ -400,7 +400,7 @@ struct PIsExcludedByDisuse
 //-----------------------------------------------------------------------------
 CProjBulderApp::CProjBulderApp(void)
 {
-    SetVersion( CVersionInfo(3,1,0) );
+    SetVersion( CVersionInfo(3,1,1) );
     m_ScanningWholeTree = false;
     m_Dll = false;
     m_AddMissingLibs = false;
