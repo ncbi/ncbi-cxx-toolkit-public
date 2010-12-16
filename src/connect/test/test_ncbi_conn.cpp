30e056fcea5d1b0f18aa5e4b961b8a439a1ba5da
Anton Lavrentiev <lavr@ncbi.nlm.nih.gov>
2010-12-16 15:20:59 +0000

    Reformat final message JIRA: CXX-2043
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@48166 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/connect/test/test_ncbi_conn.cpp
+++ src/connect/test/test_ncbi_conn.cpp
@@ -148,15 +148,15 @@ int CTest::Run(void)
 
     if (status != eIO_Success) {
         list<string> msg;
-        NStr::Justify("Check " + CDirEntry::CreateAbsolutePath(kLogfile)
-                      + " for more information.", PAGE_WIDTH, msg);
+        NStr::Justify("Detailed transaction log has been saved in:",
+                      PAGE_WIDTH, msg);
+        msg.push_back('"' + CDirEntry::CreateAbsolutePath(kLogfile) + "\".");
         msg.push_back(kEmptyStr);
-        NStr::Justify("If you choose to make its contents available to"
-                      " NCBI, please keep in mind that the log can"
-                      " contain authorization credentials, which you may"
-                      " want to delete from the file prior to actually"
-                      " submitting it for review.", PAGE_WIDTH, msg,
-                      "      ", "NOTE: ");
+        NStr::Justify("Should you choose to make its contents available to"
+                      " NCBI, please keep in mind that the log can contain"
+                      " authorization credentials, which you may want to"
+                      " delete from the file prior to actually submitting"
+                      " it for review.", PAGE_WIDTH, msg, "      ", "NOTE: ");
         ITERATE(list<string>, line, msg) {
             m_Tee << NcbiEndl << *line;
         }
