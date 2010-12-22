0052b4083f351a4115608fc7582be3c90276e330
Anton Lavrentiev <lavr@ncbi.nlm.nih.gov>
2010-12-22 18:09:10 +0000

    Comments
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@48224 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/corelib/stream_utils.cpp
+++ src/corelib/stream_utils.cpp
@@ -185,10 +185,10 @@ CT_POS_TYPE CPushback_Streambuf::seekoff(CT_OFF_TYPE off,
                                          IOS_BASE::seekdir whence,
                                          IOS_BASE::openmode which)
 {
-    if (whence == ios::cur  &&  (which & ios::in)) {
-        if (off == 0  &&  which == ios::in) {
+    if (whence == IOS_BASE::cur  &&  (which & IOS_BASE::in)) {
+        if (off == 0  &&  which == IOS_BASE::in) {
             // it's a call from tellg()
-            CT_POS_TYPE ret = m_Sb->PUBSEEKOFF(0, ios::cur, ios::in);
+            CT_POS_TYPE ret = m_Sb->PUBSEEKOFF(0, IOS_BASE::cur, IOS_BASE::in);
             if (ret != (CT_POS_TYPE)((CT_OFF_TYPE)(-1))) {
                 off = (CT_OFF_TYPE)(egptr() - gptr());
                 if ((CT_OFF_TYPE) ret >= off) {
