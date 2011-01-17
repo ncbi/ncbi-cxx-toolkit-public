7cef8676cb0c67ee62bb19812a185cf759e9b374
Eyal Mozes <mozese2@ncbi.nlm.nih.gov>
2011-01-17 16:36:22 +0000

    JIRA GP-1300 1. Add high-quality coverage to the unit test. 2. In the unit test only test scores that are in the input data.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@48437 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/algo/align/util/unit_test/score_builder_unit_test.cpp
+++ src/algo/align/util/unit_test/score_builder_unit_test.cpp
@@ -63,6 +63,7 @@
 
 #include <common/test_assert.h>  /* This header must go last */
 
+extern const char *sc_TestEntries;
 
 USING_NCBI_SCOPE;
 USING_SCOPE(objects);
@@ -84,6 +85,21 @@ BOOST_AUTO_TEST_CASE(Test_Score_Builder)
     CGBDataLoader::RegisterInObjectManager(*om);
     CRef<CScope> scope(new CScope(*om));
     scope->AddDefaults();
+
+    {{
+         CNcbiIstrstream istr(sc_TestEntries);
+         while (istr) {
+             CRef<CSeq_entry> entry(new CSeq_entry);
+             try {
+                 istr >> MSerial_AsnText >> *entry;
+             }
+             catch (CEofException&) {
+                 break;
+             }
+             scope->AddTopLevelSeqEntry(*entry);
+         }
+    }}
+
 
     CScoreBuilder score_builder(blast::eBlastn);
 
@@ -122,49 +138,65 @@ BOOST_AUTO_TEST_CASE(Test_Score_Builder)
         ///
 
         int kExpectedIdentities = 0;
+        bool has_kExpectedIdentities = 
         alignment.GetNamedScore(CSeq_align::eScore_IdentityCount,
                                 kExpectedIdentities);
 
         int kExpectedMismatches = 0;
+        bool has_kExpectedMismatches = 
         alignment.GetNamedScore(CSeq_align::eScore_MismatchCount,
                                 kExpectedMismatches);
 
         int kExpectedGapOpen = 0;
+        bool has_kExpectedGapOpen = 
         alignment.GetNamedScore("gapopen",
                                 kExpectedGapOpen);
 
         int kExpectedGaps = 0;
+        bool has_kExpectedGaps = 
         alignment.GetNamedScore("gap_bases",
                                 kExpectedGaps);
 
         int kExpectedLength = 0;
+        bool has_kExpectedLength = 
         alignment.GetNamedScore("length",
                                 kExpectedLength);
 
         int kExpectedScore = 0;
+        bool has_kExpectedScore = 
         alignment.GetNamedScore(CSeq_align::eScore_Score,
                                 kExpectedScore);
 
         double kExpectedPctIdentity_Gapped = 0;
+        bool has_kExpectedPctIdentity_Gapped = 
         alignment.GetNamedScore(CSeq_align::eScore_PercentIdentity_Gapped,
                                 kExpectedPctIdentity_Gapped);
 
         double kExpectedPctIdentity_Ungapped = 0;
+        bool has_kExpectedPctIdentity_Ungapped = 
         alignment.GetNamedScore(CSeq_align::eScore_PercentIdentity_Ungapped,
                                 kExpectedPctIdentity_Ungapped);
 
         double kExpectedPctIdentity_GapOpeningOnly = 0;
+        bool has_kExpectedPctIdentity_GapOpeningOnly = 
         alignment.GetNamedScore(CSeq_align::eScore_PercentIdentity_GapOpeningOnly,
                                 kExpectedPctIdentity_GapOpeningOnly);
 
         double kExpectedPctCoverage = 0;
+        bool has_kExpectedPctCoverage = 
         alignment.GetNamedScore(CSeq_align::eScore_PercentCoverage,
                                 kExpectedPctCoverage);
+
+        double kExpectedHighQualityPctCoverage = 0;
+        bool has_kExpectedHighQualityPctCoverage = 
+        alignment.GetNamedScore(CSeq_align::eScore_HighQualityPercentCoverage,
+                                kExpectedHighQualityPctCoverage);
 
         /// reset scores to avoid any taint in score generation
         alignment.ResetScore();
 
         /// check alignment length
+        if(has_kExpectedLength)
         {{
              int actual =
                  score_builder.GetAlignLength(alignment);
@@ -175,6 +207,7 @@ BOOST_AUTO_TEST_CASE(Test_Score_Builder)
 
         /// check identity count
         /// NB: not for std-segs!
+        if(has_kExpectedIdentities)
         if ( !alignment.GetSegs().IsStd() ) {
             int actual =
                 score_builder.GetIdentityCount(*scope, alignment);
@@ -184,6 +217,7 @@ BOOST_AUTO_TEST_CASE(Test_Score_Builder)
         }
 
         /// check mismatch count
+        if(has_kExpectedMismatches)
         if ( !alignment.GetSegs().IsStd() ) {
             int actual =
                 score_builder.GetMismatchCount(*scope, alignment);
@@ -194,6 +228,7 @@ BOOST_AUTO_TEST_CASE(Test_Score_Builder)
 
         /// check uninitialized / wrongly initialized variables
         /// (CXX-1594 - GetMismatchCount() adds to incoming values blindly)
+        if(has_kExpectedMismatches || has_kExpectedIdentities)
         if ( !alignment.GetSegs().IsStd() ) {
             int mismatches = 1000;
             int identities = 1000;
@@ -204,6 +239,7 @@ BOOST_AUTO_TEST_CASE(Test_Score_Builder)
         }
 
         /// check gap count (= gap openings)
+        if(has_kExpectedGapOpen)
         {{
              int actual =
                  score_builder.GetGapCount(alignment);
@@ -213,6 +249,7 @@ BOOST_AUTO_TEST_CASE(Test_Score_Builder)
          }}
 
         /// check gap base length (= sum of lengths of all gaps)
+        if(has_kExpectedGaps)
         {{
              int actual =
                  score_builder.GetGapBaseCount(alignment);
@@ -222,6 +259,7 @@ BOOST_AUTO_TEST_CASE(Test_Score_Builder)
          }}
 
         /// check percent identity (gapped)
+        if(has_kExpectedPctIdentity_Gapped)
         if ( !alignment.GetSegs().IsStd() ) {
             double actual =
                 score_builder.GetPercentIdentity(*scope, alignment,
@@ -246,6 +284,7 @@ BOOST_AUTO_TEST_CASE(Test_Score_Builder)
         }
 
         /// check percent identity (ungapped)
+        if(has_kExpectedPctIdentity_Ungapped)
         if ( !alignment.GetSegs().IsStd() ) {
             double actual =
                 score_builder.GetPercentIdentity(*scope, alignment,
@@ -270,6 +309,7 @@ BOOST_AUTO_TEST_CASE(Test_Score_Builder)
         }
 
         /// check percent identity (GapOpeningOnly-style)
+        if(has_kExpectedPctIdentity_GapOpeningOnly)
         if ( !alignment.GetSegs().IsStd() ) {
             double actual =
                 score_builder.GetPercentIdentity(*scope, alignment,
@@ -294,6 +334,7 @@ BOOST_AUTO_TEST_CASE(Test_Score_Builder)
         }
 
         /// check percent coverage
+        if(has_kExpectedPctCoverage)
         {{
              double actual =
                  score_builder.GetPercentCoverage(*scope, alignment);
@@ -315,6 +356,31 @@ BOOST_AUTO_TEST_CASE(Test_Score_Builder)
              **/
          }}
 
+        /// check high-quality percent coverage if data has it
+        if(has_kExpectedHighQualityPctCoverage)
+        {{
+             double actual;
+             score_builder.AddScore(*scope, alignment, CSeq_align::eScore_HighQualityPercentCoverage);
+             alignment.GetNamedScore(CSeq_align::eScore_HighQualityPercentCoverage, actual);
+             LOG_POST(Error << "Verifying score: pct_coverage_hiqual: "
+                      << kExpectedHighQualityPctCoverage << " == " << actual);
+
+             /// machine precision is a problem here
+             /// we verify to 12 digits of precision
+             Uint8 int_hq_pct_coverage_expected = kExpectedHighQualityPctCoverage * 1e12;
+             Uint8 int_hq_pct_coverage_actual = actual * 1e12;
+             BOOST_CHECK_EQUAL(int_hq_pct_coverage_expected,
+                               int_hq_pct_coverage_actual);
+
+             /**
+             CScore score;
+             score.SetId().SetStr("pct_coverage_hiqual");
+             score.SetValue().SetReal(actual);
+             cerr << MSerial_AsnText << score;
+             **/
+         }}
+
+        if(has_kExpectedScore)
         if (alignment.GetSegs().IsDenseg()) {
             ///
             /// our encoded dense-segs have a BLAST-style 'score'
@@ -328,4 +394,342 @@ BOOST_AUTO_TEST_CASE(Test_Score_Builder)
     }
 }
 
-
+const char *sc_TestEntries = "\
+Seq-entry ::= seq {\
+  id {\
+    general {\
+      db \"dbEST\",\
+      tag id 5929311\
+    },\
+    genbank {\
+      accession \"BE669435\",\
+      version 1\
+    },\
+    gi 10029976\
+  },\
+  descr {\
+    molinfo {\
+      biomol mRNA,\
+      tech est\
+    },\
+    title \"7e24e11.x1 NCI_CGAP_Lu24 Homo sapiens cDNA clone IMAGE:3283436 3'\
+ similar to gb:U02368 PAIRED BOX PROTEIN PAX-3 (HUMAN);.\",\
+    create-date std {\
+      year 2000,\
+      month 9,\
+      day 8\
+    },\
+    update-date std {\
+      year 2011,\
+      month 1,\
+      day 8\
+    },\
+    source {\
+      org {\
+        taxname \"Homo sapiens\",\
+        common \"human\",\
+        db {\
+          {\
+            db \"taxon\",\
+            tag id 9606\
+          }\
+        },\
+        orgname {\
+          name binomial {\
+            genus \"Homo\",\
+            species \"sapiens\"\
+          },\
+          mod {\
+            {\
+              subtype other,\
+              subname \"Organ: lung; Vector: pT7T3D-PacI; Plasmid DNA from the\
+ normalized library NCI_CGAP_Lu5 was prepared, and ss circles were made in\
+ vitro. Following HAP purification, this DNA was used as tracer in a\
+ subtractive hybridization reaction. The driver was PCR-amplified cDNAs from a\
+ pool of 5,000 clones made from the same library (cloneIDs 1414920-1417991 and\
+ 1520904-1522439). Subtraction by Bento Soares and M. Fatima Bonaldo.  \"\
+            }\
+          },\
+          lineage \"Eukaryota; Metazoa; Chordata; Craniata; Vertebrata;\
+ Euteleostomi; Mammalia; Eutheria; Euarchontoglires; Primates; Haplorrhini;\
+ Catarrhini; Hominidae; Homo\",\
+          gcode 1,\
+          mgcode 2,\
+          div \"PRI\"\
+        }\
+      },\
+      subtype {\
+        {\
+          subtype clone,\
+          name \"IMAGE:3283436\"\
+        },\
+        {\
+          subtype clone-lib,\
+          name \"LIBEST_001611 NCI_CGAP_Lu24\"\
+        },\
+        {\
+          subtype tissue-type,\
+          name \"carcinoid\"\
+        },\
+        {\
+          subtype lab-host,\
+          name \"DH10B\"\
+        }\
+      }\
+    }\
+  },\
+  inst {\
+    repr raw,\
+    mol rna,\
+    length 683,\
+    strand ss,\
+    seq-data ncbi2na 'FFC14200700A8BEB8084DFE879F774BD79ED210DE0B1FF0BB05E7470\
+5749784549CEEDBEDF847BBA827FAFA9044FB40BC0350ECDD4D4E2B4F5838E8F53B44B7099D0E0\
+4E5350B47EA27DD7AE882953DE53253E49E7468A2C9554A91D3793AAEA92291FB12BB7D1FAB4A6\
+BD3158AEEA93AE7C5BBAA4AA195BC1E48EDE7893B526EACEA27A8F4D3FE7388E5E9E532BE13858\
+3CA9D3CE1BDE852C769EFA9EA7144E3413BB8C3B7C1EA1A22C20'H\
+  },\
+  annot {\
+    {\
+      desc {\
+        name \"NCBI_GPIPE\"\
+      },\
+      data ftable {\
+        {\
+          data region \"alignable\",\
+          location int {\
+            from 0,\
+            to 426,\
+            strand unknown,\
+            id gi 10029976\
+          }\
+        }\
+      }\
+    }\
+  }\
+}\
+Seq-entry ::= seq {\
+  id {\
+    general {\
+      db \"dbEST\",\
+      tag id 5929424\
+    },\
+    genbank {\
+      accession \"BE669548\",\
+      version 1\
+    },\
+    gi 10030089\
+  },\
+  descr {\
+    molinfo {\
+      biomol mRNA,\
+      tech est\
+    },\
+    title \"7e14g09.x1 NCI_CGAP_Lu24 Homo sapiens cDNA clone IMAGE:3282496 3'\
+ similar to SW:RL2B_HUMAN P29316 60S RIBOSOMAL PROTEIN L23A. ;.\",\
+    create-date std {\
+      year 2000,\
+      month 9,\
+      day 8\
+    },\
+    update-date std {\
+      year 2011,\
+      month 1,\
+      day 8\
+    },\
+    source {\
+      org {\
+        taxname \"Homo sapiens\",\
+        common \"human\",\
+        db {\
+          {\
+            db \"taxon\",\
+            tag id 9606\
+          }\
+        },\
+        orgname {\
+          name binomial {\
+            genus \"Homo\",\
+            species \"sapiens\"\
+          },\
+          mod {\
+            {\
+              subtype other,\
+              subname \"Organ: lung; Vector: pT7T3D-PacI; Plasmid DNA from the\
+ normalized library NCI_CGAP_Lu5 was prepared, and ss circles were made in\
+ vitro. Following HAP purification, this DNA was used as tracer in a\
+ subtractive hybridization reaction. The driver was PCR-amplified cDNAs from a\
+ pool of 5,000 clones made from the same library (cloneIDs 1414920-1417991 and\
+ 1520904-1522439). Subtraction by Bento Soares and M. Fatima Bonaldo.  \"\
+            }\
+          },\
+          lineage \"Eukaryota; Metazoa; Chordata; Craniata; Vertebrata;\
+ Euteleostomi; Mammalia; Eutheria; Euarchontoglires; Primates; Haplorrhini;\
+ Catarrhini; Hominidae; Homo\",\
+          gcode 1,\
+          mgcode 2,\
+          div \"PRI\"\
+        }\
+      },\
+      subtype {\
+        {\
+          subtype clone,\
+          name \"IMAGE:3282496\"\
+        },\
+        {\
+          subtype clone-lib,\
+          name \"LIBEST_001611 NCI_CGAP_Lu24\"\
+        },\
+        {\
+          subtype tissue-type,\
+          name \"carcinoid\"\
+        },\
+        {\
+          subtype lab-host,\
+          name \"DH10B\"\
+        }\
+      }\
+    }\
+  },\
+  inst {\
+    repr raw,\
+    mol rna,\
+    length 470,\
+    strand ss,\
+    seq-data ncbi2na 'E6965809820A09D79575C09E09409829FC0A504292EF80AED4494400\
+8208D646D1517D69A58211E61D6884950335DA0899D52880427E14739CD342FD67851E2DE53810\
+8C81104111FBBD3EE8EF0250424523C04A7B8209ECE13E3BAD06D04578F7978E88820A4CEF61E9\
+D78F18E7FA3BE50403E234D0'H\
+  },\
+  annot {\
+    {\
+      desc {\
+        name \"NCBI_GPIPE\"\
+      },\
+      data ftable {\
+        {\
+          data region \"alignable\",\
+          location int {\
+            from 0,\
+            to 417,\
+            strand unknown,\
+            id gi 10030089\
+          }\
+        }\
+      }\
+    }\
+  }\
+}\
+Seq-entry ::= seq {\
+  id {\
+    general {\
+      db \"dbEST\",\
+      tag id 5929425\
+    },\
+    genbank {\
+      accession \"BE669549\",\
+      version 1\
+    },\
+    gi 10030090\
+  },\
+  descr {\
+    molinfo {\
+      biomol mRNA,\
+      tech est\
+    },\
+    title \"7e14h04.x1 NCI_CGAP_Lu24 Homo sapiens cDNA clone IMAGE:3282487 3'\
+ similar to SW:RL2B_HUMAN P29316 60S RIBOSOMAL PROTEIN L23A. ;.\",\
+    create-date std {\
+      year 2000,\
+      month 9,\
+      day 8\
+    },\
+    update-date std {\
+      year 2011,\
+      month 1,\
+      day 8\
+    },\
+    source {\
+      org {\
+        taxname \"Homo sapiens\",\
+        common \"human\",\
+        db {\
+          {\
+            db \"taxon\",\
+            tag id 9606\
+          }\
+        },\
+        orgname {\
+          name binomial {\
+            genus \"Homo\",\
+            species \"sapiens\"\
+          },\
+          mod {\
+            {\
+              subtype other,\
+              subname \"Organ: lung; Vector: pT7T3D-PacI; Plasmid DNA from the\
+ normalized library NCI_CGAP_Lu5 was prepared, and ss circles were made in\
+ vitro. Following HAP purification, this DNA was used as tracer in a\
+ subtractive hybridization reaction. The driver was PCR-amplified cDNAs from a\
+ pool of 5,000 clones made from the same library (cloneIDs 1414920-1417991 and\
+ 1520904-1522439). Subtraction by Bento Soares and M. Fatima Bonaldo.  \"\
+            }\
+          },\
+          lineage \"Eukaryota; Metazoa; Chordata; Craniata; Vertebrata;\
+ Euteleostomi; Mammalia; Eutheria; Euarchontoglires; Primates; Haplorrhini;\
+ Catarrhini; Hominidae; Homo\",\
+          gcode 1,\
+          mgcode 2,\
+          div \"PRI\"\
+        }\
+      },\
+      subtype {\
+        {\
+          subtype clone,\
+          name \"IMAGE:3282487\"\
+        },\
+        {\
+          subtype clone-lib,\
+          name \"LIBEST_001611 NCI_CGAP_Lu24\"\
+        },\
+        {\
+          subtype tissue-type,\
+          name \"carcinoid\"\
+        },\
+        {\
+          subtype lab-host,\
+          name \"DH10B\"\
+        }\
+      }\
+    }\
+  },\
+  inst {\
+    repr raw,\
+    mol rna,\
+    length 490,\
+    strand ss,\
+    seq-data ncbi2na 'D108E9960260828275E55D70278250260A7F029420A4BBE02BB51251\
+0020823591B4545F5A69608479875A212540CD768226754A20109F851CE734D0BF59E1478B794E\
+0823208410447EEF4FBA3BC094109148F0129EE0827B384F8EE942B4115E3DA5E3A22082933BD8\
+7A75E3C639FE8EF94100FA8D3701E2D490'H\
+  },\
+  annot {\
+    {\
+      desc {\
+        name \"NCBI_GPIPE\"\
+      },\
+      data ftable {\
+        {\
+          data region \"alignable\",\
+          location int {\
+            from 0,\
+            to 438,\
+            strand unknown,\
+            id gi 10030090\
+          }\
+        }\
+      }\
+    }\
+  }\
+}\
+";
