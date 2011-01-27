a123649aef43a808ad1b17fc5af41b0f1c56bafb
Michael Kornbluh <kornbluh@ncbi.nlm.nih.gov>
2011-01-27 16:32:08 +0000

    Commit style changes suggested by Aaron Ucko for compatibility with Workshop Compiler.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@48524 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/cleanup/newcleanup.cpp
+++ src/objtools/cleanup/newcleanup.cpp
@@ -3229,7 +3229,7 @@ public:
     {
         int ii = 0;
         for( ; ii < num_keys; ++ii ) {
-            insert(pair<char, const char*>( keys[ii].second, keys[ii].first ));
+            insert(value_type( keys[ii].second, keys[ii].first ));
         }
     }
 };
@@ -4338,11 +4338,13 @@ char s_ParseSeqFeatTRnaString( const string &comment, bool *out_justTrnaText, st
     return aa;
 }
 
+inline
 static
-bool s_CodonCompare( int codon1, int codon2 ) {
+bool s_CodonCompare( const int &codon1, const int &codon2 ) {
     return (codon1 < codon2);
 }
 
+inline
 static
 bool s_CodonEqual( int codon1, int codon2 ) {
     return (codon1 == codon2);
