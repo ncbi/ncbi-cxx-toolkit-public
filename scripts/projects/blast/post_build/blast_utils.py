e07fb378eee4d58902c510018b4245cf5918c6e1
Christiam Camacho <camacho@ncbi.nlm.nih.gov>
2011-02-03 23:11:20 +0000

    Documentation is now online JIRA SB-727
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@48568 78c7ea69-d796-4a43-9a09-de51944f1b03

--- scripts/projects/blast/post_build/blast_utils.py
+++ scripts/projects/blast/post_build/blast_utils.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python
 """ Various utilities/tools for BLAST """
 
-__all__ = [ "safe_exec", "update_blast_version", "MANUAL_URL" ]
+__all__ = [ "safe_exec", "update_blast_version" ]
 
 import os
 from subprocess import call
 from shutil import move
         
-# URL from the test Bookshelf server to download the User Manual
-MANUAL_URL = "http://www.ncbi.nlm.nih.gov/bookshelf/picrender.fcgi?"
-MANUAL_URL += "book=helpblast&part=CmdLineAppsManual&blobname=&blobtype=pdf"
-
 def safe_exec(cmd):
     """ Executes a command and checks its return value, throwing an
         exception if it fails.
