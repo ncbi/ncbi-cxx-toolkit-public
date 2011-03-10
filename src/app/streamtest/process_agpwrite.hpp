4b4ce419ab95a4f8a3d45303683a5425aa007820
Jonathan Kans <kans@ncbi.nlm.nih.gov>
2011-03-10 19:41:19 +0000

    initial checkin
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@48933 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ src/app/streamtest/process_agpwrite.hpp
@@ -0,0 +1,101 @@
+/*
+* ===========================================================================
+*
+*                            PUBLIC DOMAIN NOTICE
+*               National Center for Biotechnology Information
+*
+*  This software/database is a "United States Government Work" under the
+*  terms of the United States Copyright Act.  It was written as part of
+*  the author's official duties as a United States Government employee and
+*  thus cannot be copyrighted.  This software/database is freely available
+*  to the public for use. The National Library of Medicine and the U.S.
+*  Government have not placed any restriction on its use or reproduction.
+*
+*  Although all reasonable efforts have been taken to ensure the accuracy
+*  and reliability of the software and data, the NLM and the U.S.
+*  Government do not and cannot warrant the performance or results that
+*  may be obtained by using this software or data. The NLM and the U.S.
+*  Government disclaim all warranties, express or implied, including
+*  warranties of performance, merchantability or fitness for any particular
+*  purpose.
+*
+*  Please cite the author in any work or product based on this material.
+*
+* ===========================================================================
+*
+* Author:
+*
+* File Description:
+*
+* ===========================================================================
+*/
+
+#ifndef __process_agpwrite__hpp__
+#define __process_agpwrite__hpp__
+
+//  ============================================================================
+class CAgpwriteProcess
+//  ============================================================================
+    : public CScopedProcess
+{
+public:
+    //  ------------------------------------------------------------------------
+    CAgpwriteProcess()
+    //  ------------------------------------------------------------------------
+        : CScopedProcess()
+        , m_out( 0 )
+    {};
+
+    //  ------------------------------------------------------------------------
+    ~CAgpwriteProcess()
+    //  ------------------------------------------------------------------------
+    {
+    };
+
+    //  ------------------------------------------------------------------------
+    void ProcessInitialize(
+        const CArgs& args )
+    //  ------------------------------------------------------------------------
+    {
+        CScopedProcess::ProcessInitialize( args );
+
+        m_out = args["o"] ? &(args["o"].AsOutputFile()) : &cout;
+    };
+
+    //  ------------------------------------------------------------------------
+    void ProcessFinalize()
+    //  ------------------------------------------------------------------------
+    {
+    }
+
+    //  ------------------------------------------------------------------------
+    virtual void SeqEntryInitialize(
+        CRef<CSeq_entry>& se )
+    //  ------------------------------------------------------------------------
+    {
+        CScopedProcess::SeqEntryInitialize( se );
+    };
+
+    //  ------------------------------------------------------------------------
+    void SeqEntryProcess()
+    //  ------------------------------------------------------------------------
+    {
+        try {
+            VISIT_ALL_BIOSEQS_WITHIN_SEQENTRY (bit, *m_entry) {
+                const CBioseq& bioseq = *bit;
+                if (bioseq.IsNa()) {
+                    const CBioseq_Handle& bs = (*m_scope).GetBioseqHandle (bioseq);
+                    AgpWrite( *m_out, bs, "chr1");
+                }
+            }
+        }
+        catch (CException& e) {
+            LOG_POST(Error << "error processing seqentry: " << e.what());
+        }
+    };
+
+protected:
+    CNcbiOstream* m_out;
+};
+
+#endif
