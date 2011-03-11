a5598d4967fb926db72d3c65502a47a5c26c63e0
Anton Lavrentiev <lavr@ncbi.nlm.nih.gov>
2011-03-11 15:00:22 +0000

    Clean up the logfile before doing the check
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@48940 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/corelib/test/test_ncbi_os_unix.sh
+++ src/corelib/test/test_ncbi_os_unix.sh
@@ -1,11 +1,12 @@
 #! /bin/sh
 # $Id$
 
+rm -f  ./test_ncbi_os_unix.log
 $CHECK_EXEC test_ncbi_os_unix
 test $? = 0  ||  exit 1
-sleep 1
+sleep 5
 test -f ./test_ncbi_os_unix.log  ||  exit 1
-line=`cat ./test_ncbi_os_unix.log 2>/dev/null`
+line="`cat ./test_ncbi_os_unix.log 2>/dev/null`"
 echo "$line"
 test `echo "$line" | grep -s -c "COMPLETED SUCCESSFULLY"` != 1  &&  exit 1
 exit 0
