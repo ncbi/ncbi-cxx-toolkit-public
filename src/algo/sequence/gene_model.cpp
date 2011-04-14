6cdee99609e378f8e7f9bf8d3f9b0dcd5c0ed7cd
chetvern <chetvern@ncbi.nlm.nih.gov>
2011-04-14 17:52:37 +0000

    Reversed has-right/-left completeness logic (JIRA: GP-1427)
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@49335 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/algo/sequence/gene_model.cpp
+++ src/algo/sequence/gene_model.cpp
@@ -915,9 +915,9 @@ SImplementation::x_CreateProteinBioseq(CSeq_loc* cds_loc,
                ) {
         completeness = CMolInfo::eCompleteness_no_ends;
     } else if (cds_loc->IsPartialStart(eExtreme_Biological)) {
-        completeness = CMolInfo::eCompleteness_has_left;
-    } else if (cds_loc->IsPartialStop(eExtreme_Biological)) {
         completeness = CMolInfo::eCompleteness_has_right;
+    } else if (cds_loc->IsPartialStop(eExtreme_Biological)) {
+        completeness = CMolInfo::eCompleteness_has_left;
     } else {
         completeness = CMolInfo::eCompleteness_complete;
     }
