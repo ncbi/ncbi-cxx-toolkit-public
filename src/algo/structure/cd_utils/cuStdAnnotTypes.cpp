d412dfe36e365b545d81e3085eb2a1e82ae0cdaf
Paul Thiessen <thiessen@ncbi.nlm.nih.gov>
2011-05-20 14:33:38 +0000

    remove (more) debug output
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@49764 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/algo/structure/cd_utils/cuStdAnnotTypes.cpp
+++ src/algo/structure/cd_utils/cuStdAnnotTypes.cpp
@@ -91,10 +91,10 @@ bool CStdAnnotTypes::LoadTypes(const string& iniFile)
 
                 sort(values.begin(), values.end());
 
-                //cout << "\nTesting sort for " << *cit << endl;
+                /*cout << "\nTesting sort for " << *cit << endl;
                 ITERATE (vector<string>, vit, values) {
                     cout << *vit << endl;
-                }
+                }*/
 
             }
         }
