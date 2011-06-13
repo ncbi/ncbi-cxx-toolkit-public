2ec32091c64d98ac9df05636cf3c3ec8405a0f5c
Amelia Fong <fongah2@ncbi.nlm.nih.gov>
2011-06-13 19:38:09 +0000

    Revert back to previous version, JIRA
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@50066 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/app/blast/rpsblast_app.cpp
+++ src/app/blast/rpsblast_app.cpp
@@ -36,6 +36,7 @@ static char const rcsid[] =
 	"$Id$";
 #endif /* SKIP_DOXYGEN_PROCESSING */
 
+
 #include <ncbi_pch.hpp>
 #include <corelib/ncbiapp.hpp>
 #include <algo/blast/api/local_blast.hpp>
@@ -45,7 +46,6 @@ static char const rcsid[] =
 #include <algo/blast/api/objmgr_query_data.hpp>
 #include <algo/blast/format/blast_format.hpp>
 #include "blast_app_util.hpp"
-#include <algo/blast/api/rpsblast_local.hpp>
 
 #ifndef SKIP_DOXYGEN_PROCESSING
 USING_NCBI_SCOPE;
@@ -152,18 +152,15 @@ int CRPSBlastApp::Run(void)
 
             CRef<CSearchResultSet> results;
 
-            if (m_CmdLineArgs->ExecuteRemotely())
-            {
+            if (m_CmdLineArgs->ExecuteRemotely()) {
                 CRef<CRemoteBlast> rmt_blast = 
                     InitializeRemoteBlast(queries, db_args, opts_hndl,
                           m_CmdLineArgs->ProduceDebugRemoteOutput(),
                           m_CmdLineArgs->GetClientId());
                 results = rmt_blast->GetResultSet();
-            }
-            else
-            {
-            	CLocalRPSBlast  local_search (query_batch, args[kArgDb].AsString(), opts_hndl, args[kArgNumThreads].AsInteger() );
-            	results = local_search.Run();
+            } else {
+                CLocalBlast lcl_blast(queries, opts_hndl, db_adapter);
+                results = lcl_blast.Run();
             }
 
             if (fmt_args->ArchiveFormatRequested(args)) {
@@ -189,8 +186,6 @@ int CRPSBlastApp::Run(void)
 #ifndef SKIP_DOXYGEN_PROCESSING
 int main(int argc, const char* argv[] /*, const char* envp[]*/)
 {
-	int status = CRPSBlastApp().AppMain(argc, argv, 0, eDS_Default, 0);
-
-	return status;
+    return CRPSBlastApp().AppMain(argc, argv, 0, eDS_Default, 0);
 }
 #endif /* SKIP_DOXYGEN_PROCESSING */
