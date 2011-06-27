b33475537a203ed95c9d71d5268618ec507d9633
Jonathan Kans <kans@ncbi.nlm.nih.gov>
2011-06-27 16:56:54 +0000

    corrected Pterobranchia Mitochondrial genetic code (24) - authors admitted typo in manuscript
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@50208 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objects/seqfeat/Genetic_code_table.cpp
+++ src/objects/seqfeat/Genetic_code_table.cpp
@@ -716,7 +716,7 @@ const char * CGen_code_table_imp::sm_GenCodeTblMemStr [] =
     "ncbieaa  \"FF*LSSSSYY**CC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG\",\n",
     "sncbieaa \"--------------------------------M--M---------------M------------\" } };\n",
     "{ name \"Pterobranchia Mitochondrial\" , id 24 ,\n",
-    "ncbieaa  \"FFLLSSSSYY**CCWWLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSSKVVVVAAAADDDDGGGG\",\n",
+    "ncbieaa  \"FFLLSSSSYY**CCWWLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSSKVVVVAAAADDEEGGGG\",\n",
     "sncbieaa \"---M---------------M---------------M----------------------------\" } ,\n",
     0  // to indicate that there is no more data
 };
