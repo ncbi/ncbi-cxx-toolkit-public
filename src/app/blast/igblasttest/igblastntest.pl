527569b72a61d47656b9a91d3505116e7f01bbcd
Jian Ye <jianye@ncbi.nlm.nih.gov>
2011-07-20 16:12:15 +0000

    more cas, JIRA IGB-21e
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@50516 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/app/blast/igblasttest/igblastntest.pl
+++ src/app/blast/igblasttest/igblastntest.pl
@@ -7,12 +7,26 @@ use strict;
 my $new_program = "./igblastn";
 my $old_program = "/export/home/jianye/igblast/trunk/c++/src/app/blast/igblastn";
 my $testcasefile = "igblastntestcase";
-my $base_parameters = " -germline_db_V Human_gl_V -germline_db_J Human_gl_J -germline_db_D Human_gl_D -query $testcasefile -outfmt "; 
+my $base_parameters = " -germline_db_V Human_gl_V -germline_db_J Human_gl_J -germline_db_D Human_gl_D -query $testcasefile -show_translation -outfmt "; 
 my @format = ("3", "6", "7");
+
 
 system ("rm *.out");
 system ("rm *.diff");
 
+foreach my $fmt (@format){
+  my $parameters = $base_parameters." ".$fmt;
+  print ("$parameters\n");
+  print ("old\n");
+  
+  system("time $old_program $parameters > $testcasefile.$fmt.old.out");
+  print ("new\n");
+  system("time $new_program $parameters > $testcasefile.$fmt.new.out");
+  system("diff  $testcasefile.$fmt.old.out $testcasefile.$fmt.new.out > $testcasefile.$fmt.diff");
+}
+
+$testcasefile = "igblastntestcase2";
+$base_parameters = "  -germline_db_V UNSWIgVRepertoire_fasta.txt -germline_db_J UNSWIgJRepertoire_fasta.txt -germline_db_D UNSWIgDRepertoire_fasta.txt -show_translation -query $testcasefile  -outfmt  "; 
 foreach my $fmt (@format){
   my $parameters = $base_parameters." ".$fmt;
   print ("$parameters\n");
