f6651a1ede3ab9c375423ef69305c988c0808c2a
Anton Lavrentiev <lavr@ncbi.nlm.nih.gov>
2011-08-04 17:45:10 +0000

    +NcbiInitLocalIP() and bugfixes in IP range parsing
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@50742 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/connect/ncbi_priv.h
+++ src/connect/ncbi_priv.h
@@ -261,7 +261,7 @@ NCBI_C_DEFINE_ERRCODE_X(Connect_Sendmail, 309,  31);
 NCBI_C_DEFINE_ERRCODE_X(Connect_Service,  310,   8);
 NCBI_C_DEFINE_ERRCODE_X(Connect_Socket,   311, 160);
 NCBI_C_DEFINE_ERRCODE_X(Connect_Crypt,    312,   6);
-NCBI_C_DEFINE_ERRCODE_X(Connect_LocalNet, 313,  11);
+NCBI_C_DEFINE_ERRCODE_X(Connect_LocalNet, 313,   4);
 NCBI_C_DEFINE_ERRCODE_X(Connect_Mghbn,    314,  16);
 
 
