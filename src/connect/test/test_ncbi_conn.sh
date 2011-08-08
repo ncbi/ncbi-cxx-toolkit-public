d2af0bebc816b1aa3742aed95ff1c338dc6be4ac
Anton Lavrentiev <lavr@ncbi.nlm.nih.gov>
2011-08-08 14:56:23 +0000

    Client-Host w/real IP
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@50773 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/connect/test/test_ncbi_conn.sh
+++ src/connect/test/test_ncbi_conn.sh
@@ -24,7 +24,7 @@ trap 'echo "`date`."' 0 1 2 3 15
 
 if [ -r /am/ncbiapdata/test_data/proxy/test_ncbi_proxy ]; then
   . /am/ncbiapdata/test_data/proxy/test_ncbi_proxy
-  export CONN_HTTP_USER_HEADER="Client-Host: 0"
+  export CONN_HTTP_USER_HEADER="Client-Host: 1.1.1.1"
   export CONN_LB_DISABLE=1
 fi
 
