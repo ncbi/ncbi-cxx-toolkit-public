10f73c09df6015401523091e4b5b4c4e5903bf6c
Jonathan Kans <kans@ncbi.nlm.nih.gov>
2011-08-11 18:36:14 +0000

    in ValidateSeqDescContext, if fasta brackets, check to see if it is due to organism name
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@50838 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/validator/validerror_bioseq.cpp
+++ src/objtools/validator/validerror_bioseq.cpp
@@ -6682,6 +6682,21 @@ void CValidError_bioseq::ValidateSeqDescContext(const CBioseq& seq)
                               }
                           }
                           if (report_fasta_brackets) {
+                              IF_EXISTS_CLOSEST_BIOSOURCE (bs_ref, seq, NULL) {
+                                  const CBioSource& bsrc = (*bs_ref).GetSource();
+                                  if (! bsrc.IsSetOrg()) continue;
+                                  const COrg_ref& orgref = bsrc.GetOrg();
+                                  if (! orgref.IsSetTaxname()) continue;
+                                  string taxname = orgref.GetTaxname();
+                                  size_t pos = NStr::Find (taxname, "=");
+                                  if (pos == string::npos) continue;
+                                  pos = NStr::Find (title, taxname);
+                                  if (pos == string::npos) continue;
+                                  report_fasta_brackets = false;
+                                  break;
+                              }
+                          }
+                          if (report_fasta_brackets) {
                               PostErr(eDiag_Warning, eErr_SEQ_DESCR_FastaBracketTitle, 
                                       "Title may have unparsed [...=...] construct",
                                       ctx, desc);
