bbc3711d77d3e203b129a229f6f8789b0d5ffb14
Christiam Camacho <camacho@ncbi.nlm.nih.gov>
2011-08-30 20:18:28 +0000

    Some refactorings JIRA SB-857
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@51068 78c7ea69-d796-4a43-9a09-de51944f1b03

--- scripts/projects/blast/post_build/blast_utils.py
+++ scripts/projects/blast/post_build/blast_utils.py
@@ -6,6 +6,7 @@ __all__ = [ "safe_exec", "update_blast_version" ]
 import os
 from subprocess import call
 from shutil import move
+from platform import platform
         
 def safe_exec(cmd):
     """ Executes a command and checks its return value, throwing an
@@ -42,3 +43,54 @@ def update_blast_version(config_file, blast_version):
         out.close()
         infile.close()
         os.unlink(temp_fname)
+
+def create_new_tarball_name(platform, version):
+    """ Converts the name of a platform as specified to the prepare_release 
+    framework to an archive name according to BLAST release naming conventions.
+    
+    Note: the platform names come from the prepare_release script conventions,
+    more information can be found in http://mini.ncbi.nih.gov/3oo
+    """
+    
+    retval = "ncbi-blast-" + version + "+"
+    if platform.startswith("Win32"):
+        retval += "-ia32-win32"
+    elif platform.startswith("Win64"):
+        retval += "-x64-win64"
+    elif platform.startswith("Linux32"):
+        retval += "-ia32-linux"
+    elif platform.startswith("Linux64"):
+        retval += "-x64-linux"
+    elif platform == "IntelMAC":
+        retval += "-universal-macosx"
+    elif platform == "SunOSSparc":
+        retval += "-sparc64-solaris"
+    elif platform == "SunOSx86":
+        retval += "-x64-solaris"
+    else:
+        raise RuntimeError("Unknown platform: " + platform)
+    return retval
+
+def determine_platform():
+    """ Determines the platform (as defined in prepare_release) for the current
+    hostname 
+    """
+
+    p = platform().lower()
+    if p.find("linux") != -1:
+        if p.find("x86_64") != -1:
+            return "Linux64"
+        else:
+            return "Linux32"
+    elif p.find("sunos") != -1:
+        if p.find("sparc") != -1:
+            return "SunOSSparc"
+        else:
+            return "SunOSx86"
+    elif p.find("microsoft") != -1:
+        if p.find("32") != -1:
+            return "Win32"
+        else:
+            return "Win64"
+    else:
+        raise RuntimeError("Unknown platform: " + p)
