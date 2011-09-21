4409ffa308f68f28abb8f83e034ac3e6f2eb6f25
Christiam Camacho <camacho@ncbi.nlm.nih.gov>
2011-09-21 14:39:33 +0000

    Bump BLAST version to 2.2.26 JIRA SB-857
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@51307 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/algo/blast/core/blast_engine.c
+++ src/algo/blast/core/blast_engine.c
@@ -84,8 +84,8 @@ static char const rcsid[] =
 
 NCBI_XBLAST_EXPORT const int   kBlastMajorVersion = 2;
 NCBI_XBLAST_EXPORT const int   kBlastMinorVersion = 2;
-NCBI_XBLAST_EXPORT const int   kBlastPatchVersion = 25;
-NCBI_XBLAST_EXPORT const char* kBlastReleaseDate = "Feb-01-2011";
+NCBI_XBLAST_EXPORT const int   kBlastPatchVersion = 26;
+NCBI_XBLAST_EXPORT const char* kBlastReleaseDate = "Sep-21-2011";
 
 /** Structure to be passed to s_BlastSearchEngineCore, containing pointers 
     to various preallocated structures and arrays. */
