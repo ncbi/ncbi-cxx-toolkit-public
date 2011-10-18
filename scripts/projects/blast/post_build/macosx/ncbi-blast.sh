383b82c9f09e1b0b19b74b92a4f1c910587e3d03
Christiam Camacho <camacho@ncbi.nlm.nih.gov>
2011-10-18 14:01:36 +0000

    Add seedtop and makeprofiledb
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@51612 78c7ea69-d796-4a43-9a09-de51944f1b03

--- scripts/projects/blast/post_build/macosx/ncbi-blast.sh
+++ scripts/projects/blast/post_build/macosx/ncbi-blast.sh
@@ -10,9 +10,9 @@ if [ $# -ne 3 ] ; then
     exit 1;
 fi
 
-BLAST_BINS="blastn blastp blastx tblastn tblastx psiblast rpsblast rpstblastn blast_formatter legacy_blast.pl update_blastdb.pl"
+BLAST_BINS="blastn blastp blastx tblastn tblastx psiblast rpsblast rpstblastn blast_formatter legacy_blast.pl update_blastdb.pl seedtop"
 MASKING_BINS="windowmasker dustmasker segmasker"
-DB_BINS="blastdbcmd makeblastdb makembindex blastdb_aliastool convert2blastmask blastdbcheck"
+DB_BINS="blastdbcmd makeblastdb makeprofiledb makembindex blastdb_aliastool convert2blastmask blastdbcheck"
 ALL_BINS="$BLAST_BINS $MASKING_BINS $DB_BINS"
 
 rm -rf $PRODUCT.dmg $PRODUCT _stage $INSTALLDIR/installer
