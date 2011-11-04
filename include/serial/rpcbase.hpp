c008b192ca23c89a8fb30abf3b69f8f3c5af1fe2
Anton Lavrentiev <lavr@ncbi.nlm.nih.gov>
2011-11-04 16:06:28 +0000

    Reinstate (for a year) ncbi_sid cookie mock-up (using new API) JIRA: CXX-2730
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@51838 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/serial/rpcbase.hpp
+++ include/serial/rpcbase.hpp
@@ -33,9 +33,10 @@
  *
  */
 
-#include <connect/ncbi_conn_stream.hpp>
-#include <corelib/ncbi_system.hpp>
 #include <corelib/ncbimtx.hpp>
+#include <corelib/ncbi_system.hpp>
+#include <connect/ncbi_conn_stream.hpp>
+#include <connect/ncbi_util.h>
 #include <serial/objistr.hpp>
 #include <serial/objostr.hpp>
 #include <serial/serial.hpp>
@@ -64,7 +65,14 @@ public:
                unsigned int      retry_limit = 3)
         : m_Service(service), m_Format(format), m_Timeout(kDefaultTimeout),
           m_RetryLimit(retry_limit)
-        { }
+    {
+#if NCBI_DEVELOPMENT_VER < 20121104
+        const char* sid = CORE_GetNcbiSid();
+        if (sid  &&  *sid) {
+            m_SessionID.assign(sid, strlen(sid));
+        }
+#endif
+    }
     virtual ~CRPCClient(void);
 
     virtual void Ask(const TRequest& request, TReply& reply);
@@ -121,6 +129,7 @@ private:
     auto_ptr<CObjectOStream> m_Out;
     string                   m_Service; ///< Used by default Connect().
     string                   m_Affinity;
+    string                   m_SessionID;
     ESerialDataFormat        m_Format;
     CMutex                   m_Mutex;   ///< To allow sharing across threads.
     const STimeout*          m_Timeout; ///< Cloned if not special.
@@ -293,6 +302,10 @@ void CRPCClient<TRequest, TReply>::x_Connect(void)
 {
     _ASSERT( !m_Service.empty() );
     SConnNetInfo* net_info = ConnNetInfo_Create(m_Service.c_str());
+    if (!m_SessionID.empty()) {
+        string header = "Cookie: ncbi_sid=" + m_SessionID;
+        ConnNetInfo_AppendUserHeader(net_info, header.c_str());
+    }
     if (!m_Affinity.empty()) {
         ConnNetInfo_PostOverrideArg(net_info, m_Affinity.c_str(), 0);
     }
