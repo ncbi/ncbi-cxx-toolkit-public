379cb727a42058665650f7e5ad9f600d89f96b2e
Anton Lavrentiev <lavr@ncbi.nlm.nih.gov>
2011-11-16 22:20:48 +0000

    Load proxy settings on CYGWIN and do not use test.gnutls.org (which seems to be down permanently)
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@51990 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/connect/test/test_ncbi_http_get.sh
+++ src/connect/test/test_ncbi_http_get.sh
@@ -4,8 +4,11 @@
 CONN_DEBUG_PRINTOUT=SOME;  export CONN_DEBUG_PRINTOUT
 
 ext="`expr $$ '%' 3`"
-if [ -r /am/ncbiapdata/test_data/proxy/test_ncbi_proxy.$ext ]; then
-  . /am/ncbiapdata/test_data/proxy/test_ncbi_proxy.$ext
+if   [ -r /am/ncbiapdata/test_data/proxy/test_ncbi_proxy.$ext ]; then
+  .       /am/ncbiapdata/test_data/proxy/test_ncbi_proxy.$ext
+  proxy=1
+elif [ -r /cygdrive/s/test_data/proxy/test_ncbi_proxy.$ext ]; then
+  .       /cygdrive/s/test_data/proxy/test_ncbi_proxy.$ext
   proxy=1
 fi
 
@@ -15,12 +18,10 @@ if [ "$ssl" = "1" -a "`echo $FEATURES | grep -vic '[-]GNUTLS'`" = "1" ]; then
   CONN_USESSL=1
   CONN_GNUTLS_LOGLEVEL=7
   export CONN_USESSL CONN_GNUTLS_LOGLEVEL
-  if [ "${proxy:-0}" = "1" -o "`expr '(' $$ / 100 ')' '%' 2`" = "0" ]; then
-    url='https://www.ncbi.nlm.nih.gov'
-  elif [ "`netstat -a -n | grep -w 5556 | grep -c ':5556'`" != "0" ]; then
+  if [ "${proxy:-0}" != "1" -a "`netstat -a -n | grep -w 5556 | grep -c ':5556'`" != "0" ]; then
     url='https://localhost:5556'
   else
-    url='https://test.gnutls.org:5556'
+    url='https://www.ncbi.nlm.nih.gov'
   fi
 else
   url='http://www.ncbi.nlm.nih.gov/entrez/viewer.cgi?view=0&maxplex=1&save=idf&val=4959943'
