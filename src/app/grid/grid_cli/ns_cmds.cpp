98395df69efed048daf2f49f63db73229c5aac17
kazimird <kazimird@ncbi.nlm.nih.gov>
2011-11-18 17:10:25 +0000

    Removed a reference to the deleted CNetScheduleException code eNoJobsWithAffinity from grid_cli. JIRA: CXX-2807
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@52015 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/app/grid/grid_cli/ns_cmds.cpp
+++ src/app/grid/grid_cli/ns_cmds.cpp
@@ -734,25 +734,19 @@ int CGridCommandLineInterfaceApp::Cmd_RequestJob()
 
     CNetScheduleJob job;
 
-    try {
-        if (!IsOptionSet(eWaitTimeout)) {
-            if (m_NetScheduleExecutor.GetJob(job, m_Opts.affinity))
-                return PrintJobAttrsAndDumpInput(job);
-        } else {
-            if (!IsOptionSet(eWNodePort)) {
-                fprintf(stderr, PROGRAM_NAME " " REQUESTJOB_COMMAND
-                    ": option '--" WAIT_TIMEOUT_OPTION "' requires '--"
-                    WNODE_PORT_OPTION "' to be specified as well\n");
-                return 2;
-            }
-            if (m_NetScheduleExecutor.WaitJob(job,
-                    m_Opts.timeout, m_Opts.affinity))
-                return PrintJobAttrsAndDumpInput(job);
+    if (!IsOptionSet(eWaitTimeout)) {
+        if (m_NetScheduleExecutor.GetJob(job, m_Opts.affinity))
+            return PrintJobAttrsAndDumpInput(job);
+    } else {
+        if (!IsOptionSet(eWNodePort)) {
+            fprintf(stderr, PROGRAM_NAME " " REQUESTJOB_COMMAND
+                ": option '--" WAIT_TIMEOUT_OPTION "' requires '--"
+                WNODE_PORT_OPTION "' to be specified as well\n");
+            return 2;
         }
-    }
-    catch (CNetScheduleException& e) {
-        if (e.GetErrCode() != CNetScheduleException::eNoJobsWithAffinity)
-            throw;
+        if (m_NetScheduleExecutor.WaitJob(job,
+                m_Opts.timeout, m_Opts.affinity))
+            return PrintJobAttrsAndDumpInput(job);
     }
 
     return 0;
