a3991ffacaab158c98644cb3965fbc350f5a488a
Yan Raytselis <raytseli@ncbi.nlm.nih.gov>
2012-02-13 13:03:58 +0000

    find alignment length using CSeq_align::GetAlignLength() instead of CAlignFormatUtil::GetAlignmentLength() in CAlignFormatUtil::GetSeqAlignSetCalcParams(). .
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@52978 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/align_format/align_format_util.cpp
+++ src/objtools/align_format/align_format_util.cpp
@@ -3172,7 +3172,7 @@ CAlignFormatUtil::GetSeqAlignSetCalcParams(const CSeq_align_set& aln,int queryLe
     seqSetInfo->percent_coverage = 100*seqSetInfo->master_covered_length/queryLength;
 
     ITERATE(CSeq_align_set::Tdata, iter, aln.Get()) {
-        int align_length = CAlignFormatUtil::GetAlignmentLength(**iter, do_translation);
+        int align_length = (*iter)->GetAlignLength();
                                                         
         CAlignFormatUtil::GetAlnScores(**iter, score, bits, evalue, sum_n, 
                                    num_ident, use_this_gi);  
