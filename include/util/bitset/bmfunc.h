8a4da0627d19ae45d56c73ba95367fe9c0dcaa2c
Aaron Ucko <ucko@ncbi.nlm.nih.gov>
2012-02-28 16:35:31 +0000

    bmfunc.h: prototype bit_block_{any,calc_count}_range so that the LLVM Clang compiler knows they're available.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@53162 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/util/bitset/bmfunc.h
+++ include/util/bitset/bmfunc.h
@@ -39,6 +39,13 @@ For more information please visit:  http://bmagic.sourceforge.net
 namespace bm
 {
 
+bm::id_t bit_block_any_range(const bm::word_t* block,
+                             bm::word_t left,
+                             bm::word_t right);
+
+bm::id_t bit_block_calc_count_range(const bm::word_t* block,
+                                    bm::word_t left,
+                                    bm::word_t right);
 
 /*!
     @brief Structure with statistical information about bitset's memory 
