e2e6f66b8e9628e46406253914ad52af6831b1aa
Aaron Ucko <ucko@ncbi.nlm.nih.gov>
2012-02-28 16:42:41 +0000

    alnmap.hpp: When building with the LLVM Clang compiler, let CAlnChunkVec consider CAlnMap as a whole to be a friend to work around errors observed with Clang 3.0 when only authorizing specific methods, some of which have default parameters.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@53163 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/objtools/alnmgr/alnmap.hpp
+++ include/objtools/alnmgr/alnmap.hpp
@@ -258,7 +258,7 @@ public:
         TNumchunk size(void) const { return TNumchunk(m_StartSegs.size()); };
 
     private:
-#if defined(NCBI_COMPILER_MSVC) // kludge
+#if defined(NCBI_COMPILER_MSVC) || defined(__clang__) // kludge
         friend class CAlnMap;
 #elif defined(NCBI_COMPILER_WORKSHOP)  &&  NCBI_COMPILER_VERSION >= 550
         friend class CAlnMap;        
