e66567a82e86c4c9ea5c8543a81180d993ad2aa4
Vladimir Ivanov <ivanov@ncbi.nlm.nih.gov>
2012-03-13 16:40:55 +0000

    Added simplified log generation for parallel project builds support. JIRA:CXX-3085
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@53394 78c7ea69-d796-4a43-9a09-de51944f1b03

--- compilers/msvc1000_prj/build.sh
+++ compilers/msvc1000_prj/build.sh
@@ -5,14 +5,14 @@
 # Build C++ Toolkit.
 
 
-########### Arguments
+---------------- Arguments ----------------
 
 script="$0"
 cfgs="${1:-DebugDLL ReleaseDLL}"
 arch="$2"
 
 
-########### Global variables
+---------------- Global variables ----------------
 
 build_trees='static dll'
 ###sol_static="ncbi_cpp.sln"
@@ -21,8 +21,12 @@ sol_static="ncbi_cpp.sln gui/ncbi_gui.sln"
 sol_dll="ncbi_cpp_dll.sln gui/ncbi_gui_dll.sln"
 timer="date +'%H:%M'"
 
+# TRUE if parallell project build system is enabled in Visual Studio
+is_ppb=false
+need_ppb_check=true
 
-########## Functions
+
+-------------- Functions --------------
 
 error()
 {
@@ -30,9 +34,14 @@ error()
     exit 1
 }
 
-generate_msvc10_error_check_file() {
+generate_msvc10_error_check_file()
+{
     cat <<-EOF >$1
 	/.*--* (Reb|B)uild( All | )started: Project:/ {
+	  expendable = ""
+	}
+
+	/^--* Project:/ {
 	  expendable = ""
 	}
 
@@ -49,8 +58,52 @@ generate_msvc10_error_check_file() {
 	EOF
 }
 
+generate_simple_log()
+{
+    log=$1
+    tree=$2
+    sol=$3
+    cfg=$4
 
-########## Main
+    # Get solution directory
+    sol_dir=`echo $sol | sed 's%/[^/]*$%%'`
+    if [ $sol_dir = $sol ] ; then
+        sol_dir=''
+    fi 
+
+    # Get built projects
+    projects=`grep '.*--* Build started:' $log | awk '{ sub(/^.* started:/, ""); gsub(/ /,"#"); print $0}'`
+
+    for p in $projects ; do
+        echo "------$p" | awk '{gsub(/[#]/," "); print}'
+        echo
+        prj_name=`echo $p | awk '{gsub(/[#,]/," "); print $2}'`
+###        cfg=`echo $p | awk '{gsub(/[#,]/," "); print $4}'`
+
+        # Get path for specified project name from solution
+        s=`grep \"$prj_name\" $tree/build/$sol | awk '{gsub(/,/," "); print $4}' | sed -e 's%"%%g' -e 's%\\\%/%g' -e 's%.vcxproj%%'`
+
+        target_dir=`echo $s | sed 's%/[^/]*$%%'`
+        test $target_dir = $s  &&  target_dir=''
+        target_name=`echo $s | sed 's%^.*/%%'`
+
+        # Path to regular logfile for current project
+        prj_log="$tree/build/$target_dir/$cfg/$prj_name/$target_name.log"
+
+        # Add it to new combined log
+        if test ! -f "$prj_log" ; then
+            echo "BUILD_SYSTEM_ERROR: Cannot find log file for this project: $prj_log"
+            echo
+            continue
+        fi
+        # Remove 3 first bytes from logfile (EF BB BF)
+        cat $prj_log | tr -d '\357\273\277'
+        echo
+    done
+}
+
+
+---------------- Main ----------------
 
 # Get build dir
 build_dir=`dirname $script`
@@ -98,25 +151,38 @@ for tree in $build_trees ; do
         fi
         alias=`echo $sol | sed -e 's|\\\\.*$||g' -e 's|_.*$||g'`
         start=`eval $timer`
+        echo
         echo Start time: $start
         echo "INFO: Configure \"$tree\\$alias\""
         echo "Command line: " $build_dir/build_exec.bat "$tree\\build\\$sol" build "$arch" "$cfg_configure" "-CONFIGURE-" $out
         $build_dir/build_exec.bat "$tree\\build\\$sol" build "$arch" "$cfg_configure" "-CONFIGURE-" $out >/dev/null
+        echo
         status=$?
         # Wait a bit to allow compiler to exit and flush logfile
         sleep 20
-        cat $out
-        cat $out >> ${log_dir}/${tree}_${cfg_configure}.log
+        if $need_ppb_check; then
+            need_ppb_check=false
+            grep '^1>------ Build started:' $out >/dev/null 2>&1  &&  is_ppb=true
+        fi
+        if $is_ppb; then
+            generate_simple_log $out $tree "$sol" $cfg_configure > $out.simple
+            cat $out.simple
+###            cat $out.simple >> ${log_dir}/${tree}_${cfg_configure}.log
+        else 
+            cat $out
+            cat $out >> ${log_dir}/${tree}_${cfg_configure}.log
+        fi 
         echo "Build time: $start - `eval $timer`"
         if [ $status -ne 0 ] ; then
             echo "FAILED: Configure $tree\\build\\$sol, $cfg_configure"
         fi
-        rm -f $out >/dev/null 2>&1
+        rm -f $out $out.simple >/dev/null 2>&1
         if [ $status -ne 0 ] ; then
             exit 3
         fi
     done
 done
+
 
 
 # Generate errors check script
@@ -140,22 +206,29 @@ for tree in $build_trees ; do
             fi
             alias=`echo $sol | sed -e 's|\\\\.*$||g' -e 's|_.*$||g'`
             start=`eval $timer`
+            echo
             echo Start time: $start
             echo "$tree,$sol,$cfg" >> $build_dir/cfgs.log
             echo "INFO: Building \"$tree\\$cfg\\$alias\""
             echo "Command line: " $build_dir/build_exec.bat "$tree\\build\\$sol" build "$arch" "$cfg" "-BUILD-ALL-" $out
+            echo
             $build_dir/build_exec.bat "$tree\\build\\$sol" build "$arch" "$cfg" "-BUILD-ALL-" $out >/dev/null
             status=$?
             # Wait a bit to allow compiler to exit and flush logfile
             sleep 20
-            cat $out
-            cat $out >> ${log_dir}/${tree}_${cfg}.log
+            if $is_ppb; then
+                generate_simple_log $out $tree "$sol" $cfg > $out.simple
+                cat $out.simple
+###                cat $out.simple >> ${log_dir}/${tree}_${cfg}.log
+            else 
+                cat $out
+                cat $out >> ${log_dir}/${tree}_${cfg}.log
+            fi 
             echo "Build time: $start - `eval $timer`"
             if [ $status -ne 0 ] ; then
                 # Check on errors (skip expendable projects)
                 failed="1"
-                grep '^==* Build: .* succeeded, .* failed' $out >/dev/null 2>&1  && \
-                    awk -f $check_awk $out >$out.res 2>/dev/null  &&  test ! -s $out.res  &&  failed="0"
+                awk -f $check_awk $out >$out.res 2>/dev/null  &&  test ! -s $out.res  &&  failed="0"
                 if [ "$failed" = "1" ]; then
                     echo "FAILED: Build $tree\\build\\$sol, $cfg"
                     echo "FAILED: Build $tree\\build\\$sol, $cfg" > failed.build.log
@@ -163,12 +236,12 @@ for tree in $build_trees ; do
                     cat $out >> failed.build.log
                     cat $tree/build/${sol}_watchers.txt > failed.watchers.log
                 fi
-                rm -f $out $out.res >/dev/null 2>&1
+                rm -f $out $out.simple $out.res >/dev/null 2>&1
                 if [ "$failed" = "1" ]; then
                     exit 4
                 fi
             fi
-            rm -f $out >/dev/null 2>&1
+            rm -f $out $out.simple >/dev/null 2>&1
         done
     done
 done
