eb5bfa7bb092af58d7890866e6bed765cfdd6a45
Anton Lavrentiev <lavr@ncbi.nlm.nih.gov>
2012-05-16 16:52:47 +0000

    CONN-enabled tar
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@54457 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ src/connect/test/Makefile.test_conn_tar.app
@@ -0,0 +1,14 @@
+# $Id$
+
+APP = test_conn_tar
+SRC = test_conn_tar
+CPPFLAGS = $(CMPRS_INCLUDE) $(ORIG_CPPFLAGS)
+LIB = $(COMPRESS_LIBS) connssl xconnect xutil xncbi $(NCBIATOMIC_LIB)
+
+LIBS = $(CMPRS_LIBS) $(GNUTLS_LIBS) $(NETWORK_LIBS) $(ORIG_LIBS)
+#LINK = purify $(ORIG_LINK)
+
+CHECK_CMD = test_conn_tar.sh
+CHECK_COPY = test_conn_tar.sh ../../util/test/test_tar.sh ../../check/ncbi_test_data
+
+WATCHERS = lavr
---
+++ src/connect/test/test_conn_tar.sh
@@ -0,0 +1,31 @@
+#! /bin/sh
+
+CONN_DEBUG_PRINTOUT=SOME;  export CONN_DEBUG_PRINTOUT
+
+. ncbi_test_data
+
+n="`ls -m $NCBI_TEST_DATA/proxy 2>/dev/null | wc -w`"
+n="`expr ${n:-0} + 1`"
+n="`expr $$ '%' $n`"
+
+if [ -r $NCBI_TEST_DATA/proxy/test_ncbi_proxy.$n ]; then
+  .     $NCBI_TEST_DATA/proxy/test_ncbi_proxy.$n
+fi
+
+case "`expr '(' $$ / 10 ')' '%' 2`" in
+  0)
+  ssl="`expr '(' $$ / 100 ')' '%' 2`"
+  if [ "$ssl" = "1" -a "`echo $FEATURES | grep -vic '[-]GNUTLS'`" = "1" ]; then
+    CONN_GNUTLS_LOGLEVEL=7;  export CONN_GNUTLS_LOGLEVEL
+    url='https://www.ncbi.nlm.nih.gov'
+  else
+    url='http://www.ncbi.nlm.nih.gov'
+  fi
+  huge_tar="$url"/"geosuppl/?acc=GSE1580"
+  ;;
+  1)
+  huge_tar="ftp://ftp.ncbi.nlm.nih.gov/pub/geo/DATA/supplementary/series/GSE1580/GSE1580_RAW.tar"
+  ;;
+esac
+
+. ./test_tar.sh
