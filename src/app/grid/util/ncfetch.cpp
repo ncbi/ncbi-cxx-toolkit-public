72f0afa61f43cc3e2b83250d701e7a71858c8116
kazimird <kazimird@ncbi.nlm.nih.gov>
2012-06-14 22:40:37 +0000

    ncfetch.cgi: use proper 40X HTTP status codes for client-side errors. JIRA: CXX-3203
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@54842 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/app/grid/util/ncfetch.cpp
+++ src/app/grid/util/ncfetch.cpp
@@ -34,10 +34,14 @@
  */
 
 #include <ncbi_pch.hpp>
+
 #include <cgi/cgiapp.hpp>
 #include <cgi/cgictx.hpp>
+#include <cgi/cgi_exception.hpp>
+
 #include <connect/services/netcache_api.hpp>
 #include <connect/services/grid_app_version_info.hpp>
+
 #include <corelib/reader_writer.hpp>
 #include <corelib/rwstream.hpp>
 #include <corelib/ncbiargs.hpp>
@@ -53,6 +57,7 @@ class CNetCacheBlobFetchApp : public CCgiApplication
 protected:
     virtual void Init();
     virtual int ProcessRequest(CCgiContext& ctx);
+    virtual int OnException(std::exception& e, CNcbiOstream& os);
 
 private:
     enum {
@@ -120,11 +125,12 @@ int CNetCacheBlobFetchApp::ProcessRequest(CCgiContext& ctx)
 
     reply.SetContentType(fmt);
 
-    CNetCacheAPI cli("ncfetch");
+    CNetCacheAPI nc_api(GRID_APP_NAME);
+
     size_t blob_size = 0;
     auto_ptr<IReader> reader;
     if (m_PasswordSource == eNoPassword)
-        reader.reset(cli.GetReader(key, &blob_size,
+        reader.reset(nc_api.GetReader(key, &blob_size,
             CNetCacheAPI::eCaching_Disable));
     else {
         string password;
@@ -133,23 +139,26 @@ int CNetCacheBlobFetchApp::ProcessRequest(CCgiContext& ctx)
                 request.GetCookies().Find(m_PasswordSourceName, "", "");
             if (!cookie) {
                 ERR_POST("Cookie '" << m_PasswordSourceName << "' not found");
-                return 2;
+                NCBI_CGI_THROW_WITH_STATUS(CCgiException, eUnknown,
+                        "Password required.", CCgiException::e403_Forbidden);
             }
             password = cookie->GetValue();
         } else {
             bool password_found = false;
             password = request.GetEntry(m_PasswordSourceName, &password_found);
             if (!password_found) {
-                ERR_POST("CGI entry '" << m_PasswordSourceName << "' not found");
-                return 2;
+                ERR_POST("CGI parameter '" <<
+                        m_PasswordSourceName << "' not found");
+                NCBI_CGI_THROW_WITH_STATUS(CCgiException, eUnknown,
+                        "Password required.", CCgiException::e403_Forbidden);
             }
         }
-        reader.reset(CNetCachePasswordGuard(cli, password)->GetReader(key,
+        reader.reset(CNetCachePasswordGuard(nc_api, password)->GetReader(key,
             &blob_size, CNetCacheAPI::eCaching_Disable));
     }
     if (!reader.get()) {
         ERR_POST("Could not retrieve blob " << key);
-        return 3;
+        NCBI_THROW(CNetCacheException, eBlobNotFound, "Blob not found.");
     }
 
     reply.WriteHeader();
@@ -158,6 +167,64 @@ int CNetCacheBlobFetchApp::ProcessRequest(CCgiContext& ctx)
 
     CRStream in_stream(reader.get());
     NcbiStreamCopy(reply.out(), in_stream);
+
+    return 0;
+}
+
+int CNetCacheBlobFetchApp::OnException(std::exception& e, CNcbiOstream& os)
+{
+    union {
+        CArgException* arg_exception;
+        CNetCacheException* nc_exception;
+    };
+
+    string status_str;
+    string message;
+
+    if ((arg_exception = dynamic_cast<CArgException*>(&e)) != NULL) {
+        status_str = "400 Bad Request";
+        message = arg_exception->GetMsg();
+        SetHTTPStatus(CRequestStatus::e400_BadRequest);
+    } else if ((nc_exception = dynamic_cast<CNetCacheException*>(&e)) != NULL) {
+        switch (nc_exception->GetErrCode()) {
+        case CNetCacheException::eAccessDenied:
+            status_str = "403 Forbidden";
+            message = nc_exception->GetMsg();
+            SetHTTPStatus(CRequestStatus::e403_Forbidden);
+            break;
+        case CNetCacheException::eBlobNotFound:
+            status_str = "404 Not Found";
+            message = nc_exception->GetMsg();
+            SetHTTPStatus(CRequestStatus::e404_NotFound);
+            break;
+        default:
+            return CCgiApplication::OnException(e, os);
+        }
+    } else
+        return CCgiApplication::OnException(e, os);
+
+    // Don't try to write to a broken output
+    if (!os.good()) {
+        return -1;
+    }
+
+    try {
+        // HTTP header
+        os << "Status: " << status_str << HTTP_EOL <<
+                "Content-Type: text/plain" HTTP_EOL HTTP_EOL <<
+                "ERROR:  " << status_str << " " HTTP_EOL HTTP_EOL <<
+                message << HTTP_EOL;
+
+        // Check for problems in sending the response
+        if (!os.good()) {
+            ERR_POST("CNetCacheBlobFetchApp::OnException() "
+                    "failed to send error page back to the client");
+            return -1;
+        }
+    }
+    catch (exception& e) {
+        NCBI_REPORT_EXCEPTION("(CGI) CNetCacheBlobFetchApp::OnException", e);
+    }
 
     return 0;
 }
