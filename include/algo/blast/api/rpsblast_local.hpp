946892109c7ff81cf4ad0db1d4ad2f16e6025622
Christiam Camacho <camacho@ncbi.nlm.nih.gov>
2012-07-16 18:56:11 +0000

    Remove unnecessary reference type in data member. JIRA SB-1016
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@55098 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/algo/blast/api/rpsblast_local.hpp
+++ include/algo/blast/api/rpsblast_local.hpp
@@ -1,4 +1,32 @@
+/* $Id$
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE                          
+ *               National Center for Biotechnology Information
+ *                                                                          
+ *  This software/database is a "United States Government Work" under the   
+ *  terms of the United States Copyright Act.  It was written as part of    
+ *  the author's official duties as a United States Government employee and 
+ *  thus cannot be copyrighted.  This software/database is freely available 
+ *  to the public for use. The National Library of Medicine and the U.S.    
+ *  Government have not placed any restriction on its use or reproduction.  
+ *                                                                          
+ *  Although all reasonable efforts have been taken to ensure the accuracy  
+ *  and reliability of the software and data, the NLM and the U.S.          
+ *  Government do not and cannot warrant the performance or results that    
+ *  may be obtained by using this software or data. The NLM and the U.S.    
+ *  Government disclaim all warranties, express or implied, including       
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.                                                                
+ *                                                                          
+ *  Please cite the author in any work or product based on this material.   
+ *
+ * ===========================================================================
+ */
 
+/** @file rpsblast_local.hpp
+ * Declares the CLocalRPSBlast class
+ */
 #ifndef ALGO_BLAST_API___RPSBLAST_LOCAL_HPP
 #define ALGO_BLAST_API___RPSBLAST_LOCAL_HPP
 
@@ -10,7 +38,7 @@ BEGIN_NCBI_SCOPE
 BEGIN_SCOPE(blast)
 
 
-class NCBI_XBLAST_EXPORT CLocalRPSBlast :public CObject
+class NCBI_XBLAST_EXPORT CLocalRPSBlast : public CObject
 {
 
 private:
@@ -22,7 +50,7 @@ private:
 
 
     unsigned int					m_num_of_threads;
-    const string & 					m_db_name;
+    const string  					m_db_name;
     CRef<CBlastOptionsHandle>  		m_opt_handle;
     CRef<CBlastQueryVector>	 		m_query_vector;
     unsigned int					m_num_of_dbs;
