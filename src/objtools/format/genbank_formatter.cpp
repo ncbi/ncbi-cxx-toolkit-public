b67569e581f724be11b9047026b3c1b4d0d17f2b
Michael Kornbluh <kornbluh@ncbi.nlm.nih.gov>
2012-10-02 14:57:58 +0000

    fix extra line in COMMENT. JIRA SQD-747
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@55870 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/format/genbank_formatter.cpp
+++ src/objtools/format/genbank_formatter.cpp
@@ -1125,6 +1125,11 @@ void CGenbankFormatter::FormatComment
 
     list<string> l;
     NON_CONST_ITERATE( list<string>, comment_it, strComment ) {
+        if( NStr::TruncateSpaces(*comment_it).empty() ) {
+            // skip if string is empty or all whitespace
+            continue;
+        }
+
         bool bHtml = GetContext().GetConfig().DoHTML();
         if ( bHtml ) {
             s_GenerateWeblinks( "http", *comment_it );
