78a3912c2b9e84b2ff76d09dd2ab3367ac1a1f75
Sergey Satskiy <satskyse@ncbi.nlm.nih.gov>
2012-12-14 16:11:08 +0000

    Extending tests for 'notif_handicap' timeout feature. (JIRA: CXX-3662)
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@56570 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/app/netschedule/test/netschedule_tests_pack_4_16.py
+++ src/app/netschedule/test/netschedule_tests_pack_4_16.py
@@ -123,3 +123,80 @@ class Scenario601( TestBase ):
         return True
 
 
+class Scenario602( TestBase ):
+    " Scenario 602 "
+
+    def __init__( self, netschedule ):
+        TestBase.__init__( self, netschedule )
+        return
+
+    @staticmethod
+    def getScenario():
+        " Provides the scenario "
+        return "Notifications to 2 WNs with a handicap timeout"
+
+    def execute( self ):
+        " Should return True if the execution completed successfully "
+        self.fromScratch( 6 )
+
+        # Client1 for a job
+        ns_client1 = self.getNetScheduleService( 'TEST', 'scenario602' )
+        ns_client1.set_client_identification( 'node1', 'session1' )
+        # Socket to receive notifications
+        notifSocket1 = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
+        notifSocket1.bind( ( "", 9007 ) )
+
+        # Client2 for a job
+        ns_client2 = self.getNetScheduleService( 'TEST', 'scenario602' )
+        ns_client2.set_client_identification( 'node2', 'session2' )
+        # Socket to receive notifications
+        notifSocket2 = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
+        notifSocket2.bind( ( "", 9008 ) )
+
+        execAny( ns_client1,
+                 'GET2 wnode_aff=0 any_aff=1 exclusive_new_aff=0 port=9007 timeout=15' )
+        execAny( ns_client2,
+                 'GET2 wnode_aff=0 any_aff=1 exclusive_new_aff=0 port=9008 timeout=15' )
+
+        # Submit a job and wait for notifications
+        jobID = self.ns.submitJob( 'TEST', 'bla', 'a0' )
+
+        # First notification must come immediately and the second later on
+        time.sleep( 0.1 )
+        result1 = self.getNotif( notifSocket1, notifSocket2 )
+        if result1 == 0:
+            raise Exception( "Expected one notification, received nothing" )
+
+        time.sleep( 10 )
+        result2 = self.getNotif( notifSocket1, notifSocket2 )
+        if result2 == 0:
+            raise Exception( "Expected another notification, received nothing" )
+
+        if result1 + result2 != 3:
+            raise Exception( "Expected notifications to both worker nodes, " \
+                             "received in the same" )
+
+        return True
+
+    @staticmethod
+    def getNotif( s1, s2 ):
+        try:
+            data = s1.recv( 8192, socket.MSG_DONTWAIT )
+            if "queue=TEST" not in data:
+                raise Exception( "Unexpected notification in socket 1" )
+            return 1
+        except Exception, ex:
+            if "Unexpected notification in socket 1" in str( ex ):
+                raise
+            pass
+
+        try:
+            data = s2.recv( 8192, socket.MSG_DONTWAIT )
+            if "queue=TEST" not in data:
+                raise Exception( "Unexpected notification in socket 2" )
+            return 2
+        except Exception, ex:
+            if "Unexpected notification in socket 2" in str( ex ):
+                raise
+            pass
+        return 0
