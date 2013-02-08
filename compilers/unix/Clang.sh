8f0ad07d6ab44478b87747567818bc478b09727b
Aaron Ucko <ucko@ncbi.nlm.nih.gov>
2013-02-08 16:59:55 +0000

    Improve support for LLVM GCC and Clang: * Identify both in build roots and signatures. * Add corresponding compilers/unix/*.sh scripts, taking optional   version arguments as with GCC.sh. * Extend GCC.sh itself to check for llvm-gcc-$1 (and for that matter   gcc-fsf-$1) when looking for a specific GCC version. Otherwise continue identifying them with corresponding GCC versions, with which they maintain close compatibility.  JIRA: CXX-3820.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@57148 78c7ea69-d796-4a43-9a09-de51944f1b03

--- compilers/unix/Clang.sh
+++ compilers/unix/Clang.sh
@@ -1,52 +1,36 @@
 #! /bin/sh
 #############################################################################
 # Setup the local working environment for the "configure" script
-#   Compiler:   GCC
+#   Compiler:   LLVM Clang
 #
-# $Revision$  // by Denis Vakatov, NCBI (vakatov@ncbi.nlm.nih.gov)
+# $Revision$  // by Aaron Ucko, NCBI (ucko@ncbi.nlm.nih.gov)
 #############################################################################
 
 
 ## Path to the compiler
-CXX="g++"
-CC="gcc"
+CXX="clang++"
+CC="clang"
 
 Usage() {
     echo "USAGE: `basename $0` [version] [[build_dir] [--configure-flags] | -h]"
     exit $1
 }
 
-platform="`uname -s``uname -r`-`uname -p`"
-platform="`echo $platform | sed -e 's/SunOS5\./solaris/; s/\(sol.*\)-i386/\1-intel/'`"
-
 case "$1" in
   [1-9].*)
      # Look for the specified version in various reasonable places
      # (tuned for NCBI's installations).
-     if /usr/local/gcc-$1/bin/$CXX -dumpversion >/dev/null 2>&1; then
-       CXX=/usr/local/gcc-$1/bin/$CXX
-       CC=/usr/local/gcc-$1/bin/$CC
-     elif /usr/local/gcc/$1/bin/$CXX -dumpversion >/dev/null 2>&1; then
-       CXX=/usr/local/gcc/$1/bin/$CXX
-       CC=/usr/local/gcc/$1/bin/$CC
-     elif /netopt/gcc/$1/$platform/bin/$CXX -dumpversion >/dev/null 2>&1; then
-       CXX=/netopt/gcc/$1/$platform/bin/$CXX
-       CC=/netopt/gcc/$1/$platform/bin/$CC
+     if /usr/local/llvm/$1/bin/$CXX -dumpversion >/dev/null 2>&1; then
+       CXX=/usr/local/llvm/$1/bin/$CXX
+       CC=/usr/local/llvm/$1/bin/$CC
      elif $CXX-$1 -dumpversion >/dev/null 2>&1; then
        CXX="$CXX-$1"
        CC="$CC-$1"
-     elif $CXX-fsf-$1 -dumpversion >/dev/null 2>&1; then
-       CXX="$CXX-fsf-$1"
-       CC="$CC-fsf-$1"
-     elif llvm-$CXX-$1 -dumpversion >/dev/null 2>&1; then
-       CXX="llvm-$CXX-$1"
-       CC="llvm-$CC-$1"
-     elif $CXX -V$1 -dumpversion >/dev/null 2>&1; then
-       CXX="$CXX -V$1"
-       CC="$CC -V$1"
-     elif test "`$CXX -dumpversion 2>/dev/null`" \!= "$1"; then
+     elif "$CXX -dumpversion 2>/dev/null" | fgrep -q " $1"; then
+       :
+     else
        cat <<EOF
-ERROR:  cannot find GCC version $1; you may need to adjust PATH explicitly.
+ERROR:  cannot find Clang version $1; you may need to adjust PATH explicitly.
 EOF
        exit 1
      fi
@@ -58,7 +42,7 @@ esac
 $CXX -dumpversion > /dev/null 2>&1
 if test "$?" -ne 0 ; then
    cat <<EOF
-ERROR:  cannot find GCC compiler ($CXX)
+ERROR:  cannot find Clang compiler ($CXX)
 EOF
     exit 1
 fi
