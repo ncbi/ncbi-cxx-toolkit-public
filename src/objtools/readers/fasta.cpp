69b718b8e388a57ba2634ee869ae5cb5ad1d78ac
Michael Kornbluh <kornbluh@ncbi.nlm.nih.gov>
2013-03-08 13:11:29 +0000

    In CFastaReader, when there's no space between a sequence ID and a fasta mod, it will try to still accept it.  This involves at least a rough attempt to determine if it's a legitimate FASTA mod or intended to be part of the Sequence ID.  JIRA BI-324
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@57435 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/readers/fasta.cpp
+++ src/objtools/readers/fasta.cpp
@@ -482,11 +482,71 @@ void CFastaReader::ParseDefLine(const TStr& s)
         if (TestFlag(fNoParseID)) {
             title_start = start;
         } else {
+            // This loop finds the end of the sequence ID
             for (pos = start;  pos < len;  ++pos) {
-                if ((unsigned char) s[pos] <= ' ') { // assumes ASCII
+                unsigned char c = s[pos];
+                
+                if (c <= ' ' ) { // assumes ASCII
                     break;
+                } else if( c == '[' ) {
+
+                    // see if this is part of a FASTA mod, which
+                    // implies a pattern like "[key=value]".  We only check
+                    // that it looks *roughly* like a FASTA mod and only before the '='
+                    //
+                    // It might be worth it to put the body of this "if" into its own function,
+                    // for clarity if nothing else.
+
+                    const size_t left_bracket_pos = pos;
+                    ++pos;
+
+                    // arbitrary, but shouldn't be too much bigger than the largest possible mod key for efficiency
+                    const static size_t kMaxCharsToLookAt = 30; 
+                    // we give up much sooner than the length of the string, if the string is long.
+                    // also note that we give up *before* the end so even if pos
+                    // reaches bracket_give_up_pos, we can still say s[pos] without worrying
+                    // about array-out-of-bounds issues.
+                    const size_t bracket_give_up_pos = min(len - 1, kMaxCharsToLookAt);
+                    // keep track of the first space we find, because that becomes the end of the seqid
+                    // if this turns out not to be a FASTA mod.
+                    size_t first_space_pos = kMax_UI4;
+
+                    // find the end of the key
+                    for( ; pos < bracket_give_up_pos ; ++pos ) {
+                        const unsigned char c = s[pos];
+                        if( c == '=' ) {
+                            break;
+                        } else if( c <= ' ' ) {
+                            first_space_pos = min(first_space_pos, pos);
+                            // keep going
+                        } else if( isalnum(c) || c == '-' || c == '_' ) {
+                            // this is fine; keep going
+                        } else {
+                            // bad character, so this is NOT a FASTA mod
+                            break;
+                        }
+                    }
+
+                    if( s[pos] == '=' ) {
+                        // this seems to be a FASTA mod, so consider the left square bracket
+                        // to be the end of the seqid
+                        pos = left_bracket_pos;
+                        break;
+                    } else {
+                        // if we stopped on anything but an equal sign, this is NOT a 
+                        // FASTA mod.
+                        if( first_space_pos < len ) {
+                            // If we've found a space at any point, we consider that the end of the seq-id
+                            pos = first_space_pos;
+                            break;
+                        }
+
+                        // it's not a FASTA mod and we didn't find any spaces, so just
+                        // keep going as normal, continuing from where we let off at "pos"
+                    }
                 }
             }
+
             range_len = ParseRange(TStr(s.data() + start, pos - start),
                                    range_start, range_end);
             has_id = ParseIDs(TStr(s.data() + start, pos - start - range_len));
@@ -494,7 +554,7 @@ void CFastaReader::ParseDefLine(const TStr& s)
                 start = pos + 1;
                 continue;
             }
-            title_start = pos + 1;
+            title_start = pos;
             // trim leading whitespace from title (is this appropriate?)
             while (title_start < len
                    &&  isspace((unsigned char) s[title_start])) {
