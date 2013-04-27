e5a9886f62f7e6ca9eee589ca60c169861d6d1ad
Anton Lavrentiev <lavr@ncbi.nlm.nih.gov>
2013-04-27 14:05:07 +0000

    Private UNIX feature API for ID<->name conversions
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@57961 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/corelib/ncbi_os_unix.cpp
+++ src/corelib/ncbi_os_unix.cpp
@@ -30,17 +30,366 @@
  */
 
 #include <ncbi_pch.hpp>
-//#include <corelib/ncbi_os_unix.hpp>
 #include <corelib/error_codes.hpp>
+#include <corelib/ncbistr.hpp>
+#include "ncbi_os_unix_p.hpp"
+#include <errno.h>
+#include <unistd.h>
 
 
 #define NCBI_USE_ERRCODE_X   Corelib_Unix
 
 
+#define _STRINGIFY(x)            #x
+#define  STRINGIFY(x)  _STRINGIFY(x)
+
+
+// Some initial defaults for faster lookups
+#define PWD_BUF  1024
+#define GRP_BUF  4096
+
+
 BEGIN_NCBI_SCOPE
 
 
-/* empty */
+string CUnixFeature::GetUserNameByUID(uid_t uid)
+{
+    string user;
+
+#  if defined(NCBI_OS_SOLARIS)  ||                                  \
+    (defined(HAVE_GETPWUID)  &&  !defined(NCBI_HAVE_GETPWUID_R))
+    // NB:  getpwuid() is MT-safe on Solaris
+    const passwd* pwd = getpwuid(uid);
+    if (pwd  &&  pwd->pw_name) {
+        user.assign(pwd->pw_name);
+    }
+
+#  elif defined(NCBI_HAVE_GETPWUID_R)
+    struct passwd* pwd;
+
+    char x_buf[PWD_BUF + sizeof(*pwd)];
+    size_t size = sizeof(x_buf);
+    char*  buf  = x_buf;
+
+    for (int n = 0;  n < 3;  ++n) {
+#    if   NCBI_HAVE_GETPWUID_R == 4
+        // obsolete but still existent
+        pwd = getpwuid_r(uid,
+                         (struct passwd*) buf, buf + sizeof(*pwd),
+                         size - sizeof(*pwd));
+
+#    elif NCBI_HAVE_GETPWUID_R == 5
+        /* POSIX-conforming */
+        if (getpwuid_r(uid,
+                       (struct passwd*) buf, buf + sizeof(*pwd),
+                       size - sizeof(*pwd), &pwd) != 0) {
+            pwd = 0;
+        }
+
+#    else
+#      error "Unknown value of NCBI_HAVE_GETPWUID_R: 4 or 5 expected."
+
+#    endif //NCBI_HAVE_GETPWUID_R
+
+        if (pwd  ||  errno != ERANGE) {
+            break;
+        }
+
+        if (n == 0) {
+            size_t maxsize;
+#    ifdef _SC_GETPW_R_SIZE_MAX
+            long sc = sysconf(_SC_GETPW_R_SIZE_MAX);
+            maxsize = sc < 0 ? 0 : (size_t) sc + sizeof(*pwd);
+#    else
+            maxsize = size << 1;
+#    endif //_SC_GETPW_R_SIZE_MAX
+            ERR_POST_ONCE((maxsize > size ? Error : Critical)
+                          << "getpwuid parse buffer too small ("
+                          STRINGIFY(PWD_BUF) "), please enlarge it!");
+            if (size < maxsize) {
+                size = maxsize;
+                _ASSERT(buf == x_buf);
+                buf = new char[size];
+                continue;
+            }
+        } else if (n == 2) {
+            ERR_POST_ONCE(Critical << "getpwuid parse buffer too small ("
+                          << NStr::NumericToString(size) << ")!");
+            break;
+        } else {
+            _ASSERT(buf != x_buf);
+            delete[] buf;
+        }
+
+        buf = new char[size <<= 1];
+    }
+
+    if (pwd  &&  pwd->pw_name) {
+        user.assign(pwd->pw_name);
+    }
+    if (buf != x_buf) {
+        delete[] buf;
+    }
+
+#  endif
+
+    return user;
+}
+
+
+uid_t CUnixFeature::GetUserUIDByName(const string& user)
+{
+    uid_t uid;
+
+#  if defined(NCBI_OS_SOLARIS)   ||                                 \
+    (defined(HAVE_GETPWUID)  &&  !defined(NCBI_HAVE_GETPWUID_R))
+    // NB:  getpwnam() is MT-safe on Solaris
+    const struct* pwd = getpwnam(user.c_str());
+    uid = pwd ? pwd->pw_uid : (uid_t)(-1L);
+
+#  elif defined(NCBI_HAVE_GETPWUID_R)
+    struct passwd* pwd;
+
+    char x_buf[PWD_BUF + sizeof(*pwd)];
+    size_t size = sizeof(x_buf);
+    char*  buf  = x_buf;
+
+    for (int n = 0;  n < 3;  ++n) {
+#    if   NCBI_HAVE_GETPWUID_R == 4
+        // obsolete but still existent
+        pwd = getpwnam_r(user.c_str(),
+                         (struct passwd*) buf, buf + sizeof(*pwd),
+                         size - sizeof(*pwd));
+
+#    elif NCBI_HAVE_GETPWUID_R == 5
+        // POSIX-conforming
+        if (getpwnam_r(user.c_str(),
+                       (struct passwd*) buf, buf + sizeof(*pwd),
+                       size - sizeof(*pwd), &pwd) != 0) {
+            pwd = 0;
+        }
+
+#    else
+#      error "Unknown value of NCBI_HAVE_GETPWUID_R: 4 or 5 expected."
+
+#    endif //NCBI_HAVE_GETPWUID_R
+
+        if (pwd  ||  errno != ERANGE) {
+            break;
+        }
+
+        if (n == 0) {
+            size_t maxsize;
+#    ifdef _SC_GETPW_R_SIZE_MAX
+            long sc = sysconf(_SC_GETPW_R_SIZE_MAX);
+            maxsize = sc < 0 ? 0 : (size_t) sc + sizeof(*pwd);
+#    else
+            maxsize = size << 1;
+#    endif //_SC_GETPW_R_SIZE_MAX
+            ERR_POST_ONCE((maxsize > size ? Error : Critical)
+                          << "getpwnam parse buffer too small ("
+                          STRINGIFY(PWD_BUF) "), please enlarge it!");
+            if (size < maxsize) {
+                size = maxsize;
+                _ASSERT(buf == x_buf);
+                buf = new char[size];
+                continue;
+            }
+        } else if (n == 2) {
+            ERR_POST_ONCE(Critical << "getpwnam parse buffer too small ("
+                          << NStr::NumericToString(size) << ")!");
+            break;
+        } else {
+            _ASSERT(buf != x_buf);
+            delete[] buf;
+        }
+
+        buf = new char[size <<= 1];
+    }
+
+    uid = pwd ? pwd->pw_uid : (uid_t)(-1L);
+
+    if (buf != x_buf) {
+        delete[] buf;
+    }
+
+#  else
+    uid = (uid_t)(-1L);
+
+#  endif
+
+    return uid;
+}
+
+
+string CUnixFeature::GetGroupNameByGID(gid_t gid)
+{
+    string group;
+
+#  if defined(NCBI_OS_SOLARIS)  ||                                  \
+    (defined(HAVE_GETPWUID)  &&  !defined(NCBI_HAVE_GETPWUID_R))
+    // NB:  getgrgid() is MT-safe on Solaris
+    const struct group* grp = getgrgid(gid);
+    if (grp  &&  grp->gr_name) {
+        group.assign(grp->gr_name);
+    }
+
+#  elif defined(NCBI_HAVE_GETPWUID_R)
+    struct group* grp;
+
+    char x_buf[GRP_BUF + sizeof(*grp)];
+    size_t size = sizeof(x_buf);
+    char*  buf  = x_buf;
+
+    for (int n = 0;  n < 3;  ++n) {
+#    if   NCBI_HAVE_GETPWUID_R == 4
+        // obsolete but still existent
+        grp = getgrgid_r(gid,
+                         (struct group*) buf, buf + sizeof(*grp),
+                         size - sizeof(*grp));
+
+#    elif NCBI_HAVE_GETPWUID_R == 5
+        // POSIX-conforming
+        if (getgrgid_r(gid,
+                       (struct group*) buf, buf + sizeof(*grp),
+                       size - sizeof(*grp), &grp) != 0) {
+            grp = 0;
+        }
+
+#    else
+#      error "Unknown value of NCBI_HAVE_GETPWUID_R: 4 or 5 expected."
+
+#    endif //NCBI_HAVE_GETPWUID_R
+
+        if (grp  ||  errno != ERANGE) {
+            break;
+        }
+
+        if (n == 0) {
+            size_t maxsize;
+#    ifdef _SC_GETGR_R_SIZE_MAX
+            long sc = sysconf(_SC_GETGR_R_SIZE_MAX);
+            maxsize = sc < 0 ? 0 : (size_t) sc + sizeof(*grp);
+#    else
+            maxsize = size << 1;
+#    endif //_SC_GETGR_R_SIZE_MAX
+            ERR_POST_ONCE((maxsize > size ? Error : Critical)
+                          << "getgrgid parse buffer too small ("
+                          STRINGIFY(GRP_BUF) "), please enlarge it!");
+            if (size < maxsize) {
+                size = maxsize;
+                _ASSERT(buf == x_buf);
+                buf = new char[size];
+                continue;
+            }
+        } else if (n == 2) {
+            ERR_POST_ONCE(Critical << "getgrgid parse buffer too small ("
+                          << NStr::NumericToString(size) << ")!");
+            break;
+        } else {
+            _ASSERT(buf != x_buf);
+            delete[] buf;
+        }
+
+        buf = new char[size <<= 1];
+    }
+
+    if (grp  &&  grp->gr_name) {
+        group.assign(grp->gr_name);
+    }
+
+    if (buf != x_buf) {
+        delete[] buf;
+    }
+
+#  endif
+
+    return group;
+}
+
+
+gid_t CUnixFeature::GetGroupGIDByName(const string& group)
+{
+    gid_t gid;
+
+#  if defined(NCBI_OS_SOLARIS)  ||                                  \
+    (defined(HAVE_GETPWUID)  &&  !defined(NCBI_HAVE_GETPWUID_R))
+    // NB:  getgrnam() is MT-safe on Solaris
+    const struct group* grp = getgrnam(group.c_str());
+    gid = grp ? grp->gr_gid : (gid_t)(-1L);
+
+#  elif defined(NCBI_HAVE_GETPWUID_R)
+    struct group* grp;
+
+    char x_buf[GRP_BUF + sizeof(*grp)];
+    size_t size = sizeof(x_buf);
+    char*  buf  = x_buf;
+
+    for (int n = 0;  n < 3;  ++n) {
+#    if   NCBI_HAVE_GETPWUID_R == 4
+        // obsolete but still existent
+        grp = getgrnam_r(group.c_str(),
+                         (struct group*) buf, buf + sizeof(*grp),
+                         size - sizeof(*grp));
+
+#    elif NCBI_HAVE_GETPWUID_R == 5
+        // POSIX-conforming
+        if (getgrnam_r(group.c_str(),
+                       (struct group*) buf, buf + sizeof(*grp),
+                       size - sizeof(*grp), &grp) != 0) {
+            grp = 0;
+        }
+
+#    else
+#      error "Unknown value of NCBI_HAVE_GETPWUID_R: 4 or 5 expected."
+
+#    endif //NCBI_HAVE_GETPWUID_R
+
+        if (grp  ||  errno != ERANGE) {
+            break;
+        }
+
+        if (n == 0) {
+            size_t maxsize;
+#    ifdef _SC_GETGR_R_SIZE_MAX
+            long sc = sysconf(_SC_GETGR_R_SIZE_MAX);
+            maxsize = sc < 0 ? 0 : (size_t) sc + sizeof(*grp);
+#    else
+            maxsize = size << 1;
+#    endif //_SC_GETGR_R_SIZE_MAX
+            ERR_POST_ONCE((maxsize > size ? Error : Critical)
+                          << "getgrnam parse buffer too small ("
+                          STRINGIFY(GRP_BUF) "), please enlarge it!");
+            if (size < maxsize) {
+                size = maxsize;
+                buf = new char[size];
+                continue;
+            }
+        } else if (n == 2) {
+            ERR_POST_ONCE(Critical << "getgrnam parse buffer too small ("
+                          << NStr::NumericToString(size) << ")!");
+            break;
+        } else {
+            _ASSERT(buf != x_buf);
+            delete[] buf;
+        }
+
+        buf = new char[size <<= 1];
+    }
+
+    gid = grp ? grp->gr_gid : (gid_t)(-1L);
+
+    if (buf != x_buf) {
+        delete[] buf;
+    }
+
+#else
+    gid = (gid_t)(-1L);
+
+#  endif
+
+    return gid;
+}
 
 
 END_NCBI_SCOPE
---
+++ src/corelib/ncbi_os_unix_p.hpp
@@ -0,0 +1,99 @@
+#ifndef CORELIB___NCBI_OS_UNIX_P__HPP
+#define CORELIB___NCBI_OS_UNIX_P__HPP
+
+/* $Id$
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Author:  Anton Lavrentiev
+ *
+ *
+ */
+
+/// @file ncbi_os_unix_p.hpp
+///
+/// Private UNIX specific features.
+///
+
+#include <ncbiconf.h>
+#if !defined(NCBI_OS_UNIX)
+#  error "ncbi_os_mswin_p.hpp can be used on MS Windows platforms only"
+#endif
+
+#include <string>
+
+#include <grp.h>
+#include <pwd.h>
+
+
+BEGIN_NCBI_SCOPE
+
+
+/////////////////////////////////////////////////////////////////////////////
+///
+/// CUnixFeature --
+///
+/// Utility class with wrappers for UNIX features.
+
+class CUnixFeature
+{
+public:
+    /// Look up user name by given numeric user ID.
+    ///
+    /// @param user
+    ///   Numeric user ID
+    /// @return
+    ///   Non-empty string on success, empty string on error
+    static string GetUserNameByUID(uid_t uid);
+
+    /// Look up numeric user ID by symbolic user name.
+    ///
+    /// @param user
+    ///   User name whose ID is looked up
+    /// @return
+    ///   Numeric user ID when succeeded, or (uid_t)(-1L) when failed
+    static uid_t GetUserUIDByName(const string& user);
+
+    /// Look up group name by given numeric group ID.
+    ///
+    /// @param gid
+    ///   Numeric group ID
+    /// @return
+    ///   Non-empty string on success, empty string on error
+    static string GetGroupNameByGID(gid_t gid);
+
+    /// Look up numeric group ID by symbolic group name.
+    ///
+    /// @param group
+    ///   Group name whose ID is looked up
+    /// @return
+    ///   Numeric group ID when succeeded, or (gid_t)(-1L) when failed
+    static gid_t GetGroupGIDByName(const string& group);
+};
+
+
+END_NCBI_SCOPE
+
+
+#endif  /* CORELIB___NCBI_OS_UNIX_P__HPP */
