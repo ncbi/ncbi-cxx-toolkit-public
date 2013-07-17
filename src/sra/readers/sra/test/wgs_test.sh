726730f80043c462ee03254f2680579771505abe
Eugene Vasilchenko <vasilche@ncbi.nlm.nih.gov>
2013-07-17 16:08:14 +0000

    Simpler error check.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@58929 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/sra/readers/sra/test/wgs_test.sh
+++ src/sra/readers/sra/test/wgs_test.sh
@@ -2,10 +2,6 @@
 #$Id$
 err=0
 for acc in AAAA AAAA01 AAAA02 NZ_AAAA NZ_AAAA01 ALWZ AINT AAAA01000001 AAAA01999999; do
-    wgs_test -file "$acc"
-    err1="$?"
-    if test "$err1" != 0; then
-        err="$err1"
-    fi
+    wgs_test -file "$acc" || err="$?"
 done
-exit $err
+exit "$err"
