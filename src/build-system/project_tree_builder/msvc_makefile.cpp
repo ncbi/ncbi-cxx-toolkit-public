b3e5006def20e5fcc5e1ed43f23548af9e572c4a
Andrei Gourianov <gouriano@ncbi.nlm.nih.gov>
2013-07-19 17:24:06 +0000

    Added support for MSVC2012. JIRA: CXX-4179
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@58957 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/build-system/project_tree_builder/msvc_makefile.cpp
+++ src/build-system/project_tree_builder/msvc_makefile.cpp
@@ -63,7 +63,9 @@ bool CMsvcMetaMakefile::IsEmpty(void) const
 string CMsvcMetaMakefile::TranslateOpt(
     const string& value, const string& section, const string& opt)
 {
-    if (CMsvc7RegSettings::GetMsvcVersion() < CMsvc7RegSettings::eMsvc1000 || value.empty()) {
+    if (value.empty() ||
+        (CMsvc7RegSettings::GetMsvcVersion() < CMsvc7RegSettings::eMsvc1000 ||
+        CMsvc7RegSettings::GetMsvcVersion() >= CMsvc7RegSettings::eXCode30) ) {
         return value;
     }
     string name(section+"_"+opt+"_"+value);
@@ -73,7 +75,9 @@ string CMsvcMetaMakefile::TranslateOpt(
 
 string CMsvcMetaMakefile::TranslateCommand(const string& value)
 {
-    if (CMsvc7RegSettings::GetMsvcVersion() < CMsvc7RegSettings::eMsvc1000 || value.empty()) {
+    if (value.empty() ||
+        (CMsvc7RegSettings::GetMsvcVersion() < CMsvc7RegSettings::eMsvc1000 ||
+        CMsvc7RegSettings::GetMsvcVersion() >= CMsvc7RegSettings::eXCode30) ) {
         return value;
     }
     const CMsvcMetaMakefile& meta = GetApp().GetMetaMakefile();
