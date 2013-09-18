bedaa59d3aa3b9f2315d84fc5f46d05e58d811d6
ludwigf <ludwigf@ncbi.nlm.nih.gov>
2013-09-18 12:27:18 +0000

    JIRAJIRA: MSS-151 Synchronize GVF reader unit test with BED reader unit test.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@59794 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/readers/unit_test/unit_test_gvfreader.cpp
+++ src/objtools/readers/unit_test/unit_test_gvfreader.cpp
@@ -27,7 +27,7 @@
 *          Customizations by Frank Ludwig, NCBI.
 *
 * File Description:
-*   GFF3 reader unit test.
+*   Reader unit test.
 *
 * ===========================================================================
 */
@@ -39,6 +39,7 @@
 #include <corelib/ncbifile.hpp>
 
 #include <objtools/readers/gvf_reader.hpp>
+#include "error_logger.hpp"
 
 #include <cstdio>
 
@@ -53,6 +54,8 @@ USING_SCOPE(objects);
 //  Customization data:
 const string extInput("gvf");
 const string extOutput("asn");
+const string extErrors("errors");
+const string extKeep("new");
 const string dirTestFiles("gvfreader_test_cases");
 // !!!
 // !!! Must also customize reader type in sRunTest !!!
@@ -62,6 +65,7 @@ const string dirTestFiles("gvfreader_test_cases");
 struct STestInfo {
     CFile mInFile;
     CFile mOutFile;
+    CFile mErrorFile;
 };
 typedef string TTestName;
 typedef map<TTestName, STestInfo> TTestNameToInfoMap;
@@ -71,10 +75,12 @@ public:
     CTestNameToInfoMapLoader(
         TTestNameToInfoMap * pTestNameToInfoMap,
         const string& extInput,
-        const string& extOutput)
+        const string& extOutput,
+        const string& extErrors)
         : m_pTestNameToInfoMap(pTestNameToInfoMap),
           mExtInput(extInput),
-          mExtOutput(extOutput)
+          mExtOutput(extOutput),
+          mExtErrors(extErrors)
     { }
 
     void operator()( const CDirEntry & dirEntry ) {
@@ -113,6 +119,11 @@ public:
             BOOST_REQUIRE( test_info_to_load.mOutFile.GetPath().empty() );
             test_info_to_load.mOutFile = file;
         } 
+        else if (tsFileType == mExtErrors) {
+            BOOST_REQUIRE( test_info_to_load.mErrorFile.GetPath().empty() );
+            test_info_to_load.mErrorFile = file;
+        } 
+
         else {
             BOOST_FAIL("Unknown file type " << sFileName << ".");
         }
@@ -123,12 +134,73 @@ private:
     TTestNameToInfoMap * m_pTestNameToInfoMap;
     string mExtInput;
     string mExtOutput;
+    string mExtErrors;
 };
 
-void sRunTest(const string &sTestName, const STestInfo & testInfo)
+void sUpdateCase(CDir& test_cases_dir, const string& test_name)
+{   
+    string input = CDir::ConcatPath( test_cases_dir.GetPath(), test_name + "." + extInput);
+    string output = CDir::ConcatPath( test_cases_dir.GetPath(), test_name + "." + extOutput);
+    string errors = CDir::ConcatPath( test_cases_dir.GetPath(), test_name + "." + extErrors);
+    if (!CFile(input).Exists()) {
+         BOOST_FAIL("input file " << input << " does not exist.");
+    }
+    cerr << "Creating new test case from " << input << " ..." << endl;
+
+    CErrorLogger logger(errors);
+    CGvfReader reader(0);
+    CNcbiIfstream ifstr(input.c_str());
+
+    typedef vector<CRef<CSeq_annot> > ANNOTS;
+    ANNOTS annots;
+    try {
+        reader.ReadSeqAnnots(annots, ifstr, &logger);
+    }
+    catch (...) {
+        ifstr.close();
+        BOOST_FAIL("Error: " << input << " failed during conversion.");
+    }
+    ifstr.close();
+    cerr << "    Produced new error listing " << output << "." << endl;
+
+    CNcbiOfstream ofstr(output.c_str());
+    for (ANNOTS::iterator cit = annots.begin(); cit != annots.end(); ++cit){
+        ofstr << MSerial_AsnText << **cit;
+        ofstr.flush();
+    }
+    ofstr.close();
+    cerr << "    Produced new ASN1 file " << output << "." << endl;
+
+    cerr << " ... Done." << endl;
+}
+
+void sUpdateAll(CDir& test_cases_dir) {
+
+    const vector<string> kEmptyStringVec;
+    TTestNameToInfoMap testNameToInfoMap;
+    CTestNameToInfoMapLoader testInfoLoader(
+        &testNameToInfoMap, extInput, extOutput, extErrors);
+    FindFilesInDir(
+        test_cases_dir,
+        kEmptyStringVec,
+        kEmptyStringVec,
+        testInfoLoader,
+        fFF_Default | fFF_Recursive );
+
+    ITERATE(TTestNameToInfoMap, name_to_info_it, testNameToInfoMap) {
+        const string & sName = name_to_info_it->first;
+        sUpdateCase(test_cases_dir, sName);
+    }
+}
+
+void sRunTest(const string &sTestName, const STestInfo & testInfo, bool keep)
 {
     cerr << "Testing " << testInfo.mInFile.GetName() << " against " <<
-        testInfo.mOutFile.GetName() << endl;
+        testInfo.mOutFile.GetName() << " and " <<
+        testInfo.mErrorFile.GetName() << endl;
+
+    string logName = CDirEntry::GetTmpName();
+    CErrorLogger logger(logName);
 
     CGvfReader reader(0);
     CNcbiIfstream ifstr(testInfo.mInFile.GetPath().c_str());
@@ -136,7 +208,7 @@ void sRunTest(const string &sTestName, const STestInfo & testInfo)
     typedef vector<CRef<CSeq_annot> > ANNOTS;
     ANNOTS annots;
     try {
-        reader.ReadSeqAnnots(annots, ifstr);
+        reader.ReadSeqAnnots(annots, ifstr, &logger);
     }
     catch (...) {
         BOOST_ERROR("Error: " << sTestName << " failed during conversion.");
@@ -144,8 +216,8 @@ void sRunTest(const string &sTestName, const STestInfo & testInfo)
         return;
     }
 
-    string tempName = CDirEntry::GetTmpName();
-    CNcbiOfstream ofstr(tempName.c_str());
+    string resultName = CDirEntry::GetTmpName();
+    CNcbiOfstream ofstr(resultName.c_str());
     for (ANNOTS::iterator cit = annots.begin(); cit != annots.end(); ++cit){
         ofstr << MSerial_AsnText << **cit;
         ofstr.flush();
@@ -153,10 +225,26 @@ void sRunTest(const string &sTestName, const STestInfo & testInfo)
     ifstr.close();
     ofstr.close();
 
-    bool success = testInfo.mOutFile.CompareTextContents(tempName, CFile::eIgnoreWs);
-    CDirEntry(tempName).Remove();
+    bool success = testInfo.mOutFile.CompareTextContents(resultName, CFile::eIgnoreWs);
     if (!success) {
+        CDirEntry deResult = CDirEntry(resultName);
+        if (keep) {
+            deResult.Copy(testInfo.mOutFile.GetPath() + "." + extKeep);
+        }
+        deResult.Remove();
+        CDirEntry(logName).Remove();
         BOOST_ERROR("Error: " << sTestName << " failed due to post processing diffs.");
+    }
+    CDirEntry(resultName).Remove();
+
+    success = testInfo.mErrorFile.CompareTextContents(logName, CFile::eIgnoreWs);
+    CDirEntry deErrors = CDirEntry(logName);
+    if (!success  &&  keep) {
+        deErrors.Copy(testInfo.mErrorFile.GetPath() + "." + extKeep);
+    }
+    deErrors.Remove();
+    if (!success) {
+        BOOST_ERROR("Error: " << sTestName << " failed due to error handling diffs.");
     }
 };
 
@@ -167,13 +255,21 @@ NCBITEST_AUTO_INIT()
 
 NCBITEST_INIT_CMDLINE(arg_descrs)
 {
-    arg_descrs->AddDefaultKey("test-dir", "DIRECTORY",
+    arg_descrs->AddDefaultKey("test-dir", "TEST_FILE_DIRECTORY",
         "Set the root directory under which all test files can be found.",
         CArgDescriptions::eDirectory,
         dirTestFiles );
+    arg_descrs->AddDefaultKey("update-case", "UPDATE_CASE",
+        "Produce .asn and .error files from given name for new or updated test case.",
+        CArgDescriptions::eString,
+        "" );
+    arg_descrs->AddFlag("update-all",
+        "Update all test cases to current reader code (dangerous).",
+        true );
+    arg_descrs->AddFlag("keep-diffs",
+        "Keep output files that are different from the expected.",
+        true );
 }
-
-
 
 NCBITEST_AUTO_FINI()
 {
@@ -182,15 +278,27 @@ NCBITEST_AUTO_FINI()
 BOOST_AUTO_TEST_CASE(RunTests)
 {
     const CArgs& args = CNcbiApplication::Instance()->GetArgs();
-    const vector<string> kEmptyStringVec;
-    TTestNameToInfoMap testNameToInfoMap;
-    CTestNameToInfoMapLoader testInfoLoader(
-        &testNameToInfoMap, extInput, extOutput);
-    CDir test_cases_dir( args["test-dir"].AsDirectory() );
 
+    CDir test_cases_dir( args["test-dir"].AsDirectory() );
     BOOST_REQUIRE_MESSAGE( test_cases_dir.IsDir(), 
         "Cannot find dir: " << test_cases_dir.GetPath() );
 
+    bool update_all = args["update-all"].AsBoolean();
+    if (update_all) {
+        sUpdateAll(test_cases_dir);
+        return;
+    }
+
+    string update_case = args["update-case"].AsString();
+    if (!update_case.empty()) {
+        sUpdateCase(test_cases_dir, update_case);
+        return;
+    }
+   
+    const vector<string> kEmptyStringVec;
+    TTestNameToInfoMap testNameToInfoMap;
+    CTestNameToInfoMapLoader testInfoLoader(
+        &testNameToInfoMap, extInput, extOutput, extErrors);
     FindFilesInDir(
         test_cases_dir,
         kEmptyStringVec,
@@ -203,9 +311,11 @@ BOOST_AUTO_TEST_CASE(RunTests)
         const STestInfo & testInfo = name_to_info_it->second;
         cout << "Verifying: " << sName << endl;
         BOOST_REQUIRE_MESSAGE( testInfo.mInFile.Exists(),
-            "GVF file does not exist: " << testInfo.mInFile.GetPath() );
+            extInput + " file does not exist: " << testInfo.mInFile.GetPath() );
         BOOST_REQUIRE_MESSAGE( testInfo.mOutFile.Exists(),
-            "ASN file does not exist: " << testInfo.mOutFile.GetPath() );
+            extOutput + " file does not exist: " << testInfo.mOutFile.GetPath() );
+        BOOST_REQUIRE_MESSAGE( testInfo.mErrorFile.Exists(),
+            extErrors + " file does not exist: " << testInfo.mErrorFile.GetPath() );
     }
     ITERATE(TTestNameToInfoMap, name_to_info_it, testNameToInfoMap) {
         const string & sName = name_to_info_it->first;
@@ -213,6 +323,6 @@ BOOST_AUTO_TEST_CASE(RunTests)
         
         cout << "Running test: " << sName << endl;
 
-        BOOST_CHECK_NO_THROW(sRunTest(sName, testInfo));
+        BOOST_CHECK_NO_THROW(sRunTest(sName, testInfo, args["keep-diffs"]));
     }
 }
