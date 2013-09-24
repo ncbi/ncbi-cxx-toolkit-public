231d3d219bf14c11bb8a4a4244dd88f4ddb3852b
Anton Lavrentiev <lavr@ncbi.nlm.nih.gov>
2013-09-24 19:02:38 +0000

    +test_ncbi_ftp_download.sh JIRA: SYS-167958
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@59882 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/connect/test/Makefile.test_ncbi_ftp_download.app
+++ src/connect/test/Makefile.test_ncbi_ftp_download.app
@@ -8,6 +8,7 @@ LIB = xconnect $(COMPRESS_LIBS) xutil xncbi
 LIBS = $(NETWORK_LIBS) $(CMPRS_LIBS) $(ORIG_LIBS)
 #LINK = purify $(ORIG_LINK)
 
-CHECK_CMD =
+CHECK_CMD = test_ncbi_ftp_download.sh
+CHECK_COPY = test_ncbi_ftp_download.sh
 
 WATCHERS = lavr
---
+++ src/connect/test/test_ncbi_ftp_download.sh
@@ -0,0 +1,42 @@
+#! /bin/sh
+# $Id$
+
+outlog()
+{
+  logfile="$1"
+  if [ -s "$logfile" ]; then
+    echo "=== $logfile ==="
+    if [ "`head -501 $logfile 2>/dev/null | wc -l`" -gt "500" ]; then
+      head -200 "$logfile"
+      echo '...'
+      tail -300 "$logfile"
+    else
+      cat "$logfile"
+    fi
+  fi
+}
+
+exit_code=0
+log=test_ncbi_ftp_download.log
+rm -f $log
+
+trap 'echo "`date`."' 0 1 2 3 15
+
+case "`expr '(' $$ / 10 ')' '%' 3`" in
+  0)
+    file=
+    ;;
+  1)
+    file='ftp://ftp.kernel.org/pub/'
+    ;;
+  2)
+    file='ftp://ftp.ncbi.nlm.nih.gov/'
+    ;;
+esac
+
+$CHECK_EXEC test_ncbi_ftp_download $file 2>&1
+exit_code=$?
+
+test "$exit_code" = "0"  ||  outlog "$log"
+
+exit $exit_code
