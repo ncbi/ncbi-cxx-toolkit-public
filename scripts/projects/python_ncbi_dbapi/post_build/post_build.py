935f37964de2da24542c0a71b41492524cc15a3b
Aaron Ucko <ucko@ncbi.nlm.nih.gov>
2013-10-30 17:52:37 +0000

    python_ncbi_dbapi: Install DLLs via post_build.py, so that Windows builds actually package them.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@60173 78c7ea69-d796-4a43-9a09-de51944f1b03

--- scripts/projects/python_ncbi_dbapi/post_build/post_build.py
+++ scripts/projects/python_ncbi_dbapi/post_build/post_build.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python
 # $Id$
 
-import glob, os, sys
+import glob, os, shutil, sys
 
-# Clean up after broad manifest globbing, which overshoots due to
-# needing to match both Unix and Windows library filenames.
-for x in glob.glob(sys.argv[1] + '/lib/*xxconnect*'):
-    os.unlink(x)
-if os.path.exists(sys.argv[1] + '/lib/python_ncbi_dbapi.so'):
-    os.unlink(sys.argv[1] + '/lib/python_ncbi_dbapi.so')
+(bin_dir, lib_dir, install_dir, version) = sys.argv[1:]
+stems = ('python_ncbi_dbapi', 'dbapi', 'dbapi_driver',
+         'ncbi_xdbapi_?t*', 'ct_ftds64', 'tds_ftds64',
+         'xconne?t', 'xutil', 'xncbi')
+
+os.mkdir(install_dir + '/lib')
+if os.path.exists(lib_dir + '/python_ncbi_dbapi.so'):
+    template = lib_dir.replace('%', '%%') + '/lib%s.[ds]?*'
+else:
+    template = bin_dir.replace('%', '%%') + '/ReleaseDLL/%s.[dp]??'
+    
+for s in stems:
+    for x in glob.glob(template % s):
+        shutil.copy(x, install_dir + '/lib/' + os.path.basename(x))
 
 # Build a tarball for system-wide installation only on Linux.
 if sys.platform == 'linux2':
     build_tarball = os.path.dirname(sys.argv[0]) + '/build_tarball.sh'
-    os.execv(build_tarball, [build_tarball] + sys.argv[1:])
+    os.execv(build_tarball, [build_tarball, install_dir, version])
