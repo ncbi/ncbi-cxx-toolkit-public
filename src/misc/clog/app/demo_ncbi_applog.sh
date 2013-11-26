3d2df9fc90bab0381da0d9faa0fbf087ba8996b4
Vladimir Ivanov <ivanov@ncbi.nlm.nih.gov>
2013-11-26 20:59:31 +0000

    Improved messages, added more calls inside request. JIRA:CXX-4053
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@60507 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/misc/clog/app/demo_ncbi_applog.sh
+++ src/misc/clog/app/demo_ncbi_applog.sh
@@ -31,19 +31,19 @@ fi
 
 # --- Posting
 
-$APPLOG post $token -severity info    -message "info message"    || Error "post(1)"
-$APPLOG post $token -severity warning -message "warning message" || Error "post(2)"
+$APPLOG post $token -severity info    -message "app info message"    || Error "post_app_info"
+$APPLOG post $token -severity warning -message "app warning message" || Error "post_app_warn"
 
 # Try to use logging token via $NCBI_APPLOG_TOKEN (the token parameter is empty below)
 NCBI_APPLOG_TOKEN=$token
 export NCBI_APPLOG_TOKEN
 
-$APPLOG post  '' -message "error message"                   || Error "post(3)"
-$APPLOG post  '' -severity trace -message "trace message"   || Error "post(4)"
-$APPLOG perf  '' -time 1.2                                  || Error "perf(1)"
-$APPLOG perf  '' -status=404 -time=4.5 -param="k1=v1&k2=v2" || Error "perf(2)"
-$APPLOG extra ''                                            || Error "extra(1)"
-$APPLOG extra '' -param="extra1=1"                          || Error "extra(2)"
+$APPLOG post  '' -message "app error message"                        || Error "post_app_err"
+$APPLOG post  '' -severity trace -message "app trace message"        || Error "post_app_trace"
+$APPLOG perf  '' -time 1.2                                           || Error "perf_app_1"
+$APPLOG perf  '' -status=404 -time=4.5 -param="k1=v1&k2=v2"          || Error "perf_app_2"
+$APPLOG extra ''                                                     || Error "extra_app_1"
+$APPLOG extra '' -param="extra1=1"                                   || Error "extra_app_2"
 
 # --- Request 1
 
@@ -51,20 +51,23 @@ request_token=`$APPLOG start_request '' -sid=request1 -rid=1 -client=1.2.3.4 -pa
 if [ $? -ne 0 -o -z "$request_token" ] ; then
     Error "start_request(1)"
 fi
-$APPLOG post  $request_token -message "request message"     || Error "post(r1)"
-$APPLOG extra $request_token -param="k3=v3"                 || Error "extra(r1)"
-$APPLOG stop_request $request_token -status=200 -input=11 -output=13 >/dev/null 2>&1 || Error "stop_request(r1)"
+$APPLOG post  $request_token -message "request error message"        || Error "post_request(1)_err"
+$APPLOG post  $request_token -severity trace -message "request trace message" || Error "post_request(1)_trace"
+$APPLOG perf  $request_token -time 3.4                               || Error "perf_request(1)_1"
+$APPLOG perf  $request_token -status=505 -time=50.6 -param="name=r1" || Error "perf_request(1)_2"
+$APPLOG extra $request_token -param="k3=v3"                          || Error "extra_request(1)"
+$APPLOG stop_request $request_token -status=200 -input=11 -output=13 >/dev/null 2>&1 || Error "stop_request(1)"
 
 # --- Posting between requests
-$APPLOG post         '' -message "request message" || Error "post(6)"
+$APPLOG post         '' -message "message posted between requests"   || Error "post_app_between"
 
 # --- Request 2
 request_token=`$APPLOG start_request '' -sid=request2 -rid=2 -client=5.6.7.8 -param="r21=v3&r22=v4"`
 if [ $? -ne 0 -o -z "$request_token" ] ; then
     Error "start_request(2)"
 fi
-$APPLOG extra $request_token -param="k4=v4" || Error "extra(r2)"
-token=`$APPLOG stop_request $request_token -status=600 -input=21 -output=23` || Error "stop_request(r2)"
+$APPLOG extra $request_token -param="k4=v4" || Error "extra_request(2)"
+token=`$APPLOG stop_request $request_token -status=600 -input=21 -output=23` || Error "stop_request(2)"
 
 # --- Stop 
 $APPLOG stop_app '' -status=99 || Error "stop_app"
