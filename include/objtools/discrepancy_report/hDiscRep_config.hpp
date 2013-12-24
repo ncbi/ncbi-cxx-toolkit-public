9626ab404bff8dafed5210af9591f6c5540ce9f2
Jie Chen <chenj@ncbi.nlm.nih.gov>
2013-12-24 15:13:24 +0000

    JIRA:MSS-72, comment_rules removed
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@61067 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/objtools/discrepancy_report/hDiscRep_config.hpp
+++ include/objtools/discrepancy_report/hDiscRep_config.hpp
@@ -244,7 +244,6 @@ BEGIN_SCOPE(DiscRepNmSpc)
         static vector <string>                    taxnm_env;
         static vector <string>                    virus_lineage;
         static vector <string>                    strain_tax;
-        static CConstRef <CComment_set>           comment_rules;
         static Str2UInt                           whole_word;
         static Str2Str                            fix_data;
         static CRef <CSuspect_rule_set>           orga_prod_rules;
