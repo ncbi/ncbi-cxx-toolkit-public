2aa87b0d4f75f18bc1693393bfa264522ecdcb7e
kazimird <kazimird@ncbi.nlm.nih.gov>
2013-12-27 23:32:51 +0000

    NetStorage API: fixed compilation error in misc/netstorage/netstorage.cpp. JIRA: CXX-4494
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@61112 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/misc/netstorage/netstorage.cpp
+++ src/misc/netstorage/netstorage.cpp
@@ -1191,8 +1191,13 @@ void SNetStorageByKeyAPIImpl::Remove(const string& key, TNetStorageFlags flags)
 CNetStorage g_CreateNetStorage(CNetICacheClient::TInstance icache_client,
         TNetStorageFlags default_flags)
 {
-    return new SNetStorageAPIImpl(icache_client != NULL ? icache_client :
-            CNetICacheClient(CNetICacheClient::eAppRegistry), default_flags);
+    if (icache_client != NULL)
+        return new SNetStorageAPIImpl(icache_client, default_flags);
+    else {
+        CNetICacheClient new_ic_client(CNetICacheClient::eAppRegistry);
+
+        return new SNetStorageAPIImpl(new_ic_client, default_flags);
+    }
 }
 
 CNetStorage g_CreateNetStorage(TNetStorageFlags default_flags)
@@ -1205,9 +1210,15 @@ CNetStorageByKey g_CreateNetStorageByKey(
         CNetICacheClient::TInstance icache_client,
         const string& domain_name, TNetStorageFlags default_flags)
 {
-    return new SNetStorageByKeyAPIImpl(icache_client != NULL ? icache_client :
-            CNetICacheClient(CNetICacheClient::eAppRegistry),
-            domain_name, default_flags);
+    if (icache_client != NULL)
+        return new SNetStorageByKeyAPIImpl(icache_client,
+                domain_name, default_flags);
+    else {
+        CNetICacheClient new_ic_client(CNetICacheClient::eAppRegistry);
+
+        return new SNetStorageByKeyAPIImpl(new_ic_client,
+                domain_name, default_flags);
+    }
 }
 
 CNetStorageByKey g_CreateNetStorageByKey(const string& domain_name,
