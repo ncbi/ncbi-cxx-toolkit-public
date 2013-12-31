cf047f16db5e537aa18c07ba76695b5a826b4864
kazimird <kazimird@ncbi.nlm.nih.gov>
2013-12-31 21:20:28 +0000

    NetSchedule API: queue name initialization clean-up. JIRA: CXX-4113
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@61156 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/connect/services/netschedule_api.cpp
+++ src/connect/services/netschedule_api.cpp
@@ -251,19 +251,18 @@ void CNetScheduleServerListener::OnInit(
 {
     SNetScheduleAPIImpl* ns_impl = static_cast<SNetScheduleAPIImpl*>(api_impl);
 
-    if (ns_impl->m_Queue.empty()) {
-        if (config == NULL)
-            ns_impl->m_Queue = "noname";
-        else {
-            ns_impl->m_Queue = config->GetString(config_section,
-                "queue_name", CConfig::eErr_NoThrow, kEmptyStr);
-            if (ns_impl->m_Queue.empty())
-                ns_impl->m_Queue = "noname";
-            else
-                SNetScheduleAPIImpl::VerifyQueueNameAlphabet(ns_impl->m_Queue);
-        }
-    } else
+    if (!ns_impl->m_Queue.empty())
         SNetScheduleAPIImpl::VerifyQueueNameAlphabet(ns_impl->m_Queue);
+    else if (config == NULL)
+        ns_impl->m_Queue = "noname";
+    else {
+        ns_impl->m_Queue = config->GetString(config_section,
+            "queue_name", CConfig::eErr_NoThrow, kEmptyStr);
+        if (!ns_impl->m_Queue.empty())
+            SNetScheduleAPIImpl::VerifyQueueNameAlphabet(ns_impl->m_Queue);
+        else
+            ns_impl->m_Queue = "noname";
+    }
     if (config == NULL) {
         ns_impl->m_AffinityPreference = CNetScheduleExecutor::eAnyJob;
         ns_impl->m_UseEmbeddedStorage = true;
