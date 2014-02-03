12bd1aa90d4e85787418c17d0b47631f272bec2a
Sergey Satskiy <satskyse@ncbi.nlm.nih.gov>
2014-02-03 19:52:15 +0000

    Updated with a few storage procedures. JIRA: CXX-4495
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@61609 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/app/netstorage/nst_db.sql
+++ src/app/netstorage/nst_db.sql
@@ -1,3 +1,35 @@
+--  $Id$
+-- ===========================================================================
+--
+--                            PUBLIC DOMAIN NOTICE
+--               National Center for Biotechnology Information
+--
+--  This software/database is a "United States Government Work" under the
+--  terms of the United States Copyright Act.  It was written as part of
+--  the author's official duties as a United States Government employee and
+--  thus cannot be copyrighted.  This software/database is freely available
+--  to the public for use. The National Library of Medicine and the U.S.
+--  Government have not placed any restriction on its use or reproduction.
+--
+--  Although all reasonable efforts have been taken to ensure the accuracy
+--  and reliability of the software and data, the NLM and the U.S.
+--  Government do not and cannot warrant the performance or results that
+--  may be obtained by using this software or data. The NLM and the U.S.
+--  Government disclaim all warranties, express or implied, including
+--  warranties of performance, merchantability or fitness for any particular
+--  purpose.
+--
+--  Please cite the author in any work or product based on this material.
+--
+-- ===========================================================================
+--
+-- Authors:  Sergey Satskiy
+--
+-- File Description: NetStorage server DB creation script
+--
+
+
+
 USE [NETSTORAGE];
 GO
 -- Recommended settings during procedure creation:
@@ -8,6 +40,12 @@ SET ANSI_WARNINGS ON;
 GO
 
 -- Drop all the existing objects if so
+IF EXISTS (SELECT * FROM sysobjects WHERE name = 'CreateObjectWithIDs')
+    DROP PROCEDURE CreateObjectWithIDs
+IF EXISTS (SELECT * FROM sysobjects WHERE name = 'CreateObject')
+    DROP PROCEDURE CreateObject
+IF EXISTS (SELECT * FROM sysobjects WHERE name = 'CreateClientOwnerGroup')
+    DROP PROCEDURE CreateClientOwnerGroup
 IF EXISTS (SELECT * FROM sysobjects WHERE name = 'AttrValues')
     DROP TABLE AttrValues
 IF EXISTS (SELECT * FROM sysobjects WHERE name = 'Objects')
@@ -157,5 +195,79 @@ GO
 ALTER TABLE AttrValues ADD CONSTRAINT
 FK_AttrValues_Attr FOREIGN KEY ( attr_id )
 REFERENCES Attributes ( attr_id )
+GO
+
+
+
+
+-- Stored procedures
+CREATE PROCEDURE CreateClientOwnerGroup
+    @client_name    varchar(256),
+    @owner_name     varchar(128) = NULL,
+    @group_name     varchar(128) = NULL,
+
+    @client_id_     bigint OUT,
+    @owner_id_      bigint OUT,
+    @group_id_      bigint OUT
+AS
+BEGIN
+    IF ((@client_name IS NOT NULL) AND (@client_name != ''))
+    BEGIN
+        IF NOT EXISTS (SELECT client_id FROM Clients WHERE name = @client_name)
+            INSERT INTO Clients (name) VALUES (@client_name)
+        SELECT @client_id_ = client_id FROM Clients WHERE name = @client_name
+    END
+    ELSE
+        SET @client_id_ = NULL
+
+    IF ((@owner_name IS NOT NULL) AND (@owner_name != ''))
+    BEGIN
+        IF NOT EXISTS (SELECT owner_id FROM Owners WHERE name = @owner_name)
+            INSERT INTO Owners (name) VALUES (@owner_name)
+        SELECT @owner_id_ = owner_id FROM Owners WHERE name = @owner_name
+    END
+    ELSE
+        SET @owner_id_ = NULL
+
+    IF ((@group_name IS NOT NULL) AND (@group_name != ''))
+    BEGIN
+        IF NOT EXISTS (SELECT group_id FROM Groups WHERE name = @group_name)
+            INSERT INTO Groups (name) VALUES (@group_name)
+        SELECT @group_id_ = group_id FROM Groups WHERE name = @group_name
+    END
+        SET @group_id_ = NULL
+END
+GO
+
+
+CREATE PROCEDURE CreateObject
+    @object_name    varchar(256),
+    @object_size    bigint,
+    @client_name    varchar(256),
+    @owner_name     varchar(128) = NULL,
+    @group_name     varchar(128) = NULL
+AS
+BEGIN
+    INSERT INTO Objects (name, owner_id, group_id, client_id, tm_create, size)
+    VALUES (@object_name,
+            (SELECT owner_id FROM Owners WHERE name = @owner_name),
+            (SELECT group_id FROM Groups WHERE name = @group_name),
+            (SELECT client_id FROM Clients WHERE name = @client_name),
+            GETDATE(),
+            @object_size)
+END
+GO
+
+CREATE PROCEDURE CreateObjectWithIDs
+    @object_name    varchar(256),
+    @object_size    bigint,
+    @client_id      bigint,
+    @owner_id       bigint = NULL,
+    @group_id       bigint = NULL
+AS
+BEGIN
+    INSERT INTO Objects (name, owner_id, group_id, client_id, tm_create, size)
+    VALUES (@object_name, @owner_id, @group_id, @client_id, GETDATE(), @object_size)
+END
 GO
 
