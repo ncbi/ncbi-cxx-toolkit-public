e1c9302fb514194ec11546143e099d60c54ab881
kazimird <kazimird@ncbi.nlm.nih.gov>
2014-02-11 22:34:42 +0000

    Grid worker nodes: do not stop working threads if there are no running jobs (temporary workaround for JIRA CXX-4629, CXX-4671).
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@61730 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/connect/services/grid_worker.cpp
+++ src/connect/services/grid_worker.cpp
@@ -1353,7 +1353,7 @@ int CGridWorkerNode::Run(
     if (reg.GetBool(kServerSec,
             "force_exit", false, 0, CNcbiRegistry::eReturn)) {
         ERR_POST_X(45, "Force exit");
-    } else if (m_MaxThreads > 1) {
+    } else if (m_MaxThreads > 1 && watcher.GetJobsRunningNumber() > 0) {
         try {
             LOG_POST_X(32, Info << "Stopping worker threads...");
             thread_pool->KillAllThreads(true);
