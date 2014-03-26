aea685be063b070263548fc545c7d7567515872b
Vladimir Ivanov <ivanov@ncbi.nlm.nih.gov>
2014-03-26 13:54:08 +0000

    Restore: Generate an error and exit with exit code 1 if found that shell script is passed for execution. Running shell scripts under this script ($CHECK_EXEC macro) is not allowed.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@62215 78c7ea69-d796-4a43-9a09-de51944f1b03

--- scripts/common/check/check_exec_test.sh
+++ scripts/common/check/check_exec_test.sh
@@ -58,7 +58,7 @@ fi
 echo $1 | grep '\.sh' > /dev/null 2>&1 
 if test $? -eq 0;  then
    echo "Error: Using CHECK_EXEC macro to run shell scripts is prohibited!"
-#   exit 1
+   exit 1
 fi
 
 if $is_stdin; then
