b8cf9efe7616d8632d5956de02e1d177d85b7d5d
Jonathan Kans <kans@ncbi.nlm.nih.gov>
2014-05-14 00:00:07 +0000

    JIRA:SQD-1769 Replaced logic for secondaries on non-master WGS components
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@62869 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/format/accession_item.cpp
+++ src/objtools/format/accession_item.cpp
@@ -92,8 +92,14 @@ void CAccessionItem::x_GatherInfo(CBioseqContext& ctx)
     if ( ctx.IsWGS()  && ctx.GetLocation().IsWhole() ) {
         size_t acclen = m_Accession.length();
         m_WGSAccession = m_Accession;
-        if (acclen >= 12  &&  m_WGSAccession.find('0', 6) != NPOS) {
+        if (acclen == 12  &&  ! NStr::EndsWith(m_WGSAccession, "000000") ) {
             m_WGSAccession.replace(6, acclen - 6, acclen - 6, '0');
+        } else if (acclen == 13  &&  ! NStr::EndsWith(m_WGSAccession, "0000000") ) {
+            m_WGSAccession.replace(7, acclen - 7, acclen - 7, '0');
+        } else if (acclen == 14  &&  ! NStr::EndsWith(m_WGSAccession, "00000000") ) {
+            m_WGSAccession.replace(8, acclen - 8, acclen - 8, '0');
+        } else if (acclen == 15  &&  ! NStr::EndsWith(m_WGSAccession, "00000000") ) {
+            m_WGSAccession.replace(8, acclen - 8, acclen - 8, '0');
         } else {
             m_WGSAccession.erase();
         }
