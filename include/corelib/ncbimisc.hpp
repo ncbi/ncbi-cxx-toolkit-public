bf4feb6ca5a9de48eb74a0be914aab70aa548fc4
Eugene Vasilchenko <vasilche@ncbi.nlm.nih.gov>
2014-10-08 16:46:26 +0000

    Do not use 'false' in preprocessor directives. JIRA: CXX-1088
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@64784 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/corelib/ncbimisc.hpp
+++ include/corelib/ncbimisc.hpp
@@ -1004,7 +1004,7 @@ inline bool s_ITERATE_SameObject(const Type& obj1, const Type& obj2)
 #endif
 
 // *ITERATE helper macro to declare iterator variable
-#if false && defined(NCBI_HAVE_CXX11)
+#if 0 && defined(NCBI_HAVE_CXX11)
 # define ITERATE_VAR(Type) auto
 #else
 # define ITERATE_VAR(Type) Type
