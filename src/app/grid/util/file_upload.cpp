6c436e96af01c32e1f67c7b0db19e1519390eb49
Rafael Sadyrov <sadyrovr@ncbi.nlm.nih.gov>
2014-12-03 14:46:33 +0000

    Removed MD5 storing in attributes, as this will be added to NetStorage itself. JIRA CXX-5454
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@65448 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/app/grid/util/file_upload.cpp
+++ src/app/grid/util/file_upload.cpp
@@ -187,18 +187,9 @@ void CFileUploadApplication::Copy(SContext& ctx, CCgiEntry& entry)
         }
 
         if (read_result == eRW_Eof) {
-            string md5(sum.GetHexSum());
-            netstorage_object.Close();
-
             ctx.json.SetBoolean("success", true);
             ctx.json.SetString("key", netstorage_object.GetLoc());
-            ctx.json.SetString("md5", md5);
-
-            // Ignore exceptions if the underlying storage does not support attributes
-            try {
-                netstorage_object.SetAttribute("md5", md5);
-            }
-            catch (CNetStorageException&) {}
+            ctx.json.SetString("md5", sum.GetHexSum());
             return;
         }
     }
