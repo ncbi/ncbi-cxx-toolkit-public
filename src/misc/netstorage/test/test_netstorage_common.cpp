4b47d9be2e5b411212591f42934f6c45e580de30
Rafael Sadyrov <sadyrovr@ncbi.nlm.nih.gov>
2014-12-22 17:07:16 +0000

    Implemented tests for GetInfo()/CNetStorageObjectInfo. Read/write tests were made more generic (less depend on API used). JIRA CXX-5492
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@65697 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/misc/netstorage/test/test_netstorage_common.cpp
+++ src/misc/netstorage/test/test_netstorage_common.cpp
@@ -23,7 +23,7 @@
  *
  * ===========================================================================
  *
- * Authors:  Dmitry Kazimirov
+ * Authors:  Dmitry Kazimirov, Rafael Sadyrov
  *
  * File Description:  Simple test of NetStorage API (Common parts
  *                    shared between the RPC-based and the direct
@@ -43,62 +43,482 @@
 
 USING_NCBI_SCOPE;
 
-static const string s_TestData("The quick brown fox jumps over the lazy dog.");
 
-static void s_ReadAndCompare(CNetStorageObject netstorage_object)
+// Hierarchy for GetInfo() tests
+
+class IGetInfo : protected CNetStorageObjectInfo
 {
-    char buffer[10];
-    const char* expected = s_TestData.data();
-    size_t remaining_size = s_TestData.length();
+public:
+    IGetInfo(CNetStorageObject object)
+        : CNetStorageObjectInfo(object.GetInfo())
+    {}
 
-    while (!netstorage_object.Eof()) {
-        size_t bytes_read = netstorage_object.Read(buffer, sizeof(buffer));
+    virtual ~IGetInfo() {}
 
-        BOOST_CHECK_MESSAGE(bytes_read <= remaining_size,
-                "Got more data than expected");
-        BOOST_CHECK_MESSAGE(memcmp(buffer, expected, bytes_read) == 0,
-                "Read() returned corrupted data");
-
-        expected += bytes_read;
-        remaining_size -= bytes_read;
+    void Check()
+    {
+        CheckGetLocation();
+        CheckGetObjectLocInfo();
+        CheckGetCreationTime();
+        CheckGetSize();
+        CheckGetStorageSpecificInfo();
+        CheckGetNFSPathname();
+        CheckToJSON();
     }
 
-    netstorage_object.Close();
+private:
+    virtual void CheckGetLocation() = 0;
+    virtual void CheckGetObjectLocInfo() = 0;
+    virtual void CheckGetCreationTime() = 0;
+    virtual void CheckGetSize() = 0;
+    virtual void CheckGetStorageSpecificInfo() = 0;
+    virtual void CheckGetNFSPathname() = 0;
+    virtual void CheckToJSON() = 0;
+};
 
-    BOOST_CHECK_MESSAGE(remaining_size == 0, "Got less data than expected");
+class CGetInfoBase : public IGetInfo
+{
+public:
+    CGetInfoBase(CNetStorageObject object, int location, Uint8 size)
+        : IGetInfo(object), m_Location(location), m_Size(size)
+    {
+    }
+
+private:
+    void CheckGetLocation()
+    {
+        BOOST_CHECK_EQUAL(GetLocation(), m_Location);
+    }
+
+    void CheckGetObjectLocInfo()
+    {
+        if (CJsonNode node = GetObjectLocInfo()) {
+            m_ObjectLocInfoRepr = node.Repr();
+            BOOST_CHECK(m_ObjectLocInfoRepr.size());
+        } else {
+            BOOST_ERROR("!GetObjectLocInfo()");
+        }
+    }
+
+    void CheckGetCreationTime()
+    {
+        BOOST_CHECK(GetCreationTime() != CTime());
+    }
+
+    void CheckGetSize()
+    {
+        BOOST_CHECK_EQUAL(GetSize(), m_Size);
+    }
+
+    void CheckGetStorageSpecificInfo()
+    {
+        if (CJsonNode node = GetStorageSpecificInfo()) {
+            BOOST_CHECK(node.Repr().size());
+        } else {
+            BOOST_FAIL("!GetStorageSpecificInfo()");
+        }
+    }
+
+    void CheckGetNFSPathname()
+    {
+        BOOST_CHECK_THROW(GetNFSPathname(), CNetStorageException);
+    }
+
+    void CheckToJSON()
+    {
+        if (CJsonNode node = ToJSON()) {
+            if (m_ObjectLocInfoRepr.size()) {
+                string repr = node.Repr();
+                BOOST_CHECK(NPOS != NStr::Find(repr, m_ObjectLocInfoRepr));
+            }
+        } else {
+            BOOST_ERROR("!ToJSON()");
+        }
+    }
+
+    int m_Location;
+    Uint8 m_Size;
+    string m_ObjectLocInfoRepr;
+};
+
+class CGetInfoNotFound : public CGetInfoBase
+{
+public:
+    CGetInfoNotFound(CNetStorageObject object, Uint8 = 0U)
+        : CGetInfoBase(object, eNFL_NotFound, 0U)
+    {}
+
+private:
+    void CheckGetCreationTime()
+    {
+        BOOST_CHECK(GetCreationTime() == CTime());
+    }
+
+    void CheckGetStorageSpecificInfo()
+    {
+        BOOST_CHECK(!GetStorageSpecificInfo());
+    }
+};
+
+class CGetInfoNetCache : public CGetInfoBase
+{
+public:
+    CGetInfoNetCache(CNetStorageObject object, Uint8 size)
+        : CGetInfoBase(object, eNFL_NetCache, size)
+    {
+        BOOST_CHECK(object.GetSize() == size);
+    }
+};
+
+class CGetInfoFileTrack : public CGetInfoBase
+{
+public:
+    CGetInfoFileTrack(CNetStorageObject object, Uint8 size)
+        : CGetInfoBase(object, eNFL_FileTrack, size)
+    {
+        BOOST_CHECK(object.GetSize() == size);
+    }
+
+private:
+    void CheckGetNFSPathname()
+    {
+        BOOST_CHECK(GetNFSPathname().size());
+    }
+};
+
+
+// An Extended IReader interface
+class IExtReader : public IReader
+{
+public:
+    virtual void Open() = 0;
+    virtual void Close() = 0;
+    ERW_Result Read(void* buf, size_t count, size_t* bytes_read = 0)
+    {
+        size_t to_read = 0;
+
+        if (buf) {
+            to_read = count < m_Remaining ? count : m_Remaining;
+            memcpy(buf, m_Ptr, to_read);
+            m_Ptr += to_read;
+            m_Remaining -= to_read;
+        }
+
+        if (bytes_read) *bytes_read = to_read;
+        return m_Remaining ? eRW_Success : eRW_Eof;
+    }
+
+    ERW_Result PendingCount(size_t* count)
+    {
+        if (count) {
+            *count = m_Remaining;
+        }
+        return eRW_Success;
+    }
+    virtual size_t Size() const = 0;
+
+protected:
+    const char* m_Ptr;
+    size_t m_Remaining;
+};
+
+// An extended IWriter interface
+struct IExtWriter : public IWriter
+{
+    virtual void Open() = 0;
+    virtual void Close() = 0;
+};
+
+// A reader that is used to read from a string
+class CStrReader : public IExtReader
+{
+public:
+    CStrReader() {}
+    CStrReader(const string& str) : m_Str(str) {}
+    size_t Size() const { return m_Str.size(); }
+    void Open()
+    {
+        m_Ptr = m_Str.data();
+        m_Remaining = m_Str.size();
+    }
+    void Close() {}
+
+protected:
+    string m_Str;
+};
+
+
+// String data to test APIs
+class CStrData : public CStrReader
+{
+public:
+    CStrData()
+        : CStrReader("The quick brown fox jumps over the lazy dog")
+    {}
+
+    void Close()
+    {
+        if (m_Remaining) {
+            BOOST_ERROR("Got less data than expected");
+        }
+    }
+};
+
+// TODO:
+// Implement CBigData (derived from IExtReader)
+// to test API of big data volumes
+
+
+// String reading/writing interface
+class CStrApi
+{
+private:
+    class CReader : public CStrReader
+    {
+    public:
+        CReader(CNetStorageObject o) : m_Object(o) {}
+        void Open() { m_Object.Read(&m_Str); CStrReader::Open(); }
+        void Close() { m_Object.Close(); }
+
+    private:
+        CNetStorageObject m_Object;
+    };
+
+    class CWriter : public IExtWriter
+    {
+    public:
+        CWriter(CNetStorageObject o) : m_Object(o) {}
+        ERW_Result Write(const void* buf, size_t count, size_t* bytes_written = 0)
+        {
+            if (buf) {
+                m_Output.append(static_cast<const char*>(buf), count);
+            }
+
+            if (bytes_written) {
+                *bytes_written = count;
+            }
+            return eRW_Success;
+        }
+        ERW_Result Flush(void)
+        {
+            m_Object.Write(m_Output);
+            m_Output.clear();
+            return eRW_Success;
+        }
+        void Open() { m_Output.clear(); }
+        void Close() { Flush(); m_Object.Close(); }
+
+    private:
+        CNetStorageObject m_Object;
+        string m_Output;
+    };
+
+public:
+    CStrApi(CNetStorageObject o) : m_Reader(o), m_Writer(o) {}
+
+    IExtReader& GetReader()
+    {
+        m_Reader.Open();
+        return m_Reader;
+    }
+
+    IExtWriter& GetWriter()
+    {
+        m_Writer.Open();
+        return m_Writer;
+    }
+
+private:
+    CReader m_Reader;
+    CWriter m_Writer;
+};
+
+
+// TODO:
+// Implement TApi classes to test following CNetStorage methods:
+// 1) GetReader()/GetWriter()
+// 2) Read()/Write() using a buffer
+// 3) GetRWStream();
+
+
+// A helper to read from API/expected data
+struct SReadHelper
+{
+    IExtReader& source;
+    char buf[65537]; // Prime
+    size_t length;
+
+    SReadHelper(IExtReader& s)
+        : source(s),
+          length(0),
+          m_Result(eRW_Success)
+    {
+        source.Open();
+    }
+
+    void Close()
+    {
+        source.Close();
+    }
+
+    bool operator()()
+    {
+        if (length) return true;
+        m_Result = source.Read(buf, sizeof(buf), &length);
+
+        switch (m_Result) {
+        case eRW_Success:
+        case eRW_Eof:
+            return true;
+        default:
+            BOOST_ERROR("Failed to read: " << g_RW_ResultToString(m_Result));
+            return false;
+        }
+    }
+
+    operator bool() { return m_Result == eRW_Success; }
+
+private:
+    ERW_Result m_Result;
+};
+
+// A helper to read from expected and write to object
+struct SWriteHelper
+{
+    SWriteHelper(IExtWriter& dest)
+        : m_Dest(dest),
+          m_Result(eRW_Success)
+    {
+        m_Dest.Open();
+    }
+    void Close()
+    {
+        BOOST_CHECK(m_Dest.Flush() == eRW_Success);
+        m_Dest.Close();
+    }
+
+    template <class TReader>
+    bool operator()(TReader& reader)
+    {
+        while (reader.length) {
+            size_t length = 0;
+            m_Result = m_Dest.Write(reader.buf, reader.length, &length);
+
+            switch (m_Result) {
+            case eRW_Success:
+                reader.length -= length;
+                break;
+            case eRW_Eof:
+                if (reader.length -= length) {
+                    BOOST_ERROR("Wrote less data than expected");
+                    return false;
+                }
+                break;
+            default:
+                BOOST_ERROR("Failed to write to object: " <<
+                        g_RW_ResultToString(m_Result));
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    operator bool() { return m_Result == eRW_Success; }
+
+private:
+    IExtWriter& m_Dest;
+    ERW_Result m_Result;
+};
+
+
+template <class TGetInfo, class TApi>
+static void s_ReadAndCompare(IExtReader& expected, CNetStorageObject object)
+{
+    SReadHelper expected_reader(expected);
+    TApi api(object);
+    SReadHelper actual_reader(api.GetReader());
+
+    do {
+        if (!expected_reader()) break;
+        if (!actual_reader()) break;
+
+        size_t length = min(expected_reader.length, actual_reader.length);
+
+        if (memcmp(expected_reader.buf, actual_reader.buf, length)) {
+            BOOST_ERROR("Read() returned corrupted data");
+            break;
+        }
+
+        expected_reader.length -= length;
+        actual_reader.length -= length;
+    } while (expected_reader && actual_reader);
+
+    expected_reader.Close();
+    actual_reader.Close();
+
+    TGetInfo(object, expected.Size()).Check();
 }
 
-static string s_WriteAndRead(CNetStorageObject new_netstorage_object)
+// This to help preprocessor
+template <class TApi>
+static void s_ReadAndCompareNotFound(IExtReader& expected,
+        CNetStorageObject object)
 {
-    new_netstorage_object.Write(s_TestData);
-
-    // Now close the object and then read it entirely
-    // into a string.
-    new_netstorage_object.Close();
-
-    string data;
-    data.reserve(10);
-
-    new_netstorage_object.Read(&data);
-
-    // Make sure the data is not corrupted.
-    BOOST_CHECK_MESSAGE(data == s_TestData, "Read(string*) is broken");
-
-    return new_netstorage_object.GetLoc();
+    s_ReadAndCompare<CGetInfoNotFound, TApi>(expected, object);
 }
 
-void g_TestNetStorage(CNetStorage netstorage)
+template <class TGetInfo, class TApi>
+static string s_WriteAndRead(IExtReader& source, CNetStorageObject object)
 {
+    SReadHelper source_reader(source);
+    TApi api(object);
+    SWriteHelper dest_writer(api.GetWriter());
+
+    do {
+        if (!source_reader()) break;
+        if (!dest_writer(source_reader)) break;
+    } while (source_reader && dest_writer);
+
+    source_reader.Close();
+    dest_writer.Close();
+
+    s_ReadAndCompare<TGetInfo, TApi>(source, object);
+
+    return object.GetLoc();
+}
+
+// Tests Exists() and Remove() methods
+template <class TNetStorage, class TApi>
+static void s_ExistsAndRemoveTests(const string& id, TNetStorage& netstorage,
+        IExtReader& expected)
+{
+    BOOST_CHECK(netstorage.Exists(id));
+    CNetStorageObject object(netstorage.Open(id));
+    CGetInfoFileTrack(object, expected.Size()).Check();
+    netstorage.Remove(id);
+    BOOST_CHECK(!netstorage.Exists(id));
+    CNetStorageObject no_object(netstorage.Open(id));
+    BOOST_CHECK_THROW(s_ReadAndCompareNotFound<TApi>(expected, no_object),
+            CNetStorageException);
+}
+
+template <class TExpected, class TApi>
+void s_TestNetStorage(CNetStorage netstorage)
+{
+    TExpected data;
+    string not_found(
+            "VHwxYl4jgV6Y8pM4TzhGbhfoROgxMl10Rz9GNlwqch9HHwkoJw0WbEsxAGoT0Cb0EeOG");
+    CGetInfoNotFound(netstorage.Open(not_found)).Check();
+
     // Create a NetStorage object that should to go to NetCache.
-    string object_loc = s_WriteAndRead(
+    string object_loc = s_WriteAndRead<CGetInfoNetCache, TApi>(data,
             netstorage.Create(fNST_Fast | fNST_Movable));
 
     // Now read the whole object using the buffered version
     // of Read(). Verify that the contents of each buffer
     // match the original data.
-    CNetStorageObject orig_netstorage_object = netstorage.Open(object_loc);
-
-    s_ReadAndCompare(orig_netstorage_object);
+    s_ReadAndCompare<CGetInfoNetCache, TApi>(data, netstorage.Open(object_loc));
 
     // Generate a "non-movable" object ID by calling Relocate()
     // with the same storage preferences (so the object should not
@@ -111,41 +531,65 @@ void g_TestNetStorage(CNetStorage netstorage)
     // Verify that the object has disappeared from the "fast" storage.
     CNetStorageObject fast_storage_object =
             netstorage.Open(fast_storage_object_loc);
+    CGetInfoNotFound(fast_storage_object).Check();
 
     // Make sure the relocated object does not exists in the
     // original storage anymore.
-    BOOST_CHECK_THROW(s_ReadAndCompare(fast_storage_object),
+    BOOST_CHECK_THROW(s_ReadAndCompareNotFound<TApi>(data, fast_storage_object),
             CNetStorageException);
 
     // However, the object must still be accessible
     // either using the original ID:
-    s_ReadAndCompare(netstorage.Open(object_loc));
+    s_ReadAndCompare<CGetInfoFileTrack, TApi>(data, netstorage.Open(object_loc));
     // or using the newly generated persistent storage ID:
-    s_ReadAndCompare(netstorage.Open(persistent_loc));
+    s_ReadAndCompare<CGetInfoFileTrack, TApi>(data, netstorage.Open(persistent_loc));
+
+    s_ExistsAndRemoveTests<CNetStorage, TApi>(object_loc, netstorage, data);
 }
 
-void g_TestNetStorageByKey(CNetStorageByKey netstorage)
+void g_TestNetStorage(CNetStorage netstorage)
 {
+    s_TestNetStorage<CStrData, CStrApi>(netstorage);
+}
+
+template <class TExpected, class TApi>
+void s_TestNetStorageByKey(CNetStorageByKey netstorage)
+{
+    TExpected data;
     CRandom random_gen;
 
     random_gen.Randomize();
 
     string unique_key = NStr::NumericToString(
             random_gen.GetRand() * random_gen.GetRand());
+    CGetInfoNotFound(netstorage.Open(unique_key,
+            fNST_Fast | fNST_Movable)).Check();
 
     // Write and read test data using a user-defined key.
-    s_WriteAndRead(netstorage.Open(unique_key, fNST_Fast | fNST_Movable));
+    s_WriteAndRead<CGetInfoNetCache, TApi>(data, netstorage.Open(unique_key,
+            fNST_Fast | fNST_Movable));
 
     // Make sure the object is readable with a different combination of flags.
-    s_ReadAndCompare(netstorage.Open(unique_key,
+    s_ReadAndCompare<CGetInfoNetCache, TApi>(data, netstorage.Open(unique_key,
             fNST_Persistent | fNST_Movable));
 
     // Relocate the object to FileTrack and make sure
     // it can be read from there.
     netstorage.Relocate(unique_key, fNST_Persistent, fNST_Movable);
 
-    s_ReadAndCompare(netstorage.Open(unique_key, fNST_Persistent));
+    s_ReadAndCompare<CGetInfoFileTrack, TApi>(data,
+            netstorage.Open(unique_key, fNST_Persistent));
+
+    s_ExistsAndRemoveTests<CNetStorageByKey, TApi>(unique_key, netstorage, data);
 }
+
+void g_TestNetStorageByKey(CNetStorageByKey netstorage)
+{
+    s_TestNetStorageByKey<CStrData, CStrApi>(netstorage);
+}
+
+// TODO:
+// Add tests for GetAttribute()/SetAttribute()
 
 NCBITEST_AUTO_INIT()
 {
