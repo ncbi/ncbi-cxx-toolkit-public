1e223a44681f392262bcc60c5bb18d00f2424ab9
Dmitry Rudnev <rudnev@ncbi.nlm.nih.gov>
2015-02-05 14:26:08 +0000

    Added rendering for new types of GAP tracks [JIRA:SV-2201]
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@66135 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/snputil/snp_bins.cpp
+++ src/objtools/snputil/snp_bins.cpp
@@ -191,34 +191,43 @@ CRef<NSnpBins::SBinEntry> NSnpBins::GetEntry(const objects::CSeq_annot_Handle& a
 								             int row)
 {
     const CTableFieldHandle<int>      col_pos("pos");
+    const CTableFieldHandle<int>      col_pos_end("pos_end");
     const CTableFieldHandle<double>   col_val("pvalue");
+    const CTableFieldHandle<double>   col_val_synonym("Pvalue");
     const CTableFieldHandle<string>   col_trait("trait");
     const CTableFieldHandle<string>   col_pmids("pmids");
     const CTableFieldHandle<string>   col_rgenes("reportedGenes");
     const CTableFieldHandle<string>   col_mgenes("mappedGenes");
     const CTableFieldHandle<int>      col_snpid("snpId");
-    const CTableFieldHandle<int>      col_type("trackType");
+    const CTableFieldHandle<string>   col_sub_type("trackSubType");
     const CTableFieldHandle<int>      col_clinsigid("clinSigID");
     const CTableFieldHandle<string>   col_hgvs("HGVS");
     const CTableFieldHandle<string>   col_dbgaptext("dbgaptext");
     const CTableFieldHandle<string>   col_context("context");
     const CTableFieldHandle<int>      col_source("source");
+    const CTableFieldHandle<string>   col_population("population");
+    const CTableFieldHandle<int>      col_geneId("geneId");
+    const CTableFieldHandle<string>   col_geneName("geneName");
 
-    int     pos, snpid, ClinSigID;
+    string  trackSubType;
+    int pos, pos_end;
+    int snpid, ClinSigID;
     double  pvalue;
     string  trait, pmids, rgenes, mgenes;
-    string  title, comment;
+    string  title, comment, population;
     string  sHGVS;
-	int		source;
-    string  dbgaptext;
+	int		source, geneId;
+    string  dbgaptext, geneName;
 	string  context;
 
     CRef<NSnpBins::SBinEntry> entry;
     if(col_pos.TryGet(annot, row, pos)) {
         entry.Reset(new NSnpBins::SBinEntry());
-        entry->pos = pos;
+        entry->pos = (TSeqPos)pos;
+        entry->pos_end  = col_pos_end.TryGet(annot, row, pos_end) ? (TSeqPos)pos_end : kInvalidSeqPos;
+        entry->trackSubType = col_sub_type.TryGet(annot, row, trackSubType) ? trackSubType : "";
         entry->snpid  = col_snpid.TryGet(annot, row, snpid) ? (unsigned int)snpid : 0;
-        entry->pvalue = col_val.TryGet(annot, row, pvalue) ? -log10(pvalue) : 0;
+        entry->pvalue = (col_val.TryGet(annot, row, pvalue) || col_val_synonym.TryGet(annot, row, pvalue)) ? -log10(pvalue) : 0;
         entry->trait = col_trait.TryGet(annot, row, trait) ? trait : "";
         entry->pmids  = col_pmids.TryGet(annot, row, pmids) ? pmids : "";
         entry->genes_reported = col_rgenes.TryGet(annot, row, rgenes) ? rgenes : "";
@@ -228,6 +237,9 @@ CRef<NSnpBins::SBinEntry> NSnpBins::GetEntry(const objects::CSeq_annot_Handle& a
         entry->dbgaptext = col_dbgaptext.TryGet(annot, row, dbgaptext) ? dbgaptext : "";
         entry->context = col_context.TryGet(annot, row, context) ? context : "";
         entry->source = col_source.TryGet(annot, row, source) ? source : -1;
+        entry->population = col_population.TryGet(annot, row, population) ? population : "";
+        entry->geneName = col_geneName.TryGet(annot, row, geneName) ? geneName : "";
+        entry->geneId = col_geneId.TryGet(annot, row, geneId) ? geneId : -1;
     }
     return entry;
 }
@@ -264,34 +276,41 @@ void NSnpBins::FindPosIndexRange(const CSeq_annot_Handle& annot,
                           int& pos_index_begin, int& pos_index_end)
 {
     size_t rows = annot.GetSeq_tableNumRows();
-    CTableFieldHandle<int> col_pos("pos");
+    const CTableFieldHandle<int> col_pos("pos");
+    const CTableFieldHandle<string>   col_sub_type("trackSubType");
 
     pos_index_begin = -1;
     pos_index_end   = rows-1;
 
+    string  trackSubType;
+    col_sub_type.TryGet(annot, 0, trackSubType);
+
+    const CTableFieldHandle<int>  col_pos_end(NSnpBins::isGeneMarker(trackSubType) ? "pos_end" : "pos");
+
+
     // Find 'pos_value_from'
-    int i = 0;
-    int j = rows-1;
-    int k = 0;
+    int lower_pos_index_bound = 0;
+    int upper_pos_index_bound = rows-1;
+    int pos_index_k = 0;
     int pos_value_k = 0;
     do {
-        k = (i+j)/2;
-        col_pos.TryGet(annot, k, pos_value_k);
+        pos_index_k = (lower_pos_index_bound + upper_pos_index_bound)/2;
+        col_pos_end.TryGet(annot, pos_index_k, pos_value_k);
         if(pos_value_k < pos_value_from) {
-            pos_index_begin = k;
-            i = k+1;
+            pos_index_begin = pos_index_k;
+            lower_pos_index_bound = pos_index_k+1;
         } else {
-            j = k-1;
+            upper_pos_index_bound = pos_index_k-1;
         }
-    } while (pos_value_k != pos_value_from && i <= j);
+    } while (pos_value_k != pos_value_from && lower_pos_index_bound <= upper_pos_index_bound);
 
     // position start to be inclusive (catch boundary condition)
-    pos_index_begin = (pos_value_from == pos_value_k ? k : pos_index_begin+1);
+    pos_index_begin = (pos_value_from == pos_value_k ? pos_index_k : pos_index_begin+1);
 
     // slide the start down for cases when there are several entries with the same position
     int SlidingBegin(pos_index_begin-1);
     while(SlidingBegin >= 0) {
-        col_pos.TryGet(annot, SlidingBegin, pos_value_k);
+        col_pos_end.TryGet(annot, SlidingBegin, pos_value_k);
         if(pos_value_k < pos_value_from)
             break;
         pos_index_begin = SlidingBegin;
@@ -299,20 +318,20 @@ void NSnpBins::FindPosIndexRange(const CSeq_annot_Handle& annot,
     }
 
     // find the 'pos_value_to' value
-    i=0;
-    j=rows-1;
+    lower_pos_index_bound=0;
+    upper_pos_index_bound=rows-1;
     pos_value_k = 0;
     do {
-        k = (i+j)/2;
-        col_pos.TryGet(annot, k, pos_value_k);
+        pos_index_k = (lower_pos_index_bound+upper_pos_index_bound)/2;
+        col_pos.TryGet(annot, pos_index_k, pos_value_k);
         if (pos_value_k < pos_value_to) {
-            i = k+1;
+            lower_pos_index_bound = pos_index_k+1;
         }
         else {
-            pos_index_end = k;
-            j = k-1;
+            pos_index_end = pos_index_k;
+            upper_pos_index_bound = pos_index_k-1;
         }
-    } while (pos_value_k != pos_value_to && i <= j);
+    } while (pos_value_k != pos_value_to && lower_pos_index_bound <= upper_pos_index_bound);
 
     // increase end to include in range up until the latest entry with "pos".
     size_t SlidingEnd(pos_index_end);
