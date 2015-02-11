76716b17c15ce3a363984ad0878a3986137db9eb
Sergey Satskiy <satskyse@ncbi.nlm.nih.gov>
2015-02-11 19:12:29 +0000

    Update pagkage version at the tag time
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@66213 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ scripts/projects/grid-python/tagprecommit/update_version.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import sys, os.path
+
+if len( sys.argv ) != 2:
+    print >> sys.stderr, "Wrong number of arguments. Usage:\n" \
+                         "create_version.py version"
+    sys.exit( 1 )
+
+version = sys.argv[ 1 ]
+
+fName = os.path.sep.join( [ "internal", "scripts", "common", "lib",
+                            "python", "ncbi", "__init__.py" ] )
+f = open( fName, "r" )
+content = f.read()
+f.close()
+
+
+content = content.replace( "return 'unknown'", "return '" + version + "'" )
+
+f = open( fName, "w" )
+f.write( content )
+f.close()
+
+sys.exit( 0 )
+
