b3ee2c31bdb1f603be75a472d85fcf4e65c7a098
Anton Lavrentiev <lavr@ncbi.nlm.nih.gov>
2015-02-26 20:52:10 +0000

    Bump up LBSMD version to 2.2.3 JIRA: CXX-5678
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@66411 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/connect/ncbi_version.h
+++ src/connect/ncbi_version.h
@@ -53,7 +53,7 @@
 
 #  define   NETDAEMONS_MAJOR        2
 #  define   NETDAEMONS_MINOR        2
-#  define   NETDAEMONS_PATCH        2
+#  define   NETDAEMONS_PATCH        3
 
 #  ifdef NCBI_CXX_TOOLKIT
 #    define NETDAEMONS_VERSION_STR  NCBI_PACKAGE_VERSION_COMPOSE_STR    \
