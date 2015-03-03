7da6fa529f8872e698ae99df8700ca189e5878b1
Rafael Sadyrov <sadyrovr@ncbi.nlm.nih.gov>
2015-03-03 22:03:22 +0000

    Reverted base member renaming done in r455719 that broke offline mode. JIRA CXX-5675
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@66447 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/connect/services/wn_main_loop.cpp
+++ src/connect/services/wn_main_loop.cpp
@@ -283,10 +283,10 @@ bool CWorkerNodeJobContext::IsLogRequested() const
 
 CNetScheduleAdmin::EShutdownLevel CWorkerNodeJobContext::GetShutdownLevel()
 {
-    return m_Impl->CheckJobStatus();
+    return m_Impl->GetShutdownLevel();
 }
 
-CNetScheduleAdmin::EShutdownLevel SWorkerNodeJobContextImpl::CheckJobStatus()
+CNetScheduleAdmin::EShutdownLevel SWorkerNodeJobContextImpl::GetShutdownLevel()
 {
     if (m_StatusThrottler.Approve(CRequestRateControl::eErrCode))
         try {
@@ -311,7 +311,7 @@ CNetScheduleAdmin::EShutdownLevel SWorkerNodeJobContextImpl::CheckJobStatus()
             default:
                 // The worker node does not "own" the job any longer.
                 ERR_POST("Cannot proceed with job processing: job '" <<
-                        m_Job.job_id << "' has changed its status to '" <<
+                        m_Job.job_id << "' changed status to '" <<
                         CNetScheduleAPI::StatusToString(job_status) << "'.");
                 MarkJobAsLost();
                 return CNetScheduleAdmin::eShutdownImmediate;
