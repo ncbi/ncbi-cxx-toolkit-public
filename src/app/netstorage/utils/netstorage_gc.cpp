f7be91dfd33da8561ff22868801dc5cc1d6fdb75
Sergey Satskiy <satskyse@ncbi.nlm.nih.gov>
2015-03-31 15:19:35 +0000

    Use the modern way of creating CDirectNetStorage which supports encrypted filetrack keys. JIRA: CXX-5671
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@66896 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/app/netstorage/utils/netstorage_gc.cpp
+++ src/app/netstorage/utils/netstorage_gc.cpp
@@ -185,15 +185,10 @@ void  CNetStorageGCApp::x_RemoveObjects(const vector<string> &  locators,
                                         CNetStorageGCDatabase &  db,
                                         bool  dryrun)
 {
-    /* TODO: Redo using IRegistry and ctor
     CDirectNetStorage net_storage(
-                        registry,
+                        GetConfig(),
                         CNetICacheClient(CNetICacheClient::eAppRegistry),
                         NULL, kEmptyStr);
-     */
-    CDirectNetStorage net_storage(g_CreateNetStorage(
-                        CNetICacheClient(CNetICacheClient::eAppRegistry),
-                        kEmptyStr));
     CRef<CRequestContext>   ctx;
 
     for (vector<string>::const_iterator  k = locators.begin();
