6c4c281c350cb0064cb0ea237f51514d5f111c38
Eugene Vasilchenko <vasilche@ncbi.nlm.nih.gov>
2015-04-10 15:24:58 +0000

    Use write lock for loading blob by id.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@67027 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objmgr/data_source.cpp
+++ src/objmgr/data_source.cpp
@@ -387,7 +387,7 @@ CDataSource::GetSeq_entry_Lock(const CBlobIdKey& blob_id)
 {
     TSeq_entry_Lock ret;
     {{
-        TMainLock::TReadLockGuard guard(m_DSMainLock);
+        TMainLock::TWriteLockGuard guard(m_DSMainLock);
         ret.first = m_Loader->GetBlobById(blob_id);
         if ( ret.first ) {
             x_SetLock(ret.second, ConstRef(&ret.first->GetTSE_Info()));
