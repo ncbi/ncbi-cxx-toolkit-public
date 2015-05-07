daa53a7eee0026402ed2956d36023b8d2723b3ac
Sergiy Gotvyanskyy <gotvyans@ncbi.nlm.nih.gov>
2015-05-07 15:57:28 +0000

    JIRA: SQD-2261
    Created new class to write sequence as set of FASTA files.
    
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@67320 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ include/objtools/writers/fasta_writer.hpp
@@ -0,0 +1,106 @@
+/*  $Id$
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Authors:  Sergiy Gotvyanskyy
+ *
+ * File Description:  Write object as a hierarchy of FASTA objects
+ *
+ */
+
+#ifndef __OBJTOOLS_WRITERS_AGP_WRITE_HPP__
+#define __OBJTOOLS_WRITERS_AGP_WRITE_HPP__
+
+#include <corelib/ncbistd.hpp>
+
+BEGIN_NCBI_SCOPE
+
+BEGIN_SCOPE(objects)
+
+class CSeq_entry;
+class CBioseq;
+class CSeq_loc;
+class CFastaOstream;
+class CBioseq_Handle;
+class CSeq_entry_Handle;
+
+// this would generate CDS and genes into own files
+// up to three files created: 
+// filename_without_ext.fsa for nucleotide sequences
+// filename_without_ext_cds_from_genomic.fna for CDS's
+// filename_without_ext_rna_from_genomic.fna for RNA's
+class /*NCBI_XOBJUTIL_EXPORT*/ CFastaOstreamEx
+{
+public:
+    CFastaOstreamEx(const string& dir, const string& filename_without_ext);
+    ~CFastaOstreamEx();
+
+    enum E_FileSection
+    {
+        eFS_nucleotide,
+        eFS_CDS,
+        eFS_RNA
+    };
+    typedef int TFlags; ///< binary OR of CFastaOstream::EFlags
+
+    void Write(const CSeq_entry_Handle& handle, const CSeq_loc* location = 0);
+
+    inline
+    void SetFlags(TFlags flags)
+    {
+        m_Flags = flags;
+    }
+
+    inline
+    TFlags GetFlags() const
+    {
+        return m_Flags;
+    }
+protected:
+    struct TStreams
+    {
+        string                  m_filename;
+        auto_ptr<CNcbiOstream>  m_ostream;
+        auto_ptr<CFastaOstream> m_fasta_stream;
+    };
+
+    void x_Write(const CBioseq_Handle& handle, const CSeq_loc* location);
+    // override these method to change filename policy and output stream
+    virtual void x_GetNewFilename(string& filename, E_FileSection sel);
+    virtual auto_ptr<CNcbiOstream> x_GetOutputStream(const string& filename, E_FileSection sel);
+    virtual auto_ptr<CFastaOstream> x_GetFastaOstream(CNcbiOstream& ostr, E_FileSection sel);
+
+    TStreams& x_GetStream(E_FileSection sel);
+protected:
+    string m_dir;
+    string m_filename_without_ext;
+    TFlags m_Flags;
+    vector<TStreams> m_streams;
+};
+
+END_SCOPE(objects)
+END_NCBI_SCOPE
+
+
+#endif // end of "include-guard"
---
+++ src/objtools/writers/fasta_writer.cpp
@@ -0,0 +1,173 @@
+/*  $Id$
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Authors:  Sergiy Gotvyanskyy
+ *
+ * File Description:  Write object as a hierarchy of FASTA objects
+ *
+ */
+
+#include <ncbi_pch.hpp>
+#include <objtools/writers/fasta_writer.hpp>
+
+#include <corelib/ncbifile.hpp>
+
+#include <objmgr/util/sequence.hpp>
+//#include <objmgr/object_manager.hpp>
+//#include <objmgr/scope.hpp>
+
+BEGIN_NCBI_SCOPE
+
+BEGIN_SCOPE(objects)
+
+CFastaOstreamEx::CFastaOstreamEx(const string& dir, const string& filename_without_ext)
+: m_filename_without_ext(filename_without_ext),
+  m_Flags(-1)
+{        
+    m_dir = CDir::AddTrailingPathSeparator(dir);
+}
+
+CFastaOstreamEx::~CFastaOstreamEx()
+{
+}
+
+void CFastaOstreamEx::x_GetNewFilename(string& filename, E_FileSection sel)
+{
+    filename = m_dir;
+    filename += m_filename_without_ext;
+    const char* suffix = 0;
+    switch (sel)
+    {
+    case eFS_nucleotide:
+        suffix = "";
+        break;
+    case eFS_CDS:
+        suffix = "_cds_from_genomic";
+        break;
+    case eFS_RNA:
+        suffix = "_rna_from_genomic";
+        break;
+    default:
+        _ASSERT(0);
+    }
+    filename.append(suffix);
+    const char* ext = 0;
+    switch (sel)
+    {
+    case eFS_nucleotide:
+        ext = ".fsa";
+        break;
+    case eFS_CDS:
+    case eFS_RNA:
+        ext = ".fna";
+        break;          
+    default:
+        _ASSERT(0);
+    }
+    filename.append(ext);
+}
+
+auto_ptr<CNcbiOstream> CFastaOstreamEx::x_GetOutputStream(const string& filename, E_FileSection sel)
+{
+    auto_ptr<CNcbiOstream> ostr(new CNcbiOfstream(filename.c_str()));
+    return ostr;
+}
+
+auto_ptr<CFastaOstream> CFastaOstreamEx::x_GetFastaOstream(CNcbiOstream& ostr, E_FileSection sel)
+{
+    auto_ptr<CFastaOstream> fstr(new CFastaOstream(ostr));
+    if (m_Flags != -1)
+      fstr->SetAllFlags(m_Flags);
+    return fstr;
+}
+
+CFastaOstreamEx::TStreams& CFastaOstreamEx::x_GetStream(E_FileSection sel)
+{
+    if (m_streams.size() <= sel)
+    {
+        m_streams.resize(sel + 1);
+    }
+    TStreams& res = m_streams[sel];
+    if (res.m_filename.empty())
+    { 
+        x_GetNewFilename(res.m_filename, sel);
+    }
+    if (res.m_ostream.get() == 0)
+    {
+        res.m_ostream = x_GetOutputStream(res.m_filename, sel);
+    }
+    if (res.m_fasta_stream.get() == 0)
+    {
+        res.m_fasta_stream = x_GetFastaOstream(*res.m_ostream, sel);
+    }
+    return res;
+}
+
+void CFastaOstreamEx::Write(const CSeq_entry_Handle& handle, const CSeq_loc* location)
+{
+    for (CBioseq_CI it(handle); it; ++it) {
+        if (location) {
+            CSeq_loc loc2;
+            loc2.SetWhole().Assign(*it->GetSeqId());
+            int d = sequence::TestForOverlap
+                (*location, loc2, sequence::eOverlap_Interval,
+                kInvalidSeqPos, &handle.GetScope());
+            if (d < 0) {
+                continue;
+            }
+        }
+        x_Write(*it, location);
+    }
+}
+
+void CFastaOstreamEx::x_Write(const CBioseq_Handle& handle, const CSeq_loc* location)
+{
+    E_FileSection sel = eFS_nucleotide;
+    if (handle.CanGetInst_Mol())
+    {
+        CSeq_inst::EMol mol = handle.GetInst_Mol();
+        switch (mol)
+        {
+        case ncbi::objects::CSeq_inst_Base::eMol_dna:
+            sel = eFS_RNA;
+            break;
+        case ncbi::objects::CSeq_inst_Base::eMol_rna:
+            sel = eFS_RNA;
+            break;
+        case ncbi::objects::CSeq_inst_Base::eMol_aa:
+            sel = eFS_CDS;
+            break;
+        case ncbi::objects::CSeq_inst_Base::eMol_na:
+            break;
+        default:
+            break;
+        }
+    }
+    TStreams& res = x_GetStream(sel);
+    res.m_fasta_stream->Write(handle, location);
+}
+
+END_SCOPE(objects)
+END_NCBI_SCOPE
