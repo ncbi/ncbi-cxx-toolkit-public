01211c10880eab0f6cf1abe8861b45f18b70fcaa
fukanchi <fukanchi@ncbi.nlm.nih.gov>
2015-05-08 19:21:27 +0000

    Support updated grid-python structure. JIRA CXX-5915
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@67337 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/app/netschedule/test/make_ncbi_grid_module_tree.sh
+++ src/app/netschedule/test/make_ncbi_grid_module_tree.sh
@@ -42,46 +42,27 @@ version="`perl -wne 'm/VERSION\s*=\s*(.*)/s && print $1' "$build_info_file"`"
 
 bin_dir="$toolkit_dir/bin"
 
+repo='https://svn.ncbi.nlm.nih.gov/repos/toolkit'
+
+(cd `dirname $target_dir` && \
+    svn export \
+        --quiet \
+        -r"{$version}" \
+        "$repo/trunk/internal/scripts/common/lib/python/grid-python" \
+        "${target_dir}" )
+
 if test -f "$bin_dir/grid_cli.gz"; then
-    mkdir "$target_dir" || exit 3
+    [ -d "$target_dir" ] || mkdir "$target_dir"
     cp -p "$bin_dir/grid_cli.gz" "$target_dir"
     gunzip "$target_dir/grid_cli.gz"
 elif test -f "$bin_dir/grid_cli.bz2"; then
-    mkdir "$target_dir" || exit 3
+    [ -d "$target_dir" ] || mkdir "$target_dir"
     cp -p "$bin_dir/grid_cli.bz2" "$target_dir"
     bunzip2 "$target_dir/grid_cli.bz2"
 elif test -f "$bin_dir/grid_cli"; then
-    mkdir "$target_dir" || exit 3
+    [ -d "$target_dir" ] || mkdir "$target_dir"
     cp -p "$bin_dir/grid_cli" "$target_dir"
 else
     error "cannot find grid_cli binary in '$bin_dir'"
 fi
 
-repo='https://svn.ncbi.nlm.nih.gov/repos/toolkit'
-
-(cd "$target_dir" && \
-    svn export \
-        --quiet \
-        -r"{$version}" \
-        "$repo/trunk/internal/scripts/common/lib/python/ncbi")
-
-cat <<EOF > "$target_dir/__init__.py"
-import sys
-import os.path
-
-module_path = __path__[0]
-
-def version():
-    import re
-    match = re.search('\d+(_\d+)+$', module_path)
-    if match:
-        return match.group(0).replace('_', '.')
-    return 'unknown'
-
-def get_grid_cli_pathname():
-    return __builtins__['ncbi_grid_cli_pathname']
-
-__builtins__['ncbi_grid_version_module'] = sys.modules[__name__]
-__builtins__['ncbi_grid_cli_pathname'] = os.path.abspath(
-        os.path.join(module_path, 'grid_cli'))
-EOF
