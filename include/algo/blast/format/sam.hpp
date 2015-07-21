d874612629709ddb8db9544b26cf7f35553be74e
Amelia Fong <fongah2@ncbi.nlm.nih.gov>
2015-07-21 13:38:45 +0000

    Add blast sam format class, JIRA:SB-1512
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@68207 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ include/algo/blast/format/sam.hpp
@@ -0,0 +1,63 @@
+/* $Id
+* ===========================================================================
+*
+*                            PUBLIC DOMAIN NOTICE
+*               National Center for Biotechnology Information
+*
+*  This software/database is a "United States Government Work" under the
+*  terms of the United States Copyright Act.  It was written as part of
+*  the author's offical duties as a United States Government employee and
+*  thus cannot be copyrighted.  This software/database is freely available
+*  to the public for use. The National Library of Medicine and the U.S.
+*  Government have not placed any restriction on its use or reproduction.
+*
+*  Although all reasonable efforts have been taken to ensure the accuracy
+*  and reliability of the software and data, the NLM and the U.S.
+*  Government do not and cannot warrant the performance or results that
+*  may be obtained by using this software or data. The NLM and the U.S.
+*  Government disclaim all warranties, express or implied, including
+*  warranties of performance, merchantability or fitness for any particular
+*  purpose.
+*
+*  Please cite the author in any work or product based on this material.
+*
+* ===========================================================================
+*
+* Author: Amelia Fong
+*
+* ===========================================================================
+*/
+
+/// @file: sam.hpp
+/// Formatting of pairwise sequence alignments in SAM form.
+
+#ifndef ALGO_BLAST_FORMAT___SAM_HPP
+#define ALGO_BLAST_FORMAT___SAM_HPP
+
+#include <objtools/format/sam_formatter.hpp>
+
+BEGIN_NCBI_SCOPE
+BEGIN_SCOPE(objects)
+
+class NCBI_XBLASTFORMAT_EXPORT CBlast_SAM_Formatter : public CSAM_Formatter
+{
+public:
+	CBlast_SAM_Formatter (CNcbiOstream& out,
+			              CScope& scope,
+			              const string & custom_spec,
+			              const CSAM_Formatter::SProgramInfo & info,
+			              bool isVDB);
+
+	void Print(const CSeq_align_set &  aln);
+
+private :
+	void x_ProcessCustomSpec(const string & custom_spec, const CSAM_Formatter::SProgramInfo & info);
+
+	bool m_isVDB;
+	unsigned int m_refRow;
+
+};
+
+END_SCOPE(objects)
+END_NCBI_SCOPE
+#endif /* ALGo_BLAST_FORMAT___SAM_HPP */
---
+++ src/algo/blast/format/sam.cpp
@@ -0,0 +1,78 @@
+/* $Id:
+* ===========================================================================
+*
+*                            PUBLIC DOMAIN NOTICE
+*               National Center for Biotechnology Information
+*
+*  This software/database is a "United States Government Work" under the
+*  terms of the United States Copyright Act.  It was written as part of
+*  the author's offical duties as a United States Government employee and
+*  thus cannot be copyrighted.  This software/database is freely available
+*  to the public for use. The National Library of Medicine and the U.S.
+*  Government have not placed any restriction on its use or reproduction.
+*
+*  Although all reasonable efforts have been taken to ensure the accuracy
+*  and reliability of the software and data, the NLM and the U.S.
+*  Government do not and cannot warrant the performance or results that
+*  may be obtained by using this software or data. The NLM and the U.S.
+*  Government disclaim all warranties, express or implied, including
+*  warranties of performance, merchantability or fitness for any particular
+*  purpose.
+*
+*  Please cite the author in any work or product based on this material.
+*
+* ===========================================================================
+*
+* Author: Amelia Fong
+*
+* ===========================================================================
+*/
+
+/// @file: tabular.cpp
+/// Formatting of pairwise sequence alignments in tabular form.
+/// One line is printed for each alignment with tab-delimited fielalnVec.
+
+#include <ncbi_pch.hpp>
+
+#include <algo/blast/format/sam.hpp>
+#include <serial/iterator.hpp>
+
+BEGIN_NCBI_SCOPE
+USING_SCOPE(objects);
+
+CBlast_SAM_Formatter::CBlast_SAM_Formatter(CNcbiOstream& out, CScope& scope,
+			                               const string & custom_spec,
+			                               const CSAM_Formatter::SProgramInfo & info,
+			                               bool isVDB) :
+			                               CSAM_Formatter(out,scope),
+			                               m_isVDB(isVDB),
+			                               m_refRow(isVDB? 1: 0)
+{
+	CSAM_Formatter::SetFlag(CSAM_Formatter::fSAM_PlainSeqIds);
+	x_ProcessCustomSpec(custom_spec, info);
+}
+
+void CBlast_SAM_Formatter::Print(const CSeq_align_set &  aln)
+{
+	CSAM_Formatter::Print(aln, m_refRow);
+}
+
+void CBlast_SAM_Formatter::x_ProcessCustomSpec(const string & custom_spec,
+		                                       const CSAM_Formatter::SProgramInfo & info)
+{
+	vector<string> format_tokens;
+	NStr::Tokenize(custom_spec, " ", format_tokens);
+	CSAM_Formatter::SetProgram(info);
+	ITERATE (vector<string>, iter, format_tokens) {
+		if("SR" == *iter){
+			m_refRow = 0;
+		} else if ("QR" == *iter ) {
+			m_refRow = 1;
+		} else if ("FA" == *iter) {
+			CSAM_Formatter::UnsetFlag(CSAM_Formatter::fSAM_PlainSeqIds);
+		}
+	}
+}
+
+
+END_NCBI_SCOPE
