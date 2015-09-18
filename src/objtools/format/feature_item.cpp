5f71901224d0ed555b6b4c5e74ba968d570a4f89
Jonathan Kans <kans@ncbi.nlm.nih.gov>
2015-09-18 01:24:30 +0000

    JIRA:SQD-3442 Reverted kludge - eSQ_metagenomic flag determines text to use for non-redundancy check
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@68940 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/format/feature_item.cpp
+++ src/objtools/format/feature_item.cpp
@@ -5760,11 +5760,7 @@ void CSourceFeatureItem::x_FormatNoteQuals(CFlatFeature& ff) const
             DO_NOTE(frequency);
         }
 
-        if (s_IsPresentOnNoteQuals(qvec, "metagenomic")) {
-            x_FormatNoteQual(eSQ_metagenome_source, "derived from metagenome", qvec);
-        } else {
-            x_FormatNoteQual(eSQ_metagenome_source, "metagenomic; derived from metagenome", qvec);
-        }
+        x_FormatNoteQual(eSQ_metagenome_source, "derived from metagenome", qvec);
         
         DO_NOTE(genotype);
         x_FormatNoteQual(eSQ_plastid_name, "plastid", qvec);
