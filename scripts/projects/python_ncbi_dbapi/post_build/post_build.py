616f19979438bde91f0e221110daf0a46f5628b7
Aaron Ucko <ucko@ncbi.nlm.nih.gov>
2015-09-21 18:29:09 +0000

    python_ncbi_dbapi: Rework post-build procedure to produce wheels for decentralized installation rather than tarballs for centralized installation.  JIRA: CXX-7331.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@68966 78c7ea69-d796-4a43-9a09-de51944f1b03

--- scripts/projects/python_ncbi_dbapi/post_build/post_build.py
+++ scripts/projects/python_ncbi_dbapi/post_build/post_build.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 # $Id$
 
-import glob, os, shutil, sys
+import glob, os, shutil, subprocess, sys
 
-(bin_dir, lib_dir, install_dir, version) = sys.argv[1:]
-common_stems  = ('python_ncbi_dbapi', 'ncbi_xdbapi_?t*')
+(bin_dir, lib_dir, install_dir, src_dir) = sys.argv[1:]
+common_stems  = ('python_ncbi_dbapi', 'ncbi_xdbapi_*')
 unix_stems    = ('dbapi', 'dbapi_driver', 'ct_ftds64', 'tds_ftds64',
                  'xconne?t', 'xutil', 'xncbi')
 windows_stems = ('ncbi_core', 'ncbi_dbapi', 'ncbi_dbapi_driver')
+installations = ('2.7env',)
 
 os.mkdir(install_dir + '/lib')
 if os.path.exists(lib_dir + '/python_ncbi_dbapi.so'):
@@ -21,7 +22,36 @@ for s in stems:
     for x in glob.glob(template % s):
         shutil.copy(x, install_dir + '/lib/' + os.path.basename(x))
 
-# Build a tarball for system-wide installation only on Linux.
+# Build wheels only on Linux (currently the only supported platform anyway).
 if sys.platform == 'linux2':
-    build_tarball = os.path.dirname(sys.argv[0]) + '/build_tarball.sh'
-    os.execv(build_tarball, [build_tarball, install_dir, version])
+    proj_src_dir = src_dir + '/src/dbapi/lang_bind/python'
+    tmp_dir = install_dir + '/tmp'
+    cpp_build = os.path.dirname(os.path.abspath(lib_dir))
+    #build_tarball = os.path.dirname(sys.argv[0]) + '/build_tarball.sh'
+    #os.execv(build_tarball, [build_tarball, install_dir, version])
+    for x in installations:
+        py_bin_dir = '/opt/python-' + x + '/bin'
+        if os.path.isdir(tmp_dir):
+            shutil.rmtree(tmp_dir)
+        os.mkdir(tmp_dir)
+        if 0 != subprocess.call((py_bin_dir + '/pip', 'install', '-t', tmp_dir,
+                                 'packit')):
+            raise RuntimeError('Error installing packit')
+        for x in ('python_ncbi_dbapi.cpp', 'python_ncbi_dbapi.hpp',
+                  'setup.py'):
+            shutil.copy(proj_src_dir + '/' + x, tmp_dir + '/' + x)
+        shutil.copytree(proj_src_dir + '/pythonpp', tmp_dir + '/pythonpp')
+        orig_cwd = os.getcwd()
+        os.chdir(tmp_dir)
+        if 0 != subprocess.call((py_bin_dir + '/python', 'setup.py',
+                                 '--with-c++-build=' + cpp_build,
+                                 '--command-packages', 'packit',
+                                 'bdist_wheel')):
+            raise RuntimeError('Error running setup.py')
+        os.chdir(orig_cwd)
+        wheels = glob.glob(tmp_dir + '/dist/*.whl')
+        if len(wheels) != 1:
+            raise RuntimeError('Expected to get exactly 1 wheel, but got '
+                               + len(wheels))
+        shutil.move(wheels[0], install_dir)
+        shutil.rmtree(tmp_dir)
