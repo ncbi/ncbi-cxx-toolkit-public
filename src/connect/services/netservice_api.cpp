53777f71559c935462bbbde1fc657cf17292f406
Rafael Sadyrov <sadyrovr@ncbi.nlm.nih.gov>
2015-10-08 15:54:49 +0000

    Reverted now-unnecessary r477767 back. JIRA CXX-7300
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@69196 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/connect/services/netservice_api.cpp
+++ src/connect/services/netservice_api.cpp
@@ -1016,6 +1016,7 @@ void SNetServiceImpl::IterateUntilExecOK(const string& cmd,
     unsigned number_of_servers = 0;
     unsigned ns_with_submits_disabled = 0;
     unsigned servers_throttled = 0;
+    bool blob_not_found = false;
 
     STimeout* timeout = retry_count <= 0 && !m_UseSmartRetries ?
             NULL : &m_ServerPool->m_FirstServerTimeout;
@@ -1037,7 +1038,8 @@ void SNetServiceImpl::IterateUntilExecOK(const string& cmd,
             if (retry_count <= 0 && !m_UseSmartRetries)
                 throw;
             if (ex.GetErrCode() == CNetCacheException::eBlobNotFound) {
-                throw;
+                blob_not_found = true;
+                skip_server = true;
             } else if (error_handling == eRethrowServerErrors)
                 throw;
             else
@@ -1120,6 +1122,10 @@ void SNetServiceImpl::IterateUntilExecOK(const string& cmd,
                         "]: all servers are throttled.");
             }
             if (retry_count <= 0 || servers_to_retry.empty()) {
+                if (blob_not_found) {
+                    NCBI_THROW_FMT(CNetCacheException, eBlobNotFound,
+                            "Cannot execute ["  << cmd << "]: blob not found.");
+                }
                 NCBI_THROW_FMT(CNetSrvConnException, eSrvListEmpty,
                         "Unable to execute [" << cmd <<
                         "] on any of the discovered servers.");
