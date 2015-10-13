2f563a1d1e4fe3897627b0e17bc869d1e4375b20
zakharov <zakharov@ncbi.nlm.nih.gov>
2015-10-13 21:05:24 +0000

    JIRA:CXX-6082 debugging release process
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@69230 78c7ea69-d796-4a43-9a09-de51944f1b03

--- scripts/projects/testres-kernel/copy_modules.py
+++ scripts/projects/testres-kernel/copy_modules.py
@@ -2,6 +2,7 @@
 import os
 import shutil
 import sys
+import re
 
 MODULES_PATH = "src/internal/cppcore/testres/modules"
 MODULES_DEST_PATH = "modules"
@@ -12,6 +13,9 @@ try:
     src_dir = sys.argv[1]
     bin_dir = sys.argv[2]
     install_dir = sys.argv[3]
+
+    with open("/home/zakharov/pr_args.txt", "w") as f:
+        f.writelines([src_dir, "\n", bin_dir, "\n", install_dir])
 
     #locate all modules
     modules_dir = os.path.join(src_dir, MODULES_PATH)
@@ -37,32 +41,48 @@ try:
 
             gen_conf_path = None
             def_run_path = None
+            mod_dir_path = None         # TestRes module support directory to be copied into the release
 
             if (os.path.exists(gen_conf_path_bin) and os.path.exists(def_run_path_bin)):
+                # taking modules from binary
                 gen_conf_path = gen_conf_path_bin
                 def_run_path = def_run_path_bin
+                mod_dir_path = module_src_dir
             else:
+                # modules in src
                 if os.path.exists(gen_conf_path_src) and os.path.exists(def_run_path_src):
                     gen_conf_path = gen_conf_path_src
                     def_run_path = def_run_path_src
+                    mod_dir_path = module_src_dir
 
                     #delete Id svn keyword
-                    for fname in [gen_conf_path, gen_conf_path_src]:
-                        result = []
-                        modified = False
+                    for root, dirs, files in os.walk(mod_dir_path):
+                        for fname in files:
+                            fpath = os.path.join(root, fname)
+                            result = []
+                            modified = False
 
-                        with open(fname) as f:
-                            for line in f:
-                                if "$Id" not in line:
-                                    result.append(line)
-                                else:
-                                    modified = True
+                            with open(fpath) as f:
+                                for line in f:
+                                    if "$Id" not in line:
+                                        result.append(line)
+                                    else:
+                                        modified = True
 
-                        if modified:
-                            with open(fname, "w") as f:
-                                f.writelines(result)
+                            if modified:
+                                with open(fname, "w") as f:
+                                    f.writelines(result)
 
             if gen_conf_path and def_run_path:
+                if mod_dir_path:
+                    re_fileskip = re.compile(r"(Makefile)|\.((cpp)|(hpp)|(in)|(app)|(lib))$")
+                    # filtered shutil.copytree
+                    for root, dirs, files in os.walk(mod_dir_path):
+                        for directory in dir:
+                            os.makedirs(os.path.join(root, directory))
+                        file_pathes = [x for x in files if not re_fileskip.match(x)]
+                    shutil.copytree(mod_dir_path, module_dest_dir)
+
                 module_dest_dir = os.path.join(modules_dest_dir, dir_entry)
                 os.makedirs(module_dest_dir)
                 gen_conf_dest_path = os.path.join(
