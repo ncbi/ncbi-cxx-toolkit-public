61501142a80a55c937796e1bc94b5802c0f7b6d2
Denis Vakatov <vakatov@ncbi.nlm.nih.gov>
2015-10-13 16:49:18 +0000

    Move scripts and projects for unsupported compilers and dev.tools to the graveyard: msvc9, msvc10, vs2012, xCode, WorkShop, Insure. JIRA:CXX-7428
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@69227 78c7ea69-d796-4a43-9a09-de51944f1b03

--- compilers/unix/cxx_filter.WorkShop59.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-# $Id$
-# Authors:  Denis Vakatov, NCBI; Aaron Ucko, NCBI
-#
-# Temporary wrapper for the new central WorkShop redundant warning filter.
-
-exec `dirname $0`/cxx_filter.WorkShop.sh
