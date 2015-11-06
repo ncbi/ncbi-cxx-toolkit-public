47e4800fe09ee86f2a0a0997e219c8abf7cb0450
rackerst <rackerst@ncbi.nlm.nih.gov>
2015-11-06 16:04:53 +0000

    JIRA SB-1562 Fix compile warnings
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@69666 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/blast/seqdb_reader/seqdbcommon.cpp
+++ src/objtools/blast/seqdb_reader/seqdbcommon.cpp
@@ -792,12 +792,12 @@ void SeqDB_ReadBinaryGiList(const string & fname, vector<TGi> & gis)
     Uint4 * beginp = (Uint4*) mfile.GetPtr();
     Uint4 * endp   = (Uint4*) (((char*)mfile.GetPtr()) + mfile.GetSize());
 
-    int num_gis = (int) (endp - beginp - 2);
+    Uint4 num_gis = (int) (endp - beginp - 2);
 
     gis.clear();
 
-    if (((endp - beginp) < 2)
-            ||  (beginp[0] != -1)
+    if (((endp - beginp) < 2U)
+            ||  (beginp[0] != 0xFFFFFFFFU)
             ||  (SeqDB_GetStdOrd(beginp + 1) != num_gis)) {
         NCBI_THROW(CSeqDBException,
                    eFileErr,
@@ -869,12 +869,12 @@ void SeqDB_ReadMemoryGiList(const char * fbeginp,
         Uint4* bbeginp = (Uint4*) fbeginp;
         Uint4* bendp = (Uint4*) fendp;
 
-        int num_gis = (int) (bendp - bbeginp - 2);
+        Uint4 num_gis = (int) (bendp - bbeginp - 2);
 
         gis.clear();
 
-        if (((bendp - bbeginp) < 2)
-                ||  (bbeginp[0] != -1)
+        if (((bendp - bbeginp) < 2U)
+                ||  (bbeginp[0] != 0xFFFFFFFFU)
                 ||  (SeqDB_GetStdOrd(bbeginp + 1) != num_gis)) {
             NCBI_THROW(CSeqDBException,
                        eFileErr,
