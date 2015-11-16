0459ebaa9b261b8168afcaf40f44739e1b4e3dd1
Eugene Vasilchenko <vasilche@ncbi.nlm.nih.gov>
2015-11-16 17:10:16 +0000

    Remember non-WGS gis. JIRA: GB-5388
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@69820 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/sra/data_loaders/wgs/id2wgs_impl.cpp
+++ src/sra/data_loaders/wgs/id2wgs_impl.cpp
@@ -690,24 +690,6 @@ CID2WGSProcessor_Impl::ResolveGeneral(const CDbtag& dbtag)
 CID2WGSProcessor_Impl::SWGSSeqInfo
 CID2WGSProcessor_Impl::ResolveGi(TGi gi)
 {
-    if ( 0 ) {
-        CWGSGiResolver::TSeqInfoList accs = m_GiResolver->FindAll(gi, m_Mgr);
-        ITERATE ( CWGSGiResolver::TSeqInfoList, acc_it, accs ) {
-            SWGSSeqInfo seq;
-            seq.m_WGSAcc = acc_it->wgs_acc;
-            seq.m_IsWGS = true;
-            seq.m_ValidWGS = true;
-            seq.m_RowId = acc_it->row;
-            if ( acc_it->type == 'P' ) {
-                seq.SetProtein();
-            }
-            else {
-                seq.SetContig();
-            }
-            return seq;
-        }
-        return SWGSSeqInfo();
-    }
     CWGSGiResolver::TAccessionList accs = m_GiResolver->FindAll(gi);
     ITERATE ( CWGSGiResolver::TAccessionList, acc_it, accs ) {
         if ( CWGSDb wgs_db = GetWGSDb(*acc_it) ) {
@@ -741,6 +723,9 @@ CID2WGSProcessor_Impl::ResolveGi(TGi gi)
                 return seq;
             }
         }
+    }
+    if ( !accs.empty() ) {
+        m_GiResolver->SetNonWGSGi(gi);
     }
     return SWGSSeqInfo();
 }
