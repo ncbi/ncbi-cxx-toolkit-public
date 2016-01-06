99f546c523a815c266d6eb27465f3eb64912e5cd
Aaron Ucko <ucko@ncbi.nlm.nih.gov>
2016-01-06 15:50:59 +0000

    python_ncbi_dbapi setup.py: Advance version to 1.16.1.  JIRA: CXX-7331.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@70583 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/dbapi/lang_bind/python/setup.py
+++ src/dbapi/lang_bind/python/setup.py
@@ -90,7 +90,7 @@ for x in libs + ['ncbi_xdbapi_*' + mfv('DLL'), '*_ftds??']:
 
 setup(
     name         = 'python_ncbi_dbapi',
-    version      = '1.16.0',
+    version      = '1.16.1',
     description  = 'NCBI DBAPI wrapper',
     author       = 'Aaron Ucko',
     author_email = 'ucko@ncbi.nlm.nih.gov',
