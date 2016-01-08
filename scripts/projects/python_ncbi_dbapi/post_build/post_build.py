2a0e68b9f8b7adc22faa4c9d6eac5764b38e7383
Aaron Ucko <ucko@ncbi.nlm.nih.gov>
2016-01-08 16:17:22 +0000

    python_ncbi_dbapi post_build.py: Build a wheel for Python 3.4 in addition to 2.7env.  JIRA: CXX-7331.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@70636 78c7ea69-d796-4a43-9a09-de51944f1b03

--- scripts/projects/python_ncbi_dbapi/post_build/post_build.py
+++ scripts/projects/python_ncbi_dbapi/post_build/post_build.py
@@ -8,7 +8,7 @@ common_stems  = ('python_ncbi_dbapi', 'ncbi_xdbapi_*')
 unix_stems    = ('dbapi', 'dbapi_driver', 'ct_ftds64', 'tds_ftds64',
                  'xconne?t', 'xutil', 'xncbi')
 windows_stems = ('ncbi_core', 'ncbi_dbapi', 'ncbi_dbapi_driver')
-installations = ('2.7env',)
+installations = ('2.7env', '3.4')
 
 os.mkdir(install_dir + '/lib')
 if os.path.exists(lib_dir + '/python_ncbi_dbapi.so'):
