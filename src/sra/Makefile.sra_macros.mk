c08f3e2b9fe6fc6a5120a1797e9ac6117a900868
Aaron Ucko <ucko@ncbi.nlm.nih.gov>
2016-02-01 21:06:06 +0000

    Retire the C++ Toolkit's old bundled copy of the SRA/VDB Toolkit, since it is no longer compatible with that version.  JIRA: CXX-7756.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@70955 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ src/sra/Makefile.sra_macros.mk
@@ -0,0 +1,11 @@
+# $Id$
+# Historical names for VDB macros.
+
+SRA_INCLUDE     = $(VDB_INCLUDE)
+SRA_SDK_SYSLIBS = $(VDB_LIBS)
+SRA_SDK_LIBS    = $(VDB_LIB)
+SRAXF_LIBS      = $(SRA_SDK_LIBS)
+SRA_LIBS        = $(SRA_SDK_LIBS)
+BAM_LIBS        = $(SRA_SDK_LIBS)
+SRAREAD_LDEP    = $(SRA_SDK_LIBS)
+SRAREAD_LIBS    = sraread $(SRA_SDK_LIBS)
