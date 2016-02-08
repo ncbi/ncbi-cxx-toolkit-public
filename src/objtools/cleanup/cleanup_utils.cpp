b9c551d3e75a4e5d94d5113212e555fb95c24049
ludwigf <ludwigf@ncbi.nlm.nih.gov>
2016-02-08 15:31:45 +0000

    Silence deprecation warnings (NStr::Tokenize -> NStr::Split).
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@71045 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/cleanup/cleanup_utils.cpp
+++ src/objtools/cleanup/cleanup_utils.cpp
@@ -125,7 +125,7 @@ void s_SplitMLAuthorName(string name, string& last, string& initials, string& su
     }
 
     vector<string> parts;
-    NStr::Tokenize( name, " ", parts, NStr::eMergeDelims );
+    NStr::Split( name, " ", parts, NStr::eMergeDelims );
     if ( parts.empty() ) {
         return;
     }
