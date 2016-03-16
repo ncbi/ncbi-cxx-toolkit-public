0394db90e602b6593aed8cafe54264eeebc2ae2b
Colleen Bollin <bollin@ncbi.nlm.nih.gov>
2016-03-16 18:46:38 +0000

    JIRA:VR-520 fix for reporting uncultured bacterium sequence length suspiciously high
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@71596 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/validator/valid_biosource.cpp
+++ src/objtools/validator/valid_biosource.cpp
@@ -1855,7 +1855,7 @@ const CBioseq_Handle& bsh)
         if (orgref.IsSetTaxname()) {
             if (NStr::EqualNocase(orgref.GetTaxname(), "uncultured bacterium")
                 && bsh.GetBioseqLength() >= 10000
-                && source.IsSetGenome() && source.GetGenome() != CBioSource::eGenome_plasmid) {
+                && (!source.IsSetGenome() || source.GetGenome() != CBioSource::eGenome_plasmid)) {
                 PostObjErr(eDiag_Warning, eErr_SEQ_DESCR_BioSourceInconsistency,
                     "Uncultured bacterium sequence length is suspiciously high",
                     obj, ctx);
