2629412502f32aa457ab0426bcb5397794af4520
Christiam Camacho <camacho@ncbi.nlm.nih.gov>
2016-03-21 16:00:20 +0000

    Port to python3
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@71649 78c7ea69-d796-4a43-9a09-de51944f1b03

--- scripts/projects/blast/post_build/make_installers.py
+++ scripts/projects/blast/post_build/make_installers.py
@@ -28,10 +28,10 @@ def main(): #IGNORE:R0911
     global VERBOSE #IGNORE:W0603
     VERBOSE = options.VERBOSE
     if VERBOSE:
-        print "BLAST version", blast_version
-        print "Platform:", platform
-        print "Installation directory:", installdir
-        print "Source tarball:", srctarball
+        print("BLAST version", blast_version)
+        print("Platform:", platform)
+        print("Installation directory:", installdir)
+        print("Source tarball:", srctarball)
 
     if platform.startswith("Win"):
         return launch_win_installer_build(installdir, blast_version)                
@@ -43,14 +43,14 @@ def main(): #IGNORE:R0911
     if platform == "IntelMAC":
         return mac_post_build(installdir, blast_version)
     
-    print >> sys.stderr, "Unknown OS identifier: " + platform
-    print >> sys.stderr, "Exiting post build script."
+    print("Unknown OS identifier:" + platform, file=sys.stderr)
+    print("Exiting post build script.", file=sys.stderr)
     return 2
 
 def launch_win_installer_build(installdir, blast_version):
     '''Windows post-build: create installer'''
     if VERBOSE: 
-        print "Packaging for Windows..."
+        print("Packaging for Windows...")
     cmd = "python " + os.path.join(SCRIPTS_DIR, "win", "make_win.py") + " "
     cmd += blast_version + " " + installdir
     if VERBOSE: 
@@ -61,7 +61,7 @@ def launch_win_installer_build(installdir, blast_version):
 def launch_rpm_build(installdir, blast_version, srctarball):
     '''Linux post-build: create RPM'''
     if VERBOSE: 
-        print "Packing linux RPM..."
+        print("Packing linux RPM...")
     cmd = "python " + os.path.join(SCRIPTS_DIR, "rpm", "make_rpm.py") + " "
     cmd += blast_version + " " + installdir + " " + srctarball
     if VERBOSE: 
@@ -73,7 +73,7 @@ def launch_rpm_build(installdir, blast_version, srctarball):
 def mac_post_build(installdir, blast_version):
     '''MacOSX post-build: create installer'''
     if VERBOSE:
-        print "Packaging for MacOSX..."
+        print("Packaging for MacOSX...")
     script_dir = os.path.join(SCRIPTS_DIR, "macosx")
     cmd = os.path.join(script_dir, "ncbi-blast.sh") + " "
     cmd += installdir + " " + script_dir + " " + blast_version
@@ -82,7 +82,7 @@ def mac_post_build(installdir, blast_version):
 
 def do_nothing(platform):
     '''No op function'''
-    print "No post-build step necessary for", platform
+    print("No post-build step necessary for", platform)
     return 0
 
 # The script execution entry point
