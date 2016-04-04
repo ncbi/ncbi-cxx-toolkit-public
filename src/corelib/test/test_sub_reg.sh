ac68f8a02cfe2e18cd275d660caa70bc774a9e42
elisovdn <elisovdn@ncbi.nlm.nih.gov>
2016-04-04 17:47:31 +0000

    Intel Inspector fix: capitalized section names on Windows. JIRA: CXX-7906
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@71866 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/corelib/test/test_sub_reg.sh
+++ src/corelib/test/test_sub_reg.sh
@@ -7,42 +7,63 @@ else
     flags=
 fi
 
+#Clearing existing environment variables
 for x in `printenv | sed -ne 's/^\(NCBI_CONFIG_[^=]*\)=.*/\1/p'`; do
     echo "Clearing $x setting."
     unset $x
 done
 
+# Set environment variables with registry values (Windows version has capitalized value names)
 export NCBI_CONFIG_PATH=`dirname $0`/test_sub_reg_data
-export NCBI_CONFIG_OVERRIDES=$NCBI_CONFIG_PATH/indirect_env.ini
-export NCBI_CONFIG_e__test=env
-export NCBI_CONFIG__test__e=env
-export NCBI_CONFIG__test__ex=
-export NCBI_CONFIG__environment_DOT_indirectenv__e_ie=env
-export NCBI_CONFIG__environment_DOT_indirectenv__ex_ie=
-export NCBI_CONFIG__overridesbase_DOT_environment__ob_e=env
-export NCBI_CONFIG__overridesbase_DOT_environment__obx_e=env
-export NCBI_CONFIG__a_DOT_b__c_DOT_d=e.f
-
-$CHECK_EXEC test_sub_reg -defaults "$NCBI_CONFIG_PATH/defaults.ini" \
-                         -overrides "$NCBI_CONFIG_PATH/overrides.ini" \
-                         -out test_sub_reg_1strun.out.ini
-echo "Comparing first run and expected output"
 unamestr=`uname -s`
 if [[ "$unamestr" == MINGW* ]] || [[ "$unamestr" == CYGWIN* ]]; then
     echo "Operating system is Windows"
-    diff $flags $NCBI_CONFIG_PATH/expected_win.ini test_sub_reg_1strun.out.ini
+    export NCBI_CONFIG_OVERRIDES=$NCBI_CONFIG_PATH/indirect_env.win.ini
+    export NCBI_CONFIG_E__TEST=env
+    export NCBI_CONFIG__TEST__E=env
+    export NCBI_CONFIG__TEST__EX=
+    export NCBI_CONFIG__ENVIRONMENT_DOT_INDIRECTENV__E_IE=env
+    export NCBI_CONFIG__ENVIRONMENT_DOT_INDIRECTENV__EX_IE=
+    export NCBI_CONFIG__OVERRIDESBASE_DOT_ENVIRONMENT__OB_E=env
+    export NCBI_CONFIG__OVERRIDESBASE_DOT_ENVIRONMENT__OBX_E=env
+    export NCBI_CONFIG__A_DOT_B__C_DOT_D=e.f
+    $CHECK_EXEC test_sub_reg -defaults "$NCBI_CONFIG_PATH/defaults.ini" \
+                             -overrides "$NCBI_CONFIG_PATH/overrides.win.ini" \
+                             -out test_sub_reg_1strun.out.ini
 else
     echo "Operating system is not Windows"
+    export NCBI_CONFIG_OVERRIDES=$NCBI_CONFIG_PATH/indirect_env.ini
+    export NCBI_CONFIG_e__TEST=env
+    export NCBI_CONFIG__TEST__e=env
+    export NCBI_CONFIG__TEST__ex=
+    export NCBI_CONFIG__environment_DOT_indirectenv__e_ie=env
+    export NCBI_CONFIG__environment_DOT_indirectenv__ex_ie=
+    export NCBI_CONFIG__overridesbase_DOT_environment__ob_e=env
+    export NCBI_CONFIG__overridesbase_DOT_environment__obx_e=env
+    export NCBI_CONFIG__a_DOT_b__c_DOT_d=e.f
+    $CHECK_EXEC test_sub_reg -defaults "$NCBI_CONFIG_PATH/defaults.ini" \
+                             -overrides "$NCBI_CONFIG_PATH/overrides.ini" \
+                             -out test_sub_reg_1strun.out.ini
+fi
+
+echo "Comparing first run and expected output"
+if [[ "$unamestr" == MINGW* ]] || [[ "$unamestr" == CYGWIN* ]]; then
+    diff $flags $NCBI_CONFIG_PATH/expected_win.ini test_sub_reg_1strun.out.ini
+else
     diff $flags $NCBI_CONFIG_PATH/expected.ini test_sub_reg_1strun.out.ini
 fi
 
 # Second run
-
-unset NCBI_CONFIG_PATH 
-unset NCBI_CONFIG_OVERRIDES NCBI_CONFIG_e__test
-unset NCBI_CONFIG__test__e NCBI_CONFIG__test__ex 
-unset NCBI_CONFIG__environment_DOT_indirectenv__ex_ie NCBI_CONFIG__overridesbase_DOT_environment__ob_e NCBI_CONFIG__overridesbase_DOT_environment__obx_e 
-unset NCBI_CONFIG__environment_DOT_indirectenv__e_ie NCBI_CONFIG__a_DOT_b__c_DOT_d
+unset NCBI_CONFIG_PATH NCBI_CONFIG_OVERRIDES
+if [[ "$unamestr" == MINGW* ]] || [[ "$unamestr" == CYGWIN* ]]; then
+    unset NCBI_CONFIG_E__TEST NCBI_CONFIG__TEST__E NCBI_CONFIG__TEST__EX
+    unset NCBI_CONFIG__ENVIRONMENT_DOT_INDIRECTENV__EX_IE NCBI_CONFIG__OVERRIDESBASE_DOT_ENVIRONMENT__OB_E NCBI_CONFIG__OVERRIDESBASE_DOT_ENVIRONMENT__OBX_E 
+    unset NCBI_CONFIG__ENVIRONMENT_DOT_INDIRECTENV__E_IE NCBI_CONFIG__A_DOT_B__C_DOT_D
+else
+    unset NCBI_CONFIG_e__test NCBI_CONFIG__test__e NCBI_CONFIG__test__ex 
+    unset NCBI_CONFIG__environment_DOT_indirectenv__ex_ie NCBI_CONFIG__overridesbase_DOT_environment__ob_e NCBI_CONFIG__overridesbase_DOT_environment__obx_e 
+    unset NCBI_CONFIG__environment_DOT_indirectenv__e_ie NCBI_CONFIG__a_DOT_b__c_DOT_d 
+fi
 
 cp test_sub_reg_1strun.out.ini test_sub_reg.ini
 
