fc56bf80ba24a05ce91aa02390c1bbfe6e8841f0
Amelia Fong <fongah2@ncbi.nlm.nih.gov>
2016-04-06 14:56:41 +0000

    Changes for checking duplicate ids for makeblastdb, JIRA:SB-1459
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@71907 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/blast/seqdb_writer/writedb_volume.cpp
+++ src/objtools/blast/seqdb_writer/writedb_volume.cpp
@@ -220,13 +220,16 @@ bool CWriteDB_Volume::WriteSequence(const string      & seq,
     if (m_Indices != CWriteDB::eNoIndex) {
     	set<string>::size_type orig_size = m_IdSet.size();
     	string id_u;
+    	pair<set<string>::iterator, bool > rv;
         ITERATE(TIdList, iter, idlist) {
-            string id = (*iter)->AsFastaString();
+            string id = (*iter)->GetSeqIdString(true);
             id_u = NStr::ToUpper(id);
-            if (m_IdSet.find(id_u) != m_IdSet.end() ) {
-            	continue;
+            rv = m_IdSet.insert(id_u);
+            if((rv.second == false) && (!(*iter)->IsLocal())) {
+            	CNcbiOstrstream msg;
+            	msg << "Error: Duplicate seq_ids are found: " << endl << id_u << endl;
+            	NCBI_THROW(CWriteDBException, eArgErr, CNcbiOstrstreamToString(msg));
             }
-            m_IdSet.insert(id_u);
         }
 
         if(m_IdSet.size() == orig_size) {
