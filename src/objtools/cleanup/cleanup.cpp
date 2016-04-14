b173f0766417461cace4a5ad0c21f2781300957c
Colleen Bollin <bollin@ncbi.nlm.nih.gov>
2016-04-14 10:59:20 +0000

    JIRA:SQD-2390 remove synonyms after looking up org in taxonomy, like C Toolkit does
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@72059 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/cleanup/cleanup.cpp
+++ src/objtools/cleanup/cleanup.cpp
@@ -1556,6 +1556,7 @@ bool CCleanup::TaxonomyLookup(CSeq_entry_Handle seh)
                     any_changes = true;
                     CSeqdesc* desc = const_cast<CSeqdesc*>(*desc_it);
                     desc->SetSource().SetOrg().Assign((*reply_it)->GetData().GetOrg());
+                    desc->SetSource().SetOrg().ResetSyn();
                 }
                 ++reply_it;
                 ++desc_it;
