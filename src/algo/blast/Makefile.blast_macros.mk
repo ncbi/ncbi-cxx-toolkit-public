ad1aa031f74ad1143058a4a92e55c0fc3b81e1fd
Christiam Camacho <camacho@ncbi.nlm.nih.gov>
2016-04-26 14:40:22 +0000

    Fix library order, per Andrei Gourianov
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@72247 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/algo/blast/Makefile.blast_macros.mk
+++ src/algo/blast/Makefile.blast_macros.mk
@@ -10,7 +10,7 @@ BLAST_FORMATTER_MINIMAL_LIBS = xblastformat align_format taxon1 blastdb_format \
     gene_info $(XFORMAT_LIBS) xalnmgr blastxml blastxml2 xcgi xhtml
 # BLAST_FORMATTER_LIBS = $(BLAST_FORMATTER_MINIMAL_LIBS)
 BLAST_FORMATTER_LIBS = $(BLAST_INPUT_LIBS)
-BLAST_DB_DATA_LOADER_LIBS = ncbi_xloader_blastdb ncbi_xloader_blastdb_rmt
+BLAST_DB_DATA_LOADER_LIBS = ncbi_xloader_blastdb_rmt ncbi_xloader_blastdb
 BLAST_INPUT_LIBS = blastinput \
     $(BLAST_DB_DATA_LOADER_LIBS) $(BLAST_FORMATTER_MINIMAL_LIBS)
 
@@ -19,6 +19,6 @@ BLAST_SRA_LIBS=blast_sra $(SRAXF_LIBS) vxf $(SRA_LIBS)
 
 # BLAST_FORMATTER_LIBS and BLAST_INPUT_LIBS need $BLAST_LIBS
 BLAST_LIBS = xblast xalgoblastdbindex composition_adjustment \
-		xalgodustmask xalgowinmask seqmasks_io seqdb blast_services xobjutil \
-		$(OBJREAD_LIBS) xnetblastcli xnetblast blastdb scoremat tables xalnmgr
+		xalgodustmask xalgowinmask seqmasks_io seqdb blast_services xalnmgr \
+		xobjutil $(OBJREAD_LIBS) xnetblastcli xnetblast blastdb scoremat tables
 # BLAST additionally needs xconnect $(SOBJMGR_LIBS) or $(OBJMGR_LIBS)
