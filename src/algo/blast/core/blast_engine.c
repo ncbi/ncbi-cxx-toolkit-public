41396fca4c76ab3fb978e0321762c80e7672b3f3
Christiam Camacho <camacho@ncbi.nlm.nih.gov>
2016-05-04 18:52:28 +0000

    Bump version in anticipation of public release 2.4.0 JIRA SB-1688
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@72396 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/algo/blast/core/blast_engine.c
+++ src/algo/blast/core/blast_engine.c
@@ -78,9 +78,9 @@
 #define CONV_NUCL2PROT_COORDINATES(length) (length) / CODON_LENGTH
 
 NCBI_XBLAST_EXPORT const int   kBlastMajorVersion = 2;
-NCBI_XBLAST_EXPORT const int   kBlastMinorVersion = 3;
-NCBI_XBLAST_EXPORT const int   kBlastPatchVersion = 1;
-NCBI_XBLAST_EXPORT const char* kBlastReleaseDate = "January-06-2016";
+NCBI_XBLAST_EXPORT const int   kBlastMinorVersion = 4;
+NCBI_XBLAST_EXPORT const int   kBlastPatchVersion = 0;
+NCBI_XBLAST_EXPORT const char* kBlastReleaseDate = "June-01-2016";
 
 /** Structure to be passed to s_BlastSearchEngineCore, containing pointers 
     to various preallocated structures and arrays. */
