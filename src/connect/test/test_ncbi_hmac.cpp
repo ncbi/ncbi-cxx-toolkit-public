cce3a800b18418afe7eff9bb5f30185868b47eee
elisovdn <elisovdn@ncbi.nlm.nih.gov>
2016-05-24 19:23:57 +0000

    Added CNCBIApplication support. JIRA: CXX-8126
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@72730 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/connect/test/test_ncbi_hmac.cpp
+++ src/connect/test/test_ncbi_hmac.cpp
@@ -32,6 +32,7 @@
 
 #include <ncbi_pch.hpp>
 #include <corelib/ncbistre.hpp>
+#include <corelib/ncbiapp.hpp>
 #include <connect/ncbi_util.h>
 #include <util/md5.hpp>
 #include <string.h>
@@ -39,7 +40,7 @@
 #include "test_assert.h"  // This header must go last
 
 
-BEGIN_NCBI_SCOPE
+USING_NCBI_SCOPE;
 
 
 extern "C" {
@@ -80,90 +81,94 @@ static const SHASH_Descriptor md5_hash = {
 };
 
 
-END_NCBI_SCOPE
+class CNCBITestApp : public CNcbiApplication
+{
+public:
+    int Run(void)
+    {
+        // These are taken directly from RFC2104
+        static const struct {
+            const char* text;
+            size_t      text_len;
+            const char* key;
+            size_t      key_len;
+            const char* digest;
+        } tests[] = {
+            { "Hi There",
+              8,
+              "\x0b\x0b\x0b\x0b"
+              "\x0b\x0b\x0b\x0b"
+              "\x0b\x0b\x0b\x0b"
+              "\x0b\x0b\x0b\x0b",
+              16,
+              "\x92\x94\x72\x7a"
+              "\x36\x38\xbb\x1c"
+              "\x13\xf4\x8e\xf8"
+              "\x15\x8b\xfc\x9d"
+            },
+            { "what do ya want for nothing?",
+              28,
+              "Jefe",
+              4,
+              "\x75\x0c\x78\x3e"
+              "\x6a\xb0\xb5\x03"
+              "\xea\xa8\x6e\x31"
+              "\x0a\x5d\xb7\x38"
+            },
+            { "\xDD\xDD\xDD\xDD\xDD"
+              "\xDD\xDD\xDD\xDD\xDD"
+              "\xDD\xDD\xDD\xDD\xDD"
+              "\xDD\xDD\xDD\xDD\xDD"
+              "\xDD\xDD\xDD\xDD\xDD"
+              "\xDD\xDD\xDD\xDD\xDD"
+              "\xDD\xDD\xDD\xDD\xDD"
+              "\xDD\xDD\xDD\xDD\xDD"
+              "\xDD\xDD\xDD\xDD\xDD"
+              "\xDD\xDD\xDD\xDD\xDD",
+              50,
+              "\xAA\xAA\xAA\xAA"
+              "\xAA\xAA\xAA\xAA"
+              "\xAA\xAA\xAA\xAA"
+              "\xAA\xAA\xAA\xAA",
+              16,
+              "\x56\xbe\x34\x52"
+              "\x1d\x14\x4c\x88"
+              "\xdb\xb8\xc7\x33"
+              "\xf0\xe8\xb3\xf6"
+            }
+        };
+
+        LOG_POST(Info << "Testing MD5 HMAC generation");
+
+        for (size_t i = 0;  i < sizeof(tests) / sizeof(tests[0]);  ++i) {
+            unsigned char digest[16];
+            LOG_POST(Info << "Test " << i + 1);
+            _ASSERT(UTIL_GenerateHMAC(&md5_hash,
+                                      tests[i].text,
+                                      tests[i].text_len,
+                                      tests[i].key,
+                                      tests[i].key_len,
+                                      digest));
+            _ASSERT(memcmp(digest, tests[i].digest, sizeof(digest)) == 0);
+        }
+
+        LOG_POST(Info << "All tests completed successfully");
+        return 0;
+    }
+};
 
 
 int main(int argc, char* argv[])
 {
-    USING_NCBI_SCOPE;
-
     // Setup error posting
     SetDiagTrace(eDT_Enable);
     SetDiagPostLevel(eDiag_Trace);
     SetDiagPostAllFlags(SetDiagPostAllFlags(eDPF_Default)
-                        | eDPF_All | eDPF_OmitInfoSev);
+        | eDPF_All | eDPF_OmitInfoSev);
     UnsetDiagPostFlag(eDPF_Line);
     UnsetDiagPostFlag(eDPF_File);
     UnsetDiagPostFlag(eDPF_Location);
     UnsetDiagPostFlag(eDPF_LongFilename);
     SetDiagTraceAllFlags(SetDiagPostAllFlags(eDPF_Default));
-
-    // These are taken directly from RFC2104
-    static const struct {
-        const char* text;
-        size_t      text_len;
-        const char* key;
-        size_t      key_len;
-        const char* digest;
-    } tests[] = {
-        { "Hi There",
-          8,
-          "\x0b\x0b\x0b\x0b"
-          "\x0b\x0b\x0b\x0b"
-          "\x0b\x0b\x0b\x0b"
-          "\x0b\x0b\x0b\x0b",
-          16,
-          "\x92\x94\x72\x7a"
-          "\x36\x38\xbb\x1c"
-          "\x13\xf4\x8e\xf8"
-          "\x15\x8b\xfc\x9d"
-        },
-        { "what do ya want for nothing?",
-          28,
-          "Jefe",
-          4,
-          "\x75\x0c\x78\x3e"
-          "\x6a\xb0\xb5\x03"
-          "\xea\xa8\x6e\x31"
-          "\x0a\x5d\xb7\x38"
-        },
-        { "\xDD\xDD\xDD\xDD\xDD"
-          "\xDD\xDD\xDD\xDD\xDD"
-          "\xDD\xDD\xDD\xDD\xDD"
-          "\xDD\xDD\xDD\xDD\xDD"
-          "\xDD\xDD\xDD\xDD\xDD"
-          "\xDD\xDD\xDD\xDD\xDD"
-          "\xDD\xDD\xDD\xDD\xDD"
-          "\xDD\xDD\xDD\xDD\xDD"
-          "\xDD\xDD\xDD\xDD\xDD"
-          "\xDD\xDD\xDD\xDD\xDD",
-          50,
-          "\xAA\xAA\xAA\xAA"
-          "\xAA\xAA\xAA\xAA"
-          "\xAA\xAA\xAA\xAA"
-          "\xAA\xAA\xAA\xAA",
-          16,
-          "\x56\xbe\x34\x52"
-          "\x1d\x14\x4c\x88"
-          "\xdb\xb8\xc7\x33"
-          "\xf0\xe8\xb3\xf6"
-        }
-    };
-
-    LOG_POST(Info << "Testing MD5 HMAC generation");
-
-    for (size_t i = 0;  i < sizeof(tests) / sizeof(tests[0]);  ++i) {
-        unsigned char digest[16];
-        LOG_POST(Info << "Test " << i + 1);
-        _ASSERT(UTIL_GenerateHMAC(&md5_hash,
-                                  tests[i].text,
-                                  tests[i].text_len,
-                                  tests[i].key,
-                                  tests[i].key_len,
-                                  digest));
-        _ASSERT(memcmp(digest, tests[i].digest, sizeof(digest)) == 0);
-    }
-
-    LOG_POST(Info << "All tests completed successfully");
-    return 0;
+    return CNCBITestApp().AppMain(argc, argv);
 }
