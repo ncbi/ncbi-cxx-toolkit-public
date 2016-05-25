bf7738e0a1bc042e0a7135a222235f7f2c338c99
Colleen Bollin <bollin@ncbi.nlm.nih.gov>
2016-05-25 14:41:04 +0000

    require minimum coding region length before attempting to extend to stop codon
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@72741 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/cleanup/newcleanupp.cpp
+++ src/objtools/cleanup/newcleanupp.cpp
@@ -12566,26 +12566,28 @@ void CNewCleanup_imp::CdRegionEC(CSeq_feat& sf)
     CConstRef<CSeq_feat> mrna = sequence::GetmRNAforCDS(sf, *m_Scope);
     CConstRef<CSeq_feat> gene = CCleanup::GetGeneForFeature(sf, *m_Scope);
 
-    CBioseq_Handle bsh = m_Scope->GetBioseqHandle(sf.GetLocation());
-    if (bsh && CCleanup::ExtendToStopIfShortAndNotPartial(sf, bsh)) {
-        CCdregion::TFrame frame = cdr.IsSetFrame() ? cdr.GetFrame() : CCdregion::eFrame_not_set;
-        if (gene && s_LocationShouldBeExtendedToMatch(gene->GetLocation(), sf.GetLocation())) {
-            CRef<CSeq_feat> new_gene(new CSeq_feat());
-            new_gene->Assign(*gene);
-            if (CCleanup::ExtendToStopCodon(*new_gene, bsh, 3, frame)) {
-                CSeq_feat_EditHandle efh = CSeq_feat_EditHandle(m_Scope->GetSeq_featHandle(*gene));
-                efh.Replace(*new_gene);
+    if (sequence::GetLength(sf.GetLocation(), m_Scope) > 15) {
+        CBioseq_Handle bsh = m_Scope->GetBioseqHandle(sf.GetLocation());
+        if (bsh && CCleanup::ExtendToStopIfShortAndNotPartial(sf, bsh)) {
+            CCdregion::TFrame frame = cdr.IsSetFrame() ? cdr.GetFrame() : CCdregion::eFrame_not_set;
+            if (gene && s_LocationShouldBeExtendedToMatch(gene->GetLocation(), sf.GetLocation())) {
+                CRef<CSeq_feat> new_gene(new CSeq_feat());
+                new_gene->Assign(*gene);
+                if (CCleanup::ExtendToStopCodon(*new_gene, bsh, 3, frame)) {
+                    CSeq_feat_EditHandle efh = CSeq_feat_EditHandle(m_Scope->GetSeq_featHandle(*gene));
+                    efh.Replace(*new_gene);
+                }
             }
-        }
-        if (mrna && s_LocationShouldBeExtendedToMatch(mrna->GetLocation(), sf.GetLocation())) {
-            CRef<CSeq_feat> new_mrna(new CSeq_feat());
-            new_mrna->Assign(*mrna);
-            if (CCleanup::ExtendToStopCodon(*new_mrna, bsh, 3, frame)) {
-                CSeq_feat_EditHandle efh = CSeq_feat_EditHandle(m_Scope->GetSeq_featHandle(*mrna));
-                efh.Replace(*new_mrna);
+            if (mrna && s_LocationShouldBeExtendedToMatch(mrna->GetLocation(), sf.GetLocation())) {
+                CRef<CSeq_feat> new_mrna(new CSeq_feat());
+                new_mrna->Assign(*mrna);
+                if (CCleanup::ExtendToStopCodon(*new_mrna, bsh, 3, frame)) {
+                    CSeq_feat_EditHandle efh = CSeq_feat_EditHandle(m_Scope->GetSeq_featHandle(*mrna));
+                    efh.Replace(*new_mrna);
+                }
             }
+            ChangeMade(CCleanupChange::eChangeFeatureLocation);
         }
-        ChangeMade(CCleanupChange::eChangeFeatureLocation);
     }
 
     if (sf.IsSetPseudo() && sf.GetPseudo() && sf.IsSetProduct()) {
