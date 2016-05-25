5e502807af0e24e21a1e5998a794881ab5984396
Colleen Bollin <bollin@ncbi.nlm.nih.gov>
2016-05-25 13:13:20 +0000

    JIRA:SQD-3790 added INFLUENZA_DATE_MISMATCH test
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@72738 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/misc/discrepancy/biosource_tests.cpp
+++ src/misc/discrepancy/biosource_tests.cpp
@@ -29,6 +29,7 @@
 
 #include <ncbi_pch.hpp>
 #include <objects/seqfeat/SubSource.hpp>
+#include <objects/seqfeat/Org_ref.hpp>
 #include <objmgr/seqdesc_ci.hpp>
 
 #include "discrepancy_core.hpp"
@@ -85,6 +86,124 @@ DISCREPANCY_SUMMARIZE(MAP_CHROMOSOME_CONFLICT)
     m_ReportItems = m_Objs.Export(*this)->GetSubitems();
 }
 
+
+// INFLUENZA_DATE_MISMATCH
+/* For Influenza A viruses, the year of the collection date should be
+* the last number before the second set of parentheses in the tax name.
+* For Influenza B viruses, the year should be the last token in the
+* tax name (tokens are separated by / characters).
+*/
+static int GetYearFromInfluenzaA(const string& taxname)
+{
+    size_t pos = NStr::Find(taxname, "(");
+    if (pos == string::npos) {
+        return 0;
+    }
+    pos = NStr::Find(taxname, "(", pos + 1);
+    if (pos == string::npos) {
+        return 0;
+    }
+    pos--;
+    while (pos > 0 && isspace(taxname.c_str()[pos])) {
+        pos--;
+    }
+    if (pos < 4 ||
+        !isdigit(taxname.c_str()[pos]) ||
+        !isdigit(taxname.c_str()[pos - 1]) ||
+        !isdigit(taxname.c_str()[pos - 2]) ||
+        !isdigit(taxname.c_str()[pos - 3])) {
+        return 0;
+    }
+    return NStr::StringToInt(taxname.substr(pos - 3, 4));
+}
+
+
+static int GetYearFromInfluenzaB(const string& taxname)
+{
+    size_t pos = NStr::Find(taxname, "/");
+    if (pos == string::npos) {
+        return 0;
+    }
+    ++pos;
+    while (isspace(taxname.c_str()[pos])) {
+        ++pos;
+    }
+    size_t len = 0;
+    while (isdigit(taxname.c_str()[pos + len])) {
+        len++;
+    }
+    if (len > 0) {
+        return NStr::StringToInt(taxname.substr(pos, len));
+    } else {
+        return 0;
+    }
+}
+
+
+static bool DoInfluenzaStrainAndCollectionDateMisMatch(const CBioSource& src)
+{
+    if (!src.IsSetOrg() || !src.GetOrg().IsSetTaxname()) {
+        return false;
+    }
+    
+    int year = 0;
+    if (NStr::StartsWith(src.GetOrg().GetTaxname(), "Influenza A virus ")) {
+        year = GetYearFromInfluenzaA(src.GetOrg().GetTaxname());
+    } else if (NStr::StartsWith(src.GetOrg().GetTaxname(), "Influenza B virus ")) {
+        year = GetYearFromInfluenzaB(src.GetOrg().GetTaxname());
+    } else {
+        // not influenza A or B, no mismatch can be calculated
+        return false;
+    }
+
+    if (year > 0 && src.IsSetSubtype()) {
+        ITERATE(CBioSource::TSubtype, it, src.GetSubtype()) {
+            if ((*it)->IsSetSubtype() &&
+                (*it)->GetSubtype() == CSubSource::eSubtype_collection_date &&
+                (*it)->IsSetName()) {
+                try {
+                    CRef<CDate> date = CSubSource::DateFromCollectionDate((*it)->GetName());
+                    if (date->IsStd() && date->GetStd().IsSetYear() && date->GetStd().GetYear() == year) {
+                        // match found, no mismatch
+                        return false;
+                    } else {
+                        return true;
+                    }
+                } catch (CException) {
+                    //unable to parse date, assume mismatch
+                    return true;
+                }
+            }
+        }
+    }
+    return true;
+}
+
+
+const string kInfluenzaDateMismatch = "[n] influenza strain[s] conflict with collection date";
+//  ----------------------------------------------------------------------------
+DISCREPANCY_CASE(INFLUENZA_DATE_MISMATCH, CBioSource, eOncaller, "Influenza Strain/Collection Date Mismatch")
+//  ----------------------------------------------------------------------------
+{
+    if (DoInfluenzaStrainAndCollectionDateMisMatch(obj)) {
+        if (context.GetCurrentSeqdesc() != NULL) {
+            m_Objs[kInfluenzaDateMismatch].Add(*context.NewDiscObj(context.GetCurrentSeqdesc()), false).Fatal();
+        } else if (context.GetCurrentSeq_feat() != NULL) {
+            m_Objs[kInfluenzaDateMismatch].Add(*context.NewDiscObj(context.GetCurrentSeq_feat()), false).Fatal();
+        }
+    }
+}
+
+
+//  ----------------------------------------------------------------------------
+DISCREPANCY_SUMMARIZE(INFLUENZA_DATE_MISMATCH)
+//  ----------------------------------------------------------------------------
+{
+    if (m_Objs.empty()) {
+        return;
+    }
+    m_ReportItems = m_Objs.Export(*this)->GetSubitems();
+}
 
 
 END_SCOPE(NDiscrepancy)
