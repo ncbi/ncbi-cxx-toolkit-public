d567509ea297230ad100bd605ac89585899e2b64
Colleen Bollin <bollin@ncbi.nlm.nih.gov>
2016-06-22 18:19:31 +0000

    JIRA:SQD-3864 added CULTURE_TAXNAME_MISMATCH test
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@73178 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/misc/discrepancy/biosource_tests.cpp
+++ src/misc/discrepancy/biosource_tests.cpp
@@ -974,5 +974,35 @@ DISCREPANCY_SUMMARIZE(SPECVOUCHER_TAXNAME_MISMATCH)
 }
 
 
+// CULTURE_TAXNAME_MISMATCH
+//  ----------------------------------------------------------------------------
+DISCREPANCY_CASE(CULTURE_TAXNAME_MISMATCH, CBioSource, eOncaller, "Test BioSources with the same culture collection but different taxname")
+//  ----------------------------------------------------------------------------
+{
+    if (!obj.IsSetOrg() || !obj.GetOrg().IsSetOrgname() || !obj.GetOrg().GetOrgname().IsSetMod()) {
+        return;
+    }
+    ITERATE(COrgName::TMod, om, obj.GetOrg().GetOrgname().GetMod()) {
+        if ((*om)->IsSetSubtype() && (*om)->GetSubtype() == COrgMod::eSubtype_culture_collection &&
+            (*om)->IsSetSubname() && !NStr::IsBlank((*om)->GetSubname())) {
+            AddObjSource(m_Objs, context, "[n] biosource[s] have culture collection " + (*om)->GetSubname() + " but do not have the same taxnames", true);
+        }
+    }
+}
+
+
+//  ----------------------------------------------------------------------------
+DISCREPANCY_SUMMARIZE(CULTURE_TAXNAME_MISMATCH)
+//  ----------------------------------------------------------------------------
+{
+    if (m_Objs.empty()) {
+        return;
+    }
+    SummarizeTaxnameConflict(m_Objs, "culture collection");
+
+    m_ReportItems = m_Objs.Export(*this)->GetSubitems();
+}
+
+
 END_SCOPE(NDiscrepancy)
 END_NCBI_SCOPE
