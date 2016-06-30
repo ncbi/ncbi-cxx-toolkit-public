cb81eac6627404081acd9dd29ef348f2ed518fab
Victoria Serova <serovav@ncbi.nlm.nih.gov>
2016-06-30 15:59:04 +0000

    Link corrected to the new book JIRA: CXX-8299
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@73340 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/misc/clog/ncbi_c_log_p.h
+++ src/misc/clog/ncbi_c_log_p.h
@@ -242,7 +242,7 @@ extern int /*bool*/ NcbiLogP_DisableChecks(int /*bool*/ disable);
  *  The 'logsite' parameter is used specifically by ncbi_applog utility, 
  *  to allow to check additionally /log/{{logsite}} destination.
  *  See for details:
- *  http://www.ncbi.nlm.nih.gov/toolkit/doc/book/ch_core/#ch_core.Where_Diagnostic_Messages_Go
+ *  http://ncbi.github.io/cxx-toolkit/pages/ch_log#ch_core.Where_Diagnostic_Messages_Go
  *  @sa NcbiLog_SetDestination, ENcbiLog_Destination, NcbiLog_Init
  */
 extern ENcbiLog_Destination NcbiLogP_SetDestination(ENcbiLog_Destination ds, unsigned int port, const char* logsite);
@@ -251,7 +251,7 @@ extern ENcbiLog_Destination NcbiLogP_SetDestination(ENcbiLog_Destination ds, uns
 /** Redefine value of $SERVER_PORT environment variable.
  *  Can be used only for redirect mode.
  *  See this for details:
- *  http://www.ncbi.nlm.nih.gov/toolkit/doc/book/ch_core/#ch_core.Where_Diagnostic_Messages_Go
+ *  http://ncbi.github.io/cxx-toolkit/pages/ch_log#ch_core.Where_Diagnostic_Messages_Go
  *  @sa NcbiLog_SetDestination
  */
 extern unsigned int NcbiLogP_SetServerPort(unsigned int server_port);
