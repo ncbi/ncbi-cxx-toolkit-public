98a3df727ec94b0888b32ee842558088fda5c7c8
Sergey Satskiy <satskyse@ncbi.nlm.nih.gov>
2016-07-26 16:21:40 +0000

    Extending the GC acceptable DB structure range. JIRA: CXX-8281
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@73816 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/app/netstorage/utils/netstorage_gc_version.hpp
+++ src/app/netstorage/utils/netstorage_gc_version.hpp
@@ -33,7 +33,7 @@
 #include <common/ncbi_package_ver.h>
 
 #define NETSTORAGE_GC_EXPECTED_DB_STRUCTURE_LOW     2
-#define NETSTORAGE_GC_EXPECTED_DB_STRUCTURE_HIGH    5
+#define NETSTORAGE_GC_EXPECTED_DB_STRUCTURE_HIGH    6
 #define NETSTORAGE_GC_VERSION                       NCBI_PACKAGE_VERSION
 #define NETSTORAGE_GC_BUILD_DATE                    __DATE__ " " __TIME__
 
