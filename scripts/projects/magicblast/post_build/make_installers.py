b65d45ae29d13eee96f63bf772e4a3c3a8731985
Grzegorz (Greg) Boratyn <boratyng@ncbi.nlm.nih.gov>
2016-08-10 19:17:18 +0000

    Minor fix JIRA SB-1721
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@74078 78c7ea69-d796-4a43-9a09-de51944f1b03

--- scripts/projects/magicblast/post_build/make_installers.py
+++ scripts/projects/magicblast/post_build/make_installers.py
@@ -81,7 +81,7 @@ def mac_post_build(installdir, blast_version):
     if VERBOSE:
         print("Packaging for MacOSX...")
     script_dir = os.path.join(SCRIPTS_DIR, "macosx")
-    cmd = os.path.join(script_dir, "ncbi-blast.sh") + " "
+    cmd = os.path.join(script_dir, "ncbi-magicblast.sh") + " "
     cmd += installdir + " " + script_dir + " " + blast_version
     blast_utils.safe_exec(cmd)
     return 0
