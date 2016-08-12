8d087c2a8e6f31a505458f1dfb989f8f094b5b5c
syncbot <syncbot@ncbi.nlm.nih.gov>
2016-08-12 14:57:12 +0000

    Synchronized with the C Toolkit.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@74108 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/data_loaders/genbank/gicache/gicache.h
+++ src/objtools/data_loaders/genbank/gicache/gicache.h
@@ -1,53 +1,68 @@
 #ifndef _GI_CACHE_HPP__
 #define _GI_CACHE_HPP__
 
+#include <ncbistd.h>
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 #define DEFAULT_GI_CACHE_PATH "//panfs/pan1.be-md.ncbi.nlm.nih.gov/id_dumps/gi_cache"
-#define DEFAULT_GI_CACHE_PREFIX "gi2acc"
-#define DEFAULT_64BIT_SUFFIX ".64"
+#define DEFAULT_GI_CACHE_PREFIX "gi2acc_lmdb"
 
-/* Populates the cache. One of 3 options are available to determine which gis
- * to include in cache on this run:
- * 1. Use provided SQL condition;
- * 2. Use provided temporary table (in tempdb database) with a list of gis;
- * 3. Start from the next gi after the maximal gi currently present in cache;
- */
-int         GICache_PopulateAccessions(char *server, const char *cache_prefix,
-                                       const char *sql_gi_cond,
-                                       const char *temptable,
-                                       int log_level);
+#define META_INCREMENTAL_HOST "INC_HOST"
+#define META_FULL_HOST "FULL_HOST"
+#define META_INCREMENTAL_DB "INC_DB"
+#define META_FULL_DB "FULL_DB"
+#define META_INCREMENTAL_TIME "INC_TIME"
+#define META_FULL_TIME "FULL_TIME"
+#define META_GIBYTIME_TIME "GI_BYTIME_TIME"
+#define RC_ALREADY_EXISTS 2
+
+typedef struct {
+	int64_t Count[256];
+} FreqTab;
 
 /* Initializes the cache. If cache_prefix argument is not provided, default name
  * is used. If local cache is not available, use default path and prefix.
  * Return value: 0 on success, 1 on failure. 
+ * Resources should be released with GICache_ReadEnd() call
  */
 int         GICache_ReadData(const char *cache_prefix);
-/* Remaps cache files */
-void        GICache_ReMap(int delay_in_sec);
 /* Retrieves accession.version by gi.
  * Accession buffer must be preallocated by caller. 
  * If buffer is too small, retrieved accession is truncated, and return code 
  * is 0, otherwise return code is 1.
  */
-int         GICache_GetAccession(int gi, char* acc, int buf_len);
+int         GICache_GetAccession(int64_t gi, char* acc, int buf_len);
 /* Retrieves gi length */
-int         GICache_GetLength(int gi);
+int64_t     GICache_GetLength(int64_t gi);
 /* Returns maximal gi available in cache */
-int         GICache_GetMaxGi(void);
-
+int64_t     GICache_GetMaxGi(void);
+/* Finish read, release resources */
+void         GICache_ReadEnd(void);
 /* Internal loading interface, non MT safe */
 /* Initialize cache for loading */
-int         GICache_LoadStart(const char *cache_prefix);
+int         GICache_LoadStart(const char *cache_prefix, int enablesync);
 /* Add gi's data to cache */
-int         GICache_LoadAdd(int gi, int len, const char* accession, int version);
+int         GICache_LoadAdd(int64_t gi, int64_t gi_len, const char* accession, int version, int is_incremental);
 /* Finish load, flush modifications to disk */
+int 		GiDataIndex_Commit(void);
+/* Close DB handle */
 int         GICache_LoadEnd(void);
-
-void        GICache_SetLog(void (*logfunc)(char*)); 
-
+/* Erase data in DB before full update */
+int         GICache_DropDb(void);
+/* Update meta info */
+int			GICache_SetMeta(const char* Name, const char* Value);
+/* Store meta info */
+int         GICache_UpdateMeta(int is_incremental, const char* DB, time_t starttime);
+/* Retrieve meta info */
+int			GICache_GetMeta(const char* Name, char* Value, size_t ValueSz);
+/* Install logger CB */
+void        GICache_SetLog(void (*logfunc)(char*));
+/* Dumps out content of the cache. Result is a text file with 3 columns: gi, accession, length  */
+void		GICache_Dump(const char* cache_prefix, const char* filename);
+int GICache_GetAccFreqTab(FreqTab* tab, const FreqTab* tablen);
 #ifdef __cplusplus
 }
 #endif
