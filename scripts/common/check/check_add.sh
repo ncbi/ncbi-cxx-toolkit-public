d06ee06de5d942056e617afde3f14f790e937253
Vladimir Ivanov <ivanov@ncbi.nlm.nih.gov>
2016-08-22 10:15:34 +0000

    Add sanity check for /CHECK_NAME.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@74236 78c7ea69-d796-4a43-9a09-de51944f1b03

--- scripts/common/check/check_add.sh
+++ scripts/common/check/check_add.sh
@@ -121,7 +121,12 @@ fi
 # Write data about current test into the list file
 for x_cmd in $x_run; do
     x_cmd=`echo "$x_cmd"  | sed -e 's/%gj_s4%/ /g'`
-    x_name=`echo "$x_cmd" | sed -e 's/^.*CHECK_NAME *= *\([A-Za-z0-9_-]*\).*/\1/' -e 's/.*CHECK_CMD.*//'`
+    x_name=`echo "$x_cmd" | sed -e 's/^.*CHECK_NAME *= *//' -e 's/ *$//'`
+    xx_name=`echo "$x_name" | sed -e 's/\([A-Za-z0-9_.-]*\).*/\1/'`
+    if [ "$x_name" != "$xx_name" ]; then
+       echo "Warning: Incorrect CHECK_NAME value '$x_name', '$xx_name' will be used instead. Please fix."
+       x_name=$xx_name
+    fi
     x_cmd=`echo "$x_cmd"  | sed -e 's/ *\/CHECK_NAME.*//' -e 's/^[^=]*=//' -e 's/^ *//'  -e 's/\"/\\\\"/g'`
     echo "$x_srcdir_rel$x_delim$x_test$x_delim$x_app$x_delim$x_cmd$x_delim$x_name$x_delim$x_files$x_delim$x_timeout$x_delim$x_requires$x_delim$x_watchers" >> $x_out
 done
