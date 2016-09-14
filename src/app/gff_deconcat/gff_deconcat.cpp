89d061455fa8746d02a90813802805cfc06aa78f
Justin Foley <foleyjp@ncbi.nlm.nih.gov>
2016-09-14 12:35:46 +0000

    JIRA: SQD-4038 Code now attempt to deduct extension from input file and output directory can now be specified with the -dir flag
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@74574 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/app/gff_deconcat/gff_deconcat.cpp
+++ src/app/gff_deconcat/gff_deconcat.cpp
@@ -1,13 +1,63 @@
-#include <ncbi_pch.hpp>
+/*  $Id$
+* ===========================================================================
+*
+*                            PUBLIC DOMAIN NOTICE
+*               National Center for Biotechnology Information
+*
+*  This software/database is a "United States Government Work" under the
+*  terms of the United States Copyright Act.  It was written as part of
+*  the author's official duties as a United States Government employee and
+*  thus cannot be copyrighted.  This software/database is freely available
+*  to the public for use. The National Library of Medicine and the U.S.
+*  Government have not placed any restriction on its use or reproduction.
+*
+*  Although all reasonable efforts have been taken to ensure the accuracy
+*  and reliability of the software and data, the NLM and the U.S.
+*  Government do not and cannot warrant the performance or results that
+*  may be obtained by using this software or data. The NLM and the U.S.
+*  Government disclaim all warranties, express or implied, including
+*  warranties of performance, merchantability or fitness for any particular
+*  purpose.
+*
+*  Please cite the author in any work or product based on this material.
+*
+* ===========================================================================
+*
+* Author:  Justin Foley
+*
+* File Description:
+*   Feature deconcatenor for .gff files
+*
+* ===========================================================================
+*/
 
+#include <ncbi_pch.hpp>
 #include <corelib/ncbiapp.hpp>
 #include <corelib/ncbistd.hpp>
 #include <corelib/ncbiargs.hpp>
-
+#include <corelib/ncbifile.hpp>
 #include <util/line_reader.hpp>
-
+#include <corelib/ncbiexpt.hpp>
 
 BEGIN_NCBI_SCOPE
+
+
+class CGffDeconcatException : public CException
+{
+public:
+    enum EErrCode {
+        eInvalidOutputDir,
+    };
+
+    virtual const char* GetErrCodeString() const {
+        switch (GetErrCode()) {
+            case eInvalidOutputDir:
+                return "eInvalidOutputDir";
+        }
+    }
+
+    NCBI_EXCEPTION_DEFAULT(CGffDeconcatException, CException);
+};
 
 
 class CGffDeconcatApp : public CNcbiApplication
@@ -20,11 +70,15 @@ public:
 
 private:
     void xProcessFile(CNcbiIstream& istr);
-    void xReadFile(CNcbiIstream& istr, TIdmap& id_map);
+    void xReadFile(CNcbiIstream& istr, string& header, TIdmap& id_map);
     void xProcessLine(const string& line, TIdmap& id_map);
-    void xWriteFile(CNcbiOstream& ostr,
+    void xWriteFile(CNcbiOfstream& ostr,
                     const string& header, 
                     const list<string>& body);
+    void x_SetExtension(const string& input_filename);
+
+    string m_OutputDir;
+    string m_Extension;
 };
 
 
@@ -37,7 +91,31 @@ void CGffDeconcatApp::Init(void)
                      "GFF input filename", 
                      CArgDescriptions::eInputFile);
 
+    arg_desc->AddOptionalKey("dir",
+                             "OutputDirectory",
+                             "Output Directory. Defaults to CWD",
+                             CArgDescriptions::eOutputFile);
+
     SetupArgDescriptions(arg_desc.release());
+}
+
+
+void CGffDeconcatApp::x_SetExtension(const string& input_filename)
+{
+    CDir input_file(input_filename);
+    string trial_ext(input_file.GetExt());
+
+    if (!NStr::IsBlank(trial_ext)) {
+        try {
+            NStr::StringToDouble(trial_ext); // Extension cannot be numeric
+        }
+        catch(...) {
+            m_Extension = trial_ext;    
+            return;
+        }
+    }
+
+    m_Extension = ".gff";
 }
 
 
@@ -45,6 +123,46 @@ int CGffDeconcatApp::Run(void)
 {
     const CArgs& args = GetArgs();
     CNcbiIstream& istr = args["i"].AsInputFile();
+
+    x_SetExtension(args["i"].AsString());
+/*
+    CDirEntry input_file(args["i"].AsString());
+    string extension(input_file.GetExt());
+
+    if (!NStr::IsBlank(extension)) {
+        try {
+            NStr::StringToDouble(extension); // Extension cannot be numeric
+            m_Extension = ".gff";
+        }
+        catch(...) {
+            m_Extension = extension;
+        }
+    } 
+    else {
+        m_Extension = ".gff";
+    }
+
+*/
+
+    if (args["dir"]) {
+        auto dirname = args["dir"].AsString();
+
+        if (!CDir::IsAbsolutePath(dirname)) {
+            dirname = CDir::CreateAbsolutePath(dirname);
+        }
+
+        CDir output_dir(dirname);
+        if (!output_dir.Exists()) {
+            string err_msg = dirname 
+                           + " does not exist";
+            NCBI_THROW(CGffDeconcatException, 
+                       eInvalidOutputDir,
+                       err_msg);
+        }
+
+        m_OutputDir = dirname;
+    }
+
     xProcessFile(istr);
 
     return 0;
@@ -54,8 +172,22 @@ int CGffDeconcatApp::Run(void)
 void CGffDeconcatApp::xProcessFile(CNcbiIstream& istr) {
 
     TIdmap id_map;
-    xReadFile(istr, id_map);
+    string header;
+    xReadFile(istr, header, id_map);
 
+    string output_dir = ".";
+    if (!NStr::IsBlank(m_OutputDir)) {
+        output_dir = m_OutputDir;
+    }
+
+    for (const auto& key_val : id_map) {
+        string filename = output_dir + "/" 
+                        + key_val.first 
+                        + m_Extension;
+
+        unique_ptr<CNcbiOfstream> ostr(new CNcbiOfstream(filename.c_str()));
+        xWriteFile(*ostr, header, key_val.second);
+    }
     return;
 }
 
@@ -70,7 +202,7 @@ void CGffDeconcatApp::xProcessLine(const string& line, TIdmap& id_map) {
 }
 
 
-void CGffDeconcatApp::xReadFile(CNcbiIstream& istr, TIdmap& id_map)
+void CGffDeconcatApp::xReadFile(CNcbiIstream& istr, string& header, TIdmap& id_map)
 {
     id_map.clear();
     CStreamLineReader lr(istr);
@@ -80,6 +212,11 @@ void CGffDeconcatApp::xReadFile(CNcbiIstream& istr, TIdmap& id_map)
         line = *++lr;
         if (NStr::IsBlank(line) || 
             line[0] == '#') {
+            if (line.size() > 2 &&
+                line[1] == '#' && 
+                header.empty() ) {
+                header = line;
+            }
             continue;
         }
         xProcessLine(line, id_map);
@@ -87,13 +224,21 @@ void CGffDeconcatApp::xReadFile(CNcbiIstream& istr, TIdmap& id_map)
     return;
 }
 
-void CGffDeconcatApp::xWriteFile(CNcbiOstream& ostr, 
+
+void CGffDeconcatApp::xWriteFile(CNcbiOfstream& ostr, 
                                  const string& header, 
                                  const list<string>& body) 
 {
+    if (!NStr::IsBlank(header)) {
+       ostr << header << "\n";
+    }
+
+    for (const string& line : body) {
+        ostr << line << "\n";
+    }
+
     return;
 }
-
 
 
 END_NCBI_SCOPE
