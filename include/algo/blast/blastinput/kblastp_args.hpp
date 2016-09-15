33fbf5025ecfea6afb39be9dd7b075042936c402
madden <madden@ncbi.nlm.nih.gov>
2016-09-15 17:22:37 +0000

    Use blastinput lib for KBLASTP application, JIRA:SB-1772
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@74613 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ include/algo/blast/blastinput/kblastp_args.hpp
@@ -0,0 +1,75 @@
+/*  $Id$
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Author: Tom Madden
+ *
+ */
+
+/** @file kblastp_args.hpp
+ * Main argument class for KBLASTP application
+ */
+
+#ifndef ALGO_BLAST_BLASTINPUT___KBLASTP_ARGS__HPP
+#define ALGO_BLAST_BLASTINPUT___KBLASTP_ARGS__HPP
+
+#include <algo/blast/blastinput/blast_args.hpp>
+
+BEGIN_NCBI_SCOPE
+BEGIN_SCOPE(blast)
+
+/// Handles command line arguments for blastp binary
+class NCBI_BLASTINPUT_EXPORT CKBlastpAppArgs : public CBlastAppArgs
+{
+public:
+    /// Constructor
+    CKBlastpAppArgs();
+
+     double GetJaccardDistance() { return m_KBlastpArgs->GetJaccardDistance(); }
+
+     int GetMinHits() { return m_KBlastpArgs->GetMinHits(); }
+
+     string GetDatabase() {return m_KBlastpArgs->GetDatabase(); }
+
+     int GetTargetSeqs() {return m_KBlastpArgs->GetTargetSeqs(); }
+
+    /// @inheritDoc
+    virtual int GetQueryBatchSize() const;
+
+protected:
+    /// @inheritDoc
+    virtual CRef<CBlastOptionsHandle>
+    x_CreateOptionsHandle(CBlastOptions::EAPILocality locality,
+                          const CArgs& args);
+
+    /// KBlastp search specific arguments
+    CRef<CKBlastpArgs> m_KBlastpArgs;
+
+};
+
+
+END_SCOPE(blast)
+END_NCBI_SCOPE
+
+#endif  /* ALGO_BLAST_BLASTINPUT___KBLASTP_ARGS__HPP */
