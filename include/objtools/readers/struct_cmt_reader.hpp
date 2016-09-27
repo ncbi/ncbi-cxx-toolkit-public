76361793ed70ccb0932681934b7e0db8cbd905cd
Sergiy Gotvyanskyy <gotvyans@ncbi.nlm.nih.gov>
2016-09-27 13:57:03 +0000

    JIRA: GB-2439
    Structured comments readers moved to library
    
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@74803 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ include/objtools/readers/struct_cmt_reader.hpp
@@ -0,0 +1,96 @@
+#ifndef __STRUCT_CMT_READER_HPP_INCLUDED__
+#define __STRUCT_CMT_READER_HPP_INCLUDED__
+
+#include <corelib/ncbistl.hpp>
+#include <corelib/ncbiobj.hpp>
+
+BEGIN_NCBI_SCOPE
+
+// forward declarations
+namespace objects
+{
+    class CSeq_descr;
+    class CSeqdesc;
+    class CSeq_id;
+    class CUser_object;
+    class ILineErrorListener;
+};
+
+class CSerialObject;
+class ILineReader;
+
+/*
+  Usage examples
+
+  CStructuredCommentsReader reader(error_logger);
+
+  std::list<CStructuredCommentsReader::TStructComment> comments;
+
+  ILineReader reader1(ILineReader::New(filename);
+  reader.LoadComments(reader1, comments);
+
+  for (const CStructuredCommentsReader::TStructComment& cmt: comments)
+  {
+     // do something
+  }
+*/
+
+class CStructuredCommentsReader
+{
+public:
+   // If you need messages and error to be logged
+   // supply an optional ILineErrorListener instance
+   CStructuredCommentsReader(objects::ILineErrorListener* logger);
+   ~CStructuredCommentsReader();
+
+   typedef struct {
+       CRef<objects::CSeq_id> m_id;
+       vector<CRef<objects::CSeqdesc> > m_descs;
+   } TStructComment;
+
+#if 0
+   template<typename _container>
+   size_t LoadComments(ILineReader& reader, _container& cont)
+   {
+       vector<string> cols;
+       _LoadHeaderLine(reader, cols);
+       if (cols.empty())
+           return 0;
+
+       size_t loader = 0;
+
+       while (!reader.AtEOF())
+       {
+           reader.ReadLine();
+           // First line is a collumn definitions
+           CTempString current = reader.GetCurrentLine();
+           if (current.empty())
+               continue;
+
+           // Each line except first is a set of values, first collumn is a sequence id
+           vector<CTempString> values;
+           NStr::Split(current, "\t", values);
+           if (!values[0].empty())
+           {
+               // try to find destination sequence
+               cont.push_back(TStructComment());
+               TStructComment& cmt = cont.back();
+               cmt.m_id.Reset(new objects::CSeq_id(values[0], objects::CSeq_id::fParse_AnyLocal));
+               _BuildStructuredComment(cmt, cols, values);
+           }
+       }
+   }
+#endif
+
+   objects::CUser_object* FindStructuredComment(objects::CSeq_descr& descr);
+protected:
+   void _LoadHeaderLine(ILineReader& reader, vector<string>& cols);
+   void _BuildStructuredComment(TStructComment& cmt, const vector<string>& cols, const vector<CTempString>& values);
+   objects::CUser_object* _AddStructuredComment(objects::CUser_object* user_obj, TStructComment& cmt, const CTempString& name, const CTempString& value);
+   objects::ILineErrorListener* m_logger;
+};
+
+END_NCBI_SCOPE
+
+
+#endif
---
+++ src/objtools/readers/struct_cmt_reader.cpp
@@ -0,0 +1,130 @@
+/*  $Id$
+* ===========================================================================
+*
+*                            PUBLIC DOMAIN NOTICE
+*               National Center for Biotechnology Information
+*
+*  This software/database is a "United States Government Work" under the
+*  terms of the United States Copyright Act.  It was written as part of
+*  the author's official duties as a United States Government employee and
+*  thus cannot be copyrighted.  This software/database is freely available
+*  to the public for use. The National Library of Medicine and the U.S.
+*  Government have not placed any restriction on its use or reproduction.
+*
+*  Although all reasonable efforts have been taken to ensure the accuracy
+*  and reliability of the software and data, the NLM and the U.S.
+*  Government do not and cannot warrant the performance or results that
+*  may be obtained by using this software or data. The NLM and the U.S.
+*  Government disclaim all warranties, express or implied, including
+*  warranties of performance, merchantability or fitness for any particular
+*  purpose.
+*
+*  Please cite the author in any work or product based on this material.
+*
+* ===========================================================================
+*
+* Author:  Sergiy Gotvyanskyy, NCBI
+*
+* File Description:
+*   Reader for structured comments for sequences
+*
+* ===========================================================================
+*/
+
+#include <ncbi_pch.hpp>
+
+#include <objtools/readers/struct_cmt_reader.hpp>
+
+#include <objects/seq/Seqdesc.hpp>
+#include <objects/seq/Seq_descr.hpp>
+#include <objects/general/User_object.hpp>
+#include <objects/general/Object_id.hpp>
+#include <objects/seqloc/Seq_id.hpp>
+
+#include <util/line_reader.hpp>
+
+#include <common/test_assert.h>  /* This header must go last */
+
+BEGIN_NCBI_SCOPE
+USING_SCOPE(objects);
+
+CStructuredCommentsReader::CStructuredCommentsReader(ILineErrorListener* logger) : m_logger(logger)
+{
+}
+
+CStructuredCommentsReader::~CStructuredCommentsReader()
+{
+}
+
+CUser_object* CStructuredCommentsReader::FindStructuredComment(CSeq_descr& descr)
+{
+    NON_CONST_ITERATE(CSeq_descr::Tdata, it, descr.Set())
+    {
+        if ((**it).IsUser())
+        {
+            if ((**it).GetUser().GetType().GetStr().compare("StructuredComment") == 0)
+                return &((**it).SetUser());
+        }
+    }
+    return 0;
+}
+
+objects::CUser_object* CStructuredCommentsReader::_AddStructuredComment(objects::CUser_object* user_obj, TStructComment& cmt, const CTempString& name, const CTempString& value)
+{
+    if (name.compare("StructuredCommentPrefix") == 0)
+        user_obj = 0; // reset user obj so to create a new one
+    else
+    {
+        //if (user_obj == 0)
+        //    user_obj = FindStructuredComment(cmt.m_descs);
+    }
+
+    if (user_obj == 0)
+    {
+        // create new user object
+        CRef<CSeqdesc> user_desc(new CSeqdesc);
+        user_obj = &(user_desc->SetUser());
+        user_obj->SetType().SetStr("StructuredComment");
+        cmt.m_descs.push_back(user_desc);
+    }
+    user_obj->AddField(name, value);
+    // create next user object
+    if (name.compare("StructuredCommentSuffix") == 0)
+        return 0;
+    else
+        return user_obj;
+}
+
+void CStructuredCommentsReader::_BuildStructuredComment(TStructComment& cmt, const vector<string>& cols, const vector<CTempString>& values)
+{
+    cmt.m_descs.reserve(values.size() - 1);
+    objects::CUser_object* user = 0;
+
+    for (size_t i = 1; i<values.size(); i++)
+    {
+        if (!values[i].empty())
+        {
+            // create new user object
+            user = _AddStructuredComment(user, cmt, cols[i], values[i]);
+        }
+    }
+}
+
+void CStructuredCommentsReader::_LoadHeaderLine(ILineReader& reader, vector<string>& cols)
+{
+    cols.clear();
+
+    while (!reader.AtEOF() && cols.empty())
+    {
+        reader.ReadLine();
+        // First line is a collumn definitions
+        CTempString current = reader.GetCurrentLine();
+        if (NStr::StartsWith(current, '#'))
+            continue;
+
+        NStr::Split(current, "\t", cols);
+    }
+}
+
+END_NCBI_SCOPE
+
