624c53565051c2b1774d7e42a2ce8da4ccea529d
syncbot <syncbot@ncbi.nlm.nih.gov>
2016-11-19 05:03:02 +0000

    Mark as 20161119 DEVELOPMENT.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@75485 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/common/ncbi_source_ver.h
+++ include/common/ncbi_source_ver.h
@@ -27,7 +27,7 @@
 #include <ncbiconf.h>
 
 /* #undef NCBI_PRODUCTION_VER */
-#define NCBI_DEVELOPMENT_VER 20161118
+#define NCBI_DEVELOPMENT_VER 20161119
 
 #ifdef HAVE_COMMON_NCBI_BUILD_VER_H
 #  include <common/ncbi_build_ver.h>
