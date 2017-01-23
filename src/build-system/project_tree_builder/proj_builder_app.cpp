bbf08390fa066fceab64727768941a9848efe873
Andrei Gourianov <gouriano@ncbi.nlm.nih.gov>
2017-01-23 18:25:29 +0000

    JIRA: CXX-8983
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@76202 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/build-system/project_tree_builder/proj_builder_app.cpp
+++ src/build-system/project_tree_builder/proj_builder_app.cpp
@@ -405,7 +405,7 @@ struct PIsExcludedByDisuse
 //-----------------------------------------------------------------------------
 CProjBulderApp::CProjBulderApp(void)
 {
-    SetVersion( CVersionInfo(4,2,0) );
+    SetVersion( CVersionInfo(4,2,1) );
     m_ScanningWholeTree = false;
     m_Dll = false;
     m_AddMissingLibs = false;
