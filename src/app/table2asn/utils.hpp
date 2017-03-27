64c4c65b383f970a2122a426d8f083a67119c712
Sergiy Gotvyanskyy <gotvyans@ncbi.nlm.nih.gov>
2017-03-27 15:55:42 +0000

    JIRA: RW-364
    Enabled eco-set to be instantiates proteins
    
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@77126 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ src/app/table2asn/utils.hpp
@@ -0,0 +1,10 @@
+#ifndef __TABLE2ASN_UTILS_HPP
+#define __TABLE2ASN_UTILS_HPP
+
+BEGIN_NCBI_SCOPE
+
+bool AssignLocalIdIfEmpty(ncbi::objects::CSeq_feat& feature, int& id);
+
+END_NCBI_SCOPE
+
+#endif
