d2f6a7ff7ca5610eecf6265bb43871270233307d
perkovan <perkovan@ncbi.nlm.nih.gov>
2017-03-29 20:24:24 +0000

    JIRA: GP-19953 hackishly force CMake build to link with static GnuTLS (more nice way doesn't work)
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@77150 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/build-system/cmake/CMakeChecks.openssl.cmake
+++ src/build-system/cmake/CMakeChecks.openssl.cmake
@@ -16,8 +16,13 @@ set(TLS_INCLUDE     ${NCBI_TOOLS_ROOT}/libgcrypt-1.4.3/include
 set(TLS_LIBS        -L${NCBI_TOOLS_ROOT}/gnutls-2.4.2/GCC401-ReleaseMT64/lib -Wl,-rpath,/opt/ncbi/64/gnutls-2.4.2/GCC401-ReleaseMT64/lib:${NCBI_TOOLS_ROOT}/gnutls-2.4.2/GCC401-ReleaseMT64/lib -lgnutls -L${NCBI_TOOLS_ROOT}/libgcrypt-1.4.3/GCC401-ReleaseMT64/lib -Wl,-rpath,/opt/ncbi/64/libgcrypt-1.4.3/GCC401-ReleaseMT64/lib:${NCBI_TOOLS_ROOT}/libgcrypt-1.4.3/GCC401-ReleaseMT64/lib -lgcrypt -L${NCBI_TOOLS_ROOT}/libgpg-error-1.6/GCC401-ReleaseMT64/lib -Wl,-rpath,/opt/ncbi/64/libgpg-error-1.6/GCC401-ReleaseMT64/lib:${NCBI_TOOLS_ROOT}/libgpg-error-1.6/GCC401-ReleaseMT64/lib -lgpg-error -lz)
 
 #include(FindGnuTLS)
-find_package(GnuTLS)
-set(GNUTLS_INCLUDE ${GNUTLS_INCLUDE_DIR})
-set(GNUTLS_LIBS ${GNUTLS_LIBRARIES})
+if (EXISTS "${NCBI_TOOLS_ROOT}/gnutls-3.4.0/")
+  set(GNUTLS_INCLUDE "${NCBI_TOOLS_ROOT}/gnutls-3.4.0/include")
+  set(GNUTLS_LIBS "-L${NCBI_TOOLS_ROOT}/gnutls-3.4.0/lib -lgnutls -lz -lidn -lrt -L/netopt/ncbi_tools64/nettle-3.1.1/lib64 -lhogweed -lnettle -L/netopt/ncbi_tools64/gmp-6.0.0a/lib64 -lgmp")
+else()
+  find_package(GnuTLS)
+  set(GNUTLS_INCLUDE ${GNUTLS_INCLUDE_DIR})
+  set(GNUTLS_LIBS ${GNUTLS_LIBRARIES})
+endif()
 
 
