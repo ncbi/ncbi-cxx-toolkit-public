8398403f45a947421c33e445a5edc102e08ecfd3
David McElhany <mcelhany@ncbi.nlm.nih.gov>
2017-04-18 14:09:22 +0000

    Added Windows compatibility (via Cygwin) to test script; relocated test data to snowman.  JIRA CXX-8439
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@77463 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/connect/test/test_ncbi_namerd.sh
+++ src/connect/test/test_ncbi_namerd.sh
@@ -1,6 +1,35 @@
 #! /bin/sh
 # $Id$
 
-. ./ncbi_test_data
+# Factor out OS- and invocation-specific stuff.
+if test "$OSTYPE" = "cygwin"; then
+  bases=". //snowman/toolkit_test_data/connect"
+else
+  bases=". /net/snowman/vol/projects/toolkit_test_data/connect"
+fi
+if test -z "$CHECK_EXEC"; then
+  tester="./test_ncbi_namerd"
+else
+  tester="test_ncbi_namerd"
+fi
 
-$CHECK_EXEC test_ncbi_namerd -f $NCBI_TEST_DATA/connect/test_ncbi_namerd_tests.json
+# Find a directory containing the test file.
+for base in $bases; do
+    if test ! -d $base; then
+        echo "Test data dir not found: $base"
+        continue
+    fi
+    test_file="$base/test_ncbi_namerd_tests.json"
+    if test ! -r $test_file; then
+        echo "Test file not found: $test_file"
+        continue
+    fi
+    break;
+done
+
+if test ! -r "$test_file"; then
+    echo "ERROR: Test file not found in any base directory."
+    exit 1
+fi
+
+$CHECK_EXEC $tester -f $test_file
