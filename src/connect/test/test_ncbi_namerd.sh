dbee057772de47b63108bce95a005342aac3e862
David McElhany <mcelhany@ncbi.nlm.nih.gov>
2017-04-19 17:16:06 +0000

    Add standard location for test data for automated tests.  JIRA CXX-8439
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@77503 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/connect/test/test_ncbi_namerd.sh
+++ src/connect/test/test_ncbi_namerd.sh
@@ -1,35 +1,40 @@
 #! /bin/sh
 # $Id$
 
+# Use standard location, if operating from inside automated build.
+if test -r ./ncbi_test_data; then
+    . ./ncbi_test_data
+    base_std=$NCBI_TEST_DATA/connect
+fi
+
 # Factor out OS- and invocation-specific stuff.
 if test "$OSTYPE" = "cygwin"; then
-  bases=". //snowman/toolkit_test_data/connect"
+  bases=". $base_std //snowman/toolkit_test_data/connect"
 else
-  bases=". /net/snowman/vol/projects/toolkit_test_data/connect"
-fi
-if test -z "$CHECK_EXEC"; then
-  tester="./test_ncbi_namerd"
-else
-  tester="test_ncbi_namerd"
+  bases=". $base_std /net/snowman/vol/projects/toolkit_test_data/connect"
 fi
 
 # Find a directory containing the test file.
+test_file_name="test_ncbi_namerd_tests.json"
 for base in $bases; do
     if test ! -d $base; then
-        echo "Test data dir not found: $base"
+        echo "INFO: Candidate test data dir not found: $base"
         continue
     fi
-    test_file="$base/test_ncbi_namerd_tests.json"
+    test_file="$base/$test_file_name"
     if test ! -r $test_file; then
-        echo "Test file not found: $test_file"
+        echo "INFO: Candidate test data file not found: $test_file"
         continue
     fi
     break;
 done
-
 if test ! -r "$test_file"; then
-    echo "ERROR: Test file not found in any base directory."
+    echo "ERROR: Test file '$test_file_name' not found in any base directory ($bases)."
     exit 1
 fi
 
-$CHECK_EXEC $tester -f $test_file
+if test -n "$CHECK_EXEC"; then
+    $CHECK_EXEC test_ncbi_namerd -f $test_file
+else
+    ./test_ncbi_namerd -f $test_file
+fi
