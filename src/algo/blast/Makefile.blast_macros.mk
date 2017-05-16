fbce88281e50b89580db9c73e8ed7799535f912a
Aaron Ucko <ucko@ncbi.nlm.nih.gov>
2017-05-16 03:19:07 +0000

    Makefile.blast_macros.mk: Move proteinkmer from BLAST_LIBS to the start of BLAST_INPUT_LIBS per its dependency on blastinput. JIRA: SB-2028.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@77936 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/algo/blast/Makefile.blast_macros.mk
+++ src/algo/blast/Makefile.blast_macros.mk
@@ -11,14 +11,14 @@ BLAST_FORMATTER_MINIMAL_LIBS = xblastformat align_format taxon1 blastdb_format \
 # BLAST_FORMATTER_LIBS = $(BLAST_FORMATTER_MINIMAL_LIBS)
 BLAST_FORMATTER_LIBS = $(BLAST_INPUT_LIBS)
 BLAST_DB_DATA_LOADER_LIBS = ncbi_xloader_blastdb_rmt ncbi_xloader_blastdb
-BLAST_INPUT_LIBS = blastinput \
+BLAST_INPUT_LIBS = proteinkmer blastinput \
     $(BLAST_DB_DATA_LOADER_LIBS) $(BLAST_FORMATTER_MINIMAL_LIBS)
 
 # Libraries required to link against the internal BLAST SRA library
 BLAST_SRA_LIBS=blast_sra $(SRAXF_LIBS) vxf $(SRA_LIBS)
 
 # BLAST_FORMATTER_LIBS and BLAST_INPUT_LIBS need $BLAST_LIBS
-BLAST_LIBS = xblast xalgoblastdbindex composition_adjustment proteinkmer \
+BLAST_LIBS = xblast xalgoblastdbindex composition_adjustment \
 		xalgodustmask xalgowinmask seqmasks_io seqdb blast_services xalnmgr \
 		xobjutil $(OBJREAD_LIBS) xnetblastcli xnetblast blastdb scoremat tables
 # BLAST additionally needs xconnect $(SOBJMGR_LIBS) or $(OBJMGR_LIBS)
