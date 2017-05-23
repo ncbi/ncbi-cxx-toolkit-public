ccd0a660c3a96624bd0be8563a516225ed197c40
perkovan <perkovan@ncbi.nlm.nih.gov>
2017-05-23 21:47:07 +0000

    JIRA: CXX-9295 check for SQLite3 fixed, --debug-trycompile added to cmake-configure (affects only cmake)
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@78078 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/build-system/cmake/CMakeChecks.sqlite3.cmake
+++ src/build-system/cmake/CMakeChecks.sqlite3.cmake
@@ -4,7 +4,11 @@ include(CheckIncludeFile)
 find_package(Sqlite3)
 
 if (SQLITE3_FOUND)
-    check_function_exists(sqlite3_unlock_notify HAVE_SQLITE3_UNLOCK_NOTIFY)
+    set(CMAKE_REQUIRED_LIBRARIES_OLD "${CMAKE_REQUIRED_LIBRARIES}")
+    set(CMAKE_REQUIRED_LIBRARIES "${CMAKE_REQUIRED_LIBRARIES} ${SQLITE3_LIBRARY}")
+    check_symbol_exists(sqlite3_unlock_notify ${SQLITE3_INCLUDE_DIR}/sqlite3.h HAVE_SQLITE3_UNLOCK_NOTIFY)
+    set(CMAKE_REQUIRED_LIBRARIES "${CMAKE_REQUIRED_LIBRARIES_OLD}")
+
     check_include_file(sqlite3async.h HAVE_SQLITE3ASYNC_H -I${SQLITE3_INCLUDE_DIR})
 endif(SQLITE3_FOUND)
 
