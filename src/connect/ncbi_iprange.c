b4fb33e546cf05e168795d6df3332a4a02a56a3d
Anton Lavrentiev <lavr@ncbi.nlm.nih.gov>
2017-05-31 15:53:43 +0000

    Split localnet and localip per JIRA: CXX-9315
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@78166 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ src/connect/ncbi_iprange.c
@@ -0,0 +1,376 @@
+/* $Id$
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Author:  Anton Lavrentiev
+ *
+ * File Description:
+ *   IP range manipulating API
+ *
+ */
+
+#include "ncbi_ansi_ext.h"
+#include "ncbi_assert.h"
+#include <connect/ncbi_iprange.h>
+#include <connect/ncbi_socket.h>
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int/*bool*/ NcbiIsInIPRange(const SIPRange* range, const TNCBI_IPv6Addr* addr)
+{
+    if (range  &&  addr) {
+        unsigned int a, b, ip;
+
+        if (range->type == eIPRange_Host) {
+            assert(!range->b);
+            assert(sizeof(range->a) == sizeof(*addr));
+            return !memcmp(&range->a, addr, sizeof(range->a));
+        }
+        switch (range->type) {
+        case eIPRange_Range:
+            assert(NcbiIsIPv4(&range->a));
+            if (!NcbiIsIPv4(addr))
+                return 0/*false*/;
+            /* all host byte order */
+            a  = SOCK_NetToHostLong(NcbiIPv6ToIPv4(&range->a, 0));
+            b  = SOCK_NetToHostLong(                range->b    );
+            ip = SOCK_NetToHostLong(NcbiIPv6ToIPv4( addr,     0));
+            assert(a <= b  &&  ip);
+            return a <= ip  &&  ip <= b;
+        case eIPRange_Network:
+            a = NcbiIsIPv4(&range->a);
+            b = NcbiIsIPv4(addr);
+            if (!(a & b)) {
+                return a ^ b
+                    ? 0/*false*/
+                    : NcbiIsInIPv6Network(&range->a, range->b, addr);
+            }
+            /* all network byte order */
+            a  = NcbiIPv6ToIPv4(&range->a, 0);
+            b  =                 range->b;
+            ip = NcbiIPv6ToIPv4( addr,     0);
+            assert(a  &&  b  &&  ip);
+            return !((ip & b) ^ a);
+        default:
+            assert(0);
+            /*FALLTHRU*/
+        case eIPRange_None:
+        case eIPRange_Application:
+            break;
+        }
+    }
+    return 0/*false*/;
+}
+
+
+SIPRange NcbiTrueIPRange(const SIPRange* range)
+{
+    SIPRange retval;
+    if (range) {
+        switch (range->type) {
+        default:
+            assert(0);
+            /*FALLTHRU*/
+        case eIPRange_None:
+        case eIPRange_Application:
+            memset(&retval, 0, sizeof(retval));
+            /*retval.type = eIPRange_None;*/
+            return retval;
+        case eIPRange_Host:
+            assert(!range->b);
+            if (!NcbiIsIPv4(&range->a)) {
+                retval = *range;
+                return retval;
+            }
+            retval.a =                  range->a;
+            retval.b =  NcbiIPv6ToIPv4(&range->a, 0);
+            break;
+        case eIPRange_Range:
+            assert(NcbiIsIPv4(&range->a)  &&  range->b);
+            retval.a =  range->a;
+            retval.b =  range->b;
+            break;
+        case eIPRange_Network:
+            assert(range->b);
+            if (!NcbiIsIPv4(&range->a)) {
+                retval = *range;
+                return retval;
+            }
+            retval.a =                  range->a;
+            retval.b = (NcbiIPv6ToIPv4(&range->a, 0) & range->b) | ~range->b;
+            break;
+        }
+        retval.type = eIPRange_Range;
+    } else
+        memset(&retval, 0, sizeof(retval));
+    return retval;
+}
+
+
+#ifdef __GNUC__
+inline
+#endif  /*__GNUC__*/
+static size_t x_size(const char* dst, size_t len, const char* ptr)
+{
+    return len > (size_t)(ptr - dst) ? len - (size_t)(ptr - dst) : 0; 
+}
+ 
+
+const char* NcbiDumpIPRange(const SIPRange* range, char* buf, size_t bufsize)
+{
+    char result[150];
+
+    if (!range  ||  !buf  ||  !bufsize)
+        return 0;
+
+    if (range->type == eIPRange_Application) {
+        *buf = '\0';
+        return buf;
+    }
+
+    if (range->type != eIPRange_None) {
+        char* s = result;
+        SIPRange temp = NcbiTrueIPRange(range);
+        switch (range->type) {
+        case eIPRange_Host:
+            assert(!range->b);
+            strcpy(s, "Host");
+            s += 4;
+            break;
+        case eIPRange_Range:
+            assert(range->b);
+            assert(NcbiIsIPv4(&range->a));
+            strcpy(s, "Range");
+            s += 5;
+            break;
+        case eIPRange_Network:
+            assert(range->b);
+            strcpy(s, "Network");
+            s += 7;
+            break;
+        default:
+            assert(0);
+            return 0;
+        }
+        *s++ = ' ';
+        if (!NcbiIsIPv4(&range->a)) {
+            assert(range->type != eIPRange_Range);
+            s = NcbiIPv6ToString(s, x_size(result, sizeof(result), s),
+                                 &range->a);
+            assert(s + 40 < result + sizeof(result));
+            if (s  &&  range->type == eIPRange_Network)
+                sprintf(s, "/%u", range->b);
+        } else {
+            if (SOCK_ntoa(NcbiIPv6ToIPv4(&range->a, 0),
+                          s, x_size(result, sizeof(result), s)) != 0) {
+                strcpy(s++, "?");
+            } else
+                s += strlen(s);
+            if (range->type != eIPRange_Host) {
+                *s++ = '-';
+                if (SOCK_ntoa(temp.b,
+                              s, x_size(result, sizeof(result), s)) != 0) {
+                    strcpy(s, "?");
+                }
+            }
+        }
+    } else
+        strcpy(result, "None");
+
+    return strncpy0(buf, result, bufsize - 1);
+}
+
+
+int/*bool*/ NcbiParseIPRange(SIPRange* range, const char* str)
+{
+    unsigned int addr, temp;
+    const char* t;
+    char* e;
+    long d;
+
+    if (!range  ||  !str)
+        return 0/*failure*/;
+
+    if (!*str) {
+        memset(range, 0, sizeof(*range));
+        /*range->type = eIPRange_None;*/
+        return 1/*success*/;
+    }
+
+    t = NcbiStringToAddr(&range->a, str, strlen(str));
+    if (t  &&  t != str  &&  *t++ == '/'  &&  !isspace((unsigned char)(*t))) {
+        errno = 0;
+        d = strtol(t, &e, 10);
+        if (!errno  &&  t != e  &&  !*e  &&  d > 0) {
+            int/*bool*/ ipv4 = NcbiIsIPv4(&range->a);
+            if (ipv4  &&  d <= 32) {
+                if (!d  ||  d == 32) {
+                    assert(!range->b);
+                    range->type = eIPRange_Host;
+                    return 1/*success*/;
+                }
+                addr = SOCK_NetToHostLong(NcbiIPv6ToIPv4(&range->a, 0));
+                temp = ~0UL << (32 - d);
+                range->b    = SOCK_HostToNetLong(temp);
+                range->type = eIPRange_Network;
+                return addr  &&  !(addr & ~temp);
+            } else if (!ipv4  &&  d <= (sizeof(range->a.octet) << 3)) {
+                if (!d  ||  d == (sizeof(range->a.octet) << 3)) {
+                    assert(!range->b);
+                    range->type = eIPRange_Host;
+                    return 1/*success*/;
+                }
+                range->b    = d;
+                range->type = eIPRange_Network;
+                return !NcbiIsEmptyIPv6(&range->a);
+            } else
+                return 0/*failure*/;
+        }
+    }
+
+    if (!SOCK_isip(str)) {
+        int dots = 0;
+        const char* p = str;
+        range->type = eIPRange_Host;
+        addr = 0/*not actually necessary*/;
+        for (;;) {
+            char s[4];
+            if (*p != '*') {
+                errno = 0;
+                d = strtol(p, &e, 10);
+                if (errno  ||  p == e  ||  e - p > 3  ||  d < 0  ||  255 < d
+                    ||  sprintf(s, "%u", (unsigned int) d) != (int)(e - p)) {
+                    break/*goto out*/;
+                }
+                p = e;
+            } else if (!*++p  &&  dots) {
+                temp = (4 - dots) << 3;
+                addr <<= temp;
+                NcbiIPv4ToIPv6(&range->a, SOCK_HostToNetLong(addr), 0);
+                range->b    = SOCK_HostToNetLong(~0UL << temp);
+                range->type = eIPRange_Network;
+                return 1/*success*/;
+            } else
+                return 0/*failure (asterisks not valid in hostnames)*/;
+            switch (range->type) {
+            case eIPRange_Host:
+                addr <<= 8;
+                addr  |= d;
+                if (*p != '.') {
+                    addr <<= (3 - dots) << 3;
+                    switch (*p) {
+                    case '/':
+                        range->type = eIPRange_Network;
+                        break;
+                    case '-':
+                        range->type = eIPRange_Range;
+                        p++;
+                        continue;
+                    default:
+                        goto out;
+                    }
+                } else if (++dots <= 3) {
+                    p++;
+                    continue;
+                } else
+                    goto out;
+                assert(*p == '/'  &&  range->type == eIPRange_Network);
+                t = NcbiStringToIPv4(&range->b, ++p, 0);
+                if (!t  ||  *t)
+                    continue;
+                NcbiIPv4ToIPv6(&range->a, SOCK_HostToNetLong(addr), 0);
+                if (!range->b  ||  !(temp = ~SOCK_NetToHostLong(range->b))) {
+                    range->b    = 0;
+                    range->type = eIPRange_Host;
+                    return 1/*success*/;
+                }
+                return !(temp & (temp + 1))  &&  addr  &&  !(addr & temp);
+            case eIPRange_Range:
+                if (*p)
+                    goto out;
+                temp  = dots > 0 ? addr : 0;
+                temp &= ~((1 << ((4 - dots) << 3)) - 1);
+                temp |=    d << ((3 - dots) << 3);
+                temp |=   (1 << ((3 - dots) << 3)) - 1;
+                NcbiIPv4ToIPv6(&range->a, SOCK_HostToNetLong(addr), 0);
+                if (addr == temp) {
+                    assert(!range->b);
+                    range->type = eIPRange_Host;
+                    return 1/*success*/;
+                }
+                range->b = SOCK_HostToNetLong(temp);
+                return addr < temp;
+            case eIPRange_Network:
+                if (*p  ||  d > 32)
+                    return 0/*failure (slashes not valid in hostnames)*/;
+                NcbiIPv4ToIPv6(&range->a, SOCK_HostToNetLong(addr), 0);
+                if (!d  ||  d == 32) {
+                    range->b    = 0;
+                    range->type = eIPRange_Host;
+                    return 1/*success*/;
+                }
+                temp = ~0UL << (32 - d);
+                range->b = SOCK_HostToNetLong(temp);
+                return addr  &&  !(addr & ~temp);
+            default:
+                assert(0);
+                return 0/*failure*/;
+            }
+        }
+    out:
+        /* last resort (and maybe expensive one): try as a regular host name */
+        ;
+    } else { /* NB: SOCK_gethostbyname() returns 0 on an unknown host */
+        size_t n;
+        if (strcasecmp(str, "255.255.255.255") == 0) {
+            addr = (unsigned int)(~0UL);
+            NcbiIPv4ToIPv6(&range->a, addr, 0);
+            range->b    = 0;
+            range->type = eIPRange_Host;
+            return 1/*success*/;
+        }
+        for (n = 0;  n < 4;  ++n) {
+            size_t len = 1 + (n << 1);
+            if (strncmp(str, "0.0.0.0", len) == 0  &&  !str[len]) {
+                NcbiIPv4ToIPv6(&range->a, 0, 0);
+                range->b    = 0;
+                range->type = eIPRange_Host;
+                return 1/*success*/;
+            }
+        }
+        /* forbid other misleading IP representations */
+        if (!SOCK_isipEx(str, 1/*fullquad*/))
+            return 0/*failure*/;
+    }
+
+    if (!(addr = SOCK_gethostbyname(str)))
+        return 0/*failure*/;
+    NcbiIPv4ToIPv6(&range->a, addr, 0);
+    range->b    = 0;
+    range->type = eIPRange_Host;
+    return 1/*success*/;
+}
---
+++ src/connect/ncbi_localip.c
@@ -0,0 +1,396 @@
+/* $Id$
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Author:  Anton Lavrentiev
+ *
+ * File Description:
+ *   Determine IP locality (within NCBI) of a given address
+ *
+ */
+
+#include "ncbi_ansi_ext.h"
+#include "ncbi_assert.h"
+#include "ncbi_priv.h"
+#include <connect/ncbi_connutil.h>
+#include <connect/ncbi_connection.h>
+#include <connect/ncbi_file_connector.h>
+#include <connect/ncbi_http_connector.h>
+#include <connect/ncbi_iprange.h>
+#include <connect/ncbi_localip.h>
+#include <errno.h>
+#include <limits.h>
+#include <stdlib.h>
+#if   defined(NCBI_OS_UNIX)
+#  include <unistd.h>
+#  include <sys/types.h>
+#  include <netinet/in.h>
+#  include <sys/param.h>
+#elif defined(NCBI_OS_MSWIN)
+#  include <io.h>
+#  ifndef   R_OK
+#    define R_OK  2
+#  endif /*!R_OK*/
+#endif /*NCBI_OS_...*/
+
+#define NCBI_USE_ERRCODE_X   Connect_LocalNet
+
+
+#ifndef   INADDR_LOOPBACK
+#  define INADDR_LOOPBACK  0x7F000001
+#endif /*!INADDR_LOOPBACK*/
+
+#if defined(NCBI_OS_MSWIN)  &&  !defined(PATH_MAX)
+#  ifdef _MAX_PATH
+#    define PATH_MAX  _MAX_PATH
+#else
+#    define PATH_MAX  1024
+#  endif /*_MAX_PATH*/
+#endif /*NCBI_OS_MSWIN && !PATH_MAX*/
+
+#if PATH_MAX < 256
+#  define BUFSIZE  256
+#else
+#  define BUFSIZE  PATH_MAX
+#endif /*PATHMAX<256*/
+
+#ifdef    SizeOf
+#  undef  SizeOf
+#endif  /*SizeOf*/
+#define   SizeOf(a)  (sizeof(a) / sizeof((a)[0]))
+
+#if defined(_DEBUG)  &&  !defined(NDEBUG)
+/*#  define NCBI_LOCALIP_DEBUG 1*/
+#endif /*_DEBUG && !NDEBUG*/
+
+
+static int/*bool*/ s_Inited = 0/*false*/;
+
+
+static SIPRange s_LocalIP[256 + 1] = { { eIPRange_None } };
+
+
+#ifdef HAVE_LIBCONNEXT
+
+
+#  include "ext/ncbi_localip.c"
+
+
+static const SIPRange* s_IsOverlappingRange(const SIPRange* range, size_t n)
+{
+    size_t i;
+    assert(n < SizeOf(s_LocalIP));
+    for (i = 0;  i < n;  ++i) {
+        if (NcbiIsInIPRange(&s_LocalIP[i], &range->a)  ||
+            NcbiIsInIPRange(range, &s_LocalIP[i].a)) {
+            return &s_LocalIP[i];
+        }
+    }
+    return 0/*no overlap*/;
+}
+
+
+static int/*bool*/ xx_LoadLocalIPs(CONN conn)
+{
+    unsigned int domain;
+    EIO_Status status;
+    char buf[128+1];
+    size_t n, len;
+    int lineno;
+
+    /* localnet */
+    NcbiIPv4ToIPv6(&s_LocalIP[0].a, SOCK_HostToNetLong(kLocalIP[0].a), 0);
+    s_LocalIP[0].b    = SOCK_HostToNetLong(kLocalIP[0].b);
+    s_LocalIP[0].type = kLocalIP[0].t;
+    /* IPv6 localhost, ::1 */
+    memset(&s_LocalIP[1], 0, sizeof(s_LocalIP[1]));
+    s_LocalIP[1].a.octet[sizeof(s_LocalIP[1].a.octet) - 1] = 1;
+    s_LocalIP[1].type = eIPRange_Host;
+
+    domain = 0;
+    lineno = 0;
+    n = 2/*localnet(s) always added*/;
+    while ((status = CONN_ReadLine(conn, buf, sizeof(buf) - 1, &len))
+           == eIO_Success) {
+        SIPRange local;
+        char* c, *err;
+
+        ++lineno;
+        if (!len)
+            continue;
+        if (buf[len - 1] == '\r')
+            --len;
+        buf[len] = '\0';
+        if (!(len = strcspn(buf, "!#")))
+            continue;
+        if (!*(c = buf + strspn(buf, " \t")))
+            continue;
+        len = strcspn(c, " \t");
+        err = c + len;
+        if (*err  &&  !*(err += strspn(err, " \t")))
+            c[len] = '\0';
+        if (!*err  &&  len  &&  *c == '['  &&  c[--len] == ']') {
+            c[len] = '\0';
+            if (len > sizeof(local.a.octet) - 1)
+                len = sizeof(local.a.octet) - 1;
+            strncpy0((char*) local.a.octet, ++c, len);
+            for (len = 2;  len < n;  ++len) {
+                if (s_LocalIP[len].type == eIPRange_Application
+                    &&  strcasecmp((const char*) s_LocalIP[len].a.octet,
+                                   (const char*) local.a.octet) == 0) {
+                    CORE_LOGF_X(4, eLOG_Error,
+                                ("Ignoring duplicate domain at line %u: '%s'",
+                                 lineno, c));
+                    break;
+                }
+            }
+            if (len < n)
+                continue;
+            local.b = ++domain;
+            local.type = eIPRange_Application;
+        } else {
+            const SIPRange* over;
+            if (*err  ||  !NcbiParseIPRange(&local, c)
+                || (local.type == eIPRange_Host && NcbiIsEmptyIPv6(&local.a))){
+                if (!*err)
+                    err = c;
+                CORE_LOGF_X(2, eLOG_Error,
+                            ("Ignoring invalid local IP spec at line %u: '%s'",
+                             lineno, err));
+                continue;
+            }
+            if ((over = s_IsOverlappingRange(&local, n)) != 0) {
+                char temp[150];
+                const char* tmp
+                    = strchr(NcbiDumpIPRange(over, temp, sizeof(temp)), ' ');
+                if (!tmp++)
+                    tmp = temp;
+                CORE_LOGF_X(4, eLOG_Warning,
+                            ("Local IP spec at line %u, '%s' overlaps with"
+                             " already defined one: %s", lineno, c, tmp));
+            }
+        }
+        assert(local.type != eIPRange_None);
+        if (n >= SizeOf(s_LocalIP)) {
+            CORE_LOGF_X(3, eLOG_Error,
+                        ("Too many local IP specs, max %u allowed",
+                         (unsigned int)(n - 2/*localnet*/)));
+            break;
+        }
+        s_LocalIP[n++] = local;
+    }
+    if (status != eIO_Closed  ||  n < 3)
+        return 0/*false*/;
+
+    if (n < SizeOf(s_LocalIP))
+        s_LocalIP[n].type = eIPRange_None;
+
+    n -= 2;  /* compensate for auto-added localnet(s) */
+    CORE_LOGF(eLOG_Trace, ("Done loading local IP specs, %u line%s, %u entr%s"
+                           " (%u domain%s)", lineno, &"s"[lineno == 1],
+                           (unsigned int) n, n == 1 ? "y" : "ies",
+                           domain, &"s"[domain == 1]));
+    return 1/*true*/;
+}
+
+
+static int/*bool*/ x_LoadLocalIPs(CONNECTOR c, const char* source)
+{
+    CONN conn;
+    int/*bool*/ loaded = 0/*false*/;
+    if (c  &&  CONN_Create(c, &conn) == eIO_Success) {
+        CORE_LOGF(eLOG_Trace,
+                  ("Loading local IP specs from \"%s\"", source));
+        loaded = xx_LoadLocalIPs(conn);
+        CONN_Close(conn);
+    } else if (c  &&  c->destroy)
+        c->destroy(c);
+    return loaded;
+}
+
+
+static void s_LoadLocalIPs(void)
+{
+    static const char* kFile[] = {
+        "/etc/ncbi/local_ips_v2",
+        "/etc/ncbi/local_ips"
+    };
+    static const char kLocalIPs[] = "LOCAL_IPS";
+    char buf[PATH_MAX + 1];
+    const char* file = ConnNetInfo_GetValue(0, kLocalIPs,
+                                            buf, sizeof(buf) - 1, "");
+    size_t n;
+
+    if (file  &&  strcasecmp(file, "--HARDCODED--") != 0) {
+        SConnNetInfo* net_info;
+        ELOG_Level level;
+        for (n = 0;  n < SizeOf(kFile);  ++n) {
+            if (n  ||  !*file)
+                file = kFile[n];
+            if (access(file, R_OK) == 0  &&
+                x_LoadLocalIPs(FILE_CreateConnector(file, 0), file)) {
+                return;
+            }
+            if (errno == ENOENT  &&  file != buf) {
+#  ifdef NCBI_OS_LINUX
+                level = n ? eLOG_Warning : eLOG_Trace;
+#  else
+                level = eLOG_Trace;
+#  endif /*NCBI_OS_LINUX*/
+            } else
+                level = eLOG_Error;
+            CORE_LOGF_ERRNO_X(1, level, errno,
+                              ("Cannot load local IP specs from \"%s\"",file));
+            if (file == buf)
+                break;
+        }
+        net_info = ConnNetInfo_Create(kLocalIPs);
+        /* Build HTTP connector here to save on a dispatcher hit */
+        if (ConnNetInfo_SetupStandardArgs(net_info, kLocalIPs)  &&
+            x_LoadLocalIPs(HTTP_CreateConnector(net_info,
+                                                "User-Agent: ncbi_localnet",
+                                                fHTTP_NoAutoRetry |
+                                                fHTTP_SuppressMessages),
+                           kLocalIPs)) {
+            ConnNetInfo_Destroy(net_info);
+            return;
+        }
+        ConnNetInfo_Destroy(net_info);
+        if (net_info) {
+#  ifdef NCBI_OS_LINUX
+            level = eLOG_Trace;
+#  else
+            level = eLOG_Warning;
+#  endif /*NCBI_OS_LINUX*/
+        } else
+            level = eLOG_Error;
+        CORE_LOGF_X(1, level,
+                    ("Cannot load local IP specs from \"%s\"", kLocalIPs));
+    }
+
+    CORE_LOG(eLOG_Trace, "Using default local IPv4 specs");
+    assert(SizeOf(s_LocalIP) > SizeOf(kLocalIP));
+    for (n = 0;  n < SizeOf(kLocalIP);  ++n) {
+        s_LocalIP[n].type = kLocalIP[n].t;
+        NcbiIPv4ToIPv6(&s_LocalIP[n].a,
+                       SOCK_HostToNetLong(kLocalIP[n].a), 0);
+        s_LocalIP[n].b    = SOCK_HostToNetLong(kLocalIP[n].b);
+    }
+    s_LocalIP[n].type = eIPRange_None;
+}
+
+
+#else
+
+
+#define s_LoadLocalIPs()  /*void*/
+
+
+#endif /*HAVE_LIBCONNEXT*/
+
+
+extern void NcbiInitLocalIP(void)
+{
+    /*CORE_LOCK_WRITE;*/
+    s_Inited = 0/*F*/;
+    /*CORE_UNLOCK;*/
+}
+
+
+extern int/*bool*/ NcbiIsLocalIPEx(const TNCBI_IPv6Addr* addr,
+                                   SNcbiDomainInfo*      info)
+{
+    size_t n;
+
+    if (!s_Inited) {
+        CORE_LOCK_WRITE;
+        if (!s_Inited) {
+            s_LoadLocalIPs();
+            s_Inited = 1;
+            CORE_UNLOCK;
+#ifdef NCBI_LOCALIP_DEBUG
+            for (n = 0;  n < SizeOf(s_LocalIP);  ++n) {
+                char buf[150];
+                const char* result;
+                if (s_LocalIP[n].type == eIPRange_Application) {
+                    assert(s_LocalIP[n].b);
+                    sprintf(buf, "Domain %u: %s",
+                            s_LocalIP[n].b, s_LocalIP[n].a.octet);
+                    assert(strlen(buf) < sizeof(buf));
+                    result = buf;
+                } else
+                    result = NcbiDumpIPRange(s_LocalIP + n, buf, sizeof(buf));
+                if (result)
+                    CORE_LOG_X(1, eLOG_Trace, result);
+                if (s_LocalIP[n].type == eIPRange_None)
+                    break;
+            }
+#endif /*NCBI_LOCALIP_DEBUG*/
+        } else
+            CORE_UNLOCK;
+    }
+
+    if (addr) {
+        SNcbiDomainInfo x_info;
+        memset(&x_info, 0, sizeof(x_info));
+        for (n = 0;  n < SizeOf(s_LocalIP);  ++n) {
+            if (s_LocalIP[n].type == eIPRange_None)
+                break;
+            if (s_LocalIP[n].type == eIPRange_Application) {
+                x_info.sfx = (const char*) s_LocalIP[n].a.octet;
+                x_info.num = s_LocalIP[n].b;
+                continue;
+            }
+            if (NcbiIsInIPRange(s_LocalIP + n, addr)) {
+                if ( info )
+                    *info = x_info;
+                return 1/*true*/;
+            }
+        }
+    }
+
+    if (info)
+        memset(info, 0, sizeof(*info));
+    return 0/*false*/;
+}
+
+
+extern int/*bool*/ NcbiIsLocalIP(unsigned int ip)
+{
+    if (ip) {
+        unsigned int addr = SOCK_NetToHostLong(ip);
+        if (
+#ifdef IN_BADCLASS
+            !IN_BADCLASS(addr)
+#else
+            (addr & 0xF0000000) ^ 0xF0000000
+#endif /*IN_BADCLASS*/
+            ) {
+            TNCBI_IPv6Addr temp;
+            NcbiIPv4ToIPv6(&temp, ip, 0);
+            return NcbiIsLocalIPEx(&temp, 0);
+        }
+    }
+    return 0/*false*/;
+}
