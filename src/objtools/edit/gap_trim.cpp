cf69749266335b84612f915eb893176e0eda37db
Colleen Bollin <bollin@ncbi.nlm.nih.gov>
2017-06-15 16:03:36 +0000

    JIRA:GB-7017 fixed bug in detecting gaps in features
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@78401 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/edit/gap_trim.cpp
+++ src/objtools/edit/gap_trim.cpp
@@ -80,7 +80,7 @@ void CFeatGapInfo::CollectGaps(const CSeq_loc& feat_loc, CScope& scope)
 
     CSeqMap_CI seq_map_ci = seq_map->ResolvedRangeIterator(&scope,
         0,
-        objects::sequence::GetLength(feat_loc, &scope),
+        m_Stop - m_Start + 1,
         feat_loc.GetStrand(),
         size_t(-1),
         objects::CSeqMap::fFindGap | objects::CSeqMap::fFindData);
