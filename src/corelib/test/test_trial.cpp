0005df47d8fa9e6f73cf0615eccb54dcd0b90c89
Denis Vakatov <vakatov@ncbi.nlm.nih.gov>
2017-06-22 15:37:34 +0000

    Move some "must break in compile time" tests to 'test_trial_fail.cpp'.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@78473 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/corelib/test/test_trial.cpp
+++ src/corelib/test/test_trial.cpp
@@ -1,3 +1,36 @@
+/*  $Id$
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Author:  Eugene Vasilchenko
+ *
+ * File Description:
+ *   Test miscellaneous functionalities
+ *
+ */
+
+
 #define NCBI_TEST_APPLICATION
 #include <ncbi_pch.hpp>
 #include <corelib/ncbicfg.h>
@@ -221,9 +254,6 @@ int CTestApplication::Run(void)
     _ASSERT(toupper('A') == 'A');
     _ASSERT(std::toupper('A') == 'A');
     _ASSERT(::toupper('A') == 'A');
-#if 0 // should fail
-    _ASSERT(::toupper('A'=='A'));
-#endif
     TGi gi = 2;
     gi = 3;
     gi = INVALID_GI;
