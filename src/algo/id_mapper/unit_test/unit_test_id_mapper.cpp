aa38a98ac7078cb4476fab8622a9a3dfaefe5777
Nathan Bouk <boukn@ncbi.nlm.nih.gov>
2017-07-31 19:30:49 +0000

    Backing gencoll changed, changing the expecting results of TestCaseGetSynonyms
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@78951 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/algo/id_mapper/unit_test/unit_test_id_mapper.cpp
+++ src/algo/id_mapper/unit_test/unit_test_id_mapper.cpp
@@ -646,14 +646,14 @@ BOOST_AUTO_TEST_CASE(TestCaseGetSynonyms)
     // Check default NCBI-Only synonyms
     Mapper.GetSynonyms(*OrigId, Synonyms);
 
-    BOOST_CHECK_EQUAL(Synonyms.size(), (size_t)5);
+    BOOST_CHECK_EQUAL(Synonyms.size(), (size_t)4);
 
 
     // Check all synonyms
     Synonyms.clear();
     Mapper.GetSynonyms(*OrigId, Synonyms, false);
     
-    BOOST_CHECK_EQUAL(Synonyms.size(), (size_t)7);
+    BOOST_CHECK_EQUAL(Synonyms.size(), (size_t)6);
 }
 
 
