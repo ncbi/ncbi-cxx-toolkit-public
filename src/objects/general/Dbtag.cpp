d754b5923ac048314a812eddeb8422b5ee77d1d0
Jonathan Kans <kans@ncbi.nlm.nih.gov>
2017-08-08 21:46:33 +0000

    JIRA:SQD-4349 Updated PeptideAtlas URL
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@79064 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objects/general/Dbtag.cpp
+++ src/objects/general/Dbtag.cpp
@@ -697,7 +697,7 @@ static const TDbtUrl sc_url_prefix[] = {
     { CDbtag::eDbtagType_Araport, "https://www.araport.org/locus/" },
     { CDbtag::eDbtagType_VGNC, "http://vertebrate.genenames.org/data/gene-symbol-report/#!/vgnc_id/VGNC:" }, // https not available tested 7/13/2016
     { CDbtag::eDbtagType_RNAcentral, "http://rnacentral.org/rna/" },
-    { CDbtag::eDbtagType_PeptideAtlas, "https://db.systemsbiology.net/sbeams/cgi/PeptideAtlas/GetPeptide?searchWithinThis=Peptide%2bName&action=QUERY&searchForThis=" },
+    { CDbtag::eDbtagType_PeptideAtlas, "https://db.systemsbiology.net/sbeams/cgi/PeptideAtlas/Search?action=GO&search_key=" },
 };
 
 typedef CStaticPairArrayMap<CDbtag::EDbtagType, const char*> TUrlPrefixMap;
