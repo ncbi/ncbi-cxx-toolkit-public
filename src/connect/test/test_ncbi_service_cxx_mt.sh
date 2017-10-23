494825d5c97b9c081fa4f6ded9028c18651dbd8c
David McElhany <mcelhany@ncbi.nlm.nih.gov>
2017-10-23 13:53:09 +0000

    Add MT wrapper script for service_cxx test.  JIRA: CXX-9631
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@79925 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ src/connect/test/test_ncbi_service_cxx_mt.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+# $Id$
+
+# Default to a basic test service.
+: ${service:=bounce}
+
+# Test the service using a pseudo-random number of threads (between 2 and 11).
+nthreads="`expr $$ % 10 + 2`"
+
+if test -n "$CHECK_EXEC"; then
+    $CHECK_EXEC test_ncbi_service_cxx_mt -threads $nthreads -service $service
+else
+    ./test_ncbi_service_cxx_mt -threads $nthreads -service $service
+fi
