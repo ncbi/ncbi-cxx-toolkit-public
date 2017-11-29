31e29416aedeafa044e59ed5aca82c5a18dc8575
Michael DiCuccio <dicuccio@ncbi.nlm.nih.gov>
2017-11-29 14:13:35 +0000

    Always add BDB and SQLite3 to include search paths, since we use non-standard locations for these JIRA: GP-22569
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@80335 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/build-system/cmake/CMakeChecks.sqlite3.cmake
+++ src/build-system/cmake/CMakeChecks.sqlite3.cmake
@@ -3,6 +3,10 @@ include(CheckIncludeFile)
 
 find_package(Sqlite3)
 
+set(SQLITE3_INCLUDE ${SQLITE3_INCLUDE_DIR})
+set(SQLITE3_LIBS ${SQLITE3_LIBRARY})
+set(SQLITE3_STATIC_LIBS ${SQLITE3_LIBS})
+
 if (SQLITE3_FOUND)
     set(CMAKE_REQUIRED_LIBRARIES_OLD "${CMAKE_REQUIRED_LIBRARIES}")
     set(CMAKE_REQUIRED_LIBRARIES "${CMAKE_REQUIRED_LIBRARIES} ${SQLITE3_LIBRARY}")
@@ -13,10 +17,13 @@ if (SQLITE3_FOUND)
     if (HAVE_SQLITE3ASYNC_H)
         message(STATUS "  SQLite3: Found sqlite3async.h")
     endif()
-endif(SQLITE3_FOUND)
 
-set(SQLITE3_INCLUDE ${SQLITE3_INCLUDE_DIR})
-set(SQLITE3_LIBS ${SQLITE3_LIBRARY})
-set(SQLITE3_STATIC_LIBS ${SQLITE3_LIBS})
+    #
+    # As a blanket statement, we now include SQLite3 everywhere
+    # This avoids a serious insidious version skew if we have both the
+    # system-installed SQLite3 libraries and a custom version of SQLite3
+    include_directories(SYSTEM ${SQLITE3_INCLUDE})
+
+endif(SQLITE3_FOUND)
 
 
