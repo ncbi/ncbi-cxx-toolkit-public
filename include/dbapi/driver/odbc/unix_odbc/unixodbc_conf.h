062a0ecb963b4bd8fdeb3da358b5df8a3347ea7e
Aaron Ucko <ucko@ncbi.nlm.nih.gov>
2017-12-28 22:15:19 +0000

    unixodbc_conf.h: +HAVE_LONG_LONG per SIZEOF_LONG_LONG.  JIRA: CXX-9729.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@80716 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/dbapi/driver/odbc/unix_odbc/unixodbc_conf.h
+++ include/dbapi/driver/odbc/unix_odbc/unixodbc_conf.h
@@ -3,5 +3,8 @@
 
 #include <ncbiconf.h>
 #define SIZEOF_LONG_INT SIZEOF_LONG
+#ifdef SIZEOF_LONG_LONG
+#  define HAVE_LONG_LONG 1
+#endif
 
 #endif /* DBAPI_DRIVER_ODBC_UNIX_ODBC___UNIXODBC_CONF__H */
