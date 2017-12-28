dd096f865ef5471528d1907f0be4a53279071978
Aaron Ucko <ucko@ncbi.nlm.nih.gov>
2017-12-28 16:14:03 +0000

    blk.c (_blk_get_col_data): Belatedly unify NULL handling.
    Treat NULL BLOBs (with no column_lenbind) like other NULL columns;
    CS_BLK_HAS_TEXT doesn't entirely make sense for them.
    
    JIRA: CXX-9729.
    
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@80634 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/dbapi/driver/ftds95/freetds/ctlib/blk.c
+++ src/dbapi/driver/ftds95/freetds/ctlib/blk.c
@@ -762,9 +762,9 @@ _blk_get_col_data(TDSBCPINFO *bulk, TDSCOLUMN *bindcol, int offset)
             src = bindcol->column_default;
             srctype = _ct_get_client_type(blkdesc->con->ctx, bindcol);
             datalen = &bindcol->column_def_size;
-        } else if (is_blob_col(bindcol)) {
+        } else if (is_blob_col(bindcol)  &&  bindcol->column_lenbind != NULL) {
             bindcol->bcp_column_data->datalen = *datalen;
-            bindcol->bcp_column_data->is_null = !bindcol->column_lenbind;
+            bindcol->bcp_column_data->is_null = 0;
             /* Data will come piecemeal, via blk_textxfer. */
             return CS_BLK_HAS_TEXT;
         } else {
