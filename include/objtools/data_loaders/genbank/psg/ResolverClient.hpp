789b1cf5bc88cbbcdc01db8903a554dac8cd2e9b
Rafael Sadyrov <sadyrovr@ncbi.nlm.nih.gov>
2018-01-25 20:23:20 +0000

    Added original code with included <ncbi_pch.hpp>. JIRA CXX-9865
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@80970 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ include/objtools/data_loaders/genbank/psg/ResolverClient.hpp
@@ -0,0 +1,324 @@
+#ifndef _RESOLVER_CLIENT_HPP_
+#define _RESOLVER_CLIENT_HPP_
+
+#include <string>
+#include <utility>
+#include <mutex>
+
+#include <objects/id2/ID2_Blob_Id.hpp>
+#include <objects/id2/ID2_Reply_Get_Blob_Id.hpp>
+#include <corelib/ncbitime.hpp>
+
+namespace HCT {
+    class io_future;
+    class http2_request;
+};
+
+BEGIN_NCBI_SCOPE
+
+constexpr const char ACCVER_RESOLVER_SERVICE_ID[] = "ID.RESOLVER.ACCVER.1";
+constexpr const char ACCVER_RESOLVER_COLUMNS[] = "ACCVER CVARCHAR NOTNULL KEY, GI INT8, LEN UINT4, SAT INT2, SAT_KEY UINT4, TAXID UINT4, DATE DATETIME, SUPPRESS BIT NOTNULL";
+//                                                                             0        1          2          3              4            5              6
+
+BEGIN_objects_SCOPE
+
+/// Base class for the user-defined context
+class IBioIdContext
+{
+public:
+    virtual ~IBioIdContext() {}
+};
+
+
+/// Bio-id + context
+class CBioId
+{
+public:
+    CBioId(const std::string& id, IBioIdContext* ctx = nullptr)
+        : m_Id(id), m_Context(ctx) {}
+    CBioId(std::string&& id, IBioIdContext* ctx = nullptr)
+        : m_Id(std::move(id)), m_Context(ctx) {}
+
+    const std::string& GetId() const { return m_Id; }
+    const IBioIdContext* GetContext() const  { return m_Context; }
+
+private:
+    std::string    m_Id;
+    IBioIdContext* m_Context;
+};
+
+class CBioIdResolutionQueue;
+
+/// The generic part of the results of id resolution.
+///
+class CBlobId
+{
+public:
+    explicit CBlobId(const CBioId& BioId) 
+        : m_BioId(BioId), m_Status(eResNone), m_StatusEx(eNone) {}
+    /// Storage blob id (as accepted by the CBlobRetrieveQueue)
+    struct SBlobId {
+        CID2_Blob_Id::TSat     sat;
+        CID2_Blob_Id::TSat_key sat_key;
+        // CID2_Blob_Id::TSub_sat sub_sat;
+        CID2_Blob_Id::TVersion version;
+        SBlobId()
+            : sat(0), sat_key(0), version(0) {}
+    };
+
+    /// Get storage blob id (as accepted by the CBlobRetrieveQueue)
+    /// @throw
+    ///  If not in "eResolved" state
+    const SBlobId& GetBlobId() const { return m_BlobInfo.blob_id; }
+
+    /// Blob storage location and other attributes
+    struct SBlobInfo {
+        SBlobId                              blob_id;
+        CID2_Reply_Get_Blob_Id::TBlob_state  state;
+        TGi                                  gi;          // informational only
+        time_t                               date_queued; // informational only
+        TTaxId                               tax_id;
+        Uint8                                seq_length;
+        SBlobInfo() 
+            : state(0), gi(0), tax_id(0), seq_length(0) {}
+    };
+
+    /// Get info about the blob (obtained as a result of its id resolving)
+    /// @throw
+    ///  If not in "eResolved" state
+    const SBlobInfo& GetBlobInfo() const { return m_BlobInfo; }
+
+    /// Resolution result
+    /// @sa GetStatus
+    enum EStatus {
+        eResNone,    ///< Resolution is not performed
+        eResolved,   ///< Successfully resolved
+        eFailed      ///< Resolution has failed
+    };
+
+    /// Get result of this id resolution
+    EStatus GetStatus() const { return m_Status; }
+
+    /// Resolution's fine-grain actionable details
+    /// @sa GetStatusEx
+    enum EStatusEx {
+        eNone,       ///< No additional info available
+        eForbidden,  ///< Access denied
+        eNotFound,   ///< Not found
+        eInvalid,    ///< Invalid id
+        eCanceled,   ///< Request canceled
+        eError       ///< Unknown error
+    };
+    /// Get result of this id resolution
+    EStatusEx GetStatusEx() const { return m_StatusEx; }
+
+    /// Unstructured text containing auxiliary info about the result
+    const std::string& GetMessage() const { return m_Message; };
+
+    /// Get the bio-blob id (such as an accession) which this result is for
+    const CBioId& GetBioId() const { return m_BioId; }
+
+
+    // TODO:  ctor, dtor, mover, etc
+
+private:
+    CBioId     m_BioId;
+    SBlobInfo  m_BlobInfo;
+    EStatus    m_Status;
+    EStatusEx  m_StatusEx;
+    std::string  m_Message;
+    friend class CBioIdResolutionQueue;
+};
+
+
+/// A queue to resolve "biological" blob ids (such as accessions) into the
+/// storage specific blob ids.
+///
+/// Add more bio ids to resolve using Resolve(), then get the resolved
+/// blob ids using GetBlobIds().
+///
+/// All methods are MT-safe.
+///
+/// The queue object can be used from more than one thread, either to add ids
+/// or to retrieve the resolved ids, in any order thereof.
+/// Resolution results for the ids which were added to a given instance of
+/// the queue will be available for retrieval using this (and only this) queue
+/// instance regardless of which threads were used to add the ids to the queue.
+
+class CBioIdResolutionQueue
+{
+public:
+    CBioIdResolutionQueue();
+    ~CBioIdResolutionQueue();
+    /// Schedule more bio-ids for resolution
+    /// @note
+    ///  If more than one thread is blocked on adding new set of bio-ids to the
+    ///  queue, then (when the queue gets free slots) one of the threads gets
+    ///  to add all (or part) of its bio-ids to the queue. Other threads will
+    ///  continue waiting (unless there is more free space left).
+    /// @param bio_ids
+    ///  List of bio-blob ids (such as accessions) to resolve.
+    ///  Those bio-blob ids from the "bio_ids" container that make it
+    ///  into the queue will be removed from the "bio_ids" container.
+    /// @param deadline
+    ///  For how long to try to push the bio-ids into the queue.
+    void Resolve(std::vector<CBioId>*  bio_ids,
+                 const CDeadline& deadline = CDeadline(0));
+
+    /// Perform signle bio-ids resolution
+    /// @note
+    /// This method is works synchronously static and does not require 
+    /// CBioIdResolutionQueue instance
+    static CBlobId Resolve(const CBioId& bio_ids,
+                 const CDeadline& deadline = CDeadline(0));
+    static CBlobId Resolve(CBioId&& bio_ids,
+                 const CDeadline& deadline = CDeadline(0));
+
+    /// Retrieve results of the id resolution that are currently ready.
+    /// @note
+    ///  If more than one thread tries to retrieve id resolution results, then
+    ///  one of the threads gets the results as they become ready; other
+    ///  threads will continue waiting. 
+    /// @param deadline
+    ///  Until when to wait for the results to get ready for retrieval.
+    ///  If no resolution results are ready by the deadline expiration time,
+    ///  then return empty container.
+    /// @param max_results
+    ///  Return no more than that number of results. Zero means no limit.
+    /// @return
+    ///  List of id resolution results
+    /// @throw
+    ///  If unrecoverable retrieval error has been detected.
+    std::vector<CBlobId> GetBlobIds(const CDeadline& deadline = CDeadline(0), 
+                                    size_t max_results = 0);
+
+    /// Cancel all ongoing retrievals and return all not yet resolved bio-ids.
+    /// You can continue working with the queue after that in a usual manner.
+    /// If any of the bio-id resolution results are available they can
+    /// be subsequently retrieved by calling GetBlobIds() method.
+    /// @param bio_ids
+    ///  The not yet resolved bio-ids will be added to "bio_ids". If "bio_ids"
+    ///  is NULL, then they be discarded.
+    void Clear(std::vector<CBioId>* bio_ids);
+
+    /// Returns true if Queue has finished all resolutions
+    /// and all items have been fetched or nothing was added for resolution at all
+    bool IsEmpty() const;
+private:
+    mutable std::mutex m_items_mtx;
+    
+    struct CBioIdResolutionQueueItem {
+        CBioIdResolutionQueueItem(std::shared_ptr<HCT::io_future> afuture, CBioId&& BioId);
+        CBioIdResolutionQueueItem(std::shared_ptr<HCT::io_future> afuture, const CBioId& BioId);
+        void SyncResolve(CBlobId& BlobId, const CDeadline& deadline);
+        void WaitFor(long timeout_ms);
+        void Wait();
+        bool IsDone() const;
+        void Cancel();
+        void PopulateData(CBlobId& BlobId) const;
+        std::shared_ptr<HCT::http2_request> m_request;
+        CBioId m_BioId;
+    };
+
+    std::vector<std::unique_ptr<CBioIdResolutionQueueItem>> m_items;
+    std::shared_ptr<HCT::io_future> m_future;
+};
+
+
+
+/// Blob that is ready to get its data retrieved
+class CBlob
+{
+public:
+    /// Blob ID and other non-data ID resolution and retrieval attributes 
+    const CBlobId& GetBlobId() const  { return m_BlobId; };
+
+    /// Blob data retrieval API 
+    // ........
+
+private:
+    CBlobId m_BlobId;
+    // ........
+};
+
+
+
+/// A queue to retrieve bio-blob data from the storage.
+///
+/// Call Retrieve() to schedule more bio-ids (or blob-ids) for retrieval, then
+/// call GetBlobs() to get the blobs that are ready for immediate retrieval.
+///
+/// All methods are MT-safe.
+///
+/// The queue object can be used from more than one thread, either to add ids
+/// or to get the list of ready-to-be-retrieved blobs.
+/// Results for the ids which were added to a given instance of
+/// the queue will be available for retrieval using this (and only this) queue
+/// instance regardless of which threads were used to add the ids to the queue.
+
+class CBlobRetrievalQueue
+{
+public:
+    /// Schedule more bio-ids or blob-ids for the blob retrieval
+    /// @note
+    ///  If more than one thread is blocked on adding new set of bio-ids (or
+    ///  blob-ids) to the queue, then (when the queue gets free slots) one of
+    ///  the threads gets to add all or part of its bio-ids  (or blob-ids) to
+    ///  the queue. Other threads will continue waiting (unless there is more
+    ///  free space left).
+    /// @param bio_ids
+    ///  List of bio-blob ids (such as accessions).
+    ///  Those bio-blob ids from the "bio_ids" container that make it
+    ///  into the queue will be removed from the "bio_ids" container.
+    /// @param deadline
+    ///  For how long to try to push the bio-ids into the queue.
+    void Retrieve(std::vector<CBioId>*  bio_ids,
+                  const CDeadline& deadline = CDeadline(0));
+    /// @param blob_ids
+    ///  List of blob ids.
+    ///  Those blob ids from the "blob_ids" container that make it
+    ///  into the queue will be removed from the "blob_ids" container.
+    void Retrieve(std::vector<CBlobId>* blob_ids,
+                  const CDeadline& deadline = CDeadline(0));
+
+    /// Get blobs that are ready for the immediate retrieval (and/or those that
+    /// cannot be retrieved for whatever reason).
+    /// @note
+    ///  If more than one thread tries to retrieve list of the ready blobs,
+    ///  then one of the threads gets the results as they become ready; other
+    ///  threads will continue waiting. 
+    /// @param deadline
+    ///  Until when to wait for the blobs to get ready for retrieval.
+    ///  If no blobs are ready for retrieval by the deadline expiration, then
+    ///  return empty container.
+    /// @param max_results
+    ///  Return no more than that number of results. Zero means no limit.
+    /// @return
+    ///  List of blobs available for retrieval
+    /// @throw
+    ///  If unrecoverable resolution or retrieval error has been detected.
+    std::vector<CBlob> GetBlobs(const CDeadline& deadline = CDeadline(0), 
+                                size_t max_results = 0);
+
+    /// Cancel all ongoing retrievals and return all bio- and blob-ids
+    /// for which there is no ready-to-retrieve blob obtained yet.
+    /// You can continue working with the queue after that in a usual manner.
+    /// If there are ready-to-be-retrieved blobs available in the queue
+    /// they can be subsequently retrieved by calling GetBlobs() method.
+    /// @param bio_ids
+    ///  The not yet resolved bio-ids will be added to "bio_ids". If "bio_ids"
+    ///  is NULL, then they be discarded.
+    /// @param blob_ids
+    ///  The bio-ids for which no retrievable blobs have yet been obtained will
+    ///  be added to "blob_ids". If "blob_ids" is NULL, then they be discarded.
+    void Clear(std::vector<CBioId>* bio_ids, std::vector<CBlobId>* blob_ids);
+
+    /// Returns true if Queue has finished all retrieval work
+    /// and all items have been fetched or nothing was added for retrieval at all
+    bool IsEmpty() const;
+};
+
+END_objects_SCOPE
+END_NCBI_SCOPE
+
+#endif
---
+++ src/objtools/data_loaders/genbank/psg/ResolverClient.cpp
@@ -0,0 +1,281 @@
+#include <ncbi_pch.hpp>
+
+#include <mutex>
+#include <string>
+#include <chrono>
+#include <thread>
+
+#include <corelib/ncbitime.hpp>
+
+#include "DdRpcCommon.hpp"
+#include "DdRpcClient.hpp"
+#include "DdRpcDataPacker.hpp"
+#include "ResolverClient.hpp"
+#include "HttpClientTransportP.hpp"
+
+
+
+BEGIN_NCBI_SCOPE
+BEGIN_objects_SCOPE
+
+namespace {
+
+long RemainingTimeMs(const CDeadline& deadline)
+{
+    unsigned int sec, nanosec;
+    deadline.GetRemainingTime().GetNano(&sec, &nanosec);
+    return nanosec / 1000000L + sec * 1000L;
+}
+
+static thread_local unique_ptr<DDRPC::DataColumns> s_accver_resolver_data_columns;
+
+void AccVerResolverUnpackData(DDRPC::DataRow& row, const string& data)
+{
+    if (!s_accver_resolver_data_columns) {
+        DDRPC::DataColumns clm;
+        clm.AssignAsText(ACCVER_RESOLVER_COLUMNS);
+        s_accver_resolver_data_columns.reset(new DDRPC::DataColumns(clm.ExtractDataColumns()));
+    }
+    row.Unpack(data, false, *s_accver_resolver_data_columns);
+}
+
+void DumpEntry(const CBlobId& BlobId)
+{
+    cout << BlobId.GetBioId().GetId() << " => sat: " << BlobId.GetBlobId().sat << " => sat_key: " << BlobId.GetBlobId().sat_key << endl;
+}
+
+}
+
+/** CBioIdResolutionQueue::CBioIdResolutionQueueItem */
+
+CBioIdResolutionQueue::CBioIdResolutionQueueItem::CBioIdResolutionQueueItem(std::shared_ptr<HCT::io_future> afuture, CBioId&& BioId) 
+    :   m_request(make_shared<HCT::http2_request>()),
+        m_BioId(std::move(BioId))
+        
+{
+    if (!HCT::HttpClientTransport::s_ioc)
+        DDRPC::EDdRpcException::raise("DDRPC is not initialized, call DdRpcClient::Init() first");
+    m_request->init_request(DDRPC::DdRpcClient::SericeIdToEndPoint(ACCVER_RESOLVER_SERVICE_ID), afuture, "accver=" + m_BioId.GetId());
+}
+
+CBioIdResolutionQueue::CBioIdResolutionQueueItem::CBioIdResolutionQueueItem(std::shared_ptr<HCT::io_future> afuture, const CBioId& BioId)
+    :   m_request(make_shared<HCT::http2_request>()),
+        m_BioId(std::move(BioId))
+{
+    if (!HCT::HttpClientTransport::s_ioc)
+        DDRPC::EDdRpcException::raise("DDRPC is not initialized, call DdRpcClient::Init() first");
+    m_request->init_request(DDRPC::DdRpcClient::SericeIdToEndPoint(ACCVER_RESOLVER_SERVICE_ID), afuture, "accver=" + m_BioId.GetId());
+}
+
+void CBioIdResolutionQueue::CBioIdResolutionQueueItem::SyncResolve(CBlobId& BlobId, const CDeadline& deadline) {
+    long wait_ms = RemainingTimeMs(deadline);
+    bool rv = HCT::HttpClientTransport::s_ioc->add_request(m_request, wait_ms);
+    if (!rv) {
+        BlobId.m_Status = CBlobId::eFailed;
+        BlobId.m_StatusEx = CBlobId::eError;
+        BlobId.m_Message = "Resolver queue is full";
+        return;
+    }
+    while (true) {
+        if (IsDone()) {
+            PopulateData(BlobId);
+            return;
+        }
+        wait_ms = RemainingTimeMs(deadline);
+        if (wait_ms <= 0) {
+            BlobId.m_Status = CBlobId::eFailed;
+            BlobId.m_StatusEx = CBlobId::eError;
+            BlobId.m_Message = "Timeout expired";
+            return;
+        }
+        WaitFor(wait_ms);
+    }
+}
+
+void CBioIdResolutionQueue::CBioIdResolutionQueueItem::WaitFor(long timeout_ms)
+{
+    m_request->get_result_data().wait_for(timeout_ms);
+}
+
+void CBioIdResolutionQueue::CBioIdResolutionQueueItem::Wait()
+{
+    m_request->get_result_data().wait();
+}
+
+bool CBioIdResolutionQueue::CBioIdResolutionQueueItem::IsDone() const 
+{
+    return m_request->get_result_data().get_finished();
+}
+
+void CBioIdResolutionQueue::CBioIdResolutionQueueItem::Cancel()
+{
+    m_request->send_cancel();
+}
+
+void CBioIdResolutionQueue::CBioIdResolutionQueueItem::PopulateData(CBlobId& BlobId) const
+{
+    if (m_request->get_result_data().get_cancelled()) {
+        BlobId.m_Status = CBlobId::eFailed;
+        BlobId.m_StatusEx = CBlobId::eCanceled;
+        BlobId.m_Message = "Request for resolution was canceled";
+    }
+    else if (m_request->has_error()) {
+        BlobId.m_Status = CBlobId::eFailed;
+        BlobId.m_StatusEx = CBlobId::eError;
+        BlobId.m_Message = m_request->get_error_description();
+    }
+    else switch (m_request->get_result_data().get_http_status()) {
+        case 200: {
+            string data = m_request->get_reply_data_move();
+            DDRPC::DataRow rec;
+            try {
+                AccVerResolverUnpackData(rec, data);
+                BlobId.m_Status = CBlobId::eResolved;
+                BlobId.m_StatusEx = CBlobId::eNone;
+                BlobId.m_BlobInfo.gi              = rec[0].AsUint8;
+                BlobId.m_BlobInfo.seq_length      = rec[1].AsUint4;
+                BlobId.m_BlobInfo.blob_id.sat     = rec[2].AsUint1;
+                BlobId.m_BlobInfo.blob_id.sat_key = rec[3].AsUint4;
+                BlobId.m_BlobInfo.tax_id          = rec[4].AsUint4;
+                BlobId.m_BlobInfo.date_queued     = rec[5].AsDateTime;
+                BlobId.m_BlobInfo.state           = rec[6].AsUint1;
+            }
+            catch (const DDRPC::EDdRpcException& e) {
+                BlobId.m_Status = CBlobId::eFailed;
+                BlobId.m_StatusEx = CBlobId::eError;
+                BlobId.m_Message = e.what();
+            }
+            break;
+        }
+        case 404: {
+            BlobId.m_Status = CBlobId::eFailed;
+            BlobId.m_StatusEx = CBlobId::eNotFound;
+            BlobId.m_Message = "Bio id is not found";
+            break;
+        }
+        default: {
+            BlobId.m_Status = CBlobId::eFailed;
+            BlobId.m_StatusEx = CBlobId::eError;
+            BlobId.m_Message = "Unexpected result";
+        }
+    }
+        
+}
+
+/** CBioIdResolutionQueue */
+
+CBioIdResolutionQueue::CBioIdResolutionQueue()
+    :   m_future(make_shared<HCT::io_future>())
+{
+}
+
+CBioIdResolutionQueue::~CBioIdResolutionQueue()
+{
+}
+
+void CBioIdResolutionQueue::Resolve(std::vector<CBioId>* bio_ids, const CDeadline& deadline)
+{    
+    if (!bio_ids)
+        return;
+    bool has_timeout = !deadline.IsInfinite();
+    unique_lock<mutex> _(m_items_mtx);
+    long wait_ms = 0;
+    auto rev_it = bio_ids->rbegin();
+    while (rev_it != bio_ids->rend()) {
+        
+        unique_ptr<CBioIdResolutionQueueItem> Qi(new CBioIdResolutionQueueItem(m_future, *rev_it));
+
+        while (true) {
+            bool rv = HCT::HttpClientTransport::s_ioc->add_request(Qi->m_request, wait_ms);
+
+            if (!rv) { // internal queue is full
+                if (has_timeout) {
+                    wait_ms = RemainingTimeMs(deadline);
+                    if (wait_ms < 0)
+                        break;
+                }
+                else
+                    wait_ms = DDRPC::INDEFINITE;
+            }
+            else {
+                m_items.emplace_back(std::move(Qi));
+                bio_ids->erase(std::next(rev_it.base()));
+                ++rev_it;
+                break;
+            }
+        }
+        if (wait_ms < 0)
+            break;
+    }
+}
+
+CBlobId CBioIdResolutionQueue::Resolve(const CBioId& bio_id, const CDeadline& deadline) 
+{
+    CBlobId rv(bio_id);
+    unique_ptr<CBioIdResolutionQueueItem> Qi(new CBioIdResolutionQueueItem(HCT::io_coordinator::get_tls_future(), bio_id));
+    Qi->SyncResolve(rv, deadline);
+    return rv;
+}
+
+CBlobId CBioIdResolutionQueue::Resolve(CBioId&& bio_id, const CDeadline& deadline)
+{
+    CBlobId rv(bio_id);
+    unique_ptr<CBioIdResolutionQueueItem> Qi(new CBioIdResolutionQueueItem(HCT::io_coordinator::get_tls_future(), std::move(bio_id)));
+    Qi->SyncResolve(rv, deadline);
+    return rv;
+}
+
+std::vector<CBlobId> CBioIdResolutionQueue::GetBlobIds(const CDeadline& deadline, size_t max_results)
+{
+    std::vector<CBlobId> rv;
+    bool has_limit = max_results != 0;
+    unique_lock<mutex> _(m_items_mtx);
+    if (m_items.size() > 0) {
+        bool has_timeout = !deadline.IsInfinite();
+        if (has_timeout) {
+            long wait_ms = RemainingTimeMs(deadline);
+            auto& last = m_items.front();
+            last->WaitFor(wait_ms);
+        }
+        for (auto rev_it = m_items.rbegin(); rev_it != m_items.rend(); ++rev_it) {
+            if (has_limit && max_results-- == 0)
+                break;
+            const auto& req = *rev_it;
+            if (req->IsDone()) {
+                auto fw_it = std::next(rev_it).base();
+                rv.emplace_back(std::move((*fw_it)->m_BioId));
+                req->PopulateData(rv.back());
+                m_items.erase(fw_it);
+            }
+        }
+    }
+    return rv;
+}
+
+void CBioIdResolutionQueue::Clear(std::vector<CBioId>* bio_ids)
+{
+    unique_lock<mutex> _(m_items_mtx);
+    if (bio_ids) {
+        bio_ids->clear();
+        bio_ids->reserve(m_items.size());
+    }
+    for (auto& it : m_items) {
+        it->Cancel();
+        if (bio_ids)
+            bio_ids->emplace_back(std::move(it->m_BioId));
+    }
+    m_items.clear();
+}
+
+bool CBioIdResolutionQueue::IsEmpty() const
+{
+    unique_lock<mutex> _(m_items_mtx);
+    return m_items.size() == 0;
+}
+
+
+/** CBlobRetrievalQueue */
+
+
+END_objects_SCOPE
+END_NCBI_SCOPE
