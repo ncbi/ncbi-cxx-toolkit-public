00bd441e1e17da75de8fbc0aa37b07672d766e93
Amelia Fong <fongah2@ncbi.nlm.nih.gov>
2018-02-02 14:38:34 +0000

    Fix ouput and add features to get_species_taxids.sh, JIRA:SB-2175
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@81096 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/app/blast/get_species_taxids.sh
+++ src/app/blast/get_species_taxids.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 # $Id$
 # ===========================================================================
 #
@@ -36,16 +36,21 @@
 
 export PATH=/bin:/usr/bin:/am/ncbiapdata/bin:$HOME/edirect
 
-TMP=`mktemp -t $(basename -s .sh $0)-XXXXXXX`
+TMP1=`mktemp -t $(basename -s .sh $0)-XXXXXXX`
 TMP2=`mktemp -t $(basename -s .sh $0)-XXXXXXX`
-trap " /bin/rm -fr $TMP $TMP2" INT QUIT EXIT HUP KILL ALRM
+trap " /bin/rm -fr $TMP1 $TMP2" INT QUIT EXIT HUP KILL ALRM
 
 usage() { 
     echo "Usage:"; 
-    echo -e "-t Input Tax ID\n\tGet taxonomy IDss at or below input taxonomy ID level"; 
-    echo -e "-n Scientic Name, Common Name or Keyword\n\tGet taxonomy information for organism";
+    echo -e "-t Input Tax ID\n\tGet taxonomy IDs at or below input taxonomy ID level"; 
+    echo -e "-n Scientific Name, Common Name or Keyword\n\tGet taxonomy information for organism";
     echo -e "-o Output file name (default stdout)"; 
     exit 1; 
+}
+
+error_exit() {
+    echo $1;
+    exit 1;
 }
 
 while getopts  "ht::n::o::" OPT; do
@@ -68,41 +73,74 @@ done
 if [ -z "${TAXID}" ] && [ -z "${NAME}" ]; then
     usage
 fi
-
+if [ ! -z "${TAXID}" ] && [ ! -z "${NAME}" ]; then
+    echo -e "Input Error: -t is incompatible with -n\n"
+    usage
+fi
 
 if  [ ! -z "${TAXID}" ]; then
-    esearch -db taxonomy -query "txid$TAXID[orgn] AND \"at or below species level\"[prop]" > $TMP
+    esearch -db taxonomy -query "txid$TAXID[orgn] AND \"at or below species level\"[prop]" > $TMP1 
     if [ $? -ne 0 ]; then
-        echo "esearch error";
-        exit 1;
+        error_exit  "esearch error"
     fi
-    efetch -format uid < $TMP > $TMP2
+
+    efetch -format uid < $TMP1 > $TMP2 
     if [ $? -ne 0 ]; then
-        echo "efetch error";
-        exit 1;
+        error_exit "efetch error"
     fi
-    sort -n $TMP2 > $TMP
+
+    if [ ! -s $TMP2 ]; then
+        error_exit "Tax Id not found"
+    fi
+
+    sort -n $TMP2 > $TMP1
 fi
 
 if [ ! -z "${NAME}" ]; then
-    esearch -db taxonomy -query "$NAME[All Names]" > $TMP
-    if [ $? -ne 0 ]; then
-        echo "esearch error";
-        exit 1;
-    fi
-    esummary -mode json < $TMP > $TMP2
 
+    esearch -db taxonomy -query "$NAME[All Names]" > $TMP1 
     if [ $? -ne 0 ]; then
-        echo "esummary error";
-        exit 1;
+        error_exit "esearch error"
     fi
 
-    grep 'uid\|rank\|division\|scientificname\|commonname\|genbankdivision' $TMP2 | grep -v uids | tr -d '"\|,' | tr -s ' ' | sed 's/uid/\nTaxid/g' > $TMP
+    NUM_RESULTS=$(grep "<Count>" $TMP1 | sed -e 's,.*<Count>\([^<]*\)</Count>.*,\1,g')
 
+    if [ $NUM_RESULTS -eq 0 ]; then
+        CORRECT_NAME=$(espell -db taxonomy -query "$NAME" | grep "<CorrectedQuery>" | sed -e 's,.*<CorrectedQuery>\([^<]*\)</CorrectedQuery>.*,\1,g')
+
+        if [ ! -z "${CORRECT_NAME}" ]; then
+            echo "Do you mean? \"$CORRECT_NAME\"";
+        fi
+
+        esearch -db taxonomy -query "$NAME[Name Tokens]" > $TMP1 
+        if [ $? -ne 0 ]; then
+            error_exit "esearch error"
+        fi
+        NUM_RESULTS=$(grep "<Count>" $TMP1 | sed -e 's,.*<Count>\([^<]*\)</Count>.*,\1,g')
+        if [ $NUM_RESULTS -gt 500 ]; then
+            error_exit "More than 500 matches, please refine your search"
+        fi
+        if [ $NUM_RESULTS -eq 0 ]; then
+            error_exit "No matches for \"$NAME\""
+        fi
+    fi
+
+    esummary -db taxonomy -mode json < $TMP1 > $TMP2
+
+    if [ $? -ne 0 ]; then
+        error_exit "esummary error"
+    fi
+        
+    grep 'uid\|rank\|division\|scientificname\|commonname' $TMP2 | \
+    grep -v "uids\|genbankdivision" | tr -d '"\|,' | tr -s ' ' | \
+    sed 's/uid/\nTaxid/g;s/name/ name/g' > $TMP1
+
+    echo -e "\n$NUM_RESULTS matches found\n" >> $TMP1
 fi
 
 if [ ! -z "${OUTFILE}" ]; then
-    cp $TMP $OUTFILE
+    mv $TMP1 $OUTFILE
 else
-    cat $TMP
+    cat $TMP1
 fi
+
