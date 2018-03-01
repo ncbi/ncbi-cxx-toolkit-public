d54699dee27350a4a1933472d0c46a7c1c7e27b2
Amelia Fong <fongah2@ncbi.nlm.nih.gov>
2018-03-01 16:09:50 +0000

    Use OpenMP for sorting LMDB entries, JIRA:SB-2242
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@81431 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/objtools/blast/seqdb_writer/writedb_lmdb.hpp
+++ include/objtools/blast/seqdb_writer/writedb_lmdb.hpp
@@ -117,6 +117,7 @@ private:
     	}
     };
     vector<SKeyValuePair> m_list;
+    void x_Split(vector<SKeyValuePair>::iterator  b, vector<SKeyValuePair>::iterator e, const unsigned int min_chunk_size);
 };
 
 
