455cd282a3be661b73a23065892a56af6c270e58
ludwigf <ludwigf@ncbi.nlm.nih.gov>
2018-04-10 13:45:31 +0000

    JIRA: RW-564 Do not percent encode commas that are used as value separators in GTF db_xref attributes.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@81847 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/writers/gtf_write_data.cpp
+++ src/objtools/writers/gtf_write_data.cpp
@@ -78,7 +78,7 @@ string s_GtfDbtag( const CDbtag& dbtag )
             strGffTag += dbtag.GetTag().GetStr();
         }
     }
-    return strGffTag;
+    return CWriteUtil::UrlEncode(strGffTag);
 }
         
 //  ----------------------------------------------------------------------------
@@ -684,7 +684,7 @@ string CGtfRecord::x_FeatureToDbxref(
         if ( dbxrefs.size() > 0 ) {
             string value = s_GtfDbtag( *dbxrefs[ 0 ] );
             for ( size_t i=1; i < dbxrefs.size(); ++i ) {
-                value += "%2C";
+                value += ",";
                 value += s_GtfDbtag( *dbxrefs[ i ] );
             }
             return value;
