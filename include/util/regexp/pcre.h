d5233dd9be331781e831252beee0c491101e6282
Vladimir Ivanov <ivanov@ncbi.nlm.nih.gov>
2018-07-18 12:30:10 +0000

    Define PCRE_STATIC to force linking statically on Windows. JIRA:CXX-10132
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@82947 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/util/regexp/pcre.h
+++ include/util/regexp/pcre.h
@@ -51,6 +51,12 @@ imported have to be identified as such. When building PCRE, the appropriate
 export setting is defined in pcre_internal.h, which includes this file. So we
 don't change existing definitions of PCRE_EXP_DECL and PCRECPP_EXP_DECL. */
 
+/* NCBI fix: force to link statically on Windows */
+#if defined(_WIN32)
+#  define PCRE_STATIC 1
+#endif
+
+
 #if defined(_WIN32) && !defined(PCRE_STATIC)
 #  ifndef PCRE_EXP_DECL
 #    define PCRE_EXP_DECL  extern __declspec(dllimport)
