c20a330da8051ade9ceb29a525b063df683df2a2
Sergei (Sergey) Bazhin <bazhin@ncbi.nlm.nih.gov>
2018-07-31 10:34:24 +0000

    JIRA SF-3535: More related fixes: in case if current master record in ID does not have comment and no common comment found in updated contigs, then will not generate ERR_MASTER_MissingComment warning message. For EBI and DDBJ only.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@83135 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/app/wgs_parse/wgs_master.cpp
+++ src/app/wgs_parse/wgs_master.cpp
@@ -1198,11 +1198,6 @@ static CRef<CSeq_entry> CreateMasterBioseq(CMasterInfo& info, CRef<CCit_sub>& ci
         if (info.m_common_structured_comments.empty()) {
             ERR_POST_EX(0, 0, Info << "All contigs are missing both text and structured comments.");
         }
-
-        if (GetParams().GetUpdateMode() == eUpdateFull || GetParams().GetUpdateMode() == eUpdateExtraContigs ||
-            GetParams().GetUpdateMode() == eUpdatePartial || GetParams().GetUpdateMode() == eUpdateAssembly) {
-            ERR_POST_EX(0, 0, Warning << "All contigs are missing text comments. Master Bioseq will not receive a comment descriptor.");
-        }
     }
 
     for (auto& comment : info.m_common_comments) {
