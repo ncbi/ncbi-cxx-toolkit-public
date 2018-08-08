42c969f27b142d2186c60ce5e054bf0adc0ff977
Michael DiCuccio <dicuccio@ncbi.nlm.nih.gov>
2018-08-08 15:57:35 +0000

    Implement full local PCRE.  Turn on local PCRE when building GPIpe distribution JIRA: GCOL-7657
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@83281 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/build-system/cmake/CMakeChecks.pcre.cmake
+++ src/build-system/cmake/CMakeChecks.pcre.cmake
@@ -3,6 +3,7 @@
 # PCRE additions
 #
 # Perl-Compatible Regular Expressions (PCRE)
+
 find_package(PCRE)
 set(PCRE_INCLUDE ${PCRE_INCLUDE_DIR})
 set(PCRE_LIBS ${PCRE_LIBRARIES})
@@ -18,5 +19,6 @@ if (HAVE_LIBPCRE AND NOT USE_LOCAL_PCRE)
     set(PCRE_LIBS -lpcre)
 else(HAVE_LIBPCRE AND NOT USE_LOCAL_PCRE)
     set(USE_LOCAL_PCRE 1 CACHE INTERNAL "Using local PCRE due to system library absence")
+    set(PCRE_LIBS regexp)
 endif(HAVE_LIBPCRE AND NOT USE_LOCAL_PCRE)
 
