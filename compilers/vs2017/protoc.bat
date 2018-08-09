8133fa5cde4475576670346c86cc1ab3804ba784
Andrei Gourianov <gouriano@ncbi.nlm.nih.gov>
2018-08-09 18:02:12 +0000

    Move PROTOBUF generated headers, instead of copying them. JIRA: CXX-9251
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@83303 78c7ea69-d796-4a43-9a09-de51944f1b03

--- compilers/vs2017/protoc.bat
+++ compilers/vs2017/protoc.bat
@@ -91,3 +91,4 @@ if not exist "%TREE_ROOT%\include\%subtree%" (
   mkdir "%TREE_ROOT%\include\%subtree%"
 )
 xcopy /Y /D /F "%input_spec_dir%\*.h" "%TREE_ROOT%\include\%subtree%"
+del "%input_spec_dir%\*.h"
