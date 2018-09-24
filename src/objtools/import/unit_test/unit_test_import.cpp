dbdda48e3ad04326abdcc8d179d243f54fff6383
ludwigf <ludwigf@ncbi.nlm.nih.gov>
2018-09-24 14:08:24 +0000

    Add unit tests for new import library.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@83827 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ src/objtools/import/unit_test/unit_test_import.cpp
@@ -0,0 +1,217 @@
+/*  $Id$
+* ===========================================================================
+*
+*                            PUBLIC DOMAIN NOTICE
+*               National Center for Biotechnology Information
+*
+*  This software/database is a "United States Government Work" under the
+*  terms of the United States Copyright Act.  It was written as part of
+*  the author's official duties as a United States Government employee and
+*  thus cannot be copyrighted.  This software/database is freely available
+*  to the public for use. The National Library of Medicine and the U.S.
+*  Government have not placed any restriction on its use or reproduction.
+*
+*  Although all reasonable efforts have been taken to ensure the accuracy
+*  and reliability of the software and data, the NLM and the U.S.
+*  Government do not and cannot warrant the performance or results that
+*  may be obtained by using this software or data. The NLM and the U.S.
+*  Government disclaim all warranties, express or implied, including
+*  warranties of performance, merchantability or fitness for any particular
+*  purpose.
+*
+*  Please cite the author in any work or product based on this material.
+*
+* ===========================================================================
+*
+* Author:  Frank Ludwig, NCBI.
+*
+* File Description:
+*   Feature importer unit test.
+*
+* ===========================================================================
+*/
+
+#include <ncbi_pch.hpp>
+
+#include <corelib/ncbi_system.hpp>
+#include <corelib/ncbiapp.hpp>
+#include <corelib/ncbifile.hpp>
+#include <util/line_reader.hpp>
+
+#include <objtools/import/feat_error_handler.hpp>
+#include <objtools/import/feat_importer.hpp>
+
+// This header must be included before all Boost.Test headers if there are any
+#include <corelib/test_boost.hpp>
+
+USING_NCBI_SCOPE;
+USING_SCOPE(objects);
+
+const string DIRDATA = "unit_test/data";
+
+//  ============================================================================
+void sRunTest(
+    const string& testName,
+    const string& format,
+    const string& inputName,
+    const string& goldenNameAnnot,
+    const string& goldenNameErrors,
+    const string& outputNameAnnot,
+    const string& outputNameErrors,
+    const string& diffName,
+    bool updateAll)
+//  ============================================================================
+{
+    cerr << "Running test " << testName << " ...\n";
+    // produce output:
+    unique_ptr<CFeatImporter> pImporter(CFeatImporter::Get(format, 0));
+    CFeatErrorHandler errorHandler;
+    CNcbiIfstream iStr(inputName, ios::binary);
+    CSeq_annot annot;
+    try {
+        pImporter->ReadSeqAnnot(iStr, annot, errorHandler);
+    }
+    catch (const CFeatureImportError& error) {
+        BOOST_ERROR("Error: Test \"" << testName << "\" failed during import:");
+        BOOST_ERROR("  " << error.Message() << ", at line " << error.LineNumber());
+        iStr.close();
+        return;
+    }
+    iStr.close();
+
+    CNcbiOfstream oStrErrors(outputNameErrors);
+    errorHandler.Dump(oStrErrors);
+    oStrErrors.close();
+
+    CNcbiOfstream oStrAnnot(outputNameAnnot);
+    oStrAnnot << MSerial_Format_AsnText() << annot;
+    oStrAnnot.close();
+
+    // compare with golden data:
+    bool successAnnot = CFile(goldenNameAnnot).CompareTextContents(
+        outputNameAnnot, CFile::eIgnoreWs);
+    if (!successAnnot) {
+        BOOST_ERROR(
+            "Error: Test \"" << testName << "\" failed due to annotation diffs.");
+    }
+    bool successErrors = CFile(goldenNameErrors).CompareTextContents(
+        outputNameErrors, CFile::eIgnoreWs);
+    if (!successErrors) {
+        BOOST_ERROR(
+            "Error: Test \"" << testName << "\" failed due to error diffs.");
+    }
+
+    // clean up, if applicable, update golden data:
+    if (successAnnot) {
+        CFile(outputNameAnnot).Remove();
+    }
+    else {
+        if (updateAll) {
+            CFile(outputNameAnnot).Copy(goldenNameAnnot);
+            CFile(outputNameAnnot).Remove();
+        }
+    }
+    if (successErrors) {
+        CFile(outputNameErrors).Remove();
+    }
+    else {
+        if (updateAll) {
+            CFile(outputNameErrors).Copy(goldenNameErrors);
+            CFile(outputNameErrors).Remove();
+        }
+    }
+};
+
+//  ============================================================================
+NCBITEST_AUTO_INIT()
+//  ============================================================================
+{
+}
+
+
+//  ============================================================================
+NCBITEST_INIT_CMDLINE(arg_descrs)
+//  ============================================================================
+{
+    arg_descrs->AddDefaultKey(
+        "data-dir", 
+        "TEST_DATA_DIRECTORY",
+        "Set the root directory under which all test files can be found.",
+        CArgDescriptions::eString,
+        DIRDATA);
+    arg_descrs->AddFlag(
+        "update-all",
+        "Update all test cases to current reader code.",
+        true );
+    arg_descrs->AddFlag("keep-diffs",
+        "Keep output files that are different from the expected.",
+        true );
+}
+
+//  ============================================================================
+NCBITEST_AUTO_FINI()
+//  ============================================================================
+{
+}
+
+//  ============================================================================
+BOOST_AUTO_TEST_CASE(RunTests)
+//  ============================================================================
+{
+    const CArgs& args = CNcbiApplication::Instance()->GetArgs();
+
+    string dataDir( args["data-dir"].AsString() );
+    bool updateAll = args["update-all"].AsBoolean();
+    bool keepDiffs = args["keep-diffs"].AsBoolean();
+
+    const string fileTestCases(dataDir + "/" + "test-cases.txt");
+    const string dirInput(dataDir + "/input/");
+    const string dirGolden(dataDir + "/golden/");
+    const string dirOutput(dataDir + "/output/");
+    const string dirDiffs(dataDir + "/diffs/");
+
+    BOOST_REQUIRE_MESSAGE(
+        CFile(fileTestCases).Exists(), 
+        "Cannot find test data file: " << fileTestCases);
+
+    // clean out stale results from prior tests:
+    auto oldOutputFiles = CDir(dirOutput).GetEntries("", CDir::fIgnoreRecursive);
+    for (auto oldFile: oldOutputFiles) {
+        oldFile->Remove();
+    }
+
+    CNcbiIfstream ifStream(fileTestCases);
+    CStreamLineReader testCaseStream(ifStream);
+    while (!testCaseStream.AtEOF()) {
+        auto testDescription = *(++testCaseStream);
+        NStr::TruncateSpacesInPlace(testDescription);
+        if (testDescription.empty()) {
+            continue;
+        }
+        if (NStr::StartsWith(testDescription, '#')) {
+            continue;
+        }
+        vector<string> testComponents;
+        NStr::Split(testDescription, " ", testComponents);
+        BOOST_REQUIRE_MESSAGE(
+            (testComponents.size() == 2), 
+            "Invalid test description: " << testDescription);
+        auto testName = testComponents[0];
+        auto format = testComponents[1];
+        auto inFile = dirInput + testName + "." + format;
+        auto goldenFile = dirGolden + testName + ".asn1";
+        auto errorsFile = dirGolden + testName + ".errors";
+        auto outputFileAnnot = dirOutput + testName + ".asn1";
+        auto outputFileErrors = dirOutput + testName + ".errors";
+        string diffsFile = "";
+        if (keepDiffs) {
+            diffsFile = dirDiffs + testName + ".diff";
+        }
+        BOOST_CHECK_NO_THROW(
+            sRunTest(
+                testName, format,
+                inFile, goldenFile, errorsFile, 
+                outputFileAnnot, outputFileErrors, diffsFile, 
+                updateAll));
+    }
+}
