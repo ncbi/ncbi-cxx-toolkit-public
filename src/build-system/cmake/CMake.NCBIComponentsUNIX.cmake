5448f208546086dae836c946d7f25476ef5b620e
Aaron Ucko <ucko@ncbi.nlm.nih.gov>
2018-10-26 13:14:00 +0000

    Advance to VDB 2.9.3 by default (from 2.9.2-1).  JIRA: CXX-10284.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@84228 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/build-system/cmake/CMake.NCBIComponentsUNIX.cmake
+++ src/build-system/cmake/CMake.NCBIComponentsUNIX.cmake
@@ -609,8 +609,8 @@ if (WIN32)
 	find_external_library(VDB
 		INCLUDES sra/sradb.h
 		LIBS ncbi-vdb
-		INCLUDE_HINTS "\\\\snowman\\trace_software\\vdb\\vdb-versions\\2.9.2-1\\interfaces"
-		LIBS_HINTS "\\\\snowman\\trace_software\\vdb\\vdb-versions\\2.9.2-1\\win\\release\\x86_64\\lib")
+		INCLUDE_HINTS "\\\\snowman\\trace_software\\vdb\\vdb-versions\\2.9.3\\interfaces"
+		LIBS_HINTS "\\\\snowman\\trace_software\\vdb\\vdb-versions\\2.9.3\\win\\release\\x86_64\\lib")
 else (WIN32)
 	find_external_library(VDB
 		INCLUDES sra/sradb.h
