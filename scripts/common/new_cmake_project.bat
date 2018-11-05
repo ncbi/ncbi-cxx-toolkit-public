2ec65b511f48a6426edfa08f8f114b78b2278ac1
Andrei Gourianov <gouriano@ncbi.nlm.nih.gov>
2018-11-05 19:50:25 +0000

    Added new_cmake_project scripts. JIRA: CXX-10116
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@84385 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ scripts/common/new_cmake_project.bat
@@ -0,0 +1,142 @@
+@echo off
+setlocal ENABLEDELAYEDEXPANSION
+REM #########################################################################
+REM  $Id$
+REM  Create new CMake project which uses prebuilt NCBI C++ toolkit from a sample
+REM  Author: Andrei Gourianov, gouriano@ncbi
+REM #########################################################################
+
+set initial_dir=%CD%
+set script_name=%~nx0
+cd %0\..
+set script_dir=%CD%
+cd %initial_dir%
+set tree_root=%initial_dir%
+
+set repository=https://svn.ncbi.nlm.nih.gov/repos/toolkit/trunk/c++
+set rep_inc=include
+set rep_src=src
+set rep_sample=sample
+set prj_tmp=CMakeLists.tmp
+set prj_prj=CMakeLists.txt
+set prj_cfg=configure.bat
+goto :RUN
+
+REM #########################################################################
+:USAGE
+echo USAGE:
+echo    %script_name% ^<name^> ^<type^> [builddir]
+echo SYNOPSIS:
+echo   Create new CMake project which uses prebuilt NCBI C++ toolkit from a sample.
+echo ARGUMENTS:
+echo   --help       -- print Usage
+echo   ^<name^>       -- project name (destination directory)
+echo   ^<type^>       -- project type
+echo   [builddir]   -- root directory of the pre-built NCBI C++ toolkit
+if "%~1"=="" (
+    echo:
+    echo The following project types are available:
+    for /f %%a in ('svn list -R %repository%/%rep_src%/%rep_sample%') do (
+        for /f %%b in ('echo %%a^| findstr /r ".*/$"') do (
+            if not "%%b"=="" (
+                for /f "tokens=1,2,* delims=/" %%c in ('echo %%b') do (
+                    if not "%%d"=="" (
+                        if "%%e"=="" (
+                            echo   %%c/%%d
+                        )
+                    )
+                )
+            )
+        )
+    )
+)
+echo:
+goto :eof
+
+:ERROR
+call :USAGE notype
+if not "%~1"=="" (
+  echo ----------------------------------------------------------------------
+  echo ERROR:  %* 1>&2
+)
+goto :eof
+
+REM -------------------------------------------------------------------------
+:RUN
+if "%1"=="--help" (
+  call :USAGE
+  goto :DONE
+)
+if "%1"=="-help" (
+  call :USAGE
+  goto :DONE
+)
+if "%1"=="help" (
+  call :USAGE
+  goto :DONE
+)
+set prj_name=%~1
+set prj_type=%~2
+set toolkit=%~3
+if "%prj_name%"=="" (
+  call :ERROR Mandatory argument 'name' is missing
+  goto :DONE
+)
+if "%prj_type%"=="" (
+  call :ERROR Mandatory argument 'type' is missing
+  goto :DONE
+)
+if "%toolkit%"=="" (
+  call :ERROR Mandatory argument 'builddir' is missing
+  goto :DONE
+)
+if exist %prj_name% (
+  call :ERROR File or directory %prj_name% already exists
+  goto :DONE
+)
+mkdir %prj_name%
+if not exist %prj_name% (
+  call :ERROR Failed to create directory %prj_name%
+  goto :DONE
+)
+cd %prj_name%
+
+REM -------------------------------------------------------------------------
+svn co %repository%/%rep_inc%/%rep_sample%/%prj_type& %rep_inc% >NUL 2>&1
+svn co %repository%/%rep_src%/%rep_sample%/%prj_type% %rep_src% >NUL
+for /f %%a in ('dir /s /a:h /b .svn') do (
+  rmdir /S /Q %%a
+)
+del /S /Q Makefile.* >NUL
+
+REM create configure script
+(
+    echo @echo off
+    echo set srcdir=%%CD%%
+    echo set script_name=%%~nx0
+    echo "%toolkit%\src\build-system\cmake\cmake-cfg-vs.bat" --rootdir="%%srcdir%%" --caller=%%script_name%% %%*
+) > %prj_cfg%
+
+REM modify CMakeLists.txt
+cd %rep_src%
+(
+    echo cmake_minimum_required^(VERSION 3.3^)
+    echo:
+    echo project^(%prj_name%^)
+    echo:
+    echo set^(NCBI_EXTERNAL_TREE_ROOT   "%toolkit:\=/%"^)
+    echo include^(${NCBI_EXTERNAL_TREE_ROOT}/src/build-system/cmake/CMake.NCBItoolkit.cmake^)
+    echo:
+    type %prj_prj%
+) >%prj_tmp%
+del %prj_prj%
+move /Y %prj_tmp% %prj_prj% >NUL
+REM #########################################################################
+
+echo Created project %prj_name%
+echo To configure:  cd %prj_name%; ./%prj_cfg% ^<arguments^>
+echo For help:      cd %prj_name%; ./%prj_cfg% --help
+:DONE
+cd %initial_dir%
+endlocal
+exit /b 0
---
+++ scripts/common/new_cmake_project.sh
@@ -0,0 +1,133 @@
+#!/bin/sh
+#############################################################################
+# $Id$
+#   Create new CMake project which uses prebuilt NCBI C++ toolkit from a sample
+#   Author: Andrei Gourianov, gouriano@ncbi
+#############################################################################
+initial_dir=`pwd`
+script_name=`basename $0`
+script_dir=`dirname $0`
+script_dir=`(cd "${script_dir}" ; pwd)`
+cd "$initial_dir"
+tree_root=`pwd`
+
+repository="https://svn.ncbi.nlm.nih.gov/repos/toolkit/trunk/c++"
+rep_inc="include"
+rep_src="src"
+rep_sample="sample"
+prj_tmp="CMakeLists.tmp"
+prj_prj="CMakeLists.txt"
+prj_cfg="configure.sh"
+############################################################################# 
+Usage()
+{
+    cat <<EOF 1>&2
+USAGE:
+  $script_name <name> <type> [builddir] 
+SYNOPSIS:
+  Create new CMake project which uses prebuilt NCBI C++ toolkit from a sample.
+ARGUMENTS:
+  --help       -- print Usage
+  <name>       -- project name (destination directory)
+  <type>       -- project type
+  [builddir]   -- root directory of the pre-built NCBI C++ toolkit
+EOF
+if test -z "$1"; then
+  echo
+  echo The following project types are available:
+  svn list -R $repository/$rep_src/$rep_sample | while IFS= read -r line; do
+    if [[ $line =~ .*/$ ]]; then
+      count=`echo $line | tr -cd '/' | wc -c`
+      if [[ $count -eq 2 ]]; then
+        echo "  "${line%?}
+      fi
+    fi
+  done
+fi
+echo " "
+}
+
+Error()
+{
+  Usage notype
+  if test -n "$1"; then
+    echo ----------------------------------------------------------------------
+    echo ERROR: $@ 1>&2
+  fi
+  cd "$initial_dir"
+  exit 1
+}
+
+#----------------------------------------------------------------------------
+if [ $# -lt 2 ]; then
+  if [ $# -eq 1 ]; then
+    if [ $1 = "--help" -o $1 = "-help" -o $1 = "help" ]; then
+      Usage
+      exit 0
+    fi
+  fi
+  Error Mandatory argument is missing
+fi
+prj_name=$1
+prj_type=$2
+if [ $# -gt 2 ]; then
+  toolkit=$3
+fi
+if [ -z $toolkit ]; then
+  Error Mandatory argument builddir is missing
+fi
+if [ -e $prj_name ]; then
+  Error File or directory $prj_name already exists
+fi
+mkdir -p $prj_name
+if [ ! -d $prj_name ]; then
+  Error Failed to create directory $prj_name
+fi
+cd $prj_name
+
+#----------------------------------------------------------------------------
+#path=`echo $prj_type | tr "/" " "`
+#for d in $path; do
+#  echo ---- $d
+#done
+
+svn co $repository/$rep_src/$rep_sample/$prj_type $rep_src 1>/dev/null
+svn co $repository/$rep_inc/$rep_sample/$prj_type $rep_inc 1>/dev/null 2>/dev/null
+find . -name ".svn" -exec rm -rf {} \; 1>/dev/null 2>/dev/null
+find $rep_src -name "Makefile.*" -exec rm -rf {} \; 1>/dev/null 2>/dev/null
+
+# create configure script
+host_os=`uname`
+if test $host_os = "Darwin"; then
+  cmake_cfg="cmake-cfg-xcode.sh"
+else
+  cmake_cfg="cmake-cfg-unix.sh"
+fi
+{
+  echo "#!/bin/sh"
+  echo "srcdir=\`pwd\`"
+  echo "script_name=\`basename \$0\`"
+  echo "exec $toolkit/src/build-system/cmake/$cmake_cfg --rootdir=\$srcdir --caller=\$script_name \"\$@\""
+
+} > $prj_cfg
+chmod a+x $prj_cfg
+
+# modify CMakeLists.txt
+cd $rep_src
+{
+  echo "cmake_minimum_required(VERSION 3.3)"
+  echo " "
+  echo "project($prj_name)"
+  echo " "
+  echo "set(NCBI_EXTERNAL_TREE_ROOT   $toolkit)"
+  echo "include(\${NCBI_EXTERNAL_TREE_ROOT}/src/build-system/cmake/CMake.NCBItoolkit.cmake)"
+  echo " "
+  cat $prj_prj
+} > $prj_tmp
+rm -f $prj_prj
+mv $prj_tmp $prj_prj
+
+echo "Created project $prj_name"
+echo "To configure:  cd $prj_name; ./$prj_cfg <arguments>"
+echo "For help:      cd $prj_name; ./$prj_cfg --help"
+cd "$initial_dir"
