bfae68e0b4e95ff2b92977a7a850c00547c8f8ef
Sergey Satskiy <satskyse@ncbi.nlm.nih.gov>
2018-11-14 15:43:43 +0000

    Check only 3 last exceptions when a new one is accepted (to avoid performance penalties when exceptions are used for passing information). JIRA: CXX-9576
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@84488 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/dbapi/driver/dbapi_driver_exception_storage.cpp
+++ src/dbapi/driver/dbapi_driver_exception_storage.cpp
@@ -78,15 +78,27 @@ CDBExceptionStorage::~CDBExceptionStorage(void) throw()
 
 void CDBExceptionStorage::Accept(unique_ptr<CDB_Exception>& e)
 {
+    static const size_t     kMaxItemsToCheck = 3;
+
     CFastMutexGuard mg(m_Mutex);
 
+    // Note 1:
     // Sometimes the very same exception is pushed to the storage via different
     // paths. To avoid double output in a log file the accumulated list is
     // checked if there is this exception here already.
-    for (const auto &  item: m_Exceptions) {
-        if (item->GetDBErrCode() == e->GetDBErrCode())
-            if (item->GetMsg() == e->GetMsg())
+    // Note 2:
+    // There are some cases though when the exception storage is used not for
+    // the exceptions but for passing data. Sometimes there a lot of data and
+    // checking the whole container introduces severe performance penalties. To
+    // avoid them the only limited number of the latest items is checked.
+    size_t checked_items = 0;
+    for (auto it = m_Exceptions.rbegin();
+         checked_items < kMaxItemsToCheck && it != m_Exceptions.rend();
+         ++it) {
+        if ((*it)->GetDBErrCode() == e->GetDBErrCode())
+            if ((*it)->GetMsg() == e->GetMsg())
                 return;     // the same exception has already been registered
+        ++checked_items;
     }
 
     CDB_Exception *  raw_pointer = e.get();
