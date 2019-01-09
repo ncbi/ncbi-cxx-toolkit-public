b1da896363afc8e1a56d7d847bdc36ae01607c53
Sergiy Gotvyanskyy <gotvyans@ncbi.nlm.nih.gov>
2019-01-09 15:38:30 +0000

    JIRA: CXX-10115
    Included ncbi_source_ver.h after ncbi_pch.hpp to correctly show source version 
    
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@85030 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/app/asn2flat/asn2flat.cpp
+++ src/app/asn2flat/asn2flat.cpp
@@ -31,6 +31,7 @@
 * ===========================================================================
 */
 #include <ncbi_pch.hpp>
+#include <common/ncbi_source_ver.h>
 #include <corelib/ncbiapp.hpp>
 #include <corelib/ncbi_signal.hpp>
 #include <connect/ncbi_core_cxx.hpp>
