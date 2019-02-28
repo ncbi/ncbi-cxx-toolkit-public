87df6dba92321e3c8ebb198e35420ca588c4e6e8
Rafael Sadyrov <sadyrovr@ncbi.nlm.nih.gov>
2019-02-28 20:12:40 +0000

    Fixed PSG API for cases when user context is used (and temporarily broke psg_client along the way). JIRA CXX-10404
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@85661 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/pubseq_gateway/client/psg_client.cpp
+++ src/objtools/pubseq_gateway/client/psg_client.cpp
@@ -444,8 +444,7 @@ bool CPSG_Queue::SImpl::SendRequest(shared_ptr<const CPSG_Request> user_request,
     static HCT::io_coordinator ioc(m_Service);
 
     chrono::milliseconds wait_ms{};
-    auto user_context = user_request->GetUserContext<string>();
-    const auto request_id = user_context ? *user_context : ioc.get_new_request_id();
+    const auto request_id = ioc.get_new_request_id();
     auto reply = make_shared<SPSG_Reply>();
     auto abs_path_ref = user_request->x_GetAbsPathRef();
     auto http_request = make_shared<HCT::http2_request>(request_id, reply, m_Requests, move(abs_path_ref));
