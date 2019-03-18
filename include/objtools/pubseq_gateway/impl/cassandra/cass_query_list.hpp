b210c991ca8d00c7f0c88b71badfa7454c4179a8
dmitrie1 <dmitrie1@ncbi.nlm.nih.gov>
2019-03-18 13:59:08 +0000

    Fixed drop-downs in ql performance; JIRA:ID-5405
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@85833 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/objtools/pubseq_gateway/impl/cassandra/cass_query_list.hpp
+++ include/objtools/pubseq_gateway/impl/cassandra/cass_query_list.hpp
@@ -13,9 +13,6 @@ USING_NCBI_SCOPE;
 
 class CCassQueryList;
 
-const int k_ready_wait_timeout = 1000000;
-constexpr const size_t k_max_queries = 1024;
-
 using TCassQueryListTickCB = function<void()>;
 
 class ICassQueryListConsumer {
@@ -26,64 +23,55 @@ public:
         return true;
     }
     virtual bool ProcessRow(shared_ptr<CCassQuery> query, CCassQueryList& list, size_t query_idx) = 0;
-    virtual void Reset(shared_ptr<CCassQuery> query, CCassQueryList& list, size_t query_idx) {
-    }
+    virtual void Reset(shared_ptr<CCassQuery> query, CCassQueryList& list, size_t query_idx) {}
     virtual void Failed(shared_ptr<CCassQuery> query, CCassQueryList& list, size_t query_idx, const exception* e) = 0;
 };
 
 class CCassQueryList {
 public:
-    static constexpr const unsigned int k_dflt_max_stmt = 128;
-    CCassQueryList(shared_ptr<CCassConnection> cass_conn) :
-        m_cass_conn(cass_conn),
-        m_cnt(0),
-        m_has_error(false),
-        m_max_queries(k_dflt_max_stmt)
-    {}
+    static constexpr const unsigned int kDfltMaxQuery = 128;
+    static constexpr const uint64_t kReadyPushWaitTimeout = 500000;
+    static constexpr const uint64_t kReadyPopWaitTimeout = 500;
+    static constexpr const size_t kNotifyQueueLen = 2048;
+    static shared_ptr<CCassQueryList> Create(shared_ptr<CCassConnection> cass_conn) noexcept;
+    virtual ~CCassQueryList();
 
-    CCassQueryList& SetMaxQueries(size_t max_queries) {
-        assert(max_queries <= k_max_queries); // can't have more than k_max_queries because m_ready is a static array of this size * 2
-        assert(m_query_arr.empty());          // can't resize m_notification_arr if there are potentially pending query callbacks
-        if (m_query_arr.empty()) {
-            m_max_queries = min(max_queries, k_max_queries);
-            if (m_notification_arr.size() < m_max_queries) {
-                m_notification_arr.reserve(m_max_queries);
-                while (m_notification_arr.size() < m_max_queries) {
-                    m_notification_arr.push_back({this, m_notification_arr.size()});
-                }
-            }
-        }
-        return *this;
-    }
-    CCassQueryList& SetKeyspace(const string& keyspace) {
-        m_cass_conn->SetKeyspace(keyspace);
-        return *this;
-    }
-    CCassQueryList& SetTickCB(TCassQueryListTickCB cb) {
-        m_tick_cb = cb;
-        return *this;
-    }
-    size_t GetMaxQueries() const {
-        return m_max_queries;
-    }
-    bool HasError() const {
-        return m_has_error;
-    }
-    void Wait();
+    CCassQueryList& SetMaxQueries(size_t max_queries);
+    CCassQueryList& SetKeyspace(const string& keyspace);
+    CCassQueryList& SetTickCB(TCassQueryListTickCB cb);
+    string GetKeyspace() const;
+    size_t GetMaxQueries() const;
+    bool HasError() const;
+    size_t NumberOfActiveQueries() const;
+    size_t NumberOfBusySlots() const;
+    size_t NumberOfPendingSlots() const;
+    string ToString() const;
+
+    void Finalize();
     void Cancel(const exception* e = nullptr);
     void Cancel(ICassQueryListConsumer* consumer, const exception* e = nullptr);
     void Execute(unique_ptr<ICassQueryListConsumer> consumer, int retry_count, bool post_async = false);
     bool HasEmptySlot();
-    void Yield();
+    void Yield(bool wait);
     shared_ptr<CCassQuery> Extract(size_t slot_index);
+protected:
+    CCassQueryList() :
+        m_cnt(0),
+        m_has_error(false),
+        m_max_queries(kDfltMaxQuery),
+        m_yield_in_progress(false),
+        m_attached_slots(0)
+    {}
 private:
     enum SQrySlotState {
         ssAvailable,
         ssAttached,
-        ssReadingRows,
+        ssReadingRow,
         ssReseting,
         ssReleasing,
+        ssLast = ssReleasing + 1
     };
+    static string SQrySlotStateStr[ssLast];
     struct SQrySlot {
         unique_ptr<ICassQueryListConsumer> m_consumer;
         shared_ptr<CCassQuery> m_qry;
@@ -91,42 +79,41 @@ private:
         int m_retry_count;
         SQrySlotState m_state;
     };
-    struct SQryNotification {
-        CCassQueryList* m_query_list;
+    class CQryNotification : public CCassDataCallbackReceiver {
+    public:
+        CQryNotification(shared_ptr<CCassQueryList> query_list, size_t index);
+        virtual void OnData() override;
+    private:
+        weak_ptr<CCassQueryList> m_query_list;
         size_t m_index;
     };
     struct SPendingSlot {
         unique_ptr<ICassQueryListConsumer> m_consumer;
         int m_retry_count;
     };
-    static void SOnDataCB(CCassQuery& qry, void* data) {
-        SQryNotification* notification = static_cast<SQryNotification*>(data);
-        while (!notification->m_query_list->m_ready.push_wait(notification->m_index, k_ready_wait_timeout)) {
-            
-        }
-    }
-    void Tick() {
-        if (m_tick_cb)
-            m_tick_cb();
-    }
 
-    SQrySlot* WaitAny(bool discard, bool wait = true);
-    SQrySlot* WaitOne(size_t index, bool discard);
+    void Tick();
+    SQrySlot* CheckSlots(bool discard, bool wait = true);
+    SQrySlot* CheckSlot(size_t index, bool discard);
     void CheckPending(SQrySlot* slot);
     void AttachSlot(SQrySlot* slot, SPendingSlot&& pending_slot);
     void DetachSlot(SQrySlot* slot);
     void ReadRows(SQrySlot* slot);
     void Release(SQrySlot* slot);
 
+    weak_ptr<CCassQueryList> m_self_weak;
     shared_ptr<CCassConnection> m_cass_conn;
     vector<SQrySlot> m_query_arr;
-    mpmc_bounded_queue_w<size_t, k_max_queries * 2> m_ready;
+    mpmc_bounded_queue_w<size_t, kNotifyQueueLen> m_ready;
     int64_t m_cnt;
     bool m_has_error;
     size_t m_max_queries;
     vector<SPendingSlot> m_pending_arr;
-    vector<SQryNotification> m_notification_arr;
+    vector<shared_ptr<CQryNotification>> m_notification_arr;
     TCassQueryListTickCB m_tick_cb;
+    atomic_bool m_yield_in_progress;
+    string m_keyspace;
+    atomic_size_t m_attached_slots;
 };
 
 class CCassOneExecConsumer : public ICassQueryListConsumer {
@@ -144,10 +131,8 @@ public:
         assert(false);
         return true;
     }
-    virtual void Reset(shared_ptr<CCassQuery> query, CCassQueryList& list, size_t qry_index) {
-    }
-    virtual void Failed(shared_ptr<CCassQuery> query, CCassQueryList& list, size_t qry_index, const exception* e) {
-    }
+    virtual void Reset(shared_ptr<CCassQuery> query, CCassQueryList& list, size_t qry_index) {}
+    virtual void Failed(shared_ptr<CCassQuery> query, CCassQueryList& list, size_t qry_index, const exception* e) {}
 private:
     function<bool(CCassQuery& query, CCassQueryList& list)> m_cb;
 };
