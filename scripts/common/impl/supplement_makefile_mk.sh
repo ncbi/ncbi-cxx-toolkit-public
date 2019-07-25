812ce69620db494c4bc67f44c46f825455eebf62
Aaron Ucko <ucko@ncbi.nlm.nih.gov>
2019-07-25 19:13:23 +0000

    Automatically update Makefile.mk when any Makefile.*.mk has changed.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@87114 78c7ea69-d796-4a43-9a09-de51944f1b03

--- scripts/common/impl/supplement_makefile_mk.sh
+++ scripts/common/impl/supplement_makefile_mk.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
-srcdir=$1
+srcdir=`(cd $1 && pwd)`
 builddir=$2
 find "$srcdir"/src/* -name .svn -prune -o -name build-system -prune \
    -o -name 'Makefile.*.mk' -print \
    | while read x; do
+      echo "\$(builddir)/Makefile.mk: \$(wildcard $x)" >&3
       echo
       echo "### Extra macro definitions from $x"
       echo
       echo "#line 1 \"$x\""
       cat "$x"
-   done >> "$builddir/Makefile.mk"
+   done >> "$builddir/Makefile.mk" 3>"$builddir/Makefile.mk.d"
 "$srcdir"/scripts/common/impl/report_duplicates.awk \
    src="$srcdir/src/build-system/Makefile.mk.in" "$builddir/Makefile.mk"
