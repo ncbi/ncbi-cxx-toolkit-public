2c2b8e34961fd217d8492831859956baf32240b5
Aaron Ucko <ucko@ncbi.nlm.nih.gov>
2019-07-30 14:52:22 +0000

    CTL_RowResult (CTDS_RowResult): Prezero m_Indicator contents.
    Address corner cases in which m_Indicator values could be used
    uninitialized (notably in conjunction with upcoming backported ftds95
    row count reporting improvements).  JIRA: CXX-10776.
    
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@87141 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/dbapi/driver/ctlib/result.cpp
+++ src/dbapi/driver/ctlib/result.cpp
@@ -138,6 +138,7 @@ CTL_RowResult::CTL_RowResult(CS_COMMAND* cmd, CTL_Connection& conn) :
         m_BindItem = AutoArray<CS_VOID*>(m_BindedCols);
         m_Copied = AutoArray<CS_INT>(m_BindedCols);
         m_Indicator = AutoArray<CS_SMALLINT>(m_BindedCols);
+        memset(m_Indicator.get(), 0, m_BindedCols * sizeof(m_Indicator[0]));
 
         for(int i= 0; i < m_BindedCols; i++) {
           m_BindItem[i] = (i ? ((unsigned char*)(m_BindItem[i-1])) + m_ColFmt[i-1].maxlength : m_BindBuff);
