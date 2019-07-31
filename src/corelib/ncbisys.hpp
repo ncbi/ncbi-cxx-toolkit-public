9db2588a0def61edde065ccaaa057a389b8310bc
Vladimir Ivanov <ivanov@ncbi.nlm.nih.gov>
2019-07-31 13:43:58 +0000

    Move ncbisys.hpp to include/corelib to avoid code duplication, and these wrappers can be useful in other libraies as well.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@87157 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/corelib/ncbisys.hpp
+++ src/corelib/ncbisys.hpp
@@ -1,6 +1,3 @@
-#ifndef CORELIB___NCBISYS__HPP
-#define CORELIB___NCBISYS__HPP
-
 /*  $Id$
  * ===========================================================================
  *
@@ -32,79 +29,4 @@
  *      Wrappers around standard functions
  */
 
-
-#if defined(NCBI_OS_MSWIN)
-
-
-#  if defined(_UNICODE)
-
-#    define NcbiSys_chdir        _wchdir
-#    define NcbiSys_chmod        _wchmod
-#    define NcbiSys_creat        _wcreat
-#    define NcbiSys_fopen        _wfopen
-#    define NcbiSys_getcwd       _wgetcwd
-#    define NcbiSys_getenv       _wgetenv
-#    define NcbiSys_mkdir        _wmkdir
-#    define NcbiSys_open         _wopen
-#    define NcbiSys_putenv       _wputenv
-#    define NcbiSys_remove       _wremove
-#    define NcbiSys_rename       _wrename
-#    define NcbiSys_rmdir        _wrmdir
-#    define NcbiSys_stat         _wstat64
-#    define NcbiSys_strcmp        wcscmp
-#    define NcbiSys_strdup       _wcsdup
-#    define NcbiSys_strerror     _wcserror
-#    define NcbiSys_strerror_s   _wcserror_s
-#    define NcbiSys_system       _wsystem
-#    define NcbiSys_tempnam      _wtempnam
-#    define NcbiSys_unlink       _wunlink
-
-#  else // _UNICODE
-
-#    define NcbiSys_chdir        _chdir
-#    define NcbiSys_chmod         chmod
-#    define NcbiSys_creat        _creat
-#    define NcbiSys_fopen         fopen
-#    define NcbiSys_getcwd        getcwd
-#    define NcbiSys_getenv        getenv
-#    define NcbiSys_mkdir         mkdir
-#    define NcbiSys_open         _open
-#    define NcbiSys_putenv       _putenv
-#    define NcbiSys_remove        remove
-#    define NcbiSys_rename        rename
-#    define NcbiSys_rmdir         rmdir
-#    define NcbiSys_stat         _stat64
-#    define NcbiSys_strcmp        strcmp
-#    define NcbiSys_strdup        strdup
-#    define NcbiSys_strerror      strerror
-#    define NcbiSys_strerror_s    strerror_s
-#    define NcbiSys_system        system
-#    define NcbiSys_tempnam       tempnam
-#    define NcbiSys_unlink        unlink
-
-#  endif // _UNICODE
-
-#else // NCBI_OS_MSWIN
-
-#  define NcbiSys_chdir         chdir
-#  define NcbiSys_chmod         chmod
-#  define NcbiSys_creat         creat
-#  define NcbiSys_fopen         fopen
-#  define NcbiSys_getcwd        getcwd
-#  define NcbiSys_getenv        getenv
-#  define NcbiSys_mkdir         mkdir
-#  define NcbiSys_open          open
-#  define NcbiSys_putenv        putenv
-#  define NcbiSys_remove        remove
-#  define NcbiSys_rename        rename
-#  define NcbiSys_rmdir         rmdir
-#  define NcbiSys_stat          stat
-#  define NcbiSys_strdup        strdup
-#  define NcbiSys_strerror      strerror
-#  define NcbiSys_tempnam       tempnam
-#  define NcbiSys_unlink        unlink
-
-#endif // NCBI_OS_MSWIN
-
-
-#endif  /* CORELIB___NCBISYS__HPP */
+#include <corelib/ncbisys.hpp>
