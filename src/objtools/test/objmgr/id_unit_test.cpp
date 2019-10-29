6b06908ec717c940339cf6a25327a6d6f205eae2
Eugene Vasilchenko <vasilche@ncbi.nlm.nih.gov>
2019-10-29 17:20:13 +0000

    Test for proper WGS master desciptors. JIRA: CXX-10999
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@87990 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/test/objmgr/id_unit_test.cpp
+++ src/objtools/test/objmgr/id_unit_test.cpp
@@ -972,6 +972,48 @@ BOOST_AUTO_TEST_CASE(CheckExtGetAllTSEs)
     BOOST_CHECK(size > 0);
 }
 
+
+BOOST_AUTO_TEST_CASE(CheckWGSMasterDescr)
+{
+    LOG_POST("Checking WGS master sequence descriptors");
+    CRef<CScope> scope = s_InitScope();
+    CBioseq_Handle bh = scope->GetBioseqHandle(CSeq_id_Handle::GetHandle("BASL01000795.1"));
+    BOOST_REQUIRE(bh);
+    int desc_mask = 0;
+    map<string, int> user_count;
+    int comment_count = 0;
+    int pub_count = 0;
+    for ( CSeqdesc_CI it(bh); it; ++it ) {
+        desc_mask |= 1<<it->Which();
+        switch ( it->Which() ) {
+        case CSeqdesc::e_Comment:
+            ++comment_count;
+            break;
+        case CSeqdesc::e_Pub:
+            ++pub_count;
+            break;
+        case CSeqdesc::e_User:
+            ++user_count[it->GetUser().GetType().GetStr()];
+            break;
+        default:
+            break;
+        }
+    }
+    BOOST_CHECK(desc_mask & (1<<CSeqdesc::e_Title));
+    BOOST_CHECK(desc_mask & (1<<CSeqdesc::e_Source));
+    BOOST_CHECK(desc_mask & (1<<CSeqdesc::e_Molinfo));
+    BOOST_CHECK(desc_mask & (1<<CSeqdesc::e_Pub));
+    BOOST_CHECK_EQUAL(pub_count, 3);
+    BOOST_CHECK(desc_mask & (1<<CSeqdesc::e_Genbank));
+    BOOST_CHECK(desc_mask & (1<<CSeqdesc::e_Create_date));
+    BOOST_CHECK(desc_mask & (1<<CSeqdesc::e_Update_date));
+    BOOST_CHECK(desc_mask & (1<<CSeqdesc::e_User));
+    BOOST_CHECK_EQUAL(user_count.size(), 2u);
+    BOOST_CHECK_EQUAL(user_count["StructuredComment"], 1);
+    BOOST_CHECK_EQUAL(user_count["DBLink"], 1);
+}
+
+
 enum EInstType {
     eInst_ext, // inst.ext is set, inst.seq-data is not set
     eInst_data, // inst.ext is not set, inst.seq-data is set
