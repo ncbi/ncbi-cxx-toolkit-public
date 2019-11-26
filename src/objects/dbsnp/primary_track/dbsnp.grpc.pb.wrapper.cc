f8f14a419bfefc5fb57522ad2345f4993c9fd25a
Eugene Vasilchenko <vasilche@ncbi.nlm.nih.gov>
2019-11-26 16:29:55 +0000

    Use wappers for generated code. JIRA: CXX-11007
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@88346 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ src/objects/dbsnp/primary_track/dbsnp.grpc.pb.wrapper.cc
@@ -0,0 +1,5 @@
+#include <ncbi_pch.hpp>
+#include <ncbiconf.h>
+#ifdef HAVE_LIBGRPC
+# include "dbsnp.grpc.pb.cc"
+#endif
---
+++ src/objects/dbsnp/primary_track/dbsnp.pb.wrapper.cc
@@ -0,0 +1,5 @@
+#include <ncbi_pch.hpp>
+#include <ncbiconf.h>
+#ifdef HAVE_LIBGRPC
+# include "dbsnp.pb.cc"
+#endif
