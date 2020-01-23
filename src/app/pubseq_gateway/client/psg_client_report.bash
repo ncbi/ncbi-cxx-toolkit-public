3b2c47917f19ab5662afe5d1826212880fa8eb67
Rafael Sadyrov <sadyrovr@ncbi.nlm.nih.gov>
2020-01-23 20:29:43 +0000

    Improved script for measuring performance. JIRA CXX-11151
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@88836 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/app/pubseq_gateway/client/psg_client_report.bash
+++ src/app/pubseq_gateway/client/psg_client_report.bash
@@ -1,50 +1,116 @@
 #!/bin/bash
 
-if [ $# -lt 9 ]; then
-    echo "Usage: $0 SERVICE REQUESTS ITERATIONS USER_THREADS IO_THREADS INPUT_FILE SKIP_FIRST BINARY BINARY [BINARY ...]";
+if [ $# -ne 8 ]; then
+    echo "Usage: $0 COMMAND SERVICE REQUESTS ITERATIONS INPUT_FILE IO_THREADS REQUESTS_PER_IO BINARIES";
     exit 1;
 fi
 
-SERVICE="$1"
-REQUESTS="$2"
-ITERATIONS="$3"
-USER_THREADS="$4"
-IO_THREADS="$5"
-INPUT_FILE="$6"
-SKIP_FIRST=$7
-shift 7
-BINARIES=($@)
+COMMAND="$1"
+SERVICE="$2"
+REQUESTS="$3"
+ITERATIONS="$4"
+INPUT_FILE="$5"
+IO_THREADS=($6)
+REQUESTS_PER_IO=($7)
+BINARIES=($8)
+
+case "$COMMAND" in
+"resolve")
+    CMD=1
+    ;;
+"interactive")
+    CMD=2
+    ;;
+"performance")
+    CMD=3
+    ;;
+*)
+    echo "Unknown command: $COMMAND";
+    exit 2
+    ;;
+esac
+
+if [ -e psg_client.ini ]; then
+    echo "psg_client.ini exists"
+    exit 3;
+fi
+
+if [ ! -f $INPUT_FILE ]; then
+    echo "$INPUT_FILE does not exist or is not a file"
+    exit 4;
+fi
+
+for binary in ${BINARIES[@]}; do
+    if [ ! -x $binary/psg_client ]; then
+        echo "$binary/psg_client does not exist or is not executable"
+        exit 5;
+    fi;
+done
 
 function create_path
 {
     local IFS="-"
-    OUTPUT_PATH="$*/$SERVICE/$TYPE/$USER_THREADS-$IO_THREADS/$REQUESTS-$ITERATIONS-$SKIP_FIRST"
+    local FILE=$(basename $INPUT_FILE)
+    OUTPUT_PATH="$*/$SERVICE/$COMMAND/$FILE/$REQUESTS-$ITERATIONS"
     mkdir -p "$OUTPUT_PATH"
 }
 
 function run
 {
-    if [ $SKIP_FIRST -eq 0 ]; then
-        head -$REQUESTS $INPUT_FILE |$1/psg_client performance -service $SERVICE -user-threads $USER_THREADS -io-threads $IO_THREADS -local-queue -use-cache yes -raw-metrics;
+    OUTPUT_FILE="$OUTPUT_DIR/raw.$1.$2.$3.txt"
+    echo -e "[PSG]\nuse_cache=yes\nnum_io=$1\nrequests_per_io=$2" > psg_client.ini
+
+    if [ $CMD -eq 1 ]; then
+        head -$REQUESTS $INPUT_FILE |/usr/bin/time -ao $OUTPUT_FILE -f "%e" $3/psg_client resolve -service $SERVICE -id-file - >/dev/null;
+        echo -n "."
+    elif [ $CMD -eq 2 ]; then
+        head -$REQUESTS $INPUT_FILE |/usr/bin/time -ao $OUTPUT_FILE -f "%e" $3/psg_client interactive -service $SERVICE >/dev/null;
+        echo -n "."
     else
-        SKIP_FIRST=$(($REQUESTS+$SKIP_FIRST));
-        head -$SKIP_FIRST $INPUT_FILE |tail -$REQUESTS |$1/psg_client performance -service $SERVICE -user-threads $USER_THREADS -io-threads $IO_THREADS -local-queue -use-cache yes -raw-metrics -delay 1;
+        head -$REQUESTS $INPUT_FILE |$3/psg_client performance -service $SERVICE -user-threads $1 -local-queue -raw-metrics >/dev/null;
+
+        # Old binaries print named events, so need to replace names
+        sed -i '{ s/Start/0/; s/Submit/1/; s/Reply/2/; s/Send/1000/; s/Receive/1001/; s/Close/1002/; s/Retry/1003/; s/Fail/1004/; s/Done/3/; }' psg_client.raw.txt
+
+        # Need to rename request numbers to avoid overlap
+        awk "{ \$1 += ($4 - 1) * $REQUESTS; print }" psg_client.raw.txt >> $OUTPUT_FILE;
+
+        # Old binaries create complex metrics on performance command, so need to remove both files
+        rm psg_client.raw.txt psg_client.table.txt 2>/dev/null;
     fi
 }
 
-TYPE=$(basename $INPUT_FILE .json)
+function report
+{
+    local INPUT_FILE="$OUTPUT_DIR/raw.$1.$2.$3.txt"
+    local OUTPUT_FILE="$OUTPUT_DIR/pro.$1.$2.$3.txt"
 
-for bin in "${BINARIES[@]}"; do
-    if [ ! -x $bin/psg_client ]; then
-        echo "$bin/psg_client does not exist or is not executable"
-        exit 2;
-    fi;
-done
+    if [ $CMD -eq 3 ]; then
+        $4/psg_client report -input-file "$INPUT_FILE" -output-file "$OUTPUT_FILE";
+    else
+        sort -n "$INPUT_FILE" >> "$OUTPUT_FILE";
+    fi
+}
 
-if [ ! -f $INPUT_FILE ]; then
-    echo "$INPUT_FILE does not exist or is not a file"
-    exit 3;
-fi
+function aggregate
+{
+    local INPUT_FILE="$OUTPUT_DIR/pro.$1.$2.$3.txt"
+
+    (
+        echo -n "$3 $1 $2 ";
+
+        if [ $CMD -eq 3 ]; then
+            local MEDIAN=$(($REQUESTS * $ITERATIONS + 5))
+            local AVERAGE=$(($REQUESTS * $ITERATIONS + 10))
+            sed -n "{${MEDIAN}p;${AVERAGE}p}" "$INPUT_FILE" |awk '{print $2}' |xargs
+        else
+            local MEDIAN=$(($ITERATIONS / 2))
+            awk "NR==$MEDIAN { m = \$1 } { t += \$1 } END { print m, t / NR }" "$INPUT_FILE"
+        fi
+    ) |sed 's/ /;/g'>> "$4"
+
+    echo -n "."
+}
 
 create_path ${BINARIES[@]}
 
@@ -57,31 +123,52 @@ for ((i = 1; 1000; ++i)); do
     fi;
 done
 
-if [ $SKIP_FIRST -eq 0 ]; then
-    # A warm-up
-    run ${BINARIES[0]};
-fi
+fb=${BINARIES[0]}
+
+# A warm-up
+run 8 1 $fb 1 ">/dev/null"
+rm "$OUTPUT_FILE"
 
 for ((i = 1; $i <= $ITERATIONS; ++i)); do
-    for bin in "${BINARIES[@]}"; do
-        sleep 0.25;
-        run $bin
-        sleep 0.25;
-
-        # Old binaries print named events, so need to replace names
-        sed -i '{ s/Start/0/; s/Submit/1/; s/Reply/2/; s/Send/1000/; s/Receive/1001/; s/Close/1002/; s/Retry/1003/; s/Fail/1004/; s/Done/3/; }' psg_client.raw.txt
-
-        # Need to rename request numbers to avoid overlap
-        awk "{ \$1 += ($i - 1) * $REQUESTS; print }" psg_client.raw.txt >> $OUTPUT_DIR/raw.${bin}.txt;
-
-        # Old binaries create complex metrics on performance command, so need to remove both files
-        rm psg_client.raw.txt psg_client.table.txt 2>/dev/null;
-    done;
+    for t in ${IO_THREADS[@]}; do
+        for r in ${REQUESTS_PER_IO[@]}; do
+            for b in ${BINARIES[@]}; do
+                run $t $r $b $i
+            done
+        done
+    done
 done;
 
-for bin in "${BINARIES[@]}"; do
-    # Old binaries do not have report command, so need to use new binary
-    ${BINARIES[0]}/psg_client report -input-file $OUTPUT_DIR/raw.${bin}.txt -output-file $OUTPUT_DIR/table.${bin}.txt;
-done;
+echo
+rm psg_client.ini
 
-echo "vim -R -O -c ':windo :set nowrap' -c ':windo normal ggG' -c ':windo :set scb' $OUTPUT_DIR/table.*"
+for t in ${IO_THREADS[@]}; do
+    for r in ${REQUESTS_PER_IO[@]}; do
+        for b in ${BINARIES[@]}; do
+            report $t $r $b $fb
+        done
+    done
+done
+
+OUTPUT_FILE="$OUTPUT_DIR/agg.csv"
+
+cat << EOF > $OUTPUT_FILE
+Legend:	
+app;Version of psg_client application
+io;Number of I/O threads
+req;Number of requests consecutively given to the same I/O thread
+med;Median ($REQUESTS requests/run, $ITERATIONS runs)
+avg;Average ($REQUESTS requests/run, $ITERATIONS runs)
+*Measurements performed on $(uname -n) using $SERVICE service
+
+app;io;req;med;avg
+EOF
+
+for t in ${IO_THREADS[@]}; do
+    for r in ${REQUESTS_PER_IO[@]}; do
+        for b in ${BINARIES[@]}; do
+            aggregate $t $r $b $OUTPUT_FILE
+        done
+    done
+done
+echo
