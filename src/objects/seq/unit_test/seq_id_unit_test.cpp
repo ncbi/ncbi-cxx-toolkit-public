1da4f4043233f89fd35f9e2171c5916ad98c7436
Aaron Ucko <ucko@ncbi.nlm.nih.gov>
2020-01-29 19:36:57 +0000

    seq_id_unit_test (s_TestTypeMismatch): static_cast raw E_Choice expectations.
    Silence warnings about comparing EAccessionInfo to E_Choice.  JIRA: CXX-10895.
    
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@88884 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objects/seq/unit_test/seq_id_unit_test.cpp
+++ src/objects/seq/unit_test/seq_id_unit_test.cpp
@@ -2228,10 +2228,12 @@ BOOST_AUTO_TEST_CASE(s_TestTypeMismatch)
     BOOST_CHECK_EQUAL(CSeq_id::IdentifyAccession(acc),
                       CSeq_id::eAcc_embl_wgs_nuc);
     BOOST_CHECK_NO_THROW(id.Reset(new CSeq_id(CSeq_id::e_Genbank, acc)));
-    BOOST_CHECK_EQUAL(id->IdentifyAccession(), CSeq_id::e_Genbank);
+    BOOST_CHECK_EQUAL(id->IdentifyAccession(),
+                      static_cast<CSeq_id::EAccessionInfo>(CSeq_id::e_Genbank));
 
     acc = "F12345";
     BOOST_CHECK_NO_THROW(id.Reset(new CSeq_id(CSeq_id::e_Tpe, acc)));
-    BOOST_CHECK_EQUAL(id->IdentifyAccession(), CSeq_id::e_Tpe);
+    BOOST_CHECK_EQUAL(id->IdentifyAccession(),
+                      static_cast<CSeq_id::EAccessionInfo>(CSeq_id::e_Tpe));
     BOOST_CHECK_EQUAL(CSeq_id::IdentifyAccession(acc), CSeq_id::eAcc_embl_est);
 }
