de30a695f0ff04a627cb9849650e4d6caa40f061
Aleksey Grichenko <grichenk@ncbi.nlm.nih.gov>
2020-04-30 12:32:31 +0000

    Fixed TEntrezId usage. JIRA CXX-10489
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@89898 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objects/seq/Bioseq.cpp
+++ src/objects/seq/Bioseq.cpp
@@ -178,8 +178,8 @@ int CBioseq::GetTaxId() const
 {
     /// A taxid can be found either in a source descriptor (the newer form) or in a
     /// org descriptor. If both are there, the source descriptor should have precedence.
-    int taxid_from_source = 0,
-        taxid_from_org = 0;
+    TEntrezId taxid_from_source = ZERO_ENTREZ_ID,
+        taxid_from_org = ZERO_ENTREZ_ID;
 
     if (IsSetDescr()) {
         ITERATE (TDescr::Tdata, it, GetDescr().Get()) {
@@ -189,13 +189,13 @@ int CBioseq::GetTaxId() const
             } else if (desc.IsSource() && desc.GetSource().IsSetOrg()) {
                 taxid_from_source = desc.GetSource().GetOrg().GetTaxId();
             }
-            if (taxid_from_source) {
+            if (taxid_from_source != ZERO_ENTREZ_ID) {
                 break;
             }
         }
     }
 
-    return taxid_from_source ? taxid_from_source : taxid_from_org;
+    return ENTREZ_ID_TO(int, taxid_from_source != ZERO_ENTREZ_ID ? taxid_from_source : taxid_from_org);
 }
 
 
