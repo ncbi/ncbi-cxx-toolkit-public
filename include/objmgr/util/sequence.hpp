f2e89054a867dc3e4b302804e591ff81f9e06545
Justin Foley <foleyjp@ncbi.nlm.nih.gov>
2020-05-05 11:30:24 +0000

    JIRA: RW-1082 CFastaOstream::WriteSequence() defaults to using CSeq_loc::fMerger_AbuttingOnly
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@90020 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/objmgr/util/sequence.hpp
+++ include/objmgr/util/sequence.hpp
@@ -794,7 +794,7 @@ public:
                                const string& custom_title = kEmptyStr);
     virtual void WriteSequence(const CBioseq_Handle& handle,
                                const CSeq_loc* location = 0, 
-                               CSeq_loc::EOpFlags merge_flags=CSeq_loc::fMerge_All);
+                               CSeq_loc::EOpFlags merge_flags=CSeq_loc::fMerge_AbuttingOnly);
 
     /// These versions may set up a temporary object manager scope
     /// In the common case of a raw bioseq, no scope is needed
