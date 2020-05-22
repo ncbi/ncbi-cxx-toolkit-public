6331481ca16fb908b80521cdee9150c89e68f89b
Denis Vakatov <vakatov@ncbi.nlm.nih.gov>
2020-05-22 05:25:53 +0000

    Add THREAD to the list of Boost components.  JIRA FLS-99
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@90224 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/build-system/cmake/CMakeChecks.boost.cmake
+++ src/build-system/cmake/CMakeChecks.boost.cmake
@@ -35,7 +35,7 @@ endif()
 
 #set(Boost_DEBUG ON)
 find_package(Boost
-             COMPONENTS filesystem iostreams date_time regex system serialization
+             COMPONENTS filesystem iostreams date_time regex system serialization thread
              REQUIRED)
 set(CMAKE_PREFIX_PATH ${_foo_CMAKE_PREFIX_PATH})
 
