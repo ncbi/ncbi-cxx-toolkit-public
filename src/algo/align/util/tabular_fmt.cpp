f65cc290fa81286f118e0d1745154d56a38b264e
Aleksey Grichenko <grichenk@ncbi.nlm.nih.gov>
2020-06-08 15:55:00 +0000

    Introducing strict TTaxId. JIRA CXX-10489
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@90370 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/algo/align/util/tabular_fmt.cpp
+++ src/algo/align/util/tabular_fmt.cpp
@@ -1536,7 +1536,7 @@ void CTabularFormatter_OrgName::Print(CNcbiOstream& ostr,
         m_Taxon1->Init(100000);
     }
 
-    TTaxId taxid = ENTREZ_ID_FROM(int,
+    TTaxId taxid = TAX_ID_FROM(int,
         (int)m_Scores->GetScore(align,
                                 m_Row == 0 ? "query_taxid"
                                 : "subject_taxid"));
