43dd2164ecbba95e4b8283bfb9a3b8c1680dac25
Amelia Fong <fongah2@ncbi.nlm.nih.gov>
2020-08-04 13:19:41 +0000

    Add unit test for lmdb map size, JIRA:SB-2879
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@90879 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/blast/seqdb_writer/unit_test/writedb_lmdb_unit_test.cpp
+++ src/objtools/blast/seqdb_writer/unit_test/writedb_lmdb_unit_test.cpp
@@ -146,6 +146,103 @@ BOOST_AUTO_TEST_CASE(CreateLMDBFile)
 }
 
 
+BOOST_AUTO_TEST_CASE(TestLMDBMapSize)
+{
+	const string base_name = "tmp_lmdb";
+	DeleteLMDBFiles(true, base_name);
+	const string lmdb_name = BuildLMDBFileName(base_name, true);
+	const string tax_lmdb = GetFileNameFromExistingLMDBFile(lmdb_name, ELMDBFileType::eTaxId2Offsets);
+	const int kNumVols = 4;
+	CSeqDB source_db("data/writedb_prot",CSeqDB::eProtein);
+	vector<string> vol_names;
+	vector<blastdb::TOid> vol_num_oids;
+	for(unsigned int k=0; k < kNumVols; k++) {
+		vol_names.push_back("tmp_lmdb" + NStr::IntToString(k));
+		vol_num_oids.push_back(k*1234);
+	}
+
+	{
+		CWriteDB_LMDB test_db(lmdb_name, 10);
+		for (int i=0; i < source_db.GetNumOIDs(); i++) {
+			list< CRef<CSeq_id> >  ids = source_db.GetSeqIDs(i);
+			test_db.InsertEntries(ids, i);
+		}
+		test_db.InsertVolumesInfo(vol_names, vol_num_oids);
+
+		CWriteDB_TaxID taxdb(tax_lmdb,10);
+	    const TTaxId taxids[5] = { TAX_ID_CONST(9606), TAX_ID_CONST(562), TAX_ID_CONST(0), TAX_ID_CONST(2), TAX_ID_CONST(10239) };
+		for (int i=0; i < source_db.GetNumOIDs(); i++) {
+			set<TTaxId> t;
+			for(int j=0; j < (i % 5 + 1); j++) {
+				t.insert(taxids[j]);
+			}
+			taxdb.InsertEntries(t, i);
+		}
+	}
+
+	{
+		vector<string> test_neg_accs;
+		CSeqDBLMDB test_db(lmdb_name);
+
+		/* Test GetOids from Seq IDs */
+		for(int i=0; i < source_db.GetNumOIDs(); i++) {
+			vector<string> test_accs;
+			vector<blastdb::TOid> test_oids;
+			list< CRef<CSeq_id> >  ids = source_db.GetSeqIDs(i);
+			CRef<CSeq_id> n_id = FindBestChoice(ids, CSeq_id::WorstRank);
+			test_neg_accs.push_back(n_id->GetSeqIdString(false));
+			ITERATE(list< CRef<CSeq_id> >, itr, ids) {
+				if((*itr)->IsGi()) {
+					continue;
+				}
+				test_accs.push_back((*itr)->GetSeqIdString(true));
+				test_accs.push_back((*itr)->GetSeqIdString(false));
+			}
+			test_db.GetOids(test_accs, test_oids);
+			for(unsigned int j=0; j < test_accs.size(); j++) {
+				BOOST_REQUIRE_EQUAL(test_oids[j], i);
+			}
+		}
+
+		/* Test Negative Seq IDs  to OIDs */
+		vector<blastdb::TOid> neg_oids;
+		test_db.NegativeSeqIdsToOids(test_neg_accs, neg_oids);
+		BOOST_REQUIRE_EQUAL(neg_oids.size(), 65);
+
+		/* Test Vol Info */
+		vector<string> test_vol_names;
+		vector<blastdb::TOid> test_vol_num_oids;
+		test_db.GetVolumesInfo(test_vol_names, test_vol_num_oids);
+		for(unsigned int k=0; k < kNumVols; k++) {
+			BOOST_REQUIRE_EQUAL(test_vol_num_oids[k], vol_num_oids[k]);
+			BOOST_REQUIRE_EQUAL(test_vol_names[k], vol_names[k]);
+		}
+
+		/* Test Tax Ids */
+		vector<blastdb::TOid> tax_oids;
+		set<TTaxId> tax_ids;
+		tax_ids.insert(TAX_ID_CONST(10239));
+		vector<TTaxId> rv_tax_ids;
+		test_db.GetOidsForTaxIds(tax_ids, tax_oids, rv_tax_ids);
+		for(unsigned int i=0; i < tax_ids.size(); i++) {
+			BOOST_REQUIRE_EQUAL(tax_oids[i] % 5, 4);
+		}
+
+		test_db.NegativeTaxIdsToOids(tax_ids, tax_oids, rv_tax_ids);
+		BOOST_REQUIRE_EQUAL(tax_oids.size(), 0);
+
+		tax_ids.clear();
+		tax_ids.insert(9606);
+		tax_ids.insert(562);
+		test_db.NegativeTaxIdsToOids(tax_ids, tax_oids, rv_tax_ids);
+		for(unsigned int i=0; i < rv_tax_ids.size(); i++) {
+			BOOST_REQUIRE((tax_oids[i] % 5 < 2));
+		}
+
+	}
+	DeleteLMDBFiles(true, base_name);
+}
+
 
 BOOST_AUTO_TEST_SUITE_END()
 
