d0a30a0a41951791ef8e98bcf37d253e604c8a56
kachalos <kachalos@ncbi.nlm.nih.gov>
2020-08-12 16:41:37 +0000

    Added std scope. JIRA: GB-9001
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@90939 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/objtools/flatfile/ftacpp.hpp
+++ include/objtools/flatfile/ftacpp.hpp
@@ -38,34 +38,34 @@
 #define FTACPP_HPP
 
 #include <objtools/flatfile/ftaerr.hpp>
-inline void* MemNew(size_t sz) { void* p = malloc(sz); memset(p, 0, sz); return p; }
-inline void* MemSet(void* p, int n, size_t sz) { return memset(p, n, sz); }
-inline void* MemCpy(void* p, void* q, size_t sz) { return memcpy(p, q, sz); }
-inline void* MemFree(void* p) { free(p); return 0; }
+inline void* MemNew(size_t sz) { void* p = std::malloc(sz); std::memset(p, 0, sz); return p; }
+inline void* MemSet(void* p, int n, size_t sz) { return std::memset(p, n, sz); }
+inline void* MemCpy(void* p, void* q, size_t sz) { return std::memcpy(p, q, sz); }
+inline void* MemFree(void* p) { std::free(p); return 0; }
 
-inline size_t StringLen(const char* s) { return strlen(s); }
-inline char* StringSave(const char* s) { if (!s) return 0; const size_t n = strlen(s) + 1; char* p = (char*)malloc(n); memcpy(p, s, n); return p; }
-inline char* StringStr(const char* s1, const char* s2) { return const_cast<char*>(strstr(s1, s2)); }
-inline char* StringCat(char* d, const char* s) { return strcat(d, s); }
-inline char* StringNCat(char* d, const char* s, size_t n) { return strncat(d, s, n); }
-inline char* StringCpy(char* d, const char* s) { return strcpy(d, s); }
-inline char* StringNCpy(char* d, const char* s, size_t n) { return strncpy(d, s, n); }
-inline char* StringChr(const char* s, const int c) { return const_cast<char*>(strchr(s, c)); }
-inline char* StringRChr(char* s, const int c) { return strrchr(s, c); }
-inline int StringCmp(const char* s1, const char* s2) { return strcmp(s1, s2); }
-inline int StringNCmp(const char* s1, const char* s2, size_t n) { return strncmp(s1, s2, n); }
+inline size_t StringLen(const char* s) { return std::strlen(s); }
+inline char* StringSave(const char* s) { if (!s) return 0; const size_t n = std::strlen(s) + 1; char* p = (char*)std::malloc(n); std::memcpy(p, s, n); return p; }
+inline char* StringStr(const char* s1, const char* s2) { return const_cast<char*>(std::strstr(s1, s2)); }
+inline char* StringCat(char* d, const char* s) { return std::strcat(d, s); }
+inline char* StringNCat(char* d, const char* s, size_t n) { return std::strncat(d, s, n); }
+inline char* StringCpy(char* d, const char* s) { return std::strcpy(d, s); }
+inline char* StringNCpy(char* d, const char* s, size_t n) { return std::strncpy(d, s, n); }
+inline char* StringChr(const char* s, const int c) { return const_cast<char*>(std::strchr(s, c)); }
+inline char* StringRChr(char* s, const int c) { return std::strrchr(s, c); }
+inline int StringCmp(const char* s1, const char* s2) { return std::strcmp(s1, s2); }
+inline int StringNCmp(const char* s1, const char* s2, size_t n) { return std::strncmp(s1, s2, n); }
 
 inline int StringICmp(const char* s1, const char* s2) { return NStr::CompareNocase(s1, s2); }
 inline int StringNICmp(const char* s1, const char* s2, size_t n) { const string S1(s1), S2(s2); return NStr::CompareNocase(S1.substr(0, n), S2.substr(0, n)); }
 
-inline char* StringMove(char* d, const char* s) { return strcpy(d, s); }
+inline char* StringMove(char* d, const char* s) { return std::strcpy(d, s); }
 
 inline bool StringHasNoText(const char* s) {
     if (s) while (*s) if ((unsigned char)(*s++) > ' ') return false;
     return true;
 }
 
-inline char* SkipSpaces(char* s) { while (*s && isspace(*s)) s++; return s; }
+inline char* SkipSpaces(char* s) { while (*s && std::isspace(*s)) s++; return s; }
 
 #define IS_DIGIT(c)	('0'<=(c) && (c)<='9')
 #define IS_UPPER(c)	('A'<=(c) && (c)<='Z')
