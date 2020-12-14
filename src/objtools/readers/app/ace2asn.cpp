4e7be136195d5aa03d2f73ead37387f4068db661
Aleksey Grichenko <grichenk@ncbi.nlm.nih.gov>
2020-12-14 17:25:11 +0000

    Replaced autp_ptr with unique_ptr. JIRA CXX-11620
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@91815 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/readers/app/ace2asn.cpp
+++ src/objtools/readers/app/ace2asn.cpp
@@ -61,7 +61,7 @@ public:
 
 void CAce2AsnApp::Init(void)
 {
-    auto_ptr<CArgDescriptions> arg_desc(new CArgDescriptions);
+    unique_ptr<CArgDescriptions> arg_desc(new CArgDescriptions);
 
     arg_desc->AddKey("ace", "AceFile", "Source ACE file",
                      CArgDescriptions::eInputFile);
