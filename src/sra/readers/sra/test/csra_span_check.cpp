8b50140b85b9475c300dd5dce8a3c3b569dc696b
Aleksey Grichenko <grichenk@ncbi.nlm.nih.gov>
2020-12-16 19:23:33 +0000

    Replaced autp_ptr with unique_ptr. JIRA CXX-11620
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@91938 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/sra/readers/sra/test/csra_span_check.cpp
+++ src/sra/readers/sra/test/csra_span_check.cpp
@@ -225,7 +225,7 @@ CSpanCheckApp::CSpanCheckApp()
 void CSpanCheckApp::Init(void)
 {
     // Create command-line argument descriptions class
-    auto_ptr<CArgDescriptions> arg_desc(new CArgDescriptions);
+    unique_ptr<CArgDescriptions> arg_desc(new CArgDescriptions);
 
     // Specify USAGE context
     arg_desc->SetUsageContext(GetArguments().GetProgramBasename(),
