6e10e03d02cfa73a3f84386d48476a53c69c2c16
Aleksey Grichenko <grichenk@ncbi.nlm.nih.gov>
2020-12-16 19:23:33 +0000

    Replaced autp_ptr with unique_ptr. JIRA CXX-11620
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@91937 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/sra/readers/bam/test/bamindex_test.cpp
+++ src/sra/readers/bam/test/bamindex_test.cpp
@@ -71,7 +71,7 @@ private:
 void CBamIndexTestApp::Init(void)
 {
     // Create command-line argument descriptions class
-    auto_ptr<CArgDescriptions> arg_desc(new CArgDescriptions);
+    unique_ptr<CArgDescriptions> arg_desc(new CArgDescriptions);
 
     InitCommonArgs(*arg_desc);
 
