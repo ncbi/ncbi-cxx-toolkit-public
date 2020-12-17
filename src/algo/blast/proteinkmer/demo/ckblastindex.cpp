d8237d204b457beb48db1e6d55a5b4ecc65a1ad8
Aleksey Grichenko <grichenk@ncbi.nlm.nih.gov>
2020-12-17 15:27:50 +0000

    Replaced auto_ptr with unique_ptr. JIRA CXX-11620
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@92026 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/algo/blast/proteinkmer/demo/ckblastindex.cpp
+++ src/algo/blast/proteinkmer/demo/ckblastindex.cpp
@@ -72,7 +72,7 @@ void CCkblastindexApplication::Init(void)
     // SetDiagPostLevel(eDiag_Info);
 
     // Create command-line argument descriptions class
-    auto_ptr<CArgDescriptions> arg_desc(new CArgDescriptions);
+    unique_ptr<CArgDescriptions> arg_desc(new CArgDescriptions);
 
     // Specify USAGE context
     arg_desc->SetUsageContext(GetArguments().GetProgramBasename(),
