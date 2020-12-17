cbdc22f9d291df6e1c5a40d0d61237497755155f
Aleksey Grichenko <grichenk@ncbi.nlm.nih.gov>
2020-12-17 15:27:44 +0000

    Replaced auto_ptr with unique_ptr. JIRA CXX-11620
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@92021 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/algo/blast/gumbel_params/demo/gumbelparams_app.cpp
+++ src/algo/blast/gumbel_params/demo/gumbelparams_app.cpp
@@ -90,7 +90,7 @@ void CGumbelParamsApplication::Init(void)
 {
         
     // Create command-line argument descriptions class
-    auto_ptr<CArgDescriptions> arg_desc(new CArgDescriptions);
+    unique_ptr<CArgDescriptions> arg_desc(new CArgDescriptions);
 
     // Specify USAGE context
     arg_desc->SetUsageContext(GetArguments().GetProgramBasename(),
