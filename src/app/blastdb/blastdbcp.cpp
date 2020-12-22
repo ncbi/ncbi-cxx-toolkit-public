19e51c99fd8b9d815f8e85a53295056c94881ca2
Aleksey Grichenko <grichenk@ncbi.nlm.nih.gov>
2020-12-22 16:57:49 +0000

    Replaced auto_ptr with unique_ptr. JIRA CXX-11620
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@92144 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/app/blastdb/blastdbcp.cpp
+++ src/app/blastdb/blastdbcp.cpp
@@ -104,7 +104,7 @@ BlastdbCopyApplication::BlastdbCopyApplication()
 void BlastdbCopyApplication::Init(void)
 {
     // Create command-line argument descriptions class
-    auto_ptr<CArgDescriptions> arg_desc(new CArgDescriptions);
+    unique_ptr<CArgDescriptions> arg_desc(new CArgDescriptions);
 
     // Specify USAGE context
     arg_desc->SetUsageContext(
