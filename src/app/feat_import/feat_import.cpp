63589ddc6be99a6bd68bc6a76b00b40159e3239b
Aleksey Grichenko <grichenk@ncbi.nlm.nih.gov>
2020-12-22 18:16:56 +0000

    Replaced auto_ptr with unique_ptr. JIRA CXX-11620
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@92192 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/app/feat_import/feat_import.cpp
+++ src/app/feat_import/feat_import.cpp
@@ -84,7 +84,7 @@ void
 CFeatImportApp::Init(void)
 //  ============================================================================
 {
-    auto_ptr<CArgDescriptions> arg_desc(new CArgDescriptions);
+    unique_ptr<CArgDescriptions> arg_desc(new CArgDescriptions);
 
     arg_desc->SetUsageContext
         (GetArguments().GetProgramBasename(),
