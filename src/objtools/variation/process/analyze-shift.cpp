ffe63590977f3797d03fdde17e7fcc0057e27c65
Aleksey Grichenko <grichenk@ncbi.nlm.nih.gov>
2020-12-22 17:14:11 +0000

    Replaced auto_ptr with unique_ptr. JIRA CXX-11620
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@92155 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/variation/process/analyze-shift.cpp
+++ src/objtools/variation/process/analyze-shift.cpp
@@ -90,7 +90,7 @@ private:
 
 void CAnalyzeShiftApp::Init(void)
 {
-    auto_ptr<CArgDescriptions> arg_desc(new CArgDescriptions);
+    unique_ptr<CArgDescriptions> arg_desc(new CArgDescriptions);
     arg_desc->SetUsageContext(GetArguments().GetProgramBasename(),"Analyze Shift in HGVS objects");
     arg_desc->AddDefaultKey("i", "input", "Input file",CArgDescriptions::eInputFile, "-", CArgDescriptions::fPreOpen);
     arg_desc->AddDefaultKey("o", "output", "Output file",CArgDescriptions::eOutputFile,"-",CArgDescriptions::fPreOpen);
