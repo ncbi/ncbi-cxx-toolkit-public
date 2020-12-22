213a08170574e0a96d27eabd95d61e5d6c571b7a
Aleksey Grichenko <grichenk@ncbi.nlm.nih.gov>
2020-12-22 16:37:09 +0000

    Replaced auto_ptr with unique_ptr. JIRA CXX-11620
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@92124 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/app/blast/blast_formatter.cpp
+++ src/app/blast/blast_formatter.cpp
@@ -113,7 +113,7 @@ void CBlastFormatterApp::Init()
 {
     HideStdArgs(fHideLogfile | fHideConffile | fHideFullVersion | fHideXmlHelp | fHideDryRun);
 
-    auto_ptr<CArgDescriptions> arg_desc(new CArgDescriptions);
+    unique_ptr<CArgDescriptions> arg_desc(new CArgDescriptions);
 
     arg_desc->SetUsageContext(GetArguments().GetProgramBasename(), 
                   "Stand-alone BLAST formatter client, version " 
