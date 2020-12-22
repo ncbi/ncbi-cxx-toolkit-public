f734a04980f94eb741e6f6933742d86ecf3ecde7
Aleksey Grichenko <grichenk@ncbi.nlm.nih.gov>
2020-12-22 16:57:37 +0000

    Replaced auto_ptr with unique_ptr. JIRA CXX-11620
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@92142 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/app/blastdb/blastdbcheck.cpp
+++ src/app/blastdb/blastdbcheck.cpp
@@ -144,7 +144,7 @@ void CBlastDbCheckApplication::Init(void)
     HideStdArgs(fHideLogfile | fHideConffile | fHideFullVersion | fHideXmlHelp | fHideDryRun);
 
     // Create command-line argument descriptions class
-    auto_ptr<CArgDescriptions> arg_desc(new CArgDescriptions);
+    unique_ptr<CArgDescriptions> arg_desc(new CArgDescriptions);
 
     // Specify USAGE context
     arg_desc->SetUsageContext(GetArguments().GetProgramBasename(),
