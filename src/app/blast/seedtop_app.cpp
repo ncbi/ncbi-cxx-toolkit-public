430f8c86aef3c817c1177c78535c42454242390e
Aleksey Grichenko <grichenk@ncbi.nlm.nih.gov>
2020-12-22 16:37:26 +0000

    Replaced auto_ptr with unique_ptr. JIRA CXX-11620
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@92126 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/app/blast/seedtop_app.cpp
+++ src/app/blast/seedtop_app.cpp
@@ -111,7 +111,7 @@ void CSeedTopApp::Init()
 {
     HideStdArgs(fHideLogfile | fHideConffile | fHideFullVersion | fHideXmlHelp | fHideDryRun);
 
-    auto_ptr<CArgDescriptions> arg_desc(new CArgDescriptions);
+    unique_ptr<CArgDescriptions> arg_desc(new CArgDescriptions);
 
     arg_desc->SetUsageContext(GetArguments().GetProgramBasename(),
                  "Application to find pattern in BLAST databases or subject sequences, version "
