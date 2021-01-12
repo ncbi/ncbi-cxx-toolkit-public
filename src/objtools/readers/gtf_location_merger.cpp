f8780046a94b500fdbc2677bc65ea96a59cc9a18
ludwigf <ludwigf@ncbi.nlm.nih.gov>
2021-01-12 20:38:20 +0000

    JIRA: RW-845 Temporarily replace stable_sort() with sort(), until I figure out how to  get the stable_sort() version pf the code to compile.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@92365 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/readers/gtf_location_merger.cpp
+++ src/objtools/readers/gtf_location_merger.cpp
@@ -296,9 +296,10 @@ CGtfLocationMerger::MergeLocationDefault(
         pSeqloc = onlyOne.GetLocation(); 
         return pSeqloc;
     }
-    std::stable_sort(
-        locations.begin(), locations.end(), CGtfLocationRecord::ComparePartNumbers);
-    //locations.sort(CGtfLocationRecord::ComparePartNumbers);
+    //using CCompare = bool(*)(const CGtfLocationRecord&, const CGtfLocationRecord&);
+    //std::stable_sort<LOCATIONS::iterator>(
+    //    locations.begin(), locations.end(), CGtfLocationRecord::ComparePartNumbers);
+    locations.sort(CGtfLocationRecord::ComparePartNumbers);
     auto& mix = pSeqloc->SetMix();
     for (auto& location: locations) {
         mix.AddSeqLoc(*location.GetLocation());
@@ -315,8 +316,9 @@ CGtfLocationMerger::MergeLocationForCds(
     NCBI_ASSERT(!locations.empty(), 
         "Cannot call MergeLocationForCds with empty location");
 
-    std::stable_sort(
-        locations.begin(), locations.end(), CGtfLocationRecord::ComparePartNumbers);
+    //std::stable_sort<LOCATIONS::iterator>(
+    //    locations.begin(), locations.end(), CGtfLocationRecord::ComparePartNumbers);
+    locations.sort(CGtfLocationRecord::ComparePartNumbers);
     CRef<CSeq_loc> pSeqloc(new CSeq_loc);
     auto& mix = pSeqloc->SetMix();
     for (auto& location: locations) {
