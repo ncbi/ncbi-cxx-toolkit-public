5ad88fbc5651118e34a3b40abcde36cf224c3b21
Aleksey Grichenko <grichenk@ncbi.nlm.nih.gov>
2021-01-26 20:12:40 +0000

    Replaced auto_ptr with unique_ptr. JIRA CXX-11687
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@92521 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/algo/align/util/demo/demo_score_builder.cpp
+++ src/algo/align/util/demo/demo_score_builder.cpp
@@ -64,7 +64,7 @@ private:
 void CDemoScoreBuilderApp::Init(void)
 {
     // Create command-line argument descriptions class
-    auto_ptr<CArgDescriptions> arg_desc(new CArgDescriptions);
+    unique_ptr<CArgDescriptions> arg_desc(new CArgDescriptions);
 
     arg_desc->AddDefaultKey("i", "InputFile",
                             "Seq-align to evaluate",
