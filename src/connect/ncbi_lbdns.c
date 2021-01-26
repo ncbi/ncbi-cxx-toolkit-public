01a5c94ff3a20ee2d0f8d207c989273fe0743ecb
Anton Lavrentiev <lavr@ncbi.nlm.nih.gov>
2021-01-26 20:12:20 +0000

    Skeleton implementation (still empty) JIRA: CXX-11616
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@92518 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/connect/ncbi_lbdns.c
+++ src/connect/ncbi_lbdns.c
@@ -31,25 +31,151 @@
  *
  */
 
+#include "ncbi_config.h"
 #include "ncbi_lbdns.h"
-#include "ncbi_priv.h"
+
 #ifdef NCBI_OS_UNIX
+
+#include "ncbi_priv.h"
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <arpa/nameser.h>
 #include <netdb.h>
 #include <resolv.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #define NCBI_USE_ERRCODE_X   Connect_LBSM  /* errors: 31 and on */
 
 
+#ifdef __cplusplus
+extern "C" {
+#endif /*__cplusplus*/
+static SSERV_Info* s_GetNextInfo(SERV_ITER, HOST_INFO*);
+static void        s_Reset      (SERV_ITER);
+static void        s_Close      (SERV_ITER);
+
+static const SSERV_VTable s_op = {
+    s_GetNextInfo, 0/*Feedback*/, 0/*Update*/, s_Reset, s_Close, "LBDNS"
+};
+#ifdef __cplusplus
+} /* extern "C" */
+#endif /*__cplusplus*/
+
+
+struct SLBDNS_Data {
+    const char*    host;     /* LB DNS server host */
+    unsigned short port;     /* LB DNS server port */
+    const char*    domain;   /* Domain name to use */
+    size_t         a_info;   /* Allocated pointers */
+    size_t         n_info;   /* Used pointers      */
+    SSERV_Info*    info[1];  /* "a_info" pointers  */
+};
+
+
+
+static int/*bool*/ s_Resolve(SERV_ITER iter)
+{
+    return 0/*false*/;
+}
+
+
+static SSERV_Info* s_GetNextInfo(SERV_ITER iter, HOST_INFO* host_info)
+{
+    struct SLBDNS_Data* data = (struct SLBDNS_Data*) iter->data;
+    SSERV_Info* info;
+
+    if (!data->n_info)
+        return 0;
+    info = data->info[0];
+    assert(info);
+    if (--data->n_info) {
+        memmove(data->info, data->info + 1,
+                data->n_info * sizeof(data->info[0]));
+    }
+    if (host_info)
+        *host_info = 0;
+    return info;
+}
+
+
+static void s_Reset(SERV_ITER iter)
+{
+    struct SLBDNS_Data* data = (struct SLBDNS_Data*) iter->data;
+    if (data  &&  data->n_info) {
+        size_t n;
+        for (n = 0;  n < data->n_info;  ++n) {
+            assert(data->info[n]);
+            free(data->info[n]);
+        }
+        data->n_info = 0;
+    }
+}
+
+
+static void s_Close(SERV_ITER iter)
+{
+    struct SLBDNS_Data* data = (struct SLBDNS_Data*) iter->data;
+    iter->data = 0;
+    assert(!data->n_info); /*s_Reset() had to be called before*/
+    if (data->host)
+        free((void*) data->host);
+    if (data->domain)
+        free((void*) data->domain);
+    free(data);
+}
+
+
 /***********************************************************************
  *  EXTERNAL
  ***********************************************************************/
-#endif
+
 const SSERV_VTable* SERV_LBDNS_Open(SERV_ITER iter, SSERV_Info** info)
 {
+    const char val[NS_MAXDNAME];
+    struct SLBDNS_Data* data;
+
+    /* Can process fSERV_Any (meaning fSERV_Standalone), and explicit
+     * fSERV_Standalone and/or fSERV_Dns only */
+    if (iter->types != fSERV_Any
+        &&  !(iter->types & (fSERV_Standalone | fSERV_Dns))) {
+        return 0;
+    }
+    if (!(data = (struct SLBDNS_Data*) calloc(1, sizeof(*data)
+                                              + sizeof(data->info) * 31))) {
+        return 0;
+    }
+    data->a_info = 32;
+    iter->data = data;
+
+    if (!s_Resolve(iter)) {
+        s_Reset(iter);
+        s_Close(iter);
+        return 0;
+    }
+
+    /* call GetNextInfo subsequently if info is actually needed */
+    if (info)
+        *info = 0;
+    return &s_op;
+}
+
+
+#else
+
+#include "ncbi_once.h"
+
+
+/*ARGSUSED*/
+const SSERV_VTable* SERV_LBDNS_Open(SERV_ITER iter, SSERV_Info** info)
+{
+    /* NB: This should never be called on a non-UNIX platform */
+    static void* /*bool*/ s_Once = 0/*false*/;
+    if (CORE_Once(&s_Once))
+        CORE_LOG(eLOG_Critical, "LB DNS only available on UNIX platform(s)");
     return 0;
 }
+
+
+#endif /*NCBI_OS_UNIX*/
