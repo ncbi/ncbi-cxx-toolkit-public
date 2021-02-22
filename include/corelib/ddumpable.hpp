e888fd2d38bbac342cfa37e091f144ddda0a3404
Aleksey Grichenko <grichenk@ncbi.nlm.nih.gov>
2021-02-22 15:32:01 +0000

    Replaced CNcbiOstrstream with ostringstream. JIRA CXX-11750
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@92892 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/corelib/ddumpable.hpp
+++ include/corelib/ddumpable.hpp
@@ -206,9 +206,9 @@ template<class T>
 void DebugDumpValue( CDebugDumpContext& _this, const string& name,
     const T& value, const string& comment = kEmptyStr)
 {
-    CNcbiOstrstream os;
+    ostringstream os;
     os << value << '\0';
-    _this.Log(name, string(os.str()), CDebugDumpFormatter::eValue, comment);
+    _this.Log(name, os.str(), CDebugDumpFormatter::eValue, comment);
 }
 
 
@@ -340,7 +340,7 @@ void DebugDumpPairsValuePtr( CDebugDumpContext& _this, const string& name,
     --depth;
     CDebugDumpContext ddc2(_this,name);
     for ( int n=0; it != it_end; ++it, ++n) {
-        CNcbiOstrstream os;
+        ostringstream os;
         os << (it->first) << '\0';
         string member_name = name + "[ " + os.str() + " ]";
         ddc2.Log(member_name,it->second, depth);
