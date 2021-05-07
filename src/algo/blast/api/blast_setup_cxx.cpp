447a9e67197c8c6fd9ec0ef1ea972d7770316cc0
Amelia Fong <fongah2@ncbi.nlm.nih.gov>
2021-05-07 12:10:35 +0000

    Fix phi blast bl2seq, JIRA:SB-3070
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@93639 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/algo/blast/api/blast_setup_cxx.cpp
+++ src/algo/blast/api/blast_setup_cxx.cpp
@@ -1134,6 +1134,7 @@ GetSubjectEncoding(EBlastProgramType program)
     case eBlastTypeBlastp: 
     case eBlastTypeBlastx:
     case eBlastTypePsiBlast:
+    case eBlastTypePhiBlastp:
         retval = eBlastEncodingProtein; 
         break;
 
