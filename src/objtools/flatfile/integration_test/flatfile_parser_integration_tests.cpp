138747c6ae6ec2a6f0b12a216961c05c2177ab5f
Justin Foley <foleyjp@ncbi.nlm.nih.gov>
2021-08-02 16:37:32 +0000

    JIRA: RW-1385 Added integration test
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@94408 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ src/objtools/flatfile/integration_test/flatfile_parser_integration_tests.cpp
@@ -0,0 +1,71 @@
+/*  $Id$
+* ===========================================================================
+*
+*                            PUBLIC DOMAIN NOTICE
+*               National Center for Biotechnology Information
+*
+*  This software/database is a "United States Government Work" under the
+*  terms of the United States Copyright Act.  It was written as part of
+*  the author's official duties as a United States Government employee and
+*  thus cannot be copyrighted.  This software/database is freely available
+*  to the public for use. The National Library of Medicine and the U.S.
+*  Government have not placed any restriction on its use or reproduction.
+*
+*  Although all reasonable efforts have been taken to ensure the accuracy
+*  and reliability of the software and data, the NLM and the U.S.
+*  Government do not and cannot warrant the performance or results that
+*  may be obtained by using this software or data. The NLM and the U.S.
+*  Government disclaim all warranties, express or implied, including
+*  warranties of performance, merchantability or fitness for any particular
+*  purpose.
+*
+*  Please cite the author in any work or product based on this material.
+*
+* ===========================================================================
+*
+* Author:  Justin Foley
+*
+* File Description:
+*   Unit tests for code processing secondary accessions
+*
+*/
+
+#include <ncbi_pch.hpp>
+
+// This header must be included before all Boost.Test headers if there are any
+#include <corelib/test_boost.hpp>
+#include <corelib/ncbifile.hpp>
+#include <objtools/flatfile/flatfile_parser.hpp>
+#include <objtools/flatfile/fta_parse_buf.h>
+#include <common/test_assert.h>  /* This header must go last */
+
+
+USING_NCBI_SCOPE;
+USING_SCOPE(objects);
+
+
+BOOST_AUTO_TEST_CASE(TestIstreamInterface)
+{
+    unique_ptr<Parser> pConfig(new Parser()); 
+    pConfig->mode = Parser::EMode::Relaxed;
+    pConfig->output_format = Parser::EOutput::Seqsubmit;
+    string format {"genbank"};
+    string source {"ncbi"};
+    fta_set_format_source(*pConfig, format, source);
+
+    auto pIstr = make_unique<CNcbiIfstream>("TP53_MH011443.gb");
+    CFlatFileParser ffparser(nullptr);
+    auto pResult = ffparser.Parse(*pConfig, *pIstr);
+    BOOST_REQUIRE(pResult);
+
+    const string& outputName = CDirEntry::GetTmpName();
+    CNcbiOfstream ofstr(outputName);
+    ofstr << MSerial_AsnText << *pResult;
+    ofstr.close();
+
+    CFile goldenFile("TP53_MH011443.asn");
+    bool success = goldenFile.CompareTextContents(outputName, CFile::eIgnoreWs);
+
+    BOOST_REQUIRE(success);
+}
+
