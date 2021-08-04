8318b0dbb354dcd2bf4f96cb92105cdf4595b443
syncbot <syncbot@78c7ea69-d796-4a43-9a09-de51944f1b03>
2021-08-04 04:03:01 +0000

    Mark as 20210804 DEVELOPMENT.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@94422 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/common/ncbi_source_ver.h
+++ include/common/ncbi_source_ver.h
@@ -28,7 +28,7 @@
 #include <common/ncbi_build_info.h>
 
 /* #undef NCBI_PRODUCTION_VER */
-#define NCBI_DEVELOPMENT_VER 20210803
+#define NCBI_DEVELOPMENT_VER 20210804
 
 #if defined(NCBI_PRODUCTION_VER)
 #  define NCBI_SRCTREE_VER_PROXY  NCBI_PRODUCTION_VER
