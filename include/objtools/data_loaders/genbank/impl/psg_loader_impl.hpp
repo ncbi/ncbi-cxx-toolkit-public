7a157098185d0672ba51882966d5389c82ba23e0
Eugene Vasilchenko <vasilche@ncbi.nlm.nih.gov>
2021-08-09 15:01:58 +0000

    Removed obsolete method declaration.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@94481 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/objtools/data_loaders/genbank/impl/psg_loader_impl.hpp
+++ include/objtools/data_loaders/genbank/impl/psg_loader_impl.hpp
@@ -192,7 +192,6 @@ private:
     SReplyResult x_ProcessBlobReply(shared_ptr<CPSG_Reply> reply, CDataSource* data_source, CSeq_id_Handle req_idh, bool retry, bool lock_asap = false, CTSE_LoadLock* load_lock = nullptr);
     SReplyResult x_RetryBlobRequest(const string& blob_id, CDataSource* data_source, CSeq_id_Handle req_idh);
     shared_ptr<SPsgBioseqInfo> x_GetBioseqInfo(const CSeq_id_Handle& idh);
-    CTSE_Lock x_LoadBlob(const SPsgBlobInfo& psg_blob_info, CDataSource& data_source);
 
     enum EMainChunkType {
         eNoDelayedMainChunk,
