79e3e28d3a4f8350e9088df6c8daa9507bc61751
fukanchi <fukanchi@ncbi.nlm.nih.gov>
2021-08-09 16:19:48 +0000

    Use precommit to generate_all_objects. JIRA: CXX-12007
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@94486 78c7ea69-d796-4a43-9a09-de51944f1b03

--- scripts/projects/public/tagprecommit/run.sh
+++ scripts/projects/public/tagprecommit/run.sh
@@ -3,5 +3,5 @@
 script_dir=`dirname $0`
 script_dir=`(cd "${script_dir}" ; pwd)`
 
-$script_dir/gen_all.py
-$script_dir/use_embedded_ptb.py
+$script_dir/tagprecommit/gen_all.py
+$script_dir/tagprecommit/use_embedded_ptb.py
