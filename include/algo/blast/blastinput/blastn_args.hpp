f95ec2bd28651835295adff2b95447de3db6e0ac
fongah2 <fongah2@78c7ea69-d796-4a43-9a09-de51944f1b03>
2021-08-27 11:16:36 +0000

    Fix mtq output, JIRA:SB-3158
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@94671 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/algo/blast/blastinput/blastn_args.hpp
+++ include/algo/blast/blastinput/blastn_args.hpp
@@ -73,7 +73,7 @@ public:
     /// Get the output stream
     virtual CNcbiOstream& GetOutputStream();
 
-    CNcbiOstrstream & GetOutputStrStream() { return m_OutputStream; }
+    CNcbiStrstream & GetOutputStrStream() { return m_OutputStream; }
 
     virtual ~CBlastnNodeArgs();
 
@@ -83,7 +83,7 @@ protected:
     x_CreateOptionsHandle(CBlastOptions::EAPILocality locality, const CArgs& args);
 
 private :
-    CNcbiOstrstream m_OutputStream;
+    CNcbiStrstream m_OutputStream;
     CNcbiIstrstream * m_InputStream;
 };
 
