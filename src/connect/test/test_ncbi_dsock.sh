80493d5a8a499f52f70d861d4af15592da152bf0
lavr <lavr@78c7ea69-d796-4a43-9a09-de51944f1b03>
2021-09-07 02:16:15 +0000

    Use "uname -m" instead of "arch" (unavailable on FreeBSD)
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@94739 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/connect/test/test_ncbi_dsock.sh
+++ src/connect/test/test_ncbi_dsock.sh
@@ -24,8 +24,10 @@ client_log=test_ncbi_dsock_client.log
 
 rm -f $port $server_log $client_log
 
-if [ -x /sbin/ifconfig -a "`arch`" = "x86_64" ]; then
-  mtu="`/sbin/ifconfig lo 2>&1 | grep -iw mtu | sed 's/	/ /g;s/  */ /g;s/^.*[Mm][Tt][Uu][: ]*\([0-9][0-9]*\).*$/\1/'`"
+arch="`uname -m`"
+test "$arch" = "amd64"  &&  arch="x86_64"
+if [ -x /sbin/ifconfig -a "$arch" = "x86_64" ]; then
+  mtu="`/sbin/ifconfig lo0 2>&1 | grep -iw mtu | sed 's/	/ /g;s/  */ /g;s/^.*[Mm][Tt][Uu][: ]*\([1-9][0-9]*\).*$/\1/'`"
 fi
 
 : ${CONN_DEBUG_PRINTOUT:=SOME};  export CONN_DEBUG_PRINTOUT
