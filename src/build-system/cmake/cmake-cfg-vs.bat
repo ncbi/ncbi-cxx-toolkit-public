190d4445b82954fe3185c1ef57130342e0b043ab
gouriano <gouriano@78c7ea69-d796-4a43-9a09-de51944f1b03>
2021-09-21 18:39:50 +0000

    Correction. JIRA: CXX-12081
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@94912 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/build-system/cmake/cmake-cfg-vs.bat
+++ src/build-system/cmake/cmake-cfg-vs.bat
@@ -148,7 +148,6 @@ set PROJECT_LIST=
 set PROJECT_TAGS=
 set PROJECT_TARGETS=
 set PROJECT_DETAILS=
-set VISUAL_STUDIO=
 set INSTALL_PATH=
 set prebuilt_dir=
 set prebuilt_name=
@@ -255,6 +254,8 @@ if "%CMAKE_GENERATOR%"=="Visual Studio 15 2017 Win64" (
 if "%CMAKE_GENERATOR%"=="Visual Studio 14 2015 Win64" (
   set generator_name=VS2015
 )
+echo generator_name = %generator_name%
+exit /b 0
 
 set ttt=%tree_root%\%PROJECT_LIST%
 set ttt=%ttt:/=\%
