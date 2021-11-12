8182abc495700671c203e37b518c4b99c9bf7ca5
stakhovv <stakhovv@78c7ea69-d796-4a43-9a09-de51944f1b03>
2021-11-12 16:12:16 +0000

    CAuthor::ConvertMlToStandard: Do not deduce first name from initials. JIRA:RW-1489
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@95400 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objects/biblio/Author.cpp
+++ src/objects/biblio/Author.cpp
@@ -245,7 +245,6 @@ CRef<CPerson_id> CAuthor::x_ConvertMlToStandard(const string& name, const bool n
         person_id->SetName().SetLast(last);
         if (!initials.empty()) {
             person_id->SetName().SetInitials(initials);
-            person_id->SetName().SetFirst(initials.substr(0, 1));
         }
         if (!suffix.empty()) {
             person_id->SetName().SetSuffix(suffix);
