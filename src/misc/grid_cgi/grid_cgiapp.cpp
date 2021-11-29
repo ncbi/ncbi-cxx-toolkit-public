1f579d82b6d85c18c5c6faca0df6689172e9c9bb
sadyrovr <sadyrovr@78c7ea69-d796-4a43-9a09-de51944f1b03>
2021-11-29 20:11:24 +0000

    GRID CGIs: Exception message is not shown by default. JIRA CXX-12250
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@95593 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/misc/grid_cgi/grid_cgiapp.cpp
+++ src/misc/grid_cgi/grid_cgiapp.cpp
@@ -43,6 +43,22 @@
 
 BEGIN_NCBI_SCOPE
 
+struct SExceptionMessage
+{
+    SExceptionMessage(CNcbiRegistry* registry = nullptr) : m_Registry(registry) {}
+
+    string operator()(const string& what)
+    {
+        _ASSERT(m_Registry);
+        auto message = m_Registry->GetString("CGI", "Exception_Message", "Some exception was thrown (not shown for safety reasons)");
+        return message.empty() ? what : message;
+    }
+
+private:
+    CNcbiRegistry* m_Registry;
+};
+
+
 CGridCgiContext::CGridCgiContext(CHTMLPage& page, CCgiContext& ctx)
     : m_Page(page), m_CgiContext(ctx), m_NeedRenderPage(true)
 {
@@ -217,6 +233,8 @@ const string kGridCgiForm = "<FORM METHOD=\"GET\" ACTION=\"<@SELF_URL@>\">\n"
 
 int CGridCgiApplication::ProcessRequest(CCgiContext& ctx)
 {
+    SExceptionMessage exception_message = &GetRWConfig();
+
     // Given "CGI context", get access to its "HTTP request" and
     // "HTTP response" sub-objects
     //const CCgiRequest& request  = ctx.GetRequest();
@@ -284,11 +302,11 @@ int CGridCgiApplication::ProcessRequest(CCgiContext& ctx)
                         CNetScheduleException::eTooManyPendingJobs)
                         OnQueueIsBusy(grid_ctx);
                     else
-                        OnJobFailed(ex.what(), grid_ctx);
+                        OnJobFailed(exception_message(ex.what()), grid_ctx);
                     finished = true;
                 }
                 catch (exception& ex) {
-                    OnJobFailed(ex.what(), grid_ctx);
+                    OnJobFailed(exception_message(ex.what()), grid_ctx);
                     finished = true;
                 }
                 if (finished)
@@ -301,7 +319,7 @@ int CGridCgiApplication::ProcessRequest(CCgiContext& ctx)
         }
         } // try
         catch (/*CNetServiceException*/ exception& ex) {
-            OnJobFailed(ex.what(), grid_ctx);
+            OnJobFailed(exception_message(ex.what()), grid_ctx);
         }       
         CHTMLPlainText* self_url =
             new CHTMLPlainText(grid_ctx.GetSelfURL(),true);
