75562efe0ff97a50fdf385b01a4d49add396bd7a
stakhovv <stakhovv@78c7ea69-d796-4a43-9a09-de51944f1b03>
2022-02-16 16:42:47 +0000

    JIRA:RW-1555: Add eutils_updater
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@96148 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ include/objtools/edit/eutils_updater.hpp
@@ -0,0 +1,26 @@
+#ifndef _EUTILS_UPDATER_HPP_
+#define _EUTILS_UPDATER_HPP_
+
+#include <objtools/eutils/api/eutils.hpp>
+#include <objtools/edit/pubmed_updater.hpp>
+
+BEGIN_NCBI_SCOPE
+BEGIN_SCOPE(objects)
+BEGIN_SCOPE(edit)
+
+class CEUtilsUpdater : IPubmedUpdater
+{
+public:
+    CEUtilsUpdater();
+    TEntrezId GetPmId(const CPub&) override;
+    CRef<CPub> GetPub(TEntrezId pmid, EPubmedError* = nullptr) override;
+    CRef<CTitle_msg_list> GetTitle(const CTitle_msg&) override;
+private:
+    CRef<CEUtils_ConnContext> m_Ctx;
+};
+
+END_SCOPE(edit)
+END_SCOPE(objects)
+END_NCBI_SCOPE
+
+#endif  // _EUTILS_UPDATER_HPP_
