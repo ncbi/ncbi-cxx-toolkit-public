7f6ba30661b9abe78fba95c47659357ee7cbbd00
Vitaly Stakhovsky <stakhovv@ncbi.nlm.nih.gov>
2022-02-16 16:42:47 +0000

    JIRA:RW-1555: Add eutils_updater
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@96148 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ src/objtools/edit/eutils_updater.cpp
@@ -0,0 +1,99 @@
+/*  $Id$
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Authors:  Vitaly Stakhovsky, NCBI
+ *
+ * File Description:
+ *   Implementation of IPubmedUpdater based on EUtils
+ *
+ * ===========================================================================
+ */
+
+#include <ncbi_pch.hpp>
+#include <objtools/edit/eutils_updater.hpp>
+
+#include <objtools/eutils/api/efetch.hpp>
+#include <objects/pubmed/Pubmed_entry.hpp>
+#include <objects/medline/Medline_entry.hpp>
+#include <objects/pub/Pub.hpp>
+#include <objects/mla/Title_msg.hpp>
+#include <objects/mla/Title_msg_list.hpp>
+
+
+BEGIN_NCBI_SCOPE
+BEGIN_SCOPE(objects)
+BEGIN_SCOPE(edit)
+
+CEUtilsUpdater::CEUtilsUpdater()
+{
+    m_Ctx.Reset(new CEUtils_ConnContext);
+}
+
+
+TEntrezId CEUtilsUpdater::GetPmId(const CPub& pub)
+{
+    // Not yet implemented
+    return ZERO_ENTREZ_ID;
+}
+
+
+CRef<CPub> CEUtilsUpdater::GetPub(TEntrezId pmid, EPubmedError* perr)
+{
+    unique_ptr<CEFetch_Request> req(
+        new CEFetch_Literature_Request(CEFetch_Literature_Request::eDB_pubmed, m_Ctx)
+    );
+
+    req->SetRequestMethod(CEUtils_Request::eHttp_Get);
+    req->GetId().AddId(to_string(pmid));
+    req->SetRetMode(CEFetch_Request::eRetMode_asn);
+
+    CPubmed_entry pme;
+    string content;
+    req->Read(&content);
+    content >> pme;
+
+    if (pme.IsSetMedent()) {
+        CMedline_entry& mle = pme.SetMedent();
+        if (mle.IsSetCit()) {
+            CRef<CPub> cit_art;
+            cit_art.Reset(new CPub);
+            cit_art->SetArticle(mle.SetCit());
+            return cit_art;
+        }
+    }
+
+    return {};
+}
+
+
+CRef<CTitle_msg_list> CEUtilsUpdater::GetTitle(const CTitle_msg& msg)
+{
+    // Not yet implemented
+    return {};
+}
+
+END_SCOPE(edit)
+END_SCOPE(objects)
+END_NCBI_SCOPE
