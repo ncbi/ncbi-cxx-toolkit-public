0f4b6470184b0ad06383fdcce6fa0de180ee549f
vasilche <vasilche@78c7ea69-d796-4a43-9a09-de51944f1b03>
2022-03-10 23:58:19 +0000

    Fixed limit on row number length for 4-letter accessions. JIRA: CXX-12398
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@96302 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/app/pubseq_gateway/server/osg_resolve_base.cpp
+++ src/app/pubseq_gateway/server/osg_resolve_base.cpp
@@ -278,7 +278,7 @@ static const size_t kVersionDigits = 2;
 static const size_t kPrefixLenV1 = kNumLettersV1 + kVersionDigits;
 static const size_t kPrefixLenV2 = kNumLettersV2 + kVersionDigits;
 static const size_t kMinRowDigitsV1 = 6;
-static const size_t kMaxRowDigitsV1 = 7;
+static const size_t kMaxRowDigitsV1 = 8;
 static const size_t kMinRowDigitsV2 = 7;
 static const size_t kMaxRowDigitsV2 = 9;
 
