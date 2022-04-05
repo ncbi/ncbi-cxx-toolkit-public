88957c582bc98df18bc927108f88603ec5305060
Andrei Gourianov <gouriano@ncbi.nlm.nih.gov>
2022-04-05 14:17:25 +0000

    Define compiler-related variables in "packaged" mode. JIRA: CXX-12399
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@96471 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/build-system/cmake/CMake.NCBIpkg.conan.cmake
+++ src/build-system/cmake/CMake.NCBIpkg.conan.cmake
@@ -32,6 +32,7 @@ if(NCBI_PTBCFG_ENABLE_TOOLS)
     include(${_listdir}/CMake.NCBIptb.definitions.cmake)
     include(${_listdir}/CMakeMacros.cmake)
     include(${_listdir}/CMake.NCBIptb.cmake)
+    include(${_listdir}/CMakeChecks.compiler.cmake)
     include(${_listdir}/CMake.NCBIComponents.cmake)
     include_directories(${CONAN_NCBI-CXX-TOOLKIT-PUBLIC_ROOT}/${NCBI_DIRNAME_INCLUDE} ${NCBI_INC_ROOT})
 
