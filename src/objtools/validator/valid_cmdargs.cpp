fb126ddb70b75e58f0401239bb5da77deb331365
foleyjp <foleyjp@78c7ea69-d796-4a43-9a09-de51944f1b03>
2022-04-27 18:57:46 +0000

    JIRA: RW-1638 Removed the check for the deprecated '-a u' option in CValidatorArgUtil::ArgsToValidatorOptions()
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@96685 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/validator/valid_cmdargs.cpp
+++ src/objtools/validator/valid_cmdargs.cpp
@@ -121,9 +121,6 @@ int CValidatorArgUtil::ArgsToValidatorOptions(const CArgs& args)
     if (args["w"]) {
         options |= CValidator::eVal_seqsubmit_parent;
     }
-    if ( NStr::Equal(args["a"].AsString(), "u")) {
-        options |= CValidator::eVal_seqsubmit_parent;
-    }
     if (args["q"]) {
         options |= CValidator::eVal_do_tax_lookup;
     }
