2c838a703782ef17448306a62eb99c4ef7e8964a
Aaron Ucko <ucko@ncbi.nlm.nih.gov>
2022-06-16 18:33:24 +0000

    CMake.NCBIComponentsUNIXex.cmake: Switch back to old patched Mesa on Linux.
    With the system installation, correct runtime behavior requires libOSMesa
    to appear before the first occurrence of libGL when using both, but I have
    yet to find a way to enforce that ordering.  JIRA: CXX-12483.
    
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@97094 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/build-system/cmake/CMake.NCBIComponentsUNIXex.cmake
+++ src/build-system/cmake/CMake.NCBIComponentsUNIXex.cmake
@@ -60,6 +60,7 @@ set(NCBI_ThirdParty_XLSXWRITER    ${NCBI_TOOLS_ROOT}/libxlsxwriter-0.6.9 CACHE P
 set(NCBI_ThirdParty_SAMTOOLS      ${NCBI_TOOLS_ROOT}/samtools CACHE PATH "SAMTOOLS root")
 set(NCBI_ThirdParty_FTGL          ${NCBI_TOOLS_ROOT}/ftgl-2.1.3-rc5 CACHE PATH "FTGL root")
 if (${CMAKE_SYSTEM_NAME} MATCHES "Linux")
+  set(NCBI_ThirdParty_OpenGL        ${NCBI_TOOLS_ROOT}/Mesa-7.0.2-ncbi2 CACHE PATH "OpenGL root")
   set(NCBI_ThirdParty_GLEW          ${NCBI_TOOLS_ROOT}/glew-2.2.0-ncbi1 CACHE PATH "GLEW root")
 else()
   set(NCBI_ThirdParty_GLEW          ${NCBI_TOOLS_ROOT}/glew-1.5.8 CACHE PATH "GLEW root")
