f089f9a2d36f6a2dc11d31220edcf5543deb4951
gouriano <gouriano@78c7ea69-d796-4a43-9a09-de51944f1b03>
2022-06-24 14:20:33 +0000

    Rearranged Conan setup. JIRA: CXX-11140
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@97174 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/build-system/cmake/CMake.NCBIComponentsPackage.cmake
+++ src/build-system/cmake/CMake.NCBIComponentsPackage.cmake
@@ -16,12 +16,6 @@
 
 
 #set(NCBI_TRACE_ALLCOMPONENTS ON)
-set(__silent ${CONAN_CMAKE_SILENT_OUTPUT})
-set(CONAN_CMAKE_SILENT_OUTPUT TRUE)
-if(NOT NCBI_PTBCFG_PACKAGED OR NOT TARGET CONAN_PKG::ncbi-cxx-toolkit-public)
-    conan_define_targets()
-endif()
-set(CONAN_CMAKE_SILENT_OUTPUT ${__silent})
 
 include(CheckIncludeFile)
 include(CheckSymbolExists)
