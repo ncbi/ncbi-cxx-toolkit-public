f763485636f0a8fa5902402c213a5439f40e5ed1
Jian Ye <jianye@ncbi.nlm.nih.gov>
2022-06-27 15:14:36 +0000

    add ability to group V, D, and J sequences separately to make blast db, handle multiple files, JIRA IGB-396
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@97206 78c7ea69-d796-4a43-9a09-de51944f1b03

--- scripts/projects/igblast/makeogrdb.py
+++ scripts/projects/igblast/makeogrdb.py
@@ -24,9 +24,9 @@
  *
  * ===========================================================================
 
-Script to grab "coding_sequence" in OGRDB germline set JSON file to
-make a BLAST sequence database
-Usage: ./makeogrdb.py germline_set_file_name blast_db_output_file_name
+Script to grab "coding_sequence" in OGRDB germline set JSON file and generate
+ BLAST databases for V, D, J sequences, respectively.
+Requires MCBI makeblastdb in the running directory 
 """
 
 
@@ -36,33 +36,56 @@ import glob
 import re
 import sys
 import json
-from functools import cmp_to_key
 import subprocess
+import argparse
+from pathlib import Path
 
 
-if (len(sys.argv) <3): 
-    print("Please supply the germline set JSON file name and the output file name for blast database\n")
-    print("Usage: ./makeogrdb.py germline_set_file_name blast_db_output_file_name\n")
-
-infile_name = str(sys.argv[1])
-outfile_name = str(sys.argv[2])
-
-
-infile = open(infile_name)
-data = json.load(infile)
-
-outfile = open(outfile_name, "w")
-
-for i in data['GermlineSet']["allele_descriptions"]:
-    seq = re.sub('\.+', '', i["coding_sequence"])
-    outfile.write(">" + i["label"] + "\n")
-    outfile.write(seq + "\n")
-infile.close()  
-outfile.close()
+parser = argparse.ArgumentParser(description = "script to build blast dbs from OGR germline sets JSON files")
+parser.add_argument('--germline_file', required=True,
+                    help="JSON file name(s). Need to put multiple files inside quotation marks, i.e., \"file1 file2 file3\"")
+parser.add_argument('--output_file', required=True,
+                    help="output file name for blast database")
+args = parser.parse_args()
 
 
-os.system("./makeblastdb  -in " + outfile_name + " -dbtype prot -parse_seqids " + " -title " + "\"" + data['GermlineSet']["germline_set_name"] + " " + data["GermlineSet"]["germline_set_ref"] + "\"")
-  
+input_file_names = re.split("\s+", args.germline_file)
+outfile_name_prefix = args.output_file
+
+sequence_type = ["V", "D", "J"]
+
+outfile_names = {}
+outfile_handles = {}
+db_titles = {}
+for i in sequence_type:
+    outfile_names[i] = f"{outfile_name_prefix}.{i}"
+    outfile_handles[i] = open(outfile_names[i], "w")
+    db_titles[i] = {}
+    
+for each_in_file in input_file_names:
+    infile = open(each_in_file)
+    data = json.load(infile)
+    title = data['GermlineSet']["germline_set_name"] + " " + data["GermlineSet"]["germline_set_ref"] 
+    for i in data['GermlineSet']["allele_descriptions"]:
+        seq = re.sub('\.+', '', i["coding_sequence"])
+        outfile_handles[i["sequence_type"]].write(">" + i["label"] + "\n")
+        outfile_handles[i["sequence_type"]].write(seq + "\n")
+        if not title in db_titles[i["sequence_type"]].keys():
+            db_titles[i["sequence_type"]][title] = 1
+    infile.close()
+
+for key, value in outfile_handles.items():
+    outfile_handles[key].close()
+
+for i in sequence_type:
+    final_title = ""
+    for key, value in db_titles[i].items():
+        final_title = final_title + " " + key
+    if (os.path.getsize(outfile_names[i]) > 0):
+        os.system(f"./makeblastdb  -in {outfile_names[i]} -dbtype nucl -parse_seqids -title \"{final_title}\"")
+    else:
+        Path(outfile_names[i]).unlink(missing_ok=True)
+    
 exit(0)
 
 
