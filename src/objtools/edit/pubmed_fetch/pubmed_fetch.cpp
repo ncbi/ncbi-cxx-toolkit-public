3ed36c539d04d5b3e1d45546b04d9a30658bcabe
stakhovv <stakhovv@78c7ea69-d796-4a43-9a09-de51944f1b03>
2022-06-28 18:14:13 +0000

    JIRA:RW-1708; Add -ids -idfile
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@97236 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/edit/pubmed_fetch/pubmed_fetch.cpp
+++ src/objtools/edit/pubmed_fetch/pubmed_fetch.cpp
@@ -1,3 +1,35 @@
+/* $Id$
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Author: Vitaly Stakhovsky, NCBI
+ *
+ * File Description:
+ *  Test PubMed test application: MedArch vs EUtils
+ *
+ */
+
 #include <ncbi_pch.hpp>
 #include <corelib/ncbiapp.hpp>
 
@@ -17,7 +49,9 @@ class CPubmedFetchApplication : public CNcbiApplication
     {
         unique_ptr<CArgDescriptions> arg_desc(new CArgDescriptions);
         arg_desc->SetUsageContext("", "Retrieve publications data from medarch/eutils and display in ASN");
-        arg_desc->AddKey("id", "pmid", "PubMed ID to fetch", CArgDescriptions::eIntId);
+        arg_desc->AddOptionalKey("id", "pmid", "PubMed ID to fetch", CArgDescriptions::eIntId);
+        arg_desc->AddOptionalKey("ids", "pmids", "PubMed IDs", CArgDescriptions::eString);
+        arg_desc->AddOptionalKey("idfile", "pmids", "File containing PubMed IDs", CArgDescriptions::eInputFile);
         arg_desc->AddDefaultKey("source", "source", "Source of data", CArgDescriptions::eString, "eutils");
         arg_desc->SetConstraint("source", &(*new CArgAllow_Strings, "medarch", "eutils"));
         arg_desc->AddOptionalKey("url", "url", "eutils base URL (http://eutils.ncbi.nlm.nih.gov/entrez/eutils/ by default)", CArgDescriptions::eString);
@@ -29,13 +63,49 @@ class CPubmedFetchApplication : public CNcbiApplication
     {
         const CArgs& args = GetArgs();
 
-        TEntrezId pmid = ZERO_ENTREZ_ID;
+        vector<TEntrezId> pmids;
+
         if (args["id"]) {
             TIntId id = args["id"].AsIntId();
-            pmid = ENTREZ_ID_FROM(TIntId, id);
-        }
-        if (pmid <= ZERO_ENTREZ_ID) {
+            if (id > 0) {
+                pmids.push_back(ENTREZ_ID_FROM(TIntId, id));
+            } else {
+                cerr << "Warning: Invalid ID: " << id << endl;
+            }
+        } else if (args["ids"]) {
+            vector<string> ids;
+            NStr::Split2(args["ids"].AsString(), ",", ids);
+            for (string id_str : ids) {
+                TIntId id = 0;
+                if (NStr::StringToNumeric(id_str, &id, NStr::fConvErr_NoThrow) && id > 0) {
+                    pmids.push_back(ENTREZ_ID_FROM(TIntId, id));
+                } else {
+                    cerr << "Warning: Invalid ID: " << id_str << endl;
+                }
+            }
+        } else if (args["idfile"]) {
+            CNcbiIstream& is = args["idfile"].AsInputFile();
+            string id_str;
+            while (NcbiGetlineEOL(is, id_str)) {
+                NStr::TruncateSpacesInPlace(id_str);
+                if (id_str.empty() || id_str.front() == '#') {
+                    continue;
+                }
+                TIntId id = 0;
+                if (NStr::StringToNumeric(id_str, &id, NStr::fConvErr_NoThrow) && id > 0) {
+                    pmids.push_back(ENTREZ_ID_FROM(TIntId, id));
+                } else {
+                    cerr << "Warning: Invalid ID: " << id_str << endl;
+                }
+            }
+        } else {
+            cerr << "Error: One of -id, -ids, or -idfile must be specified." << endl;
             return 1;
+        }
+
+        if (pmids.empty()) {
+            cerr << "Warning: No PMIDs specified" << endl;
+            return 0;
         }
 
         bool bTypeMLA = false;
@@ -67,18 +137,17 @@ class CPubmedFetchApplication : public CNcbiApplication
         }
 
         EPubmedError err;
-        CRef<CPub>   pub(upd->GetPub(pmid, &err));
-        if (pub) {
-            *output << MSerial_AsnText << *pub;
+        for (TEntrezId pmid : pmids) {
+            CRef<CPub> pub(upd->GetPub(pmid, &err));
+            if (pub) {
+                *output << MSerial_AsnText << *pub;
+            } else {
+                cerr << "Error: " << err << endl;
+            }
         }
 
         if (args["o"]) {
             args["o"].CloseFile();
-        }
-
-        if (! pub) {
-            cerr << "Error: " << err << endl;
-            return 1;
         }
 
         return 0;
