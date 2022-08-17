647f896f4bd893c8b055dcbfddd8bcc6cf42b463
Sergiy Gotvyanskyy <gotvyans@ncbi.nlm.nih.gov>
2022-08-17 20:28:22 +0000

    JIRA: RW-1755
    Made some methods const
    
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@97709 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/app/table2asn/multireader.hpp
+++ src/app/table2asn/multireader.hpp
@@ -40,42 +40,39 @@ public:
 
     using TAnnots = list<CRef<CSeq_annot>>;
 
-    CFormatGuess::EFormat OpenFile(const string& filename, CRef<CSerialObject>& input_sequence, TAnnots& annots);
-    CRef<CSerialObject> ReadNextEntry();
-    void WriteObject(const CSerialObject&, ostream&);
-    void LoadTemplate(CTable2AsnContext& context, const string& ifname);
-    void LoadDescriptors(const string& ifname, CRef<objects::CSeq_descr> & out_desc);
-    void MergeDescriptors(objects::CSeq_descr & dest, const objects::CSeq_descr & source);
-    void MergeDescriptors(objects::CSeq_descr & dest, const objects::CSeqdesc & source);
-    void ApplyDescriptors(objects::CSeq_entry & obj, const objects::CSeq_descr & source);
-    bool LoadAnnot(objects::CScope& scope, const string& filename);
-    bool LoadAnnots(const string& filename, TAnnots& annots);
-    void AddAnnots(TAnnots& annots, CScope& scope);
+    void LoadTemplate(const string& ifname);
     void LoadGFF3Fasta(istream& in, TAnnots& annots);
-
+    void AddAnnots(TAnnots& annots, CScope& scope) const;
+    bool LoadAnnots(const string& filename, TAnnots& annots);
+    void LoadDescriptors(const string& ifname, CRef<objects::CSeq_descr> & out_desc) const;
+    void ApplyDescriptors(objects::CSeq_entry & obj, const objects::CSeq_descr & source) const;
+    void WriteObject(const CSerialObject&, ostream&);
+    CRef<objects::CSeq_entry> ReadAlignment(CNcbiIstream& instream, const CArgs& args);
+    CRef<CSerialObject> ReadNextEntry();
+    CFormatGuess::EFormat OpenFile(const string& filename, CRef<CSerialObject>& input_sequence, TAnnots& annots);
     static
     void GetSeqEntry(CRef<objects::CSeq_entry>& entry, CRef<objects::CSeq_submit>& submit, CRef<CSerialObject> obj);
 
-    CRef<objects::CSeq_entry> ReadAlignment(CNcbiIstream& instream, const CArgs& args);
-    bool AtSeqenceData() const { return mAtSequenceData; };
-
 protected:
 private:
-    CFormatGuess::EFormat xReadFile(CNcbiIstream& in, CRef<objects::CSeq_entry>& entry, CRef<objects::CSeq_submit>& submit);
+    void MergeDescriptors(objects::CSeq_descr & dest, const objects::CSeq_descr & source) const;
+    void MergeDescriptors(objects::CSeq_descr & dest, const objects::CSeqdesc & source) const;
+
+    bool AtSeqenceData() const { return mAtSequenceData; };
+
     CRef<objects::CSeq_entry> xReadFasta(CNcbiIstream& instream);
-    CRef<CSerialObject> xApplyTemplate(CRef<CSerialObject> obj, bool merge_template_descriptors);
-    CRef<CSerialObject> xReadASN1Text(CObjectIStream& pObjIstrm);
-    CRef<CSerialObject> xReadASN1Binary(CObjectIStream& pObjIstrm, const CFileContentInfoGenbank& content_info);
-    TAnnots xReadGFF3(CNcbiIstream& instream, bool post_process=true);
+    CRef<CSerialObject> xApplyTemplate(CRef<CSerialObject> obj, bool merge_template_descriptors) const;
+    CRef<CSerialObject> xReadASN1Text(CObjectIStream& pObjIstrm) const;
+    CRef<CSerialObject> xReadASN1Binary(CObjectIStream& pObjIstrm, const CFileContentInfoGenbank& content_info) const;
+    TAnnots xReadGFF3(CNcbiIstream& instream, bool post_process);
     TAnnots xReadGTF(CNcbiIstream& instream);
     CRef<objects::CSeq_entry> xReadFlatfile(CFormatGuess::EFormat format, const string& filename);
-    void x_PostProcessAnnots(TAnnots& annots);
+    void x_PostProcessAnnots(TAnnots& annots) const;
     bool xGetAnnotLoader(CAnnotationLoader& loader, const string& filename);
-    bool xFixupAnnot(objects::CScope&, CRef<objects::CSeq_annot>&);
+    bool xFixupAnnot(objects::CScope&, CRef<objects::CSeq_annot>&) const;
 
-    unique_ptr<CObjectIStream> xCreateASNStream(const string& filename);
-    unique_ptr<CObjectIStream> xCreateASNStream(CFormatGuess::EFormat format, unique_ptr<istream>& instream);
-    //CFormatGuess::EFormat xGetFormat(CNcbiIstream&) const;
+    unique_ptr<CObjectIStream> xCreateASNStream(const string& filename) const;
+    unique_ptr<CObjectIStream> xCreateASNStream(CFormatGuess::EFormat format, unique_ptr<istream>& instream) const;
     CFormatGuess::EFormat xInputGetFormat(CNcbiIstream&, CFileContentInfo* = nullptr) const;
     CFormatGuess::EFormat xAnnotGetFormat(CNcbiIstream&) const;
 
