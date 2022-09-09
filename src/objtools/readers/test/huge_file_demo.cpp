b6a2057f775b3423db12dad459a351de62edf68d
Sergiy Gotvyanskyy <gotvyans@ncbi.nlm.nih.gov>
2022-09-09 11:26:27 +0000

    JIRA: GB-9343
    Added another example how to edit huge files via standard Object Manager methods
    
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@97935 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/readers/test/huge_file_demo.cpp
+++ src/objtools/readers/test/huge_file_demo.cpp
@@ -44,8 +44,7 @@
 #include <objtools/edit/huge_file_process.hpp>
 #include <objtools/edit/huge_asn_loader.hpp>
 #include <objmgr/scope.hpp>
-//include <objmgr/util/sequence.hpp>
-//include <objtools/readers/fasta.hpp>
+#include <objmgr/util/sequence.hpp>
 
 USING_NCBI_SCOPE;
 USING_SCOPE(objects);
@@ -53,7 +52,6 @@ USING_SCOPE(objects);
 struct TAppContext
 {
     CRef<CObjectManager> m_ObjMgr = CObjectManager::GetInstance();
-    edit::CHugeFileProcess m_huge;
     string m_loader_name;
 };
 
@@ -65,7 +63,8 @@ protected:
     void x_RunDemo(TAppContext& context, const std::list<CConstRef<CSeq_id>>& idlist) const;
     void x_ShowIds(TAppContext& context, const std::list<CConstRef<CSeq_id>>& idlist) const;
     void x_ShowSeqSizes(TAppContext& context, const std::list<CConstRef<CSeq_id>>& idlist) const;
-
+    void x_AddUserObjects(TAppContext& context, const std::list<CConstRef<CSeq_id>>& idlist, const string filename) const;
+    bool x_ModifyBioSeq(CBioseq_EditHandle beh) const;
 };
 
 void CHugeFileDemoApp::Init()
@@ -75,6 +74,7 @@ void CHugeFileDemoApp::Init()
                               "Demonstration of huge file readers",
                               false);
     arg_desc->AddKey("i", "InputFile", "Input ASN.1 file to read", CArgDescriptions::eInputFile);
+    arg_desc->AddOptionalKey("o", "OutputFile", "Output modified ASN.1", CArgDescriptions::eOutputFile);
 
     SetupArgDescriptions(arg_desc.release());
 }
@@ -84,13 +84,20 @@ int CHugeFileDemoApp::Run()
     string filename = GetArgs()["i"].AsString();
     std::cerr << "Opening " << filename << std::endl;
 
-    TAppContext context;
-    context.m_huge.Open(filename);
+    edit::CHugeFileProcess huge;
+    huge.Open(filename);
 
+    // context doesn't have huge' classes anymore, existing implementations should use traditional OM classes and approaches
+    // huge classes are used only for opening files and non-standard algorithms
+    TAppContext context;
+
+    // object manager need unique id's for loader name
+    // so each huge file opened needs it's unique id
     context.m_loader_name = CDirEntry::ConvertToOSPath(filename);
 
-    bool result = context.m_huge.Read([this, &context](edit::CHugeAsnReader* reader, const std::list<CConstRef<CSeq_id>>& idlist)->bool
+    bool result = huge.Read([this, &context](edit::CHugeAsnReader* reader, const std::list<CConstRef<CSeq_id>>& idlist)->bool
     {
+        // one can register as many huge loaders as needed but don't mess with loader's priorities up
         auto info = edit::CHugeAsnDataLoader::RegisterInObjectManager(
             *context.m_ObjMgr, context.m_loader_name, reader, CObjectManager::eDefault, 1); //CObjectManager::kPriority_Local);
 
@@ -111,8 +118,12 @@ void CHugeFileDemoApp::x_RunDemo(TAppContext& context, const std::list<CConstRef
 {
     std::cout << "Reading " << idlist.size() << " records" << std::endl;
 
-    x_ShowIds(context, idlist);
-    x_ShowSeqSizes(context, idlist);
+    if (GetArgs()["o"]) {
+        x_AddUserObjects(context, idlist, GetArgs()["o"].AsString());
+    } else {
+        x_ShowIds(context, idlist);
+        x_ShowSeqSizes(context, idlist);
+    }
 }
 
 void CHugeFileDemoApp::x_ShowIds(TAppContext& context, const std::list<CConstRef<CSeq_id>>& idlist) const
@@ -129,6 +140,7 @@ void CHugeFileDemoApp::x_ShowSeqSizes(TAppContext& context, const std::list<CCon
 {
     // this method demostrates shortcuts for fetching vital sequence data without reading the sequence into memory
     // this must be very fast and minimal memory consumption
+    // Note, not CHugeAsnReader is used
 
     auto scope = Ref(new CScope(*context.m_ObjMgr));
     scope->AddDataLoader(context.m_loader_name);
@@ -145,8 +157,88 @@ void CHugeFileDemoApp::x_ShowSeqSizes(TAppContext& context, const std::list<CCon
             << std::endl;
 
         id_it++;
+
+    // copied from CScope
+    /// Clean all unused TSEs from the scope's cache and release the memory.
+    /// TSEs referenced by any handles are not removed.
+    /// Explicitly added entries are not removed.
+    /// Edited entries retrieved from a data loader are removed,
+    /// so that their unmodified version will be visible.
+
+        scope->ResetHistory();
+    }
+}
+
+bool CHugeFileDemoApp::x_ModifyBioSeq(CBioseq_EditHandle beh) const
+{
+    auto& descr = beh.SetDescr();
+    return true;
+}
+
+void CHugeFileDemoApp::x_AddUserObjects(TAppContext& context, const std::list<CConstRef<CSeq_id>>& idlist, const string filename) const
+{
+    auto scope = Ref(new CScope(*context.m_ObjMgr));
+    scope->AddDataLoader(context.m_loader_name);
+
+    // these are to just keep handles, this helps Object Manager to understand they're still needed
+    std::list<CBioseq_Handle> modified_records;
+    size_t i=0;
+    for (auto id: idlist)
+    {
+        // this method should force object manager to load sequence from back end
+        CBioseq_Handle bh = scope->GetBioseqHandle(*id);
+        if (bh) {
+
+            if (50 <= i && i < 100) {
+                // modify second fifty records
+                CBioseq_EditHandle beh = bh.GetEditHandle();
+                if (x_ModifyBioSeq(beh))
+                    modified_records.push_back(beh);
+            }
+
+            // report first 100 records
+            if (i < 100) {
+            std::cout
+                << "  "
+                << bh.GetSeqId()->AsFastaString()
+                << ":" << bh.GetBioseqLength()
+                << ":" << bh.GetSequenceType()
+                << ":" << bh.GetBioseqMolType()
+                << std::endl;
+            }
+
+            // other records are just iterated over and loaded
+        }
+
+    // copied from CScope
+    /// Clean all unused TSEs from the scope's cache and release the memory.
+    /// TSEs referenced by any handles are not removed.
+    /// Explicitly added entries are not removed.
+    /// Edited entries retrieved from a data loader are removed,
+    /// so that their unmodified version will be visible.
+
+        bh.Reset();
+        // so only records that are references in 'modified_records' list
+        // should be kept in object manager's memory
+        //
+        scope->ResetHistory();
+        i++;
+        if (i>200) {
+            // just quickly stop looping re-reading unneeded sequences
+            break;
+        }
     }
 
+    i=0;
+    // iterate over the list again and report which are modified
+    for (auto id: idlist)
+    {
+        CBioseq_Handle bh = scope->GetBioseqHandle(*id, CScope::eGetBioseq_Loaded);
+        if (bh) {
+            std::cout << "Sequence #" << i << ":" << bh.GetSeqId()->AsFastaString() << " was edited\n";
+        }
+        ++i;
+    }
 }
 
 int main(int argc, const char* argv[])
@@ -154,3 +246,10 @@ int main(int argc, const char* argv[])
     // Execute main application function
     return CHugeFileDemoApp().AppMain(argc, argv); //, 0, eDS_Default, 0);
 }
+
+// TODO: open, modify some of random records, read some other records and save
+// TODO: need to expose Annotations
+// TODO: we expose the callback for renaming conflicting seqids
+// TODO: follow https://jira.ncbi.nlm.nih.gov/browse/CXX-12650
+//
+//
