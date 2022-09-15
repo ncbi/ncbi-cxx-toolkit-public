5913a74379bf6925fc71d34ed3287277cccc06d4
ucko <ucko@78c7ea69-d796-4a43-9a09-de51944f1b03>
2022-09-15 18:58:04 +0000

    Add ZSTD_STATIC_LIBS and corresponding wrappers (e.g. CMPRS_STATIC_LIBS).
    JIRA: CXX-12673.
    
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@97988 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/dbapi/simple/Makefile.sdbapi.mk
+++ src/dbapi/simple/Makefile.sdbapi.mk
@@ -5,3 +5,4 @@ SDBAPI_LIB  = sdbapi ncbi_xdbapi_ftds $(FTDS_LIB) \
               dbapi dbapi_util_blobstore dbapi_driver \
               $(XCONNEXT) $(COMPRESS_LIBS)
 SDBAPI_LIBS = $(FTDS100_LIBS) $(CMPRS_LIBS)
+SDBAPI_STATIC_LIBS = $(FTDS100_LIBS) $(CMPRS_STATIC_LIBS)
