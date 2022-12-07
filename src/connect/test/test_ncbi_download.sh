85702619b0f4f44df51ebba3782db07a9c394828
lavr <lavr@78c7ea69-d796-4a43-9a09-de51944f1b03>
2022-12-07 19:14:20 +0000

    Improve use of the test frontend
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@98587 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/connect/test/test_ncbi_download.sh
+++ src/connect/test/test_ncbi_download.sh
@@ -1,11 +1,19 @@
 #! /bin/sh
 # $Id$
 
-fw="`expr $$ '%' 2`"
-
-if [ "$fw" = "1" ]; then
-  CONN_FIREWALL=TRUE
+fw="`expr $$ '%' 10`"
+if [ "`expr $fw '%' 2`" = "1" ]; then
+  : ${CONN_FIREWALL:=TRUE}
   export CONN_FIREWALL
 fi
+
+inhouse="`echo $FEATURES | grep -v '[-]in-house-resources' | grep -c 'in-house-resources'`"
+if [ "$inhouse" != "0" -a "`expr $fw '%' 3`" = "0" ]; then
+  : ${CONN_HOST:=test.ncbi.nlm.nih.gov}
+  export CONN_HOST
+fi
+
+: ${CONN_MAX_TRY:=1}
+export CONN_MAX_TRY
 
 $CHECK_EXEC test_ncbi_download downtest
