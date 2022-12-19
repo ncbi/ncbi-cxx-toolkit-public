61fca3f619ea5d8e6bd484b6e67cc8fb5b21ec4b
stakhovv <stakhovv@78c7ea69-d796-4a43-9a09-de51944f1b03>
2022-12-19 12:58:16 +0000

    JIRA:RW-1871: Reformat code
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@98666 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/app/pub_report/seq_entry_hook.cpp
+++ src/app/pub_report/seq_entry_hook.cpp
@@ -1,34 +1,34 @@
 /*
-* ===========================================================================
-*
-*                            PUBLIC DOMAIN NOTICE
-*               National Center for Biotechnology Information
-*
-*  This software/database is a "United States Government Work" under the
-*  terms of the United States Copyright Act.  It was written as part of
-*  the author's official duties as a United States Government employee and
-*  thus cannot be copyrighted.  This software/database is freely available
-*  to the public for use. The National Library of Medicine and the U.S.
-*  Government have not placed any restriction on its use or reproduction.
-*
-*  Although all reasonable efforts have been taken to ensure the accuracy
-*  and reliability of the software and data, the NLM and the U.S.
-*  Government do not and cannot warrant the performance or results that
-*  may be obtained by using this software or data. The NLM and the U.S.
-*  Government disclaim all warranties, express or implied, including
-*  warranties of performance, merchantability or fitness for any particular
-*  purpose.
-*
-*  Please cite the author in any work or product based on this material.
-*
-* ===========================================================================
-*
-* Author:  Alexey Dobronadezhdin
-*
-* File Description:
-*
-* ===========================================================================
-*/
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Author:  Alexey Dobronadezhdin
+ *
+ * File Description:
+ *
+ * ===========================================================================
+ */
 #include <ncbi_pch.hpp>
 
 #include <objects/seqset/Bioseq_set.hpp>
@@ -47,9 +47,10 @@ namespace pub_report
 CSkipSeqEntryHook::CSkipSeqEntryHook(CBaseReport& report) :
     m_report(report),
     m_level(0)
-{}
+{
+}
 
-void CSkipSeqEntryHook::SkipObject(CObjectIStream &in, const CObjectTypeInfo &info)
+void CSkipSeqEntryHook::SkipObject(CObjectIStream& in, const CObjectTypeInfo& info)
 {
     ++m_level;
 
@@ -63,11 +64,10 @@ void CSkipSeqEntryHook::SkipObject(CObjectIStream &in, const CObjectTypeInfo &in
         m_report.ClearCurrentSeqId();
         m_report.ClearData();
         // do not call ResetLocalSkipHook here, because CSkipSeqIdHook class will do that by itself
-    }
-    else
+    } else
         DefaultSkip(in, info);
 
     --m_level;
 }
 
-} // namespace pub_report
+}
--- src/app/pub_report/unpub_hook.cpp
+++ src/app/pub_report/unpub_hook.cpp
@@ -1,50 +1,47 @@
 /*
-* ===========================================================================
-*
-*                            PUBLIC DOMAIN NOTICE
-*               National Center for Biotechnology Information
-*
-*  This software/database is a "United States Government Work" under the
-*  terms of the United States Copyright Act.  It was written as part of
-*  the author's official duties as a United States Government employee and
-*  thus cannot be copyrighted.  This software/database is freely available
-*  to the public for use. The National Library of Medicine and the U.S.
-*  Government have not placed any restriction on its use or reproduction.
-*
-*  Although all reasonable efforts have been taken to ensure the accuracy
-*  and reliability of the software and data, the NLM and the U.S.
-*  Government do not and cannot warrant the performance or results that
-*  may be obtained by using this software or data. The NLM and the U.S.
-*  Government disclaim all warranties, express or implied, including
-*  warranties of performance, merchantability or fitness for any particular
-*  purpose.
-*
-*  Please cite the author in any work or product based on this material.
-*
-* ===========================================================================
-*
-* Author:  Alexey Dobronadezhdin
-*
-* File Description:
-*
-* ===========================================================================
-*/
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Author:  Alexey Dobronadezhdin
+ *
+ * File Description:
+ *
+ * ===========================================================================
+ */
 #include <ncbi_pch.hpp>
 
 #include <objects/pub/Pub.hpp>
 #include <objects/pub/Pub_equiv.hpp>
-
 #include <objects/biblio/Cit_gen.hpp>
 #include <objects/biblio/Cit_art.hpp>
 #include <objects/biblio/Cit_jour.hpp>
 #include <objects/biblio/Imprint.hpp>
 #include <objects/biblio/Cit_sub.hpp>
-
 #include <objects/general/Date_std.hpp>
 
 #include "utils.hpp"
 #include "unpub_report.hpp"
-
 #include "unpub_hook.hpp"
 
 USING_NCBI_SCOPE;
@@ -55,13 +52,14 @@ namespace pub_report
 
 CSkipPubUnpublishedHook::CSkipPubUnpublishedHook(CUnpublishedReport& report) :
     m_report(report)
-{};
+{
+}
 
 static bool IsGenUnpublished(const CCit_gen& cit)
 {
     return cit.IsSetCit() && cit.IsSetTitle() &&
-           NStr::EqualNocase(cit.GetCit(), "Unpublished") &&
-           !NStr::EqualNocase(cit.GetTitle(), "Direct Submission");
+            NStr::EqualNocase(cit.GetCit(), "Unpublished") &&
+            ! NStr::EqualNocase(cit.GetTitle(), "Direct Submission");
 }
 
 static bool IsArtUnpublished(const CCit_art& cit)
@@ -73,8 +71,8 @@ static bool IsArtUnpublished(const CCit_art& cit)
         if (journal.IsSetImp()) {
 
             const CImprint& imprint = journal.GetImp();
-            ret = (imprint.IsSetPrepub() && imprint.GetPrepub() == CImprint::ePrepub_in_press) ||
-                  (imprint.IsSetPubstatus() && imprint.GetPubstatus() == ePubStatus_aheadofprint);
+            ret                     = (imprint.IsSetPrepub() && imprint.GetPrepub() == CImprint::ePrepub_in_press) ||
+                    (imprint.IsSetPubstatus() && imprint.GetPubstatus() == ePubStatus_aheadofprint);
         }
     }
 
@@ -86,8 +84,7 @@ static bool IsUnpublished(const CPub& pub)
     bool ret = false;
     if (pub.IsGen()) {
         ret = IsGenUnpublished(pub.GetGen());
-    }
-    else if (pub.IsArticle()) {
+    } else if (pub.IsArticle()) {
         ret = IsArtUnpublished(pub.GetArticle());
     }
 
@@ -99,33 +96,27 @@ void CSkipPubUnpublishedHook::ProcessUnpublished(const CPub& pub)
     m_report.ReportUnpublished(pub);
 }
 
-void CSkipPubUnpublishedHook::SkipObject(CObjectIStream &in, const CObjectTypeInfo &info)
+void CSkipPubUnpublishedHook::SkipObject(CObjectIStream& in, const CObjectTypeInfo& info)
 {
     CPub_equiv pubs;
     DefaultRead(in, ObjectInfo(pubs));
 
-    ITERATE(CPub_equiv::Tdata, pub, pubs.Get()) {
-
+    ITERATE (CPub_equiv::Tdata, pub, pubs.Get()) {
         if ((*pub)->IsSub()) {
-
             if ((*pub)->GetSub().IsSetDate() && (*pub)->GetSub().GetDate().IsStd()) {
-
                 const CDate_std& sub_date = (*pub)->GetSub().GetDate().GetStd();
                 if (m_report.IsSetDate()) {
-
                     if (m_report.GetDate().Compare(sub_date) == CDate::eCompare_after) {
                         m_report.SetDate(sub_date);
                     }
-                }
-                else {
+                } else {
                     m_report.SetDate(sub_date);
                 }
             }
-        }
-        else if (IsUnpublished(**pub)) {
+        } else if (IsUnpublished(**pub)) {
             ProcessUnpublished(**pub);
         }
     }
 }
 
-} // namespace pub_report
+}
