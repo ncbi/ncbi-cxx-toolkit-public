6246d078a93b13444e0114371b02392fb21a01de
Vitaly Stakhovsky <stakhovv@ncbi.nlm.nih.gov>
2022-12-23 15:16:40 +0000

    JIRA:RW-1566: New enums
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@98716 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/flatfile/genbank.h
+++ src/objtools/flatfile/genbank.h
@@ -34,47 +34,45 @@
 #ifndef _GENBANK_
 #define _GENBANK_
 
-// clang-format off
-#define ParFlat_COL_DATA   12
+#define ParFlat_COL_DATA 12
 
-/* datablk.type: for detecting which keyword in the datablk's chain
- */
-#define ParFlat_LOCUS      0
-#define ParFlat_DEFINITION 1
-#define ParFlat_ACCESSION  2
-#define ParFlat_NCBI_GI    3
-#define ParFlat_GSDB_ID    4
-#define ParFlat_KEYWORDS   5
-#define ParFlat_SEGMENT    6
-#define ParFlat_SOURCE     7
-#define ParFlat_REFERENCE  8
-#define ParFlat_COMMENT    9
-#define ParFlat_FEATURES   10
-#define ParFlat_BCOUNT     11
-#define ParFlat_ORIGIN     12
-#define ParFlat_END        13
-#define ParFlat_GSDBID     14
-#define ParFlat_CONTIG     15
-#define ParFlat_VERSION    16
-#define ParFlat_USER       17
-#define ParFlat_WGS        18
-#define ParFlat_PRIMARY    19
-#define ParFlat_MGA        20
-#define ParFlat_PROJECT    21
-#define ParFlat_DBLINK     22
+/* datablk.type: for detecting which keyword in the datablk's chain */
+enum EGenbankBlockType {
+    ParFlat_LOCUS      = 0,
+    ParFlat_DEFINITION = 1,
+    ParFlat_ACCESSION  = 2,
+    ParFlat_NCBI_GI    = 3,
+    ParFlat_GSDB_ID    = 4,
+    ParFlat_KEYWORDS   = 5,
+    ParFlat_SEGMENT    = 6,
+    ParFlat_SOURCE     = 7,
+    ParFlat_REFERENCE  = 8,
+    ParFlat_COMMENT    = 9,
+    ParFlat_FEATURES   = 10,
+    ParFlat_BCOUNT     = 11,
+    ParFlat_ORIGIN     = 12,
+    ParFlat_END        = 13,
+    ParFlat_GSDBID     = 14,
+    ParFlat_CONTIG     = 15,
+    ParFlat_VERSION    = 16,
+    ParFlat_USER       = 17,
+    ParFlat_WGS        = 18,
+    ParFlat_PRIMARY    = 19,
+    ParFlat_MGA        = 20,
+    ParFlat_PROJECT    = 21,
+    ParFlat_DBLINK     = 22,
 
-/* define subkeyword
- */
-#define ParFlat_ORGANISM   23
-#define ParFlat_AUTHORS    24
-#define ParFlat_CONSRTM    25
-#define ParFlat_TITLE      26
-#define ParFlat_JOURNAL    27
-#define ParFlat_STANDARD   28
-#define ParFlat_FEATBLOCK  29
-#define ParFlat_MEDLINE    30
-#define ParFlat_REMARK     31
-#define ParFlat_PUBMED     32
-// clang-format on
+    /* define subkeyword */
+    ParFlat_ORGANISM  = 23,
+    ParFlat_AUTHORS   = 24,
+    ParFlat_CONSRTM   = 25,
+    ParFlat_TITLE     = 26,
+    ParFlat_JOURNAL   = 27,
+    ParFlat_STANDARD  = 28,
+    ParFlat_FEATBLOCK = 29,
+    ParFlat_MEDLINE   = 30,
+    ParFlat_REMARK    = 31,
+    ParFlat_PUBMED    = 32,
+};
 
 #endif
