16a42ac55b1643bc014cedf9ff76d096915db8c6
ucko <ucko@78c7ea69-d796-4a43-9a09-de51944f1b03>
2023-01-03 17:01:54 +0000

    ncbi_sanitizers.h: Correct LeakSanitizer and MemorySanitizer handling.
    - With __has_feature available, conditionalize NCBI_USE_LSAN on
      __has_feature(leak_sanitizer) and a new NCBI_USE_MSAN macro on
      __has_feature(memory_sanitizer), which is orthogonal.
    - Otherwise, conditionalize those macros on __SANITIZE_LEAK__ and
      __SANITIZE_MEMORY__ respectively.  (NB: Both conditions are likely
      hypothetical -- Clang doesn't predefine any __SANITIZE_*__ macros, and
      GCC doesn't predefine anything for -fsanitize=leak or support
      -fsanitize=memory at all.)
    - Generally have NCBI_USE_ASAN imply NCBI_USE_LSAN.
    
    JIRA: CXX-12789.
    
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@98757 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/common/ncbi_sanitizers.h
+++ include/common/ncbi_sanitizers.h
@@ -36,23 +36,34 @@
 #  if __has_feature(address_sanitizer)
 #      define NCBI_USE_ASAN
 #  endif
-#  if __has_feature(memory_sanitizer)
+#  if __has_feature(leak_sanitizer)
 #      define NCBI_USE_LSAN
+#  endif
+#  if __has_feature(memory_sanitizer)
+#      define NCBI_USE_MSAN
 #  endif
 #  if __has_feature(thread_sanitizer)
 #      define NCBI_USE_TSAN
 #  endif
 #else
-// Fallback for older compilers
+// Fallback for other compilers
 #  if defined(__SANITIZE_ADDRESS__)
 #      define NCBI_USE_ASAN
 #  endif
-#  if defined(__SANITIZE_MEMORY__)
+#  if defined(__SANITIZE_LEAK__)
 #      define NCBI_USE_LSAN
+#  endif
+#  if defined(__SANITIZE_MEMORY__)
+#      define NCBI_USE_MSAN
 #  endif
 #  if defined(__SANITIZE_THREAD__)
 #      define NCBI_USE_TSAN
 #  endif
+#endif
+
+
+#if defined(NCBI_USE_ASAN)  &&  !defined(NCBI_USE_LSAN)
+#  define NCBI_USE_LSAN
 #endif
 
 
