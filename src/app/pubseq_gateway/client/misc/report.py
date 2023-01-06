5537b4bbda94656a33916919d0e8fba7005cfdfd
Rafael Sadyrov <sadyrovr@ncbi.nlm.nih.gov>
2023-01-06 18:12:05 +0000

    report.py: All modes now use ini-files, if available. JIRA CXX-12568
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@98796 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/app/pubseq_gateway/client/misc/report.py
+++ src/app/pubseq_gateway/client/misc/report.py
@@ -374,7 +374,9 @@ def overall_cmd(input_file_option, args, path, input_file, iter_args):
         }
 
     def overall(run_no, service, user_threads, io_threads, requests_per_io, binary):
-        cmd = [ binary, args.command, input_file_option, input_file, '-service', service, '-worker-threads', str(user_threads), '-io-threads', str(io_threads), '-requests-per-io', str(requests_per_io) ]
+        conf_file = binary + '.ini'
+        conf = [ '-conffile', conf_file ] if os.path.isfile(conf_file) else []
+        cmd = [ binary, args.command, input_file_option, input_file, '-service', service, '-worker-threads', str(user_threads), '-io-threads', str(io_threads), '-requests-per-io', str(requests_per_io), *conf ]
         open_stdout = lambda: open(get_filename(path, f'raw.{run_no}', *run_args), 'w') if args.save_output else contextlib.nullcontext()
         open_stderr = lambda: open(get_filename(path, f'err.{run_no}', *run_args), 'w') if args.save_stderr else contextlib.nullcontext()
 
