e8854abca6d1f35ca555dbcdd4b8abbb6cbb3b2d
fongah2 <fongah2@ncbi.nlm.nih.gov>
2023-02-07 13:36:00 +0000

    Add default word thresholds, JIRA:WB-5553
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@99040 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/algo/blast/core/blast_options.h
+++ include/algo/blast/core/blast_options.h
@@ -104,6 +104,10 @@ extern "C" {
 #define BLAST_WORD_THRESHOLD_BLASTP 11 /**< default neighboring threshold
                                          (blastp and for rpsblast at RPS-BLAST
                                          database creation time) */
+#define BLAST_WORD_THRESHOLD_BLASTP_FAST 19.3 //neighboring threshold for word size 5 (blastp/x-fast)
+#define BLAST_WORD_THRESHOLD_BLASTP_WD_SZ_6 21 //neighboring threshold for word size 6
+#define BLAST_WORD_THRESHOLD_BLASTP_WD_SZ_7 20.25 //neighboring threshold for word size 7
+
 #define BLAST_WORD_THRESHOLD_BLASTN 0 /**< default threshold (blastn) */
 #define BLAST_WORD_THRESHOLD_BLASTX 12 /**< default threshold (blastx) */
 #define BLAST_WORD_THRESHOLD_TBLASTN 13 /**< default neighboring threshold 
