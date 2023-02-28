290d90f9c1f1a41b240b7f31f482e8b1949a3da5
satskyse <satskyse@ncbi.nlm.nih.gov>
2023-02-28 16:46:17 +0000

    Updated introspection and an automated script to convert json into an .hpp. JIRA: CXX-12830
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@99219 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/app/pubseq_gateway/server/update_introspection.py
+++ src/app/pubseq_gateway/server/update_introspection.py
@@ -1,14 +1,82 @@
 #!/usr/bin/env python3
 
-import os, sys
+import os, sys, os.path
 from optparse import OptionParser
+from urllib import request as urlrequest
+
+
+XSLT_FILE_NAME = 'json2html_test.xsl'
+
+def getContentType(response):
+    for item in response.getheaders():
+        if item[0].lower() == 'content-type':
+            return item[1]
+    return None
+
+
+def getJsonFromServer(host, port):
+    try:
+        url = 'http://' + host + ':' + str(port) + '/?fmt=json'
+        req = urlrequest.Request(url)
+        response = urlrequest.urlopen(req, timeout=5)
+        content = response.read().decode('utf-8')
+
+        if response.status != 200:
+            print('Response from ' + url + ' is not 200: ' + str(response.status))
+            return None
+
+        contentType = getContentType(response)
+        if contentType is None:
+            print('Resonse from ' + url + ' does not have a content type')
+            return None
+
+        if contentType.lower() != 'application/json':
+            print('Response from '  + url + ' is not json: ' + contentType)
+            return None
+
+        return content
+    except Exception as exc:
+        print('Error getting json from ' + url + ': ' + str(exc))
+        return None
+    return None
+
+def saveJsonContent(jsonContent):
+    fName = 'introspection.json'
+    try:
+        with open(fName, 'w') as out:
+            out.write(jsonContent)
+        return fName
+    except Exception as exc:
+        print('Error writing json into a file ' + fName + ': ' + str(exc))
+        return None
+
+def runXslt(jsonFileName):
+    htmlFileName = 'introspection.html'
+
+    if 'NCBI' in os.environ:
+        transformPath = os.environ['NCBI'] + '/'
+    else:
+        transformPath = '/netopt/ncbi_tools64/'
+    transformPath += 'saxon-11.5/c++/command/transform'
+    transformPath = os.path.normpath(transformPath)
+
+    if not os.path.exists(transformPath):
+        print('Cannot find transform. Expected here: ' + transformPath)
+        return None
+
+    ret = os.system(transformPath + ' -xsl:' + XSLT_FILE_NAME +
+                    ' -it:main input=' + jsonFileName + ' > ' + htmlFileName)
+    if ret != 0:
+        print('Transform execution error code: ' + str(ret))
+        return None
+    return htmlFileName
 
 
 def main():
 
     parser = OptionParser(
     """
-    %prog [input file (html) [output file (hpp)]]
+    %prog [output .hpp file]
     default input file name: introspection.html
     default output file name: introspection_html.hpp
     """)
@@ -16,31 +84,60 @@ def main():
     parser.add_option("-v", "--verbose",
                       action="store_true", dest="verbose", default=False,
                       help="be verbose (default: False)")
+    parser.add_option("--server",
+                      action="store", type="string", dest="server",
+                      help="PSG server instance: should host:port. Default: tonka1:2180")
 
     options, args = parser.parse_args()
     verbose = options.verbose
 
-    if len(args) not in [0, 1, 2]:
-        print('incorrect number of arguments. Expected 0, 1 or 2')
+    if len(args) not in [0, 1]:
+        print('incorrect number of arguments. Expected 0 or 1')
         return 3
 
-    inputFile = 'introspection.html'
-    if len(args) > 0:
-        inputFile = args[0]
+    srv = "tonka1:2180"
+    if options.server:
+        srv = options.server
 
-    outputFile = 'introspection_html.hpp'
-    if len(args) > 1:
-        outputFile = args[1]
-
-    if not os.path.exists(inputFile):
-        print('Cannot find file ' + inputFile)
+    parts = srv.split(':')
+    if len(parts) != 2:
+        print('Incorrect server address')
         return 3
-
-    iFile = open(inputFile, 'r')
+    host = parts[0]
     try:
-        oFile = open(outputFile, 'w')
+        port = int(parts[1])
     except:
-        print('Cannot open output file ' + outputFile)
+        print('Incorrect server port')
+        return 3
+
+    jsonContent = getJsonFromServer(host, port)
+    if jsonContent is None:
+        return 3
+
+    jsonFileName = saveJsonContent(jsonContent)
+    if jsonFileName is None:
+        return 3
+
+    # Run the xslt
+    htmlFileName = runXslt(jsonFileName)
+    if htmlFileName is None:
+        return 3
+
+    try:
+        os.remove(jsonFileName)
+    except:
+        pass
+
+    # Final step: convert html into .hpp
+    hppFile = 'introspection_html.hpp'
+    if len(args) > 0:
+        outputFile = args[0]
+
+    iFile = open(htmlFileName, 'r')
+    try:
+        oFile = open(hppFile, 'w')
+    except:
+        print('Cannot open output file ' + hppFile)
         return 3
 
     for line in iFile.readlines():
@@ -49,6 +146,12 @@ def main():
 
     iFile.close()
     oFile.close()
+
+    try:
+        os.remove(htmlFileName)
+    except:
+        pass
+
     return 0
 
 
