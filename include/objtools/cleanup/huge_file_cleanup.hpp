f237c5d541197462926fc17a85611caf0f43ea06
foleyjp <foleyjp@ncbi.nlm.nih.gov>
2023-03-02 18:52:42 +0000

    JIRA: RW-1942 Added code for updating colliding feature ids. Still need to test this, and it doesn't yet work for small-genome sets.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@99251 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/objtools/cleanup/huge_file_cleanup.hpp
+++ include/objtools/cleanup/huge_file_cleanup.hpp
@@ -66,6 +66,13 @@ public:
     CRef<CSeq_entry> LoadSeqEntry(const TBioseqSetInfo& info,
             eAddTopEntry add_top_entry = eAddTopEntry::yes) const override;
     const CCleanupChangeCore& GetChanges() const;
+
+
+    using TFeatId = CFeat_id::TLocal::TId;
+    set<TFeatId> m_FeatIds;
+    using TFeatIdMap = map<TFeatId, TFeatId>;
+    using TPosToFeatIds = map<CHugeAsnReader::TStreamPos, TFeatIdMap>;
+
 private:
     void x_SetHooks(CObjectIStream& objStream, TContext& context) override;
     void x_SetBioseqHooks(CObjectIStream& objStream, TContext& context) override;
@@ -93,19 +100,13 @@ private:
     map<TFileSize, string> m_SetPosToFluLabel;
     set<CConstRef<CSeq_id>, CRefLess> m_HasIncompleteFeats;
 
-public:
-    using TFeatId = CFeat_id::TLocal::TId;
-    set<TFeatId> m_FeatIds;
-    using TFeatIdMap = map<TFeatId, TFeatId>;
-    using TPosToFeatIds = map<CHugeAsnReader::TStreamPos, TFeatIdMap>;
-    TPosToFeatIds m_PosToFeatIdMap;
-
 private:
-    set<TFeatId> m_ExistingIds;
-    set<TFeatId> m_NewIds;
-    set<TFeatId> m_NewExistingIds;
-    TFeatIdMap   m_RemappedIds;
-    TFeatId      m_IdOffset{0};
+    set<TFeatId>  m_ExistingIds;
+    set<TFeatId>  m_NewIds;
+    set<TFeatId>  m_NewExistingIds;
+    TFeatIdMap    m_RemappedIds;
+    TFeatId       m_IdOffset{0};
+    TPosToFeatIds m_PosToFeatIdMap;
 };
 
 END_SCOPE(object)
