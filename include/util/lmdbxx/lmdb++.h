1cf2654ec27168e453f6816681015d0df42cecf3
Aaron Ucko <ucko@ncbi.nlm.nih.gov>
2023-03-03 17:07:24 +0000

    lmdb++.h: Decompose is_pod into is_standard_layout and is_trivial.
    Address C++20 deprecation warnings.  JIRA: CXX-12877.
    
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@99266 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/util/lmdbxx/lmdb++.h
+++ include/util/lmdbxx/lmdb++.h
@@ -1203,7 +1203,8 @@ public:
 };
 
 #if !(defined(__COVERITY__) || defined(_MSC_VER) || defined(__INTEL_COMPILER))
-static_assert(std::is_pod<lmdb::val>::value, "lmdb::val must be a POD type");
+static_assert(std::is_standard_layout<lmdb::val>::value, "lmdb::val must be a standard-layout type");
+static_assert(std::is_trivial<lmdb::val>::value, "lmdb::val must be a trivial type");
 static_assert(sizeof(lmdb::val) == sizeof(MDB_val), "sizeof(lmdb::val) != sizeof(MDB_val)");
 #endif
 
