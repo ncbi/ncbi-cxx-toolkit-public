9ed1dd67dd5434d831b764261640c2405e6f2db4
Andrei Gourianov <gouriano@ncbi.nlm.nih.gov>
2023-04-28 16:41:06 +0000

    Added settings with DebugInfo. JIRA: CXX-12880
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@99686 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/build-system/cmake/toolchains/aarch64-linux-gcc.cmake
+++ src/build-system/cmake/toolchains/aarch64-linux-gcc.cmake
@@ -1,16 +1,16 @@
 #############################################################################
-# : x86_64-linux-gcc.cmake.in 666608 2023-04-27 18:45:36Z gouriano $
+# $Id$
 #############################################################################
 #############################################################################
 ##
 ##  NCBI CMake wrapper
-##  GCC 12.2.0 toolchain
+##  GCC toolchain
 
 set(NCBI_PTBCFG_FLAGS_DEFINED YES)
 include_guard(GLOBAL)
 
-set(CMAKE_C_COMPILER "/opt/ncbi/gcc/12.2.0/bin/gcc")
-set(CMAKE_CXX_COMPILER "/opt/ncbi/gcc/12.2.0/bin/g++")
+set(CMAKE_C_COMPILER "/usr/bin/gcc")
+set(CMAKE_CXX_COMPILER "/usr/bin/g++")
 
 set(CMAKE_C_FLAGS              "-gdwarf-4 -Wall -Wno-format-y2k")
 set(CMAKE_C_FLAGS_DEBUG        "-ggdb3 -O0")
