d4d470df73c98a30e5759edda2d782c17c6007ea
gouriano <gouriano@ncbi.nlm.nih.gov>
2023-07-20 11:56:25 +0000

    Changed GCC version. JIRA: CXX-13149
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@100315 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/build-system/cmake/toolchains/x86_64-linux-icc-210.cmake
+++ src/build-system/cmake/toolchains/x86_64-linux-icc-210.cmake
@@ -13,9 +13,9 @@ set(CMAKE_C_COMPILER "/usr/local/intel/Compiler/21.0/compiler/latest/linux/bin/i
 set(CMAKE_CXX_COMPILER "/usr/local/intel/Compiler/21.0/compiler/latest/linux/bin/icpx")
 
 set(CMAKE_C_FLAGS
-    "-isystem /opt/ncbi/gcc/7.3.0/include/c++/7.3.0 \
-    -isystem /opt/ncbi/gcc/7.3.0/include/c++/7.3.0/x86_64-redhat-linux-gnu  \
-    -isystem /opt/ncbi/gcc/7.3.0/include/c++/7.3.0/backward \
+    "-isystem /opt/ncbi/gcc/12.2.0/include/c++/12.2.0 \
+    -isystem /opt/ncbi/gcc/12.2.0/include/c++/12.2.0/x86_64-redhat-linux-gnu  \
+    -isystem /opt/ncbi/gcc/12.2.0/include/c++/12.2.0/backward \
     -ffp-model=precise -fPIC"
 )
 set(CMAKE_C_FLAGS_DEBUG   "-g -O0")
@@ -23,9 +23,9 @@ set(CMAKE_C_FLAGS_RELEASE "-O3")
 set(CMAKE_C_FLAGS_RELWITHDEBINFO  "-g -O3")
 
 set(CMAKE_CXX_FLAGS
-    "-isystem /opt/ncbi/gcc/7.3.0/include/c++/7.3.0 \
-    -isystem /opt/ncbi/gcc/7.3.0/include/c++/7.3.0/x86_64-redhat-linux-gnu \
-    -isystem /opt/ncbi/gcc/7.3.0/include/c++/7.3.0/backward \
+    "-isystem /opt/ncbi/gcc/12.2.0/include/c++/12.2.0 \
+    -isystem /opt/ncbi/gcc/12.2.0/include/c++/12.2.0/x86_64-redhat-linux-gnu \
+    -isystem /opt/ncbi/gcc/12.2.0/include/c++/12.2.0/backward \
     -ffp-model=precise -fPIC"
 )
 set(CMAKE_CXX_FLAGS_DEBUG   "-g -O0")
@@ -36,10 +36,10 @@ set(CMAKE_EXE_LINKER_FLAGS
     "-L/usr/local/intel/Compiler/21.0/compiler/latest/linux/compiler/lib/intel64 \
     -Wl,-rpath,/usr/local/intel/Compiler/21.0/compiler/latest/linux/compiler/lib/intel64 \
     -lintlc \
-    -L/opt/ncbi/gcc/7.3.0/lib/gcc/x86_64-redhat-linux-gnu/7.3.0 \
-    -B/opt/ncbi/gcc/7.3.0/lib/gcc/x86_64-redhat-linux-gnu/7.3.0 \
-    -L/opt/ncbi/gcc/7.3.0/lib64 \
-    -Wl,-rpath,/opt/ncbi/gcc/7.3.0/lib64 \
+    -L/opt/ncbi/gcc/12.2.0/lib/gcc/x86_64-redhat-linux-gnu/12.2.0 \
+    -B/opt/ncbi/gcc/12.2.0/lib/gcc/x86_64-redhat-linux-gnu/12.2.0 \
+    -L/opt/ncbi/gcc/12.2.0/lib64 \
+    -Wl,-rpath,/opt/ncbi/gcc/12.2.0/lib64 \
     -Wl,--enable-new-dtags"
 )
 
@@ -47,10 +47,10 @@ set(CMAKE_SHARED_LINKER_FLAGS
     "-L/usr/local/intel/Compiler/21.0/compiler/latest/linux/compiler/lib/intel64 \
     -Wl,-rpath,/usr/local/intel/Compiler/21.0/compiler/latest/linux/compiler/lib/intel64 \
     -lintlc \
-    -L/opt/ncbi/gcc/7.3.0/lib/gcc/x86_64-redhat-linux-gnu/7.3.0 \
-    -B/opt/ncbi/gcc/7.3.0/lib/gcc/x86_64-redhat-linux-gnu/7.3.0 \
-    -L/opt/ncbi/gcc/7.3.0/lib64 \
-    -Wl,-rpath,/opt/ncbi/gcc/7.3.0/lib64 \
+    -L/opt/ncbi/gcc/12.2.0/lib/gcc/x86_64-redhat-linux-gnu/12.2.0 \
+    -B/opt/ncbi/gcc/12.2.0/lib/gcc/x86_64-redhat-linux-gnu/12.2.0 \
+    -L/opt/ncbi/gcc/12.2.0/lib64 \
+    -Wl,-rpath,/opt/ncbi/gcc/12.2.0/lib64 \
     -Wl,--no-undefined"
 )
 
