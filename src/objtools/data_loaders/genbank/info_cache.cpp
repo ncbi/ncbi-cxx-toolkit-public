9a9e52c765524f2ec90aeaf35377dbd80cbb6f50
vasilche <vasilche@ncbi.nlm.nih.gov>
2023-08-10 21:31:27 +0000

    Conditionally compile debug checks. JIRA: OMDM-51
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@100556 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/data_loaders/genbank/info_cache.cpp
+++ src/objtools/data_loaders/genbank/info_cache.cpp
@@ -393,8 +393,9 @@ CInfoCache_Base::~CInfoCache_Base(void)
 }
 
 
-bool CInfoCache_Base::x_Check(const vector<const CInfo_Base*>& infos) const
+bool CInfoCache_Base::x_Check(const vector<const CInfo_Base*>& _DEBUG_ARG(infos)) const
 {
+#ifdef _DEBUG
     size_t unused_count = 0, used_count = 0;
     ITERATE ( vector<const CInfo_Base*>, it, infos ) {
         const CInfo_Base& info = **it;
@@ -406,6 +407,7 @@ bool CInfoCache_Base::x_Check(const vector<const CInfo_Base*>& infos) const
         }
     }
     _ASSERT(unused_count == m_CurGCQueueSize);
+#endif
     return true;
 }
 
