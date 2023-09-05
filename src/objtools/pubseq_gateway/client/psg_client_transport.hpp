c95dbed2a6a6dee72a35cc35b5cfa3e37efba43c
sadyrovr <sadyrovr@ncbi.nlm.nih.gov>
2023-09-05 15:07:43 +0000

    Pass 'WebCubbyUser' (in 'auth_token') from CGI to PSG client API. JIRA CXX-13208
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@100731 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/pubseq_gateway/client/psg_client_transport.hpp
+++ src/objtools/pubseq_gateway/client/psg_client_transport.hpp
@@ -733,7 +733,7 @@ protected:
             const uint8_t* value, size_t valuelen, uint8_t flags);
 
 private:
-    enum EHeaders { eMethod, eScheme, eAuthority, ePath, eUserAgent, eSessionID, eSubHitID, eClientIP, eSize };
+    enum EHeaders { eMethod, eScheme, eAuthority, ePath, eUserAgent, eSessionID, eSubHitID, eCookie, eClientIP, eSize };
 
     using TRequests = unordered_map<int32_t, SPSG_TimedRequest>;
 
