7ebefa866be348fbbf654fb05e617768af2b6d1f
Dmitrii Saprykin <saprykin@ncbi.nlm.nih.gov>
2023-09-06 16:22:25 +0000

    JIRA: ID-7850 Draft implementation of async client for TxProxy(MyNCBIAccount)
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@100741 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ include/objtools/pubseq_gateway/impl/myncbi/myncbi_request.hpp
@@ -0,0 +1,171 @@
+#ifndef PSG_MYNCBI__MYNCBI_REQUEST__HPP
+#define PSG_MYNCBI__MYNCBI_REQUEST__HPP
+
+/*  $Id$
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Authors: Dmitrii Saprykin
+ *
+ * File Description: MyNCBIAccount request factory
+ *
+ */
+
+#include <corelib/ncbistr.hpp>
+#include <corelib/request_status.hpp>
+
+#include <functional>
+
+BEGIN_NCBI_SCOPE
+
+enum class EPSG_MyNCBIResponseStatus
+{
+    eUndefined = 0,
+    eSuccess,
+    eInProgress,
+    eError
+};
+
+using TDataErrorCallback = function<void(CRequestStatus::ECode status, int code, EDiagSev severity, const string & message)>;
+
+class IPSG_MyNCBIRequest
+{
+public:
+    void SetErrorCallback(TDataErrorCallback fn)
+    {
+        m_ErrorCallback = move(fn);
+    }
+
+    void ReceiveHttpResponseData(string data);
+    void SetHttpResponseStatus(long status);
+    string GetHttpResponseData() const;
+    long GetHttpResponseStatus() const;
+
+    void SetResponseStatus(EPSG_MyNCBIResponseStatus status);
+    EPSG_MyNCBIResponseStatus GetResponseStatus() const;
+
+    virtual string GetRequestXML() const = 0;
+    virtual void HandleResponse();
+
+    virtual ~IPSG_MyNCBIRequest() = default;
+protected:
+    TDataErrorCallback m_ErrorCallback;
+private:
+    string m_ResponseData;
+    long m_HttpResponseStatus{-1};
+    EPSG_MyNCBIResponseStatus m_ResponseStatus{EPSG_MyNCBIResponseStatus::eInProgress};
+};
+
+class CPSG_MyNCBIRequest_WhoAmI
+    : public IPSG_MyNCBIRequest
+{
+public:
+    struct SUserInfo {
+        Int8 user_id{0};
+        string username;
+        string email_address;
+    };
+    using TConsumeCallback = function<void(SUserInfo)>;
+
+    explicit CPSG_MyNCBIRequest_WhoAmI(string cookie)
+        : m_Cookie(move(cookie))
+    {}
+
+    void SetConsumeCallback(TConsumeCallback fn)
+    {
+        m_Consume = move(fn);
+    }
+
+    string GetRequestXML() const override
+    {
+        return
+        "<MyNcbiRequest version=\"2.0\">"
+            "<Command name=\"whoami\"/>"
+            "<Parameter>" + NStr::XmlEncode(m_Cookie) + "</Parameter>"
+        "</MyNcbiRequest>";
+    }
+
+    void HandleResponse() override;
+
+    Int8 GetUserId() const
+    {
+        return m_UserInfo.user_id;
+    }
+
+    string GetUserName() const
+    {
+        return m_UserInfo.username;
+    }
+
+    string GetEmailAddress() const
+    {
+        return m_UserInfo.email_address;
+    }
+
+private:
+    string m_Cookie;
+    SUserInfo m_UserInfo;
+    TConsumeCallback m_Consume;
+};
+
+/**
+ * @for_testing_only
+ */
+class CPSG_MyNCBIRequest_SignIn
+    : public IPSG_MyNCBIRequest
+{
+public:
+    CPSG_MyNCBIRequest_SignIn(string username, string password)
+        : m_Username(move(username))
+        , m_Password(move(password))
+    {}
+
+    string GetRequestXML() const override
+    {
+        return
+        "<myncbi-account-request version=\"1.0\">"
+            "<command name=\"sign-in\">"
+                "<param name=\"username\" value=\"" + NStr::XmlEncode(m_Username) + "\"/>"
+                "<param name=\"password\" value=\"" + NStr::XmlEncode(m_Password) + "\"/>"
+                "<param name=\"persistent\" value=\"true\"/>"
+            "</command>"
+        "</myncbi-account-request>";
+    }
+
+    void HandleResponse() override;
+
+    string GetCookie() const
+    {
+        return m_Cookie;
+    }
+
+private:
+    string m_Username;
+    string m_Password;
+    string m_Cookie;
+};
+
+END_NCBI_SCOPE
+
+#endif  // PSG_MYNCBI__MYNCBI_REQUEST__HPP
---
+++ src/objtools/pubseq_gateway/myncbi/myncbi_factory.cpp
@@ -0,0 +1,460 @@
+/*  $Id$
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Authors: Dmitrii Saprykin
+ *
+ * File Description: MyNCBIAccount request factory
+ *
+ */
+
+#include <ncbi_pch.hpp>
+
+#include <curl/curl.h>
+
+#include <string>
+#include <sstream>
+
+#include <corelib/ncbitime.hpp>
+#include <corelib/request_status.hpp>
+
+#include <objtools/pubseq_gateway/impl/myncbi/myncbi_factory.hpp>
+
+namespace
+{
+
+USING_NCBI_SCOPE;
+
+template<typename... TArgs>
+void VLOG(TArgs &&... args)
+{
+    USING_NCBI_SCOPE;
+    if (CPSG_MyNCBIFactory::sVerboseLogging) {
+        stringstream s;
+        ((s << std::forward<TArgs>(args)), ...);
+        LOG_POST(Message << "["
+            << CTime(CTime::eCurrent).AsString(CTimeFormat("Y-M-D h:m:s")) << "]     MyNCBIDebug: " << s.str());
+    }
+}
+
+struct SCurlMultiContext
+{
+    ~SCurlMultiContext()
+    {
+        VLOG("~SCurlMultiContext()");
+        if (curl_mhandle) {
+            curl_multi_cleanup(curl_mhandle);
+            curl_mhandle = nullptr;
+        }
+
+        if (connect_to) {
+            curl_slist_free_all(connect_to);
+            connect_to = nullptr;
+        }
+    }
+
+    CURLM *curl_mhandle{nullptr};
+    uv_loop_t* uv_loop{nullptr};
+    uv_timer_t uv_timer{};
+    weak_ptr<IPSG_MyNCBIRequest> request;
+    curl_slist *connect_to{nullptr};
+};
+
+struct SCurlSocketContext
+{
+    uv_poll_t poll_handle{};
+    curl_socket_t sockfd{0};
+    SCurlMultiContext* multi_context{nullptr};
+};
+
+SCurlSocketContext *create_curl_context(curl_socket_t sockfd, SCurlMultiContext* multi_context)
+{
+    VLOG("Creating curl_context for socket ", sockfd);
+    auto context = new SCurlSocketContext();
+    context->sockfd = sockfd;
+    context->multi_context = multi_context;
+    int r = uv_poll_init_socket(multi_context->uv_loop, &context->poll_handle, sockfd);
+    if (r != 0) {
+        VLOG("Failed to init socket for polling by uv_poll_init_socket for ", sockfd);
+        delete context;
+        return nullptr;
+    }
+    context->poll_handle.data = context;
+    return context;
+}
+
+void uv_curl_close_cb(uv_handle_t *handle)
+{
+    VLOG("curl_close_cb for &", reinterpret_cast<intptr_t>(handle));
+    auto context = reinterpret_cast<SCurlSocketContext*>(handle->data);
+    if (context) {
+        VLOG("curl_close_cb deleted socket context for &", reinterpret_cast<intptr_t>(handle));
+        //delete context->multi_context;
+        delete context;
+        handle->data = nullptr;
+    }
+}
+
+void destroy_curl_context(SCurlSocketContext *context)
+{
+    VLOG("destroy_curl_context: socket context - &", reinterpret_cast<intptr_t>(context));
+    uv_close(reinterpret_cast<uv_handle_t*>(&context->poll_handle), uv_curl_close_cb);
+}
+
+void check_multi_info(SCurlMultiContext *context, weak_ptr<IPSG_MyNCBIRequest> request)
+{
+    VLOG("check_multi_info: multi context - &", reinterpret_cast<intptr_t>(context));
+    char *done_url;
+    string done_str;
+    long response_code;
+    CURLMsg *message;
+    CURL *easy_handle;
+    int pending{0};
+    bool request_finished{false};
+    while ((message = curl_multi_info_read(context->curl_mhandle, &pending))) {
+        if (message->msg == CURLMSG_DONE) {
+            /* Do not use message data after calling curl_multi_remove_handle() and
+             curl_easy_cleanup(). As per curl_multi_info_read() docs:
+             "WARNING: The data the returned pointer points to will not survive
+             calling curl_multi_cleanup, curl_multi_remove_handle or
+             curl_easy_cleanup." */
+            easy_handle = message->easy_handle;
+            curl_easy_getinfo(easy_handle, CURLINFO_EFFECTIVE_URL, &done_url);
+            curl_easy_getinfo(easy_handle, CURLINFO_RESPONSE_CODE, &response_code);
+            done_str.assign(done_url);
+            curl_multi_remove_handle(context->curl_mhandle, easy_handle);
+            curl_easy_cleanup(easy_handle);
+            request_finished = true;
+
+
+            auto request_ptr = request.lock();
+            if (request_ptr) {
+                VLOG("DONE reading URL - '", done_str, "'; multi context - &", reinterpret_cast<intptr_t>(context));
+                VLOG("Response - '", request_ptr->GetHttpResponseData(), "'");
+                request_ptr->SetHttpResponseStatus(response_code);
+                request_ptr->HandleResponse();
+            }
+            else {
+                VLOG("Request cannot be locked in CURLMSG_DONE handler");
+            }
+        }
+    }
+    if (request_finished) {
+        VLOG("check_multi_info - request finished. Removing multi context - &", reinterpret_cast<intptr_t>(context));
+        delete context;
+    }
+}
+
+void uv_curl_perform_cb(uv_poll_t *req, int status, int events)
+{
+    auto context = reinterpret_cast<SCurlSocketContext*>(req);
+    VLOG("uv_curl_perform_cb: status - ", status, "; events - ", events,
+         "; socket context - &", reinterpret_cast<intptr_t>(context));
+    uv_timer_stop(&context->multi_context->uv_timer);
+    int flags{0};
+    if (status < 0) {
+        flags = CURL_CSELECT_ERR;
+    }
+    if (!status && events & UV_READABLE) {
+        flags |= CURL_CSELECT_IN;
+    }
+    if (!status && events & UV_WRITABLE) {
+        flags |= CURL_CSELECT_OUT;
+    }
+
+    int running_handles;
+    curl_multi_socket_action(context->multi_context->curl_mhandle, context->sockfd, flags, &running_handles);
+    check_multi_info(context->multi_context, context->multi_context->request);
+}
+
+int curl_handle_socket_cb(CURL */*easy*/, curl_socket_t s, int action, void *clientp, void *socketp)
+{
+    auto context = reinterpret_cast<SCurlMultiContext*>(clientp);
+    VLOG("curl_handle_socket_cb: socket - ", s, "; action - ", action, "; multi context - &", reinterpret_cast<intptr_t>(clientp));
+    SCurlSocketContext *socket_context{nullptr};
+    if (action == CURL_POLL_IN || action == CURL_POLL_OUT) {
+        if (socketp) {
+            socket_context = reinterpret_cast<SCurlSocketContext*>(socketp);
+        }
+        else {
+            socket_context = create_curl_context(s, context);
+            auto assign_rc = curl_multi_assign(context->curl_mhandle, s, socket_context);
+            if (assign_rc != 0) {
+                VLOG("Failed to assign socket context by curl_multi_assign()");
+            }
+        }
+        if (socket_context == nullptr) {
+            // returns -1 in case of error
+            VLOG("Failed to create socket context. Failing all operations");
+            return -1;
+        }
+    }
+
+    int rc{0};
+    switch (action) {
+        case CURL_POLL_IN: {
+            rc = uv_poll_start(&socket_context->poll_handle, UV_READABLE, uv_curl_perform_cb);
+            if (rc < 0) {
+                VLOG("Failed to start polling by uv_poll_start(UV_READABLE). rc = ", rc);
+                return -1;
+            }
+            break;
+        }
+        case CURL_POLL_OUT: {
+            uv_poll_start(&socket_context->poll_handle, UV_WRITABLE, uv_curl_perform_cb);
+            if (rc < 0) {
+                VLOG("Failed to start polling by uv_poll_start(UV_WRITABLE). rc = ", rc);
+                return -1;
+            }
+            break;
+        }
+        case CURL_POLL_REMOVE:
+            if (socketp) {
+                uv_poll_stop(&(reinterpret_cast<SCurlSocketContext*>(socketp))->poll_handle);
+                destroy_curl_context(reinterpret_cast<SCurlSocketContext*>(socketp));
+                curl_multi_assign(context->curl_mhandle, s, nullptr);
+            }
+            break;
+        default: {
+            // returns -1 in case of error
+            VLOG("Unexpected action ", action, " Failing all operations");
+            return -1;
+        }
+    }
+
+    return 0;
+}
+
+void uv_on_timeout_cb(uv_timer_t *req)
+{
+    auto context = reinterpret_cast<SCurlMultiContext*>(req->data);
+    VLOG("uv_on_timeout_cb: multi context - &", reinterpret_cast<intptr_t>(context));
+    int running_handles{-1};
+    curl_multi_socket_action(context->curl_mhandle, CURL_SOCKET_TIMEOUT, 0, &running_handles);
+    check_multi_info(context, context->request);
+}
+
+void curl_start_timeout_cb(CURLM */*multi*/, long timeout_ms, void *clientp)
+{
+    auto context = reinterpret_cast<SCurlMultiContext*>(clientp);
+    VLOG("curl_start_timeout_cb; multi context - &", reinterpret_cast<intptr_t>(context), "; timeout_ms - ",  timeout_ms);
+    // CURL asks to remove timer
+    if (timeout_ms < 0) {
+        uv_timer_stop(&context->uv_timer);
+    }
+    else {
+        // CURL wants to fire right now but we will postpone a little bit
+        if (timeout_ms == 0) {
+            timeout_ms = 1;
+        }
+        context->uv_timer.data = context;
+        uv_timer_start(&context->uv_timer, uv_on_timeout_cb, timeout_ms, 0);
+    }
+}
+
+size_t curl_write_data_cv(void *data, size_t size, size_t nmemb, void *clientp)
+{
+    auto context = reinterpret_cast<SCurlMultiContext*>(clientp);
+    size_t realsize = size * nmemb;
+    VLOG("curl_write_data_cv: - ", realsize, " bytes; multi context - ", reinterpret_cast<intptr_t>(context));
+    auto request = context->request.lock();
+    if (request) {
+        string data_chunk(reinterpret_cast<char *>(data), realsize);
+        request->ReceiveHttpResponseData(data_chunk);
+    }
+    // We just skip all transferred data if {{request}} is out of scope
+    else {
+        VLOG("curl_write_data_cv: data skipped");
+    }
+    return realsize;
+}
+
+void curl_send_request(SCurlMultiContext* multi_context, CPSG_MyNCBIFactory* factory, string request)
+{
+    string url = factory->GetMyNCBIURL();
+    string proxy = factory->GetHttpProxy();
+    VLOG("curl_send_request: url - '", url, "' - '", request, "'");
+    CURL *handle = curl_easy_init();
+    //curl_easy_setopt(handle, CURLOPT_PROTOCOLS, CURLPROTO_HTTP);
+    //curl_easy_setopt(handle, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+    curl_easy_setopt(handle, CURLOPT_URL, url.c_str());
+    if (!proxy.empty()) {
+        curl_easy_setopt(handle, CURLOPT_PROXY, proxy.c_str());
+    }
+    if (factory->IsVerboseCURL()) {
+        curl_easy_setopt(handle, CURLOPT_VERBOSE, 1L);
+    }
+
+    /*multi_context->connect_to = curl_slist_append(nullptr, "::sman110:8306");
+    curl_easy_setopt(handle, CURLOPT_CONNECT_TO, multi_context->connect_to);*/
+
+    curl_easy_setopt(handle, CURLOPT_COPYPOSTFIELDS, request.c_str());
+    curl_easy_setopt(handle, CURLOPT_TIMEOUT_MS, factory->GetRequestTimeout().count());
+    curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, curl_write_data_cv);
+    curl_easy_setopt(handle, CURLOPT_WRITEDATA, multi_context);
+    curl_multi_add_handle(multi_context->curl_mhandle, handle);
+}
+
+unique_ptr<SCurlMultiContext> init_request(uv_loop_t * loop, TDataErrorCallback& error_cb)
+{
+    auto multi_context = make_unique<SCurlMultiContext>();
+    multi_context->uv_loop = loop;
+    multi_context->curl_mhandle = curl_multi_init();
+    if (!multi_context->curl_mhandle) {
+        error_cb(CRequestStatus::e500_InternalServerError, 0, eDiag_Critical, "Error creating curl multi handle");
+        return nullptr;
+    }
+    int rc = uv_timer_init(loop, &multi_context->uv_timer);
+    if (rc != 0) {
+        error_cb(CRequestStatus::e500_InternalServerError, rc, eDiag_Critical,
+            "Error initializing uv_timer: " + to_string(rc) + " - " + string(uv_strerror(rc)));
+        return nullptr;
+    }
+    auto curl_rc = curl_multi_setopt(multi_context->curl_mhandle, CURLMOPT_SOCKETFUNCTION, curl_handle_socket_cb);
+    if (curl_rc != CURLMcode::CURLM_OK) {
+        error_cb(CRequestStatus::e500_InternalServerError, rc, eDiag_Critical,
+            "Error setting up socket function: " + to_string(curl_rc) + " - " + string(curl_multi_strerror(curl_rc)));
+        return nullptr;
+    }
+    curl_rc = curl_multi_setopt(multi_context->curl_mhandle, CURLMOPT_SOCKETDATA, multi_context.get());
+    if (curl_rc != CURLMcode::CURLM_OK) {
+        error_cb(CRequestStatus::e500_InternalServerError, rc, eDiag_Critical,
+            "Error setting up socket data ptr: " + to_string(curl_rc) + " - " + string(curl_multi_strerror(curl_rc)));
+        return nullptr;
+    }
+    curl_rc = curl_multi_setopt(multi_context->curl_mhandle, CURLMOPT_TIMERFUNCTION, curl_start_timeout_cb);
+    if (curl_rc != CURLMcode::CURLM_OK) {
+        error_cb(CRequestStatus::e500_InternalServerError, rc, eDiag_Critical,
+            "Error setting up timer function: " + to_string(curl_rc) + " - " + string(curl_multi_strerror(curl_rc)));
+        return nullptr;
+    }
+    curl_rc = curl_multi_setopt(multi_context->curl_mhandle, CURLMOPT_TIMERDATA, multi_context.get());
+    if (curl_rc != CURLMcode::CURLM_OK) {
+        error_cb(CRequestStatus::e500_InternalServerError, rc, eDiag_Critical,
+            "Error setting up timer data ptr: " + to_string(curl_rc) + " - " + string(curl_multi_strerror(curl_rc)));
+        return nullptr;
+    }
+    return multi_context;
+}
+
+}  // namespace
+
+BEGIN_NCBI_SCOPE
+
+atomic_bool CPSG_MyNCBIFactory::sVerboseLogging{false};
+
+bool CPSG_MyNCBIFactory::InitGlobal(string &error)
+{
+    auto curl_code = curl_global_init(CURL_GLOBAL_ALL);
+    if (curl_code != CURLE_OK) {
+        error = "Cannot init cURL library: " + to_string(curl_code) + " - " + string(curl_easy_strerror(curl_code));
+        return false;
+    }
+    return true;
+}
+
+void CPSG_MyNCBIFactory::CleanupGlobal()
+{
+    curl_global_cleanup();
+}
+
+void CPSG_MyNCBIFactory::SetRequestTimeout(chrono::milliseconds timeout)
+{
+    m_RequestTimeout = timeout;
+}
+
+chrono::milliseconds CPSG_MyNCBIFactory::GetRequestTimeout() const
+{
+    return m_RequestTimeout;
+}
+
+void CPSG_MyNCBIFactory::SetMyNCBIURL(string url)
+{
+    m_MyNCBIUrl = url;
+}
+
+string CPSG_MyNCBIFactory::GetMyNCBIURL() const
+{
+    return m_MyNCBIUrl;
+}
+
+void CPSG_MyNCBIFactory::SetHttpProxy(string proxy)
+{
+    m_HttpProxy = proxy;
+}
+
+string CPSG_MyNCBIFactory::GetHttpProxy() const
+{
+    return m_HttpProxy;
+}
+
+void CPSG_MyNCBIFactory::SetVerboseCURL(bool value)
+{
+    sVerboseLogging = value;
+    m_VerboseCURL = value;
+}
+
+bool CPSG_MyNCBIFactory::IsVerboseCURL() const
+{
+    return m_VerboseCURL;
+}
+
+shared_ptr<CPSG_MyNCBIRequest_SignIn> CPSG_MyNCBIFactory::CreateSignIn(
+    uv_loop_t * loop, string username, string password,
+    TDataErrorCallback error_cb
+)
+{
+    auto multi_context = init_request(loop, error_cb);
+    if (!multi_context) {
+        return nullptr;
+    }
+    auto request = make_shared<CPSG_MyNCBIRequest_SignIn>(move(username), move(password));
+    request->SetErrorCallback(move(error_cb));
+    multi_context->request = request;
+    curl_send_request(multi_context.get(), this, request->GetRequestXML());
+    multi_context.release();
+    return request;
+}
+
+shared_ptr<CPSG_MyNCBIRequest_WhoAmI> CPSG_MyNCBIFactory::CreateWhoAmI(
+    uv_loop_t * loop, string cookie,
+    CPSG_MyNCBIRequest_WhoAmI::TConsumeCallback consume_cb,
+    TDataErrorCallback error_cb
+)
+{
+    auto multi_context = init_request(loop, error_cb);
+    if (!multi_context) {
+        return nullptr;
+    }
+    auto request = make_shared<CPSG_MyNCBIRequest_WhoAmI>(move(cookie));
+    request->SetErrorCallback(move(error_cb));
+    request->SetConsumeCallback(move(consume_cb));
+    multi_context->request = request;
+    curl_send_request(multi_context.get(), this, request->GetRequestXML());
+    multi_context.release();
+    return request;
+}
+
+END_NCBI_SCOPE
+
