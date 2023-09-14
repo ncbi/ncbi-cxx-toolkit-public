51bb49fcf6ac2622d143f2b15128bf7a5d7a501a
Eugene Vasilchenko <vasilche@ncbi.nlm.nih.gov>
2023-09-14 14:12:34 +0000

    Added timeout detection. Added error simulation. Added expiration support. JIRA: OMDM-56
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@100806 78c7ea69-d796-4a43-9a09-de51944f1b03

---
+++ src/sra/readers/sra/vdbcache.cpp
@@ -0,0 +1,163 @@
+/*  $Id$
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Authors:  Eugene Vasilchenko
+ *
+ * File Description:
+ *   Access to SRA files
+ *
+ */
+
+#include <ncbi_pch.hpp>
+#include <sra/readers/sra/vdbcache.hpp>
+
+
+BEGIN_NCBI_NAMESPACE;
+BEGIN_NAMESPACE(objects);
+
+
+CVDBCacheWithExpiration::CVDBCacheWithExpiration(size_t size_limit,
+                                         unsigned force_reopen_seconds,
+                                         unsigned rechech_seconds)
+    : m_CacheMap(size_limit),
+      m_ForceReopenSeconds(force_reopen_seconds),
+      m_RecheckSeconds(rechech_seconds)
+{
+}
+
+
+CVDBCacheWithExpiration::~CVDBCacheWithExpiration()
+{
+}
+
+
+void CVDBCacheWithExpiration::set_size_limit(size_t limit)
+{
+    CMutexGuard guard(GetCacheMutex());
+    m_CacheMap.set_size_limit(limit);
+}
+
+
+void CVDBCacheWithExpiration::SetForceReopenSeconds(unsigned seconds)
+{
+    m_ForceReopenSeconds = seconds;
+}
+
+
+void CVDBCacheWithExpiration::SetRecheckSeconds(unsigned seconds)
+{
+    m_RecheckSeconds = seconds;
+}
+
+
+CVDBCacheWithExpiration::CSlot::CSlot()
+{
+}
+
+
+CVDBCacheWithExpiration::CSlot::~CSlot()
+{
+}
+
+
+bool CVDBCacheWithExpiration::CSlot::IsExpired(const CVDBCacheWithExpiration& cache,
+                                               const string& acc_or_path) const
+{
+    return !m_ExpirationInfo || m_ExpirationInfo->IsExpired(cache, acc_or_path);
+}
+
+
+void CVDBCacheWithExpiration::CSlot::UpdateExpiration(const CVDBCacheWithExpiration& cache,
+                                                      const string& acc_or_path)
+{
+    m_ExpirationInfo = new CExpirationInfo(cache, acc_or_path);
+}
+
+
+CVDBCacheWithExpiration::CExpirationInfo::CExpirationInfo(const CVDBCacheWithExpiration& cache,
+                                                          const string& acc_or_path)
+    : m_ForceReopenDeadline(cache.m_ForceReopenSeconds),
+      m_RecheckDeadline(cache.m_RecheckSeconds),
+      m_DereferencedPath(cache.m_Mgr.FindDereferencedAccPath(acc_or_path)),
+      m_Timestamp(cache.m_Mgr.GetTimestamp(m_DereferencedPath))
+{
+    //LOG_POST("CVDBCacheWithExpiration: "<<acc_or_path<<" info: "
+    //         << m_DereferencedPath << ", " << m_Timestamp);
+}
+
+
+CVDBCacheWithExpiration::CExpirationInfo::~CExpirationInfo()
+{
+}
+
+
+bool CVDBCacheWithExpiration::CExpirationInfo::IsExpired(const CVDBCacheWithExpiration& cache,
+                                                         const string& acc_or_path) const
+{
+    if ( m_ForceReopenDeadline.IsExpired() ) {
+        // forced expiration
+        //LOG_POST("CVDBCacheWithExpiration: force reopen of "<<acc_or_path);
+        return true;
+    }
+    if ( !m_RecheckDeadline.IsExpired() ) {
+        // not re-checking yet
+        return false;
+    }
+    // check if the file has changed
+    string new_path = cache.m_Mgr.FindDereferencedAccPath(acc_or_path);
+    if ( new_path != m_DereferencedPath ) {
+        // actual file path has changed
+        //LOG_POST("CVDBCacheWithExpiration: path of "<<acc_or_path<<" has changed: "
+        //         << m_DereferencedPath << " -> " << new_path);
+        return true;
+    }
+    CTime new_timestamp = cache.m_Mgr.GetTimestamp(new_path);
+    if ( new_timestamp != m_Timestamp ) {
+        // file timestamp has changed
+        //LOG_POST("CVDBCacheWithExpiration: timestamp of "<<acc_or_path<<" has changed: "
+        //         << m_Timestamp << " -> " << new_timestamp);
+        return true;
+    }
+    // everything is the same, set deadline for the next re-check
+    //LOG_POST("CVDBCacheWithExpiration: "<<acc_or_path<<" is the same: "
+    //         << m_DereferencedPath << ", " << m_Timestamp);
+    m_RecheckDeadline = CDeadline(cache.m_RecheckSeconds);
+    return false;
+}
+
+
+CRef<CVDBCacheWithExpiration::CSlot> CVDBCacheWithExpiration::GetSlot(const string& acc_or_path)
+{
+    CMutexGuard guard(GetCacheMutex());
+    CRef<CSlot>& slot = m_CacheMap[acc_or_path];
+    if ( !slot ) {
+        slot = new CSlot();
+    }
+    return slot;
+}
+                  
+
+END_NAMESPACE(objects);
+END_NCBI_NAMESPACE;
