4297a90faa4e1762b3f37f108b3369a92694570e
Rafael Sadyrov <sadyrovr@ncbi.nlm.nih.gov>
2023-09-19 18:15:36 +0000

    Removed a class member. JIRA CXX-13223
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@100846 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/app/pubseq_gateway/client/processing.cpp
+++ src/app/pubseq_gateway/client/processing.cpp
@@ -731,24 +731,21 @@ void SDataOnlyCopy::Process(shared_ptr<CPSG_NamedAnnotInfo> named_annot_info)
 
 struct SNonVerbose
 {
-    SNonVerbose(SJsonOut& json_out) : m_JsonOut(json_out) {}
-
-    void ItemComplete(EPSG_Status status, const shared_ptr<CPSG_ReplyItem>& item);
-    void ReplyComplete(EPSG_Status status, const shared_ptr<CPSG_Reply>& reply);
+    void ItemComplete(SJsonOut& json_out, EPSG_Status status, const shared_ptr<CPSG_ReplyItem>& item);
+    void ReplyComplete(SJsonOut& json_out, EPSG_Status status, const shared_ptr<CPSG_Reply>& reply);
 
 private:
-    SJsonOut& m_JsonOut;
     map<CPSG_ReplyItem::EType, shared_ptr<CPSG_ReplyItem>> m_Items;
 };
 
-void SNonVerbose::ItemComplete(EPSG_Status status, const shared_ptr<CPSG_ReplyItem>& item)
+void SNonVerbose::ItemComplete(SJsonOut& json_out, EPSG_Status status, const shared_ptr<CPSG_ReplyItem>& item)
 {
     const auto type = item->GetType();
 
     if (status == EPSG_Status::eNotFound) {
         m_Items.try_emplace(type, item);
     } else {
-        m_JsonOut << CJsonResponse(status, item);
+        json_out << CJsonResponse(status, item);
 
         if (auto [it, result] = m_Items.try_emplace(type, nullptr); !result && it->second) {
             it->second.reset();
@@ -756,16 +753,16 @@ void SNonVerbose::ItemComplete(EPSG_Status status, const shared_ptr<CPSG_ReplyIt
     }
 }
 
-void SNonVerbose::ReplyComplete(EPSG_Status status, const shared_ptr<CPSG_Reply>& reply)
+void SNonVerbose::ReplyComplete(SJsonOut& json_out, EPSG_Status status, const shared_ptr<CPSG_Reply>& reply)
 {
     for (const auto& p : m_Items) {
         if (auto& item = p.second) {
-            m_JsonOut << CJsonResponse(EPSG_Status::eNotFound, item);
+            json_out << CJsonResponse(EPSG_Status::eNotFound, item);
         }
     }
 
     if (status != EPSG_Status::eSuccess) {
-        m_JsonOut << CJsonResponse(status, reply);
+        json_out << CJsonResponse(status, reply);
     }
 }
 
@@ -818,7 +815,7 @@ int CProcessing::OneRequest(const SOneRequestParams& params, shared_ptr<CPSG_Req
 
     CPSG_EventLoop queue;
     SJsonOut json_out;
-    SNonVerbose non_verbose(json_out);
+    SNonVerbose non_verbose;
 
     using namespace placeholders;
 
@@ -827,8 +824,8 @@ int CProcessing::OneRequest(const SOneRequestParams& params, shared_ptr<CPSG_Req
         auto reply_complete = bind(&SDataOnlyCopy::ReplyComplete, &data_only_copy, _1, _2);
         queue = CPSG_EventLoop(params.service, item_complete, reply_complete);
     } else if (!params.verbose) {
-        auto item_complete = bind(&SNonVerbose::ItemComplete, &non_verbose, _1, _2);
-        auto reply_complete = bind(&SNonVerbose::ReplyComplete, &non_verbose, _1, _2);
+        auto item_complete = bind(&SNonVerbose::ItemComplete, &non_verbose, ref(json_out), _1, _2);
+        auto reply_complete = bind(&SNonVerbose::ReplyComplete, &non_verbose, ref(json_out), _1, _2);
         queue = CPSG_EventLoop(params.service, item_complete, reply_complete);
     } else {
         auto item_complete = bind(&s_ItemComplete<SOneRequestParams>, ref(json_out), _1, _2);
