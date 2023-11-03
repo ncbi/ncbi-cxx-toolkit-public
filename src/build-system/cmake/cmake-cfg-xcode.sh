d300ce65e524ff9415d12395ccb092b42bd6a7ef
Aaron Ucko <ucko@ncbi.nlm.nih.gov>
2023-11-03 13:50:17 +0000

    cmake-cfg-xcode.sh: Formally fix bashisms ([ ... == ...]).
    They weren't a problem in practice, since this script is platform-specific.
    
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@101136 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/build-system/cmake/cmake-cfg-xcode.sh
+++ src/build-system/cmake/cmake-cfg-xcode.sh
@@ -257,12 +257,12 @@ CMAKE_ARGS="$CMAKE_ARGS -DBUILD_SHARED_LIBS=$BUILD_SHARED_LIBS"
 if [ -z "$BUILD_ROOT" ]; then
   if [ -z "$BUILD_TYPE" ]; then
     BUILD_ROOT=CMake-${CC_NAME}${CC_VERSION}
-    if [ "$BUILD_SHARED_LIBS" == "ON" ]; then
+    if [ "$BUILD_SHARED_LIBS" = "ON" ]; then
       BUILD_ROOT="$BUILD_ROOT"-DLL
     fi
   else
     BUILD_ROOT=CMake-${CC_NAME}${CC_VERSION}-${BUILD_TYPE}
-    if [ "$BUILD_SHARED_LIBS" == "ON" ]; then
+    if [ "$BUILD_SHARED_LIBS" = "ON" ]; then
       BUILD_ROOT="$BUILD_ROOT"DLL
     fi
   fi
