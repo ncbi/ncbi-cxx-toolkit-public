c3bb83e0099b7519ff7ff55e15e33f86e5cabaa8
ucko <ucko@ncbi.nlm.nih.gov>
2023-11-03 13:37:38 +0000

    test_sub_reg.sh: Check for Windows special cases without bashisms.
    Use case rather than [[ to check whether strings match wildcards.
    
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@101135 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/corelib/test/test_sub_reg.sh
+++ src/corelib/test/test_sub_reg.sh
@@ -16,14 +16,17 @@ done
 # Set environment variables with registry values (Windows version has capitalized value names)
 export NCBI_CONFIG_PATH=`dirname $0`/test_sub_reg_data
 unamestr=`uname -s`
-if [[ "$unamestr" == MINGW* ]] || [[ "$unamestr" == CYGWIN* ]]; then
+case "$unamestr" in
+  MINGW* | CYGWIN* )
     echo "Operating system is Windows"
-    if [[ $PWD == *GCC* ]];
-    then
+    case "$PWD" in
+      *GCC* )
         echo "compiler is GCC"
-    else
+        ;;
+      * )
         echo "compiler is MSVC"
-    fi
+        ;;
+    esac
     export NCBI_CONFIG_OVERRIDES=$NCBI_CONFIG_PATH/indirect_env.win.ini
     export NCBI_CONFIG_E__TEST=env
     export NCBI_CONFIG__TEST__E=env
@@ -36,7 +39,8 @@ if [[ "$unamestr" == MINGW* ]] || [[ "$unamestr" == CYGWIN* ]]; then
     $CHECK_EXEC test_sub_reg -defaults "$NCBI_CONFIG_PATH/defaults.ini" \
                              -overrides "$NCBI_CONFIG_PATH/overrides.win.ini" \
                              -out test_sub_reg_1strun.out.ini
-else
+    ;;
+  *)
     echo "Operating system is not Windows"
     export NCBI_CONFIG_OVERRIDES=$NCBI_CONFIG_PATH/indirect_env.ini
     export NCBI_CONFIG_e__TEST=env
@@ -50,19 +54,25 @@ else
     $CHECK_EXEC test_sub_reg -defaults "$NCBI_CONFIG_PATH/defaults.ini" \
                              -overrides "$NCBI_CONFIG_PATH/overrides.ini" \
                              -out test_sub_reg_1strun.out.ini
-fi
+  ;;
+esac
 
 echo "Comparing first run and expected output"
-if [[ "$unamestr" == MINGW* ]] || [[ "$unamestr" == CYGWIN* ]]; then
-    if [[ $PWD == *GCC* ]];
-    then
+case "$unamestr" in
+  MINGW* | CYGWIN* )
+    case "$PWD" in
+      *GCC* )
         diff $flags $NCBI_CONFIG_PATH/expected_win_gcc.ini test_sub_reg_1strun.out.ini
-    else
+        ;;
+      *)
         diff $flags $NCBI_CONFIG_PATH/expected_win_msvc.ini test_sub_reg_1strun.out.ini
-    fi
-else
+        ;;
+    esac
+    ;;
+  *)
     diff $flags $NCBI_CONFIG_PATH/expected.ini test_sub_reg_1strun.out.ini
-fi
+    ;;
+esac
 
 # Second run
 unset NCBI_CONFIG_PATH NCBI_CONFIG_OVERRIDES
@@ -72,15 +82,18 @@ else
     unset HOME # still need ., since executable is elsewhere
 fi
 
-if [[ "$unamestr" == MINGW* ]] || [[ "$unamestr" == CYGWIN* ]]; then
+case "$unamestr" in
+  MINGW* | CYGWIN* )
     unset NCBI_CONFIG_E__TEST NCBI_CONFIG__TEST__E NCBI_CONFIG__TEST__EX
     unset NCBI_CONFIG__ENVIRONMENT_DOT_INDIRECTENV__EX_IE NCBI_CONFIG__OVERRIDESBASE_DOT_ENVIRONMENT__OB_E NCBI_CONFIG__OVERRIDESBASE_DOT_ENVIRONMENT__OBX_E 
     unset NCBI_CONFIG__ENVIRONMENT_DOT_INDIRECTENV__E_IE NCBI_CONFIG__A_DOT_B__C_DOT_D
-else
+    ;;
+  * )
     unset NCBI_CONFIG_e__test NCBI_CONFIG__test__e NCBI_CONFIG__test__ex 
     unset NCBI_CONFIG__environment_DOT_indirectenv__ex_ie NCBI_CONFIG__overridesbase_DOT_environment__ob_e NCBI_CONFIG__overridesbase_DOT_environment__obx_e 
-    unset NCBI_CONFIG__environment_DOT_indirectenv__e_ie NCBI_CONFIG__a_DOT_b__c_DOT_d 
-fi
+    unset NCBI_CONFIG__environment_DOT_indirectenv__e_ie NCBI_CONFIG__a_DOT_b__c_DOT_d
+    ;;
+esac
 
 cp test_sub_reg_1strun.out.ini test_sub_reg.ini
 
