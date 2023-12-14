9e0d8051d27ec207a00f84d8f5b866e35f134e82
stakhovv <stakhovv@ncbi.nlm.nih.gov>
2023-12-14 14:44:29 +0000

    Obsolete pubmed_updater.hpp; JIRA:RW-2192
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@101446 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/objtools/edit/eutils_updater.hpp
+++ include/objtools/edit/eutils_updater.hpp
@@ -1,13 +1,75 @@
+/* $Id$
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Author: Vitaly Stakhovsky, NCBI
+ *
+ * File Description:
+ *   Pubmed Updater based on EUtils
+ *
+ * ===========================================================================
+ */
+
 #ifndef _EUTILS_UPDATER_HPP_
 #define _EUTILS_UPDATER_HPP_
 
 #include <objtools/eutils/api/eutils.hpp>
-#include <objtools/edit/pubmed_updater.hpp>
+#include <corelib/ncbiobj.hpp>
 
 BEGIN_NCBI_SCOPE
 BEGIN_SCOPE(objects)
+class CPub;
+class CCit_art;
 class CPubmed_entry;
 BEGIN_SCOPE(edit)
+
+enum class EPubmedError {
+    ok,
+    not_found,
+    operational_error,
+    citation_not_found,
+    citation_ambiguous,
+    cannot_connect_pmdb,
+    cannot_connect_searchbackend_pmdb,
+};
+
+NCBI_XOBJEDIT_EXPORT
+CNcbiOstream& operator<<(CNcbiOstream&, EPubmedError);
+
+struct NCBI_XOBJEDIT_EXPORT SCitMatch {
+    string Journal;
+    string Volume;
+    string Page;
+    string Year;
+    string Author;
+    string Issue;
+    string Title;
+    bool   InPress = false;
+    bool   Option1 = false;
+
+    void FillFromArticle(const CCit_art&);
+};
 
 using TPubInterceptor = std::function<void(CRef<CPub>&)>;
 
@@ -18,8 +80,6 @@ public:
 
 public:
     CEUtilsUpdater(ENormalize = ENormalize::Off);
-    NCBI_DEPRECATED CEUtilsUpdater(bool norm) :
-        CEUtilsUpdater(norm ? ENormalize::On : ENormalize::Off) {}
     virtual ~CEUtilsUpdater() {}
 
     virtual bool       Init() { return true; }
