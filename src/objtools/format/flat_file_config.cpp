f4c35fe7fb519d77345955ecc804918e16431d2e
Jonathan Kans <kans@ncbi.nlm.nih.gov>
2024-01-02 16:43:31 +0000

    JIRA:RW-2135 Updated -help and -xmlhelp to show 2097152 for geo_loc_name
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@101550 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/format/flat_file_config.cpp
+++ src/objtools/format/flat_file_config.cpp
@@ -601,7 +601,7 @@ void CFlatFileConfig::AddArgumentDescriptions(CArgDescriptions& args)
          DENT "DoNotUseAutoDef      (262144) - suppress automatic defline generator\n"
          DENT "OldTpaDisplay        (542288) - old TPA display with PRIMARY block\n"
          DENT "DisableDefaultIndex (1048576) - disable SeqEntry indexer\n"
-         DENT "GeoLocNameCountry   (1048576) - use geo_loc_name instead of country",
+         DENT "GeoLocNameCountry   (2097152) - use geo_loc_name instead of country",
 
                                  CArgDescriptions::eString, "");
 #undef DENT
