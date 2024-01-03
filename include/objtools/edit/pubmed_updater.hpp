1d845a5cfa6130c60531739abf9debd4af1b0624
stakhovv <stakhovv@ncbi.nlm.nih.gov>
2024-01-03 20:44:38 +0000

    Avoid using the C++23 #warning; JIRA:RW-2192
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@101560 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/objtools/edit/pubmed_updater.hpp
+++ include/objtools/edit/pubmed_updater.hpp
@@ -1,2 +1,2 @@
-#warning "<pubmed_updater.hpp> is obsolete. Use <eutils_updater.hpp> instead!"
+#pragma message("<pubmed_updater.hpp> is obsolete. Use <eutils_updater.hpp> instead!")
 #include "eutils_updater.hpp"
