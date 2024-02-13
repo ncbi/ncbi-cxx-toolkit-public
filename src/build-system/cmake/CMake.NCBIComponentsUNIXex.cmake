5db56d9050b609b27e78791e748c45cc98ac8508
gouriano <gouriano@ncbi.nlm.nih.gov>
2024-02-13 21:08:53 +0000

    Disable CMAKE_PACKAGE for the time being. JIRA: CXX-13435
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@101803 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/build-system/cmake/CMake.NCBIComponentsUNIXex.cmake
+++ src/build-system/cmake/CMake.NCBIComponentsUNIXex.cmake
@@ -828,7 +828,7 @@ NCBI_define_Xcomponent(NAME AWS_SDK LIB
     aws-cpp-sdk-sts aws-cpp-sdk-s3 aws-cpp-sdk-iam aws-cpp-sdk-cognito-identity
     aws-cpp-sdk-core aws-c-event-stream aws-checksums aws-c-common
     crypto ssl ADD_COMPONENT CURL
-    CMAKE_PACKAGE AWSSDK COMPONENTS s3
+#    CMAKE_PACKAGE AWSSDK COMPONENTS s3
 )
 if(NCBI_COMPONENT_AWS_SDK_FOUND AND NOT TARGET ZLIB::ZLIB)
     find_package(ZLIB REQUIRED)
