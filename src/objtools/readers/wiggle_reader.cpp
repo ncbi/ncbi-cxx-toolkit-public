ce92a9e8fb0339a8c67c0635f16a36fe0f6d6812
vasilche <vasilche@ncbi.nlm.nih.gov>
2024-02-29 18:48:36 +0000

    Use CByte_graph::TValues typedef. JIRA: OMDM-88
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@101903 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/readers/wiggle_reader.cpp
+++ src/objtools/readers/wiggle_reader.cpp
@@ -519,7 +519,7 @@ CRef<CSeq_graph> CWiggleReader::xMakeGraph()
     b_graph.SetMin(stat.AsByte(stat.m_Min));
     b_graph.SetMax(stat.AsByte(stat.m_Max));
     b_graph.SetAxis(0);
-    vector<char>& bytes = b_graph.SetValues();
+    CByte_graph::TValues& bytes = b_graph.SetValues();
 
     if ( m_Values.empty() ) {
         graph->SetNumval(0);
