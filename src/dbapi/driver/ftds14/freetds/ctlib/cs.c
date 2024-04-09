0b3ebf75326785781f0ad114476de42254325e80
Aaron Ucko <ucko@ncbi.nlm.nih.gov>
2024-04-09 14:52:07 +0000

    FreeTDS 1.4.x: Improve type handling, at least for Sybase.
    - _cs_convert: Treat CS_NVARCHAR_TYPE like CS_VAR*_TYPE.
    - odbc_sql2tds: Treat SYB5INT8 like other numeric types.
    - tds_generic_get: Handle non-blob types with four-byte sizes (e.g.,
      XSYBCHAR at least some of the time).
    - tds_generic_put_info: Send size 255 (0xff) for NULL short character
      types in output columns.
    
    JIRA: CXX-13176.  TRUNK-1.00: 554176.  PARTIAL.
    
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@102174 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/dbapi/driver/ftds14/freetds/ctlib/cs.c
+++ src/dbapi/driver/ftds14/freetds/ctlib/cs.c
@@ -517,7 +517,9 @@ _cs_convert(CS_CONTEXT * ctx, const CS_DATAFMT_COMMON * srcfmt, CS_VOID * srcdat
 	if (src_type == TDS_INVALID_TYPE)
 		return CS_FAIL;
 	src_len = srcfmt->maxlength;
-	if (datatype == CS_VARCHAR_TYPE || datatype == CS_VARBINARY_TYPE) {
+        if (datatype == CS_VARCHAR_TYPE ||
+            datatype == CS_NVARCHAR_TYPE ||
+            datatype == CS_VARBINARY_TYPE) {
 		CS_VARCHAR *vc = (CS_VARCHAR *) srcdata;
 		src_len = vc->len;
 		srcdata = vc->str;
@@ -528,7 +530,9 @@ _cs_convert(CS_CONTEXT * ctx, const CS_DATAFMT_COMMON * srcfmt, CS_VOID * srcdat
 	if (desttype == TDS_INVALID_TYPE)
 		return CS_FAIL;
 	destlen = destfmt->maxlength;
-	if (datatype == CS_VARCHAR_TYPE || datatype == CS_VARBINARY_TYPE) {
+        if (datatype == CS_VARCHAR_TYPE ||
+            datatype == CS_NVARCHAR_TYPE ||
+            datatype == CS_VARBINARY_TYPE) {
 		destvc = (CS_VARCHAR *) destdata;
 		destlen  = sizeof(destvc->str);
 		destdata = destvc->str;
