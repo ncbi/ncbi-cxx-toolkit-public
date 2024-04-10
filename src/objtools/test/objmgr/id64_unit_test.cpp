830068f485d69c7bbbb12a63de6d5482c123df37
vasilche <vasilche@ncbi.nlm.nih.gov>
2024-04-10 15:57:43 +0000

    Updated CDD count. JIRA: OMDM-106
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@102242 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/test/objmgr/id64_unit_test.cpp
+++ src/objtools/test/objmgr/id64_unit_test.cpp
@@ -1218,8 +1218,8 @@ BOOST_AUTO_TEST_CASE(CheckWGSProt1)
     BOOST_CHECK_EQUAL(user_count["DBLink"], 1);
     BOOST_CHECK_EQUAL(user_count["StructuredComment"], 1);
     BOOST_CHECK_EQUAL(total_count, 9);
-    BOOST_CHECK_EQUAL(CFeat_CI(bh).GetSize(), 14u);
-    BOOST_CHECK_EQUAL(CFeat_CI(bh, SAnnotSelector().SetResolveAll()).GetSize(), 14u);
+    BOOST_CHECK_EQUAL(CFeat_CI(bh).GetSize(), 13u);
+    BOOST_CHECK_EQUAL(CFeat_CI(bh, SAnnotSelector().SetResolveAll()).GetSize(), 13u);
     TSeqPos len = bh.GetBioseqLength();
     BOOST_CHECK(bh.GetSeqVector().CanGetRange(0, len));
     string data;
@@ -1271,8 +1271,8 @@ BOOST_AUTO_TEST_CASE(CheckWGSProt2)
     BOOST_CHECK_EQUAL(user_count["DBLink"], 1);
     BOOST_CHECK_EQUAL(user_count["StructuredComment"], 1);
     BOOST_CHECK_EQUAL(total_count, 9);
-    BOOST_CHECK_EQUAL(CFeat_CI(bh).GetSize(), 14u);
-    BOOST_CHECK_EQUAL(CFeat_CI(bh, SAnnotSelector().SetResolveAll()).GetSize(), 14u);
+    BOOST_CHECK_EQUAL(CFeat_CI(bh).GetSize(), 13u);
+    BOOST_CHECK_EQUAL(CFeat_CI(bh, SAnnotSelector().SetResolveAll()).GetSize(), 13u);
     TSeqPos len = bh.GetBioseqLength();
     BOOST_CHECK(bh.GetSeqVector().CanGetRange(0, len));
     string data;
