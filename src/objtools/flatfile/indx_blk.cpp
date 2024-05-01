052c3ba8b6c515259683973d90661460b399000e
bazhin <bazhin@ncbi.nlm.nih.gov>
2024-05-01 18:26:33 +0000

    JIRA SF-4393: Added support for DDBJ's "PG", "PH", "PI", "PJ" and "PK" patented accession prefixes.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@102408 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/flatfile/indx_blk.cpp
+++ src/objtools/flatfile/indx_blk.cpp
@@ -108,7 +108,7 @@ static const char* ddbj_accpref[] = {
     "FX", "FY", "FZ", "GA", "GB", "HT", "HU", "HV", "HW", "HX", "HY", "HZ",
     "LA", "LB", "LC", "LD", "LE", "LF", "LG", "LH", "LI", "LJ", "LU", "LV",
     "LX", "LY", "LZ", "MA", "MB", "MC", "MD", "ME", "OF", "OG", "OH", "OI",
-    "OJ", "PA", "PE", "PF", nullptr
+    "OJ", "PA", "PE", "PF", "PG", "PH", "PI", "PJ", "PK", nullptr
 };
 
 static const char* ncbi_accpref[] = {
@@ -1651,7 +1651,10 @@ static bool IsPatentedAccPrefix(const Parser& parseInfo, const char* acc)
              StringEquN(acc, "ME", 2) || StringEquN(acc, "OF", 2) ||
              StringEquN(acc, "OG", 2) || StringEquN(acc, "OI", 2) ||
              StringEquN(acc, "OJ", 2) || StringEquN(acc, "PA", 2) ||
-             StringEquN(acc, "PE", 2) || StringEquN(acc, "PF", 2)) &&
+             StringEquN(acc, "PE", 2) || StringEquN(acc, "PF", 2) ||
+             StringEquN(acc, "PG", 2) || StringEquN(acc, "PH", 2) ||
+             StringEquN(acc, "PI", 2) || StringEquN(acc, "PJ", 2) ||
+             StringEquN(acc, "PK", 2)) &&
             (parseInfo.all == true || parseInfo.source == Parser::ESource::DDBJ))
             return true;
 
