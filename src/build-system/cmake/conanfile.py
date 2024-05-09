34f3894d25f661afe5a28e2950a9ac15090aa99a
Andrei Gourianov <gouriano@ncbi.nlm.nih.gov>
2024-05-09 14:54:43 +0000

    Support optional requirements.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@102470 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/build-system/cmake/conanfile.py
+++ src/build-system/cmake/conanfile.py
@@ -11,6 +11,50 @@ class NCBIToolkitWithConanRecipe(ConanFile):
     def _min_cppstd(self):
         return 17
 
+    def requirements(self):
+        res = subprocess.run(["conan", "remote", "list"], 
+            stdout = subprocess.PIPE, universal_newlines = True, encoding="utf-8")
+        pos = res.stdout.find("ncbi.nlm.nih.gov")
+        NCBIfound = pos > 0
+        if NCBIfound:
+            print("NCBI artifactory is found")
+        else:
+            print("NCBI artifactory is not found")
+
+        if self.settings.os == "Linux":
+            self.requires("backward-cpp/1.6")
+        self.requires("boost/[>=1.82.0 <=1.84.0]")
+        self.requires("bzip2/1.0.8")
+        if self.settings.os == "Linux":
+            self.requires("cassandra-cpp-driver/[>=2.15.3 <=2.17.1]")
+        self.requires("giflib/5.2.1")
+        self.requires("grpc/1.50.1")
+        if self.settings.os == "Linux" or NCBIfound:
+            self.requires("libdb/5.3.28")
+        self.requires("libjpeg/9e")
+        self.requires("libnghttp2/[>=1.51.0 <=1.59.0]")
+        self.requires("libpng/[>=1.6.37 <=1.6.43]")
+        self.requires("libtiff/[>=4.3.0 <=4.5.0]")
+        if self.settings.os == "Linux":
+            self.requires("libunwind/[>=1.6.2 <=1.8.0]")
+        self.requires("libuv/[>=1.45.0 <=1.48.0]")
+        self.requires("libxml2/[>=2.11.4 <=2.11.6]")
+        self.requires("libxslt/[>1.1.34 <=1.1.37]")
+        self.requires("lmdb/[>=0.9.29 <=0.9.31]")
+        self.requires("lzo/2.10")
+        self.requires("openssl/1.1.1s")
+        # self.requires("opentelemetry-cpp/1.14.2")
+        self.requires("pcre/8.45")
+        self.requires("protobuf/3.21.12")
+        self.requires("sqlite3/[>=3.40.0 <=3.45.2]")
+        self.requires("zlib/[>=1.2.11 <2]")
+        self.requires("zstd/[>=1.5.2 <=1.5.5]")
+
+        self._optional_requires("ncbicrypt/20230516")
+        if self.settings.os == "Linux":
+            self._optional_requires("ncbi-fastcgi/2.4.2")
+        self._optional_requires("ncbi-vdb/[>=3.0.1 <=3.1.0]")
+
     def configure(self):
         self.options["abseil/*"].shared = False
         self.options["grpc/*"].shared = False
@@ -77,50 +121,17 @@ class NCBIToolkitWithConanRecipe(ConanFile):
         # self.options["opentelemetry-cpp"+_s].with_otlp_http = True
         self.options["opentelemetry-cpp"+_s].with_zipkin = False
 
-    def requirements(self):
-        res = subprocess.run(["conan", "remote", "list"], 
+    def _optional_requires(self, package):
+        pkg = package[:package.find("/")]
+        res = subprocess.run(["conan", 
+            "list" if conan_version.major > "1" else "search", pkg, "-r",
+            "*"    if conan_version.major > "1" else "all"], 
             stdout = subprocess.PIPE, universal_newlines = True, encoding="utf-8")
-        pos = res.stdout.find("ncbi.nlm.nih.gov")
-        NCBIfound = pos > 0
-        if NCBIfound:
-            print("NCBI artifactory is found")
+        pos = res.stdout.find(pkg+"/")
+        if pos > 0:
+            self.requires(package)
         else:
-            print("NCBI artifactory is not found")
-
-        if self.settings.os == "Linux":
-            self.requires("backward-cpp/1.6")
-        self.requires("boost/[>=1.82.0 <=1.84.0]")
-        self.requires("bzip2/1.0.8")
-        if self.settings.os == "Linux":
-            self.requires("cassandra-cpp-driver/[>=2.15.3 <=2.17.1]")
-        self.requires("giflib/5.2.1")
-        self.requires("grpc/1.50.1")
-        if self.settings.os == "Linux" or NCBIfound:
-            self.requires("libdb/5.3.28")
-        self.requires("libjpeg/9e")
-        self.requires("libnghttp2/[>=1.51.0 <=1.59.0]")
-        self.requires("libpng/[>=1.6.37 <=1.6.43]")
-        self.requires("libtiff/[>=4.3.0 <=4.5.0]")
-        if self.settings.os == "Linux":
-            self.requires("libunwind/[>=1.6.2 <=1.8.0]")
-        self.requires("libuv/[>=1.45.0 <=1.48.0]")
-        self.requires("libxml2/[>=2.11.4 <=2.11.6]")
-        self.requires("libxslt/[>1.1.34 <=1.1.37]")
-        self.requires("lmdb/[>=0.9.29 <=0.9.31]")
-        self.requires("lzo/2.10")
-        self.requires("openssl/1.1.1s")
-        # self.requires("opentelemetry-cpp/1.14.2")
-        self.requires("pcre/8.45")
-        self.requires("protobuf/3.21.12")
-        self.requires("sqlite3/[>=3.40.0 <=3.45.2]")
-        self.requires("zlib/[>=1.2.11 <2]")
-        self.requires("zstd/[>=1.5.2 <=1.5.5]")
-
-        if NCBIfound:
-            self.requires("ncbicrypt/20230516")
-            if self.settings.os == "Linux":
-                self.requires("ncbi-fastcgi/2.4.2")
-            self.requires("ncbi-vdb/[>=3.0.1 <=3.1.0]")
+            print("NOT FOUND optional package: " + package)
 
     def validate(self):
         if self.settings.compiler.cppstd:
