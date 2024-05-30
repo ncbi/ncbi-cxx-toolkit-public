066093a16953e8fdbf9fd6460719912ee77ef4f6
Jonathan Kans <kans@ncbi.nlm.nih.gov>
2024-05-30 15:00:13 +0000

    fixed additional Multi-platform C++ compilation warnings
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@102556 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/format/genbank_formatter.cpp
+++ src/objtools/format/genbank_formatter.cpp
@@ -1988,7 +1988,7 @@ void CGenbankFormatter::FormatSequence
         // normal case: print entire sequence, including all the N's in any gap.
         try {
             s_FormatRegularSequencePiece( seq, text_os, iter, total, base_count );
-        } catch (CSeqVectorException& e) {
+        } catch (CSeqVectorException&) {
         }
     } else {
         // special case: instead of showing the N's in a gap right away, we have the
@@ -2029,7 +2029,7 @@ void CGenbankFormatter::FormatSequence
                 TSeqPos fake_total = distance_until_next_significant_gap;
                 try {
                     s_FormatRegularSequencePiece( seq, text_os, iter, fake_total, base_count);
-                } catch (CSeqVectorException& e) {
+                } catch (CSeqVectorException&) {
                 }
                 const TSeqPos amount_to_subtract_from_total =
                     ( distance_until_next_significant_gap - fake_total );
