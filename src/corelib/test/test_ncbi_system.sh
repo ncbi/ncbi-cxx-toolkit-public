4b21c46d4add56411c9619d6520255cab533ea15
ivanov <ivanov@ncbi.nlm.nih.gov>
2024-05-31 15:48:34 +0000

    Updated exit codes for MSWIN/Cygwin. JIRA:CXX-13607
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@102562 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/corelib/test/test_ncbi_system.sh
+++ src/corelib/test/test_ncbi_system.sh
@@ -31,8 +31,8 @@ case "`uname -s`" in
    test $test_cpu -eq 255  -o  $test_cpu -eq 137  ||  exit 1
    ;;
  *CYGWIN* )
-   test $test_mem -eq 3  -o  $test_mem -eq 127  -o  $test_mem -eq 129  -o  $test_mem -eq 134  -o  $test_mem -eq 139  ||  exit 1
-   test $test_cpu -eq 3  -o  $test_cpu -eq 127  -o  $test_cpu -eq 129  -o  $test_cpu -eq 134  -o  $test_cpu -eq 139  ||  exit 1
+   test $test_mem -eq 0    -o  $test_mem -eq 134  ||  exit 1
+   test $test_cpu -eq 0    -o  $test_cpu -eq 134  ||  exit 1
    ;;
  * )
    test $test_mem -eq 255  ||  exit 1
