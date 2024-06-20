61debd5dd3d6e9a40300d4d354309101677c6185
ivanov <ivanov@ncbi.nlm.nih.gov>
2024-06-20 12:55:17 +0000

    Disable running memory and cpu limit tests under Valgrind. JIRA:CXX-13607
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@102647 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/corelib/test/test_ncbi_system.sh
+++ src/corelib/test/test_ncbi_system.sh
@@ -15,6 +15,9 @@ echo "exit_code(CPU-test) = $test_cpu"
 
 test $test_general -eq 0  ||  exit 1
 
+
+# exit code 88 from the test - if unsupported, run under memory sanitizer or Valgrind
+
 case "`uname -s`" in
  SunOS )
    # exit code 158 -- signal 30 (CPU time exceeded)
@@ -22,17 +25,16 @@ case "`uname -s`" in
    test $test_cpu -eq 255  -o  $test_cpu -eq 158  ||  exit 1
    ;;
  Linux )
-   # exit code 1 if run under GCC memory sanitizer
-   test $test_mem -eq 88   -o  $test_mem -eq 1    -o  $test_mem -eq 66  -o  $test_mem -eq 134  -o  $test_mem -eq 255  ||  exit 1
-   test $test_cpu -eq 255  -o  $test_cpu -eq 137  ||  exit 1
+   test $test_mem -eq 88   -o  $test_mem -eq 1    -o  $test_mem -eq 66   -o  $test_mem -eq 134  -o  $test_mem -eq 255  ||  exit 1
+   test $test_cpu -eq 88   -o  $test_mem -eq 1    -o  $test_mem -eq 137  -o  $test_cpu -eq 255  ||  exit 1
    ;;
  Darwin | FreeBSD )
    test $test_mem -eq 88   -o  $test_mem -eq 134  -o  $test_mem -eq 255  ||  exit 1
-   test $test_cpu -eq 255  -o  $test_cpu -eq 137  ||  exit 1
+   test $test_cpu -eq 255  -o  $test_cpu -eq 137  -o  $test_cpu -eq 255  ||  exit 1
    ;;
  *CYGWIN* )
-   test $test_mem -eq 88    -o  $test_mem -eq 134  ||  exit 1
-   test $test_cpu -eq 88    -o  $test_cpu -eq 134  ||  exit 1
+   test $test_mem -eq 88   -o  $test_mem -eq 134  ||  exit 1
+   test $test_cpu -eq 88   -o  $test_cpu -eq 134  ||  exit 1
    ;;
  * )
    test $test_mem -eq 255  ||  exit 1
