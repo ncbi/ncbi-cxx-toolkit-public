2fedf75632f09816e2b65e01187be87c9e6a5825
ucko <ucko@ncbi.nlm.nih.gov>
2024-07-29 17:29:24 +0000

    CMake.NCBIComponents.cmake: Fully switch FreeTDS default to 1.4.
    JIRA: CXX-13176.
    
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@102843 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/build-system/cmake/CMake.NCBIComponents.cmake
+++ src/build-system/cmake/CMake.NCBIComponents.cmake
@@ -243,7 +243,7 @@ set(NCBI_COMPONENT_Boost.Test.Included_NCBILIB test_boost)
 set(NCBI_COMPONENT_SQLITE3_NCBILIB sqlitewrapp)
 set(NCBI_COMPONENT_Sybase_NCBILIB  ncbi_xdbapi_ctlib)
 set(NCBI_COMPONENT_ODBC_NCBILIB    ncbi_xdbapi_odbc)
-set(NCBI_COMPONENT_FreeTDS_NCBILIB ct_ftds100 ncbi_xdbapi_ftds)
+set(NCBI_COMPONENT_FreeTDS_NCBILIB ct_ftds14 ncbi_xdbapi_ftds)
 set(NCBI_COMPONENT_connext_NCBILIB xconnext)
 
 #############################################################################
