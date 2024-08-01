0ccdaf7d5fcd432b8dbf4491690ab45455ee494d
Sergiy Gotvyanskyy <gotvyans@ncbi.nlm.nih.gov>
2024-08-01 14:19:51 +0000

    JIRA: CXX-65 Replaced all std::map with ct::const_map Fixed bugs in xMapNcRna(...) Made all protected static methods of CSoMap classless Replaced map with switch in FeatureToSoType Avoided use std::string when possible In coauthorship with Vladimir Ivanov
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@102865 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/objects/seq/so_map.hpp
+++ include/objects/seq/so_map.hpp
@@ -41,86 +41,17 @@ BEGIN_objects_SCOPE
 class CSeq_feat;
 
 //  ============================================================================
-class CompareNoCase
-//  ============================================================================
-{
-public:
-    bool operator()(const string& x, const string& y) const;
-};
-    
-//  ============================================================================
 class NCBI_SEQ_EXPORT CSoMap
 //  ============================================================================
 {
 public:
-    static std::string SoTypeToId(
-        const std::string&);
-    static std::string SoIdToType(
-        const std::string&);
+    static string SoTypeToId(std::string_view);
+    static string SoIdToType(std::string_view);
+    static bool SoTypeToFeature(std::string_view, CSeq_feat&, bool = false);
+    static bool FeatureToSoType(const CSeq_feat&, string&);
 
-    static bool SoTypeToFeature(
-        const string&,
-        CSeq_feat&,
-        bool =false);
-
-    static bool FeatureToSoType(
-        const CSeq_feat&,
-        string&);
-
-    static bool GetSupportedSoTerms(
-        vector<string>&);
-
-    static string ResolveSoAlias(
-        const string&);
-
-protected:
-    static bool xCompareNoCase(const string&, const string&);
-
-    static bool xFeatureMakeGene(const string&, CSeq_feat&);
-    static bool xFeatureMakeCds(const string&, CSeq_feat&);
-    static bool xFeatureMakeRna(const string&, CSeq_feat&);
-    static bool xFeatureMakeMiscFeature(const string&, CSeq_feat&);
-    static bool xFeatureMakeMiscRecomb(const string&, CSeq_feat&);
-    static bool xFeatureMakeMiscRna(const string&, CSeq_feat&);
-    static bool xFeatureMakeNcRna(const string&, CSeq_feat&);
-    static bool xFeatureMakeImp(const string&, CSeq_feat&);
-    static bool xFeatureMakeProt(const string&, CSeq_feat&);
-    static bool xFeatureMakeRegion(const string&, CSeq_feat&);
-    static bool xFeatureMakeRegulatory(const string&, CSeq_feat&);
-    static bool xFeatureMakeRepeatRegion(const string&, CSeq_feat&);
-
-    static bool xMapGeneric(const CSeq_feat&, string&);
-    static bool xMapGene(const CSeq_feat&, string&);
-    static bool xMapCds(const CSeq_feat&, string&);
-    static bool xMapMiscFeature(const CSeq_feat&, string&);
-    static bool xMapMiscRecomb(const CSeq_feat&, string&);
-    static bool xMapRna(const CSeq_feat&, string&);
-    static bool xMapNcRna(const CSeq_feat&, string&);
-    static bool xMapOtherRna(const CSeq_feat&, string&);
-    static bool xMapRegion(const CSeq_feat&, string&);
-    static bool xMapRegulatory(const CSeq_feat&, string&);
-    static bool xMapRepeatRegion(const CSeq_feat&, string&);
-    static bool xMapBond(const CSeq_feat&, string&);
-
-
-    using TYPEMAP = map<string, string, CompareNoCase>;
-    using TYPEENTRY = TYPEMAP::const_iterator;
-    static TYPEMAP mMapSoTypeToId;
-    static TYPEMAP mMapSoIdToType;
-
-    using FEATFUNC = bool(*)(const string&, CSeq_feat&);
-    using FEATFUNCMAP = map<string, FEATFUNC, CompareNoCase>;
-    using FEATFUNCENTRY = FEATFUNCMAP::const_iterator;
-    static FEATFUNCMAP mMapFeatFunc;
-
-    using TYPEFUNC = bool(*)(const CSeq_feat&, string&);
-    using TYPEFUNCMAP = map<CSeqFeatData::ESubtype, TYPEFUNC>;
-    using TYPEFUNCENTRY = TYPEFUNCMAP::const_iterator;
-    static TYPEFUNCMAP mMapTypeFunc;
-
-    using SOALIASMAP = map<string, string, CompareNoCase>;
-    using ALIASENTRY = SOALIASMAP::const_iterator;
-    static SOALIASMAP mMapSoAliases;
+    static bool GetSupportedSoTerms(vector<string>&);
+    static string ResolveSoAlias(std::string_view);
 };
 
 END_objects_SCOPE
