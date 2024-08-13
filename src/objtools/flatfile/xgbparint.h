293d33de7405d3b19917fcd6626f0f55d56b57b8
Vitaly Stakhovsky <stakhovv@ncbi.nlm.nih.gov>
2024-08-13 19:32:37 +0000

    flat2asn: Use string_view; JIRA:RW-1883
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@102974 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/flatfile/xgbparint.h
+++ src/objtools/flatfile/xgbparint.h
@@ -13,13 +13,13 @@ extern const Char* unkseqlitdbtag;
 typedef std::list<CRef<objects::CSeq_id>>  TSeqIdList;
 typedef std::list<CRef<objects::CSeq_loc>> TSeqLocList;
 
-typedef void (*X_gbparse_errfunc)(const Char*, const Char*);
+typedef void (*X_gbparse_errfunc)(string_view, string_view);
 typedef Int4 (*X_gbparse_rangefunc)(void*, const objects::CSeq_id& id);
 
 void xinstall_gbparse_error_handler(X_gbparse_errfunc new_func);
 void xinstall_gbparse_range_func(void* data, X_gbparse_rangefunc new_func);
 
-CRef<objects::CSeq_loc> xgbparseint_ver(const char* raw_intervals, bool& keep_rawPt, int& num_errsPt, const TSeqIdList& seq_ids, bool accver);
+CRef<objects::CSeq_loc> xgbparseint_ver(string_view raw_intervals, bool& keep_rawPt, int& num_errsPt, const TSeqIdList& seq_ids, bool accver);
 
 END_NCBI_SCOPE
 #endif // XGBPARINT_H
