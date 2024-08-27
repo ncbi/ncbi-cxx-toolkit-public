211b7ef8e8a09f645a925557e18c562d759efa46
Aaron Ucko <ucko@ncbi.nlm.nih.gov>
2024-08-27 15:22:23 +0000

    FTDS 1.4: BCP into computed columns again when allowing triggers.
    The destination view may well have an INSTEAD OF INSERT trigger that
    makes meaningful use of the supplied values.  JIRA: CXX-13720.
    
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@103044 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/dbapi/driver/ftds14/freetds/freetds/tds.h
+++ include/dbapi/driver/ftds14/freetds/freetds/tds.h
@@ -1752,6 +1752,7 @@ struct tds_bcpinfo
         TDS_INT next_col;
         TDS_INT blob_cols;
         TDS_INT rows_sent;
+        bool with_triggers;
 };
 
 TDSRET tds_bcp_init(TDSSOCKET *tds, TDSBCPINFO *bcpinfo);
