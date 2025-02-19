4e0eb67b1d49ce685734b21f8abaf2a9bd7dec78
Andrei Gourianov <gouriano@ncbi.nlm.nih.gov>
2025-02-19 15:02:15 +0000

    Use GCC 13.2.0. JIRA: CXX-12934
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@103963 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/build-system/cmake/toolchains/x86_64-linux-clang-1600.cmake
+++ src/build-system/cmake/toolchains/x86_64-linux-clang-1600.cmake
@@ -13,9 +13,9 @@ set(CMAKE_C_COMPILER "/usr/local/llvm/16.0.0/bin/clang")
 set(CMAKE_CXX_COMPILER "/usr/local/llvm/16.0.0/bin/clang++")
 
 set(CMAKE_C_FLAGS_INIT
-    "-isystem /opt/ncbi/gcc/12.2.0/include/c++/12.2.0 \
-    -isystem /opt/ncbi/gcc/12.2.0/include/c++/12.2.0/x86_64-redhat-linux-gnu  \
-    -isystem /opt/ncbi/gcc/12.2.0/include/c++/12.2.0/backward \
+    "-isystem /opt/ncbi/gcc/13.2.0/include/c++/13.2.0 \
+    -isystem /opt/ncbi/gcc/13.2.0/include/c++/13.2.0/x86_64-redhat-linux-gnu  \
+    -isystem /opt/ncbi/gcc/13.2.0/include/c++/13.2.0/backward \
     -gdwarf-4"
 )
 set(CMAKE_C_FLAGS_DEBUG   "-ggdb3 -O0")
@@ -23,9 +23,9 @@ set(CMAKE_C_FLAGS_RELEASE "-ggdb1 -O3")
 set(CMAKE_C_FLAGS_RELWITHDEBINFO  "-ggdb3 -O3")
 
 set(CMAKE_CXX_FLAGS_INIT
-    "-isystem /opt/ncbi/gcc/12.2.0/include/c++/12.2.0 \
-    -isystem /opt/ncbi/gcc/12.2.0/include/c++/12.2.0/x86_64-redhat-linux-gnu \
-    -isystem /opt/ncbi/gcc/12.2.0/include/c++/12.2.0/backward \
+    "-isystem /opt/ncbi/gcc/13.2.0/include/c++/13.2.0 \
+    -isystem /opt/ncbi/gcc/13.2.0/include/c++/13.2.0/x86_64-redhat-linux-gnu \
+    -isystem /opt/ncbi/gcc/13.2.0/include/c++/13.2.0/backward \
     -gdwarf-4"
 )
 set(CMAKE_CXX_FLAGS_DEBUG   "-ggdb3 -O0")
@@ -33,18 +33,18 @@ set(CMAKE_CXX_FLAGS_RELEASE "-ggdb1 -O3")
 set(CMAKE_CXX_FLAGS_RELWITHDEBINFO  "-ggdb3 -O3")
 
 set(CMAKE_EXE_LINKER_FLAGS_INIT
-    "-L/opt/ncbi/gcc/12.2.0/lib/gcc/x86_64-redhat-linux-gnu/12.2.0 \
-    -B/opt/ncbi/gcc/12.2.0/lib/gcc/x86_64-redhat-linux-gnu/12.2.0 \
-    -L/opt/ncbi/gcc/12.2.0/lib64 \
-    -Wl,-rpath,/opt/ncbi/gcc/12.2.0/lib64 \
+    "-L/opt/ncbi/gcc/13.2.0/lib/gcc/x86_64-redhat-linux-gnu/13.2.0 \
+    -B/opt/ncbi/gcc/13.2.0/lib/gcc/x86_64-redhat-linux-gnu/13.2.0 \
+    -L/opt/ncbi/gcc/13.2.0/lib64 \
+    -Wl,-rpath,/opt/ncbi/gcc/13.2.0/lib64 \
     -Wl,--enable-new-dtags"
 )
 
 set(CMAKE_SHARED_LINKER_FLAGS_INIT
-    "-L/opt/ncbi/gcc/12.2.0/lib/gcc/x86_64-redhat-linux-gnu/12.2.0 \
-    -B/opt/ncbi/gcc/12.2.0/lib/gcc/x86_64-redhat-linux-gnu/12.2.0 \
-    -L/opt/ncbi/gcc/12.2.0/lib64 \
-    -Wl,-rpath,/opt/ncbi/gcc/12.2.0/lib64 \
+    "-L/opt/ncbi/gcc/13.2.0/lib/gcc/x86_64-redhat-linux-gnu/13.2.0 \
+    -B/opt/ncbi/gcc/13.2.0/lib/gcc/x86_64-redhat-linux-gnu/13.2.0 \
+    -L/opt/ncbi/gcc/13.2.0/lib64 \
+    -Wl,-rpath,/opt/ncbi/gcc/13.2.0/lib64 \
     -Wl,--no-undefined"
 )
 
