c9696727c3145f8df96136978050a6c8cb8bda13
Aaron Ucko <ucko@ncbi.nlm.nih.gov>
2025-04-04 15:56:05 +0000

    ncbicxx_build_info.py: Fall back on sysconfig in the absence of distutils.
    JIRA: CXX-13995.
    
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@104314 78c7ea69-d796-4a43-9a09-de51944f1b03

--- scripts/common/impl/ncbicxx_build_info.py
+++ scripts/common/impl/ncbicxx_build_info.py
@@ -1,7 +1,10 @@
 # $Id$
 import ast
 from datetime import date, datetime, timedelta, timezone
-from distutils.sysconfig import parse_makefile, expand_makefile_vars
+try:
+    from distutils.sysconfig import parse_makefile, expand_makefile_vars
+except ImportError:
+    from sysconfig import _parse_makefile as parse_makefile, expand_makefile_vars
 import hashlib
 import mmap
 import os
