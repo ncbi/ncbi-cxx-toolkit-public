a5ea66f2102edd311f76fb72ed8c7e84659a7080
Christiam Camacho <camacho@ncbi.nlm.nih.gov>
2025-04-08 13:43:05 +0000

    Fix to previous commit JIRA SB-4373
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@104330 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/algo/blast/unit_tests/seqdb_reader/seqdb_unit_test.cpp
+++ src/algo/blast/unit_tests/seqdb_reader/seqdb_unit_test.cpp
@@ -4557,7 +4557,8 @@ BOOST_AUTO_TEST_CASE(TestTaxIdsLookup_v4)
     }
 }
 
-BOOST_AUTO_TEST_CASE_TIMEOUT(TestGiToOidTimeout, 15)
+BOOST_AUTO_TEST_CASE_TIMEOUT(TestGiToOidTimeout, 15);
+BOOST_AUTO_TEST_CASE(TestGiToOidTimeout)
 {
 	int pig=0;
 	const int gi = 1492455000;
