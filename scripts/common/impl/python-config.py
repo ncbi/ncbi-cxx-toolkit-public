b8866642c7865944208363a0b0d9f261bc54e9a4
Aaron Ucko <ucko@ncbi.nlm.nih.gov>
2025-05-01 15:31:17 +0000

    python-config.py: Favor sysconfig over old distutils.sysconfig.
    Python 3.12 dropped distutils from the standard library; some 3.12+
    installations have it grafted back on, but we shouldn't count on that.
    To that end, rely only on get_config_var and get_config_vars, and in
    particular NOT on get_python_inc.  Fall back to distutils.sysconfig if
    necessary to retain compatibility with ancient Python versions.
    JIRA: CXX-13780.
    
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@104485 78c7ea69-d796-4a43-9a09-de51944f1b03

--- scripts/common/impl/python-config.py
+++ scripts/common/impl/python-config.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python
 # $Id$
 
-from distutils import sysconfig
+import os
+try:
+    import sysconfig
+except ImportError:
+    from distutils import sysconfig
 import sys
 
 def lookup(want):
     if want == 'VERSION':
         return sysconfig.get_config_var('VERSION')
     elif want == 'INCLUDE':
-        return ('-I%s -I%s' % (sysconfig.get_python_inc(),
-                               sysconfig.get_python_inc(True)))
+        if os.name == "posix":
+            tail = os.path.join('include', 'python' + lookup('VERSION'))
+        else:
+            tail = 'include'
+        return ' '.join(['-I' + os.path.join(sysconfig.get_config_var(x), tail)
+                         for x in ('exec_prefix', 'prefix')])
     elif want == 'LIBPATH':
         return ' '.join(sysconfig.get_config_vars('LIBDIR', 'LIBPL'))
     elif want == 'DEPS':
