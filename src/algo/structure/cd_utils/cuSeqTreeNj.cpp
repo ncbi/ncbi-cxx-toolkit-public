c187fe0c3a73c44b47c9186a668c52ce7557c478
Christopher Lanczycki <lanczyck@ncbi.nlm.nih.gov>
2025-05-06 14:42:37 +0000

    provide initialization w/ parens to dyn allocated array JIRA CXX-5700
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@104506 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/algo/structure/cd_utils/cuSeqTreeNj.cpp
+++ src/algo/structure/cd_utils/cuSeqTreeNj.cpp
@@ -268,7 +268,7 @@ void NJ_TreeAlgorithm::ComputeTree(SeqTree* atree, pProgressFunction pFunc) {
     //  Map rows in distance matrix to index of corresponding node in m_seqiters;
     //  when there is no corresponding distance matrix row, enter USED_ROW.
 
-    int* indexMap = new int[m_nseqs]{};  
+    int* indexMap = new int[m_nseqs]();  
 
     m_nextNode = m_nseqs + 1;
 
