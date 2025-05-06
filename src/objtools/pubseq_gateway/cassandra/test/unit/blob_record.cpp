469c7ae9b095d213e0cae38114f534750f7c0ae7
Dmitrii Saprykin <saprykin@ncbi.nlm.nih.gov>
2025-05-06 14:51:41 +0000

    JIRA: ID-8643 Reverted addition of methods to access ID based suppress/withdrawn
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@104507 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/pubseq_gateway/cassandra/test/unit/blob_record.cpp
+++ src/objtools/pubseq_gateway/cassandra/test/unit/blob_record.cpp
@@ -42,6 +42,23 @@ namespace {
     USING_NCBI_SCOPE;
     USING_IDBLOB_SCOPE;
 
+    int16_t GetSybaseWithdrawn(CBlobRecord const& a)
+    {
+        static constexpr uint64_t withdrawn_offset = 7;
+        static constexpr uint64_t withdrawn_mask = 0b1111;
+        return static_cast<int16_t>((a.GetFlags() >> withdrawn_offset) & withdrawn_mask);
+    }
+
+    int16_t GetSybaseSuppress(CBlobRecord const& a)
+    {
+        static constexpr uint64_t suppress_offset = 11;
+        static constexpr uint64_t suppress_mask = 0b111'1110;
+        auto flags = static_cast<uint64_t>(a.GetFlags());
+        return
+            (flags & static_cast<TBlobFlagBase>(EBlobFlags::eSuppress) ? 1 : 0)
+            | static_cast<int16_t>(flags >> (suppress_offset - 1) & suppress_mask);
+    }
+
     TEST(BlobRecordTest, DataComparison) {
         {
             CBlobRecord a, b;
@@ -98,22 +115,22 @@ namespace {
         {
             CBlobRecord a;
             a.SetFlag(true, EBlobFlags::eSuppress);
-            EXPECT_EQ(1, a.GetIDSuppress());
+            EXPECT_EQ(1, GetSybaseSuppress(a));
             a.SetFlag(true, EBlobFlags::eSuppressTemporary);
-            EXPECT_EQ(5, a.GetIDSuppress());
+            EXPECT_EQ(5, GetSybaseSuppress(a));
             a.SetFlag(true, EBlobFlags::eSuppressEditBlocked);
-            EXPECT_EQ(7, a.GetIDSuppress());
+            EXPECT_EQ(7, GetSybaseSuppress(a));
         }
         {
             CBlobRecord a;
             a.SetFlag(true, EBlobFlags::eNoIncrementalProcessing);
-            EXPECT_EQ(16, a.GetIDSuppress());
+            EXPECT_EQ(16, GetSybaseSuppress(a));
         }
         {
             CBlobRecord a;
             a.SetFlags(4150);
-            EXPECT_EQ(5, a.GetIDSuppress());
-            EXPECT_EQ(0, a.GetIDWithdrawn());
+            EXPECT_EQ(5, GetSybaseSuppress(a));
+            EXPECT_EQ(0, GetSybaseWithdrawn(a));
         }
     }
 
@@ -121,41 +138,41 @@ namespace {
         {
             CBlobRecord a;
             a.SetFlag(true, EBlobFlags::eWithdrawnBase);
-            EXPECT_EQ(1, a.GetIDWithdrawn());
+            EXPECT_EQ(1, GetSybaseWithdrawn(a));
         }
         {
             CBlobRecord a;
             a.SetFlag(true, EBlobFlags::eWithdrawnPermanently);
-            EXPECT_EQ(2, a.GetIDWithdrawn());
+            EXPECT_EQ(2, GetSybaseWithdrawn(a));
         }
     }
 
     TEST(BlobRecordTest, FlagsExamplesFromProduction) {
         CBlobRecord a;
         a.SetFlags(4150);
-        EXPECT_EQ(5, a.GetIDSuppress());
-        EXPECT_EQ(0, a.GetIDWithdrawn());
+        EXPECT_EQ(5, GetSybaseSuppress(a));
+        EXPECT_EQ(0, GetSybaseWithdrawn(a));
 
         a.SetFlags(2082);
-        EXPECT_EQ(2, a.GetIDSuppress());
-        EXPECT_EQ(0, a.GetIDWithdrawn());
+        EXPECT_EQ(2, GetSybaseSuppress(a));
+        EXPECT_EQ(0, GetSybaseWithdrawn(a));
         EXPECT_TRUE(a.GetFlag(EBlobFlags::eDead));
 
         a.SetFlags(184);
-        EXPECT_EQ(1, a.GetIDSuppress());
-        EXPECT_EQ(1, a.GetIDWithdrawn());
+        EXPECT_EQ(1, GetSybaseSuppress(a));
+        EXPECT_EQ(1, GetSybaseWithdrawn(a));
         EXPECT_TRUE(a.GetFlag(EBlobFlags::eDead));
 
         a.SetFlags(16390);
-        EXPECT_EQ(16, a.GetIDSuppress());
-        EXPECT_EQ(0, a.GetIDWithdrawn());
+        EXPECT_EQ(16, GetSybaseSuppress(a));
+        EXPECT_EQ(0, GetSybaseWithdrawn(a));
         EXPECT_FALSE(a.GetFlag(EBlobFlags::eDead));
         EXPECT_TRUE(a.GetFlag(EBlobFlags::eNot4Gbu));
         EXPECT_TRUE(a.GetFlag(EBlobFlags::eGzip));
 
         a.SetFlags(20530);
-        EXPECT_EQ(21, a.GetIDSuppress());
-        EXPECT_EQ(0, a.GetIDWithdrawn());
+        EXPECT_EQ(21, GetSybaseSuppress(a));
+        EXPECT_EQ(0, GetSybaseWithdrawn(a));
         EXPECT_TRUE(a.GetFlag(EBlobFlags::eDead));
         EXPECT_FALSE(a.GetFlag(EBlobFlags::eNot4Gbu));
         EXPECT_TRUE(a.GetFlag(EBlobFlags::eGzip));
