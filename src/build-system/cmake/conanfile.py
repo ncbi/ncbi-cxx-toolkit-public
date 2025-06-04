048738155f19cf81666aaee0e8b8e0afb93efca6
Andrei Gourianov <gouriano@ncbi.nlm.nih.gov>
2025-06-04 15:20:48 +0000

    Updated fastcgi++ requirement. JIRA: CXX-14007
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@104646 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/build-system/cmake/conanfile.py
+++ src/build-system/cmake/conanfile.py
@@ -101,7 +101,7 @@ class NCBIToolkitWithConanRecipe(ConanFile):
         self._internal_requires("ncbicrypt/20230516")
         if self.settings.os == "Linux":
             self._internal_requires("ncbi-fastcgi/2.4.2")
-            self._internal_requires("ncbi-fastcgipp/3.1.0")
+            self._internal_requires("ncbi-fastcgipp/[>=3.1.0.4]")
             self._internal_requires("libcurl/8.8.0")
         self._internal_requires("ncbi-vdb/[>=3.0.1 <=3.2.1]")
 
