c4e9e5aca10c722de5eabec39444d847998c1efe
Jonathan Kans <kans@ncbi.nlm.nih.gov>
2025-08-06 18:54:54 +0000

    JIRA:RW-2531 Removed code where experiment suppresses model evidence in TWO versions of x_AddQualsExt
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@104982 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/format/feature_item.cpp
+++ src/objtools/format/feature_item.cpp
@@ -3437,12 +3437,6 @@ void CFeatureItem::x_AddQualsExt(
     if ( field.IsSetLabel()  &&  field.GetLabel().IsStr() ) {
         const string& oid = field.GetLabel().GetStr();
         if ( oid == "ModelEvidence" ) {
-            FOR_EACH_GBQUAL_ON_SEQFEAT (gbq_itr, m_Feat) {
-                const CGb_qual& gbq = **gbq_itr;
-                if (gbq.IsSetQual()) {
-                    if (NStr::Equal (gbq.GetQual(), "experiment")) return;
-                }
-            }
             x_AddQual(eFQ_modelev, new CFlatModelEvQVal(ext));
         } else if ( oid == "Process" || oid == "Component" || oid == "Function" ) {
             x_AddGoQuals(field);
@@ -3477,12 +3471,6 @@ void CFeatureItem::x_AddQualsExt(
     if ( ext.IsSetType()  &&  ext.GetType().IsStr() ) {
         const string& oid = ext.GetType().GetStr();
         if ( oid == "ModelEvidence" ) {
-            FOR_EACH_GBQUAL_ON_SEQFEAT (gbq_itr, m_Feat) {
-                const CGb_qual& gbq = **gbq_itr;
-                if (gbq.IsSetQual()) {
-                    if (NStr::Equal (gbq.GetQual(), "experiment")) return;
-                }
-            }
             x_AddQual(eFQ_modelev, new CFlatModelEvQVal(ext));
         } else if ( oid == "GeneOntology" ) {
             x_AddGoQuals(ext);
