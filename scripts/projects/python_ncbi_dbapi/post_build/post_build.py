75e087a9546f584d982c4dffad006010361ff3f4
Aaron Ucko <ucko@ncbi.nlm.nih.gov>
2025-08-07 18:59:20 +0000

    python_ncbi_dbapi post_build.py: Drop Python 3.9 for 1.27.0.
    Also, go ahead and cover 3.13 unconditionally now that CentOS 7 is no
    longer a concern.  JIRA: CXX-14125.
    
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@104991 78c7ea69-d796-4a43-9a09-de51944f1b03

--- scripts/projects/python_ncbi_dbapi/post_build/post_build.py
+++ scripts/projects/python_ncbi_dbapi/post_build/post_build.py
@@ -8,10 +8,7 @@ common_stems  = ('python_ncbi_dbapi', 'ncbi_xdbapi_*')
 unix_stems    = ('dbapi', 'dbapi_driver', 'ct_ftds*', 'tds_ftds*',
                  'xconne?t', 'xutil', 'xncbi')
 windows_stems = ('ncbi_core', 'ncbi_dbapi', 'ncbi_dbapi_driver')
-installations = ['3.9', '3.11']
-
-if os.path.isdir('/opt/python-3.13'):
-    installations.append('3.13')
+installations = ['3.11', '3.13']
 
 os.mkdir(install_dir + '/lib')
 if os.path.exists(lib_dir + '/python_ncbi_dbapi.so'):
