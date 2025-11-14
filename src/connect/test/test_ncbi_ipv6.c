c977e642e3644040f8cf786d927de0ad753bfe96
Anton Lavrentiev <lavr@ncbi.nlm.nih.gov>
2025-11-14 17:31:44 +0000

    Make IPv6 test a utility program as well
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@105414 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/connect/test/test_ncbi_ipv6.c
+++ src/connect/test/test_ncbi_ipv6.c
@@ -32,6 +32,7 @@
 
 #include "../ncbi_priv.h"
 #include <connect/ncbi_ipv6.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -44,65 +45,104 @@ int main(int argc, const char* argv[])
 {
     TNCBI_IPv6Addr addr, a, b, temp;
     unsigned int n, m;
-    const char *str;
+    const char* str;
     char buf[150];
+    int c;
 
-    g_NCBI_ConnectRandomSeed
-        = (unsigned int) time(0) ^ NCBI_CONNECT_SRAND_ADDEND;
-    srand(g_NCBI_ConnectRandomSeed);
-    if (rand() % 13) {
-        for (n = 0;  n < sizeof(addr.octet);  ++n)
-            addr.octet[n] = rand() & 0xFF;
-    } else
-        memset(&addr, 0, sizeof(addr));
-    if (rand() % 11)
-        m = rand() % (sizeof(addr.octet) * 8 + 1);
-    else
-        m = (rand() & 1) * 0xFF;
-    if (!NcbiAddrToString(buf, sizeof(buf), &addr))
-        assert(*buf == '\0');
-    printf("Address  = %s/%u\n", buf, m);
-    a = addr;
-    NcbiIPv6Subnet(&a,                          m);
-    if (!NcbiAddrToString(buf, sizeof(buf), &a))
-        assert(*buf == '\0');
-    printf("Subnet   = %s\n", buf);
-    b = addr;
-    NcbiIPv6Suffix(&b, sizeof(addr.octet) * 8 - m);
-    if (!NcbiAddrToString(buf, sizeof(buf), &b))
-        assert(*buf == '\0');
-    printf("Suffix   = %s\n", buf);
-    for (n = 0;  n < sizeof(addr.octet);  ++n) {
-        /* XOR here (instead of OR) to test that there's no overlap */
-        temp.octet[n] = a.octet[n] ^ b.octet[n];
+    assert(sizeof(addr) == sizeof(addr.octet));
+    if (argc != 2) {
+        printf("(internal self-test begins)\n");
+        g_NCBI_ConnectRandomSeed
+            = (unsigned int) time(0) ^ NCBI_CONNECT_SRAND_ADDEND;
+        srand(g_NCBI_ConnectRandomSeed);
+        if (rand() % 13) {
+            for (n = 0;  n < sizeof(addr.octet);  ++n)
+                addr.octet[n] = rand() & 0xFF;
+        } else
+            memset(&addr, 0, sizeof(addr));
+        if (rand() % 11)
+            m = rand() % (sizeof(addr.octet) * 8 + 1);
+        else
+            m = (rand() & 1) * 0xFF;
+        assert(NcbiAddrToString(buf, sizeof(buf), &addr)  ||  *buf == '\0');
+        printf("Address   = %s/%u\n", buf, m);
+        if (rand() & 1) {
+            char* p;
+            for (p = buf;  *p;  ++p) {
+                if (isalpha((unsigned char)(*p))) {
+                    assert('a' <= *p  &&  *p <= 'f');
+                    if (rand() & 1)
+                        *p = toupper((unsigned char)(*p));
+                } else
+                    assert(*p == ':'  ||  isdigit((unsigned char)(*p)));
+            }
+            printf("Reparsing = %s\n", buf);
+            assert(NcbiStringToAddr(&a, buf, 0));
+            assert(memcmp(&addr, &a, sizeof(addr)) == 0);
+        } else
+            a = addr;
+        NcbiIPv6Subnet(&a,                          m);
+        assert(NcbiAddrToString(buf, sizeof(buf), &a)  ||  *buf == '\0');
+        printf("Subnet    = %s\n", buf);
+        b = addr;
+        NcbiIPv6Suffix(&b, sizeof(addr.octet) * 8 - m);
+        assert(NcbiAddrToString(buf, sizeof(buf), &b)  ||  *buf == '\0');
+        printf("Suffix    = %s\n", buf);
+        for (n = 0;  n < sizeof(addr.octet);  ++n) {
+            /* XOR here (instead of OR) to test that there's no overlap */
+            temp.octet[n] = a.octet[n] ^ b.octet[n];
+        }
+        assert(NcbiAddrToString(buf, sizeof(buf), &temp)  ||  *buf == '\0');
+        printf("Combined  = %s\n", buf);
+        if (m > sizeof(addr.octet) * 8) {
+            /* Because "first" and "last" bit are the same in this case */
+            assert(memcmp(&a, &addr, sizeof(addr)) == 0);
+            assert(memcmp(&b, &addr, sizeof(addr)) == 0);
+            assert(NcbiIsEmptyIPv6(&temp));
+        } else {
+            if (!m)
+                assert(NcbiIsEmptyIPv6(&a));
+            else if (m == sizeof(addr.octet) * 8)
+                assert(NcbiIsEmptyIPv6(&b));
+            assert(memcmp(&temp, &addr, sizeof(addr)) == 0);
+        }
+        printf("(internal self-test ends)\n\n");
     }
-    if (!NcbiAddrToString(buf, sizeof(buf), &temp))
-        assert(*buf == '\0');
-    printf("Combined = %s\n", buf);
-    if (m > sizeof(addr.octet) * 8) {
-        /* Because "first" and "last" bit are the same in this case */
-        assert(memcmp(&a, &addr, sizeof(addr)) == 0);
-        assert(memcmp(&b, &addr, sizeof(addr)) == 0);
-        assert(NcbiIsEmptyIPv6(&temp));
-    } else
-        assert(memcmp(&temp, &addr, sizeof(addr)) == 0);
 
-    if (!(str = NcbiStringToAddr(&addr, argv[1], 0))) {
-        printf("\"%s\" is not a valid IPv6 address\n", argv[1]);
-        return 1;
+    n = 0;
+    for (c = 1;  c < argc;  ++c) {
+        int d, q;
+        if (!(str = NcbiStringToAddr(&addr, argv[c], 0))) {
+            printf("\"%s\" is not a valid IPv6 address\n", argv[c]);
+            ++n;
+            continue;
+        }
+        if (!NcbiAddrToString(buf, sizeof(buf), &addr)) {
+            printf("Cannot print IPv6 address for \"%s\"\n", argv[c]);
+            ++n;
+            continue;
+        }
+        printf("\"%.*s\" = %s\n", (int)(str - argv[c]), argv[c], buf);
+        if (*str)
+            printf("Unparsed part: \"%s\"\n", str);
+        if (sscanf(str, "/%u%n", &d, &q) >= 1  &&  !str[q]) {
+            m = d;
+            a = addr;
+            NcbiIPv6Subnet(&a,                          m);
+            assert(NcbiAddrToString(buf, sizeof(buf), &a)  ||  *buf == '\0');
+            printf("Subnet    = %s\n", buf);
+            b = addr;
+            NcbiIPv6Suffix(&b, sizeof(addr.octet) * 8 - m);
+            assert(NcbiAddrToString(buf, sizeof(buf), &b)  ||  *buf == '\0');
+            printf("Suffix    = %s\n", buf);
+        }
+        if (NcbiAddrToDNS(buf, sizeof(buf), &addr)) {
+            printf("Domain    = %s\n", buf);
+            str = NcbiStringToAddr(&temp, buf, 0);
+            assert(str  &&  !*str);
+            assert(memcmp(&temp, &addr, sizeof(addr)) == 0);
+        }
     }
-    if (!NcbiAddrToString(buf, sizeof(buf), &addr)) {
-        printf("Cannot print IPv6 address\n");
-        return 1;
-    }
-    printf("\"%.*s\" = %s\n", (int)(str - argv[1]), argv[1], buf);
-    if (*str)
-        printf("Unparsed part: \"%s\"\n", str);
-    if (NcbiAddrToDNS(buf, sizeof(buf), &addr)) {
-        printf("Domain = %s\n", buf);
-        str = NcbiStringToAddr(&temp, buf, 0);
-        assert(str  &&  !*str);
-        assert(memcmp(&temp, &addr, sizeof(addr)) == 0);
-    }
-    return 0;
+
+    return n ? 1/*failure*/ : 0/*success*/;
 }
