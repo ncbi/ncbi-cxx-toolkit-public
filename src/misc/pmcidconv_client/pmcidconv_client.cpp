7a4dbb22204583b9961de0b2308d3d35e4fc8a4e
Justin Foley <foleyjp@ncbi.nlm.nih.gov>
2025-11-19 19:33:59 +0000

    JIRA: RW-2596 Removed personal email address from pmcidconv_client source code
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@105434 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/misc/pmcidconv_client/pmcidconv_client.cpp
+++ src/misc/pmcidconv_client/pmcidconv_client.cpp
@@ -70,7 +70,7 @@ bool CPMCIDConverterServer::GetPmids(const vector<string>& uids)
 {
     string hostname = "www.ncbi.nlm.nih.gov";
     string path = "/pmc/utils/idconv/v1.0/";
-    string args = "tool=NCBIToolkit&email=bollin@ncbi.nlm.nih.gov&versions=no&";
+    string args = "tool=NCBIToolkit&versions=no&";
 
     m_Results.clear();
 
