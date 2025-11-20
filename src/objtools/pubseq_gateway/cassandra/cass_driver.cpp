4cd4b0285a198ae3775c47b21717b55a829bd3a5
Dmitrii Saprykin <saprykin@ncbi.nlm.nih.gov>
2025-11-20 15:40:10 +0000

    JIRA: CXX-14241 Added Datacenter name caching for CCassConnection (fixed potential data race)
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@105454 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/pubseq_gateway/cassandra/cass_driver.cpp
+++ src/objtools/pubseq_gateway/cassandra/cass_driver.cpp
@@ -500,7 +500,7 @@ void CCassConnection::CloseSession()
             cass_session_free(m_session);
         }
         m_session = nullptr;
-        m_datacenter.clear();
+        atomic_store(&m_datacenter, shared_ptr<string>());
     }
 }
 
@@ -556,7 +556,7 @@ void CCassConnection::Close()
     if (m_cluster) {
         cass_cluster_free(m_cluster);
         m_cluster = nullptr;
-        m_datacenter.clear();
+        atomic_store(&m_datacenter, shared_ptr<string>());
     }
 }
 
@@ -886,15 +886,21 @@ vector<string> CCassConnection::GetLocalPeersAddressList(string const & datacent
 string CCassConnection::GetDatacenterName()
 {
     if (IsConnected()) {
-        if (m_datacenter.empty()) {
+        auto datacenter = atomic_load(&m_datacenter);
+        if (datacenter == nullptr || datacenter->empty()) {
             auto query = NewQuery();
             query->SetSQL("SELECT data_center FROM system.local", 0);
             query->Query(CCassConsistency::kLocalOne, false, false);
             if (query->NextRow() == ar_dataready) {
-                m_datacenter = query->FieldGetStrValue(0);
+                string dc = query->FieldGetStrValue(0);
+                datacenter = make_shared<string>(dc);
+                atomic_store(&m_datacenter, std::move(datacenter));
+                return dc;
             }
         }
-        return m_datacenter;
+        else {
+            return *datacenter;
+        }
     }
     return "";
 }
