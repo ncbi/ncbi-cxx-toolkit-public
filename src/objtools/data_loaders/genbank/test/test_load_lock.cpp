0d1495541eb004a6676c0d85eb02351ea47cc193
Eugene Vasilchenko <vasilche@ncbi.nlm.nih.gov>
2025-12-16 17:14:33 +0000

    Avoid mis-synchronization under high CPU load. JIRA: OMDM-170
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@105583 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/data_loaders/genbank/test/test_load_lock.cpp
+++ src/objtools/data_loaders/genbank/test/test_load_lock.cpp
@@ -62,6 +62,7 @@
 #ifdef NCBI_THREADS
 # include <thread>
 # include <future>
+# include <condition_variable>
 #endif // NCBI_THREADS
 
 #include <corelib/test_boost.hpp>
@@ -71,11 +72,26 @@
 USING_NCBI_SCOPE;
 USING_SCOPE(objects);
 
-static string Log(CStopWatch& sw)
+static CStopWatch run_sw;
+
+static void reset_Log()
+{
+    run_sw.Restart();
+}
+static string Log()
 {
     ostringstream str;
-    str << fixed << setprecision(3) << sw.Elapsed() << " T"<<CThread::GetSelf()<<": ";
+    str << fixed << setprecision(3) << run_sw.Elapsed() << " T"<<CThread::GetSelf()<<": ";
     return str.str();
+}
+
+static void SimulateDelay(unsigned ms)
+{
+    // sleep for 'ms' milliseconds, probably more by factor of up to 3
+    double factor = 1+rand()%2000*1e-3;
+    ms = unsigned(ms*factor);
+    LOG_POST(Log()<<"Sleeping for "<<ms<<" milliseconds");
+    SleepMilliSec(ms);
 }
 
 BOOST_AUTO_TEST_CASE(LoadLock)
@@ -89,130 +105,179 @@ BOOST_AUTO_TEST_CASE(LoadLock)
     BOOST_REQUIRE(bh1);
     CDataSource& ds = bh1.x_GetInfo().GetDataSource();
 
-    CStopWatch sw(CStopWatch::eStart);
+    reset_Log();
 
     {{
-        LOG_POST(Log(sw)<<"Starting sequential test");
+        LOG_POST(Log()<<"Starting sequential test");
         // sequential
         int sat = -1;
         int sat_key = 1;
     
         {{
             auto blob_id = gbloader->GetBlobIdFromSatSatKey(sat, sat_key);
-            LOG_POST(Log(sw)<<"Calling GetLoadedTSE_Lock(1)");
+            LOG_POST(Log()<<"Calling GetLoadedTSE_Lock(1)");
             auto tse_lock = ds.GetLoadedTSE_Lock(blob_id, CTimeout(1));
-            LOG_POST(Log(sw)<<"Returned GetLoadedTSE_Lock(1)");
+            LOG_POST(Log()<<"Returned GetLoadedTSE_Lock(1)");
             BOOST_CHECK(!tse_lock);
         }}
     
         {{
             auto blob_id = gbloader->GetBlobIdFromSatSatKey(sat, sat_key);
-            LOG_POST(Log(sw)<<"Calling GetTSE_LoadLock()");
+            LOG_POST(Log()<<"Calling GetTSE_LoadLock()");
             auto load_lock = ds.GetTSE_LoadLock(blob_id);
-            LOG_POST(Log(sw)<<"Returned GetTSE_LoadLock()");
+            LOG_POST(Log()<<"Returned GetTSE_LoadLock()");
             BOOST_REQUIRE(load_lock);
             CRef<CSeq_entry> entry(new CSeq_entry);
             entry->SetSet().SetSeq_set();
             load_lock->SetSeq_entry(*entry);
-            LOG_POST(Log(sw)<<"Calling SetLoaded()");
+            LOG_POST(Log()<<"Calling SetLoaded()");
             load_lock.SetLoaded();
-            LOG_POST(Log(sw)<<"Returned SetLoaded()");
+            LOG_POST(Log()<<"Returned SetLoaded()");
         }}
 
         {{
             auto blob_id = gbloader->GetBlobIdFromSatSatKey(sat, sat_key);
-            LOG_POST(Log(sw)<<"Calling GetTSE_LoadLockIfLoaded()");
+            LOG_POST(Log()<<"Calling GetTSE_LoadLockIfLoaded()");
             auto load_lock = ds.GetTSE_LoadLockIfLoaded(blob_id);
-            LOG_POST(Log(sw)<<"Returned GetTSE_LoadLockIfLoaded()");
+            LOG_POST(Log()<<"Returned GetTSE_LoadLockIfLoaded()");
             BOOST_CHECK(load_lock);
         }}
     
         {{
             auto blob_id = gbloader->GetBlobIdFromSatSatKey(sat, sat_key);
-            LOG_POST(Log(sw)<<"Calling GetLoadedTSE_Lock(1)");
+            LOG_POST(Log()<<"Calling GetLoadedTSE_Lock(1)");
             auto tse_lock = ds.GetLoadedTSE_Lock(blob_id, CTimeout(1));
-            LOG_POST(Log(sw)<<"Returned GetLoadedTSE_Lock(1)");
+            LOG_POST(Log()<<"Returned GetLoadedTSE_Lock(1)");
             BOOST_CHECK(tse_lock);
         }}
-        LOG_POST(Log(sw)<<"Finished sequential test");
+        LOG_POST(Log()<<"Finished sequential test");
     }}
 
-    sw.Restart();
+    reset_Log();
     {{
-        LOG_POST(Log(sw)<<"Starting parallel test");
+        LOG_POST(Log()<<"Starting parallel test");
         // parallel
+        atomic<bool> all_done_before_SetLoaded{false};
+        mutex mutex_all_done_before_SetLoaded;
+        condition_variable cv_all_done_before_SetLoaded;
+        
+        enum SetLoadedState {
+            before_Setloaded,
+            during_SetLoaded,
+            after_SetLoaded
+        };
+        atomic<SetLoadedState> SetLoaded_state{before_Setloaded};
+        mutex mutex_SetLoaded_state;
+        condition_variable cv_SetLoaded_state;
+
+        function<void(SetLoadedState)> set_SetLoaded_state = [&](SetLoadedState s){
+            {{
+                unique_lock lock(mutex_SetLoaded_state);
+                SetLoaded_state = s;
+            }}
+            cv_SetLoaded_state.notify_all();
+        };
+        function<void(SetLoadedState)> wait_for_SetLoaded_state = [&](SetLoadedState s){
+            if ( SetLoaded_state >= s ) {
+                return;
+            }
+            LOG_POST(Log()<<"Waiting for SetLoaded_state");
+            unique_lock lock(mutex_SetLoaded_state);
+            cv_SetLoaded_state.wait(lock, [&](){return SetLoaded_state >= s;});
+        };
+        
         int sat = -1;
         int sat_key = 2;
         thread t1 = thread([&]
             {
                 auto blob_id = gbloader->GetBlobIdFromSatSatKey(sat, sat_key);
-                LOG_POST(Log(sw)<<"Waiting before getting TSE load lock");
-                SleepMilliSec(1000);
-                LOG_POST(Log(sw)<<"Calling GetTSE_LoadLock()");
+                LOG_POST(Log()<<"Waiting before getting TSE load lock");
+                SimulateDelay(1000);
+                LOG_POST(Log()<<"Calling GetTSE_LoadLock()");
                 auto load_lock = ds.GetTSE_LoadLock(blob_id);
-                LOG_POST(Log(sw)<<"Returned GetTSE_LoadLock()");
+                LOG_POST(Log()<<"Returned GetTSE_LoadLock()");
                 BOOST_REQUIRE_MT_SAFE(load_lock);
-                SleepMilliSec(1000);
+                SimulateDelay(1000);
+                LOG_POST(Log()<<"Creating Seq-entry");
                 CRef<CSeq_entry> entry(new CSeq_entry);
                 entry->SetSet().SetSeq_set();
                 load_lock->SetSeq_entry(*entry);
-                LOG_POST(Log(sw)<<"Calling SetLoaded()");
+                if ( !all_done_before_SetLoaded ) {
+                    LOG_POST(Log()<<"Waiting for all_done_before_SetLoaded");
+                    unique_lock lock(mutex_all_done_before_SetLoaded);
+                    cv_all_done_before_SetLoaded.wait(lock, [&]()->bool{
+                        return all_done_before_SetLoaded;
+                    });
+                }
+                LOG_POST(Log()<<"Calling SetLoaded()");
+                set_SetLoaded_state(during_SetLoaded);
                 load_lock.SetLoaded();
-                LOG_POST(Log(sw)<<"Returned SetLoaded()");
+                set_SetLoaded_state(after_SetLoaded);
+                LOG_POST(Log()<<"Returned SetLoaded()");
             });
         thread t2 = thread([&]
             {
                 auto blob_id = gbloader->GetBlobIdFromSatSatKey(sat, sat_key);
                 {{
-                    LOG_POST(Log(sw)<<"Calling GetTSE_LoadLockIfLoaded()");
+                    LOG_POST(Log()<<"Calling GetTSE_LoadLockIfLoaded()");
                     auto load_lock = ds.GetTSE_LoadLockIfLoaded(blob_id);
-                    LOG_POST(Log(sw)<<"Returned GetTSE_LoadLockIfLoaded()");
+                    LOG_POST(Log()<<"Returned GetTSE_LoadLockIfLoaded()");
                     BOOST_CHECK_MT_SAFE(!load_lock);
                 }}
-                SleepMilliSec(1000);
+                SimulateDelay(1000);
                 {{
-                    LOG_POST(Log(sw)<<"Calling GetTSE_LoadLockIfLoaded()");
+                    LOG_POST(Log()<<"Calling GetTSE_LoadLockIfLoaded()");
                     auto load_lock = ds.GetTSE_LoadLockIfLoaded(blob_id);
-                    LOG_POST(Log(sw)<<"Returned GetTSE_LoadLockIfLoaded()");
+                    LOG_POST(Log()<<"Returned GetTSE_LoadLockIfLoaded()");
                     BOOST_CHECK_MT_SAFE(!load_lock);
                 }}
-                SleepMilliSec(500);
+                SimulateDelay(500);
                 {{
-                    LOG_POST(Log(sw)<<"Calling GetTSE_LoadLockIfLoaded()");
+                    LOG_POST(Log()<<"Calling GetTSE_LoadLockIfLoaded()");
                     auto load_lock = ds.GetTSE_LoadLockIfLoaded(blob_id);
-                    LOG_POST(Log(sw)<<"Returned GetTSE_LoadLockIfLoaded()");
+                    LOG_POST(Log()<<"Returned GetTSE_LoadLockIfLoaded()");
                     BOOST_CHECK_MT_SAFE(!load_lock);
                 }}
-                SleepMilliSec(1000);
+
                 {{
-                    LOG_POST(Log(sw)<<"Calling GetTSE_LoadLockIfLoaded()");
+                    unique_lock lock(mutex_all_done_before_SetLoaded);
+                    all_done_before_SetLoaded = true;
+                }}
+                cv_all_done_before_SetLoaded.notify_all();
+                
+                SimulateDelay(1000);
+                wait_for_SetLoaded_state(after_SetLoaded);
+                {{
+                    LOG_POST(Log()<<"Calling GetTSE_LoadLockIfLoaded()");
                     auto load_lock = ds.GetTSE_LoadLockIfLoaded(blob_id);
-                    LOG_POST(Log(sw)<<"Returned GetTSE_LoadLockIfLoaded()");
+                    LOG_POST(Log()<<"Returned GetTSE_LoadLockIfLoaded()");
                     BOOST_CHECK_MT_SAFE(load_lock);
                 }}
             });
         thread t3 = thread([&]
             {
                 auto blob_id = gbloader->GetBlobIdFromSatSatKey(sat, sat_key);
-                LOG_POST(Log(sw)<<"Calling GetLoadedTSE_Lock(3)");
-                auto tse_lock = ds.GetLoadedTSE_Lock(blob_id, CTimeout(3));
-                LOG_POST(Log(sw)<<"Returned GetLoadedTSE_Lock(3)");
+                double timeout = 10;
+                LOG_POST(Log()<<"Calling GetLoadedTSE_Lock("<<timeout<<")");
+                auto tse_lock = ds.GetLoadedTSE_Lock(blob_id, CTimeout(timeout));
+                LOG_POST(Log()<<"Returned GetLoadedTSE_Lock("<<timeout<<")");
                 BOOST_CHECK_MT_SAFE(tse_lock);
             });
         thread t4 = thread([&]
             {
                 auto blob_id = gbloader->GetBlobIdFromSatSatKey(sat, sat_key);
-                LOG_POST(Log(sw)<<"Waiting before calling GetLoadedTSE_Lock(2)");
-                SleepMilliSec(1500);
-                LOG_POST(Log(sw)<<"Calling GetLoadedTSE_Lock(2)");
-                auto tse_lock = ds.GetLoadedTSE_Lock(blob_id, CTimeout(2));
-                LOG_POST(Log(sw)<<"Returned GetLoadedTSE_Lock(2)");
+                double timeout = 8;
+                LOG_POST(Log()<<"Waiting before calling GetLoadedTSE_Lock("<<timeout<<")");
+                SimulateDelay(1500);
+                LOG_POST(Log()<<"Calling GetLoadedTSE_Lock("<<timeout<<")");
+                auto tse_lock = ds.GetLoadedTSE_Lock(blob_id, CTimeout(timeout));
+                LOG_POST(Log()<<"Returned GetLoadedTSE_Lock("<<timeout<<")");
                 BOOST_CHECK_MT_SAFE(tse_lock);
             });
         t1.join();
         t2.join();
         t3.join();
         t4.join();
-        LOG_POST(Log(sw)<<"Finished parallel test");
+        LOG_POST(Log()<<"Finished parallel test");
     }}
 }
