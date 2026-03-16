4968a2bc3a311d2c1a966c72b4a19ad6a3e4a03d
Aleksey Grichenko <grichenk@ncbi.nlm.nih.gov>
2026-03-16 18:17:24 +0000

    Removed obsolete workarounds. JIRA CXX-14331
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@106035 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/corelib/ncbistr.hpp
+++ include/corelib/ncbistr.hpp
@@ -5016,31 +5016,6 @@ bool AStrEquiv(const Arg1& x, const Arg2& y, Pred pr)
 
 
 /////////////////////////////////////////////////////////////////////////////
-//  CNcbiEmptyString::
-//
-#if !defined(NCBI_OS_MSWIN)  &&  \
-    !(defined(NCBI_OS_LINUX)  &&  \
-      (defined(NCBI_COMPILER_GCC)  ||  defined(NCBI_COMPILER_ANY_CLANG)))
-inline
-const string& CNcbiEmptyString::Get(void)
-{
-    const string* str = m_Str;
-    return str ? *str: FirstGet();
-}
-
-#  ifdef HAVE_WSTRING
-inline
-const wstring& CNcbiEmptyWString::Get(void)
-{
-    const wstring* str = m_Str;
-    return str ? *str: FirstGet();
-}
-#  endif
-#endif
-
-
-
-/////////////////////////////////////////////////////////////////////////////
 //  NStr::
 //
 
