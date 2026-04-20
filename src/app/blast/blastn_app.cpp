ce7c66b3ba7dee7ce98a747f9943beb20ce87ea7
Amelia Fong <fongah2@ncbi.nlm.nih.gov>
2026-04-20 14:30:25 +0000

    Add warning for max seq length, JIRA:SB-4602
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@106422 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/app/blast/blastn_app.cpp
+++ src/app/blast/blastn_app.cpp
@@ -277,6 +277,7 @@ int CBlastnApp::x_RunMTBySplitDB()
         for (; !input.End(); formatter.ResetScopeHistory(), QueryBatchCleanup() ) {
 	    BLAST_PROF_START( APP.LOOP.PRE );
             CRef<CBlastQueryVector> query_batch(input.GetNextSeqBatch(*scope));
+            if (query_batch->Empty())  continue;
             CRef<IQueryFactory> queries(new CObjMgr_QueryFactory(*query_batch));
 
             SaveSearchStrategy(args, m_CmdLineArgs, queries, m_OptsHndl);
