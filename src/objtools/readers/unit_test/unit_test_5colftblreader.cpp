de9c7a6a466c0040e591b0a6d95d7ed9b38e6bef
Justin Foley <foleyjp@ncbi.nlm.nih.gov>
2026-05-18 15:24:47 +0000

    JIRA: RW-2238 Modified feature-table reader so that filtering is not applied to bad features. Added a test case. AUTHOR: foleyjp MERGE_REQUEST: 617 1c4e897e86778f60c0bf59c13483fc93ff42827e
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@106627 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/readers/unit_test/unit_test_5colftblreader.cpp
+++ src/objtools/readers/unit_test/unit_test_5colftblreader.cpp
@@ -1,36 +1,36 @@
 /*  $Id$
-* ===========================================================================
-*
-*                            PUBLIC DOMAIN NOTICE
-*               National Center for Biotechnology Information
-*
-*  This software/database is a "United States Government Work" under the
-*  terms of the United States Copyright Act.  It was written as part of
-*  the author's official duties as a United States Government employee and
-*  thus cannot be copyrighted.  This software/database is freely available
-*  to the public for use. The National Library of Medicine and the U.S.
-*  Government have not placed any restriction on its use or reproduction.
-*
-*  Although all reasonable efforts have been taken to ensure the accuracy
-*  and reliability of the software and data, the NLM and the U.S.
-*  Government do not and cannot warrant the performance or results that
-*  may be obtained by using this software or data. The NLM and the U.S.
-*  Government disclaim all warranties, express or implied, including
-*  warranties of performance, merchantability or fitness for any particular
-*  purpose.
-*
-*  Please cite the author in any work or product based on this material.
-*
-* ===========================================================================
-*
-* Author:  Mostly Mike Kornbluh, NCBI.
-*          Customizations by Frank Ludwig, NCBI.
-*
-* File Description:
-*   GFF3 reader unit test.
-*
-* ===========================================================================
-*/
+ * ===========================================================================
+ *
+ *                            PUBLIC DOMAIN NOTICE
+ *               National Center for Biotechnology Information
+ *
+ *  This software/database is a "United States Government Work" under the
+ *  terms of the United States Copyright Act.  It was written as part of
+ *  the author's official duties as a United States Government employee and
+ *  thus cannot be copyrighted.  This software/database is freely available
+ *  to the public for use. The National Library of Medicine and the U.S.
+ *  Government have not placed any restriction on its use or reproduction.
+ *
+ *  Although all reasonable efforts have been taken to ensure the accuracy
+ *  and reliability of the software and data, the NLM and the U.S.
+ *  Government do not and cannot warrant the performance or results that
+ *  may be obtained by using this software or data. The NLM and the U.S.
+ *  Government disclaim all warranties, express or implied, including
+ *  warranties of performance, merchantability or fitness for any particular
+ *  purpose.
+ *
+ *  Please cite the author in any work or product based on this material.
+ *
+ * ===========================================================================
+ *
+ * Author:  Mostly Mike Kornbluh, NCBI.
+ *          Customizations by Frank Ludwig, NCBI.
+ *
+ * File Description:
+ *   GFF3 reader unit test.
+ *
+ * ===========================================================================
+ */
 
 #include <ncbi_pch.hpp>
 
@@ -39,6 +39,8 @@
 #include <corelib/ncbifile.hpp>
 
 #include <objtools/readers/readfeat.hpp>
+#include <objtools/readers/table_filter.hpp>
+
 #include "error_logger.hpp"
 
 #include <cstdio>
@@ -46,8 +48,7 @@
 #include "unit_test_utils.hpp"
 // This header must be included before all Boost.Test headers if there are any
 #include <corelib/test_boost.hpp>
-
-
+#include <sstream>
 USING_NCBI_SCOPE;
 USING_SCOPE(objects);
 
@@ -66,27 +67,26 @@ const string dirTestFiles("5colftblreader_test_cases");
 
 void sUpdateCase(CDir& test_cases_dir, const string& test_name)
 {
-    string input = CDir::ConcatPath( test_cases_dir.GetPath(), test_name + "." + extInput);
-    string output = CDir::ConcatPath( test_cases_dir.GetPath(), test_name + "." + extOutput);
-    string errors = CDir::ConcatPath( test_cases_dir.GetPath(), test_name + "." + extErrors);
-    if (!CFile(input).Exists()) {
-         BOOST_FAIL("input file " << input << " does not exist.");
+    string input  = CDir::ConcatPath(test_cases_dir.GetPath(), test_name + "." + extInput);
+    string output = CDir::ConcatPath(test_cases_dir.GetPath(), test_name + "." + extOutput);
+    string errors = CDir::ConcatPath(test_cases_dir.GetPath(), test_name + "." + extErrors);
+    if (! CFile(input).Exists()) {
+        BOOST_FAIL("input file " << input << " does not exist.");
     }
     cerr << "Creating new test case from " << input << " ..." << endl;
 
-    CErrorLogger logger(errors);
-    CNcbiIfstream ifstr(input.c_str());
-    CRef<ILineReader> pLineReader = ILineReader::New(ifstr);
+    CErrorLogger          logger(errors);
+    CNcbiIfstream         ifstr(input.c_str());
+    CRef<ILineReader>     pLineReader = ILineReader::New(ifstr);
     CFeature_table_reader reader;
 
     list<CRef<CSeq_annot>> annots;
     try {
-        while(!pLineReader->AtEOF()) {
+        while (! pLineReader->AtEOF()) {
             CRef<CSeq_annot> pAnnot = reader.ReadSeqAnnot(*pLineReader, &logger);
             annots.push_back(pAnnot);
         }
-    }
-    catch (...) {
+    } catch (...) {
         ifstr.close();
         BOOST_FAIL("Error: " << input << " failed during conversion.");
     }
@@ -107,10 +107,11 @@ void sUpdateCase(CDir& test_cases_dir, const string& test_name)
 }
 
 
-void sUpdateAll(CDir& test_cases_dir) {
+void sUpdateAll(CDir& test_cases_dir)
+{
 
-    const vector<string> kEmptyStringVec;
-    TTestNameToInfoMap testNameToInfoMap;
+    const vector<string>     kEmptyStringVec;
+    TTestNameToInfoMap       testNameToInfoMap;
     CTestNameToInfoMapLoader testInfoLoader(
         &testNameToInfoMap, extInput, extOutput, extErrors);
     FindFilesInDir(
@@ -118,39 +119,36 @@ void sUpdateAll(CDir& test_cases_dir) {
         kEmptyStringVec,
         kEmptyStringVec,
         testInfoLoader,
-        fFF_Default | fFF_Recursive );
+        fFF_Default | fFF_Recursive);
 
-    ITERATE(TTestNameToInfoMap, name_to_info_it, testNameToInfoMap) {
-        const string & sName = name_to_info_it->first;
+    ITERATE (TTestNameToInfoMap, name_to_info_it, testNameToInfoMap) {
+        const string& sName = name_to_info_it->first;
         sUpdateCase(test_cases_dir, sName);
     }
 }
 
 
-void sRunTest(const string &sTestName, const STestInfo & testInfo, bool keep)
+void sRunTest(const string& sTestName, const STestInfo& testInfo, bool keep)
 {
-    cerr << "Testing " << testInfo.mInFile.GetName() << " against " <<
-        testInfo.mOutFile.GetName() << " and " <<
-        testInfo.mErrorFile.GetName() << endl;
+    cerr << "Testing " << testInfo.mInFile.GetName() << " against " << testInfo.mOutFile.GetName() << " and " << testInfo.mErrorFile.GetName() << endl;
 
-    string logName = CDirEntry::GetTmpName();
+    string       logName = CDirEntry::GetTmpName();
     CErrorLogger logger(logName);
 
 
-    CNcbiIfstream ifstr(testInfo.mInFile.GetPath().c_str());
-    CRef<ILineReader> pLineReader = ILineReader::New(ifstr);
+    CNcbiIfstream         ifstr(testInfo.mInFile.GetPath().c_str());
+    CRef<ILineReader>     pLineReader = ILineReader::New(ifstr);
     CFeature_table_reader reader;
 
-    string resultName = CDirEntry::GetTmpName();
+    string        resultName = CDirEntry::GetTmpName();
     CNcbiOfstream ofstr(resultName.c_str());
 
     try {
-        while(!pLineReader->AtEOF()) {
+        while (! pLineReader->AtEOF()) {
             CRef<CSeq_annot> pAnnot = reader.ReadSeqAnnot(*pLineReader, &logger);
             ofstr << MSerial_AsnText << *pAnnot;
         }
-    }
-    catch (...) {
+    } catch (...) {
         BOOST_ERROR("Error: " << sTestName << " failed during conversion.");
         ifstr.close();
         return;
@@ -160,7 +158,7 @@ void sRunTest(const string &sTestName, const STestInfo & testInfo, bool keep)
 
 
     bool success = testInfo.mOutFile.CompareTextContents(resultName, CFile::eIgnoreWs);
-    if (!success) {
+    if (! success) {
         CDirEntry deResult = CDirEntry(resultName);
         if (keep) {
             deResult.Copy(testInfo.mOutFile.GetPath() + "." + extKeep);
@@ -171,13 +169,13 @@ void sRunTest(const string &sTestName, const STestInfo & testInfo, bool keep)
     }
     CDirEntry(resultName).Remove();
 
-    success = testInfo.mErrorFile.CompareTextContents(logName, CFile::eIgnoreWs);
+    success            = testInfo.mErrorFile.CompareTextContents(logName, CFile::eIgnoreWs);
     CDirEntry deErrors = CDirEntry(logName);
-    if (!success  &&  keep) {
+    if (! success && keep) {
         deErrors.Copy(testInfo.mErrorFile.GetPath() + "." + extKeep);
     }
     deErrors.Remove();
-    if (!success) {
+    if (! success) {
         BOOST_ERROR("Error: " << sTestName << " failed due to error handling diffs.");
     }
 };
@@ -190,20 +188,14 @@ NCBITEST_AUTO_INIT()
 
 NCBITEST_INIT_CMDLINE(arg_descrs)
 {
-    arg_descrs->AddDefaultKey("test-dir", "TEST_FILE_DIRECTORY",
-        "Set the root directory under which all test files can be found.",
-        CArgDescriptions::eDirectory,
-        dirTestFiles );
-    arg_descrs->AddDefaultKey("update-case", "UPDATE_CASE",
-        "Produce .asn and .error files from given name for new or updated test case.",
-        CArgDescriptions::eString,
-        "" );
+    arg_descrs->AddDefaultKey("test-dir", "TEST_FILE_DIRECTORY", "Set the root directory under which all test files can be found.", CArgDescriptions::eDirectory, dirTestFiles);
+    arg_descrs->AddDefaultKey("update-case", "UPDATE_CASE", "Produce .asn and .error files from given name for new or updated test case.", CArgDescriptions::eString, "");
     arg_descrs->AddFlag("update-all",
-        "Update all test cases to current reader code (dangerous).",
-        true );
+                        "Update all test cases to current reader code (dangerous).",
+                        true);
     arg_descrs->AddFlag("keep-diffs",
-        "Keep output files that are different from the expected.",
-        true );
+                        "Keep output files that are different from the expected.",
+                        true);
 }
 
 NCBITEST_AUTO_FINI()
@@ -214,9 +206,9 @@ BOOST_AUTO_TEST_CASE(RunTests)
 {
     const CArgs& args = CNcbiApplication::Instance()->GetArgs();
 
-    CDir test_cases_dir( args["test-dir"].AsDirectory() );
-    BOOST_REQUIRE_MESSAGE( test_cases_dir.IsDir(),
-        "Cannot find dir: " << test_cases_dir.GetPath() );
+    CDir test_cases_dir(args["test-dir"].AsDirectory());
+    BOOST_REQUIRE_MESSAGE(test_cases_dir.IsDir(),
+                          "Cannot find dir: " << test_cases_dir.GetPath());
 
     bool update_all = args["update-all"].AsBoolean();
     if (update_all) {
@@ -225,13 +217,13 @@ BOOST_AUTO_TEST_CASE(RunTests)
     }
 
     string update_case = args["update-case"].AsString();
-    if (!update_case.empty()) {
+    if (! update_case.empty()) {
         sUpdateCase(test_cases_dir, update_case);
         return;
     }
 
-    const vector<string> kEmptyStringVec;
-    TTestNameToInfoMap testNameToInfoMap;
+    const vector<string>     kEmptyStringVec;
+    TTestNameToInfoMap       testNameToInfoMap;
     CTestNameToInfoMapLoader testInfoLoader(
         &testNameToInfoMap, extInput, extOutput, extErrors);
     FindFilesInDir(
@@ -239,25 +231,49 @@ BOOST_AUTO_TEST_CASE(RunTests)
         kEmptyStringVec,
         kEmptyStringVec,
         testInfoLoader,
-        fFF_Default | fFF_Recursive );
+        fFF_Default | fFF_Recursive);
 
-    ITERATE(TTestNameToInfoMap, name_to_info_it, testNameToInfoMap) {
-        const string & sName = name_to_info_it->first;
-        const STestInfo & testInfo = name_to_info_it->second;
+    ITERATE (TTestNameToInfoMap, name_to_info_it, testNameToInfoMap) {
+        const string&    sName    = name_to_info_it->first;
+        const STestInfo& testInfo = name_to_info_it->second;
         cout << "Verifying: " << sName << endl;
-        BOOST_REQUIRE_MESSAGE( testInfo.mInFile.Exists(),
-            extInput + " file does not exist: " << testInfo.mInFile.GetPath() );
-        BOOST_REQUIRE_MESSAGE( testInfo.mOutFile.Exists(),
-            extOutput + " file does not exist: " << testInfo.mOutFile.GetPath() );
-        BOOST_REQUIRE_MESSAGE( testInfo.mErrorFile.Exists(),
-            extErrors + " file does not exist: " << testInfo.mErrorFile.GetPath() );
+        BOOST_REQUIRE_MESSAGE(testInfo.mInFile.Exists(),
+                              extInput + " file does not exist: " << testInfo.mInFile.GetPath());
+        BOOST_REQUIRE_MESSAGE(testInfo.mOutFile.Exists(),
+                              extOutput + " file does not exist: " << testInfo.mOutFile.GetPath());
+        BOOST_REQUIRE_MESSAGE(testInfo.mErrorFile.Exists(),
+                              extErrors + " file does not exist: " << testInfo.mErrorFile.GetPath());
     }
-    ITERATE(TTestNameToInfoMap, name_to_info_it, testNameToInfoMap) {
-        const string & sName = name_to_info_it->first;
-        const STestInfo & testInfo = name_to_info_it->second;
+    ITERATE (TTestNameToInfoMap, name_to_info_it, testNameToInfoMap) {
+        const string&    sName    = name_to_info_it->first;
+        const STestInfo& testInfo = name_to_info_it->second;
 
         cout << "Running test: " << sName << endl;
 
         BOOST_CHECK_NO_THROW(sRunTest(sName, testInfo, args["keep-diffs"]));
     }
+}
+
+
+BOOST_AUTO_TEST_CASE(Test_RW_2238)
+{
+    string input_string{ ">Feature mSeq1\n"
+                         "100 200 bogus_feat\n"
+                         "            note    no such feature" };
+
+    istringstream           istr(input_string);
+    auto                    pLineReader = ILineReader::New(istr);
+    CMessageListenerLenient listener;
+
+
+    auto feat_filter = make_unique<CSimpleTableFilter>(ITableFilter::eAction_Disallowed);
+
+    auto pAnnot = CFeature_table_reader::ReadSequinFeatureTable(*pLineReader, 0, &listener, feat_filter.get());
+
+    BOOST_CHECK_EQUAL(listener.Count(), 1);
+    string expected_msg =
+        "On SeqId 'lcl|mSeq1', line 2, severity Warning: 'Unrecognized feature name', with feature name 'bogus_feat'";
+
+    string msg = listener.Get(0).Message();
+    BOOST_CHECK_EQUAL(msg, expected_msg);
 }
