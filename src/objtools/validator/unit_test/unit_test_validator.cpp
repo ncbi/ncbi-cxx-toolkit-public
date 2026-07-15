cc68f72a002f48a62cd83d4ae6b3092c665ad909
Justin Foley <foleyjp@ncbi.nlm.nih.gov>
2026-07-15 14:27:46 +0000

    JIRA: RW-2721 Disabling Test_FormatBadSpecificHostAlternateName until taxonomy issues have been resolved.
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@106978 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objtools/validator/unit_test/unit_test_validator.cpp
+++ src/objtools/validator/unit_test/unit_test_validator.cpp
@@ -24669,6 +24669,7 @@ BOOST_AUTO_TEST_CASE(Test_NoCDSbetweenUTRs)
     TestUTRPair(true, true);
 }
 
+/* RW-2721 - temporarily disable until taxonomy issues have been resolved
 BOOST_AUTO_TEST_CASE(Test_FormatBadSpecificHostAlternateName)
 {
     CRef<CSeq_entry> entry = BuildGoodSeq();
@@ -24689,6 +24690,7 @@ BOOST_AUTO_TEST_CASE(Test_FormatBadSpecificHostAlternateName)
 
     CLEAR_ERRORS
 }
+*/
 
 BOOST_FIXTURE_TEST_CASE(Test_VR_803, CGenBankFixture)
 {
