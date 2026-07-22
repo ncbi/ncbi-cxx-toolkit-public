6050644e4b69b8ba32c50d7e3cec43a58930ec7a
Amelia Fong <fongah2@ncbi.nlm.nih.gov>
2026-07-22 11:52:50 +0000

    Add CBlastPhoneHomePolicy, JIRA:SB-4670
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@107019 78c7ea69-d796-4a43-9a09-de51944f1b03

--- include/algo/blast/api/blast_usage_report.hpp
+++ include/algo/blast/api/blast_usage_report.hpp
@@ -36,88 +36,215 @@
 
 #include <connect/ncbi_usage_report.hpp>
 #include <algo/blast/core/blast_export.h>
+#include <corelib/phone_home_policy.hpp>
 
 BEGIN_NCBI_SCOPE
 BEGIN_SCOPE(blast)
 
-class NCBI_XBLAST_EXPORT CBlastUsageReport : public CUsageReport
+class NCBI_XBLAST_EXPORT CBlastPhoneHomePolicy : public IPhoneHomePolicy
 {
-
 public:
-	enum EUsageParams {
-		eApp,
-		eVersion,
-		eProgram,
-		eTask,
-		eExitStatus,
-		eRunTime,
-		eDBName,
-		eDBLength,
-		eDBNumSeqs,
-		eDBDate,
-		eBl2seq,
-		eNumSubjects,
-		eSubjectsLength,
-		eNumQueries,
-		eTotalQueryLength,
-		eEvalueThreshold,
-		eNumThreads,
-		eHitListSize,
-		eOutputFmt,
-		eTaxIdList,
-		eNegTaxIdList,
-		eGIList,
-		eNegGIList,
-		eSeqIdList,
-		eNegSeqIdList,
-		eIPGList,
-		eNegIPGList,
-		eMaskAlgo,
-		eCompBasedStats,
-		eRange,
-		eMTMode,
-		eNumQueryBatches,
-		eNumErrStatus,
-		ePSSMInput,
-		eConverged,
-		eArchiveInput,
-		eRIDInput,
-		eDBInfo,
-		eDBTaxInfo,
-		eDBEntry,
-		eDBDumpAll,
-		eDBType,
-		eInputType,
-		eParseSeqIDs,
-		eSeqType,
-		eDBTest,
-		eDBAliasMode,
-		eDocker,
-		eGCP,
-		eAWS,
-		eELBJobId,
-		eELBBatchNum,
-        eSRA,
-        eELBVersion
-	};
+    /// Constructor
+    /// Check phone home configurations
+    CBlastPhoneHomePolicy();
 
-	static const int kNumRetries=0;
-	static const int kTimeout=10;
+    /// Destructor
+    ~CBlastPhoneHomePolicy() = default;
 
-	CBlastUsageReport();
-	~CBlastUsageReport();
-	void AddParam(EUsageParams p, int val);
-	void AddParam(EUsageParams p, const string & val);
-	void AddParam(EUsageParams p, const double & val);
-	void AddParam(EUsageParams p, Int8 val);
-	void AddParam(EUsageParams p, bool val);
+    /// Apply policy for an application.
+    void Apply(CNcbiApplicationAPI* app) {}
+
+    /// Print a message about collecting data, disablig telemetry and privacy policies.
+    void Print();
+
+    /// Save policy configuration to opt-in file
+    void Save();
+
+    /// Restore policy configuration.
+    void Restore();
+
+    /// Automatically called from the destructor.
+    void Finish() {}
+
+    /// @return true if OptInFile exists
+    bool OptInFileExists() const { return m_OptInFileFound; }
+
+    /// Determine if OptInFile exists, update opt-in status if found
+    /// @return TRUE if opt-in file is found
+    bool CheckOptInFileConfiguration();
+
+    /// Check for usage params in env and registry
+    /// @return TRUE if usage setting is found
+    bool CheckBlastUsageConfigurations();
+
+    /// Set usage status based on usage config and ranking priority
+    /// Call after config check or config change functions
+    /// @return TRUE if usage report enabled
+    bool UpdatePhoneHomeStatus();
+
+    /// Check if any usage config (exclude opt-in) exisits
+    bool IsUsageConfigured() const {
+        return (m_DoNotTrackEnv.configured ||
+            m_NCBIUsageReportEnv.configured ||
+            m_BlastUsageReportEnv.configured ||
+            m_NCBIUsageReportRegistry.configured ||
+            m_BlastUsageReportRegistry.configured);
+    }
+
+    /// Usage agreement opt-in/opt-out
+    /// @input TRUE to enable, FALSE to disable
+    void EnableOptIn(bool enable);
+
+    string PhoneHomeStatusReport();
+
+    static const string kDoNotTrackEnv;
+    static const string kUsageReportEnv;
+    static const string kBlastUsageReportEnv;
+    static const string kNCBIUsageReportRegistry;
+    static const string kNCBIUsageReportRegistryParam;
+    static const string kBlastUsageReportRegistry;
+    static const string kBlastUsageReportRegistryParam;
 
 private:
-	void x_CheckBlastUsageEnv();
-	string x_EUsageParmsToString(EUsageParams p);
-	void x_CheckRunEnv();
-	CUsageReportParameters m_Params;
+
+    static const string kConfigFileName;
+    static const string kOptInStr;
+
+    struct SUsageConfig {
+        SUsageConfig(): configured(false), enabled(false), override(false) {}
+        void Set(bool b) {
+            configured = true;
+            enabled = b;
+        }
+        void Reset() {
+            configured = false;
+            enabled = false;
+            override = false;
+        }
+        bool configured;
+        bool enabled;
+        bool override;
+    };
+
+    SUsageConfig m_DoNotTrackEnv;
+    SUsageConfig m_NCBIUsageReportEnv;
+    SUsageConfig m_BlastUsageReportEnv;
+    SUsageConfig m_NCBIUsageReportRegistry;
+    SUsageConfig m_BlastUsageReportRegistry;
+    SUsageConfig m_OptInFile;
+
+    string m_ConfigFilePath;
+    bool m_OptInFileFound;
+
+    /// Helper function to format usage configuration
+    /// @param string stream  output for formatted string
+    /// @usage_type config type
+    /// @config config param to format
+    /// @flip reverse logic true for parameter DO_NOT_TRACK
+    void x_FormatUsage(CNcbiOstrstream & ss, const  string & usage_type,
+                       const SUsageConfig & config, bool flip);
+
+    /// Reset config parms
+    void x_ResetUsageConfigs() {
+        m_DoNotTrackEnv.Reset();
+        m_NCBIUsageReportEnv.Reset();
+        m_BlastUsageReportEnv.Reset();
+        m_NCBIUsageReportRegistry.Reset();
+        m_BlastUsageReportRegistry.Reset();
+    }
+
+    /// Convert string to bool with valiadtaion
+    /// @param input string value to convert to boolean
+    /// @param output converted value, no change if fucntion return false
+    /// @param  usage_str Used in warning message to identify error setting
+    /// @return true if input string is valid and input successfully converted
+    /// @return false for invalid input, CStringException handled and warning message posted
+    bool x_ValidateStringToBool(const string & input, bool & output, const string & usage_type);
 };
+
+
+
+class NCBI_XBLAST_EXPORT CBlastUsageReport : public CUsageReport
+{
+public:
+    enum EUsageParams {
+        eApp,
+        eVersion,
+        eProgram,
+        eTask,
+        eExitStatus,
+        eRunTime,
+        eDBName,
+        eDBLength,
+        eDBNumSeqs,
+        eDBDate,
+        eBl2seq,
+        eNumSubjects,
+        eSubjectsLength,
+        eNumQueries,
+        eTotalQueryLength,
+        eEvalueThreshold,
+        eNumThreads,
+        eHitListSize,
+        eOutputFmt,
+        eTaxIdList,
+        eNegTaxIdList,
+        eGIList,
+        eNegGIList,
+        eSeqIdList,
+        eNegSeqIdList,
+        eIPGList,
+        eNegIPGList,
+        eMaskAlgo,
+        eCompBasedStats,
+        eRange,
+        eMTMode,
+        eNumQueryBatches,
+        eNumErrStatus,
+        ePSSMInput,
+        eConverged,
+        eArchiveInput,
+        eRIDInput,
+        eDBInfo,
+        eDBTaxInfo,
+        eDBEntry,
+        eDBDumpAll,
+        eDBType,
+        eInputType,
+        eParseSeqIDs,
+        eSeqType,
+        eDBTest,
+        eDBAliasMode,
+        eDocker,
+        eGCP,
+        eAWS,
+        eELBJobId,
+        eELBBatchNum,
+        eSRA,
+        eELBVersion
+    };
+
+    static const int kNumRetries=0;
+    static const int kTimeout=10;
+
+    CBlastUsageReport();
+    ~CBlastUsageReport();
+    void AddParam(EUsageParams p, int val);
+    void AddParam(EUsageParams p, const string & val);
+    void AddParam(EUsageParams p, const double & val);
+    void AddParam(EUsageParams p, Int8 val);
+    void AddParam(EUsageParams p, bool val);
+
+    void SetUsageReport(bool enable);
+
+private:
+    void x_CheckRunEnv();
+    string x_EUsageParamsToString(EUsageParams p);
+    CUsageReportParameters m_Params;
+};
+
+
+
 
 END_SCOPE(blast)
 END_NCBI_SCOPE
--- src/algo/blast/api/blast_usage_report.cpp
+++ src/algo/blast/api/blast_usage_report.cpp
@@ -33,6 +33,7 @@
 
 #include <ncbi_pch.hpp>
 #include <algo/blast/api/blast_usage_report.hpp>
+#include <algo/blast/api/blast_exception.hpp>
 #include <corelib/ncbifile.hpp>
 
 #ifndef SKIP_DOXYGEN_PROCESSING
@@ -42,6 +43,17 @@ USING_SCOPE(blast);
 
 static const string kNcbiAppName="standalone-blast";
 static const string kIdFile="/sys/class/dmi/id/sys_vendor";
+const string CBlastPhoneHomePolicy::kConfigFileName = ".blast-usage-report.ini";
+const string CBlastPhoneHomePolicy::kOptInStr = "Enabled";
+
+const string CBlastPhoneHomePolicy::kDoNotTrackEnv = "DO_NOT_TRACK";
+const string CBlastPhoneHomePolicy::kUsageReportEnv = "NCBI_USAGE_REPORT_ENABLED";
+const string CBlastPhoneHomePolicy::kBlastUsageReportEnv = "BLAST_USAGE_REPORT";
+const string CBlastPhoneHomePolicy::kNCBIUsageReportRegistry = "USAGE_REPORT";
+const string CBlastPhoneHomePolicy::kNCBIUsageReportRegistryParam = "Enabled";
+const string CBlastPhoneHomePolicy::kBlastUsageReportRegistry = "BLAST";
+const string CBlastPhoneHomePolicy::kBlastUsageReportRegistryParam = "BLAST_USAGE_REPORT";
+
 
 void CBlastUsageReport::x_CheckRunEnv()
 {
@@ -83,7 +95,20 @@ void CBlastUsageReport::x_CheckRunEnv()
 
 CBlastUsageReport::CBlastUsageReport()
 {
-	x_CheckBlastUsageEnv();
+	SetUsageReport(false);
+	CBlastPhoneHomePolicy phone_home_policy;
+	phone_home_policy.Restore();
+	if (!phone_home_policy.OptInFileExists()) {
+		if (phone_home_policy.IsUsageConfigured()) {
+		    phone_home_policy.EnableOptIn(phone_home_policy.IsEnabled());
+		}
+		else {
+		    phone_home_policy.Print();
+		    phone_home_policy.SetEnabled(false);
+		}
+	}
+	SetUsageReport(phone_home_policy.IsEnabled());
+
 	AddParam(eApp, kNcbiAppName);
 	if (IsEnabled()) {
         CUsageReportAPI::SetRetries(kNumRetries);
@@ -102,7 +127,7 @@ CBlastUsageReport::~CBlastUsageReport()
 	}
 }
 
-string CBlastUsageReport::x_EUsageParmsToString(EUsageParams p)
+string CBlastUsageReport::x_EUsageParamsToString(EUsageParams p)
 {
     string retval;
     switch (p) {
@@ -171,7 +196,7 @@ string CBlastUsageReport::x_EUsageParmsToString(EUsageParams p)
 void CBlastUsageReport::AddParam(EUsageParams p, int val)
 {
 	if (IsEnabled()){
-		string t = x_EUsageParmsToString(p);
+		string t = x_EUsageParamsToString(p);
 		m_Params.Add(t, NStr::IntToString(val));
 	}
 }
@@ -179,7 +204,7 @@ void CBlastUsageReport::AddParam(EUsageParams p, int val)
 void CBlastUsageReport::AddParam(EUsageParams p, const string & val)
 {
 	if (IsEnabled()) {
-		string t = x_EUsageParmsToString(p);
+		string t = x_EUsageParamsToString(p);
 		m_Params.Add(t, val);
 	}
 }
@@ -187,44 +212,21 @@ void CBlastUsageReport::AddParam(EUsageParams p, const string & val)
 void CBlastUsageReport::AddParam(EUsageParams p, const double & val)
 {
 	if (IsEnabled()) {
-		string t = x_EUsageParmsToString(p);
+		string t = x_EUsageParamsToString(p);
 		m_Params.Add(t, val);
 	}
 }
 
-void CBlastUsageReport::x_CheckBlastUsageEnv()
-{
-	char * blast_usage_env = getenv("BLAST_USAGE_REPORT");
-	if(blast_usage_env != NULL){
-		bool enable = NStr::StringToBool(blast_usage_env);
-		if (!enable) {
-			SetEnabled(false);
-			CUsageReportAPI::SetEnabled(false);
-			LOG_POST(Info <<"Phone home disabled");
-			return ;
-		}
-	}
-
-	CNcbiIstrstream empty_stream(kEmptyStr);
-	CRef<CNcbiRegistry> registry(new CNcbiRegistry(empty_stream, IRegistry::fWithNcbirc));
-	if (registry->HasEntry("BLAST", "BLAST_USAGE_REPORT")) {
-		bool enable = NStr::StringToBool(registry->Get("BLAST", "BLAST_USAGE_REPORT"));
-		if (!enable) {
-			SetEnabled(false);
-			CUsageReportAPI::SetEnabled(false);
-			LOG_POST(Info <<"Phone home disabled by config setting");
-			return ;
-		}
-	}
-	CUsageReportAPI::SetEnabled(true);
-	SetEnabled(true);
-	LOG_POST(Info <<"Phone home enabled");
+void CBlastUsageReport::SetUsageReport(bool enable) {
+    SetEnabled(enable);
+    // Global setting, bypass disable for not having DO_NOT_TRACK in env
+    CUsageReportAPI::SetEnabled(enable);
 }
 
 void CBlastUsageReport::AddParam(EUsageParams p, Int8 val)
 {
 	if (IsEnabled()) {
-		string t = x_EUsageParmsToString(p);
+		string t = x_EUsageParamsToString(p);
 		m_Params.Add(t, val);
 	}
 
@@ -233,8 +235,330 @@ void CBlastUsageReport::AddParam(EUsageParams p, Int8 val)
 void CBlastUsageReport::AddParam(EUsageParams p, bool val)
 {
 	if (IsEnabled()) {
-		string t = x_EUsageParmsToString(p);
+		string t = x_EUsageParamsToString(p);
 		m_Params.Add(t, val);
 	}
 
+}
+
+/*****************************************************************************/
+CBlastPhoneHomePolicy::CBlastPhoneHomePolicy():
+m_ConfigFilePath(kEmptyStr), m_OptInFileFound(false)
+{
+    const string home = CDir::GetHome();
+    m_ConfigFilePath = CDirEntry::MakePath(home, kConfigFileName);
+}
+
+bool CBlastPhoneHomePolicy::CheckOptInFileConfiguration() {
+    try {
+        m_OptInFile.Reset();
+	    CFile optInFile (m_ConfigFilePath);
+	    if (optInFile.Exists()){
+	        m_OptInFileFound = true;
+	        CNcbiIfstream is(m_ConfigFilePath, IOS_BASE::in);
+	        string line;
+	        while (getline(is, line)) {
+	            // Skip comment or blank line
+	            string::size_type pos = line.find_first_not_of(" \t");
+	            if ((pos == string::npos) || (line[pos] == '#')) {
+	                continue;
+	            }
+	            if (line.find(kOptInStr) != string::npos) {
+	                string p1, p2;
+	                if (NStr::SplitInTwo(line, "=", p1, p2)) {
+	                    NStr::TruncateSpacesInPlace(p1);
+	                    NStr::TruncateSpacesInPlace(p2);
+	                    bool value = false;
+	                    if (x_ValidateStringToBool(p2, value, kOptInStr)){
+	                    m_OptInFile.Set(value);
+	                    }
+	                    break;
+	                }
+	            }
+	        }
+	    }
+    } catch (CException & e) {
+        m_OptInFile.Reset();
+        LOG_POST(Warning << "Opt-in file read error: " << e.GetMsg());
+    } catch (...){
+        m_OptInFile.Reset();
+        LOG_POST(Warning << "Opt-in file read error: Unknown exception ");
+    }
+	return m_OptInFile.configured;
+}
+
+bool CBlastPhoneHomePolicy::UpdatePhoneHomeStatus()
+{
+    if(m_DoNotTrackEnv.configured) {
+        m_DoNotTrackEnv.override = true;
+        SetEnabled(m_DoNotTrackEnv.enabled);
+    }
+    else if(m_NCBIUsageReportEnv.configured) {
+        m_NCBIUsageReportEnv.override = true;
+        SetEnabled(m_NCBIUsageReportEnv.enabled);
+    }
+    else if(m_BlastUsageReportEnv.configured) {
+        m_BlastUsageReportEnv.override = true;
+        SetEnabled(m_BlastUsageReportEnv.enabled);
+    }
+    else if(m_NCBIUsageReportRegistry.configured) {
+        m_NCBIUsageReportRegistry.override = true;
+        SetEnabled(m_NCBIUsageReportRegistry.enabled);
+    }
+    else if(m_BlastUsageReportRegistry.configured) {
+        m_BlastUsageReportRegistry.override = true;
+        SetEnabled(m_BlastUsageReportRegistry.enabled);
+    }
+    else if(m_OptInFile.configured) {
+        m_OptInFile.override = true;
+        SetEnabled(m_OptInFile.enabled);
+    }
+    else {
+        SetEnabled(false);
+    }
+    return IsEnabled();
+}
+
+void CBlastPhoneHomePolicy::Print()
+{
+	cout << R"DELIM( 
+BLAST+ Usage Reporting and Privacy Notice
+-----------------------------------------
+
+NCBI/NLM would like your permission to collect limited usage data from this application to help improve BLAST+ reliability, performance, and long-term support.
+
+Participation is OPTIONAL.
+
+If you choose to Opt In, BLAST+ will transmit limited technical and usage information to NCBI. No query or subject sequences, database contents, search results, or other biological research data are transmitted.
+
+Data transmitted to NCBI may include:
+- Application name and BLAST program (e.g. blastp, blastn)
+- BLAST+ version
+- Operating system type
+- Runtime duration and exit status
+- Number of query sequences
+- Total query length
+- Selected BLAST parameters and options
+- Database metadata, including:
+    - database name
+    - database creation date
+    - database size
+    - number of sequences
+- Thread count
+- Output format
+- Apparent IP address of the host system
+- Internal NCBI logging fields required for usage data processing
+
+
+How changes to data collection will be communicated:
+Any changes to usage data collection practices or transmitted data elements will be documented in:
+- BLAST+ release notes - https://www.ncbi.nlm.nih.gov/books/NBK131777/
+- BLAST_PRIVACY.md documentation - https://github.com/ncbi/ncbi-cxx-toolkit-public/tree/main/include/algo/blast/BLAST_PRIVACY.md 
+- the BLAST+ User Manual - https://www.ncbi.nlm.nih.gov/books/NBK569851/
+Updated information will also reference the applicable NLM privacy policies.
+
+You may enable or disable usage data sharing at any time by running the utility: blast_usage_report
+
+NLM Web Policies: https://www.nlm.nih.gov/web_policies.html
+
+)DELIM";
+}
+
+void CBlastPhoneHomePolicy::Save()
+{
+    try {
+        const string config_str = kOptInStr + "=" + string(m_OptInFile.enabled? "true" : "false");
+        if (OptInFileExists()) {
+            CNcbiIfstream in_file(m_ConfigFilePath);
+            CNcbiOfstream out_file(m_ConfigFilePath);
+            bool config_found = false;
+            if (!in_file.is_open() || !out_file.is_open()) {
+                NCBI_THROW(CBlastException, eSystem, "Failed to open " + kConfigFileName);
+            }
+            string line;
+            while (getline(in_file, line)) {
+                if (line.empty() || line.starts_with('#')) {
+                    out_file << line << '\n';
+                    continue;
+                }
+	            if (line.find(kOptInStr) != string::npos) {
+	                config_found = true;
+	                out_file << config_str << endl;
+                }
+                else {
+                    out_file << line;
+                }
+            }
+            if (!config_found) {
+                out_file << config_str << endl;
+            }
+        }
+        else {
+            CNcbiOfstream file(m_ConfigFilePath);
+            if (file.is_open()) {
+    	        file << config_str << endl;
+            }
+            else {
+                NCBI_THROW(CBlastException, eSystem, "Failed to open " + kConfigFileName);
+            }
+            m_OptInFileFound = true;
+        }
+        LOG_POST(Info << "Blast Usage Report: " << config_str);
+    } catch (CException & e) {
+        m_OptInFile.Reset();
+        LOG_POST(Warning << "Opt-in file write error: " << e.GetMsg());
+    } catch (...){
+        m_OptInFile.Reset();
+        LOG_POST(Warning << "Opt-in file write error: Unknown exception ");
+    }
+}
+
+bool CBlastPhoneHomePolicy::x_ValidateStringToBool(const string & input, bool & output, const string & usage_str)
+{
+    try {
+        output = NStr::StringToBool(input);
+    }
+    catch (CStringException & e){
+        ERR_POST(Warning << usage_str << " has invalid boolean value.");
+        return false;
+    }
+    return true;
+}
+
+bool CBlastPhoneHomePolicy::CheckBlastUsageConfigurations()
+{
+    x_ResetUsageConfigs();
+    try {
+        CNcbiEnvironment env;
+        string do_not_track_env = env.Get(kDoNotTrackEnv);
+        bool value = false;
+        if(!do_not_track_env.empty()){
+            if (x_ValidateStringToBool(do_not_track_env, value, kDoNotTrackEnv)){
+                m_DoNotTrackEnv.Set(!value);
+            }
+            //m_DoNotTrackEnv.Set(NStr::StringToBool(do_not_track_env));
+        }
+        string usage_report_env = env.Get(kUsageReportEnv);
+        if(!usage_report_env.empty() ){
+            bool value = false;
+            if (x_ValidateStringToBool(usage_report_env, value, kUsageReportEnv)){
+                m_NCBIUsageReportEnv.Set(value);
+            }
+        }
+        string blast_usage_env = env.Get(kBlastUsageReportEnv);
+        if(!blast_usage_env.empty()){
+            bool value = false;
+            if (x_ValidateStringToBool(blast_usage_env, value, kBlastUsageReportEnv)){
+                m_BlastUsageReportEnv.Set(value);
+            }
+        }
+
+        CNcbiIstrstream empty_stream(kEmptyStr);
+        CRef<CNcbiRegistry> registry(new CNcbiRegistry(empty_stream, IRegistry::fWithNcbirc));
+        if (registry->HasEntry(kNCBIUsageReportRegistry, kNCBIUsageReportRegistryParam)) {
+            bool value = false;
+            if (x_ValidateStringToBool(registry->Get(kNCBIUsageReportRegistry, kNCBIUsageReportRegistryParam),
+                                       value, kNCBIUsageReportRegistry)){
+                m_NCBIUsageReportRegistry.Set(value);
+            }
+        }
+        if (registry->HasEntry(kBlastUsageReportRegistry, kBlastUsageReportRegistryParam)) {
+            bool value = false;
+            if (x_ValidateStringToBool(registry->Get(kBlastUsageReportRegistry, kBlastUsageReportRegistryParam),
+                                       value, kBlastUsageReportRegistryParam)) {
+                m_BlastUsageReportRegistry.Set(value);
+            }
+        }
+
+    } catch(CException & e) {
+        // This catches CStringException as well
+        x_ResetUsageConfigs();
+        ERR_POST(Warning << "Usage configuration error: " << e.GetMsg());
+
+    }catch (...) {
+        x_ResetUsageConfigs();
+        LOG_POST(Warning << "Usage configuration unknown error");
+
+    }
+
+    return IsUsageConfigured();
+}
+
+void CBlastPhoneHomePolicy::Restore()
+{
+    CheckBlastUsageConfigurations();
+    CheckOptInFileConfiguration();
+    UpdatePhoneHomeStatus();
+};
+
+void CBlastPhoneHomePolicy::EnableOptIn(bool enable)
+{
+    m_OptInFile.Set(enable);
+    Save();
+    UpdatePhoneHomeStatus();
+}
+
+void CBlastPhoneHomePolicy::x_FormatUsage(CNcbiOstrstream & ss, const  string & usage_type, const SUsageConfig & config, bool flip)
+{
+    const int w = 30;
+    const int t = 4;
+
+    if (config.configured) {
+        string s_str = kEmptyStr;
+        if (flip) {
+            s_str = config.enabled? "False" : "True";
+        }
+        else {
+            s_str = config.enabled? "True" : "False";
+        }
+        if (config.override) {
+            s_str += "*";
+        }
+        ss << string(t, ' ') << std::left << std::setw(w) << usage_type << s_str << endl;
+    }
+}
+
+string CBlastPhoneHomePolicy::PhoneHomeStatusReport()
+{
+    Restore();
+
+    string s_str = IsEnabled()? "Enabled" : "Disabled";
+    CNcbiOstrstream ss;
+    ss << "BLAST Usage Report : " << s_str;
+    if (!IsUsageConfigured() && !OptInFileExists()) {
+        ss << " by default.";
+    }
+
+    ss << "\n\nConfiguration source(s):" << endl;
+    ss << "  BLAST Usage Report ini file:" << endl;
+    if(m_OptInFile.configured) {
+        x_FormatUsage(ss, "Opt-in", m_OptInFile, false);
+    }
+    else {
+        ss << "    File not found. Use -on or -off to configure usage reporting." << endl;
+    }
+
+    if(m_DoNotTrackEnv.configured ||
+       m_NCBIUsageReportEnv.configured ||
+       m_BlastUsageReportEnv.configured)
+    {
+        ss << "\n  Environment Variable: " << endl;
+        x_FormatUsage(ss, kDoNotTrackEnv, m_DoNotTrackEnv, true);
+        x_FormatUsage(ss, kUsageReportEnv, m_NCBIUsageReportEnv, false);
+        x_FormatUsage(ss, kBlastUsageReportEnv, m_BlastUsageReportEnv, false);
+    }
+
+    if (m_NCBIUsageReportRegistry.configured ||
+        m_BlastUsageReportRegistry.configured) {
+        ss << "\n  NCBI Registry File: " << endl;
+        const string registry_usage_report = "[" + kNCBIUsageReportRegistry + "] " + kNCBIUsageReportRegistryParam;
+        const string registry_blast_usage_report = "[" + kBlastUsageReportRegistry + "] " + kBlastUsageReportRegistryParam;
+        x_FormatUsage(ss, registry_usage_report, m_NCBIUsageReportRegistry, false);
+        x_FormatUsage(ss, registry_blast_usage_report, m_BlastUsageReportRegistry, false);
+    }
+
+    if (IsUsageConfigured() || OptInFileExists()) {
+        ss << "\n* Marks the setting that determines the current On/Off state." << endl;
+    }
+    return ss.str();
 }
