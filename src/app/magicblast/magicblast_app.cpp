149abf1c36d800d16f88de70879cb1cfd5087cd0
blastadm <blastadm@ncbi.nlm.nih.gov>
2026-07-22 15:33:21 +0000

    Update magicblast version to 1.8.0 JIRA SB-4661. AUTHOR: boratyng MERGE_REQUEST: 1 cb319e8f896739fbc4e3fc0551a6d44bf96a5a3f
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@107025 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/app/magicblast/magicblast_app.cpp
+++ src/app/magicblast/magicblast_app.cpp
@@ -58,8 +58,8 @@ USING_SCOPE(objects);
 #endif
 
 static const int   kMajorVersion = 1;
-static const int   kMinorVersion = 7;
-static const int   kPatchVersion = 2;
+static const int   kMinorVersion = 8;
+static const int   kPatchVersion = 0;
 
 class CMagicBlastVersion : public CVersionInfo
 {
