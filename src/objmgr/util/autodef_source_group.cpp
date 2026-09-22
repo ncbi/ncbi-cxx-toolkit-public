8b4ae73e973709028491950e3e1398fd2275898b
Vladimir Ivanov <ivanov@ncbi.nlm.nih.gov>
2026-09-22 12:42:55 +0000

    Drop unnecessary namespace before string::npos. JIRA:CXX-14550
    git-svn-id: https://anonsvn.ncbi.nlm.nih.gov/repos/v1/trunk/c++@107487 78c7ea69-d796-4a43-9a09-de51944f1b03

--- src/objmgr/util/autodef_source_group.cpp
+++ src/objmgr/util/autodef_source_group.cpp
@@ -326,7 +326,7 @@ bool IsSpName (string taxname)
 {
     bool is_sp_name = false;
     string::size_type pos = NStr::Find(taxname, " sp.");
-    if (pos != NCBI_NS_STD::string::npos
+    if (pos != string::npos
         && (pos < 2 || !NStr::StartsWith(taxname.substr(pos - 2), "f."))) {
         is_sp_name = true;
     }
